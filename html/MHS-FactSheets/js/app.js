4d7e6691ff9d4c4ef7109954d5897d12336f336c
mvenkataraman <mvenkataraman@deloitte.com>
2014-03-05 16:26:47 +0000

    cleaning up JS files
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@657 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f5a2b95e95d403fbd1b42ffb0233606ef4011784

--- html/MHS-FactSheets/js/app.js
+++ html/MHS-FactSheets/js/app.js
@@ -1,34 +1,5 @@
 'use strict';
 
-// Declare app level module which depends on filters, and services
-var app = angular.module('app', []);
-
-app.controller('IndexCtrl', ['$scope', '$http', function($scope, $http) {
-
-    /* $http.get("data2.json").success(function(jsonData) {
-        $scope.data = jsonData;
-    }); */
-
-	$scope.setJSONData = function (data) {
-		
-        $scope.$apply(function () {
-            
-            $scope.data = jQuery.parseJSON(data);
-
-        });
-
-    }; 
-
-}]);
-
-app.filter('replaceUnderscores', function() {
-    // Takes the string after the last slash and replaces underscores with spaces
-    return function(str) {
-        str = String(str);
-        var myRe = new RegExp("([^/]*)$");
-        var shortStr = myRe.exec(str);
-        var result = shortStr[0].replace(/_/g, " ");
-        
-        return result;
-    }
-});
+var app = angular.module('app', ['app.controllers',
+                                'app.filters'
+                                ]);
