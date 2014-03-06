2471b6288cb98d279a277cc8357bbd8e3838f148
ksmart <ksmart@deloitte.com>
2014-03-06 16:41:13 +0000

    removing unused methods
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@672 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ui/components/specific/tap/CapabilityFactSheet.java
+++ src/prerna/ui/components/specific/tap/CapabilityFactSheet.java
@@ -52,10 +52,10 @@ import com.teamdev.jxbrowser.events.NavigationFinishedEvent;
 import com.teamdev.jxbrowser.events.NavigationListener;
 import com.teamdev.jxbrowser.dom.DOMDocument;
 
-import org.apache.fop.apps.FOUserAgent;
-import org.apache.fop.apps.Fop;
-import org.apache.fop.apps.FopFactory;
-import org.apache.fop.apps.MimeConstants;
+//import org.apache.fop.apps.FOUserAgent;
+//import org.apache.fop.apps.Fop;
+//import org.apache.fop.apps.FopFactory;
+//import org.apache.fop.apps.MimeConstants;
 
 import prerna.rdf.engine.impl.SesameJenaSelectWrapper;
 import prerna.ui.components.playsheets.BrowserPlaySheet;
@@ -109,155 +109,155 @@ public class CapabilityFactSheet extends BrowserPlaySheet{
 	//		createPDF();
 	}
 	
-
-	
-    public void convertDOM2PDF(Document xslfoDoc, File pdf) {
-        // configure fopFactory as desired
-    	FopFactory fopFactory = FopFactory.newInstance();
-    	
-        try {
-            FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
-            // configure foUserAgent as desired
-
-            // Setup output
-            OutputStream out = new java.io.FileOutputStream(pdf);
-            out = new java.io.BufferedOutputStream(out);
-
-            try {
-                // Construct fop with desired output format and output stream
-                Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
-
-                // Setup Identity Transformer
-                TransformerFactory factory = TransformerFactory.newInstance();
-                //Transformer transformer = factory.newTransformer(); // identity transformer
-                Transformer transformer = factory.newTransformer(); // identity transformer
-
-                // Setup input for XSLT transformation
-                Source src = new DOMSource(xslfoDoc);
-
-                // Resulting SAX events (the generated FO) must be piped through to FOP
-                Result res = new SAXResult(fop.getDefaultHandler());
-
-                // Start XSLT transformation and FOP processing
-                transformer.transform(src, res);
-            } finally {
-                out.close();
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
-            System.exit(-1);
-        }
-
-    }
-
-	
-    
-    
-	public void createPDF()
-	{
-        DOMDocument domDocument = (DOMDocument)browser.getDocument();
-        Document document = browser.getDocument();
-        NodeList children = document.getChildNodes();
-        DOMImplementation domImple = domDocument.getImplementation();
-        
-        String workingDir = System.getProperty("user.dir");
-        String filePath =  workingDir + "/export";
-        File baseDir = new File(filePath);
-        File outDir = new File(baseDir, "out");
-
-        outDir.mkdirs();
-
-        //Setup output file
-        File pdffile = new File(outDir, "ResultDOM2PDF.pdf");
-        System.out.println("PDF Output File: " + pdffile);
-        System.out.println();
-
-        Document foDoc;
-        Document foDoc2;
-		try {
-			foDoc = buildDOMDocument();
-			//foDoc2 = (Document)(browser.getDocument().getImplementation());
-
-			for(int i=0;i<children.getLength();i++)
-			{
-				foDoc.adoptNode(children.item(i));
-			}
-	        convertDOM2PDF(foDoc, pdffile);
-
-	        System.out.println("Success!"); 
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-   
-        
-	}
-	
-    /** xsl-fo namespace URI */
-    protected static String foNS = "http://www.w3.org/1999/XSL/Format";
-
-	
-    /**
-     * Builds the example FO document as a DOM in memory.
-     * @return the FO document
-     * @throws ParserConfigurationException In case there is a problem creating a DOM document
-     */
-    private static Document buildDOMDocument() throws ParserConfigurationException {
-        // Create a sample XSL-FO DOM document
-    	
-        Document foDoc = null;
-        Element root = null, ele1 = null, ele2 = null, ele3 = null;
-
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        dbf.setNamespaceAware(true);
-        DocumentBuilder db = dbf.newDocumentBuilder();
-        foDoc = db.newDocument();
-
-        root = foDoc.createElementNS(foNS, "fo:root");
-        foDoc.appendChild(root);
-
-        ele1 = foDoc.createElementNS(foNS, "fo:layout-master-set");
-        root.appendChild(ele1);
-        ele2 = foDoc.createElementNS(foNS, "fo:simple-page-master");
-        ele1.appendChild(ele2);
-        ele2.setAttributeNS(null, "master-name", "letter");
-        ele2.setAttributeNS(null, "page-height", "11in");
-        ele2.setAttributeNS(null, "page-width", "8.5in");
-        ele2.setAttributeNS(null, "margin-top", "1in");
-        ele2.setAttributeNS(null, "margin-bottom", "1in");
-        ele2.setAttributeNS(null, "margin-left", "1in");
-        ele2.setAttributeNS(null, "margin-right", "1in");
-        ele3 = foDoc.createElementNS(foNS, "fo:region-body");
-        ele2.appendChild(ele3);
-        ele1 = foDoc.createElementNS(foNS, "fo:page-sequence");
-        root.appendChild(ele1);
-        ele1.setAttributeNS(null, "master-reference", "letter");
-        ele2 = foDoc.createElementNS(foNS, "fo:flow");
-        ele1.appendChild(ele2);
-        ele2.setAttributeNS(null, "flow-name", "xsl-region-body");
-        addElement(ele2, "fo:block", "Hello World!");
-        return foDoc;
-    }
-
-    /**
-     * Adds an element to the DOM.
-     * @param parent parent node to attach the new element to
-     * @param newNodeName name of the new node
-     * @param textVal content of the element
-     */
-    protected static void addElement(Node parent, String newNodeName,
-                                String textVal) {
-        if (textVal == null) {
-            return;
-        }  // use only with text nodes
-        Element newElement = parent.getOwnerDocument().createElementNS(
-                                        foNS, newNodeName);
-        Text elementText = parent.getOwnerDocument().createTextNode(textVal);
-        newElement.appendChild(elementText);
-        parent.appendChild(newElement);
-    }
-
+//
+//	
+//    public void convertDOM2PDF(Document xslfoDoc, File pdf) {
+//        // configure fopFactory as desired
+//    	FopFactory fopFactory = FopFactory.newInstance();
+//    	
+//        try {
+//            FOUserAgent foUserAgent = fopFactory.newFOUserAgent();
+//            // configure foUserAgent as desired
+//
+//            // Setup output
+//            OutputStream out = new java.io.FileOutputStream(pdf);
+//            out = new java.io.BufferedOutputStream(out);
+//
+//            try {
+//                // Construct fop with desired output format and output stream
+//                Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, foUserAgent, out);
+//
+//                // Setup Identity Transformer
+//                TransformerFactory factory = TransformerFactory.newInstance();
+//                //Transformer transformer = factory.newTransformer(); // identity transformer
+//                Transformer transformer = factory.newTransformer(); // identity transformer
+//
+//                // Setup input for XSLT transformation
+//                Source src = new DOMSource(xslfoDoc);
+//
+//                // Resulting SAX events (the generated FO) must be piped through to FOP
+//                Result res = new SAXResult(fop.getDefaultHandler());
+//
+//                // Start XSLT transformation and FOP processing
+//                transformer.transform(src, res);
+//            } finally {
+//                out.close();
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace(System.err);
+//            System.exit(-1);
+//        }
+//
+//    }
+//
+//	
+//    
+//    
+//	public void createPDF()
+//	{
+//        DOMDocument domDocument = (DOMDocument)browser.getDocument();
+//        Document document = browser.getDocument();
+//        NodeList children = document.getChildNodes();
+//        DOMImplementation domImple = domDocument.getImplementation();
+//        
+//        String workingDir = System.getProperty("user.dir");
+//        String filePath =  workingDir + "/export";
+//        File baseDir = new File(filePath);
+//        File outDir = new File(baseDir, "out");
+//
+//        outDir.mkdirs();
+//
+//        //Setup output file
+//        File pdffile = new File(outDir, "ResultDOM2PDF.pdf");
+//        System.out.println("PDF Output File: " + pdffile);
+//        System.out.println();
+//
+//        Document foDoc;
+//        Document foDoc2;
+//		try {
+//			foDoc = buildDOMDocument();
+//			//foDoc2 = (Document)(browser.getDocument().getImplementation());
+//
+//			for(int i=0;i<children.getLength();i++)
+//			{
+//				foDoc.adoptNode(children.item(i));
+//			}
+//	        convertDOM2PDF(foDoc, pdffile);
+//
+//	        System.out.println("Success!"); 
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//   
+//        
+//	}
+//	
+//    /** xsl-fo namespace URI */
+//    protected static String foNS = "http://www.w3.org/1999/XSL/Format";
+//
+//	
+//    /**
+//     * Builds the example FO document as a DOM in memory.
+//     * @return the FO document
+//     * @throws ParserConfigurationException In case there is a problem creating a DOM document
+//     */
+//    private static Document buildDOMDocument() throws ParserConfigurationException {
+//        // Create a sample XSL-FO DOM document
+//    	
+//        Document foDoc = null;
+//        Element root = null, ele1 = null, ele2 = null, ele3 = null;
+//
+//        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+//        dbf.setNamespaceAware(true);
+//        DocumentBuilder db = dbf.newDocumentBuilder();
+//        foDoc = db.newDocument();
+//
+//        root = foDoc.createElementNS(foNS, "fo:root");
+//        foDoc.appendChild(root);
+//
+//        ele1 = foDoc.createElementNS(foNS, "fo:layout-master-set");
+//        root.appendChild(ele1);
+//        ele2 = foDoc.createElementNS(foNS, "fo:simple-page-master");
+//        ele1.appendChild(ele2);
+//        ele2.setAttributeNS(null, "master-name", "letter");
+//        ele2.setAttributeNS(null, "page-height", "11in");
+//        ele2.setAttributeNS(null, "page-width", "8.5in");
+//        ele2.setAttributeNS(null, "margin-top", "1in");
+//        ele2.setAttributeNS(null, "margin-bottom", "1in");
+//        ele2.setAttributeNS(null, "margin-left", "1in");
+//        ele2.setAttributeNS(null, "margin-right", "1in");
+//        ele3 = foDoc.createElementNS(foNS, "fo:region-body");
+//        ele2.appendChild(ele3);
+//        ele1 = foDoc.createElementNS(foNS, "fo:page-sequence");
+//        root.appendChild(ele1);
+//        ele1.setAttributeNS(null, "master-reference", "letter");
+//        ele2 = foDoc.createElementNS(foNS, "fo:flow");
+//        ele1.appendChild(ele2);
+//        ele2.setAttributeNS(null, "flow-name", "xsl-region-body");
+//        addElement(ele2, "fo:block", "Hello World!");
+//        return foDoc;
+//    }
+//
+//    /**
+//     * Adds an element to the DOM.
+//     * @param parent parent node to attach the new element to
+//     * @param newNodeName name of the new node
+//     * @param textVal content of the element
+//     */
+//    protected static void addElement(Node parent, String newNodeName,
+//                                String textVal) {
+//        if (textVal == null) {
+//            return;
+//        }  // use only with text nodes
+//        Element newElement = parent.getOwnerDocument().createElementNS(
+//                                        foNS, newNodeName);
+//        Text elementText = parent.getOwnerDocument().createTextNode(textVal);
+//        newElement.appendChild(elementText);
+//        parent.appendChild(newElement);
+//    }
+//
 	
 	/**
 	 * Method processQueryData.  Processes the data from the SPARQL query into an appropriate format for the specific play sheet.
