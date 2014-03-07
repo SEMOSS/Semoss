4e4f16b98602c5be47b706912c54bf5c0e305477
mvenkataraman <mvenkataraman@deloitte.com>
2014-03-07 15:14:27 +0000

    internal links
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@695 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- html/MHS-FactSheets/js/controllers.js
+++ html/MHS-FactSheets/js/controllers.js
@@ -2,12 +2,17 @@
 
 var controllers = angular.module('app.controllers', [])
 
-	controllers.controller('IndexCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
+	controllers.controller('IndexCtrl', ['$scope', '$http', '$location', '$anchorScroll', function($scope, $http, $location, $anchorScroll) {
 
     	// Uncomment this part of the code to test JSON data locally
     	/* $http.get("capabilitylist.json").success(function(jsonData) {
         	$scope.list = jsonData;
     	}); */
+
+        $scope.scrollTo = function(id) {
+            $location.hash(id);
+            $anchorScroll();
+        }
 
 		$scope.setJSONData = function (data) {
         	$scope.$apply(function () {
