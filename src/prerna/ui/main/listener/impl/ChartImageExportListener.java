7ef2269b49e14a42bec0329ca22bc2d9c6ee38e0
karverma <karverma@deloitte.com>
2014-03-11 13:41:59 +0000

    Adding functionality to export charts in PDF format
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@737 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ui/main/listener/impl/ChartImageExportListener.java
+++ src/prerna/ui/main/listener/impl/ChartImageExportListener.java
@@ -42,6 +42,9 @@ import prerna.ui.components.api.IChakraListener;
 import prerna.ui.components.playsheets.BrowserPlaySheet;
 import prerna.util.Utility;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.teamdev.jxbrowser.Browser;
 
 /**
@@ -59,6 +62,11 @@ public class ChartImageExportListener extends AbstractAction implements IChakraL
 	int cropHeight = -1;
 	int scaleWidth = -1;
 	int scaleHeight = -1;
+
+	final double MAX_DIM = 14400;
+	final String HIGH_QUALITY_IMAGE_TYPE = "EPS";
+	final String LOW_QUALITY_IMAGE_TYPE = "PNG";
+	final String PDF_TYPE = "PDF";
 	
 	/**
 	 * Method actionPerformed.  Dictates what actions to take when an Action Event is performed.
@@ -73,17 +81,23 @@ public class ChartImageExportListener extends AbstractAction implements IChakraL
 		Image i = browser.toImage(true);
 		
 		try {
-			boolean highQuality = false;
+			String exportType = "";
 			if(!autoExport)
 			{
 				//Display dialog to choose export quality
-				Object[] options = {"High Quality (*.eps)", "Low Quality (*.png)"};
+				Object[] options = {"High Quality (*.eps)", "Low Quality (*.png)", "PDF"};
 				int n = JOptionPane.showOptionDialog(splitPane,"Please choose the type of export: ",
 						"Export", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
-				if(n == -1) {
-					return;
+				
+				switch(n) {
+					case -1: return;
+					case 0: exportType = this.HIGH_QUALITY_IMAGE_TYPE;
+										 break;
+					case 1: exportType = this.LOW_QUALITY_IMAGE_TYPE;
+										 break;
+					case 2: exportType = this.PDF_TYPE;
+										 break;
 				}
-				highQuality = n == 0 ? true : false;
 			}	
 			
 			if(fileLoc.isEmpty())
@@ -91,7 +105,7 @@ public class ChartImageExportListener extends AbstractAction implements IChakraL
 				String workingDir = System.getProperty("user.dir");
 				String folder = "\\export\\Images\\";
 				String writeFileName = "Graph_Export_" + DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT).format(new Date()).replace(":", "");
-				if(highQuality) {
+				if(exportType.equals(this.HIGH_QUALITY_IMAGE_TYPE)) {
 					writeFileName += ".eps";
 				} else {
 					writeFileName += ".png";
@@ -114,7 +128,7 @@ public class ChartImageExportListener extends AbstractAction implements IChakraL
 			
 	        BufferedImage dest;
 	        //Export chart based upon user-chosen quality value
-			if(highQuality) {
+			if(exportType.equals(this.HIGH_QUALITY_IMAGE_TYPE)) {
 				Graphics2D g = new EpsGraphics2D("Chart Export", new FileOutputStream(fileLoc), 0, 0, imageWidth, imageHeight);
 				g.drawImage(i, 0, 0, null);
 			} else {
@@ -133,9 +147,63 @@ public class ChartImageExportListener extends AbstractAction implements IChakraL
 		        if(crop)
 		        	dest = dest.getSubimage(cropX,cropY,cropWidth,cropHeight);
 	        	ImageIO.write(dest, "PNG", new File(fileLoc));
+	        	
+	        	// PDF Export
+	        	if(exportType.equals(this.PDF_TYPE)) {
+					try {
+						com.itextpdf.text.Image image1 = com.itextpdf.text.Image.getInstance(fileLoc);
+						Rectangle r;
+						if(image1.getHeight() > this.MAX_DIM) {
+							r = new Rectangle((int)image1.getWidth(), (int) MAX_DIM);
+						} else if(image1.getWidth() > this.MAX_DIM) {
+							r = new Rectangle((int) MAX_DIM, (int)image1.getHeight());
+						} else {
+							r = new Rectangle((int)image1.getWidth()+20, (int)image1.getHeight()+20);
+						}
+
+						Document document = new Document(r, 15, 25, 15, 25);
+						PdfWriter.getInstance(document, new FileOutputStream(fileLoc.replace(this.LOW_QUALITY_IMAGE_TYPE.toLowerCase(), this.PDF_TYPE.toLowerCase())));
+						document.open();
+
+						int pages = (int) Math.ceil((double)dest.getHeight() / this.MAX_DIM);
+						System.out.println(dest.getHeight());
+						System.out.println(dest.getHeight()/this.MAX_DIM);
+						System.out.println(Math.ceil(dest.getHeight() / this.MAX_DIM));
+						if(pages == 0)
+							pages = 1;
+						for(int j = 0; j < pages; j++) {
+							BufferedImage temp;
+							if(j < pages-1) {
+								temp = dest.getSubimage(0, j*(int)this.MAX_DIM, dest.getWidth(), (int)this.MAX_DIM);
+							} else {
+								temp = dest.getSubimage(0, j*(int)this.MAX_DIM, dest.getWidth(), dest.getHeight() % (int)this.MAX_DIM);
+							}
+							File tempFile = new File(i+this.LOW_QUALITY_IMAGE_TYPE);
+							ImageIO.write(temp, this.LOW_QUALITY_IMAGE_TYPE, tempFile);
+							com.itextpdf.text.Image croppedImage = com.itextpdf.text.Image.getInstance(i+this.LOW_QUALITY_IMAGE_TYPE);
+							document.add(croppedImage);
+							tempFile.delete();
+							
+							if(j < pages-1) {
+								document.newPage();
+							}
+						}
+						document.close();
+
+						File f = new File(fileLoc);
+						f.delete();
+					} catch(Exception e) {
+						e.printStackTrace();
+					}
+				} // end if PDF
 			}
-	        if(!autoExport)
-	        	Utility.showMessage("Graph export successful: " + fileLoc);
+	        if(!autoExport) {
+	        	if(exportType.equals(this.PDF_TYPE)) {
+					Utility.showMessage("Export successful: " + fileLoc.replace(this.LOW_QUALITY_IMAGE_TYPE.toLowerCase(), this.PDF_TYPE.toLowerCase()));
+				} else {
+					Utility.showMessage("Export successful: " + fileLoc);
+				}
+	        }
 		} catch (IOException e) {
 			Utility.showError("Graph export failed.");
 			e.printStackTrace();
