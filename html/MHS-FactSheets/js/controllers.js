df4fd8598ca59a2e15aaa28366611a668bf4d821
karverma <karverma@deloitte.com>
2014-03-11 22:32:30 +0000

    Adding functionality to have Capability Fact Sheet open in desktop browser when selected from list - must use Firefox, as it needs to use Angular to grab the json written locally
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@764 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 59e76b8835c769818dfa5627497885e47b2139c9

--- html/MHS-FactSheets/js/controllers.js
+++ html/MHS-FactSheets/js/controllers.js
@@ -5,9 +5,13 @@ var controllers = angular.module('app.controllers', [])
 	controllers.controller('IndexCtrl', ['$scope', '$http', '$location', '$anchorScroll', function($scope, $http, $location, $anchorScroll) {
 
     	// Uncomment this part of the code to test JSON data locally
-    	/* $http.get("capabilitylist.json").success(function(jsonData) {
+    	$http.get("capabilitylist.json").success(function(jsonData) {
         	$scope.list = jsonData;
-    	}); */
+    	});
+    	
+    	$http.get("export.json").success(function(jsonData) {
+        	$scope.data = (jsonData);
+    	});
 
         $scope.scrollTo = function(id) {
             $location.hash(id);
@@ -22,7 +26,6 @@ var controllers = angular.module('app.controllers', [])
 
         $scope.getFactSheet = function(capName) {
             $scope.data = jQuery.parseJSON(singleCapFactSheet(capName));
-
             /* generate data for pie charts
              $scope.pieChartData = [
                 {"key":"Create",
