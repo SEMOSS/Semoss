cafb8ac2d79981a5f3b905ea2594bf57c75af6cc
mahkhalil <mahkhalil@deloitte.com>
2014-03-13 19:28:33 +0000

    Helper for fact sheet and capability sheet processing
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@792 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6d94d619e309dacfc8debb48929c38d184012aad

---
+++ src/prerna/ui/components/specific/tap/DHMSMHelper.java
@@ -0,0 +1,131 @@
+package prerna.ui.components.specific.tap;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import prerna.rdf.engine.api.IEngine;
+import prerna.rdf.engine.impl.SesameJenaSelectStatement;
+import prerna.rdf.engine.impl.SesameJenaSelectWrapper;
+import prerna.util.DIHelper;
+
+public class DHMSMHelper {
+
+	private String GET_ALL_SYSTEM_WITH_CREATE_AND_DOWNSTREAM_QUERY = "SELECT DISTINCT ?system ?data ?crm WHERE { filter( !regex(str(?crm),\"R\")) {?system <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/System> } {?icd <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/InterfaceControlDocument> } {?data <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/DataObject>} {?provideData <http://www.w3.org/2000/01/rdf-schema#subPropertyOf> <http://semoss.org/ontologies/Relation/Provide>} {?system ?provideData ?data} {?provideData <http://semoss.org/ontologies/Relation/Contains/CRM> ?crm} {?system <http://semoss.org/ontologies/Relation/Provide> ?icd} {?icd <http://semoss.org/ontologies/Relation/Payload> ?data} }";
+
+	private String GET_ALL_SYSTEM_WITH_DOWNSTREAM_AND_NO_UPSTREAM = "SELECT DISTINCT ?System ?Data ?CRM WHERE { BIND(\"C\" as ?CRM) {?Data <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/DataObject>;}{?System <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/ActiveSystem> ;}{?icd <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/InterfaceControlDocument> ;}OPTIONAL{{?icd2 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/InterfaceControlDocument> ;}{?icd2 <http://semoss.org/ontologies/Relation/Consume> ?System}{?icd2 <http://semoss.org/ontologies/Relation/Payload> ?Data}}{?System <http://semoss.org/ontologies/Relation/Provide> ?icd ;} {?icd <http://semoss.org/ontologies/Relation/Payload> ?Data ;}FILTER(!BOUND(?icd2)) } ORDER BY ?System";	
+
+	private String GET_ALL_SYSTEM_WITH_UPSTREAM = "SELECT DISTINCT ?system ?data ?crm WHERE { BIND(\"C\" as ?crm) FILTER NOT EXISTS{?icd <http://semoss.org/ontologies/Relation/Consume> ?system} {?system <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/System> } {?icd <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/InterfaceControlDocument> } {?data <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/DataObject>} {?system <http://semoss.org/ontologies/Relation/Provide> ?data} {?system <http://semoss.org/ontologies/Relation/Provide> ?icd} {?icd <http://semoss.org/ontologies/Relation/Payload> ?data} }";
+
+	private String GET_ALL_CAPABILITIES_AND_CRM = "SELECT DISTINCT ?cap ?data ?crm WHERE { BIND(<http://health.mil/ontologies/Concept/DHMSM/DHMSM> as ?dhmsm) {?cap <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/Capability> } {?task <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/Task>} {?data <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/DataObject>} {?dhmsm <http://semoss.org/ontologies/Relation/TaggedBy> ?cap} {?cap <http://semoss.org/ontologies/Relation/Consists> ?task} {?needs <http://www.w3.org/2000/01/rdf-schema#subPropertyOf> <http://semoss.org/ontologies/Relation/Needs>} {?task ?needs ?data} {?needs <http://semoss.org/ontologies/Relation/Contains/CRM> ?crm} }";
+
+	private Hashtable<String, Hashtable<String, String>> dataListSystems = new Hashtable<String, Hashtable<String, String>>();
+	private Hashtable<String, Hashtable<String, String>> dataListCapabilities = new Hashtable<String, Hashtable<String, String>>();
+
+
+	public void runData()
+	{
+		String engineName = "HR_Core";
+		IEngine engine = (IEngine) DIHelper.getInstance().getLocalProp(engineName);
+
+		SesameJenaSelectWrapper sjswQuery1 = processQuery(GET_ALL_SYSTEM_WITH_CREATE_AND_DOWNSTREAM_QUERY, engine);
+		processResults(sjswQuery1, dataListSystems);
+
+		SesameJenaSelectWrapper sjswQuery2 = processQuery(GET_ALL_SYSTEM_WITH_DOWNSTREAM_AND_NO_UPSTREAM, engine);
+		processResults(sjswQuery2, dataListSystems);
+
+		SesameJenaSelectWrapper sjswQuery3 = processQuery(GET_ALL_SYSTEM_WITH_UPSTREAM, engine);
+		processResults(sjswQuery3, dataListSystems);
+
+		SesameJenaSelectWrapper sjswQuery4 = processQuery(GET_ALL_CAPABILITIES_AND_CRM, engine);
+		processResults(sjswQuery4, dataListCapabilities);
+	}
+
+	private ArrayList<ArrayList<String>> getSysOrCapAndData(String cap, String capCRM, String sysCRM, boolean getSys) 
+	{
+		ArrayList<ArrayList<String>> resultSet = new ArrayList<ArrayList<String>>();
+		ArrayList<String> capDataList = new ArrayList<String>();
+		Hashtable<String, Hashtable<String, String>> searchList = new Hashtable<String, Hashtable<String, String>>();
+		Hashtable<String, Hashtable<String, String>> getList = new Hashtable<String, Hashtable<String, String>>();
+
+		if(getSys)
+		{	
+			searchList = dataListCapabilities;
+			getList = dataListSystems;
+		}
+		else
+		{
+			getList = dataListCapabilities;
+			searchList = dataListSystems;
+		}
+
+		for( String data : searchList.keySet() )
+		{
+			Hashtable<String, String> innerHash = searchList.get(data);
+			if(innerHash.containsKey(cap))
+			{
+				String crm = innerHash.get(cap);
+				if(crm.equals(capCRM))
+				{
+					capDataList.add(data);
+				}
+			}
+		}
+
+		for( String data : capDataList)
+		{
+			Hashtable<String, String> innerHash = getList.get(data);
+			for( String sys : innerHash.keySet())
+			{
+				if(innerHash.get(sys).equals(sysCRM))
+				{
+					ArrayList<String> innerArray = new ArrayList<String>();
+					innerArray.add(sys);
+					innerArray.add(data);
+					resultSet.add(innerArray);
+				}
+			}
+
+		}
+
+		return resultSet;
+	}
+
+	private void processResults(SesameJenaSelectWrapper sjsw, Hashtable<String, Hashtable<String, String>> data )
+	{
+		String[] vars = sjsw.getVariables();
+		while(sjsw.hasNext())
+		{
+			SesameJenaSelectStatement sjss = sjsw.next();
+			String sub = sjss.getRawVar(vars[0]).toString();
+			String obj = sjss.getRawVar(vars[1]).toString();
+			String crm = sjss.getRawVar(vars[2]).toString();
+
+			Hashtable<String, String> innerHash = new Hashtable<String, String>();
+			if(!data.containsKey(obj))
+			{
+				innerHash.put(sub, crm);
+				data.put(obj, innerHash);
+			}
+			else if(!data.get(obj).containsKey(sub))
+			{
+				innerHash = data.get(obj);
+				innerHash.put(sub,  crm);
+			}
+			else
+			{
+				System.err.println("SHOULD NEVER ENTER HERE");
+			}
+		}
+	}
+
+	//process the query
+	private SesameJenaSelectWrapper processQuery(String query, IEngine engine){
+		SesameJenaSelectWrapper sjsw = new SesameJenaSelectWrapper();
+		//run the query against the engine provided
+		sjsw.setEngine(engine);
+		sjsw.setQuery(query);
+		sjsw.executeQuery();		
+		sjsw.getVariables();
+		return sjsw;
+	}
+}
