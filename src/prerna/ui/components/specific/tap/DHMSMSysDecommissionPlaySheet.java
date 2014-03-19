34f4adeccb4599c0528d9151700f75e3188adad7
mahkhalil <mahkhalil@deloitte.com>
2014-03-19 12:57:10 +0000

    Playsheet for dhmsm decommission visualization
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@905 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 4e921abc5280d54792832e04faa3379c8e6ea0c6

---
+++ src/prerna/ui/components/specific/tap/DHMSMSysDecommissionPlaySheet.java
@@ -0,0 +1,51 @@
+package prerna.ui.components.specific.tap;
+
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import prerna.ui.components.playsheets.BrowserPlaySheet;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+
+public class DHMSMSysDecommissionPlaySheet extends BrowserPlaySheet{
+
+	private DHMSMSysDecommissionReport data;
+	
+	
+	//TODO: this class should not extend BrowserPlaySheet and should create new methods to send information to JS
+	public void setData(DHMSMSysDecommissionReport data) {
+		this.data = data;
+		this.list = new ArrayList();
+		this.list.add(new Object[1]);
+		this.names = new String[1];
+	}
+
+	/**
+	 * Constructor for DHMSMSysDecommissionPlaySheet.
+	 */
+	public DHMSMSysDecommissionPlaySheet() {
+		super();
+		this.setPreferredSize(new Dimension(800,600));
+		String workingDir = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
+		fileName = "file://" + workingDir + "/html/MHS-RDFSemossCharts/app/world-map-timeline.html";
+	}
+	
+	@Override
+	public void createData()
+	{
+		if(data == null || data.masterHash == null)
+		{
+			data = new DHMSMSysDecommissionReport();
+			data.runCalculation();
+		}
+		
+		DHMSMSysDecommissionDataProcessing dataProcessing = new DHMSMSysDecommissionDataProcessing();
+		dataProcessing.setDataSource(data);
+		dataProcessing.setAllData(data.masterHash);
+		dataProcessing.decomposeData();
+		Hashtable<Integer, Object> allHash = dataProcessing.constructHash();
+		
+		this.dataHash = allHash;
+	}
+}
