66ab432389d803731be363ab9c0086c85635d55f
tomtchen <tomtchen@deloitte.com>
2014-03-24 18:06:40 +0000

    adding in querybuilder
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@972 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/rdf/query/util/ISPARQLReturnModifier.java
@@ -0,0 +1,26 @@
+package prerna.rdf.query.util;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public interface ISPARQLReturnModifier {
+	
+	//this interface API does not support two modifiers on one level
+	public final static String MOD = "MODIFIER";
+	
+	
+	public void setLowerLevelModifier(Hashtable<String, ISPARQLReturnModifier> lowerMods);
+	
+	public Hashtable<String, ISPARQLReturnModifier> getLowerLevelModifier();
+	
+	public String getModifierAsString();
+	
+	public void setModID(String id);
+	
+	public String getModID();
+	
+	public void setModType(SPARQLModifierConstant modConstant);
+	
+	public String getModType();
+
+}
