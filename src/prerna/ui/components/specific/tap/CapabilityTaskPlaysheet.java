847c00218bd5f42bf063b45d0a44240ffa2c7af9
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2014-03-25 19:12:14 +0000

    Capability to Task Heatmap Playsheet Capability to Task based on a vendor heatmap
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@1010 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/ui/components/specific/tap/CapabilityTaskPlaysheet.java
@@ -0,0 +1,134 @@
+/*******************************************************************************
+ * Copyright 2013 SEMOSS.ORG
+ * 
+ * This file is part of SEMOSS.
+ * 
+ * SEMOSS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SEMOSS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SEMOSS.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package prerna.ui.components.specific.tap;
+
+import java.awt.Dimension;
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+import prerna.ui.components.playsheets.BrowserPlaySheet;
+import prerna.util.Constants;
+import prerna.util.ConstantsTAP;
+import prerna.util.DIHelper;
+
+/**
+ * The Play Sheet for creating a heat map diagram.  
+ */
+public class CapabilityTaskPlaysheet extends BrowserPlaySheet {
+	
+	/**
+	 * Constructor for HeatMapPlaySheet.
+	 */
+	public CapabilityTaskPlaysheet() {
+		super();
+		this.setPreferredSize(new Dimension(800,600));
+		String workingDir = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
+		fileName = "file://" + workingDir + "/html/MHS-RDFSemossCharts/app/heatmap.html";
+	}
+	
+	/**
+	 * Method processQueryData.  Processes the data from the SPARQL query into an appropriate format for the specific play sheet.
+	
+	 * @return Hashtable - Consists of the x-value, y-value, x- and y-axis titles, and the title of the map.*/
+	public Hashtable processQueryData()
+	{
+		Hashtable dataHash = new Hashtable();
+		Hashtable dataSeries = new Hashtable();
+		
+		String[] var = wrapper.getVariables();
+		String venVar = var[0];
+		String capVar = var[1];
+		String taskVar = var[2];
+		String supVar = var[3];
+		
+		String venName = null;
+		
+		// most of the logic would go in here
+		// I am getting the vendor and the capability in x and y
+		// now I need to find a way to get to the various tasks in the capability
+		// I have to have a running counter to assign the scores
+		// for each tasks's support level
+		// add all of this and we are ready to go
+		Hashtable<String,Integer> options = new Hashtable<String,Integer>();
+		options.put("Out_of_Box", Integer.parseInt(DIHelper.getInstance().getProperty(ConstantsTAP.VENDOR_FULFILL_LEVEL_1)));
+		options.put("Out_of_box", Integer.parseInt(DIHelper.getInstance().getProperty(ConstantsTAP.VENDOR_FULFILL_LEVEL_1)));
+		options.put("Out_of_Box_with_Configuration",  Integer.parseInt(DIHelper.getInstance().getProperty(ConstantsTAP.VENDOR_FULFILL_LEVEL_2)));
+		options.put("Out_of_Box_with_Customization", Integer.parseInt(DIHelper.getInstance().getProperty(ConstantsTAP.VENDOR_FULFILL_LEVEL_3)));
+		options.put("Does_Not_Support", Integer.parseInt(DIHelper.getInstance().getProperty(ConstantsTAP.VENDOR_FULFILL_LEVEL_4)));
+		options.put("Does_not_support", Integer.parseInt(DIHelper.getInstance().getProperty(ConstantsTAP.VENDOR_FULFILL_LEVEL_4)));
+
+		// what I am getting is of the form
+		// vendor capability task fullfillment level
+		
+		
+		Hashtable <String, Integer> capHash = new Hashtable<String, Integer>();
+		for (int i=0;i<list.size();i++)
+		{
+			
+			Object[] listElement = list.get(i);
+			venName = (String) listElement[0];
+			
+			// since I know all of this is just one vendor, I am not going to worry much here
+			String capName = (String) listElement[1];
+			String supLevel = (String)listElement[3];
+			
+			Integer score = new Integer(0);
+			if(capHash.containsKey(capName))
+				score = (Integer)capHash.get(capName);
+			System.err.println("Support is "+ supLevel);
+			score += options.get(supLevel);
+			
+			capHash.put(capName, score);
+		}
+		
+		
+		// now run the regular routine
+		Enumeration<String> en = capHash.keys();
+		venName = venName.replaceAll("\"", "");
+		while(en.hasMoreElements())
+		{
+			String capName = en.nextElement();
+			capName = capName.replaceAll("\"", "");
+			Integer score = capHash.get(capName);
+			
+					
+			Hashtable elementHash = new Hashtable();
+			elementHash.put(venVar, venName);
+			elementHash.put(capVar, capName);
+			elementHash.put(supVar, score);
+			
+			dataHash.put(venName+capName, elementHash);
+			
+		}
+
+		Hashtable allHash = new Hashtable();
+		allHash.put("dataSeries", dataHash);
+		String[] var1 = wrapper.getVariables();
+		allHash.put("title",  "Vendor : " + venName + " vs Capabilities ");
+		allHash.put("xAxisTitle", capVar);
+		allHash.put("yAxisTitle", venVar);
+		allHash.put("value", supVar);
+		
+		System.err.println("> >>>>>>>>>>>>>>>>>>>>>>>>>>> " + allHash);
+		
+		return allHash;
+	}
+	
+
+}
