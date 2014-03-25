3172df4491a1779a2b75c89bcb2404bf78475d41
karverma <karverma@deloitte.com>
2014-03-25 13:45:31 +0000

    Fixing regex bug, allowing for decimal points rather than all characters
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@994 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ca8a1100d9d022e818a564d54f87d9129523513d

--- src/prerna/poi/main/NodeLoadingSheetWriter.java
+++ src/prerna/poi/main/NodeLoadingSheetWriter.java
@@ -90,7 +90,7 @@ public class NodeLoadingSheetWriter {
 	public void writeSheet(String key, Vector<String[]> sheetVector, XSSFWorkbook workbook) {
 		XSSFSheet worksheet = workbook.createSheet(key);
 		int count=0;//keeps track of rows; one below the row int because of header row
-		final Pattern NUMERIC = Pattern.compile("^\\d+.?\\d*$");
+		final Pattern NUMERIC = Pattern.compile("^\\d+\\.?\\d*$");
 		//for each row, create the row in excel
 		for (int row=0; row<sheetVector.size();row++){
 			XSSFRow row1 = worksheet.createRow( count);
