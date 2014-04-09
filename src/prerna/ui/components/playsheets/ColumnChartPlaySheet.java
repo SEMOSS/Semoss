b5c1a84322de66ac029acd050352600443a74671
mahkhalil <mahkhalil@deloitte.com>
2014-04-09 16:26:38 +0000

    New column chart play sheet vis
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@1423 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f01010d60c65b709fc0f365602fa603ca1b3b9b3

---
+++ src/prerna/ui/components/playsheets/ColumnChartPlaySheet.java
@@ -0,0 +1,43 @@
+package prerna.ui.components.playsheets;
+
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+
+public class ColumnChartPlaySheet extends BrowserPlaySheet{
+
+	public ColumnChartPlaySheet() 
+	{
+		super();
+		this.setPreferredSize(new Dimension(800,600));
+		String workingDir = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
+		fileName = "file://" + workingDir + "/html/MHS-RDFSemossCharts/app/singlechartgrid.html";
+	}
+	
+	public Hashtable<String, Object> processQueryData()
+	{		
+		Hashtable<String, ArrayList<Object>> data = new Hashtable<String, ArrayList<Object>>();
+		for( int i = 0; i < list.size(); i++)
+		{
+			Object[] elemValues = list.get(i);
+			ArrayList<Object> values = new ArrayList<Object>();
+			for( int j = 1; j < elemValues.length; j++)
+			{
+				values.add(elemValues[j]);
+			}
+			data.put(elemValues[0].toString(), values);	
+		}
+		
+		
+		Hashtable<String, Object> columnChartHash = new Hashtable<String, Object>();
+		columnChartHash.put("xAxis", names);
+		columnChartHash.put("type", "column");
+		columnChartHash.put("dataSeries", data);
+		
+		return columnChartHash;
+	}
+	
+}
