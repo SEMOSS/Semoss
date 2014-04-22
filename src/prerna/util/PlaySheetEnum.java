14ae6d4b7d62ed36e7e86222967669f4107585d3
mahkhalil <mahkhalil@deloitte.com>
2014-04-22 15:56:37 +0000

    edited bad hints
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@1531 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 36b533a5c1979c0d625502cc44805dd2c425ecd0

--- src/prerna/util/PlaySheetEnum.java
+++ src/prerna/util/PlaySheetEnum.java
@@ -27,18 +27,19 @@ import java.util.ArrayList;
 
 public enum PlaySheetEnum {
 	
-	Graph("Graph", "prerna.ui.components.playsheets.GraphPlaySheet", "Hint: CONSTRUCT {?subject ?predicate ?object} WHERE{ ... }"),
-	Grid("Grid", "prerna.ui.components.playsheets.GridPlaySheet", "Hint: SELECT ?x1 ?x2 ?x3 WHERE{ ... }"),
-	Raw_Grid("Raw Grid", "prerna.ui.components.playsheets.GridRAWPlaySheet", "Hint: SELECT ?x1 ?x2 ?x3 WHERE{ ... }"),
-	Heat_Map("Heat Map", "prerna.ui.components.playsheets.HeatMapPlaySheet", "Hint: SELECT ?xAxisList ?yAxisList ?numericHeatValue WHERE{ ... } GROUP BY ?xAxisList ?yAxisList"),
-	Parallel_Coordinates("Parallel Coordinates", "prerna.ui.components.playsheets.ParallelCoordinatesPlaySheet", "Hint: SELECT ?axis1 ?axis2 ?axis3 WHERE{ ... }"),
-	Grid_Scatter("Grid Scatter", "prerna.ui.components.playsheets.GridScatterSheet", "Hint: SELECT ?elementName ?xAxisValues ?yAxisValues (OPTIONAL)?zAxisValues WHERE{ ... }"),
-	Column_Chart("Column Chart", "prerna.ui.components.playsheets.ColumnChartPlaySheet", "Hint: SELECT ?xAxisValues ?numericColumnValue ... WHERE { ... }"),
-	Pie_Chart("Pie Chart", "prerna.ui.components.playsheets.PieChartPlaySheet", "Hint: SELECT ?wedgeName ?wedgeValue WHERE { ... }"),
-	Sankey_Diagram("Sankey Diagram","prerna.ui.components.playsheets.SankeyPlaySheet", "Hint: SELECT ?source ?target ?value ?target2 ?value2 ?target3 ?value3...etc  Note: ?target is the source for ?target2 and ?target2 is the source for ?target3...etc WHERE{ ... }"),
-	World_Heat_Map("World Heat Map","prerna.ui.components.playsheets.WorldHeatMapPlaySheet", "Hint: SELECT ?country ?numericHeatValue WHERE{ ... }"),
-	US_Heat_Map("US Heat Map","prerna.ui.components.playsheets.USHeatMapPlaySheet", "Hint: SELECT ?state ?numericHeatValue WHERE{ ... }");	
+	Graph("Graph", "prerna.ui.components.playsheets.GraphPlaySheet", "GraphPlaySheet Hint: CONSTRUCT {?subject ?predicate ?object} WHERE{ ... }"),
+	Grid("Grid", "prerna.ui.components.playsheets.GridPlaySheet", "GridPlaySheet Hint: SELECT ?x1 ?x2 ?x3 WHERE{ ... }"),
+	Raw_Grid("Raw Grid", "prerna.ui.components.playsheets.GridRAWPlaySheet", "GridRAWPlaySheet Hint: SELECT ?x1 ?x2 ?x3 WHERE{ ... }"),
+	Heat_Map("Heat Map", "prerna.ui.components.playsheets.HeatMapPlaySheet", "HeatMapPlaySheet Hint: SELECT ?xAxisList ?yAxisList ?numericHeatValue WHERE{ ... } GROUP BY ?xAxisList ?yAxisList"),
+	Parallel_Coordinates("Parallel Coordinates", "prerna.ui.components.playsheets.ParallelCoordinatesPlaySheet", "ParallelCoordinatesPlaySheet Hint: SELECT ?axis1 ?axis2 ?axis3 WHERE{ ... }"),
+	Grid_Scatter("Grid Scatter", "prerna.ui.components.playsheets.GridScatterSheet", "GridScatterSheet Hint: SELECT ?elementName ?xAxisValues ?yAxisValues (OPTIONAL)?zAxisValues WHERE{ ... }"),
+	Column_Chart("Column Chart", "prerna.ui.components.playsheets.ColumnChartPlaySheet", "ColumnChartPlaySheet Hint: SELECT ?xAxis ?yAxis1 (OPTIONAL) ?yAxis2 ?yAxis3 ... (where all yAxis values are numbers) WHERE { ... }"),
+	Pie_Chart("Pie Chart", "prerna.ui.components.playsheets.PieChartPlaySheet", "PieChartPlaySheet Hint: SELECT ?wedgeName ?wedgeValue WHERE { ... }"),
+	Sankey_Diagram("Sankey Diagram","prerna.ui.components.playsheets.SankeyPlaySheet", "SankeyPlaySheet Hint: SELECT ?source ?target ?value ?target2 ?value2 ?target3 ?value3...etc  Note: ?target is the source for ?target2 and ?target2 is the source for ?target3...etc WHERE{ ... }"),
+	World_Heat_Map("World Heat Map","prerna.ui.components.playsheets.WorldHeatMapPlaySheet", "WorldHeatMapPlaySheet Hint: SELECT ?country ?numericHeatValue WHERE{ ... }"),
+	US_Heat_Map("US Heat Map","prerna.ui.components.playsheets.USHeatMapPlaySheet", "USHeatMapPlaySheet Hint: SELECT ?state ?numericHeatValue WHERE{ ... }");
 	
+
 	private final String sheetName;
 	private final String sheetClass;
 	private final String sheetHint;
