27309b7afe6338035bff2e420b440521987bc1c2
mahkhalil <mahkhalil@deloitte.com>
2014-05-14 16:39:15 +0000

    Creation of prop files during monolith csv loading
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@1754 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ui/components/CSVPropFileBuilder.java
+++ src/prerna/ui/components/CSVPropFileBuilder.java
@@ -7,10 +7,17 @@ import java.util.Iterator;
 public class CSVPropFileBuilder{
 
 	private StringBuilder relationships = new StringBuilder();
-	private StringBuilder node_properties = new StringBuilder();
-
+	private StringBuilder node_properties = new StringBuilder();	
+	
 	private Hashtable<String, String> propHash = new Hashtable<String, String>();
 	private Hashtable<String, String> dataTypeHash = new Hashtable<String, String>();
+	
+	private StringBuilder propFile = new StringBuilder();
+	
+	public CSVPropFileBuilder(){
+		propFile.append("START_ROW\t2\n");
+		propFile.append("END_ROW\t100000\n");
+	}
 	
 	public void addProperty(ArrayList<String> sub, ArrayList<String> obj, String dataType) {
 		String subject = "";
@@ -49,33 +56,44 @@ public class CSVPropFileBuilder{
 	}
 
 	public void columnTypes(ArrayList<String> header){
+		propHash.put("NUM_COLUMNS", Integer.toString(header.size()));
+		propFile.append("NUM_COLUMNS" + "\t" + Integer.toString(header.size()) + "\n");
 		for(int i = 0; i < header.size(); i++)
 		{
 			if(dataTypeHash.containsKey(header.get(i)))
 			{
 				propHash.put(Integer.toString(i+1), dataTypeHash.get(header.get(i)));
 				System.out.println(header.get(i) + ":" + Integer.toString(i+1) + ":" + dataTypeHash.get(header.get(i)));
+				propFile.append(Integer.toString(i+1) + "\t" + dataTypeHash.get(header.get(i)) + "\n");
 			}
 			else
 			{
 				propHash.put(Integer.toString(i+1), "STRING");
 				System.out.println(header.get(i) + ":" + Integer.toString(i+1) + ": STRING");
+				propFile.append(Integer.toString(i+1) + "\tSTRING\n");
 			}
 		}
-		propHash.put("NUM_COLUMNS", Integer.toString(header.size()));
 	}
 
 	public void constructPropHash(){
+		propHash.put("START_ROW","2");
+		propHash.put("END_ROW","100000");
+		propHash.put("NOT_OPTIONAL", ";");
 		propHash.put("RELATION", relationships.toString());
 		propHash.put("NODE_PROP", node_properties.toString());
 		propHash.put("RELATION_PROP", "");
-		propHash.put("NOT_OPTIONAL", ";");
-		propHash.put("START_ROW","2");
-		propHash.put("END_ROW","100000");
+		
+		propFile.append("RELATION\t" + relationships.toString() + "\n");
+		propFile.append("NODE_PROP\t" + node_properties.toString() + "\n");
+		propFile.append("RELATION_PROP\t \n");
 	}
 
 	public Hashtable<String, String> getPropHash() {
 		constructPropHash();
 		return propHash;
+	}
+	
+	public String getPropFile() {
+		return propFile.toString();
 	}
 }
