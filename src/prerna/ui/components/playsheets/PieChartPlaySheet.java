b33cc774fbc9facbb82738139e31967e4a246f62
bkirk <bkirk@deloitte.com>
2014-06-20 19:42:55 +0000

    base code for pie chart - updates to come
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@2238 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ui/components/playsheets/PieChartPlaySheet.java
+++ src/prerna/ui/components/playsheets/PieChartPlaySheet.java
@@ -14,10 +14,10 @@ public class PieChartPlaySheet extends BrowserPlaySheet{
 		super();
 		this.setPreferredSize(new Dimension(800,600));
 		String workingDir = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
-		fileName = "file://" + workingDir + "/html/MHS-RDFSemossCharts/app/singlechartgrid.html";
+		fileName = "file://" + workingDir + "/html/MHS-RDFSemossCharts/app/piechart.html";
 	}
 	
-	public Hashtable<String, Object> processQueryData()
+	/*public Hashtable<String, Object> processQueryData()
 	{		
 		Hashtable<String, ArrayList<Object[]>> data = new Hashtable<String, ArrayList<Object[]>>();
 		ArrayList<Object[]> values = new ArrayList<Object[]>();
@@ -30,6 +30,39 @@ public class PieChartPlaySheet extends BrowserPlaySheet{
 		Hashtable<String, Object> columnChartHash = new Hashtable<String, Object>();
 		columnChartHash.put("type", "pie");
 		columnChartHash.put("dataSeries", data);
+		
+		return columnChartHash;
+	}*/
+	
+	
+	
+	public Hashtable<String, Object> processQueryData()
+	{		
+		ArrayList< ArrayList<Hashtable<String, Object>>> dataObj = new ArrayList< ArrayList<Hashtable<String, Object>>>();
+
+		for( int i = 0; i < list.size(); i++)
+		{
+			Object[] elemValues = list.get(i);
+			for( int j = 1; j < elemValues.length; j++)
+			{
+				ArrayList<Hashtable<String,Object>> seriesArray = new ArrayList<Hashtable<String,Object>>();
+				if(dataObj.size() >= j)
+					seriesArray = dataObj.get(j-1);
+				else
+					dataObj.add(j-1, seriesArray);
+				Hashtable<String, Object> elementHash = new Hashtable();
+				elementHash.put("pieCat", elemValues[0].toString());
+				//elementHash.put("pieVal", elemValues[j]);
+				System.out.println(elemValues[0].toString() + elemValues[j]);
+				elementHash.put("pieVal", elemValues[j]);
+				seriesArray.add(elementHash);
+			}
+		}
+		
+		Hashtable<String, Object> columnChartHash = new Hashtable<String, Object>();
+		columnChartHash.put("names", names);
+		columnChartHash.put("type", "pie");
+		columnChartHash.put("dataSeries", dataObj);
 		
 		return columnChartHash;
 	}
