afc7fa82516659152da89344b80c3484629c3075
ksmart <ksmart@deloitte.com>
2014-06-23 16:05:07 +0000

    automating the processing of all lpi system transition reports
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@2264 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e6e1d82d63f73b94dab6b84d30d0cf0706e5a969

---
+++ src/prerna/ui/components/specific/tap/LPISystemTransitionReport.java
@@ -0,0 +1,73 @@
+package prerna.ui.components.specific.tap;
+
+import org.apache.log4j.Logger;
+
+import com.bigdata.rdf.model.BigdataURIImpl;
+
+import prerna.rdf.engine.api.IEngine;
+import prerna.rdf.engine.impl.SesameJenaSelectStatement;
+import prerna.rdf.engine.impl.SesameJenaSelectWrapper;
+import prerna.ui.components.playsheets.AbstractRDFPlaySheet;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.Utility;
+
+public class LPISystemTransitionReport extends AbstractRDFPlaySheet{
+
+	Logger logger = Logger.getLogger(getClass());
+	private IEngine hr_Core;
+	
+	@Override
+	public void createData() {
+		try{
+			hr_Core = (IEngine) DIHelper.getInstance().getLocalProp("HR_Core");
+		} catch(Exception e) {
+			e.printStackTrace();
+			Utility.showError("Could not find necessary databases:\nHR_Core, TAP_Cost_Data");
+		}
+		
+		SesameJenaSelectWrapper sjsw = processQuery(hr_Core, query);
+		String[] names = sjsw.getVariables();
+		
+		while(sjsw.hasNext())
+		{
+			SesameJenaSelectStatement sjss = sjsw.next();
+			BigdataURIImpl sysRawURI = (BigdataURIImpl)sjss.getRawVar(names[0]);
+			String sysURI = "<"+sysRawURI.stringValue()+">";
+			logger.info("Creating LPI System Transition Report for system >>> "+sysURI);
+			IndividualLPISystemTransitionReport lpiReport = new IndividualLPISystemTransitionReport();
+			lpiReport.enableMessages(false);
+			lpiReport.setQuery(sysURI);
+			lpiReport.createData();
+		}
+		
+		String fileLoc = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + "\\export\\Reports\\";
+		Utility.showMessage("System Transition Reports Finished! Files located in:\n" +fileLoc);
+	}
+
+	private SesameJenaSelectWrapper processQuery(IEngine engine, String query){
+		logger.info("PROCESSING QUERY: " + query);
+		SesameJenaSelectWrapper sjsw = new SesameJenaSelectWrapper();
+		//run the query against the engine provided
+		sjsw.setEngine(engine);
+		sjsw.setQuery(query);
+		sjsw.executeQuery();	
+		return sjsw;
+	}
+
+	@Override
+	public void refineView() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void overlayView() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void runAnalytics() {
+		// TODO Auto-generated method stub
+	}
+
+}
