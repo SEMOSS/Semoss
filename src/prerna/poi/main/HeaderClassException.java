1737f83ef30676b85e62762a122f844b7853f0e3
Maher Khalil <themaherkhalil@gmail.com>
2014-07-10 20:01:14 +0000

    throwing specific exceptions throughout data loading procedure
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@2646 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 9822d913d08551089f3dcc1ac0d3a3fadcc2a5b0

---
+++ src/prerna/poi/main/HeaderClassException.java
@@ -0,0 +1,12 @@
+package prerna.poi.main;
+
+@SuppressWarnings("serial")
+public class HeaderClassException extends Exception {
+	public HeaderClassException() {
+		
+	}
+	
+	public HeaderClassException(String message) {
+		super(message);
+	}
+}
