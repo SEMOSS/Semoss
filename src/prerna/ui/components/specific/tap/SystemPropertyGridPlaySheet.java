4befa4654b7cc84064977512c8856abac000a505
mahkhalil <mahkhalil@deloitte.com>
2014-07-15 16:59:05 +0000

    added cost to the system info query
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@2672 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: cf515e1fb28577d8792b361ee716874d75d6a76a

--- src/prerna/ui/components/specific/tap/SystemPropertyGridPlaySheet.java
+++ src/prerna/ui/components/specific/tap/SystemPropertyGridPlaySheet.java
@@ -1,8 +1,112 @@
 package prerna.ui.components.specific.tap;
 
-import prerna.ui.components.playsheets.GridPlaySheet;
+import java.util.ArrayList;
+import java.util.Hashtable;
 
+import prerna.rdf.engine.api.IEngine;
+import prerna.rdf.engine.impl.SesameJenaSelectStatement;
+import prerna.rdf.engine.impl.SesameJenaSelectWrapper;
+import prerna.ui.components.playsheets.GridPlaySheet;
+import prerna.util.DIHelper;
+
+@SuppressWarnings("serial")
 public class SystemPropertyGridPlaySheet  extends GridPlaySheet {
+
+	private String costQuery = "SELECT DISTINCT ?System (SUM(?FY15) as ?fy15) (SUM(?FY16) as ?fy16) (SUM(?FY17) as ?fy17) (SUM(?FY18) as ?fy18) WHERE { { SELECT DISTINCT ?System ?FY15 ?FY16 ?FY17 ?FY18 WHERE { {?System <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/System> ;} {?SystemBudgetGLItem <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/SystemBudgetGLItem> ;} {?System <http://semoss.org/ontologies/Relation/Has> ?SystemBudgetGLItem} OPTIONAL { {?SystemBudgetGLItem <http://semoss.org/ontologies/Relation/Contains/Cost> ?FY15 ;} {?SystemBudgetGLItem <http://semoss.org/ontologies/Relation/OccursIn> <http://health.mil/ontologies/Concept/FYTag/FY15>} } OPTIONAL { {?SystemBudgetGLItem <http://semoss.org/ontologies/Relation/Contains/Cost> ?FY16 ;} {?SystemBudgetGLItem <http://semoss.org/ontologies/Relation/OccursIn> <http://health.mil/ontologies/Concept/FYTag/FY16>} } OPTIONAL { {?SystemBudgetGLItem <http://semoss.org/ontologies/Relation/Contains/Cost> ?FY17 ;} {?SystemBudgetGLItem <http://semoss.org/ontologies/Relation/OccursIn> <http://health.mil/ontologies/Concept/FYTag/FY17>} } OPTIONAL { {?SystemBudgetGLItem <http://semoss.org/ontologies/Relation/Contains/Cost> ?FY18 ;} {?SystemBudgetGLItem <http://semoss.org/ontologies/Relation/OccursIn> <http://health.mil/ontologies/Concept/FYTag/FY18>} } } } UNION { SELECT DISTINCT ?System ?FY15 ?FY16 ?FY17 ?FY18 WHERE { {?System <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/System> ;} {?System <http://semoss.org/ontologies/Relation/ConsistsOf> ?SystemService} {?SystemServiceBudgetGLItem <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/SystemServiceBudgetGLItem> ;} {?SystemService <http://semoss.org/ontologies/Relation/Has> ?SystemServiceBudgetGLItem} OPTIONAL { {?SystemServiceBudgetGLItem <http://semoss.org/ontologies/Relation/Contains/Cost> ?FY15 ;} {?SystemServiceBudgetGLItem <http://semoss.org/ontologies/Relation/OccursIn> <http://health.mil/ontologies/Concept/FYTag/FY15>} } OPTIONAL { {?SystemServiceBudgetGLItem <http://semoss.org/ontologies/Relation/Contains/Cost> ?FY16 ;} {?SystemServiceBudgetGLItem <http://semoss.org/ontologies/Relation/OccursIn> <http://health.mil/ontologies/Concept/FYTag/FY16>} } OPTIONAL { {?SystemServiceBudgetGLItem <http://semoss.org/ontologies/Relation/Contains/Cost> ?FY17 ;} {?SystemServiceBudgetGLItem <http://semoss.org/ontologies/Relation/OccursIn> <http://health.mil/ontologies/Concept/FYTag/FY17>} } OPTIONAL { {?SystemServiceBudgetGLItem <http://semoss.org/ontologies/Relation/Contains/Cost> ?FY18 ;} {?SystemServiceBudgetGLItem <http://semoss.org/ontologies/Relation/OccursIn> <http://health.mil/ontologies/Concept/FYTag/FY18>} } } } } GROUP BY ?System ";
+	private String tapPortfolio = "TAP_Portfolio";
+	private String tapCost = "TAP_Cost_Data";
+	
+	private String[] costDataVarNames;
+	private Integer costDataLength = 0;
+	@Override
+	public void createData() {
+		Hashtable<String, Hashtable<String, Double>> costHash = new Hashtable<String, Hashtable<String, Double>>();
+
+		boolean includeCost = true;
+		IEngine portfolioEngine =  (IEngine) DIHelper.getInstance().getLocalProp(tapPortfolio);
+		IEngine costEngine = (IEngine) DIHelper.getInstance().getLocalProp(tapCost);
+		if(portfolioEngine != null) { // process cost query from portfolio (faster on smaller db)
+			costHash = processCostQuery(portfolioEngine);
+		} else if(costEngine != null) {
+			costHash = processCostQuery(costEngine);
+		} else {
+			includeCost = false;
+		}
+		
+		SesameJenaSelectWrapper runQuery = new SesameJenaSelectWrapper();
+		runQuery.setQuery(this.query);
+		runQuery.setEngine(this.engine);
+		runQuery.executeQuery();
+		String[] varNames = runQuery.getVariables();
+		
+		this.names = new String[varNames.length + costDataLength];
+		for(int i = 0; i < names.length; i++) {
+			if(i < varNames.length) {
+				names[i] = varNames[i];
+			} else {
+				names[i] = costDataVarNames[i-varNames.length+1];
+			}
+		}
+		
+		list = new ArrayList<Object[]>();
+		while(runQuery.hasNext()) {
+			Object[] addRow = new Object[varNames.length + costDataLength];
+			
+			SesameJenaSelectStatement sjss = runQuery.next();
+			String sysName = "";
+			for(int i = 0; i < varNames.length + 1; i++) {
+				if(i == 0) {
+					sysName = sjss.getVar(varNames[i]).toString();
+					addRow[i] = sysName;
+				} else if(i == varNames.length) {
+					if(includeCost) {
+						Hashtable<String, Double> costInfo = costHash.get(sysName);
+						if(costInfo != null) {
+							for(int j = 0; j < costDataLength; j++) {
+								addRow[i+j] = Math.round(costInfo.get(costDataVarNames[j+1]));
+							}
+						} else {
+							for(int j = 0; j < costDataLength; j++) {
+								addRow[i+j] = "No cost info received.";
+							}
+						}
+					} else {
+						for(int j = 0; j < costDataLength; j++) {
+							addRow[i+j] = "*";
+						}
+					}
+				} else {
+					addRow[i] = sjss.getVar(varNames[i]);
+				}
+			}
+			list.add(addRow);
+		}
+		
+	}
+
+	private Hashtable<String, Hashtable<String, Double>> processCostQuery(IEngine engine) 
+	{
+		Hashtable<String, Hashtable<String, Double>> costHash = new Hashtable<String, Hashtable<String, Double>>();
+		SesameJenaSelectWrapper costWrapper = new SesameJenaSelectWrapper();
+		costWrapper.setQuery(costQuery);
+		costWrapper.setEngine(engine);
+		costWrapper.executeQuery();
+		
+		costDataVarNames = costWrapper.getVariables();
+		costDataLength = costDataVarNames.length - 1;
+		while(costWrapper.hasNext()) {
+			SesameJenaSelectStatement sjss = costWrapper.next();
+			String sys = sjss.getVar(costDataVarNames[0]).toString();
+			Hashtable<String, Double> innerHash = new Hashtable<String, Double>();
+			costHash.put(sys, innerHash);
+			for(int i = 1; i < costDataVarNames.length; i++) {
+				innerHash.put(costDataVarNames[i], (Double) sjss.getVar(costDataVarNames[i]));
+			}
+		}
+		
+		return costHash;
+	}
+
 
 	/**
 	 * Sets the string version of the SPARQL query on the playsheet. 
