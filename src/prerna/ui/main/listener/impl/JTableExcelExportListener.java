3b4fa7e114b119dc01bf31e032b4835fc121315d
karverma <karverma@deloitte.com>
2014-07-16 19:51:16 +0000

    Adding functionality to export any active Grid/JTable to Excel through right-click option
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@2726 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: cdf0be7618436f3c81ebf9817ddcd4150e88d20b

---
+++ src/prerna/ui/main/listener/impl/JTableExcelExportListener.java
@@ -0,0 +1,105 @@
+/*******************************************************************************
+ * Copyright 2013 SEMOSS.ORG
+ * 
+ * This file is part of SEMOSS.
+ * 
+ * SEMOSS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SEMOSS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SEMOSS.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package prerna.ui.main.listener.impl;
+
+import java.awt.event.ActionEvent;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.swing.JComponent;
+import javax.swing.JTable;
+import javax.swing.table.TableModel;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.Utility;
+
+public class JTableExcelExportListener extends AbstractListener {
+	private final String WORKING_DIR = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
+	private final String EXPORT_FOLDER = "\\export\\Excel\\";
+	private JTable table;
+	private String filename;
+
+	public JTableExcelExportListener(JTable table, String questionTitle) {
+		this.table = table;
+		this.filename = this.WORKING_DIR + this.EXPORT_FOLDER + questionTitle + ".xlsx";
+	}
+
+	/**
+	 * Runs Excel sheet export of current values displayed in Grid Table format.
+	 * 
+	 * @return	boolean successfulExport - whether or not the export was completed successfully, used to display the correct alert message
+	 */
+	public boolean runExport() {
+		boolean successfulExport = true;
+
+		try{
+			TableModel model = this.table.getModel();
+			XSSFWorkbook wb = new XSSFWorkbook();
+			XSSFSheet sheet = wb.createSheet("Export");
+
+			int columnCount = model.getColumnCount();
+
+			//Write out column headers in first row
+			XSSFRow headerRow = sheet.createRow(0);
+			for(int i = 0; i < columnCount; i++){
+				XSSFCell cell = headerRow.createCell(i);
+				cell.setCellValue(model.getColumnName(i));
+			}
+
+			//Write out instance data in following rows
+			for(int i=0; i < model.getRowCount(); i++) {
+				XSSFRow dataRow = sheet.createRow(i+1);
+				for(int j=0; j < columnCount; j++) {
+					XSSFCell cell = dataRow.createCell(j);
+					cell.setCellValue(model.getValueAt(i,j).toString());
+				}
+			}
+
+			//Write all data to file
+			FileOutputStream stream = new FileOutputStream(this.filename);
+			wb.write(stream);
+			stream.close();
+		} catch(IOException e) {
+			successfulExport = false;
+			System.out.println(e);
+		}
+
+		return successfulExport;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		boolean success = runExport();
+
+		if(success) {
+			Utility.showMessage("Data export complete:\n\n" + this.filename);
+		} else {
+			Utility.showError("Export failed. Please check export folder structure (" + this.WORKING_DIR + this.EXPORT_FOLDER + ").");
+		}
+	}
+
+	@Override
+	public void setView(JComponent view) { }
+}
