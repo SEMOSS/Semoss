103253f001075c1eae10d22280d04941485c880e
mahkhalil <mahkhalil@deloitte.com>
2014-07-31 13:59:22 +0000

    clustering data processing
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@2926 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/algorithm/impl/DistanceCalculator.java
+++ src/prerna/algorithm/impl/DistanceCalculator.java
@@ -2,27 +2,51 @@ package prerna.algorithm.impl;
 
 import java.math.BigDecimal;
 
+import prerna.error.BadInputException;
+
 public class DistanceCalculator {
 	
-	public Double calculateEuclidianDistance(Double[] values) {
-		Double sum = (double) 0;
-		for(int i = 0; i < values.length; i++) {
-			sum += Math.pow(values[i],2);
+	public Double calculateEuclidianDistance(Double[] values1, Double[] values2) throws BadInputException {
+		if(values1.length != values2.length) {
+			throw new BadInputException("The inputs must be of the same size!");
 		}
-		return Math.sqrt(sum);
+		
+		Double sumSquare = (double) 0;
+		for(int i = 0; i < values1.length; i++) {
+			sumSquare += Math.pow(values1[i]-values2[i],2);
+		}
+		
+		return Math.sqrt(sumSquare);
 	}
 	
-	public BigDecimal calculateEuclidianDistance(BigDecimal[] values) {
-		BigDecimal sum = new BigDecimal(0);
-		for(int i = 0; i < values.length; i++) {
-			sum.add(values[i].pow(2));
+	public BigDecimal calculateEuclidianDistance(BigDecimal[] values1, BigDecimal[] values2) throws BadInputException {
+		if(values1.length != values2.length) {
+			throw new BadInputException("The inputs must be of the same size!");
 		}
-		return sqrt(sum);
+		
+		BigDecimal sumSquare = new BigDecimal(0);
+		for(int i = 0; i < values1.length; i++) {
+			sumSquare.add(values1[i].subtract(values2[i]).pow(2));
+		}
+		return sqrt(sumSquare);
 	}
 
 	private BigDecimal sqrt(BigDecimal num) {
 		BigDecimal val = new BigDecimal(Math.sqrt(num.doubleValue()));
 		return val.add(new BigDecimal(num.subtract(val.multiply(val)).doubleValue() / (val.doubleValue() * 2.0) ));
+	}
+
+	public double calculateEuclidianDistance(double[] values1, double[] values2) throws BadInputException {
+		if(values1.length != values2.length) {
+			throw new BadInputException("The inputs must be of the same size!");
+		}
+		
+		Double sumSquare = (double) 0;
+		for(int i = 0; i < values1.length; i++) {
+			sumSquare += Math.pow(values1[i]-values2[i],2);
+		}
+		
+		return Math.sqrt(sumSquare);
 	}
 
 }
