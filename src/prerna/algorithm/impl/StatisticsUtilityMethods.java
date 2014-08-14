4818e655fa59d3ae535c5e59bb4273f09ea40a6f
Maher Khalil <themaherkhalil@gmail.com>
2014-08-14 00:07:18 +0000

    updated cluster algorithm to send data regarding bar charts, re-factored some code to move away from java data structures and focus mainly on primitives and arrays
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@3122 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3347e4dc7a1131b2e6d576e3630851892f26b31e

---
+++ src/prerna/algorithm/impl/StatisticsUtilityMethods.java
@@ -0,0 +1,21 @@
+package prerna.algorithm.impl;
+
+import java.util.Arrays;
+
+public class StatisticsUtilityMethods {
+
+	public static double quartile(double[] values, double lowerPercent, boolean ordered) {
+		if (values == null || values.length == 0) {
+			throw new IllegalArgumentException("The data array either is null or does not contain any data.");
+		}
+
+		// Rank order the values if not already ordered
+		if(!ordered) {
+			Arrays.sort(values);
+		}
+
+		int n = (int) Math.round(values.length * lowerPercent / 100);
+		return values[n];
+	}
+
+}
