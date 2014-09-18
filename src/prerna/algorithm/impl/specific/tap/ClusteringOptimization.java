24ffddb406fb65b269277f336ee71f4bb3b7990a
Maher Khalil <themaherkhalil@gmail.com>
2014-09-18 20:21:15 +0000

    updating clustering algorithm
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@3425 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 849e0d459127055035d20c68450c7158d9b7f4b6

---
+++ src/prerna/algorithm/impl/specific/tap/ClusteringOptimization.java
@@ -0,0 +1,61 @@
+package prerna.algorithm.impl.specific.tap;
+
+import java.util.ArrayList;
+
+import org.apache.commons.math3.optim.MaxEval;
+import org.apache.commons.math3.optim.OptimizationData;
+import org.apache.commons.math3.optim.nonlinear.scalar.GoalType;
+import org.apache.commons.math3.optim.univariate.BrentOptimizer;
+import org.apache.commons.math3.optim.univariate.MultiStartUnivariateOptimizer;
+import org.apache.commons.math3.optim.univariate.SearchInterval;
+import org.apache.commons.math3.optim.univariate.UnivariateObjectiveFunction;
+import org.apache.commons.math3.optim.univariate.UnivariateOptimizer;
+import org.apache.commons.math3.optim.univariate.UnivariatePointValuePair;
+import org.apache.commons.math3.random.RandomGenerator;
+import org.apache.commons.math3.random.Well1024a;
+
+import prerna.algorithm.impl.ClusterOptFunction;
+import prerna.algorithm.impl.ClusteringAlgorithm;
+
+public class ClusteringOptimization extends ClusteringAlgorithm {
+
+	public ClusteringOptimization(ArrayList<Object[]> masterTable, String[] varNames) {
+		super(masterTable, varNames);
+	}
+
+	public void determineOptimalCluster() {
+		ClusterOptFunction f = new ClusterOptFunction();
+		f.setList(masterTable);
+		f.setNames(varNames);
+		UnivariateOptimizer optimizer = new BrentOptimizer(1E-6, 1E-6);
+        RandomGenerator rand = new Well1024a(500);
+        MultiStartUnivariateOptimizer multiOpt = new MultiStartUnivariateOptimizer(optimizer, 5, rand);
+        UnivariateObjectiveFunction objF = new UnivariateObjectiveFunction(f);
+        SearchInterval search = new SearchInterval(2, (int) Math.round(Math.sqrt(masterTable.size()))); //considering range from 2 to square root of number of instances
+        MaxEval eval = new MaxEval(200);
+        
+        OptimizationData[] data = new OptimizationData[]{search, objF, GoalType.MAXIMIZE, eval};
+        UnivariatePointValuePair pair = multiOpt.optimize(data);
+        
+        // must calculate two endpoints to determine which is larger
+        double val = pair.getPoint();
+        int numClusterCeil = (int) Math.ceil(val);
+        int numClusterFloor = (int) Math.floor(val);
+        
+        double avg1 = f.value(numClusterCeil);
+        double avg2 = f.value(numClusterFloor);
+        if(avg1 > avg2) {
+        	numClusters = numClusterCeil;
+        } else {
+        	numClusters = numClusterFloor;
+        }
+        f.closerWriter();
+	}
+	
+	public int getNumClusters(){
+		return numClusters;
+	}
+	
+	
+	
+}
