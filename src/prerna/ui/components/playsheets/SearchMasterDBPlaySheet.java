14c030756109b14ead2ab67844a2dd984b637349
bisutton <bisutton@deloitte.com>
2014-09-22 22:44:06 +0000

    Adjusting search master db playsheets to flatten hash for grid. Still need to go through debug part of SearchMasterDB
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@3461 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: de1b289932bd9016223af23c1c07aa96e42c6d7a

--- src/prerna/ui/components/playsheets/SearchMasterDBPlaySheet.java
+++ src/prerna/ui/components/playsheets/SearchMasterDBPlaySheet.java
@@ -19,7 +19,9 @@
 package prerna.ui.components.playsheets;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Hashtable;
+import java.util.Iterator;
 
 import prerna.algorithm.impl.SearchMasterDB;
 import prerna.om.SEMOSSEdge;
@@ -64,9 +66,33 @@ public class SearchMasterDBPlaySheet extends GridPlaySheet{
 		ArrayList<Hashtable<String, Object>> hashArray = searchAlgo.searchDB();
 		flattenHash(hashArray);
 	}
-	
+
 	private void flattenHash(ArrayList<Hashtable<String, Object>> hashArray){
 		//TODO write this method that stores headers and list
+		//assuming every hash has the same keys
+		//get the first hash to know what keys we are working with (these are going to be our headers)
+		if(hashArray.size()>0)
+		{
+			list = new ArrayList<Object[]>();
+			Hashtable<String, Object> exampleHash = hashArray.get(0);
+			Collection<String> keySet = exampleHash.keySet();
+			this.names = new String[keySet.size()];
+			Iterator<String> keyIt = keySet.iterator();
+			for(int namesIdx = 0; keyIt.hasNext(); namesIdx++){
+				this.names[namesIdx] = keyIt.next();
+			}
+			
+			// now that names has been created, just need to fill out the list to match the headers
+			for(Hashtable<String,Object> hash : hashArray){
+				Object[] newRow = new Object[this.names.length];
+				for(int namesIdx = 0; namesIdx<this.names.length; namesIdx++)
+				{
+					newRow[namesIdx] = hash.get(this.names[namesIdx]);
+				}
+				list.add(newRow);
+			}
+				
+		}
 	}
 	
 	private void createMetamodelSubgraphData() {
