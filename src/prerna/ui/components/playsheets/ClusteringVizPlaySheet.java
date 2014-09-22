5abe21e445f05650edc9e27e1f0f55f049eabf3b
ksmart <ksmart@deloitte.com>
2014-09-22 21:10:56 +0000

    added in clustering refresh tab
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@3456 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ui/components/playsheets/ClusteringVizPlaySheet.java
+++ src/prerna/ui/components/playsheets/ClusteringVizPlaySheet.java
@@ -1,21 +1,28 @@
 package prerna.ui.components.playsheets;
 
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import aurelienribon.ui.css.Style;
 import prerna.algorithm.impl.AbstractClusteringAlgorithm;
 import prerna.algorithm.impl.AgglomerativeClusteringAlgorithm;
 import prerna.algorithm.impl.ArrayUtilityMethods;
@@ -28,6 +35,10 @@ import prerna.ui.components.GridFilterData;
 import prerna.ui.components.GridTableModel;
 import prerna.ui.components.GridTableRowSorter;
 import prerna.ui.components.NewScrollBarUI;
+import prerna.ui.main.listener.impl.ClusteringRefreshParamListener;
+import prerna.ui.main.listener.specific.tap.OptFunctionRadioBtnListener;
+import prerna.ui.swing.custom.CustomButton;
+import prerna.util.CSSApplication;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
 
@@ -39,6 +50,9 @@ public class ClusteringVizPlaySheet extends BrowserPlaySheet{
 	private double n;
 	private String type = "";
 	private ArrayList<Object[]> clusterInfo;
+	private ArrayList<JCheckBox> paramCheckboxes;
+	private ArrayList<Object []> masterList;
+	private String[] masterNames;
 
 	//indexing used for bar graph visualizations
 	private int[] numericalPropIndices;
@@ -54,6 +68,14 @@ public class ClusteringVizPlaySheet extends BrowserPlaySheet{
 	@Override
 	public void createView() {
 		super.createView();
+		if(jTab.getTabCount()==1) {
+			addSelectorTab();
+			addGridTab();
+		}
+		
+		new CSSApplication(getContentPane());
+	}
+	public void addGridTab() {
 		JPanel panel = new JPanel();
 		table = new JTable();
 		panel.add(table);
@@ -77,7 +99,69 @@ public class ClusteringVizPlaySheet extends BrowserPlaySheet{
 
 		jTab.addTab("Raw Data", panel);
 	}
+	public void addSelectorTab() {
+		JPanel panel = new JPanel();
+		
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblParamSelect = new JLabel("Select Parameters:");
+		lblParamSelect.setFont(new Font("Tahoma", Font.BOLD, 12));
+		GridBagConstraints gbc_lblParamSelect = new GridBagConstraints();
+		gbc_lblParamSelect.anchor = GridBagConstraints.WEST;
+		gbc_lblParamSelect.insets = new Insets(10, 5, 0, 0);
+		gbc_lblParamSelect.gridx = 0;
+		gbc_lblParamSelect.gridy = 0;
+		panel.add(lblParamSelect, gbc_lblParamSelect);
+		
+		paramCheckboxes = new ArrayList<JCheckBox>();
+		
+		for(int i=1;i<masterNames.length;i++) {
+			String checkboxLabel = masterNames[i];
+			JCheckBox checkbox = new JCheckBox(checkboxLabel);
+			checkbox.setName(checkboxLabel+"checkBox");
+			checkbox.setSelected(true);
+			GridBagConstraints gbc_checkbox = new GridBagConstraints();
+			gbc_checkbox.anchor = GridBagConstraints.NORTHWEST;
+			gbc_checkbox.fill = GridBagConstraints.NONE;
+			gbc_checkbox.insets = new Insets(5, 10, 0, 0);
+			gbc_checkbox.gridx = 0;
+			gbc_checkbox.gridy = i;
+			panel.add(checkbox, gbc_checkbox);
+			paramCheckboxes.add(checkbox);
+		}
+		
+		JButton btnRefreshParam = new CustomButton("Refresh Parameters");
+		btnRefreshParam.setFont(new Font("Tahoma", Font.BOLD, 11));
+		GridBagConstraints gbc_btnRefreshParam = new GridBagConstraints();
+		gbc_btnRefreshParam.insets = new Insets(10, 5, 0, 0);
+		gbc_btnRefreshParam.anchor = GridBagConstraints.NORTH;
+		gbc_btnRefreshParam.fill = GridBagConstraints.VERTICAL;
+		gbc_btnRefreshParam.gridx = 0;
+		gbc_btnRefreshParam.gridy = names.length;
+		panel.add(btnRefreshParam, gbc_btnRefreshParam);
+		Style.registerTargetClassName(btnRefreshParam,  ".createBtn");
 
+		ClusteringRefreshParamListener refListener = new ClusteringRefreshParamListener();
+		refListener.setPlaySheet(this);
+		refListener.setCheckBoxes(paramCheckboxes);
+		refListener.setMasterData(masterNames, masterList);
+		btnRefreshParam.addActionListener(refListener);
+		
+		jTab.insertTab("Param Selector",null,panel,null,0);
+		jTab.setSelectedIndex(0);
+	}
+
+	public void filterData(String[] filteredNames,ArrayList<Object []> filteredList) {
+		this.names = filteredNames;
+		this.list = filteredList;
+		createData();
+		createView();
+	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
@@ -314,7 +398,11 @@ public class ClusteringVizPlaySheet extends BrowserPlaySheet{
 
 	@Override
 	public void createData() {
-		processQuery();
+		if(masterList==null) {
+			processQuery();
+			masterList = list;
+			masterNames = names;
+		}
 		AbstractClusteringAlgorithm clusterAlg;
 		//TODO: need to split out the process to deal with duplicate values
 		if(type.equalsIgnoreCase("agglomerative")){
---
+++ src/prerna/ui/main/listener/impl/ClusteringRefreshParamListener.java
@@ -0,0 +1,104 @@
+/*******************************************************************************
+ * Copyright 2013 SEMOSS.ORG
+ * 
+ * This file is part of SEMOSS.
+ * 
+ * SEMOSS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SEMOSS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SEMOSS.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+package prerna.ui.main.listener.impl;
+
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
+
+import prerna.ui.components.playsheets.ClusteringVizPlaySheet;
+
+/**
+ * Determines which functional areas the user wants to incorporate in RFP report
+ * Used to determine if user wants to include HSD, HSS, or FHP functional areas in RFP report
+ * Will populate sourceSelectPanel with all capabilities included in functional areas
+ */
+public class ClusteringRefreshParamListener extends AbstractListener {
+	
+	//need to pass the playsheet, the checkboxes, the names, and the master data list,
+	//sends back the updates names and data list
+	ClusteringVizPlaySheet playSheet;
+	ArrayList<JCheckBox> paramCheckboxes;
+	String[] masterNames;
+	ArrayList<Object []> masterList;
+	
+	/**
+	 * Updates the parameters to cluster on based on the params selected
+	 * @param e ActionEvent
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Integer numberSelected = 0;
+		for(int i=0;i<paramCheckboxes.size();i++) {
+			if(paramCheckboxes.get(i).isSelected())
+				numberSelected++;
+		}
+		
+		String[] filteredNames = new String[numberSelected+1];
+		filteredNames[0] = masterNames[0];
+		int colInd=1;
+		for(int i=0;i<paramCheckboxes.size();i++) {
+			if(paramCheckboxes.get(i).isSelected()) {
+				filteredNames[colInd] = masterNames[1+i];
+				colInd++;
+			}
+		}
+
+		ArrayList<Object[]> filteredList = new ArrayList<Object []>();
+		for(Object[] row : masterList) {
+			Object[] filteredRow = new Object[numberSelected+1];
+			filteredRow[0] = row[0];//whatever object name we're clustering on
+			colInd=1;
+			for(int i=0;i<paramCheckboxes.size();i++) {
+				if(paramCheckboxes.get(i).isSelected()) {
+					filteredRow[colInd] = row[1+i];
+					colInd++;
+				}
+			}
+			filteredList.add(filteredRow);
+		}
+		
+		playSheet.filterData(filteredNames,filteredList);
+
+	}
+
+	public void setPlaySheet(ClusteringVizPlaySheet playSheet) {
+		this.playSheet = playSheet;
+	}
+	
+	public void setCheckBoxes(ArrayList<JCheckBox> paramCheckboxes) {
+		this.paramCheckboxes = paramCheckboxes;
+	}
+
+	public void setMasterData(String[] masterNames,ArrayList<Object []> masterList) {
+		this.masterNames = masterNames;
+		this.masterList = masterList;
+	}
+	
+	/**
+	 * Override method from AbstractListener
+	 * @param view JComponent
+	 */
+	@Override
+	public void setView(JComponent view) {
+
+	}
+}
