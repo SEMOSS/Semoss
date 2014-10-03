856c311fb05d7474e0b72dae01029801e6a8b430
mahkhalil <mahkhalil@deloitte.com>
2014-10-03 14:44:02 +0000

    updated classification algorithm
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@3602 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/algorithm/weka/impl/ClassificationFactory.java
@@ -0,0 +1,28 @@
+package prerna.algorithm.weka.impl;
+
+import weka.classifiers.Classifier;
+import weka.classifiers.functions.SimpleLogistic;
+import weka.classifiers.rules.DecisionTable;
+import weka.classifiers.rules.PART;
+import weka.classifiers.trees.DecisionStump;
+import weka.classifiers.trees.J48;
+import weka.classifiers.trees.LMT;
+import weka.classifiers.trees.REPTree;
+
+public final class ClassificationFactory {
+
+	public static Classifier create(String type) {
+		switch(type) {
+		case "J48" : return new J48();
+		case "PART" : return new PART();
+		case "DecisionTable" : return new DecisionTable();
+		case "DecisionStump" : return new DecisionStump();
+		case "REPTree" : return new REPTree();
+		case "LMT" : return new LMT();
+		case "SimpleLogistic" : return new SimpleLogistic();
+		}
+
+		return null;
+	}
+
+}
