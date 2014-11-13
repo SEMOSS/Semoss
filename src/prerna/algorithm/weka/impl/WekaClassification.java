0baafef7fc5fbc74e4f05c6c70179daebbad5ad5
mahkhalil <mahkhalil@deloitte.com>
2014-11-13 15:24:07 +0000

    moving towards creating decision tree visualization
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@4489 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/algorithm/weka/impl/WekaClassification.java
+++ src/prerna/algorithm/weka/impl/WekaClassification.java
@@ -1,105 +1,55 @@
 package prerna.algorithm.weka.impl;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
-import weka.core.FastVector;
+import weka.classifiers.trees.DecisionStump;
 import weka.core.Instances;
 
 public class WekaClassification {
 
 	private Instances data;
-	private double[] accuracy;
-	private double[] precision;
 	
 	private String[] names;
 	private ArrayList<Object[]> list;
 	private Classifier model;
+	private String tree;
 	
-	PrintWriter writer = null;
-
 	public WekaClassification(String name, ArrayList<Object[]> list, String[] names, String modelName) {
 		this.list = list;
 		this.names = names;
 		this.model = ClassificationFactory.create(modelName);
-		
-		//writer only used for debugging
-		//TODO: delete once stable
-		if(writer == null) {
-			try {
-				writer = new PrintWriter("Classifier_Results_" + modelName + ".txt");
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-		} 
 	}
 
-	public double[] getAccuracy() {
-		return accuracy.clone();
-	}
-
-	public double[] getPrecision() {
-		return precision.clone();
+	public String getTreeAsString() {
+		return tree;
 	}
 	
 	public void execute() throws Exception{
 		int numAttributes = names.length;
 		
-		// Run for each attribute
-		accuracy = new double[numAttributes - 1];
-		precision = new double[numAttributes - 1];
+		this.data = WekaUtilityMethods.createInstancesFromQuery("Test", list, names, numAttributes - 1);
+		data.setClassIndex(numAttributes - 1);
+		// Do 10-split cross validation
+		Instances[][] split = crossValidationSplit(data, 10);
+
+		// Separate split into training and testing arrays
+		Instances[] trainingSplits = split[0];
+		Instances[] testingSplits = split[1];
 		
-		int i;
-		for(i = 1; i < numAttributes; i++) {
-			this.data = WekaUtilityMethods.createInstancesFromQuery("Test", list, names, i);
-			data.setClassIndex(i);
-			// Do 10-split cross validation
-			Instances[][] split = crossValidationSplit(data, 10);
-	
-			// Separate split into training and testing arrays
-			Instances[] trainingSplits = split[0];
-			Instances[] testingSplits = split[1];
-	
-			//Collect every group of predictions for current model in FastVector
-			FastVector predCorrect = new FastVector();
-			FastVector kappaValues = new FastVector();
-			
-			// For each training-testing split pair, train and test the classifier
-			writer.println(names[i]);
-			int j;
-			for(j = 0; j < trainingSplits.length; j++) {
-				Evaluation validation = classify(model, trainingSplits[j], testingSplits[j]);
-				predCorrect.addElement(validation.pctCorrect());
-				kappaValues.addElement(validation.kappa());
-	
-				writer.println(validation.toSummaryString(true));
-				writer.println("Kappa Value                             " + validation.kappa());
-				writer.println(validation.toMatrixString());
+		double pctCorrect = -1; // assign any negative value
+		// For each training-testing split pair, train and test the classifier
+		int j;
+		for(j = 0; j < trainingSplits.length; j++) {
+			Evaluation validation = classify(model, trainingSplits[j], testingSplits[j]);
+			double newPctCorrect = validation.pctCorrect();
+			if(newPctCorrect > pctCorrect) {
+				tree = model.toString();
 			}
-	
-			accuracy[i-1] = calculateAccuracy(predCorrect);
-			precision[i-1] = calculatePercision(kappaValues);
-			// Print current classifier's name and accuracy in a complicated,
-			// but nice-looking way.
-			writer.println("Accuracy for " + names[i] + ": "
-					+ String.format("%.2f%%", accuracy[i-1])
-					+ "\n---------------------------------");
-			writer.println("Percision for " + names[i] + ": "
-					+ String.format("%.2f%%", precision[i-1])
-					+ "\n---------------------------------");
-	
-			System.out.println("Accuracy for " + names[i] + ": "
-					+ String.format("%.2f%%", accuracy[i-1])
-					+ "\n---------------------------------");
-			System.out.println("Percision for " + names[i] + ": "
-					+ String.format("%.2f", precision[i-1])
-					+ "\n---------------------------------");
 		}
 
-		writer.close();
+		System.out.println(tree);
 	}
 
 	public Instances[][] crossValidationSplit(Instances data, int numberOfFolds) {
@@ -120,25 +70,5 @@ public class WekaClassification {
 		evaluation.evaluateModel(model, testingSet);
 
 		return evaluation;
-	}
-
-	public double calculateAccuracy(FastVector predCorrect) {
-		double sumPerCorrect = 0;
-		int size = predCorrect.size();
-		for (int i = 0; i < size; i++) {
-			sumPerCorrect+= (double) predCorrect.elementAt(i);
-		}
-
-		return sumPerCorrect / size;
-	}
-
-	private double calculatePercision(FastVector kappaValues) {
-		double sumKappa = 0;
-		int size = kappaValues.size();
-		for (int i = 0; i < size; i++) {
-			sumKappa+= (double) kappaValues.elementAt(i);
-		}
-
-		return sumKappa / size;
 	}
 }
--- src/prerna/ui/components/playsheets/WekaClassificationPlaySheet.java
+++ src/prerna/ui/components/playsheets/WekaClassificationPlaySheet.java
@@ -1,7 +1,5 @@
 package prerna.ui.components.playsheets;
 
-import java.util.ArrayList;
-
 import prerna.algorithm.weka.impl.WekaClassification;
 import prerna.rdf.engine.impl.SesameJenaSelectStatement;
 
@@ -18,25 +16,13 @@ public class WekaClassificationPlaySheet extends GridPlaySheet{
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		double[] accuracy = alg.getAccuracy();
-		double[] precision = alg.getPrecision();
-		list = new ArrayList<Object[]>();
-		int size = accuracy.length;
-		int i;
-		for(i = 0; i < size; i++) {
-			Object[] row = new Object[]{names[i+1], String.format("%.2f%%", accuracy[i]), String.format("%.2f", precision[i])};
-			list.add(row);
-		}
-		
-		names = new String[]{"Attribute","Accuracy","Precision"};
 	}
 	
 	@Override
 	public void setQuery(String query) {
 		String[] querySplit = query.split("\\+\\+\\+");
 		this.query = querySplit[0];
-		this.modelName = querySplit[1];
+		this.modelName = querySplit[1].trim();
 	}
 	
 	@Override
