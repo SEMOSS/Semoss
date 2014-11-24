bb1d7fffd7c18a28b29ff5ce2797890b0b6dc900
kepark <kepark@deloitte.com>
2014-11-24 17:41:36 +0000

    Added system capability heatmap excel exporter and template. Used by HR_Core/System-Perspective/Q5. Report is created in export/Reports.
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@4705 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ui/components/specific/tap/SysCapSimHeatMapSheet.java
+++ src/prerna/ui/components/specific/tap/SysCapSimHeatMapSheet.java
@@ -18,11 +18,22 @@
  ******************************************************************************/
 package prerna.ui.components.specific.tap;
 
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Hashtable;
 
+import javax.swing.JButton;
+
 import prerna.rdf.engine.api.IEngine;
+import prerna.util.Constants;
 import prerna.util.DIHelper;
+import prerna.util.Utility;
 
 /**
  * Heat Map that shows how much a System Supports a Capability
@@ -30,9 +41,14 @@ import prerna.util.DIHelper;
 public class SysCapSimHeatMapSheet extends SimilarityHeatMapSheet {
 
 	private final String hrCoreDB = "HR_Core";
+	private String fileLoc;
 	private IEngine coreDB = (IEngine) DIHelper.getInstance().getLocalProp(hrCoreDB);
 	private final String capabilityQuery = "SELECT DISTINCT ?Capability WHERE {{?Capability <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/Capability>}}";
 	private final String businessProcessQuery = "SELECT DISTINCT ?BusinessProcess WHERE {{?BusinessProcess <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://semoss.org/ontologies/Concept/BusinessProcess>}}";	
+	String workingDir = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
+	private ArrayList<String> systemNames;
+	private ArrayList<String> capabilityNames;
+	private JButton excelExportButton;
 	
 	/**
 	 * Constructor for CapSimHeatMapSheet.
@@ -42,34 +58,60 @@ public class SysCapSimHeatMapSheet extends SimilarityHeatMapSheet {
 	}	
 	
 	public void createData() {
+		capabilityNames = new ArrayList<String>();
+		systemNames = new ArrayList<String>();
 		addPanel();
 		SimilarityFunctions sdf = new SimilarityFunctions();	
 		SysBPCapInsertProcessor processor = new SysBPCapInsertProcessor(0.0, 0.0, "AND");
 		String comparisonType = this.query;
 		logger.info("Creating " + comparisonType + " to System Heat Map.");
 		
-			if (comparisonType.equals("Capability")) {
-				allHash.put("xAxisTitle", "Capability");
-				updateProgressBar("10%...Getting " + comparisonType + " list for evaluation", 10);
-				comparisonObjectList = sdf.createComparisonObjectList(hrCoreDB, capabilityQuery);
-				setComparisonObjectTypes("Capability", "System");
-				updateProgressBar("35%...Querying Data", 35);
-				processor.genStorageInformation(coreDB, comparisonType);
-			}
-			else if(comparisonType.equals("BusinessProcess")) {
-				allHash.put("xAxisTitle", "BusinessProcess");
-				updateProgressBar("10%...Getting " + comparisonType + " list for evaluation", 10);
-				comparisonObjectList = sdf.createComparisonObjectList(hrCoreDB, businessProcessQuery);
-				setComparisonObjectTypes("BusinessProcess", "System");
-				updateProgressBar("35%...Querying Data", 35);
-				processor.genStorageInformation(coreDB, comparisonType);
-			}		
+		if (comparisonType.equals("Capability"))
+		{
+			allHash.put("xAxisTitle", "Capability");
+			updateProgressBar("10%...Getting " + comparisonType + " list for evaluation", 10);
+			comparisonObjectList = sdf.createComparisonObjectList(hrCoreDB, capabilityQuery);
+			setComparisonObjectTypes("Capability", "System");
+			updateProgressBar("35%...Querying Data", 35);
+			processor.genStorageInformation(coreDB, comparisonType);
+		} else if (comparisonType.equals("BusinessProcess"))
+		{
+			allHash.put("xAxisTitle", "BusinessProcess");
+			updateProgressBar("10%...Getting " + comparisonType + " list for evaluation", 10);
+			comparisonObjectList = sdf.createComparisonObjectList(hrCoreDB, businessProcessQuery);
+			setComparisonObjectTypes("BusinessProcess", "System");
+			updateProgressBar("35%...Querying Data", 35);
+			processor.genStorageInformation(coreDB, comparisonType);
+		}
+
 		sdf.setComparisonObjectList(comparisonObjectList);
 		HashMap getData = new HashMap();
 		
 		Hashtable resultHash = new Hashtable();
-		resultHash.putAll(processor.storageHash);		
+		resultHash.putAll(processor.storageHash);
 		
+		// comparisonObjectList contains every capability, even the ones not shown on heatmap
+		// causing the program to crash due to too many system capability combinations
+		for (String capabilityKey : processor.storageHash.get("Data").keySet())
+		{
+			capabilityNames.add(capabilityKey);
+			for (String systemKey : processor.storageHash.get("Data").get(capabilityKey).keySet())
+			{
+				if (!systemNames.contains(systemKey))
+					systemNames.add(systemKey);
+			}
+		}
+		for (String capabilityKey : processor.storageHash.get("BLU").keySet())
+		{
+			if (!capabilityNames.contains(capabilityKey))
+				capabilityNames.add(capabilityKey);
+			for (String systemKey : processor.storageHash.get("BLU").get(capabilityKey).keySet())
+			{
+				if (!systemNames.contains(systemKey))
+					systemNames.add(systemKey);
+			}
+		}
+
 		updateProgressBar("50%...Evaluating Data Objects Created for a " + comparisonType, 50);
 		getData = (HashMap) resultHash.get(processor.DATAC);
 		Hashtable dataCScoreHash = convertMapToTable(getData);
@@ -113,5 +155,63 @@ public class SysCapSimHeatMapSheet extends SimilarityHeatMapSheet {
 		}
 		
 		return returnHash;
+	}
+	
+	@Override
+	public void createControlPanel()
+	{
+		super.createControlPanel();
+		addExportExcelButton(5);
+		
+		this.controlPanel.setPlaySheet(this);
+	}
+	
+	public void addExportExcelButton(int gridXidx)
+	{
+		excelExportButton = new JButton("Export to Excel");
+		
+		// SimHeatMapExcelExportListener excelExportListener = new SimHeatMapExcelExportListener();
+		excelExportButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				writeToFile();
+				Utility.showMessage("Export successful: " + fileLoc);
+			}
+		});
+		excelExportButton.setToolTipText("Export percentage data to excel");
+		
+		GridBagConstraints gbc_excelExportButton = new GridBagConstraints();
+		gbc_excelExportButton.insets = new Insets(10, 0, 0, 5);
+		gbc_excelExportButton.anchor = GridBagConstraints.WEST;
+		gbc_excelExportButton.gridx = gridXidx;
+		gbc_excelExportButton.gridy = 0;
+		controlPanel.add(excelExportButton, gbc_excelExportButton);
+	}
+
+	/**
+	 * Create the report file name and location, and call the writer to write the report for the specified system Create the
+	 * location for the fact sheet report template
+	 * 
+	 * @param databaseName
+	 *            String containing the system name to produce the fact sheet
+	 * @param systemDataHash
+	 *            Hashtable containing the results for the query for the specified system
+	 */
+	public void writeToFile()
+	{
+		SysCapSimHeatMapWriter writer = new SysCapSimHeatMapWriter();
+		String folder = System.getProperty("file.separator") + "export" + System.getProperty("file.separator") + "Reports"
+				+ System.getProperty("file.separator");
+		String writeFileName = "SystemCapabilityHeatMap_for_" + coreDB.getEngineName().replaceAll(":", "") + "_"
+				+ DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT).format(new Date()).replace(":", "").replaceAll(" ", "_")
+				+ ".xlsx";
+		
+		fileLoc = workingDir + folder + writeFileName;
+		String templateFileName = "HeatMap_Template.xlsx";
+		String templateLoc = workingDir + folder + templateFileName;
+		logger.info(fileLoc);
+		
+		writer.exportExcelPercent(fileLoc, templateLoc, capabilityNames, systemNames, paramDataHash);
 	}
 }
---
+++ src/prerna/ui/components/specific/tap/SysCapSimHeatMapWriter.java
@@ -0,0 +1,104 @@
+package prerna.ui.components.specific.tap;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import prerna.util.Utility;
+
+public class SysCapSimHeatMapWriter
+{
+	public void exportExcelPercent(String fileLoc, String templateFileLoc, ArrayList<String> capabilities, ArrayList<String> systems,
+			Hashtable<String, Hashtable<String, Hashtable<String, Object>>> paramDataHash)
+	{
+		XSSFWorkbook wb;
+		// if a report template exists, then create a copy of the template, otherwise create a new workbook
+		if (templateFileLoc != null)
+		{
+			try
+			{
+				wb = (XSSFWorkbook) WorkbookFactory.create(new File(templateFileLoc));
+			} catch (InvalidFormatException e)
+			{
+				wb = new XSSFWorkbook();
+			} catch (IOException e)
+			{
+				wb = new XSSFWorkbook();
+			}
+		} else
+		{
+			wb = new XSSFWorkbook();
+		}
+		
+		XSSFSheet sheet = wb.getSheet("Heat Map Percentages");
+
+
+		Hashtable<String, Hashtable<String, Object>> allData = paramDataHash.get("Data_Objects_Created");
+		Hashtable<String, Hashtable<String, Object>> allBLU = paramDataHash.get("Business_Logic_Provided");
+		Hashtable<String, Object> systemData = new Hashtable<String, Object>();
+		Hashtable<String, Object> systemBLU = new Hashtable<String, Object>();
+		ArrayList<String> capabilityNames = capabilities;
+		ArrayList<String> systemNames = systems;
+		
+		java.util.Collections.sort(capabilityNames);
+		java.util.Collections.sort(systemNames);
+
+		int column = 1;
+		int row = 2;
+		
+		XSSFRow selectedRow = sheet.getRow(1);
+		XSSFCell selectedCell;
+		XSSFCellStyle systemStyle = sheet.getRow(2).getCell(0).getCellStyle();
+		XSSFCellStyle capabilityStyle = sheet.getRow(1).getCell(1).getCellStyle();
+		XSSFCellStyle dataStyle = sheet.getRow(2).getCell(1).getCellStyle();
+
+		for (String capability : capabilityNames)
+		{
+			selectedCell = selectedRow.createCell(column);
+			selectedCell.setCellStyle(capabilityStyle);
+			selectedCell.setCellValue(capability);
+			column++;
+		}
+		
+		for (String systemKey : systemNames)
+		{
+			selectedRow = sheet.createRow(row);
+			selectedCell = selectedRow.createCell(0);
+			selectedCell.setCellValue(systemKey);
+			selectedCell.setCellStyle(systemStyle);
+
+			for (int columnMinusOne = 0; columnMinusOne < capabilityNames.size(); columnMinusOne++)
+			{
+				String key = capabilityNames.get(columnMinusOne) + "-" + systemKey;
+				
+				selectedCell = selectedRow.createCell(columnMinusOne + 1);
+				selectedCell.setCellStyle(dataStyle);
+				Object dataScore;
+				Object bluScore;
+				if (allData.get(key) != null)
+					dataScore = allData.get(key).get("Score");
+				else
+					dataScore = 0.0;
+				if (allBLU.get(key) != null)
+					bluScore = allBLU.get(key).get("Score");
+				else
+					bluScore = 0.0;
+
+				selectedCell.setCellValue((((Double) dataScore) + ((Double) bluScore)) / 200); // Because cell format is set
+																								// as percentage
+			}
+			row++;
+		}
+		
+		Utility.writeWorkbook(wb, fileLoc);
+	}
+}
