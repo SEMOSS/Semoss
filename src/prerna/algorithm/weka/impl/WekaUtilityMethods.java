3708b78e43e47cd50b91655a2a2e42c7e3e1634f
mahkhalil <mahkhalil@deloitte.com>
2014-12-08 22:01:06 +0000

    fixed generating instances for missing data due to update in weka jar
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@4889 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: cf828ef2fd0ee1ac8ea5fded027e2bd00334d136

--- src/prerna/algorithm/weka/impl/WekaUtilityMethods.java
+++ src/prerna/algorithm/weka/impl/WekaUtilityMethods.java
@@ -39,13 +39,16 @@ public final class WekaUtilityMethods {
 			int numNumeric = 0;
 			for(j = 0; j < numInstances; j++) {
 				Object dataElement = dataList.get(j)[i];
-				String type = Utility.processType(dataElement.toString());
-				if(type.equals("STRING")) {
-					nominalValues[i].add(dataElement.toString());
-					numNominal++;
-				} else {
-					numericValues[i][j] = (double) dataElement;
-					numNumeric++;
+				// ignore missing values to determine data type of column
+				if(!dataElement.toString().isEmpty()) {
+					String type = Utility.processType(dataElement.toString());
+					if(type.equals("STRING")) {
+						nominalValues[i].add(dataElement.toString());
+						numNominal++;
+					} else {
+						numericValues[i][j] = (double) dataElement;
+						numNumeric++;
+					}
 				}
 			}
 
@@ -96,9 +99,7 @@ public final class WekaUtilityMethods {
 					dataEntry.setValue(j, binForInstance[i]);
 				} else {
 					Object valAttr = dataRow[j];
-					if(valAttr.toString().isEmpty()) {
-						dataEntry.setValue(j, "?"); // weka takes in "?" for missing data
-					} else {
+					if(!valAttr.toString().isEmpty()) {
 						if(isCategorical[j]) {
 							dataEntry.setValue(j, valAttr.toString());
 						} else {
@@ -113,83 +114,7 @@ public final class WekaUtilityMethods {
 
 		return data;
 	}
-	
-	public static Instances createInstancesFromQuery(String nameDataSet, ArrayList<Object[]> dataList, String[] names) {
-		int numInstances = dataList.size();	
 
-		int i;
-		int j;
-		int numAttr = names.length;
-		boolean[] isCategorical = new boolean[numAttr];
-		HashSet<String>[] nominalValues = new HashSet[numAttr];
-		Double[][] numericValues = new Double[numAttr][numInstances];
-		for(i = 0; i < numAttr; i++) {
-			if(nominalValues[i] == null) {
-				nominalValues[i] = new HashSet<String>();
-			}
-
-			int numNominal = 0;
-			int numNumeric = 0;
-			for(j = 0; j < numInstances; j++) {
-				Object dataElement = dataList.get(j)[i];
-				String type = Utility.processType(dataElement.toString());
-				if(type.equals("STRING")) {
-					nominalValues[i].add(dataElement.toString());
-					numNominal++;
-				} else {
-					numericValues[i][j] = (double) dataElement;
-					numNumeric++;
-				}
-			}
-
-			if(numNominal > numNumeric) {
-				isCategorical[i] = true;
-			} else {
-				isCategorical[i] = false;
-			}
-		}
-
-		String[] binForInstance = null;
-		FastVector attributeList = new FastVector();
-		for(i = 0; i < numAttr; i++ ) {
-			//special case for predictor since it must be nominal
-			if(isCategorical[i]) {
-				FastVector nominalValuesInFV = new FastVector();
-				HashSet<String> allPossibleValues = nominalValues[i];
-				for(String val : allPossibleValues) {
-					nominalValuesInFV.addElement(val);
-				}
-				attributeList.addElement(new Attribute(names[i], nominalValuesInFV));
-			} else {
-				attributeList.addElement(new Attribute(names[i]));
-			}
-		}
-		//create the Instances Object to contain all the instance information
-		Instances data = new Instances(nameDataSet, attributeList, numInstances);
-
-		for(i = 0; i < numInstances; i++) {
-			Instance dataEntry = new DenseInstance(numAttr);
-			dataEntry.setDataset(data);
-			Object[] dataRow = dataList.get(i);
-			for(j = 0; j < numAttr; j++) {
-				Object valAttr = dataRow[j];
-				if(valAttr.toString().isEmpty()) {
-					dataEntry.setValue(j, "?"); // weka takes in "?" for missing data
-				} else {
-					if(isCategorical[j]) {
-						dataEntry.setValue(j, valAttr.toString());
-					} else {
-						dataEntry.setValue(j, (double) valAttr);
-					}
-				}
-			}
-			//			System.out.println(dataEntry);
-			data.add(dataEntry);
-		}
-
-		return data;
-	}
-	
 	public static Instances[][] crossValidationSplit(Instances data, int numberOfFolds) {
 		Instances[][] split = new Instances[2][numberOfFolds];
 
@@ -209,7 +134,7 @@ public final class WekaUtilityMethods {
 
 		return evaluation;
 	}
-	
+
 	public static double calculateAccuracy(FastVector predCorrect) {
 		double sumPerCorrect = 0;
 		int size = predCorrect.size();
