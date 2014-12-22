f149ba765b37b598ccae11fdaf80bc57ec2b4914
karverma <karverma@deloitte.com>
2014-12-22 15:32:59 +0000

    Changed license/copyright info in *.java from GPLv3 to ASL.
    git-svn-id: svn://107.170.10.188:8080/Semoss/trunk@5052 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: fa526f5f3c2292bb298370ee903d4defb09a313e

--- src/prerna/poi/main/TextExtractor.java
+++ src/prerna/poi/main/TextExtractor.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2014 SEMOSS.ORG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 //http://stackoverflow.com/questions/6112419/handling-the-tab-character-in-java
 //http://stackoverflow.com/questions/10250617/java-apache-poi-can-i-get-clean-text-from-ms-word-doc-files
 package prerna.poi.main;
