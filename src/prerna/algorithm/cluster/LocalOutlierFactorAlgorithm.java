af8a70f65689b8b598f35f99ab8cfcbe0d420bbb
Maher Khalil <themaherkhalil@gmail.com>
2015-01-13 01:07:44 +0000

    updating clustering data processing functions, multi-threading for outliers algorithm
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@5197 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 206cac20a19c4fbd2b2063d43a6e17a968c15377

--- src/prerna/algorithm/cluster/LocalOutlierFactorAlgorithm.java
+++ src/prerna/algorithm/cluster/LocalOutlierFactorAlgorithm.java
@@ -16,6 +16,9 @@
 package prerna.algorithm.cluster;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 import org.apache.commons.math3.special.Erf;
 import org.apache.log4j.LogManager;
@@ -24,71 +27,91 @@ import org.apache.log4j.Logger;
 import prerna.math.StatisticsUtilityMethods;
 
 public class LocalOutlierFactorAlgorithm {
-	
+
 	private static final Logger LOGGER = LogManager.getLogger(LocalOutlierFactorAlgorithm.class.getName());
 
 	private ArrayList<Object[]> masterTable;
 	private String[] masterNames;
 	private int numInstances;
+
+	private String[][] numericalBinMatrix;
+	private String[][] numericalBinOrderingMatrix;
+	private String[][] categoricalMatrix;
+	private double[] categoricalWeights;
+	private double[] numericalWeights;
+
+	private Queue<Integer> processingQueue;
+	int numProcessors;
+
 	InstanceNumericalMethods inm;
-	
+
 	private double[] lrd;
 	private double[] lof;
 	private double[] lop;
-	
+
 	private int k;
-	
+
 	private double[][] similarityMatrix;
 	private double[] kSimilarityArr; 
 	private int[][] kSimilarityIndicesMatrix;
 	private double[][] reachSimMatrix;
-	
+
 	public void setK(int k) {
 		this.k = k;
 	}
-	
+
 	public ArrayList<Object[]> getMasterTable() {
 		return masterTable;
 	}
-	
+
 	public String[] getNames() {
 		return masterNames;
 	}
-	
+
 	public double[] getLRD() {
 		return lrd;
 	}
-	
+
 	public double[] getLOF() {
 		return lof;
 	}
-	
+
 	public double[] getLOP() {
 		return lop;
 	}
-	
+
 	public LocalOutlierFactorAlgorithm(ArrayList<Object[]> list, String[] names) {
 		LOGGER.info("Removing any duplicated instances...");
 		ClusterRemoveDuplicates crd = new ClusterRemoveDuplicates(list, names);
 		this.masterTable = crd.getRetMasterTable();
 		this.masterNames = crd.getRetVarNames();
-		
+		this.numInstances = masterTable.size();
+
 		LOGGER.info("Formatting dataset to run algorithm...");
 		ClusteringDataProcessor cdp = new ClusteringDataProcessor(masterTable, masterNames);
-		inm = new InstanceNumericalMethods(cdp.getNumericalBinMatrix(), cdp.getCategoricalMatrix(), cdp.getNumericalBinOrderingMatrix());
-		inm.setCategoricalWeights(cdp.getCategoricalWeights());
-		inm.setNumericalWeights(cdp.getNumericalWeights());
-		
-		numInstances = masterTable.size();
+		numericalBinMatrix = cdp.getNumericalBinMatrix();
+		numericalBinOrderingMatrix = cdp.getNumericalBinOrderingMatrix();
+		categoricalMatrix = cdp.getCategoricalMatrix();
+		categoricalWeights = cdp.getCategoricalWeights();
+		numericalWeights = cdp.getNumericalWeights();
+
+		inm = new InstanceNumericalMethods(numericalBinMatrix, categoricalMatrix, numericalBinOrderingMatrix);
+		inm.setCategoricalWeights(categoricalWeights);
+		inm.setNumericalWeights(numericalWeights);
+
+		numProcessors = Runtime.getRuntime().availableProcessors();
 	}
-	
+
 	public LocalOutlierFactorAlgorithm(ArrayList<Object[]> masterTable, String[] masterNames, int k) {
 		this(masterTable, masterNames);
 		this.k = k;
 		LOGGER.info("Starting local outlier algorithm using " + k + "-size neighborhood...");
 	}
-	
+
 	public void execute() {
+
+		long startTime = System.currentTimeMillis();
+
 		LOGGER.info("Generating similarity matrix between every instance...");
 		calculateSimilarityMatrix();
 		LOGGER.info("Generating " + k + "-neighborhood similarity matrix for every instance...");
@@ -101,110 +124,229 @@ public class LocalOutlierFactorAlgorithm {
 		calculateLOF();
 		LOGGER.info("Generating local outlier probability...");
 		calculateLOOP();
+
+		long endTime = System.currentTimeMillis();
+		System.out.println("Total Time = " + (endTime-startTime)/1000 );
 	}
 
 	private void calculateSimilarityMatrix() {
 		similarityMatrix = new double[numInstances][numInstances];
-		
+		processingQueue = new PriorityQueue<Integer>();
+
+		List<Thread> threads = new ArrayList<Thread>();
 		int i;
-		int j;
-		int counter = 0;
-		for(i = 0; i < numInstances; i++) {
-			for(j = numInstances - 1; j >= 0 + counter; j--) {
-				double val = inm.calculateSimilarityBetweenInstances(i, j);
-				similarityMatrix[i][j] = val;
-				similarityMatrix[j][i] = val;
+		synchronized(processingQueue){
+			for(i = 0; i < numInstances; i++) {
+				InstanceNumericalMethods inm = new InstanceNumericalMethods(numericalBinMatrix, categoricalMatrix, numericalBinOrderingMatrix);
+				inm.setCategoricalWeights(categoricalWeights);
+				inm.setNumericalWeights(numericalWeights);
+				inm.setNumInstances(numInstances);
+				inm.setSimilarityMatrix(similarityMatrix);
+				inm.setCalculateSimMatrix(true);
+				inm.setIndex(i);
+				inm.setStart(i);
+				inm.setProcessingQueue(processingQueue);
+				processingQueue.add(i);
+				while(processingQueue.size() >= numProcessors * 5) {
+					try {
+						LOGGER.info("Waiting for queue...");
+						processingQueue.wait();
+					} catch (InterruptedException ex) {
+						ex.printStackTrace();
+					}
+				}
+				Thread t = new Thread(inm);
+				threads.add(t);
+				t.start();
 			}
-			counter++;
 		}
-		
+		int size = threads.size();
+		i = 0;
+		for(; i < size; i++) {
+			try {
+				threads.get(i).join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
 		// print out similarity matrix for debugging
-//		System.out.println("SIMILARITY MATRIX");
-//		for(i = 0; i < numInstances; i++) {
-//			for(j = 0; j < numInstances; j++) {
-//				System.out.print(similarityMatrix[i][j] + ", ");
-//			}
-//			System.out.println();
-//		}
+		//		System.out.println("SIMILARITY MATRIX");
+		//		for(int i = 0; i < numInstances; i++) {
+		//			for(int j = 0; j < numInstances; j++) {
+		//				System.out.print(similarityMatrix[i][j] + ", ");
+		//			}
+		//			System.out.println();
+		//		}
 	}
-	
+
 	private void calculateKSimilarityMatrix() {
 		kSimilarityArr = new double[numInstances];
 		kSimilarityIndicesMatrix = new int[numInstances][];
-		
+		processingQueue = new PriorityQueue<Integer>();
+
+		List<Thread> threads = new ArrayList<Thread>();
 		int i;
-		for(i = 0; i < numInstances; i++) {
-			double[] similarityArrBetweenInstanceToAllOtherInstances = similarityMatrix[i];
-			int[] kNearestNeighbors = inm.kNearestNeighbors(similarityArrBetweenInstanceToAllOtherInstances.clone(), i, k);
-			// kNearestNeighbors has the most similar similar index first and least similar index last
-			if(kNearestNeighbors != null && kNearestNeighbors.length != 0) {
-				int mostDistantNeighborIndex = kNearestNeighbors[kNearestNeighbors.length - 1];
-				kSimilarityArr[i] = similarityArrBetweenInstanceToAllOtherInstances[mostDistantNeighborIndex];
-				kSimilarityIndicesMatrix[i] = kNearestNeighbors;
-			} else {
-				kSimilarityArr[i] = 0;
+		synchronized(processingQueue){
+			for(i = 0; i < numInstances; i++) {
+				InstanceNumericalMethods inm = new InstanceNumericalMethods(numericalBinMatrix, categoricalMatrix, numericalBinOrderingMatrix);
+				inm.setCategoricalWeights(categoricalWeights);
+				inm.setNumericalWeights(numericalWeights);
+				inm.setNumInstances(numInstances);
+				inm.setSimilarityMatrix(similarityMatrix);
+				inm.setkSimilarityArr(kSimilarityArr);
+				inm.setkSimilarityIndicesMatrix(kSimilarityIndicesMatrix);
+				inm.setCalculateSimMatrix(false);
+				inm.setCalculateKSimMatrix(true);
+				inm.setIndex(i);
+				inm.setK(k);
+				inm.setProcessingQueue(processingQueue);
+				processingQueue.add(i);
+				while(processingQueue.size() >= numProcessors * 5) {
+					try {
+						LOGGER.info("Waiting for queue...");
+						processingQueue.wait();
+					} catch (InterruptedException ex) {
+						ex.printStackTrace();
+					}
+				}
+				Thread t = new Thread(inm);
+				threads.add(t);
+				t.start();
 			}
 		}
-		
+		int size = threads.size();
+		i = size - (numProcessors * 10) - 1;
+		for(; i < size; i++) {
+			try {
+				threads.get(i).join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
 		// print out k neighborhood matrix for debugging
-//		System.out.println("K-NEIGHBORHOOD SIM MATRIX");
-//		for(i = 0; i < numInstances; i++) {
-//			int[] simIndicies = kSimilarityIndicesMatrix[i];
-//			if(simIndicies != null) {
-//				for(int j = 0; j < simIndicies.length - 1; j++) {
-//					int col = simIndicies[j];
-//					System.out.print(similarityMatrix[i][col] + ", ");
-//				}
-//			}
-//			System.out.println();
-//		}
-		
+		//		System.out.println("K-NEIGHBORHOOD SIM MATRIX");
+		//		for(i = 0; i < numInstances; i++) {
+		//			int[] simIndicies = kSimilarityIndicesMatrix[i];
+		//			if(simIndicies != null) {
+		//				for(int j = 0; j < simIndicies.length - 1; j++) {
+		//					int col = simIndicies[j];
+		//					System.out.print(similarityMatrix[i][col] + ", ");
+		//				}
+		//			}
+		//			System.out.println();
+		//		}
+
 		// print out k neighborhood matrix for debugging
-//		System.out.println("K-NEIGHBORHOOD MATRIX");
-//		for(i = 0; i < numInstances; i++) {
-//			int[] simIndicies = kSimilarityIndicesMatrix[i];
-//			if(simIndicies != null) {
-//				for(int j = 0; j < simIndicies.length - 1; j++) {
-//					System.out.print(simIndicies[j] + ", ");
-//				}
-//			}
-//			System.out.println();
-//		}
+		//		System.out.println("K-NEIGHBORHOOD MATRIX");
+		//		for(i = 0; i < numInstances; i++) {
+		//			int[] simIndicies = kSimilarityIndicesMatrix[i];
+		//			if(simIndicies != null) {
+		//				for(int j = 0; j < simIndicies.length - 1; j++) {
+		//					System.out.print(simIndicies[j] + ", ");
+		//				}
+		//			}
+		//			System.out.println();
+		//		}
 	}
 
 	private void calculateReachSimilarity() {
 		reachSimMatrix = new double[numInstances][numInstances];
-		
+		processingQueue = new PriorityQueue<Integer>();
+
+		List<Thread> threads = new ArrayList<Thread>();
 		int i;
-		int j;
-		int counter = 0;
-		for(i = 0; i < numInstances; i++) {
-			for(j = numInstances - 1; j >= 0 + counter; j--) {
-				// reach similarity is the minimum of the similarity between object i and j and the min similarity of the k closest clusters
-				if(kSimilarityArr[i] == 0 || kSimilarityArr[j] == 0) {
-					reachSimMatrix[i][j] = 0;
-					reachSimMatrix[j][i] = 0;
-				} else {
-					double val = Math.min(similarityMatrix[i][j], kSimilarityArr[i]);
-					reachSimMatrix[i][j] = val;
-					reachSimMatrix[j][i] = val;
+		synchronized(processingQueue){
+			for(i = 0; i < numInstances; i++) {
+				InstanceNumericalMethods inm = new InstanceNumericalMethods(numericalBinMatrix, categoricalMatrix, numericalBinOrderingMatrix);
+				inm.setCategoricalWeights(categoricalWeights);
+				inm.setNumericalWeights(numericalWeights);
+				inm.setNumInstances(numInstances);
+				inm.setSimilarityMatrix(similarityMatrix);
+				inm.setkSimilarityArr(kSimilarityArr);
+				inm.setReachSimMatrix(reachSimMatrix);
+				inm.setCalculateSimMatrix(false);
+				inm.setCalculateReachability(true);
+				inm.setIndex(i);
+				inm.setStart(i);
+				inm.setProcessingQueue(processingQueue);
+				processingQueue.add(i);
+				while(processingQueue.size() >= numProcessors * 5) {
+					try {
+						LOGGER.info("Waiting for queue...");
+						processingQueue.wait();
+					} catch (InterruptedException ex) {
+						ex.printStackTrace();
+					}
 				}
+				Thread t = new Thread(inm);
+				threads.add(t);
+				t.start();
 			}
-			counter++;
+		}
+		int size = threads.size();
+		i = 0;
+		for(; i < size; i++) {
+			try {
+				threads.get(i).join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		// print out reach-similarity matrix for debugging
-//		System.out.println("REACH SIMILARITY MATRIX");
-//		for(i = 0; i < numInstances; i++) {
-//			for(j = 0;  j < numInstances; j++) {
-//				System.out.print(reachSimMatrix[i][j] + ", ");
-//			}
-//			System.out.println();
-//		}
+		//		System.out.println("REACH SIMILARITY MATRIX");
+		//		for(i = 0; i < numInstances; i++) {
+		//			for(j = 0;  j < numInstances; j++) {
+		//				System.out.print(reachSimMatrix[i][j] + ", ");
+		//			}
+		//			System.out.println();
+		//		}
 	}
-	
+
 	private void calculateLRD() {
 		lrd = new double[numInstances];
+//		processingQueue = new PriorityQueue<Integer>();
+//
+//		List<Thread> threads = new ArrayList<Thread>();
+//		int i;
+//		synchronized(processingQueue){
+//			for(i = 0; i < numInstances; i++) {
+//				InstanceNumericalMethods inm = new InstanceNumericalMethods(numericalBinMatrix, categoricalMatrix, numericalBinOrderingMatrix);
+//				inm.setCategoricalWeights(categoricalWeights);
+//				inm.setNumericalWeights(numericalWeights);
+//				inm.setNumInstances(numInstances);
+//				inm.setkSimilarityIndicesMatrix(kSimilarityIndicesMatrix);
+//				inm.setReachSimMatrix(reachSimMatrix);
+//				inm.setLrd(lrd);
+//				inm.setCalculateSimMatrix(false);
+//				inm.setCalculateLRD(true);
+//				inm.setIndex(i);
+//				inm.setProcessingQueue(processingQueue);
+//				processingQueue.add(i);
+//				while(processingQueue.size() >= numProcessors * 5) {
+//					try {
+//						LOGGER.info("Waiting for queue...");
+//						processingQueue.wait();
+//					} catch (InterruptedException ex) {
+//						ex.printStackTrace();
+//					}
+//				}
+//				Thread t = new Thread(inm);
+//				threads.add(t);
+//				t.start();
+//			}
+//		}
+//		int size = threads.size();
+//		i = 0;
+//		for(; i < size; i++) {
+//			try {
+//				threads.get(i).join();
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//		}
 		
 		int i;
 		for(i = 0; i < numInstances; i++) {
@@ -223,7 +365,7 @@ public class LocalOutlierFactorAlgorithm {
 
 	private void calculateLOF() {
 		lof = new double[numInstances];
-		
+
 		int i;
 		for(i = 0; i < numInstances; i++) {
 			double sumLRD = 0;
@@ -240,17 +382,18 @@ public class LocalOutlierFactorAlgorithm {
 			}
 		}
 	}
-	
+
 	private void calculateLOOP() {
 		lop = new double[numInstances];
-		
+
 		double[] ploof = new double[numInstances];
 		int i;
 		for(i = 0; i < numInstances; i++) {
 			ploof[i] = lof[i] - 1;
 		}
-		
+
 		double stdev = StatisticsUtilityMethods.getSampleStandardDeviationIgnoringInfinity(ploof);
+		double squareRoot2 = Math.sqrt(2);
 		// no variation
 		if(stdev == 0) {
 			for(i = 0; i < numInstances; i++) {
@@ -261,11 +404,10 @@ public class LocalOutlierFactorAlgorithm {
 				}
 			}
 		} else {
-			double squareRoot2 = Math.sqrt(2);
 			for(i = 0; i < numInstances; i++) {
 				lop[i] = Math.max(0, Erf.erf(ploof[i]/(stdev * squareRoot2)));
 			}
 		}
-		
+
 	}
 }
