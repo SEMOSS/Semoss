3fcc580c1d96a853ca7c1acdbaf428eecebcfad5
ksmart <ksmart@deloitte.com>
2015-01-22 13:54:55 +0000

    reinforcement learning examples
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@5345 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e078b9c9dfa64ea422268954c87853a0cfc1685f

---
+++ src/prerna/algorithm/impl/rl/ValueIterationAlgorithm.java
@@ -0,0 +1,206 @@
+package prerna.algorithm.impl.rl;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class ValueIterationAlgorithm {
+	
+	private double discountRate = 1.0;
+	private double errorApprox = 0.1; //if this becomes >=1, need to adjust while loop.
+	private BigDecimal initialPolicyVal = new BigDecimal(0.0);
+	private ArrayList<State> stateList;
+	private ArrayList<Action> actionList;
+	private Hashtable<State, BigDecimal> optimalPolicyValHash;
+	private Hashtable<State, ArrayList<Action>> optimalActionHash;
+
+	
+	public ValueIterationAlgorithm(ArrayList<State> stateList, ArrayList<Action> actionList) {
+		this.stateList = stateList;
+		this.actionList = actionList;
+	}
+	
+	public ValueIterationAlgorithm(double discountRate,ArrayList<State> stateList, ArrayList<Action> actionList) {
+		this.discountRate = discountRate;
+		this.stateList = stateList;
+		this.actionList = actionList;
+	}
+
+	public void findOptimalPolicy() {
+		initialize();
+		
+		int count = 1;
+		double delta = (1-discountRate) / discountRate + 1;
+		while(delta > (1-discountRate) / discountRate* errorApprox) {
+			Hashtable<State, BigDecimal> fvHash =  calculateMaxPolicyValueIteration();
+			delta = calculateFVAndPolicyValDelta(fvHash);
+			optimalPolicyValHash = fvHash;
+//			System.out.println("ITERATION "+count+" RESULTS:");
+//			System.out.println("MAXIMUM DELTA: "+delta);
+//			printStateActionPolicyVal();
+//			System.out.println();
+			
+			count++;
+		}
+		System.out.println("ITERATION "+count+" RESULTS:");
+		System.out.println("MAXIMUM DELTA: "+delta);
+		printStateActionPolicyVal();
+		System.out.println();
+	}
+	
+	public void findNormalPolicy() {
+		initialize();
+		
+		int count = 1;
+		double delta = (1-discountRate) / discountRate + 1;
+		while(delta> (1-discountRate) / discountRate * errorApprox) {
+			Hashtable<State, BigDecimal> fvHash =  calculateNormalPolicyValueIteration();
+			delta = calculateFVAndPolicyValDelta(fvHash);
+			optimalPolicyValHash = fvHash;
+			System.out.println("ITERATION "+count+" RESULTS:");
+			System.out.println("MAXIMUM DELTA: "+delta);
+			printStateActionPolicyVal();
+			System.out.println();
+			
+			count++;
+		}
+	}
+	
+	public void initialize() {
+		//initialize policyValHash
+		optimalPolicyValHash = new Hashtable<State, BigDecimal>();
+		for(State state : stateList) {
+			optimalPolicyValHash.put(state,initialPolicyVal);
+		}
+		optimalActionHash = new Hashtable<State, ArrayList<Action>>();
+	}
+	
+	private Hashtable<State, BigDecimal> calculateMaxPolicyValueIteration() {
+		Hashtable<State, BigDecimal> fvHash = new Hashtable<State, BigDecimal> ();
+		for(State state : stateList) {
+			ArrayList<Action> maxActionList = new ArrayList<Action>();
+			if(!state.isTerminal()) {				
+				Action firstAction = actionList.get(0);
+				BigDecimal maxPolicyVal = calculatePolicyValue(state,firstAction);
+				for(Action action : actionList) {
+					BigDecimal policyVal = calculatePolicyValue(state,action);
+					if(policyVal.equals(maxPolicyVal)) {
+						maxActionList.add(action);
+					}
+					else 
+						if(policyVal.compareTo(maxPolicyVal) > 0) {
+						maxPolicyVal = policyVal;
+						maxActionList = new ArrayList<Action>();
+						maxActionList.add(action);
+					}
+				}			
+				fvHash.put(state, maxPolicyVal);
+			}else {
+				fvHash.put(state, new BigDecimal(state.getReward()));
+			}
+			optimalActionHash.put(state, maxActionList);
+		}
+
+		return fvHash;
+	}
+
+	
+	private Hashtable<State, BigDecimal> calculateNormalPolicyValueIteration() {
+		Hashtable<State, BigDecimal> fvHash = new Hashtable<State, BigDecimal> ();
+		for(State state : stateList) {
+			if(!state.isTerminal()) {
+				Action action = actionList.get(0);
+				BigDecimal policyVal = calculateRandomValue(state,action);
+				fvHash.put(state, policyVal);
+			} else
+				fvHash.put(state, new BigDecimal(state.getReward()));
+		}
+
+		return fvHash;
+	}
+	
+	private BigDecimal calculateRandomValue(State state,Action action) {
+		Hashtable<String,State> nextStates = action.calculateNextStates(stateList,state);
+		
+		if(nextStates.isEmpty())
+			return new BigDecimal(0.0);
+		
+		BigDecimal expectedValue = new BigDecimal(0.0);
+		
+		for(String option : nextStates.keySet()) {
+			State nextState = nextStates.get(option);
+			
+			BigDecimal nextStatePolVal = optimalPolicyValHash.get(nextState);
+			expectedValue = expectedValue.add(nextStatePolVal);
+			expectedValue = expectedValue.add(new BigDecimal(state.getReward()));
+		}
+
+		expectedValue = expectedValue.divide(new BigDecimal(nextStates.keySet().size()));
+		
+		return expectedValue;
+	}
+	
+	private BigDecimal calculatePolicyValue(State state,Action action) {
+		Hashtable<String,State> nextStates = action.calculateNextStates(stateList,state);
+		
+		if(nextStates.isEmpty())
+			return new BigDecimal(0.0);
+		
+		BigDecimal optionSum = new BigDecimal(0.0);
+		BigDecimal expectedValue = new BigDecimal(0.0);
+		
+		for(String option : nextStates.keySet()) {
+			State nextState = nextStates.get(option);
+			
+			BigDecimal prob = new BigDecimal(action.getProbablity(option));
+			BigDecimal nextStatePolVal = optimalPolicyValHash.get(nextState);
+			BigDecimal mult = prob.multiply(nextStatePolVal);
+			optionSum = optionSum.add(mult);
+			
+			BigDecimal reward = new BigDecimal(state.getReward());
+			mult = prob.multiply(reward);
+			expectedValue = expectedValue.add(mult);
+		}
+
+		expectedValue = expectedValue.divide(new BigDecimal(nextStates.keySet().size()));
+		
+		BigDecimal mult = optionSum.multiply(new BigDecimal(discountRate));
+		BigDecimal policyVal = expectedValue.add(mult);
+		return policyVal;
+	}
+	
+	private double calculateFVAndPolicyValDelta(Hashtable<State, BigDecimal> fvHash) {
+		State state0 = stateList.get(0);
+		double maxDelta = Math.abs(optimalPolicyValHash.get(state0).doubleValue() - fvHash.get(state0).doubleValue());
+		
+		for(State state : stateList) {
+			double delta = Math.abs(optimalPolicyValHash.get(state).doubleValue() - fvHash.get(state).doubleValue());
+			maxDelta = Math.max(maxDelta, delta);
+		}
+		
+		return maxDelta;
+	}
+	
+	private void printStateActionPolicyVal() {
+		for(State state : stateList) {
+			System.out.print("State: "+state.getID()+" value: "+optimalPolicyValHash.get(state).doubleValue());
+			if(optimalActionHash.size()>0) {
+				ArrayList<Action> actionList = optimalActionHash.get(state);
+				System.out.print(" actions:");
+				for(Action action : actionList) {
+					System.out.print(" "+action.getID());
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	public Hashtable<State, ArrayList<Action>> getOptimalActionHash() {
+		return optimalActionHash;
+	}
+	
+	public Hashtable<State, BigDecimal> getOptimalPolicyValHash() {
+		return optimalPolicyValHash;
+	}
+
+}
---
+++ src/prerna/ui/main/listener/impl/RLGridExampleListener.java
@@ -0,0 +1,129 @@
+package prerna.ui.main.listener.impl;
+
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+import javax.swing.JComponent;
+import javax.swing.JDesktopPane;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import prerna.algorithm.impl.rl.Action;
+import prerna.algorithm.impl.rl.GridState;
+import prerna.algorithm.impl.rl.MultiDirectionAction;
+import prerna.algorithm.impl.rl.SingleDirectionAction;
+import prerna.algorithm.impl.rl.State;
+import prerna.algorithm.impl.rl.ValueIterationAlgorithm;
+import prerna.ui.components.api.IChakraListener;
+import prerna.ui.components.playsheets.RLColumnChartPlaySheet;
+import prerna.ui.components.playsheets.RLGridPlaySheet;
+import prerna.ui.helpers.PlaysheetCreateRunner;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+
+/**
+ * This is the listener that runs the reinforcement learning example of moving in a grid.
+ * @author ksmart
+ *
+ */
+public class RLGridExampleListener implements IChakraListener {
+	
+	static final Logger LOGGER = LogManager.getLogger(RLGridExampleListener.class.getName());
+	
+	private ArrayList<State> stateList;
+	private ArrayList<Action> actionList;
+	private int xSize = 4;
+	private int ySize = 4;
+	
+	/**
+	 * Uses Reinforcement Learning to create an optimal strategy for moving in a grid.
+	 * @param actionevent ActionEvent
+	 */
+	@Override
+	public void actionPerformed(ActionEvent actionevent) {
+		LOGGER.info("RL Grid example button pushed ...");
+		
+		//builds test data so that the user has the option to choose to move in 4 directions (up, down, left, right)
+		//finds the optimal strategy for how the user should move at each state to maximize reward
+		fillDirectionData();
+		
+		RLGridPlaySheet playSheet = new RLGridPlaySheet();
+		JDesktopPane pane = (JDesktopPane) DIHelper.getInstance().getLocalProp(Constants.DESKTOP_PANE);
+		playSheet.setJDesktopPane(pane);
+		playSheet.setStateList(stateList);
+		playSheet.setActionList(actionList);
+		playSheet.setGridDimensions(xSize,ySize);
+		playSheet.setTitle("Reinforcement Learning Grid Example Results");
+		
+		PlaysheetCreateRunner runner = new PlaysheetCreateRunner(playSheet);
+		Thread playThread = new Thread(runner);
+		playThread.start();		
+		
+//		//builds test data so that the action is to move, but the direction is random.
+//		//uses random strategy
+//		fillMoveData();
+//		ValueIterationAlgorithm alg2 = new ValueIterationAlgorithm(stateList,actionList);
+//		alg2.findNormalPolicy();
+	}
+
+	
+	/**
+	 * Builds test data with single action move that has 4 options (left, right, down, up) with equal probability.
+	 */
+	public void fillMoveData() {
+		stateList = new ArrayList<State>();
+		actionList = new ArrayList<Action>();
+		
+		MultiDirectionAction move = new MultiDirectionAction("move",xSize,ySize);
+		move.addOption("left", 0.25);
+		move.addOption("right", 0.25);
+		move.addOption("up", 0.25);
+		move.addOption("down", 0.25);
+		
+		actionList.add(move);
+		
+		stateList.add(new GridState("s0",0.0,0,0,true));
+		for(int i=1;i<xSize*ySize - 1;i++) {
+			stateList.add(new GridState("s"+i,-1.0,i%xSize,i/xSize,false));
+		}		
+	}
+	
+	/**
+	 * Builds test data with 4 actions (left, right down, up) that the user can choose between.
+	 */
+	public void fillDirectionData() {
+		stateList = new ArrayList<State>();
+		actionList = new ArrayList<Action>();
+		
+		SingleDirectionAction left = new SingleDirectionAction("left",xSize,ySize);
+		left.addOption("left",1.0);	
+		actionList.add(left);
+
+		SingleDirectionAction right = new SingleDirectionAction("right",xSize,ySize);
+		right.addOption("right",1.0);	
+		actionList.add(right);
+
+		SingleDirectionAction up = new SingleDirectionAction("up",xSize,ySize);
+		up.addOption("up",1.0);	
+		actionList.add(up);
+
+		SingleDirectionAction down = new SingleDirectionAction("down",xSize,ySize);
+		down.addOption("down",1.0);		
+		actionList.add(down);
+		
+		stateList.add(new GridState("s0",0.0,0,0,true));
+		for(int i=1;i<xSize*ySize - 1;i++) {
+			stateList.add(new GridState("s"+i,-1.0,i%xSize,i/xSize,false));
+		}		
+	}
+
+	/**
+	 * Override method from IChakraListener
+	 * @param view JComponent
+	 */
+	@Override
+	public void setView(JComponent view) {
+		
+	}
+}
