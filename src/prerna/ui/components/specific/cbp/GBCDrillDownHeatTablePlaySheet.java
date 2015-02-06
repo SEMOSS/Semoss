a22eb91351eb282cc8cc5f9ee6ff1afffd0c95da
bisutton <bisutton@deloitte.com>
2015-02-06 13:54:45 +0000

    Grabbing column ordering from presentation file and genericizing everything (mainly summing of lights on it) to work with any product
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@5585 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e76bf96ca4de9b2c8d0c802600f7fc8126eefc4e

--- src/prerna/ui/components/specific/cbp/GBCDrillDownHeatTablePlaySheet.java
+++ src/prerna/ui/components/specific/cbp/GBCDrillDownHeatTablePlaySheet.java
@@ -16,43 +16,25 @@
 package prerna.ui.components.specific.cbp;
 
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Hashtable;
-import java.util.Set;
+import java.util.Properties;
 import java.util.StringTokenizer;
-
-import javax.swing.JButton;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.openrdf.model.Literal;
-import org.openrdf.query.parser.ParsedQuery;
-import org.openrdf.query.parser.sparql.SPARQLParser;
-import org.openrdf.repository.Repository;
-import org.openrdf.repository.RepositoryException;
-import org.openrdf.repository.sail.SailRepository;
-import org.openrdf.sail.inferencer.fc.ForwardChainingRDFSInferencer;
-import org.openrdf.sail.memory.MemoryStore;
 
-import com.bigdata.rdf.model.BigdataLiteralImpl;
-
-import prerna.om.GraphDataModel;
-import prerna.om.SEMOSSEdge;
-import prerna.om.SEMOSSVertex;
-import prerna.rdf.engine.api.IEngine;
 import prerna.rdf.engine.impl.SesameJenaSelectStatement;
 import prerna.rdf.engine.impl.SesameJenaSelectWrapper;
-import prerna.rdf.util.StatementCollector;
-import prerna.ui.components.ChartControlPanel;
-import prerna.ui.components.RDFEngineHelper;
 import prerna.ui.components.playsheets.BrowserPlaySheet;
-import prerna.ui.main.listener.impl.ColumnChartGroupedStackedListener;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
 import prerna.util.Utility;
+
+import com.bigdata.rdf.model.BigdataLiteralImpl;
 
 public class GBCDrillDownHeatTablePlaySheet extends BrowserPlaySheet{
 
@@ -61,16 +43,16 @@ public class GBCDrillDownHeatTablePlaySheet extends BrowserPlaySheet{
 	final String row1Name = "Functions";
 	final String row2Name = "Process Categories";
 	final String row3Name = "Lights-on IT";
-	final String customerServiceLightsOnIT = "/Customer_Service_Lights-On_IT";
-	final String customerServiceColKey = "customer_service";
+	final String lightsOnIT = "_Lights-On_IT";
 	final String lightsOnItKey = "lights-on_it";
 	final String clientValueKey = "clientValueKey";
 	final String peerValueKey = "peerValueKey";
 	final String rowKey = "rowKey";
 	final String colKey = "colKey";
 	final String cellKey = "cellKey";
-	final String[] colNames = new String[] {"http://semoss.org/ontologies/Concept/FunctionLevel1/Sales", "http://semoss.org/ontologies/Concept/FunctionLevel1/Marketing", "http://semoss.org/ontologies/Concept/FunctionLevel1/Product", "http://semoss.org/ontologies/Concept/FunctionLevel1/Distribution", "http://semoss.org/ontologies/Concept/FunctionLevel1/New_Business", "http://semoss.org/ontologies/Concept/FunctionCategoryLevel0/Customer_Service", "http://semoss.org/ontologies/Concept/FunctionCategoryLevel0/Corporate_Overhead", "http://semoss.org/ontologies/Concept/FunctionLevel1/Premium_Tax", "Adjusted_Line_of_Business", "Total_Line_of_Business"};
+	//String[] colNames = new String[] {"http://semoss.org/ontologies/Concept/FunctionLevel1/Sales", "http://semoss.org/ontologies/Concept/FunctionLevel1/Marketing", "http://semoss.org/ontologies/Concept/FunctionLevel1/Product", "http://semoss.org/ontologies/Concept/FunctionLevel1/Distribution", "http://semoss.org/ontologies/Concept/FunctionLevel1/New_Business", "http://semoss.org/ontologies/Concept/FunctionCategoryLevel0/Customer_Service", "http://semoss.org/ontologies/Concept/FunctionCategoryLevel0/Corporate_Overhead", "http://semoss.org/ontologies/Concept/FunctionLevel1/Premium_Tax", "Adjusted_Line_of_Business", "Total_Line_of_Business"};
 	String customTitle = "";
+	Properties presProps = new Properties();
 	
 	ArrayList<String> passedQueries = new ArrayList<String>();
 	
@@ -80,6 +62,23 @@ public class GBCDrillDownHeatTablePlaySheet extends BrowserPlaySheet{
 		this.setPreferredSize(new Dimension(800,600));
 		String workingDir = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
 		fileName = "file://" + workingDir + "/html/MHS-RDFSemossCharts/app/columnchart.html";
+		
+		Properties props = (Properties) DIHelper.getInstance().getLocalProp("GBC_PROPERTIES");
+		if(props == null)
+		{
+			String fileName = workingDir + "/GBCPresentationV2.properties";
+			try {
+				presProps.load(new FileInputStream(fileName));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else
+			presProps = props;
 	}
 	
 	public Hashtable<String, Object> processQueryData()
@@ -103,17 +102,28 @@ public class GBCDrillDownHeatTablePlaySheet extends BrowserPlaySheet{
 	
 	private ArrayList<Hashtable<String, Hashtable<String, Object>>> getOrderedArray(Hashtable<String, Hashtable<String, Hashtable<String, Object>>> processingHash)
 	{
-		ArrayList<Hashtable<String, Hashtable<String, Object>>> orderedArray = new ArrayList<Hashtable<String, Hashtable<String, Object>>>();
-		
-		int count = 0;
-		for(int colIdx = 0; colIdx < this.colNames.length; colIdx++){
-			String colName = this.colNames[colIdx];
-			if(processingHash.containsKey(colName)){
-				orderedArray.add(count, processingHash.get(colName));
-				count++;
-			}
+		Object colList = presProps.get(engine.getEngineName());
+		if(colList == null){
+			colList = presProps.get(engine.getEngineName().replaceAll("_[vV]{1}[0-9]+$", ""));
 		}
-		return orderedArray;
+		if (colList!=null){ // if orders of the columns have been specified in the presentation file, use those, otherwise random order
+			String colNames = colList + "";
+			ArrayList<Hashtable<String, Hashtable<String, Object>>> orderedArray = new ArrayList<Hashtable<String, Hashtable<String, Object>>>();
+			StringTokenizer tokens = new StringTokenizer(colNames, ";");
+			int count = 0;
+			while(tokens.hasMoreTokens()){
+				String colName = tokens.nextToken();
+				if(processingHash.containsKey(colName)){
+					orderedArray.add(count, processingHash.get(colName));
+					count++;
+				}
+			}
+			return orderedArray;
+		}
+		else{
+			ArrayList<Hashtable<String, Hashtable<String, Object>>> orderedArray = new ArrayList<Hashtable<String, Hashtable<String, Object>>>(processingHash.values());
+			return orderedArray;
+		}
 	}
 	
 	private Hashtable<String, Hashtable<String, Hashtable<String, Object>>> processQuery(String passedQuery, Hashtable<String, Hashtable<String, Hashtable<String, Object>>> processingHash)
@@ -140,10 +150,9 @@ public class GBCDrillDownHeatTablePlaySheet extends BrowserPlaySheet{
 			String rowName = "";
 			if(cellName.toLowerCase().contains(this.lightsOnItKey)){
 				rowName = this.row3Name;
-				if(colHeader.toLowerCase().contains(this.customerServiceColKey)){
-					//set cell name
-					cellName = cellName.substring(0, cellName.lastIndexOf("/"))+this.customerServiceLightsOnIT;
-				}
+				
+				//set cell name
+				cellName = cellName.substring(0, cellName.lastIndexOf("/")+1)+Utility.getInstanceName(colHeader) + this.lightsOnIT;
 			}
 			else {
 				rowName = this.row2Name;
@@ -162,8 +171,9 @@ public class GBCDrillDownHeatTablePlaySheet extends BrowserPlaySheet{
 		Hashtable<String, Object> cellHash = new Hashtable<String, Object>();
 		Hashtable<String, Hashtable<String, Object>> colHash = new Hashtable<String, Hashtable<String, Object>>();
 		boolean exists = false;
-		if(processingHash.containsKey(colHeader)){
-			colHash = processingHash.get(colHeader);
+		String colHashKey = Utility.getInstanceName(colHeader);
+		if(processingHash.containsKey(colHashKey)){
+			colHash = processingHash.get(colHashKey);
 			if(colHash.containsKey(cellName)){
 				cellHash = colHash.get(cellName);
 				clientVal = clientVal + (Double) cellHash.get(clientValueKey);
@@ -173,13 +183,13 @@ public class GBCDrillDownHeatTablePlaySheet extends BrowserPlaySheet{
 		}
 		if (!exists){
 			colHash.put(cellName, cellHash);
-			cellHash.put(rowKey, rowName);
-			cellHash.put(colKey, colHeader);
-			cellHash.put(cellKey, cellName);
+			cellHash.put(this.rowKey, rowName);
+			cellHash.put(this.colKey, colHeader);
+			cellHash.put(this.cellKey, cellName);
 		}
 		cellHash.put(clientValueKey, clientVal);
 		cellHash.put(peerValueKey, peerVal);
-		processingHash.put(colHeader, colHash);
+		processingHash.put(colHashKey, colHash);
 		
 	}
 
