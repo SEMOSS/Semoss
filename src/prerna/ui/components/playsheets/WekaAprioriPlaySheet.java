996ace7e3bfdd9f424da289ec12246c64cf9da09
mahkhalil <mahkhalil@deloitte.com>
2015-02-19 04:23:46 +0000

    adding playsheet for apriori alg, removing unnecessary code from algorithm
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@5876 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c084d851b0a623e98687d12fa01622012a2a56b8

---
+++ src/prerna/ui/components/playsheets/WekaAprioriPlaySheet.java
@@ -0,0 +1,152 @@
+package prerna.ui.components.playsheets;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.beans.PropertyVetoException;
+import java.util.ArrayList;
+
+import javax.swing.JComponent;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import prerna.algorithm.weka.impl.WekaAprioriAlgorithm;
+import prerna.rdf.engine.api.ISelectStatement;
+import prerna.rdf.engine.api.ISelectWrapper;
+import prerna.ui.components.NewScrollBarUI;
+import prerna.ui.main.listener.impl.GridPlaySheetListener;
+import prerna.ui.main.listener.impl.JTableExcelExportListener;
+import prerna.util.Utility;
+
+public class WekaAprioriPlaySheet extends GridPlaySheet{
+
+	private static final Logger LOGGER = LogManager.getLogger(WekaAprioriPlaySheet.class.getName());
+
+	private WekaAprioriAlgorithm alg;
+	
+	protected JTabbedPane jTab;
+
+	public WekaAprioriPlaySheet() {
+		super();
+	}
+
+	@Override
+	public void createData() {
+		generateData();
+		runAlgorithm();
+	}
+
+	private void generateData() {
+		if(query!=null) {
+			list = new ArrayList<Object[]>();
+
+			ISelectWrapper sjsw = Utility.processQuery(engine, query);
+			names = sjsw.getVariables();
+			int length = names.length;
+			while(sjsw.hasNext()) {
+				ISelectStatement sjss = sjsw.next();
+				Object[] row = new Object[length];
+				int i = 0;
+				for(; i < length; i++) {
+					row[i] = sjss.getVar(names[i]);
+				}
+				list.add(row);
+			}
+		}
+	}
+
+	public void runAlgorithm() {
+		LOGGER.info("Creating apriori algorithm for instance: " + names[0]);
+		alg = new WekaAprioriAlgorithm(list, names);
+		try {
+			alg.execute();
+			alg.generateDecisionRuleTable();
+			
+			list = alg.getRetList();
+			names = alg.getRetNames();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setJTab(JTabbedPane jTab) {
+		this.jTab = jTab;
+	}
+
+	@Override
+	public void addPanel()
+	{
+		if(jTab==null) {
+			super.addPanel();
+		} else {
+			String lastTabName = jTab.getTitleAt(jTab.getTabCount()-1);
+			LOGGER.info("Parsing integer out of last tab name");
+			int count = 1;
+			if(jTab.getTabCount()>1)
+				count = Integer.parseInt(lastTabName.substring(0,lastTabName.indexOf(".")))+1;
+			addPanelAsTab(count+". Apriori");
+		}
+	}
+
+	public void addPanelAsTab(String tabName) {
+		//	setWindow();
+		try {
+			table = new JTable();
+
+			//Add Excel export popup menu and menuitem
+			JPopupMenu popupMenu = new JPopupMenu();
+			JMenuItem menuItemAdd = new JMenuItem("Export to Excel");
+			String questionTitle = this.getTitle();
+			menuItemAdd.addActionListener(new JTableExcelExportListener(table, questionTitle));
+			popupMenu.add(menuItemAdd);
+			table.setComponentPopupMenu(popupMenu);
+
+			GridPlaySheetListener gridPSListener = new GridPlaySheetListener();
+			LOGGER.debug("Created the table");
+			this.addInternalFrameListener(gridPSListener);
+			LOGGER.debug("Added the internal frame listener ");
+			//table.setAutoCreateRowSorter(true);
+
+			JPanel panel = new JPanel();
+			panel.add(table);
+			GridBagLayout gbl_mainPanel = new GridBagLayout();
+			gbl_mainPanel.columnWidths = new int[]{0, 0};
+			gbl_mainPanel.rowHeights = new int[]{0, 0};
+			gbl_mainPanel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+			gbl_mainPanel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+			panel.setLayout(gbl_mainPanel);
+
+			addScrollPanel(panel, table);
+
+			jTab.addTab(tabName, panel);
+
+			this.pack();
+			this.setVisible(true);
+			this.setSelected(false);
+			this.setSelected(true);
+			LOGGER.debug("Added new Outlier Sheet");
+
+		} catch (PropertyVetoException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void addScrollPanel(JPanel panel, JComponent obj) {
+		JScrollPane scrollPane = new JScrollPane(obj);
+		scrollPane.getVerticalScrollBar().setUI(new NewScrollBarUI());
+		scrollPane.setAutoscrolls(true);
+
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 0;
+		panel.add(scrollPane, gbc_scrollPane);
+	}
+
+}
