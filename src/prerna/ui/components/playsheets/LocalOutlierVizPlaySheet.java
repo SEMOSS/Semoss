75385e96d21bed1ddffc24199fd5294987987dd8
ksmart <ksmart@deloitte.com>
2015-03-02 20:18:16 +0000

    added in outlier visualization playsheet
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@6129 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 41f6de6c5e16eea64e884cd2bd668663bd1ef722

---
+++ src/prerna/ui/components/playsheets/LocalOutlierVizPlaySheet.java
@@ -0,0 +1,156 @@
+/*******************************************************************************
+ * Copyright 2015 SEMOSS.ORG
+ *
+ * If your use of this software does not include any GPLv2 components:
+ * 	Licensed under the Apache License, Version 2.0 (the "License");
+ * 	you may not use this file except in compliance with the License.
+ * 	You may obtain a copy of the License at
+ *
+ * 	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * 	Unless required by applicable law or agreed to in writing, software
+ * 	distributed under the License is distributed on an "AS IS" BASIS,
+ * 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 	See the License for the specific language governing permissions and
+ * 	limitations under the License.
+ * ----------------------------------------------------------------------------
+ * If your use of this software includes any GPLv2 components:
+ * 	This program is free software; you can redistribute it and/or
+ * 	modify it under the terms of the GNU General Public License
+ * 	as published by the Free Software Foundation; either version 2
+ * 	of the License, or (at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *******************************************************************************/
+package prerna.ui.components.playsheets;
+
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import prerna.algorithm.learning.unsupervised.LocalOutlierFactorAlgorithm;
+import prerna.util.CSSApplication;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+
+public class LocalOutlierVizPlaySheet extends BrowserPlaySheet{
+
+	private static final Logger LOGGER = LogManager.getLogger(LocalOutlierVizPlaySheet.class.getName());
+	private ArrayList<Object[]> masterList;
+	private String[] masterNames;
+	private double[] lop;
+	private int k;
+	
+	
+	/**
+	 * Constructor for LocalOutlierVizPlaySheet.
+	 * TODO needs to be changed to correct playsheet name when created
+	 */
+	public LocalOutlierVizPlaySheet() {
+		super();
+		this.setPreferredSize(new Dimension(800,600));
+		String workingDir = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
+		fileName = "file://" + workingDir + "/html/MHS-RDFSemossCharts/app/scatter-plot-matrix.html";
+	}
+	
+	@Override
+	public void createData() {
+		if(list==null)
+			super.createData();
+		runAlgorithm();
+		dataHash = processQueryData();
+	}
+	
+	public void runAlgorithm() {
+		
+		LocalOutlierFactorAlgorithm alg = new LocalOutlierFactorAlgorithm(list, names);
+		alg.setK(k);
+		alg.execute();
+		
+		list = alg.getMasterTable();
+		names = alg.getNames();
+		lop = alg.getLOP();
+		
+	}
+	
+	@Override
+	public Hashtable processQueryData() {
+
+		int i;
+		int j;
+		int numInstances = list.size();
+		int numCols = names.length;
+		int newNumCols = numCols + 2;
+		
+		List<List<Object>> retItemList = new ArrayList<List<Object>>();
+		for(i=0; i<numInstances; i++){
+			Object[] instanceRow = list.get(i);
+			List<Object> item = new ArrayList<Object>();
+
+			//count is first item
+			//outlier lop is second item
+			//then all the variables from analysis
+			item.add(1);
+			
+			if(Double.isNaN(lop[i])) {
+				item.add("NaN");
+			} else {
+				item.add(String.format("%.0f%%",lop[i]*100));
+			}
+
+			for(j = 0; j<numCols; j++)
+				item.add(instanceRow[j]);
+
+			retItemList.add(item);
+		}
+		
+
+		String[] headers = new String[newNumCols];
+		headers[0] = "Count";
+		headers[1] = "LOP";
+		for(i = 0; i < numCols; i++) {
+			headers[i+2] = names[i];
+		}
+
+		Hashtable<String, Object> retHash = new Hashtable<String, Object>();
+		retHash.put("headers", headers);
+		retHash.put("dataSeries", retItemList);
+		
+		return retHash;
+	}
+	
+	@Override
+	public void addPanel()
+	{
+		if(jTab==null) {
+			super.addPanel();
+		} else {
+			String lastTabName = jTab.getTitleAt(jTab.getTabCount()-1);
+			LOGGER.info("Parsing integer out of last tab name");
+			int count = 1;
+			if(jTab.getTabCount()>1)
+				count = Integer.parseInt(lastTabName.substring(0,lastTabName.indexOf(".")))+1;
+			addPanelAsTab(count+". Outliers");
+		}
+
+		new CSSApplication(getContentPane());
+	}
+	
+	public void setMasterList(ArrayList<Object[]> masterList) {
+		this.masterList = masterList;
+	}
+	public void setMasterNames(String[] masterNames) {
+		this.masterNames = masterNames;
+	}
+	public void setKNeighbors(int k) {
+		this.k = k;
+	}
+
+}
