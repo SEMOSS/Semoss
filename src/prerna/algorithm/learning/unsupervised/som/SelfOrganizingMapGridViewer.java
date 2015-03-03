e191ae2b0af6ad83f9f2e47a7d85973d04f74a6e
mahkhalil <mahkhalil@deloitte.com>
2015-03-03 18:55:23 +0000

    updates for SOM algorithm
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@6152 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/algorithm/learning/unsupervised/som/SelfOrganizingMapGridViewer.java
@@ -0,0 +1,94 @@
+package prerna.algorithm.learning.unsupervised.som;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelfOrganizingMapGridViewer extends SelfOrganizingMapGrid{
+
+	private SelfOrganizingMapGridViewer() {
+		super();
+	}
+	
+	public static double[][] getGridCoordinates(int length, int height, int[] numInstancesInGrid) {
+		double[][] coordinates = new double[height+1][length+1];
+		
+		int currHeight = 0;
+		int currLength = 0;
+		int currCell = 0;
+		
+		for(; currHeight < height; currHeight++) {
+			currLength = 0;
+			double[] newRow = new double[length+1];
+			for(; currLength < length; currLength++) {
+				List<Integer> adjacentCells = new ArrayList<Integer>();
+				// add currentCell to list
+				adjacentCells.add(currCell);
+				// add surrounding cells for border point
+				getLeftCells(currLength, 1, currCell, adjacentCells, new ArrayList<Integer>());
+				getDiagonalTopLeftCells(currLength, length, 1, currCell, adjacentCells, new ArrayList<Integer>());
+				getTopCells(currLength, length, 1, currCell, adjacentCells,  new ArrayList<Integer>());
+				double avgCount = getAverageCount(adjacentCells, numInstancesInGrid);
+				newRow[currLength] = avgCount;
+				currCell++;
+				// special processing for last column
+				if(currLength == length - 1) {
+					currCell--;
+					adjacentCells = new ArrayList<Integer>();
+					// add currentCell to list
+					adjacentCells.add(currCell);
+					// add surrounding cells for border point
+					getTopCells(currLength, length, 1, currCell, adjacentCells,  new ArrayList<Integer>());
+					avgCount = getAverageCount(adjacentCells, numInstancesInGrid);
+					newRow[currLength] = avgCount;
+					currCell++;
+				}
+			}
+			coordinates[currHeight] = newRow;
+			// special processing for last row
+			if(currHeight == height - 1) {
+				currHeight++;
+				double[] lastRow = new double[length];
+				for(; currLength < length; currLength++) {
+					List<Integer> adjacentCells = new ArrayList<Integer>();
+					// add currentCell to list
+					adjacentCells.add(currCell);
+					// add surrounding cells for border point
+					getLeftCells(currLength, 1, currCell, adjacentCells, new ArrayList<Integer>());
+					double avgCount = getAverageCount(adjacentCells, numInstancesInGrid);
+					lastRow[currLength] = avgCount;
+					currCell++;
+					// special processing for last column
+					if(currLength == length - 1) {
+						currLength--;
+						adjacentCells = new ArrayList<Integer>();
+						// add currentCell to list
+						adjacentCells.add(currCell);
+						// add surrounding cells for border point
+						// no surrounding cells for last point
+						avgCount = getAverageCount(adjacentCells, numInstancesInGrid);
+						lastRow[currLength] = avgCount;
+						currLength++;
+					}
+				}
+				coordinates[currHeight] = lastRow;
+			}
+		}
+		return coordinates;
+	}
+	
+	private static double getAverageCount(List<Integer> adjacentCells, int[] numInstancesInGrid) {
+		if(adjacentCells == null || adjacentCells.isEmpty()) {
+			throw new IllegalArgumentException("Cannot calculate an average if no instances are found");
+		}
+		
+		double avgCount = 0;
+		
+		int i = 0;
+		int size = adjacentCells.size();
+		for(; i < size; i++) {
+			avgCount += numInstancesInGrid[adjacentCells.get(i)];
+		}
+		
+		return avgCount / size;
+	}
+}
