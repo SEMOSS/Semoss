ff23754cc32168bcf5b749315f5df3525456abbc
Maher Khalil <themaherkhalil@gmail.com>
2015-03-03 18:55:23 +0000

    updates for SOM algorithm
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@6152 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3f4e5ed1198d0d7bbcec558f2948ee98e1de6730

--- src/prerna/ui/components/playsheets/SelfOrganizingMapPlaySheet.java
+++ src/prerna/ui/components/playsheets/SelfOrganizingMapPlaySheet.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import cern.colt.Arrays;
 import prerna.algorithm.learning.unsupervised.som.SelfOrganizingMap;
+import prerna.algorithm.learning.unsupervised.som.SelfOrganizingMapGridViewer;
 import prerna.rdf.engine.api.ISelectStatement;
 import prerna.rdf.engine.api.ISelectWrapper;
 import prerna.util.Utility;
@@ -15,7 +17,9 @@ public class SelfOrganizingMapPlaySheet extends GridPlaySheet{
 	private static final Logger LOGGER = LogManager.getLogger(SelfOrganizingMapPlaySheet.class.getName());
 
 	private SelfOrganizingMap alg;
-
+	private double[][] coordinates;
+	
+	
 	public SelfOrganizingMapPlaySheet() {
 		super();
 	}
@@ -24,6 +28,7 @@ public class SelfOrganizingMapPlaySheet extends GridPlaySheet{
 	public void createData() {
 		generateData();
 		runAlgorithm();
+		processAlgorithm();
 	}
 
 	private void generateData() {
@@ -51,8 +56,55 @@ public class SelfOrganizingMapPlaySheet extends GridPlaySheet{
 		boolean success = alg.execute();
 		if(success == false) {
 			Utility.showError("Error occured running SOM Algorithm!");
+		} else {
+			coordinates = SelfOrganizingMapGridViewer.getGridCoordinates(alg.getLength(), alg.getHeight(), alg.getNumInstancesInGrid());
 		}
 	}
-
+	
+	public void processAlgorithm() {
+		if(coordinates != null) {
+			int i = 0;
+			for(; i < coordinates.length; i++) {
+				System.out.println(Arrays.toString(coordinates[i]));
+			}
+			int[] gridAssignmentForInstance = alg.getGridAssignmentForInstances();
+			
+			int gridLength = alg.getLength();
+			
+			i = 0;
+			int numRows = list.size();
+			int numColumns = list.get(0).length;
+			ArrayList<Object[]> retList = new ArrayList<Object[]>();
+			for(; i < numRows; i++) {
+				Object[] values = new Object[numColumns + 3];
+				Object[] oldValues = list.get(i);
+				int j = 0;
+				for(; j < numColumns; j++) {
+					values[j] = oldValues[j];
+				}
+				values[j] = gridAssignmentForInstance[i];
+				j++;
+				int[] cellPosition = SelfOrganizingMapGridViewer.getCoordinatesOfCell(gridAssignmentForInstance[i], gridLength);
+				values[j] = cellPosition[0];
+				j++;
+				values[j] = cellPosition[1];
+				
+				retList.add(values);
+			}
+			list = retList;
+			
+			i = 0;
+			String[] retNames = new String[numColumns + 3];
+			for(; i < numColumns; i++) {
+				retNames[i] = names[i];
+			}
+			retNames[i] = "Cell";
+			i++;
+			retNames[i] = "X-Pos";
+			i++;
+			retNames[i] = "Y-Pos";
+			names = retNames;
+		}
+	}
 
 }
