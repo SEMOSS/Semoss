2df65ef66e710522f81d7b812e6aab1d7ca950e4
mahkhalil <mahkhalil@deloitte.com>
2015-03-05 21:58:19 +0000

    sending class types in rules
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@6217 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 892ff3e86cc2e005c7f920dd54541f4dfbc5d90f

--- src/prerna/poi/main/insights/InsightRuleConstants.java
+++ src/prerna/poi/main/insights/InsightRuleConstants.java
@@ -5,6 +5,10 @@ public interface InsightRuleConstants {
 	// specific parameter keys
 	String QUESTION_KEY = "@QUESTION";
 	String OUTPUT_KEY = "@OUTPUT";
+	String PROPERTY_KEY = "@PROPERTY";
+	String CONCEPT_KEY = "@CONCEPT";
+	String PROPERTY_VALUE = "PROPERTY";
+	String CONCEPT_VALUE = "CONCEPT";
 	
 	// constraints
 	String ENTROPY_DENSITY_MAX = "EntropyD_Max";
