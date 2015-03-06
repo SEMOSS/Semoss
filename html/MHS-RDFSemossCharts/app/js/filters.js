b029a1ec2c2935ab7d4e6a58cbee1d8e4b27760b
jcapalbo <jcapalbo@deloitte.com>
2015-03-06 16:31:54 +0000

    added shortenAndReplaceUnderscores as well as replaceUnderscores
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@6234 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 33f69f0a3ca1075d87c6cd61554b11e5a6f6dcd4

--- html/MHS-RDFSemossCharts/app/js/filters.js
+++ html/MHS-RDFSemossCharts/app/js/filters.js
@@ -3,26 +3,75 @@
 /* Filters */
 
 angular.module('rdfGraphFilters', [])
-    .filter('removeUnderScoreAddSpaces', function() {
-	//gets last value in url after the /
-	return function(str) {
-        if (str) {
-            var shortKey = '';
-            shortKey = str.replace(/_/g, " ")
-            return shortKey;
-        } else{
+    .filter('removeUnderScoreAddSpaces', function () {
+        //gets last value in url after the /
+        return function (str) {
+            if (str) {
+                var shortKey = '';
+                shortKey = str.replace(/_/g, " ");
+                return shortKey;
+            }
+
             return;
-        }
-        
-	}
-}).filter('shortenDecimalPlaces', function() {
-	//gets last value in url after the /
-	return function(num) {
-        if (num) {
-            return parseFloat(num).toFixed(2);
-        } else {
+        };
+    }).filter('shortenDecimalPlaces', function () {
+        //gets last value in url after the /
+        return function (num) {
+            if (num) {
+                return parseFloat(num).toFixed(2);
+            }
+
             return;
-        }
-        
-	}
-});
+        };
+    }).filter('replaceUnderscores', function () {
+        return function (str) {
+            str = String(str);
+            var myRe = new RegExp("([^/]*)$"),
+                shortStr = myRe.exec(str),
+                result = shortStr[0].replace(/_/g, " ");
+
+            return result;
+        };
+    }).filter('replaceSpaces', function () {
+        return function (str) {
+            str = String(str);
+            var myRe = new RegExp("([^/]*)$"),
+                shortStr = myRe.exec(str),
+                result = shortStr[0].replace(/ /g, "_");
+
+            return result;
+        };
+    }).filter('shortenAndReplaceUnderscores', function () {
+        //will take the string after the last slash and will replaces underscores with spaces
+        return function (str) {
+            str = String(str);
+            var returnStr = "",
+                shortStr = "",
+                result = "",
+                myRe;
+
+            if (str.indexOf('"') === 0) {
+                shortStr = str.substring(1, str.length);
+                returnStr = shortStr.substring(0, shortStr.indexOf('"')).replace(/_/g, " ");
+                return returnStr;
+            }
+            if (str.indexOf('http://') !== -1) {
+                returnStr = '';
+                myRe = new RegExp("([^/]*)$");
+                shortStr = myRe.exec(str);
+                result = shortStr[0].replace(/_/g, " ");
+                /*if (result.length > 20) {
+                 returnStr = result.replace(/.{20}\S*\s+/g, "$&@").split(/\s+@/);
+                 if (returnStr.length > 1) {
+                 return returnStr[0] + "...";
+                 }
+
+                 return returnStr[0];
+                 }*/
+
+                return result;
+            }
+
+            return str;
+        };
+    });
