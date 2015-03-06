f0e17012e1f6e880d303f5cd2504303105806a20
kepark <kepark@deloitte.com>
2015-03-06 15:50:17 +0000

    Adding enum for machine learning algorithm options
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@6230 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/util/MachineLearningEnum.java
@@ -0,0 +1,5 @@
+package prerna.util;
+
+public enum MachineLearningEnum {
+	Clustering, AssociationLearning, Classify, Outliers, Similarity, Predictability, MatrixRegression, NumericalCorrelation
+}
