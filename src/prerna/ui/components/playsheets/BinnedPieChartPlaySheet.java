7d15935fd2ac3066215d3d72dcabc8457ab13e59
ksmart <ksmart@deloitte.com>
2015-03-09 17:28:19 +0000

    new auto generation template, includes type of the property
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@6289 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e655f794e68647dc948166eabd7bd0b0959aa4d0

--- src/prerna/ui/components/playsheets/BinnedPieChartPlaySheet.java
+++ src/prerna/ui/components/playsheets/BinnedPieChartPlaySheet.java
@@ -50,28 +50,43 @@ public class BinnedPieChartPlaySheet extends BrowserPlaySheet{
 	
 	public Hashtable<String, Object> processQueryData()
 	{	
+		Object[][] data = AlgorithmDataFormatter.manipulateValues(list,true);	
+		String[] columnTypes = AlgorithmDataFormatter.determineColumnTypes(list);
 		
-		Object[][] data = AlgorithmDataFormatter.manipulateValues(list,true);		
 		Object[] objDataRow = data[1];
-		Double[] dataRow = ArrayUtilityMethods.convertObjArrToDoubleWrapperArr(objDataRow);
-		BarChart chart = new BarChart(dataRow);
-
 		String[] uniqueValues;
 		String labels;
 		int[] uniqueCounts;
-		if(chart.isUseCategoricalForNumericInput()) {
+		
+		if(columnTypes[1].equals(AlgorithmDataFormatter.STRING_KEY)) {
+			
+			String[] dataRow = ArrayUtilityMethods.convertObjArrToStringArr(objDataRow);
+			BarChart chart = new BarChart(dataRow);
 			chart.calculateCategoricalBins("?", true, true);
 			
 			uniqueValues = chart.getStringUniqueValues();
 			labels = chart.getCategoricalLabel();
 			uniqueCounts = chart.getStringUniqueCounts();
+			
+		}else {
+			
+			Double[] dataRow = ArrayUtilityMethods.convertObjArrToDoubleWrapperArr(objDataRow);
+			BarChart chart = new BarChart(dataRow);
 
-		} else {
+			if(chart.isUseCategoricalForNumericInput()) {
+				chart.calculateCategoricalBins("?", true, true);
+				
+				uniqueValues = chart.getStringUniqueValues();
+				labels = chart.getCategoricalLabel();
+				uniqueCounts = chart.getStringUniqueCounts();
 
-			uniqueValues = chart.getNumericalBinOrder();
-			labels = chart.getNumericalLabel();
-			uniqueCounts = chart.getNumericBinCounterArr();
+			} else {
 
+				uniqueValues = chart.getNumericalBinOrder();
+				labels = chart.getNumericalLabel();
+				uniqueCounts = chart.getNumericBinCounterArr();
+
+			}
 		}
 
 		int j;		
