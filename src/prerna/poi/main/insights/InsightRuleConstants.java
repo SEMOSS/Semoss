3cc4c9a00d16c55c4a37504d3b592f5bf14e6de2
mahkhalil <mahkhalil@deloitte.com>
2015-03-17 19:30:08 +0000

    changed interface to constants class and fixed required changes
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@6462 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 52a7d9585558f807c79e81c142de7ea7fb1655e3

--- src/prerna/poi/main/insights/InsightRuleConstants.java
+++ src/prerna/poi/main/insights/InsightRuleConstants.java
@@ -1,27 +1,31 @@
 package prerna.poi.main.insights;
 
-public interface InsightRuleConstants {
+public final class InsightRuleConstants {
 
+	private InsightRuleConstants() {
+		
+	}
+	
 	// specific parameter keys
-	String QUESTION_KEY = "@QUESTION";
-	String OUTPUT_KEY = "@OUTPUT";
-	String PERSPECTIVE_KEY = "@PERSPECTIVE";
-	String PROPERTY_VALUE = "PROPERTY";
-	String CONCEPT_VALUE = "CONCEPT";
-	String CENTRAL_CONCEPT_VALUE = "CENTRALCONCEPT";
+	public static final String QUESTION_KEY = "@QUESTION";
+	public static final String OUTPUT_KEY = "@OUTPUT";
+	public static final String PERSPECTIVE_KEY = "@PERSPECTIVE";
+	public static final String PROPERTY_VALUE = "PROPERTY";
+	public static final String CONCEPT_VALUE = "CONCEPT";
+	public static final String CENTRAL_CONCEPT_VALUE = "CENTRALCONCEPT";
 	
 	// constraints
-	String ENTROPY_DENSITY_MAX = "ENTROPY_D_MAX";
-	String ENTROPY_DENSITY_MIN = "ENTROPY_D_MIN";
-	String DATA_TYPE = "TYPE";
-	String CLASS = "CLASS";
+	public static final String ENTROPY_DENSITY_MAX = "ENTROPY_D_MAX";
+	public static final String ENTROPY_DENSITY_MIN = "ENTROPY_D_MIN";
+	public static final String DATA_TYPE = "TYPE";
+	public static final String CLASS = "CLASS";
 	
 	// aggregation types
-	String AGGREGATION = "AGGREGATION";
-	String COUNT = "COUNT";
-	String AVERAGE = "AVG";
-	String SUM = "SUM";
-	String MAX = "MAX";
-	String MIN = "MIN";
+	public static final String AGGREGATION = "AGGREGATION";
+	public static final String COUNT = "COUNT";
+	public static final String AVERAGE = "AVG";
+	public static final String SUM = "SUM";
+	public static final String MAX = "MAX";
+	public static final String MIN = "MIN";
 
 }
