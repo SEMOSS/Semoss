bf5fc5b9d38b9fbd0ab459ff5a84b8fd2d64fbf8
mahkhalil <mahkhalil@deloitte.com>
2015-03-19 20:13:33 +0000

    moving view changes out of logic processing
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@6514 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 613fe7589f0cfaec6dcf7a4545447fdf4d36f4f2

--- src/prerna/ui/components/specific/AutoInsightGeneratorTest.java
+++ src/prerna/ui/components/specific/AutoInsightGeneratorTest.java
@@ -27,11 +27,19 @@
  *******************************************************************************/
 package prerna.ui.components.specific;
 
+import java.util.Collections;
 import java.util.Hashtable;
+import java.util.List;
+import java.util.Vector;
+
+import javax.swing.JComboBox;
+import javax.swing.JList;
 
 import prerna.poi.main.insights.AutoInsightExecutor;
 import prerna.rdf.engine.impl.AbstractEngine;
 import prerna.ui.components.playsheets.AbstractRDFPlaySheet;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
 import prerna.util.Utility;
 
 /**
@@ -46,6 +54,28 @@ public class AutoInsightGeneratorTest extends AbstractRDFPlaySheet{
 		AutoInsightExecutor gen = new AutoInsightExecutor((AbstractEngine)engine);
 		gen.run();
 		Utility.showMessage("Complete!");
+		reloadDB();
+	}
+	
+	private void reloadDB() {
+		// selects the db in repolist so the questions refresh with the changes
+		JList list = (JList) DIHelper.getInstance().getLocalProp(Constants.REPO_LIST);
+		List selectedList = list.getSelectedValuesList();
+		String selectedValue = selectedList.get(selectedList.size() - 1).toString();
+
+		// don't need to refresh if selected db is not the db you're modifying.
+		// when you click to it it will refresh anyway.
+		if (engine.getEngineName().equals(selectedValue)) {
+			Vector<String> perspectives = engine.getPerspectives();
+			Collections.sort(perspectives);
+
+			JComboBox<String> box = (JComboBox<String>) DIHelper.getInstance().getLocalProp(Constants.PERSPECTIVE_SELECTOR);
+			box.removeAllItems();
+
+			for (int itemIndex = 0; itemIndex < perspectives.size(); itemIndex++) {
+				box.addItem(perspectives.get(itemIndex).toString());
+			}
+		}
 	}
 
 	@Override
