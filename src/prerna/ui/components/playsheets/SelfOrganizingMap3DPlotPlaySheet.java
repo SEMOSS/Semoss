227f880760b5abb1d1b5f040a666cc14793c768d
mahkhalil <mahkhalil@deloitte.com>
2015-04-06 19:39:31 +0000

    fixing output error in playsheet
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@6767 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 9d73fd6c82cf839f8ace310855ae08431074e8ca

--- src/prerna/ui/components/playsheets/SelfOrganizingMap3DPlotPlaySheet.java
+++ src/prerna/ui/components/playsheets/SelfOrganizingMap3DPlotPlaySheet.java
@@ -55,6 +55,7 @@ public class SelfOrganizingMap3DPlotPlaySheet extends BrowserPlaySheet {
 	public void createData() {
 		generateData();
 		runAlgorithm();
+		dataHash = processQueryData();
 	}
 
 	private void generateData() {
