1f334c8236e772e91819d7b79ace7d3433a44006
einzana <einzana@deloitte.com>
2015-04-20 15:50:36 +0000

    changes made to file so that we dont get result set open cursor issues.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@6989 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e9c0959e4819d0e1d3f253c0031aeba12f13399b

--- src/prerna/rdf/engine/wrappers/RDBMSSelectWrapper.java
+++ src/prerna/rdf/engine/wrappers/RDBMSSelectWrapper.java
@@ -31,6 +31,8 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Hashtable;
 
 import prerna.rdf.engine.api.ISelectStatement;
@@ -39,66 +41,86 @@ import prerna.rdf.engine.api.ISelectWrapper;
 public class RDBMSSelectWrapper extends AbstractWrapper implements
 		ISelectWrapper {
 
+	private ArrayList<ISelectStatement> queryResults = new ArrayList();
 	public static String uri = "http://semoss.org/ontologies/concept";
 	ResultSet rs = null;
 	boolean hasMore = false;
 	Hashtable columnTypes = new Hashtable();
+	private int currentQueryIndex = 0;
 
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
+		
+		queryResults.clear(); //clear the query results arraylis
 		rs = (ResultSet)engine.execSelectQuery(query);
+		setVariables(); //get the variables
+		populateQueryResults();
+		
+		//close the result set
+		try{
+			rs.close();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	private String currentDate(){
+		Date dt = new Date();
+		SimpleDateFormat dtFormatter = new SimpleDateFormat("MM/dd/yy HH:mm:ss:SSS");
+		
+		return dtFormatter.format(dt);
 	}
 
 	@Override
 	public boolean hasNext() {
-		// TODO Auto-generated method stub
-		try {
-			hasMore =  rs.next();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		boolean hasMore = false;
+		if(queryResults.size() > currentQueryIndex)
+			hasMore = true;
 		return hasMore;
 	}
-
-	@Override
-	public ISelectStatement next() {
-		// TODO Auto-generated method stub
-		ISelectStatement stmt = new SelectStatement();
+	
+	private void populateQueryResults(){
+		ISelectStatement stmt;
 		try {
-			for(int colIndex = 0;colIndex < var.length;colIndex++)
-			{
-				Object value = rs.getObject(var[colIndex]);
-				if(value==null){
-					value = ""; //prevent null pointer exception.
-				}
-				
-				stmt.setVar(var[colIndex], value);
-				//set the type and URI based on the type
-				int type = (int)columnTypes.get(var[colIndex]);
-				String qualifiedValue = null;
-				if(type == Types.LONGNVARCHAR || type == Types.VARCHAR || type == Types.CHAR || type == Types.LONGNVARCHAR || type == Types.NCHAR)
+			while(rs.next()){
+				stmt = new SelectStatement();
+				for(int colIndex = 0;colIndex < var.length;colIndex++)
 				{
-					qualifiedValue = uri + "/" + var[colIndex] + "/" + value;
-					stmt.setRawVar(var[colIndex], qualifiedValue);
+					Object value = rs.getObject(var[colIndex]);
+					if(value==null){
+						value = ""; //prevent null pointer exception.
+					}
+					
+					stmt.setVar(var[colIndex], value);
+					//set the type and URI based on the type
+					int type = (int)columnTypes.get(var[colIndex]);
+					String qualifiedValue = null;
+					if(type == Types.LONGNVARCHAR || type == Types.VARCHAR || type == Types.CHAR || type == Types.LONGNVARCHAR || type == Types.NCHAR)
+					{
+						qualifiedValue = uri + "/" + var[colIndex] + "/" + value;
+						stmt.setRawVar(var[colIndex], qualifiedValue);
+					}
+					else
+						stmt.setRawVar(var[colIndex], value);
 				}
-				else
-					stmt.setRawVar(var[colIndex], value);
+				queryResults.add(stmt);
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return stmt;
 	}
 
 	@Override
-	public String[] getVariables() {
-		
-		if(var != null)
-			return var;
-		// get the result set metadata to get the column names
+	public ISelectStatement next() {
+		// TODO Auto-generated method stub
+		ISelectStatement stmt = queryResults.get(currentQueryIndex);
+		currentQueryIndex++;
+		return stmt;
+	}
+
+	private void setVariables(){
 		try {
 			ResultSetMetaData rsmd = rs.getMetaData();
 			int numColumns = rsmd.getColumnCount();
@@ -115,7 +137,11 @@ public class RDBMSSelectWrapper extends AbstractWrapper implements
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+	}
+	
+	@Override
+	public String[] getVariables() {
+		// get the result set metadata to get the column names
 		return var;
 	}
 	
