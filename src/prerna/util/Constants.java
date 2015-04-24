45aebee90c24a1b98ecee26747a93437dba3e544
karverma <karverma@deloitte.com>
2015-04-24 21:10:43 +0000

    Changing security_enabled Constants value to align with context param rather than RDF_Map.prop value
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@7093 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 1ecfb43062d46da13a6c4e22443a3f110d169bcc

--- src/prerna/util/Constants.java
+++ src/prerna/util/Constants.java
@@ -465,6 +465,6 @@ public class Constants {
 	public static final String MHS_FUNCTIONS = "prerna.semoss.web.services.specific.tap.";
 	
 	// Security
-	public static final String SECURITY_ENABLED = "SECURITY_ENABLED";
+	public static final String SECURITY_ENABLED = "security_enabled";
 	public static final String SESSION_USER = "user";
 }
