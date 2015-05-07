ee1f75caf52b950623ef9c8e1499dcbca451e16a
rluthar <rluthar@deloitte.com>
2015-05-07 16:50:01 +0000

    adding functionality for calculation of most similar items via streaming given an input matrix.  specific implementation for calculating recommended users. Created interface similarityanalytics until finalization of IDataTable interface
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@7422 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/algorithm/learning/unsupervised/recommender/Recommender.java
@@ -0,0 +1,61 @@
+package prerna.algorithm.learning.unsupervised.recommender;
+
+import org.apache.commons.math3.linear.BlockRealMatrix;
+
+/**
+ *
+ *
+ * Duties of Recommender
+ * 	handling the update of top users with new information
+ * 	figure out when to stop running in the event of streaming
+ * 	getting and receiving new top users
+ */
+public class Recommender {
+	
+	private SimilarityAnalytics algorithm;
+	private RecommendedUsers topUsers;
+	private final String user;
+	
+    public Recommender(String u) {
+    	user = u;
+    	algorithm = new VectorSimilarity();
+    	topUsers = new RecommendedUsers();
+    }
+	public Recommender(String u, SimilarityAnalytics a) {
+        user = u;
+    	algorithm = a;
+        topUsers = new RecommendedUsers();
+    }
+    
+	//Set which algorithm to use to calculate the Similarity Scores to the user
+    public void setMode(SimilarityAnalytics a) {
+        algorithm = a;
+        topUsers = new RecommendedUsers();
+    }
+    
+    //update the top users with new information
+    public void update(BlockRealMatrix data, String[] ids) {
+		double[] simScores = algorithm.runAlgorithm(data);
+		for(int i=0; i<ids.length; i++) {
+			topUsers.insert(ids[i], simScores[i]);//turn this into a stream and/or filter out really bad scores
+		}
+    }
+    
+    public void update(double[][] data, String[] ids){
+    	BlockRealMatrix d = new BlockRealMatrix(data);
+    	update(d, ids);
+    }
+    
+    public String[] getAllRecommendedUsers() {
+    	return topUsers.getAllUsers();
+    }
+    
+    public String[] getRecommendedUsers(int num){
+    	return topUsers.getTop(num);
+    }
+    
+    //Get the ID of the user this recommender is tied to
+    public String getUser(){
+    	return user;
+    }
+}
--- src/prerna/algorithm/learning/unsupervised/recommender/VectorSimilarity.java
+++ src/prerna/algorithm/learning/unsupervised/recommender/VectorSimilarity.java
@@ -1,29 +1,57 @@
 package prerna.algorithm.learning.unsupervised.recommender;
 
+import java.util.Map;
+
 import org.apache.commons.math3.linear.BlockRealMatrix;
 import org.apache.commons.math3.linear.RealVector;
 
-public class VectorSimilarity{
+import prerna.algorithm.api.ITableDataFrame;
 
-	public static double[] getUserSimilarityArray(BlockRealMatrix dataMatrix, int user) {
+/*
+ * algorithm to calculate the similarity of users with a user using cosine similarity
+ */
+public class VectorSimilarity implements SimilarityAnalytics{
 
-		long start = System.currentTimeMillis();
+	private Map options;
+	private final String USER = "user";
+	
+	@Override
+	public double[] runAlgorithm(BlockRealMatrix dataMatrix) {
+
+		if(!options.containsKey(USER)) {
+			throw new NullPointerException("No User Defined in Options To Run Algorithm");
+		}
+		//long start = System.currentTimeMillis();
 
 		int numRows = dataMatrix.getRowDimension();
 		double[] similarityArray = new double[numRows];
 
+		RealVector v1 = (RealVector) options.get(USER);
 		int i = 0;
 		for(; i < numRows; i++){
-			RealVector v1 = dataMatrix.getRowVector(user);
 			double simVal = v1.cosine(dataMatrix.getRowVector(i));
 			similarityArray[i] = simVal;
 		}
 
-		long end = System.currentTimeMillis();
-
-		System.out.println("Time in sec = " + ((end-start)/1000) );
+		//long end = System.currentTimeMillis();
+		//System.out.println("Time in sec = " + ((end-start)/1000) );
 
 		return similarityArray;
+	}
+	
+	@Override
+	public void setOptions(Map options) {
+		this.options = options;
+	}
+
+	@Override
+	public Map getOptions() {
+		return options;
 	}	
+
+	@Override
+	public ITableDataFrame runAlgorithm(ITableDataFrame data) {
+		return null;
+	}
 }
 
