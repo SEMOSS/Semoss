6573a59e14a4986fe7e3d0c98aebf6a914779ff6
mahkhalil <mahkhalil@deloitte.com>
2015-05-31 22:57:26 +0000

    fixed btreeiterator to use non-recursive depth first search to go through index tree to get roots in value tree. uses roots in value tree to get parents and return an Object[] corresponding to a row if the tree was flattened
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@7907 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 159792c177d951d813230bdd40e55e1721f4e9b3

--- src/prerna/ds/BTreeIterator.java
+++ src/prerna/ds/BTreeIterator.java
@@ -5,50 +5,104 @@ import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
-public class BTreeIterator implements Iterator<Object[]>{
+public class BTreeIterator implements Iterator<Object[]> {
 
-	private List<SimpleTreeNode> leavesVec;
+	private TreeNode currNode;
 	private int index = 0;
+	private List<SimpleTreeNode> instances;
+	private List<TreeNode> childrenList;
 	
 	/**
 	 * Constructor for the BTreeIterator
 	 * Uses the leaves in the tree to traverse up and get the data corresponding to a row if the tree was flattened
-	 * @param leavesVec			A list of nodes corresponding to the leaves in the tree
+	 * @param typeRoot			A list of nodes corresponding to the leaves in the tree
 	 */
-	public BTreeIterator(List<SimpleTreeNode> leavesVec) {
-		this.leavesVec = leavesVec;
+	public BTreeIterator(TreeNode typeRoot) {
+		currNode = typeRoot;
+		currNode = currNode.getLeft(currNode);
+		instances = currNode.getInstances();
+		
+		childrenList = new ArrayList<TreeNode>();
+		
+		if(currNode.rightChild != null) {
+			childrenList.add(0, currNode.rightChild);
+		}
+		if(currNode.leftChild != null) {
+			childrenList.add(0, currNode.leftChild);
+		}
 	}
 	
 	@Override
+	/**
+	 * Perform a non-recursive depth-first-search (DFS)
+	 * Must also take into consideration the number of instances associated with each node
+	 * Must also take into consideration the fan-out of the btree for siblings of node
+	 */
 	public boolean hasNext() {
-		if(index >= leavesVec.size()) {
-			return false;
+		// if more instances associated with current node still need to be 
+		if(index < instances.size()) {
+			return true;
+		} else {
+			// since we are always starting at most left, only need to check if right child exists
+			if(currNode.rightSibling != null) {
+				currNode = currNode.rightSibling;
+				instances = currNode.getInstances();
+				// got new instances list, set index back to zero
+				index = 0;
+				return true;
+			} else {
+				// grab first child
+				if(!childrenList.isEmpty()) {
+					currNode = childrenList.get(0);
+					childrenList.remove(0);
+					// since we didn't make sure we add the most left child, ensure it is here
+					currNode = currNode.getLeft(currNode);
+					instances = currNode.getInstances();
+					// got new instances list, set index back to zero
+					index = 0;
+					
+					// add children to children list
+					if(currNode.rightChild != null) {
+						childrenList.add(0, currNode.rightChild);
+					}
+					if(currNode.leftChild != null) {
+						childrenList.add(0, currNode.leftChild);
+					}
+					return true;
+				}
+			}
 		}
-		return true;
+		
+		return false;
 	}
-
+	
 	@Override
 	public Object[] next() {
-		SimpleTreeNode leaf = leavesVec.get(index);
+		if(index > instances.size()) {
+			throw new IndexOutOfBoundsException("No more rows in data-frame.");
+		}
+		
+		SimpleTreeNode currValueNode = instances.get(index);
+		// update index for next time method is called
+		index++;
+		
 		// add values into leaf from leaf to parent
 		List<Object> retRow = new ArrayList<Object>();
-		retRow.add(leaf.leaf.getValue());
-		while(leaf.parent != null) {
-			leaf = leaf.parent;
-			retRow.add(leaf.leaf.getValue());
+		retRow.add(currValueNode.leaf.getValue());
+		while(currValueNode.parent != null) {
+			currValueNode = currValueNode.parent;
+			retRow.add(currValueNode.leaf.getValue());
 		}
 		
 		// reverse the values to be from parent to leaf
 		Collections.reverse(retRow);
-		// update index for next time method is called
-		index++;
 		
 		return retRow.toArray();
 	}
 
+	// Note: when updating to Java 8, no longer need to override remove() method
 	@Override
 	public void remove() {
-		// what would this do.... should it remove the row from the entire tree?
-	}
 
+	}
 }
