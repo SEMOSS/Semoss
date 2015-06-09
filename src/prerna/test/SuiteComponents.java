700bd65b5ec0a833f84eae973c497cc6a6c4e034
abender <abender@deloitte.com>
2015-06-09 15:08:23 +0000

    Refactoring test suites and adding new alg test.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@8077 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 11d9a0a42ab267b0ce2bd08e8d5d14f2f354a145

---
+++ src/prerna/test/SuiteComponents.java
@@ -0,0 +1,27 @@
+package prerna.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+*
+* @author  August Bender
+* @version 1.0
+* @since   06-09-2015 
+* Questions? Email abender@deloitte.com
+*/
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+				ImportDataProcessorTest.class
+				})
+public class SuiteComponents {
+/*
+ * 2) ImportDataProcessorTest checks the 3 main Components of the ImportDataProcessor class
+ * 	 a) Creating a new Database
+ * 	 b) Adding to and existing Database
+ * 	 c) Overriding/Replacing an existing Database	
+ */
+
+}
