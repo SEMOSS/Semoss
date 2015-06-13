f6fcf8c329c851ee053296d6cf233bd8370aed52
bisutton <bisutton@deloitte.com>
2015-06-13 01:29:43 +0000

    Adding DoubleClass serialization
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@8160 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ds/DoubleClass.java
+++ src/prerna/ds/DoubleClass.java
@@ -11,10 +11,57 @@ public class DoubleClass implements ISEMOSSNode {
 		rawValue = r;
 		type = t;
 	}
+
+	public DoubleClass(String string, boolean serialized){
+		if(!serialized){
+			this.value = Double.parseDouble(string);
+		}
+		else{
+			fromString(string);
+		}
+	}
 	
 	public DoubleClass(double d, String t) {
 		value = d;
 		type = t;
+	}
+	
+	@Override
+	public String toString(){
+		String className = "";
+		Class<?> enclosingClass = getClass().getEnclosingClass();
+		if (enclosingClass != null) {
+			className = enclosingClass.getName();
+		} else {
+			className = getClass().getName();
+		}
+		String ret = className + "###" + 
+				"innerString==="+this.value +
+				"&&&type==="+this.type +
+				"&&&rawValue==="+this.rawValue;
+		return ret;
+	}
+	
+	private void fromString(String serializedString){
+//		System.err.println(serializedString);
+		String[] mainParts = serializedString.split("&{3}");
+		for(int idx = 0; idx < mainParts.length; idx++){
+			String element = mainParts[idx];
+			String[] parts = element.split("={3}");
+			String name = parts[0];
+			String value = parts[1];
+			if(!value.equals("null")){
+				if(name.equals("innerString")){
+					this.value = Double.parseDouble(value);
+				}
+				else if(name.equals("type")){
+					this.type = value;
+				}
+				else if(name.equals("rawValue")){
+					this.rawValue = value;
+				}
+			}
+		}
 	}
 	
 	@Override
