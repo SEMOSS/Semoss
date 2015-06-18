f772cf305c7701bbe590ae1943bc4f7679677296
abender <abender@deloitte.com>
2015-06-18 16:20:58 +0000

    Adding in some Alg Tests
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@8242 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/test/MatrixRegressionAlgorithmTest.java
@@ -0,0 +1,129 @@
+package prerna.test;
+
+import static org.junit.Assert.*;
+
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import prerna.algorithm.learning.supervized.MatrixRegressionAlgorithm;
+import prerna.util.DIHelper;
+
+/**
+*
+* @author  August Bender
+* @version 1.0
+* @since   06-18-2015 
+* Questions? Email abender@deloitte.com
+*/
+public class MatrixRegressionAlgorithmTest {
+
+	private static String workingDir = System.getProperty("user.dir");
+	static int testCounter;
+	
+	private static MatrixRegressionAlgorithm alg;
+	private static double[][] matrixPrime;
+	private static double[] matrixSecunde;
+	
+	@BeforeClass 
+	public static void setUpOnce(){
+		//Set the Sudo-Prop
+		System.setProperty("file.separator", "/");
+		String propFile = workingDir + "/RDF_Map.prop";
+		DIHelper.getInstance().loadCoreProp(propFile);
+		PropertyConfigurator.configure(workingDir + "/log4j.prop");
+		
+		//Initialize variables
+		int complexity = 100;
+		matrixPrime = new double[complexity][1];
+		matrixSecunde = new double[complexity];
+		
+		//Set data values
+		for(int x = 0; x < matrixPrime.length; x++){
+			for(int y = 0; y < matrixPrime[x].length; y++){
+				if(x%2 == 0){
+					matrixPrime[x][y] = 0.0;
+				} else {
+					matrixPrime[x][y] = 1.0;
+				}
+			}
+		}
+		for(int x = 0; x < matrixSecunde.length; x++){
+			if(x%2 == 0){
+				matrixSecunde[x] = 0.0;
+			} else {
+				matrixSecunde[x] = 1.0;
+			}
+		}
+		
+		//TODO Set Matrix
+		
+		System.out.println("Test Started..");
+	}
+	
+	@Before
+	public void setUp(){
+		testCounter++;
+		System.out.println("Test " + testCounter + " starting..");
+		alg = new MatrixRegressionAlgorithm(matrixPrime, matrixSecunde);
+	}
+	
+	@After
+	public void tearDown(){
+		System.out.println("Test " + testCounter + " ended..");
+	}
+	
+	@AfterClass
+	public static void finalTearDown(){
+		System.out.println("Class Tear Down ...");
+	}
+	
+	@Test
+	public void executeTest(){
+		 alg.execute();
+		 
+		 //Coeff Array Data Asserts
+		 for(int i = 0; i < alg.getCoeffArray().length; i++){
+			 if(i%2 == 0){
+				 assertTrue("Coeff Array Data..",(alg.getEstimateArray()[i] == -8.881784197001253E-17));
+			 } else {
+				 assertTrue("Coeff Array Data..",(alg.getEstimateArray()[i] == 1.0000000000000002));
+			 }
+		 }
+
+		 //CoeffErrorsArray: ");
+		 for(int i = 0; i < alg.getCoeffErrorsArray().length; i++){
+			 if(i%2 == 0){
+				 assertTrue("Coeff Errors Array Data..",(alg.getCoeffErrorsArray()[i] == 2.4157733292717553E-17 ));
+			 } else {
+				 assertTrue("Coeff Errors Array Data..",(alg.getCoeffErrorsArray()[i] == 3.41641940587532E-17));
+			 }
+		 }
+
+		 //ResidualArray: ");
+		 for(int i = 0; i < alg.getResidualArray().length; i++){
+			 if(i%2 == 0){
+				 assertTrue("Residual Array Data..",(alg.getResidualArray()[i] == 8.881784197001253E-17));
+			 } else {
+				 assertTrue("Residual Array Data..",(alg.getResidualArray()[i] == -2.220446049250313E-16));
+			 }
+		 }
+
+		 //Estimated Array Data Asserts
+		 for(int i = 0; i < alg.getEstimateArray().length; i++){
+			 if(i%2 == 0){
+				 assertTrue("Estimated Array Data..",(alg.getEstimateArray()[i] == -8.881784197001253E-17));
+			 } else {
+				 assertTrue("Estimated Array Data..",(alg.getEstimateArray()[i] == 1.0000000000000002));
+			 }
+			 
+		 }
+
+		 assertTrue("Estimated Array Data..",(alg.getStandardError() == 1.691041330490229E-16));
+		
+	}
+	
+}
