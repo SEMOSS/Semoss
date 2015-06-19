5896347b4a6d56dedaa73ad316f241ac1604504c
abender <abender@deloitte.com>
2015-06-19 16:00:32 +0000

    Fixing Errors
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@8256 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/test/WekaClassificationTest.java
+++ src/prerna/test/WekaClassificationTest.java
@@ -13,7 +13,6 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import prerna.algorithm.learning.weka.WekaClassification;
