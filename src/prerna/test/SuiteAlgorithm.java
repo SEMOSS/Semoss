3cc5d991931fd7da521e6e58af049ad85b239aa7
abender <abender@deloitte.com>
2015-06-19 16:00:32 +0000

    Fixing Errors
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@8256 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c3d8753cfec4c582d5f8030ff6c4ff2bfd690083

--- src/prerna/test/SuiteAlgorithm.java
+++ src/prerna/test/SuiteAlgorithm.java
@@ -4,8 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import prerna.algorithm.learning.unsupervised.clustering.WekaClassification;
-
 /**
 *
 * @author  August Bender
