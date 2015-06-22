af6291ac184cd578c65a8df758b87a7b423e6648
mahkhalil <mahkhalil@deloitte.com>
2015-06-22 20:05:20 +0000

    adding partial outer join matcher
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@8288 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/algorithm/impl/ExactStringPartialOuterJoinMatcher.java
@@ -0,0 +1,62 @@
+package prerna.algorithm.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import prerna.algorithm.api.ITableDataFrame;
+import prerna.ds.BTreeDataFrame;
+import prerna.ds.SimpleTreeNode;
+
+public class ExactStringPartialOuterJoinMatcher extends ExactStringMatcher {
+
+	private static final Logger LOGGER = LogManager.getLogger(ExactStringPartialOuterJoinMatcher.class.getName());
+
+	public ExactStringPartialOuterJoinMatcher() {
+		super();
+	}
+	
+	@Override
+	protected ITableDataFrame performMatch(Object[] table1Col, Object[] table2Col) {
+		String table1ValueKey = "Table1Value";
+		String table2ValueKey = "Table2Value";
+
+		ITableDataFrame bTree = new BTreeDataFrame(new String[]{table1ValueKey, table2ValueKey});
+
+		int success = 0;
+		int total = 0;
+		for(int i = 0; i < table1Col.length; i++) {
+			boolean matchFound = false;
+			for(int j = 0; j < table2Col.length; j++) {
+				if(match(table1Col[i],table2Col[j])) {
+					matchFound = true;
+					System.out.println("MATCHED::::::::::::::::: " + table1Col[i] + "      " +   table2Col[j]  );
+					Map<String, Object> row = new HashMap<String, Object>();
+					row.put(table1ValueKey , table1Col[i]);
+					row.put(table2ValueKey, table2Col[j]);
+					bTree.addRow(row, row); //TODO: adding values as both raw and clean
+					success++;
+				}
+				total++;
+			}
+
+			// if no match found, add to a blank node
+			if(!matchFound) {
+				System.out.println("OUTER JOIN MATCHED::::::::::::::::: " + table1Col[i] + "       NULL ");
+				Map<String, Object> row = new HashMap<String, Object>();
+				row.put(table1ValueKey , table1Col[i]);
+				row.put(table2ValueKey, SimpleTreeNode.EMPTY);
+				bTree.addRow(row, row); //TODO: adding values as both raw and clean
+			}
+		}
+
+		this.resultMetadata.put("success", success);
+		this.resultMetadata.put("total", total);
+
+		return bTree;
+	}
+
+}
+
