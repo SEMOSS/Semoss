8ee4b6712f7a28f49ffc2b8d34d4474cc115221c
mahkhalil <mahkhalil@deloitte.com>
2015-06-23 17:00:13 +0000

    matcher to perform full outer join
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@8319 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 7e99a61f21a5ba331699366d739be0777f95b502

--- src/prerna/algorithm/impl/ExactStringOuterJoinMatcher.java
+++ src/prerna/algorithm/impl/ExactStringOuterJoinMatcher.java
@@ -10,14 +10,11 @@ import prerna.algorithm.api.ITableDataFrame;
 import prerna.ds.BTreeDataFrame;
 import prerna.ds.SimpleTreeNode;
 
-public class ExactStringPartialOuterJoinMatcher extends ExactStringMatcher {
+public class ExactStringOuterJoinMatcher extends ExactStringMatcher {
 
-	private static final Logger LOGGER = LogManager.getLogger(ExactStringPartialOuterJoinMatcher.class.getName());
+	private static final Logger LOGGER = LogManager.getLogger(ExactStringOuterJoinMatcher.class.getName());
 
-	/**
-	 * Assumes the partial outer join is keeping all values in table1Col for performMatch method
-	 */
-	public ExactStringPartialOuterJoinMatcher() {
+	public ExactStringOuterJoinMatcher() {
 		super();
 	}
 	
@@ -28,14 +25,17 @@ public class ExactStringPartialOuterJoinMatcher extends ExactStringMatcher {
 
 		ITableDataFrame bTree = new BTreeDataFrame(new String[]{table1ValueKey, table2ValueKey});
 
+		boolean[] fullOuter = new boolean[table2Col.length];
+		
 		int success = 0;
 		int total = 0;
 		for(int i = 0; i < table1Col.length; i++) {
 			boolean matchFound = false;
 			for(int j = 0; j < table2Col.length; j++) {
 				if(match(table1Col[i],table2Col[j])) {
-					// update boolean for partial outer join
+					// update booleans for outer joins
 					matchFound = true;
+					fullOuter[j] = true;
 					
 					System.out.println("MATCHED::::::::::::::::: " + table1Col[i] + "      " +   table2Col[j]  );
 					Map<String, Object> row = new HashMap<String, Object>();
@@ -57,6 +57,16 @@ public class ExactStringPartialOuterJoinMatcher extends ExactStringMatcher {
 			}
 		}
 
+		for(int i = 0; i < table2Col.length; i++) {
+			if(!fullOuter[i]) {
+				System.out.println("OUTER JOIN MATCHED::::::::::::::::: NULL        " + table2Col[i]);
+				Map<String, Object> row = new HashMap<String, Object>();
+				row.put(table1ValueKey , SimpleTreeNode.EMPTY);
+				row.put(table2ValueKey, table2Col[i]);
+				bTree.addRow(row, row); //TODO: adding values as both raw and clean
+			}
+		}
+		
 		this.resultMetadata.put("success", success);
 		this.resultMetadata.put("total", total);
 
