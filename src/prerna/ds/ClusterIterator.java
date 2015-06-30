da50414aba1973e434ae12a89ad0fb85e97efb96
rluthar <rluthar@deloitte.com>
2015-06-30 03:41:06 +0000

    Special Iterator needed for Modified K-means clustering
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@8438 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/ds/ClusterIterator.java
@@ -0,0 +1,64 @@
+package prerna.ds;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+public class ClusterIterator implements Iterator<List<Object[]>>{
+
+	private IndexTreeIterator iterator;
+	
+	ClusterIterator(TreeNode columnRoot) {
+		iterator = new IndexTreeIterator(columnRoot);
+	}
+	@Override
+	public boolean hasNext() {
+		return iterator.hasNext();
+	}
+
+	@Override
+	public List<Object[]> next() {
+		
+		List<Object[]> retList = new ArrayList<Object[]>();
+		
+		TreeNode treenode = iterator.next();
+		Vector<SimpleTreeNode> instances = treenode.instanceNode;
+		
+		for(SimpleTreeNode node: instances) {
+			retList.addAll(getInstanceRows(node));
+		}
+		
+		return retList;
+	}
+	
+	private List<Object[]> getInstanceRows(SimpleTreeNode node) {
+		List<Object[]> instanceRows = new ArrayList<>();
+		//populate list recursively
+		getRow(instanceRows, node);
+		return instanceRows;
+	}
+	
+	private void getRow(List<Object[]> list, SimpleTreeNode node) {
+		if(node.leftChild == null) {
+			List<Object> arraylist = new ArrayList<>();
+			SimpleTreeNode n = node;
+			while(n!=null) {
+				arraylist.add(((ISEMOSSNode)n.leaf).getValue());
+				n = n.parent;
+			}
+			
+			Collections.reverse(arraylist);
+			list.add(arraylist.toArray());
+			
+		} else {
+			getRow(list, node.leftChild);
+		}
+		
+		if(node.rightSibling!=null) {
+			getRow(list, node.rightSibling);
+		}
+	}
+
+}
