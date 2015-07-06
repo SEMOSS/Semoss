090e78c0946b781464db1bac24a12da52379d243
bisutton <bisutton@deloitte.com>
2015-07-06 19:05:04 +0000

    Initial commit of SQL Server. Some refactoring is still needed on the RDBMS side of the house.
    As listed by Elena:
    
    1.	Can the method getDialectRemoveDuplicates be over ridden in SQLServerQueryUtil.java instead of adding SQL Server specific code to the abstract SQLUtility class?
    2.	In RDBMSReader, method writeDefaultQuestionSheet, we should look into overriding getDialectForceGraph in SQLServerQueryUtil instead of doing custom code for SQL server in RDBMSReader.  
    3.	I see one other spot that we have custom code for both maria and sql server and I'll make the necessary adjustments to abstract that logic, I’ll most likely wait until next sprint in order to minimize risk.
    4.	In RDBMSReader some code (2 lines of code) was removed from the method importFileWithConnection, if you plan on making any of these suggested changes can you please check it back, otherwise please let me know and I can move the code back.
    5.	In RDBMSReader can we look into abstracting the custom logic for SQLServerQueryUtil in method getAlterTable?  It looks like SQL server doesn’t need the leading parenthesis for the add statement, but we don’t have any special code to prevent adding the trailing parenthesis – is this correct?  It seems a bit odd to me.
    
    
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@8552 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 059da21320ad194fbfe30544e8a5c5222a8356b6

---
+++ src/prerna/util/sql/SQLServerQueryUtil.java
@@ -0,0 +1,146 @@
+package prerna.util.sql;
+
+import java.util.ArrayList;
+
+public class SQLServerQueryUtil extends SQLQueryUtil {	
+	public static final String DATABASE_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	private static String connectionBase = "jdbc:sqlserver://127.0.0.1:1433"; // localhost using SQL Server authentication, default port number is 1433 or use jdbc:sqlserver://127.0.0.1:1433;databaseName=TestDB;user=root;Password=root
+
+	public SQLServerQueryUtil(){
+		super.setDialectAllTables(" SELECT * FROM INFORMATION_SCHEMA.TABLES ");//
+		super.setDialectAllColumns(" SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ");//
+		super.setResultAllTablesTableName("TABLE_NAME");//
+		super.setResultAllColumnsColumnName("COLUMN_NAME");
+		super.setResultAllColumnsColumnType("DATA_TYPE");
+		super.setDialectAllIndexesInDB("SELECT DISTINCT NAME FROM SYS.INDEXES ORDER BY NAME");//
+		super.setDialectOuterJoinLeft(" LEFT OUTER JOIN ");
+		super.setDialectOuterJoinRight(" RIGHT OUTER JOIN ");
+		super.setDefaultDbUserName("root");//
+		super.setDefaultDbPassword("root");//
+	}
+
+	@Override
+	public SQLQueryUtil.DB_TYPE getDatabaseType(){
+		return SQLQueryUtil.DB_TYPE.SQL_SERVER;
+	}
+	public String getDialectAllColumns(String tableName){
+		return super.getDialectAllColumns() + "'" + tableName + "'" ;
+	}
+	@Override
+	public String getDialectAllIndexesInDB(String schema){
+		return super.getDialectAllIndexesInDB(); //dont plop schema into here
+	}
+	//SQL Server - USE_OUTER_JOINS_NO
+	@Override
+	public String getDefaultOuterJoins(){
+		return SQLQueryUtil.USE_OUTER_JOINS_YES;
+	}
+
+	//jdbc:sqlserver://localhost:1433;databaseName=dbname;user=username;Password=password;selectMethod=cursor
+	@Override
+	public String getConnectionURL(String baseFolder,String dbname){
+		return connectionBase + ";databaseName=" + dbname + ";user=" + super.getDefaultDBUserName() + ";Password=" + super.getDefaultDBPassword() + ";selectMethod=cursor";
+	}
+	public static String getTempConnectionURL(){
+		return connectionBase;
+	}
+
+	@Override
+	public String getDatabaseDriverClassName(){
+		return DATABASE_DRIVER;
+	}
+
+	@Override
+	public String getDialectIndexInfo(String indexName, String dbName){
+		return super.getDialectIndexInfo() + "'" + indexName+ "'"; 
+	}
+
+	public String getDialectAlterTableName(String fromName, String toName){
+		return "sp_rename '" + fromName + "', '" + toName + "'"; //"sp_rename '" + fromName', '" + toName + "'"		
+	}
+
+	//For SQL server, TOP is used instead of LIMIT (last parameter)
+	public String getDialectDistinctInnerJoinQuery(String selectors, String froms, String joins, int top){		
+		String query = super.dialectSelect + super.dialectDistinct;
+		if(top!=-1){
+			query += "TOP " + top + " ";
+		}
+		query += selectors + "  FROM  " + froms + joins;		
+		return query;
+	}
+
+	//"full outer join"
+	//For SQL server, TOP is used instead of LIMIT (last parameter)
+	public String getDialectFullOuterJoinQuery(boolean distinct, String selectors, ArrayList<String> rightJoinsArr, 
+			ArrayList<String> leftJoinsArr, ArrayList<String> joinsArr, String filters, int top, String groupBy){
+
+		String rightOuterJoins = "";
+		String leftOuterJoins = "";
+		String joins = "";
+
+		if(rightJoinsArr.size() == leftJoinsArr.size() && (rightJoinsArr.size()-1) <= joinsArr.size()){
+			System.out.println("getDialectDistinctFullOuterJoinQuery: can continue");
+		} else {
+			System.out.println("getDialectDistinctFullOuterJoinQuery: cant continue");
+		}
+
+		for(int i = 0; i < rightJoinsArr.size(); i++){
+			rightOuterJoins += rightJoinsArr.get(i);
+			leftOuterJoins += leftJoinsArr.get(i);
+			if(i!=0){
+				rightOuterJoins += " ON " + joinsArr.get(i-1);
+				leftOuterJoins += " ON " + joinsArr.get(i-1);
+			}
+		}
+		// if the joinsArray still has more
+		if((rightJoinsArr.size()-1) < joinsArr.size()){
+			for(int i = rightJoinsArr.size()-1; i < joinsArr.size(); i++){
+				if(joins.length()>0) joins += " AND ";
+				joins += joinsArr.get(i);
+			}
+		}
+
+		String queryJoinsAndFilters = "";
+		if(joins.length() > 0 && filters.length() > 0)
+			queryJoinsAndFilters = joins + " AND " + filters;
+		else if(joins.length() > 0){
+			queryJoinsAndFilters = joins;
+		}
+		else if(filters.length() > 0){
+			queryJoinsAndFilters = filters;
+		}
+		if(queryJoinsAndFilters.length() > 0)
+			queryJoinsAndFilters = " WHERE " + queryJoinsAndFilters;
+
+		String selectStr = super.dialectSelect;
+		if(distinct) selectStr+= super.dialectDistinct;
+		//String query = super.dialectSelect + super.dialectDistinct;
+		if(top!=-1){
+			selectStr += " TOP " + top + " ";
+		}
+
+		String query = selectStr + selectors + "  FROM  " + leftOuterJoins + queryJoinsAndFilters; //+ " LIMIT " + limit ;
+		if(groupBy !=null && groupBy.length()>0){
+			query += " GROUP BY " + groupBy;
+		}
+
+		query += " UNION ";
+		query += selectStr + selectors + "  FROM  " + rightOuterJoins + queryJoinsAndFilters; //+ " LIMIT " + limit ;
+
+		if(groupBy !=null && groupBy.length()>0){
+			query += " GROUP BY " + groupBy;
+		}
+		return query;
+
+	}
+
+//	@Override
+	public String getDialectFullOuterJoinQuery1(boolean distinct,
+			String selectors, ArrayList<String> rightJoinsArr,
+			ArrayList<String> leftJoinsArr, ArrayList<String> joinsArr,
+			String filters, int limit, String groupBy) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
