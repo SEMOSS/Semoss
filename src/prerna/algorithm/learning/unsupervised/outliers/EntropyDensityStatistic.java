80bebb89633da8b9bd0caf3e08dade413e91cf10
Maher Khalil <themaherkhalil@gmail.com>
2015-07-31 17:29:16 +0000

    update to send header uri's in btree
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@9140 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 9a02f4b2d095a5a2a9b81a1dc54362537d49e5d7

--- src/prerna/algorithm/learning/unsupervised/outliers/EntropyDensityStatistic.java
+++ src/prerna/algorithm/learning/unsupervised/outliers/EntropyDensityStatistic.java
@@ -1,14 +1,31 @@
-/**
- *  This code looks for a specified number of outliers within a categorical dataset.
- *  
- *  It is used strictly for categorical data, and can be thought of as LOF for
- *     categorical data.
- *     
- *  For more documentation about how it works, see the original paper here:
- *  http://arxiv.org/ftp/cs/papers/0503/0503081.pdf
- *  
- *  @author Jason Adleberg, Rishi Luthar, Maher Ashraf Khalil
- */
+/*******************************************************************************
+ * Copyright 2015 Defense Health Agency (DHA)
+ *
+ * If your use of this software does not include any GPLv2 components:
+ * 	Licensed under the Apache License, Version 2.0 (the "License");
+ * 	you may not use this file except in compliance with the License.
+ * 	You may obtain a copy of the License at
+ *
+ * 	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * 	Unless required by applicable law or agreed to in writing, software
+ * 	distributed under the License is distributed on an "AS IS" BASIS,
+ * 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 	See the License for the specific language governing permissions and
+ * 	limitations under the License.
+ * ----------------------------------------------------------------------------
+ * If your use of this software includes any GPLv2 components:
+ * 	This program is free software; you can redistribute it and/or
+ * 	modify it under the terms of the GNU General Public License
+ * 	as published by the Free Software Foundation; either version 2
+ * 	of the License, or (at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *******************************************************************************/
+
 package prerna.algorithm.learning.unsupervised.outliers;
 
 import java.util.ArrayList;
