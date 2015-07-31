9d6c6b4aa2c72096ea2253ead673ad31e842c1b7
rluthar <rluthar@deloitte.com>
2015-07-31 21:15:32 +0000

    fixing more filter bugs
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@9172 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c802d64ebdb791b53b3ec3279e91855415491086

--- src/prerna/ds/SimpleTreeNode.java
+++ src/prerna/ds/SimpleTreeNode.java
@@ -1,3 +1,31 @@
+/*******************************************************************************
+ * Copyright 2015 Defense Health Agency (DHA)
+ *
+ * If your use of this software does not include any GPLv2 components:
+ * 	Licensed under the Apache License, Version 2.0 (the "License");
+ * 	you may not use this file except in compliance with the License.
+ * 	You may obtain a copy of the License at
+ *
+ * 	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * 	Unless required by applicable law or agreed to in writing, software
+ * 	distributed under the License is distributed on an "AS IS" BASIS,
+ * 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 	See the License for the specific language governing permissions and
+ * 	limitations under the License.
+ * ----------------------------------------------------------------------------
+ * If your use of this software includes any GPLv2 components:
+ * 	This program is free software; you can redistribute it and/or
+ * 	modify it under the terms of the GNU General Public License
+ * 	as published by the Free Software Foundation; either version 2
+ * 	of the License, or (at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *******************************************************************************/
+
 package prerna.ds;
 
 import java.io.BufferedReader;
@@ -152,6 +180,23 @@ public class SimpleTreeNode {
 		}
 		return count;
 	}
+	
+	public int countNodeChildren(SimpleTreeNode counterNode)
+	{
+		int count = 0;
+		SimpleTreeNode leftChild = counterNode.leftChild;
+		if(leftChild == null) {
+			return count;
+		} else {
+			count++;
+		}
+		while(leftChild.rightSibling != null) {
+			count++;
+			leftChild = leftChild.rightSibling;
+		}
+		return count;
+	}
+
 
 	public boolean hasChildren(SimpleTreeNode counterNode)
 	{
@@ -184,20 +229,21 @@ public class SimpleTreeNode {
 	
 	public static SimpleTreeNode getLeft(SimpleTreeNode node)
 	{
-		while(node.leftSibling != null)
-		{
-			node = node.leftSibling;
+		if(node.leftSibling==null) {
+			return node;
 		}
-		return node;
+		else {
+			return getLeft(node.leftSibling);
+		}
 	}
 
 	public static SimpleTreeNode getRight(SimpleTreeNode node)
 	{
-		while(node.rightSibling != null)
-		{
-			node = node.rightSibling;
+		if(node.rightSibling==null) {
+			return node;
+		} else {
+			return getRight(node.rightSibling);
 		}
-		return node;
 	}
 	
 
