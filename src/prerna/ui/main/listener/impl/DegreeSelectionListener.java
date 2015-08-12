5a6b866b907ff0a2d373914331c6dd1acc70fce5
rluthar <rluthar@deloitte.com>
2015-08-12 16:40:40 +0000

    forgot one class
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@9459 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2363a94841cef0b45d0bda779562a3b5effea31a

---
+++ src/prerna/ui/main/listener/impl/DegreeSelectionListener.java
@@ -0,0 +1,42 @@
+package prerna.ui.main.listener.impl;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import prerna.ui.components.playsheets.MachineLearningModulePlaySheet;
+
+public class DegreeSelectionListener extends AbstractListener{
+	static final Logger logger = LogManager.getLogger(ClassificationSelectionListener.class.getName());
+	
+	//shows or hides the JTextField for inputting a number of clusters depending on if the JComboBox is set to manually select
+	private MachineLearningModulePlaySheet playSheet;
+	
+	/**
+	 * Method actionPerformed.
+	 * @param e ActionEvent
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JComboBox<String> bx = (JComboBox<String>)e.getSource();
+		String selection = bx.getSelectedItem() + "";
+		//if manually select need to show text field, otherwise hide it
+		if(!selection.equals("Polynomial") && !selection.equals("Sigmoid"))
+			playSheet.showPolynomialDegreeTextField(false);
+		else 
+			playSheet.showPolynomialDegreeTextField(true);
+	}
+
+	/**
+	 * Method setView. Sets a JComponent that the listener will access and/or modify when an action event occurs.  
+	 * @param view the component that the listener will access
+	 */
+	@Override
+	public void setView(JComponent view) {
+		this.playSheet = (MachineLearningModulePlaySheet) view;
+	}
+}
