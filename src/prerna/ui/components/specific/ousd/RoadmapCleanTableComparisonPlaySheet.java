35608e52dd2bf60899a978270660e91c32fa9496
spanderson <spanderson@deloitte.com>
2015-09-01 21:06:56 +0000

    spanderson: refactored some of the rows and created a new method
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@9738 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 0e2fc180f536cbc44e5e38da8b64727b570b0867

--- src/prerna/ui/components/specific/ousd/RoadmapCleanTableComparisonPlaySheet.java
+++ src/prerna/ui/components/specific/ousd/RoadmapCleanTableComparisonPlaySheet.java
@@ -17,7 +17,7 @@ public class RoadmapCleanTableComparisonPlaySheet extends GridPlaySheet{
 	OUSDTimeline comparatorTimeline;
 	String roadmapName;
 	String comparatorName;
-	
+
 	@Override
 	public void setQuery(String query){
 		String delimiters = "[;]";
@@ -42,19 +42,19 @@ public class RoadmapCleanTableComparisonPlaySheet extends GridPlaySheet{
 		}
 
 		List<Integer> fyList = timeline.getFiscalYears();
-			
+
 		List<Integer> comparatorFyList = comparatorTimeline.getFiscalYears();
-		
+
 		List<Integer> yearList = new ArrayList<Integer>();
 		yearList.addAll(fyList);
-		
+
 		for(Integer year: comparatorFyList){
 			if(!yearList.contains(year)){
 				yearList.add(year);
 			}
 		}
 		Collections.sort(yearList);
-		
+
 		String[] columns = new String[yearList.size() + 1];
 		columns[0] = "System";
 		int count = 1;
@@ -79,7 +79,7 @@ public class RoadmapCleanTableComparisonPlaySheet extends GridPlaySheet{
 
 	private void createTable(List<Integer> yearList, OUSDTimeline timeline, List<Integer> fyList, OUSDTimeline comparatorTimeline, List<Integer> comparatorFyList){
 
-//		this.list = new ArrayList<Object[]>();
+		//		this.list = new ArrayList<Object[]>();
 		String[] names = this.dataFrame.getColumnHeaders();
 		List<String> processedSystems = new ArrayList<String>();
 		List<String> comparatorProcessedSystems = new ArrayList<String>();
@@ -87,43 +87,37 @@ public class RoadmapCleanTableComparisonPlaySheet extends GridPlaySheet{
 		List<Map<String, List<String>>> systemYears = timeline.getTimeData();
 		Map<String, Double> budgets = timeline.getBudgetMap();
 		Object[] row = new Object[names.length];
-		row[0] = this.roadmapName;
-		Object[] rowCost = new Object[names.length];
-		rowCost[0] = this.roadmapName + " Build Cost";
+		row[0] = this.roadmapName + " Annual Savings";
+		Object[] rowBuildCost = new Object[names.length];
+		rowBuildCost[0] = this.roadmapName + " Build Cost";
 		Object[] rowSustainCost = new Object[names.length];
 		rowSustainCost[0] = this.roadmapName + " Sustainment Cost";
 
 		List<Map<String, List<String>>> comparatorYears = comparatorTimeline.getTimeData();
 		Map<String, Double> comparatorBudgets = comparatorTimeline.getBudgetMap();
 		Object[] comparatorRow = new Object[names.length];
-		comparatorRow[0] = this.comparatorName;
-		Object[] rowCostComp = new Object[names.length];
-		rowCostComp[0] = this.comparatorName + " Build Cost";
-		Object[] rowSustainCostComp = new Object[names.length];
-		rowSustainCostComp[0] = this.comparatorName + " Sustainment Cost";
-		Object[] cumulativeSavingComp = new Object[names.length];
-		cumulativeSavingComp[0] = this.comparatorName + " Cumulative Savings";
-		Object[] cumulativeCostComp = new Object[names.length];
-		cumulativeCostComp[0] = this.comparatorName + " Cumulative Cost";
-		Object[] roiCostComp = new Object[names.length];
-		roiCostComp[0] = this.comparatorName + " ROI";
-		
-		
+		comparatorRow[0] = this.comparatorName + " Annual Savings";
+		Object[] compRowBuildCost = new Object[names.length];
+		compRowBuildCost[0] = this.comparatorName + " Build Cost";
+		Object[] compRowSustainCost = new Object[names.length];
+		compRowSustainCost[0] = this.comparatorName + " Sustainment Cost";
 
 		NumberFormat formatter = NumberFormat.getCurrencyInstance(Locale.US);
+		NumberFormat percentFormat = NumberFormat.getPercentInstance();
+		percentFormat.setMinimumFractionDigits(1);
 
 		List<Map<String, Double>> invest = timeline.getSystemInvestmentMap();
 		List<Map<String, Double>> investComparator = comparatorTimeline.getSystemInvestmentMap();
-		
+
 		List<Map<String, Double>> sustain = timeline.getInterfaceSustainmentMap();
 		List<Map<String, Double>> sustainComparator = comparatorTimeline.getInterfaceSustainmentMap();
-		
+
 		for(int i = names.length-1; i>0; i--){
 			int year = yearList.get(i-1);
 			if(fyList.contains(year)){
 				int yearIdx = timeline.getFyIndexFiscalYear(year);	
 				double yearTotal = 0.0;
-				
+
 				Map<String, List<String>> yearMap = systemYears.get(yearIdx);
 				for(String system: yearMap.keySet()){
 					if(!processedSystems.contains(system)){
@@ -133,14 +127,14 @@ public class RoadmapCleanTableComparisonPlaySheet extends GridPlaySheet{
 				}
 				String formattedTotal = formatter.format(yearTotal);
 				row[i] = formattedTotal;
-				
+
 				if(invest!=null){
 					Map<String, Double> yearInvest = invest.get(yearIdx);
 					double totalInvest = 0.;
 					for(Double val : yearInvest.values()){
 						totalInvest = totalInvest + val;
 					}
-					rowCost[i] = formatter.format(totalInvest);
+					rowBuildCost[i] = formatter.format(totalInvest);
 				}
 				if(sustain!=null){
 					Map<String, Double> yearSustain = sustain.get(yearIdx);
@@ -150,11 +144,28 @@ public class RoadmapCleanTableComparisonPlaySheet extends GridPlaySheet{
 					}
 					rowSustainCost[i] = formatter.format(totalSustain);
 				}
+			}else{				
+				int tempYear = year-1;
+				while(!fyList.contains(tempYear) && tempYear >2000){
+					tempYear--;
+				}
+				if(tempYear != 2000){
+					int yearIdx = timeline.getFyIndexFiscalYear(tempYear);
+
+					if(sustain!=null){
+						Map<String, Double> yearSustain = sustain.get(yearIdx);
+						double totalSustain = 0.;
+						for(Double val : yearSustain.values()){
+							totalSustain = totalSustain + val;
+						}
+						rowSustainCost[i] = formatter.format(totalSustain);
+					}
+				}
 			}
 			if(comparatorFyList.contains(year)){
 				int yearIdx = comparatorTimeline.getFyIndexFiscalYear(year);	
 				double yearTotal = 0.0;
-				
+
 				Map<String, List<String>> yearMap = comparatorYears.get(yearIdx);
 				for(String system: yearMap.keySet()){
 					if(!comparatorProcessedSystems.contains(system)){
@@ -171,7 +182,7 @@ public class RoadmapCleanTableComparisonPlaySheet extends GridPlaySheet{
 					for(Double val : yearInvest.values()){
 						totalInvest = totalInvest + val;
 					}
-					rowCostComp[i] = formatter.format(totalInvest);
+					compRowBuildCost[i] = formatter.format(totalInvest);
 				}
 				if(sustainComparator!=null){
 					Map<String, Double> yearSustain = sustainComparator.get(yearIdx);
@@ -179,7 +190,7 @@ public class RoadmapCleanTableComparisonPlaySheet extends GridPlaySheet{
 					for(Double val : yearSustain.values()){
 						totalSustain = totalSustain + val;
 					}
-					rowSustainCostComp[i] = formatter.format(totalSustain);
+					compRowSustainCost[i] = formatter.format(totalSustain);
 				}
 			}else{				
 				int tempYear = year-1;
@@ -188,65 +199,85 @@ public class RoadmapCleanTableComparisonPlaySheet extends GridPlaySheet{
 				}
 				if(tempYear != 2000){
 					int yearIdx = comparatorTimeline.getFyIndexFiscalYear(tempYear);
-					
+
 					if(sustainComparator!=null){
 						Map<String, Double> yearSustain = sustainComparator.get(yearIdx);
 						double totalSustain = 0.;
 						for(Double val : yearSustain.values()){
 							totalSustain = totalSustain + val;
 						}
-						rowSustainCostComp[i] = formatter.format(totalSustain);
+						compRowSustainCost[i] = formatter.format(totalSustain);
 					}
 				}
 			}			
 		}
-		
+		this.dataFrame.addRow(row, row);
+		this.dataFrame.addRow(rowBuildCost, rowBuildCost);
+		this.dataFrame.addRow(rowSustainCost, rowSustainCost);
+
+		this.dataFrame.addRow(comparatorRow, comparatorRow);
+		this.dataFrame.addRow(compRowBuildCost, compRowBuildCost);
+		this.dataFrame.addRow(compRowSustainCost, compRowSustainCost);
+
+		additionalRowBuilder(comparatorRow, compRowSustainCost, compRowBuildCost, names, formatter, percentFormat, this.comparatorName);
+	}
+
+	private void additionalRowBuilder(Object[] baseRow, Object[] sustainRow, Object[] buildCostRow, String[] names, NumberFormat formatter, NumberFormat percentFormat, String name){
+
 		double cumulativeCost=0.0;
 		double cumulativeSavings=0.0;
+
+		List<Double> annualSavings = new ArrayList<Double>();
+
+		Object[] cumulativeSavingsRow = new Object[names.length];
+		cumulativeSavingsRow[0] = name + " Cumulative Savings";
+		Object[] cumulativeTotalCostRow = new Object[names.length];
+		cumulativeTotalCostRow[0] = name + " Cumulative Cost";
+		Object[] roiRow = new Object[names.length];
+		roiRow[0] = name + " ROI";
 		
 		for(int i=2; i<names.length; i++){
-			if(rowSustainCostComp[i] != null){
-				String sustainmentCost = rowSustainCostComp[i].toString().replace("$", "").replace(",", "");
+
+			//cumulative savings
+			if(baseRow[i] != null){
+				String savings = baseRow[i].toString().replace("$", "").replace(",", "");
+				annualSavings.add(Double.parseDouble(savings));				
+			}
+			for(Double value: annualSavings){
+				cumulativeSavings = cumulativeSavings + value;
+			}
+			cumulativeSavingsRow[i] = formatter.format(cumulativeSavings);
+
+
+			//row sustainment cost and cumulative total cost
+			if(sustainRow[i] != null){
+				String sustainmentCost = sustainRow[i].toString().replace("$", "").replace(",", "");
 				cumulativeCost = cumulativeCost + Double.parseDouble(sustainmentCost);
 			}
-			if(rowCostComp[i] != null){
-				String cost = rowCostComp[i].toString().replace("$", "").replace(",", "");
+			if(buildCostRow[i] != null){
+				String cost = buildCostRow[i].toString().replace("$", "").replace(",", "");
 				cumulativeCost = cumulativeCost + Double.parseDouble(cost);
 			}
-			cumulativeCostComp[i] = formatter.format(cumulativeCost);
-		}
-		
-		for(int i=2; i<names.length; i++){
-			if(comparatorRow[i] != null){
-				String savings = comparatorRow[i].toString().replace("$", "").replace(",", "");
-				cumulativeSavings = cumulativeSavings + Double.parseDouble(savings);
-			}
-			cumulativeSavingComp[i] = formatter.format(cumulativeSavings);
-		}
-		
-		for(int i=2; i<names.length; i++){
+			cumulativeTotalCostRow[i] = formatter.format(cumulativeCost);
+
+
+			//ROI value for each year
 			double netSavings = 0.0;
 			double investment = 0.0;
-			if(cumulativeCostComp[i] != null){
-				String cost = cumulativeCostComp[i].toString().replace("$", "").replace(",", "");
+			if(cumulativeTotalCostRow[i] != null){
+				String cost = cumulativeTotalCostRow[i].toString().replace("$", "").replace(",", "");
 				investment = Double.parseDouble(cost);
 			}
-			if(cumulativeSavingComp[i] != null){
-				String savings = cumulativeSavingComp[i].toString().replace("$", "").replace(",", "");
+			if(cumulativeSavingsRow[i] != null){
+				String savings = cumulativeSavingsRow[i].toString().replace("$", "").replace(",", "");
 				netSavings = Double.parseDouble(savings) - investment; 
 			}
 			double roi = netSavings/investment;
-			roiCostComp[i] = roi;
+			roiRow[i] = percentFormat.format(roi);
 		}
 		
-		this.dataFrame.addRow(row, row);
-		this.dataFrame.addRow(rowCost, rowCost);
-		this.dataFrame.addRow(comparatorRow, comparatorRow);
-		this.dataFrame.addRow(rowCostComp, rowCostComp);
-		this.dataFrame.addRow(rowSustainCost, rowSustainCost);
-		this.dataFrame.addRow(rowSustainCostComp, rowSustainCostComp);
-		this.dataFrame.addRow(cumulativeSavingComp, cumulativeSavingComp);
-		this.dataFrame.addRow(cumulativeCostComp, cumulativeCostComp);
-		this.dataFrame.addRow(roiCostComp, roiCostComp);
+		this.dataFrame.addRow(cumulativeSavingsRow, cumulativeSavingsRow);
+		this.dataFrame.addRow(cumulativeTotalCostRow, cumulativeTotalCostRow);
+		this.dataFrame.addRow(roiRow, roiRow);
 	}
 }
