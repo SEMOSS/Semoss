53f1c8f4d4cfba9a535b80cab73dd340494c1207
rluthar <rluthar@deloitte.com>
2015-09-16 16:01:50 +0000

    Updates to InfiniteScroller - now infinite scroll by column, need to add ascending/descending capability, currently only ascending
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@9988 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 0da987c73615469c61ff104d860504be0fd376de

--- src/prerna/ds/BTreeInfiniteScroller.java
+++ src/prerna/ds/BTreeInfiniteScroller.java
@@ -11,8 +11,10 @@ public class BTreeInfiniteScroller implements InfiniteScroller {
 	private BTreeDataFrame table;
 	private int returnCount;
 	private Map<String, Iterator<TreeNode>> iteratorMap;
-	private Iterator<TreeNode> rowIterator;
+	private Iterator<List<HashMap<String, Object>>> rowIterator;
 	private String lastColumn;
+	private String iteratorColumn;
+	private int currentCount;
 	private final static String VALUE = "value";
 	private final static String SELECTED = "selected";
 	
@@ -20,7 +22,8 @@ public class BTreeInfiniteScroller implements InfiniteScroller {
 		this.table = table;
 		returnCount = 100;
 		iteratorMap = new HashMap<String, Iterator<TreeNode>>();
-		rowIterator = new IndexTreeIterator(table.getBuilder().nodeIndexHash.get(lastColumn));
+		rowIterator = new WebBTreeIterator(table.getBuilder().nodeIndexHash.get(lastColumn));
+		currentCount = 0;
 	}
 	
 	public void resetIterators() {
@@ -36,7 +39,8 @@ public class BTreeInfiniteScroller implements InfiniteScroller {
 			lastColumn = columnHeaders[lastIndex];
 		}
 		
-		rowIterator = new FilteredIndexTreeIterator(table.getBuilder().nodeIndexHash.get(lastColumn));
+		rowIterator = new WebBTreeIterator(table.getBuilder().nodeIndexHash.get(lastColumn), false);
+		currentCount = 0;
 	}
 	
 	public List<HashMap<String, String>> getNextUniqueValues(String columnHeader) {
@@ -72,29 +76,38 @@ public class BTreeInfiniteScroller implements InfiniteScroller {
 //		return null;
 //	}
 	
-	public List<HashMap<String, Object>> getNextData() {
+	public List<HashMap<String, Object>> getNextData(String column) {
 		
-		//Check to see if the table has changed
+		//Check to see if the table has changed or column has changed
 		String[] columnHeaders = table.getColumnHeaders();
 		int lastIndex = columnHeaders.length - 1;
-		if(!columnHeaders[lastIndex].equalsIgnoreCase(lastColumn)) {
+		if(!columnHeaders[lastIndex].equalsIgnoreCase(lastColumn) || !column.equalsIgnoreCase(iteratorColumn)) {
+			iteratorColumn = column;
 			lastColumn = columnHeaders[lastIndex];
-			rowIterator = new FilteredIndexTreeIterator(table.getBuilder().nodeIndexHash.get(lastColumn));
+			ArrayList<String> columns2skip = new ArrayList<String>(columnHeaders.length);
+			for(String column2skip : table.getColumnsToSkip()) {
+				columns2skip.add(column2skip);
+			}
+			rowIterator = new WebBTreeIterator(table.getBuilder().nodeIndexHash.get(column), false, columns2skip);
 		}
 		
 		//create the data in the format the front end expects
 		ArrayList<HashMap<String, Object>> nextData = new ArrayList<HashMap<String, Object>>();
-		HashMap<String, Object> row;
-		for(int i = 0; i < returnCount && rowIterator.hasNext(); i++) {
-			row = new HashMap<String, Object>();
-			TreeNode node = rowIterator.next();
-			while(node != null) {
-				ISEMOSSNode sNode = ((ISEMOSSNode)node.leaf);
-				row.put(sNode.getType(), sNode.getRawValue());
-				node = node.parent;
-			}
-			nextData.add(row);
+		while(nextData.size() < 100 && rowIterator.hasNext()) {
+			nextData.addAll(rowIterator.next());
 		}
+//		HashMap<String, Object> row;
+//		for(int i = 0; i < returnCount && rowIterator.hasNext(); i++) {
+//			row = new HashMap<String, Object>();
+//			TreeNode node = rowIterator.next();
+//			while(node != null) {
+//				ISEMOSSNode sNode = ((ISEMOSSNode)node.leaf);
+//				row.put(sNode.getType(), sNode.getRawValue());
+//				node = node.parent;
+//			}
+//			nextData.add(row);
+//			currentCount++;
+//		}
 		return nextData;
 	}
 }
