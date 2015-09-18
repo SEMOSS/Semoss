1d116d6b732383f43c0a2afe81cdfa9194e11506
rluthar <rluthar@deloitte.com>
2015-09-18 20:44:17 +0000

    updates to infinite scroller to take start/end parameters
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@10051 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ds/BTreeInfiniteScroller.java
+++ src/prerna/ds/BTreeInfiniteScroller.java
@@ -9,109 +9,110 @@ import java.util.Map;
 public class BTreeInfiniteScroller implements InfiniteScroller {
 
 	private BTreeDataFrame table;
-	private int returnCount;
-	private Map<String, Iterator<TreeNode>> iteratorMap;
+	private static final int returnCount = 100;
 	private Iterator<List<HashMap<String, Object>>> rowIterator;
-	private String lastColumn;
 	private String iteratorColumn;
 	private int currentCount;
-	private final static String VALUE = "value";
-	private final static String SELECTED = "selected";
+	
+	private List<HashMap<String, Object>> rowBuffer;
+//	private Map<String, Iterator<TreeNode>> iteratorMap;
 	
 	public BTreeInfiniteScroller(BTreeDataFrame table) {
 		this.table = table;
-		returnCount = 100;
-		iteratorMap = new HashMap<String, Iterator<TreeNode>>();
-		String[] columns = table.getColumnHeaders();
-		lastColumn = columns[columns.length-1];
-		iteratorColumn = lastColumn;
-		rowIterator = new WebBTreeIterator(table.getBuilder().nodeIndexHash.get(lastColumn));
-		currentCount = 0;
+		iteratorColumn = table.getColumnHeaders()[0];
+		resetTable();
+		
+//		iteratorMap = new HashMap<String, Iterator<TreeNode>>();
 	}
 	
 	public void resetIterators() {
-		iteratorMap.clear();
+//		iteratorMap.clear();
 	}
 	
-	public void resetTable() {
-		
-		//Check to see if the table has changed
+	public void resetTable() {	
 		String[] columnHeaders = table.getColumnHeaders();
-		int lastIndex = columnHeaders.length - 1;
-		if(!columnHeaders[lastIndex].equalsIgnoreCase(lastColumn)) {
-			lastColumn = columnHeaders[lastIndex];
+		ArrayList<String> columns2skip = new ArrayList<String>(columnHeaders.length);
+		for(String column2skip : table.getColumnsToSkip()) {
+			columns2skip.add(column2skip);
 		}
-		
-		rowIterator = new WebBTreeIterator(table.getBuilder().nodeIndexHash.get(lastColumn), false);
+		rowIterator = new WebBTreeIterator(table.getBuilder().nodeIndexHash.get(iteratorColumn), false, columns2skip);
+		rowBuffer = new ArrayList<HashMap<String, Object>>();
 		currentCount = 0;
 	}
 	
 	public List<HashMap<String, String>> getNextUniqueValues(String columnHeader) {
 		
-		if(!iteratorMap.containsKey(columnHeader)) {
-			TreeNode root = table.getBuilder().nodeIndexHash.get(columnHeader);
-			iteratorMap.put(columnHeader, new IndexTreeIterator(root));
-		}
-		
-		Iterator<TreeNode> iterator = iteratorMap.get(columnHeader);
-		List<HashMap<String, String>> returnList = new ArrayList<HashMap<String, String>>();
-		int count = 0;
-		while(iterator.hasNext() && count < returnCount) {
-			HashMap<String, String> nextMap = new HashMap<String, String>();
-			TreeNode t = iterator.next();
-			String selected;
-			if(t.instanceNode.size() > 0) {
-				selected = "true";
-			} else if(t.filteredInstanceNode.size() > 0){
-				selected = "false";
-			} else {
-				continue;
-			}
-			String value = iterator.next().leaf.getRawValue().toString();
-			nextMap.put(VALUE, value);
-			nextMap.put(SELECTED, selected);
-			returnList.add(nextMap);
-		}
-		return returnList;
-	}
-	
-//	public List<HashMap<String, Object>> getData(int start, int end) {
-//		return null;
-//	}
-	
-	public List<HashMap<String, Object>> getNextData(String column) {
-		
-		if(column == null) column = iteratorColumn;
-		//Check to see if the table has changed or column has changed
-		String[] columnHeaders = table.getColumnHeaders();
-		int lastIndex = columnHeaders.length - 1;
-		if(!columnHeaders[lastIndex].equalsIgnoreCase(lastColumn) || !column.equalsIgnoreCase(iteratorColumn)) {
-			iteratorColumn = column;
-			lastColumn = columnHeaders[lastIndex];
-			ArrayList<String> columns2skip = new ArrayList<String>(columnHeaders.length);
-			for(String column2skip : table.getColumnsToSkip()) {
-				columns2skip.add(column2skip);
-			}
-			rowIterator = new WebBTreeIterator(table.getBuilder().nodeIndexHash.get(column), false, columns2skip);
-		}
-		
-		//create the data in the format the front end expects
-		ArrayList<HashMap<String, Object>> nextData = new ArrayList<HashMap<String, Object>>();
-		while(nextData.size() < 100 && rowIterator.hasNext()) {
-			nextData.addAll(rowIterator.next());
-		}
-//		HashMap<String, Object> row;
-//		for(int i = 0; i < returnCount && rowIterator.hasNext(); i++) {
-//			row = new HashMap<String, Object>();
-//			TreeNode node = rowIterator.next();
-//			while(node != null) {
-//				ISEMOSSNode sNode = ((ISEMOSSNode)node.leaf);
-//				row.put(sNode.getType(), sNode.getRawValue());
-//				node = node.parent;
-//			}
-//			nextData.add(row);
-//			currentCount++;
+//		if(!iteratorMap.containsKey(columnHeader)) {
+//			TreeNode root = table.getBuilder().nodeIndexHash.get(columnHeader);
+//			iteratorMap.put(columnHeader, new IndexTreeIterator(root));
 //		}
+//		
+//		Iterator<TreeNode> iterator = iteratorMap.get(columnHeader);
+//		List<HashMap<String, String>> returnList = new ArrayList<HashMap<String, String>>();
+//		int count = 0;
+//		while(iterator.hasNext() && count < returnCount) {
+//			HashMap<String, String> nextMap = new HashMap<String, String>();
+//			TreeNode t = iterator.next();
+//			String selected;
+//			if(t.instanceNode.size() > 0) {
+//				selected = "true";
+//			} else if(t.filteredInstanceNode.size() > 0){
+//				selected = "false";
+//			} else {
+//				continue;
+//			}
+//			String value = iterator.next().leaf.getRawValue().toString();
+//			nextMap.put(VALUE, value);
+//			nextMap.put(SELECTED, selected);
+//			returnList.add(nextMap);
+//		}
+//		return returnList;
+		return null;
+	}
+
+	
+	public List<HashMap<String, Object>> getNextData(String column, int start, int end) {
+		
+		List<HashMap<String, Object>> nextData = new ArrayList<HashMap<String, Object>>();
+		
+		//If a new column is used to base sorting off of, reset with new column
+		if(column != null && !column.equals(iteratorColumn)) {
+			iteratorColumn = column;
+			resetTable();
+		}
+		
+		//if iterator is ahead of the start point
+		if(currentCount < start) {
+			
+			resetTable();
+			nextData = this.getNextData(column, 0, end);
+			nextData = nextData.subList(start, end);
+		
+		} else if(currentCount > start) {
+			
+		} else {
+		
+			if(rowBuffer.size() > returnCount) {
+				
+				nextData = rowBuffer.subList(0, returnCount);
+				rowBuffer = rowBuffer.subList(returnCount, rowBuffer.size());
+			
+			} else {
+				
+				nextData.addAll(rowBuffer);
+				rowBuffer.clear();
+				while(nextData.size() < returnCount && rowIterator.hasNext()) {
+					nextData.addAll(rowIterator.next());
+				}
+			
+				if(nextData.size() > returnCount) {
+					rowBuffer = nextData.subList(returnCount, nextData.size());
+					nextData = nextData.subList(0, returnCount);
+				}	
+			}			
+			currentCount += end;
+		}
+
 		return nextData;
 	}
 }
