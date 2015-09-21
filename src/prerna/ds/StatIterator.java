b05444500dfec10085731df5bc23b9b233ba1d00
rluthar <rluthar@deloitte.com>
2015-09-21 22:54:33 +0000

    updates to column grouping
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@10079 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ds/StatIterator.java
+++ src/prerna/ds/StatIterator.java
@@ -48,17 +48,20 @@ public class StatIterator implements Iterator<Object[]>{
 		
 		for(Object[] array : nextData) {
 			for(int i = 0; i < array.length; i++) {
-				statArray[i].addValue(array[i]);
+				if(statArray[i]==null) {
+					returnRow[i] = array[i];
+				} else {
+					statArray[i].addValue(array[i]);
+				}
 			}
 		}
 		
-		for(int i = 0; i < returnRow.length; i++) {
-//			if(reconMode[i] == DuplicationReconciliation.ReconciliationMode.COUNT) {
-//				returnRow[i] = count;
-//			} else {
+//		returnRow[0] = 
+		for(int i = 1; i < returnRow.length; i++) {
+			if(statArray[i] != null) {
 				returnRow[i] = statArray[i].getReconciliatedValue();
 				statArray[i].clearValue();
-//			}
+			}
 		}
 		
 		return returnRow;
