23cb9ab7af6f9c5d508851bfdaa652a8da9753ee
mahkhalil <mahkhalil@deloitte.com>
2015-09-23 20:01:31 +0000

    fixing compile error to use different method call
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@10117 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c2e6a0459b03ec70bb456e5f0484937879c57d48

--- src/prerna/ds/ITableWebAdapter.java
+++ src/prerna/ds/ITableWebAdapter.java
@@ -13,21 +13,18 @@ public class ITableWebAdapter {
 	}
 	
 	public static List<HashMap<String, Object>> getData(ITableDataFrame table) {
-		
 		BTreeDataFrame btable = (BTreeDataFrame)table;
-		
 		String[] columnHeaders = btable.getColumnHeaders();
 		String lastColumn = columnHeaders[columnHeaders.length - 1];
 		TreeNode root = btable.getBuilder().nodeIndexHash.get(lastColumn);
 		
-		String[] skipColumns = btable.getColumnsToSkip();
+		String[] skipColumns = btable.getFilteredColumns();
 		List<String> skipCols = new ArrayList<String>();
 		for(String s : skipColumns) {
 			skipCols.add(s);
 		}
 		
 		WebBTreeIterator iterator = new WebBTreeIterator(root,"", true, skipCols);
-		
 		List<HashMap<String, Object>> retData = new ArrayList<HashMap<String, Object>>();
 		while(iterator.hasNext()) {
 			retData.addAll(iterator.next());
