f1b462365fca126f85c5d1d22be8722d5d381314
rluthar <rluthar@deloitte.com>
2015-10-07 15:45:07 +0000

    BTreeDataFrame: update for MultiColumn join, BTreeDataFrameJoiner: updates for multi column join, StatIterator: revision for single column, MultiColumnStatIterator: iterator for multi column grouping, MultiColumnTableStatCounter: multi column group by
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@10370 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ds/BTreeDataFrameJoiner.java
+++ src/prerna/ds/BTreeDataFrameJoiner.java
@@ -13,43 +13,6 @@ import prerna.algorithm.api.ITableDataFrame;
 import prerna.util.ArrayUtilityMethods;
 
 public class BTreeDataFrameJoiner {
-
-	//Multi Column join for BTreeDataFrame
-	//Assumptions
-	//	
-//	public static void join(ITableDataFrame table1, ITableDataFrame table2, Map<String, String> tableHeaders) {
-//		
-//		String[] table1ColumnHeaders = table1.getColumnHeaders();
-//		String[] table2ColumnHeaders = table2.getColumnHeaders();
-//		
-//		Set<String> keySet = tableHeaders.keySet();
-//		int[] table1indexes = new int[keySet.size()];
-//		int[] table2indexes = new int[keySet.size()];
-//		
-//		int i = 0;
-//		for(String key : keySet) {
-//			
-//			String table1Column = key;
-//			String table2Column = tableHeaders.get(key);
-//			
-//			int table1index = ArrayUtilityMethods.arrayContainsValueAtIndex(table1ColumnHeaders, table1Column);
-//			int table2index = ArrayUtilityMethods.arrayContainsValueAtIndex(table2ColumnHeaders, table2Column);
-//			
-//			table1indexes[i] = table1index;
-//			table2indexes[i] = table2index;
-//			
-//			i++;
-//		}
-//		
-//		Iterator<List<Object[]>> table1Iterator = table1.uniqueIterator(table1ColumnHeaders[0], false);
-//		Iterator<List<Object[]>> table2Iterator = table2.uniqueIterator(tableHeaders.get(table1ColumnHeaders[0]), false);
-//		
-////		ITableDataFrame results = performMatch(table1Iterator, table2Iterator, table1indexes, table2indexes);
-//		ArrayList<TreeNode> results = performMatch(table1Iterator, table2Iterator, table1indexes, table2indexes);
-//		
-//		return results;
-//	}
-	
 	
 	/**
 	 * 
@@ -75,7 +38,7 @@ public class BTreeDataFrameJoiner {
 			String key = nextNode.leaf.getValue().toString();
 			
 			SimpleTreeNode parentNode = nextNode.parent;
-			int index = table1ColHeaders.length - 1;
+			int index = table1ColHeaders.length - 2;
 			
 			while(parentNode != null) {
 				String type = ((ISEMOSSNode)parentNode.leaf).getType();
@@ -109,7 +72,7 @@ public class BTreeDataFrameJoiner {
 			String key = nextNode.leaf.getValue().toString();
 			
 			SimpleTreeNode parentNode = nextNode.parent;
-			int index = table1ColHeaders.length - 1;
+			int index = table1ColHeaders.length - 2;
 			
 			while(parentNode != null) {
 				String type = ((ISEMOSSNode)parentNode.leaf).getType();
@@ -142,6 +105,11 @@ public class BTreeDataFrameJoiner {
 		}
 		
 		//add more code later for optimization
+		//use the leftover simpletreenodes in the bucket map after removing used ones to either remove from the tree or adjust
+		//this will prevent having to iterate through the whole value tree, at least in the case of inner joins
+		
+		//Also need to not create a new ITable for the purpose of simply iterating through---not worth the effort to create the index trees, just create
+		//an array list to begin with to iterate through, this means not using an IAnalyticRoutine as parameter for joins
 //		for(String usedKey : usedKeys) {
 //			bucketMap.remove(usedKey);
 //		}
@@ -150,116 +118,59 @@ public class BTreeDataFrameJoiner {
 		
 		
 	}
-
-//	protected ArrayList<TreeNode> performMatch(Iterator<List<Object[]>> table1Iterator, Iterator<List<Object[]>> table2Iterator, int[] table1Indexes, int[] table2Indexes) {
-//		
-//		//Need to fix in case table is only one value in length, won't go in while loop
-//		List<Object[]> table1Values = table1Iterator.next();
-//		List<Object[]> table2Values = table2Iterator.next();
-//		
-//		int table1MainIndex = table1Indexes[0];
-//		int table2MainIndex = table2Indexes[0];
-//		
-//		
-//		while(table1Iterator.hasNext() && table2Iterator.hasNext()) {
-//			
-//			int comparison = this.compareTo(table1Values.get(0)[table1MainIndex].toString(), table2Values.get(0)[table2MainIndex].toString());
-//
-//			//if values are equal
-//			if(comparison == 0) {
-//					
-//				Set<String> valueSet;
-//				if(table1Values.size() < table2Values.size()) {
-//					
-//					valueSet = new HashSet<String>(table1Values.size());
-//					for(int i = 0; i < table1Values.size(); i++) {
-//						Object[] nextRow = table1Values.get(i);
-//						
-//						String row = "";
-//						for(int j = 0; j < table1Indexes.length; j++) {
-//							row = row + nextRow[table1Indexes[j]];
-//						}
-//						valueSet.add(row);
-//					}
-//					
-//					for(int i = 0; i < table2Values.size(); i++) {
-//						Object[] nextRow = table2Values.get(i);
-//						String row = "";
-//						for(int j = 0; j < table2Indexes.length; j++) {
-//							row = row + nextRow[table2Indexes[j]];
-//						}
-//						
-//						if(valueSet.contains(row)) {
-//							//do something
-//						}
-//					}
-//					
-//				} else {
-//					valueSet = new HashSet<String>(table2Values.size());
-//					for(int i = 0; i < table2Values.size(); i++) {
-//						
-//						Object[] nextRow = table2Values.get(i);
-//						
-//						String row = "";
-//						for(int j = 0; j < nextRow.length; j++) {
-//							row = row + nextRow[table2Indexes[j]];
-//						}
-//						valueSet.add(row);
-//					}
-//					
-//					for(int i = 0; i < table1Values.size(); i++) {
-//						Object[] nextRow = table1Values.get(i);
-//						String row = "";
-//						for(int j = 0; j < nextRow.length; j++) {
-//							row = row + nextRow[table1Indexes[j]];
-//						}
-//						
-//						if(valueSet.contains(row)) {
-//							//do something
-//						}
-//					}
-//				}
-//				
-//			} else if(comparison == -1) {
-//				table2Values = table2Iterator.next();
-//			} else if(comparison == 1) {
-//				table1Values = table1Iterator.next();
-//			}
-//		}
-//		
-//		
-//		
-//		String table1ValueKey = "Table1Value";
-//		String table2ValueKey = "Table2Value";
-//		
-//		ITableDataFrame bTree = new BTreeDataFrame(new String[]{table1ValueKey, table2ValueKey});
-//
-//		//make this O(n)
-//		int success = 0;
-//		int total = 0;
-//		for(int j = 0; j < table1Col.length; j++) {
-//			if(match(table1Col[i],table2Col[j])) {
-//				Map<String, Object> row = new HashMap<String, Object>();
-//				row.put(table1ValueKey , table1Col[i]);
-//				row.put(table2ValueKey, table2Col[j]);
-//				bTree.addRow(row, row); //TODO: adding values as both raw and clean
-//				success++;
-//			}
-//			total++;
-//		}
-//		
-//		boolean matchNext = true;
-//		while(matchNext) {
-//			
-//		}
-//		
-//		this.resultMetadata.put("success", success);
-//		this.resultMetadata.put("total", total);
-//		
-//		return bTree;
-//	}
-//	
-//	protected int compareTo(String obj1, String obj2) {
-//		return obj1.compareTo(obj2);
-//	}
+	
+	public static void innerJoin(BTreeDataFrame table1, BTreeDataFrame table2, String table1ColHeader, String table2ColHeader) {
+		
+		Map<String, ArrayList<SimpleTreeNode>> bucketMap = new HashMap<String, ArrayList<SimpleTreeNode>>();
+		List<String> usedKeys = new ArrayList<String>();
+		
+		TreeNode root = table1.getBuilder().nodeIndexHash.get(table1ColHeader);
+		ValueTreeColumnIterator treeLevelIterator = new ValueTreeColumnIterator(root);
+		while(treeLevelIterator.hasNext()) {
+			
+			SimpleTreeNode nextNode = treeLevelIterator.next();
+			String key = nextNode.leaf.getValue().toString();
+			
+			if(bucketMap.containsKey(key)) {
+				bucketMap.get(key).add(nextNode);
+			} else {
+				ArrayList<SimpleTreeNode> newList = new ArrayList<SimpleTreeNode>();
+				newList.add(nextNode);
+				bucketMap.put(key, newList);
+			}
+		}
+		
+		root = table2.getBuilder().nodeIndexHash.get(table2ColHeader);
+		treeLevelIterator = new ValueTreeColumnIterator(root);
+		while(treeLevelIterator.hasNext()) {
+			
+			SimpleTreeNode nextNode = treeLevelIterator.next();
+			String key = nextNode.leaf.getValue().toString();
+			
+			if(bucketMap.containsKey(key)) {
+				ArrayList<SimpleTreeNode> instanceList = bucketMap.get(key);
+				
+				Vector<SimpleTreeNode> vec = new Vector<SimpleTreeNode>();
+				vec.add(nextNode.leftChild);
+				String serialized = SimpleTreeNode.serializeTree("", vec, true, 0);
+				
+				for(SimpleTreeNode instance : instanceList) {
+					SimpleTreeNode hookUp = SimpleTreeNode.deserializeTree(serialized);
+					SimpleTreeNode.addLeafChild(instance, hookUp);
+				}
+				
+				usedKeys.add(key);
+			}
+		}
+		
+		//add more code later for optimization
+		//use the leftover simpletreenodes in the bucket map after removing used ones to either remove from the tree or adjust
+		//this will prevent having to iterate through the whole value tree, at least in the case of inner joins
+		
+		//Also need to not create a new ITable for the purpose of simply iterating through---not worth the effort to create the index trees, just create
+		//an array list to begin with to iterate through, this means not using an IAnalyticRoutine as parameter for joins
+		for(String usedKey : usedKeys) {
+			bucketMap.remove(usedKey);
+		}	
+	}
 }
