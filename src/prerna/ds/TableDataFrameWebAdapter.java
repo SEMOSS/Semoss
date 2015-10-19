ec0fbc4e6f866fcbaee5b7092650dde2ff1bf3f6
rluthar <rluthar@deloitte.com>
2015-10-19 14:06:43 +0000

    Changing name of file to not include 'I'
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@10546 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3db6c103edec85bb2abd8983e3ee45a8bb37e5c2

--- src/prerna/ds/TableDataFrameWebAdapter.java
+++ src/prerna/ds/TableDataFrameWebAdapter.java
@@ -4,27 +4,45 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import prerna.algorithm.api.ITableDataFrame;
 
-public class ITableWebAdapter {
+public class TableDataFrameWebAdapter {
 	
-	private ITableWebAdapter() {
+	private static final Logger LOGGER = LogManager.getLogger(TableDataFrameWebAdapter.class.getName());
+	
+	private TableDataFrameWebAdapter() {
 		
 	}
 	
+	/**
+	 * 
+	 * @param table - the dataframe to extract data from
+	 * @return  - formatted data in an optimal form for the Front End
+	 */
 	public static List<HashMap<String, Object>> getData(ITableDataFrame table) {
+		
+		//Cast to Btree
 		BTreeDataFrame btable = (BTreeDataFrame)table;
+		
+		//get the root of the index tree for the last column
 		String[] columnHeaders = btable.getColumnHeaders();
 		String lastColumn = columnHeaders[columnHeaders.length - 1];
 		TreeNode root = btable.getBuilder().nodeIndexHash.get(lastColumn);
 		
+		//figure out which columns to skip
 		String[] skipColumns = btable.getFilteredColumns();
 		List<String> skipCols = new ArrayList<String>();
 		for(String s : skipColumns) {
 			skipCols.add(s);
 		}
 		
+		//use the iterator specific for returning data for the front end
 		WebBTreeIterator iterator = new WebBTreeIterator(root,"", true, skipCols);
+		
+		//List of Hashmaps is optimal for front end to reduce looping 
 		List<HashMap<String, Object>> retData = new ArrayList<HashMap<String, Object>>();
 		while(iterator.hasNext()) {
 			retData.addAll(iterator.next());
