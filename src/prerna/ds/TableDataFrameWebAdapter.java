11819732b5ee1a60e5c52b8aab058333601049d6
rluthar <rluthar@deloitte.com>
2015-11-03 15:59:22 +0000

    adding to TableDataFrameWebAdapter to get chunks from table
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@10862 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2edfa8f4b075742b10600d3ce3f9b168c1d61c1a

--- src/prerna/ds/TableDataFrameWebAdapter.java
+++ src/prerna/ds/TableDataFrameWebAdapter.java
@@ -10,6 +10,7 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import prerna.algorithm.api.ITableDataFrame;
+import prerna.util.ArrayUtilityMethods;
 
 public class TableDataFrameWebAdapter {
 	
@@ -50,6 +51,65 @@ public class TableDataFrameWebAdapter {
 			retData.addAll(iterator.next());
 		}
 		return retData;
+	}
+	
+	public static List<HashMap<String, Object>> getData(ITableDataFrame tableData, String concept, String sortType, int startRow, int endRow) {
+
+		// Cast to B-tree, table from which data is being extracted
+		BTreeDataFrame table = (BTreeDataFrame) tableData; 
+
+		// figure out which columns to skip for the return data
+		String[] skipColumns = table.getFilteredColumns();
+		List<String> skipCols = new ArrayList<String>();
+		for (String s : skipColumns) {
+			skipCols.add(s);
+		}
+
+		// sort should either be "desc" or "asc", if sort is null then set sort
+		// to "asc"
+		String sort = "asc"; //sorting, asc for ascending, desc for descending
+		if (!sort.equalsIgnoreCase(sortType)) {
+			sort = sortType;
+		}
+		
+		// if column is null, column should equal first column in table
+		String[] columnHeaders = table.getColumnHeaders();
+		String iteratorColumn = null; //the column on which to sort on
+		if (concept != null && ArrayUtilityMethods.arrayContainsValueIgnoreCase(columnHeaders, concept)) {
+			iteratorColumn = concept;
+		} else {
+			//default to first column
+			iteratorColumn = columnHeaders[0];
+		}
+		TreeNode root = table.getBuilder().nodeIndexHash.get(iteratorColumn);
+		
+		// use the iterator specific for returning data for the front end
+		WebBTreeIterator rowIterator = new WebBTreeIterator(root, sort, true, skipCols);
+
+		endRow = endRow + 1;		
+
+		//gather the necessary return data
+		List<HashMap<String, Object>> nextData = new ArrayList<HashMap<String, Object>>();
+		while (nextData.size() <= endRow && rowIterator.hasNext()) {
+			nextData.addAll(rowIterator.next());
+		}
+
+		//if size is less than end row, increment end row by 1
+		if (nextData.size() < endRow) {
+			endRow = nextData.size() + 1;
+		}
+
+		//if empty return empty list of hashmaps
+		if (nextData.size() == 0) {
+			return nextData;
+		}
+
+		try {
+			nextData = nextData.subList(startRow, endRow);
+		} catch (Exception e) {
+			nextData = nextData.subList(startRow, --endRow);
+		}
+		return  nextData;
 	}
 	
 	public static Object[] getRawFilterModel(ITableDataFrame table) {
@@ -95,4 +155,5 @@ public class TableDataFrameWebAdapter {
 		
 		return new Object[]{visibleValues, invisibleValues};
 	}
+	
 }
