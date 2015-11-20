9f7d4f58861a7c2f83e0fab9ac29c877efe378dd
mahkhalil <mahkhalil@deloitte.com>
2015-11-20 20:05:15 +0000

    adding solr code to load information from insights rdbms on start up.. will only work if solr is present on server
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@11284 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/solr/SolrIndexEngine.java
+++ src/prerna/solr/SolrIndexEngine.java
@@ -76,7 +76,7 @@ public class SolrIndexEngine {
 		SolrIndexEngine.url = url;
 	}
 
-	public static SolrIndexEngine getInstsance() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
+	public static SolrIndexEngine getInstance() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException {
 		if (singleton == null) {
 			singleton = new SolrIndexEngine();
 		}
@@ -97,223 +97,253 @@ public class SolrIndexEngine {
 	}
 
 	public void addDocument(String uniqueID, Map<String, Object> fieldData) throws SolrServerException, IOException {
-		// create new Document
-		SolrInputDocument doc = new SolrInputDocument();
-		// set document ID to uniqueID
-		doc.setField("id", uniqueID);
-		// add field names and data to new Document
-		for (String fieldname : fieldData.keySet()) {
-			doc.setField(fieldname, fieldData.get(fieldname));
+		if(serverActive()) {
+			// create new Document
+			SolrInputDocument doc = new SolrInputDocument();
+			// set document ID to uniqueID
+			doc.setField("id", uniqueID);
+			// add field names and data to new Document
+			for (String fieldname : fieldData.keySet()) {
+				doc.setField(fieldname, fieldData.get(fieldname));
+			}
+			server.add(doc);
+			server.commit();
 		}
-		server.add(doc);
-		server.commit();
 	}
 
 	public void removeDocument(List<String> uniqueID) throws SolrServerException, IOException {
-		// delete document based on ID
-		server.deleteById(uniqueID);
-		server.commit();
+		if(serverActive()) {
+			// delete document based on ID
+			server.deleteById(uniqueID);
+			server.commit();
+		}
 	}
 
 	public void modifyFields(String uniqueID, Map<String, Object> fieldsToModify) throws SolrServerException, IOException {
-		Map<String, Object> queryMap = new HashMap<String, Object>();
-		queryMap.put(SET_DEFAULT, ID + ":" + uniqueID);
-		SolrDocument origDoc = queryDocument(queryMap).get(0);
-		Iterator<Entry<String, Object>> iterator = origDoc.iterator();  //iterating through it modified list
-
-		SolrInputDocument doc = new SolrInputDocument();
-		doc.get(uniqueID); //getting the doc set based on the id
-		
-		Set<String> currFieldNames = new HashSet<String>(); 
-		while (iterator.hasNext()) {
-			// getName & getValues
-			Entry<String, Object> field = iterator.next();
-			String fieldName = field.getKey();
-			currFieldNames.add(fieldName);
-			// if modified field, grab new value
-			if (fieldsToModify.containsKey(fieldName)) {
-				doc.setField(fieldName, fieldsToModify.get(fieldName));
-			} else {
-				// if not modified field, use existing value
-				doc.setField(fieldName, field.getValue());
+		if(serverActive()) {
+			Map<String, Object> queryMap = new HashMap<String, Object>();
+			queryMap.put(SET_DEFAULT, ID + ":" + uniqueID);
+			SolrDocument origDoc = queryDocument(queryMap).get(0);
+			Iterator<Entry<String, Object>> iterator = origDoc.iterator();  //iterating through it modified list
+	
+			SolrInputDocument doc = new SolrInputDocument();
+			doc.get(uniqueID); //getting the doc set based on the id
+			
+			Set<String> currFieldNames = new HashSet<String>(); 
+			while (iterator.hasNext()) {
+				// getName & getValues
+				Entry<String, Object> field = iterator.next();
+				String fieldName = field.getKey();
+				currFieldNames.add(fieldName);
+				// if modified field, grab new value
+				if (fieldsToModify.containsKey(fieldName)) {
+					doc.setField(fieldName, fieldsToModify.get(fieldName));
+				} else {
+					// if not modified field, use existing value
+					doc.setField(fieldName, field.getValue());
+				}
 			}
-		}
-		// loop through if new fields are being added
-		for (String newField : fieldsToModify.keySet()) {
-			if (!currFieldNames.contains(newField)) {
-				doc.setField(newField, fieldsToModify.get(newField));
+			// loop through if new fields are being added
+			for (String newField : fieldsToModify.keySet()) {
+				if (!currFieldNames.contains(newField)) {
+					doc.setField(newField, fieldsToModify.get(newField));
+				}
 			}
+			// when committing, automatically overrides existing field values with the new ones
+			server.add(doc);
+			server.commit();
 		}
-		// when committing, automatically overrides existing field values with the new ones
-		server.add(doc);
-		server.commit();
 	}
 
 	@SuppressWarnings({ "unchecked" })
 	public SolrDocumentList queryDocument(Map<String, Object> queryOptions) throws SolrServerException, IOException {
+		SolrDocumentList results = null;
+		
+		if(serverActive()) {
 
-		SolrQuery Q = new SolrQuery();
-		//Default
-		Q.setQuery("*:*");
-
-		//COMMON FILTERS
-		//q & df- If query is specified, match based on relevance will be returned 
-		//Default will be set to search for everything
-		if (queryOptions.get(SET_DEFAULT) != null) {
-			String f = (String) queryOptions.get(SET_DEFAULT);
-			Q.set("q", f);
-			if (queryOptions.get(SEARCH_FIELD) != null) {
-				String s = (String) queryOptions.get(SEARCH_FIELD);
-				Q.set("df", s);
+			SolrQuery Q = new SolrQuery();
+			//Default
+			Q.setQuery("*:*");
+	
+			//COMMON FILTERS
+			//q & df- If query is specified, match based on relevance will be returned 
+			//Default will be set to search for everything
+			if (queryOptions.get(SET_DEFAULT) != null) {
+				String f = (String) queryOptions.get(SET_DEFAULT);
+				Q.set("q", f);
+				if (queryOptions.get(SEARCH_FIELD) != null) {
+					String s = (String) queryOptions.get(SEARCH_FIELD);
+					Q.set("df", s);
+				} else {
+					Q.set("df", "all_text");
+				}
+			}
+	
+			//fq - filters search based on exact match
+			if (queryOptions.get(FITLER_QUERY) != null) {
+				Map<String, String> query = (Map<String, String>) queryOptions.get(FITLER_QUERY);
+				for (String fQuery : query.keySet()) {
+					Q.addFilterQuery(fQuery + ":" + query.get(fQuery));
+				}
+			}
+	
+			//sorts - orders list asc/desc or  by relevance 
+			if (queryOptions.get(FIELD_SORT) != null) {
+				Map<String, List<String>> sort = (Map<String, List<String>>) queryOptions.get(FIELD_SORT);
+				for (String fsort : sort.keySet()) {
+					List<String> sortField = sort.get(fsort);
+					String desc = "Descending";
+					String asc = "Ascending";
+					String relevance = "Relevance";
+					if (fsort.equals(desc)) {
+						for (String fields : sortField) {
+							Q.setSort(fields, SolrQuery.ORDER.desc);
+						}
+					} else if (fsort.equals(asc)) {
+						for (String fields : sortField) {
+							Q.setSort(fields, SolrQuery.ORDER.asc);
+						}
+					} else if (fsort.equals(relevance)) {
+						for (String fields : sortField) {
+							Q.removeSort(fields);
+						}
+					}
+				}
+			}
+	
+			// start - returns list of insights starting from the specified int
+			if (queryOptions.get(SET_START) != null) {
+				int numStart = (int) queryOptions.get(SET_START);
+				Q.setStart(numStart);
+			}
+	
+			// rows - returns specified int of insights 
+			if (queryOptions.get(SET_ROWS) != null) {
+				int numRows = (int) queryOptions.get(SET_ROWS);
+				Q.setRows(numRows);
 			} else {
-				Q.set("df", "all_text");
+				Q.setRows(200);
 			}
-		}
-
-		//fq - filters search based on exact match
-		if (queryOptions.get(FITLER_QUERY) != null) {
-			Map<String, String> query = (Map<String, String>) queryOptions.get(FITLER_QUERY);
-			for (String fQuery : query.keySet()) {
-				Q.addFilterQuery(fQuery + ":" + query.get(fQuery));
+	
+			// fl - this is determining which fields in the schema will be returned
+			if (queryOptions.get(ADD_FIELD) != null) {
+				List<String> fields = (List<String>) queryOptions.get(ADD_FIELD);
+				for (String f : fields) {
+					Q.addField(f);
+				}
 			}
-		}
-
-		//sorts - orders list asc/desc or  by relevance 
-		if (queryOptions.get(FIELD_SORT) != null) {
-			Map<String, List<String>> sort = (Map<String, List<String>>) queryOptions.get(FIELD_SORT);
-			for (String fsort : sort.keySet()) {
-				List<String> sortField = sort.get(fsort);
-				String desc = "Descending";
-				String asc = "Ascending";
-				String relevance = "Relevance";
-				if (fsort.equals(desc)) {
-					for (String fields : sortField) {
-						Q.setSort(fields, SolrQuery.ORDER.desc);
-					}
-				} else if (fsort.equals(asc)) {
-					for (String fields : sortField) {
-						Q.setSort(fields, SolrQuery.ORDER.asc);
-					}
-				} else if (fsort.equals(relevance)) {
-					for (String fields : sortField) {
-						Q.removeSort(fields);
+	
+			//FACET INFO FILTERS
+	
+			//facet.query - calculated count on any query
+			if (queryOptions.get(FACET_QUERY) != null) {
+				Map<String, String> query = (Map<String, String>) queryOptions.get(FACET_QUERY);
+				for (String fQuery : query.keySet()) {
+					Q.addFacetQuery(fQuery + ":" + query.get(fQuery) );
+				}
+			}
+	
+			// facet.field - calculates count of total insights 
+			if (queryOptions.get(FACET_FIELD) != null) {
+				List<String> query = (List<String>) queryOptions.get(FACET_FIELD);
+				for (String fQuery : query) {
+					Q.addFacetField(fQuery);
+				}
+				// facet.prefix -  Restricts the possible constraints to only indexed values with a specified prefix
+				//cannot be set to case insensitive
+				if (queryOptions.get(PREFIX_FACET) != null) {
+					List<String> prefix = (List<String>) queryOptions.get(PREFIX_FACET);
+					for (String fprefix : prefix) {
+						Q.setFacetPrefix(fprefix);
 					}
 				}
 			}
-		}
-
-		// start - returns list of insights starting from the specified int
-		if (queryOptions.get(SET_START) != null) {
-			int numStart = (int) queryOptions.get(SET_START);
-			Q.setStart(numStart);
-		}
-
-		// rows - returns specified int of insights 
-		if (queryOptions.get(SET_ROWS) != null) {
-			int numRows = (int) queryOptions.get(SET_ROWS);
-			Q.setRows(numRows);
-		}
-
-		// fl - this is determining which fields in the schema will be returned
-		if (queryOptions.get(ADD_FIELD) != null) {
-			List<String> fields = (List<String>) queryOptions.get(ADD_FIELD);
-			for (String f : fields) {
-				Q.addField(f);
+	
+			// minCount - specifies minimum number that are necessary for the facet field to be visible
+			//no maxCount
+			if (queryOptions.get(MIN_COUNT) != null) {
+				int numRows = (int) queryOptions.get(MIN_COUNT);
+				Q.setFacetMinCount(numRows);
 			}
-		}
-
-		//FACET INFO FILTERS
-
-		//facet.query - calculated count on any query
-		if (queryOptions.get(FACET_QUERY) != null) {
-			Map<String, String> query = (Map<String, String>) queryOptions.get(FACET_QUERY);
-			for (String fQuery : query.keySet()) {
-				Q.addFacetQuery(fQuery + ":" + query.get(fQuery) );
+	
+			// FacetLimit - limits the number of facets that are shows.
+			if (queryOptions.get(FACET_LIMIT) != null) {
+				int numLimit = (int) queryOptions.get(FACET_LIMIT);
+				Q.setFacetLimit(numLimit);
 			}
+	
+	
+			System.out.println("query is ::: " + Q.getQuery());
+	
+			// report number of results found from query
+			QueryResponse res = server.query(Q);
+			results = res.getResults();
+			long numFound = results.getNumFound();
+			System.out.println("num hits: " + numFound);
+			System.out.println(Q);
+	
+			//		for (int i = 0; i < results.size(); ++i) {
+			//			SolrDocument getDoc = results.get(i);
+			//			for (Iterator<Entry<String, Object>> fieldIterator = getDoc.iterator(); fieldIterator.hasNext();) {
+			//				Entry<String, Object> entry = fieldIterator.next();
+			//				System.out.println(entry.getKey() + ": " + entry.getValue());
+			//
+			//			} System.out.println();
+			//		}
+			//
+			//
+			//		List<FacetField> facetFieldList = res.getFacetFields();
+			//		for (int i = 0; i < facetFieldList.size(); i++) {
+			//			FacetField facetField = facetFieldList.get(i);
+			//			List<Count> facetInfo = facetField.getValues();
+			//			for (FacetField.Count facetInstance : facetInfo) {
+			//				System.out.println(facetInstance.getName() + " : " + facetInstance.getCount());
+			//			}
+			//		}
 		}
-
-		// facet.field - calculates count of total insights 
-		if (queryOptions.get(FACET_FIELD) != null) {
-			List<String> query = (List<String>) queryOptions.get(FACET_FIELD);
-			for (String fQuery : query) {
-				Q.addFacetField(fQuery);
-			}
-			// facet.prefix -  Restricts the possible constraints to only indexed values with a specified prefix
-			//cannot be set to case insensitive
-			if (queryOptions.get(PREFIX_FACET) != null) {
-				List<String> prefix = (List<String>) queryOptions.get(PREFIX_FACET);
-				for (String fprefix : prefix) {
-					Q.setFacetPrefix(fprefix);
-				}
-			}
-		}
-
-		// minCount - specifies minimum number that are necessary for the facet field to be visible
-		//no maxCount
-		if (queryOptions.get(MIN_COUNT) != null) {
-			int numRows = (int) queryOptions.get(MIN_COUNT);
-			Q.setFacetMinCount(numRows);
-		}
-
-		// FacetLimit - limits the number of facets that are shows.
-		if (queryOptions.get(FACET_LIMIT) != null) {
-			int numLimit = (int) queryOptions.get(FACET_LIMIT);
-			Q.setFacetLimit(numLimit);
-		}
-
-
-		System.out.println("query is ::: " + Q.getQuery());
-
-		// report number of results found from query
-		QueryResponse res = server.query(Q);
-		SolrDocumentList results = res.getResults();
-		long numFound = results.getNumFound();
-		System.out.println("num hits: " + numFound);
-		System.out.println(Q);
-
 		return results;
-
-		//		for (int i = 0; i < results.size(); ++i) {
-		//			SolrDocument getDoc = results.get(i);
-		//			for (Iterator<Entry<String, Object>> fieldIterator = getDoc.iterator(); fieldIterator.hasNext();) {
-		//				Entry<String, Object> entry = fieldIterator.next();
-		//				System.out.println(entry.getKey() + ": " + entry.getValue());
-		//
-		//			} System.out.println();
-		//		}
-		//
-		//
-		//		List<FacetField> facetFieldList = res.getFacetFields();
-		//		for (int i = 0; i < facetFieldList.size(); i++) {
-		//			FacetField facetField = facetFieldList.get(i);
-		//			List<Count> facetInfo = facetField.getValues();
-		//			for (FacetField.Count facetInstance : facetInfo) {
-		//				System.out.println(facetInstance.getName() + " : " + facetInstance.getCount());
-		//			}
-		//		}
 	}
 
-	public void deleteAllSolrData() {
-		try {
-			server.deleteByQuery("*:*");
-		} catch (SolrServerException ex) {
-			throw new RuntimeException("Failed to delete data in Solr. " + ex.getMessage(), ex);
-		} catch (IOException ex) {
-			throw new RuntimeException("Failed to delete data in Solr. " + ex.getMessage(), ex);
+	public void deleteAllSolrData() throws IOException {
+		if(serverActive()) {
+			try {
+				server.deleteByQuery("*:*");
+			} catch (SolrServerException ex) {
+				throw new IOException("Failed to delete data in Solr. " + ex.getMessage(), ex);
+			} catch (IOException ex) {
+				throw new IOException("Failed to delete data in Solr. " + ex.getMessage(), ex);
+			}
 		}
 	}
 
 	public void deleteEngine(String engineName){
-		try {
-			server.deleteByQuery("core_engine:" + engineName);
-		} catch (SolrServerException e1) {
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
+		if(serverActive()) {
+			try {
+				server.deleteByQuery("core_engine:" + engineName);
+			} catch (SolrServerException e1) {
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
 		}
 	}
 	
 	public static DateFormat getDateFormat() {
 		return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+	}
+	
+	public boolean serverActive() {
+		boolean isActive = true;
+		try {
+			server.ping();
+		} catch(SolrServerException e) {
+			isActive = false;
+		} catch(IOException e) {
+			isActive = false;
+		} catch(Exception e) {
+			isActive = false;
+		}
+		
+		return isActive;
 	}
 }
