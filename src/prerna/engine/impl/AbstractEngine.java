2c7661d696b76832c0eea04965819cf3d3ec00b0
karverma <karverma@deloitte.com>
2015-12-21 17:11:35 +0000

    Bugfix to use physical URI instead of display when getting properties
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@11782 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/engine/impl/AbstractEngine.java
+++ src/prerna/engine/impl/AbstractEngine.java
@@ -542,7 +542,7 @@ public abstract class AbstractEngine implements IEngine {
 		String query = "SELECT ?property WHERE { {?concept <http://www.w3.org/2000/01/rdf-schema#subClassOf> <http://semoss.org/ontologies/Concept> }"
 				+ "{?concept  <http://www.w3.org/2002/07/owl#DatatypeProperty> ?property}"
 				+ "{?property a <" + CONTAINS_BASE_URI + ">}}"
-				+ "BINDINGS ?concept {(<"+concept+">)}";
+				+ "BINDINGS ?concept {(<"+ this.getTransformedNodeName(concept, false) +">)}";
 		Vector<String> uriProps = Utility.getVectorOfReturn(query, baseDataEngine, true);
 		if(!logicalNames){
 			return uriProps;
