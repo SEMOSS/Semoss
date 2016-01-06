f94bf181f7785fa793d18393e55f1c360f2980fc
rluthar <rluthar@deloitte.com>
2016-01-06 22:31:54 +0000

    Changing WebBtreeIterator to be iterative when looping through the value tree as opposed to recursive, recursion causes stackoverflow with sufficiently large/wide trees
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@11901 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f773a428ce65a0b394767778466c694eec0fdab4

--- src/prerna/ds/WebBTreeIterator.java
+++ src/prerna/ds/WebBTreeIterator.java
@@ -56,11 +56,17 @@ public class WebBTreeIterator implements Iterator<List<HashMap<String, Object>>>
 		//TreeNode treenode = iterator.next();
 		TreeNode treenode = indexTreeNodes.poll();
 		for(SimpleTreeNode node: treenode.instanceNode) {
-			retList.addAll(getInstanceRows(node));
+//			retList.addAll(getInstanceRows(node));
+			retList.addAll(collect(node));
 		}
 		
 		addToQueue();
 		return retList;
+	}
+	
+	//TODO: do the skipping intelligently for FE 
+	public List<HashMap<String, Object>> next(int start, int end) {
+		return null;
 	}
 	
 	@Override
@@ -77,37 +83,95 @@ public class WebBTreeIterator implements Iterator<List<HashMap<String, Object>>>
 		}
 	}
 	
-	private List<HashMap<String, Object>> getInstanceRows(SimpleTreeNode node) {
-		List<HashMap<String, Object>> instanceRows = new ArrayList<>();
-		//populate list recursively
-		getRow(instanceRows, node, true);
-		return instanceRows;
+//	private List<HashMap<String, Object>> getInstanceRows(SimpleTreeNode node) {
+//		List<HashMap<String, Object>> instanceRows = new ArrayList<>();
+//		//populate list recursively
+//		getRow(instanceRows, node, true);
+//		return instanceRows;
+//	}
+//	
+//	private void getRow(List<HashMap<String, Object>> list, SimpleTreeNode node, boolean firstIt) {
+//		
+//		//get to most left
+//		while(node.leftChild != null) {
+//			node = node.leftChild;
+//		}
+//		
+//		
+//		if(node.leftChild == null) {
+//			list.add(collectRowData(node));
+//			
+//		} 
+//		
+//		else {
+//			getRow(list, node.leftChild, false);
+//		}
+//		
+//		if(node.rightSibling!=null && !firstIt) {
+//			getRow(list, node.rightSibling, false);
+//		}
+//	}
+	
+	//Collect the rows from root node iteratively
+	private List<HashMap<String, Object>> collect(SimpleTreeNode node) {
+		int levelCount = 0; //indicates how many levels down from root node we are
+		List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>(); //return data
+		
+		do {
+			//if we reach end of branch
+			if(node.leftChild == null && node.rightSibling == null) {
+				
+				//collect data for that node
+				list.add(collectRowData(node));
+				
+				//go back up 
+				while(levelCount > 0) {
+					
+					//if parent is end of branch continue up
+					if(node.parent.rightSibling == null) {
+						node = node.parent;
+						levelCount--;
+					} 
+					
+					//else go to parents right sibling and break
+					else {
+						node = node.parent.rightSibling;
+						levelCount--;
+						break;
+					}
+				}
+			}
+			
+			//else go to right and collect nodes
+			else if(node.leftChild == null && node.rightSibling != null) {
+				list.add(collectRowData(node));
+				node = node.rightSibling;
+			} 
+			
+			//else go down
+			else if(node.leftChild != null) {
+				node = node.leftChild;
+				levelCount++;
+			}
+			
+		} while(levelCount > 0); //continue as long as we are below the root node
+		
+		return list; 
 	}
 	
-	private void getRow(List<HashMap<String, Object>> list, SimpleTreeNode node, boolean firstIt) {
-		
-		if(node.leftChild == null) {
-			HashMap<String, Object> rowMap = new HashMap<>();
-			SimpleTreeNode n = node;
-			while(n!=null) {
-				String type = ((ISEMOSSNode)n.leaf).getType();
-				//TODO : make this check more efficient
-				if(!columns2skip.contains(type)) {
-					Object value = useRawData ? n.leaf.getRawValue() : n.leaf.getValue();
-					rowMap.put(type, value);
-				}
-				n = n.parent;
+	//get the row for a leaf node, basically just traverse up and collect values
+	private HashMap<String, Object> collectRowData(SimpleTreeNode node) {
+		HashMap<String, Object> rowMap = new HashMap<>();
+		SimpleTreeNode n = node;
+		while(n!=null) {
+			String type = ((ISEMOSSNode)n.leaf).getType();
+			//TODO : make this check more efficient
+			if(!columns2skip.contains(type)) {
+				Object value = useRawData ? n.leaf.getRawValue() : n.leaf.getValue();
+				rowMap.put(type, value);
 			}
-
-			list.add(rowMap);
-			
-		} else {
-			getRow(list, node.leftChild, false);
+			n = n.parent;
 		}
-		
-		if(node.rightSibling!=null && !firstIt) {
-			getRow(list, node.rightSibling, false);
-		}
+		return rowMap;
 	}
-
 }
