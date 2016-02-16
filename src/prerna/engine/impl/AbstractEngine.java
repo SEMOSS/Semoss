b419b97f0a5e66e376609af152a8eac1af722b2e
bisutton <bisutton@deloitte.com>
2016-02-16 22:40:04 +0000

    Changing all graphdatamakers into tinkerframes
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@12511 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: febeee6cf1bcd34348b9065817ad52156f57b1db

--- src/prerna/engine/impl/AbstractEngine.java
+++ src/prerna/engine/impl/AbstractEngine.java
@@ -189,6 +189,8 @@ public abstract class AbstractEngine implements IEngine {
 					prop.put(Constants.USERNAME, insightUsername);
 					insightRDBMS.setProperties(prop);
 					insightRDBMS.openDB(null);
+					insightRDBMS.insertData("UPDATE QUESTION_ID p SET QUESTION_DATA_MAKER = REPLACE(QUESTION_DATA_MAKER, 'GraphDataModel', 'TinkerFrame')");
+					//insightRDBMS.insertData("UPDATE QUESTION_ID p SET QUESTION_DATA_MAKER = REPLACE(QUESTION_DATA_MAKER, 'PrimaryKeyTinkerFrame', 'TinkerFrame')");
 					// Update existing dbs to not include QUESTION_ID column if it is there. Can remove this once everyone has updated their dbs
 //					ITableDataFrame f = WrapperManager.getInstance().getSWrapper(insightRDBMS, "select count(*) from information_schema.columns where table_name = 'QUESTION_ID' and column_name = 'QUESTION_ID'").getTableDataFrame();
 //					if((Double)f.getData().get(0)[0] != 0 ){
@@ -570,7 +572,7 @@ public abstract class AbstractEngine implements IEngine {
 		//String returnNodeURI = nodeURI;
 		
 		//these validation peices are seperated out intentionally for readability.
-		if(baseDataEngine == null || nodeURI.isEmpty() ){ 
+		if(baseDataEngine == null || nodeURI == null || nodeURI.isEmpty() ){ 
 			return nodeURI;
 		}
 		
