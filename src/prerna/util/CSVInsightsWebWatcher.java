ac59c3633e3bcf108d1baa36fe91102eac0e66f6
mahkhalil <mahkhalil@deloitte.com>
2016-02-25 01:35:06 +0000

    adding in csv watcher for insights
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@12810 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 90d14c33ae4cc77a1534dae3f828856e514debe0

---
+++ src/prerna/util/CSVInsightsWebWatcher.java
@@ -0,0 +1,46 @@
+package prerna.util;
+
+import java.io.File;
+import java.io.IOException;
+
+import prerna.solr.SolrImportUtility;
+
+public class CSVInsightsWebWatcher extends AbstractFileWatcher{
+
+	@Override
+	public void loadFirst() {
+		File dir = new File(folderToWatch);
+		String[] fileNames = dir.list(this);
+		for (int fileIdx = 0; fileIdx < fileNames.length; fileIdx++) {
+			try {
+				process(fileNames[fileIdx]);
+			} catch (RuntimeException ex) {
+				ex.printStackTrace();
+				logger.fatal("CSV Insight Failed " + folderToWatch + "/" + fileNames[fileIdx]);
+			}
+		}
+	}
+
+	@Override
+	public void process(String fileName) {
+		String solrInformationFile = folderToWatch + "\\" + fileName.substring(0, fileName.length()-3); // get rid of the .tg
+		solrInformationFile += "_Solr.txt";
+		try {
+			SolrImportUtility.processSolrTextDocument(solrInformationFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public void run() {
+		logger.info("Starting CSV Insights thread");
+		synchronized(monitor) {
+			loadFirst();
+			super.run();
+		}
+	}
+}
