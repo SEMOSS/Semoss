254bcf059f7811aec52f8d830cbb5fe5d1c44b8f
rluthar <rluthar@deloitte.com>
2016-03-28 12:37:50 +0000

    committing TinkerH2Frame code in new package, updating TinkerFrameStatRoutine.java to also take into account H2 for group by (needs to be pushed to PKQL), adding factory to produce standard tinkerframe types
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@13540 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 1ce5e62ca25a8ee9792c699bdf1a57f390c5a3c5

---
+++ src/prerna/ds/H2/TinkerH2Iterator.java
@@ -0,0 +1,53 @@
+package prerna.ds.H2;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class TinkerH2Iterator implements Iterator<Object[]>{
+
+	ResultSet resultSet;
+	Object[] nextRow;
+	
+	TinkerH2Iterator(ResultSet resultSet) {
+		this.resultSet = resultSet;
+		nextRow = getNextRow();
+	}
+	
+	@Override
+	public boolean hasNext() {
+		return nextRow != null;
+	}
+
+	@Override
+	public Object[] next() {
+		
+		if(hasNext()) {
+			Object[] row = nextRow;
+			nextRow = getNextRow();
+			return row;
+		} else {
+			throw new NoSuchElementException("No more elements");
+		}
+
+	}
+	
+	private Object[] getNextRow() {
+    	Object[] row = null;
+		try {
+			ResultSetMetaData rsmd = resultSet.getMetaData();
+	        int NumOfCol = rsmd.getColumnCount();
+	        if (resultSet.next()){
+	            row = new Object[NumOfCol];
+	            for(int i = 1; i <= NumOfCol; i++) {
+	                row[i-1] = resultSet.getObject(i);
+	            }
+	        }
+	        return row;			
+		} catch (SQLException e) {
+			return null;
+		}    	
+	}
+}
