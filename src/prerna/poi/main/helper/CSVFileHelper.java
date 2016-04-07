582adbc3300e34bdd42b7f41c7d6cbaca209d1b6
mahkhalil <mahkhalil@deloitte.com>
2016-04-07 16:49:56 +0000

    adding routine to predict the dataType for csv files
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@13861 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 00e3d060e52ade0742df5c98807f45d1a8839133

--- src/prerna/poi/main/helper/CSVFileHelper.java
+++ src/prerna/poi/main/helper/CSVFileHelper.java
@@ -4,11 +4,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Map;
 
 import com.univocity.parsers.csv.CsvParser;
 import com.univocity.parsers.csv.CsvParserSettings;
 
 import cern.colt.Arrays;
+import prerna.util.Utility;
 
 public class CSVFileHelper {
 	
@@ -88,6 +91,7 @@ public class CSVFileHelper {
 		makeSettings();
 		settings.selectFields(columns);
 		currHeaders = columns;
+		allHeaders = null;
 		reset();
 	}
 	
@@ -140,6 +144,57 @@ public class CSVFileHelper {
 	public String getFileLocation() {
 		return this.fileLocation;
 	}
+	
+	/**
+	 * Loop through all the data to see what the data types are for each column
+	 * @return
+	 */
+	public Map<String, String> predictTypes() {
+		Map<String, String> types = new Hashtable<String, String>();
+		for(String col : allHeaders) {
+			parseColumns(new String[]{col});
+			String type = null;
+			String[] row = null;
+			WHILE_LOOP : while( ( row = parser.parseNext()) != null) {
+				String val = row[0];
+				if(val.isEmpty()) {
+					continue;
+				}
+				String newTypePred = (Utility.findTypes(val)[0] + "").toUpperCase();
+				if(newTypePred.contains("VARCHAR")) {
+					type = newTypePred;
+					break WHILE_LOOP;
+				}
+				
+				// need to also add the type null check for the first row
+				if(!newTypePred.equals(type) && type != null) {
+					// this means there are multiple types in one column
+					// assume it is a string 
+					if( (type.equals("BOOLEAN") || type.equals("INT") || type.equals("DOUBLE")) && 
+							(newTypePred.equals("INT") || newTypePred.equals("INT") || newTypePred.equals("DOUBLE") ) ){
+						// for simplicity, make it a double and call it a day
+						// TODO: see if we want to impl the logic to choose the greater of the newest
+						// this would require more checks though
+						type = "DOUBLE";
+					} else {
+						// should only enter here when there are numbers and dates
+						// TODO: need to figure out what to handle this case
+						// for now, making assumption to put it as a string
+						type = "VARCHAR(800)";
+						break WHILE_LOOP;
+					}
+				} else {
+					// type is the same as the new predicated type
+					// or type is null on first iteration
+					type = newTypePred;
+				}
+			}
+			types.put(col, type);
+		}
+		
+		return types;
+	}
+	
 
 	///// TESTING CODE STARTS HERE /////
 	
@@ -153,6 +208,7 @@ public class CSVFileHelper {
 		before = System.nanoTime();
 		CSVFileHelper test = new CSVFileHelper();
 		test.parse(fileName);
+		System.out.println(test.predictTypes());
 		test.printRow(test.getNextRow());
 		test.printRow(test.allHeaders);
 		test.allHeaders = null;
