92d145f39668dfd2731e705f92638bfd0fb38077
mahkhalil <mahkhalil@deloitte.com>
2016-04-07 17:14:29 +0000

    cleaning up file helpers
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@13862 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 5cccba87bcb47fb7a4f9a5cbaee3f24f06e1c184

--- src/prerna/poi/main/helper/XLFileHelper.java
+++ src/prerna/poi/main/helper/XLFileHelper.java
@@ -3,12 +3,11 @@ package prerna.poi.main.helper;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Vector;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
@@ -16,128 +15,36 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import cern.colt.Arrays;
-import prerna.util.Utility;
 
 public class XLFileHelper {
 	
-	private static final Logger LOGGER = LogManager.getLogger(XLFileHelper.class.getName());
-
 	int colStarter = 0;
-	
+
 	private	XSSFWorkbook workbook = null;
 	private FileInputStream sourceFile = null;
-	private String fileName = null;
+	private String fileLocation = null;
 
-	private Hashtable <String, XSSFSheet> sheetNames = new Hashtable<String, XSSFSheet>();
-	private Hashtable <String, Integer> sheetCounter = new Hashtable<String, Integer>();
-	private Hashtable <String, Vector<String>> allProps = new Hashtable <String, Vector<String>> ();
+	private Map <String, XSSFSheet> sheetNames = new Hashtable<String, XSSFSheet>();
+	private Map <String, Integer> sheetCounter = new Hashtable<String, Integer>();
+	private Map <String, Vector<String>> allProps = new Hashtable <String, Vector<String>> ();
 
-	public void parse(String fileName) {
-		this.fileName = fileName;
+	/**
+	 * Parse the new file passed
+	 * @param fileLocation		The String location of the fileLocation
+	 */
+	public void parse(String fileLocation) {
+		this.fileLocation = fileLocation;
 		createParser();
 	}
 	
-	public String [] getTables()
-	{
-		String [] sheets = new String[sheetNames.size()];
-		Enumeration <String> sheetKeys = sheetNames.keys();
-		
-		
-		for(int sheetIndex = 0;sheetKeys.hasMoreElements();sheets[sheetIndex] = sheetKeys.nextElement(),sheetIndex++);
-		
-		return sheets;
-	}
-
-	private void putProp(String propertyName, String sheetName)
-	{
-		System.out.println(sheetName + " <>" + propertyName);
-		Vector <String> propValue = new Vector<String>();
-		if(allProps.containsKey(propertyName))
-			propValue = allProps.get(propertyName);
-		if(!propValue.contains(sheetName))
-		{
-			propValue.add(sheetName);
-			allProps.put(propertyName, propValue);
-		}
-	}
 	
-	public Vector<String> getRelations()
-	{
-		Vector <String> retString = new Vector<String>();
-		// for each property
-		// get the sheetnames and tag them together
-		Enumeration <String> propKeys = allProps.keys();
-		
-		while(propKeys.hasMoreElements())
-		{
-			String thisProp = propKeys.nextElement();
-			
-			Vector <String> sheets = allProps.get(thisProp);
-			
-			for(int thisSheetIndex = 0;thisSheetIndex < sheets.size();thisSheetIndex++)
-			{
-				String curSheet = sheets.remove(0);
-				for(int otherSheetIndex = 0;otherSheetIndex < sheets.size();otherSheetIndex++)
-				{
-					String fkString = curSheet + "." + thisProp + "." + sheets.get(otherSheetIndex) + "." + thisProp;
-					retString.add(fkString); // it is not what you are thinking
-				}
-			}
-		}
-		return retString;
-	}
-	
-	public String[] getCells(XSSFRow row)
-	{
-		int colLength = row.getLastCellNum();
-		return getCells(row, colLength);
-	}
-
-	public String[] getCells(XSSFRow row, int totalCol)
-	{
-		int colLength = totalCol;
-		String [] cols = new String[colLength];
-		for(int colIndex = colStarter;colIndex < colLength;colIndex++)
-		{
-			XSSFCell thisCell = row.getCell(colIndex);
-			// get all of this into a string
-			if(thisCell != null && row.getCell(colIndex).getCellType() != Cell.CELL_TYPE_BLANK)
-			{
-				if(thisCell.getCellType() == Cell.CELL_TYPE_STRING)
-				{
-					cols[colIndex] = thisCell.getStringCellValue();
-					cols[colIndex] = Utility.cleanString(cols[colIndex], true);
-				}
-				if(thisCell.getCellType() == Cell.CELL_TYPE_NUMERIC)
-					cols[colIndex] = "" + thisCell.getNumericCellValue();
-			}
-			else
-			{
-				cols[colIndex] = "";
-			}
-		}	
-
-		return cols;
-	}
-	
-
-	public String getCell(XSSFCell thisCell) {
-		if(thisCell != null && thisCell.getCellType() != Cell.CELL_TYPE_BLANK)
-		{
-			if(thisCell.getCellType() == Cell.CELL_TYPE_STRING) {
-				return thisCell.getStringCellValue();
-			}
-			else if(thisCell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
-				return thisCell.getNumericCellValue() + "";
-			}
-		}
-		return "";
-	}
-
+	/**
+	 * opens the workbook and gets all the sheets
+	 */
 	private void createParser()
 	{
 		try {
-			sourceFile = new FileInputStream(fileName);
+			sourceFile = new FileInputStream(fileLocation);
 			workbook = new XSSFWorkbook(sourceFile);
 
 			// get all the sheets
@@ -154,26 +61,26 @@ public class XLFileHelper {
 			e.printStackTrace();
 		}
 	}
-
 	
-
-	public String[] getNextRow(String sheetName)
-	{
+	public String[] getNextRow(String sheetName) {
 		XSSFSheet thisSheet = sheetNames.get(sheetName);
 		
 		int counter = 0;
-		if(sheetCounter.containsKey(sheetName))
+		if(sheetCounter.containsKey(sheetName)) {
 			counter = sheetCounter.get(sheetName);
+		}
 		
 		String [] thisRow = null;
-		if(counter < thisSheet.getLastRowNum())
+		if(counter < thisSheet.getLastRowNum()) {
 			thisRow = getCells(thisSheet.getRow(counter));
+		}
 		
 		// assimilate the properties
 		if(counter == 0)
 		{
-			for(int colIndex = colStarter;colIndex < thisRow.length;colIndex++)
+			for(int colIndex = colStarter;colIndex < thisRow.length;colIndex++) {
 				putProp(thisRow[colIndex], sheetName);
+			}
 		}
 		
 		// set counter back
@@ -182,37 +89,130 @@ public class XLFileHelper {
 
 		return thisRow;
 	}
+	
+	private String[] getCells(XSSFRow row) {
+		int colLength = row.getLastCellNum();
+		return getCells(row, colLength);
+	}
 
-	// resets everything.. just something to watch
-	public void reset()
+
+	private String[] getCells(XSSFRow row, int totalCol)
 	{
+		int colLength = totalCol;
+		String [] cols = new String[colLength];
+		for(int colIndex = colStarter; colIndex < colLength; colIndex++) {
+			XSSFCell thisCell = row.getCell(colIndex);
+			cols[colIndex] = getCell(thisCell);
+		}	
+
+		return cols;
+	}
+	
+
+	private String getCell(XSSFCell thisCell) {
+		if(thisCell != null && thisCell.getCellType() != Cell.CELL_TYPE_BLANK) {
+			if(thisCell.getCellType() == Cell.CELL_TYPE_STRING) {
+				return thisCell.getStringCellValue();
+			}
+			else if(thisCell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
+				return thisCell.getNumericCellValue() + "";
+			}
+		}
+		return "";
+	}
+	
+	/**
+	 * Each sheet becomes a table
+	 * @return
+	 */
+	public String [] getTables() {
+		String[] sheets = new String[sheetNames.size()];
+		int counter = 0;
+		for(String sheet : sheets) {
+			sheets[counter] = sheet;
+			counter++;
+		}
+		
+		return sheets;
+	}
+
+	/**
+	 * 
+	 * @param propertyName
+	 * @param sheetName
+	 */
+	private void putProp(String propertyName, String sheetName)
+	{
+		System.out.println(sheetName + " <>" + propertyName);
+		
+		Vector<String> propValue = null;
+		if(allProps.containsKey(propertyName)) {
+			propValue = allProps.get(propertyName);
+		} else {
+			propValue = new Vector<String>();
+		}
+		
+		if(!propValue.contains(sheetName)) {
+			propValue.add(sheetName);
+			allProps.put(propertyName, propValue);
+		}
+	}
+	
+	/**
+	 * Determine the relationships that should exist based on each sheet and its headers
+	 * @return
+	 */
+	public Vector<String> getRelations()
+	{
+		Vector <String> retVec = new Vector<String>();
+		// for each property
+		// get the sheetnames and tag them together
+		Iterator<String> propKeys = allProps.keySet().iterator();
+		
+		while(propKeys.hasNext())
+		{
+			String thisProp = propKeys.next();
+			Vector <String> sheets = allProps.get(thisProp);
+			
+			for(int thisSheetIndex = 0;thisSheetIndex < sheets.size();thisSheetIndex++)
+			{
+				String curSheet = sheets.remove(0);
+				for(int otherSheetIndex = 0; otherSheetIndex < sheets.size(); otherSheetIndex++)
+				{
+					String fkString = curSheet + "." + thisProp + "." + sheets.get(otherSheetIndex) + "." + thisProp;
+					retVec.add(fkString); // it is not what you are thinking
+				}
+			}
+		}
+		return retVec;
+	}
+	
+	/**
+	 * Reset to start the parser from the beginning of the file
+	 */
+	public void reset() {
+		clear();
+		createParser();
+	}
+	
+	/**
+	 * Clears the parser and requires you to start the parsing from scratch	
+	 */
+	public void clear() {
 		try {
-			sourceFile.close();
-			createParser();
+			if(sourceFile != null) {
+				sourceFile.close(); 
+			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public void reset(boolean getHeader)
-	{
-		reset();
-	}
+	
+	///// TESTING CODE STARTS HERE /////
 
-	public String[] getSheets() {
-		return null;
-	}
-	
-	
-	/**
-	 * Used for testing
-	 * @param args
-	 * @throws Exception
-	 */
 	public static void main(String [] args) throws Exception
 	{
 		String fileName = "C:/Users/pkapaleeswaran/workspacej3/datasets/Movie.csv";
