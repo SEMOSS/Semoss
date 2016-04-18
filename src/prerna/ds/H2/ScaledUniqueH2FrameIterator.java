105e71345fb08b186e62e61863094635184142fe
rluthar <rluthar@deloitte.com>
2016-04-18 22:10:34 +0000

    committing h2builder to be stateless, i.e. depend on types externally, adding optimization such that group by columns are created in advance after first group by
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@14092 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 508a52cb35aab051c87bf6c17c6da3543398ec37

--- src/prerna/ds/H2/ScaledUniqueH2FrameIterator.java
+++ src/prerna/ds/H2/ScaledUniqueH2FrameIterator.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 import org.apache.tinkerpop.gremlin.structure.Graph;
@@ -17,6 +18,7 @@ public class ScaledUniqueH2FrameIterator implements Iterator<List<Object[]>> {
 	private String columnName;
 	private int columnNameIndex;
 	private List<String> selectors;
+	private String tableName;
 	private String[] finalColumns;
 	
 	private Double[] maxArr;
@@ -27,21 +29,27 @@ public class ScaledUniqueH2FrameIterator implements Iterator<List<Object[]>> {
 	private H2Builder builder;
 	private Iterator<Object> valueIterator;
 	
+	private Map<String, String> headerTypeMap;
+	
 	public ScaledUniqueH2FrameIterator(
 		String columnName,
 		boolean getRawData, 
-		List<String> selectors, 
+		String tableName, 
 		H2Builder builder, 
 		Double[] maxArr, 
-		Double[] minArr) {
+		Double[] minArr,
+		Map<String, String> headerTypeMap,
+		List<String> selectors) {
 		
 		this.selectors = selectors;
+		this.tableName = tableName;
 		this.dataType = getRawData ? Constants.VALUE : Constants.NAME;
 		this.columnName = columnName;
 		this.columnNameIndex = selectors.indexOf(columnName);
 		this.maxArr = maxArr;
 		this.minArr = minArr;
 		this.builder = builder;
+		this.headerTypeMap = headerTypeMap;
 		
 		Object[] column = builder.getColumn(columnName, true);
 		valueIterator = Arrays.asList(column).iterator();
@@ -56,7 +64,7 @@ public class ScaledUniqueH2FrameIterator implements Iterator<List<Object[]>> {
 		
 		if(hasNext()) {
 			Object nextVal = valueIterator.next();
-			List<Object[]> retData = builder.getScaledData(selectors, columnName, nextVal, maxArr, minArr);
+			List<Object[]> retData = builder.getScaledData(tableName, selectors, headerTypeMap, columnName, nextVal, maxArr, minArr);
 			return retData;
 		} else {
 			throw new NoSuchElementException("No more elements"); 
