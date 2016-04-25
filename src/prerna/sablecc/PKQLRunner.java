eeb795563ef1cd1d961ff4335bed8d31d032cd38
mahkhalil <mahkhalil@deloitte.com>
2016-04-25 16:23:05 +0000

    setting a success at the end of the math reactor,  out term expression no longer sets a value in the PKQL runner, PKQL runner now processes through arrays to show the information instead of sending back the hashcode
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@14234 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 984dcea5fbe95bba85f4a3a1e3ea29ea3456fc76

--- src/prerna/sablecc/PKQLRunner.java
+++ src/prerna/sablecc/PKQLRunner.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 import org.apache.commons.lang.StringEscapeUtils;
 
+import cern.colt.Arrays;
 import prerna.algorithm.api.ITableDataFrame;
 import prerna.sablecc.lexer.Lexer;
 import prerna.sablecc.lexer.LexerException;
@@ -39,11 +40,55 @@ public class PKQLRunner {
 		}
 
 		HashMap<String, Object> result = new HashMap<String, Object>();
-		result.put("result", StringEscapeUtils.escapeHtml(response + ""));
+		if(response instanceof Object[]) {
+			StringBuilder builder = new StringBuilder();
+			String retResponse = getStringFromObjectArray( (Object[]) response, builder);
+			result.put("result", StringEscapeUtils.escapeHtml(retResponse));
+		} else if(response instanceof double[]) {
+			result.put("result", StringEscapeUtils.escapeHtml(Arrays.toString( (double[]) response)));
+		} else if(response instanceof int[]) {
+			result.put("result", StringEscapeUtils.escapeHtml(Arrays.toString( (int[]) response)));
+		} else { 
+			result.put("result", StringEscapeUtils.escapeHtml(response + ""));
+		}
 		result.put("status", currentStatus);
 		return result;
 	}
 
+	private String getStringFromObjectArray(Object[] objArray, StringBuilder builder) {
+		builder.append("[");
+		for(int i = 0; i < objArray.length; i++) {
+			Object obj = objArray[i];
+			if(obj instanceof Object[]) {
+				getStringFromObjectArray((Object[]) obj, builder);
+			} else {
+				if(i == objArray.length-1) {
+					if(obj instanceof double[]) {
+						builder.append(Arrays.toString((double[]) obj)).append("]");
+					} else if(obj instanceof int[]) {
+						builder.append(Arrays.toString((int[]) obj)).append("]");
+					} else {
+						builder.append(obj).append("]");
+					}
+				} else {
+					// since primitive arrays are stupid in java
+					if(obj instanceof double[]) {
+						builder.append(Arrays.toString((double[]) obj)).append(", ");
+					} else if(obj instanceof int[]) {
+						builder.append(Arrays.toString((int[]) obj)).append(", ");
+					} else {
+						builder.append(obj).append(", ");
+					}
+				}
+			}
+			// not sure when we would ever send an uneven matrix
+			// but if we do, this break would be weird
+			builder.append("\n");
+		}
+		
+		return builder.toString();
+	}
+	
 	public void setResponse(Object response) {
 		this.response = response;
 	}
