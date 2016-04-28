6e423b9d20086c657f4f01a1f9141b89f41fdbcf
rluthar <rluthar@deloitte.com>
2016-04-28 13:27:39 +0000

    TableDataFrameFactory - adding conversion to h2 from tinker, ColAddReactor - aligning group by to match columns, still need to optimize, RemoveDataReactor - working for tinker and h2 now, DataFrameHelper.java - committing as helper to do operations that cannot be done from outside classes due to scope
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@14340 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: cdb8a06dfdbc68f5b61961fdb20e7838eaf1939d

---
+++ src/prerna/ds/DataFrameHelper.java
@@ -0,0 +1,49 @@
+package prerna.ds;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import prerna.algorithm.api.IMetaData;
+import prerna.algorithm.api.ITableDataFrame;
+import prerna.ds.H2.TinkerH2Frame;
+import prerna.engine.api.ISelectStatement;
+import prerna.engine.api.ISelectWrapper;
+
+public class DataFrameHelper {
+
+	public static void removeData(ITableDataFrame frame, ISelectWrapper it) {
+		if(frame instanceof TinkerH2Frame) {
+			
+			while(it.hasNext()){
+				ISelectStatement ss = (ISelectStatement) it.next();
+				System.out.println(((ISelectStatement)ss).getPropHash());
+				frame.removeRelationship(ss.getPropHash(), ss.getRPropHash());
+			}
+			
+		} else if(frame instanceof TinkerFrame) {
+			
+			IMetaData metaData = ((TinkerFrame)frame).metaData;
+			String[] columnHeaders = frame.getColumnHeaders();
+			TinkerH2Frame tempFrame = TableDataFrameFactory.convertToH2Frame(frame);
+			while(it.hasNext()){
+				ISelectStatement ss = (ISelectStatement) it.next();
+				System.out.println(((ISelectStatement)ss).getPropHash());
+				tempFrame.removeRelationship(ss.getPropHash(), ss.getRPropHash());
+			}
+			
+			TinkerFrame tframe = new TinkerFrame();
+			tframe.metaData = metaData;
+			Iterator<Object[]> iterator = tempFrame.iterator(false);
+			while(iterator.hasNext()) {
+				Map<String, Object> nextRow = new HashMap<String, Object>();
+				Object[] row = iterator.next();
+				for(int i = 0; i < row.length; i++) {
+					nextRow.put(columnHeaders[i], row[i]);
+				}
+				tframe.addRelationship(nextRow, nextRow);
+			}
+			((TinkerFrame) frame).g= tframe.g;
+		}
+	}
+}
