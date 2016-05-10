30d9c338fc4d12d7ecc241a350b413153d5558d4
Maher Khalil <themaherkhalil@gmail.com>
2016-05-10 15:46:11 +0000

    update to allow for multi csv uploading with mismatching column names
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@14636 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: dc079b2cb055a7b18bb5bbd401c2b31b6c1779cc

--- src/prerna/ds/util/FileIterator.java
+++ src/prerna/ds/util/FileIterator.java
@@ -110,7 +110,7 @@ public class FileIterator implements Iterator<IHeadersDataRow>{
 				Set<Object> nextSet = filters.get(headers[i]);
 				if(nextSet != null ){
 					if(dataTypeMap.get(headers[i]).toUpperCase().startsWith("VARCHAR")) {
-						if(nextSet.contains(Utility.cleanString(row[i], false))) {
+						if(nextSet.contains(Utility.cleanString(row[i], true))) {
 							newRow = row;
 						}
 					} else if(nextSet.contains(row[i])) {
