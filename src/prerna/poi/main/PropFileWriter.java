aaab9d4d14aa3d258c1403925f9b65b08df8a4c2
mahkhalil <mahkhalil@deloitte.com>
2016-05-12 03:29:41 +0000

    adding code such that the logic to determine data types into owl is controlled by a boolean in the smss file to avoid performing tests on start up every time
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@14713 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/poi/main/PropFileWriter.java
+++ src/prerna/poi/main/PropFileWriter.java
@@ -276,6 +276,7 @@ public class PropFileWriter {
 				pw.write(Constants.ENGINE_TYPE + "\t" + this.defaultEngine + "\n");
 				pw.write(Constants.DREAMER + "\t" + questionFileName + "\n\n\n");
 			}
+			pw.write(Constants.FILL_EMPTY_DATATYPES + "\tfalse\n");
 			if (dbType == ImportDataProcessor.DB_TYPE.RDBMS) {
 				if(this.queryUtil == null) {
 					this.queryUtil = SQLQueryUtil.initialize(dbDriverType);
