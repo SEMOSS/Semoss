0647357747da8d1b7ae6d4725cd230b9d94c6693
mahkhalil <mahkhalil@deloitte.com>
2016-06-14 21:33:08 +0000

    adding anthem specific playsheets. adding selectors in TablePlaySheet
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@15171 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 984941a6f5437728202227cd56a942baa930bb17

---
+++ src/prerna/ui/components/specific/anthem/AnthemPainpointsPlaysheet.java
@@ -0,0 +1,61 @@
+package prerna.ui.components.specific.anthem;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import prerna.ds.H2.H2Frame;
+import prerna.ui.components.playsheets.TablePlaySheet;
+import prerna.ui.components.playsheets.datamakers.DataMakerComponent;
+import prerna.ui.components.playsheets.datamakers.IDataMaker;
+
+public class AnthemPainpointsPlaysheet extends TablePlaySheet implements IDataMaker {
+
+	private static final Logger logger = LogManager.getLogger(TablePlaySheet.class.getName());
+	private DataMakerComponent dmComponent;
+	static String masterQuery = "SELECT DISTINCT OBA_L1.OBA_L0_FK AS OBA_L0, OBA_L1.OBA_L1 AS OBA_L1, OBA_L2.OBA_L2  OBA_L2, PAIN_POINT.PAIN_POINT_DESCRIPTION AS PAIN_POINT "
+			+ "FROM OBA_L1, PAIN_POINT "
+			+ "INNER JOIN OBA_L2 ON OBA_L1.OBA_L1 = OBA_L2.OBA_L1_FK AND PAIN_POINT.PAIN_POINT = OBA_L2.PAIN_POINT_FK";
+
+	//OBA_L0.OBA_L0.OBA_L1.OBA_L0_FK
+
+	public static String instanceOfPlaysheet = "prerna.ui.components.specific.tap.AnthemPainpointsPlaysheet";
+
+	//create a datamaker
+	@Override
+	@Deprecated
+	public void createData() {
+		if (this.dmComponent == null) {
+			this.dmComponent = new DataMakerComponent(this.engine, masterQuery);
+		}
+
+		if(this.dataFrame == null) {
+			this.dataFrame = new H2Frame();
+		}
+		this.dataFrame.processDataMakerComponent(this.dmComponent);
+	}
+
+
+	@Override
+	public void setUserId(String userId) {
+		if(this.dataFrame == null) {
+			this.dataFrame = new H2Frame();
+		}
+		this.dataFrame.setUserId(userId);
+	}
+
+	// just calls default getDataMakerOutput
+	public Map getData(Hashtable<String, Object> obj) {
+		Map<String, Object> returnHashMap =  getDataMakerOutput();
+		returnHashMap.put("Styling", "Anthem");
+		return returnHashMap;
+	}
+
+	@Override
+	public Map getDataMakerOutput(String... selectors) {
+		return super.getDataMakerOutput(selectors);
+	}     
+
+}
