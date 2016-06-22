caec2d0a57f02c334d12f821b83c151af79326f6
mahkhalil <mahkhalil@deloitte.com>
2016-06-22 21:26:39 +0000

    commenting around import data reactors, modification to isEmpty() method in abstract table data frame, commenting in CSVPropFileBuilder, separation of used and unused code in FileUploader
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@15255 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/poi/main/CSVPropFileBuilder.java
+++ src/prerna/poi/main/CSVPropFileBuilder.java
@@ -27,92 +27,186 @@
  *******************************************************************************/
 package prerna.poi.main;
 
-import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.List;
 
 import prerna.util.Constants;
 import prerna.util.Utility;
 
 public class CSVPropFileBuilder{
 
+	/*
+	 * This class is used to take the information passed from the user during the 
+	 * metamodel phase of creating a new database and writing it into a prop file.
+	 * 
+	 * The prop file is passed into the proper readers to create the engine and is 
+	 * written onto the server.  This will enable a user to reuse the prop file generated
+	 * to load additional databases or add to the existing database assuming the desired 
+	 * database metamodel is the same and the csv files are formatted the same.
+	 */
+
+	// string builder to contain the relationship string 
 	private StringBuilder relationships = new StringBuilder();
+	
+	// string builder to contain the node_properties
 	private StringBuilder node_properties = new StringBuilder();
-	private StringBuilder node_displaynames = new StringBuilder();	
-	private Hashtable<String, String> propHash = new Hashtable<String, String>();
+	
+	// string builder to contain the display names
+	private StringBuilder node_displaynames = new StringBuilder();
+	
+	// map containing the data types for each column of the csv file
 	private Hashtable<String, String> dataTypeHash = new Hashtable<String, String>();
 	
+	// prop hash containing the information of the prop file
+	// passed into the readers to get schema metadata around the file to upload
+	private Hashtable<String, String> propHash = new Hashtable<String, String>();
+
+	// string containing the entire prop file
+	// used to write the prop file to be reused for future database loading
+	// this contains the same format as the propHash class variable
 	private StringBuilder propFile = new StringBuilder();
 	
-	public void addProperty(ArrayList<String> sub, ArrayList<String> obj, String dataType) {
+	/**
+	 * Used to add a property into the prop file
+	 * @param sub					List containing the column headers to concatenate as the main node
+	 * @param obj					List containing the column headers to concatenate as the property on the main node
+	 * @param dataType				The data type of the property column.
+	 */
+	public void addProperty(List<String> sub, List<String> obj, String dataType) {
+		// concatenate the subject column headers using a plus sign
 		Iterator<String> subIt = sub.iterator();
 		String subject = subIt.next();
 		while(subIt.hasNext()){
 			subject = subject + "+" + subIt.next();
 		}
 		
+		// concatenate the object column headers using a plus sign
 		Iterator<String> objIt = obj.iterator();
 		String object = objIt.next();
 		while(objIt.hasNext()){
 			object = object + "+" + objIt.next();
 		}
 		
+		// store the data type of the column 
 		dataTypeHash.put(object, dataType);
+		// add the node_property to the string builder
+		// all the node property are combined in a string which is semicolon delimited
+		// each individual property is the concatenation of the subject, a percent sign, and the object
 		node_properties.append(subject+"%"+object+";");
 	}
 
-	public void addRelationship(ArrayList<String> sub, String pred, ArrayList<String> obj) {		
+	/**
+	 * Used to add a relationship into the prop file
+	 * @param sub					List containing the column headers to concatenate as the upstream node of the relationship
+	 * @param pred					String containing the verb describing the relationship between the subject and object
+	 * @param obj					List containing the column headers to concatenate as the downstream node of the relationship
+	 */
+	public void addRelationship(List<String> sub, String pred, List<String> obj) {		
+		// concatenate the subject column headers using a plus sign
 		Iterator<String> subIt = sub.iterator();
 		String subject = subIt.next();
 		while(subIt.hasNext()){
 			subject = subject + "+" + subIt.next();
 		}
 
+		// concatenate the object column headers using a plus sign
 		Iterator<String> objIt = obj.iterator();
 		String object = objIt.next();
 		while(objIt.hasNext()){
 			object = object + "+" + objIt.next();
 		}
 
+		// clean the predicate value such that it can be used and still form a valid URI
+		// NOTE: this is only import for RDF databases... RDBMS database will not even use this
 		pred = Utility.cleanString(pred, true).replaceAll("[()]", "").replaceAll(",", "");
 		
+		// add the relationship to the string builder
+		// all the relationships are combined in a string which is semicolon delimited
+		// each individual relationship is the concatenation of the subject, the at symbol, the predicate, the at symbol, and the object
 		relationships.append(subject+"@"+pred+"@"+object+";");
 	}
 	
-	public void addDisplayName(ArrayList<String> node, ArrayList<String> prop, ArrayList<String> dspName){
+	/**
+	 * Adds the display name (or logical name) for each concept and property into the prop file
+	 * @param node					List containing the column headers to concatenate representing the concept 
+	 * @param prop					List containing the column headers to concatenate representing a property
+	 * 								Properties require the concept node as well to add the proper display name
+	 * @param dspName				List containing the display name for the concept/property...
+	 * 								TODO: not sure who set this up and why they thought this should be a list as
+	 * 								opposed to just a string... :/
+	 */
+	public void addDisplayName(List<String> node, List<String> prop, List<String> dspName){
+		// concatenate the concept column headers using a plus sign
 		Iterator<String> nodeIt = node.iterator();
 		String nodeSubject = nodeIt.next();
 		while(nodeIt.hasNext()){
 			nodeSubject = nodeSubject + "+" + nodeIt.next();
 		}
 		
+		// if present, concatenate the property column headers using a plus sign
+		// this is present if we are adding the display name for a property
+		// 		which will use utilize the node string as well
+		// this is not present if the display name is for a concept
 		Iterator<String> nodePropIt = prop.iterator();
 		String nodeProperty = nodePropIt.next();
 		while(nodeIt.hasNext()){
 			nodeProperty = nodeProperty + "+" + nodePropIt.next();
 		}
 		
+		// grab the first value in the display name list
+		// that is going to be the logical name for the concept/property
+		// TODO: again, not sure why a list is passed instead of a string
 		Iterator<String> displayNameIt = dspName.iterator();
 		String displayName = displayNameIt.next();
 		
+		// concatenate the property string with the node string 
+		// this concatenation is done with a percent sign between the concept and the property
 		String nodes = nodeSubject;
 		if(nodeProperty.length() >0 && !nodeProperty.equals(nodeSubject)){
 			nodes += "%" + nodeProperty;
 		}
+		
+		// add the display name to the string builder
+		// all the display names are combined in a string which is semicolon delimited
+		// display name format is slightly different if it is for a concept vs. a property
+		// for a concept the structure is: concatenation of the concept, a colon, and the display name
+		// for a property the structure is: concatenation of the property_parent_concept, a percent sign, 
+		// 									the property, a colon, and the display name
 		node_displaynames.append(nodes+":"+displayName+";");
 	}
 
-	public void columnTypes(ArrayList<String> header){
-		propHash.put("NUM_COLUMNS", Integer.toString(header.size()));
-		propFile.append("NUM_COLUMNS" + "\t" + Integer.toString(header.size()) + "\n");
-		for(int i = 0; i < header.size(); i++)
+	/**
+	 * TODO: there is a fundamental assumption in this code that concepts are always going to be loaded in as strings
+	 * This dependency was added since URIs are always strings and upload used to be only for RDF.  This code only
+	 * considers the data types for the columns passed in as properties.  These are the only columns stored within 
+	 * the dataTypeHash.  If the column is not stored in the dataTypeHash, we automatically assume that each value is
+	 * a string.
+	 * 
+	 * 
+	 * Stores the data type for each column. Each column is referred to by its position in the csv and is assigned a data type value
+	 * @param header				The input list of headers matching the header order from the file
+	 */
+	public void columnTypes(List<String> header){
+		// input the total number of columns into the prop file and prop hash
+		int numHeaders = header.size();
+		propHash.put("NUM_COLUMNS", Integer.toString(numHeaders));
+		propFile.append("NUM_COLUMNS" + "\t" + Integer.toString(numHeaders) + "\n");
+		// loop through all the headers
+		for(int i = 0; i < numHeaders; i++)
 		{
+			//TODO: this is where the assumption kicks in that if the column never appeared when we were adding node property
+			// then the column is just a string
+			
+			// if the data type is defined for the column header
 			if(header.get(i) != null && dataTypeHash.containsKey(header.get(i)))
 			{
+				// grab the data type and append it to the propFile and to the propHash
 				propHash.put(Integer.toString(i+1), dataTypeHash.get(header.get(i)));
 //				System.out.println(header.get(i) + ":" + Integer.toString(i+1) + ":" + dataTypeHash.get(header.get(i)));
 				propFile.append(Integer.toString(i+1) + "\t" + dataTypeHash.get(header.get(i)) + "\n");
 			}
+			// else, the data type is not defined, so just load it as a string
 			else
 			{
 				propHash.put(Integer.toString(i+1), "STRING");
@@ -122,7 +216,15 @@ public class CSVPropFileBuilder{
 		}
 	}
 
+	/**
+	 * TODO: name is not consistent with functionality... this also constructs the propFile!!!
+	 * 
+	 * Adds all the instance string builder objects and values into the prop file and the prop hash
+	 * @param startRowNumAsString			The value for the start row to load in the file during the data loading process
+	 * @param endRowNumAsString				The value for the end row to load in the file during the data loading process
+	 */
 	public void constructPropHash(String startRowNumAsString, String endRowNumAsString){
+		// just add everything into the prop hash
 		propHash.put("START_ROW", startRowNumAsString);
 		propHash.put("END_ROW", endRowNumAsString);
 		propHash.put("NOT_OPTIONAL", ";");
@@ -131,6 +233,7 @@ public class CSVPropFileBuilder{
 		propHash.put(Constants.DISPLAY_NAME, node_displaynames.toString());
 		propHash.put("RELATION_PROP", "");
 		
+		// and do the same and add it in the prop file
 		propFile.append("START_ROW\t" + startRowNumAsString + "\n");
 		propFile.append("END_ROW\t" + endRowNumAsString + "\n");
 		propFile.append("RELATION\t" + relationships.toString() + "\n");
@@ -139,11 +242,23 @@ public class CSVPropFileBuilder{
 		propFile.append("RELATION_PROP\t \n");
 	}
 
+	/**
+	 * TODO: name is not consistent with functionality... this also constructs the propFile!!!
+	 * 
+	 * Finishes construction of the prop hash and prop fileand returns it
+	 * @param startRowNumAsString			The value for the start row to load in the file during the data loading process
+	 * @param endRowNumAsString				The value for the end row to load in the file during the data loading process
+	 * @return								The prop hash
+	 */
 	public Hashtable<String, String> getPropHash(String startRowNumAsString, String endRowNumAsString) {
 		constructPropHash(startRowNumAsString, endRowNumAsString);
 		return propHash;
 	}
-	
+
+	/**
+	 * Returns the string containing all the data to write as a prop file
+	 * @return
+	 */
 	public String getPropFile() {
 		return propFile.toString();
 	}
