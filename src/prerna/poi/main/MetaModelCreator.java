e6582f995d102795cce7daaa7d11fac89aabc66a
rluthar <rluthar@deloitte.com>
2016-07-12 16:57:46 +0000

    optimization updates, adding start/end counts for return data
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@15444 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 7dbd60a2d95fd4c49718eacbe38ac46133592957

--- src/prerna/poi/main/MetaModelCreator.java
+++ src/prerna/poi/main/MetaModelCreator.java
@@ -1,27 +1,18 @@
 package prerna.poi.main;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.Properties;
 
-
-
-
-import prerna.algorithm.api.IMetaData.DATA_TYPES;
-import prerna.algorithm.api.ITableDataFrame;
-import prerna.ds.TableDataFrameFactory;
-import prerna.ds.H2.H2Frame;
+import prerna.poi.main.helper.CSVFileHelper;
 import prerna.util.ArrayUtilityMethods;
 
 /**
@@ -30,15 +21,19 @@ import prerna.util.ArrayUtilityMethods;
  */
 public class MetaModelCreator {
 
-	ITableDataFrame frame;
+//	ITableDataFrame frame;
+	String[] columnHeaders;
 	int limit = 500;
 	Map<String, Boolean> columnPropMap;
-	List<Object[]> data;
+	List<String[]> data;
 	Map<String, Set<String>> matches;
 	int[] processOrder; //order in which to process columns 
 	Map<String, String> dataTypeMap;
 	String propFile;
 	CreatorMode mode;
+	int endRow = 2;
+	int startRow = 2;
+	
 	//return data
 	Map<String, List<String>> allowableDataTypes;
 	private Map<String, List<Map<String, Object>>> propFileData = new HashMap<>();
@@ -46,20 +41,34 @@ public class MetaModelCreator {
 	public enum CreatorMode {AUTO, PROP, TABLE};
 	
 	public MetaModelCreator() {
-		frame = new H2Frame();
 	}
 	
-	public MetaModelCreator(String fileName, Map<String, Map<String, String>> dataTypeMap, String delimeter, CreatorMode setting) {
+	public MetaModelCreator(String fileName, Map<String, Map<String, String>> dataTypeMap, String delimiter, CreatorMode setting) {
 		//use the file name
-		//TODO : limit to first 500 rows and use CSVHelper to read data\
+		//TODO : limit to first 500 rows and use CSVHelper to read data
 		//Migrate away from frame since it is unnecessary overhead
-		if(setting != CreatorMode.PROP) {
-			Map<String, String> mainColMap = new HashMap<>();
-			mainColMap.put("CSV", null);
-			frame = TableDataFrameFactory.generateDataFrameFromFile(fileName, delimeter, "H2", dataTypeMap, mainColMap);
-		}
 		this.dataTypeMap = dataTypeMap.get("CSV");
 		this.mode = setting;
+		this.data = new ArrayList<>(500);
+		
+		
+		CSVFileHelper helper = new CSVFileHelper();
+		helper.setDelimiter(delimiter.charAt(0));
+		helper.parse(fileName);
+		
+		String[] headers = this.dataTypeMap.keySet().toArray(new String[]{});
+		headers = helper.orderHeadersToGet(headers);
+		this.columnHeaders = headers;
+		
+		String [] cells = null;
+		int count = 0;
+		while((cells = helper.getNextRow()) != null) {
+			if(count < limit) {
+				data.add(cells);
+			}
+			count++;
+		}
+		this.endRow = count;
 	}
 	
 
@@ -96,21 +105,21 @@ public class MetaModelCreator {
 	 * auto generate the meta model
 	 */
 	private void autoGenerateMetaModel() {
-		String[] columnHeaders = frame.getColumnHeaders();
 		
 		//need to sort columns by number of unique instances
 		
 		//track which columns should be properties
 		matches = new HashMap<>(columnHeaders.length);
-		populateData();
+//		populateData();
 		populateProcessOrder();
 		populateColumnPropMap();
 		
 		//run comparisons for strings
 		for(int i = 0; i < columnHeaders.length; i++) {
 			
-			DATA_TYPES datatype = frame.getDataType(columnHeaders[i]);
-			if(datatype.equals(DATA_TYPES.STRING)) {
+//			DATA_TYPES datatype = frame.getDataType(columnHeaders[i]);
+			String datatype = this.dataTypeMap.get(columnHeaders[i]).toUpperCase();
+			if(datatype.equals("STRING")) {
 				//run all comparisons
 				runAllComparisons(columnHeaders, i);
 			}
@@ -118,8 +127,9 @@ public class MetaModelCreator {
 		
 		//run comparisons for non strings
 		for(int i = 0; i < columnHeaders.length; i++) {
-			DATA_TYPES datatype = frame.getDataType(columnHeaders[i]);
-			if(!datatype.equals(DATA_TYPES.STRING)) {
+//			DATA_TYPES datatype = frame.getDataType(columnHeaders[i]);
+			String datatype = this.dataTypeMap.get(columnHeaders[i]).toUpperCase();
+			if(!datatype.equals("STRING")) {
 				//run all comparisons
 				runAllComparisons(columnHeaders, i);
 			}
@@ -133,13 +143,14 @@ public class MetaModelCreator {
 		for(String subject : matches.keySet()) {
 			Set<String> set = matches.get(subject);
 			for(String object : set) {
-				DATA_TYPES dataType = frame.getDataType(object);
+//				DATA_TYPES dataType = frame.getDataType(object);
+				String dataType = this.dataTypeMap.get(object).toUpperCase();
 				Map<String, Object> predMap = new HashMap<>();
 				
 				String[] subjectArr = {subject};
 				String[] objectArr = {object};
 				
-				if(dataType.equals(DATA_TYPES.STRING)) {
+				if(dataType.equals("STRING")) {
 					String predHolder = subject + "_" + object;
 					predMap.put("sub", subjectArr);
 					predMap.put("pred", predHolder);
@@ -148,7 +159,7 @@ public class MetaModelCreator {
 				} else {
 					predMap.put("sub", subjectArr);
 					predMap.put("prop", objectArr);
-					predMap.put("dataType", dataType.toString());
+					predMap.put("dataType", dataType);
 					this.propFileData.get("propFileNodeProp").add(predMap);
 				}
 			}
@@ -161,7 +172,7 @@ public class MetaModelCreator {
 	private void generateMetaModelFromProp() throws Exception{
 		//TODO: check if prop file headers and csv headers match
 		if(this.propFile != null) {
-			String[] columnHeaders = frame.getColumnHeaders();
+//			String[] columnHeaders = frame.getColumnHeaders();
 			try {
 				InputStream input = new FileInputStream(propFile);
 				Properties prop = new Properties();
@@ -232,10 +243,10 @@ public class MetaModelCreator {
 						predMap.put("sub", subjectArr);
 						predMap.put("prop", objectArr);
 						
-						String dataType = frame.getDataType(object).toString();
-						if(dataType.equals("DOUBLE")) {
-							dataType = "NUMBER";
-						}
+						String dataType = this.dataTypeMap.get(object);
+//						if(dataType.equals("DOUBLE")) {
+//							dataType = "NUMBER";
+//						}
 						predMap.put("dataType", dataType);
 						this.propFileData.get("propFileNodeProp").add(predMap);
 					}
@@ -291,10 +302,23 @@ public class MetaModelCreator {
 					//itemDisplayName -> [{selectedNode: [title], selectedProperty: [title], selectedDisplayName : [moovietitles]}, {selectedNode : title, selectedProperty : budget}]
 				} 
 				
+				if(prop.containsKey("START_ROW")) {
+					String startRow = prop.getProperty("START_ROW");
+					try {
+						this.startRow = Integer.parseInt(startRow);
+					} catch(NumberFormatException e) {
+						this.startRow = 2;
+					}
+				}
 				
-//				if(prop.containsKey("RELATION_PROP")) {
-//					String relationProp = prop.getProperty("RELATION_PROP");
-//				}
+				if(prop.containsKey("END_ROW")) {
+					String endRow = prop.getProperty("END_ROW");
+					try {
+						this.endRow = Integer.parseInt(endRow);
+					} catch(NumberFormatException e) {
+						
+					}
+				}
 				
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -307,8 +331,8 @@ public class MetaModelCreator {
 	 * generate a metamodel in which the column with the most unique instances is the primary key and all other columns are properties of that column
 	 */
 	private void generateTableMetaModel() {
-		String[] columnHeaders = frame.getColumnHeaders();
-		populateData();
+//		String[] columnHeaders = frame.getColumnHeaders();
+//		populateData();
 		populateProcessOrder();
 		
 		List<Map<String, Object>> initList1 = new ArrayList<>();
@@ -320,7 +344,7 @@ public class MetaModelCreator {
 		String subject = null;
 		while(findPrimKey) {
 			subject = columnHeaders[processOrder[0]];
-			if(frame.getDataType(subject).equals(DATA_TYPES.STRING)) {
+			if(this.dataTypeMap.get(subject).equals("STRING")) {
 				findPrimKey = false;
 			}
 		}
@@ -332,7 +356,8 @@ public class MetaModelCreator {
 		
 		for(int i = 1; i < processOrder.length; i++) {
 			String object = columnHeaders[processOrder[i]];
-			DATA_TYPES dataType = frame.getDataType(object);
+//			DATA_TYPES dataType = frame.getDataType(object);
+			String dataType = this.dataTypeMap.get(object);
 			Map<String, Object> predMap = new HashMap<>();
 			
 			String[] subjectArr = {subject};
@@ -347,6 +372,14 @@ public class MetaModelCreator {
 	
 	public Map<String, List<Map<String, Object>>> getMetaModelData() {
 		return propFileData;
+	}
+	
+	public int getEndRow() {
+		return this.endRow;
+	}
+	
+	public int getStartRow() {
+		return this.startRow;
 	}
 
 	/**
@@ -375,8 +408,9 @@ public class MetaModelCreator {
 					if(firstColIndexInCSV > secondColIndexInCSV) {
 						//try to see if inverse order is better
 						//but first, check to make sure the second column in not a double or date
-						DATA_TYPES datatype = frame.getDataType(secondColumn);
-						if(datatype.equals(DATA_TYPES.STRING)) {
+//						DATA_TYPES datatype = frame.getDataType(secondColumn);
+						String datatype = dataTypeMap.get(secondColumn).toUpperCase();
+						if(datatype.equals("STRING")) {
 							if(!columnPropMap.get(firstColumn) && compareCols(i, firstColIndex)) {
 								//use reverse order
 								useInverse = true;
@@ -430,22 +464,40 @@ public class MetaModelCreator {
 	/**
 	 * collect the data that we will use to predict a meta model
 	 */
-	private void populateData() {
-		data = new ArrayList<Object[]>(limit);
-		Iterator<Object[]> it = frame.iterator(false);
-		int count = 0;
-		while(it.hasNext() && count < limit) {
-			data.add(it.next());
-			count++;
-		}
-	}
+//	private void populateData() {
+//		data = new ArrayList<Object[]>(limit);
+//		Iterator<Object[]> it = frame.iterator(false);
+//		int count = 0;
+//		while(it.hasNext() && count < limit) {
+//			data.add(it.next());
+//			count++;
+//		}
+//	}
 	
 	/**
 	 * process order is stored based on ascending order of unique instance count
 	 */
 	private void populateProcessOrder() {
-		String[] columnHeaders = frame.getColumnHeaders();
-		Integer[] instanceCount = frame.getUniqueInstanceCount();
+//		String[] columnHeaders = frame.getColumnHeaders();
+		Integer[] instanceCount = new Integer[columnHeaders.length];
+		
+		Map<String, Set<String>> instanceCountMap = new HashMap<>();
+		
+		for(String header : columnHeaders) {
+			instanceCountMap.put(header, new HashSet<>());
+		}
+		
+		for(String[] row : data) {
+			for(int i = 0; i < row.length; i++) {
+				instanceCountMap.get(columnHeaders[i]).add(row[i]);
+			}
+		}
+		
+		for(int i = 0; i < columnHeaders.length; i++) {
+			instanceCount[i] = instanceCountMap.get(columnHeaders[i]).size();
+		}
+		
+		
 		processOrder = new int[columnHeaders.length];
 		//sort by instance count
 		Map<Integer, String> sortedMap = new TreeMap<>();
@@ -466,7 +518,7 @@ public class MetaModelCreator {
 	 * initialize the column property map, each column defaulted to false
 	 */
 	private void populateColumnPropMap() {
-		String[] columnHeaders = frame.getColumnHeaders();
+//		String[] columnHeaders = frame.getColumnHeaders();
 		columnPropMap = new HashMap<>(columnHeaders.length);
 		for(String header : columnHeaders) {
 			columnPropMap.put(header, false);
