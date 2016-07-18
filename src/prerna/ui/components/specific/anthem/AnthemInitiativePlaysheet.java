f021a4d1925856fcd59f57f471b6dc20fc6ad1b6
ksmart <ksmart@deloitte.com>
2016-07-18 15:59:20 +0000

    updated anthem heat map playsheets
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@15518 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f4127b70028163d373a9cbf55d9364d301cee4cd

--- src/prerna/ui/components/specific/anthem/AnthemInitiativePlaysheet.java
+++ src/prerna/ui/components/specific/anthem/AnthemInitiativePlaysheet.java
@@ -1,6 +1,10 @@
 package prerna.ui.components.specific.anthem;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.LogManager;
@@ -14,17 +18,20 @@ import prerna.ui.components.playsheets.datamakers.IDataMaker;
 public class AnthemInitiativePlaysheet extends TablePlaySheet implements IDataMaker {
 	private static final Logger logger = LogManager.getLogger(TablePlaySheet.class.getName());
 	private DataMakerComponent dmComponent;
-	static String masterQuery = "SELECT DISTINCT OBA_L1.OBA_L0_FK AS OBA_L0, OBA_L1.OBA_L1 AS OBA_L1, OBA_L2.OBA_L2 AS OBA_L2, INITIATIVE.INITIATIVE_NAME AS INITIATIVE "
+	static String masterQuery = "SELECT DISTINCT OBA_L1.OBA_L0_FK AS OBA_L0, OBA_L1.OBA_L1 AS OBA_L1, OBA_L2.OBA_L2 AS OBA_L2, INITIATIVE.INITIATIVE_NAME AS PAIN_POINT_DESCRIPTION "
 			+ "FROM OBA_L1, INITIATIVE "
-			+ "INNER JOIN OBA_L2 ON OBA_L1.OBA_L1 = OBA_L2.OBA_L1_FK ";
+			+ "INNER JOIN OBA_L2 ON OBA_L1.OBA_L1 = OBA_L2.OBA_L1_FK AND INITIATIVE.INITIATIVE = OBA_L2.INITIATIVE_FK "
+			+ "where OBA_L2 is not null";
 
-	//OBA_L0.OBA_L0.OBA_L1.OBA_L0_FK
+	private final String OBA_L0 = "OBA_L0_FK";
+	private final String OBA_L1 = "OBA_L1";
+	private final String OBA_L2 = "OBA_L2";
+//	private final String INITIATIVE = "INITIATIVE_NAME";
 
-	public static String instanceOfPlaysheet = "prerna.ui.components.specific.tap.AnthemInitiativePlaysheet";
+	public static String instanceOfPlaysheet = "prerna.ui.components.specific.anthem.AnthemInitiativePlaysheet";
 
 	//create a datamaker
 	@Override
-	@Deprecated
 	public void createData() {
 		if (this.dmComponent == null) {
 			this.dmComponent = new DataMakerComponent(this.engine, masterQuery);
@@ -54,7 +61,24 @@ public class AnthemInitiativePlaysheet extends TablePlaySheet implements IDataMa
 
 	@Override
 	public Map getDataMakerOutput(String... selectors) {
-		return super.getDataMakerOutput(selectors);
+		Map<String, Object> returnHashMap = new HashMap <String, Object> ();
+		
+		Map<String, String> dataHash = super.getDataMakerOutput(selectors);
+		returnHashMap.putAll(dataHash);
+		
+		String[] headers = (String[]) returnHashMap.get("headers");
+		headers[3] = "PAIN_POINT_DESCRIPTION";//SHOULD CHANGE THIS TO MORE GENERIC, Better if we can make the query label this field as the right name (Filter) so we don't hard hard code it here
+		returnHashMap.put("headers",headers);
+		
+		
+		Map<String, String> dataTableAlign = new HashMap <String, String> ();
+		dataTableAlign.put("levelOne", OBA_L0);
+		dataTableAlign.put("levelTwo", OBA_L1);
+		dataTableAlign.put("levelThree", OBA_L2);
+//		dataTableAlign.put("heatValue", HEAT_VALUE);
+//		dataTableAlign.put("minValue", MIN_ACTIVITY_VALUE);
+		returnHashMap.put("dataTableAlign", dataTableAlign);
+		return returnHashMap;
 	}     
 
 }
