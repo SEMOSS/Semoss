e8127dbd0d69c8568b23f19675379ef2855c874e
mahkhalil <mahkhalil@deloitte.com>
2016-08-18 13:28:21 +0000

    adding fix for sql format to enable multiple left/right joins
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@15939 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/rdf/query/builder/SQLJoinObject.java
+++ src/prerna/rdf/query/builder/SQLJoinObject.java
@@ -3,22 +3,35 @@ package prerna.rdf.query.builder;
 import java.util.HashSet;
 import java.util.Set;
 
-public class SQLJoinObject {
+public class SqlJoinObject {
 
+	public enum SqlJoinTypeEnum {inner, left, right, outer, cross};
+	
+	// unique id for easy identification of the join object
+	private String id;
+	
 	// contains the query string associated with this join object
 	private StringBuilder queryString = new StringBuilder();
 	
 	// contains the list of values that are needed in order to determine 
 	private Set<String> requiredTableDefinitions = new HashSet<String>(); 
 	
+	// contains the list of alias defined within the join object
 	private Set<String> definedAliasWithinJoin = new HashSet<String>();
+	
+	// contains the join type
+	private SqlJoinTypeEnum joinType;
+	
+	public SqlJoinObject(String id) {
+		this.id = id;
+	}
 	
 	// add the required information to get the appropriate query pertaining to the join
 	public void addQueryString(String additionalJoinInfo) {
 		if(queryString.length() == 0) {
 			queryString.append(additionalJoinInfo);
 		} else {
-			queryString.append(" AND ").append(additionalJoinInfo);
+			queryString.append(" INNER JOIN ").append(additionalJoinInfo);
 		}
 	}
 	
@@ -38,11 +51,53 @@ public class SQLJoinObject {
 		return definedAliasSet.containsAll(requiredTableDefinitions);
 	}
 	
+	// get the query string for this join
 	public String getQueryString() {
 		return queryString.toString();
 	}
 	
+	// get the defined alias for the tables within this join
 	public Set<String> getDefinedAliasWithinJoin() {
 		return this.definedAliasWithinJoin;
+	}
+	
+	// set the join type
+	public void setSqlJoinType(SqlJoinTypeEnum joinType) {
+		this.joinType = joinType;
+	}
+	
+	// get the join type
+	public SqlJoinTypeEnum getSqlJoinType() {
+		return this.joinType;
+	}
+	
+	// get the join unique id
+	public String getId() {
+		return this.id;
+	}
+	
+	@Override
+	public String toString() {
+		return this.id;
+	}
+	
+	@Override
+	public boolean equals(Object object) {
+		// did the user pass in the id?
+		if(object instanceof String) {
+			if(object.toString().equals(this.id)) {
+				return true;
+			}
+		} else if(object instanceof SqlJoinObject) {
+			if(this == object) {
+				return true;
+			}
+			
+			if(this.queryString.toString().equals(((SqlJoinObject) object).queryString.toString())) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
