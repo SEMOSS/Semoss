4ba62fa42bfce667a9667e42eecfdc3ecf485c44
pvirdi <pvirdi@deloitte.com>
2016-09-06 13:20:08 +0000

    changes for /predictPKQL call - added content for "Add a new column", made changes for "Split a column by delimiter"
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@16185 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 251069cc38ff4f89334b5d61e7204db2b53c539b

--- src/prerna/sablecc/ColAddReactor.java
+++ src/prerna/sablecc/ColAddReactor.java
@@ -1,6 +1,5 @@
 package prerna.sablecc;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Iterator;
@@ -16,7 +15,7 @@ import prerna.util.Utility;
 
 public class ColAddReactor extends AbstractReactor {
 
-	Hashtable<String, String[]> values2SyncHash = new Hashtable<String, String[]>();
+	Hashtable <String, String[]> values2SyncHash = new Hashtable <String, String[]>();
 
 	public ColAddReactor() {
 		// these are the input columns - there is also expr Term which I will come to shortly
@@ -26,37 +25,35 @@ public class ColAddReactor extends AbstractReactor {
 
 		// this is the point where I specify what are the child values required
 		// for various input
-		String[] dataFromExpr = { PKQLEnum.COL_DEF };
+		String [] dataFromExpr = {PKQLEnum.COL_DEF};
 		values2SyncHash.put(PKQLEnum.EXPR_TERM, dataFromExpr);
 
-		String[] dataFromApi = { PKQLEnum.COL_CSV };
+		String [] dataFromApi = {PKQLEnum.COL_CSV};
 		values2SyncHash.put(PKQLEnum.API, dataFromApi);
-
-		// setting pkqlMetaData
+		
+		//setting pkqlMetaData
 		String title = "Add a new column";
-		String pkqlCommand = "col.add(c:newCol, (expression));";
+		String pkqlCommand = "col.add(c:newCol,(expression));";
 		String description = "Adds a new column named newCol, setting each cell to the result of the expression";
 		boolean showMenu = true;
 		boolean pinned = true;
 		super.setPKQLMetaData(title, pkqlCommand, description, showMenu, pinned);
-		//super.setPKQLMetaDataInput(populatePKQLMetaDataInput());
 		super.setPKQLMetaDataInput();
-		//super.setPKQLMetaDataConsole(populatePKQLMetaDataConsole());
 	}
-
+	
 	@Override
 	public Iterator process() {
 		// TODO Auto-generated method stub
 		// I need to take the col_def
 		// and put it into who am I
 		modExpression();
-		String nodeStr = (String) myStore.get(whoAmI);
+		String nodeStr = (String)myStore.get(whoAmI);
 		System.out.println("My Store on COL CSV " + myStore);
 		ITableDataFrame frame = (ITableDataFrame) myStore.get("G");
 
-		String[] joinCols = null;
+		String [] joinCols = null;
 		Iterator it = null;
-		String newCol = (String) myStore.get(PKQLEnum.COL_DEF + "_1");
+		String newCol = (String)myStore.get(PKQLEnum.COL_DEF + "_1");
 
 		// ok this came in as an expr term
 		// I need to do the iterator here
@@ -67,7 +64,7 @@ public class ColAddReactor extends AbstractReactor {
 		// however I dont have this shit
 		String expr = (String) myStore.get(PKQLEnum.EXPR_TERM);
 
-		Vector<String> cols = (Vector<String>) myStore.get(PKQLEnum.COL_DEF);
+		Vector <String> cols = (Vector <String>)myStore.get(PKQLEnum.COL_DEF);
 		// col def of the parent will have all of the col defs of the children
 		// need to remove the new column as it doesn't exist yet
 		cols.remove(newCol);
@@ -77,12 +74,12 @@ public class ColAddReactor extends AbstractReactor {
 		if (value == null)
 			value = myStore.get(PKQLEnum.API);
 
-		// if(value instanceof ColAddIterator) {
-		// ((ColAddIterator)value).updateNewColName(newCol);
-		// ((ColAddIterator)value).processIterator(frame);
-		// } else
+		//		if(value instanceof ColAddIterator) {
+		//			((ColAddIterator)value).updateNewColName(newCol);
+		//			((ColAddIterator)value).processIterator(frame);
+		//		} else 
 		if (value instanceof Iterator) {
-			it = (ExpressionIterator) value;
+			it = (ExpressionIterator)value;
 			processIt(it, frame, joinCols, newCol);
 		} else if (value instanceof Map){ // this will be the case when we are adding group by data
 			// this map is in the form { {groupedColName=groupedColValue} =
@@ -91,21 +88,21 @@ public class ColAddReactor extends AbstractReactor {
 			boolean addMetaData = true;
 			for (Object mapKey : vMap.keySet()) {
 				Vector<String> cols2 = new Vector<String>();
-				for (Object key : ((Map) mapKey).keySet()) {
-					cols2.add(key + "");
+				for(Object key : ((Map)mapKey).keySet()){
+					cols2.add(key+"");
 				}
 				String[] joinColss = convertVectorToArray(cols2);
 
-				Map mk = (Map) mapKey;
+				Map mk = (Map)mapKey;
 				Map<String, Object> row = new HashMap<>();
-				for (Object key : mk.keySet()) {
-					row.put(key + "", mk.get(key));
+				for(Object key : mk.keySet()) {
+					row.put(key+"", mk.get(key));
 				}
 
 				Object newVal = vMap.get(mapKey);
 				row.put(newCol, vMap.get(mapKey));
 
-				if (addMetaData) {
+				if(addMetaData) {
 					Object[] newType = Utility.findTypes(newVal.toString());
 					String type = "";
 					type = newType[0].toString();
@@ -119,7 +116,7 @@ public class ColAddReactor extends AbstractReactor {
 				frame.addRelationship(row, row);
 			}
 		} else {
-			if (value == null) {
+			if(value == null){
 				value = modExpression(expr); // expr doesn't get modded initially since its grabbed separately from whoAmI. Need to mod it here.
 			}
 			it = new ExpressionIterator(it, joinCols, value.toString());
@@ -148,31 +145,31 @@ public class ColAddReactor extends AbstractReactor {
 	}
 
 	private void processIt(Iterator it, ITableDataFrame frame, String[] joinCols, String newCol) {
-		if (it.hasNext()) {
+		if(it.hasNext()) {
 
 			boolean addMetaData = true;
 
 			if (joinCols.length > 1) { // multicolumn join
 				String primKeyName = TinkerMetaHelper.getPrimaryKey(joinCols);
-				while (it.hasNext()) {
+				while(it.hasNext()) {
 					HashMap<String, Object> row = new HashMap<String, Object>();
 					Object newVal = it.next();
 					Object[] values = new Object[joinCols.length];
-					if ((newVal instanceof List) && ((List) newVal).size() == 1)
-						row.put(newCol, ((List) newVal).get(0));
+					if ((newVal instanceof List) && ((List)newVal).size() == 1)
+						row.put(newCol, ((List)newVal).get(0));
 					else {
 						row.put(newCol, newVal);
 					}
-					for (int i = 0; i < joinCols.length; i++) {
+					for(int i = 0; i < joinCols.length; i++) {
 						if (it instanceof ExpressionIterator) {
-							Object rowVal = ((ExpressionIterator) it).getOtherBindings().get(joinCols[i]);
+							Object rowVal = ((ExpressionIterator)it).getOtherBindings().get(joinCols[i]);
 							row.put(joinCols[i], rowVal);
 							values[i] = rowVal;
 						}
 					}
 					row.put(primKeyName, TinkerMetaHelper.getPrimaryKey(values));
 
-					if (addMetaData) {
+					if(addMetaData) {
 						Object[] newType = Utility.findTypes(newVal.toString());
 						String type = "";
 						type = newType[0].toString();
@@ -185,23 +182,23 @@ public class ColAddReactor extends AbstractReactor {
 
 					frame.addRelationship(row, row);
 				}
-				myStore.put("STATUS", STATUS.SUCCESS);
+				myStore.put("STATUS", STATUS.SUCCESS);			
 			} else {
-				while (it.hasNext()) {
+				while(it.hasNext()) {
 					HashMap<String, Object> row = new HashMap<String, Object>();
 					Object newVal = it.next();
-					if ((newVal instanceof List) && ((List) newVal).size() == 1)
-						row.put(newCol, ((List) newVal).get(0));
+					if ((newVal instanceof List) && ((List)newVal).size() == 1)
+						row.put(newCol, ((List)newVal).get(0));
 					else {
 						row.put(newCol, newVal);
 					}
-					for (int i = 0; i < joinCols.length; i++) {
+					for(int i = 0; i < joinCols.length; i++) {
 						if (it instanceof ExpressionIterator) {
-							row.put(joinCols[i], ((ExpressionIterator) it).getOtherBindings().get(joinCols[i]));
+							row.put(joinCols[i], ((ExpressionIterator)it).getOtherBindings().get(joinCols[i]));
 						}
 					}
 
-					if (addMetaData) {
+					if(addMetaData) {
 						Object[] newType = Utility.findTypes(newVal.toString());
 						String type = "";
 						type = newType[0].toString();
@@ -220,48 +217,18 @@ public class ColAddReactor extends AbstractReactor {
 		}
 	}
 
-	// gets all the values to synchronize for this
+	// gets all the values to synchronize for this 
 	public String[] getValues2Sync(String input) {
 		return values2SyncHash.get(input);
 	}
 
-	////////////// setting the values for PKQL JSON for FE//////////////////////
-
+//////////////setting the values for PKQL JSON for FE//////////////////////
+	
 	/*private List<HashMap<String, Object>> populatePKQLMetaDataInput(){
 		List<HashMap<String, Object>> input = new ArrayList<HashMap<String, Object>>();
 		HashMap<String, Object> inputMap = new HashMap<String, Object>();
 		Object restrictions = new Object();
-		// first variable in PKQL
-		inputMap.put("label", "New Column Name");
-		inputMap.put("varName", "c:newCol");
-		inputMap.put("dataType", "text");
-		inputMap.put("type", "dropdown");
-		inputMap.put("restrictions", restrictions);
-		inputMap.put("source", "");
-		input.add(inputMap);
-
-		// second variable in PKQL
-		inputMap = new HashMap<String, Object>();
-		inputMap.put("label", "New Column Value");
-		inputMap.put("varName", "(expression)");
-		inputMap.put("dataType", "expression");
-		inputMap.put("type", "dropdown");
-		inputMap.put("restrictions", restrictions);
-		inputMap.put("source", "");
-		input.add(inputMap);
-		return input;
-	}*/
-	
-	private List<HashMap<String, Object>> populatePKQLMetaDataInput(){
-		List<HashMap<String, Object>> input = new ArrayList<HashMap<String, Object>>();
-		//HashMap<String, Object> inputMap = new HashMap<String, Object>();
-		//Object restrictions = new Object();
-		
-		for(String var: this.whatIReactTo){
-			//create a Utility method and pass whoAmI and whatIReactTo[i]/var
-			input.add(Utility.getPKQLInputVar(var, this.whoAmI));
-	}
-		/*//first variable in PKQL
+		//first variable in PKQL
 		inputMap.put("label", "New Column Name");
 		inputMap.put("varName", "c:newCol");
 		inputMap.put("dataType", "text");
@@ -278,16 +245,46 @@ public class ColAddReactor extends AbstractReactor {
 		inputMap.put("type", "dropdown");
 		inputMap.put("restrictions", restrictions);
 		inputMap.put("source", "");
-		input.add(inputMap);*/
+		input.add(inputMap);
 		return input;		
-	}
+	}*/
+	
+	/*private List<HashMap<String, Object>> populatePKQLMetaDataInput(){
+		List<HashMap<String, Object>> input = new ArrayList<HashMap<String, Object>>();
+		//HashMap<String, Object> inputMap = new HashMap<String, Object>();
+		//Object restrictions = new Object();
+		
+		for(String var: this.whatIReactTo){
+			//create a Utility method and pass whoAmI and whatIReactTo[i]/var
+			input.add(Utility.getPKQLInputVar(var, this.whoAmI));
+		}
+		//first variable in PKQL
+		inputMap.put("label", "New Column Name");
+		inputMap.put("varName", "c:newCol");
+		inputMap.put("dataType", "text");
+		inputMap.put("type", "dropdown");
+		inputMap.put("restrictions", restrictions);
+		inputMap.put("source", "");
+		input.add(inputMap);
 
+		//second variable in PKQL
+		inputMap = new HashMap<String, Object>();
+		inputMap.put("label", "New Column Value");
+		inputMap.put("varName", "(expression)");
+		inputMap.put("dataType", "expression");
+		inputMap.put("type", "dropdown");
+		inputMap.put("restrictions", restrictions);
+		inputMap.put("source", "");
+		input.add(inputMap);
+		return input;		
+	}*/
+	
 	/*private HashMap<String, Object> populatePKQLMetaDataConsole(){
 
 		HashMap<String, Object> console = new HashMap<String, Object>();
 		String[] groups = null;
 		Object buttonClass = new Object();
-		Object buttonActions = new Object();
+		Object buttonActions = new Object();		
 
 		console.put("name", "Console Name");
 		console.put("groups", groups);
