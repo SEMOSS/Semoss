2876feb335f49150f291c3f1b33864daa0d6624f
pvirdi <pvirdi@deloitte.com>
2016-09-07 00:26:36 +0000

    updated /predictPKQL - added pkql metadata for col.unfilter, minor changes for col.filter
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@16204 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc/ColUnfilterReactor.java
+++ src/prerna/sablecc/ColUnfilterReactor.java
@@ -11,30 +11,39 @@ public class ColUnfilterReactor extends AbstractReactor{
 		String [] thisReacts = {PKQLEnum.COL_DEF}; // these are the input columns - there is also expr Term which I will come to shortly
 		super.whatIReactTo = thisReacts;
 		super.whoAmI = PKQLEnum.UNFILTER_DATA;
+
+		//setting pkqlMetaData
+		String title = "Unfilter data in a column";
+		String pkqlCommand = "col.unfilter(c:col1);";
+		String description = "Unfilters the data in the selected column";
+		boolean showMenu = true;
+		boolean pinned = true;
+		super.setPKQLMetaData(title, pkqlCommand, description, showMenu, pinned);
+		super.setPKQLMetaDataInput();
 	}
-	
+
 	@Override
 	public Iterator process() {
 		// I need to take the col_def
 		// and put it into who am I
 		modExpression();
 		String nodeStr = (String)myStore.get(whoAmI);
-//		System.out.println("My Store on COL CSV " + myStore);
-		
+		//		System.out.println("My Store on COL CSV " + myStore);
+
 		ITableDataFrame frame = (ITableDataFrame) myStore.get("G");
-		
+
 		Vector<String> column = (Vector<String>) myStore.get(PKQLEnum.COL_DEF);
-		
+
 		for(String c : column) {
 			frame.unfilter(c);
 			myStore.put("STATUS", PKQLRunner.STATUS.SUCCESS);
 			myStore.put("FILTER_COLUMN", c);
 		}
-		
+
 		// update the data id so FE knows data has been changed
 		frame.updateDataId();
-		
+
 
 		return null;
 	}
-}
+	}
