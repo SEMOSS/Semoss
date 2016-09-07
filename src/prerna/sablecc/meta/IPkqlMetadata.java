31f37e87062fe62c273c766bba0c878eacb098a5
mahkhalil <mahkhalil@deloitte.com>
2016-09-07 15:08:55 +0000

    fixing error with casting within translation and fix in postfix naming
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@16217 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc/meta/IPkqlMetadata.java
+++ src/prerna/sablecc/meta/IPkqlMetadata.java
@@ -6,22 +6,45 @@ import prerna.ui.components.playsheets.datamakers.PKQLTransformation;
 
 public interface IPkqlMetadata {
 
-	String PKQL_NAME = "PKQL_NAME";
-	
 	/**
 	 * Returns the relevant information of the pkql
 	 * @return
 	 */
 	Map<String, Object> getMetadata();
 	
+	/**
+	 * Get the explanation of the PKQL executed, to be sent to the FE as a label
+	 * in the recipe
+	 * @return
+	 */
 	String getExplanation();
 	
+	/**
+	 * Set the pkql transformation that invoked this pkql expression
+	 * Used when we need to update the pkql recipe
+	 * Change must be made in pkql transformation since that is used for saving
+	 * @param trans
+	 */
 	void setInvokingPkqlTransformation(PKQLTransformation trans);
 	
+	/**
+	 * Get the pkql transformation that invoked this pkql expression
+	 * Used when we need to update the pkql recipe
+	 * Change must be made in pkql transformation since that is used for saving
+	 * @param trans
+	 */
 	PKQLTransformation getInvokingPkqlTransformation();
 	
+	/**
+	 * Set the pkql string associated with this metadata
+	 * @param pkqlStr
+	 */
 	void setPkqlStr(String pkqlStr);
 	
+	/**
+	 * Get the pkql string associated with this metadata
+	 * @return
+	 */
 	String getPkqlStr();
 	
 }
