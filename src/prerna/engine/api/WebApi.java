6803d6da617f02b7595cb7b183625564a06a0c0f
pvirdi <pvirdi@deloitte.com>
2016-09-12 21:26:04 +0000

    updated WebApi for Amazon and Import.io pkls
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@16349 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 5b9d47d06e1aff3810bbc6decc6d76bf1ec3f595

--- src/prerna/engine/api/WebApi.java
+++ src/prerna/engine/api/WebApi.java
@@ -3,70 +3,61 @@ package prerna.engine.api;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.Vector;
 
-import prerna.ds.H2.H2Frame;
 import prerna.ds.util.WebApiIterator;
 import prerna.sablecc.AbstractReactor;
 import prerna.sablecc.PKQLEnum;
 import prerna.sablecc.PKQLRunner;
 
 public class WebApi extends AbstractReactor{
+
+	public final static String URL = "url";
+	public final static String ITEM_SEARCH = "itemSearch";
+	public final static String ITEM_LOOKUP = "itemLookup";
+	
+	private Map<String, String> mapOptions = null;
 	
 	private String url = null;
 	private String itemSearch = null;
-	private String itemLookup = null;
-	
+	private String itemLookup = null;	
+
 	@Override
 	public Iterator process() {
-		// grab the engine from the my store
+
 		String engine = (String)myStore.get("ENGINE");
-		
-		// we currently have 2 different cases for the web api
-		if(engine.equalsIgnoreCase("ImportIO")){
-			if (myStore.get(PKQLEnum.G) instanceof H2Frame) {
-				if(myStore.containsKey("KEY_VALUE") && ((Map) ((Vector) myStore.get("KEY_VALUE")).get(0)).containsKey("url")){
-					this.url = (String) ((Map) ((Vector) myStore.get("KEY_VALUE")).get(0)).get("url");
-				} else {
-					System.out.println("Invalid PKQL: Missing URL. Required Syntax: data.import(api:ImportIO.Query({'url':'enter_your_url_here'})); ");
-					myStore.put("RESPONSE", "Error: Invalid PKQL: Missing URL. Required Syntax: data.import(api:ImportIO.Query({'url':'enter_your_url_here'}));");
-					myStore.put("STATUS", PKQLRunner.STATUS.ERROR);
-					return null;
-				}
-			}
-		} else if(engine.equalsIgnoreCase("AmazonProduct")){
-			if(myStore.get(PKQLEnum.G) instanceof H2Frame) {
-				if(myStore.containsKey("KEY_VALUE") && ((Map) ((Vector) myStore.get("KEY_VALUE")).get(0)).containsKey("itemSearch")){
-					this.itemSearch = (String) ((Map) ((Vector) myStore.get("KEY_VALUE")).get(0)).get("itemSearch");
-				} else if(myStore.containsKey("KEY_VALUE") && ((Map) ((Vector) myStore.get("KEY_VALUE")).get(0)).containsKey("itemLookup")){
-					this.itemLookup = (String) ((Map) ((Vector) myStore.get("KEY_VALUE")).get(0)).get("itemLookup");
-				} else {
-					System.out.println("Invalid PKQL: Required Syntax: data.import(api:AmazonProduct.Query({'itemSearch':'enter_search_keywords_here'})); OR data.import(api:AmazonProduct.Query({'itemLookup':'enter_item_ASIN_here'}));");
-					myStore.put("RESPONSE", "Error: Invalid PKQL: Required Syntax: data.import(api:AmazonProduct.Query({'itemSearch':'enter_search_keywords_here'})); OR data.import(api:AmazonProduct.Query({'itemLookup':'enter_item_ASIN_here'}));");
-					myStore.put("STATUS", PKQLRunner.STATUS.ERROR);
-					return null;
-				}
-			}
-		}
-		
-		String key = (this.url != null)? "URL" : (this.itemSearch != null)? "ITEM_SEARCH" : "ITEM_LOOKUP";
-		
 		Iterator<IHeadersDataRow> it = null;
+
 		try {
-			switch(key){
-			case "URL":	it = new WebApiIterator(url); break;
-			case "ITEM_SEARCH":	it = new WebApiIterator("ItemSearch", itemSearch); break;
-			case "ITEM_LOOKUP": it = new WebApiIterator("ItemLookup", itemLookup); break;
-			default: it = null; break;
+			if(engine.equalsIgnoreCase("ImportIO") || engine.equalsIgnoreCase("AmazonProduct")) {
+				if(myStore.get(PKQLEnum.MAP_OBJ) != null){
+					mapOptions = (Map<String, String>) myStore.get(PKQLEnum.MAP_OBJ);
+					for(String key : mapOptions.keySet()) {
+						switch(key){
+						case URL: url = mapOptions.get(URL); 
+								  it = new WebApiIterator(url); break;
+						case ITEM_SEARCH: itemSearch = mapOptions.get(ITEM_SEARCH); 
+										  it = new WebApiIterator("ItemSearch", itemSearch); break;
+						case ITEM_LOOKUP: itemLookup = mapOptions.get(ITEM_LOOKUP); 
+										  it = new WebApiIterator("ItemLookup", itemLookup); break;
+						}				
+					}
+					
+					this.put((String) getValue(PKQLEnum.API), it);
+					this.put("RESPONSE", "success");
+					this.put("STATUS", PKQLRunner.STATUS.SUCCESS);
+					return null;
+				}else{
+					String response = engine.equalsIgnoreCase("ImportIO") ? "Error: Invalid PKQL: Missing URL. Required Syntax: data.import(api:ImportIO.Query({'url':'enter_your_url_here'}));" : (engine.equalsIgnoreCase("AmazonProduct") ? "Error: Invalid PKQL: Required Syntax: data.import(api:AmazonProduct.Query({'itemSearch':'enter_search_keywords_here'})); OR data.import(api:AmazonProduct.Query({'itemLookup':'enter_item_ASIN_here'}));" : "Error: Invalid PKQL" );
+					myStore.put("RESPONSE", response);
+					myStore.put("STATUS", PKQLRunner.STATUS.ERROR);
+					return null;
+				}
+				
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}		
-		
-		this.put((String) getValue(PKQLEnum.API), it);
-		this.put("RESPONSE", "success");
-		this.put("STATUS", PKQLRunner.STATUS.SUCCESS);
-		
+		} catch (IOException e) {		
+			e.printStackTrace();			
+		}			
+
 		return null;
 	}
 }
