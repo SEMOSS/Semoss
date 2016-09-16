c7691075041a6a6f51a771a5b01a8801de5286cd
mahkhalil <mahkhalil@deloitte.com>
2016-09-16 15:27:44 +0000

    fixing filter on tinker frame, adding removeNode for tinker on baseJavaReactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@16465 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 7b433531a9e4d97d38b81c97a8c1c4094bcd871a

--- src/prerna/sablecc/BaseJavaReactor.java
+++ src/prerna/sablecc/BaseJavaReactor.java
@@ -1,132 +1,14 @@
 package prerna.sablecc;
 
 import java.sql.Connection;
-
-import prerna.algorithm.api.ITableDataFrame;
-import prerna.ds.H2.H2Frame;
-
-import java.sql.*;
-
-import prerna.engine.api.IEngine;
-
-import java.util.*;
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.Vector;
 
-import javax.script.ScriptContext;
-import javax.script.ScriptException;
-
-import org.apache.commons.configuration.BaseConfiguration;
-import org.apache.commons.configuration.Configuration;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.tinkerpop.gremlin.groovy.jsr223.GremlinGroovyScriptEngine;
-import org.apache.tinkerpop.gremlin.process.traversal.P;
-import org.apache.tinkerpop.gremlin.process.traversal.Path;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
-import org.apache.tinkerpop.gremlin.process.traversal.strategy.decoration.SubgraphStrategy;
-import org.apache.tinkerpop.gremlin.structure.Direction;
-import org.apache.tinkerpop.gremlin.structure.Edge;
-import org.apache.tinkerpop.gremlin.structure.Graph;
-import org.apache.tinkerpop.gremlin.structure.T;
-import org.apache.tinkerpop.gremlin.structure.Vertex;
-import org.apache.tinkerpop.gremlin.structure.io.Io.Builder;
-import org.apache.tinkerpop.gremlin.structure.io.IoCore;
-import org.apache.tinkerpop.gremlin.structure.io.IoRegistry;
-import org.apache.tinkerpop.gremlin.structure.io.gryo.GryoIo;
-import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory;
-import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
-
-import prerna.algorithm.api.IMatcher;
-import prerna.algorithm.api.IMetaData;
 import prerna.algorithm.api.ITableDataFrame;
-import prerna.engine.api.IConstructStatement;
-import prerna.engine.api.IEngine;
-import prerna.engine.api.ISelectStatement;
-import prerna.engine.api.ISelectWrapper;
-import prerna.om.SEMOSSEdge;
-import prerna.om.SEMOSSVertex;
-import prerna.om.TinkerGraphDataModel;
-import prerna.rdf.engine.wrappers.WrapperManager;
-import prerna.rdf.query.builder.GremlinBuilder;
-import prerna.sablecc.PKQLEnum;
-import prerna.sablecc.PKQLEnum.PKQLReactor;
-import prerna.ui.components.playsheets.datamakers.DataMakerComponent;
-import prerna.ui.components.playsheets.datamakers.ISEMOSSTransformation;
-import prerna.ui.components.playsheets.datamakers.JoinTransformation;
-import prerna.util.ArrayUtilityMethods;
-import prerna.util.Console;
-import prerna.util.Constants;
-import prerna.util.MyGraphIoRegistry;
-import prerna.util.Utility;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
-
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.rosuda.REngine.Rserve.RserveException;
-
-import prerna.algorithm.api.IMatcher;
-import prerna.algorithm.api.IMetaData;
-import prerna.algorithm.api.IMetaData.DATA_TYPES;
-import prerna.algorithm.api.ITableDataFrame;
-import prerna.ds.AbstractTableDataFrame;
 import prerna.ds.TinkerFrame;
-import prerna.ds.TinkerMetaData;
-import prerna.ds.TinkerMetaHelper;
-import prerna.ds.H2.H2Builder.Join;
+import prerna.ds.H2.H2Frame;
 import prerna.engine.api.IEngine;
-import prerna.engine.api.IHeadersDataRow;
-import prerna.engine.api.ISelectWrapper;
-import prerna.engine.impl.r.RRunner;
-import prerna.rdf.engine.wrappers.WrapperManager;
-import prerna.sablecc.PKQLEnum;
-import prerna.sablecc.PKQLEnum.PKQLReactor;
-import prerna.ui.components.playsheets.datamakers.DataMakerComponent;
-import prerna.ui.components.playsheets.datamakers.ISEMOSSTransformation;
-import prerna.ui.components.playsheets.datamakers.JoinTransformation;
-import prerna.util.ArrayUtilityMethods;
-import prerna.util.Constants;
-import prerna.util.Utility;
+import prerna.util.Console;
 
 
 public abstract class BaseJavaReactor extends AbstractReactor{
@@ -308,6 +190,20 @@ public abstract class BaseJavaReactor extends AbstractReactor{
 			Object degree = ((TinkerFrame)dataframe).degree(type, data);
 			String output = "Degrees for  " + data + ":" + degree;
 			System.out.println(output);
+		}
+		java.lang.System.setSecurityManager(reactorManager);
+	}
+	
+	public void removeNode(String type, String data) {
+		java.lang.System.setSecurityManager(curManager);
+		if(dataframe instanceof TinkerFrame)
+		{
+			List<Object> removeList = new Vector<Object>();
+			removeList.add(data);
+			((TinkerFrame)dataframe).remove(type, removeList);
+			String output = "Removed nodes for  " + data + " with values " + removeList;
+			System.out.println(output);
+			dataframe.updateDataId();
 		}
 		java.lang.System.setSecurityManager(reactorManager);
 	}
