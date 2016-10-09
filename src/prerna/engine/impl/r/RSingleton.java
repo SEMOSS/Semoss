875e1c1e95d632c430ab98c93cbee74ab95a76cb
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2016-10-09 18:56:07 +0000

    H2Builder - Adjusting path on connectframe for NIO, minor enhancements RSingleton - Ability to reinitiate a master connection on R BaseJavaReactor - Whole bunch of cleanup methods that utilize R - synchronizeGridToR - synchronizes the grid to R. You could specify columns as CSV on it and it will only pull those and synchronize - synchronizeGridFromR - Given the framename, and a table name - this will write the table back to the H2. I have not yet added columns. But shortly - splitColumn - Splits a column based on a separator. Optional - you can specify if you want to drop the column that you are splitting (defaults to false), as well as if you want to replace your existing table  (Defaults to True) - Unpivot - Takes the columns and unpivots them. Optionally, takes a different frame name - GetColumnCount - Gives you distinct elements in a column and their counts - Get Type Count - Gives you a different type of elements and their respective counts such as character - 50, etc. - Pivot - takes the column specified as key value and then pivots it back as a column. Optionally can specify if to replace the table or not - Defaults to True - Get Column Names - Gives you the current column names - join columns - takes input as framename, new column name, separator, list of columns and then joins them to create a new column - reconnectR - takesa  port and reconnects the master connection for R using this port. Incase your 6311 is stale. Use this call to reinitiate it - setH2Driver - Sets the path so we dont need to go into the source code to change it every time.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@16875 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d2d58303ec0a3adfd2a954791facd7e014721d84

--- src/prerna/engine/impl/r/RSingleton.java
+++ src/prerna/engine/impl/r/RSingleton.java
@@ -1,10 +1,15 @@
 package prerna.engine.impl.r;
 
+import java.util.Hashtable;
+
 import org.rosuda.REngine.Rserve.RConnection;
 
 public class RSingleton {
 	
 	private static RConnection rcon = null;
+	static int port = 6311;
+	static Hashtable <Integer, RConnection> portToCon = new Hashtable<Integer, RConnection>(); // RServe is not allowing me to inspect the port so I have to do this.. sorry
+	
 	
 	private RSingleton()
 	{
@@ -13,17 +18,37 @@ public class RSingleton {
 	
 	public static RConnection getConnection()
 	{
-		if(rcon == null || (rcon != null && !rcon.isConnected())) 
+		if(rcon == null)
+			return getConnection("127.0.0.1", 6311);
+		return rcon;
+	}
+	
+	public static RConnection getConnection(int port)
+	{
+		return getConnection("127.0.0.1", port);		
+	}
+	
+	public static RConnection getConnection(String host, int port)
+	{
+		// this basically tries to do the same as get connection with a port
+		if(portToCon.containsKey(port))
+			rcon = portToCon.get(port);
+		
+		else
 		{
+			System.out.println("Making a master connection now on port" + port);;
 			try {
 				
-				rcon = new RConnection();
+				rcon = new RConnection(host, port);
+				portToCon.put(port, rcon);
 				
 			}catch(Exception ex)
 			{
 				ex.printStackTrace();
 			}
+			
 		}
+		
 		return rcon;
 	}
 	
