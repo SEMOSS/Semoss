bb0befbfeff7338ccc9db802aec41ed2133380ba
mahkhalil <mahkhalil@deloitte.com>
2016-10-10 14:58:39 +0000

    adding base r frame code
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@16879 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c0151bba36bf67d0b721214ff9da49ae11f3acf8

---
+++ src/prerna/sablecc/RCsvApiReactor.java
@@ -0,0 +1,63 @@
+package prerna.sablecc;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import prerna.ds.util.FileIterator;
+import prerna.engine.api.IHeadersDataRow;
+import prerna.engine.impl.rdf.AbstractApiReactor;
+import prerna.sablecc.meta.FilePkqlMetadata;
+import prerna.sablecc.meta.IPkqlMetadata;
+
+public class RCsvApiReactor extends AbstractApiReactor {
+
+	// TODO: should modify this to be a bit more of a unique name so it does not
+	// relate to a column name
+	public final static String FILE_KEY = "file";
+	public final static String DATA_TYPE_MAP = "dataTypeMap";
+
+	private String fileName;
+	private Map<String, String> dataTypeMap;
+	
+	@Override
+	public Iterator<IHeadersDataRow> process() {
+
+		// the mapOptions stores all the information being passed from the user
+		// this will contain the fileName and the data types from each column
+		dataTypeMap = new Hashtable<String, String>();
+		
+		this.mapOptions = (Map<Object, Object>) myStore.get(PKQLEnum.MAP_OBJ);
+		for(Object key : this.mapOptions.keySet()) {
+			if(key.equals(FILE_KEY)) {
+				fileName = (String)this.mapOptions.get(FILE_KEY);
+			} else {
+				dataTypeMap.put(key.toString(), (String)this.mapOptions.get(key));
+			}
+		}
+		
+		// pass in delimiter as a comma and return the FileIterator which uses the QS (if not empty) to 
+		// to determine what selectors to send
+		
+		// the qs is passed from AbstractApiReactor
+		this.put((String) getValue(PKQLEnum.API), new FileIterator(fileName, ',', this.qs, dataTypeMap));
+		this.put("RESPONSE", "success");
+		this.put("STATUS", PKQLRunner.STATUS.SUCCESS);
+		
+		return null;
+	}
+	
+	public IPkqlMetadata getPkqlMetadata() {
+		FilePkqlMetadata fileData = new FilePkqlMetadata();
+		fileData.setFileLoc(this.fileName);
+		fileData.setDataMap(this.dataTypeMap);
+		fileData.setSelectors((Vector<String>) getValue(PKQLEnum.COL_CSV));
+		fileData.setTableJoin((List<Map<String, Object>>) getValue(PKQLEnum.TABLE_JOINS));
+		fileData.setPkqlStr((String) getValue(PKQLEnum.API));
+		
+		return fileData;
+	}
+	
+}
