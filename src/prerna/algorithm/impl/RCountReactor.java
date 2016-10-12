8d65b71e5ceb295647196bffbd310fd9979ac4a3
mahkhalil <mahkhalil@deloitte.com>
2016-10-12 13:26:05 +0000

    moving count to data.table specific syntax
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@16922 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/algorithm/impl/RCountReactor.java
+++ src/prerna/algorithm/impl/RCountReactor.java
@@ -1,36 +1,37 @@
 package prerna.algorithm.impl;
 
-import java.util.List;
-
-import prerna.ds.R.RSyntaxHelper;
-
-public class RCountReactor extends RBaseReducer {
+public class RCountReactor extends RBasicMathReactor {
 
 	/*
-	 * This class is not like the other math reactors
-	 * This is because the execution of the method 
-	 * does not follow the same format as count requires 2 function calls
+	 * Only need to set the Math Routine
+	 * Everything else is handled by inheritance
 	 */
 	
 	public RCountReactor() {
-		
-	}
-
-	public String process(String tableName, String column) {
-		// generate a string similar to 
-		// datatable[, sum(na.omit(distance))]
-		StringBuilder builder = new StringBuilder();
-		builder.append(tableName).append("[ , as.numeric(length(unique(na.omit(").append(column).append("))))]");
-		return builder.toString();
+		this.setMathRoutine("uniqueN");
 	}
 	
-	public String processGroupBy(String tableName, String column, List<String> groupByCols) {
-		// generate a string similar to 
-		// datatable[, sum(na.omit(distance)), by =c("year")]
-		StringBuilder builder = new StringBuilder();
-		builder.append(tableName).append("[ , as.numeric(length(unique(na.omit(").append(column).append(")))), by = ")
-			.append( RSyntaxHelper.createStringRColVec(groupByCols.toArray()) ).append(" ]");
-		return builder.toString();
-	}
+//	/*
+//	 * This class is not like the other math reactors
+//	 * This is because the execution of the method 
+//	 * does not follow the same format as count requires 2 function calls
+//	 */
+//	
+//	public String process(String tableName, String column) {
+//		// generate a string similar to 
+//		// datatable[, sum(na.omit(distance))]
+//		StringBuilder builder = new StringBuilder();
+//		builder.append(tableName).append("[ , as.numeric(length(unique(na.omit(").append(column).append("))))]");
+//		return builder.toString();
+//	}
+//	
+//	public String processGroupBy(String tableName, String column, List<String> groupByCols) {
+//		// generate a string similar to 
+//		// datatable[, sum(na.omit(distance)), by =c("year")]
+//		StringBuilder builder = new StringBuilder();
+//		builder.append(tableName).append("[ , as.numeric(length(unique(na.omit(").append(column).append(")))), by = ")
+//			.append( RSyntaxHelper.createStringRColVec(groupByCols.toArray()) ).append(" ]");
+//		return builder.toString();
+//	}
 
 }
