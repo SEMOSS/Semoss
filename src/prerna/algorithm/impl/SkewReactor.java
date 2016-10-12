39d9d04e450cd85b92864163dca98fbd48012fc9
clsunny <clsunny@deloitte.com>
2016-10-12 13:09:57 +0000

    advanced and excel functions
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@16919 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/algorithm/impl/SkewReactor.java
@@ -0,0 +1,74 @@
+package prerna.algorithm.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Vector;
+import org.apache.commons.math3.util.FastMath;
+import org.apache.commons.math3.util.MathUtils;
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics.*;
+
+import prerna.sablecc.MathReactor;
+
+public class SkewReactor extends BaseReducerReactor   {
+	@Override
+	public Object reduce() {
+		int count = 0;
+		double[] sumAndSumOfSquares = new double[2];; // index 0 for sum, index 1 for sum squared
+		double StdDev = 0.0;
+		double Variance=0.0;
+		 double accum3 = 0.0;
+		 double mean=0.0;
+		 double d = 0.0;
+		 
+		while(inputIterator.hasNext() && !errored) {
+			ArrayList dec = (ArrayList)getNextValue();
+			//System.out.println(dec.get(0));
+			mean=((Number)dec.get(1)).doubleValue();
+			 d = ((Number)dec.get(0)).doubleValue()-mean;
+             accum3 += d * d * d;
+           //  System.out.println("accum value is ");
+             //System.out.println(accum3);
+             //System.out.println("d value is");
+             //System.out.println(d);
+			if(dec.get(0) instanceof Number) {
+				sumAndSumOfSquares[0] += ((Number)dec.get(0)).doubleValue();
+				sumAndSumOfSquares[1] += (Math.pow(((Number)dec.get(0)).doubleValue(), 2));
+				count++;
+			}
+		}
+		double sum = sumAndSumOfSquares[0];
+		double sumOfSquares = sumAndSumOfSquares[1];
+		double average = sum/count;
+		StdDev = Math.sqrt(((count * average * average) - (2 * average * sum) + sumOfSquares)/count);
+		//System.out.println("stdDev is");
+		//System.out.println(StdDev);
+		Variance=StdDev*StdDev;
+		//System.out.println(Variance);
+		
+
+	
+		while(inputIterator.hasNext() && !errored)
+		{
+			ArrayList dec = (ArrayList)getNextValue();
+
+			
+		}
+		accum3 /= Variance * FastMath.sqrt(Variance);
+		 double n0 = count;
+
+         // Calculate skewness
+        double skew = (n0 / ((n0 - 1) * (n0 - 2))) * accum3;
+        System.out.println("skew value is");
+        System.out.println(skew);
+		return skew;
+
+}
+
+	@Override
+	public HashMap<HashMap<Object, Object>, Object> reduceGroupBy(Vector<String> groupBys,
+			Vector<String> processedColumns, String[] columnsArray, Iterator it) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ src/prerna/algorithm/impl/VLookupReactor.java
@@ -0,0 +1,98 @@
+package prerna.algorithm.impl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Vector;
+
+import prerna.algorithm.api.ITableDataFrame;
+import prerna.sablecc.PKQLEnum;
+
+public class VLookupReactor extends BaseReducerReactor {
+	
+	@Override
+	public Object reduce() {
+		String output=null;
+		String inputS=null;
+		double tp;
+		double tp1;
+		double tp2=0;
+		int rfound=0;
+		int iInit=0;
+		int sValPos=1;
+	    ArrayList<String> sArray  = new ArrayList<String>();
+	    ArrayList<String> rArray  = new ArrayList<String>();
+		//Vector<String> columns = (Vector <String>) myStore.get(PKQLEnum.COL_DEF);
+		Map<String, Object> options = (Map<String, Object>) myStore.get(PKQLEnum.MATH_PARAM);
+		int valPosition = Integer.parseInt(options.get("CONDITION1") + "");
+		int colPosition = Integer.parseInt(options.get("CONDITION2") + "");		
+		boolean exact_approx =Boolean.valueOf(options.get("CONDITION3") + "");
+		int startRow = Integer.parseInt(options.get("CONDITION4") + "");	
+		int endRow = Integer.parseInt(options.get("CONDITION5") + "");	
+		while(inputIterator.hasNext() && !errored)
+		{
+			ArrayList dec = (ArrayList)getNextValue();
+			//need to check index
+			if(sValPos==valPosition){
+			inputS =dec.get(0).toString();
+			}
+			sValPos +=1;
+			sArray.add(dec.get(1).toString());
+			rArray.add(dec.get(colPosition).toString());			
+			
+		}
+		sValPos=1;
+		int length=sArray.size();
+		for (int i = 0; ((i < length)&&rfound==0) ; i++)
+		{
+			System.out.println(sArray.get(i).toString());
+			System.out.println(rArray.get(i).toString());
+			if (i>=(startRow-1) && i<=(endRow-1)  )
+			{
+					if((inputS).equals(sArray.get(i).toString()) && exact_approx==false)
+						{
+							output=rArray.get(i).toString();
+							rfound=1;
+							System.out.println(output);
+							
+						}
+				
+					else if(exact_approx==true)
+						{   
+							tp=Double.parseDouble(sArray.get(i).toString()) ;
+							tp1=Math.abs(tp-Double.parseDouble(inputS));
+														
+							if (iInit==0)
+							{output=rArray.get(i).toString();;
+							iInit=1;
+							tp2=tp1 ;								
+							}
+							else if (tp1<tp2)
+							{tp2=tp1;
+							output=rArray.get(i).toString();
+							}
+					    }
+			}
+			
+			
+		}
+	
+		if(output instanceof String) {
+			output = "\"" + output + "\"";
+		}
+		
+		System.out.println(output);
+		return output;
+	}
+
+	@Override
+	public HashMap<HashMap<Object, Object>, Object> reduceGroupBy(Vector<String> groupBys,
+			Vector<String> processedColumns, String[] columnsArray, Iterator it) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+//SearchValue,resultposition,T/F,StartRow,EndRow
+//m:VLookup([c:CYREVENUE,c:PYREVENUE,c:CUSTOMER],{"CONDITION1":5,"CONDITION2":3,"CONDITION3":"FALSE","CONDITION4":5,"CONDITION5":10});
+//m: VLookup ( [ c: PYREVENUE , c: CYREVENUE ] , { "CONDITION1" : 12 , "CONDITION2" : 2 , "CONDITION3" : "TRUE" } ) ; 
