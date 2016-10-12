b6aaee514d45d4800d354ad2b640b1cf738b9fd4
clsunny <clsunny@deloitte.com>
2016-10-12 13:09:57 +0000

    advanced and excel functions
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@16919 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 651e0bfa1ce067a0aa88b93eb59c0ceda7696ffd

---
+++ src/prerna/algorithm/impl/MIDReactor.java
@@ -0,0 +1,35 @@
+package prerna.algorithm.impl;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Vector;
+
+import prerna.algorithm.api.ITableDataFrame;
+import prerna.ds.ExpressionIterator;
+import prerna.sablecc.MathReactor;
+import prerna.sablecc.PKQLEnum;
+import prerna.sablecc.PKQLRunner.STATUS;
+
+public class MIDReactor   extends MathReactor { // TODO create BaseMapperReactor once more mapping algorithms have been added
+	
+	@Override
+	public Iterator process() {
+		modExpression();
+		Vector <String> columns = (Vector <String>)myStore.get(PKQLEnum.COL_DEF);
+		String[] columnsArray = convertVectorToArray(columns);
+		Map<String, Object> options = (Map<String, Object>) myStore.get(PKQLEnum.MATH_PARAM);
+		int start_position = Integer.parseInt(options.get("CONDITION1") + "");
+		int number_of_characters = Integer.parseInt(options.get("CONDITION2") + "");
+		String script = myStore.get("MOD_" + whoAmI).toString().replace(" ]", "");
+		Iterator iterator = getTinkerData(columns, (ITableDataFrame)myStore.get("G"), false);
+	    String str=script+".drop("+start_position+").take("+number_of_characters+")]";
+		String nodeStr = myStore.get(whoAmI).toString();
+		
+		ExpressionIterator expIt = new ExpressionIterator(iterator, columnsArray,str );
+		myStore.put(nodeStr, expIt);
+		myStore.put("STATUS",STATUS.SUCCESS);
+		
+		return expIt;
+	}
+	
+}
