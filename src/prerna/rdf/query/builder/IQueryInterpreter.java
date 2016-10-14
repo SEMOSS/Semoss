c2b566a16023330f20969f27e3fae76c39e032e4
karverma <karverma@deloitte.com>
2016-10-14 16:39:09 +0000

    Allowing for search comparator in SQL Interpreter, constructing WHERE col LIKE <searchTerm> clause
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@16966 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e5fd3118a2e28cbdceb2d53a3f428054cf39645d

--- src/prerna/rdf/query/builder/IQueryInterpreter.java
+++ src/prerna/rdf/query/builder/IQueryInterpreter.java
@@ -3,6 +3,8 @@ package prerna.rdf.query.builder;
 import prerna.ds.QueryStruct;
 
 public interface IQueryInterpreter {
+	
+	static final String SEARCH_COMPARATOR = "like";
 
 	void setQueryStruct(QueryStruct qs);
 
