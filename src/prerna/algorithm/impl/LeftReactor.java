0404b65026c2cc97148685f3843b7b3af07aa8e8
clsunny <clsunny@deloitte.com>
2016-10-17 10:34:43 +0000

    fixed bug in left function
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@16972 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2f887bc7cc875f400c98775e7d0a5710dc7b1a02

--- src/prerna/algorithm/impl/LeftReactor.java
+++ src/prerna/algorithm/impl/LeftReactor.java
@@ -1,6 +1,7 @@
 package prerna.algorithm.impl;
 
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Vector;
 
 import prerna.algorithm.api.ITableDataFrame;
@@ -16,10 +17,11 @@ public class LeftReactor  extends MathReactor { // TODO create BaseMapperReactor
 		modExpression();
 		Vector <String> columns = (Vector <String>)myStore.get(PKQLEnum.COL_DEF);
 		String[] columnsArray = convertVectorToArray(columns);
+		Map<String, Object> options = (Map<String, Object>) myStore.get(PKQLEnum.MATH_PARAM);
 		String script = myStore.get("MOD_" + whoAmI).toString();
+		String indx = (options.get("CONDITION1") + "").toString();
 		Iterator iterator = getTinkerData(columns, (ITableDataFrame)myStore.get("G"), false);
-		String indx = myStore.get("MATH_PARAM").toString().replace("{\"CONDITION1\"=", "").replace("}", "");
-	    String str=script.replace(" ]", "")+".take("+indx+")]";
+		 String str=script.replace(" ]", "")+".take("+indx+")]";
 		String nodeStr = myStore.get(whoAmI).toString();
 		
 		ExpressionIterator expIt = new ExpressionIterator(iterator, columnsArray,str );
