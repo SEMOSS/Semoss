a80760b445c99b2f9c793a7813bf80ed97a23b08
mahkhalil <mahkhalil@deloitte.com>
2016-10-19 14:22:44 +0000

    renaming sql expression class.  modifying scripts to allow for multiplication, etc. into the scripts
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@17036 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/algorithm/impl/SqlAbsoluteReactor.java
+++ src/prerna/algorithm/impl/SqlAbsoluteReactor.java
@@ -1,19 +1,28 @@
 package prerna.algorithm.impl;
 
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 import java.util.Vector;
 
 import prerna.ds.H2.H2Frame;
+import prerna.sablecc.H2SqlExpressionIterator;
 import prerna.sablecc.MathReactor;
 import prerna.sablecc.PKQLEnum;
 import prerna.sablecc.PKQLRunner.STATUS;
-import prerna.sablecc.SqlExpressionIterator;
 import prerna.util.Utility;
 
 public class SqlAbsoluteReactor extends MathReactor {
 
 	@Override
 	public Iterator process() {
+		String nodeStr = myStore.get(whoAmI).toString();
+
+		// if this is wrapping an existing expression iterator
+		if(myStore.get(nodeStr) instanceof H2SqlExpressionIterator) {
+			((H2SqlExpressionIterator) myStore.get(nodeStr)).close();
+		}
+		
 		// get the frame
 		H2Frame frame = (H2Frame) myStore.get("G");
 				
@@ -24,15 +33,19 @@ public class SqlAbsoluteReactor extends MathReactor {
 		String script = myStore.get("MOD_" + whoAmI).toString();
 		script = script.replace("[", "").replace("]", "");
 
+		// since multiple math routines can be added together
+		// need to get a unique set of values used in the join
 		Vector<String> columns = (Vector <String>) myStore.get(PKQLEnum.COL_DEF);
+		Set<String> joins = new HashSet<String>();
+		joins.addAll(columns);
+
 		String name = "ABS_" + Utility.getRandomString(6);
 
 		// sql script is ABS around the column
 		String expression = "ABS(" + script + ")";
-		SqlExpressionIterator it = new SqlExpressionIterator(frame, expression, name, convertVectorToArray(columns));
+		H2SqlExpressionIterator it = new H2SqlExpressionIterator(frame, expression, name, joins.toArray(new String[]{}));
 		
-		String pkql = myStore.get(whoAmI).toString();
-		myStore.put(pkql, it);
+		myStore.put(nodeStr, it);
 		myStore.put("STATUS",STATUS.SUCCESS);
 			
 		return it;
