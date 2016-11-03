5b4b7b10e502974c132a0893dc3445da0baaffde
mahkhalil <mahkhalil@deloitte.com>
2016-11-03 17:43:24 +0000

    update for distinct math selector to use math selector
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@17261 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6bea92f3dc2294b1fff0afe7bdb5fa509406a596

--- src/prerna/sablecc/expressions/sql/builder/SqlDistinctMathSelector.java
+++ src/prerna/sablecc/expressions/sql/builder/SqlDistinctMathSelector.java
@@ -1,49 +1,21 @@
 package prerna.sablecc.expressions.sql.builder;
 
-import java.util.List;
-
 import prerna.sablecc.expressions.IExpressionSelector;
 
-public class SqlDistinctMathSelector implements IExpressionSelector {
+public class SqlDistinctMathSelector extends SqlMathSelector {
 
-	private IExpressionSelector selector;
-	private String math;
-	private String pkqlMath;
-	
 	/*
 	 * Create a math routine around an existing selector
 	 */
 	
 	public SqlDistinctMathSelector(IExpressionSelector selector, String math, String pkqlMath) {
-		this.selector = selector;
-		this.math = math;
-		this.pkqlMath = pkqlMath;
-	}
-	
-	public String getPkqlMath() {
-		return this.pkqlMath;
-	}
-	
-	public String getMath() {
-		return this.math;
+		super(selector, math, pkqlMath);
 	}
 	
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append(math).append("(DISTINCT ").append(selector.toString()).append(")");
-		return builder.toString();
-	}
-	
-	@Override
-	public List<String> getTableColumns() {
-		return this.selector.getTableColumns();
-	}
-
-	@Override
-	public String getName() {
-		StringBuilder builder = new StringBuilder();
-		builder.append(pkqlMath).append("_").append(selector.getName());
 		return builder.toString();
 	}
 	
