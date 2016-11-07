893331596c02459f766c894d4be82fed9260605e
soursinha <soursinha@deloitte.com>
2016-11-07 09:24:20 +0000

    updated Basic stats reactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@17309 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8ae9d889c5e2bd7e60a329feff259d75e612e637

--- src/prerna/algorithm/impl/BasicStatsReactor.java
+++ src/prerna/algorithm/impl/BasicStatsReactor.java
@@ -1,6 +1,7 @@
 package prerna.algorithm.impl;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -21,14 +22,14 @@ public class BasicStatsReactor extends BaseReducerReactor {
 		double output = 0.0;
 		int count = 0;
 		int positiveCount =0, negativeCount =0, zeroCount =0;
+		ArrayList<Double> values = new ArrayList<Double>();
 		
 		while(inputIterator.hasNext() && !errored)
 		{
 			ArrayList dec = (ArrayList)getNextValue();
 				if(dec.get(0) instanceof Number) {
 					stats.addValue(((Number)dec.get(0)).doubleValue());
-					//output += ((Number)dec.get(0)).doubleValue();
-					//count++;
+					values.add(((Number)dec.get(0)).doubleValue());
 					if (((Number)dec.get(0)).doubleValue() > 0)
 						positiveCount++;
 					else if(((Number)dec.get(0)).doubleValue() < 0)
@@ -38,10 +39,40 @@ public class BasicStatsReactor extends BaseReducerReactor {
 				}
 		}
 		
+		//Calculate Median
+		Collections.sort(values);
+		int totalCount = values.size();
+		double median;
+		if (totalCount % 2 == 0) { 
+			median = values.get(totalCount/2);
+		}
+		else {
+			median = (values.get(totalCount/2) + values.get((int)Math.ceil(totalCount/2))) / 2.0;
+		}
+		
+		//Calcuate Mode
+		HashMap<Double,Integer> hm=new HashMap<Double,Integer>();
+		int max=1;
+	    double mode = 0;
+	    for(int i=0;i<values.size();i++)
+	        {
+	            if(hm.get(values.get(i))!=null)
+	            {int counthm=hm.get(values.get(i));
+	            counthm=counthm+1;
+	            hm.put(values.get(i),counthm);
+	            if(counthm>max)
+	                {max=counthm;
+	                 mode=values.get(i);}
+	            }
+	            else
+	            {hm.put(values.get(i),1);}
+	        }
+		
+		//Rest of Basic Stats
 		double mean = stats.getMean();
 		double std = stats.getStandardDeviation();
 		double min = stats.getMin();
-		double max = stats.getMax();
+		double maximum = stats.getMax();
 		double variance = stats.getVariance();
 		double geometricMean = stats.getGeometricMean();
 		double sum = stats.getSum();
@@ -51,7 +82,7 @@ public class BasicStatsReactor extends BaseReducerReactor {
 		returnData.put("mean", mean);
 		returnData.put("standardDeviation", std);
 		returnData.put("min", min);
-		returnData.put("max", max);
+		returnData.put("max", maximum);
 		returnData.put("variance", variance);
 		returnData.put("geometricMean", geometricMean);
 		returnData.put("sum", sum);
@@ -59,9 +90,12 @@ public class BasicStatsReactor extends BaseReducerReactor {
 		returnData.put("negativeCount", negativeCount);
 		returnData.put("zeroCount", zeroCount);
 		returnData.put("sumOfSquare", sumOfSquare);
+		returnData.put("median", median);
+		returnData.put("mode", mode);
 		
-		
-		myStore.put("ADDITIONAL_INFO", returnData);
+		HashMap<String,Object> basicStats = new HashMap<>();
+		basicStats.put("basicStats", returnData);
+		myStore.put("ADDITIONAL_INFO", basicStats);
 		myStore.put("STATUS", STATUS.SUCCESS);
 		System.out.println(output);
 		return returnData;
