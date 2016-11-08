d8671c248fdb236dfbb0fb368463fc3501237ad2
mahkhalil <mahkhalil@deloitte.com>
2016-11-08 21:03:57 +0000

    adding sql expression fixes
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@17355 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 52e12369938a34ddda1bb165741e116d6c9beb14

---
+++ src/prerna/sablecc/expressions/sql/SqlDistinctGroupConcatReactor.java
@@ -0,0 +1,27 @@
+package prerna.sablecc.expressions.sql;
+
+import java.util.Map;
+
+import prerna.ds.h2.H2Frame;
+import prerna.sablecc.PKQLEnum;
+import prerna.sablecc.expressions.IExpressionSelector;
+import prerna.sablecc.expressions.sql.builder.SqlDistinctGroupConcat;
+import prerna.sablecc.expressions.sql.builder.SqlExpressionBuilder;
+
+public class SqlDistinctGroupConcatReactor extends AbstractH2SqlBaseReducer {
+
+	
+	@Override
+	public SqlExpressionBuilder process(H2Frame frame, SqlExpressionBuilder builder) {
+		Map<String, Object> options = (Map<String, Object>) myStore.get(PKQLEnum.MATH_PARAM);
+		String separator = null;
+		if(options != null) {
+			separator = (String) options.get("SEPARATOR");
+		}
+		IExpressionSelector previousSelector = builder.getLastSelector();
+		SqlDistinctGroupConcat newSelector = new SqlDistinctGroupConcat(previousSelector, separator);
+		builder.replaceSelector(previousSelector, newSelector);
+		return builder;
+	}
+	
+}
