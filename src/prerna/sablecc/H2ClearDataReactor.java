1631ab5f3757e0f8b4a03312062cb67bfea18c32
rluthar <rluthar@deloitte.com>
2016-11-14 22:26:40 +0000

    adding reactor to clear an h2frame and replace with a fresh copy, will clear the parent dashboard if necessary
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@17508 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 4a29700c1cf79edb649cf1078436d702b94e56ab

---
+++ src/prerna/sablecc/H2ClearDataReactor.java
@@ -0,0 +1,31 @@
+package prerna.sablecc;
+
+import java.util.Iterator;
+
+import prerna.algorithm.api.ITableDataFrame;
+import prerna.ds.h2.H2Frame;
+import prerna.om.Dashboard;
+import prerna.util.Utility;
+
+public class H2ClearDataReactor extends AbstractReactor {
+
+	public H2ClearDataReactor() {
+		String[] thisReacts = {}; 
+		super.whatIReactTo = thisReacts;
+		super.whoAmI = PKQLEnum.CLEAR_DATA;
+	}
+	
+	@Override
+	public Iterator process() {
+		H2Frame frame = (H2Frame)myStore.get(PKQLEnum.G);
+		if(frame.isJoined()) {
+			Dashboard dashboard = frame.getJoiner().getDashboard();
+			dashboard.dropDashboard();
+			dashboard.clearData();
+		}
+		ITableDataFrame newFrame = (ITableDataFrame) Utility.getDataMaker(null, frame.getClass().getSimpleName());
+		newFrame.setUserId(frame.getUserId());
+		myStore.put(PKQLEnum.G, newFrame);
+		return null;
+	}
+}
