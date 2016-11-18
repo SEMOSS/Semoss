80ddeb82a8414afb49a50898327757b9289fb52c
rluthar <rluthar@deloitte.com>
2016-11-18 18:15:57 +0000

    BUGFIX ::: preserving filters of insights (pre-join) after doing a dashboard join
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@17603 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ds/RdbmsFrameUtility.java
+++ src/prerna/ds/RdbmsFrameUtility.java
@@ -6,12 +6,32 @@ public class RdbmsFrameUtility {
 		return value.replace("'", "''");
 	}
 	
-	protected static String cleanHeader(String header) {
+	public static String cleanHeader(String header) {
 		header = header.replaceAll("[#%!&()@#$'./-]*\"*", ""); // replace all the useless shit in one go
 		header = header.replaceAll("\\s+", "_");
 		header = header.replaceAll(",", "_");
 		if (Character.isDigit(header.charAt(0)))
 			header = "c_" + header;
 		return header;
+	}
+	
+	public static String getComparatorStringValue(AbstractTableDataFrame.Comparator comparator) {
+		
+		if(AbstractTableDataFrame.Comparator.EQUAL.equals(comparator)) {
+			return "=";
+		} else if(AbstractTableDataFrame.Comparator.GREATER_THAN.equals(comparator)) {
+			return ">";
+		} else if(AbstractTableDataFrame.Comparator.GREATER_THAN_EQUAL.equals(comparator)) {
+			return ">=";
+		} else if(AbstractTableDataFrame.Comparator.LESS_THAN.equals(comparator)) {
+			return "<";
+		} else if(AbstractTableDataFrame.Comparator.LESS_THAN_EQUAL.equals(comparator)) {
+			return "<=";
+		} else if(AbstractTableDataFrame.Comparator.NOT_EQUAL.equals(comparator)) {
+			return "!=";
+		} else {
+			return null;
+		}
+		
 	}
 }
