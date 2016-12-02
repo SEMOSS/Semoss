01e0153aac449cdd3629f73c5495f314e3f483ed
rluthar <rluthar@deloitte.com>
2016-12-02 17:52:17 +0000

    adding panel.handle echo pkql
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@17684 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e137b5f6ab9c6ef83f1e0cf46a0a65401cf7c3fc

--- src/prerna/sablecc/analysis/ReversedDepthFirstAdapter.java
+++ src/prerna/sablecc/analysis/ReversedDepthFirstAdapter.java
@@ -1000,6 +1000,27 @@ public class ReversedDepthFirstAdapter extends AnalysisAdapter
         outAOutputInsightPanelop(node);
     }
 
+    public void inAPanelHandlePanelop(APanelHandlePanelop node)
+    {
+        defaultIn(node);
+    }
+
+    public void outAPanelHandlePanelop(APanelHandlePanelop node)
+    {
+        defaultOut(node);
+    }
+
+    @Override
+    public void caseAPanelHandlePanelop(APanelHandlePanelop node)
+    {
+        inAPanelHandlePanelop(node);
+        if(node.getPanelHandle() != null)
+        {
+            node.getPanelHandle().apply(this);
+        }
+        outAPanelHandlePanelop(node);
+    }
+
     public void inADatatypeDataop(ADatatypeDataop node)
     {
         defaultIn(node);
@@ -1666,6 +1687,39 @@ public class ReversedDepthFirstAdapter extends AnalysisAdapter
             node.getPanelclose().apply(this);
         }
         outAPanelClose(node);
+    }
+
+    public void inAPanelHandle(APanelHandle node)
+    {
+        defaultIn(node);
+    }
+
+    public void outAPanelHandle(APanelHandle node)
+    {
+        defaultOut(node);
+    }
+
+    @Override
+    public void caseAPanelHandle(APanelHandle node)
+    {
+        inAPanelHandle(node);
+        if(node.getRPar() != null)
+        {
+            node.getRPar().apply(this);
+        }
+        if(node.getHandlekeys() != null)
+        {
+            node.getHandlekeys().apply(this);
+        }
+        if(node.getLPar() != null)
+        {
+            node.getLPar().apply(this);
+        }
+        if(node.getPanelhandle() != null)
+        {
+            node.getPanelhandle().apply(this);
+        }
+        outAPanelHandle(node);
     }
 
     public void inADataFrame(ADataFrame node)
