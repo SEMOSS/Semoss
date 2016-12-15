6c3b719ddc625203032f830a9b14612ece567302
mahkhalil <mahkhalil@deloitte.com>
2016-12-15 00:24:32 +0000

    UPDATE ::: Modification to push all the metadata processing outside of the frame specific import data reactors. UPDATE ::: Fix to be able to join raw api's together with table joins within the data.import UPDATE ::: Allowing for SPARQL queries to be part of raw api but will only load columns as strings
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@17863 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc/NativeImportDataReactor.java
+++ src/prerna/sablecc/NativeImportDataReactor.java
@@ -1,42 +1,32 @@
 package prerna.sablecc;
 
 import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
-import prerna.algorithm.api.ITableDataFrame;
-import prerna.ds.QueryStruct;
 import prerna.ds.TableDataFrameFactory;
 import prerna.ds.h2.H2Frame;
 import prerna.ds.nativeframe.NativeFrame;
 import prerna.engine.api.IEngine;
 import prerna.engine.api.IScriptReactor;
-import prerna.rdf.query.builder.IQueryInterpreter;
 import prerna.sablecc.PKQLRunner.STATUS;
 import prerna.util.DIHelper;
-import prerna.util.Utility;
 
 public class NativeImportDataReactor extends ImportDataReactor {
 
 	public Iterator process() {
-		
 		super.process();
-			
+
 		if(myStore.containsKey(PKQLEnum.API)) {	
 			NativeFrame frame = (NativeFrame)myStore.get("G");
-//			Map<String, Set<String>> edgeHash = (Map<String, Set<String>>) this.getValue(PKQLEnum.API + "_EDGE_HASH");
 			IEngine engine = (IEngine) DIHelper.getInstance().getLocalProp((this.getValue(PKQLEnum.API + "_ENGINE")+"").trim());
-//			String query  = (String) myStore.get(PKQLEnum.API);
 
 			String frameEngine = frame.getEngineName();
 			if(frameEngine != null && engine != null) {
-				
 				//if we have data coming from a different engine, i.e. traversing across dbs
 				if(!frameEngine.equals(engine.getEngineName())) {
 					//need to convert from native frame to h2 frame
 					H2Frame newFrame = TableDataFrameFactory.convertToH2FrameFromNativeFrame(frame);
 					myStore.put(PKQLEnum.G, newFrame);
-					
+
 					String reactorName = newFrame.getScriptReactors().get(PKQLEnum.IMPORT_DATA);
 					try {
 						IScriptReactor importReactor = (IScriptReactor)Class.forName(reactorName).newInstance();
@@ -46,38 +36,32 @@ public class NativeImportDataReactor extends ImportDataReactor {
 					}
 				}
 			}
-			
-//			frame.createView(query);
-			// 4 & 5) if engine is not null, merge the data into the frame
-			// the engine is null when the api is actually a csv file
-//			if(engine != null) {
-				// put the edge hash and the logicalToValue maps within the myStore
-				// will be used when the data is actually imported
-//				Map[] mergedMaps = frame.mergeQSEdgeHash(edgeHash, engine, joinCols);
-//				myStore.put("edgeHash", mergedMaps[0]);
-//				myStore.put("logicalToValue", mergedMaps[1]);
-//			} 
 		}
-		
+
 		myStore.put("STATUS", STATUS.SUCCESS);
 		return null;
 	}
-	
+
+	/**
+	 * This is to auto-convert to H2Frame if there are table joins present within the table
+	 * @param reactor
+	 * @return
+	 */
 	private Iterator processScriptReactor(IScriptReactor reactor) {
 		//set data into the reactor
 		for(String key : myStore.keySet()) {
 			reactor.put(key, myStore.get(key));
 		}
-		
+
 		if(reactor instanceof H2ImportDataReactor) {
 			((H2ImportDataReactor)reactor).process();
 		}
-		
+
 		//set my data with data coming from the reactor
 		for(String retKey : reactor.getKeys()) {
 			myStore.put(retKey, reactor.getValue(retKey));
 		}
-		
+
 		return null;
 	}
 }
--- src/prerna/sablecc/TinkerImportDataReactor.java
+++ src/prerna/sablecc/TinkerImportDataReactor.java
@@ -1,14 +1,10 @@
 package prerna.sablecc;
 
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-import java.util.Vector;
 
 import prerna.ds.TinkerFrame;
-import prerna.ds.TinkerMetaHelper;
-import prerna.ds.util.FileIterator;
 import prerna.engine.api.IHeadersDataRow;
 import prerna.sablecc.PKQLRunner.STATUS;
 import prerna.util.Utility;
@@ -21,79 +17,26 @@ public class TinkerImportDataReactor extends ImportDataReactor{
 		super.process();
 		
 		// get all the appropriate values
-		
 		// get the frame
 		TinkerFrame frame = (TinkerFrame) myStore.get("G");
-		// get the iterator containing the information to add
-		Iterator<IHeadersDataRow> it = (Iterator<IHeadersDataRow>) myStore.get("iterator");
-		
-		// get the edge hash and the logicalToVAlue hash
-		// these values are a pair (are either both null or both not null)
-		// they are not null when the child reactor is an APIReactor and contains an engine
-		// in this case, the metadata has already been merged with the existing data
-		Map<String, Set<String>> edgeHash = (Map<String, Set<String>>) myStore.get("edgeHash");
-		Map<String, String> logicalToValue = (Map<String, String>) myStore.get("logicalToValue");
-
-		// get the join data 
-		Vector<Map<String,String>> joinCols = (Vector<Map<String, String>>) myStore.get(PKQLEnum.JOINS);
 		
 		// cardinality helps determine the relationship between the instance values in a column
 		// this is helpful in optimizing the extraction of the upstream node and downstream node when creating
 		// these vertices in the TinkerFrame
-		Map<Integer, Set<Integer>> cardinality = null;
+		Map<Integer, Set<Integer>> cardinality = Utility.getCardinalityOfValues(this.newHeaders, this.edgeHash);;
 		
-		// get the headers as this shouldn't change with each iteration
-		String[] headers = null;
-		// default assumption is that the metamodel has been defined and the primKey is false
-		boolean isPrimKey = false;
-		while(it.hasNext()){
-			IHeadersDataRow ss = it.next();
+		while(this.dataIterator.hasNext()){
+			IHeadersDataRow ss = this.dataIterator.next();
 			
-			if(headers == null) { // happens only during first loop
-				headers = ss.getHeaders();
-
-				// TODO this merge edge hash should go inside the super.process()!!!!
-				
-				// if the edge hash is not defined from when the super.process()
-				// we need to perform the mergeEdgeHash logic here
-
-				if(edgeHash == null) {
-					Map<String, Set<String>> primKeyEdgeHash = TinkerMetaHelper.createPrimKeyEdgeHash(headers);
-					//TODO: need to make all these wrappers that give a IHeaderDataRow be the same type to get this info
-					String[] types = null;
-					if(it instanceof FileIterator) {
-						types = ((FileIterator) it).getTypes();
-					} else if(it instanceof CsvTableWrapper) {
-						types = ((CsvTableWrapper) it).getTypes();
-					}
-					
-					String[] headersCopy = new String[types.length]; // need to create a copy to not override settings in iterator
-					Map<String, String> dataType = new HashMap<>();
-					for(int i = 0; i < types.length; i++) {
-						dataType.put(headers[i], types[i]);
-						headersCopy[i] = headers[i];
-					}
-					
-					updateDataForJoins(primKeyEdgeHash, dataType, headersCopy, joinCols);
-					frame.mergeEdgeHash(primKeyEdgeHash, dataType);
-					headers = headersCopy; // change variable pointer instead of modifying iterator header pointer
-					isPrimKey = true;
-				} else {
-					// get the cardinality mapping to be used
-					cardinality = Utility.getCardinalityOfValues(ss.getHeaders(), edgeHash);
-				}
-			}
-
-			// TODO: need to have a smart way of determining when it is an "addRow" vs. "addRelationship"
 			if(isPrimKey) {
-				frame.addRow(ss.getValues(), headers);
+				frame.addRow(ss.getValues(), this.newHeaders);
 			} else {
-				frame.addRelationship(ss.getHeaders(), ss.getValues(), cardinality, logicalToValue);
+				frame.addRelationship(this.newHeaders, ss.getValues(), cardinality, this.modifyNamesMap);
 			}
 		}
 		
 		// store the response string
-		inputResponseString(it, headers);
+		inputResponseString(this.dataIterator, this.newHeaders);
 		// set status to success
 		myStore.put("STATUS", STATUS.SUCCESS);
 		
