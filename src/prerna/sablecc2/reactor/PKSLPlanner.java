816629daae71349cb05ef3970d6c3c995af652ff
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2017-01-15 23:11:44 +0000

    Final installment of current optimization for the PKSL. Next stop - trident.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@18200 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/PKSLPlanner.java
+++ src/prerna/sablecc2/reactor/PKSLPlanner.java
@@ -129,10 +129,16 @@ public class PKSLPlanner {
 		// and plan my operation
 		// I need a block keeper 
 		
+		// when there is a reduce which multiple thing depends on, I need to validatee so that it doesn't do it twice
+		// Just as a stage has various operations and their sequence
+		// I also need to keep something with all stages and its sequence
+		// but at the same time ensure that it is not overwriting anything
+		
+		
 		
 		// I need to test
-		// Single Stage
-		// Multi Stage with reduce
+		// Single Stage - done
+		// Multi Stage with reduce - done
 		// Multi stage with reduce 
 		
 		Vector <Object> inputs = new Vector<Object>();
@@ -151,8 +157,11 @@ public class PKSLPlanner {
 			String code = null;
 			//code = loadVertex(codeStack);
 			
-			Stack <Stage> newStack = new Stack<Stage>();
-			code = loadVertex2(newStack, verticesToProcess);
+			//Stack <Stage> newStack = new Stack<Stage>();
+			//code = loadVertex2(newStack, verticesToProcess);
+			
+			
+			code = loadVertex3(new StageKeeper(), verticesToProcess);
 			// execute the code and return the iterator
 		}		
 		return null;
@@ -168,6 +177,7 @@ public class PKSLPlanner {
 		else
 		{
 			thisStage = new Stage();
+			thisStage.stageNum = 0;
 		}
 		Vertex thisVertex = verticesToProcess.remove(0);
 		System.out.println("Processing Vertex >>>>>>>>>>>>>>>>>>>> " + thisVertex.property(TINKER_NAME));
@@ -292,6 +302,8 @@ public class PKSLPlanner {
 			thisStage.removeOperation(operationName);
 			Stage newStage = new Stage();
 			codeStack.push(thisStage);
+			int newNum = thisStage.stageNum + 1;
+			newStage.stageNum = newNum;
 			newStage.addOperation(operationName, opHash);			
 			//codeStack.push(newStage);
 			thisStage = newStage;
@@ -310,9 +322,170 @@ public class PKSLPlanner {
 		{
 			System.out.println("====================================");
 			Stage stage = codeStack.elementAt(stageIndex);
-			System.out.println(" STAGE " + stageIndex + " \n\n" + stage.getCode());
+			System.out.println(" STAGE " + (codeStack.size() - stageIndex) + " \n\n" + stage.getCode());
 			System.out.println("====================================");
 		}
+		
+		System.out.println("Final CODE..\n\n\n\n");
+		System.out.println(curCode);	
+		return curCode;
+	}
+
+
+	private String loadVertex3(StageKeeper keeper, Vector <Vertex> verticesToProcess)
+	{
+		
+		// I need to break this into stages i.e. the codeStack
+		Stage thisStage = null;
+		if(keeper.lastStage != null)
+			thisStage = keeper.lastStage;
+		else
+		{
+			thisStage = new Stage();
+			thisStage.stageNum = 0;
+		}
+		Vertex thisVertex = verticesToProcess.remove(0);
+		System.out.println("Processing Vertex >>>>>>>>>>>>>>>>>>>> " + thisVertex.property(TINKER_NAME));
+
+		String operationName = thisVertex.property(TINKER_NAME).value() + "";
+		keeper.addStage(operationName, thisStage);
+		
+		Hashtable opHash = thisStage.addOperation(operationName);
+		// add inputs to these and in the end synchronize
+		// this might get the as so something to be aware of for now
+		Iterator <Vertex> inputs = thisVertex.vertices(Direction.IN);
+
+		Vector <String> opInputs = (Vector<String>)opHash.get(Stage.INPUTS);
+		Vector <String> deInputs = (Vector<String>)opHash.get(Stage.DERIVED_INPUTS);
+		Vector <String> depends = (Vector<String>)opHash.get(Stage.DEPENDS);
+		String curCode = (String)opHash.get(Stage.CODE);
+		// we will refine this code piece later
+
+		// run through each input 
+		// and 
+		// the input to these are almost.. wait always nouns
+		// I need a nounstore to get this assigned
+		// this way I can keep the filters and everything else
+		// should I just load the reactor ?
+		// that way I can get what I want ?
+		// the problem then is I have to worry about filters
+		// let the filters be for now.. let us run through other things
+		boolean close = false;
+
+		// assimilate the inputs in a single place
+		IReactor.TYPE opType = (IReactor.TYPE)thisVertex.property("OP_TYPE").value();
+		
+		// add the code
+		Object codeBlock = thisVertex.property("CODE").value();
+		
+		// write the code if you see a piece of code
+		if(codeBlock instanceof CodeBlock)
+		{
+			// I need to print this code
+			String code = ((CodeBlock)codeBlock).getCode();
+			curCode = curCode + "\n" + code; // building from the bottom
+			opHash.put(Stage.CODE, curCode);
+		}
+
+		// find if this is a reduce operation
+		while(inputs.hasNext())
+		{
+			// I need to account for when the input is an operation
+			// vs. when the input is not an operation
+			
+			// collect the operations that we need to process next
+			// as well as columns it is looking for
+			// get this vertex
+			// not accounting for parallel run yet, I will get to it
+			Vertex thisNoun = inputs.next();
+			String nounName = thisNoun.property(TINKER_NAME).value() + "";
+			
+			System.out.println("Adding noun.. " + nounName);
+			
+			// add if this column is not there already
+			// I should do this at a later point because I dont know when it will be a reduce..
+			//if(allInputCols.indexOf(nounName) < 0)
+			//	allInputCols.add(thisNoun.property(TINKER_NAME).value() + "");
+			
+			// get the input for this to find if this is a reduce
+			// this will be a an operation here
+			// what is producing this noun
+			Iterator <Vertex> opVertices = thisNoun.vertices(Direction.IN);
+			
+			Vertex thisOp = null;
+			if(opVertices.hasNext()) // ok there is an operation
+			{
+				thisOp = opVertices.next();
+				
+				// also need to add the dependency
+				
+				// add this for the next run
+				// take it out and add it to the end so it comes towards the end
+				System.out.println("\t Vertex" + thisOp.property(TINKER_NAME).value());
+				if(verticesToProcess.indexOf(thisOp) >= 0)
+					verticesToProcess.remove(thisOp);
+				verticesToProcess.add(thisOp);		
+				depends.addElement(thisOp.property(TINKER_NAME).value() + "");
+				// I need to find if this is reduce and if do find what do I do again ?
+				// some how I need to move to next code block
+				// no idea what this means hehe.. 
+				// need to close this block after this is done
+				codeBlock = thisOp.property("CODE").value();
+				if(codeBlock instanceof String)
+					opInputs.add(nounName);
+				else // this is a derived call 
+					deInputs.add(nounName);
+			}
+			else // this is the base input with no parent operation
+				opInputs.add(nounName);
+		}
+		
+
+		
+		// I need to add the decorator on top
+		// so that this will only be those fields that this particular routine needs
+		// so I could be retrieving [a,b,c,d,e] and this routine may only require
+		// [a,d,e] so I need to seggregate before passing it on
+		// this is all sit on the input Cols
+		opHash.put(Stage.INPUTS, opInputs);
+		opHash.put(Stage.DERIVED_INPUTS, deInputs);
+		opHash.put(Stage.CODE, curCode);	
+		opHash.put(Stage.DEPENDS, depends);
+		
+		if(opType == IReactor.TYPE.REDUCE) // this is a reduce operation
+		{
+			// couple of things I need to do
+			// I need to add the query to the top of it
+			// which means I need sum total of all columns
+			// need to take all the input columns and generate the query for it
+			// this is where I convert this into a query and then let it rip from here on
+			// close out the first piece
+			
+			// remove this operation
+			// add a new stage
+			// add this operation to the new stage
+			// recurse
+			thisStage.removeOperation(operationName);
+			Stage newStage = new Stage();
+			int newNum = thisStage.stageNum + 1;
+			newStage.stageNum = newNum;
+			newStage.addOperation(operationName, opHash);			
+			keeper.addStage(operationName, newStage);
+			//codeStack.push(newStage);
+			thisStage = newStage;
+		}
+		//codeStack.push(thisStage);
+		thisStage.synchronizeInput(operationName);
+		
+		if(verticesToProcess.size() > 0)
+		{
+			return loadVertex3(keeper, verticesToProcess);
+		}
+
+		keeper.adjustStages();
+		keeper.printCode();
+		// walk through the code stack poping one by one
+		// and asking it to getcode
 		
 		System.out.println("Final CODE..\n\n\n\n");
 		System.out.println(curCode);	
