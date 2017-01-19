16223aad2a469bb7523dd6c045846692943ccdc8
rluthar <rluthar@deloitte.com>
2017-01-19 16:57:36 +0000

    UPDATE/BUGFIX ::: modifying grammar such that database.concept.properties takes in an engine, small bug fix with databasepkqservice...correcting URI in query, separating out MetamodelVertex into its own class
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@18272 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/sablecc/services/MetamodelVertex.java
@@ -0,0 +1,48 @@
+package prerna.sablecc.services;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * class to simplify the implementation
+ * of combining alias names and properties 
+ * on vertices
+ * @author mahkhalil
+ *
+ */
+public class MetamodelVertex {
+	
+	// store the property conceptual names
+	private Set<String> propSet = new TreeSet<String>();
+	// the conceptual name for the concept
+	private String conceptualName;
+	
+	public MetamodelVertex(String conceptualName) {
+		this.conceptualName = conceptualName;
+	}
+	
+	/**
+	 * Add to the properties for the vertex
+	 * @param propertyConceptual
+	 * @param propertyAlias
+	 */
+	public void addProperty(String propertyConceptual) {
+		if(propertyConceptual.equals("noprop")) {
+			return;
+		}
+		propSet.add(propertyConceptual);
+	}
+	
+	public Map<String, Object> toMap() {
+		Map<String, Object> vertexMap = new Hashtable<String, Object>();
+		vertexMap.put("conceptualName", this.conceptualName);
+		vertexMap.put("propSet", this.propSet);
+		return vertexMap;
+	}
+	
+	public String toString() {
+		return toMap().toString();
+	}
+}
