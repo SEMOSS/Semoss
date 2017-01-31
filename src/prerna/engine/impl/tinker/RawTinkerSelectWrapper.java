ce22504cc1dfe0eae253a6f9f0674ebb7a9e0579
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-01-31 15:22:01 +0000

    Tinker filter properties in import
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@18442 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ccd46ae9aabc8ee9d605390800640bd644f2f51f

--- src/prerna/engine/impl/tinker/RawTinkerSelectWrapper.java
+++ src/prerna/engine/impl/tinker/RawTinkerSelectWrapper.java
@@ -14,7 +14,7 @@ import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 import prerna.ds.QueryStruct;
 import prerna.ds.TinkerFrameIterator;
 import prerna.ds.TinkerIterator;
-import prerna.ds.TinkerQueryEngineInterpreter;
+import prerna.ds.TinkerQueryInterpreter;
 import prerna.engine.api.IHeadersDataRow;
 import prerna.engine.api.IRawSelectWrapper;
 import prerna.engine.impl.rdf.HeadersDataRow;
@@ -26,9 +26,8 @@ public class RawTinkerSelectWrapper extends AbstractWrapper implements IRawSelec
 
 	@Override
 	public IHeadersDataRow next() {
-		// TODO Auto-generated method stub
 		Object[] row = it.next();
-		if(displayVar == null) {
+		if (displayVar == null) {
 			getDisplayVariables();
 		}
 		return new HeadersDataRow(displayVar, row, row);
@@ -36,25 +35,22 @@ public class RawTinkerSelectWrapper extends AbstractWrapper implements IRawSelec
 
 	@Override
 	public String[] getDisplayVariables() {
-		// displayVar = new String[numColumns];
-		QueryStruct qs = (QueryStruct) ((TinkerEngine) engine).getQueryStruct();
-		System.out.println("?");
-		Hashtable<String, Vector<String>> selectors = qs.selectors;
-		ArrayList<String> dV = new ArrayList<>();
-		for (String selectorKey : selectors.keySet()) {
-			Vector<String> props = selectors.get(selectorKey);
-			// Set<String> downNodeTypes = edgeHash.get(selectorKey);
-			System.out.println(selectorKey);
-			dV.add(selectorKey);
-			for (String prop : props) {
-				// TODO fix properties
-				if (!prop.contains("PRIM_KEY_PLACEHOLDER")) {
-					dV.add(prop);
+		if (displayVar != null) {
+			QueryStruct qs = (QueryStruct) ((TinkerEngine) engine).getQueryStruct();
+			Hashtable<String, Vector<String>> selectors = qs.selectors;
+			ArrayList<String> dV = new ArrayList<>();
+			for (String selectorKey : selectors.keySet()) {
+				Vector<String> props = selectors.get(selectorKey);
+				dV.add(selectorKey);
+				for (String prop : props) {
+					if (!prop.contains("PRIM_KEY_PLACEHOLDER")) {
+						dV.add(prop);
+					}
 				}
-			}
 
+			}
+			displayVar = dV.toArray(new String[dV.size()]);
 		}
-		displayVar = dV.toArray(new String[dV.size()]);
 		return displayVar;
 	}
 
@@ -66,8 +62,7 @@ public class RawTinkerSelectWrapper extends AbstractWrapper implements IRawSelec
 
 	@Override
 	public void execute() {
-		TinkerQueryEngineInterpreter interp = (TinkerQueryEngineInterpreter) ((TinkerEngine) this.engine)
-				.getQueryInterpreter();
+		TinkerQueryInterpreter interp = (TinkerQueryInterpreter) ((TinkerEngine) this.engine).getQueryInterpreter();
 		interp.setQueryStruct(((TinkerEngine) this.engine).getQueryStruct());
 		it = (TinkerIterator) interp.composeIterator();
 	}
