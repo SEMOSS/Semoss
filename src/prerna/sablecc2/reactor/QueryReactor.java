3363d1af7b6eaf3bbd755247c9af024dc7f95631
rluthar <rluthar@deloitte.com>
2017-02-01 17:51:37 +0000

    UPDATE ::: adding filter class analogous to join class, will commit the split soon, QueryReactor.java - this will be the equivalent to current api reactor, essentially produces a query
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@18474 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 9a542b206f3c4ffb0aab88cdbf87060be787a384

---
+++ src/prerna/sablecc2/reactor/QueryReactor.java
@@ -0,0 +1,29 @@
+package prerna.sablecc2.reactor;
+
+public class QueryReactor extends AbstractReactor {
+
+	@Override
+	public void In() {
+	}
+
+	@Override
+	public Object Out() {
+		return null;
+	}
+
+	@Override
+	public void mergeUp() {
+
+	}
+
+	@Override
+	public Object execute() {
+		return null;
+	}
+
+	@Override
+	public void updatePlan() {
+		
+	}
+
+}
