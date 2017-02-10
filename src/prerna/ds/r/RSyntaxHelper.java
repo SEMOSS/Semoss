11a0ad009d91b842046aafe537bde1bae264f27d
mahkhalil <mahkhalil@deloitte.com>
2017-02-10 20:24:04 +0000

    UPDATE ::: Adding method to insert row inside R frame
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@18660 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 282a78afdad81e5db3fe6816e67875bcb5ac495f

--- src/prerna/ds/r/RSyntaxHelper.java
+++ src/prerna/ds/r/RSyntaxHelper.java
@@ -26,6 +26,48 @@ public class RSyntaxHelper {
 	}
 	
 	/**
+	 * Convert a java object[] into a r column vector
+	 * @param row				The object[] to convert
+	 * @param dataType			The data type for each entry in the object[]
+	 * @return					String containing the equivalent r column vector
+	 */
+	public static String createStringRColVec(Integer[] row) {
+		StringBuilder str = new StringBuilder("c(");
+		int i = 0;
+		int size = row.length;
+		for(; i < size; i++) {
+			str.append(row[i]);
+			// if not the last entry, append a "," to separate entries
+			if( (i+1) != size) {
+				str.append(",");
+			}
+		}
+		str.append(")");
+		return str.toString();
+	}
+	
+	/**
+	 * Convert a java object[] into a r column vector
+	 * @param row				The object[] to convert
+	 * @param dataType			The data type for each entry in the object[]
+	 * @return					String containing the equivalent r column vector
+	 */
+	public static String createStringRColVec(Double[] row) {
+		StringBuilder str = new StringBuilder("c(");
+		int i = 0;
+		int size = row.length;
+		for(; i < size; i++) {
+			str.append(row[i]);
+			// if not the last entry, append a "," to separate entries
+			if( (i+1) != size) {
+				str.append(",");
+			}
+		}
+		str.append(")");
+		return str.toString();
+	}
+	
+	/**
 	 * Converts a R column type to numeric
 	 * @param tableName				The name of the R table
 	 * @param colName				The name of the column
