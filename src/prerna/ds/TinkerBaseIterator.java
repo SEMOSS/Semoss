afa14b116d0210ebe918b7f5ed963a95fed4b61e
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-02-13 17:07:01 +0000

    FIX: Tinker engine search edge cases
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@18671 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f127e36e5f24cba7552b9349674221b10e34b3c9

--- src/prerna/ds/TinkerBaseIterator.java
+++ src/prerna/ds/TinkerBaseIterator.java
@@ -1,20 +1,33 @@
 package prerna.ds;
 
+import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.apache.tinkerpop.gremlin.process.traversal.P;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+
+import prerna.rdf.query.builder.IQueryInterpreter;
 
 public class TinkerBaseIterator implements Iterator {
 	private GraphTraversal gt;
 	private List<String> selectors;
 	private Object[] nextRow;
+	protected Hashtable<String, Hashtable<String, Vector>> filters;
+	private int performCount;
+	private int rowCount;
 
-	public TinkerBaseIterator(GraphTraversal gt, List<String> selectors) {
+	public TinkerBaseIterator(GraphTraversal gt, List<String> selectors, QueryStruct qs) {
 		this.gt = gt;
 		this.selectors = selectors;
+		this.filters = qs.andfilters;
+		this.performCount = qs.getPerformCount();
+		this.rowCount = 0;
 		getNextValidRow();
 
 	}
@@ -40,11 +53,41 @@ public class TinkerBaseIterator implements Iterator {
 			nextRow = getNextRow();
 			if (nextRow != null) {
 				validRow = true;
+				rowCount++;
 			}
 		}
 		if (!validRow) {
 			nextRow = null;
 		}
+	}
+
+	/**
+	 * Tinker does not support regular expressions so treat the value as a
+	 * string and apply a regex
+	 * 
+	 * @param value
+	 * @param filterInfo
+	 * @return
+	 */
+	public String search(String value, Hashtable<String, Vector> filterInfo) {
+		String match = null;
+		for (String filterType : filterInfo.keySet()) {
+			if (filterType.equals(IQueryInterpreter.SEARCH_COMPARATOR)) {
+				Vector filterVals = filterInfo.get(filterType);
+				String strPattern = (String) filterVals.get(0);
+				String pattern = ".*" + strPattern.toLowerCase() + ".*";
+				Pattern r = Pattern.compile(pattern);
+
+				Matcher m = r.matcher(value.toLowerCase());
+				if (m.find()) {
+					match = value;
+				}
+			}
+			else {
+				match = value;
+			}
+		}
+		return match;
 	}
 
 	private Object[] getNextRow() {
@@ -73,14 +116,25 @@ public class TinkerBaseIterator implements Iterator {
 			for (int colIndex = 0; colIndex < selectors.size(); colIndex++) {
 				String select = selectors.get(colIndex);
 				Object value = data;
+				boolean filtered = false;
 				if (data instanceof Vertex) {
 					value = ((Vertex) data).values(TinkerFrame.TINKER_NAME).next();
+					System.out.println(value);
+					if (filters.containsKey(select)) {
+						value = search(value + "", filters.get(select));
+						filtered = true;
+					}
 				}
 
 				else {
 					value = data;
 				}
 				retObject[colIndex] = value;
+				if (filtered) {
+					if (value == null) {
+						retObject = null;
+					}
+				}
 			}
 		}
 
