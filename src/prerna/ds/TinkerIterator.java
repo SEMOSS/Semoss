45da70a33cfe7095d49621aea4764c77a67a53b2
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-02-13 17:07:01 +0000

    FIX: Tinker engine search edge cases
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@18671 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ds/TinkerIterator.java
+++ src/prerna/ds/TinkerIterator.java
@@ -9,8 +9,8 @@ import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 public class TinkerIterator implements Iterator {
 	private TinkerBaseIterator it;
 
-	public TinkerIterator(GraphTraversal gt, List<String> selectors) {
-		it = new TinkerBaseIterator(gt, selectors);
+	public TinkerIterator(GraphTraversal gt, List<String> selectors, QueryStruct qs) {
+		it = new TinkerBaseIterator(gt, selectors, qs);
 	}
 
 	@Override
