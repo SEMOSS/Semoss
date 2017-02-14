5eacb6268bb9614e042b1399c4c18220fadf6447
tbanach <tbanach@deloitte.com>
2017-02-14 16:21:17 +0000

    UPDATE::Update logic for anomaly detection, including categorical anomaly detection BUGFIX::Fixed capitalization issue with column names in predefined R-Script runner (RRoutine)
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@18690 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: be2bb034400d2f03d409ef5b7661fcf4895ed6fc

--- src/prerna/algorithm/learning/unsupervised/anomaly/AnomalyDetectorTest.java
+++ src/prerna/algorithm/learning/unsupervised/anomaly/AnomalyDetectorTest.java
@@ -15,7 +15,6 @@ import prerna.ds.h2.H2Frame;
 import prerna.ds.util.FileIterator;
 import prerna.ds.util.FileIterator.FILE_DATA_TYPE;
 import prerna.engine.api.IHeadersDataRow;
-import prerna.sablecc.BaseJavaReactor;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
 import prerna.util.Utility;
@@ -27,42 +26,13 @@ public class AnomalyDetectorTest {
 
 	// Test case for AnomalyDetection
 	public static void main(String[] args) throws Exception {
-		System.out.println("----------Anomaly Detector Test----------");
-		anomalyDetectorTest();
-		System.out.println("------------Java Reactor Test------------");
-		javaReactorTest();
+		System.out.println("----------Numeric Test----------");
+		numericTest();
+		System.out.println("--------Categorical Test--------");
+		categoricalTest();
 	}
 
-	private static void anomalyDetectorTest() throws RRoutineException {
-		ITableDataFrame frame = makeTestFrame();
-
-		// Create a new anomaly detector
-		AnomalyDetector anomalyDetector = new AnomalyDetector(frame, "count_1", "timestamp_1", 0.01, AnomDirection.BOTH,
-				0.05, 100);
-
-		// Detect anomalies
-		ITableDataFrame newFrame = anomalyDetector.detectAnomalies();
-
-		// Final frame first row
-		Iterator<IHeadersDataRow> finalData = newFrame.query("SELECT * FROM " + newFrame.getTableName() + ";");
-		System.out.println(finalData.next());
-	}
-
-	private static void javaReactorTest() {
-		ITableDataFrame frame = makeTestFrame();
-
-		// Test javaReactor code
-		BaseJavaReactor javaReactor = new BaseJavaReactor();
-		javaReactor.dataframe = frame;
-		javaReactor.runAnomalyDetection("count_1", "timestamp_1", 0.01, "both", 0.05, 100);
-
-		// Final frame first row
-		Iterator<IHeadersDataRow> finalData = javaReactor.dataframe
-				.query("SELECT * FROM " + javaReactor.dataframe.getTableName() + ";");
-		System.out.println(finalData.next());
-	}
-
-	private static ITableDataFrame makeTestFrame() {
+	private static void numericTest() throws RRoutineException {
 		DIHelper.getInstance().loadCoreProp(System.getProperty("user.dir") + "/RDF_Map.prop");
 
 		String baseDirectory = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
@@ -107,7 +77,75 @@ public class AnomalyDetectorTest {
 		// Original frame first row
 		Iterator<IHeadersDataRow> originalData = frame.query("SELECT * FROM " + frame.getTableName() + ";");
 		System.out.println(originalData.next());
-		return frame;
+
+		// Create a new anomaly detector
+		AnomalyDetector anomalyDetector = new AnomalyDetector(frame, "timestamp_1", "count_1", "sum", 0.01,
+				AnomDirection.BOTH, 0.05, 1440, true);
+
+		// Detect anomalies
+		ITableDataFrame newFrame = anomalyDetector.detectAnomalies();
+
+		// Final frame first row
+		Iterator<IHeadersDataRow> finalData = newFrame.query("SELECT * FROM " + newFrame.getTableName() + ";");
+		System.out.println(finalData.next());
+	}
+
+	private static void categoricalTest() throws RRoutineException {
+		DIHelper.getInstance().loadCoreProp(System.getProperty("user.dir") + "/RDF_Map.prop");
+
+		String baseDirectory = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
+		String scriptsDirectory = baseDirectory + "\\" + R_BASE_FOLDER + "\\" + ANALYTICS_SCRIPTS_FOLDER;
+
+		// Create an H2Frame for testing
+		H2Frame frame = new H2Frame();
+
+		String fileName = scriptsDirectory + "\\test_categorical_anom.csv";
+
+		String[] colNames = new String[3];
+		colNames[0] = "date_1";
+		colNames[1] = "event_1";
+		colNames[2] = "group_1";
+
+		String[] colTypes = new String[3];
+		colTypes[0] = "DATE";
+		colTypes[1] = "STRING";
+		colTypes[2] = "STRING";
+
+		// Need to create a data type map and a query structure
+		QueryStruct qs = new QueryStruct();
+		Map<String, IMetaData.DATA_TYPES> dataTypeMap = new Hashtable<String, IMetaData.DATA_TYPES>();
+		Map<String, String> dataTypeMapStr = new Hashtable<String, String>();
+		for (int i = 0; i < colNames.length; i++) {
+			dataTypeMapStr.put(colNames[i], colTypes[i]);
+			dataTypeMap.put(colNames[i], Utility.convertStringToDataType(colTypes[i]));
+			qs.addSelector(colNames[i], null);
+		}
+
+		Map<String, Set<String>> edgeHash = TinkerMetaHelper.createPrimKeyEdgeHash(colNames);
+		frame.mergeEdgeHash(edgeHash, dataTypeMapStr);
+
+		// Iterate through file and insert values
+		FileIterator dataIterator = FileIterator.createInstance(FILE_DATA_TYPE.META_DATA_ENUM, fileName, ',', qs,
+				dataTypeMap);
+		frame.addRowsViaIterator(dataIterator, dataTypeMap);
+
+		// Update the user id to match the new schema
+		frame.setUserId(frame.getSchema());
+
+		// Original frame first row
+		Iterator<IHeadersDataRow> originalData = frame.query("SELECT * FROM " + frame.getTableName() + ";");
+		System.out.println(originalData.next());
+
+		// Create a new anomaly detector
+		AnomalyDetector categoricalDetector = new AnomalyDetector(frame, "date_1", "event_1", "group_1",
+				"count distinct", 0.01, AnomDirection.BOTH, 0.05, 7);
+
+		// Detect anomalies
+		ITableDataFrame newFrame = categoricalDetector.detectAnomalies();
+
+		// Final frame first row
+		Iterator<IHeadersDataRow> finalData = newFrame.query("SELECT * FROM " + newFrame.getTableName() + ";");
+		System.out.println(finalData.next());
 	}
 
 }
