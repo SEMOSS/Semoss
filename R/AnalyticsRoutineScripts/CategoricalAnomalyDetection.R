ee5ed80d38a14ab3e437c424639524eacd7a8a89
tbanach <tbanach@deloitte.com>
2017-02-14 16:21:17 +0000

    UPDATE::Update logic for anomaly detection, including categorical anomaly detection BUGFIX::Fixed capitalization issue with column names in predefined R-Script runner (RRoutine)
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@18690 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ R/AnalyticsRoutineScripts/CategoricalAnomalyDetection.R
@@ -0,0 +1,144 @@
+library(AnomalyDetection)
+
+# For test cases
+# library(data.table)
+# setwd("C:\\Users\\tbanach\\RWorkingDirectory")
+# dt <- read.csv("test_categorical_anom.csv")
+# args <- list('date', 'event', 'group', 'count distinct', 0.25, 'both', 0.05, 7)
+# setDT(dt)
+# head(dt)
+
+# Store the arguments in more readable form
+# Note that the args[[i]] string must be different from the variable name for the case of headers
+# For example, time <- "time" would cause an issue when calling get(time)
+# So make these variables contain a period, which is an illegal header character in Semoss
+# That way they will be distinct
+time.col <- args[[1]]
+event.col <- args[[2]]
+group.col <- args[[3]]
+
+# Get the aggregate function
+agg.string <- args[[4]]
+if (agg.string == 'count distinct') {
+  agg <- uniqueN
+  agg.string <- sub(" ", "_", args[[4]])
+} else if (agg.string == 'count') {
+  agg <- length  
+} else {
+  agg <- get(agg.string)
+}
+max.anoms <- args[[5]]
+direction <- args[[6]]
+alpha <- args[[7]]
+period <- args[[8]]
+
+# Make sure group is a factor, because the levels of group must be accessed later
+dt[, toString(group.col):=as.factor(get(group.col))]
+
+# For cases where the group is just the time
+DetectWithoutGroups <- function(dt, time.col, event.col, agg, agg.string, max.anoms, direction, alpha, period) {
+
+  # Set the time column as the key
+  setkeyv(dt, time.col)
+  
+  # Store the levels of the group that had an anomaly at the most recent time
+  anom.levels <- list()
+  i <- 1
+  
+  # Aggregate for each time using the user-specified aggregate function
+  agg.col <- paste0(toupper(agg.string), "_", event.col)
+  dt[, toString(agg.col) := as.numeric(agg(get(event.col))), keyby = time.col]
+  
+  # Detect anomalies on only one value for each group per unique time
+  unique.times <- unique(dt, by=time.col)[, c(time.col, agg.col), with=FALSE]
+  
+  # So that a graphics window does not pop up
+  options(device=pdf)
+  
+  # Detect anomalies
+  res <- AnomalyDetectionVec(unique.times[, get(agg.col)], max_anoms=max.anoms, direction=direction, alpha=alpha, period=period, plot=FALSE)
+  anom.col <- paste0("ANOM_", agg.col)
+  unique.times[, toString(anom.col) := 0]
+  unique.times[res$anoms$index, toString(anom.col) := res$anoms$anoms]
+  
+  # Store the most recent time if it was an anomaly
+  if (!is.null(res$anoms$index) && res$anoms$index[length(res$anoms$index)] == nrow(unique.times)) {
+    anom.levels[[i]] <- unique.times[nrow(unique.times), get(time.col)]
+  }
+  return(list(dt=dt, unique.times=unique.times, anom.levels=anom.levels))
+}
+
+# For cases where the user specifies groups
+DetectWithGroups <- function(dt, time.col, event.col, group.col, agg, agg.string, max.anoms, direction, alpha, period) {
+
+  # Set the time column as the key
+  setkeyv(dt, c(time.col, group.col))
+  
+  # Store the levels of the group that had an anomaly at the most recent time
+  anom.levels <- list()
+  i <- 1
+  
+  # Aggregate for each group and time using the user-specified aggregate function
+  agg.col <- paste0(toupper(agg.string), "_", event.col, "_BY_", group.col)
+  dt[, toString(agg.col) := as.numeric(agg(get(event.col))), keyby = c(time.col, group.col)]
+  
+  # Detect anomalies on only one value for each group per unique time
+  unique.times <- unique(dt, by=time.col)[, time.col, with=FALSE]
+  
+  # So that a graphics window does not pop up
+  options(device=pdf)
+  
+  # Loop through each level of the group,
+  # and determine the series for that level
+  for (level in levels(dt[, get(group.col)])) {
+
+    # Determine the series for this particular level
+    group.series <- unique(dt, by=c(time.col, group.col))[get(group.col) == level, c(time.col, group.col, agg.col), with=FALSE]
+    
+    # Perform an outer join between the series for a particular group,
+    # and the data table of all days, so that each day is represented
+    vec <- merge(unique.times, group.series, by=time.col, all=TRUE)[, get(agg.col)]
+    
+    # Replace NA's from the outer join (that occur when this level is not represented for a given time) with zeros
+    vec[is.na(vec)] <- 0
+    
+    # Detect anomalies
+    res <- AnomalyDetectionVec(vec, max_anoms=max.anoms, direction=direction, alpha=alpha, period=period, plot=FALSE)
+    
+    # Add columns
+    unique.times[, toString(level) := vec]
+    anom.col <- paste0("ANOM_", level)
+    unique.times[, toString(anom.col) := 0]
+    unique.times[res$anoms$index, toString(anom.col) := res$anoms$anoms]
+    
+    # Store the levels of the group that had an anomaly at the most recent time
+    if (!is.null(res$anoms$index) && res$anoms$index[length(res$anoms$index)] == nrow(unique.times)) {
+      anom.levels[[i]] <- level
+      i <- i + 1
+    }
+  }
+  return(list(dt=dt, unique.times=unique.times, anom.levels=anom.levels))
+}
+
+# If the group is time, then detect without groups
+# Default to detect with groups
+result <- list()
+if (group.col == time.col) {
+  result <- DetectWithoutGroups(dt, time.col, event.col, agg, agg.string, max.anoms, direction, alpha, period)
+} else {
+  result <- DetectWithGroups(dt, time.col, event.col, group.col, agg, agg.string, max.anoms, direction, alpha, period)
+}
+dt <- result$dt
+unique.times <- result$unique.times
+anom.levels <- result$anom.levels
+
+# Return whether an anomaly occured at the most recent time
+GetResult <- function() {
+  return(anom.levels)
+}
+
+# For test cases
+# See the final results
+# dt
+# unique.times
+# GetResult()
--- src/prerna/algorithm/learning/unsupervised/anomaly/AnomalyDetector.java
+++ src/prerna/algorithm/learning/unsupervised/anomaly/AnomalyDetector.java
@@ -9,43 +9,136 @@ public class AnomalyDetector {
 
 	private RRoutine rRoutine;
 
+	/**
+	 * The direction in which to detect anomalies
+	 * 
+	 * @author tbanach
+	 *
+	 */
 	public enum AnomDirection {
 		POSITIVE, NEGATIVE, BOTH
 	}
 
 	private static final String SCRIPT_NAME = "AnomalyDetection.R";
-	private static final String R_FRAME_NAME = "df";
+	private static final String CATEGORICAL_SCRIPT_NAME = "CategoricalAnomalyDetection.R";
+	private static final String R_SYNC_FRAME_NAME = "dt";
 
-	// TODO enforce restrictions on parameters
-	public AnomalyDetector(ITableDataFrame dataFrame, String seriesColumn, String timeColumn, double maxAnoms,
-			AnomDirection direction, double alpha, int period) {
+	// If the user chooses to keep existing columns, then the data frame with
+	// the same name as R_SYNC_FRAME_NAME will be returned
+	// If the user chooses not to keep existing columns,
+	// a simplified frame is returned, given by the following name
+	private static final String R_SIMPLIFIED_FRAME_NAME = "unique.times";
 
-		String stringDirection;
-		switch (direction) {
-		case POSITIVE:
-			stringDirection = "pos";
-			break;
-		case NEGATIVE:
-			stringDirection = "neg";
-			break;
-		case BOTH:
-			stringDirection = "both";
-			break;
-		default:
-			stringDirection = "both";
-			break;
+	/**
+	 * 
+	 * This constructor creates an anomaly detector that detects anomalies in
+	 * numeric data.
+	 * 
+	 * @param dataFrame
+	 *            The ITableDataFrame containing a numeric series
+	 * @param timeColumn
+	 *            The column containing time stamps; can be date, string, or
+	 *            numeric representation
+	 * @param seriesColumn
+	 *            The column containing a numeric series with potential
+	 *            anomalies
+	 * @param aggregateFunction
+	 *            The function used to aggregate the series when there are
+	 *            duplicated time stamps
+	 * @param maxAnoms
+	 *            The maximum proportion of the series of counts that can be
+	 *            considered an anomaly, must be between 0 and 1
+	 * @param direction
+	 *            The direction in which anomalies can occur, includes POSITIVE,
+	 *            NEGATIVE, and BOTH
+	 * @param alpha
+	 *            The level of statistical significance, must be between 0 and
+	 *            1, but should generally be less than 0.1
+	 * @param period
+	 *            The number of time stamps per natural cycle; anomalies are
+	 *            sensitive to this input
+	 * @param keepExistingColumns
+	 *            Whether to keep the existing column structure and add to it,
+	 *            or return a simplified data frame
+	 */
+	public AnomalyDetector(ITableDataFrame dataFrame, String timeColumn, String seriesColumn, String aggregateFunction,
+			double maxAnoms, AnomDirection direction, double alpha, int period, boolean keepExistingColumns) {
+		String arguments = "'" + timeColumn + "';'" + seriesColumn + "';'" + aggregateFunction + "';" + maxAnoms + ";'"
+				+ determineDirectionString(direction) + "';" + alpha + ";" + period;
+		if (keepExistingColumns) {
+			rRoutine = new RRoutine.Builder(dataFrame, SCRIPT_NAME, R_SYNC_FRAME_NAME).arguments(arguments)
+					.routineType(RRoutineType.ANALYTICS).build();
+		} else {
+			rRoutine = new RRoutine.Builder(dataFrame, SCRIPT_NAME, R_SYNC_FRAME_NAME)
+					.rReturnFrameName(R_SIMPLIFIED_FRAME_NAME).arguments(arguments).routineType(RRoutineType.ANALYTICS)
+					.build();
 		}
-
-		String selectedColumns = timeColumn + ";" + seriesColumn;
-
-		String arguments = "'" + seriesColumn + "';" + maxAnoms + ";'" + stringDirection + "';" + alpha + ";" + period
-				+ ";FALSE";
-
-		rRoutine = new RRoutine.Builder(dataFrame, SCRIPT_NAME, R_FRAME_NAME).selectedColumns(selectedColumns)
-				.arguments(arguments).routineType(RRoutineType.ANALYTICS).build();
 	}
 
+	/**
+	 * This constructor creates an anomaly detector that detects anomalies in
+	 * categorical data. This is done by counting the number of events that
+	 * occur per unit time for each group.
+	 * 
+	 * @param dataFrame
+	 *            The ITableDataFrame containing categorical data
+	 * @param timeColumn
+	 *            The column containing time stamps; can be date, string, or
+	 *            numeric representation
+	 * @param eventColumn
+	 *            The column containing events to count, usually the primary key
+	 *            when counting records
+	 * @param groupColumn
+	 *            The column to group by; the count of events is reported for
+	 *            each level of this group
+	 * @param aggregateFunction
+	 *            The function used to aggregate events, count or count distinct
+	 * @param maxAnoms
+	 *            The maximum proportion of the series of counts that can be
+	 *            considered an anomaly, must be between 0 and 1
+	 * @param direction
+	 *            The direction in which anomalies can occur, includes POSITIVE,
+	 *            NEGATIVE, and BOTH
+	 * @param alpha
+	 *            The level of statistical significance, must be between 0 and
+	 *            1, but should generally be less than 0.1
+	 * @param period
+	 *            The number of time stamps per natural cycle; anomalies are
+	 *            sensitive to this input
+	 */
+	public AnomalyDetector(ITableDataFrame dataFrame, String timeColumn, String eventColumn, String groupColumn,
+			String aggregateFunction, double maxAnoms, AnomDirection direction, double alpha, int period) {
+		String arguments = "'" + timeColumn + "';'" + eventColumn + "';'" + groupColumn + "';'" + aggregateFunction
+				+ "';" + maxAnoms + ";'" + determineDirectionString(direction) + "';" + alpha + ";" + period;
+		rRoutine = new RRoutine.Builder(dataFrame, CATEGORICAL_SCRIPT_NAME, R_SYNC_FRAME_NAME)
+				.rReturnFrameName(R_SIMPLIFIED_FRAME_NAME).arguments(arguments).routineType(RRoutineType.ANALYTICS)
+				.build();
+	}
+
+	/**
+	 * Detects anomalies using this detector.
+	 * 
+	 * @return ITableDataFrame - The data frame after running anomaly detection
+	 * @throws RRoutineException
+	 */
 	public ITableDataFrame detectAnomalies() throws RRoutineException {
 		return rRoutine.returnDataFrame();
+	}
+
+	// Determines the proper string to represent the direction in which to
+	// detect anomalies
+	// This string is what is passed into the arguments list that is
+	// synchronized to R
+	private String determineDirectionString(AnomDirection direction) {
+		switch (direction) {
+		case POSITIVE:
+			return ("pos");
+		case NEGATIVE:
+			return ("neg");
+		case BOTH:
+			return ("both");
+		default:
+			return ("both");
+		}
 	}
 }
