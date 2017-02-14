598ea376a064ab8fb5f292ec226d325db247a8d5
tbanach <tbanach@deloitte.com>
2017-02-14 22:59:32 +0000

    UPDATE ::: Batch script to configure R-Portable for Semoss. Copy SemossConfigR folder into R-Portable and run configureR-Portable.bat to install all the packages needed for Semoss (will not run unless it is in an R-Portable directory for security reasons). Additional packages can be registered in SemossConfigR/scripts/Packages.R.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@18736 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b1546a746f1833cac6bcfc78e1c5d5b304585918

---
+++ R/SemossConfigR/scripts/Packages.R
@@ -0,0 +1,12 @@
+# General Semoss packages
+install.packages("Rserve")
+install.packages("splitstackshape")
+install.packages("data.table")
+install.packages("reshape2")
+install.packages("RJDBC")
+install.packages("sqldf")
+install.packageS("stringr")
+
+# Anomaly detection packages
+install.packages("devtools")
+devtools::install_github("twitter/AnomalyDetection")
