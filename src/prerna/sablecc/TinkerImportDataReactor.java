c8e4317d3fff6a04538cab04145d0d9732644b58
Maher Khalil <themaherkhalil@gmail.com>
2017-02-28 15:18:48 +0000

    BUGFIX::: Fixing the logic to allow for traversing from "loop" columns within tinker
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@18959 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 13b43a28af69d57496642b4f550525694a98d037

--- src/prerna/sablecc/TinkerImportDataReactor.java
+++ src/prerna/sablecc/TinkerImportDataReactor.java
@@ -1,8 +1,10 @@
 package prerna.sablecc;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.Vector;
 
 import prerna.ds.TinkerFrame;
 import prerna.engine.api.IHeadersDataRow;
@@ -13,12 +15,26 @@ public class TinkerImportDataReactor extends ImportDataReactor{
 
 	@Override
 	public Iterator process() {
-		// use the import data reactor to go through the logic to get the necessary data 
-		super.process();
-		
-		// get all the appropriate values
 		// get the frame
 		TinkerFrame frame = (TinkerFrame) myStore.get("G");
+
+		// additional logic required for loops
+		// we need to also take into consideration a meta level header
+		// that doesn't match the actual instance name
+		// i.e. we create a meta level System_1 vertex
+		// but the instances are still only referenced as System
+		Vector<Map<String, String>> joins = (Vector<Map<String, String>>) myStore.get(PKQLEnum.JOINS);
+		if(joins!=null){
+			for(Map<String,String> join : joins) {
+				String fromCol = join.get(PKQLEnum.FROM_COL);
+				// here we replace the to column with the actual instance name
+				fromCol = frame.getValueForUniqueName(fromCol);
+				join.put(PKQLEnum.FROM_COL, fromCol);
+			}
+		}
+		
+		// use the import data reactor to go through the logic to get the necessary data 
+		super.process();
 		
 		// cardinality helps determine the relationship between the instance values in a column
 		// this is helpful in optimizing the extraction of the upstream node and downstream node when creating
