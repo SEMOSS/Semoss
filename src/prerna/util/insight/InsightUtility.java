cf6f91cf73d4de5859c839001c57301f55d37535
rluthar <rluthar@deloitte.com>
2017-03-01 18:17:07 +0000

    UPDATE ::: adding prerna.util.insight package, adding insight utility class so that pkql commands can be executed on backend without rest call
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@18997 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 102f8e15ee2f38756d83aa584fd41a1c7c992a45

---
+++ src/prerna/util/insight/InsightUtility.java
@@ -0,0 +1,112 @@
+package prerna.util.insight;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import javax.ws.rs.core.Response;
+
+import prerna.ds.h2.H2Frame;
+import prerna.ds.r.RDataTable;
+import prerna.om.Dashboard;
+import prerna.om.Insight;
+import prerna.om.InsightStore;
+import prerna.sablecc.PKQLRunner;
+import prerna.ui.components.playsheets.datamakers.IDataMaker;
+import prerna.ui.components.playsheets.datamakers.ISEMOSSTransformation;
+import prerna.ui.components.playsheets.datamakers.PKQLTransformation;
+
+public class InsightUtility {
+
+	/**
+	 * 
+	 * @param insight
+	 * @param pkqlCmd
+	 * @return
+	 * 
+	 * Runs a pkql command on an insight
+	 */
+	public static Map runPkql(Insight insight, String pkqlCmd) {
+		PKQLTransformation pkql = new PKQLTransformation();
+		Map<String, Object> props = new HashMap<String, Object>();
+		props.put(PKQLTransformation.EXPRESSION, pkqlCmd);
+		pkql.setProperties(props);
+		PKQLRunner runner = insight.getPKQLRunner();
+		pkql.setRunner(runner);
+		List<ISEMOSSTransformation> list = new Vector<ISEMOSSTransformation>();
+		list.add(pkql);
+
+		Map resultHash = null;
+		//synchronize applyCalc calls for each insight to prevent interference during calculation
+		synchronized(insight) {
+			insight.processPostTransformation(list);
+			insight.syncPkqlRunnerAndFrame(runner);
+			resultHash = insight.getPKQLData(true);
+		}
+		
+		return resultHash;
+	}
+	
+	/**
+	 * 
+	 * @param insightId
+	 * @param pkqlCmd
+	 * @return
+	 * 
+	 * Runs a pkql command on an insight with the given insight ID
+	 */
+	public static Map runPkql(String insightId, String pkqlCmd) {
+		Insight insight = InsightStore.getInstance().get(insightId);
+		return runPkql(insight, pkqlCmd);
+	}
+	
+	/**
+	 * 
+	 * @param insight
+	 * @param sessionId
+	 * @return
+	 * 
+	 * Static Utility Method to drop an Insight
+	 */
+	public static boolean dropInsight(Insight insight, String sessionId) {
+		String insightID = insight.getInsightID();
+//		if(insight.isJoined()){
+//			insight.unJoin();
+//		}
+		boolean success = InsightStore.getInstance().remove(insightID);
+		if(sessionId != null) {
+			InsightStore.getInstance().removeFromSessionHash(sessionId, insightID);
+		}
+		
+		IDataMaker dm = insight.getDataMaker();
+		if(dm instanceof H2Frame) {
+			H2Frame frame = (H2Frame)dm;
+			frame.closeRRunner();
+			frame.dropTable();
+			if(!frame.isInMem()) {
+				frame.dropOnDiskTemporalSchema();
+			}
+		} else if(dm instanceof RDataTable) {
+			RDataTable frame = (RDataTable)dm;
+			frame.closeConnection();
+		} else if(dm instanceof Dashboard) {
+			Dashboard dashboard = (Dashboard)dm;
+			dashboard.dropDashboard();
+		}
+		
+		// also see if other variables in runner that need to be dropped
+		PKQLRunner runner = insight.getPKQLRunner();
+		runner.cleanUp();
+		
+		// native frame just holds a QueryStruct on an engine
+		// nothing to do
+//		else if(dm instanceof NativeFrame) {
+//			NativeFrame frame = (NativeFrame) dm;
+//			frame.close();
+//		} 
+
+		return success;
+	}
+	
+}
