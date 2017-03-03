d8858bb93fe54b8ae37e1ee880a57c885dd2650c
Maher Khalil <themaherkhalil@gmail.com>
2017-03-03 16:25:11 +0000

    UPDATE ::: Adding r reactor for determining duplicates
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19051 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 66cbd49261b73855c7685335c82637ddeaefc42c

---
+++ src/prerna/sablecc/RDuplicatesReactor.java
@@ -0,0 +1,41 @@
+package prerna.sablecc;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+import prerna.ds.r.RDataTable;
+import prerna.sablecc.PKQLRunner.STATUS;
+
+public class RDuplicatesReactor extends DataFrameDuplicatesReactor {
+
+	@Override
+	public Iterator process() {
+		// This reactor checks for duplicates
+		boolean hasDuplicates = false;
+		RDataTable dataframe = (RDataTable) myStore.get("G");
+		String rVarName = dataframe.getTableVarName();
+		Vector<String> columns = (Vector) myStore.get(PKQLEnum.COL_CSV);
+
+		StringBuilder columnSelectBuilder = new StringBuilder("c(");
+		int numColumns = columns.size();
+		for(int i = 0; i < numColumns; i++) {
+			if( (i+1)  == numColumns) {
+				columnSelectBuilder.append("\"").append(columns.get(i)).append("\"");
+			} else {
+				columnSelectBuilder.append("\"").append(columns.get(i)).append("\", ");
+			}
+		}
+		columnSelectBuilder.append(")");
+		String colSelect = columnSelectBuilder.toString();
+		
+		int val1 = dataframe.getNumRows(rVarName + "[ ," + colSelect + "]");
+		int val2 = dataframe.getNumRows("unique( " + rVarName + "[ ," + colSelect + "] )");
+
+		hasDuplicates = val1 != val2;
+
+		myStore.put("hasDuplicates", hasDuplicates);
+		myStore.put("RESPONSE", STATUS.SUCCESS.toString());
+		myStore.put("STATUS", STATUS.SUCCESS);
+		return null;
+	}
+}
