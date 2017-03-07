e22e73a9657d44ceef6ebf804a981ac4a922847f
adaclarke <adaclarke@deloitte.com>
2017-03-07 15:56:44 +0000

    Added Site to the regular ICD file **HAS BUGS**
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19094 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ui/components/specific/tap/forms/FormsSiteICDDataProcessor.java
+++ src/prerna/ui/components/specific/tap/forms/FormsSiteICDDataProcessor.java
@@ -55,45 +55,22 @@ import prerna.util.Utility;
 
 public class FormsSiteICDDataProcessor extends BaseFormsICDDataProcessor {
 	public static final Logger LOGGER = LogManager.getLogger(FormsSiteICDDataProcessor.class.getName());	
-	public int SITE_COL_NUM = 4;
+	//public int SITE_COL_NUM = 6;
 	
 	public FormsSiteICDDataProcessor(){
-		PROVIDER_COL_NUM = 5;
-		CONSUMER_COL_NUM = 6;
-		INTERFACE_NAME_COL_NUM = 8;
-		PROTOCOL_COL_NUM = 10;
-		FREQUENCY_COL_NUM = 11;
-		FORMAT_COL_NUM = 12;
-		ICD_DATA_OBJECT_STARTING_COL_NUM = 35;
-		MY_QUERY = SYSTEM_INTERFACE_SITE_SPECIFIC_QUERY;
-		INTERFACES_FILE = "\\ICD SiteSpecific.xlsm";
-		
-		ICD_FREQUENCY_STARTING_COL_NUM = 16;
-		ICD_FREQUENCY_ENDING_COL_NUM = 23;
-		ICD_PROTOCOL_STARTING_COL_NUM = 24;
-		ICD_PROTOCOL_ENDING_COL_NUM = 42;
-		ICD_FORMAT_STARTING_COL_NUM = 43;
-		ICD_FORMAT_ENDING_COL_NUM = 60;
+		//SITE_COL_NUM = 6;
+
+		//MY_QUERY = SYSTEM_INTERFACE_SITE_SPECIFIC_QUERY;
 	}
 	
-	public int getICDColumnNumber(String name){
-		if(name.equalsIgnoreCase("Site")){
-			return SITE_COL_NUM;
-		}
-		else {
-			return super.getICDColumnNumber(name);
-		}
-	}
+	// public int getICDColumnNumber(String name){
+	// 	if(name.equalsIgnoreCase("Site")){
+	// 		return SITE_COL_NUM;
+	// 	}
+	// 	else {
+	// 		return super.getICDColumnNumber(name);
+	// 	}
+	// }
 	
-	public void addInterfaceName(XSSFRow row, String key1){
-		//LOGGER.info("*********** key1: " + key1);
-		String[] strs = key1.split("%");
-		//LOGGER.info("*********** strs 1: " + strs[0]);
-		//LOGGER.info("*********** strs 2: " + strs[1]);
-		XSSFCell cell = row.createCell(INTERFACE_NAME_COL_NUM);
-		cell.setCellValue(strs[0]);
-		
-		XSSFCell cell1 = row.createCell(SITE_COL_NUM);
-		cell1.setCellValue(strs[1]);
-	}
+
 }
