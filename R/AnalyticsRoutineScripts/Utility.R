96b618cb951391346799b2ede5506fec53f8546c
tbanach <tbanach@deloitte.com>
2017-03-21 22:45:55 +0000

    BUGFIX ::: For the semantic matcher: Returning an empty frame that says no matches when no matches are generated (instead of throwing an error). Also making the frame name used long and unique to avoid conflicts with other r objects in memory.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19414 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d312aa6d707731b7dd3890629db0604716585134

--- R/AnalyticsRoutineScripts/Utility.R
+++ R/AnalyticsRoutineScripts/Utility.R
@@ -9,39 +9,72 @@ GetResult <- function() {
 }
 
 # Function to run locality sensitive hashing match
-run_lsh_matching <- function(path,N,b,cutoff){
+run_lsh_matching <- function(path, N, b, cutoff, delimiter){
+  
+  # Library the necessary packages
   library(textreuse)
+  library(data.table)
+  
+  # Create a minhash generator
   corpus_minhash <- minhash_generator(n = N, seed = 953)
-  corpus <- TextReuseCorpus(dir = path,tokenizer = tokenize_ngrams, n = 1,minhash_func = corpus_minhash)
   
+  # Throws an error if there are no files in the corpus
+  corpus <- tryCatch({
+    TextReuseCorpus(dir = path,tokenizer = tokenize_ngrams, n = 1,minhash_func = corpus_minhash)
+  }, error = function(e) {
+    return("error")
+  })
+
+  # Return an empty frame if there was an error
+  if (!is.list(corpus)) {
+    df <- data.frame(item_engine=character(1), item_concept=character(1), match_engine=character(1), match_concept=character(1), score=numeric(1), stringsAsFactors=FALSE)
+    df[1, 1] <- "(No Concepts Found)"
+    return(df)
+  }
+
+  # Determine candidates
   buckets <- lsh(corpus, bands = b)
-  
   candidates<-lsh_candidates(buckets)
   subset<-lsh_subset(candidates)
-  
   mycorpus<-corpus[subset]
-  m<-pairwise_compare(mycorpus,ratio_of_matches,directional=TRUE)
   
-  n<-nrow(m)
-  df<-data.frame(item=character(50),match=character(50),score=numeric(5),stringsAsFactors=FALSE)
-  r<-0
-  
-  for (i in 1:n){
-    for (j in 1:n){
-      if(i!=j & m[i,j] > cutoff/2){
-        r<-r+1
-        df[r,2]<-rownames(m)[i]
-        df[r,1]<-colnames(m)[j]
-        df[r,3]<-m[i,j]
-      }
-    }
+  # Throws an error when there are not enough concepts to consider matches
+  m <- tryCatch({
+    pairwise_compare(mycorpus, ratio_of_matches, directional=TRUE)
+  }, error = function(e) {
+    return("error")
+  })
+
+  # Return an empty frame if there was an error
+  if (!is.matrix(m)) {
+    df <- data.frame(item_engine=character(1), item_concept=character(1), match_engine=character(1), match_concept=character(1), score=numeric(1), stringsAsFactors=FALSE)
+    df[1, 1] <- "(No Matches Found)"
+    return(df)
   }
+  
+  # Create the data frame
+  df<-pairwise_candidates(m,directional=TRUE)
+  df<-df[c(2,1,3)]
+  names(df)<-c("item","match","score")
   df<-df[df$score > cutoff,]
   df<-df[order(df$item,-df$score,df$match),]
+
+  # Split out the engine and concept
   setDT(df)
+  df[, c("item_engine", "item_concept") := tstrsplit(item, delimiter, fixed=TRUE)]
+  df[, c("match_engine", "match_concept") := tstrsplit(match, delimiter, fixed=TRUE)]
+  
+  # Delete redundant columns
+  df[, item:=NULL]
+  df[, match:=NULL]
   return(df)
 }
 
 # For test cases
-#path<-"C:/Users/tbanach/Workspace/SemossDev/R/MatchingRepository";
-#run_lsh_matching(path,210,70,0.5)
+#path <- "C:\\Users\\tbanach\\Documents\\SEMOSS Dev Projects\\Semantic Matching\\TAP_Core_Data_Corpus"
+#path <- "C:\\Users\\tbanach\\Workspace\\SemossDev\\R\\MatchingRepository"
+#N <- 200
+#b <- 40
+#cutoff <- 0.5
+#delimiter = ";"
+#run_lsh_matching(path, 210, 70, 0.5, ";")
