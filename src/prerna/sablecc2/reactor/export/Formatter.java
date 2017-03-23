2596b061771cdf0eda4211e3aa2a6c24288be7fe
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-03-23 21:04:26 +0000

    UPDATE ::: Payload progress
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19483 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/export/Formatter.java
+++ src/prerna/sablecc2/reactor/export/Formatter.java
@@ -1,10 +1,14 @@
 package prerna.sablecc2.reactor.export;
 
+import java.util.HashMap;
 import prerna.engine.api.IHeadersDataRow;
 
 public interface Formatter {
-
+		
 	public void addData(IHeadersDataRow nextData);
 	
 	public Object getFormattedData();
+	
+	void addHeader(String[] keys);
+	//void addHeader(String varKey, String type, String vizType);
 }
