abdd264c67178370ddd520bb01d105172f5d5427
rluthar <rluthar@deloitte.com>
2017-03-24 06:20:19 +0000

    UPDATE ::: grabbing frame limit variable from utility method instead of directly from DIHelper, not sending back duplicative data in payload for data.output
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19485 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 93e97a66c4589e0fbe3637cb1cabd910df8815d4

--- src/prerna/sablecc2/reactor/imports/MetaH2Importer.java
+++ src/prerna/sablecc2/reactor/imports/MetaH2Importer.java
@@ -11,6 +11,7 @@ import prerna.sablecc.PKQLEnum;
 import prerna.util.ArrayUtilityMethods;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
+import prerna.util.Utility;
 
 public class MetaH2Importer extends Importer{
 
@@ -26,11 +27,12 @@ public class MetaH2Importer extends Importer{
 				
 		// perform additional logic regarding if the frame should be ported on-disk
 		
-		int LIMIT_SIZE = 10_000;
-		String limitSize = (String) DIHelper.getInstance().getProperty(Constants.H2_IN_MEM_SIZE);
-		if(limitSize != null) {
-			LIMIT_SIZE = Integer.parseInt( limitSize.trim() );
-		}
+		int LIMIT_SIZE = Utility.getLimitSize();
+//		int LIMIT_SIZE = 10_000;
+//		String limitSize = (String) DIHelper.getInstance().getProperty(Constants.H2_IN_MEM_SIZE);
+//		if(limitSize != null) {
+//			LIMIT_SIZE = Integer.parseInt( limitSize.trim() );
+//		}
 
 		boolean overLimit = false;
 		int numNewRecords = -1;
