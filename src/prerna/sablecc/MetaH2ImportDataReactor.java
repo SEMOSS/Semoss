afce443619ccbb0eb94fe5a7d29d00258eaec018
mahkhalil <mahkhalil@deloitte.com>
2017-03-24 13:48:43 +0000

    UPDATE ::: Shifting utility method to rdbms specific class.  Removing duplicate classes
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19489 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 32289ea30816b7d1231ee4764099766c74cd684f

--- src/prerna/sablecc/MetaH2ImportDataReactor.java
+++ src/prerna/sablecc/MetaH2ImportDataReactor.java
@@ -6,11 +6,9 @@ import java.util.Set;
 
 import prerna.ds.h2.H2Frame;
 import prerna.ds.util.FileIterator;
+import prerna.ds.util.RdbmsFrameUtility;
 import prerna.engine.api.IRawSelectWrapper;
 import prerna.util.ArrayUtilityMethods;
-import prerna.util.Constants;
-import prerna.util.DIHelper;
-import prerna.util.Utility;
 
 public class MetaH2ImportDataReactor extends ImportDataReactor{
 
@@ -25,14 +23,7 @@ public class MetaH2ImportDataReactor extends ImportDataReactor{
 		frame = (H2Frame) myStore.get("G");
 				
 		// perform additional logic regarding if the frame should be ported on-disk
-		
-		int LIMIT_SIZE = Utility.getLimitSize();
-//		int LIMIT_SIZE = 10_000;
-//		String limitSize = (String) DIHelper.getInstance().getProperty(Constants.H2_IN_MEM_SIZE);
-//		if(limitSize != null) {
-//			LIMIT_SIZE = Integer.parseInt( limitSize.trim() );
-//		}
-
+		int LIMIT_SIZE = RdbmsFrameUtility.getLimitSize();
 		boolean overLimit = false;
 		int numNewRecords = -1;
 
