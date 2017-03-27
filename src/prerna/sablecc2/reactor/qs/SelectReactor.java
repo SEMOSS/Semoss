5c37154633a6d5b51122377341c7e782f765d731
rluthar <rluthar@deloitte.com>
2017-03-27 15:15:15 +0000

    BUGFIX ::: fixing concurrency issue with dashboards when converting an in-mem h2frame to an ondisk H2FRAME UPDATE ::: adding string building methods to RdbmsQueryBuilder, adding comments to QueryStructReactor and selectreactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19516 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8fdb000101d2ab8713e06606cea8c63b4ab0c0d9

--- src/prerna/sablecc2/reactor/qs/SelectReactor.java
+++ src/prerna/sablecc2/reactor/qs/SelectReactor.java
@@ -21,6 +21,10 @@ public class SelectReactor extends QueryStructReactor {
 		//get the selectors
 		GenRowStruct allNouns = getNounStore().getNoun(NounStore.all); //can either be strings or column selectors
 		
+		//if the query struct is supposed to be used to query a database
+		//selectors are as such:
+		//concept is "concept"
+		//property is "concept__property" where concept is the parent, property is the column name of the property
 		if(isDatabaseQueryStruct()) {
 			for(int selectIndex = 0;selectIndex < allNouns.size();selectIndex++) {
 				String thisSelector = (String)allNouns.get(selectIndex);
@@ -34,7 +38,11 @@ public class SelectReactor extends QueryStructReactor {
 					qs.addSelector(thisSelector, null);
 				}
 			}
-		} else {
+		} 
+		
+		//Otherwise we are going to use the query struct for a frame in which case
+		//EVERY column will be referenced as Table__Column
+		else {
 			H2Frame frame = (H2Frame)planner.getFrame();
 			String tableName = frame.getBuilder().getTableName();
 			for(int selectIndex = 0;selectIndex < allNouns.size();selectIndex++) {
@@ -69,15 +77,20 @@ public class SelectReactor extends QueryStructReactor {
 		if(result != null && result.getMeta(0).toString().equals("QUERYSTRUCT")) {
 			NounMetadata storedResult = (NounMetadata)result.get(0);
 			if(storedResult.getValue() instanceof QueryStruct2) {
+				
+				//if no engine name in query struct, we will use it for a frame
 				if( ((QueryStruct2)storedResult.getValue()).getEngineName() == null) {
 					return false;
 				} else {
 					return true;
 				}
 			} else {
+				
+				//if we have no query struct, we will default to this being used on a frame
 				return false;
 			}
 		}
+		//if we have no query struct, we will default to this being used on a frame
 		return false;
 	}	
 }
