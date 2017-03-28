c7b532a7faa3d281cf53ffe8d57ed8c80a2fa313
rluthar <rluthar@deloitte.com>
2017-03-28 19:27:32 +0000

    UPDATE ::: adding method on ITable to query the frame using QueryStruct2..this will eventually replace QueryStruct, adding some comments in querystruct2, translation - formatting logging
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19580 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e0a101e230710c60294ff62189bd2e3145c7afb1

--- src/prerna/sablecc2/reactor/imports/MergeDataReactor.java
+++ src/prerna/sablecc2/reactor/imports/MergeDataReactor.java
@@ -37,36 +37,15 @@ public class MergeDataReactor extends AbstractReactor {
 		return null;
 	}
 	
+	@Override
 	public void updatePlan() {
-		//just need to set the frame to the planner here...that's it
-		getType();
-		Enumeration <String> keys = store.nounRow.keys();
-		
-		String reactorOutput = reactorName;
-		
-		while(keys.hasMoreElements())
-		{
-			String singleKey = keys.nextElement();
-			GenRowStruct struct = store.nounRow.get(singleKey);
-			Vector <String> inputs = struct.getAllColumns();
-			
-			// need a better way to do it
-			if(asName == null)
-				reactorOutput = reactorOutput + "_" + struct.getColumns();
-	
-			// find if code exists
-			if(!propStore.containsKey("CODE"))
-			{
-				if(inputs.size() > 0)
-					planner.addInputs(signature, inputs, type);
-			}
-		}
+
 	}
 
 
 	@Override
 	protected void mergeUp() {
-		//this reactor should not need to merge up
+
 	}
 	
 	private void importToFrame()  {
@@ -83,12 +62,12 @@ public class MergeDataReactor extends AbstractReactor {
 		SQLInterpreter2 interp;
 		Iterator<IHeadersDataRow> iterator;
 		if(engineName != null) {
-			IEngine engine = Utility.getEngine(removeQuotes(engineName.trim()));
+			IEngine engine = Utility.getEngine(engineName.trim());
 			interp = new SQLInterpreter2(engine);
 			interp.setQueryStruct(queryStruct);
 			String importQuery = interp.composeQuery();
 			iterator = WrapperManager.getInstance().getRawWrapper(engine, importQuery);
-			curReactor.put(PKQLEnum.API + "_ENGINE", removeQuotes(engineName.trim()));
+			curReactor.put(PKQLEnum.API + "_ENGINE", engineName.trim());
 		} else {
 			interp = new SQLInterpreter2();
 			interp.setQueryStruct(queryStruct);
@@ -123,28 +102,12 @@ public class MergeDataReactor extends AbstractReactor {
 			return (QueryStruct2)object.getValue();
 		} 
 		
-//		else {
-//			NounMetadata result = (NounMetadata)this.planner.getProperty("RESULT", "RESULT");
-//			if(result.getNounName().equals("QUERYSTRUCT")) {
-//				queryStruct = (QueryStruct2)result.getValue();
-//			}
-//		}
 		return queryStruct;
 	}
 	
 	@Override
 	public List<NounMetadata> getInputs() {
-		//1 query OR 1 queryStruct
-		//n joins
 		return null;
-	}
-
-	
-	private String removeQuotes(String value) {
-		if(value.startsWith("'") || value.startsWith("\"")) {
-			value = value.trim().substring(1, value.length() - 1);
-		}
-		return value;
 	}
 	
 	private Vector<Map<String,String>> getJoinCols(GenRowStruct joins) {
