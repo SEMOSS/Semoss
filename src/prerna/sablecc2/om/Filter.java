87548a3c20a879f2a808c09272441024cda862e6
mahkhalil <mahkhalil@deloitte.com>
2017-03-28 19:48:06 +0000

    UPDATE ::: Building out logic behind if statements to only execute when needed UPDATE ::: Initial building out of filter being able to evaluate and return a boolean (currently only set for basic math expressions without variables) UPDATE ::: Assimilator object is used for basic operations instead of evaluating
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19582 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c221df23d7b4576a7e902cab9b90204aff43541a

--- src/prerna/sablecc2/om/Filter.java
+++ src/prerna/sablecc2/om/Filter.java
@@ -1,5 +1,11 @@
 package prerna.sablecc2.om;
 
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtNewMethod;
+import javassist.NotFoundException;
+
 public class Filter {
 
 	private String comparator = null; //'=', '!=', '<', '<=', '>', '>=', '?like'
@@ -23,5 +29,58 @@ public class Filter {
 	
 	public String getComparator() {
 		return this.comparator;
+	}
+
+	public boolean evaluate() {
+		ClassPool pool = ClassPool.getDefault();
+		// generate the method
+		StringBuilder method = new StringBuilder();
+		
+		String lString = getString(lComparison);
+		String rString = getString(rComparison);
+		
+		method.append("public boolean evaluate() {");
+		method.append("if(" + lString + this.comparator + rString + ") {"
+				+ "return true;"
+				+ "} else {"
+				+ "return false;"
+				+ "}"
+				+ "}");
+		
+		boolean evaluteResult = false;
+		String packageName = "t" + System.currentTimeMillis(); // make it unique
+		CtClass cc = pool.makeClass(packageName + ".c" + System.currentTimeMillis());
+		try {
+			cc.setSuperclass(pool.get("prerna.sablecc2.om.FilterEvaluator"));
+			cc.addMethod(CtNewMethod.make(method.toString(), cc));
+			Class retClass = cc.toClass();
+			FilterEvaluator c = (FilterEvaluator) retClass.newInstance();
+			evaluteResult = c.evaluate();
+		} catch (CannotCompileException e1) {
+			e1.printStackTrace();
+		} catch (NotFoundException e1) {
+			e1.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+
+		return evaluteResult;
+	}
+	
+	private String getString(GenRowStruct grs) {
+		Object type = grs.get(0);
+		PkslDataTypes metaType = grs.getMeta(0);
+		if(type instanceof Expression) {
+			return ((Expression) type).getExpression();
+		} else {
+			// if it is a constant
+			if(metaType == PkslDataTypes.CONST_STRING) {
+				return "\"" + type.toString() + "\"";
+			} else {
+				return type.toString();
+			}
+		}
 	}
 }
