8f7cf42eb60b6d901d1002ad7223c2b07817ca64
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-03-29 23:36:44 +0000

    UPDATE ::: Forgot to commit this class still working on it
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19634 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: fb3f316a8a6ce23d8d0c7547ee0fa7606deac07c

---
+++ src/prerna/sablecc2/reactor/qs/AsReactor.java
@@ -0,0 +1,47 @@
+package prerna.sablecc2.reactor.qs;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+import prerna.ds.querystruct.QueryStruct2;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.NounStore;
+import prerna.sablecc2.om.PkslDataTypes;
+
+public class AsReactor extends QueryStructReactor {
+
+	@Override
+	QueryStruct2 createQueryStruct() {
+		 //add the inputs from the store as well as this operation
+		// first is all the inputs
+		// really has one job pick the parent.. 
+		// replace the as Name
+		// the as name could come in as an array too
+		// for now I will go with the name
+		Enumeration <String> keys = store.nounRow.keys();
+
+		String [] asNames = null;
+		while(keys.hasMoreElements())
+		{
+			String singleKey = keys.nextElement();
+			GenRowStruct struct = store.nounRow.get(singleKey);
+			Vector <String> inputs = struct.getAllColumns(); // ideally this should get only one column for now
+
+			asNames = new String[1];
+			asNames[0] = inputs.elementAt(0).trim();
+		}
+		if(this.parentReactor != null && asNames != null)
+		{
+			// get the columns on as
+			parentReactor.setAs(asNames);
+		}		
+		
+		//TODO create as for select Reactor
+		NounMetadata noun = new NounMetadata(asNames, PkslDataTypes.ALIAS);
+		planner.addVariable("AS", noun);
+		
+		GenRowStruct allNouns = getNounStore().getNoun(NounStore.all);
+		return qs;
+	}
+}
