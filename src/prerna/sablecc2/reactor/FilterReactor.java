84bf6e0376997131fb41984a1e39770931967b5e
mahkhalil <mahkhalil@deloitte.com>
2017-04-04 12:25:55 +0000

    UPDATE ::: Modification for logic around how filters are handled within the planner
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19704 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ea8b7c420058fc72eb9d9962308158099b50d3b3

--- src/prerna/sablecc2/reactor/FilterReactor.java
+++ src/prerna/sablecc2/reactor/FilterReactor.java
@@ -1,7 +1,6 @@
 package prerna.sablecc2.reactor;
 
 import java.util.List;
-import java.util.Vector;
 
 import prerna.sablecc2.om.Filter;
 import prerna.sablecc2.om.GenRowStruct;
@@ -43,10 +42,33 @@ public class FilterReactor extends AbstractReactor{
 	}
 	
 	@Override
+	public void mergeUp() {
+		// we need to push the filter object to the parent
+		// creation of the filter object is lazy
+		// so it doesn't evaluate yet
+		// so we will just call execute to get the object
+		NounMetadata filterNoun = (NounMetadata) execute();
+		// and we will just push this into the cur row of the parent
+		this.parentReactor.getCurRow().add(filterNoun);
+	}
+	
+	@Override
+	public List<NounMetadata> getInputs() {
+		// we do not want this to be added to the planner
+		// as its own OP
+		// since a filter is only useful when evaluated 
+		// within another expression
+		// we will have merge up handle this
+		return null;
+	}
+	
+	@Override
 	public List<NounMetadata> getOutputs() {
-		List<NounMetadata> outputs = new Vector<NounMetadata>();
-		NounMetadata output = new NounMetadata(this.signature, PkslDataTypes.FILTER);
-		outputs.add(output);
-		return outputs;
+		// we do not want this to be added to the planner
+		// as its own OP
+		// since a filter is only useful when evaluated 
+		// within another expression
+		// we will have merge up handle this
+		return null;
 	}
 }
