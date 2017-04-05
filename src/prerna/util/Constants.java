89881e3995471d012757b7bf799af07bc37a24cc
tbanach <tbanach@deloitte.com>
2017-04-05 17:57:34 +0000

    UPDATE ::: Overloading methods in OWLER to allow for the specification of multiple columns per concept. If more than one column is passed in, the concept is given a composite key, comprised of multiple primary keys. The URIs of each of the primary keys associated with the concept are saved to the OWL. The original methods accepting only one column remain as well.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19754 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 600604cfd4e827063e4502dd380a33fec2da2a1f

--- src/prerna/util/Constants.java
+++ src/prerna/util/Constants.java
@@ -518,4 +518,8 @@ public class Constants {
 	// Function name to calculate locality sensitive hashing
 	public static final String R_LSH_MATCHING_FUN = "run_lsh_matching";
 	
+	// Composite key constants
+	public static final String COMPOSITE_KEY_SEPARATOR = "~+~";
+	public static final String COMPOSITE_KEY_TYPE = "COMPOSITE";
+	public static final String PRIMARY_META_KEY = "URI:PRIMARY_KEY";	
 }
