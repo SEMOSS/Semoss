2fb4b77f3870146568a70f63aa5195fca9e16c0d
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-04-10 02:49:40 +0000

    UPDATE ::: fuzzy matching r script
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19836 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ R/FuzzyMatching/best_match.r
@@ -0,0 +1,49 @@
+match_metrics <- function(df){
+library(stringdist)
+c<-ncol(df)
+c1<-as.character(unique(df[,1]))
+c2<-unique(df[,2])
+d<-as.character(c2)
+
+r <- data.frame(item=character(), fuzzyMatch=character(),similarity=numeric(),method = character(),stringsAsFactors=FALSE)
+r<-best_match(r,c1,d,method="lv",q=1,p=0)
+r<-best_match(r,c1,d,method="dl",q=1,p=0)
+r<-best_match(r,c1,d,method="qgram",q=2,p=0)
+r<-best_match(r,c1,d,method="cosine",q=2,p=0)
+r<-best_match(r,c1,d,method="osa",q=1,p=0)
+r<-best_match(r,c1,d,method="jw",q=1,p=0.1)
+r<-best_match(r,c1,d,method="lcs",q=1,p=0)
+r<-best_match(r,c1,d,method="soundex",q=1,p=0)
+r<-best_match(r,c1,d,method="jaccard",q=2,p=0)
+return(r)
+}
+
+best_match <- function(r,c1,d,method=method,q,p){
+methods<-list()
+methods[["lv"]]<-"Levenshtein"
+methods[["dl"]]<-"Damerau-Levenshtein"
+methods[["qgram"]]<-"q-gram"
+methods[["cosine"]]<-"Cosine q-gram"
+methods[["osa"]]<-"Optimal string aligment"
+methods[["jw"]]<-"Jaro-Winker"
+methods[["lcs"]]<-"Longest common substring"
+methods[["jaccard"]]<-"Jaccard q-gram"
+methods[["soundex"]]<-"Soundex"
+n<-length(c1)
+m<-stringdistmatrix(c1,d,method=method,q=q,p=p)
+
+j<-nrow(r)
+for (i in 1:n){
+  i.min<-min(m[i,])
+  j<-j+1
+  ind<-which(m[i,]== i.min)[1]
+  sim<-stringsim(c1[i],d[ind],method=method,q=q,p=p)
+  r[j,1]<-c1[i]
+  r[j,2]<-d[ind] 
+  r[j,3]<-sim
+  r[j,4]<-methods[[method]]  
+}
+r$similarity<-round(r$similarity,2)
+r<-r[order(r$item,r$fuzzyMatch,-r$similarity,r$method),]
+return(r)
+}
