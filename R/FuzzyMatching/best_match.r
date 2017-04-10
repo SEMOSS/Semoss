420a89d637c9e1c5f234e8df81db5a60c207d534
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-04-10 21:45:03 +0000

    UPDATE ::: New script to change Fuzzy Matching headers
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19883 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 7253cdf27a08c350bafbe737a41808dcdfa18de3

--- R/FuzzyMatching/best_match.r
+++ R/FuzzyMatching/best_match.r
@@ -5,7 +5,7 @@ c1<-as.character(unique(df[,1]))
 c2<-unique(df[,2])
 d<-as.character(c2)
 
-r <- data.frame(item=character(), fuzzyMatch=character(),similarity=numeric(),method = character(),stringsAsFactors=FALSE)
+r <- data.frame(item=character(), match=character(),similarity=numeric(),appliedmethod = character(),stringsAsFactors=FALSE)
 r<-best_match(r,c1,d,method="lv",q=1,p=0)
 r<-best_match(r,c1,d,method="dl",q=1,p=0)
 r<-best_match(r,c1,d,method="qgram",q=2,p=0)
@@ -34,7 +34,7 @@ m<-stringdistmatrix(c1,d,method=method,q=q,p=p)
 
 j<-nrow(r)
 for (i in 1:n){
-  i.min<-min(m[i,])
+  i.min<-min(m[i,],na.rm=TRUE)
   j<-j+1
   ind<-which(m[i,]== i.min)[1]
   sim<-stringsim(c1[i],d[ind],method=method,q=q,p=p)
@@ -44,6 +44,10 @@ for (i in 1:n){
   r[j,4]<-methods[[method]]  
 }
 r$similarity<-round(r$similarity,2)
-r<-r[order(r$item,r$fuzzyMatch,-r$similarity,r$method),]
+r<-r[order(r$item,-r$similarity,r$match,r$appliedmethod),]
 return(r)
 }
+
+df<-read.csv("C:/fuzzyMatching.csv")
+match_metrics(df)
+
