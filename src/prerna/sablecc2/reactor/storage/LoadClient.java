11c78bf01479595131e9b9c089c9bd4ff046df95
rluthar <rluthar@deloitte.com>
2017-04-11 19:20:36 +0000

    UPDATE ::: adding a getiterator method on the job, implementing get iterator method on load client
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19926 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/storage/LoadClient.java
+++ src/prerna/sablecc2/reactor/storage/LoadClient.java
@@ -1,9 +1,14 @@
 package prerna.sablecc2.reactor.storage;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import prerna.sablecc2.om.Job;
 import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.PkslDataTypes;
 import prerna.sablecc2.reactor.AbstractReactor;
 
 public class LoadClient extends AbstractReactor {
@@ -33,5 +38,16 @@ public class LoadClient extends AbstractReactor {
 		//TODO: need to implement
 		
 		return null;
+	}
+	
+	private Iterator getIterator() {
+		List<Object> jobs = this.curRow.getColumnsOfType(PkslDataTypes.JOB);
+		if(jobs != null && jobs.size() > 0) {
+			Job job = (Job)jobs.get(0);
+			return job.getIterator();
+		}
+
+		Job job = (Job)this.getNounStore().getNoun(PkslDataTypes.JOB.toString()).get(0);
+		return job.getIterator();
 	}
 }
