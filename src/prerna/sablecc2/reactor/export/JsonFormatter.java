3b0a169c21c5a286bc3fcb2a818d515149900f1f
mahkhalil <mahkhalil@deloitte.com>
2017-04-11 12:12:37 +0000

    UPDATE ::: Adding abstract formatter to handle basic setters/getters UPDATE ::: Adding base version of a key-value formatter
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19893 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/export/JsonFormatter.java
+++ src/prerna/sablecc2/reactor/export/JsonFormatter.java
@@ -1,10 +1,8 @@
 package prerna.sablecc2.reactor.export;
 
-import java.util.HashMap;
-
 import prerna.engine.api.IHeadersDataRow;
 
-public class JsonFormatter implements Formatter {
+public class JsonFormatter extends AbstractFormatter {
 
 	@Override
 	public void addData(IHeadersDataRow nextData) {
@@ -17,27 +15,8 @@ public class JsonFormatter implements Formatter {
 	}
 
 	@Override
-	public void addHeader(String[] keys) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void clear() {
 		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public void setIdentifier(String name) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public String getIdentifier() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
--- src/prerna/sablecc2/reactor/export/TableFormatter.java
+++ src/prerna/sablecc2/reactor/export/TableFormatter.java
@@ -1,21 +1,14 @@
 package prerna.sablecc2.reactor.export;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import prerna.algorithm.api.ITableDataFrame;
 import prerna.engine.api.IHeadersDataRow;
-import prerna.sablecc2.om.GenRowStruct;
 
-public class TableFormatter implements Formatter {
+public class TableFormatter extends AbstractFormatter {
 
 	private List<Object[]> data;
-	private String[] headers;
-	private String name;
-//	private ArrayList<HashMap<String, Object>> headers;
-	
+
 	public TableFormatter() {
 		data = new ArrayList<>(100);
 	}
@@ -32,11 +25,6 @@ public class TableFormatter implements Formatter {
 //		return returnData;
 		return data;
 	}
-	
-	@Override
-	public void addHeader(String[] keys) {
-		this.headers = keys;
-	}
 
 	@Override
 	public void clear() {
@@ -44,14 +32,4 @@ public class TableFormatter implements Formatter {
 		headers = null;
 	}
 
-	@Override
-	public void setIdentifier(String name) {
-		// TODO Auto-generated method stub
-		this.name = name;
-	}
-	
-	@Override
-	public String getIdentifier() {
-		return this.name;
-	}
 }
