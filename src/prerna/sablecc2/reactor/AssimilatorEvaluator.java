a1e8b9be41055f602872df6594b261baf3f6917f
mahkhalil <mahkhalil@deloitte.com>
2017-04-11 04:01:17 +0000

    UPDATE ::: Modifying the assimilator to actually evaluate to a value instead of pushing an expression BUGFIX ::: Had to revert previous inATerm and outATerm until we account for select reactors
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19888 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 0f5b4c2eec556639831b7873b3119c85d4f242b5

---
+++ src/prerna/sablecc2/reactor/AssimilatorEvaluator.java
@@ -0,0 +1,22 @@
+package prerna.sablecc2.reactor;
+
+/**
+ * This is just a internal class
+ * so that when we compile to execute the assimilator
+ * we can have a method to call based on the super that is 
+ * assigned to the new class
+ *
+ */
+public abstract class AssimilatorEvaluator {
+
+	public AssimilatorEvaluator() {
+		
+	}
+	
+	/**
+	 * Method that return the evaluation of 
+	 * the signature
+	 * @return
+	 */
+	public abstract Object execute();
+}
