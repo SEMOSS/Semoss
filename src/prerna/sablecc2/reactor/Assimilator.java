2ce9914913cebe79d9cf9293e88ca8c71fe36615
mahkhalil <mahkhalil@deloitte.com>
2017-04-11 04:01:17 +0000

    UPDATE ::: Modifying the assimilator to actually evaluate to a value instead of pushing an expression BUGFIX ::: Had to revert previous inATerm and outATerm until we account for select reactors
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19888 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 0f5b4c2eec556639831b7873b3119c85d4f242b5

--- src/prerna/sablecc2/reactor/Assimilator.java
+++ src/prerna/sablecc2/reactor/Assimilator.java
@@ -1,49 +1,24 @@
 package prerna.sablecc2.reactor;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.Vector;
 
-import prerna.sablecc2.om.Expression;
-import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PkslDataTypes;
 
-/**
- * 	NounStore:
- *		key -> GenrowStruct
- *		where each key is a signature and the GenRowStruct contains either a string, lambda, or expression 
- *		
- *		so for the case of 2 + ( 3 + X(5) )
- *		
- *		AssimilatorA's NounStore:
- *		'left' -> '2'
- *		'right' -> '( 3 + X(5))'
- *		'operation' -> '+'
- *		'2' -> '2'
- *		'(3 + X (5))' -> AssimilatorB
- *			
- *			AssimilatorB's NounStore
- *			'left' -> '3'
- *			'right' -> 'X(5)'
- *			'operation' -> '+'
- *			'3' -> '3'
- *			'X(5)' -> Lambda
- *		
- *		Out() method will produce an expression by iterating recursively through the left and right to produce a single expression which can be executed
- */
 public class Assimilator extends AbstractReactor {
-	
+
 	// roles of the assimilator is simple, just assimilate an expression and then
 	// plug it into the parent
 	// filter is a good example of assimilator for example
 
-	private Expression thisExpression;
-	private NounMetadata leftExpression;
-	private NounMetadata rightExpression;
+	private boolean containsStringValue = false;
 	
 	@Override
 	public void In() {
-        curNoun("all");
+		curNoun("all");
 	}
 
 	@Override
@@ -53,125 +28,112 @@ public class Assimilator extends AbstractReactor {
 		// the data to set into the parent
 		return parentReactor;
 	}
-	
+
 	@Override
 	public NounMetadata execute() {
-		if(this.thisExpression == null) {
-			this.thisExpression = getExpression();
+		// evaluate the assimilator as an object
+		ClassMaker maker = new ClassMaker();
+		// keep a string to generate the method to execute that will
+		// return an object that runs the expression
+		StringBuilder expressionBuilder = new StringBuilder();
+		expressionBuilder.append("public Object execute(){");
+		// we need to grab any variables and define them at the top of the method
+		appendVariables(expressionBuilder);
+		// now that the variables are defined
+		// we just want to add the expression as a return
+		if(this.containsStringValue) {
+			expressionBuilder.append("return new String(").append(this.signature).append(");}");
+		} else {
+			expressionBuilder.append("return new Double(").append(this.signature).append(");}");
 		}
-		NounMetadata noun = new NounMetadata(thisExpression, PkslDataTypes.E);
+		maker.addMethod(expressionBuilder.toString());
+		// add a super so we have a base method to execute
+		maker.addSuper("prerna.sablecc2.reactor.AssimilatorEvaluator");
+		Class newClass = maker.toClass();
+
+		// noun object to return
+		// need to cast to get the type of the NounMetadata object
+		NounMetadata noun = null;
+
+		try {
+			AssimilatorEvaluator newInstance = (AssimilatorEvaluator) newClass.newInstance();
+			Object retVal = newInstance.execute();
+			if(this.containsStringValue) {
+				noun = new NounMetadata(retVal.toString(), PkslDataTypes.CONST_STRING);
+			} else {
+				noun = new NounMetadata( ((Number) retVal).doubleValue(), PkslDataTypes.CONST_DECIMAL);
+			}
+		} catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		}
 		return noun;
 	}
 
-	private Expression getExpression() {
-		if(this.thisExpression == null) {
-			List<String> inputColumns = curRow.getAllColumns();
-			String [] allColumns = new String[inputColumns.size()];
-			for(int colIndex = 0;colIndex < inputColumns.size(); colIndex++) {
-				allColumns[colIndex] = inputColumns.get(colIndex);
+	/**
+	 * Append the variables used within the expression
+	 * @param expressionBuilder
+	 */
+	private void appendVariables(StringBuilder expressionBuilder) {
+		List<String> inputColumns = curRow.getAllColumns();
+		// these input columns should be defined at the beginning of the expression
+		// technically, someone can use the same variable multiple times
+		// so need to account for this
+		// ... just add them to a set and call it a day
+		Set<String> uniqueInputs = new HashSet<String>();
+		uniqueInputs.addAll(inputColumns);
+		for(String input : uniqueInputs) {
+			NounMetadata data = planner.getVariable(input);
+			PkslDataTypes dataType = data.getNounName();
+			if(dataType == PkslDataTypes.CONST_DECIMAL) {
+				expressionBuilder.append("double ").append(input).append(" = ").append(data.getValue()).append(";");
+			} else if(dataType == PkslDataTypes.CONST_STRING) {
+				this.containsStringValue = true;
+				expressionBuilder.append("String ").append(input).append(" = \"").append(data.getValue()).append("\";");
+			} else if(dataType == PkslDataTypes.LAMBDA){
+				// in case the variable points to another reactor
+				// that we need to get the value from
+				// evaluate the lambda
+				// object better be a reactor to run
+				Object rVal = data.getValue();
+				if(rVal instanceof IReactor) {
+					NounMetadata newNoun = ((IReactor) rVal).execute(); 
+					PkslDataTypes newDataType = data.getNounName();
+					if(newDataType == PkslDataTypes.CONST_DECIMAL) {
+						expressionBuilder.append("double ").append(input).append(" = ").append(newNoun.getValue()).append(";");
+					} else if(newDataType == PkslDataTypes.CONST_STRING) {
+						this.containsStringValue = true;
+						expressionBuilder.append("String ").append(input).append(" = \"").append(newNoun.getValue()).append("\";");
+					}
+				} else {
+					// this should never ever happen....
+					throw new IllegalArgumentException("Assimilator cannot handle this type if input");
+				}
+			} else {
+				throw new IllegalArgumentException("Assimilator can currently only handle outputs of scalar variables");
 			}
-			// the expression will just store the entire signature and the columns
-			// the columns is added because as we keep going through the parsing
-			// columns definitions just get appended into the GenRowStruct curRow 
-			thisExpression = new Expression(signature, allColumns);
-			thisExpression.setLeft(getSubExpression("LEFT")); //get the left side and set it
-			thisExpression.setRight(getSubExpression("RIGHT")); //get the right side and set it
-			thisExpression.setOperation(getOperation()); //get the operation string and set it
 		}
-		return thisExpression;
-	}
-	
-	/**
-	 * 
-	 * @param key
-	 * @return
-	 * 
-	 * This method recursively builds the expression for each side
-	 */
-	private Object getSubExpression(String key) {
-		//key will be 'LEFT' or 'RIGHT'
-		GenRowStruct exprNoun = this.getNounStore().getNoun(key);
-		
-		//exprNounKey will be the signature of the operation
-		String exprNounKey = (String)exprNoun.get(0);
-		
-		//lets see if we have a lambda stored under the signature
-		GenRowStruct expr = this.getNounStore().getNoun(exprNounKey);
-		
-		//if we don't return the key, this will be the form of '2' or 'x' or something like that
-		if(expr == null) {
-			// how do i know what this is???
-			setInputs(key, exprNounKey, PkslDataTypes.CONST_STRING);
-			return exprNounKey;
-		}
-		
-		//if we have a key it will be either an assimilator or a labda (i.e. reactor) or a nodekey (meaning we need to look again)
-		//Ex: 2 + (3 + Sum(MB))
-		// left will be '2'
-		// right will be '(3 + Sum(MB))'
-		//		but the assimilator will be stored under the key '3 + Sum(MB)' since that is the logical start point for the assimilator in translation
-		//		so we then must redirect '(3 + Sum(MB)' to '3 + Sum(MB)'
-		//		we do this by adding a key/value pair in the nounstore to say: '(3 + Sum(MB)' -> '3 + Sum(MB)'
-		//		this is done in outAFormula of translation
-		//		we identify that this is redirection by the pkslDataType being a NODEKEY
-		while(PkslDataTypes.NODEKEY.equals(expr.getMeta(0))) {
-			exprNounKey = (String)expr.get(0);
-			expr = this.getNounStore().getNoun(exprNounKey);
-		}
-		
-		//We have found an object, if its an assimilator then return the expression that will be built from that assimilator
-		Object exprObj = expr.get(0);
-		if(exprObj instanceof Assimilator) {
-			setInputs(key, ((Assimilator) exprObj).getExpression(), PkslDataTypes.E);
-			return ((Assimilator) exprObj).getExpression();
-		}
-		
-		//else just return the object which in this case is a lambda
-		setInputs(key, expr.get(0), PkslDataTypes.LAMBDA);
-		return expr.get(0);
-	}
-	
-	/**
-	 * Need to set the inputs so we can do this via the planner!
-	 * @param key
-	 * @param value
-	 * @param datatype
-	 */
-	private void setInputs(String key, Object value, PkslDataTypes datatype) {
-		if(key.equalsIgnoreCase("LEFT")) {
-			this.leftExpression = new NounMetadata(value, datatype);
-		} else {
-			this.rightExpression = new NounMetadata(value, datatype);
-
-		}
-	}
-	
-	private String getOperation() {
-		GenRowStruct operation = this.getNounStore().getNoun("OPERATOR");
-		String op = (String)operation.get(0);
-		return op;
 	}
 	
 	public List<NounMetadata> getInputs() {
-		
-		//TODO : do i need to add a check?
 		List<NounMetadata> inputs = new Vector<>();
-		String[] exprInputs = getExpression().getInputs();
+		List<String> exprInputs = curRow.getAllColumns();
 		for(String exprInput : exprInputs) {
-			NounMetadata noun = new NounMetadata(exprInputs, PkslDataTypes.COLUMN);
+			NounMetadata noun = new NounMetadata(exprInput, PkslDataTypes.COLUMN);
 			inputs.add(noun);
 		}
-		
+
 		return inputs;
 	}
-	
+
 	@Override
 	public List<NounMetadata> getOutputs() {
 		List<NounMetadata> outputs = super.getOutputs();
-		if(outputs != null) return outputs;
-		
+		if(outputs != null) {
+			return outputs;
+		}
+
 		outputs = new Vector<NounMetadata>();
-		NounMetadata output = new NounMetadata(this.thisExpression, PkslDataTypes.E);
+		NounMetadata output = new NounMetadata(this.signature, PkslDataTypes.LAMBDA);
 		outputs.add(output);
 		return outputs;
 	}
