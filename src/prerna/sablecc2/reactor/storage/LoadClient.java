76a5be0f9620926d459cf2a3e5b40271fef6e694
mahkhalil <mahkhalil@deloitte.com>
2017-04-12 16:02:47 +0000

    UPDATE ::: Modifying the load client to execute the pksl queries while generating them instead of storing in list UPDATE ::: Modifying the load client to pass in the information instead of hard coding the values
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@19956 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d49443d90d0ea680ac3a89610897881f233f8c51

--- src/prerna/sablecc2/reactor/storage/LoadClient.java
+++ src/prerna/sablecc2/reactor/storage/LoadClient.java
@@ -1,10 +1,9 @@
 package prerna.sablecc2.reactor.storage;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PushbackReader;
-import java.io.StringBufferInputStream;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -21,6 +20,7 @@ import prerna.sablecc2.PlannerTranslation;
 import prerna.sablecc2.lexer.Lexer;
 import prerna.sablecc2.lexer.LexerException;
 import prerna.sablecc2.node.Start;
+import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.Job;
 import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PkslDataTypes;
@@ -34,91 +34,140 @@ public class LoadClient extends AbstractReactor {
 
 	private static final Logger LOGGER = LogManager.getLogger(LoadClient.class.getName());
 
-	//TODO: find a common place to put these
-	public static final String STORE_NOUN = "store";
-	public static final String ENGINE_NOUN = "engine";
-	public static final String CLIENT_NOUN = "client";
-	public static final String SCENARIO_NOUN = "scenario";
-	public static final String VERSION_NOUN = "version";
+	public static final String ASSIGNMENT_NOUN = "assignment";
+	public static final String VALUE_NOUN = "value";
+	public static final String SEPARATOR_NOUN = "separator";
 
 	@Override
 	public void In() {
-        curNoun("all");
+		curNoun("all");
 	}
 
 	@Override
 	public Object Out() {
-		// TODO Auto-generated method stub
+		return this.parentReactor;
+	}
+
+	@Override
+	public NounMetadata execute()
+	{
+		// run through all the results in the iterator
+		// and append them into a new plan
+		PKSLPlanner newPlan = createPlanner();
+
+		// for debugging
+		// right now just print the plan
+		printPlan(newPlan);
+
+
+		//replace this planner with generated planner
+		this.planner = newPlan;
+
 		return null;
 	}
 	
-	 @Override
-     public NounMetadata execute()
-     {
-     
-		 //Create the pkslQueries
-		 List<String> pkslQueries = createPkslQueries();
-		 
-		 //Run them through a PlannerTranslation
-		 //Grab the planner
-		 PKSLPlanner newPlan = createPlanner(pkslQueries);
-		 
-		 
-		 //Print the plan to verify results
-		 printPlan(newPlan);
-		 
-		 
-		 
-		 //replace this planner with generated planner
-         this.planner = newPlan;
-            
-       return null;
-     }
-	
-	private PKSLPlanner createPlanner(List<String> pkslQueries) {
-		PlannerTranslation t = new PlannerTranslation();
-        
-        for(String pkslQuery : pkslQueries) {
-        	Parser p = new Parser(new Lexer(new PushbackReader(new InputStreamReader(new StringBufferInputStream(pkslQuery)))));
-        	Start tree;
+	private PKSLPlanner createPlanner() {
+		// generate our lazy translation
+		// which only ingests the routines
+		// without executing
+		PlannerTranslation plannerT = new PlannerTranslation();
+		
+		// get the iterator we are loading
+		IRawSelectWrapper iterator = (IRawSelectWrapper)getIterator();
+		String[] headers = iterator.getDisplayVariables();
+		
+		int[] assignmentIndices = getAssignmentIndices(headers);
+		int valIndex = getValueIndex(headers);
+		String separator = getSeparator();
+		
+		// as we iterate through
+		// run the values through the planner
+		while(iterator.hasNext()) {
+			IHeadersDataRow nextData = iterator.next();
+			String pkslString = generatePKSLString(nextData.getValues(), assignmentIndices, valIndex, separator);
+			
 			try {
-				tree = p.parse();
-				tree.apply(t);
+				Parser p = new Parser(new Lexer(new PushbackReader(new InputStreamReader(new ByteArrayInputStream(pkslString.getBytes("UTF-8"))))));
+				Start tree = p.parse();
+				tree.apply(plannerT);
 			} catch (ParserException | LexerException | IOException e) {
 				e.printStackTrace();
 			}
-        	
-        }
-        return t.planner;
+		}
+		
+		// grab the planner from the new translation
+		return plannerT.planner;
 	}
 	
-	 //Generate out list of pksl queries based on the data
-	private List<String> createPkslQueries() {
-		List<String> pkslQueries = new ArrayList<>();
-		IRawSelectWrapper iterator = (IRawSelectWrapper)getIterator();
-        String[] headers = iterator.getDisplayVariables();
-        int fieldIndex = ArrayUtilityMethods.arrayContainsValueAtIndex(headers, "FiedName");
-        int formIndex = ArrayUtilityMethods.arrayContainsValueAtIndex(headers, "FormName");
-        int valueIndex = ArrayUtilityMethods.arrayContainsValueAtIndex(headers, "Value_1");
-        while(iterator.hasNext()) {
-          
-          IHeadersDataRow nextData = iterator.next();
-          Object[] values = nextData.getValues();
-          Object field = values[fieldIndex];
-          Object form = values[formIndex];
-          Object value = values[valueIndex];
-          
-          String nextPksl = generatePkslQuery(field, form, value, null);
-          pkslQueries.add(nextPksl);
-        }
-        
-        return pkslQueries;
+	private String generatePKSLString(Object[] values, int[] assignmentIndices, int valIndex, String separator) {
+		StringBuilder pkslBuilder = new StringBuilder();
+		pkslBuilder.append("\"");
+		
+		// add the assignment
+		int numAssignments = assignmentIndices.length;
+		for(int i = 0; i < numAssignments; i++) {
+			// add the name
+			pkslBuilder.append(values[assignmentIndices[i]]);
+			if( (i+1) != numAssignments) {
+				// concatenate the multiple ones with the defined value
+				pkslBuilder.append(separator);
+			}
+		}
+		// add the equals and value
+		pkslBuilder.append("\"=").append(values[valIndex]).append(";");
+		
+		// return the new pksl
+		return pkslBuilder.toString();
 	}
 	
-	private String generatePkslQuery(Object field, Object form, Object value, Object type) {
-        return "\""+form+"__"+field+"\" = "+value+";";
-    }
+	/**
+	 * Get the unique identifier for the group of pksl scripts to load
+	 * @param iteratorHeaders
+	 * @return
+	 */
+	private int[] getAssignmentIndices(String[] iteratorHeaders) {
+		// assumption that we have only one column which contains the pksl queries
+		// TODO: in future, maybe allow for multiple and do a "|" between them?
+		GenRowStruct assignments = this.store.getNoun(ASSIGNMENT_NOUN);
+		int numAssignmentCols = assignments.size();
+		int[] assignmentIndices = new int[numAssignmentCols];
+		for(int index = 0; index < numAssignmentCols; index++) {
+			String assignmentName = assignments.get(index).toString();
+			assignmentIndices[index] = ArrayUtilityMethods.arrayContainsValueAtIndex(iteratorHeaders, assignmentName);
+		}
+		return assignmentIndices;
+	}
 	
+	/**
+	 * Get the index which contains the pksl scripts to load
+	 * @return
+	 */
+	private int getValueIndex(String[] iteratorHeaders) {
+		// assumption that we have only one column which contains the pksl queries
+		// TODO: in future, maybe allow for multiple and do a "|" between them?
+		String valueName = this.store.getNoun(VALUE_NOUN).get(0).toString();
+		int valueIndex = ArrayUtilityMethods.arrayContainsValueAtIndex(iteratorHeaders, valueName);
+		return valueIndex;
+	}
+	
+	/**
+	 * Get the optional separator if defined
+	 * @return
+	 */
+	private String getSeparator() {
+		String separator = null;
+		// this is an optional key
+		// if we need to concatenate multiple things together
+		if(this.store.getNounKeys().contains(SEPARATOR_NOUN)) {
+			separator = this.store.getNoun(SEPARATOR_NOUN).get(0).toString();
+		}
+		return separator;
+	}
+
+	/**
+	 * Get the job input the reactor
+	 * @return
+	 */
 	private Iterator getIterator() {
 		List<Object> jobs = this.curRow.getColumnsOfType(PkslDataTypes.JOB);
 		if(jobs != null && jobs.size() > 0) {
@@ -129,38 +178,38 @@ public class LoadClient extends AbstractReactor {
 		Job job = (Job)this.getNounStore().getNoun(PkslDataTypes.JOB.toString()).get(0);
 		return job.getIterator();
 	}
-	
+
 	//method to see the structure of the plan
 	private void printPlan(PKSLPlanner planner) {
 		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-        GraphTraversal<Vertex, Vertex> getAllV = planner.g.traversal().V().has(PKSLPlanner.TINKER_TYPE, PKSLPlanner.OPERATION);
-        while(getAllV.hasNext()) {
-                        // get the vertex
-                        Vertex v = getAllV.next();
-                        System.out.println(">> " + v.property(PKSLPlanner.TINKER_ID).value().toString());
-                        
-                        // all of the vertex inputs
-                        Iterator<Edge> inputEdges = v.edges(Direction.IN);
-                        while(inputEdges.hasNext()) {
-                                        Edge inputE = inputEdges.next();
-                                        Vertex inputV = inputE.outVertex();
-                                        System.out.println("\tinput >> " + inputV.property(PKSLPlanner.TINKER_ID).value().toString());
-                        }
-                        
-                        // all of the vertex inputs
-                        Iterator<Edge> outputEdges = v.edges(Direction.OUT);
-                        while(outputEdges.hasNext()) {
-                                        Edge outputE = outputEdges.next();
-                                        Vertex outputV = outputE.inVertex();
-                                        System.out.println("\toutput >> " + outputV.property(PKSLPlanner.TINKER_ID).value().toString());
-                        }
-        }
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		GraphTraversal<Vertex, Vertex> getAllV = planner.g.traversal().V().has(PKSLPlanner.TINKER_TYPE, PKSLPlanner.OPERATION);
+		while(getAllV.hasNext()) {
+			// get the vertex
+			Vertex v = getAllV.next();
+			System.out.println(">> " + v.property(PKSLPlanner.TINKER_ID).value().toString());
+
+			// all of the vertex inputs
+			Iterator<Edge> inputEdges = v.edges(Direction.IN);
+			while(inputEdges.hasNext()) {
+				Edge inputE = inputEdges.next();
+				Vertex inputV = inputE.outVertex();
+				System.out.println("\tinput >> " + inputV.property(PKSLPlanner.TINKER_ID).value().toString());
+			}
+
+			// all of the vertex inputs
+			Iterator<Edge> outputEdges = v.edges(Direction.OUT);
+			while(outputEdges.hasNext()) {
+				Edge outputE = outputEdges.next();
+				Vertex outputV = outputE.inVertex();
+				System.out.println("\toutput >> " + outputV.property(PKSLPlanner.TINKER_ID).value().toString());
+			}
+		}
 	}
 
 
