660c60936656dcafaf529d65c45e7c3231c220e8
rluthar <rluthar@deloitte.com>
2017-04-18 02:40:04 +0000

    UPDATE ::: assignment checks to see if value beings assigned is a variable, if so use the actual variable value, adding getInputs method
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20054 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/AssignmentReactor.java
+++ src/prerna/sablecc2/reactor/AssignmentReactor.java
@@ -1,8 +1,10 @@
 package prerna.sablecc2.reactor;
 
 import java.util.List;
+import java.util.Set;
 import java.util.Vector;
 
+import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PkslDataTypes;
 
@@ -20,6 +22,14 @@ public class AssignmentReactor extends AbstractReactor {
 	public Object Out() {
 		NounMetadata result = planner.getVariable("$RESULT");
 		if(result != null) {
+			//if we get a lambda, just store the final result
+			if(result.getNounName().equals(PkslDataTypes.LAMBDA)) {
+				result = ((AbstractReactor)result.getValue()).execute();
+			} 
+			
+			if(planner.hasVariable(result.getValue().toString())) {
+				result = planner.getVariable(result.getValue().toString());
+			}
 			planner.addVariable(operationName.toUpperCase(), result);
 			planner.removeVariable("$RESULT");
 		} else {
@@ -44,6 +54,43 @@ public class AssignmentReactor extends AbstractReactor {
 		return true;
 	}
 
+	@Override
+	public List<NounMetadata> getInputs() {
+		List<NounMetadata> inputs = new Vector<NounMetadata>();
+		// grab all the nouns in the noun store
+		Set<String> nounKeys = this.getNounStore().nounRow.keySet();
+		for(String nounKey : nounKeys) {
+			// grab the genrowstruct for the noun
+			// and add its vector to the inputs list
+			GenRowStruct struct = this.getNounStore().getNoun(nounKey);
+			
+			// add everything except the variable name
+			for(NounMetadata noun : struct.vector) {
+				if(!noun.getValue().toString().equals(this.operationName)) {
+					inputs.add(noun);
+				}
+			}
+		}
+		
+		// we also need to account for some special cases
+		// when we have a filter reactor
+		// it doesn't get added as an op
+		// so we need to go through the child of this reactor
+		// and if it has a filter
+		// add its nouns to the inputs for this reactor
+		for(IReactor child : childReactor) {
+			if(child instanceof FilterReactor) {
+				// child nouns should contain LCOL, RCOL, COMPARATOR
+				Set<String> childNouns = child.getNounStore().nounRow.keySet();
+				for(String cNoun : childNouns) {
+					inputs.addAll(child.getNounStore().getNoun(cNoun).vector);
+				}
+			}
+		}
+		
+		return inputs;
+	}
+	
 	@Override
 	public List<NounMetadata> getOutputs() {
 		// output is the variable name to be referenced
