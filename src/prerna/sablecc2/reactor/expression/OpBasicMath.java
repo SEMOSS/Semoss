a020ee6e2469f20892403d78b56193d9deb42733
mahkhalil <mahkhalil@deloitte.com>
2017-04-18 13:02:36 +0000

    UPDATE ::: Adding error handling checks for expected inputs when dealing with variables UPDATE ::: Adding method to properly replace numerical values with java correct syntax (i.e. "0 . 75" to "0.75")
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20059 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d74fb918258addb779627ec6ef8daf2929a310bf

--- src/prerna/sablecc2/reactor/expression/OpBasicMath.java
+++ src/prerna/sablecc2/reactor/expression/OpBasicMath.java
@@ -1,5 +1,6 @@
 package prerna.sablecc2.reactor.expression;
 
+import prerna.algorithm.api.ITableDataFrame;
 import prerna.ds.h2.H2Frame;
 import prerna.sablecc.expressions.sql.H2SqlExpressionIterator;
 import prerna.sablecc.expressions.sql.builder.SqlColumnSelector;
@@ -7,6 +8,7 @@ import prerna.sablecc.expressions.sql.builder.SqlExpressionBuilder;
 import prerna.sablecc.expressions.sql.builder.SqlMathSelector;
 import prerna.sablecc2.om.NounMetadata;
 import prerna.ui.components.playsheets.datamakers.IDataMaker;
+import prerna.util.ArrayUtilityMethods;
 
 public abstract class OpBasicMath extends OpReactor{
 
@@ -20,22 +22,40 @@ public abstract class OpBasicMath extends OpReactor{
 	 * 		existing classes
 	 */
 	
+	
+	protected double evaluateString(String operation, NounMetadata frameColNoun) {
+		// to enter here
+		// it is assumed that the value is a string within the frame
+		IDataMaker dataMaker = this.planner.getFrame();
+		String frameColName = frameColNoun.getValue().toString();
+		
+		if(dataMaker instanceof ITableDataFrame) {
+			ITableDataFrame dataframe = (ITableDataFrame) dataMaker;
+			String[] headers = dataframe.getColumnHeaders();
+			if(headers == null) {
+				throw new IllegalArgumentException("Cannot find variable or column with value " + frameColName);
+			}
+			if(ArrayUtilityMethods.arrayContainsValue(headers, frameColName)) {
+				return evaluateString(dataframe, operation,  frameColName);
+			} else {
+				throw new IllegalArgumentException("Cannot find variable or column with value " + frameColName);
+			}
+		} else {
+			throw new IllegalArgumentException("Cannot execute " + operation + " of " + frameColName + " with the given frame");
+		}
+	}
+	
 	/**
 	 * Evaluate a string input - assumes the input is a column on the frame
 	 * @param operation			The operation to execute - i.e. sum/min/max
 	 * @param val				The name of the column to execute on
 	 * @return
 	 */
-	protected double evaluateString(String operation, NounMetadata frameColNoun) {
-		// to enter here
-		// it is assumed that the value is a string within the frame
-		IDataMaker frameToEvaluate = this.planner.getFrame();
-		String frameColName = frameColNoun.getValue().toString();
-		
+	private double evaluateString(ITableDataFrame frame, String operation, String frameColName) {
 		// TODO: make this generic
 		// at the moment, just going to assume this is sql
-		if(frameToEvaluate instanceof H2Frame) {
-			H2Frame h2Frame = (H2Frame) frameToEvaluate;
+		if(frame instanceof H2Frame) {
+			H2Frame h2Frame = (H2Frame) frame;
 			SqlColumnSelector colS = new SqlColumnSelector(h2Frame, frameColName);
 			SqlMathSelector sumS = new SqlMathSelector(colS, operation, operation);
 			
@@ -45,10 +65,10 @@ public abstract class OpBasicMath extends OpReactor{
 			if(it.hasNext()) {
 				return ((Number) it.next()[0]).doubleValue();
 			} else {
-				throw new IllegalArgumentException("Error!!! Failure in execution of " + operation + " " + frameColName);
+				throw new IllegalArgumentException("Failure in execution of " + operation + " " + frameColName);
 			}
 		} else {
-			throw new IllegalArgumentException("Error!!! Cannot execute " + operation + " of " + frameColName + " with the given frame");
+			throw new IllegalArgumentException("Cannot execute " + operation + " of " + frameColName + " with the given frame");
 		}
 	}
 }
