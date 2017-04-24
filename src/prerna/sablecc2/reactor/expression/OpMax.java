74e5fe523bf3a501efaf7bb68277667057d86fa5
rluthar <rluthar@deloitte.com>
2017-04-24 15:22:30 +0000

    UPDATE ::: refactoring OpBasicMath reactor such that child classes of basic math are simpler to implement
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20213 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6935f765da8ffcd1f739422889d90a05e00304f4

--- src/prerna/sablecc2/reactor/expression/OpMax.java
+++ src/prerna/sablecc2/reactor/expression/OpMax.java
@@ -3,46 +3,21 @@ package prerna.sablecc2.reactor.expression;
 import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PkslDataTypes;
 
-public class OpMax extends OpBasicMath {
-
+public class OpMax extends OpBasicMath {	
+	
+	public OpMax() {
+		this.operation = "max";
+	}
+	
 	@Override
-	public NounMetadata execute() {
-		double maxValue = -1.0 * Double.MAX_VALUE;
+	protected NounMetadata evaluate(Object[] values) {
+		double max = -1.0 * Double.MAX_VALUE;
 		
-		// get the values
-		// this evaluated any lambda that 
-		// was stored in currow
-		NounMetadata[] values = getValues();
-		for(int i = 0; i < values.length; i++) {
-			NounMetadata val = values[i];
-			PkslDataTypes valType = val.getNounName();
-			if(valType == PkslDataTypes.CONST_DECIMAL) {
-				maxValue = performComp(maxValue, ((Number) val.getValue()).doubleValue()); 
-			} else if(valType == PkslDataTypes.COLUMN) {
-				// at this point, we have already checked if this is a 
-				// variable, so it better exist on the frame
-				// also, you can only have one of these
-				maxValue = performComp(maxValue, evaluateString("max", val)); 
-			} else {
-				throw new IllegalArgumentException("Invalid input for Max. Require all values to be numeric");
-			}
+		for(Object val : values) {
+			Double nextDouble = ((Number) val).doubleValue();
+			max = max < nextDouble ? nextDouble : max;
 		}
-		NounMetadata maxNoun = new NounMetadata(maxValue, PkslDataTypes.CONST_DECIMAL);
+		NounMetadata maxNoun = new NounMetadata(max, PkslDataTypes.CONST_DECIMAL);
 		return maxNoun;
-	}
-	
-	/**
-	 * Do a basic math comparison
-	 * @param curMax
-	 * @param newMax
-	 * @return
-	 */
-	public double performComp(double curMax, double newMax) {
-		if(curMax < newMax) {
-			return newMax;
-		} else {
-			return curMax;
-		}
-	}
-	
+	}	
 }
--- src/prerna/sablecc2/reactor/expression/OpPower.java
+++ src/prerna/sablecc2/reactor/expression/OpPower.java
@@ -5,41 +5,16 @@ import prerna.sablecc2.om.PkslDataTypes;
 
 public class OpPower extends OpBasicMath {
 
+	public OpPower() {
+		this.operation = "power";
+	}
+	
 	@Override
-	public NounMetadata execute() {
-		// get the values
-		// this evaluated any lambda that 
-		// was stored in currow
-		NounMetadata[] values = getValues();
-
-		if(values.length != 2) {
-			throw new IllegalArgumentException("Invalid input for Power. Must have exactly two inputs: first input is the number and the second input is the power to raise that number.");
-		}
-		
-		// we need to calculate all of the values
-		// and then order them
-		// and then find the middle one
-		double[] evals = new double[values.length];
-
-		for(int i = 0; i < values.length; i++) {
-			NounMetadata val = values[i];
-			PkslDataTypes valType = val.getNounName();
-			if(valType == PkslDataTypes.CONST_DECIMAL) {
-				evals[i] = ((Number) val.getValue()).doubleValue(); 
-			} else if(valType == PkslDataTypes.COLUMN) {
-				// TODO: need to figure out what this would do
-				// should only enter here if we have a column
-				// which is from the frame
-				// at which point, should i return an iterator?
-				
-				throw new IllegalArgumentException("Invalid input for Power. Cannot handle frame columns at this time.");
-			} else {
-				throw new IllegalArgumentException("Invalid input for Power. Require all values to be numeric at this time.");
-			}
-		}
-		
-		double powerVal = Math.pow(evals[0], evals[1]);
-		NounMetadata medianNoun = new NounMetadata(powerVal, PkslDataTypes.CONST_DECIMAL);
-		return medianNoun;
+	protected NounMetadata evaluate(Object[] values) {
+		double number = ((Number)values[0]).doubleValue();
+		double power = ((Number)values[1]).doubleValue();
+		double powerVal = Math.pow(number, power);
+		NounMetadata powerNoun = new NounMetadata(powerVal, PkslDataTypes.CONST_DECIMAL);
+		return powerNoun;
 	}
 }
