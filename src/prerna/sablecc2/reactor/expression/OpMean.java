ebf284d7c4b4ab228a94ec877e961a81fa2f8005
rluthar <rluthar@deloitte.com>
2017-04-24 15:22:30 +0000

    UPDATE ::: refactoring OpBasicMath reactor such that child classes of basic math are simpler to implement
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20213 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6935f765da8ffcd1f739422889d90a05e00304f4

--- src/prerna/sablecc2/reactor/expression/OpMean.java
+++ src/prerna/sablecc2/reactor/expression/OpMean.java
@@ -5,28 +5,17 @@ import prerna.sablecc2.om.PkslDataTypes;
 
 public class OpMean extends OpBasicMath {
 
-	@Override
-	public NounMetadata execute() {
+	public OpMean() {
+		this.operation = "avg";
+	}
+	
+	protected NounMetadata evaluate(Object[] values) {
 		double sum = 0;
-		// get the values
-		// this evaluated any lambda that 
-		// was stored in currow
-		NounMetadata[] values = getValues();
-		for(int i = 0; i < values.length; i++) {
-			NounMetadata val = values[i];
-			PkslDataTypes valType = val.getNounName();
-			if(valType == PkslDataTypes.CONST_DECIMAL) {
-				sum += ((Number) val.getValue()).doubleValue(); 
-			} else if(valType == PkslDataTypes.COLUMN) {
-				// at this point, we have already checked if this is a 
-				// variable, so it better exist on the frame
-				// also, you can only have one of these
-				sum += evaluateString("avg", val); 
-			} else {
-				throw new IllegalArgumentException("Invalid input for Average. Require all values to be numeric");
-			}
+		for(Object val : values) {
+			sum += ((Number) val).doubleValue();
 		}
-		NounMetadata maxNoun = new NounMetadata(sum / values.length, PkslDataTypes.CONST_DECIMAL);
+		double mean = sum/values.length;
+		NounMetadata maxNoun = new NounMetadata(mean, PkslDataTypes.CONST_DECIMAL);
 		return maxNoun;
 	}
 }
