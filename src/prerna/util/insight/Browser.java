d2d32c25ce1d5d0290428d4636aa76a9114fd37c
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-04-25 20:43:44 +0000

    UPDATE ::: Insight image screenshot on backend utility classes
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20271 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6a70914fc9220801d641303a4fc49bc301f42ff1

---
+++ src/prerna/util/insight/Browser.java
@@ -0,0 +1,40 @@
+package prerna.util.insight;
+
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.layout.Region;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+class Browser extends Region {
+
+    private final WebView browser = new WebView();
+
+    Browser(String url) {
+        WebEngine webEngine = browser.getEngine();
+        webEngine.load(url);
+        getChildren().add(browser);
+    }
+
+    Boolean isPageLoaded(){
+        return browser.getEngine().getDocument() != null;
+    }
+
+    @Override
+    protected void layoutChildren() {
+        double w = getWidth();
+        double h = getHeight();
+        layoutInArea(browser, 0, 0, w, h, 0, HPos.CENTER, VPos.CENTER);
+    }
+
+    @Override
+    protected double computePrefWidth(double width) {
+        return 1280;
+    }
+
+    @Override
+    protected double computePrefHeight(double height) {
+        return 720;
+    }
+
+}
