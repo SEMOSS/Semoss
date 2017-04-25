b5990e6d1e657fb8cc3ab73bca56ba61f8bf35ca
mahkhalil <mahkhalil@deloitte.com>
2017-04-25 16:05:31 +0000

    BUGFIX ::: Fixing compilation error
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20252 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/PKSLRunner.java
+++ src/prerna/sablecc2/PKSLRunner.java
@@ -6,16 +6,14 @@ import java.io.PushbackReader;
 import java.io.StringBufferInputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
 import prerna.sablecc.meta.IPkqlMetadata;
-import prerna.sablecc2.node.AConfiguration;
 import prerna.sablecc2.lexer.Lexer;
 import prerna.sablecc2.lexer.LexerException;
-import prerna.sablecc2.node.PConfiguration;
+import prerna.sablecc2.node.AConfiguration;
 import prerna.sablecc2.node.PScriptchain;
 import prerna.sablecc2.node.Start;
 import prerna.sablecc2.parser.Parser;
@@ -24,9 +22,9 @@ import prerna.sablecc2.reactor.PKSLPlanner;
 import prerna.ui.components.playsheets.datamakers.IDataMaker;
 
 public class PKSLRunner {
-	
+
 	public enum STATUS {SUCCESS, ERROR, INPUT_NEEDED}
-	
+
 	private STATUS currentStatus = PKSLRunner.STATUS.SUCCESS;
 	private Object response = "PKSL processing complete"; //i feel as if this should be an object coming from the reactor...not a class variable
 	private String explain = "";
@@ -35,47 +33,47 @@ public class PKSLRunner {
 	private Object returnData = null;
 	private List<Map> newInsights = new ArrayList<>();
 	private boolean dataCleared = false;
-	
+
 	private Map<String,String> newColumns = new HashMap<String,String>();
 	private Map<String, Map<String,Object>> masterFeMap = new HashMap<String, Map<String,Object>>(); // this holds all active front end data. in the form panelId --> prop --> value
 
 	//	private Map<String, List<Map<String, Object>>> expiredFeMaps =  new HashMap<String, List<Map<String,Object>>>();
-	
+
 	private Map<String, Object> activeFeMap; // temporally grabbed out of master
 	private List<Map> responseArray = new Vector<Map>();
 	private Map<String, Map<String, Object>> varMap = new HashMap<String, Map<String, Object>>();
 	List<String> unassignedVars = new Vector<String>();
-	
-//	private Map<String, Object> dataMap = new HashMap<>();
-	
+
+	//	private Map<String, Object> dataMap = new HashMap<>();
+
 	private List<IPkqlMetadata> metadataResponse = new Vector<IPkqlMetadata>();
-	
+
 	// there is a getter for this
 	// but we never set this... so not used
-//	private String newInsightID;
-	
+	//	private String newInsightID;
+
 	private Map dashboardMap;
-	
-	
-	
+
+
+
 	/**
 	 * Runs a given pksl expression (can be multiple if semicolon delimited) on a provided data maker 
 	 * @param expression			The sequence of semicolon delimited pkql expressions.
 	 * 								If just one expression, still must end with a semicolon
 	 * @param frame					The data maker to run the pkql expression on
 	 */
-	
+
 	private Translation translation;
 	private IDataMaker dataMaker;
 	private String insightId;
 	private PKSLPlanner planner;
-	
+
 	public static void main(String[] args) throws Exception {
 		String pksl = "A = 10; B = \"Apple\";";
 		List<String> x = parsePKSL(pksl);
 		System.out.println(x);
 	}
-	
+
 	/**
 	 * 
 	 * @param expression
@@ -86,34 +84,27 @@ public class PKSLRunner {
 	public static List<String> parsePKSL(String expression) throws ParserException, LexerException, IOException {
 		Parser p = new Parser(new Lexer(new PushbackReader(new InputStreamReader(new StringBufferInputStream(expression)), expression.length())));
 		Start tree = p.parse();
-		
+
 		AConfiguration configNode = (AConfiguration)tree.getPConfiguration();
-		
+
 		List<String> pksls = new ArrayList<>();
 		for(PScriptchain script : configNode.getScriptchain()) {
 			pksls.add(script.toString());
 		}
 		return pksls;
 	}
-	
-	public void runPKSL(String expression, IDataMaker frame) {
-		
-		this.dataMaker = frame;
+
+	public void runPKSL(String expression) {
 		Parser p = new Parser(new Lexer(new PushbackReader(new InputStreamReader(new StringBufferInputStream(expression)), expression.length())));
-		Start tree;
-//		if(planner == null) {
-//			planner = new PKSLPlanner();
-//		}
 		if(translation == null){
-			translation = new Translation(frame, this);
+			translation = new Translation(this);
 		}
 
 		try {
 			// parsing the pkql - this process also determines if expression is syntactically correct
-			tree = p.parse();
+			Start tree = p.parse();
 			// apply the translation.
 			tree.apply(translation);
-			
 		} catch (ParserException | LexerException | IOException | RuntimeException e) {
 			e.printStackTrace();
 		} finally {
@@ -121,7 +112,28 @@ public class PKSLRunner {
 		}
 		return;
 	}
-	
+
+	public void runPKSL(String expression, IDataMaker frame) {
+
+		this.dataMaker = frame;
+		Parser p = new Parser(new Lexer(new PushbackReader(new InputStreamReader(new StringBufferInputStream(expression)), expression.length())));
+		Start tree;
+		if(translation == null){
+			translation = new Translation(frame, this);
+		}
+		try {
+			// parsing the pkql - this process also determines if expression is syntactically correct
+			tree = p.parse();
+			// apply the translation.
+			tree.apply(translation);
+		} catch (ParserException | LexerException | IOException | RuntimeException e) {
+			e.printStackTrace();
+		} finally {
+			translation.postProcess();
+		}
+		return;
+	}
+
 	public void setInsightId(String insightId) {
 		this.insightId = insightId;
 	}
@@ -141,10 +153,9 @@ public class PKSLRunner {
 	}
 
 	public Object getResults() {
-		int dataId = dataMaker.getDataId();
 		return translation.getResults();
 	}
-	
+
 	public PKSLPlanner getPlanner() {
 		return this.planner;
 	}
