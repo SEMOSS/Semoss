ba297a13f96cc62f49c26647769367ac2d6a3862
rluthar <rluthar@deloitte.com>
2017-05-01 09:44:23 +0000

    UPDATE ::: adding specific reactors to run tax plan, filter and assimilator no longer saving class to cache, adding methods to pkslutility, assignment reactor returns column as output, abstract reactor not adding reactor to graph, PKSLPlanner sending back 0 if value does not exist (for now), load client adding variables directly for atomic values, Var store not creating variable loops anymore...as in X pointing to itself
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20393 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 04694a8d3e42b3b65b90d72e8474bb0437f1628b

--- src/prerna/util/MyGraphIoRegistry.java
+++ src/prerna/util/MyGraphIoRegistry.java
@@ -6,11 +6,22 @@ import org.apache.tinkerpop.gremlin.structure.io.AbstractIoRegistry;
 import org.apache.tinkerpop.gremlin.structure.io.gryo.GryoIo;
 
 import prerna.algorithm.api.IMetaData;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.PkslDataTypes;
+import prerna.sablecc2.reactor.AssignmentReactor;
+import prerna.sablecc2.reactor.IReactor;
 
 public class MyGraphIoRegistry extends AbstractIoRegistry {
     public MyGraphIoRegistry() {
         register(GryoIo.class, Vector.class, null);
         register(GryoIo.class, IMetaData.NAME_TYPE.class, null);
         register(GryoIo.class, IMetaData.DATA_TYPES.class, null);
+        register(GryoIo.class, IReactor.TYPE.class, null);
+        register(GryoIo.class, AssignmentReactor.class, null);
+        register(GryoIo.class, GenRowStruct.class, null);
+        register(GryoIo.class, NounMetadata.class, null);
+        register(GryoIo.class, PkslDataTypes.class, null);
+        
     }
 }
