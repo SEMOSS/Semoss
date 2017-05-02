552ff4499f69a68f9f8ef30db286a752ccc5411f
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-05-02 19:10:13 +0000

    UPDATE ::: Image Capture check front end visualLoaded
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20458 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/insight/Browser.java
+++ src/prerna/util/insight/Browser.java
@@ -8,33 +8,37 @@ import javafx.scene.web.WebView;
 
 class Browser extends Region {
 
-    private final WebView browser = new WebView();
+	private final WebView browser = new WebView();
 
-    Browser(String url) {
-        WebEngine webEngine = browser.getEngine();
-        webEngine.load(url);
-        getChildren().add(browser);
-    }
+	Browser(String url) {
+		WebEngine webEngine = browser.getEngine();
+		webEngine.load(url);
+		getChildren().add(browser);
+	}
 
-    Boolean isPageLoaded(){
-        return browser.getEngine().getDocument() != null;
-    }
+	Boolean isPageLoaded() {
+		if (((Boolean) browser.getEngine().executeScript("window.visualLoaded;")) == true) {
+			return true;
+		}
+		
+		return false;
+	}
 
-    @Override
-    protected void layoutChildren() {
-        double w = getWidth();
-        double h = getHeight();
-        layoutInArea(browser, 0, 0, w, h, 0, HPos.CENTER, VPos.CENTER);
-    }
+	@Override
+	protected void layoutChildren() {
+		double w = getWidth();
+		double h = getHeight();
+		layoutInArea(browser, 0, 0, w, h, 0, HPos.CENTER, VPos.CENTER);
+	}
 
-    @Override
-    protected double computePrefWidth(double width) {
-        return 1280;
-    }
+	@Override
+	protected double computePrefWidth(double width) {
+		return 1280;
+	}
 
-    @Override
-    protected double computePrefHeight(double height) {
-        return 720;
-    }
+	@Override
+	protected double computePrefHeight(double height) {
+		return 720;
+	}
 
 }
