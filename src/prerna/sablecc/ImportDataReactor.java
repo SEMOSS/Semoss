30326cbece45e80bd21d3e7921553216049740d0
mahkhalil <mahkhalil@deloitte.com>
2017-05-05 21:06:55 +0000

    UPDATE ::: Renaming FileIterator to CsvFileIterator to make room for ExcelFileIterator
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20570 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 93a35bb815cbd19d832f2fd05a2df77be76d6cf2

--- src/prerna/sablecc/ImportDataReactor.java
+++ src/prerna/sablecc/ImportDataReactor.java
@@ -17,7 +17,7 @@ import prerna.algorithm.api.ITableDataFrame;
 import prerna.ds.TinkerMetaHelper;
 import prerna.ds.h2.H2HeadersDataRowIterator;
 import prerna.ds.nativeframe.NativeFrame;
-import prerna.ds.util.FileIterator;
+import prerna.ds.util.CsvFileIterator;
 import prerna.engine.api.IEngine;
 import prerna.engine.api.IEngineWrapper;
 import prerna.engine.api.IHeadersDataRow;
@@ -248,14 +248,14 @@ public abstract class ImportDataReactor extends AbstractReactor {
 				}
 			}
 			// this is the second possibility when it is a file
-			else if(myStore.get(PKQLEnum.API) instanceof FileIterator) {
+			else if(myStore.get(PKQLEnum.API) instanceof CsvFileIterator) {
 				// grab the iterator to use for importing
-				dataIterator  = (FileIterator) myStore.get(PKQLEnum.API);
+				dataIterator  = (CsvFileIterator) myStore.get(PKQLEnum.API);
 				
 				LOGGER.info(" >>> FRAME IS LOADING FROM A FILE!!!!");
 				
-				String[] types = ((FileIterator) dataIterator).getTypes();
-				String[] headers = ((FileIterator) dataIterator).getHeaders();
+				String[] types = ((CsvFileIterator) dataIterator).getTypes();
+				String[] headers = ((CsvFileIterator) dataIterator).getHeaders();
 				Map<String, String> dataTypes = new HashMap<>();
 				for(int i = 0; i < types.length; i++) {
 					dataTypes.put(headers[i], types[i]);
@@ -312,10 +312,10 @@ public abstract class ImportDataReactor extends AbstractReactor {
 			LOGGER.info(" >>> FRAME IS LOADING FROM PASTED TEXT!!!!");
 
 			// grab the iterator to use for importing
-			dataIterator  = (FileIterator) myStore.get(PKQLEnum.PASTED_DATA);
+			dataIterator  = (CsvFileIterator) myStore.get(PKQLEnum.PASTED_DATA);
 			
-			String[] types = ((FileIterator) dataIterator).getTypes();
-			String[] headers = ((FileIterator) dataIterator).getHeaders();
+			String[] types = ((CsvFileIterator) dataIterator).getTypes();
+			String[] headers = ((CsvFileIterator) dataIterator).getHeaders();
 			Map<String, String> dataTypes = new HashMap<>();
 			for(int i = 0; i < types.length; i++) {
 				dataTypes.put(headers[i], types[i]);
