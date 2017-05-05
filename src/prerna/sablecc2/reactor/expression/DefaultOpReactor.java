a59d4637653474c7eba93199f36bec2547e3e631
rluthar <rluthar@deloitte.com>
2017-05-05 19:12:48 +0000

    UPDATE ::: adding default op reactor in reactor factor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20561 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8e1806742687a70d2908e5079394e433fc1b6bbb

---
+++ src/prerna/sablecc2/reactor/expression/DefaultOpReactor.java
@@ -0,0 +1,12 @@
+package prerna.sablecc2.reactor.expression;
+
+import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.PkslDataTypes;
+
+public class DefaultOpReactor extends OpBasic {
+	@Override
+	protected NounMetadata evaluate(Object[] values) {
+		// TODO Auto-generated method stub
+		return new NounMetadata(0, PkslDataTypes.CONST_DECIMAL);
+	}
+}
