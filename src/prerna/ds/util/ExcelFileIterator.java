101912dba0559df8009054c044bb5d01087b3e4b
mahkhalil <mahkhalil@deloitte.com>
2017-05-08 20:34:59 +0000

    UPDATE ::: Adding abstract file iterator to reduce redundant code between excel and csv loading
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20608 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 546398711a541ad99657282cb68131a9768a2d01

--- src/prerna/ds/util/ExcelFileIterator.java
+++ src/prerna/ds/util/ExcelFileIterator.java
@@ -1,36 +1,24 @@
 package prerna.ds.util;
 
-import java.io.File;
-import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import prerna.ds.QueryStruct;
-import prerna.engine.api.IHeadersDataRow;
-import prerna.engine.impl.rdf.HeadersDataRow;
 import prerna.poi.main.helper.XLFileHelper;
 import prerna.util.ArrayUtilityMethods;
 import prerna.util.Utility;
 
-public class ExcelFileIterator implements IFileIterator {
+public class ExcelFileIterator extends AbstractFileIterator {
 
 	private XLFileHelper helper;
-	private String[] headers;
-	private String[] types;
-	private String[] nextRow;
 	private int[] headerIndices;
-	
-	private Map<String, Set<Object>> filters;
-	private Map<String, String> dataTypeMap;
-	
-	private int numRecords = -1;
 	private String sheetToLoad;
 	
 	public ExcelFileIterator(String fileLocation, String sheetToLoad, QueryStruct qs, Map<String, String> dataTypeMap) {
 		this.helper = new XLFileHelper();
-		this.helper.parse(fileLocation);
+		this.helper.parse(fileLocation);	
 		this.sheetToLoad = sheetToLoad;
 		this.dataTypeMap = dataTypeMap;
 		
@@ -61,48 +49,6 @@ public class ExcelFileIterator implements IFileIterator {
 		}
 	}
 
-	@Override
-	public boolean hasNext() {
-		if(this.nextRow == null) {
-			this.helper.clear();
-			return false;
-		}
-		return true;
-	}
-
-	@Override
-	public IHeadersDataRow next() {
-		String[] row = nextRow;
-		getNextRow();
-
-		// couple of things to take care of here
-		
-		Object[] cleanRow = new Object[row.length];
-		for(int i = 0; i < row.length; i++) {
-			String type = types[i];
-			if(type.contains("DOUBLE")) {
-				String val = row[i].trim();
-				try {
-					//added to remove $ and , in data and then try parsing as Double
-					int mult = 1;
-					if(val.startsWith("(") || val.startsWith("-")) // this is a negativenumber
-						mult = -1;
-					val = val.replaceAll("[^0-9\\.E]", "");
-					cleanRow[i] = mult * Double.parseDouble(val.trim());
-				} catch(NumberFormatException ex) {
-					//do nothing
-					cleanRow[i] = null;
-				}
-			} else if(type.contains("DATE")) {
-				cleanRow[i] = row[i]; //TODO do i need to do anything for dates???
-			} else {
-				cleanRow[i] = Utility.cleanString(row[i], true, true, false);
-			} 
-		}
-		IHeadersDataRow nextData = new HeadersDataRow(this.headers, cleanRow, cleanRow);
-		return nextData;
-	}
-	
 	public void getNextRow() {
 		String[] row = this.helper.getNextRow(this.sheetToLoad, this.headerIndices);
 		if(this.filters == null || this.filters.isEmpty()) {
@@ -139,19 +85,6 @@ public class ExcelFileIterator implements IFileIterator {
 		this.nextRow = newRow;
 	}
 
-	private void setFilters(Map<String, Map<String, List>> andfilters) {
-		for(String column : andfilters.keySet()) {
-			Map<String, List> filterValues = andfilters.get(column);
-			Set<Object> values = new HashSet<>();
-			for(String comparator : filterValues.keySet()) {
-				List vals = filterValues.get(comparator);
-				values.addAll(vals);
-			}
-			
-			this.filters.put(column, values);
-		}
-	}
-	
 	private void setSelectors(Map<String, List<String>> selectorSet) {
 		if(selectorSet.isEmpty()) {
 			return; // if no selectors, return everything
@@ -181,54 +114,14 @@ public class ExcelFileIterator implements IFileIterator {
 			}
 		}
 	}
-	
-	public void deleteFile() {
-		this.helper.clear();
-		File file = new File(this.helper.getFileLocation());
-		file.delete();
-	}
-	
-	public String getFileLocation() {
-		return this.helper.getFileLocation();
-	}
-	
-	public boolean numberRowsOverLimit(int limit) {
-		boolean overLimit = false;
-		int counter = 0;
-		while(this.hasNext()) {
-			this.getNextRow();
-			counter++;
-			if(counter > limit) {
-				overLimit = true;
-				break;
-			}
-		}
-		
-		// we only keep the number of records
-		// if we actually iterated through everything
-		// and found it not larger than the limit set
-		if(!overLimit) {
-			numRecords = counter;
-		}
-		
-		// reset so we get the values again
+
+	@Override
+	public void resetHelper() {
 		this.helper.reset();
-		getNextRow();
-		return overLimit;
-	}
-	
-	public int getNumRecords() {
-		return this.numRecords;
 	}
 
 	@Override
-	public String[] getTypes() {
-		return this.types;
+	public void clearHelper() {
+		this.helper.clear();
 	}
-
-	@Override
-	public String[] getHeaders() {
-		return this.headers;
-	}
-	
 }
