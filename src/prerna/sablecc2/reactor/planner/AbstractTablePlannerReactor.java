2e466b39c575feead9835ca5c1e55805c0dcf3bb
rluthar <rluthar@deloitte.com>
2017-05-09 22:15:59 +0000

    UPDATE ::: Changing queries to traverse the h2 table to get pksls in order
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20674 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/planner/AbstractTablePlannerReactor.java
+++ src/prerna/sablecc2/reactor/planner/AbstractTablePlannerReactor.java
@@ -3,9 +3,7 @@ package prerna.sablecc2.reactor.planner;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import prerna.sablecc2.SimpleTable;
 import prerna.sablecc2.reactor.AbstractReactor;
@@ -13,95 +11,130 @@ import prerna.sablecc2.reactor.TablePKSLPlanner;
 
 public abstract class AbstractTablePlannerReactor extends AbstractReactor {
 
+	/**
+	 * CREATE TABLE TESTING(OP VARCHAR(100), NOUN VARCHAR(100), DIRECTION VARCHAR(100), PROCESSED BOOLEAN);
+	 * INSERT INTO TESTING (OP, NOUN, DIRECTION, PROCESSED) VALUES ('A=0', '0','IN',FALSE);
+	 * INSERT INTO TESTING (OP, NOUN, DIRECTION, PROCESSED) VALUES ('A=0', 'A','OUT',FALSE);
+	 * INSERT INTO TESTING (OP, NOUN, DIRECTION, PROCESSED) VALUES ('B=-A', 'A','IN',FALSE);
+	 * INSERT INTO TESTING (OP, NOUN, DIRECTION, PROCESSED) VALUES ('B=-A', 'B','OUT',FALSE);
+	 * INSERT INTO TESTING (OP, NOUN, DIRECTION, PROCESSED) VALUES ('C=A+B', 'A','IN',FALSE);
+	 * INSERT INTO TESTING (OP, NOUN, DIRECTION, PROCESSED) VALUES ('C=A+B', 'B','IN',FALSE);
+	 * INSERT INTO TESTING (OP, NOUN, DIRECTION, PROCESSED) VALUES ('C=A+B', 'C','OUT',FALSE);
+	 * 
+	 * -- GET ROOTS --
+	 * SELECT OP, NOUN FROM TESTING WHERE DIRECTION = 'IN' AND NOUN NOT IN (SELECT NOUN FROM TESTING WHERE DIRECTION = 'OUT');
+	 * 
+	 * -- UPDATE FOR THOSE OPS --
+	 * UPDATE TESTING SET PROCESSED=TRUE WHERE OP IN ('A=0');
+	 * 
+	 * -- GET NEW ROOTS --
+	 * SELECT OP FROM TESTING WHERE PROCESSED=FALSE AND NOUN IN(SELECT NOUN FROM TESTING WHERE DIRECTION='OUT' AND PROCESSED='TRUE') MINUS SELECT OP FROM TESTING WHERE DIRECTION='IN' AND NOUN IN(SELECT NOUN FROM TESTING WHERE DIRECTION='OUT' AND PROCESSED=FALSE)
+	 * 
+	 * -- UPDATE FOR THOSE OPS --
+	 * UPDATE TESTING SET PROCESSED=TRUE WHERE OP IN ('B=-A');
+	 * 
+ 	 * -- GET NEW ROOTS --
+	 * SELECT OP FROM TESTING WHERE PROCESSED=FALSE AND NOUN IN(SELECT NOUN FROM TESTING WHERE DIRECTION='OUT' AND PROCESSED='TRUE') MINUS SELECT OP FROM TESTING WHERE DIRECTION='IN' AND NOUN IN(SELECT NOUN FROM TESTING WHERE DIRECTION='OUT' AND PROCESSED=FALSE)
+	 */
+	
 	String OPERATION_COLUMN = "OP";
 	String NOUN_COLUMN = "NOUN";
 	String DIRECTION_COLUMN = "DIRECTION";
+	String PROCESSED_COLUMN = "PROCESSED";
 	
 	String inDirection = "IN";
 	String outDirection = "OUT";
 	
-	public List<String> collectPksls(TablePKSLPlanner planner) {
-		
-		List<String> pksls = new ArrayList<>();
-		return collectNextPksls(planner, pksls);
-	}
+	String falseProcessed = "FALSE";
+	String trueProcessed = "TRUE";
 	
-	private List<String> collectNextPksls(TablePKSLPlanner planner, List<String> pksls) {
-		String rootsQuery = getNextLevelQuery(pksls);
+	String tempTableName = "TEMP_TABLE";
+	
+	
+	public List<String> collectNextPksls(TablePKSLPlanner planner) {
+		List<String> pksls = new ArrayList<>();
 		SimpleTable table = planner.getSimpleTable();
+		String rootsQuery = getNextQuery(table.getTableName());
+		System.out.println(rootsQuery);
 		ResultSet results = table.executeQuery(rootsQuery);
 		try {
-			if(results.next()) {
-				String nextPksl = results.getString(1);
-				pksls.add(nextPksl);
-			} else {
-				return pksls;
-			}
-			
 			while(results.next()) {
-				String nextPksl = results.getString(1);
-				pksls.add(nextPksl);
+				pksls.add(results.getString(1)+";");
 			}
-			
-			return collectNextPksls(planner, pksls);
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				results.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		return pksls;
 	}
 	
-	private String getNextLevelQuery(List<String> collectedOps) {
-		StringBuilder query = new StringBuilder();
-		query.append("SELECT ").append(OPERATION_COLUMN);
-		query.append(" FROM ");
-		query.append(queryTable(collectedOps));
-		query.append(" WHERE ").append(DIRECTION_COLUMN).append("=").append("'"+inDirection+"'");
-		query.append(" AND ").append(NOUN_COLUMN).append(" NOT IN ");
-		query.append("(").append(queryNounFilters(collectedOps)).append(")"); //this can be optimized
-		return query.toString();
-	}
-	
-	private String queryNounFilters(List<String> collectedOps) {
-		StringBuilder query = new StringBuilder();
-		query.append(" SELECT ").append(NOUN_COLUMN);
-		query.append(" FROM ").append(queryTable(collectedOps));
-		query.append(" WHERE ").append(DIRECTION_COLUMN).append("=").append("'"+outDirection+"'");
-		return query.toString();
-	}
-	
-	private String queryTable(List<String> collectedOps) {
-		String query = null;
-		if(collectedOps.isEmpty()) {
-			query = getTableName();
-		} else {
-			StringBuilder q = new StringBuilder();
-			q.append("SELECT ");
-			q.append(this.OPERATION_COLUMN);
-			q.append(", "+this.NOUN_COLUMN);
-			q.append(", "+this.DIRECTION_COLUMN);
-			
-			q.append(" FROM ").append(getTableName());
-			q.append(" WHERE "+this.OPERATION_COLUMN+" NOT IN ");
-			
-			//TODO: this can be optimized by adding to a table or something and using where 'not exists' in that table or something like that
-			q.append("(");
-			int i = 0;
-			for(String op : collectedOps) {
-				if(i != 0) {
-					q.append(", ");
-				}
-				q.append(op);
-				i++;
+	public List<String> collectRootPksls(TablePKSLPlanner planner) {
+		List<String> pksls = new ArrayList<>();
+		SimpleTable table = planner.getSimpleTable();
+		String rootsQuery = getRootsQuery(table.getTableName());
+		System.out.println(rootsQuery);
+		ResultSet results = table.executeQuery(rootsQuery);
+		try {
+			while(results.next()) {
+				pksls.add(results.getString(1)+";");
 			}
-			q.append(")");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				results.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 		
-		return query;
+		return pksls;
+	} 
+	
+	public void updateTable(TablePKSLPlanner planner, List<String> pksls) {
+		StringBuilder updateQuery = new StringBuilder();
+		updateQuery.append("UPDATE ").append(planner.getSimpleTable().getTableName()).append(" SET PROCESSED=TRUE WHERE OP IN(");
+		int size = pksls.size();
+		for(int i = 0; i < size; i++) {
+			updateQuery.append("'").append(pksls.get(i)).append("'");
+			if(i +1 != size) {
+				updateQuery.append(",");
+			}
+		}
+		updateQuery.append(")");
+		try {
+			planner.getSimpleTable().runQuery(updateQuery.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	private String getTableName() {
-		return "";
+	
+	
+	private String getNextQuery(String tableName) {
+//		StringBuilder rootsQuery = new StringBuilder();
+//		rootsQuery.append("SELECT ").append(OPERATION_COLUMN);  //SELECT OP
+//		rootsQuery.append(" FROM ").append(tableName); //FROM TABLE
+//		rootsQuery.append(" WHERE ").append(PROCESSED_COLUMN).append("=").append(falseProcessed); //WHERE PROCESSED=FALSE
+//		rootsQuery.append(" AND ");
+//		rootsQuery.append(NOUN_COLUMN).append("IN(");
+		return "SELECT DISTINCT OP FROM " + tableName + " WHERE PROCESSED=FALSE AND NOUN IN(SELECT NOUN FROM "+tableName+" WHERE DIRECTION='OUT' AND PROCESSED=TRUE) MINUS SELECT OP FROM "+tableName+" WHERE DIRECTION='IN' AND NOUN IN(SELECT NOUN FROM "+tableName+" WHERE DIRECTION='OUT' AND PROCESSED=FALSE)";
 	}
-
+	
+	private String getRootsQuery(String tableName) {
+//		StringBuilder rootsQuery = new StringBuilder();
+//		rootsQuery.append("SELECT ").append(OPERATION_COLUMN);  //SELECT OP
+//		rootsQuery.append(" FROM ").append(tableName); //FROM TABLE
+//		rootsQuery.append(" WHERE ").append(PROCESSED_COLUMN).append("=").append(falseProcessed); //WHERE PROCESSED=FALSE
+//		rootsQuery.append(" AND ");
+//		rootsQuery.append(NOUN_COLUMN).append("IN(");
+		return "SELECT DISTINCT OP, NOUN FROM "+tableName+" WHERE DIRECTION = 'IN' AND NOUN NOT IN (SELECT NOUN FROM "+tableName+" WHERE DIRECTION = 'OUT')";
+	}
+	
 }
