a93775860cd85b89a4726b17342e8b9e304b2313
rluthar <rluthar@deloitte.com>
2017-05-09 22:15:59 +0000

    UPDATE ::: Changing queries to traverse the h2 table to get pksls in order
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20674 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 894eb9bd73237a53bc6b200a1fc52f2b35016cbf

--- src/prerna/sablecc2/reactor/TablePKSLPlanner.java
+++ src/prerna/sablecc2/reactor/TablePKSLPlanner.java
@@ -2,7 +2,6 @@ package prerna.sablecc2.reactor;
 
 import java.util.List;
 
-
 import prerna.sablecc2.SimpleTable;
 
 public class TablePKSLPlanner extends PKSLPlanner {
@@ -11,13 +10,18 @@ public class TablePKSLPlanner extends PKSLPlanner {
 	public static final String OPERATION_COLUMN = "OP";
 	public static final String NOUN_COLUMN = "NOUN";
 	public static final String DIRECTION_COLUMN = "DIRECTION";
-	
-	public static final String OP_TYPE = "Varchar(80)";
-	public static final String NOUN_TYPE = "Varchar(16000)";
-	public static final String DIRECTION_TYPE = "Varchar(3)";
-	
+	public static final String PROCESSED_COLUMN = "PROCESSED";
+
+	public static final String OP_TYPE = "Varchar(16000)";
+	public static final String NOUN_TYPE = "Varchar(250)";
+	public static final String DIRECTION_TYPE = "Varchar(3)"; //either 'IN' or 'OUT'
+	public static final String PROCESSED_TYPE = "BOOLEAN";
+
 	public static final String inDirection = "IN";
 	public static final String outDirection = "OUT";
+	
+	public static final String TRUE_PROCESSED = "TRUE";
+	public static final String FALSE_PROCESSED = "FALSE";
 	
 	public TablePKSLPlanner() {
 		super();
@@ -33,10 +37,11 @@ public class TablePKSLPlanner extends PKSLPlanner {
 		String[] types = getTypes();
 		
 		for(String output : outputs) {
-			String[] values = new String[3];
+			String[] values = new String[4];
 			values[0] = opName;
 			values[1] = output;
 			values[2] = outDirection;
+			values[3] = FALSE_PROCESSED;
 			table.addRow(values, headers, types);
 		}
 	}
@@ -48,10 +53,11 @@ public class TablePKSLPlanner extends PKSLPlanner {
 		String[] types = getTypes();
 		
 		for(String input : inputs) {
-			String[] values = new String[3];
+			String[] values = new String[4];
 			values[0] = opName;
 			values[1] = input;
-			values[2] = outDirection;
+			values[2] = inDirection;
+			values[3] = FALSE_PROCESSED;
 			table.addRow(values, headers, types);
 		}
 	}
@@ -61,10 +67,10 @@ public class TablePKSLPlanner extends PKSLPlanner {
 	}
 	
 	private String[] getHeaders() {
-		return new String[]{OPERATION_COLUMN, NOUN_COLUMN, DIRECTION_COLUMN};
+		return new String[]{OPERATION_COLUMN, NOUN_COLUMN, DIRECTION_COLUMN, PROCESSED_COLUMN};
 	}
 	
 	private String[] getTypes() {
-		return new String[]{OP_TYPE, NOUN_TYPE, DIRECTION_TYPE};
+		return new String[]{OP_TYPE, NOUN_TYPE, DIRECTION_TYPE, PROCESSED_TYPE};
 	}
 }
