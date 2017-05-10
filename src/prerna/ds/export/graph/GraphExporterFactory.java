fc526e0196cc2d2f2c44f45625ac09f13b0e7ac0
mahkhalil <mahkhalil@deloitte.com>
2017-05-10 12:22:20 +0000

    BUGFIX ::: Properly sending back status on responses UPDATE ::: Adding check for H2 Graph Exporting to throw an error if there are primary keys in the model.  Error message informs user to use data.frame.setEdgeHash in order to define how objects are related to one another UPDATE ::: Adding initial class for exporting R to graphs
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20680 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ds/export/graph/GraphExporterFactory.java
+++ src/prerna/ds/export/graph/GraphExporterFactory.java
@@ -1,26 +1,30 @@
 package prerna.ds.export.graph;
 
-import prerna.algorithm.api.ITableDataFrame;
 import prerna.ds.TinkerFrame;
 import prerna.ds.h2.H2Frame;
 import prerna.ds.nativeframe.NativeFrame;
 import prerna.ds.r.RDataTable;
+import prerna.ui.components.playsheets.datamakers.IDataMaker;
 
 public class GraphExporterFactory {
 
+	private GraphExporterFactory() {
+		
+	}
+	
 	/**
 	 * Based on the frame, get the correct exporter
 	 * @param frame
 	 * @return
 	 */
-	static IGraphExporter getExporter(ITableDataFrame frame) {
+	public static IGraphExporter getExporter(IDataMaker frame) {
 		IGraphExporter graphExporter = null;
 		if(frame instanceof TinkerFrame) {
 			graphExporter = new TinkerFrameGraphExporter((TinkerFrame) frame);
 		} else if(frame instanceof H2Frame) {
 			graphExporter = new RdbmsGraphExporter((H2Frame) frame);
 		} else if(frame instanceof RDataTable) {
-			
+//			graphExporter = new RGraphExporter((RDataTable) frame);
 		} else if(frame instanceof NativeFrame) {
 			
 		}
