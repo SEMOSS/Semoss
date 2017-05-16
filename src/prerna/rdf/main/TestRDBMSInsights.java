54413234c1d7029619a2b299216b4b8b61205061
Maher Khalil <themaherkhalil@gmail.com>
2017-05-16 12:30:03 +0000

    BUGFIX ::: Adding new r csv file wrapper class
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@20838 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 675206598327ff5cf35f4912a57b13dcf4d80c87

--- src/prerna/rdf/main/TestRDBMSInsights.java
+++ src/prerna/rdf/main/TestRDBMSInsights.java
@@ -1,11 +1,9 @@
 package prerna.rdf.main;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Vector;
 
-import prerna.engine.api.ISelectStatement;
-import prerna.engine.api.ISelectWrapper;
+import prerna.engine.api.IHeadersDataRow;
+import prerna.engine.api.IRawSelectWrapper;
 import prerna.engine.impl.rdbms.RDBMSNativeEngine;
 import prerna.rdf.engine.wrappers.WrapperManager;
 import prerna.test.TestUtilityMethods;
@@ -17,32 +15,60 @@ public class TestRDBMSInsights {
 		
 		TestUtilityMethods.loadDIHelper();
 
-		//TODO: put in correct path for your database
-		String engineProp = "C:\\workspace\\Semoss_Dev\\db\\test_this.smss";
+		String engineProp = "C:\\workspace\\Semoss_Dev\\db\\ab.smss";
 		RDBMSNativeEngine coreEngine = new RDBMSNativeEngine();
-		coreEngine.setEngineName("test_this");
+		coreEngine.setEngineName("ab");
 		coreEngine.openDB(engineProp);
-		DIHelper.getInstance().setLocalProperty("test_this", coreEngine);
+		DIHelper.getInstance().setLocalProperty("ab", coreEngine);
 		
-		String query = "SELECT DISTINCT ?relation where "
-				+ "{<http://semoss.org/ontologies/Concept/MOVIE_RESULTS_UNIQUE_ROW_ID/MOVIE_RESULTS> "
-				+ "?relation " 
-				+ "<http://semoss.org/ontologies/Concept/MOVIE_CHARACTERISTICS_UNIQUE_ROW_ID/MOVIE_CHARACTERISTICS> }"
-				+ "";
+		String query = "select * from customers";
 		
-		ISelectWrapper wrapper = WrapperManager.getInstance().getSWrapper(coreEngine.getBaseDataEngine(), query);
-		String[] names = wrapper.getDisplayVariables();
-		System.out.println(Arrays.toString(names));
-		// we will keep a set of the concepts such that we know when we need to append a PRIM_KEY_PLACEHOLDER
+		IRawSelectWrapper wrapper = WrapperManager.getInstance().getRawWrapper(coreEngine, query);
 		while(wrapper.hasNext()) {
-			ISelectStatement sjss = wrapper.next();
-			List<Object> row = new Vector<Object>();
-			for(int i = 0; i < names.length; i++) {
-				row.add(sjss.getVar(names[i]));
-			}
-			System.out.println(row);
+			IHeadersDataRow row = wrapper.next();
+			System.out.println(Arrays.toString(row.getRawValues()));
 		}
 		
+		System.out.println(">>>>>>>>>>>>>");
+		System.out.println(">>>>>>>>>>>>>");
+		System.out.println(">>>>>>>>>>>>>");
+		System.out.println(">>>>>>>>>>>>>");
+
+		query = "select * from user_tables";
+		
+		wrapper = WrapperManager.getInstance().getRawWrapper(coreEngine, query);
+		while(wrapper.hasNext()) {
+			IHeadersDataRow row = wrapper.next();
+			System.out.println(Arrays.toString(row.getRawValues()));
+		}
+		
+		
+//		//TODO: put in correct path for your database
+//		String engineProp = "C:\\workspace\\Semoss_Dev\\db\\test_this.smss";
+//		RDBMSNativeEngine coreEngine = new RDBMSNativeEngine();
+//		coreEngine.setEngineName("test_this");
+//		coreEngine.openDB(engineProp);
+//		DIHelper.getInstance().setLocalProperty("test_this", coreEngine);
+//		
+//		String query = "SELECT DISTINCT ?relation where "
+//				+ "{<http://semoss.org/ontologies/Concept/MOVIE_RESULTS_UNIQUE_ROW_ID/MOVIE_RESULTS> "
+//				+ "?relation " 
+//				+ "<http://semoss.org/ontologies/Concept/MOVIE_CHARACTERISTICS_UNIQUE_ROW_ID/MOVIE_CHARACTERISTICS> }"
+//				+ "";
+//		
+//		ISelectWrapper wrapper = WrapperManager.getInstance().getSWrapper(coreEngine.getBaseDataEngine(), query);
+//		String[] names = wrapper.getDisplayVariables();
+//		System.out.println(Arrays.toString(names));
+//		// we will keep a set of the concepts such that we know when we need to append a PRIM_KEY_PLACEHOLDER
+//		while(wrapper.hasNext()) {
+//			ISelectStatement sjss = wrapper.next();
+//			List<Object> row = new Vector<Object>();
+//			for(int i = 0; i < names.length; i++) {
+//				row.add(sjss.getVar(names[i]));
+//			}
+//			System.out.println(row);
+//		}
+//		
 		
 //		Properties diProp = new Properties();
 //		diProp.put("BaseFolder", "C:\\workspace\\Semoss");
