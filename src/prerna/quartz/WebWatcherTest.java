f3fb4f2a87327c71abca6ea8a94fa0bf0fc4f62c
tbanach <tbanach@deloitte.com>
2017-05-21 18:26:29 +0000

    UPDATE ::: Configuring Anthem web watchers for TS anomaly email notification
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@21021 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/quartz/WebWatcherTest.java
+++ src/prerna/quartz/WebWatcherTest.java
@@ -18,7 +18,6 @@ import org.quartz.SchedulerException;
 
 import prerna.algorithm.learning.unsupervised.anomaly.AnomalyDetector.AnomDirection;
 import prerna.engine.api.IEngine;
-import prerna.quartz.specific.anthem.ComposeTSAnomalyEmailJob;
 import prerna.quartz.specific.anthem.DetermineIfAnomalyJob;
 import prerna.util.AbstractFileWatcher;
 import prerna.util.Utility;
@@ -32,7 +31,7 @@ public class WebWatcherTest extends AbstractFileWatcher {
 
 	private static final String SMTP_SERVER = "127.0.0.1";
 	private static final int SMTP_PORT = 25;
-	
+
 	@Override
 	public void loadFirst() {
 		// Not really a web watcher, just want to run the test while the server
@@ -64,45 +63,46 @@ public class WebWatcherTest extends AbstractFileWatcher {
 		System.out.println("Testing out the job sequence");
 
 		// Logic here:
-		// Chain 1) Import TS data, get data from insight, detect anomalies, determine if anomaly, if anomaly then chain 2	
+		// Chain 1) Import TS data, get data from insight, detect anomalies,
+		// determine if anomaly, if anomaly then chain 2
 		// Chain 2) Compose the email, send the email
 		// Need to create 2) first because it is passed into chain 1
-		
+
 		//////////////////////////////////////////////////
 		// Chain 2
 		//////////////////////////////////////////////////
-		
+
 		List<Class<? extends Job>> chain2Sequence = new ArrayList<Class<? extends Job>>();
-//		chain2Sequence.add(ComposeTSAnomalyEmailJob.class);
+		// chain2Sequence.add(ComposeTSAnomalyEmailJob.class);
 		chain2Sequence.add(SendEmailJob.class);
-		
+
 		//////////////////////////////
 		// Define the initial data map
 		//////////////////////////////
 		JobDataMap chain2DataMap = new JobDataMap();
-		
+
 		// The job sequence
 		chain2DataMap.put(JobChain.IN_SEQUENCE, chain2Sequence);
-		
+
 		// Email params
 		Properties sessionProps = new Properties();
 		sessionProps.put("mail.smtp.host", SMTP_SERVER);
 		sessionProps.put("mail.smtp.port", Integer.toString(SMTP_PORT));
 		Session session = Session.getInstance(sessionProps);
 		chain2DataMap.put(SendEmailJob.IN_FROM_KEY, "tbanach@deloitte.com");
-		chain2DataMap.put(SendEmailJob.IN_TO_KEY, new String[] {"tbanach@deloitte.com"});
+		chain2DataMap.put(SendEmailJob.IN_TO_KEY, new String[] { "tbanach@deloitte.com" });
 		chain2DataMap.put(SendEmailJob.IN_SUBJECT_KEY, "hello world");
 		chain2DataMap.put(SendEmailJob.IN_BODY_KEY, "this is an automated message from SEMOSS");
 		chain2DataMap.put(SendEmailJob.IN_BODY_IS_HTML_KEY, false);
 		chain2DataMap.put(SendEmailJob.IN_SESSION_KEY, session);
-		
+
 		JobDetail jobChain2 = newJob(JobChain.class).withIdentity("jobChain2", "tsAnomaly").usingJobData(chain2DataMap)
 				.build();
-		
+
 		//////////////////////////////////////////////////
 		// Chain 1
 		//////////////////////////////////////////////////
-		
+
 		// Specify the chain of jobs
 		List<Class<? extends Job>> chain1Sequence = new ArrayList<Class<? extends Job>>();
 		chain1Sequence.add(CreateInsightJob.class);
@@ -115,21 +115,21 @@ public class WebWatcherTest extends AbstractFileWatcher {
 		// Define the initial data map
 		//////////////////////////////
 		JobDataMap chain1DataMap = new JobDataMap();
-		
+
 		// The job sequence
 		chain1DataMap.put(JobChain.IN_SEQUENCE, chain1Sequence);
-		
+
 		// Import params
 		String importRecipe = "data.import ( api: " + ENGINE_NAME + " . query ( [ c: " + TIMESERIES_ID_KEY + " , c: "
 				+ TIMESERIES_ID_KEY + "__" + TIME_COLUMN + " , c: " + TIMESERIES_ID_KEY + "__" + SERIES_COLUMN
 				+ " ] ) ) ;";
-		
+
 		// For testing add another pkql
 		importRecipe += "panel[0].viz ( Line , [ label= c: " + TIME_COLUMN + " , value= m: Average ( [ c: "
 				+ SERIES_COLUMN + " ] [ c: " + TIME_COLUMN + " ] ) ] , { \"offset\" : 0 , \"limit\" : 1000 } ) ; ";
 		chain1DataMap.put(CreateInsightJob.IN_RECIPE_KEY, importRecipe);
 		chain1DataMap.put(CreateInsightJob.IN_ENGINE_NAME_KEY, ENGINE_NAME);
-		
+
 		// Anomaly params
 		chain1DataMap.put(DetectAnomaliesJob.IN_TIME_COLUMN_KEY, TIME_COLUMN);
 		chain1DataMap.put(DetectAnomaliesJob.IN_SERIES_COLUMN_KEY, SERIES_COLUMN);
@@ -139,16 +139,16 @@ public class WebWatcherTest extends AbstractFileWatcher {
 		chain1DataMap.put(DetectAnomaliesJob.IN_ALPHA_KEY, 0.05);
 		chain1DataMap.put(DetectAnomaliesJob.IN_PERIOD_KEY, 7);
 		chain1DataMap.put(DetectAnomaliesJob.IN_KEEP_EXISTING_COLUMNS_KEY, false);
-		
+
 		// The second chain for notification if anomaly
 		chain1DataMap.put(IfJob.IN_IF_TRUE_JOB, jobChain2);
-		
+
 		//////////////////////////////
 		// Create the job chain
 		//////////////////////////////
 		JobDetail jobChain1 = newJob(JobChain.class).withIdentity("jobChain1", "tsAnomaly").usingJobData(chain1DataMap)
 				.build();
-		
+
 		//////////////////////////////////////////////////
 		// Execute the work
 		//////////////////////////////////////////////////
