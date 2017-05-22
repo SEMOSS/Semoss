8267848d72504c46d7476aa3777c3735fbf99332
mahkhalil <mahkhalil@deloitte.com>
2017-05-22 20:23:00 +0000

    UPDATE ::: Allowing for sorting headers to be defined in options map
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@21091 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 14b195ba67deacb044111b49d7dfc1864c8a93c4

--- src/prerna/sablecc/DataFrameHeaderReactor.java
+++ src/prerna/sablecc/DataFrameHeaderReactor.java
@@ -1,5 +1,7 @@
 package prerna.sablecc;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -11,8 +13,9 @@ import prerna.sablecc.meta.IPkqlMetadata;
 
 public class DataFrameHeaderReactor extends AbstractReactor {
 
-	public static final String NUMERIC_VALUES_ONLY = "onlyNumeric";
-	
+	private static final String NUMERIC_VALUES_ONLY = "onlyNumeric";
+	private static final String SORT_VALUES = "sortDir";
+
 	public DataFrameHeaderReactor() {
 		String[] thisReacts = {PKQLEnum.MAP_OBJ};
 		super.whatIReactTo = thisReacts;
@@ -24,11 +27,17 @@ public class DataFrameHeaderReactor extends AbstractReactor {
 		ITableDataFrame table = (ITableDataFrame) myStore.get("G");
 		
 		// get additional options
+		boolean sort = false;
+		String sortDirection = "";
 		boolean returnOnlyNumbers = false;
 		Map<Object, Object> mapOptions = (Map<Object, Object>) myStore.get(PKQLEnum.MAP_OBJ);
 		if(mapOptions != null) {
 			if(mapOptions.containsKey(NUMERIC_VALUES_ONLY)) {
 				returnOnlyNumbers = Boolean.parseBoolean(mapOptions.get(NUMERIC_VALUES_ONLY) + "");
+			}
+			if(mapOptions.containsKey(SORT_VALUES)) {
+				sort = true;
+				sortDirection = mapOptions.get(SORT_VALUES) + "";
 			}
 		}
 		
@@ -50,6 +59,7 @@ public class DataFrameHeaderReactor extends AbstractReactor {
 			}
 		}
 		
+		// taking into consideration option to only return numerical values
 		if(returnOnlyNumbers) {
 			int offset = 0;
 			for(int i = 0; i < size; i++) {
@@ -60,6 +70,13 @@ public class DataFrameHeaderReactor extends AbstractReactor {
 					offset++;
 				}
 			}
+		}
+		
+		// taking into consideration to sort values alphabetically
+		// default sort is based on order it was added to the frame
+		// TODO: future optimization to auto sort when getting data from meta data
+		if(sort) {
+			Collections.sort(tableHeaderObjects, new HeaderComparable(sortDirection));
 		}
 		
 		myStore.put("tableHeaders", tableHeaderObjects);
@@ -75,3 +92,22 @@ public class DataFrameHeaderReactor extends AbstractReactor {
 	}
 
 }
+
+class HeaderComparable implements Comparator<Map<String, Object>> {
+
+	private String sortDir;
+	
+	public HeaderComparable(String sortDir) {
+		this.sortDir = sortDir.toLowerCase();
+	}
+	
+	@Override
+	public int compare(Map<String, Object> o1, Map<String, Object> o2) {
+		if(sortDir.startsWith("asc")) {
+			return -1 * o1.get("name").toString().compareTo(o2.get("name").toString());
+		} else {
+			return o1.get("name").toString().compareTo(o2.get("name").toString());
+		}
+	}
+}
+
