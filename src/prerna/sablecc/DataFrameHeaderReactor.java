c238440d4ad2b8f9d2b9b6f7678b84714ec39a3b
mahkhalil <mahkhalil@deloitte.com>
2017-05-22 17:02:37 +0000

    UPDATE ::: Modification to data.frame.getHeaders to pass in map object to know if only to return numeric columns
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@21059 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc/DataFrameHeaderReactor.java
+++ src/prerna/sablecc/DataFrameHeaderReactor.java
@@ -1,12 +1,8 @@
 package prerna.sablecc;
 
-import java.util.Arrays;
-import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
 
 import prerna.algorithm.api.ITableDataFrame;
 import prerna.sablecc.PKQLRunner.STATUS;
@@ -15,10 +11,10 @@ import prerna.sablecc.meta.IPkqlMetadata;
 
 public class DataFrameHeaderReactor extends AbstractReactor {
 
-	public static final String ADDITIONAL_INFO_BOOL = "additionalInfoBool";
+	public static final String NUMERIC_VALUES_ONLY = "onlyNumeric";
 	
 	public DataFrameHeaderReactor() {
-		String[] thisReacts = {};
+		String[] thisReacts = {PKQLEnum.MAP_OBJ};
 		super.whatIReactTo = thisReacts;
 		super.whoAmI = PKQLEnum.DATA_FRAME_HEADER;
 	}
@@ -26,24 +22,47 @@ public class DataFrameHeaderReactor extends AbstractReactor {
 	@Override
 	public Iterator process() {
 		ITableDataFrame table = (ITableDataFrame) myStore.get("G");
-		Boolean includeAdditionalInfo = (Boolean) myStore.get(ADDITIONAL_INFO_BOOL);
-		if(!(includeAdditionalInfo == null || includeAdditionalInfo.equals(false))) {
-			Set<String> orderHeaders = new TreeSet<String>();
-			orderHeaders.addAll(Arrays.asList(table.getColumnHeaders()));
-			Map<String, Set<String>> retMap = new Hashtable<String, Set<String>>();
-			retMap.put("list", orderHeaders);
-			myStore.put("tableHeaders", retMap);
-		} else {
-			List<Map<String, Object>> tableHeaderObjects = table.getTableHeaderObjects();
-			for(Map<String, Object> nextObject : tableHeaderObjects) {
-				Object name = nextObject.remove("varKey");
-				nextObject.remove("uri");
-				nextObject.put("name", name);
+		
+		// get additional options
+		boolean returnOnlyNumbers = false;
+		Map<Object, Object> mapOptions = (Map<Object, Object>) myStore.get(PKQLEnum.MAP_OBJ);
+		if(mapOptions != null) {
+			if(mapOptions.containsKey(NUMERIC_VALUES_ONLY)) {
+				returnOnlyNumbers = Boolean.parseBoolean(mapOptions.get(NUMERIC_VALUES_ONLY) + "");
 			}
-			
-			myStore.put("tableHeaders", tableHeaderObjects);
 		}
-		myStore.put("RESPONSE", STATUS.SUCCESS.toString());
+		
+		// get the table header object
+		List<Map<String, Object>> tableHeaderObjects = table.getTableHeaderObjects();
+		int size = tableHeaderObjects.size();
+		// keep track in case we need to remove specific indices
+		boolean[] arrayRemoveIndex = new boolean[size];
+		for(int i = 0; i < size; i++) {
+			Map<String, Object> nextObject = tableHeaderObjects.get(i);
+			Object name = nextObject.remove("varKey");
+			nextObject.remove("uri");
+			nextObject.put("name", name);
+			if(returnOnlyNumbers) {
+				String type = nextObject.get("type").toString();
+				if(!type.equals("NUMBER")) {
+					arrayRemoveIndex[i] = true;
+				}
+			}
+		}
+		
+		if(returnOnlyNumbers) {
+			int offset = 0;
+			for(int i = 0; i < size; i++) {
+				if(arrayRemoveIndex[i] == true) {
+					// need to keep track of offset since 
+					// indices change with each removal
+					tableHeaderObjects.remove(i - offset);
+					offset++;
+				}
+			}
+		}
+		
+		myStore.put("tableHeaders", tableHeaderObjects);
 		myStore.put("STATUS", STATUS.SUCCESS);
 
 		return null;
