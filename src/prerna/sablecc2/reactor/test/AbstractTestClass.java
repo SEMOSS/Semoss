d9fb8f62a48572a946e2f9233a60352f1980c217
rluthar <rluthar@deloitte.com>
2017-05-31 15:19:31 +0000

    UPDATE ::: adding a test for performance of compiled classes
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@21325 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/sablecc2/reactor/test/AbstractTestClass.java
@@ -0,0 +1,46 @@
+package prerna.sablecc2.reactor.test;
+
+import java.util.*;
+
+import prerna.sablecc2.om.VarStore;
+
+/**
+ * This is just a internal class
+ * so that when we compile to execute the assimilator
+ * we can have a method to call based on the super that is 
+ * assigned to the new class
+ *
+ */
+public abstract class AbstractTestClass {
+
+	Map<String, Object> variables = new HashMap<>();
+	
+	public AbstractTestClass() {
+		
+	}
+	
+	/**
+	 * Method that return the evaluation of 
+	 * the signature
+	 * @return
+	 */
+	public void execute() {
+
+	}
+	
+	public Map<String, Object> getVariables() {
+		return this.variables;
+	}
+	
+	public void addVariable(String var) {
+		variables.put(var, 1);
+	}
+	
+	public void addVariable(String var, Integer value) {
+		variables.put(var, value);
+	}
+	
+//	public void addVariable(String var, double value) {
+//		variables.put(var, value);
+//	}
+}
