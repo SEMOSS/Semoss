2f3f5d38ba3dd7d0ee7f9f38f177b510a30107b1
mahkhalil <mahkhalil@deloitte.com>
2017-05-31 13:00:35 +0000

    UPDATE ::: Adding PKSL reactors for local master calls to get list of databases, get concepts within engine, get properties for a concept, and get the engine metamodel
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@21322 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 9d8b51f96e7f4d9448f3f938d8903cdf6960d8d7

---
+++ src/prerna/sablecc2/reactor/masterdatabase/DatabaseConceptPropertiesReactors.java
@@ -0,0 +1,38 @@
+package prerna.sablecc2.reactor.masterdatabase;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import prerna.nameserver.utility.MasterDatabaseUtility;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.PkslDataTypes;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class DatabaseConceptPropertiesReactors extends AbstractReactor {
+
+	@Override
+	public NounMetadata execute() {
+		GenRowStruct conceptNamesGrs = this.store.getNoun("concepts");
+		if(conceptNamesGrs == null) {
+			throw new IllegalArgumentException("Need to define the concepts to find relations");
+		}
+		List<String> conceptLogicals = new Vector<String>();
+		int size = conceptNamesGrs.size();
+		for(int i = 0; i < size; i++) {
+			conceptLogicals.add(conceptNamesGrs.get(i).toString());
+		}
+		
+		// account for optional engine filter
+		GenRowStruct engineFilterGrs = this.store.getNoun("engine");
+		String engineFilter = null;
+		if(engineFilterGrs != null) {
+			engineFilter = engineFilterGrs.get(0).toString();
+		}
+		
+		Map<String, Object[]> conceptProperties = MasterDatabaseUtility.getConceptProperties(conceptLogicals, engineFilter);
+		return new NounMetadata(conceptProperties, PkslDataTypes.CUSTOM_DATA_STRUCTURE);
+	}
+
+}
