1bee65888d07e1142e733c9b1320ed4a31f94ac0
rluthar <rluthar@deloitte.com>
2017-06-08 23:20:10 +0000

    UPDATE ::: implementing java signature for opAnd
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@21523 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f531172898ef664d584b35519487ce81145349c1

--- src/prerna/sablecc2/reactor/expression/OpAnd.java
+++ src/prerna/sablecc2/reactor/expression/OpAnd.java
@@ -1,7 +1,10 @@
 package prerna.sablecc2.reactor.expression;
 
+import java.util.List;
+
 import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PkslDataTypes;
+import prerna.sablecc2.reactor.JavaExecutable;
 
 public class OpAnd extends OpBasic {
 
@@ -23,10 +26,51 @@ public class OpAnd extends OpBasic {
 		}
 		return result;
 	}
+	
+	public boolean eval(boolean[] values) {
+		boolean result = true;
+		for (Object booleanValue : values) {
+			// need all values to be true
+			// in order to return true
+			if(! (boolean) booleanValue) {
+				result = false;
+				break;
+			}
+		}
+		return result;
+	}
 
 	@Override
+	public String getJavaSignature() {
+		StringBuilder javaSignature = new StringBuilder(this.getClass().getName()+".eval(new boolean[] {");
+		List<NounMetadata> inputs = this.getJavaInputs();
+		for(int i = 0; i < inputs.size(); i++) {
+			if(i > 0) {
+				javaSignature.append(", ");
+			}
+			
+			String nextArgument;
+			NounMetadata nextNoun = inputs.get(i);
+			Object nextInput = inputs.get(i).getValue();
+			if(nextInput instanceof JavaExecutable) {
+				nextArgument = ((JavaExecutable)nextInput).getJavaSignature();
+			} else {
+				if(nextNoun.getNounName() == PkslDataTypes.CONST_STRING) {
+					nextArgument = "\""+nextInput.toString() +"\"";
+				} else {
+					nextArgument = nextInput.toString();
+				}
+			}
+			javaSignature.append(nextArgument);
+		}
+		javaSignature.append("})");
+		
+		return javaSignature.toString();
+	}
+	
+	
+	@Override
 	public String getReturnType() {
-		// TODO Auto-generated method stub
 		return "boolean";
 	}
 }
