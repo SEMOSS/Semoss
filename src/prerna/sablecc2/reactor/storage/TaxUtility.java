43fa4df3bcd26c9bd8c74bbce0787bd836545103
mahkhalil <mahkhalil@deloitte.com>
2017-06-27 16:47:43 +0000

    UPDATE ::: Expecting the FE to pass us the constant alias instead of the hashcode since the hashcode changes as the model changes
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@21884 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: bdcaf9a0f57be4c6d9ee67472d273a416522849f

---
+++ src/prerna/sablecc2/reactor/storage/TaxUtility.java
@@ -0,0 +1,98 @@
+package prerna.sablecc2.reactor.storage;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+import prerna.engine.api.IEngine;
+import prerna.engine.impl.rdbms.RDBMSNativeEngine;
+import prerna.util.Utility;
+
+public class TaxUtility {
+
+	private TaxUtility() {
+
+	}
+
+	/**
+	 * Execute a query to get the hashcode used from the alias
+	 * @param aliasList
+	 * @return
+	 */
+	public static Map<String, String> mapAliasToHash(List<String> aliasList) {
+		String filterQuery = getInFilter(aliasList);
+
+		Map<String, String> aliasHashMap = new Hashtable<String, String>();
+		// execute the query on both databases
+		String sql = "SELECT ALIAS_1, HASHCODE FROM INPUTCSV WHERE ALIAS_1 " + filterQuery;
+		execAliasToHashCodeQuery(Utility.getEngine("MinInput"), sql, aliasHashMap);
+		sql = "SELECT ALIAS_1, HASHCODE FROM IMPACTCSV WHERE ALIAS_1 " + filterQuery;
+		execAliasToHashCodeQuery(Utility.getEngine("MinImpact"), sql, aliasHashMap);
+
+		return aliasHashMap;
+	}
+	
+	/**
+	 * Execute the query on the engines to get the conversion from alias to hashcode
+	 * @param engine
+	 * @param sql
+	 * @param aliasHashMap
+	 */
+	private static void execAliasToHashCodeQuery(IEngine engine, String sql, Map<String, String> aliasHashMap) {
+		Map<String, Object> queryRet = (Map<String, Object>)engine.execQuery(sql);
+		Statement stmt = (Statement) queryRet.get(RDBMSNativeEngine.STATEMENT_OBJECT);
+		ResultSet rs = (ResultSet) queryRet.get(RDBMSNativeEngine.RESULTSET_OBJECT);
+		try {
+			flushRsToMap(rs, aliasHashMap);
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				stmt.close();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Flush a result set into a map
+	 * Assumption that rs only returns 2 columns
+	 * Assumption that column1 is the key, column2 is the value
+	 * Assumption that rs only returns strings
+	 * @param rs
+	 * @param map
+	 * @throws SQLException 
+	 */
+	private static void flushRsToMap(ResultSet rs, Map<String, String> map) throws SQLException {
+		while(rs.next()) {
+			map.put(rs.getString(1), rs.getString(2));
+		}
+	}
+
+	/**
+	 * Generate a string for the SQL IN operator
+	 * assumes all inputs are strings
+	 * @param aliasList
+	 * @return
+	 */
+	private static String getInFilter(List<String> aliasList) {
+		StringBuilder sql = new StringBuilder(" IN (");
+		sql.append("'").append(aliasList.get(0)).append("'");
+		for(int i = 1; i < aliasList.size(); i++) {
+			sql.append(",'").append(aliasList.get(i)).append("'");
+		}
+		sql.append(")");
+		return sql.toString();
+	}
+
+
+}
