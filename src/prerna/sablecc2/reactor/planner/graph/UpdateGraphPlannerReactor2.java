0836fb0e759498b5a04e00e39f03e12f900abf31
mahkhalil <mahkhalil@deloitte.com>
2017-06-27 17:18:50 +0000

    UPDATE ::: Having the update pksl reactor for tax take in the alias and convert to the hashcode
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@21885 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 008d139d66a6c3e353c62665048d05de1ccaae40

--- src/prerna/sablecc2/reactor/planner/graph/UpdateGraphPlannerReactor2.java
+++ src/prerna/sablecc2/reactor/planner/graph/UpdateGraphPlannerReactor2.java
@@ -1,6 +1,7 @@
 package prerna.sablecc2.reactor.planner.graph;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Vector;
 
 import prerna.sablecc2.om.GenRowStruct;
@@ -8,6 +9,7 @@ import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PkslDataTypes;
 import prerna.sablecc2.reactor.BaseJavaRuntime;
 import prerna.sablecc2.reactor.PKSLPlanner;
+import prerna.sablecc2.reactor.storage.TaxUtility;
 
 public class UpdateGraphPlannerReactor2 extends AbstractPlannerReactor {
 
@@ -21,16 +23,11 @@ public class UpdateGraphPlannerReactor2 extends AbstractPlannerReactor {
 		
 		// grab all the pksls
 		GenRowStruct pksls = this.store.getNoun(PKSL_NOUN);
-		
 		// store them in a list
-		// and also keep a builder with all the executions
-		List<String> pkslsToAdd = new Vector<String>();
-		int numPksls = pksls.size();
+		List<String> pkslsToAdd = getPkslStrings(pksls);		
+		
 		PKSLPlanner basePlanner = getPlanner();
 		BaseJavaRuntime javaRunClass = (BaseJavaRuntime) basePlanner.getProperty("RUN_CLASS", "RUN_CLASS");
-		for(int i = 0; i < numPksls; i++) {
-			pkslsToAdd.add(pksls.get(i).toString());
-		}
 		RuntimeJavaClassBuilder builder = new RuntimeJavaClassBuilder();
 		builder.setSuperClass(javaRunClass.getClass());
 		builder.addEquations(pkslsToAdd);
@@ -46,7 +43,6 @@ public class UpdateGraphPlannerReactor2 extends AbstractPlannerReactor {
 	 * @return
 	 */
 	protected PKSLPlanner getPlanner() {
-
 		GenRowStruct allNouns = getNounStore().getNoun(PkslDataTypes.PLANNER.toString());
 		if (allNouns != null && allNouns.size() > 0) {
 			Object firstParam = allNouns.get(0);
@@ -57,4 +53,56 @@ public class UpdateGraphPlannerReactor2 extends AbstractPlannerReactor {
 		}
 		return null;
 	}
+	
+	/**
+	 * This method returns the set of pksls to add
+	 * It assumes the FE is passing the pksls using the alias
+	 * and we convert that to the hashcode
+	 * @param pksls
+	 * @return
+	 */
+	private List<String> getPkslStrings(GenRowStruct pksls) {
+		// in the grs
+		// these strings have the alias
+		// we need to convert them to the hashcode
+		
+		// keep the the list of the component pksls so we can 
+		// substitute the first index from alias to hashcode
+		List<String[]> componentPksls = new Vector<String[]>();
+		
+		// keep a list of the aliases
+		// this is so we can run fewer queries on the engines
+		List<String> aliasList = new Vector<String>();
+		
+		// iterate through the pksls being updated
+		int numPksls = pksls.size();
+		for(int i = 0; i < numPksls; i++) {
+			String aliasPksl = pksls.get(i).toString();
+			
+			// split on the equals and add it ot the list
+			String[] aliasBreak = aliasPksl.split("=");
+			componentPksls.add(aliasBreak);
+			
+			// add the alias to the list
+			aliasList.add(aliasBreak[0].trim());
+		}
+		
+		// get the proper hash for each alias
+		Map<String, String> aliasToHashMap = TaxUtility.mapAliasToHash(aliasList);
+
+		// reconstruct the pksls passed using the hashcode
+		List<String> pkslsToAdd = new Vector<String>();
+		for(int i = 0; i < numPksls; i++) {
+			String[] aliasBreak = componentPksls.get(i);
+			// replace the alias with the hashcode
+			String newPkslString = aliasToHashMap.get(aliasBreak[0].trim()) + " = " + aliasBreak[1];
+			if(!newPkslString.endsWith(";")) {
+				newPkslString = newPkslString + ";";
+			}
+			pkslsToAdd.add(newPkslString);
+		}
+		
+		return pkslsToAdd;
+	}
+	
 }
