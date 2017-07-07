8c59cda84727583333b599ef4fc1d8817b04a403
mahkhalil <mahkhalil@deloitte.com>
2017-07-07 16:03:27 +0000

    BUGFIX ::: Fixing errors with running existing algorithms on r frames UPDATE ::: Fixing annoying comment structure in other classes
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@22005 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a8459b0dc3718d3284ddbe1502bd9ee8d0aeeff0

--- src/prerna/sablecc2/reactor/FrameFactory.java
+++ src/prerna/sablecc2/reactor/FrameFactory.java
@@ -3,38 +3,17 @@ package prerna.sablecc2.reactor;
 import prerna.algorithm.api.ITableDataFrame;
 import prerna.ds.TinkerFrame;
 import prerna.ds.h2.H2Frame;
+import prerna.ds.r.RDataTable;
 
 public class FrameFactory {
 
 	public static ITableDataFrame getFrame(String frameType) {
 		switch (frameType.toUpperCase()) {
-
-		case "GRID": {
-			return new H2Frame();
+		case "GRID": { return new H2Frame(); }
+		case "GRAPH": { return new TinkerFrame(); } 
+		case "RFRAME": { return new RDataTable(); }
+		case "R": { return new RDataTable(); }
+		default: { return new H2Frame(); }
 		}
-
-		case "GRAPH": {
-			return new TinkerFrame();
-		}
-
-		//		case "SPARK": {
-		//			return new H2Frame();
-		//		}
-		//
-		//		case "NATIVE": {
-		//			return new H2Frame();
-		//		}
-		//		case "RFRAME": {
-		//			return new H2Frame();
-		//		}
-		//		case "MSSQLSERVER": {
-		//			return new H2Frame();
-		//		}
-
-		default: {
-			return new H2Frame();
-		}
-		}
-
 	}
 }
