54b824deaeb9dd912253632a11f8f2a097b5f25d
mahkhalil <mahkhalil@deloitte.com>
2017-07-11 21:32:21 +0000

    UPDATE ::: Modification to pksl format reactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@22057 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ea53a30fbce154b3ae23b39b18c7700660eaa9a7

--- src/prerna/sablecc2/reactor/export/AbstractFormatter.java
+++ src/prerna/sablecc2/reactor/export/AbstractFormatter.java
@@ -4,18 +4,7 @@ import java.util.Map;
 
 public abstract class AbstractFormatter implements Formatter {
 
-	protected String name;
 	protected Map<String, Object> optionsMap;
-	
-	@Override
-	public void setIdentifier(String name) {
-		this.name = name;
-	}
-	
-	@Override
-	public String getIdentifier() {
-		return this.name;
-	}
 	
 	@Override
 	public void setOptionsMap(Map<String, Object> optionsMap) {
