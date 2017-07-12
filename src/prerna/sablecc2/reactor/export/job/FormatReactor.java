faccd5331b2ece70e48e52215c7f918d5c766930
mahkhalil <mahkhalil@deloitte.com>
2017-07-12 23:04:08 +0000

    UPDATE ::: Revising target/views to be set in ViewOptions
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@22090 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ec5057128057f2d942143e6d86f80d2757817bd2

--- src/prerna/sablecc2/reactor/export/job/FormatReactor.java
+++ src/prerna/sablecc2/reactor/export/job/FormatReactor.java
@@ -1,8 +1,5 @@
 package prerna.sablecc2.reactor.export.job;
 
-import java.util.List;
-import java.util.Vector;
-
 import prerna.sablecc2.om.GenRowStruct;
 
 public class FormatReactor extends JobBuilderReactor {
@@ -14,30 +11,5 @@ public class FormatReactor extends JobBuilderReactor {
 			String formatType = type.get(0).toString();
 			job.setFormat(formatType);
 		}
-		
-		GenRowStruct views = getNounStore().getNoun("views");
-		if(views != null && !views.isEmpty()) {
-			job.setViews(flushOutGrs(views));
-		}
-		
-		GenRowStruct targets = getNounStore().getNoun("targets");
-		if(targets != null && !targets.isEmpty()) {
-			job.setTargets(flushOutGrs(targets));
-		}
-	}
-	
-	/**
-	 * Used to iterate through a set of grs values to get the objects
-	 * @param grs
-	 * @return
-	 */
-	private List<String> flushOutGrs(GenRowStruct grs) {
-		List<String> grsVals = new Vector<String>();
-		int grsSize = grs.size();
-		for(int i = 0; i < grsSize; i++) {
-			grsVals.add(grs.get(i).toString());
-		}
-		
-		return grsVals;
 	}
 }
