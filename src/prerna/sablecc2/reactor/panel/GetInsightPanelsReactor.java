c8c0371e8a53c78004c2079df1a721f71ddd5c02
mahkhalil <mahkhalil@deloitte.com>
2017-07-13 20:55:07 +0000

    UPDATE ::: Adding reactor to close a panel UPDATE ::: Adding reactor to get all the panels that exist for a given insight UPDATE ::: Returning the name of the frame created whenever a frame is returned as a noun meta data
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@22099 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d90086be5644d1cce70006d45ec5addede31dca1

---
+++ src/prerna/sablecc2/reactor/panel/GetInsightPanelsReactor.java
@@ -0,0 +1,16 @@
+package prerna.sablecc2.reactor.panel;
+
+import java.util.Set;
+
+import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.PkslDataTypes;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class GetInsightPanelsReactor extends AbstractReactor {
+
+	@Override
+	public NounMetadata execute() {
+		Set<String> panelIds = this.insight.getInsightPanels().keySet();
+		return new NounMetadata(panelIds, PkslDataTypes.VECTOR);
+	}
+}
