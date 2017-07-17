251e4717624751aa302853f9c9107225f205e127
Maher Khalil <themaherkhalil@gmail.com>
2017-07-17 18:40:29 +0000

    UPDATE ::: Adding in basic reactor for saving the given pksl insight UPDATE ::: Adding in basic reactor for running a saved insight and creating a new insight based of a saved insight
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@22143 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a4d312853b5457b05feea8d114a9a823239746bc

---
+++ src/prerna/sablecc2/reactor/framemeta/GetFrameHeaderMetadataReactor.java
@@ -0,0 +1,14 @@
+package prerna.sablecc2.reactor.framemeta;
+
+import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class GetFrameHeaderMetadataReactor extends AbstractReactor {
+
+	@Override
+	public NounMetadata execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
