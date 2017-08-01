5c9532dc1575f4260c74e2edba586f83875f18cc
tbanach <tbanach@deloitte.com>
2017-08-01 22:47:05 +0000

    UPDATE ::: Adding in QuartzWebWatcher.java which watches for job definitions (.json defining the job params) in the semoss home/quartz directory. Needed a method to grab a saved insight for the scheduling framework so I also added this in InsightUtility. (Renamed old method more appropriately to createTemporaryInsight.)
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@22473 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b0b4a3bdc6b3e0a13e2fd7ba33088f53bceaa856

--- src/prerna/quartz/CreateInsightJob.java
+++ src/prerna/quartz/CreateInsightJob.java
@@ -10,9 +10,8 @@ import prerna.util.insight.InsightUtility;
 public class CreateInsightJob implements org.quartz.Job {
 
 	public static final String IN_RECIPE_KEY = "createInsightRecipe";
-	public static final String IN_ENGINE_NAME_KEY = "createInsightEngineName";
 
-	public static final String OUT_INSIGHT_ID_KEY = LinkedDataKeys.INSIGHT_ID;
+	public static final String OUT_INSIGHT_ID_KEY = CommonDataKeys.INSIGHT_ID;
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
@@ -20,10 +19,9 @@ public class CreateInsightJob implements org.quartz.Job {
 		// Get inputs
 		JobDataMap dataMap = context.getMergedJobDataMap();
 		String recipe = dataMap.getString(IN_RECIPE_KEY);
-		String engineName = dataMap.getString(IN_ENGINE_NAME_KEY);
 
 		// Do work
-		Insight insight = InsightUtility.createInsight(engineName);
+		Insight insight = InsightUtility.createTemporaryInsight();
 		InsightUtility.runPkql(insight, recipe);
 
 		// Store outputs
