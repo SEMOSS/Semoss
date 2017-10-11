3adb962910536e83beed1aaea860e39ae3d491b4
mahkhalil <mahkhalil@deloitte.com>
2017-10-11 12:48:54 +0000

    BUGFIX ::: Fixing error with querying properties on Tinker engine
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24062 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/query/interpreters/GremlinInterpreter2.java
+++ src/prerna/query/interpreters/GremlinInterpreter2.java
@@ -57,12 +57,12 @@ public class GremlinInterpreter2 extends AbstractQueryInterpreter {
 		for(String nodeSelector : this.selectors) {
 			allAliasSelectors.add(nodeSelector);
 		}
-		for(String conceptKey : this.propHash.keySet()) {
-			List<String> props = this.propHash.get(conceptKey);
-			for(String propertySelector : props) {
-				allAliasSelectors.add(propertySelector);
-			}
-		}
+//		for(String conceptKey : this.propHash.keySet()) {
+//			List<String> props = this.propHash.get(conceptKey);
+//			for(String propertySelector : props) {
+//				allAliasSelectors.add(propertySelector);
+//			}
+//		}
 
 		if(allAliasSelectors.size() == 1) {
 			this.gt = this.gt.select(allAliasSelectors.get(0));
@@ -107,7 +107,7 @@ public class GremlinInterpreter2 extends AbstractQueryInterpreter {
 					this.selectors.add(table);
 				} else {
 					// add the property
-					this.selectors.add(column);
+					this.selectors.add(table + "__" +column);
 					// also store the property in the prop hash
 					List<String> properties = null;
 					if(this.propHash.containsKey(table)) {
