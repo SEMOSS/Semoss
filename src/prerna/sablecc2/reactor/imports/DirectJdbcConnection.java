35472305a4e03d0a13af97f51697f8c831118115
mahkhalil <mahkhalil@deloitte.com>
2017-10-11 15:15:08 +0000

    UPDATE ::: Shifting to single area for holding the rdbms connection info
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24067 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 04b08478973f6ecd6ed12d7b676620e147709fe6

--- src/prerna/sablecc2/reactor/imports/DirectJdbcConnection.java
+++ src/prerna/sablecc2/reactor/imports/DirectJdbcConnection.java
@@ -1,16 +1,10 @@
 package prerna.sablecc2.reactor.imports;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
-import prerna.ds.TinkerMetaHelper;
+import prerna.engine.impl.rdbms.RdbmsConnectionHelper;
 import prerna.rdf.engine.wrappers.RawRDBMSSelectWrapper;
-import prerna.sablecc.PKQLRunner;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PixelDataType;
@@ -27,35 +21,6 @@ public class DirectJdbcConnection extends AbstractReactor {
 	public static final String USERNAME_KEY = "userName";
 	public static final String PASSWORD_KEY = "password";
 
-	private static final String ASTER = "ASTER_DB";
-	private static final String ASTER_DRIVER = "com.asterdata.ncluster.jdbc.core.NClusterJDBCDriver";
-	private static final String CASSANDRA = "CASSANDRA";
-	private static final String CASSANDRA_DRIVER = "com.github.adejanovski.cassandra.jdbc.CassandraDriver";
-	private static final String DB2 = "DB2";
-	private static final String DB2_DRIVER = "com.ibm.db2.jcc.DB2Driver";
-	private static final String DERBY = "DERBY";
-	private static final String DERBY_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
-	private static final String H2 = "H2_DB";
-	private static final String H2_DRIVER = "org.h2.Driver";
-	private static final String IMPALA = "IMPALA";
-	private static final String IMPALA_DRIVER = "com.cloudera.impala.jdbc3.Driver";
-	private static final String MARIADB = "MARIA_DB";
-	private static final String MARIADB_DRIVER = "org.mariadb.jdbc.Driver";
-	private static final String MYSQL = "MYSQL";
-	private static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
-	private static final String ORACLE = "ORACLE";
-	private static final String ORACLE_DRIVER = "oracle.jdbc.driver.OracleDriver";
-	private static final String PHOENIX = "PHOENIX";
-	private static final String PHOENIX_DRIVER = "org.apache.phoenix.jdbc.PhoenixDriver";
-	private static final String POSTGRES = "POSTGRES";
-	private static final String POSTGRES_DRIVER = "org.postgresql.Driver";
-	private static final String SAP_HANA = "SAP_HANA";
-	private static final String SAP_HANA_DRIVER = "com.sap.db.jdbc.Driver";
-	private static final String SQLSERVER = "SQL_SERVER";
-	private static final String SQLSERVER_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	private static final String TERADATA = "TERADATA";
-	private static final String TERADATA_DRIVER = "com.teradata.jdbc.TeraDriver";
-
 	@Override
 	public NounMetadata execute() {
 		String query = getQuery();
@@ -64,56 +29,14 @@ public class DirectJdbcConnection extends AbstractReactor {
 		String driver = getDbDriver();
 		String connectionUrl = getConnectionString();
 
+		Connection con = null;
 		try {
-			if (driver.equalsIgnoreCase(ASTER)) {
-				Class.forName(ASTER_DRIVER);
-			} else if (driver.equalsIgnoreCase(CASSANDRA)) {
-				Class.forName(CASSANDRA_DRIVER);
-			} else if (driver.equalsIgnoreCase(DB2)) {
-				Class.forName(DB2_DRIVER);
-			} else if (driver.equalsIgnoreCase(DERBY)) {
-				Class.forName(DERBY_DRIVER);
-			} else if (driver.equalsIgnoreCase(H2)) {
-				Class.forName(H2_DRIVER);
-			} else if (driver.equalsIgnoreCase(IMPALA)) {
-				Class.forName(IMPALA_DRIVER);
-			} else if (driver.equalsIgnoreCase(MARIADB)) {
-				Class.forName(MARIADB_DRIVER);
-			} else if (driver.equalsIgnoreCase(MYSQL)) {
-				Class.forName(MYSQL_DRIVER);
-			} else if (driver.equalsIgnoreCase(ORACLE)) {
-				Class.forName(ORACLE_DRIVER);
-			} else if (driver.equalsIgnoreCase(PHOENIX)) {
-				Class.forName(PHOENIX_DRIVER);
-			} else if (driver.equalsIgnoreCase(POSTGRES)) {
-				Class.forName(POSTGRES_DRIVER);
-			} else if (driver.equalsIgnoreCase(SAP_HANA)) {
-				Class.forName(SAP_HANA_DRIVER);
-			} else if (driver.equalsIgnoreCase(SQLSERVER)) {
-				Class.forName(SQLSERVER_DRIVER);
-			} else if (driver.equalsIgnoreCase(TERADATA)) {
-				Class.forName(TERADATA_DRIVER);
-			} else {
-				throw new IllegalArgumentException("Invalid driver");
-			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			throw new IllegalArgumentException("Unable to find driver for engine type");
+			con = RdbmsConnectionHelper.getConnection(connectionUrl, userName, password, driver);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+			throw new IllegalArgumentException(e1.getMessage());
 		}
 		
-		// create the iterator
-		Connection con;
-		try {
-			if (userName == null || password == null) {
-				con = DriverManager.getConnection(connectionUrl);
-			} else {
-				con = DriverManager.getConnection(connectionUrl, userName, password);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new IllegalArgumentException(e.getMessage());
-		}
-
 		RawRDBMSSelectWrapper it = new RawRDBMSSelectWrapper();
 		try {
 			it.setCloseConenctionAfterExecution(true);
