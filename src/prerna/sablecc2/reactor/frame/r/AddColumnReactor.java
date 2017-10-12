c4bca96b02431264bb7366164111758cbd95f3d6
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-10-12 15:49:41 +0000

    BUGFIX ::: Add column, rename, drop clean reactors
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24093 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/r/AddColumnReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/AddColumnReactor.java
@@ -12,63 +12,62 @@ public class AddColumnReactor extends AbstractRFrameReactor {
 
 	@Override
 	public NounMetadata execute() {
-
+		init();
 		// get frame
 		RDataTable frame = (RDataTable) getFrame();
-		//get column name from the gen row struct
+		// get column name from the gen row struct
 		String colName = getNewColumnName();
-		//get the column type from the gen row struct
+		// get the column type from the gen row struct
 		String colType = getDataType();
 
-		if (frame != null) {
-			String table = frame.getTableName();
-			// clean colName
-			if (colName.contains("__")) {
-				String[] split = colName.split("__");
-				table = split[0];
-				colName = split[1];
-			}
-			//clean the column name to ensure that it is valid
-			colName = getCleanNewColName(table, colName);
+		String table = frame.getTableName();
+		// clean colName
+		if (colName.contains("__")) {
+			String[] split = colName.split("__");
+			table = split[0];
+			colName = split[1];
+		}
+		// clean the column name to ensure that it is valid
+		colName = getCleanNewColName(table, colName);
 
-			//define the script to be executed; this assigns a new column name with no data in columns
-			String script = table + "$" + colName + " <- \"\" ";
-			//execute the r script
+		// define the script to be executed; this assigns a new column name with
+		// no data in columns
+		String script = table + "$" + colName + " <- \"\" ";
+		// execute the r script
+		frame.executeRScript(script);
+
+		// update the metadata to include this new column
+		OwlTemporalEngineMeta metaData = this.getFrame().getMetaData();
+		metaData.addProperty(table, table + "__" + colName);
+		metaData.setAliasToProperty(table + "__" + colName, colName);
+
+		// temp table used to assign a data type to the new column
+		String tempTable = null;
+		if (colType != null && (colType.equalsIgnoreCase("number") || colType.equalsIgnoreCase("numeric"))) {
+			// update the metadata depending on the data type
+			metaData.setDataTypeToProperty(table + "__" + colName, "NUMBER");
+			tempTable = Utility.getRandomString(6);
+			script = tempTable + " <- as.numeric(" + table + "$" + colName + ")";
 			frame.executeRScript(script);
+			script = table + "$" + colName + "<-" + tempTable;
+		} else if (colType != null && colType.equalsIgnoreCase("date")) {
+			metaData.setDataTypeToProperty(table + "__" + colName, "DATE");
+			tempTable = Utility.getRandomString(6);
+			String dateFormat = "%Y/%m/%d";
+			script = tempTable + " <- as.Date(" + table + "$" + colName + ", format='" + dateFormat + "')";
+			frame.executeRScript(script);
+			script = table + "$" + colName + " <- " + tempTable;
+			frame.executeRScript(script);
+		} else {
+			// if not a number or a date then assign to string
+			metaData.setDataTypeToProperty(table + "__" + colName, "STRING");
+		}
 
-			//update the metadata to include this new column
-			OwlTemporalEngineMeta metaData = this.getFrame().getMetaData();
-			metaData.addProperty(table, table + "__" + colName);
-			metaData.setAliasToProperty(table + "__" + colName, colName);
-
-			//temp table used to assign a data type to the new column
-			String tempTable = null;
-			if (colType != null && (colType.equalsIgnoreCase("number") || colType.equalsIgnoreCase("numeric"))) {
-				//update the metadata depending on the data type
-				metaData.setDataTypeToProperty(table + "__" + colName, "NUMBER");
-				tempTable = Utility.getRandomString(6);
-				script = tempTable + " <- as.numeric(" + table + "$" + colName + ")";
-				frame.executeRScript(script);
-				script = table + "$" + colName + "<-" + tempTable;
-			} else if (colType != null && colType.equalsIgnoreCase("date")) {
-				metaData.setDataTypeToProperty(table + "__" + colName, "DATE");
-				tempTable = Utility.getRandomString(6);
-				String dateFormat = "%Y/%m/%d";
-				script = tempTable + " <- as.Date(" + table + "$" + colName + ", format='" + dateFormat + "')";
-				frame.executeRScript(script);
-				script = table + "$" + colName + " <- " + tempTable;
-				frame.executeRScript(script);
-			} else {
-				//if not a number or a date then assign to string
-				metaData.setDataTypeToProperty(table + "__" + colName, "STRING");
-			}
-
-			// garbage collection for the temp table
-			if (tempTable != null) {
-				frame.executeRScript("rm(" + tempTable + ");");
-				frame.executeRScript("gc();");
-				this.getFrame().syncHeaders();
-			}
+		// garbage collection for the temp table
+		if (tempTable != null) {
+			frame.executeRScript("rm(" + tempTable + ");");
+			frame.executeRScript("gc();");
+			this.getFrame().syncHeaders();
 		}
 		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE);
 	}
@@ -76,7 +75,7 @@ public class AddColumnReactor extends AbstractRFrameReactor {
 	private String getNewColumnName() {
 		GenRowStruct inputsGRS = this.getCurRow();
 		if (inputsGRS != null && !inputsGRS.isEmpty()) {
-			//the new column name will be the first input
+			// the new column name will be the first input
 			String colName = inputsGRS.getNoun(0).getValue() + "";
 			if (colName.length() == 0) {
 				throw new IllegalArgumentException("Need to define the new column name");
@@ -87,7 +86,7 @@ public class AddColumnReactor extends AbstractRFrameReactor {
 	}
 
 	private String getDataType() {
-		//data type defaults to string unless otherwise specified
+		// data type defaults to string unless otherwise specified
 		String dataType = "STRING";
 		GenRowStruct inputsGRS = this.getCurRow();
 		if (inputsGRS.size() > 1) {
