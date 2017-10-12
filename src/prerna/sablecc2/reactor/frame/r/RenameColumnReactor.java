ca080c7da1734491b859f9ce54679bbd85e285d1
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-10-12 15:49:41 +0000

    BUGFIX ::: Add column, rename, drop clean reactors
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24093 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ec6b1d300ee29768f9ed13c719c57ab11df60bbd

--- src/prerna/sablecc2/reactor/frame/r/RenameColumnReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/RenameColumnReactor.java
@@ -7,55 +7,57 @@ import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 
 public class RenameColumnReactor extends AbstractRFrameReactor {
-	
+
 	@Override
 	public NounMetadata execute() {
-	
+		init();
 		// get frame
-		RDataTable frame = null;
-		if (this.insight.getDataMaker() != null) {
-			frame = (RDataTable) getFrame();
-		}
+		RDataTable frame = (RDataTable) getFrame();
 
 		// get inputs
-		GenRowStruct inputsGRS = this.getCurRow();
-		String originalColName = "";
-		String updatedColName = ""; 
-		
-		if (inputsGRS != null && !inputsGRS.isEmpty()) {
-			//the first input will be the original column name
-			NounMetadata input1 = inputsGRS.getNoun(0);
-			PixelDataType nounType1 = input1.getNounType();
-			if (nounType1 == PixelDataType.COLUMN) {
-				String fullName = input1.getValue() + "";
-				originalColName = fullName.split("__")[1];
-			}
-			//the second input will be the name that we want to use
-		    NounMetadata input2 = inputsGRS.getNoun(1);
-		    PixelDataType nounType2 = input2.getNounType();
-		    if (nounType2 == PixelDataType.CONST_STRING) {
-		    	updatedColName = input2.getValue() + "";
-		    }
-		    //check that the frame isn't null
-		    if (frame != null) {
-		    	String table = frame.getTableName();
-		    	//ensure new header name is valid
-		    	//make sure that the new name we want to use is valid
-		    	String validNewHeader = getCleanNewHeader(table, updatedColName);
-				//define the r script to be executed
-		    	String script = "names(" + table + ")[names(" + table + ") == \"" + originalColName + "\"] = \"" + validNewHeader + "\"";
-				//execute the r script
-		    	//script is of the form: names(FRAME)[names(FRAME) == "Director"] = "directing_person"
-		    	frame.executeRScript(script);
-				// FE passes the column name
-				// but meta will still be table __ column
-		    	//update the metadata because column names have changed
-				this.getFrame().getMetaData().modifyPropertyName(table + "__" + originalColName, table, table + "__" + validNewHeader);
-				this.getFrame().syncHeaders();	
-		    }
+		String originalColName = getOriginalColumn();
+		String updatedColName = getNewColumnName();
+
+		// check that the frame isn't null
+		String table = frame.getTableName();
+		if (originalColName.contains("__")) {
+			String[] split = originalColName.split("__");
+			table = split[0];
+			originalColName = split[1];
 		}
+		// ensure new header name is valid
+		// make sure that the new name we want to use is valid
+		String validNewHeader = getCleanNewHeader(table, updatedColName);
+		// define the r script to be executed
+		String script = "names(" + table + ")[names(" + table + ") == \"" + originalColName + "\"] = \""
+				+ validNewHeader + "\"";
+		// execute the r script
+		// script is of the form: names(FRAME)[names(FRAME) == "Director"] = "directing_person"
+		frame.executeRScript(script);
+		// FE passes the column name
+		// but meta will still be table __ column
+		// update the metadata because column names have changed
+		this.getFrame().getMetaData().modifyPropertyName(table + "__" + originalColName, table,
+				table + "__" + validNewHeader);
+		this.getFrame().syncHeaders();
 		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE);
 	}
-	
-	
+
+	private String getOriginalColumn() {
+		GenRowStruct inputsGRS = this.getCurRow();
+		String originalColName = "";
+		if (inputsGRS != null && !inputsGRS.isEmpty()) {
+			NounMetadata input1 = inputsGRS.getNoun(0);
+			originalColName = input1.getValue() + "";
+		}
+		return originalColName;
+	}
+
+	private String getNewColumnName() {
+		GenRowStruct inputsGRS = this.getCurRow();
+		String newColName = "";
+		NounMetadata input2 = inputsGRS.getNoun(1);
+		newColName = input2.getValue() + "";
+		return newColName;
+	}
 }
