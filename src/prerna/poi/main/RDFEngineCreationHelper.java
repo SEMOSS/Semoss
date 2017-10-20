fd04e988b17338072bd71aff964b5b4e5a600206
mahkhalil <mahkhalil@deloitte.com>
2017-10-20 20:46:40 +0000

    UPDATE ::: Updating the recipe for nlp
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24337 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: cfe7bef3321e4121e51db3afb87d90fa9a59b88e

--- src/prerna/poi/main/RDFEngineCreationHelper.java
+++ src/prerna/poi/main/RDFEngineCreationHelper.java
@@ -117,47 +117,53 @@ public class RDFEngineCreationHelper {
 
 		//determine the # where the new questions should start
 		String insightName = ""; 
-		String[] pkqlRecipeToSave = new String[3];
+		String[] recipeArray = new String[3];
 		String layout = ""; 
 
 		// q1
 		insightName = "Show all roles to object";
 		layout = "Grid";
-		pkqlRecipeToSave = new String[3];
-		pkqlRecipeToSave[0] = "data.frame('grid');";
-		pkqlRecipeToSave[1] = "data.import(api:" + engineName + ".query([c:Subject, c:Object],([c:Subject, inner.join, c:Object])));";
-		pkqlRecipeToSave[2] = "panel[0].viz ( Grid , [ ] , { 'offset' : 0 , 'limit' : 1000 } );";
-		admin.addInsight(insightName, layout, pkqlRecipeToSave);
-
+		recipeArray = new String[5];
+		recipeArray[0] = "AddPanel(0);";
+		recipeArray[1] = "Panel(0)|SetPanelView(\"visualization\");";
+		recipeArray[2] = "CreateFrame(grid).as([FRAME]);";
+		recipeArray[3] = "Database(" + engineName + ") | Select(Subject, Object) | Join((Subject, inner.join, Object)) | Limit(500) | Import();"; 
+		recipeArray[4] = "Frame() | Select(f$Subject, f$Object) | Format ( type = [ 'table' ] ) | TaskOptions({\"0\":{\"layout\":\"Grid\",\"alignment\":{\"label\":[\"Subject\",\"Object\"]}}}) | Collect(500);"; 
+		admin.addInsight(insightName, layout, recipeArray);
 		
 		// q2
 		insightName = "Show all objects to actions";
 		layout = "Grid";
-		pkqlRecipeToSave = new String[3];
-		pkqlRecipeToSave[0] = "data.frame('grid');";
-		pkqlRecipeToSave[1] = "data.import(api:" + engineName + ".query([c:Object, c:Predicate],([c:Predicate, inner.join, c:Object])));";
-		pkqlRecipeToSave[2] = "panel[0].viz ( Grid , [ ] , { 'offset' : 0 , 'limit' : 1000 } );";
-		admin.addInsight(insightName, layout, pkqlRecipeToSave);
+		recipeArray = new String[5];
+		recipeArray[0] = "AddPanel(0);";
+		recipeArray[1] = "Panel(0)|SetPanelView(\"visualization\");";
+		recipeArray[2] = "CreateFrame(grid).as([FRAME]);";
+		recipeArray[3] = "Database(" + engineName + ") | Select(Object, Predicate) | Join((Predicate, inner.join, Object)) | Limit(500) | Import();"; 
+		recipeArray[4] = "Frame() | Select(f$Subject, f$Object) | Format ( type = [ 'table' ] ) | TaskOptions({\"0\":{\"layout\":\"Grid\",\"alignment\":{\"label\":[\"Object\",\"Predicate\"]}}}) | Collect(500);"; 
+		admin.addInsight(insightName, layout, recipeArray);
 
-				
 		// q3
 		insightName = "Show all roles to actions";
 		layout = "Grid";
-		pkqlRecipeToSave = new String[3];
-		pkqlRecipeToSave[0] = "data.frame('grid');";
-		pkqlRecipeToSave[1] = "data.import(api:" + engineName + ".query([c:Subject, c:Predicate],([c:Subject, inner.join, c:Predicate])));";
-		pkqlRecipeToSave[2] = "panel[0].viz ( Grid , [ ] , { 'offset' : 0 , 'limit' : 1000 } );";
-		admin.addInsight(insightName, layout, pkqlRecipeToSave);
+		recipeArray = new String[5];
+		recipeArray[0] = "AddPanel(0);";
+		recipeArray[1] = "Panel(0)|SetPanelView(\"visualization\");";
+		recipeArray[2] = "CreateFrame(grid).as([FRAME]);";
+		recipeArray[3] = "Database(" + engineName + ") | Select(Subject, Predicate) | Join((Subject, inner.join, Predicate)) | Limit(500) | Import();"; 
+		recipeArray[4] = "Frame() | Select(f$Subject, f$Object) | Format ( type = [ 'table' ] ) | TaskOptions({\"0\":{\"layout\":\"Grid\",\"alignment\":{\"label\":[\"Subject\",\"Predicate\"]}}}) | Collect(500);"; 
+		admin.addInsight(insightName, layout, recipeArray);
 
 		// q4
 		insightName = "Show all roles to actions and what they are acting on";
 		layout = "Grid";
-		pkqlRecipeToSave = new String[3];
-		pkqlRecipeToSave[0] = "data.frame('grid');";
-		pkqlRecipeToSave[1] = "data.import(api:" + engineName + ".query([c:Subject, c:Predicate, c:Object],([c:Subject, inner.join, c:Predicate], [c:Predicate, inner.join, c:Object], [c:Object, inner.join, c:Subject])));";
-		pkqlRecipeToSave[2] = "panel[0].viz ( Grid , [ ] , { 'offset' : 0 , 'limit' : 1000 } );";
-		admin.addInsight(insightName, layout, pkqlRecipeToSave);
-		
+		recipeArray = new String[5];
+		recipeArray[0] = "AddPanel(0);";
+		recipeArray[1] = "Panel(0)|SetPanelView(\"visualization\");";
+		recipeArray[2] = "CreateFrame(grid).as([FRAME]);";
+		recipeArray[3] = "Database(" + engineName + ") | Select(Subject, Object, Predicate) | Join((Subject, inner.join, Predicate), (Predicate, inner.join, Object)) | Limit(500) | Import();"; 
+		recipeArray[4] = "Frame() | Select(f$Subject, f$Object) | Format ( type = [ 'table' ] ) | TaskOptions({\"0\":{\"layout\":\"Grid\",\"alignment\":{\"label\":[\"Subject\",\"Predicate\",\"Object\"]}}}) | Collect(500);"; 
+		admin.addInsight(insightName, layout, recipeArray);
+
 		//TODO: there are more insights that i need to add from the Default_NLP_Questions.properties in the Default folder in db directory
 	}
 
