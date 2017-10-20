5b8a5fbb79a945719dcec80f68104d6a1681352e
mahkhalil <mahkhalil@deloitte.com>
2017-10-20 12:36:50 +0000

    UPDATE ::: Adding filtering on graph output for tinker
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24318 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ds/export/graph/TinkerFrameGraphExporter.java
+++ src/prerna/ds/export/graph/TinkerFrameGraphExporter.java
@@ -2,13 +2,16 @@ package prerna.ds.export.graph;
 
 import java.awt.Color;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.Vector;
 
 import org.apache.tinkerpop.gremlin.process.traversal.P;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
 import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Property;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
@@ -17,7 +20,9 @@ import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph;
 
 import prerna.ds.OwlTemporalEngineMeta;
 import prerna.ds.TinkerFrame;
+import prerna.query.querystruct.GenRowFilters;
 import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.QueryFilter;
 import prerna.ui.helpers.TypeColorShapeTable;
 import prerna.util.Constants;
 
@@ -32,58 +37,17 @@ public class TinkerFrameGraphExporter extends AbstractGraphExporter{
 	// the vert iterator
 	private GraphTraversal<Vertex, Vertex> vertsIt;
 	
+	// to keep vertices unique
+	private Set<String> vertSet;
+	
 	public TinkerFrameGraphExporter(TinkerFrame tf) {
 		this.tf = tf;
 		this.g = tf.g;
 		this.meta = tf.getMetaData();
-	}
-	
-	/**
-	 * Boolean if there are more edges to return
-	 * @return
-	 */
-	@Override
-	public boolean hasNextEdge() {
-		if(this.edgesIt == null) {
-			createEdgesIt();
-		}
-		return this.edgesIt.hasNext();
-	}
-	
-	@Override
-	public Map<String, Object> getNextEdge() {
-		if(this.edgesIt == null) {
-			createEdgesIt();
-		}
 		
-		// map representing the edge
-		Map<String, Object> edgeMap = new HashMap<String, Object>();
-		
-		Edge e = this.edgesIt.next();
-		// get the edge unique id
-		edgeMap.put("uri", e.property(TinkerFrame.TINKER_ID).value().toString());
-		// add the source and target
-		edgeMap.put("source", getNodePhysicalType(e.outVertex().property(TinkerFrame.TINKER_TYPE).value() + "") + "/" + getNodePhysicalType(e.outVertex().property(TinkerFrame.TINKER_NAME).value() + ""));
-		edgeMap.put("target", getNodePhysicalType(e.inVertex().property(TinkerFrame.TINKER_TYPE).value() + "") + "/" + getNodePhysicalType(e.inVertex().property(TinkerFrame.TINKER_NAME).value() + ""));
-
-		// also push edge properties
-		Map<String, Object> propMap = new HashMap<String, Object>();
-		
-		// need to add edge properties
-		Iterator<Property<Object>> edgeProperties = e.properties();
-		while(edgeProperties.hasNext()) {
-			Property<Object> prop = edgeProperties.next();
-			String propName = prop.key();
-			if(!propName.equals(TinkerFrame.TINKER_ID) && !propName.equals(TinkerFrame.TINKER_NAME) && !propName.equals(TinkerFrame.TINKER_TYPE)) {
-				propMap.put(propName, prop.value());
-			}
-		}
-		edgeMap.put("propHash", propMap);
-
-		// return the edge map
-		return edgeMap;
+		// because i can't get the edge traversal properly :(
+		this.vertSet = new HashSet<String>();
 	}
-	
 	
 	/**
 	 * Boolean if there are more vertices to return
@@ -111,8 +75,8 @@ public class TinkerFrameGraphExporter extends AbstractGraphExporter{
 		// add the vertex unique id
 		Object value = v.property(TinkerFrame.TINKER_NAME).value();
 		String type = getNodePhysicalType(v.property(TinkerFrame.TINKER_TYPE).value() + "");
-		
-		vertexMap.put("uri", type + "/" + value);
+		String uri =  type + "/" + value;
+		vertexMap.put("uri", uri);
 		vertexMap.put(Constants.VERTEX_TYPE, type);
 		vertexMap.put(Constants.VERTEX_NAME, value);
 		
@@ -136,7 +100,111 @@ public class TinkerFrameGraphExporter extends AbstractGraphExporter{
 		// add to the meta count
 		addVertCount(type);
 		
+		// add to set for filtering out some annoying edges
+		this.vertSet.add(uri);
+		
 		return vertexMap;
+	}
+	
+	/**
+	 * Generate vertices iterator
+	 */
+	private void createVertsIt() {
+		this.vertsIt = this.g.traversal().V();
+		
+		boolean hasFilter = false;
+		GenRowFilters filterGrs = tf.getFrameFilters();
+		List<GraphTraversal> unionT = new Vector<GraphTraversal>();
+		if(!filterGrs.isEmpty()) {
+			hasFilter = true;
+			// we have filters to consider
+			List<String> vertexNames = this.meta.getFrameColumnNames();
+			for(String v : vertexNames) {
+				GraphTraversal traversal = __.has(TinkerFrame.TINKER_TYPE, getNodePhysicalType(v));
+				
+				List<QueryFilter> colFilters = filterGrs.getAllQueryFiltersContainingColumn(v);
+				if(colFilters.isEmpty()) {
+					continue;
+				}
+				for(QueryFilter filter : colFilters) {
+					QueryFilter.FILTER_TYPE filterType = QueryFilter.determineFilterType(filter);
+					NounMetadata lComp = filter.getLComparison();
+					NounMetadata rComp = filter.getRComparison();
+					String comp = filter.getComparator();
+
+					if(filterType == QueryFilter.FILTER_TYPE.COL_TO_VALUES) {
+						// here, lcomp is the column and rComp is a set of values
+						processFilterColToValues(traversal, lComp, rComp, comp);
+					} else if(filterType == QueryFilter.FILTER_TYPE.VALUES_TO_COL) {
+						// here, lcomp is the values and rComp is a the column
+						// so same as above, but switch the order
+						processFilterColToValues(traversal, rComp, lComp, QueryFilter.getReverseNumericalComparator(comp));
+					}
+				}
+				unionT.add(traversal);
+			}
+		}
+
+		if(hasFilter) {
+			this.vertsIt.union(unionT.toArray(new GraphTraversal[]{}));
+		}
+	}
+	
+	/**
+	 * Boolean if there are more edges to return
+	 * @return
+	 */
+	@Override
+	public boolean hasNextEdge() {
+		if(this.edgesIt == null) {
+			createEdgesIt();
+		}
+		return this.edgesIt.hasNext();
+	}
+	
+	@Override
+	public Map<String, Object> getNextEdge() {
+		if(this.edgesIt == null) {
+			createEdgesIt();
+		}
+		
+		// map representing the edge
+		Map<String, Object> edgeMap = new HashMap<String, Object>();
+		
+		Edge e = this.edgesIt.next();
+		// get the edge unique id
+		edgeMap.put("uri", e.property(TinkerFrame.TINKER_ID).value().toString());
+		// add the source and target
+		String sUri = getNodePhysicalType(e.outVertex().property(TinkerFrame.TINKER_TYPE).value() + "") + "/" + e.outVertex().property(TinkerFrame.TINKER_NAME).value();
+		String tUri = getNodePhysicalType(e.inVertex().property(TinkerFrame.TINKER_TYPE).value() + "") + "/" + e.inVertex().property(TinkerFrame.TINKER_NAME).value();
+		
+		if(!this.vertSet.contains(sUri) || !this.vertSet.contains(tUri)) {
+			if(this.hasNextEdge()) {
+				return getNextEdge();
+			} else {
+				return new HashMap<String, Object>();
+			}
+		}
+		
+		edgeMap.put("source", sUri);
+		edgeMap.put("target", tUri);
+
+		// also push edge properties
+		Map<String, Object> propMap = new HashMap<String, Object>();
+		
+		// need to add edge properties
+		Iterator<Property<Object>> edgeProperties = e.properties();
+		while(edgeProperties.hasNext()) {
+			Property<Object> prop = edgeProperties.next();
+			String propName = prop.key();
+			if(!propName.equals(TinkerFrame.TINKER_ID) && !propName.equals(TinkerFrame.TINKER_NAME) && !propName.equals(TinkerFrame.TINKER_TYPE)) {
+				propMap.put(propName, prop.value());
+			}
+		}
+		edgeMap.put("propHash", propMap);
+
+		// return the edge map
+		return edgeMap;
 	}
 	
 	/**
@@ -149,105 +217,68 @@ public class TinkerFrameGraphExporter extends AbstractGraphExporter{
 		// 3) no vertex is a prim key
 		this.edgesIt = this.g.traversal().E();
 		
-//		boolean hasFilter = false;
-//		GenRowFilters filterGrs = tf.getFrameFilters();
-//		List<GraphTraversal> unionT = new Vector<GraphTraversal>();
-//		if(!filterGrs.isEmpty()) {
-//			hasFilter = true;
-//			// we have filters to consider
-//			List<String[]> relationships = this.meta.getAllRelationships();
-//			for(String[] rel : relationships) {
-//				String start = rel[0];
-//				String end = rel[1];
-//				
-//				GraphTraversal traversal = __.V().has(TinkerFrame.TINKER_TYPE, getNodePhysicalType(start));
-//				
-//				// add filters to start
-//				List<QueryFilter> colFilters = filterGrs.getAllQueryFiltersContainingColumn(start);
-//				for(QueryFilter filter : colFilters) {
-//					QueryFilter.FILTER_TYPE filterType = QueryFilter.determineFilterType(filter);
-//					NounMetadata lComp = filter.getLComparison();
-//					NounMetadata rComp = filter.getRComparison();
-//					String comp = filter.getComparator();
-//
-//					if(filterType == QueryFilter.FILTER_TYPE.COL_TO_VALUES) {
-//						// here, lcomp is the column and rComp is a set of values
-//						processFilterColToValues(traversal, lComp, rComp, comp);
-//					} else if(filterType == QueryFilter.FILTER_TYPE.VALUES_TO_COL) {
-//						// here, lcomp is the values and rComp is a the column
-//						// so same as above, but switch the order
-//						processFilterColToValues(traversal, rComp, lComp, QueryFilter.getReverseNumericalComparator(comp));
-//					}
-//				}
-//				
-//				// out edge
-//				traversal.as("startV").out(start + "+++" + end).has(TinkerFrame.TINKER_TYPE, getNodePhysicalType(end));
-//				// add filters to start
-//				colFilters = filterGrs.getAllQueryFiltersContainingColumn(end);
-//				for(QueryFilter filter : colFilters) {
-//					QueryFilter.FILTER_TYPE filterType = QueryFilter.determineFilterType(filter);
-//					NounMetadata lComp = filter.getLComparison();
-//					NounMetadata rComp = filter.getRComparison();
-//					String comp = filter.getComparator();
-//
-//					if(filterType == QueryFilter.FILTER_TYPE.COL_TO_VALUES) {
-//						// here, lcomp is the column and rComp is a set of values
-//						processFilterColToValues(traversal, lComp, rComp, comp);
-//					} else if(filterType == QueryFilter.FILTER_TYPE.VALUES_TO_COL) {
-//						// here, lcomp is the values and rComp is a the column
-//						// so same as above, but switch the order
-//						processFilterColToValues(traversal, rComp, lComp, QueryFilter.getReverseNumericalComparator(comp));
-//					}
-//				}
-//				
-//				unionT.add(traversal.inE().dedup());
-//			}
-//		}
-//
-//		if(hasFilter) {
-//			this.edgesIt.union(unionT.toArray(new GraphTraversal[]{}));
-//		}
-	}
-	
-	/**
-	 * Generate vertices iterator
-	 */
-	private void createVertsIt() {
-		this.vertsIt = this.g.traversal().V();
-		
-//		boolean hasFilter = false;
-//		GenRowFilters filterGrs = tf.getFrameFilters();
-//		List<GraphTraversal> unionT = new Vector<GraphTraversal>();
-//		if(!filterGrs.isEmpty()) {
-//			hasFilter = true;
-//			// we have filters to consider
-//			List<String> vertexNames = this.meta.getFrameColumnNames();
-//			for(String v : vertexNames) {
-//				GraphTraversal traversal = __.has(TinkerFrame.TINKER_TYPE, getNodePhysicalType(v));
-//				
-//				List<QueryFilter> colFilters = filterGrs.getAllQueryFiltersContainingColumn(v);
-//				for(QueryFilter filter : colFilters) {
-//					QueryFilter.FILTER_TYPE filterType = QueryFilter.determineFilterType(filter);
-//					NounMetadata lComp = filter.getLComparison();
-//					NounMetadata rComp = filter.getRComparison();
-//					String comp = filter.getComparator();
-//
-//					if(filterType == QueryFilter.FILTER_TYPE.COL_TO_VALUES) {
-//						// here, lcomp is the column and rComp is a set of values
-//						processFilterColToValues(traversal, lComp, rComp, comp);
-//					} else if(filterType == QueryFilter.FILTER_TYPE.VALUES_TO_COL) {
-//						// here, lcomp is the values and rComp is a the column
-//						// so same as above, but switch the order
-//						processFilterColToValues(traversal, rComp, lComp, QueryFilter.getReverseNumericalComparator(comp));
-//					}
-//				}
-//				unionT.add(traversal);
-//			}
-//		}
-//
-//		if(hasFilter) {
-//			this.vertsIt.union(unionT.toArray(new GraphTraversal[]{}));
-//		}
+		boolean hasFilter = false;
+		GenRowFilters filterGrs = tf.getFrameFilters();
+		List<GraphTraversal> unionT = new Vector<GraphTraversal>();
+		if(!filterGrs.isEmpty()) {
+			hasFilter = true;
+			// we have filters to consider
+			List<String[]> relationships = this.meta.getAllRelationships();
+			for(String[] rel : relationships) {
+				String start = rel[0];
+				String end = rel[1];
+				
+				GraphTraversal traversal = __.V().has(TinkerFrame.TINKER_TYPE, getNodePhysicalType(start));
+				
+				// make sure both have filters
+				List<QueryFilter> startFilters = filterGrs.getAllQueryFiltersContainingColumn(start);
+				List<QueryFilter> endFilters = filterGrs.getAllQueryFiltersContainingColumn(end);
+
+				// add filters to start
+				for(QueryFilter filter : startFilters) {
+					QueryFilter.FILTER_TYPE filterType = QueryFilter.determineFilterType(filter);
+					NounMetadata lComp = filter.getLComparison();
+					NounMetadata rComp = filter.getRComparison();
+					String comp = filter.getComparator();
+
+					if(filterType == QueryFilter.FILTER_TYPE.COL_TO_VALUES) {
+						// here, lcomp is the column and rComp is a set of values
+						processFilterColToValues(traversal, lComp, rComp, comp);
+					} else if(filterType == QueryFilter.FILTER_TYPE.VALUES_TO_COL) {
+						// here, lcomp is the values and rComp is a the column
+						// so same as above, but switch the order
+						processFilterColToValues(traversal, rComp, lComp, QueryFilter.getReverseNumericalComparator(comp));
+					}
+				}
+				
+				// out edge
+				traversal.as("startV").out(start + "+++" + end).has(TinkerFrame.TINKER_TYPE, getNodePhysicalType(end));
+				// add filters to end
+				for(QueryFilter filter : endFilters) {
+					QueryFilter.FILTER_TYPE filterType = QueryFilter.determineFilterType(filter);
+					NounMetadata lComp = filter.getLComparison();
+					NounMetadata rComp = filter.getRComparison();
+					String comp = filter.getComparator();
+
+					if(filterType == QueryFilter.FILTER_TYPE.COL_TO_VALUES) {
+						// here, lcomp is the column and rComp is a set of values
+						processFilterColToValues(traversal, lComp, rComp, comp);
+					} else if(filterType == QueryFilter.FILTER_TYPE.VALUES_TO_COL) {
+						// here, lcomp is the values and rComp is a the column
+						// so same as above, but switch the order
+						processFilterColToValues(traversal, rComp, lComp, QueryFilter.getReverseNumericalComparator(comp));
+					}
+				}
+				
+				// TODO: need to figure out how to make the inE be only for those V's
+				// in the first set ... couldn't get it to work ... need to come back
+				unionT.add(traversal.inE().dedup());
+			}
+		}
+
+		if(hasFilter) {
+			this.edgesIt.union(unionT.toArray(new GraphTraversal[]{}));
+		}
 	}
 	
 	/**
@@ -302,7 +333,10 @@ public class TinkerFrameGraphExporter extends AbstractGraphExporter{
 		}
 		List<Map<String, Object>> edgesMapList = new Vector<Map<String, Object>>();
 		while(hasNextEdge()) {
-			edgesMapList.add(getNextEdge());
+			Map<String, Object> edge = getNextEdge();
+			if(!edge.isEmpty()) {
+				edgesMapList.add(edge);
+			}
 		}
 		formattedData.put("nodes", nodesMapList);
 		formattedData.put("edges", edgesMapList);
