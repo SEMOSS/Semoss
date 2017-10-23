14123a585893bea62baaf0f62e000a96ddd7f1c4
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-10-23 18:52:10 +0000

    BUGFIX ::: Change Column Type not running script if the new datatype is the same as the old, this caused issues for numeric since a regex was changing the numeric column to string.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24352 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3f0c9f1ee8781a3c5c45c527144544307301cbf6

--- src/prerna/sablecc2/reactor/frame/r/ChangeColumnTypeReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/ChangeColumnTypeReactor.java
@@ -2,6 +2,7 @@ package prerna.sablecc2.reactor.frame.r;
 
 import prerna.algorithm.api.IMetaData;
 import prerna.algorithm.api.IMetaData.DATA_TYPES;
+import prerna.ds.OwlTemporalEngineMeta;
 import prerna.ds.r.RDataTable;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.NounMetadata;
@@ -22,60 +23,63 @@ public class ChangeColumnTypeReactor extends AbstractRFrameReactor {
 		init();
 		// get frame
 		RDataTable frame = (RDataTable) getFrame();
-		
 		//get table name
 		String table = frame.getTableName();
-		
 		// get inputs
 		String column = getColumn();
-		String newType = getColumnType();
-		
 		if (column.contains("__")) {
 			String[] split = column.split("__");
 			column = split[1];
 		}
+		String newType = getColumnType();
+		OwlTemporalEngineMeta metadata = this.getFrame().getMetaData();
+		String dataType = metadata.getHeaderTypeAsString(table + "__" + column);
 		
-		// define the r script to execute
-		// script depends on the new data type
-		String script = null;
-		if (newType.equalsIgnoreCase("string") || newType.equalsIgnoreCase("character")) {
-			//df$column <- as.character(df$column);
-			script = table +"$" + column + " <- as.character("+table+"$"+column+");";
-			frame.executeRScript(script);
-		} else if (newType.equalsIgnoreCase("factor")) {
-			//df$column <- as.factor(df$column);
-			script = table +"$" + column + " <- as.factor("+table+"$"+column+");";
-			frame.executeRScript(script);
-		} else if (newType.equalsIgnoreCase("number") || newType.equalsIgnoreCase("numeric")) {
-			// r script syntax cleaning characters with regex
-			script = table +"$" + column + " <- as.numeric(gsub('[^-\\\\.0-9]', '', "+table+"$"+column+"));";
-			frame.executeRScript(script);
-		} else if (newType.equalsIgnoreCase("date")) {
-			// we have a different script to run if it is a str to date
-			// conversion
-			// define date format
-			String dateFormat = "%Y/%m/%d";
-			// get the column type of the existing column
-			String type = getColumnType(table, column);
-			String tempTable = Utility.getRandomString(6);
-			if (type.equalsIgnoreCase("date")) {
-				String formatString = ", format = '" + dateFormat + "'";
-				script = tempTable + " <- format(" + table + "$" + column + formatString + ")";
+		//check if there is a new dataType
+		if (!dataType.equals(newType)) {
+			// define the r script to execute
+			// script depends on the new data type
+			String script = null;
+			if (newType.equalsIgnoreCase("string") || newType.equalsIgnoreCase("character")) {
+				// df$column <- as.character(df$column);
+				script = table + "$" + column + " <- as.character(" + table + "$" + column + ");";
 				frame.executeRScript(script);
-				script = table + "$" + column + " <- " + "as.Date(" + tempTable + formatString + ")";
+			} else if (newType.equalsIgnoreCase("factor")) {
+				// df$column <- as.factor(df$column);
+				script = table + "$" + column + " <- as.factor(" + table + "$" + column + ");";
 				frame.executeRScript(script);
-			} else {
-				script = tempTable + " <- as.Date(" + table + "$" + column + ", format='" + dateFormat + "')";
+			} else if (newType.equalsIgnoreCase("number") || newType.equalsIgnoreCase("numeric")) {
+				// r script syntax cleaning characters with regex
+				script = table + "$" + column + " <- as.numeric(gsub('[^-\\\\.0-9]', '', " + table + "$" + column
+						+ "));";
 				frame.executeRScript(script);
-				script = table + "$" + column + " <- " + tempTable;
-				frame.executeRScript(script);
+			} else if (newType.equalsIgnoreCase("date")) {
+				// we have a different script to run if it is a str to date
+				// conversion
+				// define date format
+				String dateFormat = "%Y/%m/%d";
+				// get the column type of the existing column
+				String type = getColumnType(table, column);
+				String tempTable = Utility.getRandomString(6);
+				if (type.equalsIgnoreCase("date")) {
+					String formatString = ", format = '" + dateFormat + "'";
+					script = tempTable + " <- format(" + table + "$" + column + formatString + ")";
+					frame.executeRScript(script);
+					script = table + "$" + column + " <- " + "as.Date(" + tempTable + formatString + ")";
+					frame.executeRScript(script);
+				} else {
+					script = tempTable + " <- as.Date(" + table + "$" + column + ", format='" + dateFormat + "')";
+					frame.executeRScript(script);
+					script = table + "$" + column + " <- " + tempTable;
+					frame.executeRScript(script);
+				}
+				// perform variable cleanup
+				frame.executeRScript("rm(" + tempTable + ");");
+				frame.executeRScript("gc();");
 			}
-			// perform variable cleanup
-			frame.executeRScript("rm(" + tempTable + ");");
-			frame.executeRScript("gc();");
+			// update the metadata
+			metadata.modifyDataTypeToProperty(table + "__" + column, table, newType);
 		}
-		// update the metadata
-		this.getFrame().getMetaData().modifyDataTypeToProperty(table + "__" + column, table, newType);
 		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE);
 	}
 
