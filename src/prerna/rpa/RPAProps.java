9bd35932eb6eb8394aa4b17cf888eddf1ac05ad5
tbanach <tbanach@deloitte.com>
2017-10-24 16:12:36 +0000

    UPDATE ::: Adding in rpa package for scheduling framework - will replace old quartz package
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24370 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8021bb4b6fabf3f66c9927730d6571743fc456cf

---
+++ src/prerna/rpa/RPAProps.java
@@ -0,0 +1,156 @@
+package prerna.rpa;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import prerna.rpa.security.Cryptographer;
+import prerna.util.DIHelper;
+
+public class RPAProps {
+
+	private static final Logger LOGGER = LogManager.getLogger(RPAProps.class.getName());
+
+	////////////////////////////////////////
+	// Default property names
+	public static final String RPA_CONFIG_DIRECTORY_KEY = "rpa.config.directory";
+	public static final String PROPERTY_FILE_PATH_KEY = "property.file.path";	
+	public static final String TEXT_DIRECTORY_KEY = "text.directory";
+	public static final String JSON_DIRECTORY_KEY = "json.directory";
+	////////////////////////////////////////
+	
+	////////////////////////////////////////
+	// Default config files and directories
+	private static final String PROPERTY_FILE_NAME = "rpa.properties";
+	private static final String PASSWORD_FILE_NAME = "rpa.password";
+	private static final String TEXT_FOLDER = "text";
+	private static final String JSON_FOLDER = "json";
+	////////////////////////////////////////
+	
+	private static final String ENCRYPTED_REGEX = "<encrypted>(.*?)</encrypted>";
+
+	private static RPAProps propsSingleton;
+	
+	// Assumes alphabetical order when saving to file
+	private static Properties props = new Properties() {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+	    public synchronized Enumeration<Object> keys() {
+	        return Collections.enumeration(new TreeSet<Object>(super.keySet()));
+	    }
+	};
+
+	private static String propertyFilePath;
+
+	private static char[] rpaPassword; // For decrypting encrypted properties
+	
+	// Singleton properties class
+	// This needs the rpa.config system property defined
+	private RPAProps() {
+		String rpaConfigDirectory = DIHelper.getInstance().getProperty(RPA_CONFIG_DIRECTORY_KEY);
+		
+		// Just in case make sure that it ends with a file separator
+		if (rpaConfigDirectory.endsWith("/") || rpaConfigDirectory.endsWith("\\")) {
+			rpaConfigDirectory = rpaConfigDirectory.substring(0, rpaConfigDirectory.length() - 1);
+		}
+		rpaConfigDirectory += System.getProperty("file.separator");
+
+		// Read in the properties
+		propertyFilePath = rpaConfigDirectory + PROPERTY_FILE_NAME;
+		try (InputStream in = new FileInputStream(propertyFilePath)) {
+			props.load(in);
+		} catch (IOException e) {
+			String noPropsMessage = "Unable to read properties from " + propertyFilePath + ". Check that the "
+					+ PROPERTY_FILE_NAME + " file exists in " + rpaConfigDirectory + ".";
+			LOGGER.error(noPropsMessage);
+
+			// No props, no point in running this program
+			throw new FailedToInitializeException(noPropsMessage, e);
+		}
+
+		// Read in the password for decrypting encrypted properties
+		// For security, don't store the password plainly as a property
+		// Only store the the password file path as a property
+		String passwordFilePath = rpaConfigDirectory + PASSWORD_FILE_NAME;
+		try (InputStream in = new FileInputStream(passwordFilePath)) {
+			rpaPassword = IOUtils.toCharArray(in, "UTF-8");
+		} catch (IOException e) {
+			String noPasswordMessage = "Unable to read password from " + passwordFilePath + ". Check that the "
+					+ PASSWORD_FILE_NAME + " file exists in " + rpaConfigDirectory + ".";
+			LOGGER.error(noPasswordMessage);
+
+			// No password, no point in running this program
+			throw new FailedToInitializeException(noPasswordMessage, e);
+		}
+		
+		// Set default properties
+		setProperty(RPA_CONFIG_DIRECTORY_KEY, rpaConfigDirectory);
+		setProperty(PROPERTY_FILE_PATH_KEY, propertyFilePath);
+		setProperty(TEXT_DIRECTORY_KEY, rpaConfigDirectory + TEXT_FOLDER + System.getProperty("file.separator"));
+		setProperty(JSON_DIRECTORY_KEY, rpaConfigDirectory + JSON_FOLDER + System.getProperty("file.separator"));		
+	}
+
+	public static RPAProps getInstance() {
+		if (propsSingleton == null) {
+			propsSingleton = new RPAProps();
+		}
+		return propsSingleton;
+	}
+
+	public String getProperty(String propertyName) throws Exception {
+		String propertyValue = props.getProperty(propertyName, "");
+		Matcher encryptedMatcher = Pattern.compile(ENCRYPTED_REGEX).matcher(propertyValue);
+		if (encryptedMatcher.matches()) {
+			String encryptedString = encryptedMatcher.group(1);
+			String salt = encryptedString.substring(0, 10);
+			encryptedString = encryptedString.substring(10);
+			try {
+				propertyValue = Cryptographer.decrypt(encryptedString, salt, rpaPassword);
+			} catch(Exception e) {
+				LOGGER.error("Failed to decrypt the encrypted property " + propertyName + ". Please check to make sure the password in " + PASSWORD_FILE_NAME + " is correct.");
+				throw e;
+			}
+		}
+		return propertyValue;
+	}
+
+	public void setProperty(String propertyName, String propertyValue) {
+		props.put(propertyName, propertyValue);
+	}
+	
+	public void setEncrpytedProperty(String propertyName, String propertyValue) throws Exception {
+		String salt = Cryptographer.getSalt();
+		try {
+			String encryptedPropertyValue = Cryptographer.encrypt(propertyValue, salt, rpaPassword);
+			props.put(propertyName, "<encrypted>" + salt + encryptedPropertyValue + "</encrypted>");
+		} catch (Exception e) {
+			LOGGER.error("Failed to encrpyt the " + propertyName + " property.");
+			throw e;
+		}
+	}
+
+	public void removeProperty(String propertyName) {
+		props.remove(propertyName);
+	}
+	
+	public void flushPropertiesToFile() throws IOException {
+		try (FileOutputStream out = new FileOutputStream(propertyFilePath)) {
+			props.store(out, null);
+		} catch (IOException e) {
+			LOGGER.error("Failed to flush properties to " + propertyFilePath + ".");
+			throw e;
+		}
+	}
+	
+}
