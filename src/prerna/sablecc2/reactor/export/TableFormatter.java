c3a2a319fa8a7cdb03fe357f09eb8a451ee714a0
mahkhalil <mahkhalil@deloitte.com>
2017-10-25 17:24:36 +0000

    UPDATE ::: Sending raw headers in table formatter
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24435 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/export/TableFormatter.java
+++ src/prerna/sablecc2/reactor/export/TableFormatter.java
@@ -11,22 +11,26 @@ public class TableFormatter extends AbstractFormatter {
 
 	private List<Object[]> data;
 	private String[] headers;
-	
+	private String[] rawHeaders;
+
 	public TableFormatter() {
 		this.data = new ArrayList<>(100);
 		this.headers = new String[0];
+		this.rawHeaders = new String[0];
 	}
 	
 	@Override
 	public void addData(IHeadersDataRow nextData) {
 		this.headers = nextData.getHeaders();
+		this.rawHeaders = nextData.getRawHeaders();
 		this.data.add(nextData.getValues());
 	}
 	
 	public Object getFormattedData() {
 		Map<String, Object> returnData = new Hashtable<String, Object>();
-		returnData.put("values", data);
-		returnData.put("headers", headers);
+		returnData.put("values", this.data);
+		returnData.put("headers", this.headers);
+		returnData.put("rawHeaders", this.rawHeaders);
 		return returnData;
 	}
 
