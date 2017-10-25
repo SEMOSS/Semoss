0d73c6ae0307b307e0aed57865e77d3494f61958
mahkhalil <mahkhalil@deloitte.com>
2017-10-25 19:05:40 +0000

    UPDATE ::: Passing back conceptual name in traversal options
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24444 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 484cb4f7e40cb7a15778f84ea98f3f02eb75b031

--- src/prerna/sablecc2/reactor/masterdatabase/AddLogicalNameReactor.java
+++ src/prerna/sablecc2/reactor/masterdatabase/AddLogicalNameReactor.java
@@ -11,6 +11,7 @@ import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.reactor.AbstractReactor;
 
 public class AddLogicalNameReactor extends AbstractReactor {
+	
 	private static final String ENGINE_KEY = "engine";
 	private static final String CONCEPT_KEY = "concept";
 	private static final String LOGICAL_NAME_KEY = "logicalNames";
@@ -26,6 +27,12 @@ public class AddLogicalNameReactor extends AbstractReactor {
 		}
 		return new NounMetadata(success, PixelDataType.BOOLEAN, PixelOperationType.CODE_EXECUTION);
 	}
+	
+	///////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////
+	///////////// GRAB INPUTS FROM PIXEL REACTOR //////////////
+	///////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////
 
 	private String getEngineName() {
 		GenRowStruct instanceGrs = this.store.getNoun(ENGINE_KEY);
