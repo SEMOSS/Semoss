7c809b794b001718bdcacb6aebc93066be9f014b
mahkhalil <mahkhalil@deloitte.com>
2017-10-27 17:03:52 +0000

    UPDATE ::: Adding reactor to generate a r frame from a variable
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24523 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 0d27458981bde0b11805b0060b7db1079c401d1a

---
+++ src/prerna/sablecc2/reactor/frame/r/GenerateFrameFromRVariableReactor.java
@@ -0,0 +1,49 @@
+package prerna.sablecc2.reactor.frame.r;
+
+import org.rosuda.REngine.Rserve.RConnection;
+
+import prerna.ds.r.RDataTable;
+import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.VarStore;
+import prerna.sablecc2.reactor.frame.r.util.IRJavaTranslator;
+import prerna.sablecc2.reactor.imports.ImportUtility;
+
+public class GenerateFrameFromRVariableReactor extends AbstractRFrameReactor {
+
+	@Override
+	public NounMetadata execute() {
+		String varName = getVarName();
+		this.rJavaTranslator.executeR("as.data.table(" + varName + ")");
+		// recreate a new frame and set the frame name
+		String[] colNames = this.rJavaTranslator.getColumns(varName);
+		String[] colTypes = this.rJavaTranslator.getColumns(varName);
+
+		if(colNames == null || colTypes == null) {
+			throw new IllegalArgumentException("Please make sure the variable " + varName + " exists and can be a valid data.table object");
+		}
+		
+		VarStore vars = this.insight.getVarStore();
+		RDataTable newTable = null;
+		if (vars.get(IRJavaTranslator.R_CONN) != null && vars.get(IRJavaTranslator.R_PORT) != null) {
+			newTable = new RDataTable(varName, 
+					(RConnection) vars.get(IRJavaTranslator.R_CONN).getValue(), 
+					(String) vars.get(IRJavaTranslator.R_PORT).getValue());
+		} else {
+			newTable = new RDataTable(varName);
+		}
+		ImportUtility.parseColumnsAndTypesToFlatTable(newTable, colNames, colTypes, varName);
+		this.insight.setDataMaker(newTable);
+		return new NounMetadata(newTable, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE);
+	}
+
+	/**
+	 * Get the input being the r variable name
+	 * @return
+	 */
+	private String getVarName() {
+		return this.curRow.get(0).toString();
+	}
+
+}
