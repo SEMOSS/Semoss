e5ed0a1f632758ba74fa14c82f9716fd267eaa15
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2017-10-27 22:15:47 +0000

    Checking in R Singleton and Shutdown thread. RServe starts up automatically now.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24540 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/engine/impl/r/RSingleton.java
+++ src/prerna/engine/impl/r/RSingleton.java
@@ -1,8 +1,11 @@
 package prerna.engine.impl.r;
 
+import java.io.IOException;
 import java.util.Hashtable;
 
+import org.apache.commons.lang.SystemUtils;
 import org.rosuda.REngine.Rserve.RConnection;
+import org.rosuda.REngine.Rserve.RserveException;
 
 public class RSingleton {
 	
@@ -14,7 +17,63 @@ public class RSingleton {
 		
 	}
 	
+	public static void startRServe(int port)
+	{
+		try {
+			String rHome = System.getenv("R_HOME");
+			System.out.println("RHome is ... " + rHome);
+			
+			rHome = rHome.replace("\\", System.getProperty("file.separator"));
+
+			rHome = rHome + System.getProperty("file.separator") + "bin" + System.getProperty("file.separator") + "R";
+
+			if(SystemUtils.IS_OS_WINDOWS)
+				rHome = rHome.replace(System.getProperty("file.separator"), "\\\\");
+
+			System.out.println("RHome is ... " + rHome);
+
+			Runtime.getRuntime().exec(" \"" + rHome + "\" -e \"library(Rserve);Rserve(FALSE,args='--vanilla --RS-port" + port + "');flush.console <- function(...) {return;}; options(error=function() NULL)\" --vanilla");
+			//System.out.println("R Started.. going to end");
+			//Runtime.getRuntime().exec(" \"C:\\Program Files\\R\\R-3.3.0\\bin\\R.exe\" -e \"library(Rserve); library(RSclient); rsc <- RSconnect(port = 6311); RSshutdown(rsc)\" --vanilla");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void stopRServe()
+	{
+		stopRServe(port);
+	}
+	
+	public static void stopRServe(int port)
+	{
+		try {
+			String rHome = System.getenv("R_HOME");
+			
+			rHome = rHome.replace("\\", System.getProperty("file.separator"));
+
+			rHome = rHome + System.getProperty("file.separator") + "bin" + System.getProperty("file.separator") + "R";
+
+			if(SystemUtils.IS_OS_WINDOWS)
+				rHome = rHome.replace(System.getProperty("file.separator"), "\\\\");
+
+			System.out.println("RHome is ... " + rHome);
+
+			//Runtime.getRuntime().exec(" \"C:\\Program Files\\R\\R-3.3.0\\bin\\R.exe\" -e \"library(Rserve);Rserve(FALSE,args='--vanilla --RS-port" + port + "');flush.console <- function(...) {return;}; options(error=function() NULL)\" --vanilla");
+			//System.out.println("R Started.. going to end");
+			Runtime.getRuntime().exec(" \"" + rHome+ "\" -e \"library(Rserve); library(RSclient); rsc <- RSconnect(port = " + port + "); RSshutdown(rsc)\" --vanilla");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	
 	public static RConnection getConnection() {
+		
+		
 		if(rcon == null) {
 			return getConnection("127.0.0.1", 6311);
 		}
@@ -26,7 +85,10 @@ public class RSingleton {
 	}
 	
 	public static RConnection getConnection(String host, int port) {
+		
 		// this basically tries to do the same as get connection with a port
+		
+		
 		if(portToCon.containsKey(port)) {
 			rcon = portToCon.get(port);
 		} else {
@@ -39,6 +101,17 @@ public class RSingleton {
 			}catch(Exception ex)
 			{
 				ex.printStackTrace();
+				// try to start again and see if that works
+				startRServe(port);
+				try {
+					rcon = new RConnection(host, port);
+					portToCon.put(port, rcon);
+					RSingleton.port = port;
+				} catch (RserveException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
 			}
 			
 		}
