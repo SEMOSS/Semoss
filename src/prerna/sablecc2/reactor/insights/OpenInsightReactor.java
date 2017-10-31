ec6277a46ac35c209e060e3009604d831addb06d
mahkhalil <mahkhalil@deloitte.com>
2017-10-31 16:28:09 +0000

    UPDATE ::: Allowing the open insight to take in additional pixels to append directly after the insight recipe
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24572 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/insights/OpenInsightReactor.java
+++ src/prerna/sablecc2/reactor/insights/OpenInsightReactor.java
@@ -7,6 +7,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import org.apache.solr.client.solrj.SolrServerException;
 
@@ -14,6 +15,7 @@ import prerna.engine.api.IEngine;
 import prerna.om.Insight;
 import prerna.om.InsightStore;
 import prerna.om.OldInsight;
+import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
@@ -22,21 +24,24 @@ import prerna.util.Utility;
 
 public class OpenInsightReactor extends AbstractInsightReactor {
 
+	private static final String ADDITIONAL_PIXELS = "additonalPixels";
+
 	@Override
 	public NounMetadata execute() {
 		// set the existing insight to the saved insight
-		
+
 		// get the recipe for the insight
 		// need the engine name and id that has the recipe
 		String engineName = getEngine();
 		int rdbmsId = getRdbmsId();
 		Object params = getParams();
-		
+		List<String> additionalPixels = getAdditionalPixels();
+
 		// get the engine so i can get the new insight
 		IEngine engine = Utility.getEngine(engineName);
 		List<Insight> in = engine.getInsight(rdbmsId + "");
 		Insight newInsight = in.get(0);
-				
+
 		// OLD INSIGHT
 		if(newInsight instanceof OldInsight) {
 			Map<String, Object> insightMap = new HashMap<String, Object>();
@@ -52,6 +57,13 @@ public class OpenInsightReactor extends AbstractInsightReactor {
 		// add the insight to the insight store
 		InsightStore.getInstance().put(newInsight);
 
+		// add additional pixels if necessary
+		if(additionalPixels != null && !additionalPixels.isEmpty()) {
+			// just add it directly to the pixel list
+			// and the reRunPiexelInsight will do its job
+			newInsight.getPixelRecipe().addAll(additionalPixels);
+		}
+		
 		Map<String, Object> insightMap = new HashMap<String, Object>();
 		// return to the FE the recipe
 		insightMap.put("name", newInsight.getInsightName());
@@ -60,7 +72,7 @@ public class OpenInsightReactor extends AbstractInsightReactor {
 		insightMap.put("core_engine_id", newInsight.getRdbmsId());
 		insightMap.put("insightData", newInsight.reRunPixelInsight());
 		insightMap.put("params", params);
-		
+
 		// update the solr tracker
 		try {
 			SolrIndexEngine.getInstance().updateViewedInsight(engineName + "_" + rdbmsId);
@@ -72,5 +84,20 @@ public class OpenInsightReactor extends AbstractInsightReactor {
 
 		// return the recipe steps
 		return new NounMetadata(insightMap, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.OPEN_SAVED_INSIGHT);
+	}
+
+	private List<String> getAdditionalPixels() {
+		GenRowStruct additionalPixels = this.store.getNoun(ADDITIONAL_PIXELS);
+		if(additionalPixels != null && !additionalPixels.isEmpty()) {
+			List<String> pixels = new Vector<String>();
+			int size = additionalPixels.size();
+			for(int i = 0; i < size; i++) {
+				pixels.add(additionalPixels.get(i).toString());
+			}
+			return pixels;
+		}
+
+		// no additional pixels to run
+		return null;
 	}
 }
