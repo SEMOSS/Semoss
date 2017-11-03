24ad3ef6bb6c9b1fa82abaad3dee7f749fdbbb33
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-11-03 14:15:46 +0000

    UPDATE ::: Solr interpreter2
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@24646 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b8ad0e361f278e6716719060a7a4723d51d291f9

---
+++ src/prerna/engine/impl/solr/SolrIterator.java
@@ -0,0 +1,98 @@
+package prerna.engine.impl.solr;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+
+import prerna.engine.api.IHeadersDataRow;
+import prerna.om.HeadersDataRow;
+import prerna.query.querystruct.QueryStruct2;
+import prerna.query.querystruct.selectors.IQuerySelector;
+
+public class SolrIterator implements Iterator<IHeadersDataRow> {
+	private SolrDocumentList list = null;
+	private long totalCount = 0;
+	private int returnSize = 0;
+	private int curIndex = 0;
+	private QueryStruct2 qs2;
+	protected String[] headers = null;
+	private boolean returnCount;
+
+	public SolrIterator(SolrDocumentList list, QueryStruct2 qs2) {
+		this.list = list;
+		this.qs2 = qs2;
+		this.returnCount = false;
+		this.headers = getReturnHeaders(this.returnCount);
+		this.returnSize = this.list.size();
+	}
+
+	// TODO not sure how i should take into consideration the count...
+	@Override
+	public boolean hasNext() {
+		if (this.curIndex < this.returnSize) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public IHeadersDataRow next() {
+		if (returnCount) {
+			Object[] dataRow = new Object[] { totalCount * getReturnHeaders(false).length };
+			HeadersDataRow row = new HeadersDataRow(new String[] { "NUM_FOUND" }, dataRow);
+			curIndex++;
+			return row;
+		}
+		// get the next document
+		SolrDocument doc = list.get(curIndex);
+		// get the fields that compose this document
+		Map<String, Object> docValues = doc.getFieldValueMap();
+		// loop through and turn the values into an array
+		int size = this.headers.length;
+		Object[] values = new Object[size];
+		for (int i = 0; i < size; i++) {
+			values[i] = docValues.get(headers[i]);
+		}
+
+		// update the curIndex
+		this.curIndex++;
+
+		// return the row
+		HeadersDataRow row = new HeadersDataRow(headers, values);
+		return row;
+	}
+
+	/**
+	 * Get the headers This uses the qs to determine the ordering since the solr
+	 * query returns a map
+	 * 
+	 * @param returnCount
+	 * @return
+	 */
+	private String[] getReturnHeaders(boolean returnCount) {
+		String[] returnHeaders = null;
+		if (returnCount) {
+			returnHeaders = new String[] { "NUM_FOUND" };
+		}
+		List<IQuerySelector> selectorData = this.qs2.getSelectors();
+		ArrayList<String> dV = new ArrayList<>();
+		for (IQuerySelector selector : selectorData) {
+			List<String> props = new ArrayList<>();
+			dV.add(selector.getAlias());
+			for (String prop : props) {
+				if (!prop.contains("PRIM_KEY_PLACEHOLDER")) {
+					dV.add(prop);
+				}
+			}
+
+		}
+		returnHeaders = dV.toArray(new String[dV.size()]);
+		return returnHeaders;
+	}
+
+}
