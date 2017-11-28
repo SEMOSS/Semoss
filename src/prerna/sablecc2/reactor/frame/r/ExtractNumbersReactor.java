499151c805133934e694fa1443b306ae5fbd9e57
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-11-28 20:59:17 +0000

    UPDATE ::: Clean extract numbers reactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25026 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: abe790ac8c0c3b28c4b5fd7c9a96be793610adda

---
+++ src/prerna/sablecc2/reactor/frame/r/ExtractNumbersReactor.java
@@ -0,0 +1,88 @@
+package prerna.sablecc2.reactor.frame.r;
+
+import java.util.List;
+import java.util.Vector;
+
+import prerna.ds.OwlTemporalEngineMeta;
+import prerna.ds.r.RDataTable;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+
+public class ExtractNumbersReactor extends AbstractRFrameReactor {
+	public static final String COLUMNS = "columns";
+	public static final String OVERRIDE = "override";
+	public static final String NUMERIC_COLUMN_NAME = "_NUMERIC";
+
+	@Override
+	public NounMetadata execute() {
+		init();
+		// get frame
+		RDataTable frame = (RDataTable) getFrame();
+		OwlTemporalEngineMeta metaData = frame.getMetaData();
+		// get table name
+		String table = frame.getTableName();
+		// get columns to extract numeric characters
+		List<String> columns = getColumns();
+		// check if user want to override the column or create new columns
+		boolean overrideColumn = getOverride();
+		// update existing columns
+		if (overrideColumn) {
+			for (int i = 0; i < columns.size(); i++) {
+				String column = columns.get(i);
+				String script = table + "$" + column + " <- as.numeric(gsub('[^-\\\\.0-9]', '', " + table + "$" + column
+						+ "));";
+				try {
+					frame.executeRScript(script);
+					frame.getMetaData().modifyDataTypeToProperty(table + "__" + column, table, "NUMBER");
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		// create new column
+		else {
+			for (int i = 0; i < columns.size(); i++) {
+				String column = columns.get(i);
+				String newColumn = getCleanNewColName(table, column + NUMERIC_COLUMN_NAME);
+				String update = table + "$" + newColumn + " <- " + table + "$" + column + ";";
+				update += table + "$" + newColumn + " <- as.numeric(gsub('[^-\\\\.0-9]', '', " + table + "$" + column + "));";
+				frame.executeRScript(update);
+				metaData.addProperty(table, table + "__" + newColumn);
+				metaData.setAliasToProperty(table + "__" + newColumn, newColumn);
+				metaData.setDataTypeToProperty(table + "__" + newColumn, "NUMBER");
+			}
+		}
+		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE);
+	}
+
+	private List<String> getColumns() {
+		GenRowStruct grs = this.store.getNoun(COLUMNS);
+		Vector<String> columns = new Vector<String>();
+		NounMetadata noun;
+		if (grs != null) {
+			for (int i = 0; i < grs.size(); i++) {
+				noun = grs.getNoun(i);
+				if (noun != null) {
+					String column = noun.getValue() + "";
+					if (column.length() > 0) {
+						columns.add(column);
+					}
+				}
+			}
+		}
+		return columns;
+	}
+
+	private boolean getOverride() {
+		GenRowStruct grs = this.store.getNoun(OVERRIDE);
+		boolean override = false;
+		NounMetadata noun;
+		if (grs != null) {
+			noun = grs.getNoun(0);
+			override = (Boolean) noun.getValue();
+		}
+		return override;
+	}
+}
