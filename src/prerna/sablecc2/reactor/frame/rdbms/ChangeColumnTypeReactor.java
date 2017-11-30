9e4fcd0c98c6bd0b6815bf918ea516111eb9eb76
rramirezjimenez <rramirezjimenez@deloitte.com>
2017-11-30 15:46:54 +0000

    UPDATE ::: ChangeColumnTypeReactor rdbms allows you to specify the date format when changing from string to date column type
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25067 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 0ff9bcbda4d4a134900d0ea3968ab4a347a3515d

--- src/prerna/sablecc2/reactor/frame/rdbms/ChangeColumnTypeReactor.java
+++ src/prerna/sablecc2/reactor/frame/rdbms/ChangeColumnTypeReactor.java
@@ -1,7 +1,6 @@
 package prerna.sablecc2.reactor.frame.rdbms;
 
 import java.util.Arrays;
-
 import prerna.algorithm.api.SemossDataType;
 import prerna.ds.h2.H2Frame;
 import prerna.sablecc2.om.GenRowStruct;
@@ -10,7 +9,18 @@ import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.reactor.frame.AbstractFrameReactor;
 
+/**
+* This reactor changes the data type of an existing column The inputs to the
+* reactor are: 
+* 1) the column to update 
+* 2) the desired column type
+* 3) if the desired type is a date, can enter the current date format
+*/
+
 public class ChangeColumnTypeReactor extends AbstractFrameReactor {
+
+	// this constant used when we specify the format for the dates
+	private static final String FORMAT = "format";
 
 	@Override
 	public NounMetadata execute() {
@@ -20,27 +30,51 @@ public class ChangeColumnTypeReactor extends AbstractFrameReactor {
 		String columnType = getColumnType();
 
 		String table = frame.getTableName();
-		String columnName = column;
 		if (column.contains("__")) {
 			String[] split = column.split("__");
 			table = split[0];
-			columnName = split[1];
+			column = split[1];
 		}
+
 		// check the column exists, if not then throw warning
 		String[] allCol = getColNames(table);
-		if (!Arrays.asList(allCol).contains(columnName)) {
+		if (!Arrays.asList(allCol).contains(column)) {
 			throw new IllegalArgumentException("Column doesn't exist.");
 		}
-		String update = "ALTER TABLE " + table + " ALTER COLUMN " + columnName + " " + columnType + " ; ";
-		try {
-			frame.getBuilder().runQuery(update);
-			frame.getMetaData().modifyDataTypeToProperty(table + "__" + columnName, table, columnType);
-		} catch (Exception e) {
-			e.printStackTrace();
+
+		// if we are changing from a string to a date, we need to parse using the format entered by the user
+		if (columnType.equalsIgnoreCase("date")) {
+			String dateFormat = getDateFormat();
+			// query of form: UPDATE FRAME SET birthday = PARSEDATETIME(column, 'format');
+			String convertString = "UPDATE " + table + " SET " + column + 
+					" = PARSEDATETIME (" + column + ", " + "'" + dateFormat + "');";
+			try {
+				frame.getBuilder().runQuery(convertString);
+				frame.getMetaData().modifyDataTypeToProperty(table + "__" + column, table, columnType);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			// if we are not working with a date
+			String update = "ALTER TABLE " + table + " ALTER COLUMN " + column + " " + columnType + " ; ";
+			try {
+				frame.getBuilder().runQuery(update);
+				frame.getMetaData().modifyDataTypeToProperty(table + "__" + column, table, columnType);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE);
 	}
 
+	
+	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	///////////////////////// GET PIXEL INPUT ////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	
+	// this method gets the name of the column that we are modifying
 	private String getColumn() {
 		GenRowStruct inputsGRS = this.getCurRow();
 		if (inputsGRS != null && !inputsGRS.isEmpty()) {
@@ -53,6 +87,7 @@ public class ChangeColumnTypeReactor extends AbstractFrameReactor {
 		throw new IllegalArgumentException("Need to define the new column name");
 	}
 
+	// this method gets the desired column type
 	private String getColumnType() {
 		GenRowStruct inputsGRS = this.getCurRow();
 		NounMetadata input2 = inputsGRS.getNoun(1);
@@ -71,4 +106,21 @@ public class ChangeColumnTypeReactor extends AbstractFrameReactor {
 		return columnType;
 	}
 
+	// we will get the format entered by the user so we can use PARSEDATETIME
+	private String getDateFormat() {
+		// grab the format that was inputted
+		GenRowStruct formatGRS = this.store.getNoun(FORMAT);
+		if (formatGRS != null) {
+			if (formatGRS.size() > 0) {
+				// get the user input which can either be from the drop down or free text
+				String dateFormat = formatGRS.get(0).toString();
+				// semoss uses underscores where the user might enter spaces
+				// for example, MMM yyyy will be updated to MMM_yyyy
+				dateFormat = dateFormat.replace(" ", "_");
+				return dateFormat;
+			}
+		}
+		// default to date format yyyy-mm-dd
+		return "yyyy-MM-dd";
+	}
 }
