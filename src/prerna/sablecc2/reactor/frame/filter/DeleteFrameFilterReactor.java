ca78b94a4091958cb181f67bcc0d7c1176ec988a
mahkhalil <mahkhalil@deloitte.com>
2017-12-01 16:49:04 +0000

    UPDATE ::: Adding DeleteFrameFilter to take filter indices to remove in GenRowFilter UPDATE ::: Modification to RemoveFrameFilterReactor to be a logical subtraction of instance values
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25103 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2e1cefab60b0e2a2d2fd601292c399c2addfcb22

---
+++ src/prerna/sablecc2/reactor/frame/filter/DeleteFrameFilterReactor.java
@@ -0,0 +1,74 @@
+package prerna.sablecc2.reactor.frame.filter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import prerna.algorithm.api.ITableDataFrame;
+import prerna.query.querystruct.GenRowFilters;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+
+public class DeleteFrameFilterReactor extends AbstractFilterReactor {
+
+	// this constant used when we specify the indexes for the filters that we want to remove
+	private static final String INDEX = "index";
+
+	@Override
+	public NounMetadata execute() {
+		ITableDataFrame frame = (ITableDataFrame) this.insight.getDataMaker();
+		GenRowFilters filters = null;
+
+		// get the existing filters
+		if (frame != null) {
+			filters = frame.getFrameFilters();
+		} else {
+			throw new IllegalArgumentException("No frame is defined in the insight to remove the filters from");
+		}
+
+		// first try to delete based on index
+		List<Integer> indexList = getOrderedIndexes();
+		// first we need to delete the highest index in order to not change the index of what we are deleting
+		for(int i = indexList.size(); i > 0 ; i--) {
+			// remove the filter at the index specified by the index list
+			filters.removeFilter(indexList.get(i - 1).intValue());
+		}
+
+		return new NounMetadata(filters, PixelDataType.FILTER, PixelOperationType.FRAME_FILTER);
+	}
+
+	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	///////////////////////// GET PIXEL INPUT ////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+
+	/**
+	 * return an ordered list of the filter indexes that we want to delete
+	 * @return
+	 */
+	private List<Integer> getOrderedIndexes() {
+		List<Integer> indexList = new ArrayList<Integer>();
+		// this grs will contain all indexes, each as a separate noun
+		GenRowStruct formatGRS = this.store.getNoun(INDEX);
+		if (formatGRS != null && formatGRS.size() > 0) {
+			for (int i = 0; i < formatGRS.size(); i++) {
+				indexList.add(((Number)formatGRS.getNoun(i).getValue()).intValue());
+			}
+		} else {
+			List<Object> numericInputs = this.curRow.getAllNumericColumns();
+			for (int i = 0; i < numericInputs.size(); i++) {
+				indexList.add(((Number)numericInputs.get(i)).intValue());
+			}
+		}
+		// sort so that we can later remove based on the highest first
+		if(indexList.isEmpty()) {
+			throw new IllegalArgumentException("No indices are defined");
+		}
+		Collections.sort(indexList);
+		return indexList;
+	}
+
+}
