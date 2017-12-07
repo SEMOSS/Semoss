a29fb28a38f5e945f1d4213f0885ff7b354e3393
mahkhalil <mahkhalil@deloitte.com>
2017-12-07 15:12:44 +0000

    UPDATE ::: Setting up key standardization and descriptions for inputs of reactors
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25201 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/sablecc2/om/ReactorKeysEnum.java
@@ -0,0 +1,27 @@
+package prerna.sablecc2.om;
+
+public enum ReactorKeysEnum {
+
+	INSIGHT_ID("id", "The id of the insight");
+	
+	
+	private String key;
+	private String description;
+	
+	private ReactorKeysEnum(String key, String description) {
+		this.key = key;
+		this.description = description;
+	}
+	
+	public static String getDescriptionFromKey(String key) {
+		for(ReactorKeysEnum e : ReactorKeysEnum.values()) {
+			if(e.key.equals(key)) {
+				return e.description;
+			}
+		}
+		// if we cannot find the description above
+		// it is not a standardized key
+		// so just return null
+		return null;
+	}
+}
