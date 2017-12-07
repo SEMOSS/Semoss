d79a9930e1a1f272bcf0adc0fe991e5f4bf80b0d
mahkhalil <mahkhalil@deloitte.com>
2017-12-07 18:04:42 +0000

    UPDATE ::: Modifying the return structure
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25207 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 62f4318afc4c21cd888e8f9facb2f67cb3f543cd

--- src/prerna/sablecc2/reactor/git/ListRemotes.java
+++ src/prerna/sablecc2/reactor/git/ListRemotes.java
@@ -1,6 +1,10 @@
 package prerna.sablecc2.reactor.git;
 
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
 import org.apache.log4j.Logger;
 
@@ -38,7 +42,16 @@ public class ListRemotes extends AbstractReactor {
 
 		Hashtable <String, String> repoList = helper.listConfigRemotes(dbName);
 		
-		return new NounMetadata(repoList, PixelDataType.MAP, PixelOperationType.MARKET_PLACE);
+		// FE wants an array
+		List<Map<String, String>> returnList = new Vector<Map<String, String>>();
+		for(String key : repoList.keySet()) {
+			Map<String, String> singleKey = new HashMap<String, String>();
+			singleKey.put("name", key);
+			singleKey.put("type", repoList.get(key));
+			returnList.add(singleKey);
+		}
+		
+		return new NounMetadata(returnList, PixelDataType.VECTOR, PixelOperationType.MARKET_PLACE);
 	}
 
 }
