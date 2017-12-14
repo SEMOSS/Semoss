f40f9642ea7585445dce5380a039dd45ae7eacc5
mahkhalil <mahkhalil@deloitte.com>
2017-12-14 15:29:18 +0000

    UPDATE ::: Returning fail if invalid username/password entered
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25345 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: de9d66d53739232c840cc7f45329cd733c1645cd

--- src/prerna/sablecc2/reactor/git/LoginGit.java
+++ src/prerna/sablecc2/reactor/git/LoginGit.java
@@ -1,10 +1,5 @@
 package prerna.sablecc2.reactor.git;
 
-import java.util.Hashtable;
-import java.util.List;
-import java.util.Map;
-import java.util.Vector;
-
 import org.apache.log4j.Logger;
 
 import prerna.sablecc2.om.NounMetadata;
@@ -15,79 +10,21 @@ import prerna.util.GitHelper;
 
 public class LoginGit extends AbstractReactor {
 
-	public LoginGit()
-	{
+	public LoginGit() {
 		this.keysToGet = new String[]{"username", "password"};
 	}
-	
+
 	@Override
 	public NounMetadata execute() {
-		// TODO Auto-generated method stub
 		Logger logger = getLogger(this.getClass().getName());
-		try {
-			organizeKeys();
-			
-			logger.info("Logging In");
-			
-			boolean valid = new GitHelper().login(keyValue.get(keysToGet[0]), keyValue.get(keysToGet[1]));
-			// if nothing is sent it means it is dual
-			logger.info("Complete");
-
-			return new NounMetadata(valid, PixelDataType.BOOLEAN, PixelOperationType.MARKET_PLACE);
-		} catch (Exception e) {
-			e.printStackTrace();
-			logger.fatal(e.getMessage());
+		organizeKeys();
+		logger.info("Logging In...");
+		boolean valid = new GitHelper().login(this.keyValue.get(this.keysToGet[0]), this.keyValue.get(this.keysToGet[1]));
+		if(valid) {
+			logger.info("Successfully logged in");
+		} else {
+			throw new IllegalArgumentException("Invalid Git credentials");
 		}
-		return null;
+		return new NounMetadata(valid, PixelDataType.BOOLEAN, PixelOperationType.MARKET_PLACE);
 	}
-	
-	/**
-	 * Need to get the appropriate files to perform indexing!
-	 * @param filesChanged
-	 * @return
-	 */
-	private Map<String, List<String>> getMosfetFiles(Map <String, List<String>> filesChanged) {
-		Map<String, List<String>> mosfetFiles = new Hashtable<String, List<String>>();
-		if(filesChanged.containsKey("ADD")) {
-			List<String> files = getMosfetFiles(filesChanged.get("ADD"));
-			if(!files.isEmpty()) {
-				mosfetFiles.put("ADD", files);
-			}
-		}
-		
-		if(filesChanged.containsKey("MOD")) {
-			List<String> files = getMosfetFiles(filesChanged.get("ADD"));
-			if(!files.isEmpty()) {
-				mosfetFiles.put("ADD", files);
-			}		
-		}
-		
-		if(filesChanged.containsKey("REN")) {
-			List<String> files = getMosfetFiles(filesChanged.get("ADD"));
-			if(!files.isEmpty()) {
-				mosfetFiles.put("ADD", files);
-			}		
-		}
-		
-		if(filesChanged.containsKey("DEL")) {
-			List<String> files = getMosfetFiles(filesChanged.get("ADD"));
-			if(!files.isEmpty()) {
-				mosfetFiles.put("ADD", files);
-			}		
-		}
-		return mosfetFiles;
-	}
-	
-	private List<String> getMosfetFiles(List<String> potentialFiles) {
-		List<String> mosfetFiles = new Vector<String>();
-		if(potentialFiles != null) {
-			for(String f : potentialFiles) {
-				if(f.endsWith(".mosfet")) {
-					mosfetFiles.add(f);
-				}
-			}
-		}
-		return mosfetFiles;
-	}
-
 }
