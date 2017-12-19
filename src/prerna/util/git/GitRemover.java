9876a9fa8ae59ca32fe327471279a8ee78d775b3
mahkhalil <mahkhalil@deloitte.com>
2017-12-19 13:58:01 +0000

    UPDATE ::: Fixing error with pushing db files if we had pushed data to a previous repo and now trying to push to a repo without syncing data
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25437 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/git/GitRemover.java
+++ src/prerna/util/git/GitRemover.java
@@ -3,6 +3,7 @@ package prerna.util.git;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.RmCommand;
@@ -16,23 +17,23 @@ public class GitRemover {
 	 * This class is not intended to be extended or used outside of its static method
 	 */
 	private GitRemover() {
-		
+
 	}
-	
-	public static void removeFiles(String gitFolder) {
+
+	public static void removeFiles(String localRepository) {
 		Git thisGit = null;
 		Status status = null;
 		try {
-			thisGit = Git.open(new File(gitFolder));
+			thisGit = Git.open(new File(localRepository));
 			status = thisGit.status().call();
 		} catch (IOException | NoWorkTreeException | GitAPIException e) {
 			e.printStackTrace();
-			throw new IllegalArgumentException("Unable to connect to Git directory at " + gitFolder);
+			throw new IllegalArgumentException("Unable to connect to Git directory at " + localRepository);
 		}
-		
+
 		RmCommand rm = thisGit.rm();
 		boolean removed = false;
-		
+
 		// get removed files
 		Iterator <String> upFiles = status.getRemoved().iterator();
 		while(upFiles.hasNext()) {
@@ -40,7 +41,7 @@ public class GitRemover {
 			String daFile = upFiles.next();
 			rm.addFilepattern(daFile);
 		}
-		
+
 		// get missing files
 		Iterator <String> modFiles = status.getMissing().iterator();
 		while(modFiles.hasNext()) {
@@ -54,10 +55,73 @@ public class GitRemover {
 				rm.call();
 			} catch (GitAPIException e) {
 				e.printStackTrace();
-				throw new IllegalArgumentException("Unable to remove files to Git directory at " + gitFolder);
+				throw new IllegalArgumentException("Unable to remove files to Git directory at " + localRepository);
 			}
 		}
-		
+
+		thisGit.close();
+	}
+
+	public static void removeSpecificFiles(String localRepository, List<String> files) {
+		if(files == null || files.size() == 0) {
+			return;
+		}
+		Git thisGit = null;
+		try {
+			thisGit = Git.open(new File(localRepository));
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Unable to connect to Git directory at " + localRepository);
+		}
+
+		RmCommand rm = thisGit.rm();
+		for(String daFile : files) {
+			if(daFile.contains("version")) {
+				daFile = daFile.substring(daFile.indexOf("version") + 8);
+			}
+			daFile = daFile.replace("\\", "/");
+			rm.addFilepattern(daFile);
+		}
+
+		try {
+			rm.call();
+		} catch (GitAPIException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Unable to remove files to Git directory at " + localRepository);
+		}
+
+		thisGit.close();
+	}
+	
+	public static void removeSpecificFiles(String localRepository, File... files) {
+		if(files == null || files.length == 0) {
+			return;
+		}
+		Git thisGit = null;
+		try {
+			thisGit = Git.open(new File(localRepository));
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Unable to connect to Git directory at " + localRepository);
+		}
+
+		RmCommand rm = thisGit.rm();
+		for(File f : files) {
+			String daFile = f.getAbsolutePath();
+			if(daFile.contains("version")) {
+				daFile = daFile.substring(daFile.indexOf("version") + 8);
+			}
+			daFile = daFile.replace("\\", "/");
+			rm.addFilepattern(daFile);
+		}
+
+		try {
+			rm.call();
+		} catch (GitAPIException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Unable to remove files to Git directory at " + localRepository);
+		}
+
 		thisGit.close();
 	}
 }
