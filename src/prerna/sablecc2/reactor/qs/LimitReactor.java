4426c73cd2f076319964f880aaf853be7ecf3847
micstone <micstone@deloitte.com>
2017-12-20 14:37:38 +0000

    UPDATE ::: Help functionality for qs reactors
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25476 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/qs/LimitReactor.java
+++ src/prerna/sablecc2/reactor/qs/LimitReactor.java
@@ -1,8 +1,13 @@
 package prerna.sablecc2.reactor.qs;
 
 import prerna.query.querystruct.QueryStruct2;
+import prerna.sablecc2.om.ReactorKeysEnum;
 
 public class LimitReactor extends QueryStructReactor {
+	
+	public LimitReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.NUMERIC_VALUE.getKey()};
+	}
 
 	@Override
 	QueryStruct2 createQueryStruct() {
