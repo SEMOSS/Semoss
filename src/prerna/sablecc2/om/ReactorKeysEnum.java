44cccb66993c36f2c4a21335a22ff34b27ae4cda
mahkhalil <mahkhalil@deloitte.com>
2017-12-20 20:32:36 +0000

    UPDATE ::: Using reactor keys for database sync
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25507 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 193aad3b1e1c31a2aea918cf37ccb8e63f954f6b

--- src/prerna/sablecc2/om/ReactorKeysEnum.java
+++ src/prerna/sablecc2/om/ReactorKeysEnum.java
@@ -67,6 +67,7 @@ public enum ReactorKeysEnum {
 	SORT("sort", 											"Sort direction: ascending (\"asc\") or decending (\"desc\")"),
 	STATEMENT("statement", 									"Statement to be evaluated"),
 	SUM_RANGE("sumRange", 									"Range that values to sum over"),
+	SYNC_DATABASE("syncDatabase", 							"True/False value to detetermine if the database should be published with the app"),
 	TASK("task", 											"Task object (can retrieve the object by using Task(taskId) with taskId is the unique id for the task)"),
 	TASK_ID("taskId", 										"Unique id of the task within the insight"),
 	TRAVERSAL_KEY("traversal", 								"The traversal"),
