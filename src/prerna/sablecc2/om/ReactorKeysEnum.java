b185882648c26f887f2b7ec04fba409d505de3d4
mahkhalil <mahkhalil@deloitte.com>
2017-12-21 20:41:51 +0000

    UPDATE ::: Modification of key to traversal
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25549 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/om/ReactorKeysEnum.java
+++ src/prerna/sablecc2/om/ReactorKeysEnum.java
@@ -72,7 +72,7 @@ public enum ReactorKeysEnum {
 	SYNC_DATABASE("syncDatabase", 							"True/False value to detetermine if the database should be published with the app"),
 	TASK("task", 											"Task object (can retrieve the object by using Task(taskId) with taskId is the unique id for the task)"),
 	TASK_ID("taskId", 										"Unique id of the task within the insight"),
-	TRAVERSAL_KEY("traversal", 								"The traversal"),
+	TRAVERSAL("traversal", 									"The traversal path within the map"),
 	USE_FRAME_FILTERS("useFrameFilters", 					"A boolean indication to use frame filters"), 								
 	USERNAME("username", 									"Unique identifier for the user to access a service"),
 	QUERY_KEY("query", 										"Query string to be executed on the database"),
