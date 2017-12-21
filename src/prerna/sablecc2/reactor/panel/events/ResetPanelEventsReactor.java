293cbcc245747577315ed494a3b71db46e81d550
micstone <micstone@deloitte.com>
2017-12-21 20:48:55 +0000

    UPDATE ::: Adding in panel events and ornaments help functionality
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25550 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: be770c017894c298ab0932713eb97060c5ed381f

--- src/prerna/sablecc2/reactor/panel/events/ResetPanelEventsReactor.java
+++ src/prerna/sablecc2/reactor/panel/events/ResetPanelEventsReactor.java
@@ -4,9 +4,14 @@ import prerna.om.InsightPanel;
 import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.reactor.panel.AbstractInsightPanelReactor;
 
 public class ResetPanelEventsReactor extends AbstractInsightPanelReactor {
+	
+	public ResetPanelEventsReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.PANEL.getKey()};
+	}
 
 	@Override
 	public NounMetadata execute() {
