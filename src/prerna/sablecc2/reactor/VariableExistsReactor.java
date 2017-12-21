88950232cb98ed2d3e1df9dfb073603f15a5dba2
micstone <micstone@deloitte.com>
2017-12-21 16:20:40 +0000

    UPDATE ::: Help functionality for VariableExistsReactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25525 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/VariableExistsReactor.java
+++ src/prerna/sablecc2/reactor/VariableExistsReactor.java
@@ -2,8 +2,13 @@ package prerna.sablecc2.reactor;
 
 import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
 
 public class VariableExistsReactor extends AbstractReactor {
+	
+	public VariableExistsReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.VARIABLE.getKey()};
+	}
 
 	@Override
 	public NounMetadata execute() {
