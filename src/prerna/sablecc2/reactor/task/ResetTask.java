017ef2730ae62374c82aab8e4190029fc266e6aa
micstone <micstone@deloitte.com>
2017-12-21 16:08:33 +0000

    UPDATE ::: Help functionality for TaskReactor, TaskOptionsReactor, TaskFormatReactor, ResetTask, and RemoveTaskReactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25520 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 60e64e82ae1c6626a96dec08a07da815c39b3fc6

--- src/prerna/sablecc2/reactor/task/ResetTask.java
+++ src/prerna/sablecc2/reactor/task/ResetTask.java
@@ -7,10 +7,15 @@ import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.task.ITask;
 import prerna.sablecc2.reactor.AbstractReactor;
 
 public class ResetTask extends AbstractReactor {
+	
+	public ResetTask() {
+		this.keysToGet = new String[]{ReactorKeysEnum.TASK_ID.getKey()};
+	}
 	
 	@Override
 	public NounMetadata execute() {
--- src/prerna/sablecc2/reactor/task/TaskFormatReactor.java
+++ src/prerna/sablecc2/reactor/task/TaskFormatReactor.java
@@ -3,8 +3,13 @@ package prerna.sablecc2.reactor.task;
 import java.util.Map;
 
 import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.ReactorKeysEnum;
 
 public class TaskFormatReactor extends TaskBuilderReactor {
+	
+	public TaskFormatReactor() {
+		this.keysToGet = new String[]{"type", ReactorKeysEnum.OPTIONS.getKey()};
+	}
 	
 	@Override
 	protected void buildTask() {
@@ -13,10 +18,21 @@ public class TaskFormatReactor extends TaskBuilderReactor {
 			String formatType = type.get(0).toString();
 			task.setFormat(formatType);
 		}
-		GenRowStruct options = this.store.getNoun("options");
+		GenRowStruct options = this.store.getNoun(keysToGet[1]);
 		if(options != null && !options.isEmpty()) {
 			Map<String, Object> optionValues = (Map<String, Object>) options.get(0);
 			task.setFormatOptions(optionValues);
+		}
+	}
+	
+	///////////////////////// KEYS /////////////////////////////////////
+
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if (key.equals("type")) {
+			return "The format type (e.g., a visualization can be the \"table\" or \"graph\" type)";
+		} else {
+			return super.getDescriptionForKey(key);
 		}
 	}
 }
