e6e822ed8b5f0b55b9de5517106efba2d216219b
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-01-03 21:28:12 +0000

    UPDATE ::: SendEmail pixel and added RunPixelJob to just read in a pixel and execute
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25605 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d0551a4a5eb160146c62b9f7cde9fddd5c24928b

---
+++ src/prerna/rpa/quartz/jobs/insight/RunPixelJob.java
@@ -0,0 +1,63 @@
+package prerna.rpa.quartz.jobs.insight;
+
+import java.util.Map;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.UnableToInterruptJobException;
+
+import prerna.om.Insight;
+import prerna.om.InsightStore;
+import prerna.rpa.quartz.CommonDataKeys;
+
+public class RunPixelJob implements org.quartz.InterruptableJob {
+
+	private static final Logger LOGGER = LogManager.getLogger(RunPixelJob.class.getName());
+	
+	/** {@code String} */
+	public static final String IN_PIXEL_KEY = RunPixelJob.class + ".pixel";
+	
+	/** {@code String} */
+	public static final String OUT_INSIGHT_ID_KEY = CommonDataKeys.INSIGHT_ID;
+	
+	private String jobName;
+	
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		
+		////////////////////
+		// Get inputs
+		////////////////////
+		JobDataMap dataMap = context.getMergedJobDataMap();
+		jobName = context.getJobDetail().getKey().getName();
+		String pixel = dataMap.getString(IN_PIXEL_KEY);
+		
+		////////////////////
+		// Do work
+		////////////////////
+		Insight insight = new Insight();
+		String insightId = InsightStore.getInstance().put(insight);
+		if(!pixel.endsWith(";")) {
+			pixel = pixel + ";";
+		}
+		LOGGER.info("Running pixel: " + pixel);
+		long start = System.currentTimeMillis();
+		Map<String, Object> results = insight.runPixel(pixel);
+		long end = System.currentTimeMillis();
+		LOGGER.info("Execution time: " + (end - start)/1000 + " seconds.");
+		
+		////////////////////
+		// Store outputs
+		////////////////////
+		dataMap.put(OUT_INSIGHT_ID_KEY, insightId);
+	}
+
+	@Override
+	public void interrupt() throws UnableToInterruptJobException {
+		LOGGER.warn("Received request to interrupt the " + jobName + " job. However, there is nothing to interrupt for this job.");		
+	}
+
+}
