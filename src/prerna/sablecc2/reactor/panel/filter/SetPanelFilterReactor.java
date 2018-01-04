bdcbb7878e254174824985723b433db8c394a9d7
micstone <micstone@deloitte.com>
2018-01-04 19:43:34 +0000

    UPDATE ::: Help functionality added for AddPanelFilterReactor, SetPanelFilterReactor, and UnfilterPanelReactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25615 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/panel/filter/SetPanelFilterReactor.java
+++ src/prerna/sablecc2/reactor/panel/filter/SetPanelFilterReactor.java
@@ -7,9 +7,14 @@ import prerna.query.querystruct.filters.GenRowFilters;
 import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.reactor.frame.filter.AbstractFilterReactor;
 
 public class SetPanelFilterReactor extends AbstractFilterReactor {
+	
+	public SetPanelFilterReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.PANEL.getKey(), ReactorKeysEnum.FILTERS.getKey()};
+	}
 
 	@Override
 	public NounMetadata execute() {
