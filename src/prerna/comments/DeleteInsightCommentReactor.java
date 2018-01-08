6e8cd1ddcdd7e7d3741c5cb82788ac8d93e8dd1e
mahkhalil <mahkhalil@deloitte.com>
2018-01-08 18:34:53 +0000

    UPDATE ::: Adding placeholders for insight comments
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25686 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/comments/DeleteInsightCommentReactor.java
@@ -0,0 +1,18 @@
+package prerna.comments;
+
+import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class DeleteInsightCommentReactor extends AbstractReactor {
+
+	public DeleteInsightCommentReactor() {
+		
+	}
+
+	@Override
+	public NounMetadata execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
