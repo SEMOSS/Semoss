09386bf38517074389c137129f18bcbb368fe46f
Maher Khalil <themaherkhalil@gmail.com>
2018-01-09 18:53:48 +0000

    UPDATE ::: Modification to insight comments to be append only
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25715 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8c554f1647036b4c77a1f48b95629b527f97dc82

--- src/prerna/comments/ModifyInsightCommentReactor.java
+++ src/prerna/comments/ModifyInsightCommentReactor.java
@@ -1,46 +1,43 @@
 package prerna.comments;
 
 import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.reactor.AbstractReactor;
 
 public class ModifyInsightCommentReactor extends AbstractReactor {
 
 	public ModifyInsightCommentReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.ENGINE.getKey(),
-				ReactorKeysEnum.INSIGHT_ID.getKey(),
-				ReactorKeysEnum.COMMENT_ID_KEY.getKey(),
+		this.keysToGet = new String[]{ReactorKeysEnum.COMMENT_ID_KEY.getKey(),
 				ReactorKeysEnum.COMMENT_KEY.getKey()};
-	};
+	}
 
 	@Override
 	public NounMetadata execute() {
-//		organizeKeys();
-//		String engine = this.keyValue.get(this.keysToGet[0]);
-//		if(engine == null) {
-//			throw new IllegalArgumentException("Need to define which engine this insight belongs to");
-//		}
-//		String rdbmsId = this.keyValue.get(this.keysToGet[1]);
-//		if(rdbmsId == null) {
-//			throw new IllegalArgumentException("Need to define which insight this comment belongs to");
-//		}
-//		String commentId = this.keyValue.get(this.keysToGet[2]);
-//		if(commentId == null || commentId.trim().isEmpty()) {
-//			throw new IllegalArgumentException("Need to define the comment id");
-//		}
-//		String newComment = this.keyValue.get(this.keysToGet[3]);
-//		if(newComment == null || newComment.trim().isEmpty()) {
-//			throw new IllegalArgumentException("Need a comment to save");
-//		}
-//		
-//		// after grabbing the input, write it to a file		
-//		InsightComment iComment = new InsightComment();
-//		iComment.loadFromFile(engine, rdbmsId, commentId);
-//		iComment.setComment(newComment);
-//		iComment.setLastModifedTimeStamp(InsightComment.getCurrentDate());
-//		iComment.writeToFile(engine, rdbmsId, newComment);
-//		return new NounMetadata(iComment.getCommentId(), PixelDataType.CONST_STRING);
-		return null;
+		organizeKeys();
+		String engine = this.insight.getEngineName();
+		String rdbmsId = this.insight.getRdbmsId();
+		
+		String commentId = this.keyValue.get(this.keysToGet[0]);
+		if(commentId == null || commentId.trim().isEmpty()) {
+			throw new IllegalArgumentException("Need to define the comment id");
+		}
+		String newComment = this.keyValue.get(this.keysToGet[1]);
+		if(newComment == null || newComment.trim().isEmpty()) {
+			throw new IllegalArgumentException("Need a comment to save");
+		}
+		
+		// after grabbing the input, write it to a file		
+		InsightComment iComment = new InsightComment(engine, rdbmsId);
+		iComment.setId(commentId);
+		// after we set the id
+		// we need to modify the previous id with the new timestamp of this created object
+		iComment.modifyExistingIdWithDate();
+		iComment.setComment(newComment);
+		iComment.setAction(InsightComment.EDIT_ACTION);
+		// add the comment to the chain
+		this.insight.addInsightComment(iComment);
+		return new NounMetadata(iComment.getId(), PixelDataType.CONST_STRING);
 	}
 	
 }
