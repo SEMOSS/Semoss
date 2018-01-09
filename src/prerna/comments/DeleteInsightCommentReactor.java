8c397bcd2a1bbb88063156d5f64559c190e282d1
mahkhalil <mahkhalil@deloitte.com>
2018-01-09 18:53:48 +0000

    UPDATE ::: Modification to insight comments to be append only
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25715 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8c554f1647036b4c77a1f48b95629b527f97dc82

--- src/prerna/comments/DeleteInsightCommentReactor.java
+++ src/prerna/comments/DeleteInsightCommentReactor.java
@@ -1,52 +1,34 @@
 package prerna.comments;
 
 import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.reactor.AbstractReactor;
 
 public class DeleteInsightCommentReactor extends AbstractReactor {
 
 	public DeleteInsightCommentReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.ENGINE.getKey(),
-				ReactorKeysEnum.INSIGHT_ID.getKey(),
-				ReactorKeysEnum.COMMENT_ID_KEY.getKey()};
+		this.keysToGet = new String[]{ReactorKeysEnum.COMMENT_ID_KEY.getKey()};
 	}
 
 	@Override
 	public NounMetadata execute() {
-//		organizeKeys();
-//		String engine = this.keyValue.get(this.keysToGet[0]);
-//		if(engine == null) {
-//			throw new IllegalArgumentException("Need to define which engine this insight belongs to");
-//		}
-//		String rdbmsId = this.keyValue.get(this.keysToGet[1]);
-//		if(rdbmsId == null) {
-//			throw new IllegalArgumentException("Need to define which insight this comment belongs to");
-//		}
-//		String commentId = this.keyValue.get(this.keysToGet[2]);
-//		if(commentId == null || commentId.trim().isEmpty()) {
-//			throw new IllegalArgumentException("Need to define the comment id");
-//		}
-//		
-//		// find the files relating to this id
-//		// and delete them
-//		String baseDir = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) 
-//				+ "\\" + Constants.DB + "\\" + engine + "\\version\\" + rdbmsId;		
-//		
-//		File dir = new File(baseDir);
-//		List<String> accept = new ArrayList<String>();
-//		accept.add(commentId + "*");
-//		FilenameFilter filter = new WildcardFileFilter(accept);
-//		File[] filesToDelete = dir.listFiles(filter);
-//		if(filesToDelete.length > 0) {
-//			for(File fDelete : filesToDelete) {
-//				fDelete.delete();
-//			}
-//			return new NounMetadata(true, PixelDataType.BOOLEAN);
-//		} else {
-//			return new NounMetadata(false, PixelDataType.BOOLEAN);
-//		}
-		return null;
+		organizeKeys();
+		String engine = this.insight.getEngineName();
+		String rdbmsId = this.insight.getRdbmsId();
+		
+		String commentId = this.keyValue.get(this.keysToGet[0]);
+		if(commentId == null || commentId.trim().isEmpty()) {
+			throw new IllegalArgumentException("Need to define the comment id");
+		}
+		
+		// after grabbing the input, write it to a file		
+		InsightComment iComment = new InsightComment(engine, rdbmsId);
+		iComment.setId(commentId);
+		iComment.setAction(InsightComment.DELETE_ACTION);
+		// add the comment to the chain
+		this.insight.addInsightComment(iComment);
+		return new NounMetadata(iComment.getId(), PixelDataType.CONST_STRING);
 	}
 	
 }
