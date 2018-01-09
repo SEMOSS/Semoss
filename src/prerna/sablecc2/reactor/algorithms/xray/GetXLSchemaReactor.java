d400fcf1cddef1d1ee2b8e3bb86dbc23041d54dc
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-01-09 17:01:17 +0000

    UPDATE ::: Cleaning up xray reactor inputs using organizeKeys()
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25710 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d1b010d558aa76f3cfb5aa4ff7ccf61d33b11b27

--- src/prerna/sablecc2/reactor/algorithms/xray/GetXLSchemaReactor.java
+++ src/prerna/sablecc2/reactor/algorithms/xray/GetXLSchemaReactor.java
@@ -1,6 +1,5 @@
 package prerna.sablecc2.reactor.algorithms.xray;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -9,11 +8,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.FilenameUtils;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.ObjectWriter;
 
 import prerna.poi.main.helper.XLFileHelper;
-import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
@@ -23,13 +19,22 @@ import prerna.util.Utility;
 
 public class GetXLSchemaReactor extends AbstractReactor {
 	public GetXLSchemaReactor() {
-		this.keysToGet = new String[] { ReactorKeysEnum.FILE_PATH.toString(), ReactorKeysEnum.SHEET_NAME.toString() };
+		this.keysToGet = new String[] { ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.SHEET_NAME.getKey() };
 	}
 
 	@Override
 	public NounMetadata execute() {
-		String filePath = getFilePath();
-		String sheetName = getSheetName();
+		// get inputs
+		organizeKeys();
+		String filePath = this.keyValue.get(this.keysToGet[0]);
+		if(filePath == null) {
+			throw new IllegalArgumentException("Need to define the " + ReactorKeysEnum.FILE_PATH.toString());
+		}
+		String sheetName = this.keyValue.get(this.keysToGet[1]);;
+		if(sheetName == null) {
+			throw new IllegalArgumentException("Need to define the " + ReactorKeysEnum.SHEET_NAME.toString());
+		}
+		
 		HashMap<String, Object> ret = new HashMap<String, Object>();
 		XLFileHelper helper = new XLFileHelper();
 		helper.parse(filePath);
@@ -40,16 +45,12 @@ public class GetXLSchemaReactor extends AbstractReactor {
 		Map<String, String> sheetDataMap = new LinkedHashMap<String, String>();
 		String[] columnHeaders = helper.getHeaders(sheetName);
 		String[] predicatedDataTypes = helper.predictRowTypes(sheetName);
-
 		HashMap<String, List<String>> relationshipMap = new HashMap<String, List<String>>();
 		for (String concept : columnHeaders) {
 			relationshipMap.put(concept, new ArrayList<String>());
 		}
-
 		ret.put("relationships", relationshipMap);
-
 		dataTypes.put(sheetName, sheetDataMap);
-
 		HashMap<String, HashMap> tableDetails = new HashMap<String, HashMap>();
 		for (int i = 0; i < columnHeaders.length; i++) {
 			HashMap<String, String> colDetails = new HashMap<String, String>();
@@ -58,39 +59,8 @@ public class GetXLSchemaReactor extends AbstractReactor {
 			colDetails.put("type", dataType);
 			tableDetails.put(columnHeaders[i], colDetails);
 		}
-
 		ret.put("tables", tableDetails);
-		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-		String schema = null;
-		try {
-			schema = ow.writeValueAsString(ret);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		return new NounMetadata(schema, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.CODE_EXECUTION);
-	}
-
-	private String getSheetName() {
-		GenRowStruct grs = this.store.getNoun(ReactorKeysEnum.SHEET_NAME.toString());
-		if (grs != null && !grs.isEmpty()) {
-			String sheet = grs.getNoun(0).getValue() + "";
-			if (sheet.length() > 0) {
-				return sheet;
-			}
-		}
-		throw new IllegalArgumentException("Need to define the " + ReactorKeysEnum.SHEET_NAME.toString());
-	}
-
-	private String getFilePath() {
-		GenRowStruct grs = this.store.getNoun(ReactorKeysEnum.FILE_PATH.toString());
-		if (grs != null && !grs.isEmpty()) {
-			String file = grs.getNoun(0).getValue() + "";
-			if (file.length() > 0) {
-				return file;
-			}
-		}
-		throw new IllegalArgumentException("Need to define the " + ReactorKeysEnum.FILE_PATH.toString());
+		return new NounMetadata(ret, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.CODE_EXECUTION);
 	}
 
 }
