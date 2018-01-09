8d56e188a9d02cdb1c03af64c35b35700c49a068
mahkhalil <mahkhalil@deloitte.com>
2018-01-09 14:20:34 +0000

    UPDATE ::: Pushing modify insight comment
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25703 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/comments/ModifyInsightCommentReactor.java
+++ src/prerna/comments/ModifyInsightCommentReactor.java
@@ -1,18 +1,46 @@
 package prerna.comments;
 
 import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.reactor.AbstractReactor;
 
 public class ModifyInsightCommentReactor extends AbstractReactor {
 
 	public ModifyInsightCommentReactor() {
-		
-	}
+		this.keysToGet = new String[]{ReactorKeysEnum.ENGINE.getKey(),
+				ReactorKeysEnum.INSIGHT_ID.getKey(),
+				ReactorKeysEnum.COMMENT_ID_KEY.getKey(),
+				ReactorKeysEnum.COMMENT_KEY.getKey()};
+	};
 
 	@Override
 	public NounMetadata execute() {
-		// TODO Auto-generated method stub
-		return null;
+		organizeKeys();
+		String engine = this.keyValue.get(this.keysToGet[0]);
+		if(engine == null) {
+			throw new IllegalArgumentException("Need to define which engine this insight belongs to");
+		}
+		String rdbmsId = this.keyValue.get(this.keysToGet[1]);
+		if(rdbmsId == null) {
+			throw new IllegalArgumentException("Need to define which insight this comment belongs to");
+		}
+		String commentId = this.keyValue.get(this.keysToGet[2]);
+		if(commentId == null || commentId.trim().isEmpty()) {
+			throw new IllegalArgumentException("Need to define the comment id");
+		}
+		String newComment = this.keyValue.get(this.keysToGet[3]);
+		if(newComment == null || newComment.trim().isEmpty()) {
+			throw new IllegalArgumentException("Need a comment to save");
+		}
+		
+		// after grabbing the input, write it to a file		
+		InsightComment iComment = new InsightComment();
+		iComment.loadFromFile(engine, rdbmsId, commentId);
+		iComment.setComment(newComment);
+		iComment.setLastModifedTimeStamp(InsightComment.getCurrentDate());
+		iComment.writeToFile(engine, rdbmsId, newComment);
+		return new NounMetadata(iComment.getCommentId(), PixelDataType.CONST_STRING);
 	}
 	
 }
