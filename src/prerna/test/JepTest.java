f10ed2c8097cffb5339a2321ad20dcba8a20a9bf
mahkhalil <mahkhalil@deloitte.com>
2018-01-10 03:06:08 +0000

    UPDATE ::: Adding in JEP. DEPENDENCY ::: Need to have python installed w/ JEP. Python and JEP need to be in windows path.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25734 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ae75fa0a568934ced982f1fd9b263028de9b3de0

--- src/prerna/test/JepTest.java
+++ src/prerna/test/JepTest.java
@@ -1,28 +1,28 @@
-//package prerna.test;
-//
-//import jep.Jep;
-//
-//public class JepTest {
-//
-//	public static void main(String [] args)
-//	{
-//		try(Jep jep = new Jep(false)) {
-//			long start = System.nanoTime();
-//			System.out.println("Time Start" + System.nanoTime());
-//		    jep.eval("from java.lang import System");
-//		    jep.eval("from pandasql import *");
-//		    jep.eval("import pandas as pd");
-//		    jep.eval("s = 'Hello World'");
-//		    jep.eval("System.out.println(s)");
-//		    jep.eval("print(s)");
-//		    jep.eval("print(s[1:-1])");
-//		    /*jep.eval("\ndef foo():");
-//		    jep.eval("\n\t i=5*2");
-//		    jep.eval("\n\t print(i)");
-//		    jep.eval("\n\t print(i)");
-//		    jep.eval("\n null");
-//		    jep.eval("\nfoo()");
-//		    */
+package prerna.test;
+
+import jep.Jep;
+
+public class JepTest {
+
+	public static void main(String [] args)
+	{
+		try(Jep jep = new Jep(false)) {
+			long start = System.nanoTime();
+			System.out.println("Time Start" + System.nanoTime());
+		    jep.eval("from java.lang import System");
+		    jep.eval("from pandasql import *");
+		    jep.eval("import pandas as pd");
+		    jep.eval("s = 'Hello World'");
+		    jep.eval("System.out.println(s)");
+		    jep.eval("print(s)");
+		    jep.eval("print(s[1:-1])");
+		    /*jep.eval("\ndef foo():");
+		    jep.eval("\n\t i=5*2");
+		    jep.eval("\n\t print(i)");
+		    jep.eval("\n\t print(i)");
+		    jep.eval("\n null");
+		    jep.eval("\nfoo()");
+		    */
 //		    jep.runScript("C:/Users/pkapaleeswaran/workspacej3/SemossDev/sample.py");
 //		    //jep.eval("\npath = r'C:/Users/pkapaleeswaran/workspacej3/datasets/physican data.csv'");
 //		    jep.eval("\npath = r'C:/Users/pkapaleeswaran/workspacej3/datasets/Movie.csv'");
@@ -38,19 +38,19 @@
 //		    
 //		    jep.eval("\nmyvar = sqldf('select Nominated from da', locals())");
 //		    System.out.println(jep.getValue("\nmyvar.head(3)"));
-//		    
-//		    
-//		    long end = System.nanoTime();
-//		    long micros = (end - start)/1000;
-//			System.out.println("Time Micros  " + micros);
-//			long millis = micros / 1000;
-//			System.out.println("Time Millis " + millis);
-//			long seconds = millis / 1000;
-//			System.out.println("Time Seconds " + seconds);
-//		}catch(Exception ex)
-//		{
-//			ex.printStackTrace();
-//		}
-//	}
-//	
-//}
+		    
+		    
+		    long end = System.nanoTime();
+		    long micros = (end - start)/1000;
+			System.out.println("Time Micros  " + micros);
+			long millis = micros / 1000;
+			System.out.println("Time Millis " + millis);
+			long seconds = millis / 1000;
+			System.out.println("Time Seconds " + seconds);
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+	
+}
