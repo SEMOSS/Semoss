d002e2777b23e798dae1730ae250025b0301fd51
Maher Khalil <themaherkhalil@gmail.com>
2018-01-17 17:34:20 +0000

    UPDATE ::: Sending the full image url
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25862 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 337c994850bf32d631db6aab058307b80e5316d7

--- src/prerna/sablecc2/reactor/masterdatabase/DatabaseInfoReactor.java
+++ src/prerna/sablecc2/reactor/masterdatabase/DatabaseInfoReactor.java
@@ -1,5 +1,6 @@
 package prerna.sablecc2.reactor.masterdatabase;
 
+import java.io.File;
 import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
@@ -9,6 +10,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 
 import prerna.sablecc2.om.NounMetadata;
 import prerna.sablecc2.om.PixelDataType;
@@ -16,6 +19,8 @@ import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.solr.SolrIndexEngine;
+import prerna.util.DIHelper;
+import prerna.util.insight.InsightScreenshot;
 
 public class DatabaseInfoReactor extends AbstractReactor {
 	
@@ -31,7 +36,7 @@ public class DatabaseInfoReactor extends AbstractReactor {
 		String groupByField = "core_engine";
 		String groupSort = "view_count";
 		
-		Map<String, List<Map<String, Object>>> engineMap = null;
+		Map<String, SolrDocumentList> engineMap = null;
 		try {
 			Map<String, Object> groupFieldMap = SolrIndexEngine.getInstance().executeQueryGroupBy(
 					searchString, 
@@ -41,7 +46,25 @@ public class DatabaseInfoReactor extends AbstractReactor {
 					groupSort, 
 					new HashMap<String, List<String>>());
 			Map<String, Object> queryRet = (Map<String, Object>) groupFieldMap.get("queryResponse");
-			engineMap = (Map<String, List<Map<String, Object>>>) queryRet.get("core_engine"); 
+			engineMap = (Map<String, SolrDocumentList>) queryRet.get("core_engine");
+
+			// need to replace the images
+			String basePath = DIHelper.getInstance().getProperty("BaseFolder");
+
+			for(String enginename : engineMap.keySet()) {
+				SolrDocumentList list = (SolrDocumentList) engineMap.get(enginename);
+				for (int i = 0; i < list.size(); i++) {
+					SolrDocument doc = list.get(i);
+					String imagePath = (String) doc.get("image");
+					if (imagePath != null && !imagePath.isEmpty() && !imagePath.contains("data:image/:base64")) {
+						File file = new File(basePath + imagePath);
+						if (file.exists()) {
+							String image = InsightScreenshot.imageToString(basePath + imagePath);
+							doc.put("image", "data:image/png;base64," + image);
+						}
+					}
+				}
+			}
 		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException | SolrServerException | IOException e) {
 			e.printStackTrace();
 		}
