6adfd884dc11278f0830ff1c2fc9b1d6059b2c1e
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-01-19 16:37:00 +0000

    UPDATE ::: GA recommendation r garbage clean up and deleting unused files
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25909 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- R/Recommendations/viz_tracking.r
+++ R/Recommendations/viz_tracking.r
@@ -31,33 +31,97 @@ viz_history<-function(df){
 }
 
 viz_recom<-function(df,df1,chartToExclude=NULL,top=3){
-	df$id<-paste0(df$dbname,"$",df$tblname,"$",df$colname,"$",df$chart)
-	df0<-count(df,names(df)[c(3,4,5,7)])
+	# df - is the history frame
+	# df1 - is the current frame
+	df$dbname<-as.character(df$dbname)
+	df$tblname<-as.character(df$tblname)
+	df$colname<-as.character(df$colname)
+	df1$dbname<-as.character(df1$dbname)
+	df1$tblname<-as.character(df1$tblname)
+	df1$colname<-as.character(df1$colname)
 
+	# filter the history based on the current frame
 	library(dplyr)
-	df2<-semi_join(df0,df1)
-	df3<-df2[,4:5]
+	dft<-semi_join(df,df1)
 	detach("package:dplyr", unload=TRUE)
-	library(plyr)
-	o<-count(df3,names(df3)[1])
+	# count number of records in each viz both in original history and filtered
+	a<-count(df,"unit")
+	b<-count(dft,"unit")
+	# remove those viz where at least one record does not belong the current frame
+	library(dplyr)
+	c<-semi_join(a,b)
+	detach("package:dplyr", unload=TRUE)
+	dff<-df[df$unit %in% c$unit,]
+	
+	# dff is this is the data frame we will be working with
+	# it contains only columns of the current frame
+	# count the chart/column frequency
+	dff0<-count(dff,names(dff)[c(3,4,5,7)])
+	dff1<-dff0[,4:5]
+	
+	o<-count(dff1,names(dff1)[1])
 	o<-o[order(-o$freq),]
 	if(!is.null(chartToExclude)){
 		o<-o[tolower(o$chart) != tolower(chartToExclude),]
 	}
 	o$weight<-round(o$freq/max(o$freq),4)
 	o<-o[,-2]
+	# the is the list most frequent charts
 	o<-head(o,top)
-	q<-df2[tolower(df2$chart) %in% tolower(o[,1]),]
+	
+	# Filter out history alteready filtered by our frame columns
+	# by the column frequency charts
+	q<-dff0[tolower(dff0$chart) %in% tolower(o[,1]),]
 	q1<-do.call(rbind, lapply(split(q,q$chart), function(x) {return(x[which.max(x$freq),])}))
+	
+	# determine the visualization number to get the details
+	dff$id<-paste0(dff$dbname,"$",dff$tblname,"$",dff$colname,"$",dff$chart)
 	q1$id<-paste0(q1$dbname,"$",q1$tblname,"$",q1$colname,"$",q1$chart)
-	
-	p<-df[df$id %in% q1$id,]
+	p<-dff[dff$id %in% q1$id,]
 	units<-aggregate(p$unit,by=list(chart=p$id),FUN=min)[,2]
+	# get the details
+	p1<-dff[dff$unit %in% units,][,1:7]
+	# merge with the respected chart weights
+	r<-merge(p1,o,by="chart")[,c(2,3,4,5,6,7,1,8)]
 	
-	p1<-df[df$unit %in% units,][,3:7]
-	r<-merge(p1,o,by="chart")[,c(2,3,4,1,5,6)]
-	
-	rm(df0,df2,df3,p,p1,q,q1,o)
-	return(r[order(-r$weight,r$chart,r$component,r$dbname,r$tblname,r$colname),])
+	# clean up and return
+	rm(dff,dff0,dff1,a,b,c,p,p1,q,q1,o,units)
+	return(r[order(-r$weight,r$chart,r$unit,r$element),])
 }
 
+
+get_userdata<-function(startDate,endDate,tokenPath){
+# Description
+# Retrieve the event data from GA project site then parse 
+# and assemble them with the requested level of details
+# Arguments
+# startDate - starting date for analysis
+# endDate - en date for analysis
+
+set_config(config(ssl_verifypeer = 0L))
+
+load(tokenPath)
+# Validate the token
+ValidateToken(token)
+
+viewID="ga:151491080"
+dim=c("1","2","3","4","5")
+query.list<-Init(
+            start.date=startDate,
+            end.date=endDate, 
+            dimension=toString(paste("ga:dimension", dim, sep="")),
+            metrics="ga:totalEvents",
+            sort=toString(paste("-ga:dimension", dim, sep="")),
+            max.results=10000,
+            table.id=viewID  
+        )
+ga.query<-QueryBuilder(query.list)
+#ga.df <- GetReportData(ga.query, token)
+ga.df <- tryCatch({
+    GetReportData(ga.query, token)
+  }, error = function(e) {
+    return(data.frame(dimension1 = character(), dimension2 = character(), dimension3 = character(), dimension4 = character(),dimension5 = character(),date = character(), totalEvents=numeric(1)))
+  })
+gc()
+return(ga.df)
+}
