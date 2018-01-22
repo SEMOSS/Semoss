bba524750fdb15f9f075c512bb218f54b9ff34ab
mahkhalil <mahkhalil@deloitte.com>
2018-01-22 23:36:18 +0000

    UPDATE ::: Adding in placeholder for pandas frame - need to revisit cause Jep is not useful UPDATE ::: Modification so Query Select Assimilator returns QS
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25933 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/FrameFactory.java
+++ src/prerna/sablecc2/reactor/frame/FrameFactory.java
@@ -4,6 +4,7 @@ import prerna.algorithm.api.ITableDataFrame;
 import prerna.ds.TinkerFrame;
 import prerna.ds.h2.H2Frame;
 import prerna.ds.nativeframe.NativeFrame;
+import prerna.ds.py.PandasFrame;
 import prerna.ds.r.RDataTable;
 
 public class FrameFactory {
@@ -11,10 +12,18 @@ public class FrameFactory {
 	public static ITableDataFrame getFrame(String frameType, String alias) {
 		switch (frameType.toUpperCase()) {
 		case "GRID": { return new H2Frame(alias); }
+		
 		case "GRAPH": { return new TinkerFrame(); } 
+		
 		case "RFRAME": { return new RDataTable(alias); }
 		case "R": { return new RDataTable(alias); }
+		
+		case "PYFRAME": { return new PandasFrame(alias); }
+		case "PANDAS": { return new PandasFrame(alias); }
+		case "PY": { return new PandasFrame(alias); }
+		
 		case "NATIVE": { return new NativeFrame(); }
+		
 		default: { return new H2Frame(alias); }
 		}
 	}
