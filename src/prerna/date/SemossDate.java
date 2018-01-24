15f1327183eaa1e010b4460b934cff3e993d79a8
mahkhalil <mahkhalil@deloitte.com>
2018-01-24 17:18:55 +0000

    UPDATE ::: Adding wrapper around date so we know how to format it
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@25977 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 664f95ce44d608c64b88d71a6a197e444f96ee2e

---
+++ src/prerna/date/SemossDate.java
@@ -0,0 +1,67 @@
+package prerna.date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SemossDate {
+
+	private String dateVal;
+	private String pattern;
+
+	private transient Date date;
+	
+	/*
+	 * This is just a basic wrapper around a date
+	 * so we can store the pattern with the date
+	 */
+	
+	public SemossDate(Date date) {
+		this(date, "yyyy-MM-dd");
+	}
+	
+	public SemossDate(Date date, String pattern) {
+		this.date = date;
+		this.pattern = pattern;
+		getFormattedDate();
+	}
+	
+	public SemossDate(String dateVal, String pattern) {
+		this.dateVal = dateVal;
+		this.pattern = pattern;
+		getDate();
+	}
+	
+	public String getPattern() {
+		return this.pattern;
+	}
+
+	/**
+	 * Get the string version of the date
+	 * @return
+	 */
+	public String getFormattedDate() {
+		if(this.dateVal == null) {
+			SimpleDateFormat formatter = new SimpleDateFormat(this.pattern);
+			this.dateVal = formatter.format(this.date);
+		}
+		return this.dateVal;
+	}
+	
+	/**
+	 * Get the formal date object
+	 * @return
+	 */
+	public Date getDate() {
+		if(this.date == null) {
+			SimpleDateFormat formatter = new SimpleDateFormat(this.pattern);
+			try {
+				this.date = formatter.parse(this.dateVal);
+			} catch (ParseException e) {
+				throw new IllegalArgumentException("Could not parse the date " + this.dateVal + " with the format " + formatter.toPattern());
+			}
+		}
+		return this.date;
+	}
+	
+}
