16774f323b6e67dcd7b89e596e6a8ae91ad5ce09
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-01-29 15:40:09 +0000

    UPDATE ::: DeleteEngineReactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@26060 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 91162f1c5a88798771715c34384993173668e11e

--- src/prerna/sablecc2/reactor/database/DeleteEngineReactor.java
+++ src/prerna/sablecc2/reactor/database/DeleteEngineReactor.java
@@ -1,8 +1,27 @@
 package prerna.sablecc2.reactor.database;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import prerna.engine.api.IEngine;
+import prerna.engine.impl.AbstractEngine;
+import prerna.nameserver.DeleteFromMasterDB;
+import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.NounMetadata;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.solr.SolrIndexEngine;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.Utility;
 
 public class DeleteEngineReactor extends AbstractReactor {
 
@@ -12,13 +31,140 @@ public class DeleteEngineReactor extends AbstractReactor {
 	
 	@Override
 	public NounMetadata execute() {
-		organizeKeys();
-		String engineToDelete = this.keyValue.get(this.keysToGet[0]);
+		// UserPermissionsMasterDB permissions = new UserPermissionsMasterDB();
+		// ArrayList<String> ownedEngines = permissions
+		// .getUserOwnedEngines(((User)
+		// request.getSession().getAttribute(Constants.SESSION_USER)).getId());
 		
-		
-		
-		// TODO Auto-generated method stub
-		return null;
+		// get inputs with key
+		GenRowStruct grs = this.store.getNoun(keysToGet[0]);
+		if (grs != null) {
+			for (NounMetadata noun : grs.vector) {
+				String engineName = noun.getValue().toString();
+				IEngine engine = Utility.getEngine(engineName);
+				if (engine != null) {
+					 deleteEngine(engine);
+				}
+
+			}
+		} 
+		// get all inputs
+		else {
+			for (int i = 0; i < this.curRow.size(); i++) {
+				String engineName = (String) this.curRow.get(i);
+				IEngine engine = Utility.getEngine(engineName);
+				if (engine != null) {
+					 deleteEngine(engine);
+				}
+
+				// TODO session code
+				// IEngine engine = getEngine(engineString, request);
+				// if (this.securityEnabled) {
+				// if (ownedEngines.contains(engineString)) {
+				// deleteEngine(engine, request);
+				// permissions.deleteEngine(
+				// ((User)
+				// request.getSession().getAttribute(Constants.SESSION_USER)).getId(),
+				// engineString);
+				// } else {
+				//// return Response.status(400).entity("You do not have access
+				// to
+				// delete this database.").build();
+				// return WebUtility.getResponse("You do not have access to
+				// delete
+				// this database.", 400);
+				// }
+				// } else {
+				// deleteEngine(engine, request)
+				// }
+			}
+		}
+
+		return new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.DELETE_ENGINE);
+	}
+
+	private boolean deleteEngine(IEngine coreEngine) {
+		String engineName = coreEngine.getEngineName();
+		coreEngine.deleteDB();
+
+		// remove from dihelper... this is absurd
+		String engineNames = (String) DIHelper.getInstance().getLocalProp(Constants.ENGINES);
+		engineNames = engineNames.replace(";" + engineName, "");
+		DIHelper.getInstance().setLocalProperty(Constants.ENGINES, engineNames);
+
+		DeleteFromMasterDB remover = new DeleteFromMasterDB();
+		remover.deleteEngineRDBMS(engineName);
+
+		SolrIndexEngine solrE;
+		try {
+			solrE = SolrIndexEngine.getInstance();
+			if (solrE.serverActive()) {
+				solrE.deleteEngine(engineName);
+			}
+		} catch (KeyManagementException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+		}
+
+		return true;
+	}
+	
+	// session code
+	private boolean deleteEngine(IEngine coreEngine, HttpServletRequest request) {
+		String engineName = coreEngine.getEngineName();
+		coreEngine.deleteDB();
+		// remove from session
+		HttpSession session = request.getSession();
+		ArrayList<Hashtable<String, String>> engines = (ArrayList<Hashtable<String, String>>) session
+				.getAttribute(Constants.ENGINES);
+		for (Hashtable<String, String> engine : engines) {
+			String engName = engine.get("name");
+			if (engName.equals(engineName)) {
+				engines.remove(engine);
+				System.out.println("Removed from engines");
+				session.setAttribute(Constants.ENGINES, engines);
+				break;//
+			}
+		}
+		session.removeAttribute(engineName);
+
+		// remove from dihelper... this is absurd
+		String engineNames = (String) DIHelper.getInstance().getLocalProp(Constants.ENGINES);
+		engineNames = engineNames.replace(";" + engineName, "");
+		DIHelper.getInstance().setLocalProperty(Constants.ENGINES, engineNames);
+
+		DeleteFromMasterDB remover = new DeleteFromMasterDB();
+		remover.deleteEngineRDBMS(engineName);
+
+		SolrIndexEngine solrE;
+		try {
+			solrE = SolrIndexEngine.getInstance();
+			if (solrE.serverActive()) {
+				solrE.deleteEngine(engineName);
+			}
+		} catch (KeyManagementException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+		}
+
+		return true;
+	}
+	
+	//session code
+	private AbstractEngine getEngine(String engineName, HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		AbstractEngine engine = null;
+		if (session.getAttribute(engineName) instanceof IEngine)
+			engine = (AbstractEngine) session.getAttribute(engineName);
+		else
+			engine = (AbstractEngine) Utility.getEngine(engineName);
+		return engine;
 	}
 
 }
