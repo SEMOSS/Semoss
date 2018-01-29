f08854a6d1aed74d082090573eb54e7ce4787e88
mahkhalil <mahkhalil@deloitte.com>
2018-01-29 17:47:47 +0000

    UPDATE ::: Adding app metadata calls
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@26070 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 7821813b0e3942f2db85c864e0fab4c15ccc6218

--- src/prerna/sablecc2/reactor/app/AppInfoReactor.java
+++ src/prerna/sablecc2/reactor/app/AppInfoReactor.java
@@ -1,4 +1,4 @@
-package prerna.sablecc2.reactor.masterdatabase;
+package prerna.sablecc2.reactor.app;
 
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
@@ -11,6 +11,7 @@ import java.util.Map;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
 
 import prerna.sablecc2.om.NounMetadata;
@@ -37,7 +38,7 @@ public class AppInfoReactor extends AbstractReactor {
 		builder.addReturnFields("app_name");
 		builder.addReturnFields("app_description");
 		builder.addReturnFields("app_image");
-		builder.addReturnFields("app_release_date");
+		builder.addReturnFields("app_creation_date");
 		builder.addReturnFields("app_requirements");
 		builder.addReturnFields("app_repo_url");
 		builder.addReturnFields("app_tags");
@@ -49,15 +50,21 @@ public class AppInfoReactor extends AbstractReactor {
 		builder.setFilterOptions(filterData);
 		
 		SolrQuery q = builder.getSolrQuery();
-		SolrDocumentList results = null;
+		SolrDocument appInfo = null;
 		try {
 			QueryResponse response = SolrIndexEngine.getInstance().getQueryResponse(q, SolrIndexEngine.SOLR_PATHS.SOLR_APP_PATH_NAME);
-			results = response.getResults();
+			SolrDocumentList results = response.getResults();
+			// there should only be 1
+			appInfo = results.get(0);
+			
+			// agument the metadata with the number of insights in the app
+			long numInsights = SolrIndexEngine.getInstance().getNumEngineInsights(appName);
+			appInfo.put("app_insights", numInsights);
 		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException | SolrServerException e) {
 			e.printStackTrace();
 		}
 
-		return new NounMetadata(results, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.DATABASE_INFO);
+		return new NounMetadata(appInfo, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.APP_INFO);
 	}
 
 }
