8e386e30ff22f7d38766f88c2118c7808a55237d
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-01-30 19:32:06 +0000

    UPDATE ::: Delete insight from solr ID so we don't need the engineName
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@26132 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c035b011069e8c4b7d725406d2f6d90497154fcc

--- src/prerna/sablecc2/reactor/database/DeleteInsightReactor.java
+++ src/prerna/sablecc2/reactor/database/DeleteInsightReactor.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 
@@ -28,68 +27,18 @@ import prerna.util.Utility;
 public class DeleteInsightReactor extends AbstractReactor {
 
 	public DeleteInsightReactor() {
-		this.keysToGet = new String[] { ReactorKeysEnum.ENGINE.getKey(), ReactorKeysEnum.INSIGHT_ID.getKey() };
+		this.keysToGet = new String[] { ReactorKeysEnum.INSIGHT_ID.getKey() };
 	}
 
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
-		String engineName = this.keyValue.get(this.keysToGet[0]);
-		List<String> insightIDList = getInsightIDs();
-		List<String> solrIDList = new ArrayList<String>();
-		IEngine engine = Utility.getEngine(engineName);
-		
-		// delete from insights database
-		InsightAdministrator admin = new InsightAdministrator(engine.getInsightDatabase());
-		try {
-			admin.dropInsight(insightIDList.toArray(new String[insightIDList.size()]));
-			solrIDList = SolrIndexEngine.getSolrIdFromInsightEngineId(engine.getEngineName(), insightIDList);
-		} catch (RuntimeException e) {
-			e.printStackTrace();
-		}
-		
-		// delete from solr
-		if (solrIDList != null) {
-			SolrIndexEngine solrE;
-			try {
-				solrE = SolrIndexEngine.getInstance();
-				if (solrE.serverActive()) {
-					solrE.removeInsight(solrIDList);
-				}
-			} catch (SolrServerException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (KeyManagementException e) {
-				e.printStackTrace();
-			} catch (NoSuchAlgorithmException e) {
-				e.printStackTrace();
-			} catch (KeyStoreException e) {
-				e.printStackTrace();
-			}
-		}	
-
-		// delete insight .mosfet file, if it exists
-		String recipePath = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + "\\" + Constants.DB + "\\" + engineName + "\\version\\" ;
-		for (int i = 0 ; i < insightIDList.size() ; i++){
-			File dir = new File(recipePath +  insightIDList.get(i));
-			
-			// if it exists then delete all files inside
-			if (dir.exists() && dir.isDirectory()){
-				try {
-					FileUtils.deleteDirectory(dir);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		return new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.DELETE_INSIGHT);
-	}
-	
-	private List<String> getInsightIDs() {
-		List<String> ids = new Vector<String>();
-		// see if added as key
-		GenRowStruct grs = this.store.getNoun(this.keysToGet[1]);
+		// get engine name from the id list
+		String engineName = null;
+		// get ids to delete
+		List<String> solrIDList = new Vector<String>();
+		List<String> insightIDList = new Vector<String>();
+		GenRowStruct grs = this.store.getNoun(this.keysToGet[0]);
 		if (grs != null && !grs.isEmpty()) {
 			int size = grs.size();
 			for (int i = 0; i < size; i++) {
@@ -98,25 +47,75 @@ public class DeleteInsightReactor extends AbstractReactor {
 				String id = grs.get(i).toString();
 				if (id.contains("_")) {
 					String[] split = id.split("_");
-					ids.add(split[1]);
+					engineName = split[0];
+					insightIDList.add(split[1]);
+					solrIDList.add(id);
 				}
 			}
-			return ids;
 		}
 
-		// no key is added, grab all inputs ignore first
+		// no key is added, grab all ids
 		int size = this.curRow.size();
-		for (int i = 1; i < size; i++) {
+		for (int i = 0; i < size; i++) {
 			String id = this.curRow.get(i).toString();
 			// id is passed in from solr id where it is defined as engine_id
 			// so I need to split it
 			if (id.contains("_")) {
 				String[] split = id.split("_");
-				ids.add(split[1]);
+				engineName = split[0];
+				insightIDList.add(split[1]);
+				solrIDList.add(id);
 			}
 		}
-		return ids;
+		IEngine engine = Utility.getEngine(engineName);
+		if (engine != null) {
+			// delete from insights database
+			InsightAdministrator admin = new InsightAdministrator(engine.getInsightDatabase());
+			try {
+				admin.dropInsight(insightIDList.toArray(new String[insightIDList.size()]));
+			} catch (RuntimeException e) {
+				e.printStackTrace();
+			}
+
+			// delete from solr
+			if (solrIDList != null) {
+				SolrIndexEngine solrE;
+				try {
+					solrE = SolrIndexEngine.getInstance();
+					if (solrE.serverActive()) {
+						solrE.removeInsight(solrIDList);
+					}
+				} catch (SolrServerException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (KeyManagementException e) {
+					e.printStackTrace();
+				} catch (NoSuchAlgorithmException e) {
+					e.printStackTrace();
+				} catch (KeyStoreException e) {
+					e.printStackTrace();
+				}
+			}
+
+			// delete insight .mosfet file, if it exists
+			String recipePath = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + "\\" + Constants.DB + "\\" + engineName + "\\version\\";
+			for (int i = 0; i < insightIDList.size(); i++) {
+				File dir = new File(recipePath + insightIDList.get(i));
+				// if it exists then delete all files inside
+				if (dir.exists() && dir.isDirectory()) {
+					try {
+						FileUtils.deleteDirectory(dir);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			return new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.DELETE_INSIGHT);
+		}
+		// unable to delete
+		return new NounMetadata(false, PixelDataType.BOOLEAN, PixelOperationType.DELETE_INSIGHT);
+
 	}
 
 }
-	
