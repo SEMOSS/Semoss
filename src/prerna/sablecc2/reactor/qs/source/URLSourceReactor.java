d729dbb1ca926af7848ce4aad2f0f8ba7ce7bf0a
mahkhalil <mahkhalil@deloitte.com>
2018-02-07 00:55:03 +0000

    UPDATE ::: Also sending the insight id in the GetInsightComments reactor BUGFIX ::: Commenting out URLSourceReactor until missing methods are also pushed
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@26321 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/qs/source/URLSourceReactor.java
+++ src/prerna/sablecc2/reactor/qs/source/URLSourceReactor.java
@@ -15,46 +15,46 @@ import prerna.util.Utility;
 public class URLSourceReactor extends AbstractQueryStructReactor {
 
 	public URLSourceReactor() {
-		this.keysToGet = new String[] { ReactorKeysEnum.URL.getKey(), ReactorKeysEnum.DELIMITER.getKey() };
+//		this.keysToGet = new String[] { ReactorKeysEnum.URL.getKey(), ReactorKeysEnum.DELIMITER.getKey() };
 	}
 
 	@Override
 	protected QueryStruct2 createQueryStruct() {
 		// get inputs
-		organizeKeys();
-		String urlInput = this.keyValue.get(this.keysToGet[0]);
-		String delimiterStr = this.keyValue.get(this.keysToGet[1]);
-
-		// default delimiter or use the one defined
-		char delim = ',';
-		if (delimiterStr != null && delimiterStr.length() > 0) {
-			delim = delimiterStr.charAt(0);
-		}
-
-		// flush out url to file
-		String filePath = DIHelper.getInstance().getProperty(Constants.INSIGHT_CACHE_DIR) + "\\"
-				+ DIHelper.getInstance().getProperty(Constants.CSV_INSIGHT_CACHE_FOLDER);
-		filePath += "\\" + Utility.getRandomString(10) + ".csv";
-		filePath = filePath.replace("\\", "/");
-		Utility.copyURLtoFile(urlInput, filePath);
-
-		// get datatypes
-		CSVFileHelper helper = new CSVFileHelper();
-		helper.setDelimiter(delim);
-		helper.parse(filePath);
-		MetaModelCreator predictor = new MetaModelCreator(helper, null);
-		Map<String, String> dataTypes = predictor.getDataTypeMap();
-		// assume file is csv
-		CsvQueryStruct qs = new CsvQueryStruct();
-		
-		for (String key : dataTypes.keySet()) {
-			qs.addSelector("DND", key);
-		}
-		helper.clear();
-		qs.merge(this.qs);
-		qs.setCsvFilePath(filePath);
-		qs.setDelimiter(delim);
-		qs.setColumnTypes(dataTypes);
+//		organizeKeys();
+//		String urlInput = this.keyValue.get(this.keysToGet[0]);
+//		String delimiterStr = this.keyValue.get(this.keysToGet[1]);
+//
+//		// default delimiter or use the one defined
+//		char delim = ',';
+//		if (delimiterStr != null && delimiterStr.length() > 0) {
+//			delim = delimiterStr.charAt(0);
+//		}
+//
+//		// flush out url to file
+//		String filePath = DIHelper.getInstance().getProperty(Constants.INSIGHT_CACHE_DIR) + "\\"
+//				+ DIHelper.getInstance().getProperty(Constants.CSV_INSIGHT_CACHE_FOLDER);
+//		filePath += "\\" + Utility.getRandomString(10) + ".csv";
+//		filePath = filePath.replace("\\", "/");
+//		Utility.copyURLtoFile(urlInput, filePath);
+//
+//		// get datatypes
+//		CSVFileHelper helper = new CSVFileHelper();
+//		helper.setDelimiter(delim);
+//		helper.parse(filePath);
+//		MetaModelCreator predictor = new MetaModelCreator(helper, null);
+//		Map<String, String> dataTypes = predictor.getDataTypeMap();
+//		// assume file is csv
+//		CsvQueryStruct qs = new CsvQueryStruct();
+//		
+//		for (String key : dataTypes.keySet()) {
+//			qs.addSelector("DND", key);
+//		}
+//		helper.clear();
+//		qs.merge(this.qs);
+//		qs.setCsvFilePath(filePath);
+//		qs.setDelimiter(delim);
+//		qs.setColumnTypes(dataTypes);
 
 		return qs;
 	}
