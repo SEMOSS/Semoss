3e6c71d94e7d87f0a80571e7bf91ea49cc505f28
mahkhalil <mahkhalil@deloitte.com>
2018-02-07 20:29:49 +0000

    UPDATE ::: Cleaning up a frame variable if it is being removed
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@26363 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/insights/RemoveVariableReactor.java
+++ src/prerna/sablecc2/reactor/insights/RemoveVariableReactor.java
@@ -1,5 +1,7 @@
 package prerna.sablecc2.reactor.insights;
 
+import prerna.algorithm.api.ITableDataFrame;
+import prerna.ds.h2.H2Frame;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
@@ -17,6 +19,20 @@ public class RemoveVariableReactor extends AbstractInsightReactor{
 		boolean success = true;
 		if(noun == null) {
 			success = false;
+		} else {
+			PixelDataType nType = noun.getNounType();
+			if(nType == PixelDataType.FRAME) {
+				ITableDataFrame dm = (ITableDataFrame) noun.getValue();
+				//TODO: expose a delete on the frame to hide this crap
+				// drop the existing tables/connections if present
+				if(dm instanceof H2Frame) {
+					H2Frame frame = (H2Frame)dm;
+					frame.dropTable();
+					if(!frame.isInMem()) {
+						frame.dropOnDiskTemporalSchema();
+					}
+				}
+			}
 		}
 		return new NounMetadata(success, PixelDataType.BOOLEAN);
 	}
