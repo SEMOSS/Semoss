4c9bedbf8ad9bacadfed16aea55ac15d5f3e1b23
mahkhalil <mahkhalil@deloitte.com>
2018-02-13 22:17:03 +0000

    UPDATE ::: Pushing basic processing to create an app image automatically based on name of app
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@26528 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/insight/TextToGraphic.java
+++ src/prerna/util/insight/TextToGraphic.java
@@ -8,14 +8,26 @@ import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 
 import javax.imageio.ImageIO;
 
+import prerna.algorithm.nlp.TextHelper;
+
 public class TextToGraphic {
 
-    public static void main(String[] args) {
-        String text = "Hello";
-
+	private static Random rand = new Random();
+	
+    public static void makeImage(String name, String imageLocation) {
+    	String[] split = TextHelper.formatCompountText(name).split(" ");
+    	StringBuilder textBuilder = new StringBuilder();
+    	if(split.length == 1) {
+    		textBuilder.append(split[0].toUpperCase().charAt(0));
+    	} else {
+    		textBuilder.append(split[0].toUpperCase().charAt(0));
+    		textBuilder.append(split[split.length-1].toUpperCase().charAt(0));
+    	}
+    	String text = textBuilder.toString().trim();
         /*
            Because font metrics is based on a graphics context, we need to create
            a small, temporary image so we can ascertain the width and height
@@ -23,15 +35,17 @@ public class TextToGraphic {
          */
         BufferedImage img = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g2d = img.createGraphics();
-        Font font = new Font("Arial", Font.PLAIN, 48);
+        Font font = new Font("SansSerif", Font.PLAIN, 48);
         g2d.setFont(font);
         FontMetrics fm = g2d.getFontMetrics();
-        int width = fm.stringWidth(text);
-        int height = fm.getHeight();
+        int width = 125; //fm.stringWidth(text)+20;
+        int height = 105; //fm.getHeight();
         g2d.dispose();
 
         img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         g2d = img.createGraphics();
+        g2d.setColor(getRandomColor());
+        g2d.fillRect(0, 0, width, height);
         g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
@@ -42,14 +56,30 @@ public class TextToGraphic {
         g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
         g2d.setFont(font);
         fm = g2d.getFontMetrics();
-        g2d.setColor(Color.BLACK);
-        g2d.drawString(text, 0, fm.getAscent());
+        g2d.setColor(Color.WHITE);
+        font = new Font("SansSerif", Font.PLAIN, 24);
+        fm = g2d.getFontMetrics();
+        int strWidth = (int) (width - fm.stringWidth(text))/2;
+        int strHeight = (int) (height - fm.getHeight())/2 + height/2;
+        g2d.drawString(text, strWidth, strHeight);
         g2d.dispose();
         try {
-            ImageIO.write(img, "png", new File("C:\\workspace\\Semoss_Dev\\Text.png"));
+            ImageIO.write(img, "png", new File(imageLocation));
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+    
+    public static Color getRandomColor() {
+    	String[] colors = new String[]{"#48BFA8", "#E0BF39", "#E67E22", "#4FA4DE", "#52CF87", "#EB6456", "#bdc3c7", "#9b59b6", "#34495e", "#F28E8E"};
+    	int index = rand.nextInt(colors.length);
+    	System.out.println(colors[index]);
+    	Color c = Color.decode(colors[index]);
+    	return c;
+    }
+    
+    public static void main(String[] args) {
+    	TextToGraphic.makeImage("MovieDatabase", "C:\\workspace\\Semoss_Dev\\image.png");
     }
 
 }
