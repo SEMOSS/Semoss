e14988828786241b93716a8005edb060578cd802
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-02-15 16:55:19 +0000

    UPDATE ::: DB profile
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@26601 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/utils/DatabaseProfileReactor.java
+++ src/prerna/sablecc2/reactor/utils/DatabaseProfileReactor.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
+import prerna.ds.OwlTemporalEngineMeta;
 import prerna.ds.h2.H2Frame;
 import prerna.engine.api.IEngine;
 import prerna.engine.api.IHeadersDataRow;
@@ -35,25 +36,77 @@ public class DatabaseProfileReactor extends AbstractReactor {
 		organizeKeys();
 		// output frame
 		String[] headers = new String[] { "table_name", "column_name", "numOfBlanks", "numOfUniqueValues", "min", "average", "max", "sum" };
-		String[] dataTypes = new String[] { "String", "String", "DOUBLE", "DOUBLE", "DOUBLE", "DOUBLE", "DOUBLE", "DOUBLE" };
-		H2Frame frame = new H2Frame(headers, dataTypes);
+		String[] dataTypes = new String[] { "String", "String", "Double", "Double", "Double", "Double", "Double", "Double" };
+		H2Frame frame = (H2Frame) this.insight.getDataMaker();
+		String tableName = frame.getTableName();
+		OwlTemporalEngineMeta metaData = frame.getMetaData();
+		for (int i = 0; i < headers.length; i++) {
+			String alias = headers[i];
+			String dataType = dataTypes[i];
+			String uniqueHeader = tableName + "__" + alias;
+			metaData.addProperty(tableName, uniqueHeader);
+			// metaData.setQueryStructNameToProperty(uniqueHeader, source,
+			// qsName);
+			metaData.setAliasToProperty(uniqueHeader, alias);
+			metaData.setDataTypeToProperty(uniqueHeader, dataType);
+		}
 		String dbName = this.keyValue.get(this.keysToGet[0]);
 		IEngine engine = Utility.getEngine(dbName);
 		if (engine != null) {
 			List<String> conceptList = getConceptList();
-
 			// get concept properties from local master
 			Map<String, HashMap> dbMap = MasterDatabaseUtility.getConceptProperties(conceptList, dbName);
 			HashMap<String, ArrayList<String>> conceptMap = dbMap.get(dbName);
 			for (int i = 0; i < conceptList.size(); i++) {
 				String concept = conceptList.get(i);
-				if (conceptMap.containsKey(concept)) {
+				// TODO get col name for concepts
+				String conceptColName = concept;
+				// concept data
+				String conceptDataType = MasterDatabaseUtility.getBasicDataType(dbName, conceptColName, concept);
+				if (Utility.isNumericType(conceptDataType)) {
+					String[] cells = new String[8];
+					// table name
+					cells[0] = concept;
+					// column name
+					cells[1] = concept;
+					// num of blanks
+					cells[2] = 0 + "";
+					// num of unique vals
+					cells[3] = getValue(engine, concept, concept, QueryFunctionHelper.UNIQUE_COUNT) + "";
+					// min
+					cells[4] = getValue(engine, concept, conceptColName, QueryFunctionHelper.MIN) + "";
+					// average
+					cells[5] = getValue(engine, concept, conceptColName, QueryFunctionHelper.AVERAGE_1) + "";
+					// max
+					cells[6] = getValue(engine, concept, conceptColName, QueryFunctionHelper.MAX) + "";
+					// sum
+					cells[7] = getValue(engine, concept, conceptColName, QueryFunctionHelper.SUM) + "";
+					// add data to frame
+					frame.addRow(tableName, cells, headers, dataTypes);
+				} else {
+					if (Utility.isStringType(conceptDataType)) {
+						String[] cells = new String[8];
+						// table name
+						cells[0] = concept;
+						// column name
+						cells[1] = concept;
+						// num of blanks
+						cells[2] = null;
+						// num of unique vals
+						cells[3] = getValue(engine, concept, concept, QueryFunctionHelper.UNIQUE_COUNT) + "";
+						// add data to frame
+						frame.addRow(tableName, cells, headers, dataTypes);
+					}
+				}
+				// prop data
+				
+				if (conceptMap != null && conceptMap.containsKey(concept)) {
 					ArrayList<String> properties = conceptMap.get(concept);
 					for (String prop : properties) {
 						String dataType = MasterDatabaseUtility.getBasicDataType(dbName, prop, concept);
 						if (Utility.isNumericType(dataType)) {
 							// will need to get min, average, max, sum
-							Object[] cells = new Object[8];
+							String[] cells = new String[8];
 							// table name
 							cells[0] = concept;
 							// column name
@@ -78,93 +131,23 @@ public class DatabaseProfileReactor extends AbstractReactor {
 //							}
 //							Iterator<IHeadersDataRow> blankIt = WrapperManager.getInstance().getRawWrapper(engine, qs2);
 //							long blankCount = ((Number) blankIt.next().getValues()[0]).longValue();
-							cells[2] = 0;
+							cells[2] = null;
 							// # of unique values
-							 qs2 = new QueryStruct2();
-							{
-								QueryFunctionSelector uniqueCountSelector = new QueryFunctionSelector();
-								uniqueCountSelector.setFunction(QueryFunctionHelper.UNIQUE_COUNT);
-								uniqueCountSelector.setDistinct(true);
-								QueryColumnSelector innerSelector = new QueryColumnSelector();
-
-								innerSelector.setTable(concept);
-								innerSelector.setColumn(prop);
-								uniqueCountSelector.addInnerSelector(innerSelector);
-								qs2.addSelector(uniqueCountSelector);
-							}
-							Iterator<IHeadersDataRow> uIt = WrapperManager.getInstance().getRawWrapper(engine, qs2);
-							long uniqueNRow = ((Number) uIt.next().getValues()[0]).longValue();
-							cells[3] = uniqueNRow;
-
+							cells[3] = getValue(engine, concept, prop, QueryFunctionHelper.UNIQUE_COUNT) + "";
 							// min
-							qs2 = new QueryStruct2();
-							{
-								QueryFunctionSelector uniqueCountSelector = new QueryFunctionSelector();
-								uniqueCountSelector.setFunction(QueryFunctionHelper.MIN);
-								QueryColumnSelector innerSelector = new QueryColumnSelector();
-
-								innerSelector.setTable(concept);
-								innerSelector.setColumn(prop);
-								uniqueCountSelector.addInnerSelector(innerSelector);
-								qs2.addSelector(uniqueCountSelector);
-							}
-							Iterator<IHeadersDataRow> minIt = WrapperManager.getInstance().getRawWrapper(engine, qs2);
-							long min = ((Number) minIt.next().getValues()[0]).longValue();
-							cells[4] = min;
-
+							cells[4] = getValue(engine, concept, prop, QueryFunctionHelper.MIN) + "";
 							// average
-							qs2 = new QueryStruct2();
-							{
-								QueryFunctionSelector uniqueCountSelector = new QueryFunctionSelector();
-								uniqueCountSelector.setFunction(QueryFunctionHelper.AVERAGE_1);
-								QueryColumnSelector innerSelector = new QueryColumnSelector();
-
-								innerSelector.setTable(concept);
-								innerSelector.setColumn(prop);
-								uniqueCountSelector.addInnerSelector(innerSelector);
-								qs2.addSelector(uniqueCountSelector);
-							}
-							Iterator<IHeadersDataRow> avgIt = WrapperManager.getInstance().getRawWrapper(engine, qs2);
-							long avg = ((Number) avgIt.next().getValues()[0]).longValue();
-							cells[5] = avg;
-
+							cells[5] = getValue(engine, concept, prop, QueryFunctionHelper.AVERAGE_1) + "";
 							// max
-							qs2 = new QueryStruct2();
-							{
-								QueryFunctionSelector uniqueCountSelector = new QueryFunctionSelector();
-								uniqueCountSelector.setFunction(QueryFunctionHelper.MAX);
-								QueryColumnSelector innerSelector = new QueryColumnSelector();
-
-								innerSelector.setTable(concept);
-								innerSelector.setColumn(prop);
-								uniqueCountSelector.addInnerSelector(innerSelector);
-								qs2.addSelector(uniqueCountSelector);
-							}
-							Iterator<IHeadersDataRow> maxIt = WrapperManager.getInstance().getRawWrapper(engine, qs2);
-							long max = ((Number) maxIt.next().getValues()[0]).longValue();
-							cells[6] = max;
-
+							cells[6] = getValue(engine, concept, prop, QueryFunctionHelper.MAX) + "";
 							// sum
-							qs2 = new QueryStruct2();
-							{
-								QueryFunctionSelector uniqueCountSelector = new QueryFunctionSelector();
-								uniqueCountSelector.setFunction(QueryFunctionHelper.SUM);
-								QueryColumnSelector innerSelector = new QueryColumnSelector();
-
-								innerSelector.setTable(concept);
-								innerSelector.setColumn(prop);
-								uniqueCountSelector.addInnerSelector(innerSelector);
-								qs2.addSelector(uniqueCountSelector);
-							}
-							Iterator<IHeadersDataRow> sumIt = WrapperManager.getInstance().getRawWrapper(engine, qs2);
-							long sum = ((Number) sumIt.next().getValues()[0]).longValue();
-							cells[7] = sum;
+							cells[7] = getValue(engine, concept, prop, QueryFunctionHelper.SUM) + "";
 							// add data to frame
-							frame.addRow(cells, headers);
+							frame.addRow(tableName, cells, headers, dataTypes);
 						} else {
 							// assume string
 							if (Utility.isStringType(dataType)) {
-								Object[] cells = new Object[8];
+								String[] cells = new String[8];
 								// table name
 								cells[0] = concept;
 								// column name
@@ -172,27 +155,33 @@ public class DatabaseProfileReactor extends AbstractReactor {
 								// # TODO of blanks
 								cells[2] = null;
 								// # of unique values
-								QueryStruct2 qs2 = new QueryStruct2();
-								{
-									QueryFunctionSelector uniqueCountSel = new QueryFunctionSelector();
-									uniqueCountSel.setFunction(QueryFunctionHelper.UNIQUE_COUNT);
-									uniqueCountSel.setDistinct(true);
-									QueryColumnSelector innerSelector = new QueryColumnSelector();
-									innerSelector.setTable(concept);
-									innerSelector.setColumn(prop);
-									uniqueCountSel.addInnerSelector(innerSelector);
-									qs2.addSelector(uniqueCountSel);
-								}
-								Iterator<IHeadersDataRow> uIt = WrapperManager.getInstance().getRawWrapper(engine, qs2);
-								long uniqueNRow = ((Number) uIt.next().getValues()[0]).longValue();
-								cells[3] = uniqueNRow;
-								frame.addRow(cells, headers);
+								cells[3] = getValue(engine, concept, prop, QueryFunctionHelper.UNIQUE_COUNT) + "";
+								frame.addRow(tableName, cells, headers, dataTypes);
 							}
 						}
 					}
 				}
-			}		}
-		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME);
+			}
+		}
+		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_HEADERS_CHANGE);
+	}
+
+	private Object getValue(IEngine engine, String concept, String prop, String functionName) {
+		QueryStruct2 qs2 = new QueryStruct2();
+		{
+			QueryFunctionSelector funSelector = new QueryFunctionSelector();
+			funSelector.setFunction(functionName);
+//			funSelector.setDistinct(true);
+			QueryColumnSelector innerSelector = new QueryColumnSelector();
+
+			innerSelector.setTable(concept);
+			innerSelector.setColumn(prop);
+			funSelector.addInnerSelector(innerSelector);
+			qs2.addSelector(funSelector);
+		}
+		Iterator<IHeadersDataRow> it = WrapperManager.getInstance().getRawWrapper(engine, qs2);
+		Object value = it.next().getValues()[0];
+		return value;
 	}
 
 	private List<String> getConceptList() {
