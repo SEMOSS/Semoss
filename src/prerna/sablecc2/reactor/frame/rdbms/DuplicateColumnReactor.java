1e332bc911e42e46ee172608c12b2dd6d1cdbd3b
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-02-22 21:28:29 +0000

    UPDATE ::: Splitting logic of add duplicate column from AddColumnReactor to it's own DuplicateColumnReactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@26729 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 200eef45063cbb20ca16692b894f8707ea71cabc

---
+++ src/prerna/sablecc2/reactor/frame/rdbms/DuplicateColumnReactor.java
@@ -0,0 +1,86 @@
+package prerna.sablecc2.reactor.frame.rdbms;
+
+import java.util.Arrays;
+
+import prerna.algorithm.api.SemossDataType;
+import prerna.ds.OwlTemporalEngineMeta;
+import prerna.ds.h2.H2Frame;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.AddHeaderNounMetadata;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.frame.AbstractFrameReactor;
+
+public class DuplicateColumnReactor extends AbstractFrameReactor {
+
+	/**
+	 * This reactor duplicates and existing column and adds it to the frame. The
+	 * inputs to the reactor are: 
+	 * 1) the name for the column to duplicate 
+	 * 2) the new column name
+	 */
+
+	public DuplicateColumnReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.COLUMN.getKey(), ReactorKeysEnum.NEW_COLUMN.getKey() };
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+
+		// get frame
+		H2Frame frame = (H2Frame) getFrame();
+		String table = frame.getTableName();
+
+		// get source column to duplicate
+		String srcCol = this.keyValue.get(this.keysToGet[0]);
+
+		// make sure source column exists
+		String[] allCol = getColNames(table);
+		if (srcCol == null || !Arrays.asList(allCol).contains(srcCol)) {
+			throw new IllegalArgumentException("Need to define an existing column to duplicate.");
+		}
+
+		// clean and validate new column name or use default name
+		String newColName = getCleanNewColName(table, srcCol + "_DUPLICATE");
+		String inputColName = this.keyValue.get(this.keysToGet[1]);
+		if (inputColName != null && !inputColName.isEmpty()) {
+			inputColName = getCleanNewColName(table, inputColName);
+			// entire new name could be invalid characters
+			if (!inputColName.equals("")) {
+				newColName = inputColName;
+			}
+		}
+
+		// get src column data type
+		OwlTemporalEngineMeta metaData = frame.getMetaData();
+		String dataType = metaData.getHeaderTypeAsString(table + "__" + srcCol);
+		dataType = SemossDataType.convertDataTypeToString(SemossDataType.convertStringToDataType(dataType));
+
+		// use existing column data type to make new column
+		String addNewCol = "ALTER TABLE " + table + " ADD " + newColName + " " + dataType + ";";
+		try {
+			frame.getBuilder().runQuery(addNewCol);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// update new column with the duplicate data
+		String duplicate = "UPDATE " + table + " SET " + newColName + " = " + srcCol + ";";
+		try {
+			frame.getBuilder().runQuery(duplicate);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// update meta data
+		metaData.addProperty(table, table + "__" + newColName);
+		metaData.setAliasToProperty(table + "__" + newColName, newColName);
+		metaData.setDataTypeToProperty(table + "__" + newColName, dataType);
+
+		NounMetadata retNoun = new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_HEADERS_CHANGE, PixelOperationType.FRAME_DATA_CHANGE);
+		retNoun.addAdditionalReturn(new AddHeaderNounMetadata(newColName));
+		return retNoun;
+	}
+}
