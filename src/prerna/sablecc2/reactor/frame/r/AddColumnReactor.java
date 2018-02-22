5dbaa87e4c746410247d2621c67e2e498a9d5105
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-02-22 15:07:40 +0000

    UPDATE ::: Copy column values to a new column for R Frame
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@26712 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/r/AddColumnReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/AddColumnReactor.java
@@ -1,5 +1,7 @@
 package prerna.sablecc2.reactor.frame.r;
 
+import java.util.Arrays;
+
 import prerna.algorithm.api.SemossDataType;
 import prerna.ds.OwlTemporalEngineMeta;
 import prerna.ds.r.RDataTable;
@@ -19,7 +21,7 @@ public class AddColumnReactor extends AbstractRFrameReactor {
 	 */
 	
 	public AddColumnReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.COLUMN.getKey(), ReactorKeysEnum.DATA_TYPE.getKey()};
+		this.keysToGet = new String[]{ReactorKeysEnum.NEW_COLUMN.getKey(), ReactorKeysEnum.DATA_TYPE.getKey(), ReactorKeysEnum.COLUMN.getKey()};
 	}
 
 	@Override
@@ -28,8 +30,8 @@ public class AddColumnReactor extends AbstractRFrameReactor {
 		organizeKeys();
 		// get frame
 		RDataTable frame = (RDataTable) getFrame();
-		// get column name from the gen row struct
-		String colName = this.keyValue.get(this.keysToGet[0]);
+		// get new column name from the gen row struct
+		String newColName = this.keyValue.get(this.keysToGet[0]);
 		
 		// get the column type and standardize
 		String colType = this.keyValue.get(this.keysToGet[1]);
@@ -37,56 +39,66 @@ public class AddColumnReactor extends AbstractRFrameReactor {
 			colType = SemossDataType.convertStringToDataType("STRING").toString();
 		}
 		
-		if (colName == null){
+		if (newColName == null || newColName.isEmpty()){
 			throw new IllegalArgumentException("Need to define the new column name");
 		}
 
 		String table = frame.getTableName();
 		// clean colName
-		if (colName.contains("__")) {
-			String[] split = colName.split("__");
-			colName = split[1];
+		if (newColName.contains("__")) {
+			String[] split = newColName.split("__");
+			newColName = split[1];
 		}
 		// clean the column name to ensure that it is valid
-		colName = getCleanNewColName(table, colName);
-		if (colName.equals("")){
-			throw new IllegalArgumentException("Provide valid new column name (no special characters)");
-		}
+		newColName = getCleanNewColName(table, newColName);
+
 		// define the script to be executed;
 		// this assigns a new column name with no data in columns
-		String script = table + "$" + colName + " <- \"\" ";
+		String script = table + "$" + newColName + " <- \"\" ";
 		// execute the r script
 		frame.executeRScript(script);
 
 		// update the metadata to include this new column
 		OwlTemporalEngineMeta metaData = this.getFrame().getMetaData();
-		metaData.addProperty(table, table + "__" + colName);
-		metaData.setAliasToProperty(table + "__" + colName, colName);
+		metaData.addProperty(table, table + "__" + newColName);
+		metaData.setAliasToProperty(table + "__" + newColName, newColName);
+		
+		// copy values from column to new column
+		String duplicateSrcCol = this.keyValue.get(this.keysToGet[2]).trim();
+		if (duplicateSrcCol != null && !duplicateSrcCol.isEmpty()){
+			// make sure src column to duplicate exists, if not it will just be blank
+			String[] allCol = getColumns(table);
+			if (Arrays.asList(allCol).contains(duplicateSrcCol)) {
+				String duplicate = table + "$" + newColName + "<-" + table + "$" + duplicateSrcCol + ";";
+				frame.executeRScript(duplicate);
+			}
+		}
 
 		// temp table used to assign a data type to the new column
 		String tempTable = null;
 		if (Utility.isNumericType(colType)) {
 			// update the metadata depending on the data type
-			metaData.setDataTypeToProperty(table + "__" + colName, "NUMBER");
+			metaData.setDataTypeToProperty(table + "__" + newColName, "NUMBER");
 			tempTable = Utility.getRandomString(6);
-			script = tempTable + " <- as.numeric(" + table + "$" + colName + ")";
+			script = tempTable + " <- as.numeric(" + table + "$" + newColName + ")";
 			frame.executeRScript(script);
-			script = table + "$" + colName + "<-" + tempTable;
+			script = table + "$" + newColName + "<-" + tempTable;
 			frame.executeRScript(script);
 		} else if (Utility.isDateType(colType)) {
-			metaData.setDataTypeToProperty(table + "__" + colName, "DATE");
+			metaData.setDataTypeToProperty(table + "__" + newColName, "DATE");
 			tempTable = Utility.getRandomString(6);
 			String dateFormat = "%Y/%m/%d";
-			script = tempTable + " <- as.Date(" + table + "$" + colName + ", format='" + dateFormat + "')";
+			script = tempTable + " <- as.Date(" + table + "$" + newColName + ", format='" + dateFormat + "')";
 			frame.executeRScript(script);
-			script = table + "$" + colName + " <- " + tempTable;
+			script = table + "$" + newColName + " <- " + tempTable;
 			frame.executeRScript(script);
 		} else {
 			// if not a number or a date then assign to string
-			metaData.setDataTypeToProperty(table + "__" + colName, "STRING");
+			metaData.setDataTypeToProperty(table + "__" + newColName, "STRING");
 		}
+		
 
-		// garbage collection for the temp table
+		// r temp variable clean up
 		if (tempTable != null) {
 			frame.executeRScript("rm(" + tempTable + ");");
 			frame.executeRScript("gc();");
@@ -94,7 +106,7 @@ public class AddColumnReactor extends AbstractRFrameReactor {
 		}
 		
 		NounMetadata retNoun = new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_HEADERS_CHANGE, PixelOperationType.FRAME_DATA_CHANGE);
-		retNoun.addAdditionalReturn(new AddHeaderNounMetadata(colName));
+		retNoun.addAdditionalReturn(new AddHeaderNounMetadata(newColName));
 		return retNoun;
 	}
 }
