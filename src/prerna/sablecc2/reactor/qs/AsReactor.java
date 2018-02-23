230bf8ba526deb9241a8ec7e7fbd1048e5e64271
mahkhalil <mahkhalil@deloitte.com>
2018-02-23 17:12:03 +0000

    UPDATE ::: Fixing the alias to never allow more than one "_" in the name since that causes issues with the assumption around table__column (table + 2 underscores + column)
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@26744 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/qs/AsReactor.java
+++ src/prerna/sablecc2/reactor/qs/AsReactor.java
@@ -15,10 +15,14 @@ public class AsReactor extends AbstractQueryStructReactor {
 		// the as name could come in as an array too
 		// for now I will go with the name
 		List<String> aliasInput = curRow.getAllColumns();
-		
 		if(this.parentReactor != null && aliasInput != null && !aliasInput.isEmpty()) {
-			// get the columns on as
-			parentReactor.setAs(aliasInput.toArray(new String[]{}));
+			// I need to make sure there are no __ since it causes issues
+			int size = aliasInput.size();
+			String[] aliasArray = new String[size];
+			for(int i = 0; i < size; i++) {
+				aliasArray[i] = aliasInput.get(i).replaceAll("_{2}", "_");
+			}
+			parentReactor.setAs(aliasArray);
 		}
 		
 		return qs;
