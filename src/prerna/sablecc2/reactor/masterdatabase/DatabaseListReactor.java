0dd7d5767b75d2ad23d74b91e56d1a22db8fea4e
mahkhalil <mahkhalil@deloitte.com>
2018-02-26 21:07:20 +0000

    UPDATE ::: Adding cost and type for apps (databases)
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@26773 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: cdec316e398ec129a6ded8c562b9b6d11fc87139

--- src/prerna/sablecc2/reactor/masterdatabase/DatabaseListReactor.java
+++ src/prerna/sablecc2/reactor/masterdatabase/DatabaseListReactor.java
@@ -1,19 +1,58 @@
 package prerna.sablecc2.reactor.masterdatabase;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
-import prerna.nameserver.utility.MasterDatabaseUtility;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.solr.SolrIndexEngine;
+import prerna.solr.SolrIndexEngineQueryBuilder;
 
 public class DatabaseListReactor extends AbstractReactor {
 
 	@Override
 	public NounMetadata execute() {
-		List<String> databaseList = MasterDatabaseUtility.getAllEnginesRDBMS();
-		return new NounMetadata(databaseList, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.DATABASE_LIST);
+		List<Map<String, String>> appList = new Vector<Map<String, String>>();
+		
+		// get the list of databases from the solr app
+		SolrIndexEngineQueryBuilder builder = new SolrIndexEngineQueryBuilder();
+		builder.addReturnFields("app_name");
+		builder.addReturnFields("app_type");
+		builder.addReturnFields("app_cost");
+		builder.setLimit(1000);
+		
+		SolrQuery q = builder.getSolrQuery();
+		SolrIndexEngine solrE;
+		try {
+			solrE = SolrIndexEngine.getInstance();
+			QueryResponse response = solrE.getQueryResponse(q, SolrIndexEngine.SOLR_PATHS.SOLR_APP_PATH_NAME);
+			SolrDocumentList results = response.getResults();
+			
+			for(SolrDocument doc : results) {
+				Map<String, String> appEntry = new HashMap<String, String>();
+				appEntry.put("name", doc.get("app_name") + "");
+				appEntry.put("type", doc.get("app_type") + "");
+				appEntry.put("cost", doc.get("app_cost") + "");
+				appList.add(appEntry);
+			}
+		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException | SolrServerException e) {
+			e.printStackTrace();
+		}
+
+		return new NounMetadata(appList, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.DATABASE_LIST);
 	}
 
 }
