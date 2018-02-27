c053e7160bdfe273101fd22adc1c8217377f565e
mahkhalil <mahkhalil@deloitte.com>
2018-02-27 21:31:24 +0000

    UPDATE ::: Adding more examples for open fda api
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@26836 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/engine/fda/drugs/DrugLabelsTest.java
+++ src/prerna/engine/fda/drugs/DrugLabelsTest.java
@@ -13,7 +13,7 @@ import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class AdverseEventsTest {
+public class DrugLabelsTest {
 
 	private static Gson gson = new GsonBuilder()
 			.disableHtmlEscaping()
@@ -31,62 +31,27 @@ public class AdverseEventsTest {
 		 * drugs which contains drugs taken while the event was happening
 		 * reactions is information about the reaction the patient experienced
 		 * 
+		 * 
 		 */
 		
 		CloseableHttpClient httpclient = HttpClients.createDefault();
-		String url = "https://api.fda.gov/drug/event.json";
+		String url = "https://api.fda.gov/drug/label.json";
 //		printResponse(httpclient, url,  "");
 		
 		// lets look at how we can narrow down what we want
 		url += "?limit=2?search=";
 
-		// filter values based on serious or not
-		// serious 1 = death or hospitalization / disability
-		// serious 2 = not 1
-		String component = "serious:1";
+		// brand name
+		String component = "brand_name:ADVIL";
 //		printResponse(httpclient, url,  component);
 		
-		// drug name
-		// the response is "HUGE"
-		component = "patient.drug.medicinalproduct:IBUPROFEN";
+		// substance name
+		// for advil, it is IBUPROFEN
+		component = "substance_name:IBUPROFEN";
 //		printResponse(httpclient, url,  component);
-
-		// drug characterization for the event
-		// 1 = suspect - cause
-		// 2 = concomitant - taken with suspect
-		// 3 = interacting - interacted with suspect
-		component = "patient.drug.drugcharacterization:1";
-//		printResponse(httpclient, url,  component);
-
-		// yay, i can combine this as well!!!
-		component = "patient.drug.medicinalproduct:IBUPROFEN+AND+patient.drug.drugcharacterization:2";
+		
+		component = "manufacturer_name:CVS";
 		printResponse(httpclient, url,  component);
-		
-		// where the event occurred
-		component = "occurcountry:US";
-//		printResponse(httpclient, url,  component);
-		
-		// can have the above be exact
-		component = "occurcountry.exact:US";
-//		printResponse(httpclient, url,  component);
-		
-		// patient sex
-		// 0 = unknown
-		// 1 = male
-		// 2 = female
-		component = "patient.patientsex:1";
-//		printResponse(httpclient, url,  component);
-		
-		// yay, i can combine this as well!!!
-		component = "occurcountry.exact:US&patient.patientsex:1";
-//		printResponse(httpclient, url,  component);
-		
-		// the way a drug is adminsitered 
-		// between 0 to 67
-		component = "patient.drug.drugadministrationroute:067";
-//		printResponse(httpclient, url,  component);
-		
-		
 	}
 
 	private static void printResponse(CloseableHttpClient httpclient, String baseUrl, String params) {
