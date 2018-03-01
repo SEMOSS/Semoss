052032c9b7a0f2174361ec737979277bedf9fca7
mahkhalil <mahkhalil@deloitte.com>
2018-03-01 22:53:20 +0000

    UPDATE ::: Set the new current frame for the insight
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@26903 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f18a63243f544e1d77ea7bc7832c014397034383

---
+++ src/prerna/sablecc2/reactor/frame/SetCurrentFrameReactor.java
@@ -0,0 +1,46 @@
+package prerna.sablecc2.reactor.frame;
+
+import java.util.List;
+
+import prerna.algorithm.api.ITableDataFrame;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class SetCurrentFrameReactor extends AbstractReactor {
+
+	public SetCurrentFrameReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.FRAME.getKey()};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		ITableDataFrame dm = getFrame();
+		if(dm == null) {
+			throw new IllegalArgumentException("Could not find frame to set");
+		}
+		// set the new main dm for the insight
+		this.insight.setDataMaker(dm);
+		NounMetadata noun = new NounMetadata(dm, PixelDataType.FRAME, PixelOperationType.FRAME);
+		return noun;
+	}
+	
+	private ITableDataFrame getFrame() {
+		// try specific key
+		GenRowStruct frameGrs = this.store.getNoun(keysToGet[1]);
+		if(frameGrs != null && !frameGrs.isEmpty()) {
+			return (ITableDataFrame) frameGrs.get(0);
+		}
+		
+		List<NounMetadata> frameCur = this.curRow.getNounsOfType(PixelDataType.FRAME);
+		if(frameCur != null && !frameCur.isEmpty()) {
+			return (ITableDataFrame) frameCur.get(0).getValue();
+		}
+		
+		return null;
+	}
+
+}
