5e99dd9ca0ed5f3e9caeb2f7a991432418f1ac7f
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-03-13 21:13:09 +0000

    UPDATE ::: Run rules from template or temporal
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27049 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/r/RunEditRulesReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/RunEditRulesReactor.java
@@ -1,10 +1,13 @@
 package prerna.sablecc2.reactor.frame.r;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.codehaus.jackson.map.ObjectMapper;
 
 import prerna.ds.r.RDataTable;
 import prerna.ds.r.RSyntaxHelper;
@@ -29,16 +32,57 @@ public class RunEditRulesReactor extends AbstractRFrameReactor {
 		Object[] nameCol = new Object[size];
 		Object[] descCol = new Object[size];
 		Object[] ruleCol = new Object[size];
-		for(int i = 0; i < mapList.size(); i++) {
+
+		HashMap<String, Object> editRulesTemplate = null;
+
+		// now that file is read in, we need to look through it to identify the
+		// appropriate edit rule based on the path
+		// look through the edit rules keyset to find the rule
+		for (int i = 0; i < mapList.size(); i++) {
 			Map<String, Object> mapOptions = (Map<String, Object>) mapList.get(i);
 			String name = (String) mapOptions.get("name");
 			nameCol[i] = name;
 			String description = (String) mapOptions.get("description");
-			descCol[i] = description;
 			String rule = (String) mapOptions.get("rule");
+			if (rule == null) {
+
+				// look up name in template
+				if (editRulesTemplate == null) {
+					// read in the edit rules file
+					String fileJsonPath = getBaseFolder() + "\\R\\EditRules\\editRulesTemplate.json";
+					String jsonString = "";
+					try {
+						jsonString = new String(Files.readAllBytes(Paths.get(fileJsonPath)));
+						editRulesTemplate = new ObjectMapper().readValue(jsonString, HashMap.class);
+					} catch (IOException e2) {
+						throw new IllegalArgumentException("Unable to read file from path: " + fileJsonPath);
+					}
+				}
+				HashMap<String, Object> ruleTemplate = (HashMap<String, Object>) editRulesTemplate.get(name);
+				rule = (String) ruleTemplate.get("rule");
+				HashMap<String, Object> columnTemplate = (HashMap<String, Object>) ruleTemplate.get("columns");
+				HashMap<String, Object> columnParms = (HashMap<String, Object>) mapOptions.get("columns");
+
+				// apply column params to rule
+				for (String key : columnTemplate.keySet()) {
+					String column = (String) columnParms.get(key);
+					if (column != null) {
+						// go forward with edit rules
+						// update the rule to use the appropriate column name
+						String targetString = "<" + key + ">";
+						rule = rule.replace(targetString, column);
+						System.out.println(rule + "");
+					}
+				}
+				// get description
+				description = (String) ruleTemplate.get("description");
+
+			}
 			ruleCol[i] = rule;
+			descCol[i] = description;
+
 		}
-		
+
 		// create rule frame with the following columns: name, description, rule
 		String issueFrameNameCol = "name";
 		rsb.append(issueFrameNameCol + "<-" + RSyntaxHelper.createStringRColVec(nameCol));
@@ -49,15 +93,13 @@ public class RunEditRulesReactor extends AbstractRFrameReactor {
 		String issueFrameRuleCol = "rule";
 		rsb.append(issueFrameRuleCol + "<-" + RSyntaxHelper.createStringRColVec(ruleCol));
 		rsb.append(";");
-		
-		rsb.append(issueFrame + "<- data.frame("+issueFrameNameCol+","+issueFrameDescriptionCol+","+issueFrameRuleCol +");");
-		System.out.println(rsb.toString());
-		
-		
+
+		rsb.append(issueFrame + "<- data.frame(" + issueFrameNameCol + "," + issueFrameDescriptionCol + ","
+				+ issueFrameRuleCol + ");");
 		rsb.append("library(editrules);");
 
 		// edit rules r script
-		String editRulesScriptFilePath = getBaseFolder() + "\\R\\EditRules\\editRules2.R";
+		String editRulesScriptFilePath = getBaseFolder() + "\\R\\EditRules\\editRules.R";
 		editRulesScriptFilePath = editRulesScriptFilePath.replace("\\", "/");
 		rsb.append("source(\"" + editRulesScriptFilePath + "\");");
 
