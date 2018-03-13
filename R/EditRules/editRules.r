2c87d06fe9ee21aefe3c4875106bfacdbef38ee0
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-03-13 17:02:16 +0000

    UPDATE ::: Run edit rules creating data quality frame and template changes
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27047 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 587b63620d18ad97f55bd0e88ab323a137a0355e

--- R/EditRules/editRules.r
+++ R/EditRules/editRules.r
@@ -1,23 +1,64 @@
-editRules <- function(df, E) {
-# get constraints in frame
-editFrame <- as.data.frame(E)
-editNames <- editFrame$name
+getDqFrame <- function(df, issueFrame) {
+	library(editrules)
+	originalRules <- issueFrame$rule
+	ruleDF <- data.frame(originalRules)
+	ruleDF$translatedRules <- ""
 
-# check violations
-ve <- violatedEdits(E, df)
+	# figure out translated rule from E object
+	for(i in 1:nrow(ruleDF)) {
+	  rule <- ruleDF$originalRules[i]
+	  ruleEScript <- paste("E<- editset(expression(", rule, "))", sep="") 
 
-# get subset of df that violates the conditions
-veFrame <- as.data.frame(ve)
-subsetScript <- 'subset <- veFrame[which('
-subsetScriptCondition <- ""
-for(i in 1:nrow(editFrame)) {
-	col <- editNames[i]
-	subsetScriptCondition <- paste(subsetScriptCondition, "veFrame$", col, "== TRUE ", sep="") 
-	if(i < nrow(editFrame)) {
-	subsetScriptCondition <- paste(subsetScriptCondition, " | ")
+	  tryCatch({
+	    eval(parse(text=ruleEScript))
+		  
+		if(exists('E')) {
+			editFrame <- as.data.frame(E)
+			translatedRule <- editFrame$edit
+			ruleDF$translatedRules[i] <- translatedRule
+	    }
+	  }, error = function(e) {
+	  })
+	
+	  rm(E, editFrame, translatedRule, ruleEScript)
 	}
-}
-subsetScript <- paste(subsetScript, subsetScriptCondition, "),]", sep="")
-eval(parse(text=subsetScript))
-return (subset)
+
+	# now create E with valid rules 
+	ruleDF <- ruleDF[which(ruleDF$translatedRules != ""),]
+	originalRules <- as.vector(ruleDF$originalRules)
+	E <- editset(originalRules)
+
+	# get constraints in frame
+	editFrame <- as.data.frame(E)
+	editNames <- editFrame$name
+
+	# check violations
+	ve <- violatedEdits(E, df)
+	veFrame <- as.data.frame(ve)
+
+	# add results to data quality frame
+	dqFrame <- data.frame(originalRules)
+	dqFrame$validCount <- ""
+	dqFrame$invalidCount <- ""
+	dqFrame$totalCount <- ""
+	for(i in 1:nrow(editFrame)) {
+		col <- editNames[i]
+		# get valid count syntax looks like this: validCount <- sum(veFrame$col == FALSE )
+		validCountScript <- paste("validCount <- sum(veFrame$", col, "== FALSE)", sep="") 
+		eval(parse(text=validCountScript))
+		dqFrame$validCount[i] <- validCount
+		
+		# get invalid count syntax looks like this: invalidCount <- sum(veFrame$col == TRUE )
+		invalidCountScript <- paste("invalidCount <- sum(veFrame$", col, "== TRUE)", sep="") 
+		eval(parse(text=invalidCountScript))
+		dqFrame$invalidCount[i] <- invalidCount
+
+		# get Total count
+		totalCount <- validCount + invalidCount
+		dqFrame$totalCount <- totalCount
+	}
+	
+	# join issueFrame name description columns with originalRules
+	joinDf <- merge (dqFrame, issueFrame, by.x = c("originalRules") , by.y = c("rule"), all.y = TRUE)
+	return(joinDf)
 }
