7651235b419b169f979359c02cf8ceda0861a149
mahkhalil <mahkhalil@deloitte.com>
2018-03-14 19:37:26 +0000

    UPDATE ::: Adding basic sheel for QueryUtil (AnsiSqlQueryUtil) so that we do not need to build out more QueryUtils to connect to new databases when the functionality of the QueryUtil is not used anymore (note - it was used so we could insert data directly into mariadb / other rdmbs types aside from h2) UPDATE ::: Reordering variables in flat uploaders for clarity UPDATE ::: Breaking out functionality of RdbmsConnectionHelper into separate methods
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27066 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/util/sql/AnsiSqlQueryUtil.java
@@ -0,0 +1,88 @@
+package prerna.util.sql;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import prerna.engine.impl.rdbms.RdbmsConnectionHelper;
+
+public class AnsiSqlQueryUtil extends SQLQueryUtil {
+
+	private DB_TYPE dbType = null;
+	private String hostname;
+	private String port;
+	private String schema;
+	private String username;
+	private String password;
+	private String connectionUrl;
+	
+	public AnsiSqlQueryUtil() {
+		
+	}
+	
+	public AnsiSqlQueryUtil(DB_TYPE dbType, String hostname, String port, String schema, String username, String password) {
+		this.dbType = dbType;
+		this.hostname = hostname;
+		this.port = port;
+		this.schema = schema;
+		this.username = username;
+		this.password = password;
+		try {
+			this.connectionUrl = RdbmsConnectionHelper.getConnectionUrl(dbType.name(), hostname, port, schema, "");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setDbType(DB_TYPE dbType) {
+		this.dbType = dbType;
+	}
+	
+	@Override
+	public DB_TYPE getDatabaseType() {
+		return this.dbType;
+	}
+
+	@Override
+	public String getConnectionURL(String baseFolder, String dbname) {
+		return this.connectionUrl;
+	}
+	
+	public String getDefaultDBUserName(){
+		return this.username;
+	}
+
+	public String getDefaultDBPassword(){
+		return this.password;
+	}
+
+	@Override
+	public String getDatabaseDriverClassName() {
+		return RdbmsConnectionHelper.getDriver(this.dbType.name());
+	}
+	
+	@Override
+	public String getDefaultOuterJoins() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDialectIndexInfo(String indexName, String dbName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDialectFullOuterJoinQuery(boolean distinct, String selectors, List<String> rightJoinsArr,
+			List<String> leftJoinsArr, List<String> joinsArr, String filters, int limit, String groupBy) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getEngineNameFromConnectionURL(String connectionURL) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
