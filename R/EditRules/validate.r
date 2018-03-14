dea6547fab35a8085445d8b0ffe8eb9853b82db3
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-03-14 20:45:30 +0000

    UPDATE ::: Edit Rules get RuleFrame and IssueFrame with rule meta
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27072 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8add92f9325bb2d24859de3c130fbae3dbe507e0

--- R/EditRules/validate.r
+++ R/EditRules/validate.r
@@ -1,13 +1,11 @@
-getDqFrame <- function(df, issueFrame) {
+createCF <- function (df, issueFrame) {
 	library(validate)
 	originalRules <- issueFrame$rule
 	issueFrame$translatedRules <- ""
-
-	# figure out translated rule from E object
+	# figure out translated rule from V object
 	for(i in 1:nrow(issueFrame)) {
 	  rule <- issueFrame$rule[i]
 	  ruleEScript <- paste("V<- validator(",rule,")", sep="") 
-	  print(ruleEScript)
 	  tryCatch({
 	    eval(parse(text=ruleEScript))
 		if(exists('V')) {
@@ -15,10 +13,7 @@ getDqFrame <- function(df, issueFrame) {
 			translatedRule <- editFrame$rule
 			issueFrame$translatedRules[i] <- translatedRule
 	    }
-	  }, error = function(e) {
-	  
-	  })
-	
+	  }, error = function(e) {})
 	  rm(V, editFrame, translatedRule, ruleEScript)
 	}
 
@@ -29,11 +24,80 @@ getDqFrame <- function(df, issueFrame) {
 
 	# check violations
 	cf <- confront(df, v)
-	veFrame <- summary(cf)
 	
+	# add rule alias to issueFrame
+	vFrame <- as.data.frame(v)
+	vFrame <- subset(vFrame, select=-c(label, origin, description, created))
+	colnames(vFrame) <- c("translatedRules", "ruleAlias")
+	issueFrame <- merge (vFrame, issueFrame, by.x = c("translatedRules") , by.y = c("translatedRules"), all.y = TRUE)
+
+	return (list(cf=cf, issueFrame=issueFrame))
+}
+
+getDqFrame <- function(cf, issueFrame) {
+	# check violations
+	summaryFrame <- summary(cf)
+	summaryFrame <- subset(summaryFrame, select=-c(name, warning))
 	# join issueFrame name description columns with originalRules
-	joinDf <- merge (veFrame, issueFrame, by.x = c("expression") , by.y = c("translatedRules"), all.y = TRUE)
-	joinDf <- subset(joinDf, select=-c(name.x, expression, warning))
-	colnames(joinDf) <- c("Count", "Valid_Count", "Invalid_Count", "Number_of_NAs", "Rule_Error", "Rule_Name", "Description", "Rule")
+	joinDf <- merge (summaryFrame, issueFrame, by.x = c("expression") , by.y = c("translatedRules"), all.y = TRUE)
+	joinDf <- subset(joinDf, select=-c(expression, ruleAlias))
+	colnames(joinDf) <- c("Total_Count", "Valid_Count", "Invalid_Count", "Number_of_NAs", "Rule_Error", "Rule_Name", "Description", "Rule")
 	return(joinDf)
 }
+
+getErrorIndex <- function(cf, issueFrame) {
+	values <- values(cf)
+	vdat <- as.data.frame(values)
+	size <- nrow(vdat)
+	errorFrameColSize <- ncol(vdat)
+	errorFrame <- data.frame(matrix(NA, nrow=nrow(vdat), ncol=errorFrameColSize))
+	for(i in colnames(vdat)) {
+		errorIndexScript <- paste("indicesPerRule <- which(vdat$",i,"== FALSE)", sep="")
+		eval(parse(text=errorIndexScript))
+		#indicesPerRule <- which(vdat$V2 == FALSE)
+		ruleRow <- issueFrame[which(issueFrame$ruleAlias == i),]
+		ruleName <- as.character(ruleRow$name)
+
+		#adding error indicies for rule name
+		indexCol <- as.vector(indicesPerRule)
+		length(indexCol) <- size
+		errorFrame <- cbind(errorFrame, indexCol)
+		# rename col to rule name
+		colnames(errorFrame)[colnames(errorFrame)=="indexCol"] <- ruleName
+	}
+	
+	# retrieves indices where all validations = false 
+	# only for more than 1 rule
+	if(errorFrameColSize > 1) {
+		indicesAllRules <- which(apply(values, 1, function(x) all(x == FALSE))) 
+		length(indicesAllRules) <- size
+
+		errorFrame$all <- indicesAllRules
+		colnames(errorFrame)[colnames(errorFrame)=="all"] <- "All_Rules"
+
+	}
+	errorFrame <- subset(errorFrame, select=-c(1:errorFrameColSize))	
+
+	return (errorFrame)
+}
+
+getErrorFrame <- function(cf, issueFrame) {
+	values <- values(cf)
+	vdat <- as.data.frame(values)
+	errorFrameColSize <- ncol(vdat)
+	for(i in colnames(vdat)) {
+		ruleRow <- issueFrame[which(issueFrame$ruleAlias == i),]
+		ruleName <- as.character(ruleRow$name)
+		colnames(vdat)[colnames(vdat)==i] <- ruleName
+	}
+	
+	# retrieves indices where all validations = false only for more than 1 rule
+	if(errorFrameColSize > 1) {
+		allRules <- apply(values, 1, function(x) all(x == FALSE))
+		vdat$all <- allRules
+		colnames(vdat)[colnames(vdat)=="all"] <- "All_Rules"
+	}
+
+	return (vdat)
+}
+
