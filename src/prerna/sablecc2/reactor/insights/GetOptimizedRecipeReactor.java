ec9ee987f80ad74e3068623106c9ba23e6d56cf3
micstone <micstone@deloitte.com>
2018-03-14 17:37:21 +0000

    UPDATE :: Adding functionality for GetOptimizedRecipe
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27059 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/sablecc2/reactor/insights/GetOptimizedRecipeReactor.java
@@ -0,0 +1,68 @@
+package prerna.sablecc2.reactor.insights;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PushbackReader;
+import java.util.HashMap;
+import java.util.List;
+import prerna.sablecc2.OptimizeRecipeTranslation;
+import prerna.sablecc2.PixelPreProcessor;
+import prerna.sablecc2.lexer.Lexer;
+import prerna.sablecc2.lexer.LexerException;
+import prerna.sablecc2.node.Start;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.parser.Parser;
+import prerna.sablecc2.parser.ParserException;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class GetOptimizedRecipeReactor extends AbstractReactor {
+	
+	/**
+	 * This reactor creates an optimized recipe - it removes from the recipe instances of pixels with TaskOptions that are not needed
+	 * No inputs are used
+	 */
+	
+	@Override
+	public NounMetadata execute() {
+		// grab the full recipe
+		List<String> pixelRecipe = this.insight.getPixelRecipe();
+		// return the modified recipe
+		return new NounMetadata(getModifiedRecipe(pixelRecipe), PixelDataType.CONST_STRING, PixelOperationType.CURRENT_INSIGHT_RECIPE);
+	}
+	
+	/**
+	 * This method is used to get the modified recipe based on the full recipe
+	 * 
+	 * @param recipe
+	 * @return modifiedRecipe
+	 */
+	public static String getModifiedRecipe(List<String> recipe) {
+		// create a new translation object that will do the work of figuring out which pixel expressions to keep
+		OptimizeRecipeTranslation translation = new OptimizeRecipeTranslation();
+		// we want to iterate through the current recipe
+		for (int i = 0; i < recipe.size(); i++) {
+			String expression = recipe.get(i);
+			// fill in the encodedToOriginal with map for the current expression
+			expression = PixelPreProcessor.preProcessPixel(expression.trim(), translation.encodedToOriginal);
+			try {
+				Parser p = new Parser(new Lexer(new PushbackReader(new InputStreamReader(new ByteArrayInputStream(expression.getBytes("UTF-8"))), expression.length())));
+				// parsing the pixel - this process also determines if expression is syntactically correct
+				Start tree = p.parse();
+				// apply the translation
+				// when we apply the translation, we will change encoded expressions back to their original form
+				tree.apply(translation);
+				// reset translation.encodedToOriginal for each expression
+				translation.encodedToOriginal = new HashMap<String, String>();
+			} catch (ParserException | LexerException | IOException e) {
+				e.printStackTrace();
+			}
+		}
+		// we want to run the finalizeExpressionsToKeep method only after all expressions have been run
+		// this way we can find the last expression index used 
+		translation.finalizeExpressionsToKeep();
+		return translation.getModifiedRecipe();
+	}
+}
