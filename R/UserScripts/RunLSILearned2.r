ad126b0f171f8db9417c79a748c89bfbde2a311c
kunalppatel9 <kunalppatel9@deloitte.com>
2018-03-16 19:03:23 +0000

    UPDATE ::: Edited Script to run faster based on better handling of multiple data frames and ordering
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27133 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- R/UserScripts/RunLSILearned2.r
+++ R/UserScripts/RunLSILearned2.r
@@ -1,22 +1,16 @@
 #for loop over the length of  Description
-library(data.table)
 DescriptionLength<-length( Description[[1]])
-Match=0;
-LSA_Score=0;
-LSA_Category=0;
-joinDescription=0;
+dfFinal=data.frame()
 for(i in 1: DescriptionLength){
-temp<-lsi_lookup(toString( Description[[i,1]]),lookup_tbl,1,LSAspace)
+temp<-data.frame(lsi_lookup(toString( Description[i,1]),lookup_tbl,.6,LSAspace))
 if(length(temp)!=0){
-tempDescription<-list(rep(toString( Description[[i,1]]),length(temp[[1]])))
-Match<-append( Match,temp[[1]])
-LSA_Category<-append(LSA_Category,temp[[2]])
-LSA_Score<-append(LSA_Score,temp[[3]])
-joinDescription<-append(joinDescription,tempDescription[[1]])
+colnames(temp) <- c("Match","LSA_Category","LSA_Score")
+temp2<-data.frame(head(temp[order(temp$LSA_Score,decreasing=TRUE),],numMatch))
+colnames(temp2) <- c("Match","LSA_Category","LSA_Score")
+tempDescription<-list(rep(toString( Description[i,1]),numMatch))
+temp3<-data.frame(c(temp2,Description=tempDescription))
+dfFinal<-rbind(dfFinal,temp3)
 }
 }
-df<-data.frame(joinDescription, LSA_Category,LSA_Score, Match);
-df<-df[-c(1),]
-
-sortdf<-df[with(df, order( joinDescription, -LSA_Score)), ]
+rm(temp,temp2,temp3,tempDescription)
 
