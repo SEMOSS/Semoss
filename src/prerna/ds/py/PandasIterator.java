a3765d0be2a810535772ca6da6c0559b3ac8b9c6
mahkhalil <mahkhalil@deloitte.com>
2018-03-19 21:28:01 +0000

    UPDATE ::: Adding basic code + placeholder for python execution
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27167 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/ds/py/PandasIterator.java
@@ -0,0 +1,25 @@
+package prerna.ds.py;
+
+import java.util.Iterator;
+
+import prerna.engine.api.IHeadersDataRow;
+
+public class PandasIterator implements Iterator<IHeadersDataRow>{
+
+	public PandasIterator() {
+		
+	}
+	
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public IHeadersDataRow next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
