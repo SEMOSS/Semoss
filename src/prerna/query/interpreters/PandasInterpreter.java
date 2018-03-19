50b6dcbb6b65a139f27d6c4996b0f27d574d2dd8
mahkhalil <mahkhalil@deloitte.com>
2018-03-19 21:28:01 +0000

    UPDATE ::: Adding basic code + placeholder for python execution
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27167 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e834154764c3a7dae76c70ea230399e60cdaec24

--- src/prerna/query/interpreters/PandasInterpreter.java
+++ src/prerna/query/interpreters/PandasInterpreter.java
@@ -1,20 +1,60 @@
 package prerna.query.interpreters;
 
+import java.util.List;
 import java.util.Map;
 
 import prerna.algorithm.api.SemossDataType;
+import prerna.query.querystruct.selectors.IQuerySelector;
+import prerna.query.querystruct.selectors.QueryColumnSelector;
 
 public class PandasInterpreter extends AbstractQueryInterpreter {
 
 	private String dataTableName = null;
 	private Map<String, SemossDataType> colDataTypes;
 	
+	private StringBuilder selectorCriteria;
+	
 	@Override
 	public String composeQuery() {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder query = new StringBuilder();
+		query.append(this.dataTableName)
+			.append("[")
+			.append(this.selectorCriteria.toString())
+			.append("]");
+		
+		return query.toString();
 	}
 
+	public void addSelectors() {
+		this.selectorCriteria = new StringBuilder();
+		List<IQuerySelector> selectors = qs.getSelectors();
+		int size = selectors.size();
+		for(int i = 0; i < size; i++) {
+			IQuerySelector selector = selectors.get(i);
+			if(i == 0) {
+				this.selectorCriteria.append(processSelector(selector));
+			} else {
+				this.selectorCriteria.append(", ").append(processSelector(selector));
+			}
+		}
+	}
+	
+	private String processSelector(IQuerySelector selector) {
+		if(selector.getSelectorType() == IQuerySelector.SELECTOR_TYPE.COLUMN) {
+			return processColumnSelector( (QueryColumnSelector) selector);
+		} else {
+			return null;
+		}
+	}
+	
+	private String processColumnSelector(QueryColumnSelector selector) {
+		String columnName = selector.getColumn();
+		String alias = selector.getAlias();
+		
+		// just return the column name
+		return "'" + columnName + "'";
+	}
+	
 	public void setDataTableName(String dataTableName) {
 		this.dataTableName = dataTableName;
 	}
