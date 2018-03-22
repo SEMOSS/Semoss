47b0ef2c565b8f242b9e72f881d3ccefae18cbfc
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-03-22 22:01:30 +0000

    UPDATE ::: Renamed edit rules to rules
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27284 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/r/rules/AddRuleReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/rules/AddRuleReactor.java
@@ -1,4 +1,4 @@
-package prerna.sablecc2.reactor.frame.r;
+package prerna.sablecc2.reactor.frame.r.rules;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,28 +10,32 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
 import org.codehaus.jackson.map.ObjectMapper;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import prerna.ds.r.RSyntaxHelper;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.frame.r.AbstractRFrameReactor;
 import prerna.util.Utility;
-import org.json.JSONObject;
 
-public class AddEditRuleReactor extends AbstractRFrameReactor {
+public class AddRuleReactor extends AbstractRFrameReactor {
 
 	/**
-	 * This reactor runs the adds edit rules to the validateRulesTemplate.json
+	 * This reactor runs the adds rules to the validateRulesTemplate.json
 	 * The reactor takes as an input a map: 
 	 * AddEditRule([{"testRule":{"rule":"<encode>grepl(\"^-\\\\d+$\", <x>) 
 	 * 	== TRUE</encode>","columns":{"x":{"description":"the description for x","columnType":"STRING"}},
 	 *  "description":"my description"}}]);
 	 */
 	
-	public AddEditRuleReactor() {
+	public AddRuleReactor() {
 		this.keysToGet = new String[] {ReactorKeysEnum.RULES_MAP.getKey()};
-	}
+	}	
 	
 	@Override
 	public NounMetadata execute() {
@@ -52,25 +56,31 @@ public class AddEditRuleReactor extends AbstractRFrameReactor {
 		// get input to the map
 		HashMap<String, Object> mapOptionsEntry = (HashMap<String, Object>) mapOptions.get(name);
 		// get the rule
-		String rule = (String) mapOptionsEntry.get("rule");
-		rule = Utility.decodeURIComponent(rule);
+		Object rule = (String) mapOptionsEntry.get("rule");
+		rule = Utility.decodeURIComponent((String) rule);
 		mapOptionsEntry.replace("rule", rule);
-
+		
 		// create string builder for the r script
 		StringBuilder rsb = new StringBuilder();
 
-		// library validate
-		rsb.append("library(validate);");
-
 		// before we add to the json, we need to ensure that the rule is valid
-		// in order to validate, we will need to first replace the <variableNames> with just the variableNames (remove the brackets)
-		//first we have to get the columns from the column map
-		String testRule = rule;
+		// in order to validate testRule, we will need to (1) determine if rule is a regex and if so add 1 
+		// additional escape character, where applicable, to make the regex R-syntax acceptable 
+		// and (2) replace the <variableNames> with just the 
+		// variableNames (remove the brackets)
+		String testRule = (String) rule;
+		//(1) if applicable, adding escape character(s) to regex 		
+		testRule = RSyntaxHelper.escapeRegexR(testRule);
+
+		//(2) get column name from columnMap
 		Map<String, Object> columnMap = (Map<String, Object>) mapOptionsEntry.get("columns");
 		for (String column : columnMap.keySet()) {
 			// the unbracketed rule will be the test rule
 			testRule = testRule.replaceAll("<" + column + ">", column);
 		}
+		
+		String testRuleVar = "testRule" + Utility.getRandomString(8);
+		rsb.append(testRuleVar + "<-\"" + testRule + "\";");
 		
 		// source the r function that will verify the rules validity
 		String validateRuleScriptFilePath = getBaseFolder() + "\\R\\EditRules\\validateRule.R";
@@ -82,9 +92,17 @@ public class AddEditRuleReactor extends AbstractRFrameReactor {
 		this.rJavaTranslator.runR(rsbString);
         // script for running the function
 		// when we validate, use the testRule
-		String rScript = "as.character(validateRule(\"" + testRule + "\"))";
+		String rScript = "as.character(validateRule(" + testRuleVar + "));";
+
 		// isValid will either be true or false and indicate if the rule is valid based on editrules in r
 		String isValid = this.rJavaTranslator.getString(rScript);
+		
+		// rm function names
+		StringBuilder cleanUpScript = new StringBuilder();
+		cleanUpScript.append("rm(" + testRuleVar + ");");
+		cleanUpScript.append("rm(validateRule, escapeRegexR);");
+		cleanUpScript.append("gc();");
+		this.rJavaTranslator.runR(cleanUpScript.toString());
 
 		// only go forward with updating the json if the rule is valid
 		if (isValid.equalsIgnoreCase("true")) {
@@ -93,41 +111,44 @@ public class AddEditRuleReactor extends AbstractRFrameReactor {
 			String fileJsonPath = getBaseFolder() + "\\R\\EditRules\\validateRulesTemplate.json";
 			String jsonString = "";
 
-			HashMap<String, Object> editRulesTemplate = null;
+			HashMap<String, Object> validateRulesTemplate = null;
 			try {
 				jsonString = new String(Files.readAllBytes(Paths.get(fileJsonPath)));
-				editRulesTemplate = new ObjectMapper().readValue(jsonString, HashMap.class);
+				validateRulesTemplate = new ObjectMapper().readValue(jsonString, HashMap.class);
 			} catch (IOException e2) {
 				throw new IllegalArgumentException("Unable to read file from path: " + fileJsonPath);
 			}
 			
 			// before adding, we should see if the name already exists in error rules template
 			// if it does exist, we should throw an error and tell the user
-			if (editRulesTemplate.keySet().contains(name)) {
+			if (validateRulesTemplate.keySet().contains(name)) {
 				throw new IllegalArgumentException("The name of the rule already exists. Please use a different name for the rule.");
 			}
 
 			// we need to add our new map the the editRulesTemplate
-			editRulesTemplate.put(name, mapOptionsEntry);
-
+			validateRulesTemplate.put(name, mapOptionsEntry);
+			
 			// create the json string based on the map
 			// use the GsonBuilder for proper formatting (and to avoid encoding) of the json
 			GsonBuilder builder = new GsonBuilder();
 			builder.disableHtmlEscaping();
 			builder.setPrettyPrinting();
 			Gson gson = builder.create();
-			String json = gson.toJson(editRulesTemplate);
+			String json = gson.toJson(validateRulesTemplate);
 
             // override the file with the new updated json
 			PrintWriter pw;
 			try {
 				pw = new PrintWriter(new File(fileJsonPath));
-				pw.write(json);
+				pw.write(json.toString());
 				pw.close();
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
+		} else {
+			throw new IllegalArgumentException("Invalid rule addition");
 		}
 		return null;
 	}
+	
 }
