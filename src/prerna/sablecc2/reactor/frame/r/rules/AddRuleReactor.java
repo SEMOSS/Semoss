7003a57e3174b7bb36d4a60cf7c228ed60cfefa3
micstone <micstone@deloitte.com>
2018-03-23 18:28:21 +0000

    UPDATE :: Adding ability to add more than one rule at a time
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27299 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 5bb7b48a484fe9698d8259d290ee436c00c6bfaa

--- src/prerna/sablecc2/reactor/frame/r/rules/AddRuleReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/rules/AddRuleReactor.java
@@ -34,121 +34,126 @@ public class AddRuleReactor extends AbstractRFrameReactor {
 	 */
 	
 	public AddRuleReactor() {
-		this.keysToGet = new String[] {ReactorKeysEnum.RULES_MAP.getKey()};
-	}	
-	
+		this.keysToGet = new String[] { ReactorKeysEnum.RULES_MAP.getKey() };
+	}
+
 	@Override
 	public NounMetadata execute() {
 		// initialize the rJava translator
 		init();
 		// grab the user input and get into map format
 		List<Object> ruleList = this.curRow.getValuesOfType(PixelDataType.MAP);
-		Map<String, Object> mapOptions = (Map<String, Object>) ruleList.get(0);
 
-		// get the name of the rule
-		// it is the value in the key set
-		String name = "";
-		Set<String> names = mapOptions.keySet();
-		for (String item : names) {
-			name = item;
+		// read the json. this is the file that we will be adding the rules to
+		String fileJsonPath = getBaseFolder() + "\\R\\EditRules\\validateRulesTemplate.json";
+		String jsonString = "";
+
+		HashMap<String, Object> validateRulesTemplate = null;
+		try {
+			jsonString = new String(Files.readAllBytes(Paths.get(fileJsonPath)));
+			validateRulesTemplate = new ObjectMapper().readValue(jsonString, HashMap.class);
+		} catch (IOException e2) {
+			throw new IllegalArgumentException("Unable to read file from path: " + fileJsonPath);
 		}
 
-		// get input to the map
-		HashMap<String, Object> mapOptionsEntry = (HashMap<String, Object>) mapOptions.get(name);
-		// get the rule
-		Object rule = (String) mapOptionsEntry.get("rule");
-		rule = Utility.decodeURIComponent((String) rule);
-		mapOptionsEntry.replace("rule", rule);
-		
-		// create string builder for the r script
-		StringBuilder rsb = new StringBuilder();
+		// iterate through the rule list to get each item that we want to add
+		for (int i = 0; i < ruleList.size(); i++) {
+			Map<String, Object> mapOptions = (Map<String, Object>) ruleList.get(i);
 
-		// before we add to the json, we need to ensure that the rule is valid
-		// in order to validate testRule, we will need to (1) determine if rule is a regex and if so add 1 
-		// additional escape character, where applicable, to make the regex R-syntax acceptable 
-		// and (2) replace the <variableNames> with just the 
-		// variableNames (remove the brackets)
-		String testRule = (String) rule;
-		//(1) if applicable, adding escape character(s) to regex 		
-		testRule = RSyntaxHelper.escapeRegexR(testRule);
+			// get the name of the rule
+			// it is the value in the key set
+			String name = "";
+			Set<String> names = mapOptions.keySet();
+			for (String item : names) {
+				name = item;
+			}
 
-		//(2) get column name from columnMap
-		Map<String, Object> columnMap = (Map<String, Object>) mapOptionsEntry.get("columns");
-		for (String column : columnMap.keySet()) {
-			// the unbracketed rule will be the test rule
-			testRule = testRule.replaceAll("<" + column + ">", column);
+			// get input to the map
+			HashMap<String, Object> mapOptionsEntry = (HashMap<String, Object>) mapOptions.get(name);
+			// get the rule
+			Object rule = (String) mapOptionsEntry.get("rule");
+			rule = Utility.decodeURIComponent((String) rule);
+			mapOptionsEntry.replace("rule", rule);
+
+			// create string builder for the r script
+			StringBuilder rsb = new StringBuilder();
+
+			// before we add to the json, we need to ensure that the rule is valid
+			// in order to validate testRule, we will need to 
+			// (1) determine if rule is a regex and if so add 1 additional escape character, where applicable, to make the regex R-syntax acceptable
+			// (2) replace the <variableNames> with just the variableNames (remove the brackets)
+			String testRule = (String) rule;
+			// (1) if applicable, adding escape character(s) to regex
+			testRule = RSyntaxHelper.escapeRegexR(testRule);
+
+			// (2) get column name from columnMap
+			Map<String, Object> columnMap = (Map<String, Object>) mapOptionsEntry.get("columns");
+			for (String column : columnMap.keySet()) {
+				// the unbracketed rule will be the test rule
+				testRule = testRule.replaceAll("<" + column + ">", column);
+			}
+
+			String testRuleVar = "testRule" + Utility.getRandomString(8);
+			rsb.append(testRuleVar + "<-\"" + testRule + "\";");
+
+			// source the r function that will verify the rules validity
+			String validateRuleScriptFilePath = getBaseFolder() + "\\R\\EditRules\\validateRule.R";
+			validateRuleScriptFilePath = validateRuleScriptFilePath.replace("\\", "/");
+			rsb.append("source(\"" + validateRuleScriptFilePath + "\");");
+
+			// call the function in the r script and get the return value
+			String rsbString = rsb.toString();
+			this.rJavaTranslator.runR(rsbString);
+			// script for running the function
+			// when we validate, use the testRule
+			String rScript = "as.character(validateRule(" + testRuleVar + "));";
+
+			// isValid will either be true or false and indicate if the rule is
+			// valid based on editrules in r
+			String isValid = this.rJavaTranslator.getString(rScript);
+
+			// rm function names
+			StringBuilder cleanUpScript = new StringBuilder();
+			cleanUpScript.append("rm(" + testRuleVar + ");");
+			cleanUpScript.append("rm(validateRule, escapeRegexR);");
+			cleanUpScript.append("gc();");
+			this.rJavaTranslator.runR(cleanUpScript.toString());
+
+			// only go forward with updating the json if the rule is valid
+			if (isValid.equalsIgnoreCase("true")) {
+				// if the rule is valid, then we add to the json
+
+				// before adding, we should see if the name already exists in error rules template
+				// if it does exist, we should throw an error and tell the user
+				if (validateRulesTemplate.keySet().contains(name)) {
+					throw new IllegalArgumentException(
+							"The name of the rule already exists. Please use a different name for the rule.");
+				}
+
+				// we need to add our new map the the editRulesTemplate
+				validateRulesTemplate.put(name, mapOptionsEntry);
+
+			} else {
+				throw new IllegalArgumentException("Invalid rule addition");
+			}
 		}
-		
-		String testRuleVar = "testRule" + Utility.getRandomString(8);
-		rsb.append(testRuleVar + "<-\"" + testRule + "\";");
-		
-		// source the r function that will verify the rules validity
-		String validateRuleScriptFilePath = getBaseFolder() + "\\R\\EditRules\\validateRule.R";
-		validateRuleScriptFilePath = validateRuleScriptFilePath.replace("\\", "/");
-		rsb.append("source(\"" + validateRuleScriptFilePath + "\");");
+		// create the json string based on the map
+		// use the GsonBuilder for proper formatting of the json
+		GsonBuilder builder = new GsonBuilder();
+		builder.disableHtmlEscaping();
+		builder.setPrettyPrinting();
+		Gson gson = builder.create();
+		String json = gson.toJson(validateRulesTemplate);
 
-		// call the function in the r script and get the return value
-		String rsbString = rsb.toString();
-		this.rJavaTranslator.runR(rsbString);
-        // script for running the function
-		// when we validate, use the testRule
-		String rScript = "as.character(validateRule(" + testRuleVar + "));";
-
-		// isValid will either be true or false and indicate if the rule is valid based on editrules in r
-		String isValid = this.rJavaTranslator.getString(rScript);
-		
-		// rm function names
-		StringBuilder cleanUpScript = new StringBuilder();
-		cleanUpScript.append("rm(" + testRuleVar + ");");
-		cleanUpScript.append("rm(validateRule, escapeRegexR);");
-		cleanUpScript.append("gc();");
-		this.rJavaTranslator.runR(cleanUpScript.toString());
-
-		// only go forward with updating the json if the rule is valid
-		if (isValid.equalsIgnoreCase("true")) {
-			// if the rule is valid, then we add to the json
-			// first read the existing json so that we can append
-			String fileJsonPath = getBaseFolder() + "\\R\\EditRules\\validateRulesTemplate.json";
-			String jsonString = "";
-
-			HashMap<String, Object> validateRulesTemplate = null;
-			try {
-				jsonString = new String(Files.readAllBytes(Paths.get(fileJsonPath)));
-				validateRulesTemplate = new ObjectMapper().readValue(jsonString, HashMap.class);
-			} catch (IOException e2) {
-				throw new IllegalArgumentException("Unable to read file from path: " + fileJsonPath);
-			}
-			
-			// before adding, we should see if the name already exists in error rules template
-			// if it does exist, we should throw an error and tell the user
-			if (validateRulesTemplate.keySet().contains(name)) {
-				throw new IllegalArgumentException("The name of the rule already exists. Please use a different name for the rule.");
-			}
-
-			// we need to add our new map the the editRulesTemplate
-			validateRulesTemplate.put(name, mapOptionsEntry);
-			
-			// create the json string based on the map
-			// use the GsonBuilder for proper formatting (and to avoid encoding) of the json
-			GsonBuilder builder = new GsonBuilder();
-			builder.disableHtmlEscaping();
-			builder.setPrettyPrinting();
-			Gson gson = builder.create();
-			String json = gson.toJson(validateRulesTemplate);
-
-            // override the file with the new updated json
-			PrintWriter pw;
-			try {
-				pw = new PrintWriter(new File(fileJsonPath));
-				pw.write(json.toString());
-				pw.close();
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-		} else {
-			throw new IllegalArgumentException("Invalid rule addition");
+		// override the file with the new updated json
+		PrintWriter pw;
+		try {
+			pw = new PrintWriter(new File(fileJsonPath));
+			pw.write(json.toString());
+			pw.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
 		return null;
 	}
-	
 }
