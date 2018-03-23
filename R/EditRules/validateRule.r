1e00b83f765e182353acec8a2aba54a4f9aa1807
micstone <micstone@deloitte.com>
2018-03-23 18:31:30 +0000

    UPDATE :: Changes made so that user does not have to enter extra escape characters for regex when adding and running rules
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27300 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 69bab20417beb89d6a51e929644ad47d55b954cf

--- R/EditRules/validateRule.r
+++ R/EditRules/validateRule.r
@@ -1,17 +1,23 @@
-validateRule <- function(rule) {
-
-df <- data.frame(
-  rule = c(rule)
-  , label = c("rule")
-)
-
-tryCatch({
-		v <- validator(.data=df) 
-		if(exists('v')) {
-			return("TRUE")
-	    }
-	  }, error = function(e) {
-			return("FALSE")
-	})
+escapeRegexR <- function(greplExpr){
+	splitRule <- strsplit(as.character(greplExpr), '"')[[1]]
+	lengthMinusOne <- length(splitRule) - 1
+	regex <- paste(splitRule[2:as.double(lengthMinusOne)], sep="")
+	regex <- stri_escape_unicode(regex)
+	greplExpr <- paste(splitRule[1],'"',regex,'"', splitRule[length(splitRule)], sep="")
 }
-			
+
+validateRule <- function(rule) {
+	lapply(list('validate', 'stringi'), require, character.only = TRUE)
+
+	if(grepl("^(grepl).*", rule)) rule <- escapeRegexR(rule)
+	
+	df <- data.frame(rule = c(rule), label = c("rule"))
+	tryCatch({
+			v <- validator(.data=df) 
+			if(exists('v')) {
+				return("TRUE")
+			}
+		  }, error = function(e) {
+				return("FALSE")
+		})
+}
