0f6a65e237cfe086183093e5f39b1d82244a9ea5
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2018-03-26 14:39:20 +0000

    Thread Local Test Samples
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27305 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 945133df981901e5eb8e5814c7e9e86a3f6f6f45

---
+++ src/prerna/test/UserStore.java
@@ -0,0 +1,11 @@
+package prerna.test;
+
+public class UserStore {
+
+	static private ThreadLocal threadLocal = new ThreadLocal<String>();
+
+	  static ThreadLocal<String> getThreadLocal() {
+	    return threadLocal;
+	  }
+	}
+	
