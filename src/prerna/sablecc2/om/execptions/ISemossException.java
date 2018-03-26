4fc22bb997ae5ec3dc08ece796f7fa477ca362d2
mahkhalil <mahkhalil@deloitte.com>
2018-03-26 14:13:47 +0000

    UPDATE ::: Adding SEMOSS exception to handle if we should continue thread of execution or stop UPDATE ::: Adding initial portion of GoogleSheetEngine. TODOL figure out how to pass the user into the open db
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27304 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ba9eee7f34c90603697bb6f9c8d14edb4092f9a4

---
+++ src/prerna/sablecc2/om/execptions/ISemossException.java
@@ -0,0 +1,32 @@
+package prerna.sablecc2.om.execptions;
+
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+
+public interface ISemossException {
+
+	/**
+	 * Get if this exception should stop the thread and return to the FE
+	 * or if we should continue and try to run through the other steps
+	 * @return
+	 */
+	boolean isContinueThreadOfExecution();
+	
+	/**
+	 * Set if this exception should stop the thread and return to the FE
+	 * or if we should continue and try to run through the other steps
+	 * @return
+	 */
+	void setContinueThreadOfExecution(boolean continueThreadOfExecution);
+	
+	/**
+	 * Get additional metadata to send to the FE when this exception occurs
+	 * @return
+	 */
+	NounMetadata getAdditionalReturn();
+	
+	/**
+	 * Set additional metadata to send to the FE when this exception occurs
+	 * @param noun
+	 */
+	void setAdditionalReturn(NounMetadata noun);
+}
