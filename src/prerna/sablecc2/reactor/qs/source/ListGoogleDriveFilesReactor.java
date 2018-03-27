baf56bf7defe5c50d3a75c5f4136ef8852d3125b
mahkhalil <mahkhalil@deloitte.com>
2018-03-27 20:35:23 +0000

    UPDATE ::: Modification to allow for a map to contain the type of login required when a login error occurs
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27353 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 88cface3553aea07c07adc0d5a24873737ce5af4

--- src/prerna/sablecc2/reactor/qs/source/ListGoogleDriveFilesReactor.java
+++ src/prerna/sablecc2/reactor/qs/source/ListGoogleDriveFilesReactor.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.HttpTransport;
@@ -41,7 +42,10 @@ public class ListGoogleDriveFilesReactor extends AbstractReactor {
 		if(gc == null) {
 			SemossPixelException exception = new SemossPixelException();
 			exception.setContinueThreadOfExecution(false);
-			exception.setAdditionalReturn(new NounMetadata("Please login to your Google account", PixelDataType.ERROR, PixelOperationType.GOOGLE_LOGIN_REQUIRED));
+			Map<String, Object> retMap = new HashMap<String, Object>();
+			retMap.put("type", "google");
+			retMap.put("message", "Please login to your Google account");
+			exception.setAdditionalReturn(new NounMetadata(retMap, PixelDataType.ERROR, PixelOperationType.LOGGIN_REQUIRED_ERROR));
 			throw exception;
 		}
 
