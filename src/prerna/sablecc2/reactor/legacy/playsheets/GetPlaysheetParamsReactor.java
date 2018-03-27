cd5f730c9619228a322037086a5ec19a9e813d04
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-03-27 16:35:26 +0000

    UPDATE ::: Get Params for Playsheet and Running Playsheet
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27342 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/legacy/playsheets/GetPlaysheetParamsReactor.java
+++ src/prerna/sablecc2/reactor/legacy/playsheets/GetPlaysheetParamsReactor.java
@@ -1,5 +1,55 @@
 package prerna.sablecc2.reactor.legacy.playsheets;
 
-public class GetPlaysheetParamsReactor {
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Set;
+import java.util.Vector;
+
+import prerna.engine.api.IEngine;
+import prerna.om.Insight;
+import prerna.om.OldInsight;
+import prerna.om.SEMOSSParam;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Utility;
+
+public class GetPlaysheetParamsReactor extends AbstractReactor {
+
+	public GetPlaysheetParamsReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.APP.getKey(), ReactorKeysEnum.ID.getKey() };
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String app = this.keyValue.get(this.keysToGet[0]);
+		String insightId = this.keyValue.get(this.keysToGet[1]);
+		IEngine engine = Utility.getEngine(app);
+		Insight in = engine.getInsight(insightId).get(0);
+		Hashtable outputHash = new Hashtable<String, Hashtable>();
+		outputHash.put("result", in.getRdbmsId());
+		if (in.isOldInsight()) {
+			Vector<SEMOSSParam> paramVector = ((OldInsight) in).getInsightParameters();
+			Hashtable optionsHash = new Hashtable();
+			Hashtable paramsHash = new Hashtable();
+			for (int paramIndex = 0; paramIndex < paramVector.size(); paramIndex++) {
+				SEMOSSParam param = paramVector.elementAt(paramIndex);
+				if (param.isDepends().equalsIgnoreCase("false")) {
+					Vector<Object> vals = engine.getParamOptions(param.getParamID());
+					Set<Object> uniqueVals = new HashSet<Object>(vals);
+					optionsHash.put(param.getName(), uniqueVals);
+				} else {
+					optionsHash.put(param.getName(), "");
+				}
+				paramsHash.put(param.getName(), param);
+			}
+			outputHash.put("options", optionsHash);
+			outputHash.put("params", paramsHash);
+		}
+		return new NounMetadata(outputHash, PixelDataType.MAP, PixelOperationType.PLAYSHEET_PARAMS);
+	}
 
 }
