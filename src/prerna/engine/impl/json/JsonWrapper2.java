7ce78c3b5c7f6ce55ebd07de9be54de05336bd22
mahkhalil <mahkhalil@deloitte.com>
2018-03-28 14:40:36 +0000

    BUGFIX ::: Pushing fixes for json engine 2
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27363 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/engine/impl/json/JsonWrapper2.java
+++ src/prerna/engine/impl/json/JsonWrapper2.java
@@ -2,11 +2,11 @@ package prerna.engine.impl.json;
 
 import java.util.Hashtable;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
 import net.minidev.json.JSONArray;
 import prerna.engine.api.IHeadersDataRow;
 import prerna.om.HeadersDataRow;
-
-import com.fasterxml.jackson.databind.node.ArrayNode;
 
 public class JsonWrapper2 extends JsonWrapper {
 	
@@ -19,43 +19,51 @@ public class JsonWrapper2 extends JsonWrapper {
 	public void execute() {
 		// sorry for the bad way to transport data
 		Hashtable output = (Hashtable)engine.execQuery(query);
-		if(output.containsKey("DATA"))
+		if(output.containsKey("DATA")) {
 			data = (ArrayNode)output.get("DATA");
-		if(output.containsKey("HEADERS"))
-			headerNames = (String [])output.get("HEADERS");
+		}
 		
-		if(output.containsKey("COUNT"))
+		if(output.containsKey("HEADERS")) {
+			headerNames = (String [])output.get("HEADERS");
+		}
+	
+		if(output.containsKey("COUNT")) {
 			numRows = (Integer)output.get("COUNT");
-		if(output.containsKey("TYPES"))
+		}
+		
+		if(output.containsKey("TYPES")) {
 			types = (String [])output.get("TYPES");
+		}
 	}
 
 	@Override
 	public IHeadersDataRow next() {
-		
+
 		ArrayNode thisRow = (ArrayNode)data.get(curRow);
-		
+
 		Object [] values = new Object[headerNames.length];
 		for(int colIndex = 0;colIndex < headerNames.length;colIndex++)
 		{
 			Object thisValue = null;
-			if(types[colIndex].equalsIgnoreCase("STRING"))
+			if(types[colIndex].equalsIgnoreCase("STRING")) {
 				thisValue = thisRow.get(colIndex).asText("");
-			if(types[colIndex].equalsIgnoreCase("NUMBER"))
+			} else if(types[colIndex].equalsIgnoreCase("NUMBER")) {
 				thisValue = thisRow.get(colIndex).asDouble();
-			if(thisValue instanceof JSONArray)
+			} else if(types[colIndex].equalsIgnoreCase("INT")) {
+				thisValue = thisRow.get(colIndex).asInt();
+			} else if(thisValue instanceof JSONArray)
 			{
 				// need to do the magic of delimiters etc. 
 			}
-			
+
 			values[colIndex] = thisValue;
 		}
-		
+
 		IHeadersDataRow retRow = new HeadersDataRow(headerNames, values);
 
 		curRow = curRow + 1;
 		return retRow;
 	}
 
-	
+
 }
