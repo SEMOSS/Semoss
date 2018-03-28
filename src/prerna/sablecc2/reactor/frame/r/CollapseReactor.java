820ce9f0654b2173b178761b18f8685d690db887
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-03-28 21:10:26 +0000

    UPDATE ::: Collapse with group by cols
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27399 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/r/CollapseReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/CollapseReactor.java
@@ -1,11 +1,14 @@
 package prerna.sablecc2.reactor.frame.r;
 
-import prerna.ds.OwlTemporalEngineMeta;
+import java.util.List;
+import java.util.Vector;
+
 import prerna.ds.r.RDataTable;
+import prerna.ds.r.RSyntaxHelper;
+import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
-import prerna.sablecc2.om.nounmeta.AddHeaderNounMetadata;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.util.Utility;
 
@@ -22,41 +25,78 @@ public class CollapseReactor extends AbstractRFrameReactor {
 		organizeKeys();
 		RDataTable frame = (RDataTable) getFrame();
 		String frameName = frame.getTableName();
-		String groupByCol = this.keyValue.get(this.keysToGet[0]);
+		List<String> groupByCol = getGroupByCols();
 		String valueCol = this.keyValue.get(this.keysToGet[1]);
 		String delim = this.keyValue.get(this.keysToGet[2]);
-
-		// pull in script
-		String collapseScriptPath = getBaseFolder() + "\\R\\AnalyticsRoutineScripts\\Collapse.r";
-		StringBuilder rsb2 = new StringBuilder();
-		collapseScriptPath = collapseScriptPath.replace("\\", "/");
-		rsb2.append("source(\"" + collapseScriptPath + "\");");
 		
-		// add inputs to script
-		String groupByRInput = "'"+groupByCol+"'";
-		String valueColRInput = "'"+valueCol+"'";
-		String delimRInput = "'"+delim+"'";
-		String collapsedColName = valueCol + "_Collapse";
+		StringBuilder rsb = new StringBuilder();
+		String groupByColsR = "list(";
+		Object[] row = new Object[groupByCol.size() + 1];
+		Object[] newColNames = new Object[groupByCol.size() + 1];
+		String tempFrame = "tempFrame" + Utility.getRandomString(8);
+		String collapsedColName = valueCol+"_Collapse";
+		for(int i=0; i < groupByCol.size(); i++) {
+			String groupCol = groupByCol.get(i);
+			groupByColsR += tempFrame+"$"+groupCol;
+			if(i < groupByCol.size() - 1) {
+				groupByColsR += " , ";
+			}
+			row[i] = groupCol;
+			newColNames[i] = groupCol;
+		}
+		groupByColsR += ")";
+		row[groupByCol.size()] = valueCol;
+		newColNames[groupByCol.size()] = collapsedColName;
 		
-		// get resulting df
-		String resultsDf = "resultsDf" + Utility.getRandomString(8);
-		rsb2.append(resultsDf+"<- collapse("+frameName+", "+groupByRInput+", "+valueColRInput+", "+delimRInput+");");
+		// get subset of frame using columns selected
+		String test = RSyntaxHelper.createStringRColVec(row);
+		rsb.append(tempFrame +"<- subset("+frameName+", select="+ test+");" );
 		
-		// TODO check for errors 
-		// set new frame if no error occurs
-		rsb2.append(frameName+"<- "+"as.data.table("+resultsDf+");");
-		this.rJavaTranslator.runR(rsb2.toString());
+		// get unique subset values
+		rsb.append(tempFrame +"<- unique("+tempFrame+");" );
 
-		// update the metadata to include this new collapse column
-		OwlTemporalEngineMeta metaData = this.getFrame().getMetaData();
-		metaData.addProperty(frameName, frameName + "__" + collapsedColName);
-		metaData.setAliasToProperty(frameName + "__" + collapsedColName, collapsedColName);
-		metaData.setDataTypeToProperty(frameName + "__" + collapsedColName, "STRING");
+		// aggregate values
+		String aggFrame = "aggFrame"+ Utility.getRandomString(8);
+		String delimR = "'"+delim+"'";
+		rsb.append(aggFrame+" <- aggregate("+tempFrame+"$"+valueCol+", by = "+groupByColsR+", paste, collapse="+delimR+");");
+		
+		// replace current frame with agg frame
+		rsb.append(frameName +"<- as.data.table("+aggFrame+");" );
+		
+		//rename columns
+		String names = RSyntaxHelper.createStringRColVec(newColNames);
+		rsb.append("colnames("+frameName+") <- "+names );
 
+		this.rJavaTranslator.runR(rsb.toString());
+		recreateMetadata(frameName);
+		
+		// clean up R temp variables
+		StringBuilder cleanUpScript = new StringBuilder();
+		cleanUpScript.append("rm(" + aggFrame + ");");
+		cleanUpScript.append("rm(" + tempFrame + ");");
+		cleanUpScript.append("gc();");
+		this.rJavaTranslator.runR(cleanUpScript.toString());
 
 		NounMetadata retNoun = new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_HEADERS_CHANGE, PixelOperationType.FRAME_DATA_CHANGE);
-		retNoun.addAdditionalReturn(new AddHeaderNounMetadata(collapsedColName));
 		return retNoun;
 	}
-
+	
+	private List<String> getGroupByCols() {
+		List<String> colInputs = new Vector<String>();
+		GenRowStruct colGRS = this.store.getNoun(this.keysToGet[0]);
+		if (colGRS != null) {
+			int size = colGRS.size();
+			if (size > 0) {
+				for (int i = 0; i < size; i++) {
+					//get each individual column entry and clean 
+					String column = colGRS.get(i).toString();
+					if (column.contains("__")) {
+						column = column.split("__")[1];
+					}
+					colInputs.add(column);
+				}
+			}
+		}
+		return colInputs;
+	}
 }
