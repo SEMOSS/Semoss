cf77c54ef5ea5b77ef53bffb13f794e6bde30326
mahkhalil <mahkhalil@deloitte.com>
2018-03-28 03:27:20 +0000

    UPDATE ::: Removing legacy methods used to support grabbing old playsheet parameters and put them into the GetPlaysheetParamsReactor class.  REST Endpoint used for that is now just calling reactor directly so code is in single location.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27360 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/engine/impl/MetaHelper.java
+++ src/prerna/engine/impl/MetaHelper.java
@@ -53,12 +53,7 @@ public class MetaHelper implements IExplorable {
 			+ "{?entity <http://www.w3.org/2000/01/rdf-schema#subClassOf>* ?y}"
 			+ "}";
 	
-	private static final String QUESTION_ID_FK_PARAM_KEY = "@QUESTION_ID_FK_VALUES@";
-	private static final String GET_ALL_PARAMS_FOR_QUESTION_ID = "SELECT DISTINCT PARAMETER_LABEL, PARAMETER_TYPE, PARAMETER_OPTIONS, PARAMETER_QUERY, PARAMETER_DEPENDENCY, PARAMETER_IS_DB_QUERY, PARAMETER_MULTI_SELECT, PARAMETER_COMPONENT_FILTER_ID, PARAMETER_ID FROM PARAMETER_ID WHERE QUESTION_ID_FK = " + QUESTION_ID_FK_PARAM_KEY;
 	public RDBMSNativeEngine insightRDBMS = null;
-
-	
-	protected Hashtable<String,String> transformedNodeNames = new Hashtable<String,String>();
 
 	public MetaHelper(RDFFileSesameEngine baseDataEngine, IEngine.ENGINE_TYPE engineType, String engineName)
 	{
@@ -104,18 +99,6 @@ public class MetaHelper implements IExplorable {
 
 	@Override
 	public Vector<Insight> getInsight(String... id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Vector<String> getInsight4Type(String type) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Vector<String> getInsight4Tag(String tag) {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -194,12 +177,6 @@ public class MetaHelper implements IExplorable {
 	}
 
 	@Override
-	public Vector<Object> getParamOptions(String parameterURI) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public IQueryInterpreter getQueryInterpreter() {
 		// TODO Auto-generated method stub
 		return null;
@@ -208,12 +185,6 @@ public class MetaHelper implements IExplorable {
 
 	@Override
 	public IEngine getInsightDatabase() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Map<String, Object>> getAllInsightsMetaData() {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -539,45 +510,6 @@ public class MetaHelper implements IExplorable {
 		return null;
 	}
 	
-	public Vector<SEMOSSParam> getParams(String questionID) {
-		String query = GET_ALL_PARAMS_FOR_QUESTION_ID.replace(QUESTION_ID_FK_PARAM_KEY, questionID);
-		ISelectWrapper wrap = WrapperManager.getInstance().getSWrapper(insightRDBMS, query);
-		String[] names = wrap.getVariables();
-
-		Vector<SEMOSSParam> retParam = new Vector<SEMOSSParam>();
-		while(wrap.hasNext()) {
-			ISelectStatement ss = wrap.next();
-			String label = ss.getVar(names[0]) + "";
-			SEMOSSParam param = new SEMOSSParam();
-			param.setName(label);
-			if(!ss.getVar(names[1]).toString().isEmpty())
-				param.setType(ss.getVar(names[1]) +"");
-			if(!ss.getVar(names[2]).toString().isEmpty())
-				param.setOptions(ss.getVar(names[2]) + "");
-			if(!ss.getVar(names[3]).toString().isEmpty())
-				param.setQuery(ss.getVar(names[3]) + "");
-			if(!ss.getVar(names[4]).toString().isEmpty()) {
-				String[] vars = (ss.getVar(names[4]) +"").split(";");
-				for(String var : vars){
-					param.addDependVar(var);
-				}
-			}
-			if(!ss.getVar(names[5]).toString().isEmpty()) {
-				param.setDbQuery((boolean) ss.getVar(names[5]));
-			}
-			if(!ss.getVar(names[6]).toString().isEmpty()) {
-				param.setMultiSelect((boolean) ss.getVar(names[6]));
-			}
-			if(!ss.getVar(names[7]).toString().isEmpty())
-				param.setComponentFilterId(ss.getVar(names[7]) + "");
-			if(!ss.getRawVar(names[8]).toString().isEmpty())
-				param.setParamID(ss.getVar(names[8]) +"");
-			retParam.addElement(param);
-		}
-
-		return retParam;
-	}
-
 	@Override
 	public List<String> getParentOfProperty2(String property) {
 		// TODO Auto-generated method stub
@@ -606,6 +538,12 @@ public class MetaHelper implements IExplorable {
 	public void setBasic(boolean isBasic) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public RDFFileSesameEngine getBaseDataEngine() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/prerna/sablecc2/reactor/legacy/playsheets/GetPlaysheetParamsReactor.java
+++ src/prerna/sablecc2/reactor/legacy/playsheets/GetPlaysheetParamsReactor.java
@@ -2,21 +2,31 @@ package prerna.sablecc2.reactor.legacy.playsheets;
 
 import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
 import prerna.engine.api.IEngine;
+import prerna.engine.api.IRawSelectWrapper;
+import prerna.engine.api.ISelectStatement;
+import prerna.engine.api.ISelectWrapper;
 import prerna.om.Insight;
 import prerna.om.OldInsight;
 import prerna.om.SEMOSSParam;
+import prerna.rdf.engine.wrappers.WrapperManager;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Constants;
 import prerna.util.Utility;
 
 public class GetPlaysheetParamsReactor extends AbstractReactor {
+
+	private static final String PARAMETER_ID_PARAM_KEY = "@PARAMETER_ID";
+	private static final String GET_INFO_FOR_PARAM = "SELECT DISTINCT PARAMETER_LABEL, PARAMETER_TYPE, PARAMETER_OPTIONS, PARAMETER_QUERY, PARAMETER_DEPENDENCY, PARAMETER_IS_DB_QUERY, PARAMETER_MULTI_SELECT, PARAMETER_COMPONENT_FILTER_ID FROM PARAMETER_ID WHERE PARAMETER_ID = '" + PARAMETER_ID_PARAM_KEY + "'";
 
 	public GetPlaysheetParamsReactor() {
 		this.keysToGet = new String[] { ReactorKeysEnum.APP.getKey(), ReactorKeysEnum.ID.getKey() };
@@ -38,7 +48,7 @@ public class GetPlaysheetParamsReactor extends AbstractReactor {
 			for (int paramIndex = 0; paramIndex < paramVector.size(); paramIndex++) {
 				SEMOSSParam param = paramVector.elementAt(paramIndex);
 				if (param.isDepends().equalsIgnoreCase("false")) {
-					Vector<Object> vals = engine.getParamOptions(param.getParamID());
+					List<Object> vals = getParamOptions(engine, param.getParamID());
 					Set<Object> uniqueVals = new HashSet<Object>(vals);
 					optionsHash.put(param.getName(), uniqueVals);
 				} else {
@@ -52,4 +62,104 @@ public class GetPlaysheetParamsReactor extends AbstractReactor {
 		return new NounMetadata(outputHash, PixelDataType.MAP, PixelOperationType.PLAYSHEET_PARAMS);
 	}
 
+	public List<Object> getParamOptions(IEngine engine, String parameterID) {
+		IEngine insightRDBMS = engine.getInsightDatabase();
+		String query = GET_INFO_FOR_PARAM.replace(PARAMETER_ID_PARAM_KEY, parameterID);
+		ISelectWrapper wrap = WrapperManager.getInstance().getSWrapper(insightRDBMS, query);
+		String[] names = wrap.getVariables();
+
+		Vector<SEMOSSParam> retParam = new Vector<SEMOSSParam>();
+		while(wrap.hasNext()) {
+			ISelectStatement ss = wrap.next();
+			String label = ss.getVar(names[0]) + "";
+			SEMOSSParam param = new SEMOSSParam();
+			param.setName(label);
+			if(ss.getVar(names[1]) != null)
+				param.setType(ss.getVar(names[1]) +"");
+			if(ss.getVar(names[2]) != null)
+				param.setOptions(ss.getVar(names[2]) + "");
+			if(ss.getVar(names[3]) != null)
+				param.setQuery(ss.getVar(names[3]) + "");
+			if(ss.getRawVar(names[4]) != null)
+				param.addDependVar(ss.getRawVar(names[4]) +"");
+			if(ss.getVar(names[5]) != null && !ss.getVar(names[5]).toString().isEmpty())
+				param.setDbQuery((boolean) ss.getVar(names[5]));
+			if(!ss.getVar(names[6]).toString().isEmpty())
+				param.setMultiSelect((boolean) ss.getVar(names[6]));
+			if(!ss.getVar(names[7]).toString().isEmpty())
+				param.setComponentFilterId(ss.getVar(names[7]) + "");
+			if(ss.getVar(names[0]) != null)
+				param.setParamID(ss.getVar(names[0]) +"");
+			retParam.addElement(param);
+		}
+
+		List<Object> uris = new Vector<Object>();
+		if(!retParam.isEmpty()){
+			SEMOSSParam ourParam = retParam.get(0); // there should only be one as we are getting the param from a specific param URI
+			//if the param has options defined, we are all set
+			//grab the options and we are good to go
+			Vector<String> options = ourParam.getOptions();
+			if (options != null && !options.isEmpty()) {
+				uris.addAll(options);
+			}
+			else{
+				// if options are not defined, need to get uris either from custom sparql or type
+				// need to use custom query if it has been specified in the xml
+				// otherwise use generic fill query
+				String paramQuery = ourParam.getQuery();
+				String type = ourParam.getType();
+				boolean isDbQuery = ourParam.isDbQuery();
+				// RDBMS right now does type:type... need to get just the second type. This will be fixed once insights don't store generic query
+				// TODO: fix this logic. need to decide how to store param type for rdbms
+				if(paramQuery != null && !paramQuery.isEmpty()) {
+					//TODO: rdbms has type as null... this is confusing given the other comments here....
+					if(type != null && !type.isEmpty()) {
+						if (engine.getEngineType().equals(IEngine.ENGINE_TYPE.RDBMS)) {
+							if (type.contains(":")) {
+								String[] typeArray = type.split(":");
+								type = typeArray[1];
+							}
+						}
+						Map<String, List<Object>> paramTable = new Hashtable<String, List<Object>>();
+						List<Object> typeList = new Vector<Object>();
+						typeList.add(type);
+						paramTable.put(Constants.ENTITY, typeList);
+						paramQuery = Utility.fillParam(paramQuery, paramTable);
+					}
+					if(isDbQuery) {
+						uris = getRawValues(engine, paramQuery);
+					} else {
+						uris = getRawValues(engine.getBaseDataEngine(), paramQuery);
+					}
+				} else { 
+					// anything that is get Entity of Type must be on db
+					uris = engine.getEntityOfType(type);
+				}
+			}
+		}
+		return uris;
+	}
+	
+	/**
+	 * Flush out results
+	 * @param engine
+	 * @param query
+	 * @return
+	 */
+	public List<Object> getRawValues(IEngine engine, String query) {
+		List<Object> ret = new Vector<Object>();
+		IRawSelectWrapper wrap = WrapperManager.getInstance().getRawWrapper(engine, query);
+		while(wrap.hasNext()) {
+			Object value = wrap.next().getRawValues()[0];
+			if(value instanceof Number) {
+				ret.add(value);
+			} else {
+				ret.add(value.toString());
+			}
+		}
+		return ret;
+	}
+	
+	
+	
 }
