0508bebf999710b3051a89687a513474c592e1b3
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2018-04-18 18:18:48 +0000

    GoogleLatLong - Gets the lat and long based on an adress AuthProvider - Update for google App
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27491 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 90f1e20c23499863208812e24d2599d961397921

---
+++ src/prerna/io/connector/google/GoogleLatLongGetter.java
@@ -0,0 +1,57 @@
+package prerna.io.connector.google;
+
+import java.util.Hashtable;
+
+import prerna.auth.AccessToken;
+import prerna.auth.AuthProvider;
+import prerna.auth.User2;
+import prerna.io.connector.IConnectorIOp;
+import prerna.om.GeoLocation;
+import prerna.security.AbstractHttpHelper;
+import prerna.util.BeanFiller;
+
+public class GoogleLatLongGetter implements IConnectorIOp{
+
+	String url = "https://maps.googleapis.com/maps/api/geocode/json" ;//?key=***REMOVED***;
+	
+	// name of the object to return
+	String objectName = "prerna.auth.User2"; // it will fill this object and return the data
+	String [] beanProps = {"latitude", "longitude"}; // add is done when you have a list
+	String jsonPattern = "results[*].geometry.location.[lat, lng][]";
+	
+	@Override
+	public Object execute(User2 user, Hashtable params) 
+	{
+		if(params == null)
+			params = new Hashtable();
+		// TODO Auto-generated method stub
+		AccessToken googToken = user.getAccessToken(AuthProvider.GOOGLE_MAP.name());
+				
+		String accessToken = googToken.getAccess_token();
+		
+		// you fill what you want to send on the API call
+		// the other thing it needs is an address
+		params.put("key", accessToken);
+		
+		// make the API call
+		String output = AbstractHttpHelper.makeGetCall(url, accessToken, params, false);
+		
+		//System.out.println("Output >>>>> " + output);
+		
+		// fill the bean with the return
+		GeoLocation retLocation = new GeoLocation();
+		
+		retLocation = (GeoLocation)BeanFiller.fillFromJson(output, jsonPattern, beanProps, retLocation);
+		
+		// need other methods for filling other things
+		System.out.println(" ");
+		return retLocation;
+	}
+	
+	public static void main(String [] args)
+	{
+		GoogleLatLongGetter gl = new GoogleLatLongGetter();
+		
+	}
+
+}
