28211436c834926ec9a047d6c1eedbd45c246aed
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2018-04-18 18:18:48 +0000

    GoogleLatLong - Gets the lat and long based on an adress AuthProvider - Update for google App
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27491 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 90f1e20c23499863208812e24d2599d961397921

--- src/prerna/auth/AuthProvider.java
+++ src/prerna/auth/AuthProvider.java
@@ -2,7 +2,7 @@ package prerna.auth;
 
 public enum AuthProvider {
 
-	    GOOGLE, FACEBOOK, TWITTER, SALESFORCE, GIT, AZURE_GRAPH, DROPBOX, CAC, PRODUCT_HUNT;
+	    GOOGLE, FACEBOOK, TWITTER, SALESFORCE, GIT, AZURE_GRAPH, DROPBOX, CAC, PRODUCT_HUNT, IN, GOOGLE_MAP;
 
 	    public String toString() {
 	        return name().charAt(0) + name().substring(1).toLowerCase();
---
+++ src/prerna/om/GeoLocation.java
@@ -0,0 +1,64 @@
+package prerna.om;
+
+public class GeoLocation 
+{
+	// has an address
+	// city
+	// state
+	// country
+	// latitude
+	// longitude
+	// places_id if there is one
+	
+	String address = null;
+	String city = null;
+	String state = null;
+	String postalCode = null;
+	String country = null;
+	
+	float latitude = 0;
+	float longitude = 0;
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public float getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+	public float getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+
+}
