37c192c40708069564afc7abd4f273d6085b03b8
Maher Khalil <themaherkhalil@gmail.com>
2018-04-23 20:23:41 +0000

    UPDATE ::: Allowing headers map to be passed into the get request
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27527 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 1f9f17dbb67ee8f0de091e4a36c0143d5af40691

--- src/prerna/sablecc2/reactor/utils/GetRequestReactor.java
+++ src/prerna/sablecc2/reactor/utils/GetRequestReactor.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -15,6 +18,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContextBuilder;
 
+import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
@@ -23,14 +27,15 @@ import prerna.sablecc2.reactor.AbstractReactor;
 public class GetRequestReactor extends AbstractReactor {
 
 	public GetRequestReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.URL.getKey()};
+		this.keysToGet = new String[]{ReactorKeysEnum.URL.getKey(), "headersMap"};
 	}
 	
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
 		String url = this.keyValue.get(this.keysToGet[0]);
-		
+		List<Map<String, String>> headersMap = getHeadersMap();
+				
 		ResponseHandler<String> handler = new BasicResponseHandler();
 		CloseableHttpResponse response = null;
 		try {
@@ -39,6 +44,14 @@ public class GetRequestReactor extends AbstractReactor {
 			SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build());
 			CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
 			HttpGet httpGet = new HttpGet(url);
+			if(headersMap != null && !headersMap.isEmpty()) {
+				for(int i = 0; i < headersMap.size(); i++) {
+					Map<String, String> head = headersMap.get(i);
+					for(String key : head.keySet()) {
+						httpGet.addHeader(key, head.get(key));
+					}
+				}
+			}
 			response = httpClient.execute(httpGet);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -62,6 +75,22 @@ public class GetRequestReactor extends AbstractReactor {
 		}
 		
 		return new NounMetadata(retString, PixelDataType.CONST_STRING);
+	}
+
+	/**
+	 * Get headers to add to the request
+	 * @return
+	 */
+	private List<Map<String, String>> getHeadersMap() {
+		GenRowStruct headersGrs = this.store.getNoun(this.keysToGet[1]);
+		if(headersGrs != null && !headersGrs.isEmpty()) {
+			List<Map<String, String>> headers = new Vector<Map<String, String>>();
+			for(int i = 0; i < headersGrs.size(); i++) {
+				headers.add( (Map<String, String>) headersGrs.get(i)); 
+			}
+			return headers;
+		}
+		return null;
 	}
 
 }
