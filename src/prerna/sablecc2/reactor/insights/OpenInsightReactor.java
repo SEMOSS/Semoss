def5d4ef89a4688ee01e20c44e1427ac918ecd6a
Maher Khalil <themaherkhalil@gmail.com>
2018-04-23 19:49:38 +0000

    UPDATE ::: Adding metadata to return for open insight
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27525 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 4eccdb032dc0038f372b76bddaad7f995383b030

--- src/prerna/sablecc2/reactor/insights/OpenInsightReactor.java
+++ src/prerna/sablecc2/reactor/insights/OpenInsightReactor.java
@@ -16,7 +16,6 @@ import prerna.om.Insight;
 import prerna.om.InsightStore;
 import prerna.om.OldInsight;
 import prerna.sablecc2.PixelRunner;
-import prerna.sablecc2.PixelUtility;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
@@ -77,60 +76,14 @@ public class OpenInsightReactor extends AbstractInsightReactor {
 		// get the insight output
 		PixelRunner runner = null;
 		// add additional pixels if necessary
-		List<String> fullRecipe = new Vector<String>();
 		if(additionalPixels != null && !additionalPixels.isEmpty()) {
 			// just add it directly to the pixel list
 			// and the reRunPiexelInsight will do its job
 			newInsight.getPixelRecipe().addAll(additionalPixels);
-			
-			// store the recipe in case an error occurs
-			fullRecipe.addAll(newInsight.getPixelRecipe());
-			
-			// rerun the insight
-			runner = newInsight.reRunPixelInsight();
-		} else {
-			fullRecipe.addAll(newInsight.getPixelRecipe());
-			
-//			insightData = getCachedData(appName, rdbmsId);
-//			if(insightData == null) {
-				runner = newInsight.reRunPixelInsight();
-//				cacheInsightData(appName, rdbmsId, insightData);
-//			}
 		}
+		// rerun the insight
+		runner = newInsight.reRunPixelInsight();
 		
-		// see if the last response was an error
-		// if it is an error that requires user input
-		// hydrate the last result to contain the new information
-//		if(insightData.containsKey("pixelReturn")) {
-//			Vector<Map<String, Object>> returns = (Vector<Map<String, Object>>) insightData.get("pixelReturn");
-//			Map<String, Object> lastResult = (Map<String, Object>) returns.get(returns.size()-1);
-//			Vector<PixelOperationType> opTypes = (Vector<PixelOperationType>) lastResult.get("operationType");
-//			if(PixelUtility.autoExecuteAfterUserInput(opTypes)) {
-//				Map<String, Object> openInsightNounValue = new HashMap<String, Object>();
-//				openInsightNounValue.put(this.keysToGet[0], appName);
-//				openInsightNounValue.put(this.keysToGet[1], rdbmsId);
-//				openInsightNounValue.put("recipe", fullRecipe);
-//				openInsightNounValue.put("errorData", lastResult.get("output"));
-//				NounMetadata newLastNoun = new NounMetadata(openInsightNounValue, PixelDataType.CUSTOM_DATA_STRUCTURE, opTypes);
-//				
-//				Map<String, Object> newLastResult = PixelUtility.processNounMetadata(newLastNoun);
-//				newLastResult.put("pixelExpression", lastResult.get("pixelExpression"));
-//				newLastResult.put("isMeta", lastResult.get("isMeta"));
-//
-//				returns.remove(returns.size()-1);
-//				returns.add(newLastResult);
-//			}
-//		}
-		
-//		Map<String, Object> insightMap = new HashMap<String, Object>();
-//		// return to the FE the recipe
-//		insightMap.put("name", newInsight.getInsightName());
-//		// keys below match those in solr
-//		insightMap.put("core_engine", newInsight.getEngineName());
-//		insightMap.put("core_engine_id", newInsight.getRdbmsId());
-//		insightMap.put("insightData", insightData);
-//		insightMap.put("params", params);
-
 		// update the solr universal view count
 		try {
 			SolrIndexEngine.getInstance().updateViewedInsight(appName + "_" + rdbmsId);
@@ -143,7 +96,11 @@ public class OpenInsightReactor extends AbstractInsightReactor {
 		GATracker.getInstance().trackInsightExecution(this.insight, "openinsight", appName, rdbmsId, newInsight.getInsightName());
 		
 		// return the recipe steps
-		return new NounMetadata(runner, PixelDataType.PIXEL_RUNNER, PixelOperationType.OPEN_SAVED_INSIGHT);
+		Map<String, Object> runnerWraper = new HashMap<String, Object>();
+		runnerWraper.put("runner", runner);
+		runnerWraper.put("params", params);
+		runnerWraper.put("additionalPixels", additionalPixels);
+		return new NounMetadata(runnerWraper, PixelDataType.PIXEL_RUNNER, PixelOperationType.OPEN_SAVED_INSIGHT);
 	}
 
 	private List<String> getAdditionalPixels() {
@@ -160,60 +117,5 @@ public class OpenInsightReactor extends AbstractInsightReactor {
 		// no additional pixels to run
 		return null;
 	}
-	
-//	/**
-//	 * Cache the insight data in the version folder location
-//	 * @param appName
-//	 * @param rdbmsId
-//	 * @param insightData
-//	 */
-//	private void cacheInsightData(String appName, String rdbmsId, Map<String, Object> insightData) {
-//		// get the file location
-//		String cacheLoc = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
-//		cacheLoc += "\\" + Constants.DB + "\\" + appName + "\\version\\" + rdbmsId + "\\response.json";
-//		
-//		File cacheFile = new File(cacheLoc);
-//		if(cacheFile.exists()) {
-//			// if it exists, delete it
-//			cacheFile.delete();
-//		}
-//		
-//		// write the data to the file
-//		Gson gson = new Gson();
-//		try {
-//			FileUtils.writeStringToFile(cacheFile, gson.toJson(insightData));
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	/**
-//	 * Get the cached insight data
-//	 * Return null if there is none
-//	 * @param appName
-//	 * @param rdbmsId
-//	 * @return
-//	 */
-//	private Map<String, Object> getCachedData(String appName, String rdbmsId) {
-//		String cacheLoc = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
-//		cacheLoc += "\\" + Constants.DB + "\\" + appName + "\\version\\" + rdbmsId + "\\response.json";
-//		
-//		File cacheFile = new File(cacheLoc);
-//		if(!cacheFile.exists()) {
-//			return null;
-//		}
-//		
-//		
-//		Gson gson = new Gson();
-//		try {
-//			String responseStr = FileUtils.readFileToString(cacheFile);
-//			return gson.fromJson(responseStr, new TypeToken<Map<String, Object>>() {}.getType());
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		
-//		return null;
-//	}
-	
 	
 }
