2d62587dc8e5ceb9d7edcb3fdbd2a85fc770c9b1
kunalppatel9 <kunalppatel9@deloitte.com>
2018-04-24 20:05:38 +0000

    UPDATE ::: Adding Dropbox, OneDrive, Google Drive reactors to pull for REST
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27563 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 080caa4a3713d088cd30bde5fec8698d45bcb79b

---
+++ src/prerna/sablecc2/reactor/qs/source/OneDriveFileRetrieverReactor.java
@@ -0,0 +1,132 @@
+
+package prerna.sablecc2.reactor.qs.source;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+import prerna.auth.AccessToken;
+import prerna.auth.AuthProvider;
+import prerna.auth.User2;
+import prerna.io.connector.IConnectorIOp;
+import prerna.om.RemoteItem;
+import prerna.poi.main.MetaModelCreator;
+import prerna.poi.main.helper.CSVFileHelper;
+import prerna.query.querystruct.CsvQueryStruct;
+import prerna.query.querystruct.QueryStruct2;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.execptions.SemossPixelException;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.qs.AbstractQueryStructReactor;
+import prerna.security.AbstractHttpHelper;
+import prerna.util.BeanFiller;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.Utility;
+
+public class OneDriveFileRetrieverReactor extends AbstractQueryStructReactor{
+
+	public OneDriveFileRetrieverReactor() {
+		this.keysToGet = new String[] { "name", "id" };
+	}
+
+
+
+	@Override
+	protected QueryStruct2 createQueryStruct() {
+		String fileName = this.curRow.get(0).toString();
+		if (fileName == null || fileName.length() <= 0) {
+			throw new IllegalArgumentException("Need to specify file name");
+		}
+		String msID = this.curRow.get(1).toString();
+		if (msID == null || msID.length() <= 0) {
+			throw new IllegalArgumentException("Need to specify file path");
+		}
+
+		//get access token
+		String accessToken=null;
+		User2 user = this.insight.getUser2();
+
+		try{
+			if(user==null){
+				SemossPixelException exception = new SemossPixelException();
+				exception.setContinueThreadOfExecution(false);
+				Map<String, Object> retMap = new HashMap<String, Object>();
+				retMap.put("type", "microsoft");
+				retMap.put("message", "Please login to your Microsoft account");
+				exception.setAdditionalReturn(new NounMetadata(retMap, PixelDataType.ERROR, PixelOperationType.LOGGIN_REQUIRED_ERROR, PixelOperationType.ERROR));
+				throw exception;
+			}
+			else if (user != null) {
+				AccessToken msToken = user.getAccessToken(AuthProvider.AZURE_GRAPH.name());
+				accessToken=msToken.getAccess_token();
+			}
+		}
+		catch (Exception e) {
+			SemossPixelException exception = new SemossPixelException();
+			exception.setContinueThreadOfExecution(false);
+			Map<String, Object> retMap = new HashMap<String, Object>();
+			retMap.put("type", "microsoft");
+			retMap.put("message", "Please login to your Microsoft account");
+			exception.setAdditionalReturn(new NounMetadata(retMap, PixelDataType.ERROR, PixelOperationType.LOGGIN_REQUIRED_ERROR, PixelOperationType.ERROR));
+			throw exception;
+		}
+
+		
+		Hashtable params = new Hashtable();
+		CsvQueryStruct qs = new CsvQueryStruct();
+
+		try {
+			String url_str = "https://graph.microsoft.com/v1.0/me/drive/items/"+msID+"/content";
+			BufferedReader br = AbstractHttpHelper.getHttpStream(url_str, accessToken, params, true);
+
+			// create a file
+			String filePath = DIHelper.getInstance().getProperty(Constants.INSIGHT_CACHE_DIR) + "\\"
+					+ DIHelper.getInstance().getProperty(Constants.CSV_INSIGHT_CACHE_FOLDER);
+			filePath += "\\" + Utility.getRandomString(10) + ".csv";
+			filePath = filePath.replace("\\", "/");
+			File outputFile = new File(filePath);
+
+			BufferedWriter target = new BufferedWriter(new FileWriter(outputFile));
+			String data = null;
+
+
+			while((data = br.readLine()) != null)
+			{
+				target.write(data);
+				target.write("\n");
+				target.flush();
+			}
+
+			// get datatypes
+			CSVFileHelper helper = new CSVFileHelper();
+			helper.setDelimiter(',');
+			helper.parse(filePath);
+			MetaModelCreator predictor = new MetaModelCreator(helper, null);
+			Map<String, String> dataTypes = predictor.getDataTypeMap();
+			for (String key : dataTypes.keySet()) {
+				qs.addSelector("DND", key);
+			}
+			helper.clear();
+			qs.merge(this.qs);
+			qs.setCsvFilePath(filePath);
+			qs.setDelimiter(',');
+			qs.setColumnTypes(dataTypes);
+			return qs;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return qs;
+	}
+
+
+
+
+}
