4d4d57d21226f0f24e379128417a2fe4e350b87a
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2018-04-24 17:40:21 +0000

    Twitter Searcher - API Call for searching twitter. See UserResource.performGoogleOps for more details.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27555 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6ce12961c3efce29a910e0c6b50464f014fbb259

--- src/prerna/util/BeanFiller.java
+++ src/prerna/util/BeanFiller.java
@@ -14,6 +14,7 @@ import org.apache.commons.beanutils.BeanUtils;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class BeanFiller {
 
@@ -22,6 +23,25 @@ public class BeanFiller {
 	// and then fills it
 	static ObjectMapper mapper = new ObjectMapper();
 	static JmesPath<JsonNode> jmespath = new JacksonRuntime();
+	
+	// get the jsonNode for input
+	public static JsonNode getJmesResult(String json, String jsonPattern)
+	{
+		try {
+			
+			Expression<JsonNode> expression = jmespath.compile(jsonPattern);
+
+			//AccessToken tok = mapper.readValue(json, AccessToken.class);
+			JsonNode input = mapper.readTree(json);
+			JsonNode result = expression.search(input);
+			
+			return result;
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		return null;
+	}
 
 	// fills a single bean
 	public static Object fillFromJson(String json, String jsonPattern, String [] beanProps, Object bean)
@@ -38,7 +58,8 @@ public class BeanFiller {
 			JsonNode input = mapper.readTree(json);
 			JsonNode result = expression.search(input);
 			
-			if(isJsonArray(result)) // this is a multiple value
+			System.out.println("Is array  " + (result instanceof ArrayNode));
+			if((result instanceof ArrayNode) && result.get(0) instanceof ObjectNode) // this is a multiple value
 			{
 				List <Object> retList = new Vector();
 
@@ -47,7 +68,10 @@ public class BeanFiller {
 				{
 					// I should possibly create a new instance everytime as well
 					Object newBean = bean.getClass().newInstance();
-					Object newObject = fillSingleObject(result.get(resIndex), beanProps, newBean);
+					Object newObject = null;
+					newObject = fillSingleObjectFromMap(result.get(resIndex), beanProps, newBean);
+					//else
+					//	System.out.println("Need to find if this is a Map.. ");
 					retList.add(newObject);
 				}
 				retObject = retList;
@@ -125,6 +149,58 @@ public class BeanFiller {
 		return bean;
 		
 	}
+
+	public static Object fillSingleObjectFromMap(JsonNode result, String [] beanProps, Object bean)
+	{
+		try
+		{
+			for(int inputIndex = 0;result != null && inputIndex < beanProps.length;inputIndex++)
+			{
+				String thisInput = result.get(beanProps[inputIndex]).asText();
+				if(result.size() > inputIndex)
+				{
+					String beanProp = beanProps[inputIndex];
+					if(beanProp.startsWith("add_"))
+					{
+						beanProp = beanProp.replaceAll("add_", "");
+						List thisList = null;
+						Object listObj = BeanUtils.getProperty(bean, beanProp);
+						if(listObj != null)
+							thisList = (List)listObj;
+						else
+							thisList = new ArrayList();
+						thisList.add(thisInput);
+						
+						BeanUtils.setProperty(bean, beanProp, listObj);
+					}
+					else
+					{
+						BeanUtils.setProperty(bean, beanProp, thisInput);
+					}
+				}	
+				// add to the other data
+				else
+				{
+					BeanUtils.setProperty(bean, "extra", thisInput);
+				}
+				
+			}
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		return bean;
+		
+	}
+
+	
 	
 	public static String getJson(Object object) throws Exception
 	{
