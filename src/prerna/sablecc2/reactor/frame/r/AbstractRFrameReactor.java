4b19d64374af7b4e04e01919b9752a68b0502fad
mahkhalil <mahkhalil@deloitte.com>
2018-04-25 14:34:15 +0000

    UPDATE ::: Adding get grid method to send grid data to FE
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27585 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f572c451c926c1bf69c62607b71355719e34d91d

--- src/prerna/sablecc2/reactor/frame/r/AbstractRFrameReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/AbstractRFrameReactor.java
@@ -166,67 +166,6 @@ public abstract class AbstractRFrameReactor extends AbstractFrameReactor {
 		this.rJavaTranslator.changeColumnType(frame, frameName, colName, newType, dateFormat);
 	}
 	
-	/**
-	 * This method allows us to create a bar chart from the data values object[][] created with the breaks and counts
-	 * @param label
-	 * @param value - for the histogram this is "Frequency"
-	 * @param dataValues
-	 */
-	public Map<String, Object> getBarChartInfo(String panelId, String label, String value, Object[][] dataValues) {
-		// create the weird object the FE needs to paint a bar chart
-		ConstantDataTask task = new ConstantDataTask();
-		task.setId("TEMP_ID");
-		Map<String, Object> returnData = new Hashtable<String, Object>();
-		returnData.put("values", dataValues);
-		returnData.put("headers", new String[]{label, value});
-		task.setOutputData(returnData);
-		
-		//create maps to set the task options
-		//main map that will be passed into the setTaskOptions method
-		Map<String, Object> mapOptions = new HashMap<String, Object>();
-		
-		//this map (keyMap) comprises the mapping of both layout and alignment
-		Map<String, Object> keyMap = new HashMap<String, Object>(); 
-		keyMap.put("layout", "Column");
-		
-		//within keyMap, we need a map to store the maps that comprise alignment
-		Map<String, Object> alignmentMap = new HashMap<String, Object>();
-		alignmentMap.put("label", "[" + label + "]");
-		alignmentMap.put("value", "[" + value + "]");
-		alignmentMap.put("tooltip", "[]");
-		
-		keyMap.put("alignment", alignmentMap);
-		
-		mapOptions.put(panelId, keyMap);
-		//the final mapping looks like this:
-		//taskOptions={0={layout=Column, alignment={tooltip=[], label=[MovieBudget], value=[Frequency]}}}
-		
-		//set task options
-		task.setTaskOptions(new TaskOptions(mapOptions));
-
-		List<Map<String, Object>> vizHeaders = new Vector<Map<String, Object>>();
-		Map<String, Object> labelMap = new Hashtable<String, Object>();
-		labelMap.put("header", label);
-		labelMap.put("derived", true);
-		labelMap.put("alias", label);
-
-		Map<String, Object> frequencyMap = new Hashtable<String, Object>();
-		frequencyMap.put("header", value);
-		frequencyMap.put("derived", true);
-		frequencyMap.put("alias", value);
-
-		vizHeaders.add(labelMap);
-		vizHeaders.add(frequencyMap);
-
-		task.setHeaderInfo(vizHeaders);
-
-		Map<String, Object> formatMap = new Hashtable<String, Object>();
-		formatMap.put("type", "TABLE");
-		task.setFormatMap(formatMap);
-		
-		return task.collect(true);
-	}
-	
 	protected void storeVariable(String varName, NounMetadata noun) {
 		this.insight.getVarStore().put(varName, noun);
 	}
@@ -256,7 +195,6 @@ public abstract class AbstractRFrameReactor extends AbstractFrameReactor {
 		}
 		System.out.println("R Shutdown!!");
 //		java.lang.System.setSecurityManager(reactorManager);
-
 	}
 	
 	/**
@@ -272,5 +210,129 @@ public abstract class AbstractRFrameReactor extends AbstractFrameReactor {
           }
           return baseFolder;
      }
+     
+     //////////////////////////////////////////////////////////////
+     
+     /*
+      * Below methods are for sending data to the FE as ConstantDataTask's
+      * This is because the FE needs certain information to know how to paint
+      * ^i.e. task options, etc.
+      * So creating wrappers to facilitate that
+      */
+     
+ 	/**
+ 	 * This method allows us to create a bar chart from the data values object[][] created with the breaks and counts
+ 	 * @param label
+ 	 * @param value - for the histogram this is "Frequency"
+ 	 * @param dataValues - this better be a List<Object[]> or Object[][] since the serialization to JSON is the same
+ 	 */
+ 	public Map<String, Object> getBarChartInfo(String panelId, String label, String value, Object dataValues) {
+ 		// create the weird object the FE needs to paint a bar chart
+ 		ConstantDataTask task = new ConstantDataTask();
+ 		task.setId("TEMP_ID");
+ 		Map<String, Object> returnData = new Hashtable<String, Object>();
+ 		returnData.put("values", dataValues);
+ 		returnData.put("headers", new String[]{label, value});
+ 		task.setOutputData(returnData);
+ 		
+ 		//create maps to set the task options
+ 		//main map that will be passed into the setTaskOptions method
+ 		Map<String, Object> mapOptions = new HashMap<String, Object>();
+ 		
+ 		//this map (keyMap) comprises the mapping of both layout and alignment
+ 		Map<String, Object> keyMap = new HashMap<String, Object>(); 
+ 		keyMap.put("layout", "Column");
+ 		
+ 		//within keyMap, we need a map to store the maps that comprise alignment
+ 		Map<String, Object> alignmentMap = new HashMap<String, Object>();
+ 		alignmentMap.put("label", "[" + label + "]");
+ 		alignmentMap.put("value", "[" + value + "]");
+ 		alignmentMap.put("tooltip", "[]");
+ 		
+ 		keyMap.put("alignment", alignmentMap);
+ 		
+ 		mapOptions.put(panelId, keyMap);
+ 		//the final mapping looks like this:
+ 		//taskOptions={0={layout=Column, alignment={tooltip=[], labels=[col1, col2, col3]}}}
+ 		
+ 		//set task options
+ 		task.setTaskOptions(new TaskOptions(mapOptions));
+
+ 		List<Map<String, Object>> vizHeaders = new Vector<Map<String, Object>>();
+ 		Map<String, Object> labelMap = new Hashtable<String, Object>();
+ 		labelMap.put("header", label);
+ 		labelMap.put("derived", true);
+ 		labelMap.put("alias", label);
+
+ 		Map<String, Object> frequencyMap = new Hashtable<String, Object>();
+ 		frequencyMap.put("header", value);
+ 		frequencyMap.put("derived", true);
+ 		frequencyMap.put("alias", value);
+
+ 		vizHeaders.add(labelMap);
+ 		vizHeaders.add(frequencyMap);
+
+ 		task.setHeaderInfo(vizHeaders);
+
+ 		Map<String, Object> formatMap = new Hashtable<String, Object>();
+ 		formatMap.put("type", "TABLE");
+ 		task.setFormatMap(formatMap);
+ 		
+ 		return task.collect(true);
+ 	}
+ 	
+ 	/**
+ 	 * This method allows us to create a grid view from the data values object[][] created with the breaks and counts
+ 	 * @param panelId - panel id
+ 	 * @param labels - column names of data table
+ 	 * @param dataValues
+ 	 */
+ 	public Map<String, Object> getGridData(String panelId, String[] labels, Object dataValues) {
+ 		// create the weird object the FE needs to paint a bar chart
+ 		ConstantDataTask task = new ConstantDataTask();
+ 		task.setId("TEMP_ID");
+ 		Map<String, Object> returnData = new Hashtable<String, Object>();
+ 		returnData.put("values", dataValues);
+ 		returnData.put("headers", labels);
+ 		task.setOutputData(returnData);
+ 		
+ 		//create maps to set the task options
+ 		//main map that will be passed into the setTaskOptions method
+ 		Map<String, Object> mapOptions = new HashMap<String, Object>();
+ 		
+ 		//this map (keyMap) comprises the mapping of both layout and alignment
+ 		Map<String, Object> keyMap = new HashMap<String, Object>(); 
+ 		keyMap.put("layout", "Grid");
+ 		
+ 		//within keyMap, we need a map to store the maps that comprise alignment
+ 		Map<String, Object> alignmentMap = new HashMap<String, Object>();
+ 		alignmentMap.put("label", labels);
+ 		keyMap.put("alignment", alignmentMap);
+ 		
+ 		mapOptions.put(panelId, keyMap);
+ 		//the final mapping looks like this:
+ 		//taskOptions={0={layout=Column, alignment={tooltip=[], label=[MovieBudget], value=[Frequency]}}}
+ 		
+ 		//set task options
+ 		task.setTaskOptions(new TaskOptions(mapOptions));
+
+ 		List<Map<String, Object>> vizHeaders = new Vector<Map<String, Object>>();
+ 		for(int i = 0; i < labels.length; i++) {
+ 			Map<String, Object> labelMap = new Hashtable<String, Object>();
+ 			labelMap.put("header", labels[i]);
+ 			labelMap.put("alias", labels[i]);
+ 			labelMap.put("derived", true);
+ 			labelMap.put("type", "STRING");
+ 			vizHeaders.add(labelMap);
+ 		}
+ 		task.setHeaderInfo(vizHeaders);
+
+ 		Map<String, Object> formatMap = new Hashtable<String, Object>();
+ 		formatMap.put("type", "TABLE");
+ 		task.setFormatMap(formatMap);
+ 		
+ 		return task.collect(true);
+ 	}
+     
 
 }
