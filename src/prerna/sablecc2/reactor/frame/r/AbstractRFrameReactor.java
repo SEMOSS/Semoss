74777f6c7a027d56dff6b41f93622248c8a8ab7b
micstone <micstone@deloitte.com>
2018-04-26 19:23:35 +0000

    UPDATE :: Adding class for RMatrixRegressionReactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27653 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8b39c6fe9efc0cbd8b6c81534428c836408b8639

--- src/prerna/sablecc2/reactor/frame/r/AbstractRFrameReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/AbstractRFrameReactor.java
@@ -335,7 +335,7 @@ public abstract class AbstractRFrameReactor extends AbstractFrameReactor {
  	}
  	
  	/**
- 	 * This method allows us to create a grid view from the data values object[][] created with the breaks and counts
+ 	 * This method allows us to create a heatmap from the data values object[][] created with the breaks and counts
  	 * @param panelId - panel id
  	 * @param x - x axis
  	 * @param y - y axis
@@ -374,6 +374,68 @@ public abstract class AbstractRFrameReactor extends AbstractFrameReactor {
 		mapOptions.put(panelId, keyMap);
 		// the final mapping looks like this:
 		// taskOptions={0={layout=Heat, alignment={label=[x, y, heat]}}}
+
+		// set task options
+		task.setTaskOptions(new TaskOptions(mapOptions));
+
+		List<Map<String, Object>> vizHeaders = new Vector<Map<String, Object>>();
+		for (int i = 0; i < labels.length; i++) {
+			Map<String, Object> labelMap = new Hashtable<String, Object>();
+			labelMap.put("header", labels[i]);
+			labelMap.put("alias", labels[i]);
+			labelMap.put("derived", true);
+			labelMap.put("type", "STRING");
+			vizHeaders.add(labelMap);
+		}
+		task.setHeaderInfo(vizHeaders);
+
+		Map<String, Object> formatMap = new Hashtable<String, Object>();
+		formatMap.put("type", "TABLE");
+		task.setFormatMap(formatMap);
+
+		return task.collect(true);
+	}
+	
+	
+	/**
+ 	 * This method allows us to create a scatterplot from the data values object[][] created with the breaks and counts
+ 	 * @param panelId - panel id
+ 	 * @param x - x axis
+ 	 * @param y - y axis
+ 	 * @param heat - heat for heat map
+ 	 * @param dataValues - this better be a List<Object[]> or Object[][] since the serialization to JSON is the same
+ 	 */
+	public Map<String, Object> getScatterPlotData(String panelId, String x, String y, Object dataValues) {
+
+		// create the object the FE needs to paint a bar chart
+		ConstantDataTask task = new ConstantDataTask();
+		task.setId("TEMP_ID");
+		Map<String, Object> returnData = new Hashtable<String, Object>();
+		returnData.put("values", dataValues);
+
+		String[] labels = new String[] { x, y};
+
+		returnData.put("headers", labels);
+		task.setOutputData(returnData);
+
+		// create maps to set the task options
+		// main map that will be passed into the setTaskOptions method
+		Map<String, Object> mapOptions = new HashMap<String, Object>();
+
+		// this map (keyMap) comprises the mapping of both layout and alignment
+		Map<String, Object> keyMap = new HashMap<String, Object>();
+		keyMap.put("layout", "Scatter");
+
+		// within keyMap, we need a map to store the maps that comprise
+		// alignment
+		Map<String, Object> alignmentMap = new HashMap<String, Object>();
+		alignmentMap.put("x", "[" + x + "]");
+		alignmentMap.put("y", "[" + y + "]");
+		keyMap.put("alignment", alignmentMap);
+
+		mapOptions.put(panelId, keyMap);
+		// the final mapping looks like this:
+		// taskOptions={0={layout=Scatter, alignment={label=[x, y]}}}
 
 		// set task options
 		task.setTaskOptions(new TaskOptions(mapOptions));
