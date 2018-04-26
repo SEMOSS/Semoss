1bac45d43e59ea766342b9f186efb885b5828b9c
micstone <micstone@deloitte.com>
2018-04-26 13:53:09 +0000

    UPDATE :: R script for numerical correlation
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27633 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a1ef3f78af73106b0d295d79db47a1d440851777

---
+++ R/AnalyticsRoutineScripts/NumericalCorrelation.R
@@ -0,0 +1,32 @@
+
+
+
+getCorrelationTable <- function(frameName, colVector) {
+	require(data.table)
+ 
+	#results <- eval(parse(text = colSelect))
+	results<-cor(frameName[,colVector, with=FALSE], use = "all.obs")
+
+	# format the results as a table where we have x,y,corVal
+	columns <- colnames(results)
+	rows <- rownames(results)
+
+	counter <- 1
+	l <- vector("list", length = nrow(results) * 2)
+
+	for (i in 1:nrow(results)) {
+
+		for (j in 1:nrow(results)) {
+		v <- c(columns[i], rows[j], results[columns[i],rows[j]])
+		l[[counter]] <- v
+		counter <- counter + 1
+		rm(v)
+		}
+	}
+
+	retTable <- as.data.table(matrix(unlist(l), ncol = 3, byrow=T),stringsAsFactors=FALSE)
+	names(retTable) <- c("Column Header X", "Column Header Y", "Correlation")
+	return (retTable)
+}
+
+
