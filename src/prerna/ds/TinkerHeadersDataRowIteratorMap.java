a90368f37af89c01cdd5985d94ec970cc6140973
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-04-26 00:45:13 +0000

    UPDATE ::: Temp graph interpreter need to update tinker frame to use type map and labels
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27619 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/ds/TinkerHeadersDataRowIteratorMap.java
@@ -0,0 +1,141 @@
+package prerna.ds;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerVertex;
+
+import prerna.engine.api.IHeadersDataRow;
+import prerna.om.HeadersDataRow;
+import prerna.query.querystruct.QueryStruct2;
+import prerna.query.querystruct.selectors.IQuerySelector;
+import prerna.query.querystruct.selectors.QueryFunctionSelector;
+
+public class TinkerHeadersDataRowIteratorMap implements Iterator<IHeadersDataRow> {
+
+	private QueryStruct2 qs;
+	private Iterator baseIterator;
+	private String[] headerAlias;
+	private String[] header;
+	private String[] headerOrdering;
+	private Map<String,String> typeMap;
+
+	public TinkerHeadersDataRowIteratorMap(Iterator composeIterator, QueryStruct2 qs, Map<String, String> typeMap) {
+		this.baseIterator = composeIterator;
+		this.qs = qs;
+		this.typeMap = typeMap;
+		flushOutHeaders(this.qs.getSelectors(), null);
+	}
+	
+	public TinkerHeadersDataRowIteratorMap(Iterator composeIterator, QueryStruct2 qs, OwlTemporalEngineMeta meta) {
+		this.baseIterator = composeIterator;
+		this.qs = qs;
+		flushOutHeaders(this.qs.getSelectors(), meta);
+	}
+
+	@Override
+	public boolean hasNext() {
+		boolean ret = baseIterator.hasNext();
+		return ret;
+	}
+
+	@Override
+	public IHeadersDataRow next() {
+		Object data = baseIterator.next();
+		Object[] retObject = null;
+
+		// data will be a map for multi nodes being returned
+		if(data instanceof Map) {
+			Map<String, Object> mapData = (Map<String, Object>) data;
+			int numSelectors = this.headerOrdering.length;
+			retObject = new Object[numSelectors];
+
+			for(int colIndex = 0; colIndex < numSelectors; colIndex++) {
+				Object vertOrProp = mapData.get(this.headerOrdering[colIndex]);
+				Object value = null;
+				if (vertOrProp instanceof Vertex) {
+					String node = this.headerOrdering[colIndex];
+					String name = this.typeMap.get(node);
+					value = ((Vertex) vertOrProp).value(name);
+				} else {
+					value = vertOrProp;
+				}
+				retObject[colIndex] = value;
+			}
+		} else {
+			// not sure what will happen once we add group bys -> is this a map like above or different???
+			// not sure what will happen once we add group bys -> is this a map like above or different???
+			// not sure what will happen once we add group bys -> is this a map like above or different???
+			// not sure what will happen once we add group bys -> is this a map like above or different???
+
+			// for right now, assuming it is just a single vertex to return
+			if(data instanceof Vertex) {
+				TinkerVertex vertex = (TinkerVertex) data;
+				String node = this.headerOrdering[0];
+				String name = this.typeMap.get(node);
+				retObject = new Object[]{vertex.value(name)};
+			} else {
+				// some object to return
+				retObject = new Object[]{data};
+			}
+		}
+
+		HeadersDataRow nextData = new HeadersDataRow(this.headerAlias, this.header, retObject);
+		return nextData;
+	}
+
+	/**
+	 * Store the order of the headers to return
+	 * @param selectors
+	 */
+	private void flushOutHeaders(List<IQuerySelector> selectors, OwlTemporalEngineMeta meta) {
+		int numHeaders = selectors.size();
+		this.header = new String[numHeaders];
+		this.headerAlias = new String[numHeaders];
+		this.headerOrdering = new String[numHeaders];
+		int index = 0;
+		for(IQuerySelector header : selectors) {
+			if(header.getSelectorType() == IQuerySelector.SELECTOR_TYPE.COLUMN) {
+				String alias = header.getAlias();
+				String qsName = header.getQueryStructName();
+				
+				this.headerOrdering[index] = qsName;
+				this.header[index] = alias;
+				this.headerAlias[index] = getNodeAlias(meta, alias);
+			} else if(header.getSelectorType() == IQuerySelector.SELECTOR_TYPE.FUNCTION) {
+				List<IQuerySelector> innerSelectorList = ((QueryFunctionSelector) header).getInnerSelector();
+				for(IQuerySelector innerSelector : innerSelectorList) {
+					if(innerSelector.getSelectorType() == IQuerySelector.SELECTOR_TYPE.COLUMN) {
+						String alias = innerSelector.getAlias();
+						String qsName = innerSelector.getQueryStructName();
+						
+						this.headerOrdering[index] = qsName;
+						this.header[index] = alias;
+						this.headerAlias[index] = getNodeAlias(meta, alias);
+					}
+				}
+			}
+			index++;
+		}
+	}
+	
+	/**
+	 * For some of the nodes that have not been given an alias
+	 * If there is an implicit alias on it (a physical name that matches an existing name)
+	 * We will use that
+	 * @param node
+	 * @return
+	 */
+	private String getNodeAlias(OwlTemporalEngineMeta meta, String node) {
+		if(meta == null) {
+			return node;
+		}
+		return meta.getPhysicalName(node);
+	}
+
+	public QueryStruct2 getQueryStruct() {
+		return this.qs;
+	}
+}
