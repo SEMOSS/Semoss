db3ac51c424ab9d8116ca768f1413dc36a309eaf
mahkhalil <mahkhalil@deloitte.com>
2018-04-27 13:12:06 +0000

    UPDATE ::: Adding random forest methods / classes
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27672 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 4c704adab0c2fbd20a58e4d9130a5206e3f7cff2

--- src/prerna/sablecc2/reactor/frame/r/AbstractRFrameReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/AbstractRFrameReactor.java
@@ -456,4 +456,85 @@ public abstract class AbstractRFrameReactor extends AbstractFrameReactor {
 
 		return task.collect(true);
 	}
+	
+ 	/**
+ 	 * This method allows us to create a grid view from the data values object[][] created with the breaks and counts
+ 	 * @param panelId - panel id
+ 	 * @param x - x axis
+ 	 * @param y - y axis
+ 	 * @param heat - heat for heat map
+ 	 * @param dataValues - this better be a List<Object[]> or Object[][] since the serialization to JSON is the same
+ 	 */
+ 	public Map<String, Object> getScatterPlotData(String panelId, String[] labels, Object dataValues, String series, 
+ 			String x, String y, String z, String label, String[] facet) {
+		// create the weird object the FE needs to paint a bar chart
+		ConstantDataTask task = new ConstantDataTask();
+		task.setId("TEMP_ID");
+		Map<String, Object> returnData = new Hashtable<String, Object>();  //returnData makes up the "data" parent element 
+		returnData.put("values", dataValues);
+		returnData.put("headers", labels);
+		task.setOutputData(returnData);
+
+		// create maps to set the task options
+		// main map that will be passed into the setTaskOptions method
+		Map<String, Object> mapOptions = new HashMap<String, Object>();
+
+		// this map (keyMap) comprises the mapping of both layout and alignment
+		Map<String, Object> keyMap = new HashMap<String, Object>();
+		keyMap.put("layout", "Scatter");
+
+		// within keyMap, we need a map to store the maps that comprise alignment
+		Map<String, Object> alignmentMap = new HashMap<String, Object>();
+		String[] xArray = new String[] {x};
+		alignmentMap.put("x",xArray);
+		String[] yArray = new String[] {y};
+		alignmentMap.put("y", yArray);
+		String[] labelArray = new String[] {label};
+		alignmentMap.put("label", labelArray);
+		//size is optional
+		if (z == null || z == "") {
+			alignmentMap.put("z", new String[]{});
+		} else {
+			String[] zArray = new String[] {z};
+			alignmentMap.put("z", zArray);
+		}
+		//color is optional
+		if (series == null || series == "") {
+			alignmentMap.put("series", new String[]{});
+		} else {
+			String[] seriesArray = new String[] {series};
+			alignmentMap.put("series", seriesArray);
+		}
+		//facet is optional
+		if (facet != null){
+			if (facet.length > 0) alignmentMap.put("facet", facet);
+		} else {
+			alignmentMap.put("facet", new String[]{});
+		}
+		keyMap.put("alignment", alignmentMap);
+
+		mapOptions.put(panelId, keyMap);
+		// the final mapping looks like this:
+		// taskOptions={0={layout=GRID, alignment={label=[Col1, Col2, Col3]}}}
+
+		// set task options
+		task.setTaskOptions(new TaskOptions(mapOptions));
+
+		List<Map<String, Object>> vizHeaders = new Vector<Map<String, Object>>();
+		for (int i = 0; i < labels.length; i++) {
+			Map<String, Object> labelMap = new Hashtable<String, Object>();
+			labelMap.put("header", labels[i]);
+			labelMap.put("alias", labels[i]);
+			labelMap.put("derived", true);
+			labelMap.put("type", "STRING");
+			vizHeaders.add(labelMap);
+		}
+		task.setHeaderInfo(vizHeaders);
+
+		Map<String, Object> formatMap = new Hashtable<String, Object>();
+		formatMap.put("type", "TABLE");
+		task.setFormatMap(formatMap);
+
+		return task.collect(true);
+	}
 }
