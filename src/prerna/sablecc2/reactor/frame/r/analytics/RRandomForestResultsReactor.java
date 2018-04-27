54dc1676636891d738bd81660d9d20bcb68c5e7b
mahkhalil <mahkhalil@deloitte.com>
2018-04-27 13:12:06 +0000

    UPDATE ::: Adding random forest methods / classes
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27672 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/sablecc2/reactor/frame/r/analytics/RRandomForestResultsReactor.java
@@ -0,0 +1,122 @@
+package prerna.sablecc2.reactor.frame.r.analytics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.frame.r.AbstractRFrameReactor;
+import prerna.util.Utility;
+
+public class RRandomForestResultsReactor extends AbstractRFrameReactor{
+	
+	private static final String CLASS_NAME = RRandomForestResultsReactor.class.getName();
+	private static final String SORTBY = "sortBy";
+	private static final String REQUESTITEM = "requestItem";  //either 'VarImp' or 'ConfMatrix'
+	/**
+	 * GetRFResults(requestItem = ['VarImp'], panel = [99])
+	 * GetRFResults(requestItem = ['CONFMATRIX'], panel = [99])
+	 */
+	public RRandomForestResultsReactor() {
+		this.keysToGet = new String[] { SORTBY, REQUESTITEM, ReactorKeysEnum.PANEL.getKey()};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		Logger logger = getLogger(CLASS_NAME);
+		init();
+		String[] packages = new String[] { "data.table", "randomForest", "dplyr" };
+		this.rJavaTranslator.checkPackages(packages);
+		String panelId = getPanelId();
+		StringBuilder sb = new StringBuilder();
+
+		// retrieve inputs
+		String sortBy = getStringInput(SORTBY);
+		if (sortBy == null) sortBy = "1";
+		String requestItem = getStringInput(REQUESTITEM);
+		if (!new ArrayList<String>(Arrays.asList("varimp", "confmatrix")).contains(requestItem.toLowerCase())){
+			throw new IllegalArgumentException("Invalid requestItem - requestItem must be either 'varimp' or 'confmatrix'.");
+		}
+		
+		// random forest r script
+		String scriptFilePath = getBaseFolder() + "\\R\\AnalyticsRoutineScripts\\RandomForest.R";
+		scriptFilePath = scriptFilePath.replace("\\", "/");
+		sb.append("source(\"" + scriptFilePath + "\");");
+
+		String temp_R = "tempVar" + Utility.getRandomString(8);
+		Map<String, Object> taskData = new HashMap<String,Object>();
+		switch(requestItem.toLowerCase()) {
+		case "varimp" :
+			sb.append(temp_R + " <- getRFResults( RF_VARIABLE_999988888877777, 'varimp', sortBy=" + sortBy + ");");
+			this.rJavaTranslator.runR(sb.toString());
+			
+			String[] varImpCols = this.rJavaTranslator.getColumns(temp_R + "$returnObject");
+			List<Object[]> varImpData = this.rJavaTranslator.getBulkDataRow(temp_R + "$returnObject", varImpCols);
+			//label,x,y,z,series
+			String[] varImpAlignment= this.rJavaTranslator.getStringArray(temp_R + "$alignmentInfo");
+			
+			taskData = getScatterPlotData(panelId, varImpCols, varImpData, varImpAlignment[0], varImpAlignment[1],
+					varImpAlignment[2], varImpAlignment[3], varImpAlignment[0], null);
+			break;
+		case "confmatrix":
+			String rfType = this.rJavaTranslator.getString("RF_VARIABLE_999988888877777$type");
+			if (rfType == "regression"){
+				throw new IllegalArgumentException("Confusion matrix is unavailable for regression-type random forest model.");
+			}
+			
+			sb.append(temp_R + " <- getRFResults( RF_VARIABLE_999988888877777, 'confmatrix', sortBy=" + sortBy + ");");
+			this.rJavaTranslator.runR(sb.toString());
+			
+			String[] confMatrixCols = this.rJavaTranslator.getColumns(temp_R);
+			List<Object[]> confMatrixData = this.rJavaTranslator.getBulkDataRow(temp_R, confMatrixCols);
+			
+			taskData = getGridData(panelId, confMatrixCols, confMatrixData);			
+			break;
+		}
+
+		// clean up r temp variables
+		StringBuilder cleanUpScript = new StringBuilder();
+		cleanUpScript.append("rm(" + temp_R + ",getRF,getRFResults);");
+		cleanUpScript.append("gc();");
+		this.rJavaTranslator.runR(cleanUpScript.toString());
+		
+		return new NounMetadata(taskData, PixelDataType.TASK, PixelOperationType.TASK_DATA);
+	}
+
+	//////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////
+	////////////////////// Input Methods///////////////////////////
+	//////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////
+
+	private String getStringInput(String keyName) {
+		// see if defined as individual key
+		GenRowStruct columnGrs = this.store.getNoun(keyName);
+		if (columnGrs != null) {
+			if ( columnGrs.size() > 0 ) return columnGrs.get(0).toString();
+		} else {
+			if (keyName == REQUESTITEM) {
+				throw new IllegalArgumentException("RequestItem of either 'varimp' or 'confmatrix' must be specified.");
+			}
+		}
+		return null;
+	}
+
+	private String getPanelId() {
+		// see if defined as individual key
+		GenRowStruct columnGrs = this.store.getNoun(this.keysToGet[2]);
+		if (columnGrs != null) {
+			if ( columnGrs.size() > 0 ) return columnGrs.get(0).toString();
+		}
+		return null;
+	}
+
+}
