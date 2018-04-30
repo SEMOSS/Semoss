6deca6e4be29574d2213b058e92fade21da3e961
mahkhalil <mahkhalil@deloitte.com>
2018-04-30 21:24:53 +0000

    UPDATE ::: Adding PostRequest which takes in a header map. TODO: add body
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27743 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 65a9cbeed430f3f7b43bd6de3e1bd110943c8676

---
+++ src/prerna/sablecc2/reactor/utils/PostRequestReactor.java
@@ -0,0 +1,96 @@
+package prerna.sablecc2.reactor.utils;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class PostRequestReactor extends AbstractReactor {
+
+	public PostRequestReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.URL.getKey(), "headersMap"};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String url = this.keyValue.get(this.keysToGet[0]);
+		List<Map<String, String>> headersMap = getHeadersMap();
+				
+		ResponseHandler<String> handler = new BasicResponseHandler();
+		CloseableHttpResponse response = null;
+		try {
+			SSLContextBuilder builder = new SSLContextBuilder();
+			builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
+			SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build());
+			CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+			HttpPost httpGet = new HttpPost(url);
+			if(headersMap != null && !headersMap.isEmpty()) {
+				for(int i = 0; i < headersMap.size(); i++) {
+					Map<String, String> head = headersMap.get(i);
+					for(String key : head.keySet()) {
+						httpGet.addHeader(key, head.get(key));
+					}
+				}
+			}
+			response = httpClient.execute(httpGet);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Could not connect to URL at " + url);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Could not connect to URL at " + url);
+		} catch (KeyStoreException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Could not connect to URL at " + url);
+		} catch (KeyManagementException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Could not connect to URL at " + url);
+		}
+		
+		String retString = null;
+		try {
+			retString = handler.handleResponse(response);
+		} catch (IOException e) {
+			throw new IllegalArgumentException("Could not connect to URL at " + url);
+		}
+		
+		return new NounMetadata(retString, PixelDataType.CONST_STRING);
+	}
+
+	/**
+	 * Get headers to add to the request
+	 * @return
+	 */
+	private List<Map<String, String>> getHeadersMap() {
+		GenRowStruct headersGrs = this.store.getNoun(this.keysToGet[1]);
+		if(headersGrs != null && !headersGrs.isEmpty()) {
+			List<Map<String, String>> headers = new Vector<Map<String, String>>();
+			for(int i = 0; i < headersGrs.size(); i++) {
+				headers.add( (Map<String, String>) headersGrs.get(i)); 
+			}
+			return headers;
+		}
+		return null;
+	}
+
+}
