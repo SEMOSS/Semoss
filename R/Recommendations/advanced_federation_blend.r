349d90be64aefb80e95e3a761742b0b389c58568
mahkhalil <mahkhalil@deloitte.com>
2018-04-30 14:44:29 +0000

    UPDATE ::: Pushing update for advanced federate UPDATE ::: Adding new utility to input separator when flushing out iterator to file
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27716 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- R/Recommendations/advanced_federation_blend.r
+++ R/Recommendations/advanced_federation_blend.r
@@ -1,33 +1,28 @@
-best_match_zero<-function(col1,col2){
-	z<-best_match(col1,col2)
+best_match_zero<-function(col1,col2,method="jw",p=0.1){
+	z<-best_match(col1,col2,method,p)
 	z<-z[z$dist == 0,]
 	gc()
 	return(z)
 }
 
-best_match_nonzero<-function(col1,col2){
-	z<-best_match(col1,col2)
+best_match_nonzero<-function(col1,col2,method="jw",p=0.1){
+	z<-best_match(col1,col2,method,p)
 	z<-z[z$dist != 0,]
 	gc()
 	return(z)
 }
 
 best_match<-function(col1,col2,method="jw",p=0.1){
-	# Returns the best match for each instance and the respected distance
-	# Arguments
-	# col1 - an array of instances of the column to be matched
-	# col1 - an array of instances of the matching column
-	# minimum distance allowed
-	
 	library(stringdist)
-	dist<-stringdistmatrix(col1,col2,method=method,p=p)
-	mins<-apply(dist,1,function(x)return(array(which.min(x))))
-	z<-data.frame(col1);
-	z$col2<-col2[mins]
-	z$dist<-apply(dist,1,function(x)return(array(min(x))))
-	z$dist<-round(z$dist,4)
+	d<-stringdistmatrix(col1,col2,method=method,p=p)
+	n<-dim(d)
+	x<-do.call("rbind",lapply(d,data.frame))
+	y<-do.call("rbind",lapply(rep(col1,n[2]),data.frame))
+	z<-do.call("rbind",lapply(rep(col2,each=n[1]),data.frame))
+	r<-cbind(y,z,x)
+	names(r)<-c("col1","col2","dist")
 	gc()
-	return(z)
+	return(r)
 }
 
 blend<-function(src,srccol,trg,trgcol,link,jointype="inner"){
@@ -40,31 +35,26 @@ blend<-function(src,srccol,trg,trgcol,link,jointype="inner"){
 	# link - two column dataframe defining the blend selection previously established
 	# jointype - type of join requested: left, right, full. Anything else interpreted as inner
 	
-	link$id<-seq(1:nrow(link))
-	m<-nrow(src)+nrow(trg)
+	link$i.and.d<-seq(1:nrow(link))
+	m<-max(nrow(src)+nrow(trg),nrow(src)*nrow(trg))
 	
 	cmd<-paste0("r1<-merge(src,link,by.x=\"",srccol,"\",by.y=\"",colnames(link)[1],"\",all.x=TRUE)")
 	eval(parse( text=cmd ))
-	r1[is.na(r1$id),"id"]<-m+1
+	r1[is.na(r1$i.and.d),"i.and.d"]<-m+1
 	
 	cmd<-paste0("r2<-merge(trg,link,by.x=\"",trgcol,"\",by.y=\"",colnames(link)[2],"\",all.x=TRUE)")
 	eval(parse( text=cmd ))
-	r2[is.na(r2$id),"id"]<-m+2
+	r2[is.na(r2$i.and.d),"i.and.d"]<-m+2
 	
 	if(jointype=="left"){
-		r<-merge(r1,r2,by="id",all.x=TRUE)
+		r<-merge(r1,r2,by="i.and.d",all.x=TRUE)
 	}else if(jointype=="right"){
-		r<-merge(r1,r2,by="id",all.y=TRUE)
+		r<-merge(r1,r2,by="i.and.d",all.y=TRUE)
 	}else if(jointype=="full"){
-		r<-merge(r1,r2,by="id",all=TRUE)
+		r<-merge(r1,r2,by="i.and.d",all=TRUE)
 	}else{
-		r<-merge(r1,r2,by="id")
+		r<-merge(r1,r2,by="i.and.d")
 	}
-	#r<-r[,ifelse(!(colnames(r) %in% c(colnames(link)[1],colnames(link)[2],"id")))]
-	#list <- c("id","col1","col2","dist.y","dist.x")
-	#r<-r[,colnames(r) %in% list]
-	#	r<-r[,colnames(r) %in% c("id","col1","col2","dist.y","dist.x")]
-# 
 	gc()
 	return(r)
 	
