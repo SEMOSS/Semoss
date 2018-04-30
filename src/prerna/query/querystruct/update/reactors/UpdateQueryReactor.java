964e5fcf4af9d62b608e0c9d4bad9f85f48c0878
Maher Khalil <themaherkhalil@gmail.com>
2018-04-30 14:25:41 +0000

    UPDATE ::: Adding placeholder reactor for update query reactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27715 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ac2cea6a6d3af59e5c6c7985485204260e5a436e

---
+++ src/prerna/query/querystruct/update/reactors/UpdateQueryReactor.java
@@ -0,0 +1,14 @@
+package prerna.query.querystruct.update.reactors;
+
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class UpdateQueryReactor extends AbstractReactor {
+
+	@Override
+	public NounMetadata execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
