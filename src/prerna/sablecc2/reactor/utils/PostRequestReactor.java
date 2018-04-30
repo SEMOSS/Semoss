931f962fea124866d25aad782522d084fca7ff0c
mahkhalil <mahkhalil@deloitte.com>
2018-04-30 21:30:08 +0000

    UPDATE ::: Adding body params into post request reactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27744 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 67a878eb1c238deb1bbfe8d3a402781b3ecd3399

--- src/prerna/sablecc2/reactor/utils/PostRequestReactor.java
+++ src/prerna/sablecc2/reactor/utils/PostRequestReactor.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
@@ -16,6 +19,7 @@ import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.ssl.SSLContextBuilder;
 
 import prerna.sablecc2.om.GenRowStruct;
@@ -27,7 +31,7 @@ import prerna.sablecc2.reactor.AbstractReactor;
 public class PostRequestReactor extends AbstractReactor {
 
 	public PostRequestReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.URL.getKey(), "headersMap"};
+		this.keysToGet = new String[]{ReactorKeysEnum.URL.getKey(), "headersMap", "body"};
 	}
 	
 	@Override
@@ -35,7 +39,8 @@ public class PostRequestReactor extends AbstractReactor {
 		organizeKeys();
 		String url = this.keyValue.get(this.keysToGet[0]);
 		List<Map<String, String>> headersMap = getHeadersMap();
-				
+		List<Map<String, String>> bodyMap = getBody();
+	
 		ResponseHandler<String> handler = new BasicResponseHandler();
 		CloseableHttpResponse response = null;
 		try {
@@ -43,16 +48,26 @@ public class PostRequestReactor extends AbstractReactor {
 			builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
 			SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build());
 			CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
-			HttpPost httpGet = new HttpPost(url);
+			HttpPost httpPost = new HttpPost(url);
 			if(headersMap != null && !headersMap.isEmpty()) {
 				for(int i = 0; i < headersMap.size(); i++) {
 					Map<String, String> head = headersMap.get(i);
 					for(String key : head.keySet()) {
-						httpGet.addHeader(key, head.get(key));
+						httpPost.addHeader(key, head.get(key));
 					}
 				}
 			}
-			response = httpClient.execute(httpGet);
+			if(bodyMap != null && !bodyMap.isEmpty()) {
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				for(int i = 0; i < bodyMap.size(); i++) {
+					Map<String, String> body = bodyMap.get(i);
+					for(String key : body.keySet()) {
+						params.add(new BasicNameValuePair(key, body.get(key)));
+					}
+				}
+				httpPost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+			}
+			response = httpClient.execute(httpPost);
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new IllegalArgumentException("Could not connect to URL at " + url);
@@ -89,6 +104,22 @@ public class PostRequestReactor extends AbstractReactor {
 				headers.add( (Map<String, String>) headersGrs.get(i)); 
 			}
 			return headers;
+		}
+		return null;
+	}
+	
+	/**
+	 * Get headers to add to the request
+	 * @return
+	 */
+	private List<Map<String, String>> getBody() {
+		GenRowStruct bodyGrs = this.store.getNoun(this.keysToGet[2]);
+		if(bodyGrs != null && !bodyGrs.isEmpty()) {
+			List<Map<String, String>> body = new Vector<Map<String, String>>();
+			for(int i = 0; i < bodyGrs.size(); i++) {
+				body.add( (Map<String, String>) bodyGrs.get(i)); 
+			}
+			return body;
 		}
 		return null;
 	}
