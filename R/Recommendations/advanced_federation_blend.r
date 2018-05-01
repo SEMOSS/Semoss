ecb97c9cfe40e3726f01ca1106317c719d0ed932
mahkhalil <mahkhalil@deloitte.com>
2018-05-01 03:49:58 +0000

    UPDATE ::: Best match returns a reference to a RDataTable object instead of a list of matches. Blend accepts selected matches and non-matches along with an optional propagation value. UPDATE ::: R script best_match_lessthan function added in place of best_match_zero, it accepts a parameter for matches less than or equal to some distance.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27753 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: be3a9c775d0e5da6886660bfb9f2efd0dec8dda2

--- R/Recommendations/advanced_federation_blend.r
+++ R/Recommendations/advanced_federation_blend.r
@@ -1,28 +1,40 @@
-best_match_zero<-function(col1,col2,method="jw",p=0.1){
-	z<-best_match(col1,col2,method,p)
+best_match_zero<-function(col1,col2){
+	z<-best_match(col1,col2)
 	z<-z[z$dist == 0,]
 	gc()
 	return(z)
 }
 
-best_match_nonzero<-function(col1,col2,method="jw",p=0.1){
-	z<-best_match(col1,col2,method,p)
+best_match_nonzero<-function(col1,col2){
+	z<-best_match(col1,col2)
 	z<-z[z$dist != 0,]
 	gc()
 	return(z)
 }
 
-best_match<-function(col1,col2,method="jw",p=0.1){
-	library(stringdist)
-	d<-stringdistmatrix(col1,col2,method=method,p=p)
-	n<-dim(d)
-	x<-do.call("rbind",lapply(d,data.frame))
-	y<-do.call("rbind",lapply(rep(col1,n[2]),data.frame))
-	z<-do.call("rbind",lapply(rep(col2,each=n[1]),data.frame))
-	r<-cbind(y,z,x)
-	names(r)<-c("col1","col2","dist")
+best_match_lessthan<-function(col1,col2,maxdist=0,method="jw",p=0.1){
+	z<-best_match(col1,col2,method,p)
+	z<-z[z$dist <= maxdist,]
 	gc()
-	return(r)
+	return(z)
+}
+
+best_match<-function(col1,col2,method="jw",p=0.1){
+	# Returns the best match for each instance and the respected distance
+	# Arguments
+	# col1 - an array of instances of the column to be matched
+	# col1 - an array of instances of the matching column
+	# minimum distance allowed
+	
+	library(stringdist)
+	dist<-stringdistmatrix(col1,col2,method=method,p=p)
+	mins<-apply(dist,1,function(x)return(array(which.min(x))))
+	z<-data.frame(col1);
+	z$col2<-col2[mins]
+	z$dist<-apply(dist,1,function(x)return(array(min(x))))
+	z$dist<-round(z$dist,4)
+	gc()
+	return(z)
 }
 
 blend<-function(src,srccol,trg,trgcol,link,jointype="inner"){
@@ -36,7 +48,7 @@ blend<-function(src,srccol,trg,trgcol,link,jointype="inner"){
 	# jointype - type of join requested: left, right, full. Anything else interpreted as inner
 	
 	link$i.and.d<-seq(1:nrow(link))
-	m<-max(nrow(src)+nrow(trg),nrow(src)*nrow(trg))
+	m<-nrow(src)+nrow(trg)
 	
 	cmd<-paste0("r1<-merge(src,link,by.x=\"",srccol,"\",by.y=\"",colnames(link)[1],"\",all.x=TRUE)")
 	eval(parse( text=cmd ))
@@ -55,6 +67,11 @@ blend<-function(src,srccol,trg,trgcol,link,jointype="inner"){
 	}else{
 		r<-merge(r1,r2,by="i.and.d")
 	}
+	#r<-r[,ifelse(!(colnames(r) %in% c(colnames(link)[1],colnames(link)[2],"i.and.d")))]
+	#list <- c("i.and.d","col1","col2","dist.y","dist.x")
+	#r<-r[,colnames(r) %in% list]
+	#	r<-r[,colnames(r) %in% c("i.and.d","col1","col2","dist.y","dist.x")]
+# 
 	gc()
 	return(r)
 	
