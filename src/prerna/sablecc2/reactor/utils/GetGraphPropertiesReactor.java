c37be95d6f7e949bdcb2db7ff7fa4ae30667f56f
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-05-01 15:53:45 +0000

    UPDATE ::: Get Graph properties to specify type and getting metamodel from the property need to predict property types
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27769 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 46493e9857aae20025e5161fb32d14b28f4cf5c9

--- src/prerna/sablecc2/reactor/utils/GetGraphPropertiesReactor.java
+++ src/prerna/sablecc2/reactor/utils/GetGraphPropertiesReactor.java
@@ -3,15 +3,9 @@ package prerna.sablecc2.reactor.utils;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 
 import org.apache.tinkerpop.gremlin.neo4j.structure.Neo4jGraph;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__;
-import org.apache.tinkerpop.gremlin.structure.Edge;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 import org.apache.tinkerpop.gremlin.structure.io.Io.Builder;
@@ -29,10 +23,9 @@ import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.util.MyGraphIoRegistry;
 
-public class GetGraphMetaModelReactor extends AbstractReactor {
-
-	public GetGraphMetaModelReactor() {
-		this.keysToGet = new String[] { ReactorKeysEnum.FILE_PATH.getKey(), "type" };
+public class GetGraphPropertiesReactor extends AbstractReactor {
+	public GetGraphPropertiesReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.FILE_PATH.getKey() };
 	}
 
 	@Override
@@ -42,15 +35,13 @@ public class GetGraphMetaModelReactor extends AbstractReactor {
 		 */
 		organizeKeys();
 		String fileName = this.keyValue.get(this.keysToGet[0]);
-		String prop = this.keyValue.get(this.keysToGet[1]);
-
-		HashMap<String, Object> retMap = new HashMap<String, Object>();
 		TINKER_DRIVER tinkerDriver = TINKER_DRIVER.NEO4J;
 		if (fileName.contains(".")) {
 			String fileExtension = fileName.substring(fileName.indexOf(".") + 1);
 			tinkerDriver = TINKER_DRIVER.valueOf(fileExtension.toUpperCase());
 		}
 		Graph g = null;
+		ArrayList<String> properties = new ArrayList<>();
 		/*
 		 * Open Graph
 		 */
@@ -93,71 +84,17 @@ public class GetGraphMetaModelReactor extends AbstractReactor {
 				e.printStackTrace();
 			}
 
-		}
-		Map<String, ArrayList<String>> edgeMap = new HashMap<>();
-		Map<String, Map<String, String>> nodes = new HashMap<>();
-
-		/*
-		 * Get Nodes and Edges
-		 */
-		if (g != null) {
-			// get concepts and properties
-			GraphTraversal<Vertex, Map<Object, Object>> gtTest = g.traversal().V().group().by(__.values(prop));
-
-			// get the types from the specified prop key
-			Set<Object> types = null;
-			while (gtTest.hasNext()) {
-				Map<Object, Object> v = gtTest.next();
-				types = v.keySet();
-			}
-			if (types != null) {
-				for (Object t : types) {
-					// get the properties for each type
-					GraphTraversal<Vertex, String> x = g.traversal().V().has(prop, t).properties().key().dedup();
-					Map<String, String> propMap = new HashMap<>();
-					while (x.hasNext()) {
-						String nodeProp = x.next();
-						// TODO get type
-						String nodePropType = "STRING";
-						propMap.put(nodeProp, nodePropType);
-					}
-					nodes.put(t.toString(), propMap);
+			// get graph properties
+			if (g != null) {
+				GraphTraversal<Vertex, String> x = g.traversal().V().properties().key().dedup();
+				while (x.hasNext()) {
+					String prop = x.next();
+					properties.add(prop);
 				}
 			}
 
-			Iterator<Edge> edges = g.edges();
-			while (edges.hasNext()) {
-				Edge e = edges.next();
-				String edgeLabel = e.label();
-				Vertex outV = e.outVertex();
-				Set<String> outVKeys = outV.keys();
-				Vertex inV = e.inVertex();
-				Set<String> inVKeys = inV.keys();
-				if (outVKeys.contains(prop) && inVKeys.contains(prop)) {
-					Object outVLabel = outV.value(prop);
-					Object inVLabel = inV.value(prop);
-					if (!edgeMap.containsKey(edgeLabel)) {
-						ArrayList<String> vertices = new ArrayList<>();
-						vertices.add(outVLabel.toString());
-						vertices.add(inVLabel.toString());
-						edgeMap.put(edgeLabel, vertices);
-					}
-				}
-			}
 		}
-		if (tinkerDriver == TINKER_DRIVER.NEO4J) {
-			try {
-				g.close();
-			} catch (Exception e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-		}
-		if (g != null) {
-			retMap.put("nodes", nodes);
-			retMap.put("edges", edgeMap);
-		}
-		return new NounMetadata(retMap, PixelDataType.MAP);
+		return new NounMetadata(properties, PixelDataType.CUSTOM_DATA_STRUCTURE);
 	}
 
 }
