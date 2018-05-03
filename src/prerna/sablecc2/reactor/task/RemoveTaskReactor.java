13ddb0b09f3ccda6f5af0b73fa2991320ee92817
mahkhalil <mahkhalil@deloitte.com>
2018-05-03 20:26:36 +0000

    UPDATE ::: Adding fix to remove task only at end
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27879 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 5c682c831e19ba1f8ffbaf1e3863b99b0120dabe

--- src/prerna/sablecc2/reactor/task/RemoveTaskReactor.java
+++ src/prerna/sablecc2/reactor/task/RemoveTaskReactor.java
@@ -1,10 +1,8 @@
 package prerna.sablecc2.reactor.task;
 
 import prerna.sablecc2.om.PixelDataType;
-import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
-import prerna.sablecc2.om.task.ITask;
 import prerna.sablecc2.reactor.AbstractReactor;
 
 public class RemoveTaskReactor extends AbstractReactor {
@@ -17,11 +15,6 @@ public class RemoveTaskReactor extends AbstractReactor {
 	public NounMetadata execute() {
 		// get the task id
 		String taskId = this.curRow.get(0).toString();
-		// get the task object
-		ITask task = this.insight.getTaskStore().getTask(taskId);
-		// remove the task id
-		this.insight.getTaskStore().removeTask(taskId);
-		// return the task object so we know what was removed
-		return new NounMetadata(task, PixelDataType.TASK, PixelOperationType.REMOVE_TASK);
+		return new NounMetadata(taskId, PixelDataType.REMOVE_TASK);
 	}
 }
