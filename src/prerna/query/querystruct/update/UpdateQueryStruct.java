0978985066b06eb910512175fb294f1b9926bd10
mahkhalil <mahkhalil@deloitte.com>
2018-05-03 14:38:58 +0000

    UPDATE ::: Using abstract methods / classes for update / select qs
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27842 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: cfaa4f21ee06138b42d2f6a45e4eb8f806aeaf02

--- src/prerna/query/querystruct/update/UpdateQueryStruct.java
+++ src/prerna/query/querystruct/update/UpdateQueryStruct.java
@@ -1,36 +1,15 @@
 package prerna.query.querystruct.update;
 
 import java.util.ArrayList;
-import java.util.Hashtable;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
 
-import prerna.algorithm.api.ITableDataFrame;
-import prerna.engine.api.IEngine;
 import prerna.query.querystruct.AbstractQueryStruct;
-import prerna.query.querystruct.filters.GenRowFilters;
-import prerna.query.querystruct.filters.IQueryFilter;
 import prerna.query.querystruct.selectors.QueryColumnSelector;
 
 public class UpdateQueryStruct extends AbstractQueryStruct {
 	
-	protected transient ITableDataFrame frame;
-	protected transient IEngine engine;
-	
 	private List<QueryColumnSelector> selectors = new ArrayList<>();
 	private List<Object> values = new ArrayList<>();
-	
-	protected Map <String, Map<String, List>> relations = new Hashtable<String, Map<String, List>>();
-	
-	protected GenRowFilters explicitFilters = new GenRowFilters();
-	protected GenRowFilters implicitFilters = new GenRowFilters();
-	// filters on derived calculations
-	protected GenRowFilters havingFilters = new GenRowFilters();
-	
-	protected boolean overrideImplicit = false;
-
 	
 	/**
 	 * Default constructor
@@ -51,7 +30,7 @@ public class UpdateQueryStruct extends AbstractQueryStruct {
 	
 	public void addSelector(String concept, String property) {
 		if(property == null) {
-			property = PRIM_KEY_PLACEHOLDER; 
+			property = AbstractQueryStruct.PRIM_KEY_PLACEHOLDER; 
 		}
 		QueryColumnSelector selector = new QueryColumnSelector();
 		selector.setTable(concept);
@@ -73,136 +52,5 @@ public class UpdateQueryStruct extends AbstractQueryStruct {
 	public void setValues(List<Object> values) {
 		this.values = values;
 	}
-	
-	//////////////////////////////////////////// FILTERING /////////////////////////////////////////////////
-	/*
-	 * Explicit filters alter entire query
-	 * Implicit filters alter visualizations
-	 *  
-	 */
-
-	
-	public void addExplicitFilter(IQueryFilter newFilter) {
-		GenRowFilters newGrf = new GenRowFilters();
-		newGrf.addFilters(newFilter);
-		this.explicitFilters.merge(newGrf);
-	}
-	
-	public GenRowFilters getExplicitFilters() {
-		return this.explicitFilters;
-	}
-	
-	public void setExplicitFilters(GenRowFilters filters) {
-		this.explicitFilters = filters;
-	}
-	
-	public void addImplicitFilter(IQueryFilter newFilter) {
-		GenRowFilters newGrf = new GenRowFilters();
-		newGrf.addFilters(newFilter);
-		this.implicitFilters.merge(newGrf);
-	}
-	
-	public GenRowFilters getImplicitFilters() {
-		return this.implicitFilters;
-	}
-	
-	public void setImplicitFilters(GenRowFilters filters) {
-		this.implicitFilters = filters;
-	}
-	
-	public void addHavingFilter(IQueryFilter newFilter) {
-		GenRowFilters newGrf = new GenRowFilters();
-		newGrf.addFilters(newFilter);
-		this.havingFilters.merge(newGrf);
-	}
-	
-	public GenRowFilters getHavingFilters() {
-		return this.havingFilters;
-	}
-	
-	public void setHavingFilters(GenRowFilters filters) {
-		this.havingFilters = filters;
-	}
-	
-	public GenRowFilters getCombinedFilters() {
-		GenRowFilters combinedFilters = new GenRowFilters();
-		combinedFilters.merge(this.explicitFilters.copy());
-		if(this.overrideImplicit) {
-			// if the user already filtered the column
-			// do not try to merge the other filters
-			// that are held on the data source
-			Set<String> explicitFilteredColumn = combinedFilters.getAllFilteredColumns();
-			int numFilters = this.implicitFilters.size();
-			List<IQueryFilter> implicitFilterVec = this.implicitFilters.getFilters();
-			for(int i = 0; i < numFilters; i++) {
-				IQueryFilter f = implicitFilterVec.get(i);
-				if(!containsAny(f.getAllUsedColumns(), explicitFilteredColumn)) {
-					// user didn't choose a different filter value for the column
-					// so we can add it
-					combinedFilters.addFilters(f);
-				}
-			}
-		} else {
-			combinedFilters.merge(this.implicitFilters.copy());
-		}
-		return combinedFilters;
-	}
-	
-	private boolean containsAny(Set<String> valuesToFind, Set<String> allValues) {
-		for(String valToFind : valuesToFind) {
-			if(allValues.contains(valToFind)) {
-				return true;
-			}
-		}
-			
-		return false;
-	}
-
-	//////////////////////////////////////////// JOINING ////////////////////////////////////////////////////
-	public void setRelations(Map<String, Map<String, List>> relations) {
-		this.relations = relations;
-	}
-	
-	public void addRelation(String fromConcept, String toConcept, String joinType)
-	{
-		// I need pick the keys from the table based on relationship and then add that to the relation
-		// need to figure out type of 
-		// find if this property is there
-		// ok if the logical name stops being unique this will have some weird results
-		
-		
-		Map <String, List> compHash = new Hashtable<String, List>();
-		if(relations.containsKey(fromConcept))
-			compHash = relations.get(fromConcept);
-		
-		List curData = new Vector();
-		// next piece is to see if we have the comparator
-		if(compHash.containsKey(joinType))
-			curData = compHash.get(joinType);
-		
-		curData.add(toConcept);
-		
-		// put it back
-		compHash.put(joinType, curData);	
-		
-		// put it back
-		relations.put(fromConcept, compHash);
-	}
-	
-	public Map<String, Map<String, List>> getRelations(){
-		return this.relations;
-	}
-	
-	//////////////////////////////////////////// OTHERS /////////////////////////////////////////////////////
-	public void setOverrideImplicit(boolean overrideImplicit) {
-		this.overrideImplicit = overrideImplicit;
-	}
-	
-	public boolean isOverrideImplicit() {
-		return this.overrideImplicit;
-	}
-	//////////////////////////////////////////// SETTERS AND GETTERS ////////////////////////////////////////
-	//////////////////////////////////////////// TASK META INFO /////////////////////////////////////////////
-
 	
 }
