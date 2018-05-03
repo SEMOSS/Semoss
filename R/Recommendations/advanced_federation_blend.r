faf15fe157032fb6361309e9beb53e6c81e3325a
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-05-03 14:21:59 +0000

    BUGFIX ::: Blend works when columns are numbers. R script allows for Cartesian matches and joins.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27839 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: bd7ad6fc0de5c7588798bc9f73894c5beeb83c26

--- R/Recommendations/advanced_federation_blend.r
+++ R/Recommendations/advanced_federation_blend.r
@@ -1,40 +1,32 @@
-best_match_zero<-function(col1,col2){
-	z<-best_match(col1,col2)
+best_match_zero<-function(col1,col2,method="jw",p=0.1){
+	z<-best_match(col1,col2,method,p)
 	z<-z[z$dist == 0,]
 	gc()
 	return(z)
 }
 
-best_match_nonzero<-function(col1,col2){
-	z<-best_match(col1,col2)
+best_match_nonzero<-function(col1,col2,method="jw",p=0.1){
+	z<-best_match(col1,col2,method,p)
 	z<-z[z$dist != 0,]
 	gc()
 	return(z)
 }
 
-best_match_lessthan<-function(col1,col2,maxdist=0,method="jw",p=0.1){
-	z<-best_match(col1,col2,method,p)
-	z<-z[z$dist <= maxdist,]
-	gc()
-	return(z)
-}
-
 best_match<-function(col1,col2,method="jw",p=0.1){
-	# Returns the best match for each instance and the respected distance
-	# Arguments
-	# col1 - an array of instances of the column to be matched
-	# col1 - an array of instances of the matching column
-	# minimum distance allowed
-	
 	library(stringdist)
-	dist<-stringdistmatrix(col1,col2,method=method,p=p)
-	mins<-apply(dist,1,function(x)return(array(which.min(x))))
-	z<-data.frame(col1);
-	z$col2<-col2[mins]
-	z$dist<-apply(dist,1,function(x)return(array(min(x))))
-	z$dist<-round(z$dist,4)
+	c1<-unique(col1)
+	c2<-unique(col2)
+	d<-stringdistmatrix(c1,c2,method=method,p=p)
+	n<-dim(d)
+	x<-as.vector(t(d))
+	x<-round(x,4)
+	y<-rep(c1,each=n[2])
+	z<-rep(c2,n[1])
+	r<-as.data.frame(cbind(y,z,x))
+	r[,3] <- as.numeric(as.character(r[,3]))
+	names(r)<-c("col1","col2","dist")
 	gc()
-	return(z)
+	return(r)
 }
 
 blend<-function(src,srccol,trg,trgcol,link,jointype="inner"){
@@ -48,9 +40,9 @@ blend<-function(src,srccol,trg,trgcol,link,jointype="inner"){
 	# jointype - type of join requested: left, right, full. Anything else interpreted as inner
 	
 	link$i.and.d<-seq(1:nrow(link))
-	m<-nrow(src)+nrow(trg)
+	m<-max(nrow(src)+nrow(trg),nrow(src)*nrow(trg))
 	
-	cmd<-paste0("r1<-merge(src,link,by.x=\"",srccol,"\",by.y=\"",colnames(link)[1],"\",all.x=TRUE)")
+	cmd<-paste0("r1<-merge(src,link,by.x=\"",srccol,"\",by.y=\"",colnames(link)[1],"\",all.x=TRUE,allow.cartesian=TRUE)")
 	eval(parse( text=cmd ))
 	r1[is.na(r1$i.and.d),"i.and.d"]<-m+1
 	
@@ -59,19 +51,14 @@ blend<-function(src,srccol,trg,trgcol,link,jointype="inner"){
 	r2[is.na(r2$i.and.d),"i.and.d"]<-m+2
 	
 	if(jointype=="left"){
-		r<-merge(r1,r2,by="i.and.d",all.x=TRUE)
+		r<-merge(r1,r2,by="i.and.d",all.x=TRUE,allow.cartesian=TRUE)
 	}else if(jointype=="right"){
-		r<-merge(r1,r2,by="i.and.d",all.y=TRUE)
+		r<-merge(r1,r2,by="i.and.d",all.y=TRUE,allow.cartesian=TRUE)
 	}else if(jointype=="full"){
-		r<-merge(r1,r2,by="i.and.d",all=TRUE)
+		r<-merge(r1,r2,by="i.and.d",all=TRUE,allow.cartesian=TRUE)
 	}else{
-		r<-merge(r1,r2,by="i.and.d")
+		r<-merge(r1,r2,by="i.and.d",allow.cartesian=TRUE)
 	}
-	#r<-r[,ifelse(!(colnames(r) %in% c(colnames(link)[1],colnames(link)[2],"i.and.d")))]
-	#list <- c("i.and.d","col1","col2","dist.y","dist.x")
-	#r<-r[,colnames(r) %in% list]
-	#	r<-r[,colnames(r) %in% c("i.and.d","col1","col2","dist.y","dist.x")]
-# 
 	gc()
 	return(r)
 	
