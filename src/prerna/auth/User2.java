8f6040df853e3c2db52789ccc2c7f386ff154b08
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2018-05-04 17:01:48 +0000

    Updates to get all logins
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27894 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/auth/User2.java
+++ src/prerna/auth/User2.java
@@ -1,8 +1,8 @@
 package prerna.auth;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
-import java.util.Vector;
 
 import prerna.om.AbstractValueObject;
 
@@ -13,11 +13,14 @@ public class User2 extends AbstractValueObject{
 	
 	// need to have an access token store
 	Hashtable <String, AccessToken> accessTokens = new Hashtable<String, AccessToken>();
+	List <String> loggedInProfiles = new ArrayList<String>();
 	
 	
 	public void setAccessToken(AccessToken value)
 	{
 		String name = value.getProvider();
+		if(!loggedInProfiles.contains(name))
+			loggedInProfiles.add(name);
 		accessTokens.put(name, value);
 	}
 	
@@ -32,6 +35,10 @@ public class User2 extends AbstractValueObject{
 		accessTokens.remove(name);
 	}
 	
+	public List getLogins()
+	{
+		return loggedInProfiles;
+	}
 	
 	
 
