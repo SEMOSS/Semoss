b219f33c608d221690437e8230109958803c50a2
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-05-04 19:54:24 +0000

    BUGFIX ::: Receives table__column so that iterator queries rdf correctly for primary key columns.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@27899 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- R/Recommendations/advanced_federation_blend.r
+++ R/Recommendations/advanced_federation_blend.r
@@ -12,11 +12,15 @@ best_match_nonzero<-function(col1,col2,method="jw",p=0.1){
 	return(z)
 }
 
-best_match<-function(col1,col2,method="jw",p=0.1){
+best_match<-function(col1,col2,ignoreCase=TRUE,method="jw",p=0.1){
 	library(stringdist)
 	c1<-unique(col1)
 	c2<-unique(col2)
-	d<-stringdistmatrix(c1,c2,method=method,p=p)
+	if(ignoreCase){
+		d<-stringdistmatrix(tolower(c1),tolower(c2),method=method,p=p)
+	}else{
+		d<-stringdistmatrix(c1,c2,method=method,p=p)
+	}
 	n<-dim(d)
 	x<-as.vector(t(d))
 	x<-round(x,4)
@@ -46,7 +50,7 @@ blend<-function(src,srccol,trg,trgcol,link,jointype="inner"){
 	eval(parse( text=cmd ))
 	r1[is.na(r1$i.and.d),"i.and.d"]<-m+1
 	
-	cmd<-paste0("r2<-merge(trg,link,by.x=\"",trgcol,"\",by.y=\"",colnames(link)[2],"\",all.x=TRUE)")
+	cmd<-paste0("r2<-merge(trg,link,by.x=\"",trgcol,"\",by.y=\"",colnames(link)[2],"\",all.x=TRUE,allow.cartesian=TRUE)")
 	eval(parse( text=cmd ))
 	r2[is.na(r2$i.and.d),"i.and.d"]<-m+2
 	
@@ -63,3 +67,40 @@ blend<-function(src,srccol,trg,trgcol,link,jointype="inner"){
 	return(r)
 	
 }
+
+curate<-function(col,link){
+	# link dataframe contains col1,col2 where col1 contains instances that needs to be changed 
+	# and col2 to which values
+	
+	# identify instance to be fixed
+	df<-as.data.frame(col)
+	names(df)<-"col"
+	df1<-merge(df,link,by.x="col",by.y="col1",all.x=TRUE)
+	# fix it
+	df1<-df1[order(match(df1[,1],col)),]
+	df1[!(is.na(df1$col2)),1]<-df1[!(is.na(df1$col2)),2]
+	gc()
+	return(as.character(df1[,1]))
+}
+
+self_match<-function(col,ignoreCase=TRUE,method="jw",p=0.1){
+	library(stringdist)
+	c<-unique(col)
+	if(ignoreCase){
+		d<-stringdistmatrix(tolower(c),tolower(c),method=method,p=p)
+	} else{
+		d<-stringdistmatrix(c,c,method=method,p=p)
+	}
+	n<-nrow(d)
+	x<-d[lower.tri(d)]
+	x<-round(x,4)
+	y<-matrix(c,nrow=nrow(d),ncol=ncol(d))
+	y<-y[lower.tri(y)]
+	z<-t(matrix(c,nrow=nrow(d),ncol=ncol(d)))
+	z<-z[lower.tri(z)]
+	r<-as.data.frame(cbind(y,z,x))
+	r[,3] <- as.numeric(as.character(r[,3]))
+	names(r)<-c("col1","col2","dist")
+	gc()
+	return(r)
+}	
