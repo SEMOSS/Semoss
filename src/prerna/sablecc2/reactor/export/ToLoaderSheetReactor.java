20e42221835b1616970b45634a73699a74d15470
mahkhalil <mahkhalil@deloitte.com>
2018-05-15 12:32:58 +0000

    UPDATE ::: Making properties optional in sparql queries UPDATE ::: Finishing up ToLoaderSheetReactor to export database
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28004 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/export/ToLoaderSheetReactor.java
+++ src/prerna/sablecc2/reactor/export/ToLoaderSheetReactor.java
@@ -1,35 +1,36 @@
 package prerna.sablecc2.reactor.export;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 import java.util.Vector;
 
-import org.apache.poi.ss.usermodel.Cell;
+import org.apache.log4j.Logger;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import prerna.algorithm.api.ITableDataFrame;
-import prerna.ds.h2.H2Frame;
 import prerna.engine.api.IEngine;
 import prerna.engine.api.IEngine.ENGINE_TYPE;
 import prerna.engine.api.IHeadersDataRow;
 import prerna.engine.api.IRawSelectWrapper;
 import prerna.engine.impl.rdbms.RDBMSNativeEngine;
 import prerna.engine.impl.rdf.BigDataEngine;
+import prerna.om.Insight;
 import prerna.query.querystruct.AbstractQueryStruct.QUERY_STRUCT_TYPE;
 import prerna.query.querystruct.SelectQueryStruct;
 import prerna.query.querystruct.selectors.QueryColumnOrderBySelector;
 import prerna.query.querystruct.selectors.QueryColumnSelector;
 import prerna.rdf.engine.wrappers.WrapperManager;
-import prerna.sablecc2.om.Join;
 import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
-import prerna.sablecc2.reactor.imports.IImporter;
-import prerna.sablecc2.reactor.imports.ImportFactory;
+import prerna.sablecc2.reactor.PixelPlanner;
 import prerna.test.TestUtilityMethods;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
@@ -37,12 +38,15 @@ import prerna.util.Utility;
 
 public class ToLoaderSheetReactor extends AbstractReactor {
 
+	private static final String CLASS_NAME = ToLoaderSheetReactor.class.getName();
+	
 	public ToLoaderSheetReactor() {
 		this.keysToGet = new String[]{ReactorKeysEnum.DATABASE.getKey()};
 	}
 
 	@Override
 	public NounMetadata execute() {
+		Logger logger = getLogger(CLASS_NAME);
 		organizeKeys();
 		String engineName = this.keyValue.get(this.keysToGet[0]);
 		if(engineName == null) {
@@ -50,6 +54,10 @@ public class ToLoaderSheetReactor extends AbstractReactor {
 		}
 
 		String fileLoc = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + "\\" + engineName + "_Loader_Sheet_Export.xlsx";
+		File f = new File(fileLoc);
+		if(f.exists()) {
+			f.delete();
+		}
 		Workbook workbook = new XSSFWorkbook();
 		
 		IEngine engine = Utility.getEngine(engineName);
@@ -60,58 +68,42 @@ public class ToLoaderSheetReactor extends AbstractReactor {
 			if(concept.equals("http://semoss.org/ontologies/Concept")) {
 				continue;
 			}
-			// we will create a frame
-			// and merge every property onto it
-			// using a left join
-			ITableDataFrame dataframe = new H2Frame();
 			String conceptualName = Utility.getInstanceName(concept);
-			// first add the concept by itself
-			{
-				SelectQueryStruct qs = new SelectQueryStruct();
-				qs.setQsType(QUERY_STRUCT_TYPE.ENGINE);
-				qs.setEngine(engine);
-				qs.addSelector(new QueryColumnSelector(conceptualName));
-				IImporter importer = ImportFactory.getImporter(dataframe, qs);
-				importer.insertData();
-				dataframe.syncHeaders();
-			}
-
-			List<String> properties = engine.getProperties4Concept(concept, true);
-			List<String> propertyConceptualNames = new Vector<String>(properties.size());
-			for(String property : properties) {
-				String propertyConceptual = Utility.getClassName(property);
-				propertyConceptualNames.add(propertyConceptual);
-				SelectQueryStruct qs = new SelectQueryStruct();
-				qs.setQsType(QUERY_STRUCT_TYPE.ENGINE);
-				qs.setEngine(engine);
-				qs.addSelector(new QueryColumnSelector(conceptualName));
-				qs.addSelector(new QueryColumnSelector(conceptualName + "__" + propertyConceptual));
-				IImporter importer = ImportFactory.getImporter(dataframe, qs);
-				List<Join> joins = new Vector<Join>();
-				Join j = new Join(conceptualName, "left.outer.join", conceptualName);
-				joins.add(j);
-				importer.mergeData(joins);
-				dataframe.syncHeaders();
-			}
-
-			// once i am done adding all the data
-			// write the h2frame to the excel sheet
-			writeNodePropSheet(engine, workbook, dataframe, conceptualName, propertyConceptualNames);
 			
-			// delete the frame once we are done
-			if(dataframe instanceof H2Frame) {
-				((H2Frame) dataframe).dropTable();
-				((H2Frame) dataframe).dropOnDiskTemporalSchema();
+			SelectQueryStruct qs = new SelectQueryStruct();
+			qs.setQsType(QUERY_STRUCT_TYPE.ENGINE);
+			qs.setEngine(engine);
+			qs.addSelector(new QueryColumnSelector(conceptualName));
+			
+			List<String> properties = engine.getProperties4Concept(concept, true);
+			for(String property : properties) {
+				// THIS IS BECAUSE THERE ARE OTHER ERRORS!!!
+				// Since RDF properties do not contain the table name
+				// The properties query can give me the same property name
+				// that appears on 2 different nodes
+				String conceptConceptual = Utility.getInstanceName(property);
+				if(!conceptConceptual.equals(conceptualName)) {
+					continue;
+				}
+				String propertyConceptual = Utility.getClassName(property);
+				qs.addSelector(new QueryColumnSelector(conceptualName + "__" + propertyConceptual));
 			}
+			
+			logger.info("Start node sheet for concept = " + conceptualName);
+			IRawSelectWrapper iterator = WrapperManager.getInstance().getRawWrapper(engine, qs);
+			writeNodePropSheet(engine, workbook, iterator, conceptualName, properties);
+			logger.info("Finsihed node sheet for concept = " + conceptualName);
 		}
 		
 		// now i need all the relationships
 		Vector<String[]> rels = engine.getRelationships(true);
 		if(engine.getEngineType() == ENGINE_TYPE.SESAME) {
 			for(String[] rel : rels) {
+				logger.info("Start rel sheet for " + Arrays.toString(rel));
 				String query = generateSparqlQuery(engine, rel[0], rel[1], rel[2]);
 				IRawSelectWrapper iterator = WrapperManager.getInstance().getRawWrapper(engine, query);
 				writeRelationshipSheet(engine, workbook, iterator, rel);
+				logger.info("Finsihed rel sheet for " + Arrays.toString(rel));
 			}
 		} else {
 			for(String[] rel : rels) {
@@ -123,18 +115,73 @@ public class ToLoaderSheetReactor extends AbstractReactor {
 				qs.addRelation(rel[0], "inner.join", rel[1]);
 				qs.addOrderBy(new QueryColumnOrderBySelector(rel[0]));
 
+				logger.info("Start rel sheet for " + Arrays.toString(rel));
 				IRawSelectWrapper iterator = WrapperManager.getInstance().getRawWrapper(engine, qs);
 				writeRelationshipSheet(engine, workbook, iterator, rel);
+				logger.info("Finsihed rel sheet for " + Arrays.toString(rel));
 			}
 		}
 		Utility.writeWorkbook(workbook, fileLoc);
 
-		System.out.println("done");
-		return null;
+		logger.info("Done exporting worksheet for engine = " + engineName);
+
+		String randomKey = UUID.randomUUID().toString();
+		this.insight.addExportFile(randomKey, fileLoc);
+		return new NounMetadata(randomKey, PixelDataType.CONST_STRING, PixelOperationType.FILE_DOWNLOAD);
+	}
+	
+	public static void writeNodePropSheet(IEngine engine, Workbook workbook, Iterator<IHeadersDataRow> it, String conceptualName, List<String> properties) {
+		// write the information for the headers and construct the query
+		// so it outputs in the same order
+		String physicalNodeName = getPhysicalColumnHeader(engine, conceptualName);
+		Sheet sheet = workbook.createSheet(physicalNodeName + "_Props");
+				
+		// add row 1
+		{
+			Row row = sheet.createRow(0);
+			row.createCell(0).setCellValue("Node");
+			row.createCell(1).setCellValue(physicalNodeName);
+			// add properties
+			for(int i = 0; i < properties.size(); i++) {
+				String prop = properties.get(i);
+				String physicalPropName = getPhysicalColumnHeader(engine, prop);
+				row.createCell(2+i).setCellValue(physicalPropName);
+			}
+		}
+		
+		// add row 2 - so we can add the ignore
+		{
+			Row row = sheet.createRow(1);
+			row.createCell(0).setCellValue("Ignore");
+			if(it.hasNext()) {
+				Object[] data = it.next().getValues();
+				for(int i = 0 ; i < data.length; i++) {
+					if(data[i] instanceof Number) {
+						row.createCell(i+1).setCellValue(((Number) data[i]).doubleValue());
+					} else {
+						row.createCell(i+1).setCellValue(data[i] + "");
+					}
+				}
+			}
+		}
+		
+		// add all other rows
+		int rowCounter = 2;
+		while(it.hasNext()) {
+			Row row = sheet.createRow(rowCounter);
+			Object[] data = it.next().getValues();
+			for(int i = 0 ; i < data.length; i++) {
+				if(data[i] instanceof Number) {
+					row.createCell(i+1).setCellValue(((Number) data[i]).doubleValue());
+				} else {
+					row.createCell(i+1).setCellValue(data[i] + "");
+				}			}
+			rowCounter++;
+		}
 	}
 	
 	public static void writeRelationshipSheet(IEngine engine, Workbook workbook, Iterator<IHeadersDataRow> it, String[] rel) {
-		String sheetName = rel[0] + "_" + rel[1] + "_" + rel[2];
+		String sheetName = Utility.getInstanceName(rel[0]) + "_" + Utility.getInstanceName(rel[1]) + "_" + Utility.getInstanceName(rel[2]);
 		
 		Sheet sheet = workbook.createSheet(sheetName);
 		
@@ -152,7 +199,11 @@ public class ToLoaderSheetReactor extends AbstractReactor {
 			if(it.hasNext()) {
 				Object[] data = it.next().getValues();
 				for(int i = 0 ; i < data.length; i++) {
-					row.createCell(i+1).setCellValue(data[i] + "");
+					if(data[i] instanceof Number) {
+						row.createCell(i+1).setCellValue(((Number) data[i]).doubleValue());
+					} else {
+						row.createCell(i+1).setCellValue(data[i] + "");
+					}
 				}
 			}
 		}
@@ -162,7 +213,11 @@ public class ToLoaderSheetReactor extends AbstractReactor {
 			Row row = sheet.createRow(rowCounter);
 			Object[] data = it.next().getValues();
 			for(int i = 0 ; i < data.length; i++) {
-				row.createCell(i+1).setCellValue(data[i] + "");
+				if(data[i] instanceof Number) {
+					row.createCell(i+1).setCellValue(((Number) data[i]).doubleValue());
+				} else {
+					row.createCell(i+1).setCellValue(data[i] + "");
+				}
 			}
 			rowCounter++;
 		}
@@ -177,61 +232,12 @@ public class ToLoaderSheetReactor extends AbstractReactor {
 		return query;
 	}
 
-	public static void writeNodePropSheet(IEngine engine, Workbook workbook, ITableDataFrame frame, String conceptualName, List<String> properties) {
-		// write the information for the headers and construct the query
-		// so it outputs in the same order
-		SelectQueryStruct qs = new SelectQueryStruct();
-		qs.setDistinct(false);
-		String physicalNodeName = getPhysicalColumnHeader(engine, conceptualName);
-		Sheet sheet = workbook.createSheet(physicalNodeName + "_Props");
-
-		{
-			Row row = sheet.createRow(0);
-			row.createCell(0).setCellValue("Node");
-			// add conceptual name
-			qs.addSelector(new QueryColumnSelector(conceptualName));
-			row.createCell(1).setCellValue(physicalNodeName);
-			// add properties
-			for(int i = 0; i < properties.size(); i++) {
-				String prop = properties.get(i);
-				String physicalPropName = getPhysicalColumnHeader(engine, prop);
-				qs.addSelector(new QueryColumnSelector(prop));
-				row.createCell(2+i).setCellValue(physicalPropName);
-			}
-			
-			qs.addOrderBy(new QueryColumnOrderBySelector(conceptualName));
-		}
-
-		int rowCounter = 1;
-		Iterator<IHeadersDataRow> it = frame.query(qs);
-		while(it.hasNext()) {
-			IHeadersDataRow rowValues = it.next();
-			Object[] values = rowValues.getValues();
-
-			Row row = sheet.createRow(rowCounter);
-			
-			// add the dumb ignore cell
-			if(rowCounter == 1) {
-				row.createCell(0).setCellValue("Ignore");
-			}
-			
-			for(int i = 0; i < values.length; i++) {
-				Cell cell = row.createCell(i+1);
-				if(values[i] != null) {
-					cell.setCellValue(values[i].toString());
-				}
-			}
-
-			rowCounter++;
-		}
-	}
-
 	public static String getPhysicalColumnHeader(IEngine engine, String conceptualName) {
 		String physicalNodeUri = engine.getPhysicalUriFromConceptualUri(conceptualName);
 		String physicalNodeName = Utility.getInstanceName(physicalNodeUri);
 		return physicalNodeName;
 	}
-
+	
 	public static void main(String[] args) {
 		TestUtilityMethods.loadDIHelper("C:\\workspace\\Semoss_Dev\\RDF_Map.prop");
 
@@ -243,8 +249,9 @@ public class ToLoaderSheetReactor extends AbstractReactor {
 		DIHelper.getInstance().setLocalProperty("LocalMasterDatabase", coreEngine);
 
 		String testEngine = "MovieDatabase";
+		testEngine = "TAP_Core_Data";
 		testEngine = "TAP_Site_Data";
-		
+
 		engineProp = "C:\\workspace\\Semoss_Dev\\db\\" + testEngine + ".smss";
 		coreEngine = new BigDataEngine();
 		coreEngine.setEngineName(testEngine);
@@ -252,13 +259,18 @@ public class ToLoaderSheetReactor extends AbstractReactor {
 		coreEngine.setEngineName(testEngine);
 		DIHelper.getInstance().setLocalProperty(testEngine, coreEngine);
 
+		Insight in = new Insight();
+		PixelPlanner planner = new PixelPlanner();
+		planner.setVarStore(in.getVarStore());
+		in.getVarStore().put("$JOB_ID", new NounMetadata("test", PixelDataType.CONST_STRING));
+		in.getVarStore().put("$INSIGHT_ID", new NounMetadata("test", PixelDataType.CONST_STRING));
+		
 		ToLoaderSheetReactor reactor = new ToLoaderSheetReactor();
+		reactor.setInsight(in);
+		reactor.setPixelPlanner(planner);
 		reactor.In();
 		reactor.curRow.add(new NounMetadata(testEngine, PixelDataType.CONST_STRING));
 		reactor.execute();
 	}
-
-
-
 
 }
