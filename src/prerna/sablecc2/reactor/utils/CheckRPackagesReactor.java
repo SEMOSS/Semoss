8db7405123e8ebea144c0470e018cf8c4b079ed7
mahkhalil <mahkhalil@deloitte.com>
2018-05-18 16:42:08 +0000

    UPDATE ::: Shifting how we will handle check of r packages
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28091 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 99ba66af688040fbf51bcf9bb5b27fb70a14b900

---
+++ src/prerna/sablecc2/reactor/utils/CheckRPackagesReactor.java
@@ -0,0 +1,43 @@
+package prerna.sablecc2.reactor.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.sablecc2.reactor.frame.r.util.AbstractRJavaTranslator;
+
+public class CheckRPackagesReactor extends AbstractReactor {
+
+	private static final String CLASS_NAME = CheckRPackagesReactor.class.getName();
+
+	@Override
+	public NounMetadata execute() {
+		Logger logger = getLogger(CLASS_NAME);
+
+		Map<String, Object> returnMap = new HashMap<String,Object>();
+		try{
+			AbstractRJavaTranslator rJavaTranslator = this.insight.getRJavaTranslator(logger);
+			rJavaTranslator.startR();
+
+			// R call to retrieve all of user's install packages
+			String[] pkgs = rJavaTranslator.getStringArray("as.character(unique(data.frame(installed.packages())$Package))");
+			
+			returnMap.put("RInstalled", true);
+			if (pkgs.length > 0) {
+				returnMap.put("R", pkgs);
+			} else {
+				returnMap.put("R", new String[0]);
+			}
+		} catch(Exception e){
+			e.printStackTrace();
+			returnMap.put("RInstalled", false);
+			returnMap.put("R", new String[0]);
+		}
+		
+		return new NounMetadata(returnMap, PixelDataType.CUSTOM_DATA_STRUCTURE);
+	}
+}
