fe0ca3906c3de66059cb67cd6114319c7f825c00
Maher Khalil <themaherkhalil@gmail.com>
2018-05-22 19:37:09 +0000

    BUGFIX ::: Fixing errors with setting correct properties when making new empty apps
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28139 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: aad60c79a0fa1add4d7f2cdb5c4552ca7ae20d7e

--- src/prerna/sablecc2/reactor/app/upload/GenerateEmptyAppReactor.java
+++ src/prerna/sablecc2/reactor/app/upload/GenerateEmptyAppReactor.java
@@ -15,6 +15,7 @@ import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.solr.SolrIndexEngine;
 import prerna.solr.SolrUtility;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
@@ -39,16 +40,30 @@ public class GenerateEmptyAppReactor extends AbstractReactor {
 		Logger logger = getLogger(CLASS_NAME);
 		this.organizeKeys();
 		String appName = this.keyValue.get(this.keysToGet[0]);
-		
-		// start by validation
-		logger.info("Start validating app");
-		try {
-			UploadUtilities.validateApp(appName);
-		} catch (IOException e) {
-			throw new IllegalArgumentException(e.getMessage());
+		if(appName == null || appName.isEmpty()) {
+			throw new IllegalArgumentException("Need to provide a name for the app");
 		}
+		// need to make sure the app is unique
+		boolean containsApp = true;
+		try {
+			containsApp = SolrIndexEngine.getInstance().containsApp(appName);
+		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
+			e.printStackTrace();
+		}
+		if(containsApp) {
+			throw new IllegalArgumentException("App name already exists.  Please provide a unique app name");
+		}
+		// need to make sure we are not overriding something that already exists in the file system
+		final String FILE_SEP = System.getProperty("file.separator");
+		String baseFolder = DIHelper.getInstance().getProperty("BaseFolder");
+		// need to make sure app name doesn't already exist
+		String appLocation = baseFolder + FILE_SEP + "db" + FILE_SEP + appName;
+		File appFolder = new File(appLocation);
+		if(appFolder.exists()) {
+			throw new IllegalArgumentException("Database folder already contains an app directory with the same name. Please delete the existing app folder or provide a unique app name");
+		}
+
 		logger.info("Done validating app");
-		
 		logger.info("Starting app creation");
 
 		/*
@@ -60,7 +75,7 @@ public class GenerateEmptyAppReactor extends AbstractReactor {
 		 */
 
 		logger.info("Start generating app folder");
-		UploadUtilities.generateAppFolder(appName);
+		appFolder.mkdirs();
 		logger.info("Done generating app folder");
 
 		logger.info("Start generating insights database");
@@ -68,11 +83,11 @@ public class GenerateEmptyAppReactor extends AbstractReactor {
 		logger.info("Done generating insights database");
 
 		// add to DIHelper so we dont auto load with the file watcher
-		File tempAppSmss = null;
+		File tempSmss = null;
 		logger.info("Start generating temp smss");
 		try {
-			tempAppSmss = UploadUtilities.createTemporaryAppSmss(appName);
-			DIHelper.getInstance().getCoreProp().setProperty(appName + "_" + Constants.STORE, tempAppSmss.getAbsolutePath());
+			tempSmss = UploadUtilities.createTemporaryAppSmss(appName);
+			DIHelper.getInstance().getCoreProp().setProperty(appName + "_" + Constants.STORE, tempSmss.getAbsolutePath());
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new IllegalArgumentException(e.getMessage());
@@ -88,6 +103,7 @@ public class GenerateEmptyAppReactor extends AbstractReactor {
 		logger.info("Done loading into solr");
 
 		AppEngine appEng = new AppEngine();
+		appEng.setEngineName(appName);
 		appEng.setInsightDatabase(insightDb);
 		// only at end do we add to DIHelper
 		DIHelper.getInstance().setLocalProperty(appName, appEng);
@@ -96,13 +112,21 @@ public class GenerateEmptyAppReactor extends AbstractReactor {
 		DIHelper.getInstance().setLocalProperty(Constants.ENGINES, appNames);
 		
 		// and rename .temp to .smss
-		File smssFile = new File(tempAppSmss.getAbsolutePath().replace(".temp", ".smss"));
+		File smssFile = new File(tempSmss.getAbsolutePath().replace(".temp", ".smss"));
 		try {
-			FileUtils.copyFile(tempAppSmss, smssFile);
+			FileUtils.copyFile(tempSmss, smssFile);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		tempAppSmss.delete();
+		tempSmss.delete();
+		
+		// update DIHelper & engine smss file location
+		appEng.setPropFile(smssFile.getAbsolutePath());
+		DIHelper.getInstance().getCoreProp().setProperty(appName + "_" + Constants.STORE, smssFile.getAbsolutePath());
+		DIHelper.getInstance().setLocalProperty(appName, appEng);
+		String engineNames = (String) DIHelper.getInstance().getLocalProp(Constants.ENGINES);
+		engineNames = engineNames + ";" + appName;
+		DIHelper.getInstance().setLocalProperty(Constants.ENGINES, engineNames);
 		
 		return new NounMetadata(true, PixelDataType.BOOLEAN);
 	}
