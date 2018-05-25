2837e788934ef20144180e0b966d31f1792d7a7c
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-05-25 13:39:43 +0000

    UPDATE ::: SQL translator checking distinct and creating default table name if no alias
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28224 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 5416a1592e152f32d6895a26f04b17a540465655

--- src/prerna/sablecc2/reactor/masterdatabase/DatabaseQueryTranslator.java
+++ src/prerna/sablecc2/reactor/masterdatabase/DatabaseQueryTranslator.java
@@ -2,14 +2,12 @@ package prerna.sablecc2.reactor.masterdatabase;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
 
-import prerna.ds.QueryStruct;
+import prerna.ds.h2.H2Frame;
+import prerna.engine.api.IEngine;
 import prerna.nameserver.utility.MasterDatabaseUtility;
-import prerna.query.parsers.SqlParser;
-import prerna.query.querystruct.AbstractQueryStruct;
-import prerna.query.querystruct.AbstractQueryStruct.QUERY_STRUCT_TYPE;
+import prerna.query.interpreters.IQueryInterpreter;
+import prerna.query.parsers.SqlTranslator;
 import prerna.query.querystruct.SelectQueryStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
@@ -27,67 +25,41 @@ public class DatabaseQueryTranslator extends AbstractReactor {
 	public NounMetadata execute() {
 		organizeKeys();
 		String query = this.keyValue.get(this.keysToGet[0]);
+		query = Utility.decodeURIComponent(query);
 		String sourceDB = this.keyValue.get(this.keysToGet[1]);
 		String targetDB = this.keyValue.get(this.keysToGet[2]);
-		
-		SqlParser sourceQueryParser = new SqlParser();
+		// get physical to physical translation from sourceDB to targetDB
+		// TODO need to figure out how to interpret 1-many maps
+		Map<String, List<String>> translation = MasterDatabaseUtility.databaseTranslator(sourceDB, targetDB);
+		// process query components using translation map
+		SqlTranslator translator = new SqlTranslator(translation);
+		SelectQueryStruct translatedQs;
 		try {
-			sourceQueryParser.processQuery(query);
-			Map tableAlias = sourceQueryParser.getTableAlias();
-			Map columnAlias = sourceQueryParser.getColumnAlias();
-			// get all the tables and columns used in query string
-			Map<String, Set<String>> schema = sourceQueryParser.getSchema();
-		
-			
-			// get all physical names from query schema
-			List<String> allPhysicalNames = new Vector<>();
-			for(String table: schema.keySet()) {
-				// add physical table names
-				allPhysicalNames.add(table);
-				Set<String> columns = schema.get(table);
-				// add physical col names
-				allPhysicalNames.addAll(columns);
-			}
-			
-			// get map of physical to conceptual names
-			Map<String, String> conceptPhysical = MasterDatabaseUtility.getConceptualFromPhysical(allPhysicalNames, sourceDB);
-			// map source conceptual names to target conceptual names
-			Map<String, Object> conceptMap = MasterDatabaseUtility.getConceptMapping(sourceDB,
-					new Vector<String>(conceptPhysical.values()), targetDB);
+			translatedQs = translator.processQuery(query);
+			IEngine targetEngine = Utility.getEngine(targetDB);
+			IQueryInterpreter interpreter = targetEngine.getQueryInterpreter();
+			interpreter.setQueryStruct(translatedQs);
+			String translatedQuery = interpreter.composeQuery(); 
 
-			// start translation here
-			String targetQuery = query;
-			String select = targetQuery.substring(0, targetQuery.indexOf("from"));
-			String queryEnd = targetQuery.substring(targetQuery.indexOf("from"));
-			for(String sourceConcept : conceptPhysical.keySet()) {
-				if(conceptMap.containsKey(sourceConcept)) {
-					List<String> targetConcepts = (List<String>) conceptMap.get(sourceConcept); 
-					if(!targetConcepts.isEmpty()) {
-						String targetConcept = targetConcepts.get(0);
-						queryEnd = queryEnd.replaceAll(sourceConcept, targetConcept);
-						if(schema.containsKey(sourceConcept)) {
-							targetConcept = AbstractQueryStruct.PRIM_KEY_PLACEHOLDER;
-						}
-						select=select.replaceAll(sourceConcept, targetConcept);
-					}
-				}
-			}
-			targetQuery = select+queryEnd;
-			System.out.println(targetQuery);
-			SqlParser targetQueryParser = new SqlParser();
-			SelectQueryStruct targetQS = targetQueryParser.processQuery(targetQuery);
-			targetQS.setQsType(QUERY_STRUCT_TYPE.ENGINE);
-			targetQS.setEngine(Utility.getEngine(targetDB));
-			targetQS.setEngineName(targetDB);
-			return new NounMetadata(targetQS, PixelDataType.QUERY_STRUCT);
+			// return string query for now
+			String colName = "Query";
+			String[] types = new String[] { "STRING" };
+			String[] headers = new String[] { colName };
+			H2Frame frame = new H2Frame(headers);
+			String tableName = frame.getTableName();
+			String[] cells = new String[] { translatedQuery };
+			frame.addRow(tableName, cells, headers, types);
+			this.insight.setDataMaker(frame);
+			return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 		NounMetadata noun = new NounMetadata("Unable to interpret query", PixelDataType.CONST_STRING, PixelOperationType.ERROR);
 		SemossPixelException exception = new SemossPixelException(noun);
 		exception.setContinueThreadOfExecution(false);
 		throw exception;
-		
+
 	}
 }
