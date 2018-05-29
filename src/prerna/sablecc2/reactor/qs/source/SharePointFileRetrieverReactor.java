51a3e839149671046241b65ae6dc2076e41d6624
kunalppatel9 <kunalppatel9@deloitte.com>
2018-05-29 18:40:37 +0000

    UPDATE ::: Adding SharePoint Social Reactors for site selection, drive selection, file selection, and file download.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28252 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 948bd89c84390ef1903d79006a2e52287d92230d

---
+++ src/prerna/sablecc2/reactor/qs/source/SharePointFileRetrieverReactor.java
@@ -0,0 +1,126 @@
+
+package prerna.sablecc2.reactor.qs.source;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import prerna.auth.AccessToken;
+import prerna.auth.AuthProvider;
+import prerna.auth.User2;
+import prerna.poi.main.MetaModelCreator;
+import prerna.poi.main.helper.CSVFileHelper;
+import prerna.query.querystruct.CsvQueryStruct;
+import prerna.query.querystruct.SelectQueryStruct;
+import prerna.sablecc2.reactor.qs.AbstractQueryStructReactor;
+import prerna.security.AbstractHttpHelper;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.Utility;
+
+public class SharePointFileRetrieverReactor extends AbstractQueryStructReactor{
+
+	private static final String CLASS_NAME = SharePointFileRetrieverReactor.class.getName();
+
+	public SharePointFileRetrieverReactor() {
+		this.keysToGet = new String[] { "driveId", "fileId"};
+	}
+
+
+
+	@Override
+	protected SelectQueryStruct createQueryStruct() {
+		//get keys
+		Logger logger = getLogger(CLASS_NAME);
+		organizeKeys();
+		String driveID = this.keyValue.get(this.keysToGet[0]);
+		if (driveID == null || driveID.length() <= 0) {
+			throw new IllegalArgumentException("Need to SharePoint Drive id");
+		}
+		
+		String fileID = this.keyValue.get(this.keysToGet[0]);
+		if (fileID == null || fileID.length() <= 0) {
+			throw new IllegalArgumentException("Need to SharePoint file id");
+		}
+
+		//get access token
+		String accessToken=null;
+		User2 user = this.insight.getUser2();
+
+		try{
+			if(user==null){
+				Map<String, Object> retMap = new HashMap<String, Object>();
+				retMap.put("type", "microsoft");
+				retMap.put("message", "Please login to your Microsoft account");
+				throwLoginError(retMap);
+			}
+			else if (user != null) {
+				AccessToken msToken = user.getAccessToken(AuthProvider.AZURE_GRAPH.name());
+				accessToken=msToken.getAccess_token();
+			}
+		}
+		catch (Exception e) {
+			Map<String, Object> retMap = new HashMap<String, Object>();
+			retMap.put("type", "microsoft");
+			retMap.put("message", "Please login to your Microsoft account");
+			throwLoginError(retMap);
+		}
+
+
+		Hashtable params = new Hashtable();
+		CsvQueryStruct qs = new CsvQueryStruct();
+
+		try {
+			String url_str = "https://graph.microsoft.com/v1.0/drives/"+driveID+"/items/"+fileID+"/content";
+			BufferedReader br = AbstractHttpHelper.getHttpStream(url_str, accessToken, params, true);
+
+			// create a file
+			String filePath = DIHelper.getInstance().getProperty(Constants.INSIGHT_CACHE_DIR) + "\\"
+					+ DIHelper.getInstance().getProperty(Constants.CSV_INSIGHT_CACHE_FOLDER);
+			filePath += "\\" + Utility.getRandomString(10) + ".csv";
+			filePath = filePath.replace("\\", "/");
+			File outputFile = new File(filePath);
+
+			BufferedWriter target = new BufferedWriter(new FileWriter(outputFile));
+			String data = null;
+
+
+			while((data = br.readLine()) != null)
+			{
+				target.write(data);
+				target.write("\n");
+				target.flush();
+			}
+
+			// get datatypes
+			CSVFileHelper helper = new CSVFileHelper();
+			helper.setDelimiter(',');
+			helper.parse(filePath);
+			MetaModelCreator predictor = new MetaModelCreator(helper, null);
+			Map<String, String> dataTypes = predictor.getDataTypeMap();
+			for (String key : dataTypes.keySet()) {
+				qs.addSelector("DND", key);
+			}
+			helper.clear();
+			qs.merge(this.qs);
+			qs.setFilePath(filePath);
+			qs.setDelimiter(',');
+			qs.setColumnTypes(dataTypes);
+			return qs;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return qs;
+	}
+
+
+
+
+}
