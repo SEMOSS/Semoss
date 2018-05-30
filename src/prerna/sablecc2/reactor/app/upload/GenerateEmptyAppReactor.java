0d390355ce5efc596b15304164ef2cc639bb1e17
mahkhalil <mahkhalil@deloitte.com>
2018-05-30 14:42:01 +0000

    BUGFIX ::: Fixing upload to write with new engine ids
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28277 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/app/upload/GenerateEmptyAppReactor.java
+++ src/prerna/sablecc2/reactor/app/upload/GenerateEmptyAppReactor.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
 import prerna.engine.api.IEngine;
+import prerna.engine.impl.SmssUtilities;
 import prerna.engine.impl.app.AppEngine;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
@@ -37,6 +39,8 @@ public class GenerateEmptyAppReactor extends AbstractReactor {
 
 	@Override
 	public NounMetadata execute() {
+		String appId = UUID.randomUUID().toString();
+
 		Logger logger = getLogger(CLASS_NAME);
 		this.organizeKeys();
 		String appName = this.keyValue.get(this.keysToGet[0]);
@@ -57,7 +61,7 @@ public class GenerateEmptyAppReactor extends AbstractReactor {
 		final String FILE_SEP = System.getProperty("file.separator");
 		String baseFolder = DIHelper.getInstance().getProperty("BaseFolder");
 		// need to make sure app name doesn't already exist
-		String appLocation = baseFolder + FILE_SEP + "db" + FILE_SEP + appName;
+		String appLocation = baseFolder + FILE_SEP + "db" + FILE_SEP + SmssUtilities.getUniqueName(appName, appId);
 		File appFolder = new File(appLocation);
 		if(appFolder.exists()) {
 			throw new IllegalArgumentException("Database folder already contains an app directory with the same name. Please delete the existing app folder or provide a unique app name");
@@ -79,15 +83,15 @@ public class GenerateEmptyAppReactor extends AbstractReactor {
 		logger.info("Done generating app folder");
 
 		logger.info("Start generating insights database");
-		IEngine insightDb = UploadUtilities.generateInsightsDatabase(appName);
+		IEngine insightDb = UploadUtilities.generateInsightsDatabase(appId, appName);
 		logger.info("Done generating insights database");
 
 		// add to DIHelper so we dont auto load with the file watcher
 		File tempSmss = null;
 		logger.info("Start generating temp smss");
 		try {
-			tempSmss = UploadUtilities.createTemporaryAppSmss(appName);
-			DIHelper.getInstance().getCoreProp().setProperty(appName + "_" + Constants.STORE, tempSmss.getAbsolutePath());
+			tempSmss = UploadUtilities.createTemporaryAppSmss(appId, appName);
+			DIHelper.getInstance().getCoreProp().setProperty(appId + "_" + Constants.STORE, tempSmss.getAbsolutePath());
 		} catch (IOException e) {
 			e.printStackTrace();
 			throw new IllegalArgumentException(e.getMessage());
@@ -96,19 +100,20 @@ public class GenerateEmptyAppReactor extends AbstractReactor {
 
 		logger.info("Start loading into solr");
 		try {
-			SolrUtility.addAppToSolr(appName);
+			SolrUtility.addAppToSolr(appId);
 		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
 			e.printStackTrace();
 		}
 		logger.info("Done loading into solr");
 
 		AppEngine appEng = new AppEngine();
-		appEng.setEngineId(appName);
+		appEng.setEngineId(appId);
+		appEng.setEngineName(appName);
 		appEng.setInsightDatabase(insightDb);
 		// only at end do we add to DIHelper
-		DIHelper.getInstance().setLocalProperty(appName, appEng);
+		DIHelper.getInstance().setLocalProperty(appId, appEng);
 		String appNames = (String) DIHelper.getInstance().getLocalProp(Constants.ENGINES);
-		appNames = appNames + ";" + appName;
+		appNames = appNames + ";" + appId;
 		DIHelper.getInstance().setLocalProperty(Constants.ENGINES, appNames);
 		
 		// and rename .temp to .smss
@@ -122,10 +127,10 @@ public class GenerateEmptyAppReactor extends AbstractReactor {
 		
 		// update DIHelper & engine smss file location
 		appEng.setPropFile(smssFile.getAbsolutePath());
-		DIHelper.getInstance().getCoreProp().setProperty(appName + "_" + Constants.STORE, smssFile.getAbsolutePath());
-		DIHelper.getInstance().setLocalProperty(appName, appEng);
+		DIHelper.getInstance().getCoreProp().setProperty(appId + "_" + Constants.STORE, smssFile.getAbsolutePath());
+		DIHelper.getInstance().setLocalProperty(appId, appEng);
 		String engineNames = (String) DIHelper.getInstance().getLocalProp(Constants.ENGINES);
-		engineNames = engineNames + ";" + appName;
+		engineNames = engineNames + ";" + appId;
 		DIHelper.getInstance().setLocalProperty(Constants.ENGINES, engineNames);
 		
 		return new NounMetadata(true, PixelDataType.BOOLEAN);
