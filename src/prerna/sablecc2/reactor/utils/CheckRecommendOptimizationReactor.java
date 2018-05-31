c349653e88d8f88ce820182a98bfaae35edb6f90
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-05-31 18:18:32 +0000

    UPDATE ::: new reactor for checking if a db has unique values tracked or not. UPDATE ::: store unique counts to support engine id
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28355 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 9551c6bb32dc8e58be2870a210a724062d96b0e0

---
+++ src/prerna/sablecc2/reactor/utils/CheckRecommendOptimizationReactor.java
@@ -0,0 +1,77 @@
+package prerna.sablecc2.reactor.utils;
+
+import java.util.List;
+
+import prerna.engine.api.IEngine;
+import prerna.engine.api.IEngine.ENGINE_TYPE;
+import prerna.engine.api.IRawSelectWrapper;
+import prerna.engine.impl.rdbms.RDBMSNativeEngine;
+import prerna.engine.impl.rdf.BigDataEngine;
+import prerna.engine.impl.rdf.RDFFileSesameEngine;
+import prerna.engine.impl.tinker.TinkerEngine;
+import prerna.nameserver.utility.MasterDatabaseUtility;
+import prerna.rdf.engine.wrappers.WrapperManager;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Utility;
+
+public class CheckRecommendOptimizationReactor extends AbstractReactor {
+
+	public CheckRecommendOptimizationReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.APP.getKey() };
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String db = this.keyValue.get(this.keysToGet[0]);
+		db = MasterDatabaseUtility.testEngineIdIfAlias(db);
+
+		IEngine engine = Utility.getEngine(db);
+		ENGINE_TYPE type = engine.getEngineType();
+		RDFFileSesameEngine owlEngine = null;
+		if (type.equals(ENGINE_TYPE.RDBMS)) {
+			RDBMSNativeEngine eng = (RDBMSNativeEngine) engine;
+			owlEngine = eng.getBaseDataEngine();
+		} else if (type.equals(ENGINE_TYPE.TINKER)) {
+			TinkerEngine eng = (TinkerEngine) engine;
+			owlEngine = eng.getBaseDataEngine();
+		} else if (type.equals(ENGINE_TYPE.SESAME)) {
+			BigDataEngine eng = (BigDataEngine) engine;
+			owlEngine = eng.getBaseDataEngine();
+		} else if (type.equals(ENGINE_TYPE.JENA)) {
+			RDFFileSesameEngine eng = (RDFFileSesameEngine) engine;
+			owlEngine = eng.getBaseDataEngine();
+		}
+
+		List<Object[]> allTableCols = MasterDatabaseUtility.getAllTablesAndColumns(db);
+		for (Object[] tableCol : allTableCols) {
+			if (tableCol.length == 4) {
+				String table = tableCol[0] + "";
+				String col = tableCol[1] + "";
+				String dataType = tableCol[2] + "";
+				// only care about strings
+				if (dataType != null && dataType.equals("STRING")) {
+					String queryCol = col;
+					String uniqueValQuery = "SELECT DISTINCT ?concept ?unique WHERE "
+							+ "{ BIND(<http://semoss.org/ontologies/Concept/" + queryCol + "/" + table
+							+ "> AS ?concept)"
+							+ "{?concept <http://semoss.org/ontologies/Relation/Contains/UNIQUE> ?unique}}";
+					IRawSelectWrapper it = WrapperManager.getInstance().getRawWrapper(owlEngine, uniqueValQuery);
+					if (it.hasNext()) {
+						// it had a unique value so we assume theyre all there
+						return new NounMetadata(true, PixelDataType.BOOLEAN);
+					} else {
+						// the first string value didnt have a unique count so
+						// we assume none do
+						return new NounMetadata(false, PixelDataType.BOOLEAN);
+					}
+				}
+			}
+		}
+		// none were strings so no need to get uniques
+		return new NounMetadata(true, PixelDataType.BOOLEAN);
+	}
+}
