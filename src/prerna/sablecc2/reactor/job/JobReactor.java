4c365b195f1585e83b98dca81c274f735fd7d243
Maher Khalil <themaherkhalil@gmail.com>
2018-06-05 00:24:29 +0000

    UPDATE :::: Adding debug logger for showing session id
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28480 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 5d01d29915f8422c34d35d42d590cd96230b337d

--- src/prerna/sablecc2/reactor/job/JobReactor.java
+++ src/prerna/sablecc2/reactor/job/JobReactor.java
@@ -3,6 +3,8 @@ package prerna.sablecc2.reactor.job;
 import java.util.List;
 import java.util.Vector;
 
+import org.apache.log4j.Logger;
+
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
@@ -10,28 +12,31 @@ import prerna.sablecc2.reactor.AbstractReactor;
 
 public class JobReactor extends AbstractReactor {
 	
+	private static final String CLASS_NAME = JobReactor.class.getName();
+	
 	public JobReactor() {
 		this.keysToGet = new String[]{ReactorKeysEnum.JOB_ID.getKey(), ReactorKeysEnum.ID.getKey(), ReactorKeysEnum.SESSION_ID.getKey()};
 	}
 	
 	@Override
 	public NounMetadata execute() {
+		Logger logger = getLogger(CLASS_NAME);
 		// this just returns the job id
-		if(curRow.size() > 0)
-		{
+		if(curRow.size() > 0) {
 			String jobId = this.curRow.get(0).toString();
+			logger.debug("Job ID = " + jobId);
 			planner.addVariable("$JOB_ID", new NounMetadata(jobId, PixelDataType.CONST_STRING));
 		}
 		
-		if(curRow.size() > 1)
-		{
+		if(curRow.size() > 1) {
 			String insightId = this.curRow.get(1).toString();
+			logger.debug("Insight ID = " + insightId);
 			planner.addVariable("$INSIGHT_ID", new NounMetadata(insightId, PixelDataType.CONST_STRING));
 		}
 		
-		if(curRow.size() > 2) 
-		{
+		if(curRow.size() > 2) {
 			String sessionId = this.curRow.get(2).toString();
+			logger.debug("Session ID = " + sessionId);
 			planner.addVariable("$SESSION_ID", new NounMetadata(sessionId, PixelDataType.CONST_STRING));
 		}
 
