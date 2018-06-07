e135e9d5642520a32856a550059a10d914a25f40
tbanach <tbanach@deloitte.com>
2018-06-07 18:40:36 +0000

    REQUIREMENT ::: Maven update Semoss and Monolith. UPDATE ::: Removing Spark altogether (commenting out in code and in pom.xml).
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28595 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b4cef1af2cd26d25bfc2388fc1d4eca29915fde2

--- src/prerna/algorithm/impl/spark/SparkCountReactor.java
+++ src/prerna/algorithm/impl/spark/SparkCountReactor.java
@@ -1,13 +1,13 @@
-package prerna.algorithm.impl.spark;
-
-import prerna.algorithm.api.ITableDataFrame;
-import prerna.ds.spark.SparkDataFrame;
-
-public class SparkCountReactor extends SparkBaseReducerReactor{
-
-	@Override
-	public Object reduce() {
-		ITableDataFrame frame = (ITableDataFrame)myStore.get("G");
-		return ((SparkDataFrame)frame).mapReduce(columns, groupBys, "COUNT", expr);	
-	}
-}
+//package prerna.algorithm.impl.spark;
+//
+//import prerna.algorithm.api.ITableDataFrame;
+//import prerna.ds.spark.SparkDataFrame;
+//
+//public class SparkCountReactor extends SparkBaseReducerReactor{
+//
+//	@Override
+//	public Object reduce() {
+//		ITableDataFrame frame = (ITableDataFrame)myStore.get("G");
+//		return ((SparkDataFrame)frame).mapReduce(columns, groupBys, "COUNT", expr);	
+//	}
+//}
--- src/prerna/algorithm/impl/spark/SparkMaxReactor.java
+++ src/prerna/algorithm/impl/spark/SparkMaxReactor.java
@@ -1,13 +1,13 @@
-package prerna.algorithm.impl.spark;
-
-import prerna.algorithm.api.ITableDataFrame;
-import prerna.ds.spark.SparkDataFrame;
-
-public class SparkMaxReactor extends SparkBaseReducerReactor  {
-
-	@Override
-	public Object reduce() {
-		ITableDataFrame frame = (ITableDataFrame)myStore.get("G");
-		return ((SparkDataFrame)frame).mapReduce(columns, groupBys, "MAX", expr);	
-	}
-}
+//package prerna.algorithm.impl.spark;
+//
+//import prerna.algorithm.api.ITableDataFrame;
+//import prerna.ds.spark.SparkDataFrame;
+//
+//public class SparkMaxReactor extends SparkBaseReducerReactor  {
+//
+//	@Override
+//	public Object reduce() {
+//		ITableDataFrame frame = (ITableDataFrame)myStore.get("G");
+//		return ((SparkDataFrame)frame).mapReduce(columns, groupBys, "MAX", expr);	
+//	}
+//}
