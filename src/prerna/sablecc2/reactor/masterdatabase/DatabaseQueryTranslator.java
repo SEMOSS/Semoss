4c72bea81722ccc83648953d2f774c2564bdde4e
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-06-11 16:25:20 +0000

    UPDATE ::: SQL Translator overrides table name and column name with new names
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28617 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8814272903d9399cf43e490a17eca7f8bd9098ad

--- src/prerna/sablecc2/reactor/masterdatabase/DatabaseQueryTranslator.java
+++ src/prerna/sablecc2/reactor/masterdatabase/DatabaseQueryTranslator.java
@@ -2,16 +2,11 @@ package prerna.sablecc2.reactor.masterdatabase;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-import prerna.ds.h2.H2Frame;
-import prerna.engine.api.IEngine;
 import prerna.nameserver.utility.MasterDatabaseUtility;
-import prerna.query.interpreters.IQueryInterpreter;
 import prerna.query.parsers.SqlTranslator;
-import prerna.query.querystruct.SelectQueryStruct;
 import prerna.sablecc2.om.PixelDataType;
-import prerna.sablecc2.om.PixelOperationType;
-import prerna.sablecc2.om.execptions.SemossPixelException;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.util.Utility;
@@ -26,32 +21,22 @@ public class DatabaseQueryTranslator extends AbstractReactor {
 		organizeKeys();
 		String query = this.keyValue.get(this.keysToGet[0]);
 		query = Utility.decodeURIComponent(query);
+		System.out.println(query);
 		String sourceDbId = this.keyValue.get(this.keysToGet[1]);
 		String targetDbId = this.keyValue.get(this.keysToGet[2]);
 		sourceDbId = MasterDatabaseUtility.testEngineIdIfAlias(sourceDbId);
 		targetDbId = MasterDatabaseUtility.testEngineIdIfAlias(targetDbId);
-		
 		// get physical to physical translation from sourceDB to targetDB
 		Map<String, List<String>> translation = MasterDatabaseUtility.databaseTranslator(sourceDbId, targetDbId);
-		// process query components using translation map
+		// generate translated queries
 		SqlTranslator translator = new SqlTranslator(translation);
-		SelectQueryStruct translatedQs;
+		Set<String> queries = null;
 		try {
-			translatedQs = translator.processQuery(query);
-			IEngine targetEngine = Utility.getEngine(targetDbId);
-			IQueryInterpreter interpreter = targetEngine.getQueryInterpreter();
-			interpreter.setQueryStruct(translatedQs);
-			String translatedQuery = interpreter.composeQuery(); 
-			return new NounMetadata(translatedQuery, PixelDataType.CONST_STRING);
+			queries = translator.processQuery(query);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		NounMetadata noun = new NounMetadata("Unable to interpret query", PixelDataType.CONST_STRING, PixelOperationType.ERROR);
-		SemossPixelException exception = new SemossPixelException(noun);
-		exception.setContinueThreadOfExecution(false);
-		throw exception;
-
+		return new NounMetadata(queries, PixelDataType.CUSTOM_DATA_STRUCTURE);
 	}
 }
