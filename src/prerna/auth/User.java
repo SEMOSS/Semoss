4ddac3da2f2542b57765b4a3460b21028438d022
Maher Khalil <themaherkhalil@gmail.com>
2018-06-12 16:52:38 +0000

    BUGFIX ::: Forgot to add the user object when I shifted it from user2 to user
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28649 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 1f94d0810a3811cf1a99bc7791f64544900a0bfe

---
+++ src/prerna/auth/User.java
@@ -0,0 +1,63 @@
+package prerna.auth;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+import prerna.om.AbstractValueObject;
+
+public class User extends AbstractValueObject {
+	
+	// name of this user in the SEMOSS system if there is one
+	
+	// need to have an access token store
+	Hashtable<AuthProvider, AccessToken> accessTokens = new Hashtable<AuthProvider, AccessToken>();
+	List<AuthProvider> loggedInProfiles = new ArrayList<AuthProvider>();
+	
+	/**
+	 * Set the access token for a given provider
+	 * @param value
+	 */
+	public void setAccessToken(AccessToken value) {
+		AuthProvider name = value.getProvider();
+		if(!loggedInProfiles.contains(name)) {
+			loggedInProfiles.add(name);
+		}
+		accessTokens.put(name, value);
+	}
+	
+	/**
+	 * Get the requested access token
+	 * @param name
+	 * @return
+	 */
+	public AccessToken getAccessToken(AuthProvider name) {
+		return accessTokens.get(name);
+	}
+	
+	/**
+	 * Drop the access token for a given provider
+	 * @param name			The name of the provider
+	 * @return				boolean if the provider was dropped
+	 */
+	public boolean dropAccessToken(String name) {
+		// remove from token map
+		AccessToken token = accessTokens.remove(name);
+		// remove from profiles list
+		loggedInProfiles.remove(name);
+		// return false if the token actually wasn't found
+		if(token == null) {
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Get the list of logged in profiles
+	 * @return
+	 */
+	public List<AuthProvider> getLogins() {
+		return loggedInProfiles;
+	}
+
+}
