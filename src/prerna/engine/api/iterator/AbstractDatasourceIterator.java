e1bc136524756b1a5c82517f569a57c0b30d8d52
mahkhalil <mahkhalil@deloitte.com>
2018-06-12 23:49:06 +0000

    UPDATE ::: Adding new iterator construct to replace IRawSelectWrappers - will enable useful methods like getting the count of the return UPDATE ::: Removing unused test package
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28664 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/engine/api/iterator/AbstractDatasourceIterator.java
@@ -0,0 +1,33 @@
+package prerna.engine.api.iterator;
+
+import prerna.algorithm.api.SemossDataType;
+
+public abstract class AbstractDatasourceIterator implements IDatasourceIterator {
+
+	// values for querying
+	String query;
+	
+	// number of return columns
+	int numColumns = 0;
+	
+	// values for return
+	String[] headers;
+	String[] rawHeaders;
+	SemossDataType[] types;
+
+	@Override
+	public void setQuery(String query) {
+		this.query = query;
+	}
+	
+	@Override
+	public String[] getHeaders() {
+		return this.headers;
+	}
+	
+	@Override
+	public SemossDataType[] getTypes() {
+		return this.types;
+	}
+	
+}
