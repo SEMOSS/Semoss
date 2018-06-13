65f0637922b691614c0a56bf9637a7b9110d6722
mahkhalil <mahkhalil@deloitte.com>
2018-06-13 01:17:02 +0000

    BUGFIX ::: Hotfixes pushed for working with TOPAZ
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28666 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d2fc60c0a8107ab58359a5b9f8ee023ca64f6fa4

--- src/prerna/util/insight/InsightUtility.java
+++ src/prerna/util/insight/InsightUtility.java
@@ -1,13 +1,20 @@
 package prerna.util.insight;
 
+import java.io.File;
+import java.util.Map;
+import java.util.Set;
+
 import prerna.algorithm.api.ITableDataFrame;
 import prerna.ds.h2.H2Frame;
 import prerna.ds.r.RDataTable;
 import prerna.om.Insight;
 import prerna.sablecc2.om.InMemStore;
 import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.VarStore;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.om.task.ITask;
+import prerna.sablecc2.om.task.TaskStore;
 import prerna.sablecc2.reactor.PixelPlanner;
 import prerna.sablecc2.reactor.job.JobReactor;
 
@@ -102,5 +109,71 @@ public class InsightUtility {
 		return task;
 	}
 	
+	/**
+	 * Clear an insight
+	 * @param insight
+	 * @return
+	 */
+	public static NounMetadata clearInsight(Insight insight) {
+		String insightId = insight.getInsightId();
 
+		// drop all the tasks that are currently running
+		TaskStore taskStore = insight.getTaskStore();
+		taskStore.clearAllTasks();
+//		logger.info("Successfully cleared all stored Tasks for the insight");
+
+		// drop all the frame connections
+		VarStore varStore = insight.getVarStore();
+		Set<String> keys = varStore.getKeys();
+		// find all the vars which are frames
+		// and drop them
+		for(String key : keys) {
+			NounMetadata noun = varStore.get(key);
+			PixelDataType nType = noun.getNounType();
+			if(nType == PixelDataType.FRAME) {
+				ITableDataFrame dm = (ITableDataFrame) noun.getValue();
+//				dm.setLogger(logger);
+				//TODO: expose a delete on the frame to hide this crap
+				// drop the existing tables/connections if present
+				if(dm instanceof H2Frame) {
+					H2Frame frame = (H2Frame)dm;
+					frame.dropTable();
+					if(!frame.isInMem()) {
+						frame.dropOnDiskTemporalSchema();
+					}
+				} else if(dm instanceof RDataTable) {
+					RDataTable frame = (RDataTable) dm;
+					frame.closeConnection();
+				}
+			}
+		}
+//		logger.info("Successfully removed all frames from insight");
+		
+		insight.getVarStore().clear();
+//		logger.info("Successfully removed all variables from varstore");
+
+		// also delete any files that were used
+//		List<FileMeta> fileData = insight.getFilesUsedInInsight();
+//		if (fileData != null && !fileData.isEmpty()) {
+//			for (int fileIdx = 0; fileIdx < fileData.size(); fileIdx++) {
+//				FileMeta file = fileData.get(fileIdx);
+//				File f = new File(file.getFileLoc());
+//				f.delete();
+//				logger.info("Successfully deleted File used in insight " + file.getFileLoc());
+//			}
+//		}
+		
+		Map<String, String> fileExports = insight.getExportFiles();
+		if (fileExports != null && !fileExports.isEmpty()) {
+			for (String fileKey : fileExports.keySet()){
+				File f = new File(fileExports.get(fileKey));
+				f.delete();
+//				logger.info("Successfully deleted File used in insight " + f.getName());
+			}
+		}
+		
+//		logger.info("Successfully cleared insight");
+		return new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.CLEAR_INSIGHT);
+	}
+	
 }
