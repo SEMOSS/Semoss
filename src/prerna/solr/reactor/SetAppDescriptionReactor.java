fae832ddfdc3b4bfdc93edf82f829346127a5131
Maher Khalil <themaherkhalil@gmail.com>
2018-06-26 02:19:56 +0000

    UPDATE ::: Adding logic for setting engine meta values in security database
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28890 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6c833e065846e0ea6ce382c655b32e46ce1b1bd2

--- src/prerna/solr/reactor/SetAppDescriptionReactor.java
+++ src/prerna/solr/reactor/SetAppDescriptionReactor.java
@@ -1,44 +1,31 @@
 package prerna.solr.reactor;
 
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.solr.client.solrj.SolrServerException;
-
+import prerna.auth.SecurityUtils;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
-import prerna.solr.SolrIndexEngine;
 
 public class SetAppDescriptionReactor extends AbstractReactor {
 	
+	public static final String DESCRIPTIONS = "description";
+
 	public SetAppDescriptionReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), "description"};
+		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), DESCRIPTIONS};
 	}
 
 	@Override
 	public NounMetadata execute() {
-//		organizeKeys();
-//		String appName = this.keyValue.get(this.keysToGet[0]);
-//		String description = this.keyValue.get(this.keysToGet[1]);
-//		
-//		Map<String, Object> fieldsToModify = new HashMap<String, Object>();
-//		fieldsToModify.put("app_description", description);
-//		try {
-//			SolrIndexEngine.getInstance().modifyApp(appName, fieldsToModify);
-//			return new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.APP_INFO);
-//		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException | SolrServerException
-//				| IOException e1) {
-//			e1.printStackTrace();
-//		}
-
-		return new NounMetadata(false, PixelDataType.BOOLEAN, PixelOperationType.APP_INFO);
+		organizeKeys();
+		String appName = this.keyValue.get(this.keysToGet[0]);
+		String descriptions = this.keyValue.get(this.keysToGet[1]);
+		List<String> descList = new ArrayList<String>();
+		descList.add(descriptions);
+		SecurityUtils.setEngineMeta(appName, "description", descList);
+		return new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.APP_INFO);
 	}
-
 }
