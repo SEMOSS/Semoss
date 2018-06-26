fbab5607bca4159cad1fe3d996a98486956ba851
mahkhalil <mahkhalil@deloitte.com>
2018-06-26 15:26:11 +0000

    UPDATE ::: Moving methods away from solr to security
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28897 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: aecf5e259805366d7f0ac038c2c05c7ec80ed71f

--- src/prerna/solr/reactor/AppInsightsReactor.java
+++ src/prerna/solr/reactor/AppInsightsReactor.java
@@ -27,7 +27,7 @@ public class AppInsightsReactor extends AbstractReactor {
 		String limit = this.keyValue.get(this.keysToGet[2]);
 		String offset = this.keyValue.get(this.keysToGet[3]);
 		
-		List<Map<String, String>> results = null;
+		List<Map<String, Object>> results = null;
 		if(this.securityEnabled()) {
 			if(this.getUserAppFilters().contains(appId)) {
 				results = SecurityQueryUtils.searchUserInsights(appId, this.insight.getUserId(), searchTerm, limit, offset);
@@ -39,69 +39,6 @@ public class AppInsightsReactor extends AbstractReactor {
 		}
 		
 		return new NounMetadata(results, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.APP_INSIGHTS);
-
-//		SolrIndexEngineQueryBuilder builder = new SolrIndexEngineQueryBuilder();
-//		if(searchTerm != null && !searchTerm.trim().isEmpty()) {
-//			builder.setSearchString(searchTerm);
-//			if(!searchTerm.equals("*:*")) {
-//				builder.setDefaultDisMaxWeighting();
-//			}
-//		} else {
-//			builder.setSearchString("*:*");
-//		}
-//		
-//		List<String> retFields = new ArrayList<String>();
-//		retFields.add(SolrIndexEngine.ID);
-//		retFields.add(SolrIndexEngine.APP_ID);
-//		retFields.add(SolrIndexEngine.APP_NAME);
-//		retFields.add(SolrIndexEngine.APP_INSIGHT_ID);
-//		retFields.add(SolrIndexEngine.LAYOUT);
-//		retFields.add(SolrIndexEngine.STORAGE_NAME);
-//		retFields.add(SolrIndexEngine.CREATED_ON);
-//		retFields.add(SolrIndexEngine.MODIFIED_ON);
-//		retFields.add(SolrIndexEngine.LAST_VIEWED_ON);
-//		retFields.add(SolrIndexEngine.USER_ID);
-//		retFields.add(SolrIndexEngine.TAGS);
-//		retFields.add(SolrIndexEngine.VIEW_COUNT);
-//		retFields.add(SolrIndexEngine.DESCRIPTION);
-//		builder.setReturnFields(retFields);
-//
-//		// order the return
-//		builder.setSort(SolrIndexEngine.STORAGE_NAME, "asc");
-//		
-//		Integer offsetInt = null;
-//		Integer limitInt = null;
-//		if (offset != null && !offset.isEmpty()) {
-//			offsetInt = Integer.parseInt(offset);
-//			builder.setOffset(offsetInt);
-//		} else {
-//			builder.setOffset(0);
-//		}
-//		if (limit != null && !limit.isEmpty()) {
-//			limitInt = Integer.parseInt(limit);
-//			builder.setLimit(limitInt);
-//		} else {
-//			builder.setLimit(200);
-//		}
-//		
-//		Map<String, List<String>> filterForEngine = new HashMap<String, List<String>>();
-//		List<String> engineIdList = new ArrayList<String>();
-//		engineIdList.add(appId);
-//		filterForEngine.put(SolrIndexEngine.APP_ID, engineIdList);
-//		if(tags != null && !tags.isEmpty()) {
-//			filterForEngine.put(SolrIndexEngine.TAGS, tags);
-//		}
-//		
-//		builder.setFilterOptions(filterForEngine);
-//		
-//		SolrDocumentList results;
-//		try {
-//			results = SolrIndexEngine.getInstance().queryDocument(builder);
-//			return new NounMetadata(results, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.APP_INSIGHTS);
-//		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException | SolrServerException | IOException e) { 
-//			e.printStackTrace();
-//			throw new IllegalArgumentException("Error retrieving insights for app = " + appId);
-//		}
 	}
 	
 }
--- src/prerna/solr/reactor/MyAppsReactor.java
+++ src/prerna/solr/reactor/MyAppsReactor.java
@@ -1,24 +1,14 @@
 package prerna.solr.reactor;
 
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
-
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.common.SolrDocument;
-import org.apache.solr.common.SolrDocumentList;
 
 import prerna.auth.SecurityQueryUtils;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
-import prerna.solr.SolrIndexEngine;
 
 public class MyAppsReactor extends AbstractReactor {
 
@@ -28,54 +18,18 @@ public class MyAppsReactor extends AbstractReactor {
 
 	@Override
 	public NounMetadata execute() {
-		List<Map> appInfo = new Vector<Map>();
+		List<Map<String, Object>> appInfo = new Vector<Map<String, Object>>();
 
 		if(this.securityEnabled()) {
-			appInfo.addAll(SecurityQueryUtils.getUserDatabaseList(this.insight.getUserId()));
+			appInfo = SecurityQueryUtils.getUserDatabaseList(this.insight.getUserId());
 		} else {
-			appInfo.addAll(SecurityQueryUtils.getAllDatabaseList());
+			appInfo = SecurityQueryUtils.getAllDatabaseList();
 		}
 		
-		Map<String, SolrDocumentList> imageMap = null;
-		try {
-			// get the top images for each app
-			String searchString = "*:*";
-			int groupOffsetInt = 0;
-			int groupLimitInt = 25;
-			String groupByField = SolrIndexEngine.APP_ID;
-			String groupSort = SolrIndexEngine.VIEW_COUNT;
-
-			Map<String, Object> groupFieldMap = SolrIndexEngine.getInstance().executeQueryGroupBy(
-					searchString, 
-					groupOffsetInt, 
-					groupLimitInt, 
-					groupByField, 
-					groupSort, 
-					new HashMap<String, List<String>>());
-			Map<String, Object> queryRet = (Map<String, Object>) groupFieldMap.get("queryResponse");
-			imageMap = (Map<String, SolrDocumentList>) queryRet.get(groupByField);
-		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException | SolrServerException | IOException e) {
-			e.printStackTrace();
-		}
-
-		for(Map appMap : appInfo) {
-			String appName = appMap.get("app_id") + "";
-			// do we have images that we can use to make 
-			// the information look pretty
-			if(imageMap.containsKey(appName)) {
-				List<String> insights = new Vector<String>();
-				List<String> names = new Vector<String>();
-
-				SolrDocumentList list = (SolrDocumentList) imageMap.get(appName);
-				for (int i = 0; i < list.size(); i++) {
-					SolrDocument doc = list.get(i);
-					insights.add((String) doc.get(SolrIndexEngine.APP_INSIGHT_ID));
-					names.add((String) doc.get(SolrIndexEngine.STORAGE_NAME));
-				}
-
-				appMap.put("rdbmsId", insights);
-				appMap.put("insightName", names);
-			}
+		// now we want to add most exeucted insights
+		for(Map<String, Object> app : appInfo) {
+			String appId = app.get("app_id").toString();
+			app.putAll(SecurityQueryUtils.getTopExecutedInsightsForEngine(appId, 10));
 		}
 		
 		return new NounMetadata(appInfo, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.APP_INFO);
