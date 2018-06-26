86efeeccb88cafa1592d367a3c399a54b0a9f78a
mahkhalil <mahkhalil@deloitte.com>
2018-06-26 02:19:56 +0000

    UPDATE ::: Adding logic for setting engine meta values in security database
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@28890 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6c833e065846e0ea6ce382c655b32e46ce1b1bd2

--- src/prerna/solr/reactor/AppInfoReactor.java
+++ src/prerna/solr/reactor/AppInfoReactor.java
@@ -1,19 +1,15 @@
 package prerna.solr.reactor;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.solr.client.solrj.SolrQuery;
-import org.apache.solr.common.SolrDocument;
-
+import prerna.auth.SecurityUtils;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
-import prerna.solr.SolrIndexEngineQueryBuilder;
 
 public class AppInfoReactor extends AbstractReactor {
 	
@@ -26,68 +22,39 @@ public class AppInfoReactor extends AbstractReactor {
 		organizeKeys();
 		String appId = this.keyValue.get(this.keysToGet[0]);
 		
-		SolrIndexEngineQueryBuilder builder = new SolrIndexEngineQueryBuilder();
-		builder.addReturnFields("id");
-		builder.addReturnFields("app_name");
-		builder.addReturnFields("app_description");
-		builder.addReturnFields("app_image");
-		builder.addReturnFields("app_creation_date");
-		builder.addReturnFields("app_requirements");
-		builder.addReturnFields("app_repo_url");
-		builder.addReturnFields("app_tags");
-		builder.addReturnFields("app_type");
-		builder.addReturnFields("app_cost");
-
-		Map<String, List<String>> filterData = new HashMap<String, List<String>>();
-		List<String> filterList = new ArrayList<String>();
-		filterList.add(appId);
-		filterData.put("id", filterList);
-		builder.setFilterOptions(filterData);
+		if(appId == null || appId.isEmpty()) {
+			throw new IllegalArgumentException("Must input an app id");
+		}
 		
-		SolrQuery q = builder.getSolrQuery();
-		SolrDocument appInfo = null;
-//		try {
-//			SolrIndexEngine solrE = SolrIndexEngine.getInstance();
-//			QueryResponse response = solrE.getQueryResponse(q, SolrIndexEngine.SOLR_PATHS.SOLR_APP_PATH_NAME);
-//			SolrDocumentList results = response.getResults();
-//			// there should only be 1
-//			appInfo = results.get(0);
-//			appInfo.put("app_id", appInfo.remove("id"));
-//			
-//			/*
-//			 * Queries below are done on the insight core
-//			 */
-//			
-//			// augment the metadata with the number of insights in the app
-//			long numInsights = solrE.getNumEngineInsights(appId);
-//			appInfo.put("num_insights", numInsights);
-//			
-//			// augment the metadata with the tags for the insights in the app
-//			builder = new SolrIndexEngineQueryBuilder();
-//			// need to re-add the filter but modify the key name
-//			filterData = new HashMap<String, List<String>>();
-//			filterList = new ArrayList<String>();
-//			filterList.add(appId);
-//			filterData.put(SolrIndexEngine.APP_ID, filterList);
-//			builder.setFilterOptions(filterData);
-//			
-//			List<String> facetList = new ArrayList<>();
-//			facetList.add(SolrIndexEngine.TAGS);
-//			// set facet info
-//			builder.setFacet(true);
-//			builder.setFacetField(facetList);
-//			builder.setFacetMinCount(1);
-//			builder.setFacetSortCount(true);
-//			
-//			response = solrE.getQueryResponse(builder.getSolrQuery(), SolrIndexEngine.SOLR_PATHS.SOLR_INSIGHTS_PATH);
-//			List<FacetField> facetFieldList = response.getFacetFields();
-//			Map<String, Map<String, Long>> facetInfo = solrE.processFacetFieldMap(facetFieldList);
-//			appInfo.put("insight_facets", facetInfo);
-//		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException | SolrServerException e) {
-//			e.printStackTrace();
-//		}
+		List<Map<String, String>> baseInfo = null;
+		if(this.securityEnabled()) {
+			// make sure valid id for user
+			if(!this.getUserAppFilters().contains(appId)) {
+				// you dont have access
+				throw new IllegalArgumentException("App does not exist or user does not have access to database");
+			}
+			// user has access!i
+			baseInfo = SecurityUtils.getUserDatabaseList(this.insight.getUserId(), appId);
+		} else {
+			// just grab the info
+			baseInfo = SecurityUtils.getUserDatabaseList(appId);
+		}
+		
+		if(baseInfo.isEmpty()) {
+			throw new IllegalArgumentException("Could not find any app data");
+		}
+		
+		Map<String, String> appInfo = baseInfo.get(0);
+		Map<String, List<String>> additionalMeta = SecurityUtils.getAggregateEngineMetadata(appId);
 
-		return new NounMetadata(appInfo, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.APP_INFO);
+		// combine into return object
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.putAll(appInfo);
+		if(additionalMeta.containsKey("description")) {
+			data.put("app_description", additionalMeta.get("description").get(0));
+		}
+		data.put("app_tags", additionalMeta.get("tags"));
+		return new NounMetadata(data, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.APP_INFO);
 	}
 
 }
--- src/prerna/solr/reactor/SetAppTagsReactor.java
+++ src/prerna/solr/reactor/SetAppTagsReactor.java
@@ -1,47 +1,31 @@
 package prerna.solr.reactor;
 
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Vector;
 
-import org.apache.solr.client.solrj.SolrServerException;
-
+import prerna.auth.SecurityUtils;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
-import prerna.solr.SolrIndexEngine;
 
 public class SetAppTagsReactor extends AbstractReactor {
 	
+	public static final String TAGS = "tags";
+
 	public SetAppTagsReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), "tags"};
+		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), TAGS};
 	}
 
 	@Override
 	public NounMetadata execute() {
-//		organizeKeys();
-//		String appName = this.keyValue.get(this.keysToGet[0]);
-//		List<String> tags = getTags();
-//		
-//		Map<String, Object> fieldsToModify = new HashMap<String, Object>();
-//		fieldsToModify.put("app_tags", tags);
-//		try {
-//			SolrIndexEngine.getInstance().modifyApp(appName, fieldsToModify);
-//			return new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.APP_INFO);
-//		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException | SolrServerException
-//				| IOException e1) {
-//			e1.printStackTrace();
-//		}
-
-		return new NounMetadata(false, PixelDataType.BOOLEAN, PixelOperationType.APP_INFO);
+		organizeKeys();
+		String appName = this.keyValue.get(this.keysToGet[0]);
+		List<String> tags = getTags();
+		SecurityUtils.setEngineMeta(appName, "tags", tags);
+		return new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.APP_INFO);
 	}
 	
 	public List<String> getTags() {
