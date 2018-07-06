68b5b7b4cfc64c44c534679a27d1df84db7658f0
mahkhalil <mahkhalil@deloitte.com>
2018-07-06 16:34:56 +0000

    UPDATE ::: Adding cleanup version of excel parsing
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29110 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 51bddfe1e94c906bb4ea2be974f246482894e516

---
+++ src/prerna/poi/main/helper/excel/ExcelBlock.java
@@ -0,0 +1,148 @@
+package prerna.poi.main.helper.excel;
+
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
+
+public class ExcelBlock {
+
+	// keep track of statistics on the start column
+	private SummaryStatistics startColumnIndexStats = new SummaryStatistics();
+	
+	// keep track of the statistics on the end column
+	private SummaryStatistics totalColumnsInRowStats = new SummaryStatistics();
+	
+	// contain a list of the indicies that this block contains
+	private List<Integer> rowIndicesInBlock = new Vector<Integer>();
+	
+	private int lastColMaxIndex = -1;
+	
+	private Map<Integer, SummaryStatistics> columnToRowStats = new Hashtable <Integer, SummaryStatistics>();
+	
+	/**
+	 * Get the ranges of the block
+	 * This will split up the entire segment if there are empty columns
+	 * @return
+	 */
+	public List<ExcelRange> getRanges() {
+		List<ExcelRange> ranges = new Vector<ExcelRange>();
+
+		boolean started = false;
+		int startCol = 0;
+		
+		for(int colIndex = 0; colIndex <= lastColMaxIndex; colIndex++) {
+			if(!columnToRowStats.containsKey( new Integer(colIndex) ) && started) {
+				
+				if(columnToRowStats.containsKey(startCol)) {
+					ExcelRange r = new ExcelRange(
+							startCol+1, 
+							colIndex, 
+							new Double(columnToRowStats.get(new Integer(startCol)).getMin()).intValue(), 
+							new Double(columnToRowStats.get(new Integer(colIndex-1)).getMax()).intValue());
+					ranges.add(r);
+				}
+				
+				startCol = 0;
+				started = false;
+			}
+			
+			if(columnToRowStats.containsKey(new Integer(colIndex)) && !started ) {
+				started = true;
+				startCol = colIndex;
+			}
+		}
+		
+		return ranges;
+	}
+
+	/**
+	 * Determine if 2 blocks are the same
+	 * @param block
+	 * @return
+	 */
+	public boolean sameAs(ExcelBlock newBlock) {
+		boolean retValue = false;
+		
+		double startDiff = Math.abs(this.startColumnIndexStats.getMean() - newBlock.startColumnIndexStats.getMean());
+		double endDiff = Math.abs(this.totalColumnsInRowStats.getMean() - newBlock.totalColumnsInRowStats.getMean());
+		
+		// need to check to see if they are within the standard deviation
+		// if the start column is similar
+		// and if the number of columns is similar then there is a possibilty this is the same block
+		
+		if(startDiff <= this.startColumnIndexStats.getStandardDeviation()) {
+			if(endDiff <= this.totalColumnsInRowStats.getStandardDeviation()) {
+				retValue = true;
+			}
+		}
+		
+		return retValue;
+	}
+	
+	public void merge(ExcelBlock equivBlock) {
+		this.rowIndicesInBlock.addAll(equivBlock.rowIndicesInBlock);
+		// try to set the last column
+		this.trySetLastColMaxIndex(equivBlock.lastColMaxIndex);
+	}
+	
+	
+	/**
+	 * Add the start column index of this row
+	 * @param colIndex
+	 */
+	public void addStartColumnIndex(int colIndex) {
+		startColumnIndexStats.addValue(colIndex);
+	}
+	
+	/**
+	 * Add the total number of columns to this row
+	 * @param numRows
+	 */
+	public void addTotalColumnsInRowStats(int numRows) {
+		totalColumnsInRowStats.addValue(numRows);
+	}
+	
+	/**
+	 * Add the row index if it contains data
+	 * @param rowIndex
+	 */
+	public void addRowIndexContainingData(int rowIndex) {
+		rowIndicesInBlock.add(rowIndex);
+	}
+	
+	/**
+	 * Determine if this block is empty
+	 * @return
+	 */
+	public boolean isEmpty() {
+		return rowIndicesInBlock.isEmpty();
+	}
+	
+	public void addColumnToRowIndexWithData(int columnIndex, int rowIndex) {
+		SummaryStatistics stats = null;
+		if(columnToRowStats.containsKey(columnIndex)) {
+			stats = columnToRowStats.get(new Integer(columnIndex));
+		} else {
+			stats = new SummaryStatistics();
+			columnToRowStats.put(new Integer(columnIndex), stats);
+		}
+		
+		stats.addValue(rowIndex);
+	}
+	
+	/**
+	 * Try to set a new last col max index
+	 * @param startColMinIndex
+	 */
+	public void trySetLastColMaxIndex(int lastColMaxIndex) {
+		if(this.lastColMaxIndex == -1) {
+			this.lastColMaxIndex = lastColMaxIndex;
+		} else if(this.lastColMaxIndex < lastColMaxIndex) {
+			this.lastColMaxIndex = lastColMaxIndex;
+		}
+	}
+	
+}
---
+++ src/prerna/poi/main/helper/excel/ExcelFilePreProcessor.java
@@ -0,0 +1,108 @@
+package prerna.poi.main.helper.excel;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class ExcelFilePreProcessor {
+
+	private	Workbook workbook = null;
+	private FileInputStream sourceFile = null;
+	private String fileLocation = null;
+	
+	private Map<String, ExcelSheetPreProcessor> sheetProcessor = null;
+	
+	public void parse(String fileLocation) {
+		this.fileLocation = fileLocation;
+		createParser();
+	}
+	
+	/**
+	 * Opens the workbook
+	 */
+	private void createParser() {
+		try {
+			sourceFile = new FileInputStream(fileLocation);
+			try {
+				workbook = WorkbookFactory.create(sourceFile);
+			} catch (EncryptedDocumentException e) {
+				e.printStackTrace();
+			} catch (InvalidFormatException e) {
+				e.printStackTrace();
+			}
+			sheetProcessor = new HashMap<String, ExcelSheetPreProcessor>();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Loop through all the sheets to determine the ranges of tables
+	 */
+	public void determineTableRanges() {
+		int numSheets = workbook.getNumberOfSheets();
+		for(int sheetIndex = 0; sheetIndex < numSheets; sheetIndex++) {
+			determineTableRanges(workbook.getSheetAt(sheetIndex));
+		}
+	}
+	
+	/**
+	 * Determine ranges in a specific sheet
+	 * @param sheet
+	 */
+	private void determineTableRanges(Sheet sheet) {
+		ExcelSheetPreProcessor sProcessor = new ExcelSheetPreProcessor(sheet);
+		sProcessor.determineSheetRanges();
+		sheetProcessor.put(sheet.getSheetName(), sProcessor);
+	}
+	
+	public Map<String, ExcelSheetPreProcessor> getSheetProcessors() {
+		return this.sheetProcessor;
+	}
+	
+	/**
+	 * Clears the parser and requires you to start the parsing from scratch	
+	 */
+	public void clear() {
+		try {
+			if(sourceFile != null) {
+				sourceFile.close(); 
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////
+	/////////////////////////////////////////////////////////////////////
+
+	public static void main(String[] args) {
+		String fileLocation = "C:\\Users\\SEMOSS\\Desktop\\shifted.xlsx";
+
+		ExcelFilePreProcessor processor = new ExcelFilePreProcessor();
+		processor.parse(fileLocation);
+		processor.determineTableRanges();
+		Map<String, ExcelSheetPreProcessor> sheetProcessors = processor.getSheetProcessors();
+		for(String sheet : sheetProcessors.keySet()) {
+			ExcelSheetPreProcessor sProcessor = sheetProcessors.get(sheet);
+			sProcessor.getExcelRanges();
+		}
+	}
+}
