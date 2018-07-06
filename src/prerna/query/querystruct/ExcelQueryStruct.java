27618e40e35370ce0d408d3f23f96e5f0c32e65a
Maher Khalil <themaherkhalil@gmail.com>
2018-07-06 17:28:39 +0000

    UPDATE ::: Adding sheet range to be passed into excel iterating
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29112 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8843018a5f2af12c4aa5456cdf772e77b660c3d4

--- src/prerna/query/querystruct/ExcelQueryStruct.java
+++ src/prerna/query/querystruct/ExcelQueryStruct.java
@@ -3,6 +3,7 @@ package prerna.query.querystruct;
 public class ExcelQueryStruct extends AbstractFileQueryStruct {
 
 	private String sheetName;
+	private String sheetRange;
 	
 	public ExcelQueryStruct() {
 		this.setQsType(QUERY_STRUCT_TYPE.EXCEL_FILE);
@@ -16,6 +17,14 @@ public class ExcelQueryStruct extends AbstractFileQueryStruct {
 		this.sheetName = sheetName;
 	}
 	
+	public String getSheetRange() {
+		return this.sheetRange;
+	}
+
+	public void setSheetRange(String sheetRange) {
+		this.sheetRange = sheetRange;
+	}
+	
 	@Override
 	public SelectQueryStruct getNewBaseQueryStruct() {
 		ExcelQueryStruct newQs = new ExcelQueryStruct();
@@ -23,6 +32,7 @@ public class ExcelQueryStruct extends AbstractFileQueryStruct {
 		newQs.setFilePath(this.getFilePath());
 		newQs.setColumnTypes(this.getColumnTypes());
 		newQs.setSheetName(this.getSheetName());
+		newQs.setSheetRange(this.getSheetRange());
 		return newQs;
 	}
 }
