45a3cf4bab424f8ce5140ed8e672f0a0a370eeda
mahkhalil <mahkhalil@deloitte.com>
2018-07-09 17:30:55 +0000

    UPDATE ::: Adding streaming logic to determine data types so iterating only needs to occur once
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29138 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6e15c485d24b6bb7b4788faf3695b7cca1e1d75f

--- src/prerna/poi/main/helper/excel/ExcelBlock.java
+++ src/prerna/poi/main/helper/excel/ExcelBlock.java
@@ -1,11 +1,17 @@
 package prerna.poi.main.helper.excel;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
 import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
+
+import prerna.algorithm.api.SemossDataType;
+import prerna.poi.main.helper.FileHelperUtil;
 
 public class ExcelBlock {
 
@@ -21,7 +27,11 @@ public class ExcelBlock {
 	private int lastColMaxIndex = -1;
 	
 	private Map<Integer, SummaryStatistics> columnToRowIndexStats = new Hashtable <Integer, SummaryStatistics>();
-
+	
+	// for data types
+	private Map<Integer, Map<SemossDataType, SummaryStatistics>> columnToTypeStats = new Hashtable <Integer, Map<SemossDataType, SummaryStatistics>>();
+	private Map<Integer, Map<String, SummaryStatistics>> additionalFormatTracker = new HashMap<Integer, Map<String, SummaryStatistics>>();
+	
 	/**
 	 * Get the ranges of the block
 	 * This will split up the entire segment if there are empty columns
@@ -57,6 +67,133 @@ public class ExcelBlock {
 		
 		return ranges;
 	}
+	
+	public Object[][] getRangeTypes(ExcelRange range) {
+		int[] rangeIndex = range.getIndices();
+		int numCols = rangeIndex[2] - rangeIndex[0] + 1;
+		Object[][] predictedTypes = new Object[numCols][3];
+		
+		// loop through based on the range to figure out the types
+		// note, this is just < and not <= in the loop
+		int colIndex = 0;
+		COLUMN_LOOP : for(int cellIndex = rangeIndex[0]-1; cellIndex < rangeIndex[2]; cellIndex++, colIndex++) {
+			// get column index
+			
+			int startRow = rangeIndex[1];
+			int endRow = rangeIndex[3];
+			
+			Map<SemossDataType, SummaryStatistics> typesMap = columnToTypeStats.get(cellIndex);
+			
+			// we gotta see based on the range
+			// what the type will be
+			// basically
+			// if the summary stats min/max contains the startrow/endrow
+			// we know it is that type
+			// but the order of the checks is important
+			// if it contains 1 string, then string
+			// it it contains multiple types, then string
+			// if it is combination of int and double, then double
+			// if it is combination of timestamp and date, then timestamp
+			// if other combination, then string
+			
+			boolean containsStr = false;
+			boolean containsInt = false;
+			boolean containsDouble = false;
+			boolean containsDate = false;
+			boolean containsTimestamp = false;
+			
+			if(typesMap.containsKey(SemossDataType.STRING)) {
+				containsStr = testTypeContainedWtihinRange(startRow, endRow, typesMap.get(SemossDataType.STRING));
+				// if we have string, we are done
+				if(containsStr) {
+					Object[] columnPrediction = new Object[2];
+					columnPrediction[0] = SemossDataType.STRING;
+					predictedTypes[colIndex] = columnPrediction;
+					continue COLUMN_LOOP;
+				}
+			}
+			
+			if(typesMap.containsKey(SemossDataType.INT)) {
+				containsInt = testTypeContainedWtihinRange(startRow, endRow, typesMap.get(SemossDataType.INT));
+			}
+			if(typesMap.containsKey(SemossDataType.DOUBLE)) {
+				containsDouble = testTypeContainedWtihinRange(startRow, endRow, typesMap.get(SemossDataType.DOUBLE));
+			}
+			if(typesMap.containsKey(SemossDataType.DATE)) {
+				containsDate = testTypeContainedWtihinRange(startRow, endRow, typesMap.get(SemossDataType.DATE));
+			}
+			if(typesMap.containsKey(SemossDataType.TIMESTAMP)) {
+				containsTimestamp = testTypeContainedWtihinRange(startRow, endRow, typesMap.get(SemossDataType.TIMESTAMP));
+			}
+			
+			
+			// if we have some kind of number
+			// if only int, then int
+			// otherwise, double
+			if(!containsDate && !containsTimestamp) {
+				Object[] columnPrediction = new Object[2];
+				if(containsInt && !containsDouble) {
+					columnPrediction[0] = SemossDataType.INT;
+				} else {
+					columnPrediction[0] = SemossDataType.DOUBLE;
+				}
+				predictedTypes[colIndex] = columnPrediction;
+				continue COLUMN_LOOP;
+			}
+			
+			// if we have some kind of date or timestamp
+			// if only date, then date
+			// otherwise, timestamp
+			if(!containsInt && !containsDouble) {
+				Map<String, SummaryStatistics> formatting = additionalFormatTracker.get(cellIndex);
+				
+				Map<String, Integer> mostPopularFormat = new HashMap<String, Integer>();
+				for(String s : formatting.keySet()) {
+					mostPopularFormat.put(s, (int) formatting.get(s).getN());
+				}
+				// reconcile formats
+				FileHelperUtil.reconcileDateFormats(mostPopularFormat);
+				String mostOccuringFormat = Collections.max(mostPopularFormat.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
+				
+				Object[] columnPrediction = new Object[2];
+				if(containsDate && !containsTimestamp) {
+					columnPrediction[0] = SemossDataType.DATE;
+				} else {
+					columnPrediction[0] = SemossDataType.TIMESTAMP;
+				}
+				columnPrediction[2] = mostOccuringFormat;
+				predictedTypes[colIndex] = columnPrediction;
+				continue COLUMN_LOOP;
+			}
+			
+			// we have mixed types
+			// return string
+			Object[] columnPrediction = new Object[2];
+			columnPrediction[0] = SemossDataType.STRING;
+			predictedTypes[colIndex] = columnPrediction;
+			continue COLUMN_LOOP;
+		}
+		
+		
+		return predictedTypes;
+	}
+	
+	private boolean testTypeContainedWtihinRange(int startRow, int endRow, SummaryStatistics stats) {
+		// we need to ignore the type for the startRow
+		double minRow = stats.getMin();
+		double maxRow = stats.getMax();
+		
+		if(startRow > minRow) {
+			return true;
+		}
+		
+		if(endRow <= maxRow) {
+			return true;
+		}
+		
+		return false;
+	}
+	
 
 	/**
 	 * Determine if 2 blocks are the same
@@ -121,16 +258,57 @@ public class ExcelBlock {
 		return rowIndicesInBlock.isEmpty();
 	}
 	
-	public void addColumnToRowIndexWithData(int columnIndex, int rowIndex) {
+	public void addColumnToRowIndexWithData(int columnIndex, int rowIndex, SemossDataType type, String additionalType) {
+		Integer objColumnIndex = new Integer(columnIndex);
+		// update index stats
 		SummaryStatistics stats = null;
-		if(columnToRowIndexStats.containsKey(columnIndex)) {
-			stats = columnToRowIndexStats.get(new Integer(columnIndex));
+		if(columnToRowIndexStats.containsKey(objColumnIndex)) {
+			stats = columnToRowIndexStats.get(objColumnIndex);
 		} else {
 			stats = new SummaryStatistics();
-			columnToRowIndexStats.put(new Integer(columnIndex), stats);
+			columnToRowIndexStats.put(objColumnIndex, stats);
 		}
 		
 		stats.addValue(rowIndex);
+		
+		// update data type stats
+		Map<SemossDataType, SummaryStatistics> typeMap = null;
+		if(columnToTypeStats.containsKey(objColumnIndex)) {
+			typeMap = columnToTypeStats.get(objColumnIndex);
+		} else {
+			typeMap = new HashMap<SemossDataType, SummaryStatistics>();
+			columnToTypeStats.put(objColumnIndex, typeMap);
+		}
+		
+		SummaryStatistics typeStats = null;
+		if(typeMap.containsKey(type)) {
+			typeStats = typeMap.get(type);
+		} else {
+			typeStats = new SummaryStatistics();
+			typeMap.put(type, typeStats);
+		}
+		
+		typeStats.addValue(rowIndex);
+		
+		// also acount for additional types
+		if(additionalType != null) {
+			Map<String, SummaryStatistics> tracker = null;
+			if(additionalFormatTracker.containsKey(objColumnIndex)) {
+				tracker = additionalFormatTracker.get(objColumnIndex);
+			} else {
+				tracker = new HashMap<String, SummaryStatistics>();
+				additionalFormatTracker.put(objColumnIndex, tracker);
+			}
+			
+			SummaryStatistics additionalTypeStats = null;
+			if(tracker.containsKey(additionalType)) {
+				additionalTypeStats = tracker.get(additionalType);
+			} else {
+				additionalTypeStats = new SummaryStatistics();
+				tracker.put(additionalType, additionalTypeStats);
+			}
+			stats.addValue(rowIndex);
+		}
 	}
 	
 	/**
