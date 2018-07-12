2a22b5ff629cce635c17912409f7fe848b94feb2
mahkhalil <mahkhalil@deloitte.com>
2018-07-12 16:47:02 +0000

    UPDATE ::: Modifying git to take in app id and app name for new way we are storing smss and db folders
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29224 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/git/reactors/GitStatusReactor.java
+++ src/prerna/util/git/reactors/GitStatusReactor.java
@@ -3,6 +3,8 @@ package prerna.util.git.reactors;
 import java.util.List;
 import java.util.Map;
 
+import prerna.auth.SecurityQueryUtils;
+import prerna.nameserver.utility.MasterDatabaseUtility;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
@@ -19,7 +21,27 @@ public class GitStatusReactor extends AbstractReactor {
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
-		List<Map<String, String>> fileInfo = GitUtils.getStatus(this.keyValue.get(this.keysToGet[0]));
+		
+		String appId = this.keyValue.get(this.keysToGet[0]);
+		if(appId == null || appId.isEmpty()) {
+			throw new IllegalArgumentException("Need to provide the app name");
+		}
+		
+		String appName = null;
+		
+		// you can only push
+		// if you are the owner
+		if(this.securityEnabled()) {
+			appId = SecurityQueryUtils.testUserEngineIdForAlias(this.insight.getUser(), appId);
+			if(!SecurityQueryUtils.userCanEditEngine(this.insight.getUser(), appId)) {
+				throw new IllegalArgumentException("App does not exist or user does not have access to edit database");
+			}
+			appName = SecurityQueryUtils.getEngineAliasForId(appId);
+		} else {
+			appName = MasterDatabaseUtility.getEngineAliasForId(appId);
+		}
+		
+		List<Map<String, String>> fileInfo = GitUtils.getStatus(appId, appName);
 		return new NounMetadata(fileInfo, PixelDataType.VECTOR, PixelOperationType.MARKET_PLACE);
 	}
 
