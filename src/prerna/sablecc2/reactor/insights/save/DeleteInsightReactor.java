40696dd24f7471ee7fb40e7d4f7f86b1734f5df5
mahkhalil <mahkhalil@deloitte.com>
2018-07-12 18:22:35 +0000

    UPDATE ::: Fixing deleting of an insight
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29233 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: be8510dd68b937ab740249ba6cd41c3c81071360

--- src/prerna/sablecc2/reactor/insights/save/DeleteInsightReactor.java
+++ src/prerna/sablecc2/reactor/insights/save/DeleteInsightReactor.java
@@ -1,24 +1,15 @@
 package prerna.sablecc2.reactor.insights.save;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.Vector;
-
-import org.apache.commons.io.FileUtils;
-
+import prerna.auth.SecurityQueryUtils;
 import prerna.auth.SecurityUpdateUtils;
 import prerna.engine.api.IEngine;
 import prerna.engine.impl.InsightAdministrator;
-import prerna.engine.impl.SmssUtilities;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
-import prerna.util.Constants;
-import prerna.util.DIHelper;
 import prerna.util.Utility;
 
 public class DeleteInsightReactor extends AbstractReactor {
@@ -30,69 +21,36 @@ public class DeleteInsightReactor extends AbstractReactor {
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
-		// get engine name from the id list
-		String engineName = null;
-		// get ids to delete
-		List<String> solrIDList = new Vector<String>();
-		List<String> insightIDList = new Vector<String>();
 		GenRowStruct grs = this.store.getNoun(this.keysToGet[0]);
-		if (grs != null && !grs.isEmpty()) {
-			int size = grs.size();
-			for (int i = 0; i < size; i++) {
-				// id is passed in from solr id where it is defined as engine_id
-				// so I need to split it
-				String id = grs.get(i).toString();
-				if (id.contains("_")) {
-					String insightId = id.substring(id.lastIndexOf("_") + 1, id.length());
-					engineName = id.substring(0, id.lastIndexOf("_"));
-					insightIDList.add(insightId);
-					solrIDList.add(id);
-				}
-			}
-		}
-
-		// no key is added, grab all ids
-		int size = this.curRow.size();
+		
+		int size = grs.size();
 		for (int i = 0; i < size; i++) {
-			String id = this.curRow.get(i).toString();
 			// id is passed in from solr id where it is defined as engine_id
 			// so I need to split it
-			if (id.contains("_")) {
-				String insightId = id.substring(id.lastIndexOf("_") + 1, id.length());
-				engineName = id.substring(0, id.lastIndexOf("_"));
-				insightIDList.add(insightId);
-				solrIDList.add(id);
+			String id = grs.get(i).toString();
+			String[] split = id.split("_");
+			String appId = split[0];
+			String insightId = split[1];
+			
+			if(this.securityEnabled()) {
+				if(!SecurityQueryUtils.userCanEditInsight(this.insight.getUser(), appId, insightId)) {
+					throw new IllegalArgumentException("User does not have permission to edit this insight");
+				}
 			}
-		}
-		IEngine engine = Utility.getEngine(engineName);
-		if (engine != null) {
+			
+			IEngine engine = Utility.getEngine(appId);
 			// delete from insights database
 			InsightAdministrator admin = new InsightAdministrator(engine.getInsightDatabase());
 			try {
-				admin.dropInsight(insightIDList.toArray(new String[insightIDList.size()]));
+				admin.dropInsight(insightId);
 			} catch (RuntimeException e) {
 				e.printStackTrace();
 			}
 
-			SecurityUpdateUtils.deleteInsight(engineName, insightIDList.toArray(new String[insightIDList.size()]));
-			
-			// delete insight .mosfet file, if it exists
-			String recipePath = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + "\\" + Constants.DB + "\\" + SmssUtilities.getUniqueName(engine.getEngineName(), engine.getEngineId()) + "\\version\\";
-			for (int i = 0; i < insightIDList.size(); i++) {
-				File dir = new File(recipePath + insightIDList.get(i));
-				// if it exists then delete all files inside
-				if (dir.exists() && dir.isDirectory()) {
-					try {
-						FileUtils.deleteDirectory(dir);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-			return new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.DELETE_INSIGHT);
+			SecurityUpdateUtils.deleteInsight(appId, insightId);
 		}
-		// unable to delete
-		return new NounMetadata(false, PixelDataType.BOOLEAN, PixelOperationType.DELETE_INSIGHT);
+		
+		return new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.DELETE_INSIGHT);
 	}
 
 }
