ed2c34a2ead6f50714be5559de3d397e5af2651b
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-07-24 14:47:39 +0000

    UPDATE ::: Upload rdf, rdbms, tinker reactor inputs
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29331 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2ffa6c06a5c769ae6ea3e9ea3a00d3bd6e8e2783

--- src/prerna/sablecc2/reactor/app/upload/rdf/RdfCsvUploadReactor.java
+++ src/prerna/sablecc2/reactor/app/upload/rdf/RdfCsvUploadReactor.java
@@ -44,8 +44,9 @@ public class RdfCsvUploadReactor extends AbstractReactor {
 	public RdfCsvUploadReactor() {
 		this.keysToGet = new String[] { UploadInputUtility.APP, UploadInputUtility.FILE_PATH,
 				UploadInputUtility.DELIMITER, UploadInputUtility.DATA_TYPE_MAP, UploadInputUtility.NEW_HEADERS,
-				UploadInputUtility.METAMODEL, UploadInputUtility.ADDITIONAL_DATA_TYPES,
-				UploadInputUtility.ADD_TO_EXISTING, UploadInputUtility.PROP_FILE };
+				UploadInputUtility.METAMODEL, UploadInputUtility.PROP_FILE, UploadInputUtility.ADD_TO_EXISTING,
+				UploadInputUtility.START_ROW, UploadInputUtility.END_ROW, UploadInputUtility.ADDITIONAL_DATA_TYPES,
+				UploadInputUtility.CUSTOM_BASE_URI };
 	}
 
 	@Override
