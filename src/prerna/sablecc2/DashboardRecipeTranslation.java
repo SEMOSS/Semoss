b70c731ebe06e0526eba9c2cb0031231457134c7
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-07-26 16:47:28 +0000

    UPDATE ::: Pixel utility to check if recipe is for a dashboard
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29373 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/sablecc2/DashboardRecipeTranslation.java
@@ -0,0 +1,42 @@
+package prerna.sablecc2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import prerna.sablecc2.analysis.DepthFirstAdapter;
+import prerna.sablecc2.node.AConfiguration;
+import prerna.sablecc2.node.AOperation;
+import prerna.sablecc2.node.PRoutine;
+
+public class DashboardRecipeTranslation extends DepthFirstAdapter {
+
+	private static final Logger LOGGER = LogManager.getLogger(DashboardRecipeTranslation.class.getName());
+	private boolean isDashboard = false;
+	
+	@Override
+	public void caseAConfiguration(AConfiguration node) {
+        List<PRoutine> copy = new ArrayList<PRoutine>(node.getRoutine());
+        for(PRoutine e : copy) {
+        	String expression = e.toString();
+        	LOGGER.info("Processing " + expression);
+        	e.apply(this);
+        }
+	}
+	
+	@Override
+	public void inAOperation(AOperation node) {
+		defaultIn(node);
+		String reactorId = node.getId().toString().trim();
+		if (reactorId.equals("DashboardInsightConfig")) {
+			isDashboard = true;
+		}
+	}
+
+	public boolean isDashboard() {
+		return isDashboard;
+	}
+}
+
