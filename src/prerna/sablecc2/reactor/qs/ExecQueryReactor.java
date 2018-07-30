3ee0424802aa816d56003ab1030116bbf3becd35
mahkhalil <mahkhalil@deloitte.com>
2018-07-30 15:56:03 +0000

    UPDATE ::: Adding update/delete to work on frames
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29393 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 92a17304638ffc029c301518de1eb5ea7297854f

--- src/prerna/sablecc2/reactor/qs/ExecQueryReactor.java
+++ src/prerna/sablecc2/reactor/qs/ExecQueryReactor.java
@@ -1,9 +1,14 @@
 package prerna.sablecc2.reactor.qs;
 
+import prerna.algorithm.api.ITableDataFrame;
+import prerna.ds.h2.H2Frame;
 import prerna.engine.api.IEngine;
 import prerna.engine.impl.rdbms.RDBMSNativeEngine;
+import prerna.query.querystruct.AbstractQueryStruct;
+import prerna.query.querystruct.AbstractQueryStruct.QUERY_STRUCT_TYPE;
 import prerna.query.querystruct.SelectQueryStruct;
 import prerna.query.querystruct.delete.DeleteSqlInterpreter;
+import prerna.query.querystruct.transform.QSAliasToPhysicalConverter;
 import prerna.query.querystruct.update.UpdateQueryStruct;
 import prerna.query.querystruct.update.UpdateSqlInterpreter;
 import prerna.sablecc2.om.GenRowStruct;
@@ -28,41 +33,56 @@ public class ExecQueryReactor extends AbstractReactor {
 		}
 		
 		IEngine engine = null;
-		SelectQueryStruct sQS = null;
-		UpdateQueryStruct uQS = null;
+		ITableDataFrame frame = null;
+		AbstractQueryStruct qs = null;
 		boolean success = false;
 
-		if(qStruct.getValue() instanceof SelectQueryStruct) {
-			sQS = ((SelectQueryStruct) qStruct.getValue());
-			engine = sQS.retrieveQueryStructEngine();
+		if(qStruct.getValue() instanceof AbstractQueryStruct) {
+			qs = ((AbstractQueryStruct) qStruct.getValue());
+			if(qs.getQsType() == QUERY_STRUCT_TYPE.ENGINE) {
+				engine = qs.retrieveQueryStructEngine();
+				if(!(engine instanceof RDBMSNativeEngine)) {
+					throw new IllegalArgumentException("Query update/deletes only works for rdbms databases");
+				}
+			} else if(qs.getQsType() == QUERY_STRUCT_TYPE.FRAME) {
+				frame = qs.getFrame();
+				if(!(frame instanceof H2Frame)) {
+					throw new IllegalArgumentException("Query update/deletes only works for sql frames");
+				}
+				// convert any aliases the FE is using from the frame
+				qs = QSAliasToPhysicalConverter.getPhysicalQs(qs, frame.getMetaData());
+			}
 		} else {
-			uQS = ((UpdateQueryStruct) qStruct.getValue());
-			engine = uQS.retrieveQueryStructEngine();
+			throw new IllegalArgumentException("Input to exec query requires a query struct");
 		}
 
-		if(!(engine instanceof RDBMSNativeEngine)) {
-			throw new IllegalArgumentException("Insert query only works for rdbms databases");
+		String query = null;
+		if(qs instanceof UpdateQueryStruct) {
+			UpdateSqlInterpreter interp = new UpdateSqlInterpreter((UpdateQueryStruct) qs);
+			query = interp.composeQuery();
+		} else if(qs instanceof SelectQueryStruct) {
+			DeleteSqlInterpreter interp = new DeleteSqlInterpreter((SelectQueryStruct) qs);
+			query = interp.composeQuery();
 		}
-
-		if(sQS != null) {
-			DeleteSqlInterpreter interp = new DeleteSqlInterpreter(sQS);
-			String sqlQuery = interp.composeQuery();
-			System.out.println("SQL QUERY...." + sqlQuery);
-			engine.insertData(sqlQuery);
+		
+		System.out.println("SQL QUERY...." + query);
+		if(qs.getQsType() == QUERY_STRUCT_TYPE.ENGINE) {
+			engine.insertData(query);
 			success = true;
 		} else {
-			UpdateSqlInterpreter interp = new UpdateSqlInterpreter(uQS);
-			String sqlQuery = interp.composeQuery();
-			System.out.println("SQL QUERY...." + sqlQuery);
-			engine.insertData(sqlQuery);
-			success = true;
+			try {
+				((H2Frame) frame).getBuilder().runQuery(query);
+				success = true;
+			} catch (Exception e) {
+				success = false;
+				e.printStackTrace();
+			}
 		}
 		
 		return new NounMetadata(success, PixelDataType.BOOLEAN, PixelOperationType.ALTER_DATABASE);
 	}
 	
 	private NounMetadata getQueryStruct() {
-		
 		NounMetadata object = new NounMetadata(null, PixelDataType.QUERY_STRUCT);
 		GenRowStruct allNouns = getNounStore().getNoun(PixelDataType.QUERY_STRUCT.toString());
 		NounMetadata f = new NounMetadata(false, PixelDataType.BOOLEAN);
