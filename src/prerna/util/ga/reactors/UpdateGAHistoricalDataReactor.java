3e83d8558758931a9ea5d1a71d571e653aa17f4a
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-08-01 20:24:28 +0000

    UPDATE ::: Reactor using new GA R scripts
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29430 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/ga/reactors/UpdateGAHistoricalDataReactor.java
+++ src/prerna/util/ga/reactors/UpdateGAHistoricalDataReactor.java
@@ -23,7 +23,7 @@ public class UpdateGAHistoricalDataReactor extends AbstractRFrameReactor {
 		organizeKeys();
 		
 		// check if packages are installed
-		String[] packages = {"RGoogleAnalytics", "httr", "data.table", "jsonlite", "plyr", "lubridate", "curl"};
+		String[] packages = {"RGoogleAnalytics", "httr", "data.table", "jsonlite", "plyr", "lubridate", "curl", "lsa", "LSAfun", "text2vec", "stringr", "stringdist"};
 		this.rJavaTranslator.checkPackages(packages);
 	
 		// get start day from inputs, date range default is 10 days
@@ -54,18 +54,15 @@ public class UpdateGAHistoricalDataReactor extends AbstractRFrameReactor {
 		this.rJavaTranslator.runR(script);
 		
 		// generate script for database recommendations
-		String userDf2 = "user_" + Utility.getRandomString(8);
-		script = "rm(get_userdata, " + userDf + "); source(\"" + rDirectory + "\\db_recom.r\"); " + userDf2 + "<-get_userdata(\"" + startDate + "\",\"" + todayDate + "\", \"" + rDirectory + "\\token_file\"); fileroot<-\"dataitem\"; ";
+		script = "source(\"" + rDirectory + "\\db_recom.r\"); source(\"" + rDirectory + "\\datasemantic.r\"); source(\"" + rDirectory + "\\SemanticSimilarity\\lsi_dataitem.r\"); data_domain_mgr(\"" + startDate + "\",\"" + todayDate + "\", \"dataitem\"); ";
 		script = script.replace("\\", "/");
 		this.rJavaTranslator.runR(script);
-		String run = "refresh_base(" + userDf2 + ",fileroot)";
-		this.rJavaTranslator.runR(run);
 
 		// set working directory back to normal
 		this.rJavaTranslator.runR("setwd('" + wd + "');\n");
 		
 		// garbage cleanup
-		String gc = "rm(fileroot, viz_recom_mgr, restore_datatype, get_reference, blend_tracking_semantic, get_userdata, dataitem_history, get_dataitem_rating, assign_unique_concepts, populate_ratings, build_sim, cosine_jaccard_sim, cosine_sim, jaccard_sim, apply_tfidf, compute_weight, dataitem_recom_mgr, get_item_recom, get_user_recom, hop_away_recom_mgr, hop_away_mgr, locate_user_communities, drilldown_communities, locate_data_communities, get_items_users, refresh_base," + userDf + ", " + historyDf + ", viz_history, viz_recom, get_userdata);";
+		String gc = "rm(" + userDf + ", " + historyDf + ",apply_tfidf, assign_unique_concepts,blend_mgr,blend_tracking_semantic,build_query_doc,build_query_tdm,build_sim,build_tdm,col2db,col2tbl,column_lsi_mgr,compute_column_desc_sim,compute_entity_sim,compute_weight,con,cosine_jaccard_sim,cosine_sim,data_domain_mgr,dataitem_history,dataitem_recom_mgr,datasemantic_history,drilldown_communities,exec_tfidf,get_dataitem_rating,get_item_recom,get_items_users,get_reference,get_similar_doc,get_user_recom,get_userdata,hop_away_mgr,hop_away_recom_mgr,jaccard_sim,locate_data_communities,locate_user_communities,lsi_mgr,match_desc,populate_ratings,read_datamatrix,refresh_base,remove_files,restore_datatype,viz_history,viz_recom,viz_recom_mgr)";
 		this.rJavaTranslator.runR(gc);
 		
 		return new NounMetadata(true, PixelDataType.BOOLEAN);
