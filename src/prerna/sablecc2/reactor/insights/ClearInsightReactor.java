5295eb5497731f934b1858ea82810ea7bcdc21e8
mahkhalil <mahkhalil@deloitte.com>
2018-08-01 12:35:50 +0000

    UPDATE ::: Switch for clear insight to happen upon call and not in the stream
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29419 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/insights/ClearInsightReactor.java
+++ src/prerna/sablecc2/reactor/insights/ClearInsightReactor.java
@@ -1,16 +1,17 @@
 package prerna.sablecc2.reactor.insights;
 
-import prerna.sablecc2.om.PixelDataType;
-import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.insight.InsightUtility;
 
 public class ClearInsightReactor extends AbstractReactor {
 
 	@Override
 	public NounMetadata execute() {
+		NounMetadata newNoun = InsightUtility.clearInsight(this.insight);
+		return newNoun;
 		// since we stream results in pixel web utility
 		// we will just put a placeholder here to clear the insight
-		return new NounMetadata(true, PixelDataType.CLEAR_INSIGHT, PixelOperationType.CLEAR_INSIGHT);
+//		return new NounMetadata(true, PixelDataType.CLEAR_INSIGHT, PixelOperationType.CLEAR_INSIGHT);
 	}
 }
