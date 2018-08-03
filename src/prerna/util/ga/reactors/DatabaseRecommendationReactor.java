6d4398e8ce697644970d12292649272f1c76c65b
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-08-03 19:57:50 +0000

    UPDATE ::: Sending both user communities and recommended engines.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29446 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f93832e2af0aca1d7987a304800b8473cbacdb37

--- src/prerna/util/ga/reactors/DatabaseRecommendationReactor.java
+++ src/prerna/util/ga/reactors/DatabaseRecommendationReactor.java
@@ -13,7 +13,6 @@ import com.google.gson.reflect.TypeToken;
 
 import prerna.auth.SecurityQueryUtils;
 import prerna.nameserver.utility.MasterDatabaseUtility;
-import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
@@ -50,12 +49,12 @@ public class DatabaseRecommendationReactor extends AbstractRFrameReactor {
 			String errorMessage = "\nMake sure you have all the following R libraries installed:\n" + packageError;
 			logger.info(errorMessage);
 		} else {
-			// all packages are installed run recommendations
-			boolean communities = getBool();
-
-			// get user name
+			// Step 1:
+			// Run an R script to generate all communities, then get
+			// additional data for each engine that exists on this machine,
+			// package it up as a map to add to a list of outputs for the FE.
+			
 			String userName = System.getProperty("user.name");
-
 			String baseFolder = DIHelper.getInstance().getProperty("BaseFolder");
 			String script = "source(\"" + baseFolder + "\\R\\Recommendations\\db_recom.r\"); ";
 			script += "fileroot<-\"" + baseFolder + "\\R\\Recommendations\\dataitem\" ; ";
@@ -65,16 +64,14 @@ public class DatabaseRecommendationReactor extends AbstractRFrameReactor {
 			this.rJavaTranslator.runR(script);
 
 			List<String> enginesWithAccess = SecurityQueryUtils.getUserEngineIds(this.insight.getUser());
-			if (communities) {
-				// run communities script
-				script = "output<- locate_data_communities(fileroot,\"" + userName + "\");  output<-toJSON(output[3]);";
-				this.rJavaTranslator.runR(script);
+			// run communities script
+			script = "output<- locate_data_communities(fileroot,\"" + userName + "\");  output<-toJSON(output[3]);";
+			this.rJavaTranslator.runR(script);
+			ArrayList<Object> communitiesList = new ArrayList<Object>();
 
-				String json = this.rJavaTranslator.getString("output;");
-				// the script failed or they dont have the historical data
-				if (json == null) {
-					return new NounMetadata(recommendations, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.RECOMMENDATION);
-				}
+			String json = this.rJavaTranslator.getString("output;");
+			// the script failed or they dont have the historical data
+			if (json != null) {
 				Gson gson = new Gson();
 				ArrayList<HashMap<String, ArrayList<String>>> myList = gson.fromJson(json, new TypeToken<ArrayList<HashMap<String, ArrayList<String>>>>() {}.getType());
 
@@ -85,10 +82,10 @@ public class DatabaseRecommendationReactor extends AbstractRFrameReactor {
 						ArrayList<String> communityMembers = map.get(key);
 						ArrayList<HashMap<String, String>> convertedMembers = new ArrayList<HashMap<String, String>>();
 						for (String member : communityMembers) {
-							HashMap<String, String> engineDetail = new HashMap<String, String>();
 							String[] id = member.split("\\$");
-							// only show engines on my machine that I have access to
-							if (enginesWithAccess.contains(id[0])) {
+							if (id != null && communitiesList.size() < 10 && enginesWithAccess.contains(id[0])) {
+								// only send 10 and make sure they exist on this machine
+								HashMap<String, String> engineDetail = new HashMap<String, String>();
 								String alias = MasterDatabaseUtility.getEngineAliasForId(id[0]);
 								String engId = id[0];
 								String type = (Utility.getEngine(engId)).getEngineType() + "";
@@ -98,38 +95,52 @@ public class DatabaseRecommendationReactor extends AbstractRFrameReactor {
 								convertedMembers.add(engineDetail);
 							}
 						}
-						if (convertedMembers != null && !convertedMembers.isEmpty()) {
-							recommendations.add(convertedMembers);
-						}
-					}
-				}
-			} else {
-				// run plain db recommendations script
-				script = "output<- dataitem_recom_mgr(\"" + userName + "\",fileroot,0.02);  output<-toJSON(as.data.table(output[2])[,1:2], byrow = TRUE, colNames = TRUE);";
-				this.rJavaTranslator.runR(script);
-
-				// parse R json response for final recommendation data
-				String json = this.rJavaTranslator.getString("output;");
-				// the script failed or they dont have the historical data
-				if (json == null) {
-					return new NounMetadata(recommendations, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.RECOMMENDATION);
-				}
-				Gson gson = new Gson();
-				ArrayList<Map<String, String>> myList = gson.fromJson(json, new TypeToken<ArrayList<HashMap<String, String>>>() {}.getType());
-				for (int i = 0; i < myList.size(); i++) {
-					Map<String, String> entry = new HashMap<String, String>();
-					String item = myList.get(i).get("item");
-					String[] vals = item.split("\\$");
-					if (vals == null) {
-						continue;
-					}
-					// only return databases the user can access
-					if (enginesWithAccess.contains(vals[0])) {
-						entry.put(MasterDatabaseUtility.getEngineAliasForId(vals[0]), vals[0]);
-						recommendations.add(entry);
+						communitiesList.add(convertedMembers);
 					}
 				}
 			}
+			recommendations.add(communitiesList);
+
+			// Step 2:
+			// Run another R script to generate user specific recommendations,
+			// add additional data and package as a map to be added to the list
+			// for the FE.
+
+			// run plain db recommendations script
+			script = "output<- dataitem_recom_mgr(\"" + userName + "\",fileroot,0.02);  output<-toJSON(as.data.table(output[2])[,1:2], byrow = TRUE, colNames = TRUE);";
+			this.rJavaTranslator.runR(script);
+
+			// parse R json response for final recommendation data
+			json = this.rJavaTranslator.getString("output;");
+			// the script failed or they dont have the historical data
+			ArrayList<Object> recommendationsFinal = new ArrayList<Object>();
+			if (json != null) {
+				ArrayList<Object> recommendList = new ArrayList<Object>();
+				Gson gson = new Gson();
+				ArrayList<Map<String, String>> recList = gson.fromJson(json, new TypeToken<ArrayList<HashMap<String, String>>>() {}.getType());
+				for (int i = 0; i < recList.size(); i++) {
+					String item = recList.get(i).get("item");
+					String[] vals = item.split("\\$");
+					if (vals != null && recommendationsFinal.size() < 10 && enginesWithAccess.contains(vals[0])) {
+						// only send 10 and make sure they exist on this machine
+						ArrayList<HashMap<String, String>> convertedMembers = new ArrayList<HashMap<String, String>>();
+						HashMap<String, String> engineDetail = new HashMap<String, String>();
+						String engId = vals[0];
+						String freq = recList.get(i).get("freq");
+						String alias = MasterDatabaseUtility.getEngineAliasForId(engId);
+						String type = (Utility.getEngine(engId)).getEngineType() + "";
+
+						// only return databases the user can access
+						engineDetail.put("appName", alias);
+						engineDetail.put("appId", engId);
+						engineDetail.put("appType", type);
+						engineDetail.put("freq", freq);
+						convertedMembers.add(engineDetail);
+						recommendList.add(convertedMembers);
+					}
+				}
+			}
+			recommendations.add(recommendationsFinal);
 
 			// garbage cleanup -- R script might already do this
 			String gc = "rm(fileroot, output, blend_tracking_semantic, get_userdata, dataitem_history, get_dataitem_rating, assign_unique_concepts, populate_ratings, build_sim, cosine_jaccard_sim, cosine_sim, jaccard_sim, apply_tfidf, compute_weight, dataitem_recom_mgr, get_item_recom, get_user_recom, hop_away_recom_mgr, hop_away_mgr, locate_user_communities, drilldown_communities, locate_data_communities, get_items_users, refresh_base);";
@@ -137,16 +148,4 @@ public class DatabaseRecommendationReactor extends AbstractRFrameReactor {
 		}
 		return new NounMetadata(recommendations, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.RECOMMENDATION);
 	}
-
-	private boolean getBool() {
-		GenRowStruct boolGrs = this.store.getNoun(COMMUNITIES);
-		if (boolGrs != null) {
-			if (boolGrs.size() > 0) {
-				List<Object> val = boolGrs.getValuesOfType(PixelDataType.BOOLEAN);
-				return (boolean) val.get(0);
-			}
-		}
-		return true;
-	}
-
 }
