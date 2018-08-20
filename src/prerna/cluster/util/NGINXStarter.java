6e635841ad12698e33f8360b8f83ccf188c07601
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2018-08-20 21:27:41 +0000

    Other files on the prerna.cluster - IZKListener.java
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29652 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e05cb5840a1a2056eab11fc6a3ccb9a4c7651575

---
+++ src/prerna/cluster/util/NGINXStarter.java
@@ -0,0 +1,55 @@
+package prerna.cluster.util;
+
+import java.util.Map;
+
+import org.apache.zookeeper.Watcher.Event.EventType;
+import org.apache.zookeeper.ZooKeeper;
+
+public class NGINXStarter {
+
+	
+	//public String semossHome = "/opt/semosshome/";
+	public String semossHome = "c:/users/pkapaleeswaran/workspacej3/docker/";
+
+	boolean connected = false;
+	
+	public static ZKClient zkClient = null;
+	
+	
+	
+	int version = 0;
+
+	
+	public static NginxClient getInstance()
+	{
+		if(zkClient == null)
+		{
+			zkClient = ZKClient.getInstance();
+			
+		}
+		return null;
+	}
+	
+	public static void main(String [] args)
+	{
+		if(zkClient == null)
+			zkClient = ZKClient.getInstance();
+		NGINXAppListener nginxListener = new NGINXAppListener();
+		
+		// generate once for the first time
+		nginxListener.regenConfig(zkClient.home + zkClient.app, zkClient.zk);
+		
+		zkClient.watchEvent(zkClient.home + zkClient.app, EventType.NodeChildrenChanged, nginxListener);
+		
+		while(true)
+		{
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
+}
