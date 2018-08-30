e10576c3553a279c4a0b7acd7e42de8993b1b83a
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-08-30 19:20:26 +0000

    UPDATE ::: R Reactor clean up less calls to runR, package clean up, and datatype hardcoded values
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29816 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f086222791f97eae7fa89576b9b7aaf153c28254

--- src/prerna/sablecc2/reactor/frame/r/PredictSimilarColumnValuesReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/PredictSimilarColumnValuesReactor.java
@@ -1,6 +1,7 @@
 package prerna.sablecc2.reactor.frame.r;
 
 import prerna.ds.r.RDataTable;
+import prerna.ds.r.RSyntaxHelper;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
@@ -23,41 +24,42 @@ public class PredictSimilarColumnValuesReactor extends AbstractRFrameReactor {
 		String baseFolder = DIHelper.getInstance().getProperty("BaseFolder");
 
 		// check if packages are installed
-		String[] packages = { "stringdist", "data.table" };
+		String[] packages = { "stringdist" };
 		this.rJavaTranslator.checkPackages(packages);
 
+		StringBuilder rsb = new StringBuilder();
 		// source script
-		String bestMatchScript = "source(\"" + baseFolder + "\\R\\Recommendations\\advanced_federation_blend.r\") ; library(data.table); ";
+		String bestMatchScript = "source(\"" + baseFolder + "\\R\\Recommendations\\advanced_federation_blend.r\") ; ";
 		bestMatchScript = bestMatchScript.replace("\\", "/");
-		this.rJavaTranslator.runR(bestMatchScript);
+		rsb.append(bestMatchScript);
 
 		// get single column input
 		RDataTable frame = (RDataTable) getFrame();
 		String frameName = frame.getTableName();
 		String matchesTable = Utility.getRandomString(8);
 		String col1 = matchesTable + "col1";
-		String script = col1 + "<- as.character(" + frameName + "$" + column + ");";
 
 		// run script
-		this.rJavaTranslator.runR(script);
-		this.rJavaTranslator.runR(matchesTable + " <- self_match(" + col1 + ");");
+		rsb.append(col1 + "<- as.character(" + frameName + "$" + column + ");");
+		rsb.append(matchesTable + " <- self_match(" + col1 + ");");
 
 		// add a unique combined col1 == col2, remove extra columns,
-		String combineScript = matchesTable + "$distance <- as.numeric(" + matchesTable + "$dist);" + matchesTable
-				+ "<-" + matchesTable + "[,c(\"col1\",\"col2\",\"distance\")]; " + matchesTable + "<-" + matchesTable
-				+ "[order(unique(" + matchesTable + ")$distance),] ;";
+		rsb.append(matchesTable + "$distance <- as.numeric(" + matchesTable + "$dist);");
+		rsb.append(matchesTable + "<-" + matchesTable + "[,c(\"col1\",\"col2\",\"distance\")]; ");
+		rsb.append(matchesTable + "<-" + matchesTable + "[order(unique(" + matchesTable + ")$distance),] ;");
+		rsb.append(RSyntaxHelper.asDataTable(matchesTable, matchesTable));
+		
+		// garbage collection
+		rsb.append("rm(" + col1 + ")");
+		this.rJavaTranslator.runR(rsb.toString());
 
-		this.rJavaTranslator.runR(combineScript + matchesTable + " <- as.data.table(" + matchesTable + ");");
 
-		// remove all garbage
-		this.rJavaTranslator.runR("rm(" + col1 + ")");
 
 		RDataTable returnTable = createFrameFromVaraible(matchesTable);
 		NounMetadata retNoun = new NounMetadata(returnTable, PixelDataType.FRAME);
 
 		// get count of exact matches
-		String exactMatchCount = this.rJavaTranslator
-				.getString("as.character(nrow(" + matchesTable + "[" + matchesTable + "$distance == 0,]))");
+		String exactMatchCount = this.rJavaTranslator.getString("as.character(nrow(" + matchesTable + "[" + matchesTable + "$distance == 0,]))");
 		if (exactMatchCount != null) {
 			int val = Integer.parseInt(exactMatchCount);
 			retNoun.addAdditionalReturn(new NounMetadata(val, PixelDataType.CONST_INT));
