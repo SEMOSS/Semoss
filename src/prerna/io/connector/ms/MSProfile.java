2ba5cfd34fc5bf9f3516681ab63dc199a01a444e
kunalppatel9 <kunalppatel9@deloitte.com>
2018-09-04 17:41:24 +0000

    ADDED ::: Added in call to fill MS token with user info
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29860 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 1a8707d0f77ecd7c263a056565be9ad8097e1114

---
+++ src/prerna/io/connector/ms/MSProfile.java
@@ -0,0 +1,46 @@
+package prerna.io.connector.ms;
+
+import java.util.Hashtable;
+
+import prerna.auth.AccessToken;
+import prerna.auth.AuthProvider;
+import prerna.auth.User;
+import prerna.io.connector.IConnectorIOp;
+import prerna.security.AbstractHttpHelper;
+import prerna.util.BeanFiller;
+
+public class MSProfile implements IConnectorIOp{
+
+	private static String url = "https://graph.microsoft.com/v1.0/me/";
+	private static String [] beanProps = {"name"}; // add is done when you have a list
+	private static String jsonPattern = "[displayName]";
+	
+	@Override
+	public String execute(User user, Hashtable params) {
+		AccessToken acToken = user.getAccessToken(AuthProvider.MS);
+		return fillAccessToken(acToken, params);
+	}
+	
+	public static String fillAccessToken(AccessToken acToken, Hashtable params) {
+		if(params == null) {
+			params = new Hashtable();
+		}
+		
+		String accessToken = acToken.getAccess_token();
+		
+		// you fill what you want to send on the API call
+		params.put("Bearer", accessToken);
+		params.put("alt", "json");
+		
+		// make the API call
+		String output = AbstractHttpHelper.makeGetCall(url, accessToken, null, true);
+
+		//String output = AbstractHttpHelper.makeGetCall(url, accessToken, params, false);
+		
+		// fill the bean with the return
+		acToken = (AccessToken)BeanFiller.fillFromJson(output, jsonPattern, beanProps, acToken);
+		
+		return output;
+	}
+
+}
