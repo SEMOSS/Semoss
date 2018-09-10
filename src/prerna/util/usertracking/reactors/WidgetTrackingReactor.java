e9dcf474afcb0d940018b31f015cb72ed18677f2
mahkhalil <mahkhalil@deloitte.com>
2018-09-10 13:08:08 +0000

    UPDATE ::: Silly, forgot to push the actual tracking method
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@29954 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/usertracking/reactors/WidgetTrackingReactor.java
+++ src/prerna/util/usertracking/reactors/WidgetTrackingReactor.java
@@ -14,6 +14,8 @@ import prerna.sablecc2.om.VarStore;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.sablecc2.reactor.job.JobReactor;
+import prerna.util.usertracking.IUserTracker;
+import prerna.util.usertracking.UserTrackerFactory;
 
 public class WidgetTrackingReactor extends AbstractReactor {
 
@@ -26,71 +28,77 @@ public class WidgetTrackingReactor extends AbstractReactor {
 	
 	@Override
 	public NounMetadata execute() {
-		GenRowStruct grs = getGrs();
 		List<Object[]> rows = new Vector<Object[]>();
 		
-		String id = UUID.randomUUID().toString();
-		String sessionId = null;
-		String userId = null;
-		// FE runs this on an empty insight
-		// so they are passing me an insight id that is being droped
-		// but i should be able to user the correct session id and user id from the empty insight
-		VarStore vStore = this.insight.getVarStore();
-		sessionId = RdbmsQueryBuilder.escapeForSQLStatement(vStore.get(JobReactor.SESSION_KEY).getValue().toString());
-		User user = this.insight.getUser();
-		if(user != null) {
-			userId = user.getAccessToken(user.getLogins().get(0)).getId();
-			userId = RdbmsQueryBuilder.escapeForSQLStatement(userId);
-		}
-		
-		
-		int size = grs.size();
-		for(int i = 0; i < size; i++) {
-			Object mObj = grs.get(i);
-			if(mObj instanceof Map) {
-				// values to grab
-				String insightId = null;
-				String time = null;
-				String subType = null;
-				String value = null;
-				
-				// get the values
-				Map m = (Map) mObj;
-				insightId = RdbmsQueryBuilder.escapeForSQLStatement(m.get("insightID").toString());
-				time = m.get("time").toString();
-				//TODO: have FE remove this so i can avoid these checks
-				if(time.contains("T")) {
-					time = time.replace('T', ' ');
-				}
-				if(time.contains("Z")) {
-					time = time.replace("Z", "");
-				}
-				Map inMap = (Map) m.get("change");
-				subType = inMap.get("type").toString();
-				value = inMap.get("value").toString();
-				
-				// generate row
-				Object[] row = new Object[15];
-				row[0] = id;
-				// input type
-				row[7] = WIDGET_CHANGE_KEY;
-				// input subtype
-				row[8] = subType;
-				// input name
-				row[9] = WIDGET_NAME_KEY;
-				// input value
-				row[10] = value;
-				// session id
-				row[11] = sessionId;
-				// insight id
-				row[12] = insightId;
-				// user id
-				row[13] = userId;
-				// time
-				row[14] = time;
-				// add batch
-				rows.add(row);
+		IUserTracker tracker = UserTrackerFactory.getInstance();
+		if(tracker.isActive()) {
+			GenRowStruct grs = getGrs();
+			
+			String id = UUID.randomUUID().toString();
+			String sessionId = null;
+			String userId = null;
+			// FE runs this on an empty insight
+			// so they are passing me an insight id that is being droped
+			// but i should be able to user the correct session id and user id from the empty insight
+			VarStore vStore = this.insight.getVarStore();
+			sessionId = RdbmsQueryBuilder.escapeForSQLStatement(vStore.get(JobReactor.SESSION_KEY).getValue().toString());
+			User user = this.insight.getUser();
+			if(user != null) {
+				userId = user.getAccessToken(user.getLogins().get(0)).getId();
+				userId = RdbmsQueryBuilder.escapeForSQLStatement(userId);
 			}
+			
+			
+			int size = grs.size();
+			for(int i = 0; i < size; i++) {
+				Object mObj = grs.get(i);
+				if(mObj instanceof Map) {
+					// values to grab
+					String insightId = null;
+					String time = null;
+					String subType = null;
+					String value = null;
+					
+					// get the values
+					Map m = (Map) mObj;
+					insightId = RdbmsQueryBuilder.escapeForSQLStatement(m.get("insightID").toString());
+					time = m.get("time").toString();
+					//TODO: have FE remove this so i can avoid these checks
+					if(time.contains("T")) {
+						time = time.replace('T', ' ');
+					}
+					if(time.contains("Z")) {
+						time = time.replace("Z", "");
+					}
+					Map inMap = (Map) m.get("change");
+					subType = inMap.get("type").toString();
+					value = inMap.get("value").toString();
+					
+					// generate row
+					Object[] row = new Object[15];
+					row[0] = id;
+					// input type
+					row[7] = WIDGET_CHANGE_KEY;
+					// input subtype
+					row[8] = subType;
+					// input name
+					row[9] = WIDGET_NAME_KEY;
+					// input value
+					row[10] = value;
+					// session id
+					row[11] = sessionId;
+					// insight id
+					row[12] = insightId;
+					// user id
+					row[13] = userId;
+					// time
+					row[14] = time;
+					// add batch
+					rows.add(row);
+				}
+			}
+			
+			tracker.trackUserWidgetMods(rows);
 		}
 		
 		NounMetadata noun;
