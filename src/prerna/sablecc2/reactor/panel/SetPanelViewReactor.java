f123dbb130522cc6410b2ca98f794582789db995
mahkhalil <mahkhalil@deloitte.com>
2018-09-12 14:02:16 +0000

    UPDATE ::: Updating state logic to match that of the FE to keep the options when the view is the same
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30025 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 43d490e1bd790e3d930e89bedc4146ae01793ee2

--- src/prerna/sablecc2/reactor/panel/SetPanelViewReactor.java
+++ src/prerna/sablecc2/reactor/panel/SetPanelViewReactor.java
@@ -25,16 +25,33 @@ public class SetPanelViewReactor extends AbstractInsightPanelReactor {
 		if(insightPanel == null) {
 			throw new IllegalArgumentException("Could not find the insight panel to set the view");
 		}
-		// get the ornaments that come as a map
+		
+		// get the view and options
 		String view = getPanelView();
 		String viewOptions = getPanelViewOptions();
-		// merge the map options
+		
+		// if the view and current view are the same
+		// and the view options is empty
+		// keep the same view options that is on the panel
+		boolean overrideView = true;
+		String currentView = insightPanel.getPanelView();
+		if(view.equals(currentView)) {
+			if(viewOptions == null || viewOptions.isEmpty()) {
+				overrideView = false;
+			}
+		}
+		
+		// set the new view
 		insightPanel.setPanelView(view);
-		insightPanel.setPanelViewOptions(viewOptions);
+		if(overrideView) {
+			insightPanel.setPanelViewOptions(viewOptions);
+		}
+		
 		Map<String, Object> returnMap = new HashMap<String, Object>();
 		returnMap.put("panelId", insightPanel.getPanelId());
 		returnMap.put("view", view);
-		returnMap.put("options", viewOptions);
+		// grab from panel since we do not know if we changed it or not
+		returnMap.put("options", insightPanel.getPanelViewOptions());
 		return new NounMetadata(returnMap, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.PANEL_VIEW);
 	}
 
