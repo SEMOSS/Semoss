15419193e2bf0af84ab199bbe53b56680ffdf093
Maher Khalil <themaherkhalil@gmail.com>
2018-09-13 18:23:21 +0000

    UPDATE ::: Adding useful function for processing h2 array objects
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30057 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8b4a18b14e96e96416ca73203338f23c7f645f99

---
+++ src/prerna/ds/h2/H2ArrayToStringFunction.java
@@ -0,0 +1,35 @@
+package prerna.ds.h2;
+
+public class H2ArrayToStringFunction {
+
+//	EXAMPLE FOR ADDING IN H2 CONSOLE
+//	
+//	CREATE ALIAS ARRAY_TO_STRING AS $$
+//	String turnArrayToString(Object[] array, String separator) {
+//			if(array == null || array.length == 0) {
+//				return "";
+//			}
+//			
+//			StringBuilder b = new StringBuilder();
+//			b.append(array[0]);
+//			for(int i = 1; i < array.length; i++) {
+//				b.append(separator).append(array[i]);
+//			}
+//			return b.toString();
+//		}
+//	$$;
+	
+	public static String turnArrayToString(Object[] array, String separator) {
+		if(array == null || array.length == 0) {
+			return "";
+		}
+		
+		StringBuilder b = new StringBuilder();
+		b.append(array[0]);
+		for(int i = 1; i < array.length; i++) {
+			b.append(separator).append(array[i]);
+		}
+		return b.toString();
+	}
+	
+}
