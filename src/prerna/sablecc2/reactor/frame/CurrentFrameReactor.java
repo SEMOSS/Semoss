4de9148e7907f909d44913d8590d50661406828e
mahkhalil <mahkhalil@deloitte.com>
2018-09-20 20:11:13 +0000

    UPDATE ::: Accounting for no frame
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30152 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 130a214d531f6daa87f20cb472bf96f432d45b7d

--- src/prerna/sablecc2/reactor/frame/CurrentFrameReactor.java
+++ src/prerna/sablecc2/reactor/frame/CurrentFrameReactor.java
@@ -3,6 +3,7 @@ package prerna.sablecc2.reactor.frame;
 import prerna.algorithm.api.ITableDataFrame;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.execptions.SemossPixelException;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 
@@ -11,6 +12,12 @@ public class CurrentFrameReactor extends AbstractReactor {
 	@Override
 	public NounMetadata execute() {
 		ITableDataFrame dm = (ITableDataFrame) this.insight.getDataMaker();
+		if(dm == null) {
+			SemossPixelException exception = new SemossPixelException(new NounMetadata("No frame currently exists", PixelDataType.CONST_STRING, PixelOperationType.ERROR));
+			exception.setContinueThreadOfExecution(true);
+			throw exception;
+		}
+		
 		NounMetadata noun = new NounMetadata(dm, PixelDataType.FRAME, PixelOperationType.FRAME);
 		return noun;
 	}
