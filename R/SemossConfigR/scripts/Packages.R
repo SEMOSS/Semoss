5fa5a0d7786dc74d511e296a990c84384b1276ae
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-09-25 19:54:09 +0000

    UPDATE ::: Updating R library install script for analytics and database recommendations
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30189 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- R/SemossConfigR/scripts/Packages.R
+++ R/SemossConfigR/scripts/Packages.R
@@ -2,9 +2,6 @@
 install.packages("RCurl")
 install.packages("httr")
 install.packages("devtools")
-library(RCurl)
-library(httr)
-library(devtools)
 set_config( config( ssl_verifypeer = 0L ) )
 devtools::install_github("twitter/AnomalyDetection")
 
@@ -38,6 +35,8 @@ install.packages("dplyr")
 install.packages("tidyr")
 install.packages("purrr")
 install.packages("stringdist")
+install.packages("parallel")
+
 
 # Business rules
 install.packages("arules")
@@ -47,11 +46,18 @@ install.packages("plyr")
 install.packages("WikidataR")
 install.packages("curl")
 install.packages("openssl")
-install.packages("httr")
+#install.packages("httr")
 install.packages("jsonlite")
 install.packages("WikipediR")
 install.packages("LSAfun")
 install.packages("text2vec")
+
+# Database semantic
+install.packages("lsa")
+#install.packages("WikidataR")
+#install.packages("text2vec")
+#install.packages("plyr")
+#install.packages("stringdist")
 
 # Google Analytics
 url <- "https://cran.r-project.org/src/contrib/Archive/RGoogleAnalytics/RGoogleAnalytics_0.1.1.tar.gz"
@@ -62,5 +68,67 @@ install.packages("lubridate")
 install.packages("validate")
 install.packages("yaml")
 install.packages("settings")
+
+# Xray
+install.packages("RcppProgress")
+install.packages("withr")
+install.packages("NLP")
+#install.packages("tidyr")
+#install.packages("devtools")
+install.packages("memoise")
+install.packages("digest")
+install.packages("tidyselect")
+#install.packages("purrr")
+
+# Xray Metamodel
+# install.packages("jsonlite")
+
+
+# Database Recommmendations
+#install.packages("RCurl")
+install.packages("XML")
+install.packages("doParallel")
+
+# Auto clean
+install.packages("tm")
+install.packages("cluster")
+#install.packages("stringdist")
+#install.packages("data.table")
+
+# Apriori
+install.packages("arules")
+#install.packages("data.table")
+#install.packages("dplyr")
+
+# Classification
+#install.packages("data.table")
+install.packages("partykit")
+#install.packages("dplyr")
+
+# Clustering
+install.packages("cluster")
+
+# LOF
+install.packages("Rlof")
+#install.packages("data.table")
+#install.packages("dplyr")
+install.packages("VGAM")
+
+# Outlier
+#install.packages("data.table")
+install.packages("HDoutliers")
+
+# Random Forest
+install.packages("randomForest")
+#install.packages("data.table")
+#install.packages("dplyr")
+
+# graph
+install.packages("igraph")
+
+# LSA Column
+#install.packages("LSAfun")
+#install.packages("text2vec")
+
 
 
