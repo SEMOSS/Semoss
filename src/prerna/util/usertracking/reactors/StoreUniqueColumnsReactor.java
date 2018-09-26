00d48dcf4aaf0674eab4f523d3223e40dba13c2f
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-09-26 19:14:26 +0000

    UPDATE ::: Adding reactors to pull tracking db data
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30204 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/util/usertracking/reactors/StoreUniqueColumnsReactor.java
@@ -0,0 +1,266 @@
+package prerna.util.usertracking.reactors;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+import prerna.algorithm.api.SemossDataType;
+import prerna.auth.User;
+import prerna.ds.r.RSyntaxHelper;
+import prerna.engine.api.IEngine;
+import prerna.engine.api.IEngine.ENGINE_TYPE;
+import prerna.engine.api.IRawSelectWrapper;
+import prerna.nameserver.utility.MasterDatabaseUtility;
+import prerna.query.querystruct.SelectQueryStruct;
+import prerna.query.querystruct.filters.SimpleQueryFilter;
+import prerna.query.querystruct.selectors.QueryColumnSelector;
+import prerna.rdf.engine.wrappers.WrapperManager;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.frame.r.AbstractRFrameReactor;
+import prerna.sablecc2.reactor.frame.r.CompareDbSemanticSimiliarity;
+import prerna.sablecc2.reactor.frame.r.util.AbstractRJavaTranslator;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.OWLER;
+import prerna.util.Utility;
+import prerna.util.usertracking.TrackRequestThread;
+
+/**
+ * Generates column descriptions and stores in the tracking database
+ * Adds unique count to owl file for each column
+ *
+ */
+public class StoreUniqueColumnsReactor extends AbstractRFrameReactor {
+	
+	private static final String CLASS_NAME = StoreUniqueColumnsReactor.class.getName();
+	public static final String DESCRIPTIONS_BOOL = "descriptions";
+
+	public StoreUniqueColumnsReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.APP.getKey(), DESCRIPTIONS_BOOL};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		init();
+		organizeKeys();
+		// get inputs - engine
+		String engineName = this.keyValue.get(this.keysToGet[0]);
+		engineName = MasterDatabaseUtility.testEngineIdIfAlias(engineName);
+		boolean descriptions = getDescriptionsBool();
+		IEngine engine = Utility.getEngine(engineName);
+
+		// validate engine exists
+		if (engine == null){
+			throw new IllegalArgumentException("Engine doesnt exist");
+		}
+		String owlPath = engine.getOWL();
+
+		// only executes for rdbms, tinker, and rdf
+		ENGINE_TYPE engineType = engine.getEngineType();
+		if (engineType.equals(ENGINE_TYPE.RDBMS) || engineType.equals(ENGINE_TYPE.SESAME) || engineType.equals(ENGINE_TYPE.TINKER)) {
+			OWLER owl = new OWLER(engine, owlPath);
+			owl.addUniqueCounts(engine);
+		}
+		
+		// store descriptions if requested
+		if(descriptions){
+			storeColumnDescriptions(engine);
+		}
+		
+		return new NounMetadata(true, PixelDataType.BOOLEAN);
+	}
+	
+	private boolean getDescriptionsBool() {
+		GenRowStruct boolGrs = this.store.getNoun(DESCRIPTIONS_BOOL);
+		if(boolGrs != null) {
+			if(boolGrs.size() > 0) {
+				List<Object> val = boolGrs.getValuesOfType(PixelDataType.BOOLEAN);
+				return (boolean) val.get(0);
+			}
+		}
+		return false;
+	}
+	
+	private void storeColumnDescriptions(IEngine engine) {
+		// source all the new r scripts -- check later to see which ones are
+		// truly necessary to source
+		
+		String[] packages = new String[] {"data.table", "WikidataR"};
+		Logger logger = this.getLogger(CLASS_NAME);
+		AbstractRJavaTranslator rJavaTranslator = this.insight.getRJavaTranslator(logger);
+		rJavaTranslator.startR();
+		StringBuilder rsb = new StringBuilder();
+		
+		String wd = "wd" + Utility.getRandomString(5);
+		String baseFolder = DIHelper.getInstance().getProperty("BaseFolder");
+		rsb.append(wd +"<- getwd();");
+		rsb.append("setwd(\"" + baseFolder + "\\R\\Recommendations\");\n");
+		rsb.append("source(\"" + baseFolder + "\\R\\Recommendations\\SemanticSimilarity\\lsi_dataitem.r\");\n");
+		rsb.append("source(\"" + baseFolder + "\\R\\Recommendations\\db_recom.r\");\n");
+		rsb.append("source(\"" + baseFolder + "\\R\\Recommendations\\datasemantic.r\");\n");
+		rsb.append("source(\"" + baseFolder + "\\R\\Recommendations\\topic_modelling.r\");\n");
+		rJavaTranslator.runR(rsb.toString().replace("\\", "/"));
+
+		// GENERATING DESCRIPTIONS
+		String userId = "null";
+		User user = this.insight.getUser();
+		if(user != null) {
+			userId = user.getAccessToken(user.getLogins().get(0)).getId();
+		}
+		
+		String rTempTable = "semanticTempTable";
+		List<Object[]> allTableCols = MasterDatabaseUtility.getAllTablesAndColumns(engine.getEngineId());
+		String engineName = engine.getEngineName();
+		String engineID = engine.getEngineId();
+		String seperator = "$";
+		List<Object[]> list = new ArrayList<Object[]>();
+
+		// iterate through all the rows and sample about 15 rows from each of
+		// those
+		for (Object[] tableCol : allTableCols) {
+			SelectQueryStruct qs = new SelectQueryStruct();
+			if (tableCol.length == 4) {
+				String table = tableCol[0] + "";
+				String col = tableCol[1] + "";
+				String dataType = tableCol[2] + "";
+				boolean primFlag = false;
+				String descriptions = "";
+
+				// Only generate column descriptions if the data type is a
+				// string
+				if (dataType.equals(SemossDataType.STRING.toString())) {
+					// we will fill this in once we figure out if it is a
+					// concept or property
+					QueryColumnSelector colSelector = null;
+					// this is a hack we used in advanced federate
+					if (engine.getParentOfProperty(col + "/" + table) == null) {
+						// we couldn't find a parent for this property
+						// this means it is a concept itself
+						// and we should only use table
+						colSelector = new QueryColumnSelector(table);
+						primFlag = true;
+					} else {
+						colSelector = new QueryColumnSelector(table + "__" + col);
+					}
+					qs.addSelector(colSelector);
+
+					// select only non-null values from database
+					SimpleQueryFilter nulls = new SimpleQueryFilter(new NounMetadata(colSelector, PixelDataType.COLUMN),
+							"!=", new NounMetadata("null", PixelDataType.NULL_VALUE));
+					qs.addExplicitFilter(nulls);
+					IRawSelectWrapper iterator = WrapperManager.getInstance().getRawWrapper(engine, qs);
+					if (!iterator.hasNext()) {
+						// all values are null in this column
+						continue;
+					}
+					StringBuilder sb = new StringBuilder();
+					sb.append("rm(result);");
+					// write to csv and read into R
+					String newFileLoc = DIHelper.getInstance().getProperty(Constants.INSIGHT_CACHE_DIR) + "/"
+							+ Utility.getRandomString(6) + ".tsv";
+					String header = engine.getEngineId() + seperator + engine.getEngineName() + seperator + table + seperator + col;
+					File newFile = CompareDbSemanticSimiliarity.writeResultToFile(newFileLoc, iterator, header);
+					sb.append(RSyntaxHelper.getFReadSyntax(rTempTable, newFile.getAbsolutePath(), "\\t")+"\n");
+
+					// get random subset of column data
+
+					sb.append("if(nrow(" + rTempTable + ") > 15) {");
+					sb.append(rTempTable + "<-" + rTempTable + "[sample(nrow(" + rTempTable + "),15),c(");
+					sb.append("\"" + engineID + seperator + engineName + seperator + table + seperator + col + "\"");
+					sb.append(")];}\n");
+					logger.info("Generating and storing descriptions for : " + engine.getEngineName() + ", " + table + ", " + col);
+
+					// execute script to get descriptions for this column
+					sb.append(RSyntaxHelper.asDataFrame(rTempTable, rTempTable) + "\n");
+					sb.append("semantic_tracking_mgr(" + rTempTable + ",\"dataitem\");\n");
+					sb.append("result <- readRDS('dataitem-semantic-history.rds');");
+
+					// get only the row that you are adding to the table
+					sb.append("result <- result[(result$ENGINE_ID== \"" + engineID + "\" & result$ENGINE_NAME== \""
+							+ engineName + "\" & result$TABLE== \"" + table + "\" & result$COLUMN== \"" + col
+							+ "\"),]");
+					rJavaTranslator.runR(sb.toString());
+					newFile.delete();
+					descriptions = rJavaTranslator.getString("result$DESCRIPTION[1]");
+					if (descriptions == null) {
+						// no results found
+						continue;
+					}
+
+					// Truncate row lengths to be the appropriate lengths before
+					// sending to table
+					if (engineID.length() > 50) {
+						logger.info("Engine ID was too long. Had to truncate.");
+						engineID = engineID.substring(0, 49);
+					} else if (engineName.length() > 255) {
+						logger.info("Engine Name was too long. Had to truncate.");
+						engineName = engineName.substring(0, 254);
+					} else if (table.length() > 255) {
+						logger.info("Table Name was too long. Had to truncate.");
+						table = table.substring(0, 254);
+					} else if (col.length() > 255) {
+						logger.info("Column Name was too long. Had to truncate.");
+						col = col.substring(0, 254);
+					} else if (descriptions.length() > 1000) {
+						logger.info("Description was too long. Had to truncate.");
+						descriptions = descriptions.substring(0, 999);
+					} else if (userId.length() > 50) {
+						logger.info("User ID string was too long. Had to truncate.");
+						userId = userId.substring(0, 49);
+					}
+
+					// Add a new row into the table with the necessary details
+					// We check to see if this table already exists in the
+					// endpoint
+					Object[] newRow = { engineID, engineName, table, col, descriptions, userId };
+					list.add(newRow);
+				} else {
+					continue;
+				}
+			}
+		}
+		sendTrackRequest("semantic", list);
+		logger.info("Finished Uploading Column Descriptions");
+
+		String gc = "rm(\"a5_97b6491748854929b50f55f5818b1634\",	\"a9_8ca904d356784e2d88427675e946b591\","
+				+ "\"apply_tfidf\",                        \"assign_unique_concepts\",            "
+				+ "\"aTempInsightNotStored\",              \"blend_mgr\",                         "
+				+ "\"blend_tracking_semantic\",            \"breakdown\",                         "
+				+ "\"build_data_landmarks\",               \"build_dbid_domain\",                 "
+				+ "\"build_query_doc\",                    \"build_query_tdm\",                   "
+				+ "\"build_sim\",                          \"build_tdm\",                         "
+				+ "\"col2db\",                             \"col2tbl\",                           "
+				+ "\"column_doc_mgr_do\",                  \"column_doc_mgr_dopar\",              "
+				+ "\"column_lsi_mgr\",                     \"compute_column_desc_sim\",           "
+				+ "\"compute_entity_sim\",                 \"con\",                               "
+				+ "\"construct_column_doc\",               \"constructName\",                     "
+				+ "\"cosine_jaccard_sim\",                 \"create_column_doc\",                 "
+				+ "\"data_domain_mgr\",                    \"dataitem_history_do\",               "
+				+ "\"dataitem_history_dopar\",             \"dataitem_recom_mgr\",                "
+				+ "\"datasemantic_history\",               \"discover_column_desc\",              "
+				+ "\"drilldown_communities\",              \"exec_tfidf\",                        "
+				+ "\"find_db\",                            \"get_dataitem_rating\",               "
+				+ "\"get_item_recom\",                     \"get_items_users\",                   "
+				+ "\"get_similar_doc\",                    \"get_user_recom\",                    "
+				+ "\"getSearchURL\",                       \"hop_away_mgr\",                      "
+				+ "\"hop_away_recom_mgr\",                 \"jaccard_sim\",                       "
+				+ "\"locate_data_communities\",            \"locate_data_district\",              "
+				+ "\"locate_user_communities\",            \"lsi_mgr\",                           "
+				+ "\"match_desc\",                         \"populate_ratings\",                  "
+				+ "\"read_datamatrix\",                    \"refresh_base\",                      "
+				+ "\"refresh_data_mgr\",                   \"refresh_semantic_mgr\",              "
+				+ "\"remove_files\",                       \"semantic_tracking_mgr\",             "
+				+ "\"semanticTempTable\")";
+		rJavaTranslator.runR(gc);
+	}
+
+	private void sendTrackRequest(String type, List<Object[]> rows) {
+		TrackRequestThread t = new TrackRequestThread(type, rows);
+		t.start();
+	}
+}
