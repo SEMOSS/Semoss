383f5ebcd9e205d4d6e8213b06e9db1a8314ac8e
Maher Khalil <themaherkhalil@gmail.com>
2018-10-04 00:04:24 +0000

    UPDATE ::: Adding new generic semoss rest engine that will work with new raw wrapper select service
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30290 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 5b2354beabe04c48e8c7e75b3ce29c7ff6365990

---
+++ src/prerna/engine/impl/remotesemoss/RemoteSemossEngine.java
@@ -0,0 +1,71 @@
+package prerna.engine.impl.remotesemoss;
+
+import java.util.Vector;
+
+import prerna.engine.impl.AbstractEngine;
+import prerna.om.Insight;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.Utility;
+
+public class RemoteSemossEngine extends AbstractEngine {
+
+	private String remoteAddress;
+	
+	public void openDB(String propFile) {
+		this.baseFolder = DIHelper.getInstance().getProperty("BaseFolder");
+		this.prop = Utility.loadProperties(propFile);
+		
+		// get id & name
+		this.engineId = this.prop.getProperty(Constants.ENGINE);
+		this.engineName = this.prop.getProperty(Constants.ENGINE_ALIAS);
+	}
+	
+	@Override
+	public Object execQuery(String query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector<Insight> getInsight(String... questionIDs) {
+		System.out.println("need to implement this in rest");
+		return null;
+	}
+	
+	
+	@Override
+	public void insertData(String query) {
+		System.out.println("need to implement this in rest");
+	}
+
+	@Override
+	public void removeData(String query) {
+		System.out.println("need to implement this in rest");
+		
+	}
+
+	@Override
+	public void commit() {
+		System.out.println("need to implement this in rest");
+		
+	}
+
+	@Override
+	public ENGINE_TYPE getEngineType() {
+		return ENGINE_TYPE.REMOTE_SEMOSS;
+	}
+	
+	//////////////////////////////////////////////////////
+	
+	// I am not sure this is really used much... 
+	// need to check some reactors that touch the engines directly
+	// like x-ray
+	
+	@Override
+	public Vector<Object> getEntityOfType(String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
