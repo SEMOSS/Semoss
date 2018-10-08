e75d6e467e96371c7fdbb2f1927a6d3bebd1361a
mahkhalil <mahkhalil@deloitte.com>
2018-10-08 13:47:49 +0000

    UPDATE ::: Adding method to synchronize an app's metadata with the local master database
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30323 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/sablecc2/reactor/masterdatabase/SyncAppWithLocalMasterReactor.java
@@ -0,0 +1,68 @@
+package prerna.sablecc2.reactor.masterdatabase;
+
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.SecurityQueryUtils;
+import prerna.nameserver.AddToMasterDB;
+import prerna.nameserver.DeleteFromMasterDB;
+import prerna.nameserver.utility.MasterDatabaseUtility;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.Utility;
+
+public class SyncAppWithLocalMasterReactor extends AbstractReactor {
+
+	public static final String CLASS_NAME = SyncAppWithLocalMasterReactor.class.getName();
+	
+	public SyncAppWithLocalMasterReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey()};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String appId = this.keyValue.get(this.keysToGet[0]);
+		// we may have the alias
+		if(AbstractSecurityUtils.securityEnabled()) {
+			appId = SecurityQueryUtils.testUserEngineIdForAlias(this.insight.getUser(), appId);
+			if(!SecurityQueryUtils.userCanEditEngine(this.insight.getUser(), appId)) {
+				throw new IllegalArgumentException("App " + appId + " does not exist or user does not have access to app");
+			}
+		} else {
+			appId = MasterDatabaseUtility.testEngineIdIfAlias(appId);
+			if(!MasterDatabaseUtility.getAllEngineIds().contains(appId)) {
+				throw new IllegalArgumentException("App" + appId + " does not exist");
+			}
+		}
+				
+		Logger logger = getLogger(CLASS_NAME);
+		logger.info("Starting to synchronize metadata");
+		
+		logger.info("Starting to remove exisitng metadata");
+		DeleteFromMasterDB remover = new DeleteFromMasterDB();
+		remover.deleteEngineRDBMS(appId);
+		logger.info("Finished removing exisitng metadata");
+
+		logger.info("Starting to add metadata");
+		String smssFile = DIHelper.getInstance().getCoreProp().getProperty(appId + "_" + Constants.STORE);
+		Properties prop = Utility.loadProperties(smssFile);
+		AddToMasterDB adder = new AddToMasterDB();
+		adder.registerEngineLocal(prop);
+		logger.info("Done adding new metadata");
+
+		logger.info("Synchronization complete");
+		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
+		noun.addAdditionalReturn(new NounMetadata("Successfully synchronized " + MasterDatabaseUtility.getEngineAliasForId(appId) + "'s metadata", 
+				PixelDataType.CONST_STRING, PixelOperationType.SUCCESS_MESSAGE));
+		return noun;
+	}
+
+}
