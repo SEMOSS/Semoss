1274148b77a926fc4cddbc79a3551ee76169e589
mahkhalil <mahkhalil@deloitte.com>
2018-10-08 17:53:21 +0000

    UPDATE ::: Adding reactor to reset the owl metadata when making changes to the physical file UPDATE ::: Adding reactor to add a relationship in the OWL
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30341 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: edcbf012305ade56fffd7ad780ec2ef6d16346a1

---
+++ src/prerna/sablecc2/reactor/app/metaeditor/AbstractMetaEditorReactor.java
@@ -0,0 +1,116 @@
+package prerna.sablecc2.reactor.app.metaeditor;
+
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Properties;
+import java.util.Vector;
+
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.SecurityQueryUtils;
+import prerna.engine.api.IEngine;
+import prerna.engine.impl.SmssUtilities;
+import prerna.engine.impl.rdf.RDFFileSesameEngine;
+import prerna.nameserver.utility.MasterDatabaseUtility;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.OWLER;
+import prerna.util.Utility;
+
+public abstract class AbstractMetaEditorReactor extends AbstractReactor {
+
+	protected String getAppId(String appId) {
+		if(AbstractSecurityUtils.securityEnabled()) {
+			appId = SecurityQueryUtils.testUserEngineIdForAlias(this.insight.getUser(), appId);
+			if(!SecurityQueryUtils.userCanEditEngine(this.insight.getUser(), appId)) {
+				throw new IllegalArgumentException("App " + appId + " does not exist or user does not have access to app");
+			}
+		} else {
+			appId = MasterDatabaseUtility.testEngineIdIfAlias(appId);
+			if(!MasterDatabaseUtility.getAllEngineIds().contains(appId)) {
+				throw new IllegalArgumentException("App " + appId + " does not exist");
+			}
+		}
+		return appId;
+	}
+	
+	protected RDFFileSesameEngine getOwlEngine(String appId) {
+		String smssFile = DIHelper.getInstance().getCoreProp().getProperty(appId + "_" + Constants.STORE);
+		Properties prop = Utility.loadProperties(smssFile);
+		String owlFile = SmssUtilities.getOwlFile(prop).getAbsolutePath();
+
+		// owl is stored as RDF/XML file
+		RDFFileSesameEngine rfse = new RDFFileSesameEngine();
+		rfse.openFile(owlFile, null, null);
+		return rfse;
+	}
+	
+	protected OWLER getOWLER(String appId) {
+		IEngine app = Utility.getEngine(appId);
+		OWLER owler = new OWLER(app, app.getOWL());
+		return owler;
+	}
+	
+	/**
+	 * Get values to fill in the OWLER as we query for correct uris based
+	 * on the type of operation we are performing
+	 * @param engine
+	 * @param owler
+	 */
+	protected void setOwlerValues(IEngine engine, OWLER owler) {
+		Hashtable<String, String> conceptHash = new Hashtable<String, String>();
+		Hashtable<String, String> propHash = new Hashtable<String, String>();
+		Hashtable<String, String> relationHash = new Hashtable<String, String>();
+		
+		boolean isRdbms = (engine.getEngineType() == IEngine.ENGINE_TYPE.RDBMS || 
+				engine.getEngineType() == IEngine.ENGINE_TYPE.IMPALA);
+		
+		Vector<String> concepts = engine.getConcepts(false);
+		for(String c : concepts) {
+			String tableName = Utility.getInstanceName(c);
+			String cKey = tableName;
+			if(isRdbms) {
+				cKey = Utility.getClassName(c) + cKey;
+			}
+			// add to concept hash
+			conceptHash.put(cKey, c);
+			
+			// add all the props as well
+			List<String> props = engine.getProperties4Concept(c, false);
+			for(String p : props) {
+				String propName = null;
+				if(isRdbms) {
+					propName = Utility.getClassName(p);
+				} else {
+					propName = Utility.getInstanceName(p);
+				}
+				
+				propHash.put(tableName + "%" + propName, p);
+			}
+		}
+		
+		Vector<String[]> rels = engine.getRelationships(false);
+		for(String[] r : rels) {
+			String startT = null;
+			String startC = null;
+			String endT = null;
+			String endC = null;
+			String pred = null;
+			
+			startT = Utility.getInstanceName(r[0]);
+			endT = Utility.getInstanceName(r[1]);
+			pred = Utility.getInstanceName(r[2]);
+			
+			if(isRdbms) {
+				startC = Utility.getClassName(r[0]);
+				endC = Utility.getClassName(r[1]);
+			}
+			
+			relationHash.put(startT + startC + endT + endC + pred, r[2]);
+		}
+		
+		owler.setConceptHash(conceptHash);
+		owler.setPropHash(propHash);
+		owler.setRelationHash(relationHash);
+	}
+}
