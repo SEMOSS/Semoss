08fd09ee99bb1f5ee6770fdcdc682594322b9be8
tbanach <tbanach@deloitte.com>
2018-10-08 15:18:28 +0000

    UPDATE ::: Refactored code to pull and push an app to be based off of the app id. Don't need to manually figure out what folders and files need to be pushed. Pulls the db folder first before the smss, because of the watcher code.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30331 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: dbb77a4282a6335353f4569e68df5b6811968340

--- src/prerna/cluster/util/ClusterUtil.java
+++ src/prerna/cluster/util/ClusterUtil.java
@@ -2,8 +2,13 @@ package prerna.cluster.util;
 
 public class ClusterUtil {
 
+	// Env vars used in clustered deployments
 	private static final String LOAD_ENGINES_LOCALLY_KEY = "SEMOSS_LOAD_ENGINES_LOCALLY";
 	public static final boolean LOAD_ENGINES_LOCALLY = System.getenv().containsKey(LOAD_ENGINES_LOCALLY_KEY)
 			? Boolean.parseBoolean(System.getenv(LOAD_ENGINES_LOCALLY_KEY)) : true;
+	
+	private static final String SYNC_ENGINES_FROM_REMOTE_STORAGE_KEY = "SEMOSS_SYNC_ENGINES_FROM_REMOTE_STORAGE";
+	public static final boolean SYNC_ENGINES_FROM_REMOTE_STORAGE = System.getenv().containsKey(SYNC_ENGINES_FROM_REMOTE_STORAGE_KEY)
+			? Boolean.parseBoolean(System.getenv(SYNC_ENGINES_FROM_REMOTE_STORAGE_KEY)) : false;
 	
 }
