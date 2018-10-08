0f999c0e4b33c408bb77c919b28c486b79504c57
mahkhalil <mahkhalil@deloitte.com>
2018-10-08 15:09:55 +0000

    UPDATE ::: Adding update insight name reactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30329 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 85631c837b802468ffa67b60ba68560999a90e9a

---
+++ src/prerna/sablecc2/reactor/insights/save/UpdateInsightNameReactor.java
@@ -0,0 +1,113 @@
+package prerna.sablecc2.reactor.insights.save;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.SecurityQueryUtils;
+import prerna.auth.utils.SecurityUpdateUtils;
+import prerna.engine.api.IEngine;
+import prerna.engine.impl.InsightAdministrator;
+import prerna.engine.impl.SmssUtilities;
+import prerna.nameserver.utility.MasterDatabaseUtility;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.insights.AbstractInsightReactor;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.MosfetSyncHelper;
+import prerna.util.Utility;
+
+public class UpdateInsightNameReactor extends AbstractInsightReactor {
+
+	private static final String CLASS_NAME = UpdateInsightNameReactor.class.getName();
+
+	public UpdateInsightNameReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), ReactorKeysEnum.INSIGHT_NAME.getKey(), ReactorKeysEnum.ID.getKey()};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		Logger logger = this.getLogger(CLASS_NAME);
+
+		String appId = getApp();
+		if(appId == null || appId.isEmpty()) {
+			throw new IllegalArgumentException("Need to define the app where the insight currently exists");
+		}
+		// need to know what we are updating
+		String existingId = getRdbmsId();
+		if(existingId == null) {
+			throw new IllegalArgumentException("Need to define the rdbmsId for the insight we are updating");
+		}
+		
+		// security
+		if(AbstractSecurityUtils.securityEnabled()) {
+			if(!SecurityQueryUtils.userCanEditInsight(this.insight.getUser(), appId, existingId)) {
+				throw new IllegalArgumentException("User does not have permission to edit this insight");
+			}
+		}
+		
+		String insightName = getInsightName();
+		if(insightName == null || insightName.isEmpty()) {
+			throw new IllegalArgumentException("Need to define the insight name");
+		}
+		
+		IEngine engine = Utility.getEngine(appId);
+		if(engine == null) {
+			// we may have the alias
+			engine = Utility.getEngine(MasterDatabaseUtility.testEngineIdIfAlias(appId));
+			if(engine == null) {
+				throw new IllegalArgumentException("Cannot find app = " + appId);
+			}
+		}
+		// add the recipe to the insights database
+		InsightAdministrator admin = new InsightAdministrator(engine.getInsightDatabase());
+
+		// update insight db
+		logger.info("1) Updating insight in rdbms");
+		admin.updateInsightName(existingId, insightName);
+		logger.info("1) Done");
+		
+		logger.info("2) Updating insight in index");
+		SecurityUpdateUtils.updateInsightName(appId, existingId, insightName);
+		logger.info("2) Done");
+		
+		logger.info("3) Update mosfet file for collaboration");
+		updateRecipeFile(logger, engine.getEngineId(), engine.getEngineName(), existingId, insightName);
+		logger.info("3) Done");
+
+		Map<String, Object> returnMap = new HashMap<String, Object>();
+		returnMap.put("name", insightName);
+		returnMap.put("app_insight_id", existingId);
+		returnMap.put("app_name", engine.getEngineName());
+		returnMap.put("app_id", engine.getEngineId());
+		NounMetadata noun = new NounMetadata(returnMap, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.SAVE_INSIGHT);
+		return noun;
+	}
+
+	/**
+	 * Update recipe: delete the old file and save as new
+	 * 
+	 * @param engineName
+	 * @param rdbmsID
+	 * @param recipeToSave
+	 */
+	protected void updateRecipeFile(Logger logger, String appId, String appName, String rdbmsID, String insightName) {
+		final String DIR_SEPARATOR = java.nio.file.FileSystems.getDefault().getSeparator();
+		String recipeLocation = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) 
+				+ DIR_SEPARATOR + Constants.DB + DIR_SEPARATOR + SmssUtilities.getUniqueName(appName, appId) + DIR_SEPARATOR + "version" 
+				+ DIR_SEPARATOR + rdbmsID + DIR_SEPARATOR + MosfetSyncHelper.RECIPE_FILE;
+		File mosfet = new File(recipeLocation);
+		if(mosfet.exists()) {
+			MosfetSyncHelper.updateMosfitFileInsightName(new File(recipeLocation), insightName);
+		} else {
+			logger.info("... Could not find existing mosfet file. Ignoring update.");
+		}
+	}
+	
+}
