2d431a2d8649b8743065a2fd507871866413ddde
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-10-11 19:20:05 +0000

    UPDATE ::: R package script include dependencies
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30409 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 17e66fdf89d9eea6558effa29a36b230abb3b3ef

--- R/SemossConfigR/scripts/Packages.R
+++ R/SemossConfigR/scripts/Packages.R
@@ -1,160 +1,195 @@
-# Anomaly detection packages
-install.packages("RCurl")
-install.packages("httr")
-install.packages("devtools")
-set_config( config( ssl_verifypeer = 0L ) )
-devtools::install_github("twitter/AnomalyDetection")
+###############################################################################
+# General Semoss Packages
+###############################################################################
+install.packages("splitstackshape", dependencies=TRUE)
+install.packages("data.table", dependencies=TRUE)
+install.packages("reshape2", dependencies=TRUE)
+install.packages("stringr", dependencies=TRUE)
+install.packages("lubridate", dependencies=TRUE)
+install.packages("dplyr", dependencies=TRUE)
+#install.packages("Rserve", dependencies=TRUE)
+#install.packages("RJava", dependencies=TRUE)
 
-# General Semoss packages
-install.packages("Rserve")
-install.packages("splitstackshape")
-install.packages("data.table")
-install.packages("reshape2")
-install.packages("RJDBC")
-install.packages("sqldf")
-install.packages("stringr")
-install.packages("rJava")
+###############################################################################
+# Clean Routines
+###############################################################################
 
-# Semantic matching package
-install.packages("textreuse")
-
-# Fuzzy Join 
 # Collision Resolver
-# Fuzzy matching
-install.packages("fuzzyjoin")
-install.packages('RJSONIO')
-install.packages("rlang")
-install.packages("tibble")
-install.packages("R6")
-install.packages("bindrcpp")
-install.packages("crayon")
-install.packages("pkgconfig")
-install.packages("glue")
-install.packages("plogr")
-install.packages("dplyr")
-install.packages("tidyr")
-install.packages("purrr")
-install.packages("stringdist")
-install.packages("parallel")
-
-
-# Business rules
-install.packages("arules")
-
-# Semantic matching
-install.packages("plyr")
-install.packages("WikidataR")
-install.packages("curl")
-install.packages("openssl")
-install.packages("jsonlite")
-install.packages("WikipediR")
-install.packages("LSAfun")
-install.packages("text2vec")
-#install.packages("httr")
-
-
-# Database semantic
-install.packages("lsa")
-#install.packages("WikidataR")
-#install.packages("text2vec")
-#install.packages("plyr")
-#install.packages("stringdist")
-
-# Recommendations
-install.packages('Rcpp')
-install.packages('lattice')
-install.packages('codetools')
-install.packages('digest')
-install.packages('foreach')
-install.packages('SnowballC')
-install.packages('futile.options')
-install.packages('futile.logger')
-install.packages('magrittr')
-install.packages('formatR')
-install.packages('RcppParallel')
-install.packages('stringi')
-install.packages("Matrix")
-install.packages("lambda.r")
-install.packages("doParallel")
-install.packages('grid')
-install.packages('tools')
-install.packages('iterators')
-install.packages('mlapi')
-install.packages('compiler')
-install.packages('lubridate')
-#install.packages("stringr")
-#install.packages("text2vec")
-#install.packages("parallel")
-#install.packages('lsa')
-#install.packages('plyr')
-#install.packages("R6")
-#install.packages("doParallel")
-
-
-# Edit Rules
-install.packages("validate")
-install.packages("yaml")
-install.packages("settings")
-
-# Xray
-install.packages("RcppProgress")
-install.packages("withr")
-install.packages("NLP")
-install.packages("memoise")
-install.packages("tidyselect")
-#install.packages("purrr")
-#install.packages("tidyr")
-#install.packages("devtools")
-#install.packages("digest")
-
-# Xray Metamodel
-# install.packages("jsonlite")
-
-# Database Recommmendations
-install.packages("XML")
-install.packages("doParallel")
-#install.packages("RCurl")
+install.packages("fuzzyjoin", dependencies=TRUE)
+install.packages("RJSONIO", dependencies=TRUE)
+install.packages("tidyr", dependencies=TRUE)
+install.packages("stringdist", dependencies=TRUE)
+install.packages("parallel", dependencies=TRUE)
 
 # Auto clean
-install.packages("tm")
-install.packages("cluster")
-#install.packages("stringdist")
-#install.packages("data.table")
+install.packages("tm", dependencies=TRUE)
+install.packages("cluster", dependencies=TRUE)
+#install.packages("stringdist", dependencies=TRUE)
+#install.packages("data.table", dependencies=TRUE)
+
+###############################################################################
+# Recommendations
+###############################################################################
+
+# Database Recommmendations
+install.packages("XML", dependencies=TRUE)
+install.packages("doParallel", dependencies=TRUE)
+#install.packages("RCurl", dependencies=TRUE)
+
+# Recommendations
+install.packages("Rcpp", dependencies=TRUE)
+install.packages("lattice", dependencies=TRUE)
+install.packages("codetools", dependencies=TRUE)
+install.packages("digest", dependencies=TRUE)
+install.packages("foreach", dependencies=TRUE)
+install.packages("SnowballC", dependencies=TRUE)
+install.packages("futile.options", dependencies=TRUE)
+install.packages("futile.logger", dependencies=TRUE)
+install.packages("magrittr", dependencies=TRUE)
+install.packages("formatR", dependencies=TRUE)
+install.packages("RcppParallel", dependencies=TRUE)
+install.packages("stringi", dependencies=TRUE)
+install.packages("Matrix", dependencies=TRUE)
+install.packages("lambda.r", dependencies=TRUE)
+install.packages("doParallel", dependencies=TRUE)
+install.packages("grid", dependencies=TRUE)
+install.packages("tools", dependencies=TRUE)
+install.packages("iterators", dependencies=TRUE)
+install.packages("mlapi", dependencies=TRUE)
+install.packages("compiler", dependencies=TRUE)
+install.packages("lubridate", dependencies=TRUE)
+#install.packages("stringr", dependencies=TRUE)
+#install.packages("text2vec", dependencies=TRUE)
+#install.packages("parallel", dependencies=TRUE)
+#install.packages("lsa", dependencies=TRUE)
+#install.packages("plyr", dependencies=TRUE)
+#install.packages("R6", dependencies=TRUE)
+#install.packages("doParallel", dependencies=TRUE)
+
+# Semantic matching package
+install.packages("textreuse", dependencies=TRUE)
+
+# Advanced federation
+#install.packages("stringdist", dependencies=TRUE)
+
+# Fuzzy matching
+# Fuzzy Join 
+install.packages("rlang", dependencies=TRUE)
+install.packages("tibble", dependencies=TRUE)
+install.packages("R6", dependencies=TRUE)
+install.packages("bindrcpp", dependencies=TRUE)
+install.packages("crayon", dependencies=TRUE)
+install.packages("pkgconfig", dependencies=TRUE)
+install.packages("glue", dependencies=TRUE)
+install.packages("plogr", dependencies=TRUE)
+install.packages("purrr", dependencies=TRUE)
+#install.packages("devtools", dependencies=TRUE)
+#install.packages("dplyr", dependencies=TRUE)
+
+# Semantic matching
+install.packages("plyr", dependencies=TRUE)
+install.packages("WikidataR", dependencies=TRUE)
+install.packages("curl", dependencies=TRUE)
+install.packages("openssl", dependencies=TRUE)
+install.packages("jsonlite", dependencies=TRUE)
+install.packages("WikipediR", dependencies=TRUE)
+install.packages("LSAfun", dependencies=TRUE)
+install.packages("text2vec", dependencies=TRUE)
+#install.packages("httr", dependencies=TRUE)
+
+# semantic description
+#install.packages("WikidataR", dependencies=TRUE)
+#install.packages("WikipediR", dependencies=TRUE)
+#install.packages("curl", dependencies=TRUE)
+#install.packages("jsonlite", dependencies=TRUE)
+#install.packages("httr", dependencies=TRUE)
+
+# Database semantic
+install.packages("lsa", dependencies=TRUE)
+#install.packages("WikidataR", dependencies=TRUE)
+#install.packages("text2vec", dependencies=TRUE)
+#install.packages("plyr", dependencies=TRUE)
+#install.packages("stringdist", dependencies=TRUE)
+
+# Edit Rules
+install.packages("validate", dependencies=TRUE)
+install.packages("yaml", dependencies=TRUE)
+install.packages("settings", dependencies=TRUE)
+
+# Xray
+install.packages("RcppProgress", dependencies=TRUE)
+install.packages("withr", dependencies=TRUE)
+install.packages("NLP", dependencies=TRUE)
+install.packages("memoise", dependencies=TRUE)
+install.packages("tidyselect", dependencies=TRUE)
+#install.packages("textreuse", dependencies=TRUE)
+#install.packages("purrr", dependencies=TRUE)
+#install.packages("tidyr", dependencies=TRUE)
+#install.packages("devtools", dependencies=TRUE)
+#install.packages("digest", dependencies=TRUE)
+#install.packages("WikidataR", dependencies=TRUE)
+#install.packages("WikipediR", dependencies=TRUE)
+#install.packages("httr", dependencies=TRUE)
+#install.packages("curl", dependencies=TRUE)
+#install.packages("jsonlite", dependencies=TRUE)
+
+# Xray Metamodel
+#install.packages("jsonlite", dependencies=TRUE)
+
+
+
+
+
+
+###############################################################################
+#  Graph Visualizations
+###############################################################################
+install.packages("igraph", dependencies=TRUE)
+
+
+###############################################################################
+#  Analytics
+###############################################################################
 
 # Apriori
-install.packages("arules")
-#install.packages("data.table")
-#install.packages("dplyr")
+install.packages("arules", dependencies=TRUE)
+#install.packages("data.table", dependencies=TRUE)
+#install.packages("dplyr", dependencies=TRUE)
 
 # Classification
-install.packages("partykit")
-#install.packages("dplyr")
-#install.packages("data.table")
+install.packages("partykit", dependencies=TRUE)
+#install.packages("dplyr", dependencies=TRUE)
+#install.packages("data.table", dependencies=TRUE)
 
 # Clustering
-install.packages("cluster")
+install.packages("cluster", dependencies=TRUE)
 
 # LOF
-install.packages("Rlof")
-install.packages("VGAM")
-#install.packages("data.table")
-#install.packages("dplyr")
+install.packages("Rlof", dependencies=TRUE)
+install.packages("VGAM", dependencies=TRUE)
+#install.packages("data.table", dependencies=TRUE)
+#install.packages("dplyr", dependencies=TRUE)
 
 # Outlier
-install.packages("HDoutliers")
-#install.packages("data.table")
+install.packages("HDoutliers", dependencies=TRUE)
+#install.packages("data.table", dependencies=TRUE)
 
 # Random Forest
-install.packages("randomForest")
-#install.packages("data.table")
-#install.packages("dplyr")
+install.packages("randomForest", dependencies=TRUE)
+#install.packages("data.table", dependencies=TRUE)
+#install.packages("dplyr", dependencies=TRUE)
 
-# graph
-install.packages("igraph")
 
 # LSA Column
-#install.packages("LSAfun")
-#install.packages("text2vec")
+#install.packages("LSAfun", dependencies=TRUE)
+#install.packages("text2vec", dependencies=TRUE)
 
+# Anomaly detection packages
+install.packages("RCurl", dependencies=TRUE)
+install.packages("httr", dependencies=TRUE)
+install.packages("devtools", dependencies=TRUE)
+set_config( config( ssl_verifypeer = 0L ) )
+devtools::install_github("twitter/AnomalyDetection", dependencies=TRUE)
 
-
+# Business rules
+install.packages("arules", dependencies=TRUE)
