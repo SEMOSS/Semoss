c1d551dcde4e9f6afd4606a81b4bab6512b6df5b
mahkhalil <mahkhalil@deloitte.com>
2018-10-16 17:42:43 +0000

    UPDATE ::: Adding initial semantic column match using instance values
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30482 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ac4414b1838cdee43f8ad9936e7fe1823ea81351

--- src/prerna/sablecc2/reactor/app/metaeditor/OwlInstanceSemanticCosineSimilarityMatchReactor.java
+++ src/prerna/sablecc2/reactor/app/metaeditor/OwlInstanceSemanticCosineSimilarityMatchReactor.java
@@ -6,9 +6,15 @@ import java.util.Vector;
 import org.apache.log4j.Logger;
 import org.rosuda.REngine.Rserve.RConnection;
 
+import prerna.algorithm.api.SemossDataType;
 import prerna.ds.r.RDataTable;
 import prerna.ds.r.RSyntaxHelper;
 import prerna.engine.api.IEngine;
+import prerna.engine.api.IRawSelectWrapper;
+import prerna.query.querystruct.SelectQueryStruct;
+import prerna.query.querystruct.filters.SimpleQueryFilter;
+import prerna.query.querystruct.selectors.QueryColumnSelector;
+import prerna.rdf.engine.wrappers.WrapperManager;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
@@ -19,14 +25,14 @@ import prerna.sablecc2.reactor.frame.r.util.IRJavaTranslator;
 import prerna.sablecc2.reactor.imports.ImportUtility;
 import prerna.util.Utility;
 
-public class OwlColumnSemanticCosineSimilarityMatchReactor extends AbstractMetaEditorReactor {
+public class OwlInstanceSemanticCosineSimilarityMatchReactor extends AbstractMetaEditorReactor {
 
 	private static final String CLASS_NAME = OwlIndirectNameMatchReactor.class.getName();
 
 	/**
 	 * Example script to run:
 	 
-	 source("C:/workspace/Semoss_Dev/R/OwlMatchRoutines/OwlColumnCosineDistance.R");
+	 source("C:/workspace/Semoss_Dev/R/OwlMatchRoutines/OwlCosineDistance.R");
 	 allTables_aQC7ep4 <- c('city','city','city','city','city','countrylanguage','countrylanguage','countrylanguage','countrylanguage','country','country','country','country','country','country','country','country','country','country','country','country','country','country','country');
 	 allColumns_aHGRoJ8 <- c('ID','Name','CountryCode','District','Population','CountryCode','Language','IsOfficial','Percentage','Code','Name','Continent','Region','SurfaceArea','IndepYear','Population','LifeExpectancy','GNP','GNPOld','LocalName','GovernmentForm','HeadOfState','Capital','Code2');
 	 matches_awiHmTT<- getDocumentCostineSimilarityMatrix(allTables_aQC7ep4,allColumns_aHGRoJ8);
@@ -34,7 +40,7 @@ public class OwlColumnSemanticCosineSimilarityMatchReactor extends AbstractMetaE
 	 * 
 	 */
 	
-	public OwlColumnSemanticCosineSimilarityMatchReactor() {
+	public OwlInstanceSemanticCosineSimilarityMatchReactor() {
 		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey()};
 	}
 	
@@ -61,27 +67,60 @@ public class OwlColumnSemanticCosineSimilarityMatchReactor extends AbstractMetaE
 		// matched by index
 		List<String> tableNamesList = new Vector<String>();
 		List<String> columnNamesList = new Vector<String>();
+		// also store a matrix of instances
+		List<List<String>> sampleInstances = new Vector<List<String>>();
 		
 		Vector<String> concepts = app.getConcepts(false);
 		for(String cUri : concepts) {
 			String tableName = Utility.getInstanceName(cUri);
 			String tablePrimCol = Utility.getClassName(cUri);
 			
-			tableNamesList.add(tableName);
-			columnNamesList.add(tablePrimCol);
-			
+			// we will only store string values!!!
+			SemossDataType cDataType = SemossDataType.convertStringToDataType(app.getDataTypes(cUri).replace("TYPE:", ""));
+			if(cDataType == SemossDataType.STRING) {
+				tableNamesList.add(tableName);
+				columnNamesList.add(tablePrimCol);
+				
+				List<String> colValues = new Vector<String>();
+				IRawSelectWrapper wrapper = WrapperManager.getInstance().getRawWrapper(app, getBaseQs(tableName, 5));
+				try {
+					while(wrapper.hasNext()) {
+						colValues.add(wrapper.next().getValues()[0].toString());
+					} 
+				} finally {
+					wrapper.cleanUp();
+				}
+				sampleInstances.add(colValues);
+			}
+				
 			// grab all the properties
 			List<String> properties = app.getProperties4Concept(cUri, false);
 			for(String pUri : properties) {
-				tableNamesList.add(tableName);
-				columnNamesList.add(Utility.getClassName(pUri));
+				// we will only store string values!!!
+				SemossDataType pDataType = SemossDataType.convertStringToDataType(app.getDataTypes(pUri).replace("TYPE:", ""));
+				if(pDataType == SemossDataType.STRING) {
+					tableNamesList.add(tableName);
+					String colName = Utility.getClassName(pUri);
+					columnNamesList.add(colName);
+				
+					List<String> colValues = new Vector<String>();
+					IRawSelectWrapper wrapper = WrapperManager.getInstance().getRawWrapper(app, getBaseQs(tableName + "__" + colName, 5));
+					try {
+						while(wrapper.hasNext()) {
+							colValues.add(wrapper.next().getValues()[0].toString());
+						} 
+					} finally {
+						wrapper.cleanUp();
+					}
+					sampleInstances.add(colValues);
+				}
 			}
 		}
 		
 		StringBuilder script = new StringBuilder();
 		
 		// first source the file where we have the main method for running
-		String rScriptPath = getBaseFolder() + "\\R\\OwlMatchRoutines\\OwlColumnCosineDistance.r"; 
+		String rScriptPath = getBaseFolder() + "\\R\\OwlMatchRoutines\\OwlInstanceCosineDistance.r"; 
 		rScriptPath = rScriptPath.replace("\\", "/");
 		script.append("source(\"" + rScriptPath + "\");");
 		
@@ -94,9 +133,18 @@ public class OwlColumnSemanticCosineSimilarityMatchReactor extends AbstractMetaE
 		String allColumnsVar = "allColumns_" + Utility.getRandomString(6);
 		script.append(allColumnsVar).append(" <- ").append(RSyntaxHelper.createStringRColVec(columnNamesList)).append(";");
 		
+		// will now create a list of vectors for each sample instances we have collected
+		String isntanceListVar = "instances_" + Utility.getRandomString(6);
+		int numVals = sampleInstances.size();
+		script.append(isntanceListVar).append(" <- list(");
+		script.append(RSyntaxHelper.createStringRColVec(sampleInstances.get(0)));
+		for(int i = 1; i < numVals; i++) {
+			script.append(",").append(RSyntaxHelper.createStringRColVec(sampleInstances.get(i)));
+		}
+		script.append(");");
 		// now that we have defined the inputs, just need to run the "main" method of the script
 		String matchDataFrame = "matches_" + Utility.getRandomString(6);
-		script.append(matchDataFrame).append( "<- getDocumentCostineSimilarityMatrix(").append(allTablesVar).append(",").append(allColumnsVar).append(");");
+		script.append(matchDataFrame).append( "<- getDocumentCostineSimilarityMatrix(").append(allTablesVar).append(",").append(allColumnsVar).append(",").append(isntanceListVar).append(");");
 
 		// execute!
 		logger.info("Running script to auto generate descriptions...");
@@ -129,4 +177,26 @@ public class OwlColumnSemanticCosineSimilarityMatchReactor extends AbstractMetaE
 		// return the frame
 		return retNoun;
 	}
+	
+	private SelectQueryStruct getBaseQs(String qsName, int limit) {
+		SelectQueryStruct qs = new SelectQueryStruct();
+		qs.addSelector(new QueryColumnSelector(qsName));
+		qs.setLimit(limit);
+		
+		{
+			NounMetadata lComparison = new NounMetadata(new QueryColumnSelector(qsName), PixelDataType.COLUMN);
+			NounMetadata rComparison = new NounMetadata(null, PixelDataType.NULL_VALUE);
+			SimpleQueryFilter f = new SimpleQueryFilter(lComparison, "!=", rComparison );
+			qs.addExplicitFilter(f);
+		}
+		{
+			NounMetadata lComparison = new NounMetadata(new QueryColumnSelector(qsName), PixelDataType.COLUMN);
+			NounMetadata rComparison = new NounMetadata("", PixelDataType.CONST_STRING);
+			SimpleQueryFilter f = new SimpleQueryFilter(lComparison, "!=", rComparison );
+			qs.addExplicitFilter(f);
+		}
+		
+		return qs;
+	}
+	
 }
