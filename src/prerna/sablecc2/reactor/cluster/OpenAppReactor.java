6b3e91bf5bd4e058baedfeaca05e1b008200b368
tbanach <tbanach@deloitte.com>
2018-10-18 00:34:42 +0000

    BUGFIX ::: Fixing annoying message on upload and new with no app
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30548 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e5e73082e9843757f8adba8af0d1f013832d24d0

--- src/prerna/sablecc2/reactor/cluster/OpenAppReactor.java
+++ src/prerna/sablecc2/reactor/cluster/OpenAppReactor.java
@@ -29,35 +29,31 @@ public class OpenAppReactor extends AbstractReactor {
 			throw new IllegalArgumentException("Must input an app id");
 		}
 		
-		List<Map<String, Object>> baseInfo = null;
+		if(appId.equals("NEWSEMOSSAPP")) {
+			Map<String, Object> returnMap = new HashMap<String, Object>();
+			returnMap.put("app_name", "NEWSEMOSSAPP");
+			returnMap.put("app_id", appId);
+			returnMap.put("app_type", IEngine.ENGINE_TYPE.APP.toString());	
+			return new NounMetadata(returnMap, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.OPEN_APP);
+		}
+		
 		if(AbstractSecurityUtils.securityEnabled()) {
 			// make sure valid id for user
 			if(!SecurityQueryUtils.getUserEngineIds(this.insight.getUser()).contains(appId)) {
 				// you dont have access
 				throw new IllegalArgumentException("App does not exist or user does not have access to database");
 			}
-			// user has access!
-			baseInfo = SecurityQueryUtils.getUserDatabaseList(this.insight.getUser(), appId);
-		} else {
-			// just grab the info
-			baseInfo = SecurityQueryUtils.getAllDatabaseList();
 		}
 		
-		if(baseInfo.isEmpty()) {
-			throw new IllegalArgumentException("Could not find any app data");
+		IEngine engine = Utility.getEngine(appId);
+		if(engine == null) {
+			throw new IllegalArgumentException("Could not find or load app = " + appId);
 		}
-		
+
 		Map<String, Object> returnMap = new HashMap<String, Object>();
-		if(appId.equals("NEWSEMOSSAPP")) {
-			returnMap.put("app_name", "NEWSEMOSSAPP");
-			returnMap.put("app_id", appId);
-			returnMap.put("app_type", IEngine.ENGINE_TYPE.APP.toString());	
-		} else {
-			IEngine engine = Utility.getEngine(appId);
-			returnMap.put("app_name", engine.getEngineName());
-			returnMap.put("app_id", engine.getEngineId());
-			returnMap.put("app_type", engine.getEngineType().toString());	
-		}
+		returnMap.put("app_name", engine.getEngineName());
+		returnMap.put("app_id", engine.getEngineId());
+		returnMap.put("app_type", engine.getEngineType().toString());	
 		
 		return new NounMetadata(returnMap, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.OPEN_APP);
 	}
