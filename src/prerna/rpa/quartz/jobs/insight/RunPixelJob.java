cb5e7dab9f19128c0e452877fd5d578ac3c98879
tbanach <tbanach@deloitte.com>
2018-10-25 20:34:45 +0000

    BUGFIX ::: Scheduler fixes - encodes the recipe to avoid issues communicating with the FE. UPDATE ::: Also, need to save user info for permissions. This is done by encrypting the id. UPDATE ::: Loading the job first on Pixel and have a check in the watcher to make sure its not already registered
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30670 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 5b0c123cb4452e63b4d6d5c0cc5e4102bd7309e3

--- src/prerna/rpa/quartz/jobs/insight/RunPixelJob.java
+++ src/prerna/rpa/quartz/jobs/insight/RunPixelJob.java
@@ -7,8 +7,13 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.quartz.UnableToInterruptJobException;
 
+import prerna.auth.AccessToken;
+import prerna.auth.AuthProvider;
+import prerna.auth.User;
 import prerna.om.Insight;
 import prerna.om.InsightStore;
+import prerna.rpa.RPAProps;
+import prerna.rpa.config.JobConfigKeys;
 import prerna.rpa.quartz.CommonDataKeys;
 
 public class RunPixelJob implements org.quartz.InterruptableJob {
@@ -32,11 +37,34 @@ public class RunPixelJob implements org.quartz.InterruptableJob {
 		JobDataMap dataMap = context.getMergedJobDataMap();
 		jobName = context.getJobDetail().getKey().getName();
 		String pixel = dataMap.getString(IN_PIXEL_KEY);
+		String userAccess = RPAProps.getInstance().decrypt(dataMap.getString(JobConfigKeys.USER_ACCESS));
 		
 		////////////////////
 		// Do work
 		////////////////////
 		Insight insight = new Insight();
+		
+		// Add user info to the insight
+		User user = new User();
+		String[] accessPairs = userAccess.split(",");
+		for (String accessPair : accessPairs) {
+			String[] providerAndId = accessPair.split(":");
+			
+			// Get the auth provider
+			AuthProvider provider = AuthProvider.valueOf(providerAndId[0]);
+			
+			// Get the id
+			String id = providerAndId[1];
+			
+			// Create the access token
+			AccessToken token = new AccessToken();
+			token.setProvider(provider);
+			token.setId(id);
+			
+			user.setAccessToken(token);
+		}
+		insight.setUser(user);
+		
 		String insightId = InsightStore.getInstance().put(insight);
 		if(!pixel.endsWith(";")) {
 			pixel = pixel + ";";
