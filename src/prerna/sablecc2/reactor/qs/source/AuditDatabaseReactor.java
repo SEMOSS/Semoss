cf39326ecad5e783b0760f03f29bed11f4f02c44
rramirezjimenez <rramirezjimenez@deloitte.com>
2018-10-31 21:05:33 +0000

    UPDATE ::: Added AuditDatabase as a source to query using any qs
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30728 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 089ed7d4d3c8aaa7ece8ebbfb99570cfb21791ce

---
+++ src/prerna/sablecc2/reactor/qs/source/AuditDatabaseReactor.java
@@ -0,0 +1,54 @@
+package prerna.sablecc2.reactor.qs.source;
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.SecurityQueryUtils;
+import prerna.engine.impl.rdbms.AuditDatabase;
+import prerna.engine.impl.rdbms.RDBMSNativeEngine;
+import prerna.nameserver.utility.MasterDatabaseUtility;
+import prerna.query.querystruct.AbstractQueryStruct;
+import prerna.query.querystruct.SelectQueryStruct;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.reactor.qs.AbstractQueryStructReactor;
+import prerna.util.Utility;
+
+public class AuditDatabaseReactor extends AbstractQueryStructReactor {
+	
+	public AuditDatabaseReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey()};
+	}
+	
+	@Override
+	protected AbstractQueryStruct createQueryStruct() {
+		organizeKeys();
+		String appId = this.keyValue.get(ReactorKeysEnum.APP.getKey());
+		
+		// we may have the alias
+		if(AbstractSecurityUtils.securityEnabled()) {
+			appId = SecurityQueryUtils.testUserEngineIdForAlias(this.insight.getUser(), appId);
+			if(!SecurityQueryUtils.getUserEngineIds(this.insight.getUser()).contains(appId)) {
+				throw new IllegalArgumentException("Database " + appId + " does not exist or user does not have access to database");
+			}
+		} else {
+			appId = MasterDatabaseUtility.testEngineIdIfAlias(appId);
+			if(!MasterDatabaseUtility.getAllEngineIds().contains(appId)) {
+				throw new IllegalArgumentException("Database " + appId + " does not exist");
+			}
+		}
+	
+		if (!(Utility.getEngine(appId) instanceof RDBMSNativeEngine)) {
+			throw new IllegalArgumentException("App must be using a relational database");
+		}
+		
+		// get audit database from app id
+		RDBMSNativeEngine engine = (RDBMSNativeEngine) Utility.getEngine(appId);
+		AuditDatabase audit = engine.generateAudit();
+		
+		RDBMSNativeEngine fakeEngine = new RDBMSNativeEngine();
+		fakeEngine.setEngineId("FAKE_ENGINE");
+		fakeEngine.setConnection(audit.getConnection());
+		fakeEngine.setBasic(true);
+		
+		this.qs.setEngine(fakeEngine);
+		this.qs.setQsType(SelectQueryStruct.QUERY_STRUCT_TYPE.ENGINE);
+		return this.qs;
+	}
+}
