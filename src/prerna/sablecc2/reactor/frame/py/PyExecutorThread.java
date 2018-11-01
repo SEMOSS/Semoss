fb18e0eeac4d5d9176998d7e6a20897a88ddd1bf
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2018-11-01 16:17:11 +0000

    Base implementation for Python. PyExecutorThread - The main thread which listens for commands and executes PyInterpreter - Reactor - that can be invoked as Py("greeting = 'Hello World in Python'); Py("greeting"); ReactorFactory - To add the reactor mapped to "Py"
    Pre -Requisites
    a. JEP - Install Python and then install JEP as pip install JEP https://github.com/ninia/jep/wiki/Getting-Started
    b. Library in the Path of your environment variables. i.e. <Python Base Folder> / Lib / Site-packages / jep  - this is where the DLL sits. If you are starting tomcat with -d LD_LIBRARY_PATH - then add the same there
    c. PYTHON TRUE in the RDF_MAP - if not it will not initialize
    d. Maven JEP - 3.7.0 - If you dont have it
    <dependency>
    		    <groupId>black.ninia</groupId>
    		    <artifactId>jep</artifactId>
    		    <version>3.7.0</version>
    		</dependency>
    
    
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30740 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 71757117b6ca1c408a0a3bc5da568afe56828daa

---
+++ src/prerna/sablecc2/reactor/frame/py/PyExecutorThread.java
@@ -0,0 +1,84 @@
+package prerna.sablecc2.reactor.frame.py;
+
+import jep.Jep;
+import jep.JepException;
+
+public class PyExecutorThread extends Thread {
+
+	public String process = "wait";
+	boolean eval = true; 
+	public String command = null;
+	public Object response = null;
+	
+	Jep jep = null;
+	Object daLock = new Object();
+	
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		// wait to see if process is true
+		// if process is true - process, put the result and go back to sleep
+		
+		getJep();
+		
+		while(!process.equalsIgnoreCase("stop"))
+		{
+			try {
+				synchronized(daLock)
+				{
+					System.out.println("Waiting for next command");
+					daLock.wait();
+					
+					// if someone wakes up
+					// process the command
+					// set the response go back to sleep
+				    try {
+				    	try
+				    	{
+				    		response = jep.getValue(command);
+				    	}catch (Exception ex)
+				    	{
+				    		jep.eval(command);
+				    		response = "";
+				    	}
+						daLock.notify();
+						
+					} catch (JepException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		}
+		System.out.println("Thread ENDED");
+
+	}
+	
+	public Object getMonitor()
+	{
+		return daLock;
+	}
+	
+	public Jep getJep()
+	{
+		try {
+			if(this.jep == null)
+				jep = new Jep(false);
+		} catch (JepException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return jep;
+
+	}
+
+}
---
+++ src/prerna/sablecc2/reactor/frame/py/PyInterpreter.java
@@ -0,0 +1,43 @@
+package prerna.sablecc2.reactor.frame.py;
+
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class PyInterpreter extends AbstractReactor {
+
+	@Override
+	public NounMetadata execute() {
+		// TODO Auto-generated method stub
+		// Get the current thread and run the input
+		
+		String command = getCurRow().get(0) + "";
+		
+		PyExecutorThread pyThread = getJep();
+		Thread myThread = Thread.currentThread();
+		
+		Object lock = pyThread.getMonitor();
+		pyThread.command = command;
+		
+		Object output = "";
+		
+		synchronized(lock)
+		{
+			try {
+				lock.notify();
+				lock.wait();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// waking up now
+			output = pyThread.response;
+			
+			System.out.println("Output >>> " + output);
+		}
+		
+		
+		return new NounMetadata(output+"", PixelDataType.CONST_STRING);
+	}
+
+}
