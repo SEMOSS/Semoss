3c128292df52bd1172c470708c8196332868d73d
mahkhalil <mahkhalil@deloitte.com>
2018-11-07 23:48:37 +0000

    UPDATE ::: Fixing the grammar to allow for boolean results in AND operations. Previously, could only do x > 5 AND y > 7 with the "AND" operation, but now you can do true AND true, where a widget options are directly setting the value for a boolean, and can perform more complex type operations (as long as they return a boolean) like "if(1>5, true, false) OR true"
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30841 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 0fb57709508ea37f5b9c7d60957448f9019b771f

--- src/prerna/sablecc2/node/ALeftComplexAndComparison.java
+++ src/prerna/sablecc2/node/ALeftComplexAndComparison.java
@@ -6,28 +6,28 @@ import java.util.*;
 import prerna.sablecc2.analysis.*;
 
 @SuppressWarnings("nls")
-public final class AOrComparison extends POrComparison
+public final class ALeftComplexAndComparison extends PAndComparison
 {
-    private PBaseSimpleComparison _left_;
-    private TOrComparator _orComparator_;
+    private PTerm _left_;
+    private TAndComparator _andComparator_;
     private PBaseSimpleComparison _right_;
-    private final LinkedList<PRepeatingOrComparison> _moreRight_ = new LinkedList<PRepeatingOrComparison>();
+    private final LinkedList<PRepeatingAndComparison> _moreRight_ = new LinkedList<PRepeatingAndComparison>();
 
-    public AOrComparison()
+    public ALeftComplexAndComparison()
     {
         // Constructor
     }
 
-    public AOrComparison(
-        @SuppressWarnings("hiding") PBaseSimpleComparison _left_,
-        @SuppressWarnings("hiding") TOrComparator _orComparator_,
+    public ALeftComplexAndComparison(
+        @SuppressWarnings("hiding") PTerm _left_,
+        @SuppressWarnings("hiding") TAndComparator _andComparator_,
         @SuppressWarnings("hiding") PBaseSimpleComparison _right_,
         @SuppressWarnings("hiding") List<?> _moreRight_)
     {
         // Constructor
         setLeft(_left_);
 
-        setOrComparator(_orComparator_);
+        setAndComparator(_andComparator_);
 
         setRight(_right_);
 
@@ -38,9 +38,9 @@ public final class AOrComparison extends POrComparison
     @Override
     public Object clone()
     {
-        return new AOrComparison(
+        return new ALeftComplexAndComparison(
             cloneNode(this._left_),
-            cloneNode(this._orComparator_),
+            cloneNode(this._andComparator_),
             cloneNode(this._right_),
             cloneList(this._moreRight_));
     }
@@ -48,15 +48,15 @@ public final class AOrComparison extends POrComparison
     @Override
     public void apply(Switch sw)
     {
-        ((Analysis) sw).caseAOrComparison(this);
+        ((Analysis) sw).caseALeftComplexAndComparison(this);
     }
 
-    public PBaseSimpleComparison getLeft()
+    public PTerm getLeft()
     {
         return this._left_;
     }
 
-    public void setLeft(PBaseSimpleComparison node)
+    public void setLeft(PTerm node)
     {
         if(this._left_ != null)
         {
@@ -76,16 +76,16 @@ public final class AOrComparison extends POrComparison
         this._left_ = node;
     }
 
-    public TOrComparator getOrComparator()
+    public TAndComparator getAndComparator()
     {
-        return this._orComparator_;
+        return this._andComparator_;
     }
 
-    public void setOrComparator(TOrComparator node)
+    public void setAndComparator(TAndComparator node)
     {
-        if(this._orComparator_ != null)
+        if(this._andComparator_ != null)
         {
-            this._orComparator_.parent(null);
+            this._andComparator_.parent(null);
         }
 
         if(node != null)
@@ -98,7 +98,7 @@ public final class AOrComparison extends POrComparison
             node.parent(this);
         }
 
-        this._orComparator_ = node;
+        this._andComparator_ = node;
     }
 
     public PBaseSimpleComparison getRight()
@@ -126,14 +126,14 @@ public final class AOrComparison extends POrComparison
         this._right_ = node;
     }
 
-    public LinkedList<PRepeatingOrComparison> getMoreRight()
+    public LinkedList<PRepeatingAndComparison> getMoreRight()
     {
         return this._moreRight_;
     }
 
     public void setMoreRight(List<?> list)
     {
-        for(PRepeatingOrComparison e : this._moreRight_)
+        for(PRepeatingAndComparison e : this._moreRight_)
         {
             e.parent(null);
         }
@@ -141,7 +141,7 @@ public final class AOrComparison extends POrComparison
 
         for(Object obj_e : list)
         {
-            PRepeatingOrComparison e = (PRepeatingOrComparison) obj_e;
+            PRepeatingAndComparison e = (PRepeatingAndComparison) obj_e;
             if(e.parent() != null)
             {
                 e.parent().removeChild(e);
@@ -157,7 +157,7 @@ public final class AOrComparison extends POrComparison
     {
         return ""
             + toString(this._left_)
-            + toString(this._orComparator_)
+            + toString(this._andComparator_)
             + toString(this._right_)
             + toString(this._moreRight_);
     }
@@ -172,9 +172,9 @@ public final class AOrComparison extends POrComparison
             return;
         }
 
-        if(this._orComparator_ == child)
+        if(this._andComparator_ == child)
         {
-            this._orComparator_ = null;
+            this._andComparator_ = null;
             return;
         }
 
@@ -198,13 +198,13 @@ public final class AOrComparison extends POrComparison
         // Replace child
         if(this._left_ == oldChild)
         {
-            setLeft((PBaseSimpleComparison) newChild);
+            setLeft((PTerm) newChild);
             return;
         }
 
-        if(this._orComparator_ == oldChild)
+        if(this._andComparator_ == oldChild)
         {
-            setOrComparator((TOrComparator) newChild);
+            setAndComparator((TAndComparator) newChild);
             return;
         }
 
@@ -214,13 +214,13 @@ public final class AOrComparison extends POrComparison
             return;
         }
 
-        for(ListIterator<PRepeatingOrComparison> i = this._moreRight_.listIterator(); i.hasNext();)
+        for(ListIterator<PRepeatingAndComparison> i = this._moreRight_.listIterator(); i.hasNext();)
         {
             if(i.next() == oldChild)
             {
                 if(newChild != null)
                 {
-                    i.set((PRepeatingOrComparison) newChild);
+                    i.set((PRepeatingAndComparison) newChild);
                     newChild.parent(this);
                     oldChild.parent(null);
                     return;
