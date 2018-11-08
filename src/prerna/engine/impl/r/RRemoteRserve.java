a9408b04f241692c2a7f308e757f8894d7e0b511
kunalppatel9 <kunalppatel9@deloitte.com>
2018-11-08 17:30:58 +0000

    ADDED ::: Adding in RemoteRserve
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30848 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/engine/impl/r/RRemoteRserve.java
@@ -0,0 +1,45 @@
+package prerna.engine.impl.r;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.Hashtable;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.lang.SystemUtils;
+import org.rosuda.REngine.Rserve.RConnection;
+import org.rosuda.REngine.Rserve.RserveException;
+
+import prerna.util.DIHelper;
+
+public class RRemoteRserve {
+	
+	private RConnection rcon = null;
+	
+	public RConnection getConnection() {
+		
+		if(System.getenv("REMOTE_RSERVE").equalsIgnoreCase("true")){
+			String server = System.getenv("REMOTE_RSERVE_IP");
+		if(server.contains(":")){
+			String[] hostInfo = server.split(":", 2);
+			try {
+				String host = hostInfo[0];
+				int port = Integer.parseInt(hostInfo[1]);
+				rcon = new RConnection(host, port);
+			} catch (RserveException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else{
+			try {
+				rcon = new RConnection(server);
+			} catch (RserveException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+		return rcon;
+
+}
+}
