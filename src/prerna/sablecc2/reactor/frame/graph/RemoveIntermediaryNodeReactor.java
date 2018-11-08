e689864294714fc9b5a0a1c795fc46d7135c4112
mahkhalil <mahkhalil@deloitte.com>
2018-11-08 22:29:08 +0000

    UPDATE ::: Building out tinker specific reactor to remove intermediary nodes. Syntax example : RemoveIntermediaryNode(IntermediaryNodeToRemove);  Frame must be a graph (tinker)
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30854 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 98c6c6f52b89c1451158039bcc44a9512adaf635

--- src/prerna/sablecc2/reactor/frame/graph/RemoveIntermediaryNodeReactor.java
+++ src/prerna/sablecc2/reactor/frame/graph/RemoveIntermediaryNodeReactor.java
@@ -12,6 +12,8 @@ import org.apache.tinkerpop.gremlin.structure.Vertex;
 import prerna.ds.OwlTemporalEngineMeta;
 import prerna.ds.TinkerFrame;
 import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.frame.AbstractFrameReactor;
@@ -30,13 +32,34 @@ public class RemoveIntermediaryNodeReactor extends AbstractFrameReactor {
 		
 		String physicalTypeToRemove = meta.getPhysicalName(nodeTypeToRemove);
 		
+		List<String> upstream = new Vector<String>();
+		List<String> upstreamPhysical = new Vector<String>();
+		
+		List<String> downstream = new Vector<String>();
+		List<String> downstreamPhysical = new Vector<String>();
+		
+		List<String[]> upstreamRels = meta.getUpstreamRelationships(nodeTypeToRemove);
+		List<String[]> downstreamRels = meta.getDownstreamRelationships(nodeTypeToRemove);
+		
+		for(String up[] : upstreamRels) {
+			upstream.add(up[0]);
+			upstreamPhysical.add(meta.getPhysicalName(up[0]));
+		}
+		for(String[] down : downstreamRels) {
+			downstream.add(down[1]);
+			downstreamPhysical.add(meta.getPhysicalName(down[1]));
+		}
+		
 		// we will iterate through the graph for all edges of this node type
 		GraphTraversal<Vertex, Vertex> traversal = tinker.g.traversal().V().has(TinkerFrame.TINKER_TYPE, physicalTypeToRemove);
 		while(traversal.hasNext()) {
 			Vertex vert = traversal.next();
 			
 			List<Vertex> sourceVertices = new Vector<Vertex>();
+			List<String> sourceUniqueName = new Vector<String>();
+			
 			List<Vertex> targetVertices = new Vector<Vertex>();
+			List<String> targetUniqueName = new Vector<String>();
 			
 			Iterator<Edge> inEdges = vert.edges(Direction.IN);
 			while(inEdges.hasNext()) {
@@ -45,7 +68,11 @@ public class RemoveIntermediaryNodeReactor extends AbstractFrameReactor {
 				// we can determine if this is that type of node
 				// based on the edge relationship
 				if(edge.property(TinkerFrame.TINKER_ID).value().toString().contains(nodeTypeToRemove)) {
-					sourceVertices.add(edge.outVertex());
+					Vertex outVertex = edge.outVertex();
+					String outVertexPhysical = outVertex.value(TinkerFrame.TINKER_TYPE);
+					
+					sourceVertices.add(outVertex);
+					sourceUniqueName.add(upstream.get(upstreamPhysical.indexOf(outVertexPhysical)));
 				}
 			}
 			
@@ -56,17 +83,29 @@ public class RemoveIntermediaryNodeReactor extends AbstractFrameReactor {
 				// we can determine if this is that type of node
 				// based on the edge relationship
 				if(edge.property(TinkerFrame.TINKER_ID).value().toString().contains(nodeTypeToRemove)) {
-					targetVertices.add(edge.inVertex());
+					Vertex inVertex = edge.inVertex();
+					String inVertexPhysical = inVertex.value(TinkerFrame.TINKER_TYPE);
+
+					targetVertices.add(inVertex);
+					targetUniqueName.add(downstream.get(downstreamPhysical.indexOf(inVertexPhysical)));
 				}
 			}
 			
 			// we now need to connect every source vertex with every target vertex
-			for(Vertex source : sourceVertices) {
-				String sourceType = source.value(TinkerFrame.TINKER_TYPE);
+			int numSource = sourceVertices.size();
+			int numTarget = targetVertices.size();
+			for(int i = 0; i < numSource; i++) {
+				Vertex source = sourceVertices.get(i);
+				String sourceName = sourceUniqueName.get(i);
 				String sourceValue = source.value(TinkerFrame.TINKER_NAME);
-				for(Vertex target : targetVertices) {
-					String type = sourceType + TinkerFrame.EDGE_LABEL_DELIMETER + target.value(TinkerFrame.TINKER_TYPE);
-					String edgeID = type + "/" + sourceValue + ":" + target.value(TinkerFrame.TINKER_NAME);
+
+				for(int j = 0; j < numTarget; j++) {
+					Vertex target = targetVertices.get(j);
+					String targetName = targetUniqueName.get(j);
+					String targetValue = target.value(TinkerFrame.TINKER_NAME);
+					
+					String type = sourceName + TinkerFrame.EDGE_LABEL_DELIMETER + targetName;
+					String edgeID = type + "/" + sourceValue + ":" + targetValue;
 					
 					// try to find the vertex
 					GraphTraversal<Edge, Edge> gt = tinker.g.traversal().E().has(TinkerFrame.TINKER_ID, edgeID);
@@ -76,16 +115,30 @@ public class RemoveIntermediaryNodeReactor extends AbstractFrameReactor {
 						count++;
 						retEdge.property(TinkerFrame.TINKER_COUNT, count);
 					} else {
-						source.addEdge(edgeID, target, TinkerFrame.TINKER_COUNT, 1);
+						source.addEdge(type, target, TinkerFrame.TINKER_ID, edgeID, TinkerFrame.TINKER_COUNT, 1);
 					}
 				}
 			}
+			
+			// now drop the intermediary node
+			vert.remove();
 		}
 		
-		// also need to update the meta of this frame
+		// when we drop the meta, we remove all 
+		// the relationships it has as well
+		meta.dropVertex(nodeTypeToRemove);
 		
+		// now add all the upstream to the downstream
+		for(String up : upstream) {
+			for(String down : downstream) {
+				// now we will connect the 2 together
+				meta.addRelationship(up, down, "inner.join");
+			}
+		}
 		
-		return null;
+		NounMetadata retNoun = new NounMetadata(tinker, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE, PixelOperationType.FRAME_HEADERS_CHANGE);
+		retNoun.addAdditionalReturn(new NounMetadata(meta.getTableHeaderObjects(), PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.FRAME_HEADERS));
+		return retNoun;
 	}
 	
 	////////////////////////////////////////////////////////////
