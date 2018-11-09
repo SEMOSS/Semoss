4585408ada0d06a92f006c6e59d22532e72fdc04
Maher Khalil <themaherkhalil@gmail.com>
2018-11-09 15:41:08 +0000

    UPDATE ::: AddPanelReactor() reactor will generate a random id if no value is passed UPDATE ::: Allowing the map key to be a variable (must be enclosed within a {}) UPDATE ::: Adding new operation type for enabling subroutines so FE knows to loop through the array of noun returns UPDATE ::: Adding new reactor GetPanelId to get the id to use in TaskOptions when creating a random panel
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30863 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 78441a20c611623990305c693aa8d5fd3a285a6c

--- src/prerna/sablecc2/reactor/panel/AddPanelReactor.java
+++ src/prerna/sablecc2/reactor/panel/AddPanelReactor.java
@@ -1,5 +1,7 @@
 package prerna.sablecc2.reactor.panel;
 
+import java.util.Random;
+
 import prerna.om.InsightPanel;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
@@ -9,6 +11,8 @@ import prerna.sablecc2.reactor.AbstractReactor;
 
 public class AddPanelReactor extends AbstractReactor {
 	
+	private static Random rand = new Random();
+	
 	public AddPanelReactor() {
 		this.keysToGet = new String[]{ReactorKeysEnum.PANEL.getKey()};
 	}
@@ -16,7 +20,13 @@ public class AddPanelReactor extends AbstractReactor {
 	@Override
 	public NounMetadata execute() {
 		// first input is the name of the panel
-		String panelId = this.curRow.get(0).toString();
+		String panelId = null;
+		organizeKeys();
+		if(this.keyValue.isEmpty()) {
+			panelId = rand.nextInt(5000) + "";
+		} else {
+			panelId = this.keyValue.get(this.keysToGet[0]);
+		}
 		InsightPanel newPanel = new InsightPanel(panelId);
 		this.insight.addNewInsightPanel(newPanel);
 		NounMetadata noun = new NounMetadata(newPanel, PixelDataType.PANEL, PixelOperationType.PANEL_OPEN);
