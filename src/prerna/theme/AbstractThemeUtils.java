ba00535ae313f95621f2c4bd0c22998cccef541c
mahkhalil <mahkhalil@deloitte.com>
2018-11-14 19:26:51 +0000

    UPDATE ::: Adding back end code for setting custom theming
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30911 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/theme/AbstractThemeUtils.java
@@ -0,0 +1,42 @@
+package prerna.theme;
+
+import java.sql.SQLException;
+
+import prerna.ds.util.RdbmsQueryBuilder;
+import prerna.engine.impl.rdbms.RDBMSNativeEngine;
+import prerna.util.Constants;
+import prerna.util.Utility;
+
+public abstract class AbstractThemeUtils {
+
+	static RDBMSNativeEngine themeDb;
+
+	
+	/**
+	 * Only used for static references
+	 */
+	AbstractThemeUtils() {
+		
+	}
+	
+	public static void loadSecurityDatabase() throws SQLException {
+		themeDb = (RDBMSNativeEngine) Utility.getEngine(Constants.THEMING_DB);
+		initialize();
+	}
+
+	private static void initialize() throws SQLException {
+		String[] colNames = null;
+		String[] types = null;
+		/*
+		 * Currently used
+		 */
+		
+		// ADMIN_THEME
+		colNames = new String[] { "id", "theme_name", "theme_map", "is_active" };
+		types = new String[] { "varchar(255)", "varchar(255)", "clob", "boolean" };
+		themeDb.insertData(RdbmsQueryBuilder.makeOptionalCreate("ADMIN_THEME", colNames, types));
+
+		// commit the changes
+		themeDb.commit();
+	}
+}
