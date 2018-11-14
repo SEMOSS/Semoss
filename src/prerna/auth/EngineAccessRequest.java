d4f35a49c110ab7107f82772596955e6ed2e1116
mahkhalil <mahkhalil@deloitte.com>
2018-11-14 14:13:06 +0000

    UPDATE ::: Renaming enum from EnginePermission to AccessPermission since it is used for both engines and insight level permissions
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30902 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 72f440f5dd18a96b1deed0b09eb7e702a591e08c

--- src/prerna/auth/EngineAccessRequest.java
+++ src/prerna/auth/EngineAccessRequest.java
@@ -1,25 +1,25 @@
-package prerna.auth;
-
-public class EngineAccessRequest {
-	private String requestId;
-	private String userName;
-	private String engineName;
-	
-	public EngineAccessRequest(String requestId, String userId, String engineName) {
-		this.requestId = requestId;
-		this.userName = userId;
-		this.engineName = engineName;
-	}
-	
-	public String getRequestId() {
-		return this.requestId;
-	}
-	
-	public String getUser() {
-		return this.userName;
-	}
-	
-	public String getEngineRequested() {
-		return this.engineName;
-	}	
-}
+//package prerna.auth;
+//
+//public class EngineAccessRequest {
+//	private String requestId;
+//	private String userName;
+//	private String engineName;
+//	
+//	public EngineAccessRequest(String requestId, String userId, String engineName) {
+//		this.requestId = requestId;
+//		this.userName = userId;
+//		this.engineName = engineName;
+//	}
+//	
+//	public String getRequestId() {
+//		return this.requestId;
+//	}
+//	
+//	public String getUser() {
+//		return this.userName;
+//	}
+//	
+//	public String getEngineRequested() {
+//		return this.engineName;
+//	}	
+//}
