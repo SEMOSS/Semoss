6bc5a6166bc761879e6e85afe4ea9ca4ee73ebb9
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2018-11-26 16:59:05 +0000

    Pandas Commit Create Frame - Pandas / Py GenerateFrameFromPyVariableReactor All the other classes - PandasFrame - main frame PandasInterpreter - Creates all the syntax from query struct Pandas Syntax Helper - Create pandas Syntax RawPandasWrapper.java - Pandas Wrapper PandasIterator - IHeadersRow iterator
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@30971 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 51025f3bf6586a49b35290ae9746cd3cc5fecaea

---
+++ src/prerna/ds/py/RawPandasWrapper.java
@@ -0,0 +1,84 @@
+package prerna.ds.py;
+
+import prerna.algorithm.api.SemossDataType;
+import prerna.engine.api.IEngine;
+import prerna.engine.api.IHeadersDataRow;
+import prerna.engine.api.IRawSelectWrapper;
+
+public class RawPandasWrapper implements IRawSelectWrapper {
+
+	PandasIterator iterator = null;
+	
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+	@Override
+	public void setQuery(String query) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getQuery() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void cleanUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setEngine(IEngine engine) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return iterator.hasNext();
+	}
+
+	@Override
+	public IHeadersDataRow next() {
+		// TODO Auto-generated method stub
+		return iterator.next();
+	}
+
+	@Override
+	public String[] getHeaders() {
+		// TODO Auto-generated method stub
+		return iterator.getHeaders();
+	}
+
+	@Override
+	public SemossDataType[] getTypes() {
+		// TODO Auto-generated method stub
+		return iterator.colTypes;
+	}
+
+	@Override
+	public long getNumRecords() {
+		// TODO Auto-generated method stub
+		return iterator.fullData.size();
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setPandasIterator(PandasIterator pi) {
+		// TODO Auto-generated method stub
+		this.iterator = pi;
+	}
+
+}
