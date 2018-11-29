c8839e76f7b8735fa76b778fd58d6ee9b256814a
mahkhalil <mahkhalil@deloitte.com>
2018-11-29 17:46:17 +0000

    UPDATE ::: Adding functionality to be able to pipe task data directly into operations instead of having to use the | "Iterate() | GrabScalarElement()" pixel syntax.  Example: x = Select(Median(MovieBudget))|Collect(-1); 5 - x; This will return the expected results of 5 - the return of the query x.  For this syntax to work, x must return exactly 1 element BUGFIX ::: Fixing error with the BE hanging when an operation runs and crashes without removing the last $RESULT value from the pixel store.  If the noun result of $RESULT is a lambda that has an error, all the next pixel statements will break since we check if there is a $RESULT upon execution of later pixel statements BUGFIX ::: Actually fixing the median query reactor (returning formal object and not primitive type) UPDATE ::: Cleaning up assimilator to use the reactor store instead of just formulating the signature for all the inputs UPDATE ::: Adding TaskUtility to help with some of the parsing of task data
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31042 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 165e8e344288bc29339d0a4dcca466ecffead652

---
+++ src/prerna/sablecc2/om/task/TaskUtility.java
@@ -0,0 +1,99 @@
+package prerna.sablecc2.om.task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+
+public class TaskUtility {
+
+	private TaskUtility() {
+		
+	}
+	
+	/**
+	 * Flush the task data into an array
+	 * This assumes you have table data!!!
+	 * @param taskData
+	 * @return
+	 */
+	public static List<Object> flushJobData(ITask taskData) {
+		List<Object> flushedOutCol = new ArrayList<Object>();
+		// iterate through the task to get the table data
+		List<Object[]> data = taskData.flushOutIteratorAsGrid();
+		int size = data.size();
+		// assumes we are only flushing out the first column
+		for(int i = 0; i < size; i++) {
+			flushedOutCol.add(data.get(i)[0]);
+		}
+		
+		return flushedOutCol;
+	}
+	
+	/**
+	 * We got to predict the type of the values when we have a bunch to merge
+	 * @param obj
+	 * @return
+	 */
+	public static PixelDataType predictTypeFromObject(List<Object> obj) {
+		int size = obj.size();
+		if(size == 0) {
+			return PixelDataType.CONST_STRING;
+		}
+		
+		Object firstVal = null;
+		int counter = 0;
+		while(firstVal == null && counter < size) {
+			firstVal = obj.get(counter);
+			counter++;
+		}
+		
+		return predictTypeFromObject(firstVal);
+	}
+	
+	/**
+	 * We got to predict the type of the values when we have a bunch to merge
+	 * @param obj
+	 * @return
+	 */
+	public static PixelDataType predictTypeFromObject(Object obj) {
+		if(obj instanceof Double) {
+			return PixelDataType.CONST_DECIMAL;
+		} else if(obj instanceof Integer) {
+			return PixelDataType.CONST_INT;
+		} else if(obj instanceof String) {
+			return PixelDataType.CONST_STRING;
+		} else if(obj instanceof Boolean) {
+			return PixelDataType.BOOLEAN;
+		}
+		
+		return PixelDataType.CONST_STRING;
+	}
+	
+	/**
+	 * Get a scalar element from collected table data
+	 * @param taskData
+	 * @return
+	 */
+	public static NounMetadata getTaskDataScalarElement(Object taskData) {
+		//TODO: grab the type from the task data if present instead of doing the cast
+		if(taskData instanceof Map) {
+			Map dataMap = (Map) ((Map) taskData).get("data");
+			if(dataMap != null) {
+				List<Object[]> values = (List<Object[]>) dataMap.get("values");
+				if(values != null && values.size() == 1) {
+					Object[] singleRow = values.get(0);
+					if(singleRow.length == 1) {
+						Object val = singleRow[0];
+						PixelDataType type = predictTypeFromObject(val);
+						return new NounMetadata(val, type);
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+}
