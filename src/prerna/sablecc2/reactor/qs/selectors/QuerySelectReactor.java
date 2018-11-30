cea6a5c4635c0ef263d12d4c88908ea883ecc31a
Maher Khalil <themaherkhalil@gmail.com>
2018-11-30 14:25:27 +0000

    BUGFIX ::: Fixing error with aliases when you perform a Select().as([]) | Select().as([]) where the first aliases were not preserved in the query struct
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31067 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c892e89763991c4514a5317c6d8de2b4b8611ac8

--- src/prerna/sablecc2/reactor/qs/selectors/QuerySelectReactor.java
+++ src/prerna/sablecc2/reactor/qs/selectors/QuerySelectReactor.java
@@ -31,7 +31,7 @@ public class QuerySelectReactor extends AbstractQueryStructReactor {
 					selectors.add(selector);
 				}
 			}
-			setAlias(selectors, this.selectorAlias);
+			setAlias(selectors, this.selectorAlias, 0);
 			qs.mergeSelectors(selectors);
 		}
 		return qs;
