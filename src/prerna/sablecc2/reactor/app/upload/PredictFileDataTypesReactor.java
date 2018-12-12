e5021c6f2096cac673803de9421ae1de6c201d7d
mahkhalil <mahkhalil@deloitte.com>
2018-12-12 20:30:30 +0000

    UPDATE ::: Renaming reactor + returning original headers + cleaned headers
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31358 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/app/upload/PredictFileDataTypesReactor.java
+++ src/prerna/sablecc2/reactor/app/upload/PredictFileDataTypesReactor.java
@@ -9,9 +9,9 @@ import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 
-public class PredictDataTypesReactor extends AbstractReactor {
+public class PredictFileDataTypesReactor extends AbstractReactor {
 
-	public PredictDataTypesReactor() {
+	public PredictFileDataTypesReactor() {
 		this.keysToGet = new String[] { UploadInputUtility.FILE_PATH, UploadInputUtility.DELIMITER, UploadInputUtility.ROW_COUNT };
 	}
 
@@ -27,6 +27,8 @@ public class PredictDataTypesReactor extends AbstractReactor {
 		helper.parse(filePath);
 		Map[] predictionMaps = FileHelperUtil.generateDataTypeMapsFromPrediction(helper.getHeaders(), helper.predictTypes());
 		Map<String, Object> retMap = new HashMap<>();
+		retMap.put("headers", helper.getFileOriginalHeaders());
+		retMap.put("cleanHeaders", helper.getHeaders());
 		retMap.put("dataTypes", predictionMaps[0]);
 		retMap.put("additionalDataTypes", predictionMaps[1]);
 		if (rowCount) {
