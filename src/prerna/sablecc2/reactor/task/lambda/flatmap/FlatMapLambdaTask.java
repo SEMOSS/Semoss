646c41255354e899bccdfbab38660edecb8bdee1
mahkhalil <mahkhalil@deloitte.com>
2018-12-13 18:53:35 +0000

    UPDATE ::: Accounting for null values in flat map operations
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31386 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e29fbba87d49c7804c1fa2a1cde1177b394c1f85

--- src/prerna/sablecc2/reactor/task/lambda/flatmap/FlatMapLambdaTask.java
+++ src/prerna/sablecc2/reactor/task/lambda/flatmap/FlatMapLambdaTask.java
@@ -12,6 +12,7 @@ public class FlatMapLambdaTask extends AbstractTaskOperation {
 	// need to account for many rows being created 
 	// by the lambda
 	private List<IHeadersDataRow> lambdaResults;
+	private IHeadersDataRow innerRow;
 	private int curIndex = 0;
 	
 	@Override
@@ -37,9 +38,21 @@ public class FlatMapLambdaTask extends AbstractTaskOperation {
 			return true;
 		}
 		
+		// reset the index
+		curIndex = 0;
 		// else we need to see if the inner task
 		// has more results
-		return this.innerTask.hasNext();
+		boolean hasNext = this.innerTask.hasNext();
+		if(hasNext) {
+			innerRow = this.innerTask.next();
+			lambdaResults = lambda.process(innerRow);
+			if(lambdaResults == null || lambdaResults.isEmpty()) {
+				// this result returned back nothing from the lambda
+				// repeat for the next row of the inner task
+				return hasNext();
+			}
+		}
+		return hasNext;
 	}
 
 	public void setLambda(IFlatMapLambda lambda) {
