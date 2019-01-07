f94e653b5383bd518b0c6388291de2161bbfc513
tbanach <tbanach@deloitte.com>
2019-01-07 14:47:57 +0000

    UPDATE ::: Committing test framework
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31550 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8e90c7604ddf7c3fe94a959c34a44bf27bb70bb7

---
+++ test/prerna/junit/AllTests.java
@@ -0,0 +1,13 @@
+package prerna.junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import prerna.util.gson.IHeadersDataRowAdapterTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ IHeadersDataRowAdapterTest.class })
+public class AllTests {
+		
+}
