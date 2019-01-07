4c1b2e4a61e4b50ca80e7c32d6a0033810246734
tbanach <tbanach@deloitte.com>
2019-01-07 14:47:57 +0000

    UPDATE ::: Committing test framework
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31550 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8e90c7604ddf7c3fe94a959c34a44bf27bb70bb7

---
+++ test/prerna/junit/JUnit.java
@@ -0,0 +1,34 @@
+package prerna.junit;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.BeforeClass;
+
+public class JUnit {
+	
+	protected static final Logger LOGGER = LogManager.getLogger(JUnit.class.getName());
+	
+	protected static final String FILE_SEPARATOR = System.getProperty("file.separator");
+	
+	protected static final String TEST_RESOURCES_DIRECTORY = new File("test" + FILE_SEPARATOR + "resources").getAbsolutePath(); 
+
+	@BeforeClass
+	public static void configureLog4j() {
+		String log4JPropertyFile = TEST_RESOURCES_DIRECTORY + FILE_SEPARATOR + "log4j.properties";
+		Properties prop = new Properties();
+		try {
+			prop.load(new FileInputStream(log4JPropertyFile));
+			PropertyConfigurator.configure(prop);
+			LOGGER.info("Successfully configured Log4j for JUnit suite.");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
