5da30624b79a0738158c9822cf3d84775935f2d7
mahkhalil <mahkhalil@deloitte.com>
2019-01-11 00:14:58 +0000

    UPDATE ::: Adding logger for dropping an insight
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31645 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 028da6aa999619ee114025d736535335243dbbc6

--- src/prerna/sablecc2/reactor/insights/DropInsightReactor.java
+++ src/prerna/sablecc2/reactor/insights/DropInsightReactor.java
@@ -1,13 +1,19 @@
 package prerna.sablecc2.reactor.insights;
 
+import org.apache.log4j.Logger;
+
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 
 public class DropInsightReactor extends AbstractInsightReactor{
 
+	private static final String CLASS_NAME = DropInsightReactor.class.getName();
+	
 	@Override
 	public NounMetadata execute() {
+		Logger logger = getLogger(CLASS_NAME);
+		logger.info("Drop insight with id = " + this.insight.getInsightId());
 		return new NounMetadata(true, PixelDataType.DROP_INSIGHT, PixelOperationType.DROP_INSIGHT);
 	}
 }
