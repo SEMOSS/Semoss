ed1007fbbbff6ce060a727e2a3cb625af2e05f41
tbanach <tbanach@deloitte.com>
2019-01-16 20:31:48 +0000

    REQUIREMENT ::: Maven update BUGFIX ::: Removing ws4j jar which conflicts with JUnit version BUGFIX ::: Fixing issue with Pixel regex for specifying relative paths in tests UPDATE ::: Adding in option to refresh database after a test (for example if the pixel inserts/deletes/modifies data in any way) UPDATE ::: Using parameterized test, so that the @Before and @After annotations remain valid
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31717 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ test/prerna/junit/PixelUnitWithDatabases.java
@@ -0,0 +1,41 @@
+package prerna.junit;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeThat;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+
+public class PixelUnitWithDatabases extends PixelUnit {
+
+	private static final String COLLEGE = "unit_test_college";
+	private static final String MOVIE = "unit_test_movie";
+	
+	private static final String METAMODEL_EXTENSION = "_metamodel.txt";
+	
+	@Before
+	public void checkAssumptions() {
+		checkTestDatabase(COLLEGE);
+		checkTestDatabase(MOVIE);
+	}
+	
+	public void checkTestDatabase(String alias) {
+		String appId = aliasToAppId.get(alias);
+		String pixel = "GetDatabaseMetamodel(database=[\"" + appId + "\"]);";
+		try {
+			String expectedJson = FileUtils.readFileToString(Paths.get(TEST_DATA_DIRECTORY, alias + METAMODEL_EXTENSION).toFile());
+			Object result = compareResult(pixel, expectedJson);
+			assumeThat(result, is(equalTo(new HashMap<>())));
+		} catch (IOException e) {
+			LOGGER.error(e);
+			fail();
+		}
+	}
+	
+}
