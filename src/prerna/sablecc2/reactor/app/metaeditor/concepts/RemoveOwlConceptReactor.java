a317efae6ed1906d2e9e14090475c1dcf2e533cd
mahkhalil <mahkhalil@deloitte.com>
2019-01-16 21:05:12 +0000

    UPDATE ::: Completing RemoveOwlConceptReactor to drop a node and all its properties from the OWL
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31720 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 91b9ea8fc2216625b0cadfd822bb3f359363537f

--- src/prerna/sablecc2/reactor/app/metaeditor/concepts/RemoveOwlConceptReactor.java
+++ src/prerna/sablecc2/reactor/app/metaeditor/concepts/RemoveOwlConceptReactor.java
@@ -1,24 +1,24 @@
 package prerna.sablecc2.reactor.app.metaeditor.concepts;
 
-import java.io.IOException;
-import java.util.Vector;
+import java.util.List;
 
 import prerna.engine.api.IEngine;
-import prerna.engine.api.IEngine.ENGINE_TYPE;
+import prerna.engine.api.IEngine.ACTION_TYPE;
+import prerna.engine.api.IHeadersDataRow;
+import prerna.engine.api.IRawSelectWrapper;
+import prerna.engine.impl.rdf.RDFFileSesameEngine;
+import prerna.rdf.engine.wrappers.WrapperManager;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.app.metaeditor.AbstractMetaEditorReactor;
-import prerna.util.OWLER;
 import prerna.util.Utility;
 
 public class RemoveOwlConceptReactor extends AbstractMetaEditorReactor {
 
 	public RemoveOwlConceptReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), ReactorKeysEnum.CONCEPT.getKey(),
-				ReactorKeysEnum.COLUMN.getKey(), ReactorKeysEnum.DATA_TYPE.getKey()
-			};
+		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), ReactorKeysEnum.CONCEPT.getKey()};
 	}
 	
 	@Override
@@ -35,32 +35,103 @@ public class RemoveOwlConceptReactor extends AbstractMetaEditorReactor {
 			throw new IllegalArgumentException("Must define the concept being added to the app metadata");
 		}
 		
-		// if RDBMS, we need to know the prim key of the column
+		// since we are deleting the node
+		// i need to delete the properties of this node
+		// and then everything related to this node
 		IEngine engine = Utility.getEngine(appId);
-		String column = this.keyValue.get(this.keysToGet[2]);
-		if( (column == null || column.isEmpty()) && engine.getEngineType() == ENGINE_TYPE.RDBMS ) {
-			throw new IllegalArgumentException("Must define the column for the concept being added to the app metadata");
+		RDFFileSesameEngine owlEngine = engine.getBaseDataEngine();
+		String conceptPhysical = engine.getPhysicalUriFromConceptualUri(concept);
+		List<String> properties = engine.getProperties4Concept(conceptPhysical, false);
+		StringBuilder bindings = new StringBuilder();
+		for(String prop : properties) {
+			bindings.append("(<").append(prop).append(">)");
 		}
-		String dataType = this.keyValue.get(this.keysToGet[3]);
-		if(dataType == null || dataType.isEmpty()) {
-			throw new IllegalArgumentException("Must define the data type for the concept being added to the app metadata");
-		}
-
-		Vector<String> concepts = engine.getConcepts(false);
-		for(String conceptUri : concepts) {
-			String table = Utility.getInstanceName(conceptUri);
-			if(table.equalsIgnoreCase(concept)) {
-				throw new IllegalArgumentException("A concept already exists with this name. "
-						+ "Add a new unique concept or edit the existing concept");
+		
+		if(bindings.length() > 0) {
+			// get everything downstream of the props
+			{
+				String query = "select ?s ?p ?o where "
+						+ "{ "
+						+ "{?s ?p ?o} "
+						+ "} bindings ?s {" + bindings.toString() + "}";
+			
+				IRawSelectWrapper it = WrapperManager.getInstance().getRawWrapper(owlEngine, query);
+				while(it.hasNext()) {
+					IHeadersDataRow headerRows = it.next();
+					Object[] raw = headerRows.getRawValues();
+					String s = raw[0].toString();
+					String p = raw[1].toString();
+					String o = raw[2].toString();
+					boolean bool = !objectIsLiteral(p);
+					owlEngine.doAction(ACTION_TYPE.REMOVE_STATEMENT, new Object[]{s, p, o, bool});
+				}
+			}
+			
+			// repeat for upstream of prop
+			{
+				String query = "select ?s ?p ?o where "
+						+ "{ "
+						+ "{?s ?p ?o} "
+						+ "} bindings ?o {" + bindings.toString() + "}";
+			
+				IRawSelectWrapper it = WrapperManager.getInstance().getRawWrapper(owlEngine, query);
+				while(it.hasNext()) {
+					IHeadersDataRow headerRows = it.next();
+					Object[] raw = headerRows.getRawValues();
+					String s = raw[0].toString();
+					String p = raw[1].toString();
+					String o = raw[2].toString();
+					boolean bool = !objectIsLiteral(p);
+					owlEngine.doAction(ACTION_TYPE.REMOVE_STATEMENT, new Object[]{s, p, o, bool});
+				}
 			}
 		}
-
-		OWLER owler = getOWLER(appId);
-		owler.addConcept(concept, column, dataType);
-
+		
+		// now repeat for the node itself
+		
+		// remove everything downstream of the node
+		{
+			String query = "select ?s ?p ?o where "
+					+ "{ "
+					+ "bind(<" + conceptPhysical + "> as ?s) "
+					+ "{?s ?p ?o} "
+					+ "}";
+		
+			IRawSelectWrapper it = WrapperManager.getInstance().getRawWrapper(owlEngine, query);
+			while(it.hasNext()) {
+				IHeadersDataRow headerRows = it.next();
+				Object[] raw = headerRows.getRawValues();
+				String s = raw[0].toString();
+				String p = raw[1].toString();
+				String o = raw[2].toString();
+				boolean bool = !objectIsLiteral(p);
+				owlEngine.doAction(ACTION_TYPE.REMOVE_STATEMENT, new Object[]{s, p, o, bool});
+			}
+		}
+		
+		// repeat for upstream of the node
+		{
+			String query = "select ?s ?p ?o where "
+					+ "{ "
+					+ "bind(<" + conceptPhysical + "> as ?o) "
+					+ "{?s ?p ?o} "
+					+ "}";
+		
+			IRawSelectWrapper it = WrapperManager.getInstance().getRawWrapper(owlEngine, query);
+			while(it.hasNext()) {
+				IHeadersDataRow headerRows = it.next();
+				Object[] raw = headerRows.getRawValues();
+				String s = raw[0].toString();
+				String p = raw[1].toString();
+				String o = raw[2].toString();
+				boolean bool = !objectIsLiteral(p);
+				owlEngine.doAction(ACTION_TYPE.REMOVE_STATEMENT, new Object[]{s, p, o, bool});
+			}
+		}
+		
 		try {
-			owler.export();
-		} catch (IOException e) {
+			owlEngine.exportDB();
+		} catch (Exception e) {
 			e.printStackTrace();
 			NounMetadata noun = new NounMetadata(false, PixelDataType.BOOLEAN);
 			noun.addAdditionalReturn(new NounMetadata("An error occured attempting to add the relationships defined", 
@@ -69,7 +140,8 @@ public class RemoveOwlConceptReactor extends AbstractMetaEditorReactor {
 		}
 	
 		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
-		noun.addAdditionalReturn(new NounMetadata("Successfully added new concept", PixelDataType.CONST_STRING, PixelOperationType.SUCCESS));
+		noun.addAdditionalReturn(new NounMetadata("Successfully removed concept and all its dependencies", PixelDataType.CONST_STRING, PixelOperationType.SUCCESS));
 		return noun;
 	}
+	
 }
