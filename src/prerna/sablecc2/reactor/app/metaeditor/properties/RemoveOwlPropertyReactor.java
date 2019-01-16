93933fedf43bf7bde4e522da48a8c247350351bb
mahkhalil <mahkhalil@deloitte.com>
2019-01-16 21:53:15 +0000

    UPDATE ::: Adding removing of owl property
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31723 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f2f8a9d49ae421366444e982740b137ebbd9a637

--- src/prerna/sablecc2/reactor/app/metaeditor/properties/RemoveOwlPropertyReactor.java
+++ src/prerna/sablecc2/reactor/app/metaeditor/properties/RemoveOwlPropertyReactor.java
@@ -1,24 +1,23 @@
 package prerna.sablecc2.reactor.app.metaeditor.properties;
 
-import java.io.IOException;
-import java.util.Vector;
-
 import prerna.engine.api.IEngine;
-import prerna.engine.api.IEngine.ENGINE_TYPE;
+import prerna.engine.api.IEngine.ACTION_TYPE;
+import prerna.engine.api.IHeadersDataRow;
+import prerna.engine.api.IRawSelectWrapper;
+import prerna.engine.impl.rdf.RDFFileSesameEngine;
+import prerna.rdf.engine.wrappers.WrapperManager;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.app.metaeditor.AbstractMetaEditorReactor;
-import prerna.util.OWLER;
 import prerna.util.Utility;
 
 public class RemoveOwlPropertyReactor extends AbstractMetaEditorReactor {
 
 	public RemoveOwlPropertyReactor() {
 		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), ReactorKeysEnum.CONCEPT.getKey(),
-				ReactorKeysEnum.COLUMN.getKey(), ReactorKeysEnum.DATA_TYPE.getKey()
-			};
+				ReactorKeysEnum.COLUMN.getKey() };
 	}
 	
 	@Override
@@ -34,42 +33,67 @@ public class RemoveOwlPropertyReactor extends AbstractMetaEditorReactor {
 		if(concept == null || concept.isEmpty()) {
 			throw new IllegalArgumentException("Must define the concept being added to the app metadata");
 		}
-		
+		String column = this.keyValue.get(this.keysToGet[2]);
+		if( column == null || column.isEmpty()) {
+			throw new IllegalArgumentException("Must define the property being added to the app metadata");
+		}
 		// if RDBMS, we need to know the prim key of the column
 		IEngine engine = Utility.getEngine(appId);
-		String column = this.keyValue.get(this.keysToGet[2]);
-		if( (column == null || column.isEmpty()) && engine.getEngineType() == ENGINE_TYPE.RDBMS ) {
-			throw new IllegalArgumentException("Must define the column for the concept being added to the app metadata");
-		}
-		String dataType = this.keyValue.get(this.keysToGet[3]);
-		if(dataType == null || dataType.isEmpty()) {
-			throw new IllegalArgumentException("Must define the data type for the concept being added to the app metadata");
-		}
-
-		Vector<String> concepts = engine.getConcepts(false);
-		for(String conceptUri : concepts) {
-			String table = Utility.getInstanceName(conceptUri);
-			if(table.equalsIgnoreCase(concept)) {
-				throw new IllegalArgumentException("A concept already exists with this name. "
-						+ "Add a new unique concept or edit the existing concept");
+		RDFFileSesameEngine owlEngine = engine.getBaseDataEngine();
+		String physicalPropUri = engine.getPhysicalUriFromConceptualUri(column, concept);
+		
+		// remove everything downstream of the property
+		{
+			String query = "select ?s ?p ?o where "
+					+ "{ "
+					+ "bind(<" + physicalPropUri + "> as ?s) "
+					+ "{?s ?p ?o} "
+					+ "}";
+		
+			IRawSelectWrapper it = WrapperManager.getInstance().getRawWrapper(owlEngine, query);
+			while(it.hasNext()) {
+				IHeadersDataRow headerRows = it.next();
+				Object[] raw = headerRows.getRawValues();
+				String s = raw[0].toString();
+				String p = raw[1].toString();
+				String o = raw[2].toString();
+				boolean bool = !objectIsLiteral(p);
+				owlEngine.doAction(ACTION_TYPE.REMOVE_STATEMENT, new Object[]{s, p, o, bool});
 			}
 		}
-
-		OWLER owler = getOWLER(appId);
-		owler.addConcept(concept, column, dataType);
-
+		
+		// repeat for upstream of the property
+		{
+			String query = "select ?s ?p ?o where "
+					+ "{ "
+					+ "bind(<" + physicalPropUri + "> as ?o) "
+					+ "{?s ?p ?o} "
+					+ "}";
+		
+			IRawSelectWrapper it = WrapperManager.getInstance().getRawWrapper(owlEngine, query);
+			while(it.hasNext()) {
+				IHeadersDataRow headerRows = it.next();
+				Object[] raw = headerRows.getRawValues();
+				String s = raw[0].toString();
+				String p = raw[1].toString();
+				String o = raw[2].toString();
+				boolean bool = !objectIsLiteral(p);
+				owlEngine.doAction(ACTION_TYPE.REMOVE_STATEMENT, new Object[]{s, p, o, bool});
+			}
+		}
+		
 		try {
-			owler.export();
-		} catch (IOException e) {
+			owlEngine.exportDB();
+		} catch (Exception e) {
 			e.printStackTrace();
 			NounMetadata noun = new NounMetadata(false, PixelDataType.BOOLEAN);
-			noun.addAdditionalReturn(new NounMetadata("An error occured attempting to add the relationships defined", 
+			noun.addAdditionalReturn(new NounMetadata("An error occured attempting to remove the desired property", 
 					PixelDataType.CONST_STRING, PixelOperationType.ERROR));
 			return noun;
 		}
 	
 		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
-		noun.addAdditionalReturn(new NounMetadata("Successfully added new concept", PixelDataType.CONST_STRING, PixelOperationType.SUCCESS));
+		noun.addAdditionalReturn(new NounMetadata("Successfully removed property", PixelDataType.CONST_STRING, PixelOperationType.SUCCESS));
 		return noun;
 	}
 }
