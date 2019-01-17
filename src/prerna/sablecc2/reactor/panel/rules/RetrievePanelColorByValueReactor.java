d91ef9a723a612d2af761e74a18f7a05e2bfa9b7
Maher Khalil <themaherkhalil@gmail.com>
2019-01-17 17:20:27 +0000

    UPDATE ::: Adding new panel ornament data type to hold options that we would want to send with task data to paint ornaments like color-by-value rules UPDATE ::: Adding error messages for color by value reactors UPDATE ::: Building out RetrievePanelColorByValueReactor to return additional metadata to auto formulate task options during data collection
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31734 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 1d2298b1bff82ad142d563f3cdc2d99d2f96f7fe

--- src/prerna/sablecc2/reactor/panel/rules/RetrievePanelColorByValueReactor.java
+++ src/prerna/sablecc2/reactor/panel/rules/RetrievePanelColorByValueReactor.java
@@ -1,8 +1,12 @@
 package prerna.sablecc2.reactor.panel.rules;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import prerna.om.InsightPanel;
 import prerna.query.querystruct.SelectQueryStruct;
 import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 
@@ -16,7 +20,17 @@ public class RetrievePanelColorByValueReactor extends AbstractPanelColorByValueR
 	public NounMetadata execute() {
 		// get the insight panel
 		InsightPanel insightPanel = getInsightPanel();
-		SelectQueryStruct qs = insightPanel.getColorByValue().get(getCbvId(1));
+		if(insightPanel == null) {
+			throw new NullPointerException("Could not find insight panel");
+		}
+		String cbvRule = getCbvId(1);
+		if(cbvRule == null) {
+			throw new NullPointerException("Must provide the color by value name within the panel");
+		}
+		SelectQueryStruct qs = insightPanel.getColorByValue().get(cbvRule);
+		if(qs == null) {
+			throw new NullPointerException("Could not find the color by value rule within the panel");
+		}
 		// make a copy
 		// so that the original QS is not modified if additional changes
 		// are added to the rule
@@ -24,6 +38,15 @@ public class RetrievePanelColorByValueReactor extends AbstractPanelColorByValueR
 		newQs.merge(qs);
 		newQs.setDistinct(qs.isDistinct());
 		newQs.setOverrideImplicit(qs.isOverrideImplicit());
-		return new NounMetadata(newQs, PixelDataType.QUERY_STRUCT);
+		
+		NounMetadata retNoun = new NounMetadata(newQs, PixelDataType.QUERY_STRUCT);
+		// we need to append some additional metadata to this noun
+		Map<String, Object> retMap = new HashMap<String, Object>();
+		retMap.put("panelId", insightPanel.getPanelId());
+		retMap.put("name", cbvRule);
+		retMap.put("type", "color");
+		NounMetadata additionalNoun = new NounMetadata(retMap, PixelDataType.ORNAMENT_MAP, PixelOperationType.PANEL_COLOR_BY_VALUE);
+		retNoun.addAdditionalReturn(additionalNoun);
+		return retNoun;
 	}
 }
