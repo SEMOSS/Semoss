eca27087059579592736fd14aade890cf8c046d3
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-01-28 21:42:35 +0000

    UPDATE ::: AppInsights filter multiple apps
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31869 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 4d6f5e7564e3b8d4adddc88a0883371a3f2ba180

--- src/prerna/solr/reactor/AppInsightsReactor.java
+++ src/prerna/solr/reactor/AppInsightsReactor.java
@@ -2,9 +2,12 @@ package prerna.solr.reactor;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.SecurityQueryUtils;
+import prerna.nameserver.utility.MasterDatabaseUtility;
+import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
@@ -14,40 +17,63 @@ import prerna.sablecc2.reactor.AbstractReactor;
 public class AppInsightsReactor extends AbstractReactor {
 	
 	public AppInsightsReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), 
-				ReactorKeysEnum.FILTER_WORD.getKey(), 
-				ReactorKeysEnum.LIMIT.getKey(), 
-				ReactorKeysEnum.OFFSET.getKey()};
+		this.keysToGet = new String[] { ReactorKeysEnum.APP.getKey(), ReactorKeysEnum.FILTER_WORD.getKey(),
+				ReactorKeysEnum.LIMIT.getKey(), ReactorKeysEnum.OFFSET.getKey() };
 	}
 
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
-		String appId = this.keyValue.get(this.keysToGet[0]);
+		GenRowStruct engineFilterGrs = this.store.getNoun(this.keysToGet[0]);
+		List<NounMetadata> warningNouns = new Vector<>();
+		// get list of engineIds if user has access
+		List<String> eFilters = null;
+		if (engineFilterGrs != null && !engineFilterGrs.isEmpty()) {
+			eFilters = new Vector<String>();
+			for (int i = 0; i < engineFilterGrs.size(); i++) {
+				String engineFilter = engineFilterGrs.get(i).toString();
+				if (AbstractSecurityUtils.securityEnabled()) {
+					engineFilter = SecurityQueryUtils.testUserEngineIdForAlias(this.insight.getUser(), engineFilter);
+					if (SecurityQueryUtils.userCanViewEngine(this.insight.getUser(), engineFilter)) {
+						eFilters.add(engineFilter);
+					} else {
+						// store warnings
+						String msg = engineFilter + " does not exist or user does not have access to database.";
+						warningNouns.add(new NounMetadata(msg, PixelDataType.CONST_STRING, PixelOperationType.WARNING));
+					}
+				} else {
+					engineFilter = MasterDatabaseUtility.testEngineIdIfAlias(engineFilter);
+					eFilters.add(engineFilter);
+				}
+			}
+		}
 		String searchTerm = this.keyValue.get(this.keysToGet[1]);
 		String limit = this.keyValue.get(this.keysToGet[2]);
 		String offset = this.keyValue.get(this.keysToGet[3]);
-		
+		// get results
 		List<Map<String, Object>> results = null;
-		if(appId != null) {
-			if(AbstractSecurityUtils.securityEnabled()) {
-				if(SecurityQueryUtils.userCanViewEngine(this.insight.getUser(), appId)) {
-					results = SecurityQueryUtils.searchUserInsights(this.insight.getUser(), appId, searchTerm, limit, offset);
-				} else {
-					throw new IllegalArgumentException("App does not exist or user does not have access to database");
-				}
+		if (eFilters != null) {
+			if (AbstractSecurityUtils.securityEnabled()) {
+				results = SecurityQueryUtils.searchUserInsights(this.insight.getUser(), eFilters, searchTerm, limit,offset);
 			} else {
-				results = SecurityQueryUtils.searchInsights(appId, searchTerm, limit, offset);
+				results = SecurityQueryUtils.searchInsights(eFilters, searchTerm, limit, offset);
 			}
 		} else {
-			if(AbstractSecurityUtils.securityEnabled()) {
+			if (AbstractSecurityUtils.securityEnabled()) {
 				results = SecurityQueryUtils.searchUserInsightDataByName(this.insight.getUser(), searchTerm, limit, offset);
 			} else {
 				results = SecurityQueryUtils.searchAllInsightDataByName(searchTerm, limit, offset);
 			}
 		}
 		
-		return new NounMetadata(results, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.APP_INSIGHTS);
+		NounMetadata retNoun = new NounMetadata(results, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.APP_INSIGHTS);
+		// add warnings to results
+		if(!warningNouns.isEmpty()) {
+			for(NounMetadata warning: warningNouns) {
+				retNoun.addAdditionalReturn(warning);
+			}
+		}
+		return retNoun;
 	}
 	
 }
