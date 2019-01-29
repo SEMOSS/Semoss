4a168ea65c0fc1f1039a1acbda7a837903c3befe
Maher Khalil <themaherkhalil@gmail.com>
2019-01-29 22:32:44 +0000

    BUGFIX ::: Correctly (somewhat) fixed determination if insight has a param UPDATE ::: Changing the dashboard recipe to be META|ReloadInsight() so ReloadInsight doesn't get added to the pixel recipe when saving dashboards from dashboards
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31881 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2eec1e285fab386fa0d2e97c2ad4ef09edcd2266

--- src/prerna/sablecc2/InsightParamTranslation.java
+++ src/prerna/sablecc2/InsightParamTranslation.java
@@ -1,7 +1,9 @@
 package prerna.sablecc2;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -9,12 +11,16 @@ import org.apache.log4j.Logger;
 import prerna.sablecc2.analysis.DepthFirstAdapter;
 import prerna.sablecc2.node.AConfiguration;
 import prerna.sablecc2.node.AOperation;
+import prerna.sablecc2.node.POpInput;
 import prerna.sablecc2.node.PRoutine;
 
 public class InsightParamTranslation extends DepthFirstAdapter {
 
 	private static final Logger LOGGER = LogManager.getLogger(InsightParamTranslation.class.getName());
 	private boolean hasParam = false;
+	
+	private Set<String> panelsCreated = new HashSet<String>();
+	
 	
 	@Override
 	public void caseAConfiguration(AConfiguration node) {
@@ -31,7 +37,18 @@ public class InsightParamTranslation extends DepthFirstAdapter {
 		defaultIn(node);
         
         String reactorId = node.getId().toString().trim();
-        if(reactorId.equals("SetPanelView")) {
+        if(reactorId.equals("AddPanel")) {
+			// store order of panel creation
+			POpInput input = node.getOpInput();
+        	String panel = input.toString().trim();
+        	panelsCreated.add(panel);
+
+        }  else if (reactorId.equals("Clone")) {
+			POpInput closePanelInput = node.getOpInput();
+			String panel = closePanelInput.toString().trim();
+        	panelsCreated.add(panel);
+
+		} else if(reactorId.equals("SetPanelView")) {
         	String view = node.getOpInput().toString().trim();
         	if(view.equals("\"param\"")) {
         		hasParam = true;
@@ -44,7 +61,10 @@ public class InsightParamTranslation extends DepthFirstAdapter {
 	}
 
 	public boolean hasParam() {
-		return hasParam;
+		if(panelsCreated.size() == 1 && hasParam) {
+			return true;
+		}
+		return false;
 	}
 
 }
