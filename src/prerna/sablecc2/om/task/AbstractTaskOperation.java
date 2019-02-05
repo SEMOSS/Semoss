a5d40c36b622fc3b21b66fffe603eefc4010bc8a
mahkhalil <mahkhalil@deloitte.com>
2019-02-05 20:46:05 +0000

    UPDATE ::: Modification to how the BE sends the total # of rows
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31942 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8883ed6719a85f30fe68782c7abd17c74bbe7d05

--- src/prerna/sablecc2/om/task/AbstractTaskOperation.java
+++ src/prerna/sablecc2/om/task/AbstractTaskOperation.java
@@ -1,7 +1,6 @@
 package prerna.sablecc2.om.task;
 
 import prerna.engine.api.IHeadersDataRow;
-import prerna.sablecc2.reactor.task.lambda.map.MapLambdaTask;
 
 public abstract class AbstractTaskOperation extends AbstractTask {
 
@@ -25,12 +24,6 @@ public abstract class AbstractTaskOperation extends AbstractTask {
 		this.headerInfo = innerTask.getHeaderInfo();
 		this.sortInfo = innerTask.getSortInfo();
 		this.filterInfo = innerTask.getFilterInfo();
-		
-		// pass the number of rows along
-//		if(this instanceof MapLambdaTask && 
-//				(innerTask instanceof MapLambdaTask || innerTask instanceof BasicIteratorTask)) {
-//			this.numRows = ((AbstractTask) innerTask).getNumRows();
-//		}
 	}
 	
 	@Override
