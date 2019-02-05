5554842b2262ce8a5c6cc78ad028a03f74204348
mahkhalil <mahkhalil@deloitte.com>
2019-02-05 17:31:50 +0000

    BUGFIX ::: Fixing getting count for pandas query UPDATE ::: Returning total count for task data to start to show on UI
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31932 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ds/py/PandasIterator.java
+++ src/prerna/ds/py/PandasIterator.java
@@ -1,8 +1,8 @@
 package prerna.ds.py;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
 import prerna.algorithm.api.SemossDataType;
 import prerna.engine.api.IHeadersDataRow;
@@ -11,56 +11,53 @@ import prerna.om.HeadersDataRow;
 public class PandasIterator implements Iterator<IHeadersDataRow>{
 
 	private String[] headers = null;
-	public SemossDataType[] colTypes = null;
 	private ArrayList <String> actHeaders = null;
 	private boolean transform = false;
 
 	// Current implementation.. pulls the data into the memory.. we will change it after
-	public ArrayList fullData = null;
-
-	public PandasIterator(String [] headers, ArrayList fullData, SemossDataType [] colTypes) {
-		
+	public List fullData = null;
+	public SemossDataType[] colTypes = null;
+	public int initSize = 0;
+	
+	public PandasIterator(String [] headers, List fullData, SemossDataType [] colTypes) {
 		this.headers = headers;
 		this.fullData = fullData;
 		this.colTypes = colTypes;
-		
+
+		this.initSize = fullData.size();
 	}
 	
 	// set actual headers and transform
-	public void setTransform(ArrayList <String> actHeaders, boolean transform)
-	{
+	public void setTransform(ArrayList <String> actHeaders, boolean transform) {
 		this.transform = transform;
 		this.actHeaders = actHeaders;
 	}
 		
 	@Override
 	public boolean hasNext() {
-		// TODO Auto-generated method stub
 		return fullData.size() > 0;
 	}
 
 	@Override
 	public IHeadersDataRow next() {
-		// TODO Auto-generated method stub
-		Object [] values = ((ArrayList)fullData.remove(0)).toArray();
-		if(transform)
-		{
+		Object [] values = ((List) fullData.remove(0)).toArray();
+		if(transform) {
 			// this is going to be interesting.. alrite let us see
 			Object [] newValues = new Object[headers.length];
-			for(int headerIndex = 0;headerIndex < headers.length;headerIndex++)
-			{
+			for(int headerIndex = 0; headerIndex < headers.length; headerIndex++) {
 				int index = actHeaders.indexOf(headers[headerIndex]);
 				newValues[headerIndex] = values [index];
 			}
 			values = newValues;
 		}
 		return new HeadersDataRow(headers, values);
-		
 	}
 	
-	public String [] getHeaders()
-	{
+	public String [] getHeaders() {
 		return headers;
 	}
-
+	
+	public int getInitSize() {
+		return initSize;
+	}
 }
