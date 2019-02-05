a9ec59787d5d6b77297142d584a0a9720d6b9939
kunalppatel9 <kunalppatel9@deloitte.com>
2019-02-05 20:15:09 +0000

    ADDED:::Changes to Cloud Clients to enable AWS S3 storage. All calls to pull and push app from the cloud should go through CloudClient.getClient
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@31937 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/cluster/util/ClusterUtil.java
+++ src/prerna/cluster/util/ClusterUtil.java
@@ -16,22 +16,43 @@ public class ClusterUtil {
 	public static final boolean IS_CLUSTER = System.getenv().containsKey(IS_CLUSTER_KEY)
 			? Boolean.parseBoolean(System.getenv(IS_CLUSTER_KEY)) : false;
 	
+			private static final String STORAGE_PROVIDER_KEY = "SEMOSS_STORAGE_PROVIDER";
+			public static final String STORAGE_PROVIDER = System.getenv(STORAGE_PROVIDER_KEY);
+
+			private static final String REMOTE_RSERVE_KEY = "REMOTE_RSERVE";
+			public static final boolean REMOTE_RSERVE = System.getenv().containsKey(REMOTE_RSERVE_KEY)
+					? Boolean.parseBoolean(System.getenv(REMOTE_RSERVE_KEY)) : false;
+				
 	private static final String LOAD_ENGINES_LOCALLY_KEY = "SEMOSS_LOAD_ENGINES_LOCALLY";
 	public static final boolean LOAD_ENGINES_LOCALLY = System.getenv().containsKey(LOAD_ENGINES_LOCALLY_KEY)
 			? Boolean.parseBoolean(System.getenv(LOAD_ENGINES_LOCALLY_KEY)) : true;
+	/*
+	private static final String MULTIPLE_STORAGE_ACCOUNTS_KEY = "MULTIPLE_STORAGE_ACCOUNTS";
+	public static final boolean MULTIPLE_STORAGE_ACCOUNTS = System.getenv().containsKey(MULTIPLE_STORAGE_ACCOUNTS_KEY)
+					? Boolean.parseBoolean(System.getenv(MULTIPLE_STORAGE_ACCOUNTS_KEY)) : false;	
+		
+	private static final String MAIN_STORAGE_ACCOUNT_KEY = "MAIN_STORAGE_ACCOUNT";
+	public static final String MAIN_STORAGE_ACOUNT = System.getenv(MAIN_STORAGE_ACCOUNT_KEY);
 	
+	
+	//redis table info
+	public static final String REDIS_STORAGE_ACCOUNT = "storageAccount";
+	public static final String REDIS_TIMESTAMP = "timestamp";
+*/
 	public static void reactorPushApp(Collection<String> appIds) {
 		if (ClusterUtil.IS_CLUSTER) {
 			for (String appId : appIds) {
 				reactorPushApp(appId);
 			}
 		}
+		
+		
 	}
 			
 	public static void reactorPushApp(String appId) {
 		if (ClusterUtil.IS_CLUSTER) {
 			try {
-				AZClient.getInstance().pushApp(appId);
+				CloudClient.getClient().pushApp(appId);
 			} catch (IOException | InterruptedException e) {
 				NounMetadata noun = new NounMetadata("Failed to push app to cloud storage", PixelDataType.CONST_STRING, PixelOperationType.ERROR);
 				SemossPixelException err = new SemossPixelException(noun);
@@ -44,7 +65,7 @@ public class ClusterUtil {
 	public static void reactorUpdateApp(String appId) {
 		if (ClusterUtil.IS_CLUSTER) {
 			try {
-				AZClient.getInstance().updateApp(appId);
+				CloudClient.getClient().updateApp(appId);
 			} catch (IOException | InterruptedException e) {
 				NounMetadata noun = new NounMetadata("Failed to update app from cloud storage", PixelDataType.CONST_STRING, PixelOperationType.ERROR);
 				SemossPixelException err = new SemossPixelException(noun);
