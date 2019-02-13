f650635e9947f75e3d2c7d043fb09aeccf090488
mahkhalil <mahkhalil@deloitte.com>
2019-02-13 18:31:10 +0000

    UPDATE ::: Modifying delete insight reactor to take in an app id and a list of insight ids
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32023 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/insights/save/DeleteInsightReactor.java
+++ src/prerna/sablecc2/reactor/insights/save/DeleteInsightReactor.java
@@ -1,14 +1,13 @@
 package prerna.sablecc2.reactor.insights.save;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.SecurityInsightUtils;
+import prerna.auth.utils.SecurityQueryUtils;
 import prerna.auth.utils.SecurityUpdateUtils;
 import prerna.cluster.util.ClusterUtil;
 import prerna.engine.api.IEngine;
 import prerna.engine.impl.InsightAdministrator;
+import prerna.nameserver.utility.MasterDatabaseUtility;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
@@ -20,33 +19,44 @@ import prerna.util.Utility;
 public class DeleteInsightReactor extends AbstractReactor {
 
 	public DeleteInsightReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.ID.getKey()};
+		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), ReactorKeysEnum.ID.getKey()};
 	}
 
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
-		GenRowStruct grs = this.store.getNoun(this.keysToGet[0]);
-		
+		GenRowStruct appGrs = this.store.getNoun(this.keysToGet[0]);
+		if(appGrs.isEmpty()) {
+			throw new IllegalArgumentException("Must define the app to delete the insights from");
+		}
+		String appId = appGrs.get(0).toString();
+		if(AbstractSecurityUtils.securityEnabled()) {
+			appId = SecurityQueryUtils.testUserEngineIdForAlias(this.insight.getUser(), appId);
+			if(!SecurityQueryUtils.userCanViewEngine(this.insight.getUser(), appId)) {
+				throw new IllegalArgumentException("App " + appId + " does not exist or user does not have access to database");
+			}
+		} else {
+			appId = MasterDatabaseUtility.testEngineIdIfAlias(appId);
+			if(!MasterDatabaseUtility.getAllEngineIds().contains(appId)) {
+				throw new IllegalArgumentException("App " + appId + " does not exist");
+			}
+		}
+		IEngine engine = Utility.getEngine(appId);
+		InsightAdministrator admin = new InsightAdministrator(engine.getInsightDatabase());
+
+		GenRowStruct grs = this.store.getNoun(this.keysToGet[1]);
 		int size = grs.size();
-		Set<String> appIds = new HashSet<>();
 		for (int i = 0; i < size; i++) {
 			// id is passed in from solr id where it is defined as engine_id
 			// so I need to split it
-			String id = grs.get(i).toString();
-			String[] split = id.split("_");
-			String appId = split[0];
-			String insightId = split[1];
-			
+			String insightId = grs.get(i).toString();
 			if(AbstractSecurityUtils.securityEnabled()) {
 				if(!SecurityInsightUtils.userCanEditInsight(this.insight.getUser(), appId, insightId)) {
 					throw new IllegalArgumentException("User does not have permission to edit this insight");
 				}
 			}
 			
-			IEngine engine = Utility.getEngine(appId);
 			// delete from insights database
-			InsightAdministrator admin = new InsightAdministrator(engine.getInsightDatabase());
 			try {
 				admin.dropInsight(insightId);
 			} catch (RuntimeException e) {
@@ -54,11 +64,8 @@ public class DeleteInsightReactor extends AbstractReactor {
 			}
 
 			SecurityUpdateUtils.deleteInsight(appId, insightId);
-			appIds.add(appId);
 		}
-		
-		ClusterUtil.reactorPushApp(appIds);
-		
+		ClusterUtil.reactorPushApp(appId);
 		return new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.DELETE_INSIGHT);
 	}
 
