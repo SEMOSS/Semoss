ba99fc4c926c2f77ec0661cf50d7a58b5a62071e
mahkhalil <mahkhalil@deloitte.com>
2019-02-13 18:12:01 +0000

    UPDATE ::: Adding checks for the conceptual name to be alpha numeric underscore
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32020 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 48839076f1e61f6f7272939571905f904e14329d

--- src/prerna/sablecc2/reactor/app/metaeditor/concepts/EditOwlPropertyConceptualNameReactor.java
+++ src/prerna/sablecc2/reactor/app/metaeditor/concepts/EditOwlPropertyConceptualNameReactor.java
@@ -39,6 +39,12 @@ public class EditOwlPropertyConceptualNameReactor extends AbstractMetaEditorReac
 		if(newConceptualName == null || newConceptualName.isEmpty()) {
 			throw new IllegalArgumentException("Must define the new conceptual name");
 		}
+		// make sure it conforms
+		if(!newConceptualName.matches("^[a-zA-Z0-9-_]+$")) {
+			throw new IllegalArgumentException("Conceptual name must contain only letters, numbers, and underscores");
+		}
+		newConceptualName = newConceptualName.replaceAll("_{2,}", "_");
+		
 		String newConceptualURI = "http://semoss.org/ontologies/Relation/Contains/" + newConceptualName + "/" + concept;
 
 		IEngine engine = Utility.getEngine(appId);
