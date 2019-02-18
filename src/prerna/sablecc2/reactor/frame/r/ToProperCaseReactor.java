5eb57140fd77995389822e57f15c19073e27f39e
mahkhalil <mahkhalil@deloitte.com>
2019-02-18 20:01:00 +0000

    UPDATE ::: Adding new Proper Case widget
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32069 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a13389f9f711378411befcf953807a79ae41380a

---
+++ src/prerna/sablecc2/reactor/frame/r/ToProperCaseReactor.java
@@ -0,0 +1,104 @@
+package prerna.sablecc2.reactor.frame.r;
+
+import java.util.List;
+import java.util.Vector;
+
+import prerna.ds.OwlTemporalEngineMeta;
+import prerna.ds.r.RDataTable;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.util.usertracking.AnalyticsTrackerHelper;
+import prerna.util.usertracking.UserTrackerFactory;
+
+public class ToProperCaseReactor extends AbstractRFrameReactor {
+
+	/**
+	 * This reactor changes columns to all upper case 
+	 * The inputs to the reactor are: 
+	 * 1) the columns to update
+	 */
+	
+	public ToProperCaseReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.COLUMNS.getKey() };
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		// initialize rJavaTranslator
+		init();
+		
+		this.rJavaTranslator.checkPackages(new String[] {"stringi"});
+		
+		StringBuilder builder = new StringBuilder();
+		builder.append("library(stringi);");
+		
+		// get frame
+		RDataTable frame = (RDataTable) getFrame();
+		OwlTemporalEngineMeta metaData = frame.getMetaData();
+
+		// get table name
+		String table = frame.getName();
+
+		// get inputs
+		List<String> columns = getColumns();
+		for (int i = 0; i < columns.size(); i++) {
+			String col = columns.get(i);
+			if (col.contains("__")) {
+				String[] split = col.split("__");
+				col = split[1];
+				table = split[0];
+			}
+			String dataType = metaData.getHeaderTypeAsString(table + "__" + col);
+			if (dataType.equalsIgnoreCase("STRING")) {
+				// define the script to be executed
+				builder.append(table + "$" + col + " <- stri_trans_totitle(" + table + "$" + col + ");");
+			}
+		}
+		
+		// execute the r script
+		// script will be of the form:
+		// FRAME$column <- stri_trans_totitle(FRAME$column)
+		this.rJavaTranslator.runR(builder.toString());
+		
+		// NEW TRACKING
+		UserTrackerFactory.getInstance().trackAnalyticsWidget(
+				this.insight, 
+				frame, 
+				"ToProper", 
+				AnalyticsTrackerHelper.getHashInputs(this.store, this.keysToGet));
+		
+		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE);
+	}
+	
+	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	///////////////////////// GET PIXEL INPUT ////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////
+
+	private List<String> getColumns() {
+		List<String> columns = new Vector<String>();
+
+		GenRowStruct colGrs = this.store.getNoun(this.keysToGet[0]);
+		if (colGrs != null && !colGrs.isEmpty()) {
+			for (int selectIndex = 0; selectIndex < colGrs.size(); selectIndex++) {
+				String column = colGrs.get(selectIndex) + "";
+				columns.add(column);
+			}
+		} else {
+			GenRowStruct inputsGRS = this.getCurRow();
+			// keep track of selectors to change to upper case
+			if (inputsGRS != null && !inputsGRS.isEmpty()) {
+				for (int selectIndex = 0; selectIndex < inputsGRS.size(); selectIndex++) {
+					String column = inputsGRS.get(selectIndex) + "";
+					columns.add(column);
+				}
+			}
+		}
+
+		return columns;
+	}
+}
