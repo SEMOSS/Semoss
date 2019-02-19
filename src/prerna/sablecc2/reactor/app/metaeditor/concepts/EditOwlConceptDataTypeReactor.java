443a9fa324d2a2173fb06ebbde7158497a3e570c
mahkhalil <mahkhalil@deloitte.com>
2019-02-19 14:27:38 +0000

    UPDATE ::: Cleaning up retrieval of data type given a physical uri UPDATE ::: Adding new reactors to modify an existing data type for a concept & property. Example usage: EditOwlConceptDataType(app=["c99e8a4d-ef3d-461b-b3f2-56ccb0e42470"], concept=["Nominated"], dataType=["VARCHAR(8000)"] ); EditOwlPropertyDataType(app=["c99e8a4d-ef3d-461b-b3f2-56ccb0e42470"], concept=["Title"], column=["Revenue_Domestic"], dataType=["DOUBLE"]);
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32082 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 22f8b88cc3c5381b416bd565e857d8346ee839e5

--- src/prerna/sablecc2/reactor/app/metaeditor/concepts/EditOwlConceptDataTypeReactor.java
+++ src/prerna/sablecc2/reactor/app/metaeditor/concepts/EditOwlConceptDataTypeReactor.java
@@ -1,6 +1,6 @@
 package prerna.sablecc2.reactor.app.metaeditor.concepts;
 
-import java.io.IOException;
+import org.openrdf.model.vocabulary.RDFS;
 
 import prerna.engine.api.IEngine;
 import prerna.engine.impl.rdf.RDFFileSesameEngine;
@@ -9,13 +9,12 @@ import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.app.metaeditor.AbstractMetaEditorReactor;
-import prerna.util.OWLER;
 import prerna.util.Utility;
 
-public class EditOwlConceptConceptualNameReactor extends AbstractMetaEditorReactor {
+public class EditOwlConceptDataTypeReactor extends AbstractMetaEditorReactor {
 
-	public EditOwlConceptConceptualNameReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), ReactorKeysEnum.CONCEPT.getKey(), CONCEPTUAL_NAME};
+	public EditOwlConceptDataTypeReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), ReactorKeysEnum.CONCEPT.getKey(), ReactorKeysEnum.DATA_TYPE.getKey()};
 	}
 
 	@Override
@@ -32,26 +31,15 @@ public class EditOwlConceptConceptualNameReactor extends AbstractMetaEditorReact
 			throw new IllegalArgumentException("Must define the concept being modified in the app metadata");
 		}
 		
-		String newConceptualName = this.keyValue.get(this.keysToGet[2]);
-		if(newConceptualName == null || newConceptualName.isEmpty()) {
-			throw new IllegalArgumentException("Must define the new conceptual name");
+		String newDataType = this.keyValue.get(this.keysToGet[2]);
+		if(newDataType == null || newDataType.isEmpty()) {
+			throw new IllegalArgumentException("Must define the new data type");
 		}
 		// make sure it conforms
-		newConceptualName = newConceptualName.trim();
-		if(!newConceptualName.matches("^[a-zA-Z0-9-_]+$")) {
-			throw new IllegalArgumentException("Conceptual name must contain only letters, numbers, and underscores");
-		}
-		newConceptualName = newConceptualName.replaceAll("_{2,}", "_");
+		newDataType = newDataType.trim();
 		
-		String newConceptualURI = "http://semoss.org/ontologies/Concept/" + newConceptualName;
-
 		IEngine engine = Utility.getEngine(appId);
 		RDFFileSesameEngine owlEngine = engine.getBaseDataEngine();
-		
-		// make sure this name isn't currently present in the engine
-		if(engine.getConceptPhysicalUriFromConceptualUri(newConceptualURI) != null) {
-			throw new IllegalArgumentException("This conceptual name already exists");
-		}
 		
 		String conceptualURI = "http://semoss.org/ontologies/Concept/" + concept;
 		String conceptPhysicalURI = engine.getConceptPhysicalUriFromConceptualUri(conceptualURI);
@@ -59,11 +47,13 @@ public class EditOwlConceptConceptualNameReactor extends AbstractMetaEditorReact
 			throw new IllegalArgumentException("Could not find the concept. Please define the concept first before modifying the conceptual name");
 		}
 		
-		String conceptualRel = OWLER.SEMOSS_URI + OWLER.DEFAULT_RELATION_CLASS + "/" + OWLER.CONCEPTUAL_RELATION_NAME;
+		// get the current data type
+		String curDataType = engine.getDataTypes(conceptPhysicalURI);
+		
 		// remove the current relationship
-		owlEngine.doAction(IEngine.ACTION_TYPE.REMOVE_STATEMENT, new Object[]{conceptPhysicalURI, conceptualRel, conceptualURI, true});
+		owlEngine.doAction(IEngine.ACTION_TYPE.REMOVE_STATEMENT, new Object[]{conceptPhysicalURI, RDFS.CLASS.stringValue(), curDataType, true});
 		// add the new relationship
-		owlEngine.doAction(IEngine.ACTION_TYPE.ADD_STATEMENT, new Object[]{conceptPhysicalURI, conceptualRel, newConceptualURI, true});
+		owlEngine.doAction(IEngine.ACTION_TYPE.ADD_STATEMENT, new Object[]{conceptPhysicalURI, RDFS.CLASS.stringValue(), "TYPE:" + newDataType, true});
 		
 		try {
 			owlEngine.exportDB();
@@ -75,7 +65,7 @@ public class EditOwlConceptConceptualNameReactor extends AbstractMetaEditorReact
 		}
 		
 		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
-		noun.addAdditionalReturn(new NounMetadata("Successfully edited concept name from " + concept + " to " + newConceptualName, PixelDataType.CONST_STRING, PixelOperationType.SUCCESS));
+		noun.addAdditionalReturn(new NounMetadata("Successfully edited data type of " + concept + " to " + newDataType, PixelDataType.CONST_STRING, PixelOperationType.SUCCESS));
 		return noun;
 	}
 
