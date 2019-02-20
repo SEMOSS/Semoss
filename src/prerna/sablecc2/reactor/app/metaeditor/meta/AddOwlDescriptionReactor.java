6c4daa713567fbc8b701fbbeb94ed2d0f87fd902
mahkhalil <mahkhalil@deloitte.com>
2019-02-20 22:20:03 +0000

    UPDATE ::: Allowing to add description + logical names when a new concept/property is defined
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32115 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 104d5a64eb418a6a870808b9ebbe51bb39da005b

--- src/prerna/sablecc2/reactor/app/metaeditor/meta/AddOwlDescriptionReactor.java
+++ src/prerna/sablecc2/reactor/app/metaeditor/meta/AddOwlDescriptionReactor.java
@@ -3,7 +3,6 @@ package prerna.sablecc2.reactor.app.metaeditor.meta;
 import java.io.IOException;
 
 import prerna.engine.api.IEngine;
-import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
@@ -20,13 +19,14 @@ public class AddOwlDescriptionReactor extends AbstractMetaEditorReactor {
 	
 	@Override
 	public NounMetadata execute() {
-		String appId = getAppId();
+		organizeKeys();
+		String appId = this.keyValue.get(this.keysToGet[0]);
 		// we may have an alias
 		appId = getAppId(appId, true);
 		
-		String concept = getConcept();
-		String prop = getProperty();
-		String[] descriptions = getDescriptions();
+		String concept = this.keyValue.get(this.keysToGet[1]);
+		String prop = this.keyValue.get(this.keysToGet[2]);
+		String description = this.keyValue.get(this.keysToGet[3]);
 		
 		OWLER owler = getOWLER(appId);
 		// set all the existing values into the OWLER
@@ -35,9 +35,9 @@ public class AddOwlDescriptionReactor extends AbstractMetaEditorReactor {
 		setOwlerValues(engine, owler);
 		if(prop == null || prop.isEmpty()) {
 			String physicalUri = engine.getConceptPhysicalUriFromConceptualUri(concept);
-			owler.addConceptDescription(concept, Utility.getClassName(physicalUri), descriptions);
+			owler.addConceptDescription(concept, Utility.getClassName(physicalUri), description);
 		} else {
-			owler.addPropDescription(concept, prop, descriptions);
+			owler.addPropDescription(concept, prop, description);
 		}
 		owler.commit();
 		
@@ -46,72 +46,14 @@ public class AddOwlDescriptionReactor extends AbstractMetaEditorReactor {
 		} catch (IOException e) {
 			e.printStackTrace();
 			NounMetadata noun = new NounMetadata(false, PixelDataType.BOOLEAN);
-//			noun.addAdditionalReturn(new NounMetadata("An error occured attempting to add descriptions : " + Arrays.toString(descriptions),
 			noun.addAdditionalReturn(new NounMetadata("An error occured attempting to add description", 
 					PixelDataType.CONST_STRING, PixelOperationType.ERROR));
 			return noun;
 		}
 		
 		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
-//		noun.addAdditionalReturn(new NounMetadata("Successfully added descriptions : " + Arrays.toString(descriptions), 
 		noun.addAdditionalReturn(new NounMetadata("Successfully added descriptions",
 				PixelDataType.CONST_STRING, PixelOperationType.SUCCESS));
 		return noun;
-	}
-
-	
-	///////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////
-	///////////// GRAB INPUTS FROM PIXEL REACTOR //////////////
-	///////////////////////////////////////////////////////////
-	///////////////////////////////////////////////////////////
-
-	private String getAppId() {
-		GenRowStruct grs = this.store.getNoun(keysToGet[0]);
-		if (grs != null && !grs.isEmpty()) {
-			String appId = (String) grs.get(0);
-			if (appId != null && !appId.isEmpty()) {
-				return appId;
-			}
-		}
-		throw new IllegalArgumentException("Need to define " + keysToGet[0]);
-	}
-
-	private String getConcept() {
-		GenRowStruct grs = this.store.getNoun(keysToGet[1]);
-		if (grs != null && !grs.isEmpty()) {
-			String concept = (String) grs.get(0);
-			if (concept != null && !concept.isEmpty()) {
-				return concept;
-			}
-		}
-		throw new IllegalArgumentException("Need to define " + keysToGet[1]);
-	}
-	
-	private String getProperty() {
-		GenRowStruct grs = this.store.getNoun(keysToGet[2]);
-		if (grs != null && !grs.isEmpty()) {
-			String prop = (String) grs.get(0);
-			if (prop != null && !prop.isEmpty()) {
-				return prop;
-			}
-		}
-
-		return "";
-	}
-
-	private String[] getDescriptions() {
-		String[] logicalNames = null;
-		GenRowStruct grs = this.store.getNoun(keysToGet[3]);
-		if (grs != null && !grs.isEmpty()) {
-			logicalNames = new String[grs.size()];
-			for (int i = 0; i < grs.size(); i++) {
-				String name = (String) grs.get(i);
-				if (name != null && !name.isEmpty()) {
-					logicalNames[i] = name;
-				}
-			}
-		}
-		return logicalNames;
 	}
 }
