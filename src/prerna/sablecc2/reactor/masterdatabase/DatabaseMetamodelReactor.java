ca7ec24455930e39b6ad9f99143bc9107beb915e
mahkhalil <mahkhalil@deloitte.com>
2019-02-20 17:08:40 +0000

    UPDATE ::: Expanding DatabaseMetamodelReactor to take in options for additional metadata pieces
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32101 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 01ad9b1ed36aac1d6881d56761dcfd2818026a43

--- src/prerna/sablecc2/reactor/masterdatabase/DatabaseMetamodelReactor.java
+++ src/prerna/sablecc2/reactor/masterdatabase/DatabaseMetamodelReactor.java
@@ -1,6 +1,9 @@
 package prerna.sablecc2.reactor.masterdatabase;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Vector;
+import java.util.stream.Collectors;
 
 import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.SecurityQueryUtils;
@@ -14,24 +17,19 @@ import prerna.sablecc2.reactor.AbstractReactor;
 
 public class DatabaseMetamodelReactor extends AbstractReactor {
 	
+	/*
+	 * Get the database metamodel + meta options
+	 * OPTIONS include datatypes, logicalnames, descriptions
+	 */
+	
 	public DatabaseMetamodelReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.DATABASE.getKey()};
+		this.keysToGet = new String[]{ReactorKeysEnum.DATABASE.getKey(), ReactorKeysEnum.OPTIONS.getKey()};
 	}
 
 	@Override
 	public NounMetadata execute() {
-		GenRowStruct eGrs = this.store.getNoun(keysToGet[0]);
-		if(eGrs == null) {
-			throw new IllegalArgumentException("Need to define the database to get the concepts from");
-		}
-		if(eGrs.isEmpty()) {
-			throw new IllegalArgumentException("Must define a single database");
-		}
-		if(eGrs.size() > 1) {
-			throw new IllegalArgumentException("Can only define one database within this call");
-		}
-		
-		String engineId = MasterDatabaseUtility.testEngineIdIfAlias(eGrs.get(0).toString());
+		String engineId = MasterDatabaseUtility.testEngineIdIfAlias(getDatabase());
+		List<String> options = getOptions();
 		
 		// account for security
 		// TODO: THIS WILL NEED TO ACCOUNT FOR COLUMNS AS WELL!!!	
@@ -41,8 +39,40 @@ public class DatabaseMetamodelReactor extends AbstractReactor {
 			}
 		}
 		
-		Map<String, Object> metamodelObject = MasterDatabaseUtility.getMetamodelRDBMS(engineId);
+		boolean includeDataTypes = options.contains("datatype");
+		Map<String, Object> metamodelObject = MasterDatabaseUtility.getMetamodelRDBMS(engineId, includeDataTypes);
+		
+		
 		return new NounMetadata(metamodelObject, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.DATABASE_METAMODEL);
 	}
+	
+	private String getDatabase() {
+		GenRowStruct eGrs = this.store.getNoun(this.keysToGet[0]);
+		if(eGrs != null && !eGrs.isEmpty()) {
+			if(eGrs.size() > 1) {
+				throw new IllegalArgumentException("Can only define one database within this call");
+			}
+			return eGrs.get(0).toString();
+		}
+		
+		if(this.curRow.isEmpty()) {
+			throw new IllegalArgumentException("Need to define the database to get the concepts from");
+		}
+		
+		return this.curRow.get(0).toString();
+	}
+	
+	private List<String> getOptions() {
+		GenRowStruct grs = this.store.getNoun(this.keysToGet[1]);
+		if(grs != null && !grs.isEmpty()) {
+			return grs.getAllStrValues().stream().map(p -> p.toLowerCase()).collect(Collectors.toList());
+		}
+		
+		List<String> options = new Vector<String>();
+		for(int i = 1; i < this.curRow.size(); i++) {
+			options.add(this.curRow.get(i).toString().toLowerCase());
+		}
+		return options;
+ 	}
 
 }
