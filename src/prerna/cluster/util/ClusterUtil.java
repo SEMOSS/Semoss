3b0962b8a179c0348a4911e63a3c874089045cb4
tbanach <tbanach@deloitte.com>
2019-02-21 15:25:15 +0000

    UPDATE ::: Cleaning up cluster util formatting BUGFIX ::: Renaming translator for R fix
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32123 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b42860631912e40c176e36434d71ee5f2ce30490

--- src/prerna/cluster/util/ClusterUtil.java
+++ src/prerna/cluster/util/ClusterUtil.java
@@ -11,72 +11,80 @@ import prerna.sablecc2.om.nounmeta.NounMetadata;
 public class ClusterUtil {
 
 	// Env vars used in clustered deployments
-	// TODO >>>timb: make sure that everything cluster related starts with this, also introduces tracibility
+	// TODO >>>timb: make sure that everything cluster related starts with this,
+	// also introduces tracibility
 	private static final String IS_CLUSTER_KEY = "SEMOSS_IS_CLUSTER";
 	public static final boolean IS_CLUSTER = System.getenv().containsKey(IS_CLUSTER_KEY)
-			? Boolean.parseBoolean(System.getenv(IS_CLUSTER_KEY)) : false;
+			? Boolean.parseBoolean(System.getenv(IS_CLUSTER_KEY))
+			: false;
 
-			private static final String STORAGE_PROVIDER_KEY = "SEMOSS_STORAGE_PROVIDER";
-			public static final String STORAGE_PROVIDER = System.getenv(STORAGE_PROVIDER_KEY);
+	private static final String STORAGE_PROVIDER_KEY = "SEMOSS_STORAGE_PROVIDER";
+	public static final String STORAGE_PROVIDER = System.getenv(STORAGE_PROVIDER_KEY);
 
-			private static final String REMOTE_RSERVE_KEY = "REMOTE_RSERVE";
-			public static final boolean REMOTE_RSERVE = System.getenv().containsKey(REMOTE_RSERVE_KEY)
-					? Boolean.parseBoolean(System.getenv(REMOTE_RSERVE_KEY)) : false;
+	private static final String REMOTE_RSERVE_KEY = "REMOTE_RSERVE";
+	public static final boolean REMOTE_RSERVE = System.getenv().containsKey(REMOTE_RSERVE_KEY)
+			? Boolean.parseBoolean(System.getenv(REMOTE_RSERVE_KEY))
+			: false;
 
-					private static final String SEMOSS_USER_RSERVE_KEY = "SEMOSS_USER_RSERVE";
-					public static final boolean SEMOSS_USER_RSERVE = System.getenv().containsKey(SEMOSS_USER_RSERVE_KEY)
-							? Boolean.parseBoolean(System.getenv(SEMOSS_USER_RSERVE_KEY)) : false;
+	private static final String SEMOSS_USER_RSERVE_KEY = "SEMOSS_USER_RSERVE";
+	public static final boolean SEMOSS_USER_RSERVE = System.getenv().containsKey(SEMOSS_USER_RSERVE_KEY)
+			? Boolean.parseBoolean(System.getenv(SEMOSS_USER_RSERVE_KEY))
+			: false;
 
-							private static final String LOAD_ENGINES_LOCALLY_KEY = "SEMOSS_LOAD_ENGINES_LOCALLY";
-							public static final boolean LOAD_ENGINES_LOCALLY = System.getenv().containsKey(LOAD_ENGINES_LOCALLY_KEY)
-									? Boolean.parseBoolean(System.getenv(LOAD_ENGINES_LOCALLY_KEY)) : true;
-	
+	private static final String LOAD_ENGINES_LOCALLY_KEY = "SEMOSS_LOAD_ENGINES_LOCALLY";
+	public static final boolean LOAD_ENGINES_LOCALLY = System.getenv().containsKey(LOAD_ENGINES_LOCALLY_KEY)
+			? Boolean.parseBoolean(System.getenv(LOAD_ENGINES_LOCALLY_KEY))
+			: true;
+
 	/*
-	private static final String MULTIPLE_STORAGE_ACCOUNTS_KEY = "MULTIPLE_STORAGE_ACCOUNTS";
-	public static final boolean MULTIPLE_STORAGE_ACCOUNTS = System.getenv().containsKey(MULTIPLE_STORAGE_ACCOUNTS_KEY)
-					? Boolean.parseBoolean(System.getenv(MULTIPLE_STORAGE_ACCOUNTS_KEY)) : false;	
+	 * private static final String MULTIPLE_STORAGE_ACCOUNTS_KEY =
+	 * "MULTIPLE_STORAGE_ACCOUNTS"; public static final boolean
+	 * MULTIPLE_STORAGE_ACCOUNTS =
+	 * System.getenv().containsKey(MULTIPLE_STORAGE_ACCOUNTS_KEY) ?
+	 * Boolean.parseBoolean(System.getenv(MULTIPLE_STORAGE_ACCOUNTS_KEY)) : false;
+	 * 
+	 * private static final String MAIN_STORAGE_ACCOUNT_KEY =
+	 * "MAIN_STORAGE_ACCOUNT"; public static final String MAIN_STORAGE_ACOUNT =
+	 * System.getenv(MAIN_STORAGE_ACCOUNT_KEY);
+	 * 
+	 * 
+	 * //redis table info public static final String REDIS_STORAGE_ACCOUNT =
+	 * "storageAccount"; public static final String REDIS_TIMESTAMP = "timestamp";
+	 */
+	public static void reactorPushApp(Collection<String> appIds) {
+		if (ClusterUtil.IS_CLUSTER) {
+			for (String appId : appIds) {
+				reactorPushApp(appId);
+			}
+		}
 
-	private static final String MAIN_STORAGE_ACCOUNT_KEY = "MAIN_STORAGE_ACCOUNT";
-	public static final String MAIN_STORAGE_ACOUNT = System.getenv(MAIN_STORAGE_ACCOUNT_KEY);
+	}
 
+	public static void reactorPushApp(String appId) {
+		if (ClusterUtil.IS_CLUSTER) {
+			try {
+				CloudClient.getClient().pushApp(appId);
+			} catch (IOException | InterruptedException e) {
+				NounMetadata noun = new NounMetadata("Failed to push app to cloud storage", PixelDataType.CONST_STRING,
+						PixelOperationType.ERROR);
+				SemossPixelException err = new SemossPixelException(noun);
+				err.setContinueThreadOfExecution(false);
+				throw err;
+			}
+		}
+	}
 
-	//redis table info
-	public static final String REDIS_STORAGE_ACCOUNT = "storageAccount";
-	public static final String REDIS_TIMESTAMP = "timestamp";
-									 */
-									public static void reactorPushApp(Collection<String> appIds) {
-										if (ClusterUtil.IS_CLUSTER) {
-											for (String appId : appIds) {
-												reactorPushApp(appId);
-											}
-										}
-
-
-									}
-
-									public static void reactorPushApp(String appId) {
-										if (ClusterUtil.IS_CLUSTER) {
-											try {
-												CloudClient.getClient().pushApp(appId);
-											} catch (IOException | InterruptedException e) {
-												NounMetadata noun = new NounMetadata("Failed to push app to cloud storage", PixelDataType.CONST_STRING, PixelOperationType.ERROR);
-												SemossPixelException err = new SemossPixelException(noun);
-												err.setContinueThreadOfExecution(false);
-												throw err;
-											}
-										}
-									}
-
-									public static void reactorUpdateApp(String appId) {
-										if (ClusterUtil.IS_CLUSTER) {
-											try {
-												CloudClient.getClient().updateApp(appId);
-											} catch (IOException | InterruptedException e) {
-												NounMetadata noun = new NounMetadata("Failed to update app from cloud storage", PixelDataType.CONST_STRING, PixelOperationType.ERROR);
-												SemossPixelException err = new SemossPixelException(noun);
-												err.setContinueThreadOfExecution(false);
-												throw err;
-											}
-										}
-									}
+	public static void reactorUpdateApp(String appId) {
+		if (ClusterUtil.IS_CLUSTER) {
+			try {
+				CloudClient.getClient().updateApp(appId);
+			} catch (IOException | InterruptedException e) {
+				NounMetadata noun = new NounMetadata("Failed to update app from cloud storage",
+						PixelDataType.CONST_STRING, PixelOperationType.ERROR);
+				SemossPixelException err = new SemossPixelException(noun);
+				err.setContinueThreadOfExecution(false);
+				throw err;
+			}
+		}
+	}
 }
