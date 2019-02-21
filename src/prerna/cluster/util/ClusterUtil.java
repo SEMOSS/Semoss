c418d3cb962c42a0d1fbfc3aa904b97627477da5
kunalppatel9 <kunalppatel9@deloitte.com>
2019-02-21 15:20:58 +0000

    ADDED:::RJavaUserRserve - Initial Commit to allow user level Rserve connections
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32122 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 4101b66f7500129fb17d5e060e5e0a0d25c9f6f8

--- src/prerna/cluster/util/ClusterUtil.java
+++ src/prerna/cluster/util/ClusterUtil.java
@@ -15,63 +15,68 @@ public class ClusterUtil {
 	private static final String IS_CLUSTER_KEY = "SEMOSS_IS_CLUSTER";
 	public static final boolean IS_CLUSTER = System.getenv().containsKey(IS_CLUSTER_KEY)
 			? Boolean.parseBoolean(System.getenv(IS_CLUSTER_KEY)) : false;
-	
+
 			private static final String STORAGE_PROVIDER_KEY = "SEMOSS_STORAGE_PROVIDER";
 			public static final String STORAGE_PROVIDER = System.getenv(STORAGE_PROVIDER_KEY);
 
 			private static final String REMOTE_RSERVE_KEY = "REMOTE_RSERVE";
 			public static final boolean REMOTE_RSERVE = System.getenv().containsKey(REMOTE_RSERVE_KEY)
 					? Boolean.parseBoolean(System.getenv(REMOTE_RSERVE_KEY)) : false;
-				
-	private static final String LOAD_ENGINES_LOCALLY_KEY = "SEMOSS_LOAD_ENGINES_LOCALLY";
-	public static final boolean LOAD_ENGINES_LOCALLY = System.getenv().containsKey(LOAD_ENGINES_LOCALLY_KEY)
-			? Boolean.parseBoolean(System.getenv(LOAD_ENGINES_LOCALLY_KEY)) : true;
+
+					private static final String SEMOSS_USER_RSERVE_KEY = "SEMOSS_USER_RSERVE";
+					public static final boolean SEMOSS_USER_RSERVE = System.getenv().containsKey(SEMOSS_USER_RSERVE_KEY)
+							? Boolean.parseBoolean(System.getenv(SEMOSS_USER_RSERVE_KEY)) : false;
+
+							private static final String LOAD_ENGINES_LOCALLY_KEY = "SEMOSS_LOAD_ENGINES_LOCALLY";
+							public static final boolean LOAD_ENGINES_LOCALLY = System.getenv().containsKey(LOAD_ENGINES_LOCALLY_KEY)
+									? Boolean.parseBoolean(System.getenv(LOAD_ENGINES_LOCALLY_KEY)) : true;
+	
 	/*
 	private static final String MULTIPLE_STORAGE_ACCOUNTS_KEY = "MULTIPLE_STORAGE_ACCOUNTS";
 	public static final boolean MULTIPLE_STORAGE_ACCOUNTS = System.getenv().containsKey(MULTIPLE_STORAGE_ACCOUNTS_KEY)
 					? Boolean.parseBoolean(System.getenv(MULTIPLE_STORAGE_ACCOUNTS_KEY)) : false;	
-		
+
 	private static final String MAIN_STORAGE_ACCOUNT_KEY = "MAIN_STORAGE_ACCOUNT";
 	public static final String MAIN_STORAGE_ACOUNT = System.getenv(MAIN_STORAGE_ACCOUNT_KEY);
-	
-	
+
+
 	//redis table info
 	public static final String REDIS_STORAGE_ACCOUNT = "storageAccount";
 	public static final String REDIS_TIMESTAMP = "timestamp";
-*/
-	public static void reactorPushApp(Collection<String> appIds) {
-		if (ClusterUtil.IS_CLUSTER) {
-			for (String appId : appIds) {
-				reactorPushApp(appId);
-			}
-		}
-		
-		
-	}
-			
-	public static void reactorPushApp(String appId) {
-		if (ClusterUtil.IS_CLUSTER) {
-			try {
-				CloudClient.getClient().pushApp(appId);
-			} catch (IOException | InterruptedException e) {
-				NounMetadata noun = new NounMetadata("Failed to push app to cloud storage", PixelDataType.CONST_STRING, PixelOperationType.ERROR);
-				SemossPixelException err = new SemossPixelException(noun);
-				err.setContinueThreadOfExecution(false);
-				throw err;
-			}
-		}
-	}
-	
-	public static void reactorUpdateApp(String appId) {
-		if (ClusterUtil.IS_CLUSTER) {
-			try {
-				CloudClient.getClient().updateApp(appId);
-			} catch (IOException | InterruptedException e) {
-				NounMetadata noun = new NounMetadata("Failed to update app from cloud storage", PixelDataType.CONST_STRING, PixelOperationType.ERROR);
-				SemossPixelException err = new SemossPixelException(noun);
-				err.setContinueThreadOfExecution(false);
-				throw err;
-			}
-		}
-	}
+									 */
+									public static void reactorPushApp(Collection<String> appIds) {
+										if (ClusterUtil.IS_CLUSTER) {
+											for (String appId : appIds) {
+												reactorPushApp(appId);
+											}
+										}
+
+
+									}
+
+									public static void reactorPushApp(String appId) {
+										if (ClusterUtil.IS_CLUSTER) {
+											try {
+												CloudClient.getClient().pushApp(appId);
+											} catch (IOException | InterruptedException e) {
+												NounMetadata noun = new NounMetadata("Failed to push app to cloud storage", PixelDataType.CONST_STRING, PixelOperationType.ERROR);
+												SemossPixelException err = new SemossPixelException(noun);
+												err.setContinueThreadOfExecution(false);
+												throw err;
+											}
+										}
+									}
+
+									public static void reactorUpdateApp(String appId) {
+										if (ClusterUtil.IS_CLUSTER) {
+											try {
+												CloudClient.getClient().updateApp(appId);
+											} catch (IOException | InterruptedException e) {
+												NounMetadata noun = new NounMetadata("Failed to update app from cloud storage", PixelDataType.CONST_STRING, PixelOperationType.ERROR);
+												SemossPixelException err = new SemossPixelException(noun);
+												err.setContinueThreadOfExecution(false);
+												throw err;
+											}
+										}
+									}
 }
---
+++ src/prerna/sablecc2/reactor/frame/r/util/RJavaUserRserve.java
@@ -0,0 +1,136 @@
+package prerna.sablecc2.reactor.frame.r.util;
+
+import java.util.List;
+import java.util.Map;
+
+import org.rosuda.REngine.Rserve.RConnection;
+
+public class RJavaUserRserve extends AbstractRJavaTranslator{
+
+	@Override
+	public void initREnv() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void startR() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public Object executeR(String rScript) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void executeEmptyR(String rScript) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getString(String script) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getStringArray(String script) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getInt(String script) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int[] getIntArray(String rScript) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getDouble(String rScript) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double[] getDoubleArray(String rScript) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double[][] getDoubleMatrix(String rScript) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean getBoolean(String rScript) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object getFactor(String rScript) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setConnection(RConnection connection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setPort(String port) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endR() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stopRProcess() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Map<String, Object> getHistogramBreaksAndCounts(String script) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> flushFrameAsTable(String framename, String[] colNames) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] getDataRow(String rScript, String[] headerOrdering) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Object[]> getBulkDataRow(String rScript, String[] headerOrdering) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+
+}
