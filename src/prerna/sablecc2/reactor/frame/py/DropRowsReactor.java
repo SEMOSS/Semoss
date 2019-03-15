9ded1c3b3611c4e0df8331a9700d389f647ee481
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2019-03-15 03:06:42 +0000

    Base Python Cleanup routines. Please make sure you have these libraries installed in PYTHON pandas numpy string fuzzywuzzy random datetime annoy Also Make sure you are storing PYTHON_PACKAGES on RDF_MAP to whichever your site packages folder is c:/python/python36/Lib/site-packages. Remaining reactors are one for one with R.
    Pushing JepTest as well. It has been very useful to find return types.
    
    
    
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32390 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c098a8270f43a6cd6cb2edd65fa09c6e63a0753e

---
+++ src/prerna/sablecc2/reactor/frame/py/DropRowsReactor.java
@@ -0,0 +1,82 @@
+package prerna.sablecc2.reactor.frame.py;
+
+import prerna.ds.py.PandasFrame;
+import prerna.query.interpreters.PandasInterpreter;
+import prerna.query.querystruct.SelectQueryStruct;
+import prerna.query.querystruct.filters.GenRowFilters;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.frame.AbstractFrameReactor;
+import prerna.util.usertracking.AnalyticsTrackerHelper;
+import prerna.util.usertracking.UserTrackerFactory;
+
+public class DropRowsReactor extends AbstractFrameReactor {
+
+	/**
+	 * This reactor drops rows based on a comparison The inputs to the reactor
+	 * are: 1) the filter comparison for dropping rows
+	 */
+
+	public DropRowsReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.QUERY_STRUCT.getKey() };
+	}
+
+	@Override
+	public NounMetadata execute() {
+		// get frame
+		PandasFrame frame = (PandasFrame) getFrame();
+		String table = frame.getName();
+
+		// the first noun will be a query struct - the filter
+		SelectQueryStruct qs = getQueryStruct();
+		// get the filters from the query struct
+		// and iterate through each filtered column
+		GenRowFilters grf = qs.getExplicitFilters();
+
+		// use RInterpreter to create filter syntax
+		StringBuilder pyFilterBuilder = new StringBuilder();
+		PandasInterpreter pi = new PandasInterpreter();
+		pi.setDataTableName(table);
+		pi.setDataTypeMap(frame.getMetaData().getHeaderToTypeMap());
+		pi.addFilters(grf.getFilters(), table, pyFilterBuilder, true);
+
+		// execute the r script
+		// FRAME <- FRAME[!( FRAME$Director == "value"),]
+		String newScript = table + " =  " + table + "[~" + pyFilterBuilder.toString() + "]";
+		frame.runScript(newScript);
+
+		// NEW TRACKING
+		UserTrackerFactory.getInstance().trackAnalyticsWidget(this.insight, frame, "DropRows",
+				AnalyticsTrackerHelper.getHashInputs(this.store, this.keysToGet));
+
+		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE);
+	}
+
+	private SelectQueryStruct getQueryStruct() {
+		GenRowStruct inputsGRS = this.store.getNoun(this.keysToGet[0]);
+		if (inputsGRS != null) {
+			NounMetadata filterNoun = inputsGRS.getNoun(0);
+			// filter is query struct pksl type
+			// the qs is the value of the filterNoun
+			SelectQueryStruct qs = (SelectQueryStruct) filterNoun.getValue();
+			if (qs == null) {
+				throw new IllegalArgumentException("Need to define filter condition");
+			}
+			return qs;
+		}
+
+		inputsGRS = this.getCurRow();
+		NounMetadata filterNoun = inputsGRS.getNoun(0);
+		// filter is query struct pksl type
+		// the qs is the value of the filterNoun
+		SelectQueryStruct qs = (SelectQueryStruct) filterNoun.getValue();
+		if (qs == null) {
+			throw new IllegalArgumentException("Need to define filter condition");
+		}
+		return qs;
+	}
+
+}
---
+++ src/prerna/sablecc2/reactor/frame/py/DuplicateColumnReactor.java
@@ -0,0 +1,97 @@
+package prerna.sablecc2.reactor.frame.py;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import prerna.ds.OwlTemporalEngineMeta;
+import prerna.ds.py.PandasFrame;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.AddHeaderNounMetadata;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.frame.AbstractFrameReactor;
+import prerna.util.usertracking.AnalyticsTrackerHelper;
+import prerna.util.usertracking.UserTrackerFactory;
+
+public class DuplicateColumnReactor extends AbstractFrameReactor {
+
+	/**
+	 * This reactor duplicates and existing column and adds it to the frame. The
+	 * inputs to the reactor are: 
+	 * 1) the name for the column to duplicate 
+	 * 2) the new column name
+	 */
+
+	public DuplicateColumnReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.COLUMN.getKey(), ReactorKeysEnum.NEW_COLUMN.getKey() };
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+
+		// get frame
+		PandasFrame frame = (PandasFrame) getFrame();
+		String table = frame.getName();
+
+		// get source column to duplicate
+		String srcCol = this.keyValue.get(this.keysToGet[0]);
+
+		// make sure source column exists
+		String[] allCol = getColumns(frame);
+		if (srcCol == null || !Arrays.asList(allCol).contains(srcCol)) {
+			throw new IllegalArgumentException("Need to define an existing column to duplicate.");
+		}
+
+		// clean and validate new column name or use default name
+		String newColName = getCleanNewColName(table, srcCol + "_DUPLICATE");
+		String inputColName = this.keyValue.get(this.keysToGet[1]);
+		if (inputColName != null && !inputColName.isEmpty()) {
+			inputColName = getCleanNewColName(table, inputColName);
+			// entire new name could be invalid characters
+			if (!inputColName.equals("")) {
+				newColName = inputColName;
+			}
+		}
+
+		// run duplicate script
+		frame.runScript(table + "w.dupecol('" + srcCol + "', '" + newColName + "')");
+
+		// get src column data type
+		OwlTemporalEngineMeta metaData = frame.getMetaData();
+		String dataType = metaData.getHeaderTypeAsString(table + "__" + srcCol);
+
+		// update meta data
+		metaData.addProperty(table, table + "__" + newColName);
+		metaData.setAliasToProperty(table + "__" + newColName, newColName);
+		metaData.setDataTypeToProperty(table + "__" + newColName, dataType);
+
+		// NEW TRACKING
+		UserTrackerFactory.getInstance().trackAnalyticsWidget(
+				this.insight, 
+				frame, 
+				"DuplicateColumn", 
+				AnalyticsTrackerHelper.getHashInputs(this.store, this.keysToGet));
+		
+		NounMetadata retNoun = new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_HEADERS_CHANGE, PixelOperationType.FRAME_DATA_CHANGE);
+		retNoun.addAdditionalReturn(new AddHeaderNounMetadata(newColName));
+		return retNoun;
+	}
+	
+	public String [] getColumns(PandasFrame frame)
+	{
+		
+		String frameName = frame.getName();
+		// get jep thread and get the names
+		ArrayList <String> val = (ArrayList<String>)frame.runScript("list(" + frameName + ")");
+		String [] retString = new String[val.size()];
+		
+		val.toArray(retString);
+		
+		return retString;
+		
+		
+	}
+
+}
