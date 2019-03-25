326dc524dd45421bd85a68ad98855ace2bddc822
tbanach <tbanach@deloitte.com>
2019-03-25 15:33:07 +0000

    UPDATE ::: Moving methods to register workspace and asset apps into auth/utils/WorkspaceAssetUtils and deleting duplicated methods in SecurityQueryUtils and SecurityUpdateUtils. UPDATE ::: Adding logic into UserResource to check whether these apps exists on login. If not, then make them
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32480 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b726b82158ff0e9fa6447a8dbae93bf1cc9974a0

--- src/prerna/auth/utils/WorkspaceAssetUtils.java
+++ src/prerna/auth/utils/WorkspaceAssetUtils.java
@@ -1,50 +1,155 @@
 package prerna.auth.utils;
 
+import java.sql.SQLException;
+
+import prerna.auth.AccessToken;
 import prerna.auth.AuthProvider;
 import prerna.auth.User;
+import prerna.ds.util.RdbmsQueryBuilder;
 import prerna.engine.api.IRawSelectWrapper;
 import prerna.rdf.engine.wrappers.WrapperManager;
 
 public class WorkspaceAssetUtils extends AbstractSecurityUtils {
-
+	
 	WorkspaceAssetUtils() {
 		super();
 	}
 	
+	
+	//////////////////////////////////////////////////////////////////////
+	// Creating workspace and asset metadata 
+	//////////////////////////////////////////////////////////////////////
+	
 	/**
-	 * Create the default user workspace app for the provided auth token
-	 * @param user
+	 * Create the user workspace app for the provided access token
 	 * @param token
+	 * @return
 	 */
-	public static void createUserWorkspaceApp(User user, AuthProvider token) {
-		// look at GenerateEmptyAppReactor
-		
+	public static String createUserWorkspaceApp(AccessToken token) {
+		String appId = null;
+		// TODO >>>timb: WORKSPACE - look at GenerateEmptyAppReactor, use AppEngine
+		registerUserWorkspaceApp(token, appId);
+		return null;
 	}
 	
 	/**
-	 * Create the default user asset app for the provided auth token
-	 * @param user
-	 * @param token
-	 */
-	public static void createUserAssetApp(User user, AuthProvider token) {
-		// look at GenerateEmptyAppReactor
-
-	}
-
-	/**
-	 * Get the default user workspace app for the provided auth token
+	 * Create the user workspace app for the provided user and auth token
 	 * @param user
 	 * @param token
 	 * @return
 	 */
-	public static String getUserWorkspaceApp(User user, AuthProvider token) {
+	public static String createUserWorkspaceApp(User user, AuthProvider token) {
+		return createUserWorkspaceApp(user.getAccessToken(token));
+	}
+	
+	/**
+	 * Create the user asset app for the provided access token
+	 * @param token
+	 * @return
+	 */
+	public static String createUserAssetApp(AccessToken token) {
+		String appId = null;
+		// TODO >>>timb: WORKSPACE - look at GenerateEmptyAppReactor, use AppEngine
+		registerUserAssetApp(token, appId);
+		return null;
+	}
+	
+	/**
+	 * Create the user asset app for the provided user and auth token
+	 * @param user
+	 * @param token
+	 * @return
+	 */
+	public static String createUserAssetApp(User user, AuthProvider token) {
+		return createUserAssetApp(user.getAccessToken(token));
+	}
+	
+	
+	//////////////////////////////////////////////////////////////////////
+	// Updating workspace and asset metadata 
+	//////////////////////////////////////////////////////////////////////
+	// TODO >>>timb: WORKSPACE - DONE - register workspace
+
+	/**
+	 * Register the user workspace app for the provided access token and app id
+	 * @param token
+	 * @param appId
+	 */
+	public static void registerUserWorkspaceApp(AccessToken token, String appId) {
+		String[] colNames = new String[] {"TYPE", "USERID", "ENGINEID"};
+		String[] types = new String[] {"varchar(255)", "varchar(255)", "varchar(255)"};
+		String insertQuery = RdbmsQueryBuilder.makeInsert("WORKSPACEENGINE", colNames, types, 
+				new String[] {	token.getProvider().name(), 
+								token.getId(), 
+								appId});
+		try {
+			securityDb.insertData(insertQuery);
+			securityDb.commit();
+		} catch (SQLException e) {
+			// TODO >>>timb: WORKSPACE - How to deal with this exception properly?
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Register the user workspace app for the provided user, auth provider, and app id
+	 * @param user
+	 * @param provider
+	 * @param appId
+	 */
+	public static void registerUserWorkspaceApp(User user, AuthProvider provider, String appId) {
+		registerUserWorkspaceApp(user.getAccessToken(provider), appId);
+	}
+	
+	/**
+	 * Register the user asset app for the provided access token and app id
+	 * @param token
+	 * @param appId
+	 */
+	public static void registerUserAssetApp(AccessToken token, String appId) {
+		String[] colNames = new String[] {"TYPE", "USERID", "ENGINEID"};
+		String[] types = new String[] {"varchar(255)", "varchar(255)", "varchar(255)"};
+		String insertQuery = RdbmsQueryBuilder.makeInsert("ASSETENGINE", colNames, types, 
+				new String[] {	token.getProvider().name(), 
+								token.getId(), 
+								appId});
+		try {
+			securityDb.insertData(insertQuery);
+			securityDb.commit();
+		} catch (SQLException e) {
+			// TODO >>>timb: WORKSPACE - How to deal with this exception properly?
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Register the user asset app for the provided user, auth provider, and app id
+	 * @param user
+	 * @param provider
+	 * @param appId
+	 */
+	public static void registerUserAssetApp(User user, AuthProvider provider, String appId) {
+		registerUserAssetApp(user.getAccessToken(provider), appId);
+	}
+
+	
+	//////////////////////////////////////////////////////////////////////
+	// Querying workspace and asset metadata 
+	//////////////////////////////////////////////////////////////////////
+	
+	/**
+	 * Get the user workspace app for the provided access token; returns null if there is none
+	 * @param token
+	 * @return
+	 */
+	public static String getUserWorkspaceApp(AccessToken token) {
 		String query = "SELECT ENGINEID FROM WORKSPACEENGINE WHERE "
-				+ "TYPE = '" + token.toString() + "' AND "
-				+ "USERID = '" + user.getAccessToken(token).getId() + "'"
+				+ "TYPE = '" + token.getProvider().name() + "' AND "
+				+ "USERID = '" + token.getId() + "'"
 				;
 		IRawSelectWrapper wrapper = WrapperManager.getInstance().getRawWrapper(securityDb, query);
 		try {
-			if(wrapper.hasNext()) {
+			if (wrapper.hasNext()) {
 				return wrapper.next().getValues()[0].toString();
 			}
 		} finally {
@@ -54,25 +159,45 @@ public class WorkspaceAssetUtils extends AbstractSecurityUtils {
 	}
 	
 	/**
-	 * Get the default user asset app for the provided auth token
+	 * Get the user workspace app for the provided user and auth provider; returns null if is there is none
+	 * @param user
+	 * @param provider
+	 * @return
+	 */
+	public static String getUserWorkspaceApp(User user, AuthProvider provider) {
+		return getUserWorkspaceApp(user.getAccessToken(provider));
+	}
+	
+	/**
+	 * Get the user asset app for the provided access token; returns null if there is none
 	 * @param user
 	 * @param token
 	 * @return
 	 */
-	public static String getUserAssetApp(User user, AuthProvider token) {
+	public static String getUserAssetApp(AccessToken token) {
 		String query = "SELECT ENGINEID FROM ASSETENGINE WHERE "
-				+ "TYPE = '" + token.toString() + "' AND "
-				+ "USERID = '" + user.getAccessToken(token).getId() + "'"
+				+ "TYPE = '" + token.getProvider().name() + "' AND "
+				+ "USERID = '" + token.getId() + "'"
 				;
 		IRawSelectWrapper wrapper = WrapperManager.getInstance().getRawWrapper(securityDb, query);
 		try {
-			if(wrapper.hasNext()) {
+			if (wrapper.hasNext()) {
 				return wrapper.next().getValues()[0].toString();
 			}
 		} finally {
 			wrapper.cleanUp();
 		}
 		return null;
+	}
+	
+	/**
+	 * Get the user asset app for the provided user and auth provider; returns null if there is none
+	 * @param user
+	 * @param provider
+	 * @return
+	 */
+	public static String getUserAssetApp(User user, AuthProvider provider) {
+		return getUserAssetApp(user.getAccessToken(provider));
 	}
 	
 }
