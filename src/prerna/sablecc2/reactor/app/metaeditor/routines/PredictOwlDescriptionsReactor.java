81d915b3f2bed9c5a3c405849562d69d0105342b
mahkhalil <mahkhalil@deloitte.com>
2019-03-25 14:14:12 +0000

    UPDATE ::: Switching description to utilizing Java instead of R to pull descriptions together UPDATE ::: Logical names returning top N results that appear the most
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32477 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d17bcec9ea152229bf3a4f7ed056cbeb544e441d

--- src/prerna/sablecc2/reactor/app/metaeditor/routines/PredictOwlDescriptionsReactor.java
+++ src/prerna/sablecc2/reactor/app/metaeditor/routines/PredictOwlDescriptionsReactor.java
@@ -1,6 +1,5 @@
 package prerna.sablecc2.reactor.app.metaeditor.routines;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.Vector;
@@ -8,7 +7,6 @@ import java.util.Vector;
 import org.apache.log4j.Logger;
 
 import prerna.algorithm.api.SemossDataType;
-import prerna.ds.r.RSyntaxHelper;
 import prerna.engine.api.IEngine;
 import prerna.engine.api.IRawSelectWrapper;
 import prerna.nameserver.utility.MasterDatabaseUtility;
@@ -20,8 +18,8 @@ import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.execptions.SemossPixelException;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.app.metaeditor.AbstractMetaEditorReactor;
-import prerna.sablecc2.reactor.frame.r.util.AbstractRJavaTranslator;
 import prerna.util.Utility;
+import prerna.wikidata.WikiDescriptionExtractor;
 
 public class PredictOwlDescriptionsReactor extends AbstractMetaEditorReactor {
 
@@ -44,18 +42,23 @@ public class PredictOwlDescriptionsReactor extends AbstractMetaEditorReactor {
 	
 	@Override
 	public NounMetadata execute() {
-		// make sure R is good to go
 		Logger logger = getLogger(CLASS_NAME);
-		AbstractRJavaTranslator rJavaTranslator = this.insight.getRJavaTranslator(logger);
-		rJavaTranslator.startR(); 
-		// check if packages are installed
-		String[] packages = { "WikidataR", "XML", "RCurl", "stringr"};
-		rJavaTranslator.checkPackages(packages);
+
+		List<String> values = new Vector<String>();
+		
+		// make sure R is good to go
+//		Logger logger = getLogger(CLASS_NAME);
+//		AbstractRJavaTranslator rJavaTranslator = this.insight.getRJavaTranslator(logger);
+//		rJavaTranslator.startR(); 
+//		// check if packages are installed
+//		String[] packages = { "WikidataR", "XML", "RCurl", "stringr"};
+//		rJavaTranslator.checkPackages(packages);
 		
 		String appId = getAppId();
 		// we may have an alias
 		appId = getAppId(appId, true);
 		
+		String physicalUri = null;
 		String concept = getConcept();
 		String prop = getProperty();
 		
@@ -63,57 +66,92 @@ public class PredictOwlDescriptionsReactor extends AbstractMetaEditorReactor {
 		SemossDataType dataType = null;
 		String qsName = null;
 		if(prop == null || prop.isEmpty()) {
+			values.add(concept);
 			qsName = concept;
 			dataType = SemossDataType.convertStringToDataType(MasterDatabaseUtility.getBasicDataType(appId, qsName, null));
+			physicalUri = engine.getConceptPhysicalUriFromConceptualUri(concept);
 		} else {
+			values.add(prop);
 			qsName = concept + "__" + prop;
 			dataType = SemossDataType.convertStringToDataType(MasterDatabaseUtility.getBasicDataType(appId, prop, concept));
+			physicalUri = engine.getPropertyPhysicalUriFromConceptualUri(prop, concept);
 		}
 		
 		if(dataType != SemossDataType.STRING) {
 			throw new SemossPixelException(new NounMetadata("Can only generate descriptions on string valued input", PixelDataType.CONST_STRING, PixelOperationType.ERROR));
 		}
 		
-		List<String> values = new Vector<String>();
+		Set<String> logicalNames = engine.getLogicalNames(physicalUri);
+		values.addAll(logicalNames);
+		
 		IRawSelectWrapper wrapper = WrapperManager.getInstance().getRawWrapper(engine, getSingleColumnNonEmptyQs(qsName, 5));
 		while(wrapper.hasNext()) {
-			values.add(wrapper.next().getValues()[0].toString());
-		}
-		
-		StringBuilder script = new StringBuilder();
-		// first source the file where we have the main method for running
-		String rScriptPath = getBaseFolder() + "\\R\\OwlMatchRoutines\\OwlPredictDescriptions.R"; 
-		rScriptPath = rScriptPath.replace("\\", "/");
-		script.append("source(\"" + rScriptPath + "\");");
-		
-		String descriptionsFrameVar = "descriptionsFrame_" + Utility.getRandomString(6);
-		String instanceVectorVar = "instanceValues_" + Utility.getRandomString(6);
-		script.append(instanceVectorVar).append(" <- ").append(RSyntaxHelper.createStringRColVec(values)).append(";");
-		script.append(descriptionsFrameVar).append(" <- predictDescriptions(").append(instanceVectorVar).append(");");
-
-		// execute!
-		logger.info("Running script to auto generate descriptions...");
-		rJavaTranslator.runR(script.toString());
-		logger.info("Finished running scripts!");
-
-		String[] descriptionValues = rJavaTranslator.getStringArray(descriptionsFrameVar);
-
-		// try to get rid of the duplications
-		Set<String> uniqueDescriptions = new HashSet<String>();
-		for(String s : descriptionValues) {
-			if(s.trim().isEmpty()) {
+			Object value = wrapper.next().getValues()[0];
+			if(value == null || value.toString().isEmpty()) {
 				continue;
 			}
-			String[] newSplit = s.trim().split("\\*\\*\\*NEW LINE\\*\\*\\*");
-			for(String split : newSplit) {
-				uniqueDescriptions.add(split);
+			values.add(value.toString());
+		}
+		
+		List<String> descriptions = new Vector<String>();
+		WikiDescriptionExtractor extractor = new WikiDescriptionExtractor();
+		extractor.setLogger(logger);
+		for(String value : values) {
+			try {
+				descriptions.addAll(extractor.getDescriptions(value));
+			} catch (Exception e) {
+				logger.info("ERROR ::: Could not process input = " + value);
+				e.printStackTrace();
 			}
+		}
+		int numDescriptions = descriptions.size();
+		if(numDescriptions == 0) {
+			throw new SemossPixelException(new NounMetadata("Unable to generate a description for the input column", PixelDataType.CONST_STRING, PixelOperationType.ERROR));
 		}
 		
 		StringBuilder masterDescription = new StringBuilder();
-		for(String uniqueVal : uniqueDescriptions) {
-			masterDescription.append(uniqueVal).append(". ");
+		for(int i = 0; i < numDescriptions; i++) {
+			masterDescription.append(descriptions.get(i));
+			if((i+1) < numDescriptions) {
+				masterDescription.append("\n");
+			}
 		}
+		
+		
+//		StringBuilder script = new StringBuilder();
+//		// first source the file where we have the main method for running
+//		String rScriptPath = getBaseFolder() + "\\R\\OwlMatchRoutines\\OwlPredictDescriptions.R"; 
+//		rScriptPath = rScriptPath.replace("\\", "/");
+//		script.append("source(\"" + rScriptPath + "\");");
+//		
+//		String descriptionsFrameVar = "descriptionsFrame_" + Utility.getRandomString(6);
+//		String instanceVectorVar = "instanceValues_" + Utility.getRandomString(6);
+//		script.append(instanceVectorVar).append(" <- ").append(RSyntaxHelper.createStringRColVec(values)).append(";");
+//		script.append(descriptionsFrameVar).append(" <- predictDescriptions(").append(instanceVectorVar).append(");");
+//
+//		// execute!
+//		logger.info("Running script to auto generate descriptions...");
+//		rJavaTranslator.runR(script.toString());
+//		logger.info("Finished running scripts!");
+//
+//		String[] descriptionValues = rJavaTranslator.getStringArray(descriptionsFrameVar);
+//
+//		// try to get rid of the duplications
+//		Set<String> uniqueDescriptions = new HashSet<String>();
+//		for(String s : descriptionValues) {
+//			if(s.trim().isEmpty()) {
+//				continue;
+//			}
+//			String[] newSplit = s.trim().split("\\*\\*\\*NEW LINE\\*\\*\\*");
+//			for(String split : newSplit) {
+//				uniqueDescriptions.add(split);
+//			}
+//		}
+//		
+//		StringBuilder masterDescription = new StringBuilder();
+//		for(String uniqueVal : uniqueDescriptions) {
+//			masterDescription.append(uniqueVal).append(". ");
+//		}
 		
 //		OWLER owler = getOWLER(appId);
 //		for(String desc : descriptionValues) {
@@ -142,7 +180,6 @@ public class PredictOwlDescriptionsReactor extends AbstractMetaEditorReactor {
 		}
 		
 		NounMetadata noun = new NounMetadata(new String[]{description}, PixelDataType.CONST_STRING);
-//		noun.addAdditionalReturn(new NounMetadata("Predicted and stored descriptions for review",
 		noun.addAdditionalReturn(new NounMetadata("Predicted descriptions for review",
 				PixelDataType.CONST_STRING, PixelOperationType.SUCCESS));
 		return noun;
