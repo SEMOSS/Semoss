500e05a43983d9c3294655c06a4756e1e94474f5
mahkhalil <mahkhalil@deloitte.com>
2019-03-25 14:14:12 +0000

    UPDATE ::: Switching description to utilizing Java instead of R to pull descriptions together UPDATE ::: Logical names returning top N results that appear the most
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32477 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d17bcec9ea152229bf3a4f7ed056cbeb544e441d

--- src/prerna/wikidata/WikiDescriptionExtractor.java
+++ src/prerna/wikidata/WikiDescriptionExtractor.java
@@ -13,42 +13,44 @@ import org.apache.log4j.Logger;
 import org.wikidata.wdtk.wikibaseapi.WbSearchEntitiesResult;
 import org.wikidata.wdtk.wikibaseapi.WikibaseDataFetcher;
 
-public class WikiLogicalNameExtractor {
+public class WikiDescriptionExtractor {
 
-	public static final Logger LOGGER = LogManager.getLogger(WikiLogicalNameExtractor.class.getName());
+	public static final Logger LOGGER = LogManager.getLogger(WikiDescriptionExtractor.class.getName());
 	private Logger logger;
 
-	public WikiLogicalNameExtractor() {
+	public WikiDescriptionExtractor() {
 		
 	}
 	
-	public List<String> getLogicalNames(String searchTerm) throws Exception {
+	public List<String> getDescriptions(String searchTerm) throws Exception {
 		Logger logger = getLogger();
-		List<String> logicalNames = new Vector<String>();
+		List<String> descriptionList = new Vector<String>();
 
 		WikibaseDataFetcher wbdf = WikibaseDataFetcher.getWikidataDataFetcher();
 		List<WbSearchEntitiesResult> searchResults = wbdf.searchEntities(searchTerm, new Long(10));
 		int numReturns = searchResults.size();
 		logger.info("Querying wikidata returned " + numReturns + " results");
-		List<Callable<List<String>>> logicalNamesExtractors = new Vector<Callable<List<String>>>();
+		List<Callable<String>> descriptionExtractors = new Vector<Callable<String>>();
 		for(int i = 0; i < searchResults.size(); i++) {
 			WbSearchEntitiesResult res = searchResults.get(i);
-			WikiLogicalNameCallable callable = new WikiLogicalNameCallable(wbdf, res);
+			WikiDescriptionCallable callable = new WikiDescriptionCallable(wbdf, res);
 			callable.setLogger(this.logger);
-			logicalNamesExtractors.add(callable);
+			descriptionExtractors.add(callable);
 		}
 		
 		ExecutorService executorService = Executors.newFixedThreadPool(searchResults.size());
-		CompletionService<List<String>> completionService = new ExecutorCompletionService<>(executorService);
-		for (Callable<List<String>> logicalNameExtractor : logicalNamesExtractors) {
-			completionService.submit(logicalNameExtractor);
+		CompletionService<String> completionService = new ExecutorCompletionService<>(executorService);
+		for (Callable<String> descriptionExtractor : descriptionExtractors) {
+			completionService.submit(descriptionExtractor);
 		}
 		
 		// Continue until all have completed
 		while (numReturns > 0) {
 			try {
-				List<String> foundLogicalNames = completionService.take().get();
-				logicalNames.addAll(foundLogicalNames);
+				String foundDescription = completionService.take().get();
+				if(foundDescription != null && !foundDescription.trim().isEmpty()) {
+					descriptionList.add(foundDescription.trim());
+				}
 				numReturns--;
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -58,7 +60,7 @@ public class WikiLogicalNameExtractor {
 			}
 		}
 		
-		return logicalNames;
+		return descriptionList;
 	}
 	
 	public void setLogger(Logger logger) {
