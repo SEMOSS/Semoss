18d53d66f3a9621c5b97b34ad6eb709d9e6dbbf3
kunalppatel9 <kunalppatel9@deloitte.com>
2019-03-25 17:25:34 +0000

    UPDATE ::: Adding basic user asset space functions (delete, upload, traverse, mkdir)
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32487 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b7bab09b498007b6cff5c6e4ce617822c88d4659

--- src/prerna/sablecc2/reactor/workspace/UserRootDirectoryReactor.java
+++ src/prerna/sablecc2/reactor/workspace/UserRootDirectoryReactor.java
@@ -7,39 +7,69 @@ import java.util.Map;
 import prerna.auth.AuthProvider;
 import prerna.auth.User;
 import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.WorkspaceAssetUtils;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
+import prerna.util.Utility;
 
 public class UserRootDirectoryReactor extends AbstractReactor {
 
 	private static final String DIR_SEPARATOR = java.nio.file.FileSystems.getDefault().getSeparator();
 
+	public UserRootDirectoryReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.FILE_PATH.getKey()};
+	}
+	
+	
 	@Override
 	public NounMetadata execute() {
-		String userSpace = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + DIR_SEPARATOR + "users";
 		
-		String userSpaceId = null;
-		if(AbstractSecurityUtils.securityEnabled()) {
-			User user = this.insight.getUser();
-			// TODO: figure out which user space to put you into
-			AuthProvider token = user.getLogins().get(0);
-			userSpaceId = token.toString() + "_" + user.getAccessToken(token).getId();
-		} else {
-			userSpaceId = "anonymous";
+		organizeKeys();
+		String relativePath = this.keyValue.get(this.keysToGet[0]);
+		
+		Boolean isRoot = false;
+		if(relativePath == null || relativePath.isEmpty()) {
+			relativePath = "";
+			isRoot=true;
 		}
 		
-		File userFolder = new File(userSpace + DIR_SEPARATOR + userSpaceId);
+		String assetEngineID = null;
+		if(AbstractSecurityUtils.securityEnabled()) {
+		User user = this.insight.getUser();
+		if(user != null){
+			AuthProvider token = user.getPrimaryLogin();
+			if(token != null){
+				 assetEngineID = user.getAssetEngineId(token);
+				 Utility.getEngine(assetEngineID);
+			}
+		}
+		}
 		
+		if(assetEngineID == null){
+			throw new IllegalArgumentException("Unable to find user asset app");
+		}
+
+		String userFolderPath = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + DIR_SEPARATOR + "db" + DIR_SEPARATOR + WorkspaceAssetUtils.ASSET_APP_NAME + "__" +  assetEngineID ;
+
+		
+		File userFolder = new File(userFolderPath);
+		if(!userFolder.isDirectory()){
+			throw new IllegalArgumentException("Folder does not exist");
+		}
 		File[] userFiles = userFolder.listFiles();
 		int numFiles = userFiles.length;
 		
 		String[] fileNames = new String[numFiles];
 		boolean[] isDir = new boolean[numFiles];
 		for(int i = 0; i < numFiles; i++) {
+			if(userFiles[i].getName().equalsIgnoreCase("hidden.semoss")){
+				continue;
+			}
 			fileNames[i] = userFiles[i].getName();
 			isDir[i] = userFiles[i].isDirectory();
 		}
@@ -47,7 +77,7 @@ public class UserRootDirectoryReactor extends AbstractReactor {
 		Map<String, Object> retMap = new HashMap<String, Object>();
 		retMap.put("files", fileNames);
 		retMap.put("isDir", isDir);
-		retMap.put("isRoot", true);
+		retMap.put("isRoot", isRoot);
 		
 		return new NounMetadata(retMap, PixelDataType.MAP, PixelOperationType.USER_DIR);
 	}
