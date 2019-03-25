e7e462e5e4c2cbc2fe2bef59c743457e375de85a
tbanach <tbanach@deloitte.com>
2019-03-25 16:24:36 +0000

    BUGFIX ::: Calling the logic to create the app directly in the workspace utils. Tested this and is working now.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32485 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/auth/utils/WorkspaceAssetUtils.java
+++ src/prerna/auth/utils/WorkspaceAssetUtils.java
@@ -1,18 +1,28 @@
 package prerna.auth.utils;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Map;
+import java.util.UUID;
+
+import org.apache.commons.io.FileUtils;
 
 import prerna.auth.AccessToken;
 import prerna.auth.AuthProvider;
 import prerna.auth.User;
 import prerna.ds.util.RdbmsQueryBuilder;
+import prerna.engine.api.IEngine;
 import prerna.engine.api.IRawSelectWrapper;
-import prerna.om.Insight;
+import prerna.engine.impl.SmssUtilities;
+import prerna.engine.impl.app.AppEngine;
 import prerna.rdf.engine.wrappers.WrapperManager;
-import prerna.sablecc2.PixelRunner;
+import prerna.sablecc2.reactor.app.upload.UploadUtilities;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
 
 public class WorkspaceAssetUtils extends AbstractSecurityUtils {
+	
+	private static final String FS = System.getProperty("file.separator");
 	
 	private static final String WORKSPACE_APP_NAME = "Workspace";
 	private static final String ASSET_APP_NAME = "Asset";
@@ -30,9 +40,10 @@ public class WorkspaceAssetUtils extends AbstractSecurityUtils {
 	 * Create the user workspace app for the provided access token
 	 * @param token
 	 * @return
+	 * @throws Exception 
 	 */
-	public static String createUserWorkspaceApp(AccessToken token) {
-		String appId = createEmptyApp(token, WORKSPACE_APP_NAME); 
+	public static String createUserWorkspaceApp(AccessToken token) throws Exception {
+		String appId = createEmptyApp(token, WORKSPACE_APP_NAME);
 		registerUserWorkspaceApp(token, appId);
 		return appId;
 	}
@@ -42,8 +53,9 @@ public class WorkspaceAssetUtils extends AbstractSecurityUtils {
 	 * @param user
 	 * @param token
 	 * @return
+	 * @throws Exception 
 	 */
-	public static String createUserWorkspaceApp(User user, AuthProvider token) {
+	public static String createUserWorkspaceApp(User user, AuthProvider token) throws Exception {
 		return createUserWorkspaceApp(user.getAccessToken(token));
 	}
 	
@@ -51,9 +63,10 @@ public class WorkspaceAssetUtils extends AbstractSecurityUtils {
 	 * Create the user asset app for the provided access token
 	 * @param token
 	 * @return
+	 * @throws Exception 
 	 */
-	public static String createUserAssetApp(AccessToken token) {
-		String appId = createEmptyApp(token, ASSET_APP_NAME); 
+	public static String createUserAssetApp(AccessToken token) throws Exception {
+		String appId = createEmptyApp(token, ASSET_APP_NAME);
 		registerUserAssetApp(token, appId);
 		return appId;
 	}
@@ -63,25 +76,59 @@ public class WorkspaceAssetUtils extends AbstractSecurityUtils {
 	 * @param user
 	 * @param token
 	 * @return
+	 * @throws Exception 
 	 */
-	public static String createUserAssetApp(User user, AuthProvider token) {
+	public static String createUserAssetApp(User user, AuthProvider token) throws Exception {
 		return createUserAssetApp(user.getAccessToken(token));
 	}
 	
 	// TODO >>>timb: WORKSPACE - DONE - look at GenerateEmptyAppReactor, use AppEngine
-	private static String createEmptyApp(AccessToken token, String appName) {
+	private static String createEmptyApp(AccessToken token, String appName) throws IOException {
 		
-		// Make sure the app is created with the proper access token
-		Insight insight = new Insight();
-		User user = new User();
-		user.setAccessToken(token);
-		insight.setUser(user);
+		// Create a new app id
+		String appId = UUID.randomUUID().toString();
+
+		// Create the app folder
+		String baseFolder = DIHelper.getInstance().getProperty("BaseFolder");
+		String appLocation = baseFolder + FS + "db" + FS + SmssUtilities.getUniqueName(appName, appId);
+		File appFolder = new File(appLocation);
+		appFolder.mkdirs();
 		
-		// Run the pixel to import the data
-		PixelRunner returnData = insight.runPixel("GenerateEmptyAppReactor(app=[\"" + appName + "\"])");
+		// Create the insights database
+		IEngine insightDb = UploadUtilities.generateInsightsDatabase(appId, appName);
+
+		// Add database into DIHelper so that the web watcher doesn't try to load as well
+		File tempSmss = UploadUtilities.createTemporaryAppSmss(appId, appName);
+		DIHelper.getInstance().getCoreProp().setProperty(appId + "_" + Constants.STORE, tempSmss.getAbsolutePath());
 		
-		@SuppressWarnings("unchecked")
-		String appId = ((Map<String, String>) returnData.getResults().get(0).getValue()).get("app_id");
+		// Add the app to security db
+		SecurityUpdateUtils.addApp(appId, !AbstractSecurityUtils.securityEnabled());
+
+		// Create the app engine
+		AppEngine appEng = new AppEngine();
+		appEng.setEngineId(appId);
+		appEng.setEngineName(appName);
+		appEng.setInsightDatabase(insightDb);
+		
+		// Only at end do we add to DIHelper
+		DIHelper.getInstance().setLocalProperty(appId, appEng);
+		String appNames = (String) DIHelper.getInstance().getLocalProp(Constants.ENGINES);
+		appNames = appNames + ";" + appId;
+		DIHelper.getInstance().setLocalProperty(Constants.ENGINES, appNames);
+		
+		// Rename .temp to .smss
+		File smssFile = new File(tempSmss.getAbsolutePath().replace(".temp", ".smss"));
+		FileUtils.copyFile(tempSmss, smssFile);
+		tempSmss.delete();
+		
+		// Update engine smss file location
+		appEng.setPropFile(smssFile.getAbsolutePath());
+		DIHelper.getInstance().getCoreProp().setProperty(appId + "_" + Constants.STORE, smssFile.getAbsolutePath());
+		DIHelper.getInstance().setLocalProperty(appId, appEng);
+		
+		// Even if no security, just add user as engine owner
+		SecurityUpdateUtils.addEngineOwner(appId, token.getId());
+		
 		return appId;
 	}
 	
@@ -95,21 +142,17 @@ public class WorkspaceAssetUtils extends AbstractSecurityUtils {
 	 * Register the user workspace app for the provided access token and app id
 	 * @param token
 	 * @param appId
+	 * @throws SQLException 
 	 */
-	public static void registerUserWorkspaceApp(AccessToken token, String appId) {
+	public static void registerUserWorkspaceApp(AccessToken token, String appId) throws SQLException {
 		String[] colNames = new String[] {"TYPE", "USERID", "ENGINEID"};
 		String[] types = new String[] {"varchar(255)", "varchar(255)", "varchar(255)"};
 		String insertQuery = RdbmsQueryBuilder.makeInsert("WORKSPACEENGINE", colNames, types, 
 				new String[] {	token.getProvider().name(), 
 								token.getId(), 
 								appId});
-		try {
-			securityDb.insertData(insertQuery);
-			securityDb.commit();
-		} catch (SQLException e) {
-			// TODO >>>timb: WORKSPACE - How to deal with this exception properly?
-			e.printStackTrace();
-		}
+		securityDb.insertData(insertQuery);
+		securityDb.commit();
 	}
 	
 	/**
@@ -117,8 +160,9 @@ public class WorkspaceAssetUtils extends AbstractSecurityUtils {
 	 * @param user
 	 * @param provider
 	 * @param appId
+	 * @throws SQLException 
 	 */
-	public static void registerUserWorkspaceApp(User user, AuthProvider provider, String appId) {
+	public static void registerUserWorkspaceApp(User user, AuthProvider provider, String appId) throws SQLException {
 		registerUserWorkspaceApp(user.getAccessToken(provider), appId);
 	}
 	
@@ -126,21 +170,17 @@ public class WorkspaceAssetUtils extends AbstractSecurityUtils {
 	 * Register the user asset app for the provided access token and app id
 	 * @param token
 	 * @param appId
+	 * @throws SQLException 
 	 */
-	public static void registerUserAssetApp(AccessToken token, String appId) {
+	public static void registerUserAssetApp(AccessToken token, String appId) throws SQLException {
 		String[] colNames = new String[] {"TYPE", "USERID", "ENGINEID"};
 		String[] types = new String[] {"varchar(255)", "varchar(255)", "varchar(255)"};
 		String insertQuery = RdbmsQueryBuilder.makeInsert("ASSETENGINE", colNames, types, 
 				new String[] {	token.getProvider().name(), 
 								token.getId(), 
 								appId});
-		try {
-			securityDb.insertData(insertQuery);
-			securityDb.commit();
-		} catch (SQLException e) {
-			// TODO >>>timb: WORKSPACE - How to deal with this exception properly?
-			e.printStackTrace();
-		}
+		securityDb.insertData(insertQuery);
+		securityDb.commit();
 	}
 	
 	/**
@@ -148,8 +188,9 @@ public class WorkspaceAssetUtils extends AbstractSecurityUtils {
 	 * @param user
 	 * @param provider
 	 * @param appId
+	 * @throws SQLException 
 	 */
-	public static void registerUserAssetApp(User user, AuthProvider provider, String appId) {
+	public static void registerUserAssetApp(User user, AuthProvider provider, String appId) throws SQLException {
 		registerUserAssetApp(user.getAccessToken(provider), appId);
 	}
 
