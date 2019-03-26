a228d82b6b6fb9af4e782b9fae04d57545c5c94b
kunalppatel9 <kunalppatel9@deloitte.com>
2019-03-26 15:46:18 +0000

    UPDATE ::: Adding push to the cloud if clustered
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32503 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 014bb0f142ff7aee8d21b1ed5d67e7541fad0763

--- src/prerna/sablecc2/reactor/workspace/NewUserDirectoryReactor.java
+++ src/prerna/sablecc2/reactor/workspace/NewUserDirectoryReactor.java
@@ -7,6 +7,7 @@ import prerna.auth.AuthProvider;
 import prerna.auth.User;
 import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.WorkspaceAssetUtils;
+import prerna.cluster.util.CloudClient;
 import prerna.cluster.util.ClusterUtil;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
@@ -23,7 +24,7 @@ public class NewUserDirectoryReactor extends AbstractReactor {
 
 
 	public NewUserDirectoryReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.RELATIVE_PATH.getKey(),ReactorKeysEnum.FILE_NAME.getKey() };
+		this.keysToGet = new String[]{ReactorKeysEnum.RELATIVE_PATH.getKey() };
 	}
 
 	@Override
@@ -31,26 +32,25 @@ public class NewUserDirectoryReactor extends AbstractReactor {
 		organizeKeys();
 		String relativePath = this.keyValue.get(this.keysToGet[0]);
 
-		String folderName = this.keyValue.get(this.keysToGet[1]);
 
 
-		if(relativePath == null || relativePath.isEmpty() || folderName == null || folderName.isEmpty()  ) {
+		if(relativePath == null || relativePath.isEmpty() ) {
 			throw new IllegalArgumentException("Must input file path and file name to delete");
 		}
 
 
 		String assetEngineID = null;
 		if(AbstractSecurityUtils.securityEnabled()) {
-		User user = this.insight.getUser();
-		if(user != null){
-			AuthProvider token = user.getPrimaryLogin();
-			if(token != null){
-				 assetEngineID = user.getAssetEngineId(token);
-				 Utility.getEngine(assetEngineID);
+			User user = this.insight.getUser();
+			if(user != null){
+				AuthProvider token = user.getPrimaryLogin();
+				if(token != null){
+					assetEngineID = user.getAssetEngineId(token);
+					Utility.getEngine(assetEngineID);
+				}
 			}
 		}
-		}
-		
+
 		if(assetEngineID == null){
 			throw new IllegalArgumentException("Unable to find user asset app");
 		}
@@ -59,18 +59,17 @@ public class NewUserDirectoryReactor extends AbstractReactor {
 				WorkspaceAssetUtils.ASSET_APP_NAME + "__" +  assetEngineID + DIR_SEPARATOR +  "version";
 
 		File relativeFolder = new File(userFolder + DIR_SEPARATOR + relativePath);
-		
-		File folderCreate = new File(relativeFolder + DIR_SEPARATOR + folderName);
+
 		Boolean created = false;
-		if(folderCreate.exists()){
+		if(relativeFolder.exists()){
 			throw new IllegalArgumentException("There is already a folder at this location with that name");
 		} else{
-			created = folderCreate.mkdirs();
+			created = relativeFolder.mkdirs();
 			//made folder but now we need to add a hidden file for the cloud 
 			if(ClusterUtil.IS_CLUSTER){
-				
-				File hidden = new File(folderCreate+ DIR_SEPARATOR + WorkspaceAssetUtils.HIDDEN_FILE);
-				
+
+				File hidden = new File(relativeFolder+ DIR_SEPARATOR + WorkspaceAssetUtils.HIDDEN_FILE);
+
 				//override created boolean if its cloud to be at the hidden file level
 				try {
 					created = hidden.createNewFile();
@@ -78,12 +77,12 @@ public class NewUserDirectoryReactor extends AbstractReactor {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
+				ClusterUtil.reactorPushApp(assetEngineID);
 			}
+
 		}
-		
-		//When i get appId
-		//ClusterUtil.reactorPushApp(appId);
-		
+
+
 		return new NounMetadata(created, PixelDataType.BOOLEAN, PixelOperationType.USER_DIR);
 	}
 
