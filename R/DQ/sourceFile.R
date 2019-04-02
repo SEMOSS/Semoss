38f4f1c8f43329b4038a1115b1e8850090b774f0
mahkhalil <mahkhalil@deloitte.com>
2019-04-02 20:38:19 +0000

    UPDATE ::: Adding initial data quality widget r routines
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32576 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ R/DQ/sourceFile.R
@@ -0,0 +1,12 @@
+source("C:/workspace/Semoss_Dev/R/DQ/missionControl.R")
+
+source("C:/workspace/Semoss_Dev/R/DQ/Rules/isNull.r")
+source("C:/workspace/Semoss_Dev/R/DQ/Rules/isUnique.r")
+
+source("C:/workspace/Semoss_Dev/R/DQ/Rules/dateRule.r")
+source("C:/workspace/Semoss_Dev/R/DQ/Rules/emailRule.r")
+source("C:/workspace/Semoss_Dev/R/DQ/Rules/findValue.r")
+source("C:/workspace/Semoss_Dev/R/DQ/Rules/genderRule.r")
+source("C:/workspace/Semoss_Dev/R/DQ/Rules/nameRule.r")
+source("C:/workspace/Semoss_Dev/R/DQ/Rules/PhoneNumRule.r")
+source("C:/workspace/Semoss_Dev/R/DQ/Rules/validator.r")
