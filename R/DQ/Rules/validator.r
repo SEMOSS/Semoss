520bd8bbc8346c6985a022ea489efd8907924d0f
Maher Khalil <themaherkhalil@gmail.com>
2019-04-19 14:52:48 +0000

    UPDATE ::: Updating DQ widget
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32788 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3f2a617a4b01b0e12a787169dd0125a30945bb1c

--- R/DQ/Rules/validator.r
+++ R/DQ/Rules/validator.r
@@ -2,11 +2,11 @@ library(data.table)
 
 validator <- function(dt, rule){
   
-  ruleName <- c("Validate Values")
   currCol <- rule$col
   currRule <- rule$rule
   
   tempArray <- dt[, get(rule$col)]
+  dataType <- sapply(tempArray[1], class)
   totLength <- length(tempArray)
   
   toValidateArray <- rule$options
@@ -14,21 +14,27 @@ validator <- function(dt, rule){
   
   vec <- (tempArray %in% toValidateArray)
   totCorrect <- sum(vec, na.rm = TRUE)
-  tempTotErrs <- totLength - totCorrect
+  totErrors <- totLength - totCorrect
   
-  toPaint <- character(totLength)
+  errorArray <- character(totLength)
   vec <- as.integer(!vec)
   for(i in 1:totLength) {
     if(vec[i] == 1) {
-      toPaint[i] <- tempArray[i]
+      errorArray[i] <- tempArray[i]
     }
   }
-  toPaint <- toPaint[!duplicated(toPaint)]
-  toPaint <- paste(toPaint, collapse = "\", \"" )
-  toPaint <- paste0('\"', toPaint, "null", '\"')
+  errorArray <- na.omit(errorArray[!duplicated(errorArray)])
+  if(dataType == "character") {
+    toPaint <- paste(errorArray, collapse = "\", \"" )
+    toPaint <- paste0('\"', toPaint, '\"')
+  }
+  else if(dataType == "numeric") {
+    toPaint <- paste(errorArray, collapse = ", ")
+  }
+  toPaint <- paste(toPaint, "null", sep = ', ')
   description <- paste(toValidateArray, collapse = ", ")
-  returnTable <- data.table(currCol, tempTotErrs, totCorrect, totLength, ruleName, description, currRule, toPaint)
-  names(returnTable) <- c('Columns','Errors', 'Valid','Total','Rules', 'Description', 'ruleID', 'toColor')
+  returnTable <- data.table(currCol, totErrors, totCorrect, totLength, currRule, description, toPaint)
+  names(returnTable) <- c('Columns', 'Errors', 'Valid', 'Total', 'Rules', 'Description', 'toColor')
   
   return (returnTable)  
 }
