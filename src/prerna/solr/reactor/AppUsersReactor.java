88eb38546d5b5af95301c413bfc896a7657a2ab9
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2019-05-02 15:01:28 +0000

    Updates for Reactor Factory
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@32915 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 39708771d723ccc64b34574f2b8aaad1e282a9fb

---
+++ src/prerna/solr/reactor/AppUsersReactor.java
@@ -0,0 +1,47 @@
+package prerna.solr.reactor;
+
+import java.util.List;
+import java.util.Map;
+
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.SecurityAppUtils;
+import prerna.auth.utils.SecurityQueryUtils;
+import prerna.nameserver.utility.MasterDatabaseUtility;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class AppUsersReactor extends AbstractReactor {
+	
+	public AppUsersReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey()};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String appId = this.keyValue.get(this.keysToGet[0]);
+		
+		if(appId == null || appId.isEmpty()) {
+			throw new IllegalArgumentException("Must input an app id");
+		}
+		
+		List<Map<String, Object>> baseInfo = null;
+		if(AbstractSecurityUtils.securityEnabled()) {
+			// make sure valid id for user
+			appId = SecurityQueryUtils.testUserEngineIdForAlias(this.insight.getUser(), appId);
+			if(!SecurityAppUtils.userCanViewEngine(this.insight.getUser(), appId)) {
+				// you dont have access
+				throw new IllegalArgumentException("App does not exist or user does not have access to database");
+			}
+		} else {
+			appId = MasterDatabaseUtility.testEngineIdIfAlias(appId);
+		}
+		
+		baseInfo = SecurityQueryUtils.getAllDatabaseOwnersAndEditors(appId);
+		return new NounMetadata(baseInfo, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.APP_USERS);
+	}
+
+}
