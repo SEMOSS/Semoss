9551d453b20726051da0404d67a0ef54d673a00e
mahkhalil <mahkhalil@deloitte.com>
2019-05-14 18:28:50 +0000

    UPDATE ::: More generic version of grabbing the frame
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@33073 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 9093accde227923a9450749d0301eb58fe105ff5

--- src/prerna/sablecc2/reactor/frame/HasDuplicatesReactor.java
+++ src/prerna/sablecc2/reactor/frame/HasDuplicatesReactor.java
@@ -3,13 +3,11 @@ package prerna.sablecc2.reactor.frame;
 import java.util.List;
 
 import prerna.algorithm.api.ITableDataFrame;
-import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
-import prerna.sablecc2.reactor.AbstractReactor;
 
-public class HasDuplicatesReactor extends AbstractReactor {
+public class HasDuplicatesReactor extends AbstractFrameReactor {
 	
 	public HasDuplicatesReactor() {
 		this.keysToGet = new String[]{ReactorKeysEnum.FRAME.getKey(), ReactorKeysEnum.COLUMNS.getKey()};
@@ -36,18 +34,4 @@ public class HasDuplicatesReactor extends AbstractReactor {
 		return new NounMetadata(true, PixelDataType.BOOLEAN);
 	}
 	
-	private ITableDataFrame getFrame() {
-		// try specific key
-		GenRowStruct frameGrs = this.store.getNoun(keysToGet[0]);
-		if(frameGrs != null && !frameGrs.isEmpty()) {
-			return (ITableDataFrame) frameGrs.get(0);
-		}
-		
-		List<NounMetadata> frameCur = this.curRow.getNounsOfType(PixelDataType.FRAME);
-		if(frameCur != null && !frameCur.isEmpty()) {
-			return (ITableDataFrame) frameCur.get(0).getValue();
-		}
-		
-		return (ITableDataFrame) this.insight.getDataMaker();
-	}
 }
