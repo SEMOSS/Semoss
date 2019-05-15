7991dec41402e4653e8c909c4cf6e6d9ba87ffc2
tbanach <tbanach@deloitte.com>
2019-05-15 20:39:11 +0000

    UPDATE ::: Chris
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@33100 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 5a75fce58d195caa5e63cf03983376a9555f175d

--- R/AnalyticsRoutineScripts/text_summary.R
+++ R/AnalyticsRoutineScripts/text_summary.R
@@ -168,8 +168,6 @@ summarize_topics_text<-function(filename="",page_url="",content="",topTopics=5,t
 
 bind_results<-function(topic_keywords,topic_text){
 	topic_keywords$text<-""
-	topic_text$prevalence<-0
-	topic_text$coherence<-0
 	topic_text$keyword<-""
 	topic_text$freq<-0
 	topic_text$topic_share<-0
@@ -196,12 +194,12 @@ topics_text<-function(txt,topics,topN){
 			z<-z[order(-z$freq),]
 			z<-head(z,topN)
 			if(i==1){
-				df<-data.frame(topic=mytopics[i],label=mytopic$label[1],text=txt[z$x])
+				df<-data.frame(topic=mytopics[i],label=mytopic$label[1],prevalence=mytopic$prevalence[1],coherence=mytopic$coherence[1],text=txt[z$x])
 			}else{
-				df<-rbind(df,data.frame(topic=mytopics[i],label=mytopic$label[1],text=txt[z$x]))
+				df<-rbind(df,data.frame(topic=mytopics[i],label=mytopic$label[1],prevalence=mytopic$prevalence[1],coherence=mytopic$coherence[1],text=txt[z$x]))
 			}
 		}
-		df<-format(df,justify="left")
+		df$text<-format(df$text,justify="left")
 	}
 	gc()
 	return(df)
