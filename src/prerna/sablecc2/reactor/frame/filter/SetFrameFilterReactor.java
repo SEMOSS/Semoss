a556a34701af0c4303a809414be8cb440d9572d7
mahkhalil <mahkhalil@deloitte.com>
2019-06-18 18:35:52 +0000

    UPDATE ::: Removing frame headers change that was pushed for FE as temporary hack
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@33358 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/filter/SetFrameFilterReactor.java
+++ src/prerna/sablecc2/reactor/frame/filter/SetFrameFilterReactor.java
@@ -24,10 +24,7 @@ public class SetFrameFilterReactor extends AbstractFilterReactor {
 		}
 		frame.setFilter(grf);
 
-		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.FRAME_FILTER, PixelOperationType.FRAME_HEADERS_CHANGE);
-		noun.addAdditionalReturn(
-				new NounMetadata(frame.getFrameHeadersObject(), 
-						PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.FRAME_HEADERS));
+		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.FRAME_FILTER);
 		return noun;
 	}
 }
