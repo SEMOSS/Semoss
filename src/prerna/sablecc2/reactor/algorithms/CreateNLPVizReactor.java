ce920fa6a188441b902d79a1100ae8cc7de00700
kunalppatel9 <kunalppatel9@deloitte.com>
2019-07-10 21:15:57 +0000

    UPDATE ::: Adding in checks for non strings and aligning with updated R script
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@33536 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: be889797f28e1de2553353aadddee5daf3221295

--- src/prerna/sablecc2/reactor/algorithms/CreateNLPVizReactor.java
+++ src/prerna/sablecc2/reactor/algorithms/CreateNLPVizReactor.java
@@ -2,6 +2,8 @@ package prerna.sablecc2.reactor.algorithms;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -57,10 +59,28 @@ public class CreateNLPVizReactor extends AbstractRFrameReactor {
 		String fileroot = "dataitem";
 		OwlTemporalEngineMeta metadata = frame.getMetaData();
 		String appName = MasterDatabaseUtility.getEngineAliasForId(appId).replace(" ", "_");
+		boolean allStrings = true;
+		
+		// if it only has one column or one row, just return it as a grid
+		if (cols.size() < 2 || frame.getNumRows(frameName) < 2) {
+			String oneColPixel = "Frame () | QueryAll ( ) | AutoTaskOptions ( panel = [ \"0\" ] , layout = [ \"Grid\" ] ) | Collect ( 500 );";
+			PixelRunner runner = this.insight.runPixel(oneColPixel);
+			return runner.getResults().get(0);
+//			Map<String, Object> runnerWraper = new HashMap<String, Object>();
+//			runnerWraper.put("runner", runner);
+//			return new NounMetadata(runnerWraper, PixelDataType.PIXEL_RUNNER, PixelOperationType.VECTOR);	
+		}
 
 		// check if packages are installed
 		String[] packages = { "data.table", "plyr" , "jsonlite" };
 		this.rJavaTranslator.checkPackages(packages);
+		
+		// sort the columns list in ascending order of unique inst
+		Collections.sort(cols, new Comparator<String>() {
+			public int compare(String firstCol, String secondCol) {
+				return frame.getUniqueInstanceCount(firstCol) - frame.getUniqueInstanceCount(secondCol);
+			}
+		});
 
 		// let's first create the input frame
 		String inputFrame = "inputFrame" + Utility.getRandomString(5);
@@ -83,7 +103,18 @@ public class CreateNLPVizReactor extends AbstractRFrameReactor {
 				colName = col;
 			}
 
+			// get datatype
 			String dataType = metadata.getHeaderTypeAsString(tableName + "__" + colName);
+			
+			// If it is an int or double, convert to NUMBER
+			if(dataType.equalsIgnoreCase("INT") || dataType.equalsIgnoreCase("DOUBLE")) {
+				dataType = "NUMBER";
+			}
+			
+			// check to make sure at least one column is not a string
+			if(!dataType.equalsIgnoreCase("STRING")) {
+				allStrings = false;
+			}
 
 			// get unique column values
 			int uniqueValues = frame.getUniqueInstanceCount(colName);
@@ -97,6 +128,16 @@ public class CreateNLPVizReactor extends AbstractRFrameReactor {
 
 			rowCounter++;
 		}
+		
+		// if all the columns were strings, then make it a grid
+		if(allStrings) {
+			String nullPixel = "Frame () | QueryAll ( ) | AutoTaskOptions ( panel = [ \"0\" ] , layout = [ \"Grid\" ] ) | Collect ( 500 );";
+			PixelRunner runner = this.insight.runPixel(nullPixel);
+			return runner.getResults().get(0);
+//			Map<String, Object> runnerWraper = new HashMap<String, Object>();
+//			runnerWraper.put("runner", runner);
+//			return new NounMetadata(runnerWraper, PixelDataType.PIXEL_RUNNER, PixelOperationType.VECTOR);
+		}
 
 		// now let's look for the shared history vs personal history
 		File userHistory = new File(baseFolder + "\\R\\Recommendations\\dataitem-user-history.rds");
@@ -104,14 +145,20 @@ public class CreateNLPVizReactor extends AbstractRFrameReactor {
 			// user history does not exist, let's use shared history
 			fileroot = "shared";
 			logger.info("Selecting proper visualization for insight");
-		}
+		}		
 
 		// now lets run the script and return a json
+		// source the files and init
 		String outputJson = "outputJson" + Utility.getRandomString(5);
 		String recommend = "recommend" + Utility.getRandomString(5);
 		rsb.append("origDir <- getwd();");
 		rsb.append("setwd(\"" + baseFolder.replace("\\", "/") + "/R/Recommendations/\");");
 		rsb.append("source(\"viz_recom.r\");");
+		
+		// change int/double to number in the history
+		rsb.append("sync_numeric(\"" +fileroot + "\",\"" + fileroot + "\");");		
+
+		// run function and create json
 		rsb.append(recommend + "<-viz_recom_mgr(\"" + fileroot + "\", " + inputFrame + ", \"Grid\", 1);");
 		rsb.append("library(jsonlite);");
 		rsb.append(outputJson + " <-toJSON(" + recommend + ", byrow = TRUE, colNames = TRUE);");
@@ -123,7 +170,7 @@ public class CreateNLPVizReactor extends AbstractRFrameReactor {
 		Map recommendations = new HashMap<String, HashMap<String, String>>();
 
 		// if no recommendation was found, lets just return it in a grid
-		if (json == null) {
+		if (json == null || json.equals("[]")) {
 			String nullPixel = "Frame () | QueryAll ( ) | AutoTaskOptions ( panel = [ \"0\" ] , layout = [ \"Grid\" ] ) | Collect ( 500 );";
 			PixelRunner runner = this.insight.runPixel(nullPixel);
 			return runner.getResults().get(0);
@@ -190,7 +237,10 @@ public class CreateNLPVizReactor extends AbstractRFrameReactor {
 			String frameName) {
 		StringBuilder pixel = new StringBuilder();
 		String chartType = recommendations.keySet().toArray()[0].toString();
-
+		
+		//prep to catch the error
+		pixel.append("ifError( ( ");
+		
 		pixel.append("Frame ( frame = [ " + frameName + " ] ) | ");
 
 		// Append the Select and the Alias
@@ -239,7 +289,11 @@ public class CreateNLPVizReactor extends AbstractRFrameReactor {
 		}
 
 		// wrap it up and return
-		pixel.append("} } } ) | Collect ( 2000 );");
+		pixel.append("} } } ) | Collect ( 2000 )");
+		
+		// catch error and paint as grid
+		pixel.append(") , ( Frame () | QueryAll ( ) | AutoTaskOptions ( panel = [ \"0\" ] , layout = [ \"Grid\" ] ) | Collect ( 500 ) ) );");
+		
 		return pixel.toString();
 	}
 }
