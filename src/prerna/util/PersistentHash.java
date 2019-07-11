1d4995ccc3366ce9f3943ecaaa11296c81587ac9
mahkhalil <mahkhalil@deloitte.com>
2019-07-11 14:18:41 +0000

    UPDATE ::: Not creating the PersistentHash unless it is local master db UPDATE ::: Not erroring out in creating PersistentHash due to init before the KVStore table is created UPDATE ::: Throwing error immediately when local master or security fail instead of trying to continue with the app initialization
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@33545 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3877cb8fe5f038b18a80e7e48dd26bd8e386c661

--- src/prerna/util/PersistentHash.java
+++ src/prerna/util/PersistentHash.java
@@ -7,53 +7,51 @@ import java.util.Enumeration;
 import java.util.Hashtable;
 
 import prerna.ds.util.RdbmsQueryBuilder;
+import prerna.engine.api.IRawSelectWrapper;
+import prerna.engine.impl.rdbms.RDBMSNativeEngine;
+import prerna.rdf.engine.wrappers.WrapperManager;
+import prerna.util.sql.AbstractSqlQueryUtil;
 
 public class PersistentHash {
-	
+
 	// simple hash table that saves and gets values from the database
-	private final String tableName = "kvstore";
+	private static final String TABLE_NAME = "KVSTORE";
+
 	private Connection conn = null;
-	private String engineId = null;
-	private boolean validEngine = false;
-	
+
 	public Hashtable<String, String> thisHash = new Hashtable<String, String>();
 	boolean dirty = false;
-	
-	public PersistentHash(String engineId) {
-		this.engineId = engineId;
-		this.validEngine = Constants.LOCAL_MASTER_DB_NAME.equals(this.engineId);
+
+	public PersistentHash() {
+
 	}
-	
+
 	public void setConnection(Connection conn) {
 		this.conn = conn;
 	}
 
 	public void load() {
 		// this is only for local master!!!
-		if(validEngine) {
-			try {
-				if(conn != null) {
-					ResultSet rs = conn.createStatement().executeQuery("SELECT K, V from " + this.tableName);
-					while(rs.next()) {
-						this.thisHash.put(rs.getString(1),rs.getString(2));
-					}	
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
+		try {
+			if(conn != null) {
+				ResultSet rs = conn.createStatement().executeQuery("SELECT K, V from " + TABLE_NAME);
+				while(rs.next()) {
+					this.thisHash.put(rs.getString(1),rs.getString(2));
+				}	
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
-	
+
 	public void put(String key, String value) {
-		if(validEngine) {
-			this.thisHash.put(key, value);
-			dirty = true;
-		}
+		this.thisHash.put(key, value);
+		dirty = true;
 	}
-	
+
 	public boolean containsKey(String key) {
 		return this.thisHash.containsKey(key);
-				
+
 	}
 
 	public String get(String key) {
@@ -61,23 +59,44 @@ public class PersistentHash {
 	}
 
 	public void persistBack() {
-		if(this.validEngine && this.dirty) {
+		if(this.dirty) {
 			try {
 				String [] colNames = {"K","V"};
 				String [] types = {"varchar(800)", "varchar(800)"};
 				Enumeration <String> keys = thisHash.keys();
-				this.conn.createStatement().execute("DELETE from " + this.tableName);
+				this.conn.createStatement().execute("DELETE from " + TABLE_NAME);
 				while(keys.hasMoreElements()) {
 					String key = keys.nextElement();
 					String value = thisHash.get(key);
 					String [] values = {key, value};
-					String insertString = RdbmsQueryBuilder.makeInsert(this.tableName, colNames, types, values);
+					String insertString = RdbmsQueryBuilder.makeInsert(TABLE_NAME, colNames, types, values);
 					this.conn.createStatement().execute(insertString);
 				}
 				this.dirty = false;
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
+		}
+	}
+
+	/**
+	 * See if we can init the persistent hash
+	 * @param engine
+	 * @return
+	 */
+	public static boolean canInit(RDBMSNativeEngine engine) {
+		AbstractSqlQueryUtil queryUtil = engine.getQueryUtil();
+		if(queryUtil == null) {
+			return false;
+		}
+
+		// make sure the KVSTORE table exists
+		String tableExistsQuery = queryUtil.tableExistsQuery(TABLE_NAME, engine.getSchema());
+		IRawSelectWrapper wrapper = WrapperManager.getInstance().getRawWrapper(engine, tableExistsQuery);
+		try {
+			return wrapper.hasNext();
+		} finally {
+			wrapper.cleanUp();
 		}
 	}
 }
