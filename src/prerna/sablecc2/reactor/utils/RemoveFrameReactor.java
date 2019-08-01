28e08e4e0ad8c96afea4db0159ea5d225498ba03
mahkhalil <mahkhalil@deloitte.com>
2019-08-01 00:51:36 +0000

    UPDATE ::: Allowing the dropping of a frame/variable to occur immediately as opposed to during the stream process
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@33904 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/utils/RemoveFrameReactor.java
+++ src/prerna/sablecc2/reactor/utils/RemoveFrameReactor.java
@@ -5,16 +5,23 @@ import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.insights.AbstractInsightReactor;
+import prerna.util.insight.InsightUtility;
 
 public class RemoveFrameReactor extends AbstractInsightReactor {
 	
+	private static final String DROP_NOW_KEY = "dropNow";
+
 	public RemoveFrameReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.VARIABLE.getKey()};
+		this.keysToGet = new String[]{ReactorKeysEnum.VARIABLE.getKey(), DROP_NOW_KEY};
 	}
 
 	@Override
 	public NounMetadata execute() {
 		String variableName = this.curRow.get(0).toString();
+		if(dropNow()) {
+			return InsightUtility.removeFrameVaraible(this.insight.getVarStore(), variableName);
+		}
+		
 		NounMetadata noun = new NounMetadata(variableName, PixelDataType.REMOVE_VARIABLE, PixelOperationType.REMOVE_FRAME);
 
 		// make sure it is a valid removal
@@ -27,5 +34,15 @@ public class RemoveFrameReactor extends AbstractInsightReactor {
 		
 		return noun;
 	}
-
+	
+	/**
+	 * Determine if we should remove right away or during the stream
+	 * @return
+	 */
+	private boolean dropNow() {
+		if(this.curRow.size() > 1) {
+			return Boolean.parseBoolean(this.curRow.get(1).toString());
+		}
+		return false;
+	}
 }
--- src/prerna/sablecc2/reactor/utils/RemoveVariableReactor.java
+++ src/prerna/sablecc2/reactor/utils/RemoveVariableReactor.java
@@ -4,17 +4,33 @@ import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.insights.AbstractInsightReactor;
+import prerna.util.insight.InsightUtility;
 
 public class RemoveVariableReactor extends AbstractInsightReactor {
 	
+	private static final String DROP_NOW_KEY = "dropNow";
+	
 	public RemoveVariableReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.VARIABLE.getKey()};
+		this.keysToGet = new String[]{ReactorKeysEnum.VARIABLE.getKey(), DROP_NOW_KEY};
 	}
 
 	@Override
 	public NounMetadata execute() {
 		String variableName = this.curRow.get(0).toString();
+		if(dropNow()) {
+			return InsightUtility.removeVaraible(this.insight.getVarStore(), variableName);
+		}
 		return new NounMetadata(variableName, PixelDataType.REMOVE_VARIABLE);
 	}
-
+	
+	/**
+	 * Determine if we should remove right away or during the stream
+	 * @return
+	 */
+	private boolean dropNow() {
+		if(this.curRow.size() > 1) {
+			return Boolean.parseBoolean(this.curRow.get(1).toString());
+		}
+		return false;
+	}
 }
