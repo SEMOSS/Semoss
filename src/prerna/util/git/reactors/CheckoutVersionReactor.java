20e545c9409171bac8a41659877610ab38887110
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2019-08-06 18:46:01 +0000

    Git Specific Reactors Browse - Gives you Dir and Files at every level Clone - Clones to a folder - this needs to change to cache GetAsset - Gets a asset for specific commit without checkout Checkout - checksout a particular version - CheckoutVersion(version) if no version given moves it to the master GitInit - Initializes this insight with GIT ListAsset - Search an asset by a pattern ListAssetReactor.java("*.py") - will give all the python scripts ListCommnet - Gives all the comments as a single line, you can use this to checkout
    Utils - Accompanying Utils that are implementations. 
    
    Insight Reactors - Accomodates Git Automatically now. 
    
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@33994 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/util/git/reactors/CheckoutVersionReactor.java
@@ -0,0 +1,61 @@
+package prerna.util.git.reactors;
+
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.security.InstallCertNow;
+import prerna.util.Utility;
+import prerna.util.git.GitFetchUtils;
+import prerna.util.git.GitRepoUtils;
+
+public class CheckoutVersionReactor extends AbstractReactor {
+
+
+
+	// checks out this app to a specific version
+	// this is a bit dangerous if another user is operating on this at the same time
+	// I dont know if we should use the cache copy and move there
+	// if the version is not provided it will reset checkout
+	
+	public CheckoutVersionReactor() {
+		this.keysToGet = new String[]{"version"};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+
+		String assetFolder = this.insight.getInsightFolder(); // we need it where this would be the cache
+		assetFolder = assetFolder.replaceAll("\\\\", "/");
+		
+		// I need to do the job of creating this directory i.e. the name of the repo
+		// TBD
+		
+		String version = null;
+		
+		if(keyValue.containsKey(keysToGet[0]))
+			version = keyValue.get(keysToGet[0]);
+		
+		// I need a better way than output
+		// probably write the file and volley the file ?
+		String output = null;
+		try {
+			if(version != null)
+			{
+				GitRepoUtils.checkout(assetFolder, version); 
+				output = "Version - " + version + " active now";
+			}
+			else
+			{
+				GitRepoUtils.resetCheckout(assetFolder);
+				output = "Version - latest" + " active now";
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new NounMetadata(output, PixelDataType.CONST_STRING, PixelOperationType.OPERATION);
+	}
+
+}
---
+++ src/prerna/util/git/reactors/GitInitReactor.java
@@ -0,0 +1,52 @@
+package prerna.util.git.reactors;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.git.GitAssetUtils;
+import prerna.util.git.GitRepoUtils;
+
+public class GitInitReactor extends AbstractReactor {
+
+	// pulls the latest for this project / asset
+	// the asset is basically the folder where it sits
+	// this can be used enroute in a pipeline
+	
+	public GitInitReactor() {
+	}
+
+	@Override
+	public NounMetadata execute() {
+
+		String assetFolder = this.insight.getInsightFolder();
+		assetFolder = assetFolder.replaceAll("\\\\", "/");
+
+		try {
+			Git.init().setDirectory(new File(assetFolder)).call();
+			Git.open(new File(assetFolder)).close();
+			
+			GitRepoUtils.addAllFiles(assetFolder, true);
+			GitRepoUtils.commitAddedFiles(assetFolder);
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (GitAPIException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return new NounMetadata("Success !", PixelDataType.VECTOR, PixelOperationType.OPERATION);
+	}
+
+}
