3ac0ea40168c4e00c0823e6443718dee42d6e83b
mahkhalil <mahkhalil@deloitte.com>
2019-08-07 19:44:31 +0000

    UPDATE ::: Minor code cleanup + type casting return variables
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34034 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/git/GitAssetUtils.java
+++ src/prerna/util/git/GitAssetUtils.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 import org.codehaus.plexus.util.FileUtils;
 
@@ -15,141 +16,121 @@ public class GitAssetUtils {
 	// gets all the information on assets
 	// gives the information in 2 chunks
 	// files and directories
-	public static Hashtable browse(String gitFolder, String replacer)
-	{
-		Hashtable retHash = new Hashtable();
-		// list or string ?
-		
-		List files = new ArrayList<String>();
-		List directories = new ArrayList<String>();
-		retHash.put("FILE_LIST", "none");
-		retHash.put("DIR_LIST", "none");
-		
+	public static Map<String, List<String>> browse(String gitFolder, String replacer) {
+		Map<String, List<String>> retHash = new Hashtable<String, List<String>>();
+
+		List<String> files = new ArrayList<String>();
+		List<String> directories = new ArrayList<String>();
+		retHash.put("FILE_LIST", files);
+		retHash.put("DIR_LIST", directories);
+
 		File folder = new File(gitFolder);
 		File[] listOfFiles = folder.listFiles();
-		  String repString = ""; // can be $IF
-
+		String repString = ""; // can be $IF
 
 		for (int i = 0; i < listOfFiles.length; i++) {
-		  if (listOfFiles[i].isFile()) {
-			  String path = listOfFiles[i].getAbsolutePath().replaceAll("\\\\", "/");
-			    if(replacer != null)
-			    	path = path.replaceAll(replacer, repString);			 
-		    System.out.println("File " + path);
-	    	files.add(path.replaceFirst("/", ""));
-		  } else if (listOfFiles[i].isDirectory()) {
-		    //System.out.println("Directory " + listOfFiles[i].getName());
-			  String path = listOfFiles[i].getName().replaceAll("\\\\", "/");
-			  if(!path.startsWith(".")) // no hidden files
-			  {
-				    if(replacer != null)
-				    	path = path.replaceAll(replacer, repString);			 
-				  directories.add(path);
-			  }
-		  }
+			if (listOfFiles[i].isFile()) {
+				String path = listOfFiles[i].getAbsolutePath().replaceAll("\\\\", "/");
+				if(replacer != null) {
+					path = path.replaceAll(replacer, repString);
+				}
+				System.out.println("File " + path);
+				files.add(path.replaceFirst("/", ""));
+			} else if (listOfFiles[i].isDirectory()) {
+				// System.out.println("Directory " + listOfFiles[i].getName());
+				String path = listOfFiles[i].getName().replaceAll("\\\\", "/");
+				// no hidden files
+				if(!path.startsWith(".")) {
+					if(replacer != null) {
+						path = path.replaceAll(replacer, repString);
+					}
+					directories.add(path);
+				}
+			}
 		}
 
-		if(directories.size() > 0)
-			retHash.put("DIR_LIST", directories);
-		if(files.size() > 0)
-			retHash.put("FILE_LIST", files);
-		
-		
 		return retHash;
-		
 	}
 
 	// recursively print it
-	public void getAssets(String gitFolder, List <String> dirList)
-	{
+	public void getAssets(String gitFolder, List <String> dirList) {
 		File folder = new File(gitFolder);
 		File[] listOfFiles = folder.listFiles();
 		if(dirList == null)
 			dirList = new ArrayList<String>();
 
 		for (int i = 0; i < listOfFiles.length; i++) {
-		  if (listOfFiles[i].isFile()) {
-		    System.out.println("File " + listOfFiles[i].getAbsolutePath());
-		  } else if (listOfFiles[i].isDirectory()) {
-		    //System.out.println("Directory " + listOfFiles[i].getName());
-			  String path = listOfFiles[i].getName();
-			  if(!path.startsWith("."))
-				  dirList.add(listOfFiles[i].getAbsolutePath());
-		  }
+			if (listOfFiles[i].isFile()) {
+				System.out.println("File " + listOfFiles[i].getAbsolutePath());
+			} else if (listOfFiles[i].isDirectory()) {
+				//System.out.println("Directory " + listOfFiles[i].getName());
+				String path = listOfFiles[i].getName();
+				if(!path.startsWith(".")) {
+					dirList.add(listOfFiles[i].getAbsolutePath());
+				}
+			}
 		}
-		if(dirList.size() == 0)
-			return;
-		else
-			getAssets(dirList.remove(0), dirList);
 		
+		if(!dirList.isEmpty()) {
+			getAssets(dirList.remove(0), dirList);
+		}
 	}
-	
+
 	// should this also commit ?
-	public static void deleteAsset(String  assetFolder)
-	{
+	public static void deleteAsset(String  assetFolder) {
 		try {
 			File assetFile = new File(assetFolder);
-			if(assetFile.exists())
-			{
-				if(assetFile.isDirectory())
+			if(assetFile.exists()) {
+				if(assetFile.isDirectory()) {
 					FileUtils.deleteDirectory(assetFile);
-				else
+				} else {
 					assetFile.delete();
+				}
 			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	// this is to get all the assets
 	// with a particular extension
 	// runs through recursion to get what is needed
-	public static List listAssets(String gitFolder, String extn, String replacer, ArrayList <String> dirList, ArrayList <String> retList)
-	{
-		Hashtable retHash = new Hashtable();
-		StringBuilder files = new StringBuilder();
-		StringBuilder directories = new StringBuilder();
-		retHash.put("FILE_LIST", "none");
-		retHash.put("DIR_LIST", "none");
-		
-		if(dirList == null)
+	public static List<String> listAssets(String gitFolder, String extn, String replacer, List<String> dirList, List<String> retList) {
+		if(dirList == null) {
 			dirList = new ArrayList<String>();
-		
-		if(retList == null)
+		}
+		if(retList == null) {
 			retList = new ArrayList<String>();
-		
+		}
+
 		File folder = new File(gitFolder);
 		File[] listOfFiles = folder.listFiles();
 
 		for (int i = 0; i < listOfFiles.length; i++) {
-		  if (listOfFiles[i].isFile()) {
-			 
-		    System.out.println("File " + listOfFiles[i].getAbsolutePath());
-		    String path = listOfFiles[i].getAbsolutePath();
-		    path = path.replaceAll("\\\\", "/");
-		    if(isMatch(Utility.getInstanceName(path),extn))
-		    {
-			    if(replacer != null)
-			    	path = path.replaceAll(replacer, "");
-
-			    path.replaceFirst("/", "");
-		    	retList.add(path);
-		    }
-		  } else if (listOfFiles[i].isDirectory()) {
-		    //System.out.println("Directory " + listOfFiles[i].getName());
-			  String path = listOfFiles[i].getName();
-			  if(!path.startsWith(".")) // no hidden files
-				  dirList.add(listOfFiles[i].getAbsolutePath());
-		  }
+			if (listOfFiles[i].isFile()) {
+				System.out.println("File " + listOfFiles[i].getAbsolutePath());
+				String path = listOfFiles[i].getAbsolutePath();
+				path = path.replaceAll("\\\\", "/");
+				if(isMatch(Utility.getInstanceName(path),extn)) {
+					if(replacer != null) {
+						path = path.replaceAll(replacer, "");
+					}
+					path.replaceFirst("/", "");
+					retList.add(path);
+				}
+			} else if (listOfFiles[i].isDirectory()) {
+				//System.out.println("Directory " + listOfFiles[i].getName());
+				String path = listOfFiles[i].getName();
+				if(!path.startsWith(".")) // no hidden files
+					dirList.add(listOfFiles[i].getAbsolutePath());
+			}
 		}
 
-		if(dirList.size() > 0)
-			return listAssets(dirList.remove(0),extn, replacer, dirList, retList);
-		else 
-			return retList;
-		
+		if(!dirList.isEmpty()) {
+			return listAssets(dirList.remove(0), extn, replacer, dirList, retList);
+		} 
+
+		return retList;
 	}
 	
 	public static boolean isMatch(String s, String p) {
