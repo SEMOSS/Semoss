1c1d519adb678a65eebcaf36ab69365f3be7dd94
mahkhalil <mahkhalil@deloitte.com>
2019-08-14 02:18:27 +0000

    BUGFIX ::: Fixing errors with overriding reactors generated causing issues with lazy translation BUGFIX ::: Properly setting lazy translation to not execute and overriding necessary methods in greedy translation UPDATE ::: Exposing necessary getters in IReactor to setup appropriate encapsulation + easily determine variable modifications
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34183 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/json/JsonTranslation.java
+++ src/prerna/sablecc2/reactor/json/JsonTranslation.java
@@ -146,7 +146,7 @@ public class JsonTranslation extends LazyJsonTranslation {
 	    		e.printStackTrace();
 				throw new IllegalArgumentException(e.getMessage());
 			}
-	    	this.planner = ((AbstractReactor)curReactor).planner;
+	    	this.planner = curReactor.getPixelPlanner();
 	    	
 	    	//set the curReactor
 	    	if(parent != null && parent instanceof IReactor) {
