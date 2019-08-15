061cd1b725435a394d54ffc10c33a778e9f4df68
mahkhalil <mahkhalil@deloitte.com>
2019-08-15 17:27:45 +0000

    UPDATE ::: Adding direct jdbc query to pipeline translation
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34226 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/qs/source/DirectJdbcConnectionReactor.java
+++ src/prerna/sablecc2/reactor/qs/source/DirectJdbcConnectionReactor.java
@@ -2,10 +2,12 @@ package prerna.sablecc2.reactor.qs.source;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 import prerna.engine.impl.rdbms.RDBMSNativeEngine;
 import prerna.engine.impl.rdbms.RdbmsConnectionHelper;
-import prerna.query.querystruct.HardSelectQueryStruct;
+import prerna.query.querystruct.JdbcHardSelectQueryStruct;
 import prerna.query.querystruct.SelectQueryStruct;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.reactor.qs.AbstractQueryStructReactor;
@@ -26,6 +28,13 @@ public class DirectJdbcConnectionReactor extends AbstractQueryStructReactor {
 		String userName = this.keyValue.get(this.keysToGet[3]);
 		String password = this.keyValue.get(this.keysToGet[4]);
 		
+		// need to maintain what the FE passed to create this 
+		Map<String, Object> config = new HashMap<String, Object>();
+		config.put(this.keysToGet[0], connectionUrl);
+		config.put(this.keysToGet[1], driver);
+		config.put(this.keysToGet[2], userName);
+		config.put(this.keysToGet[3], password);
+		
 		Connection con = null;
 		try {
 			con = RdbmsConnectionHelper.getConnection(connectionUrl, userName, password, driver);
@@ -39,10 +48,10 @@ public class DirectJdbcConnectionReactor extends AbstractQueryStructReactor {
 		fakeEngine.setConnection(con);
 		fakeEngine.setBasic(true);
 		
-		HardSelectQueryStruct qs = new HardSelectQueryStruct();
+		JdbcHardSelectQueryStruct qs = new JdbcHardSelectQueryStruct();
+		qs.setConfig(config);
 		qs.setQuery(query);
 		qs.setEngine(fakeEngine);
-		qs.setQsType(SelectQueryStruct.QUERY_STRUCT_TYPE.RAW_ENGINE_QUERY);
 		return qs;
 	}
 
