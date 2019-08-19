d49f40f8ab9fc9941748a66a96285c0396637ee4
mahkhalil <mahkhalil@deloitte.com>
2019-08-19 16:48:45 +0000

    UPDATE ::: Adding helper implementation to start allowing schema modifications directly on existing app (add new column + remove existing column from rdbms to start)
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34276 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: bdeba1d53a3e3c220f2a316b0c25e4a2438853f0

---
+++ src/prerna/sablecc2/reactor/app/upload/modifications/AppRemoveColumnReactor.java
@@ -0,0 +1,68 @@
+package prerna.sablecc2.reactor.app.upload.modifications;
+
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.SecurityAppUtils;
+import prerna.auth.utils.SecurityQueryUtils;
+import prerna.engine.api.IEngine;
+import prerna.engine.api.IEngineModifier;
+import prerna.engine.impl.modifications.EngineModificationFactory;
+import prerna.nameserver.utility.MasterDatabaseUtility;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.sablecc2.reactor.app.metaeditor.properties.RemoveOwlPropertyReactor;
+import prerna.util.Utility;
+
+public class AppRemoveColumnReactor extends AbstractReactor {
+
+	public AppRemoveColumnReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(),
+				ReactorKeysEnum.CONCEPT.getKey(),
+				ReactorKeysEnum.COLUMN.getKey(), 
+				ReactorKeysEnum.DATA_TYPE.getKey()
+		};
+		this.keyRequired = new int[]{1, 1, 1, 1};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		this.organizeKeys();
+
+		String engineId = this.keyValue.get(this.keysToGet[0]);
+		// we may have the alias
+		if(AbstractSecurityUtils.securityEnabled()) {
+			engineId = SecurityQueryUtils.testUserEngineIdForAlias(this.insight.getUser(), engineId);
+			if(!SecurityAppUtils.userCanViewEngine(this.insight.getUser(), engineId)) {
+				throw new IllegalArgumentException("App " + engineId + " does not exist or user does not have access to database");
+			}
+		} else {
+			engineId = MasterDatabaseUtility.testEngineIdIfAlias(engineId);
+			if(!MasterDatabaseUtility.getAllEngineIds().contains(engineId)) {
+				throw new IllegalArgumentException("App " + engineId + " does not exist");
+			}
+		}
+
+		String table = this.keyValue.get(this.keysToGet[1]);
+		String column = this.keyValue.get(this.keysToGet[2]);
+
+
+		// update the owl for any engine
+		// we will just update the 
+		RemoveOwlPropertyReactor owlUpdated = new RemoveOwlPropertyReactor();
+		owlUpdated.setInsight(this.insight);
+		owlUpdated.setNounStore(this.store);
+		owlUpdated.execute();
+
+		IEngine engine = Utility.getEngine(engineId);
+		IEngineModifier modifier = EngineModificationFactory.getEngineModifier(engine);
+		try {
+			modifier.removeProperty(table, column);
+		} catch (Exception e) {
+			// an error occurred here, so we need to delete from the OWL
+			throw new IllegalArgumentException("Error occured to alter the table. Error returned from driver: " + e.getMessage(), e);
+		}
+
+		return new NounMetadata(true, PixelDataType.BOOLEAN);
+	}
+}
