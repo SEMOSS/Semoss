cae9b00e1f0a574153cad101438600c98d263038
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-08-20 17:32:04 +0000

    UPDATE ::: Adding new default form-builder insights
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34317 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6beae22e2daea9e9c09bad93baa595110aee8721

--- src/prerna/poi/main/helper/excel/ExcelDataValidationHelper.java
+++ src/prerna/poi/main/helper/excel/ExcelDataValidationHelper.java
@@ -23,6 +23,7 @@ import com.google.gson.Gson;
 
 import prerna.algorithm.api.SemossDataType;
 import prerna.poi.main.HeadersException;
+import prerna.sablecc2.reactor.app.upload.FormUtility;
 import prerna.util.Utility;
 import prerna.util.gson.GsonUtility;
 
@@ -166,8 +167,8 @@ public class ExcelDataValidationHelper {
 				String[] split = address.split(":");
 				CellReference cellReference = new CellReference(split[0]);
 				int cellRow = cellReference.getRow();
-				// this is the case if we are only uploading headers 
-				if(cellRow == 0) {
+				// this is the case if we are only uploading headers
+				if (cellRow == 0) {
 					cellRow = 1;
 				}
 				Row row = sheet.getRow(cellRow - 1);
@@ -272,12 +273,12 @@ public class ExcelDataValidationHelper {
 						headerMeta.put("type", SemossDataType.TIMESTAMP.toString());
 						int validationType = DataValidationConstraint.ValidationType.TIME;
 						headerMeta.put("validationType", validationTypeToString(validationType));
-					} 
+					}
 					if (type == SemossDataType.DATE) {
 						headerMeta.put("type", SemossDataType.DATE.toString());
 						int validationType = DataValidationConstraint.ValidationType.DATE;
 						headerMeta.put("validationType", validationTypeToString(validationType));
-					} 
+					}
 					// TODO
 					headerMeta.put("range", "");
 					headerMeta.put("emptyCells", true);
@@ -302,7 +303,7 @@ public class ExcelDataValidationHelper {
 		}
 		return validationMap;
 	}
-	
+
 	/**
 	 * Get description for headers to create form
 	 * 
@@ -326,18 +327,15 @@ public class ExcelDataValidationHelper {
 					headerMeta.put("type", SemossDataType.STRING.toString());
 					int validationType = DataValidationConstraint.ValidationType.TEXT_LENGTH;
 					headerMeta.put("validationType", validationTypeToString(validationType));
-				}
-				else if (type == SemossDataType.INT || type == SemossDataType.DOUBLE) {
+				} else if (type == SemossDataType.INT || type == SemossDataType.DOUBLE) {
 					headerMeta.put("type", SemossDataType.DOUBLE.toString());
 					int validationType = DataValidationConstraint.ValidationType.DECIMAL;
 					headerMeta.put("validationType", validationTypeToString(validationType));
-				}
-				else if (type == SemossDataType.DATE) {
+				} else if (type == SemossDataType.DATE) {
 					headerMeta.put("type", SemossDataType.DATE.toString());
 					int validationType = DataValidationConstraint.ValidationType.DATE;
 					headerMeta.put("validationType", validationTypeToString(validationType));
-				}
-				else if (type == SemossDataType.TIMESTAMP) {
+				} else if (type == SemossDataType.TIMESTAMP) {
 					headerMeta.put("type", SemossDataType.TIMESTAMP.toString());
 					int validationType = DataValidationConstraint.ValidationType.DATE;
 					headerMeta.put("validationType", validationTypeToString(validationType));
@@ -348,7 +346,7 @@ public class ExcelDataValidationHelper {
 					int validationType = DataValidationConstraint.ValidationType.TEXT_LENGTH;
 					headerMeta.put("validationType", validationTypeToString(validationType));
 				}
-				
+
 				headerMeta.put("range", "");
 				headerMeta.put("emptyCells", true);
 				// add comment
@@ -382,6 +380,10 @@ public class ExcelDataValidationHelper {
 	public static Map<String, Object> createInsertForm(String appId, String sheetName,
 			Map<String, Object> dataValidationMap, String[] headerList) {
 		Map<String, Object> formMap = new HashMap<>();
+		formMap.put("js", new Vector<>());
+		formMap.put("css", new Vector<>());
+
+		// grab all the properties
 		List<String> propertyList = new ArrayList<String>();
 		if (headerList != null && headerList.length > 0) {
 			for (String header : headerList) {
@@ -392,36 +394,39 @@ public class ExcelDataValidationHelper {
 				propertyList.add(header);
 			}
 		}
-		// create values and into strings for query
+		// create values and into strings for insert query
 		StringBuilder intoString = new StringBuilder();
 		StringBuilder valuesString = new StringBuilder();
 		for (int i = 0; i < propertyList.size(); i++) {
 			String property = propertyList.get(i);
 			intoString.append(sheetName + "__" + property);
-			valuesString.append(" \"<Parameter_" + i + ">\"");
+			valuesString.append(" (<" + property + ">)");
 			if (i < propertyList.size() - 1) {
 				intoString.append(",");
 				valuesString.append(",");
 			}
 		}
-		formMap.put("query", "Database(database=[\"" + appId + "\"]) | Insert (into=[" + intoString + "], values=["
+
+		// create insert pixel map
+		Map<String, Object> pixelMap = new HashMap<>();
+		Map<String, Object> insertMap = new HashMap<>();
+		insertMap.put("name", "Insert");
+		insertMap.put("pixel", "Database(database=[\"" + appId + "\"]) | Insert (into=[" + intoString + "], values=["
 				+ valuesString + "]);");
-		// TODO
-		formMap.put("label", "");
-		formMap.put("description", "");
-		// build param list
-		List<Map<String, Object>> paramList = new Vector<>();
+		pixelMap.put("Insert", insertMap);
+		formMap.put("pixel", pixelMap);
+
+		StringBuilder htmlSb = new StringBuilder();
+		Map<String, Object> dataMap = new HashMap<>();
 		for (int i = 0; i < propertyList.size(); i++) {
 			String property = propertyList.get(i);
+			htmlSb.append(FormUtility.getTextComponent(property));
 			Map<String, Object> propMap = (Map<String, Object>) dataValidationMap.get(property);
-			// build param for each property
-			Map<String, Object> paramMap = new HashMap<>();
-			paramMap.put("paramName", "Parameter_" + i);
 			String type = (String) propMap.get("type");
 			SemossDataType propType = SemossDataType.valueOf(type);
-			// build view for param map
-			Map<String, Object> viewMap = new HashMap<>();
-			viewMap.put("label", property);
+			
+			// grab description from excel we will add this after
+			// adding appropriate input component
 			String description = "";
 			if (propMap.containsKey("description")) {
 				description = (String) propMap.get("description");
@@ -433,53 +438,59 @@ public class ExcelDataValidationHelper {
 					description = "Please enter a date (yyyy-mm-dd)";
 				}
 			}
-			viewMap.put("description", description);
+			
+			// build data property map for data binding
+			Map<String, Object> propertyMap = new HashMap<>();
+			propertyMap.put("defaultValue", "");
+			propertyMap.put("options", new Vector());
+			propertyMap.put("name", property);
+			propertyMap.put("dependsOn", new Vector());
+			propertyMap.put("required", true);
+			propertyMap.put("autoPopulate", false);
+			Map<String, Object> configMap = new HashMap<>();
+			configMap.put("table", sheetName);
+			Map<String, Object> appMap = new HashMap<>();
+			appMap.put("value", appId);
+			configMap.put("app", appMap);
+			propertyMap.put("config", configMap);
+			propertyMap.put("pixel", "");
+
 			// change validation type to display type
 			String validationType = (String) propMap.get("validationType");
 			int vt = ExcelDataValidationHelper.stringToValidationType(validationType);
 			WIDGET_COMPONENT wc = ExcelDataValidationHelper.validationTypeToComponent(vt);
-			viewMap.put("displayType", wc.toString().toLowerCase());
-			paramMap.put("view", viewMap);
-			// build model for param map
-			Map<String, Object> modelMap = new HashMap<>();
-			modelMap.put("defaultValue", "");
-			modelMap.put("defaultOptions", new Vector());
+			
+			// build html based on input component
 			if (wc == WIDGET_COMPONENT.DROPDOWN) {
 				String[] values = (String[]) propMap.get("values");
-				modelMap.put("defaultOptions", values);
+				propertyMap.put("manualOptions", String.join(",", values));
+				htmlSb.append(FormUtility.getDropdownComponent(property));
+			} else if (wc == WIDGET_COMPONENT.NUMBER) {
+				Object f1 = propMap.get("f1");
+				Object f2 = propMap.get("f2");
+				//TODO use slider?
+				propertyMap.put("defaultValue", "0");
+				htmlSb.append(FormUtility.getNumberPickerComponent());
+			} else if (wc == WIDGET_COMPONENT.TEXTAREA) {
+				htmlSb.append(FormUtility.getTextAreaComponent());
 			} else {
-				if (wc != WIDGET_COMPONENT.TEXTAREA) {
-					if (propType == SemossDataType.STRING) {
-						// if prop type is a string
-						modelMap.put("query",
-								"(Parameter_" + i + "_infinite = Database( database=[\"" + appId + "\"] )|" + "Select("
-										+ sheetName + "__" + property + ").as([" + property + "])|" + "Filter("
-										+ sheetName + "__" + property + " ?like \"<Parameter_" + i
-										+ "_search>\") | Iterate())| Collect(50);");
-						modelMap.put("infiniteQuery", "Parameter_" + i + "_infinite | Collect(50);");
-						modelMap.put("searchParam", "Parameter_" + i + "_search");
-						// add dependency
-						List<String> dependencies = new Vector<>();
-						dependencies.add("Parameter_" + i + "_search");
-						modelMap.put("dependsOn", dependencies);
-						// if prop type is a string build a search param
-						Map<String, Object> searchMap = new HashMap<>();
-						searchMap.put("paramName", "Parameter_" + i + "_search");
-						searchMap.put("view", false);
-						Map<String, Object> searchModelMap = new HashMap<>();
-						searchModelMap.put("defaultValue", "");
-						searchMap.put("model", searchModelMap);
-						paramList.add(searchMap);
-					}
-				}
+				htmlSb.append(FormUtility.getInputComponent(property));
 			}
-			if (wc != WIDGET_COMPONENT.TEXTAREA) {
-				paramMap.put("model", modelMap);
+
+			if (description.length() > 0) {
+				htmlSb.append(FormUtility.getDescriptionComponent(description));
 			}
-			paramList.add(paramMap);
+			// adding pixel data binding for non-numeric values
+			if (propType == SemossDataType.STRING && wc != WIDGET_COMPONENT.DROPDOWN) {
+				String pixel = "Database( database=[\"" + appId + "\"] )|" + "Select(" + sheetName + "__" + property
+						+ ").as([" + property + "])| Collect(-1);";
+				propertyMap.put("pixel", pixel);
+			}
+			dataMap.put(property, propertyMap);
 		}
-		formMap.put("params", paramList);
-		formMap.put("execute", "Submit");
+		htmlSb.append(FormUtility.getSubmitComponent());
+		formMap.put("html", htmlSb.toString());
+		formMap.put("data", dataMap);
 		return formMap;
 	}
 
@@ -523,7 +534,7 @@ public class ExcelDataValidationHelper {
 					validationList.add(regex);
 					configPropMap.put("validation", validationList);
 				} else if (type == SemossDataType.STRING) {
-//					configPropMap.put("selection-type", "database");
+					// configPropMap.put("selection-type", "database");
 				} else if (type == SemossDataType.DATE) {
 					ArrayList<String> validationList = new ArrayList<>();
 					String regex = "^\\d{4}-\\d{2}-\\d{2}$";
@@ -658,7 +669,5 @@ public class ExcelDataValidationHelper {
 		System.out.println(gson.toJson(form));
 
 	}
-
-
 
 }
