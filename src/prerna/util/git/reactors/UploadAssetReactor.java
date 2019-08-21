f452094bcf8c6dc9e9d2ff238904f988cca164d3
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-08-21 18:45:31 +0000

    UPDATE ::: Added UploadAssest(); moves a tempFile to the new asset location and adds the file and commits
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34348 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 991f7a554409ccc43d4ad9fd3149308d503c2130

---
+++ src/prerna/util/git/reactors/UploadAssetReactor.java
@@ -0,0 +1,67 @@
+package prerna.util.git.reactors;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Vector;
+
+import org.codehaus.plexus.util.FileUtils;
+
+import prerna.auth.User;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.execptions.SemossPixelException;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.git.GitRepoUtils;
+
+public class UploadAssetReactor extends AbstractReactor{
+	
+	public UploadAssetReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.FILE_PATH.getKey(), "tempPath", ReactorKeysEnum.COMMENT_KEY.getKey()};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String filePath = this.keyValue.get(this.keysToGet[0]);
+		String tempPath = this.keyValue.get(this.keysToGet[1]);
+		String comment = this.keyValue.get(this.keysToGet[2]);
+
+		// get insight asset path
+		String assetFolder = this.insight.getInsightFolder();
+		assetFolder = assetFolder.replaceAll("\\\\", "/");
+
+		// create path where asset will be uploaded to
+		String assetPath = assetFolder + "/" + filePath;
+		assetPath = assetPath.replaceAll("\\\\", "/");
+
+		// move temp file to asset file
+		File tempFile = new File(tempPath);
+		try {
+			FileUtils.copyFileToDirectory(tempPath, assetFolder);
+			//TODO should I delete the old file?
+			// tempFile.delete();
+		} catch (IOException e) {
+			e.printStackTrace();
+			NounMetadata error = NounMetadata.getErrorNounMessage("Unable to Upload Asset");
+			SemossPixelException exception = new SemossPixelException(error);
+			exception.setContinueThreadOfExecution(false);
+			throw exception;
+		}
+
+		// add file to git
+		List<String> files = new Vector<>();
+		files.add(assetPath);
+		GitRepoUtils.addSpecificFiles(assetFolder, files);
+		
+		String author = this.insight.getUserId();
+		// TODO how can I get the user email
+		User user = this.insight.getUser();
+		String email = null;
+		// commit it
+		GitRepoUtils.commitAddedFiles(assetFolder, comment, author, email);;
+
+		return NounMetadata.getSuccessNounMessage("Success!");
+	}
+
+}
