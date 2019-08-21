66675055ddada7cce38a5e87eabcf2d39a978153
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-08-21 14:55:05 +0000

    UPDATE ::: Adding BrowseAsset and SearchAsset with asset metadata
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34344 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f48f8d174b48099d7c9e1de1d0e69ed2c6169947

---
+++ src/prerna/util/git/reactors/BrowseAssetReactor.java
@@ -0,0 +1,34 @@
+package prerna.util.git.reactors;
+
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.git.GitAssetUtils;
+
+public class BrowseAssetReactor extends AbstractReactor {
+
+	// pulls the latest for this project / asset
+	// the asset is basically the folder where it sits
+	// this can be used enroute in a pipeline
+
+	public BrowseAssetReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.FILE_PATH.getKey()};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+
+		String assetFolder = this.insight.getInsightFolder();
+		assetFolder = assetFolder.replaceAll("\\\\", "/");
+		String locFolder = assetFolder;
+		if(keyValue.containsKey(keysToGet[0])) {
+			locFolder = assetFolder + "/" + keyValue.get(keysToGet[0]);
+		}
+
+		return new NounMetadata(GitAssetUtils.getAssetMetadata(locFolder, assetFolder), PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.OPERATION);
+	}
+
+}
