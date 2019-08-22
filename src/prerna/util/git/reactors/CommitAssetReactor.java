577218899bbed908cde28fa43429697ce311ec1f
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-08-22 14:02:10 +0000

    UPDATE ::: CommitAsset
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34354 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6cb952566219752cf9ccb7c159a13a26b69b5d9c

---
+++ src/prerna/util/git/reactors/CommitAssetReactor.java
@@ -0,0 +1,50 @@
+package prerna.util.git.reactors;
+
+import java.util.List;
+import java.util.Vector;
+
+import prerna.auth.User;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.git.GitRepoUtils;
+
+public class CommitAssetReactor extends AbstractReactor {
+
+	public CommitAssetReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.COMMENT_KEY.getKey() };
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		User user = this.insight.getUser();
+		
+		String comment = this.keyValue.get(this.keysToGet[1]);
+
+		// get asset base folder to create relative path
+		String assetFolder = this.insight.getInsightFolder();
+		assetFolder = assetFolder.replaceAll("\\\\", "/");
+
+		// get path
+		String filePath = assetFolder;
+		if (keyValue.containsKey(keysToGet[0])) {
+			filePath = assetFolder + "/" + keyValue.get(keysToGet[0]);
+			filePath = filePath.replaceAll("\\\\", "/");
+		}
+		
+		// add file to git
+		List<String> files = new Vector<>();
+		files.add(filePath);
+		GitRepoUtils.addSpecificFiles(assetFolder, files);
+
+		String author = this.insight.getUserId();
+		// TODO how can I get the user email
+		String email = null;
+		// commit it
+		GitRepoUtils.commitAddedFiles(assetFolder, comment, author, email);
+
+		return NounMetadata.getSuccessNounMessage("Success!");
+	}
+
+}
