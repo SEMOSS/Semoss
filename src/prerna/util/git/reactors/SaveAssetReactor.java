2dddadbe1db5a65e0d2b17868113054b6c8b28c8
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-08-23 14:35:09 +0000

    UPDATE ::: Committing files in SaveAsset call
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34365 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/git/reactors/SaveAssetReactor.java
+++ src/prerna/util/git/reactors/SaveAssetReactor.java
@@ -2,16 +2,21 @@ package prerna.util.git.reactors;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Vector;
 
 import org.apache.commons.io.FileUtils;
 
+import prerna.auth.User;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.execptions.SemossPixelException;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.util.Utility;
 import prerna.util.git.GitFetchUtils;
+import prerna.util.git.GitRepoUtils;
 
 public class SaveAssetReactor extends AbstractReactor {
 
@@ -20,34 +25,53 @@ public class SaveAssetReactor extends AbstractReactor {
 	// this can be used enroute in a pipeline
 	
 	public SaveAssetReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.FILE_NAME.getKey(), ReactorKeysEnum.CONTENT.getKey()};
-		this.keyRequired = new int[]{1,1};
+		this.keysToGet = new String[]{ReactorKeysEnum.FILE_NAME.getKey(), ReactorKeysEnum.CONTENT.getKey(), ReactorKeysEnum.COMMENT_KEY.getKey()};
+		this.keyRequired = new int[]{1,1,0};
 	}
 
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
+		User user = this.insight.getUser();
+		
+		String comment = this.keyValue.get(this.keysToGet[2]);
 
-		String assetFolder = this.insight.getInsightFolder(); // we need it where this would be the cache
+		// get asset base folder to create relative path
+		String assetFolder = this.insight.getInsightFolder();
 		assetFolder = assetFolder.replaceAll("\\\\", "/");
 		
 		// I need to do the job of creating this directory i.e. the name of the repo
 		// TBD
 		
-		String fileName = assetFolder + "/" + keyValue.get(keysToGet[0]);
+		String fileName = keyValue.get(keysToGet[0]);
+		String filePath =  assetFolder + "/" + fileName;
 		String content = keyValue.get(keysToGet[1]);
 
 		content = Utility.decodeURIComponent(content);
-		File file = new File(fileName);
+		File file = new File(filePath);
 		try {
 			FileUtils.writeStringToFile(file, content);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			NounMetadata error = NounMetadata.getErrorNounMessage("Unable to save file");
+			SemossPixelException exception = new SemossPixelException(error);
+			exception.setContinueThreadOfExecution(false);
+			throw exception;
 		}
 		
-		
-		return new NounMetadata("Success!", PixelDataType.CONST_STRING, PixelOperationType.OPERATION);
+		// add file to git
+		List<String> files = new Vector<>();
+		files.add(filePath);
+		GitRepoUtils.addSpecificFiles(assetFolder, files);
+
+		String author = this.insight.getUserId();
+		// TODO how can I get the user email
+		String email = null;
+		// commit it
+		GitRepoUtils.commitAddedFiles(assetFolder, comment, author, email);
+
+		return NounMetadata.getSuccessNounMessage("Success!");
 	}
 
 }
