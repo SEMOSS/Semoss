e64f49a24728b0b67d153c7bc241008b40155b47
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-08-26 15:14:54 +0000

    UPDATE ::: Standard keys for GetAssetReactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34382 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/git/reactors/GetAssetReactor.java
+++ src/prerna/util/git/reactors/GetAssetReactor.java
@@ -2,26 +2,26 @@ package prerna.util.git.reactors;
 
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.util.git.GitRepoUtils;
 
 public class GetAssetReactor extends AbstractReactor {
 
-
 	// gets a particular asset in a particular version
 	// if the version is not provided - this gets the head
-	
+
 	public GetAssetReactor() {
-		this.keysToGet = new String[]{"asset", "version"};
-		this.keyRequired = new int[]{1,0};
+		this.keysToGet = new String[] { ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.VERSION.getKey() };
+		this.keyRequired = new int[] { 1, 0 };
 	}
 
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
 
-		String assetFolder = this.insight.getInsightFolder(); // we need it where this would be the cache
+		String assetFolder = this.insight.getInsightFolder(); 
 		assetFolder = assetFolder.replaceAll("\\\\", "/");
 		
 		// I need to do the job of creating this directory i.e. the name of the repo
@@ -29,14 +29,15 @@ public class GetAssetReactor extends AbstractReactor {
 
 		String asset = keyValue.get(keysToGet[0]);
 		String version = null;
-		
-		if(keyValue.containsKey(keysToGet[1]))
+
+		if (keyValue.containsKey(keysToGet[1])) {
 			version = keyValue.get(keysToGet[1]);
-		
+		}
+
 		// I need a better way than output
 		// probably write the file and volley the file ?
 		String output = GitRepoUtils.getFile(version, asset, assetFolder);
-		
+
 		return new NounMetadata(output, PixelDataType.CONST_STRING, PixelOperationType.OPERATION);
 	}
 
