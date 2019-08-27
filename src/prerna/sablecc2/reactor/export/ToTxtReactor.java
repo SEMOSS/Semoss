fa56cf427712076fdcd187afb5b8e55363cf2c71
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-08-27 14:46:49 +0000

    UPDATE ::: ToCsv, ToExcel, ToTxt, ToTsv can write to a filepath if a user specifies it or it will be downloaded with the FE download triggered
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34394 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/export/ToTxtReactor.java
+++ src/prerna/sablecc2/reactor/export/ToTxtReactor.java
@@ -16,24 +16,34 @@ public class ToTxtReactor extends AbstractExportTxtReactor {
 	private static final String CLASS_NAME = ToTxtReactor.class.getName();
 	
 	public ToTxtReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.TASK.getKey(), ReactorKeysEnum.DELIMITER.getKey()};
+		this.keysToGet = new String[]{ReactorKeysEnum.TASK.getKey(), ReactorKeysEnum.DELIMITER.getKey(), ReactorKeysEnum.FILE_PATH.getKey()};
 	}
 	
 	@Override
 	public NounMetadata execute() {
+		organizeKeys();
 		this.logger = getLogger(CLASS_NAME);
 		this.task = getTask();
 		// get the input delimiter
 		this.setDelimiter(getDelimiter());
+		NounMetadata retNoun = null;
 		// get a random file name
 		String randomKey = UUID.randomUUID().toString();
-		this.fileLocation = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + DIR_SEPARATOR + randomKey + ".txt";
+		// grab file path to write the file
+		this.fileLocation = this.keyValue.get(ReactorKeysEnum.FILE_PATH.getKey());
+		// if the file location is not defined generate a random path and set
+		// location so that the front end will download
+		if (this.fileLocation == null) {
+			this.fileLocation = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + DIR_SEPARATOR + randomKey + ".txt";
+			// store it in the insight so the FE can download it
+			// only from the given insight
+			this.insight.addExportFile(randomKey, this.fileLocation);
+			retNoun = new NounMetadata(randomKey, PixelDataType.CONST_STRING, PixelOperationType.FILE_DOWNLOAD);
+		} else {
+			retNoun = NounMetadata.getSuccessNounMessage("Successfully generated the txt file.");
+		}
 		buildTask();
-		
-		// store it in the insight so the FE can download it
-		// only from the given insight
-		this.insight.addExportFile(randomKey, this.fileLocation);
-		return new NounMetadata(randomKey, PixelDataType.CONST_STRING, PixelOperationType.FILE_DOWNLOAD);
+		return retNoun;
 	}
 	
 	private String getDelimiter() {
