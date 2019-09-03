3dca7b07e2108ab1d2974b117b999557e0404556
mahkhalil <mahkhalil@deloitte.com>
2019-09-03 16:59:29 +0000

    BUGFIX ::: Using new method for select table reactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34527 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/qs/selectors/SelectTableReactor.java
+++ src/prerna/sablecc2/reactor/qs/selectors/SelectTableReactor.java
@@ -1,12 +1,9 @@
 package prerna.sablecc2.reactor.qs.selectors;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import prerna.nameserver.utility.MasterDatabaseUtility;
 import prerna.query.querystruct.AbstractQueryStruct;
-import prerna.query.querystruct.SelectQueryStruct;
 import prerna.query.querystruct.selectors.QueryColumnSelector;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.reactor.qs.AbstractQueryStructReactor;
@@ -28,26 +25,9 @@ public class SelectTableReactor extends AbstractQueryStructReactor {
 		}
 		String table  = this.keyValue.get(ReactorKeysEnum.TABLE.getKey());
 		
-		// add the table in the list
-		List<String> tables = new ArrayList<String>();
-		tables.add(table);
-		
-		Map<String, List<String>> props = MasterDatabaseUtility.getConceptProperties(tables, appId);
-		List<String> properties =  props.get(table);
-		if (properties != null && !properties.isEmpty()) {
-			for (String column : properties) {
-				QueryColumnSelector qsSelector = new QueryColumnSelector();
-				qsSelector.setTable(table);
-				qsSelector.setColumn(column);
-				qsSelector.setAlias(column);
-				qs.addSelector(qsSelector);
-			}
-			
-			// add prim key
-			QueryColumnSelector qsSelector = new QueryColumnSelector();
-			qsSelector.setTable(table);
-			qsSelector.setColumn(SelectQueryStruct.PRIM_KEY_PLACEHOLDER);
-			qsSelector.setAlias(table);
+		List<String> selectors = MasterDatabaseUtility.getConceptPixelSelectors(table, appId);
+		for(int i = 0; i < selectors.size(); i++) {
+			QueryColumnSelector qsSelector = new QueryColumnSelector(selectors.get(i));
 			qs.addSelector(qsSelector);
 		}
 		
