71bed71938c3b3535258e157253a0d176d6b7298
mahkhalil <mahkhalil@deloitte.com>
2019-09-03 21:15:28 +0000

    UPDATE ::: Pushing NLP with modifications to local master - some more modifications will be committed around updating existing instance values
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34538 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2b0c0714790258c622b35e513056dfae2d67affa

--- src/prerna/sablecc2/reactor/algorithms/StoreColumnValuesReactor.java
+++ src/prerna/sablecc2/reactor/algorithms/StoreColumnValuesReactor.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Vector;
+
 import prerna.algorithm.api.SemossDataType;
 import prerna.ds.r.RSyntaxHelper;
 import prerna.engine.api.IEngine;
 import prerna.engine.api.IRawSelectWrapper;
+import prerna.engine.api.impl.util.MetadataUtility;
 import prerna.nameserver.utility.MasterDatabaseUtility;
 import prerna.query.querystruct.SelectQueryStruct;
 import prerna.query.querystruct.filters.SimpleQueryFilter;
@@ -24,6 +26,7 @@ import prerna.sablecc2.reactor.frame.r.AbstractRFrameReactor;
 import prerna.util.DIHelper;
 import prerna.util.Utility;
 
+//TODO: change to NLPInstanceCacheReactor
 public class StoreColumnValuesReactor extends AbstractRFrameReactor {
 
 	/**
@@ -33,6 +36,7 @@ public class StoreColumnValuesReactor extends AbstractRFrameReactor {
 	// StoreColumnValues(app=["2c8c41da-391a-4aa8-a170-9925211869c8"],table=[],columns=[],addDuplicates=[false]);
 	// StoreColumnValues(app=["2c8c41da-391a-4aa8-a170-9925211869c8"],table=["Title"],columns=["Title"],addDuplicates=[false]);
 
+	// TODO: change addDuplicates to updateExistingValues
 	private static final String ADD_DUPLICATES = "addDuplicates";
 	protected static final String CLASS_NAME = StoreColumnValuesReactor.class.getName();
 
@@ -45,26 +49,28 @@ public class StoreColumnValuesReactor extends AbstractRFrameReactor {
 		// initialize inputs
 		init();
 		organizeKeys();
-		String baseFolder = DIHelper.getInstance().getProperty("BaseFolder").replace("\\", "/");
 		String appId = UploadInputUtility.getAppName(this.store);
 		String table = this.keyValue.get(this.keysToGet[1]);
-		List<String> colFilters = getSpecificColumns(appId, table);
-		boolean addDuplicates = getDuplicate();
-		boolean allCols = false;
-		String retFrame = "retFrame_" + Utility.getRandomString(5);
+		List<String> columnsToUpdate = getSpecificColumns(appId, table);
+		boolean updateExistingValues = updateExistingValues();
+		boolean allValues = false;
 		
 		// if no columns were entered, default to all string columns
-		if(colFilters == null || colFilters.isEmpty()) {
-			colFilters = getStringColumns(appId);
-			allCols = true;
+		if(columnsToUpdate == null || columnsToUpdate.isEmpty()) {
+			allValues = true;
 		}
 		
 		// check for rds file
+		String baseFolder = DIHelper.getInstance().getProperty("BaseFolder").replace("\\", "/");
 		String filePath = baseFolder + "/R/AnalyticsRoutineScripts/unique-values-table.rds";
 		File rdsFile = new File(filePath);
 		String rdsFrame = "rdsFrame_" + Utility.getRandomString(5);
 		boolean rdsExists = rdsFile.exists();
 
+		//TODO: need to fix the rdsExist
+		//TODO: need to delete with table name
+		//TODO: if you are allValues, then delete just based on appID
+		
 		// if the rds exists, the filter out the existing cols from colFilters if
 		// desired
 		if (rdsExists) {
@@ -73,10 +79,10 @@ public class StoreColumnValuesReactor extends AbstractRFrameReactor {
 
 			// if updating existing, then remove the current values
 			// otherwise, skip the duplicate columns to improve performance
-			if (!addDuplicates) {
+			if (!updateExistingValues) {
 				String existingColsScript = "unique(" + rdsFrame + "[" + rdsFrame + "$AppID == \"" + appId + "\",]$Column);";
 				String[] existingCols = this.rJavaTranslator.getStringArray(existingColsScript);
-				colFilters.removeAll(Arrays.asList(existingCols));
+				columnsToUpdate.removeAll(Arrays.asList(existingCols));
 			} else {
 				// get the current columns that we have in that app
 				String existingColsScript = "unique(" + rdsFrame + "[" + rdsFrame + "$AppID == \"" + appId + "\",]$Column);";
@@ -84,7 +90,7 @@ public class StoreColumnValuesReactor extends AbstractRFrameReactor {
 				
 				// get the intersection of existing columns and columns inputted
 				ArrayList<String> colsToRemove = new ArrayList<String>(existingCols);
-				colsToRemove.retainAll(colFilters);
+				colsToRemove.retainAll(columnsToUpdate);
 				
 				// now remove the current ones that we are about to replace with this script
 				String removeScript = rdsFrame + " <- " + rdsFrame + "[( ";
@@ -99,111 +105,89 @@ public class StoreColumnValuesReactor extends AbstractRFrameReactor {
 			}
 		}
 		
-		if(colFilters.isEmpty()) {
+		if(columnsToUpdate.isEmpty()) {
 			return new NounMetadata(true, PixelDataType.BOOLEAN);
 		}
 
 		// store and change working directory
+		String retFrame = "retFrame_" + Utility.getRandomString(5);
+
 		StringBuilder setupBuilder = new StringBuilder();
 		setupBuilder.append("origDir <- getwd();");
 		setupBuilder.append("setwd(\"" + baseFolder.replace("\\", "/") + "/R/AnalyticsRoutineScripts/\");");
-		setupBuilder.append(retFrame
-				+ " <- data.frame(AppID = character(), Table = character(), Column = character(), Value = character() , stringsAsFactors = FALSE);");
+		setupBuilder.append(retFrame + " <- data.frame(AppID = character(), Table = character(), Column = character(), Value = character() , stringsAsFactors = FALSE);");
 		this.rJavaTranslator.runR(setupBuilder.toString());
-		int colCounter = 0;
 
 		String tempFrame = "tempFrame_" + Utility.getRandomString(5);
-
 		// create empty vectors that will populate the temp dfs
 		List<String> appIdVector = new ArrayList<String>();
 		List<String> tableVector = new ArrayList<String>();
 		List<String> colVector = new ArrayList<String>();
 		List<String> valueVector = new ArrayList<String>();
 
-		// to check for a new table
-		String prevTable = "";
-
+		// a concept (or table) in RDBMS/R has no meaning - the data is in the properties (columns)
+		IEngine engine = Utility.getEngine(appId);
+		boolean ignoreData = MetadataUtility.ignoreConceptData(engine.getEngineType());
+		
 		// loop through each column with in the app
-		for (String col : colFilters) {
-			// init variables for the col
-			SelectQueryStruct qs = new SelectQueryStruct();
-			IEngine engine = Utility.getEngine(appId);
-			colCounter++;
-			
-			// if it is all columns, then get the specific columns table
-			// otherwise use the entered table
-			if(allCols) {
-				table = MasterDatabaseUtility.getTableForColumn(appId, col);
-				if(table == null) {
-					table = col;
+		if(allValues) {
+			int counter = 0;
+			List<Object[]> allTableCols = MasterDatabaseUtility.getAllTablesAndColumns(appId);
+			for(Object[] tableCol : allTableCols) {
+				// only care about string values
+				if(!tableCol[2].equals(SemossDataType.STRING.toString())) {
+					continue;
 				}
+				String parent = tableCol[0].toString();
+				String name = tableCol[1].toString();
+				String colQs = null;
+				if(parent == null) {
+					colQs = name;
+				} else {
+					colQs = parent + "__" + name;
+				}
+				
+				counter++;
+				if(counter % 20 == 0) {
+					// add the info
+					addTable(retFrame, tempFrame, appIdVector, tableVector, colVector, valueVector);
+					
+					// reset everything to start process over
+					appIdVector.clear();
+					tableVector.clear();
+					colVector.clear();
+					valueVector.clear();
+					counter = 0;
+				}
+				
+				processSingleColumn(engine, colQs, appId, appIdVector, tableVector, colVector, valueVector);
 			}
 			
-			// we will fill this in once we figure out if it is a concept or property
-			QueryColumnSelector colSelector = null;
-			if (table.equals(col)) {
-				colSelector = new QueryColumnSelector(col);
-			} else {
-				colSelector = new QueryColumnSelector(table + "__" + col);
-			}
-			qs.addSelector(colSelector);
-
-			// select only non-null values from database
-			SimpleQueryFilter nulls = new SimpleQueryFilter(new NounMetadata(colSelector, PixelDataType.COLUMN), "!=",
-					new NounMetadata("null", PixelDataType.NULL_VALUE));
-			qs.addExplicitFilter(nulls);
-
-			// select distinct values
-			qs.setDistinct(true);
-
-			// get the data for the db
-			IRawSelectWrapper iterator = null;
-			try {
-				iterator = WrapperManager.getInstance().getRawWrapper(engine, qs);
-			} catch (Exception e) {
-				continue;
-			}
-
-			// all values are null in this column so continue
-			if (!iterator.hasNext()) {
-				continue;
-			}
-
-			// if the column is starting a new table, rbind to current data frame
-			if (!table.equals(prevTable)) {
-				prevTable = table;
-				addTable(retFrame, tempFrame, appIdVector, tableVector, colVector, valueVector);
-
-				// reset everything to start process over
-				appIdVector.clear();
-				tableVector.clear();
-				colVector.clear();
-				valueVector.clear();
-			}
-
-			// for each value, create a row in our data table (i.e. add to the vector)
-			while (iterator.hasNext()) {
-				appIdVector.add(appId);
-				tableVector.add(table);
-				colVector.add(col);
-
-				// only add the first 50 characters of a value
-				String value = iterator.next().getValues()[0].toString();
-				if (value.length() > 50) {
-					value = value.substring(0, 49);
-				}
-				valueVector.add(value);
-			}
-
-			// if it is the last column, rbind the last table
-			if (colCounter == colFilters.size()) {
+			// process anything left over
+			if(counter > 0) {
 				addTable(retFrame, tempFrame, appIdVector, tableVector, colVector, valueVector);
 			}
+		} else {
+			if(!ignoreData) {
+				// we have a RDF/Graph database where we need to index the concept itself
+				
+				String colQs = table;
+				processSingleColumn(engine, colQs, appId, appIdVector, tableVector, colVector, valueVector);
+			}
+			
+			// loop through the passed in columns
+			for (String col : columnsToUpdate) {
+				// now add all the other columns
+				String colQs = table + "__" + col;
+				processSingleColumn(engine, colQs, appId, appIdVector, tableVector, colVector, valueVector);
+			}
+			
+			// add table at the end since we only do 1 table when specifying columns
+			addTable(retFrame, tempFrame, appIdVector, tableVector, colVector, valueVector);
 		}
 
 		// create gc string
 		String gc = retFrame + "," + tempFrame;
-
 		// now save it to an RDS Files
 		StringBuilder saveBuilder = new StringBuilder();
 		if (rdsExists) {
@@ -224,8 +208,119 @@ public class StoreColumnValuesReactor extends AbstractRFrameReactor {
 		noun.addAdditionalReturn(new NounMetadata("Successfully indexed data for NLP searching", PixelDataType.CONST_STRING, PixelOperationType.SUCCESS));
 		return noun;
 	}
-
 	
+	private void processSingleColumn(IEngine engine,
+			String colQs, 
+			String appId,
+			List<String> appIdVector, 
+			List<String> tableVector,
+			List<String> colVector, 
+			List<String> valueVector) {
+		// init variables for the col
+		SelectQueryStruct qs = new SelectQueryStruct();
+		
+		QueryColumnSelector colSelector = new QueryColumnSelector(colQs);
+		qs.addSelector(colSelector);
+
+		// select only non-null values from database
+		qs.addExplicitFilter(SimpleQueryFilter.makeColToValFilter(colSelector, "!=", null, PixelDataType.NULL_VALUE));
+
+		// select distinct values
+		qs.setDistinct(true);
+
+		// get the data for the db
+		IRawSelectWrapper iterator = null;
+		try {
+			iterator = WrapperManager.getInstance().getRawWrapper(engine, qs);
+		} catch (Exception e) {
+			return;
+		}
+
+		// all values are null in this column so continue
+		if (!iterator.hasNext()) {
+			return;
+		}
+
+		// for each value, create a row in our data table (i.e. add to the vector)
+		while (iterator.hasNext()) {
+			appIdVector.add(appId);
+			if(colQs.contains("__")) {
+				String[] split = colQs.split("__");
+				tableVector.add(split[0]);
+				tableVector.add(split[1]);
+			} else {
+				tableVector.add(colQs);
+				colVector.add(colQs);
+			}
+
+			// only add the first 50 characters of a value
+			String value = iterator.next().getValues()[0].toString();
+			if (value.length() > 50) {
+				value = value.substring(0, 49);
+			}
+			valueVector.add(value);
+		}
+	}
+	
+	/**
+	 * Process the table
+	 * @param rdsFrame
+	 * @param tempFrame
+	 * @param appIdVector
+	 * @param tableVector
+	 * @param colVector
+	 * @param valueVector
+	 */
+	private void addTable(String rdsFrame, String tempFrame, List<String> appIdVector, List<String> tableVector,
+			List<String> colVector, List<String> valueVector) {
+		// run this one separately to avoid the string getting too long
+		String valueVectorName = "valueVector_" + Utility.getRandomString(5);
+		String vectorScript = RSyntaxHelper.createStringRColVec(valueVector);
+		String gc = valueVectorName;
+
+		// if the values vector is too large, break it into doable pieces
+		if (vectorScript.length() > 200000) {
+			// set up two vectors to run
+			String valueVectorName1 = "valueVector1_" + Utility.getRandomString(5);
+			String valueVectorName2 = "valueVector2_" + Utility.getRandomString(5);
+
+			// find that midpoint comma
+			int index = vectorScript.indexOf("\",\"", vectorScript.length() / 2);
+
+			// create the first half of the vector
+			this.rJavaTranslator.runR(valueVectorName1 + " <- " + vectorScript.substring(0, index) + "\");");
+
+			// create the second half of the vector
+			this.rJavaTranslator.runR(valueVectorName2 + " <- c(" + vectorScript.substring(index + 2, vectorScript.length()) + ";");
+
+			// merge them together
+			this.rJavaTranslator.runR(valueVectorName + " <- c(" + valueVectorName1 + "," + valueVectorName2 + ");");
+
+			// add those two to the gc
+			gc += "," + valueVectorName1 + "," + valueVectorName2;
+		} else {
+			// just add the new vector name and run it
+			vectorScript = valueVectorName + " <- " + vectorScript + ";";
+			this.rJavaTranslator.runR(vectorScript);
+		}
+
+		// rbind to current frame
+		String addTableScript = tempFrame + " <- data.frame(AppID = " + RSyntaxHelper.createStringRColVec(appIdVector)
+				+ " , Table = " + RSyntaxHelper.createStringRColVec(tableVector) + " , Column = "
+				+ RSyntaxHelper.createStringRColVec(colVector) + " , Value = " + valueVectorName
+				+ ", stringsAsFactors = FALSE);" + rdsFrame + " <- rbind(" + rdsFrame + "," + tempFrame + ");";
+		this.rJavaTranslator.runR(addTableScript);
+
+		// garbage cleanup
+		this.rJavaTranslator.executeEmptyR("rm( " + gc + ");");
+	}
+	
+	/**
+	 * Get specific string columns in the table - make sure they are strings
+	 * @param appId
+	 * @param table
+	 * @return
+	 */
 	private List<String> getSpecificColumns(String appId, String table) {
 		// see if defined as individual key
 		GenRowStruct columnGrs = this.store.getNoun(this.keysToGet[2]);
@@ -251,70 +346,15 @@ public class StoreColumnValuesReactor extends AbstractRFrameReactor {
 		return null;
 	}
 	
-	
-	private List<String> getStringColumns(String appId) {
-		List<String> retCols = new ArrayList<String>();
-		List<Object[]> allTableCols = MasterDatabaseUtility.getAllTablesAndColumns(appId);
-		for(Object[] tableCol : allTableCols) {
-			if(tableCol[2].equals(SemossDataType.STRING.toString())) {
-				retCols.add(tableCol[1].toString());
-			}
-		}
-		return retCols;
-	}
-
-	private boolean getDuplicate() {
+	/**
+	 * Determine if we should override existing values
+	 * @return
+	 */
+	private boolean updateExistingValues() {
 		GenRowStruct grs = this.store.getNoun(this.keysToGet[3]);
 		if (grs == null || grs.isEmpty()) {
 			return false;
 		}
 		return (boolean) grs.get(0);
-	}
-
-	private void addTable(String rdsFrame, String tempFrame, List<String> appIdVector, List<String> tableVector,
-			List<String> colVector, List<String> valueVector) {
-		// run this one separately to avoid the string getting too long
-		String valueVectorName = "valueVector_" + Utility.getRandomString(5);
-		String vectorScript = RSyntaxHelper.createStringRColVec(valueVector);
-		String gc = valueVectorName;
-
-		// if the values vector is too large, break it into doable pieces
-		if (vectorScript.length() > 200000) {
-			// set up two vectors to run
-			String valueVectorName1 = "valueVector1_" + Utility.getRandomString(5);
-			String valueVectorName2 = "valueVector2_" + Utility.getRandomString(5);
-
-			// find that midpoint comma
-			int index = vectorScript.indexOf("\",\"", vectorScript.length() / 2);
-
-			// create the first half of the vector
-			this.rJavaTranslator.runR(valueVectorName1 + " <- " + vectorScript.substring(0, index) + "\");");
-
-			// create the second half of the vector
-			this.rJavaTranslator
-					.runR(valueVectorName2 + " <- c(" + vectorScript.substring(index + 2, vectorScript.length()) + ";");
-
-			// merge them together
-			this.rJavaTranslator.runR(valueVectorName + " <- c(" + valueVectorName1 + "," + valueVectorName2 + ");");
-
-			// add those two to the gc
-			gc += "," + valueVectorName1 + "," + valueVectorName2;
-
-		} else {
-			// just add the new vector name and run it
-			vectorScript = valueVectorName + " <- " + vectorScript + ";";
-			this.rJavaTranslator.runR(vectorScript);
-		}
-
-		// rbind to current frame
-		String addTableScript = tempFrame + " <- data.frame(AppID = " + RSyntaxHelper.createStringRColVec(appIdVector)
-				+ " , Table = " + RSyntaxHelper.createStringRColVec(tableVector) + " , Column = "
-				+ RSyntaxHelper.createStringRColVec(colVector) + " , Value = " + valueVectorName
-				+ ", stringsAsFactors = FALSE);" + rdsFrame + " <- rbind(" + rdsFrame + "," + tempFrame + ");";
-		this.rJavaTranslator.runR(addTableScript);
-
-		// garbage cleanup
-		this.rJavaTranslator.executeEmptyR("rm( " + gc + ");");
-
 	}
 }
