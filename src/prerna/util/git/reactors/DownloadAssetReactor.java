648eff03acf07cbb09dcb674739a1531eecc5746
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-09-04 18:13:43 +0000

    UPDATE ::: DownloadAssetReactor allow downloading directories
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34559 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: cedd161f62f96e182e4aed95a74c15304266f1de

--- src/prerna/util/git/reactors/DownloadAssetReactor.java
+++ src/prerna/util/git/reactors/DownloadAssetReactor.java
@@ -1,5 +1,6 @@
 package prerna.util.git.reactors;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
@@ -8,6 +9,7 @@ import java.util.zip.ZipOutputStream;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.execptions.SemossPixelException;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.util.Constants;
@@ -26,36 +28,65 @@ public class DownloadAssetReactor extends AbstractReactor {
 		// get base asset folder
 		String assetFolder = this.insight.getInsightFolder();
 		assetFolder = assetFolder.replaceAll("\\\\", "/");
-		String randomKey = UUID.randomUUID().toString();
 		String downloadPath = assetFolder;
+		// create path for a zip file
+		String randomKey = UUID.randomUUID().toString();
+		String OUTPUT_PATH = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + DIR_SEPARATOR + "export"
+				+ DIR_SEPARATOR + "ZIPs";
+		String zipPath = OUTPUT_PATH + DIR_SEPARATOR + this.insight.getInsightId() + "__" + randomKey + ".zip";
 		// if a specific file is specified for download
-		if (keyValue.containsKey(keysToGet[0])) {
-			downloadPath = assetFolder + "/" + keyValue.get(keysToGet[0]);
-		} else {
-			// zip and download asset dir
-			String OUTPUT_PATH = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + "/export/ZIPs";
-			downloadPath = OUTPUT_PATH + this.insight.getInsightId() + "__" + randomKey + ".zip";
-			ZipOutputStream zos = null;
-			try {
-				zos = ZipUtils.zipFolder(assetFolder, downloadPath);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				try {
-					if (zos != null) {
-						zos.flush();
-						zos.close();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+		String relativeAssetPath = keyValue.get(keysToGet[0]);
+		if (relativeAssetPath != null && relativeAssetPath.length() > 0) {
+			downloadPath = assetFolder + DIR_SEPARATOR + relativeAssetPath;
+			File assetToDownload = new File(downloadPath);
+			if (!assetToDownload.exists()) {
+				NounMetadata error = NounMetadata.getErrorNounMessage("File does not exist");
+				SemossPixelException exception = new SemossPixelException(error);
+				exception.setContinueThreadOfExecution(false);
+				throw exception;
 			}
+			// if the file to download is a dir it will be zipped
+			if (assetToDownload.isDirectory()) {
+				// zip asset folder to the zip path
+				zipFolder(downloadPath, zipPath);
+				// set download path to the zipPath
+				downloadPath = zipPath;
+			}
+
+		} else {
+			// zip asset folder to the zip path
+			zipFolder(assetFolder, zipPath);
+			downloadPath = zipPath;
 		}
 		this.insight.addExportFile(randomKey, downloadPath);
 		return new NounMetadata(randomKey, PixelDataType.CONST_STRING, PixelOperationType.FILE_DOWNLOAD);
 
+	}
+
+	private void zipFolder(String folder, String downloadPath) {
+		ZipOutputStream zos = null;
+		try {
+			zos = ZipUtils.zipFolder(folder, downloadPath);
+		} catch (IOException e) {
+			e.printStackTrace();
+			NounMetadata error = NounMetadata.getErrorNounMessage("Unable to zip and download");
+			SemossPixelException exception = new SemossPixelException(error);
+			exception.setContinueThreadOfExecution(false);
+			throw exception;
+		} finally {
+			try {
+				if (zos != null) {
+					zos.flush();
+					zos.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+				NounMetadata error = NounMetadata.getErrorNounMessage("Unable to zip and download");
+				SemossPixelException exception = new SemossPixelException(error);
+				exception.setContinueThreadOfExecution(false);
+				throw exception;
+			}
+		}
 	}
 
 }
