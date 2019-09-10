71af8bbe26fc041db5c31f2f420c3f2c59ca48a1
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-09-10 14:19:57 +0000

    UPDATE ::: Grabbing username/email for CommitAssetReactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34654 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/git/reactors/CommitAssetReactor.java
+++ src/prerna/util/git/reactors/CommitAssetReactor.java
@@ -3,7 +3,9 @@ package prerna.util.git.reactors;
 import java.util.List;
 import java.util.Vector;
 
+import prerna.auth.AccessToken;
 import prerna.auth.User;
+import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
@@ -12,50 +14,46 @@ import prerna.util.git.GitRepoUtils;
 public class CommitAssetReactor extends AbstractReactor {
 
 	public CommitAssetReactor() {
-		this.keysToGet = new String[] { ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.COMMENT_KEY.getKey(), ReactorKeysEnum.IN_APP.getKey() };
+		this.keysToGet = new String[] { ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.COMMENT_KEY.getKey(),
+				ReactorKeysEnum.IN_APP.getKey() };
 	}
 
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
 		User user = this.insight.getUser();
-		
+		String author = null;
+		String email = null;
+		// check if user is logged in
+		if (AbstractSecurityUtils.securityEnabled()) {
+			if (AbstractSecurityUtils.anonymousUsersEnabled() && user.isAnonymous()) {
+				throwAnonymousUserError();
+			}
+			// Get the user's email
+			AccessToken accessToken = user.getAccessToken(user.getPrimaryLogin());
+			email = accessToken.getEmail();
+			author = accessToken.getUsername();
+		}
+		String filePath = this.keyValue.get(this.keysToGet[0]);
 		String comment = this.keyValue.get(this.keysToGet[1]);
 
-		// get asset base folder to create relative path
-		//boolean app = keyValue.containsKey(keysToGet[2]);
-		boolean app = keyValue.containsKey(keysToGet[2]) || (keyValue.containsKey(keysToGet[0]) && keyValue.get(keysToGet[0]).startsWith("app_assets"));
-
-		// get the asset folder path
-		String assetFolder = this.insight.getInsightFolder(); 
-		if(app)
+		// get insight asset path
+		boolean app = keyValue.containsKey(keysToGet[2])
+				|| (keyValue.containsKey(keysToGet[0]) && keyValue.get(keysToGet[0]).startsWith("app_assets"));
+		String assetFolder = this.insight.getInsightFolder();
+		if (app) {
 			assetFolder = this.insight.getAppFolder();
-		
+		}
 		assetFolder = assetFolder.replaceAll("\\\\", "/");
 
-		// get path
-		String filePath = assetFolder;
-		if (keyValue.containsKey(keysToGet[0])) {
-			filePath = assetFolder + "/" + keyValue.get(keysToGet[0]);
-			filePath = filePath.replaceAll("\\\\", "/");
-		}
-		
-		// neutraize app_assets
-		//filePath = filePath.replaceAll("app_assets", "");
-
-		
 		// add file to git
 		List<String> files = new Vector<>();
 		files.add(filePath);
 		GitRepoUtils.addSpecificFiles(assetFolder, files);
 
-		String author = this.insight.getUserId();
-		// TODO how can I get the user email
-		String email = null;
 		// commit it
 		GitRepoUtils.commitAddedFiles(assetFolder, comment, author, email);
-
 		return NounMetadata.getSuccessNounMessage("Success!");
-	}
 
+	}
 }
