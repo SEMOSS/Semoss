a046bd4a6fc6efe92132ecdd1ece7245cbc7cecc
mahkhalil <mahkhalil@deloitte.com>
2019-09-10 15:37:07 +0000

    UPDATE ::: Adding python thread to session and grabbing when appropriate UPDATE ::: Adding ROOT variable in python to be used UPDATE ::: Removing unnecessary direct execution and instead using the base execution
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34658 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/r/util/RSourceReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/util/RSourceReactor.java
@@ -1,12 +1,9 @@
 package prerna.sablecc2.reactor.frame.r.util;
 
-import java.io.File;
-
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
-import prerna.util.DIHelper;
 
 public class RSourceReactor extends AbstractReactor {
 
@@ -18,37 +15,14 @@ public class RSourceReactor extends AbstractReactor {
 	public NounMetadata execute() {
 		this.organizeKeys();
 		String relativePath = this.keyValue.get(this.keysToGet[0]);
-		String path = getBaseFolder() + "/R/" + relativePath;
 		
 		AbstractRJavaTranslator rJavaTranslator = this.insight.getRJavaTranslator(this.getLogger(this.getClass().getName()));
 		rJavaTranslator.startR(); 
 		
-		File file = new File(path);
+		String path = insight.getInsightFolder() + "/" + relativePath;
+		path = path.replace('\\', '/');
 		
-		// if the file is not there try in the insight
-		if(!file.exists())
-			path = insight.getInsightFolder() + "/" + relativePath;
-		path = path.replace("\\", "/");
-		
-		String env = rJavaTranslator.env;
-		
-		rJavaTranslator.executeEmptyRDirect("source(\"" + path + "\"," + env + ");");
-		
-		
+		rJavaTranslator.executeEmptyR("source(\"" + path + "\");");
 		return new NounMetadata(true, PixelDataType.BOOLEAN);
 	}
-
-	/**
-     * Get the base folder
-     * @return
-     */
-     protected String getBaseFolder() {
-          String baseFolder = null;
-          try {
-              baseFolder = DIHelper.getInstance().getProperty("BaseFolder");
-          } catch (Exception ignored) {
-              //logger.info("No BaseFolder detected... most likely running as test...");
-          }
-          return baseFolder;
-     }
 }
