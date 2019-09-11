ed2866008cd12ca792a0939bea6553be384f61ac
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-09-11 16:25:36 +0000

    UPDATE ::: Adding user asset space
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34690 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3dc9c21e553e382073ce975713e0ca0dd55bc412

--- src/prerna/util/AssetUtility.java
+++ src/prerna/util/AssetUtility.java
@@ -1,11 +1,18 @@
 package prerna.util;
 
+import java.io.File;
+
+import prerna.auth.AuthProvider;
+import prerna.auth.User;
 import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.SecurityAppUtils;
+import prerna.engine.impl.SmssUtilities;
 import prerna.nameserver.utility.MasterDatabaseUtility;
 import prerna.om.Insight;
+import prerna.util.git.GitRepoUtils;
 
 public class AssetUtility {
+	private static final String DIR_SEPARATOR = java.nio.file.FileSystems.getDefault().getSeparator();
 
 	/**
 	 * Grab the workspace to work with assets
@@ -19,7 +26,16 @@ public class AssetUtility {
 		// find out what space the user wants to use to get the base asset path
 		if (space != null) {
 			if ("USER".equalsIgnoreCase(space)) {
-				// TODO
+				if (AbstractSecurityUtils.securityEnabled()) {
+					User user = in.getUser();
+					if (AbstractSecurityUtils.anonymousUsersEnabled() && user.isAnonymous()) {
+						throw new IllegalArgumentException("Must be logged in to perform this operation");
+					}		
+					AuthProvider provider = user.getPrimaryLogin();
+					String appId = user.getAssetEngineId(provider);
+					String appName = "Asset";
+					assetFolder = getAppAssetFolder(appName, appId);
+				}
 			} else if ("INSIGHT".equalsIgnoreCase(space)) {
 				assetFolder = in.getInsightFolder();
 			} else {
@@ -32,10 +48,25 @@ public class AssetUtility {
 						throw new IllegalArgumentException("User does not have permission for this app");
 					}
 				}
-				assetFolder = in.getAppFolder(appName, appId);
+				assetFolder = getAppAssetFolder(appName, appId);
 			}
 		}
 		assetFolder = assetFolder.replaceAll("\\\\", "/");
 		return assetFolder;
+	}
+	
+	public static String getAppAssetFolder(String appName, String appId) {
+		String appFolder = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + DIR_SEPARATOR + "db"
+				+ DIR_SEPARATOR + SmssUtilities.getUniqueName(appName, appId) + DIR_SEPARATOR + "version"
+				+ DIR_SEPARATOR + "assets";
+
+		// if this folder does not exist create it and git init it
+		File file = new File(appFolder);
+		if (!file.exists()) {
+			file.mkdir();
+			GitRepoUtils.init(appFolder);
+		}
+		return appFolder;
+		
 	}
 }
