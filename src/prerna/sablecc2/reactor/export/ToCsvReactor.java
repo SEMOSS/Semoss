ece656af1875dbc19a007560389d87f10d2f93ad
mahkhalil <mahkhalil@deloitte.com>
2019-09-12 23:02:30 +0000

    UPDATE ::: Modifying so downloadFile endpoint returns the file name. UPDATE ::: Modifying To*Reactors to generate a unique id as semoss export + use the insight folder UPDATE ::: Ensuring only 1 instance of Jep is started per session
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34743 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 16668eb5f41f4d5e3585ec00ef5e2ba5c9bf0fa9

--- src/prerna/sablecc2/reactor/export/ToCsvReactor.java
+++ src/prerna/sablecc2/reactor/export/ToCsvReactor.java
@@ -1,13 +1,9 @@
 package prerna.sablecc2.reactor.export;
 
-import java.util.UUID;
-
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
-import prerna.util.Constants;
-import prerna.util.DIHelper;
 
 public class ToCsvReactor extends AbstractExportTxtReactor {
 
@@ -26,21 +22,20 @@ public class ToCsvReactor extends AbstractExportTxtReactor {
 		this.setDelimiter(",");
 		NounMetadata retNoun = null;
 		// get a random file name
-		String randomKey = UUID.randomUUID().toString();
+		String exportName = getExportFileName("csv");
 		// grab file path to write the file
 		this.fileLocation = this.keyValue.get(ReactorKeysEnum.FILE_PATH.getKey());
 		// if the file location is not defined generate a random path and set
 		// location so that the front end will download
 		if (this.fileLocation == null) {
-			this.fileLocation = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + DIR_SEPARATOR + randomKey + ".csv";
+			this.fileLocation = this.insight.getInsightFolder() + DIR_SEPARATOR + exportName;
 			// store it in the insight so the FE can download it
 			// only from the given insight
-			this.insight.addExportFile(randomKey, this.fileLocation);
-			retNoun = new NounMetadata(randomKey, PixelDataType.CONST_STRING, PixelOperationType.FILE_DOWNLOAD);
-		} else {
-			retNoun = NounMetadata.getSuccessNounMessage("Successfully generated the csv file.");
+			this.insight.addExportFile(exportName, this.fileLocation);
+			retNoun = new NounMetadata(exportName, PixelDataType.CONST_STRING, PixelOperationType.FILE_DOWNLOAD);
 		}
 		buildTask();
+		retNoun.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Successfully generated the csv file"));
 		return retNoun;
 	}
 
