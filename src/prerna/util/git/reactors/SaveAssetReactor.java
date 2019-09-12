b5b8c9b177eb1ae3fbb8ec5c0081a76aa1d84708
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-09-12 23:15:34 +0000

    BUGFIX ::: Asset reactors grabbing app version folder and masking paths
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34744 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/git/reactors/SaveAssetReactor.java
+++ src/prerna/util/git/reactors/SaveAssetReactor.java
@@ -2,12 +2,9 @@ package prerna.util.git.reactors;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.Vector;
 
 import org.apache.commons.io.FileUtils;
 
-import prerna.auth.AccessToken;
 import prerna.auth.User;
 import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.sablecc2.om.ReactorKeysEnum;
@@ -16,13 +13,8 @@ import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.util.AssetUtility;
 import prerna.util.Utility;
-import prerna.util.git.GitRepoUtils;
 
 public class SaveAssetReactor extends AbstractReactor {
-
-	// pulls the latest for this project / asset
-	// the asset is basically the folder where it sits
-	// this can be used enroute in a pipeline
 
 	public SaveAssetReactor() {
 		this.keysToGet = new String[] { ReactorKeysEnum.FILE_NAME.getKey(), ReactorKeysEnum.CONTENT.getKey(),
@@ -34,20 +26,13 @@ public class SaveAssetReactor extends AbstractReactor {
 	public NounMetadata execute() {
 		organizeKeys();
 		User user = this.insight.getUser();
-		String author = null;
-		String email = null;
 		// check if user is logged in
 		if (AbstractSecurityUtils.securityEnabled()) {
 			if (AbstractSecurityUtils.anonymousUsersEnabled() && user.isAnonymous()) {
 				throwAnonymousUserError();
 			}
-			// Get the user's email
-			AccessToken accessToken = user.getAccessToken(user.getPrimaryLogin());
-			email = accessToken.getEmail();
-			author = accessToken.getUsername();
 		}
 
-		String comment = this.keyValue.get(this.keysToGet[2]);
 		String space = this.keyValue.get(this.keysToGet[3]);
 		String assetFolder = AssetUtility.getAssetBasePath(this.insight, space);
 		String fileName = keyValue.get(keysToGet[0]);
@@ -66,18 +51,6 @@ public class SaveAssetReactor extends AbstractReactor {
 			exception.setContinueThreadOfExecution(false);
 			throw exception;
 		}
-
-		// add file to git
-		List<String> files = new Vector<>();
-		files.add(fileName);
-		assetFolder = insight.getInsightFolder();
-		File file2 = new File(assetFolder);
-		assetFolder = file2.getParentFile().getAbsolutePath();
-		
-		GitRepoUtils.addSpecificFiles(assetFolder, files);
-
-		// commit it
-		GitRepoUtils.commitAddedFiles(assetFolder, comment, author, email);
 
 		return NounMetadata.getSuccessNounMessage("Success!");
 	}
