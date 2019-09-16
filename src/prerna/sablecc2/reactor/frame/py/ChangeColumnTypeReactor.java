c260034d9e8454af2a330d1143abe510138cf49a
mahkhalil <mahkhalil@deloitte.com>
2019-09-16 16:51:04 +0000

    UPDATE ::: Adding additional op type for FE
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34782 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: eba0c8116fd97c6bd34d74516bf90456e49fe007

--- src/prerna/sablecc2/reactor/frame/py/ChangeColumnTypeReactor.java
+++ src/prerna/sablecc2/reactor/frame/py/ChangeColumnTypeReactor.java
@@ -99,6 +99,6 @@ public class ChangeColumnTypeReactor extends AbstractFrameReactor {
 				"ChangeColumnType", 
 				AnalyticsTrackerHelper.getHashInputs(this.store, this.keysToGet));
 		
-		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE);
+		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE, PixelOperationType.FRAME_HEADERS_CHANGE);
 	}
 }
