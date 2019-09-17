1edb6c1a991099c876ce3e2f009f807dc22c54d2
mahkhalil <mahkhalil@deloitte.com>
2019-09-17 18:06:48 +0000

    UPDATE ::: Adding tag filters in GetInsightsReactor UPDATE ::: Adding ability to get available tags filtered to set of engines
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34835 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 5ab6fda4061cbfce74743c3d72305eb32ca1385d

--- src/prerna/query/interpreters/sql/SqlJoinStructList.java
+++ src/prerna/query/interpreters/sql/SqlJoinStructList.java
@@ -8,8 +8,6 @@ import java.util.Set;
 import java.util.Vector;
 import java.util.stream.Collectors;
 
-import prerna.util.Utility;
-
 public class SqlJoinStructList {
 
 	private List<SqlJoinStruct> joins = new Vector<SqlJoinStruct>();
@@ -92,6 +90,7 @@ public class SqlJoinStructList {
 				definedTables.add(targetTable);
 				
 			} else {
+				String append = " on ";
 				// the join order matters
 				// so the next join needs to have at least one of its tables
 				// already defined
@@ -112,7 +111,7 @@ public class SqlJoinStructList {
 					// add target table as it is now defined
 					definedTables.add(targetTable);
 				} 
-//				else {
+				else {
 //					// both are defined
 //					// need to make a new alias for the table
 //					// at this point, i am not using this to bring in new values
@@ -120,10 +119,12 @@ public class SqlJoinStructList {
 //					jSyntax.append(" ").append(jType).append(" ");
 //					targetTableAlias = targetTableAlias + Utility.getRandomString(6);
 //					jSyntax.append(targetTable).append(" ").append(targetTableAlias);
-//				}
+					
+					append = " and ";
+				}
 				
 				// define the rest of the join portion
-				jSyntax.append(" on ")
+				jSyntax.append(append)
 				.append(sourceTableAlias).append(".").append(sourceCol)
 				.append("=")
 				.append(targetTableAlias).append(".").append(targetCol);
