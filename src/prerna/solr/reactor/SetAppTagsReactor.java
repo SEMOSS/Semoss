1da6755b5631992b63bf43f3ab460521fba4274c
mahkhalil <mahkhalil@deloitte.com>
2019-09-18 21:08:23 +0000

    BUGFIX ::: Corrected success message on set insight tags UPDATE ::: Adding in new routine to set the app tags + app description UPDATE ::: Removing redundant methods UPDATE ::: Adding additional error check when attempting to join data onto an empty frame
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34940 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/solr/reactor/SetAppTagsReactor.java
+++ src/prerna/solr/reactor/SetAppTagsReactor.java
@@ -6,21 +6,17 @@ import java.util.Vector;
 import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.SecurityAppUtils;
 import prerna.auth.utils.SecurityQueryUtils;
-import prerna.auth.utils.SecurityUpdateUtils;
 import prerna.nameserver.utility.MasterDatabaseUtility;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
-import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 
 public class SetAppTagsReactor extends AbstractReactor {
 	
-	public static final String TAGS = "tags";
-
 	public SetAppTagsReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), TAGS};
+		this.keysToGet = new String[]{ReactorKeysEnum.APP.getKey(), ReactorKeysEnum.TAGS.getKey()};
 	}
 
 	@Override
@@ -37,34 +33,26 @@ public class SetAppTagsReactor extends AbstractReactor {
 			appId = MasterDatabaseUtility.testEngineIdIfAlias(appId);
 		}
 		
-		if(!SecurityQueryUtils.getEngineIds().contains(appId)) {
-			throw new IllegalArgumentException("App id does not exist");
-		}
-		
 		List<String> tags = getTags();
-		SecurityUpdateUtils.setEngineMeta(appId, "tags", tags);
-
-		return new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.APP_INFO);
+		SecurityAppUtils.updateAppTags(appId, tags);
+		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
+		noun.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Successfully saved new tags for app"));
+		return noun;
 	}
 	
-	public List<String> getTags() {
+	/**
+	 * Get the tags to set for the insight
+	 * @return
+	 */
+	protected List<String> getTags() {
 		List<String> tags = new Vector<String>();
-		
-		// see if added as key
-		GenRowStruct grs = this.store.getNoun(this.keysToGet[1]);
+		GenRowStruct grs = this.store.getNoun(ReactorKeysEnum.TAGS.getKey());
 		if(grs != null && !grs.isEmpty()) {
-			int size = grs.size();
-			for(int i = 0; i < size; i++) {
+			for(int i = 0; i < grs.size(); i++) {
 				tags.add(grs.get(i).toString());
 			}
-			return tags;
 		}
 		
-		// start at index 1 and see if in cur row
-		int size = this.curRow.size();
-		for(int i = 1; i < size; i++) {
-			tags.add(this.curRow.get(i).toString());
-		}
 		return tags;
 	}
 
