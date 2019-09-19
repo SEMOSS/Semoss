d478227243616d7e2d8bae9f87f95df5f2728c3e
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-09-19 20:18:29 +0000

    UPDATE ::: Adding new html form syntax for auto generated insert forms on RDBMS database creation
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@34989 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 317ac418f3d563c5e70244e836c60fbdd2ed16a3

--- src/prerna/sablecc2/reactor/app/upload/FormUtility.java
+++ src/prerna/sablecc2/reactor/app/upload/FormUtility.java
@@ -6,20 +6,19 @@ public class FormUtility {
 		return new StringBuilder("<div smss-form-model=\"" + label + "\">" + "{{item.model}}</div>\\n").toString();
 	}
 
-	public static String getInputComponent(String dataModelComp) {
-		return new StringBuilder(
-				" <smss-input ng-model=\"form.dataModel." + dataModelComp + ".selected\">" + "</smss-input>\\n")
-						.toString();
+	public static String getInputComponent() {
+		return new StringBuilder(" <smss-input ng-model=\"item.selected\">" + "</smss-input>\\n").toString();
 	}
 
 	public static String getDropdownComponent(String dataModelComp) {
-		return new StringBuilder("<smss-dropdown model=\"form.dataModel." + dataModelComp
-				+ ".selected\" options=\"form.dataModel.Genre.options\""
-				+ " placeholder=\"Select One\">\\n</smss-dropdown>\\n").toString();
+		return new StringBuilder(
+				"<smss-dropdown model=\"form.dataModel." + dataModelComp + ".selected\" options=\"form.dataModel."
+						+ dataModelComp + ".options\"" + " placeholder=\"Select One\">\\n</smss-dropdown>\\n")
+								.toString();
 	}
 
 	public static String getNumberPickerComponent() {
-		return new StringBuilder("<smss-number-picker model=\"item.selected\">" + "</smss-number-picker>\\n")
+		return new StringBuilder("<smss-input type=\"number\" model=\"item.selected\">" + "</smss-input>\\n")
 				.toString();
 	}
 
@@ -38,12 +37,13 @@ public class FormUtility {
 	}
 
 	public static String getSubmitComponent() {
-		return new StringBuilder("<div smss-form-model=\"Submit\"><smss-btn class=\"smss-btn--primary\""
-				+ " ng-click=\"form.runPixel(form.pixelModel.Insert.pixel)\">{{item.model}}</smss-btn></div>\\n")
-						.toString();
+		return new StringBuilder(
+				"<div smss-form-model=\"Submit\" class=\"form-builder__submit\"><smss-btn class=\"smss-btn--primary\""
+						+ " ng-click=\"form.runPixel(form.pixelModel.Insert.pixel)\">{{item.model}}</smss-btn></div>\\n")
+								.toString();
 	}
-	
+
 	public static String getDescriptionComponent(String description) {
-		return new StringBuilder("<div class=\"smss-form-group__description\">"+description+"</div>\\n").toString();
+		return new StringBuilder("<div class=\"smss-form-group__description\">" + description + "</div>\\n").toString();
 	}
 }
