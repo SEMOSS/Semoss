49cceead35137ca82f7eaaac8481d8b0cc478905
mahkhalil <mahkhalil@deloitte.com>
2019-09-30 18:41:24 +0000

    UPDATE ::: Making the insight folder if it doesn't exist (legacy insights might not have the folder/they might be deleted at various points in time)
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@35129 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f5b747d399cfcff941b568bf9ac5d320b6f52175

--- src/prerna/sablecc2/reactor/export/ToExcelReactor.java
+++ src/prerna/sablecc2/reactor/export/ToExcelReactor.java
@@ -1,5 +1,6 @@
 package prerna.sablecc2.reactor.export;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -51,7 +52,14 @@ public class ToExcelReactor extends TaskBuilderReactor {
 		// if the file location is not defined generate a random path and set
 		// location so that the front end will download
 		if (this.fileLocation == null) {
-			this.fileLocation = this.insight.getInsightFolder() + DIR_SEPARATOR + exportName;
+			String insightFolder = this.insight.getInsightFolder();
+			{
+				File f = new File(insightFolder);
+				if(!f.exists()) {
+					f.mkdirs();
+				}
+			}
+			this.fileLocation = insightFolder + DIR_SEPARATOR + exportName;
 			// store it in the insight so the FE can download it
 			// only from the given insight
 			this.insight.addExportFile(exportName, this.fileLocation);
