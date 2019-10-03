8be8b479cd2f6b344e77325a01c7b9dc0235d206
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-10-03 17:02:07 +0000

    UPDATE ::: allow retrieval of specific app types GetDatabaseList(appType=["RDBMS", "TINKER"]);
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@35227 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 77f9e6cff6e0bbfcbd736646bfa8ad8417425c02

--- src/prerna/sablecc2/reactor/masterdatabase/GetDatabaseListReactor.java
+++ src/prerna/sablecc2/reactor/masterdatabase/GetDatabaseListReactor.java
@@ -2,26 +2,69 @@ package prerna.sablecc2.reactor.masterdatabase;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.SecurityQueryUtils;
+import prerna.engine.api.IEngine;
+import prerna.engine.api.IEngine.ENGINE_TYPE;
+import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.sql.RdbmsTypeEnum;
 
 public class GetDatabaseListReactor extends AbstractReactor {
+	
+	public GetDatabaseListReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.APP_TYPE.getKey()};
+	}
 
 	@Override
 	public NounMetadata execute() {
+		organizeKeys();
 		List<Map<String, Object>> retList = null;
+		List<String> appTypeFilter = getAppTypeFilter();
 		if(AbstractSecurityUtils.securityEnabled()) {
-			retList = SecurityQueryUtils.getUserDatabaseList(this.insight.getUser());
+			retList = SecurityQueryUtils.getUserDatabaseList(this.insight.getUser(), appTypeFilter);
 		} else {
-			retList = SecurityQueryUtils.getAllDatabaseList();
+			retList = SecurityQueryUtils.getAllDatabaseList(appTypeFilter);
 		}
 		
 		return new NounMetadata(retList, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.DATABASE_LIST);
+	}
+	
+	private List<String> getAppTypeFilter() {
+		// get input list
+		List<String> retTypes = new Vector<String>();
+		GenRowStruct headerTypesGrs = this.store.getNoun(this.keysToGet[0]);
+		if(headerTypesGrs != null && !headerTypesGrs.isEmpty()) {
+			retTypes = headerTypesGrs.getAllStrValues();
+		}
+		
+		if(retTypes.isEmpty()) {
+			retTypes = this.curRow.getAllStrValues();
+		}
+		
+		// create filters
+		List<String> appTypeFilter = new Vector<String>();
+		for(String appFilter : retTypes) {
+			ENGINE_TYPE engineType = IEngine.ENGINE_TYPE.valueOf(appFilter);
+			if(engineType == IEngine.ENGINE_TYPE.RDBMS) {
+				// grab lower level rdbms types to filter
+				for( RdbmsTypeEnum rdbmsType: RdbmsTypeEnum.values()) {
+					appTypeFilter.add(rdbmsType.getLabel());
+				}
+			} else {
+				appTypeFilter.add(engineType.toString());
+			}
+		}
+		
+
+		
+		return appTypeFilter;
 	}
 
 }
