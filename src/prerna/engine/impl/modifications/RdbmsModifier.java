fcda3354be3fb30b6b3a266437999508dbbe0146
mahkhalil <mahkhalil@deloitte.com>
2019-10-03 14:11:33 +0000

    UPDATE ::: Setting correct permissions to app modifications UPDATE ::: Adding index method
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@35219 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/engine/impl/modifications/RdbmsModifier.java
+++ src/prerna/engine/impl/modifications/RdbmsModifier.java
@@ -4,24 +4,28 @@ import java.sql.SQLException;
 
 import prerna.engine.api.IEngine;
 import prerna.engine.api.IEngineModifier;
+import prerna.engine.api.IRawSelectWrapper;
 import prerna.engine.impl.rdbms.RDBMSNativeEngine;
+import prerna.rdf.engine.wrappers.WrapperManager;
 import prerna.util.sql.AbstractSqlQueryUtil;
 
 public class RdbmsModifier implements IEngineModifier {
 
 	private RDBMSNativeEngine engine = null;
 	private AbstractSqlQueryUtil queryUtil = null;
+	private String schmea = null;
 	
 	@Override
 	public void setEngine(IEngine engine) {
 		this.engine = (RDBMSNativeEngine) engine;
 		this.queryUtil = this.engine.getQueryUtil();
+		this.schmea = this.engine.getSchema();
 	}
 
 	@Override
-	public void addProperty(String existingConcept, String newColumn, String dataType) throws Exception {
+	public void addProperty(String tableName, String newColumn, String dataType) throws Exception {
 		if(queryUtil.allowAddColumn()) {
-			String sqlQuery = queryUtil.alterTableAddColumn(existingConcept, newColumn, dataType);
+			String sqlQuery = queryUtil.alterTableAddColumn(tableName, newColumn, dataType);
 			try {
 				this.engine.insertData(sqlQuery);
 			} catch (SQLException e) {
@@ -38,9 +42,9 @@ public class RdbmsModifier implements IEngineModifier {
 	}
 	
 	@Override
-	public void editProperty(String existingConcept, String existingColumn, String newDataType) throws Exception {
+	public void editProperty(String tableName, String newColumn, String newDataType) throws Exception {
 		if(queryUtil.allowAddColumn()) {
-			String sqlQuery = queryUtil.modColumnType(existingConcept, existingColumn, newDataType);
+			String sqlQuery = queryUtil.modColumnType(tableName, newColumn, newDataType);
 			try {
 				this.engine.insertData(sqlQuery);
 			} catch (SQLException e) {
@@ -58,9 +62,9 @@ public class RdbmsModifier implements IEngineModifier {
 	
 
 	@Override
-	public void removeProperty(String existingConcept, String existingColumn) throws Exception {
+	public void removeProperty(String tableName, String columnName) throws Exception {
 		if(queryUtil.allowDropColumn()) {
-			String sqlQuery = queryUtil.alterTableDropColumn(existingConcept, existingColumn);
+			String sqlQuery = queryUtil.alterTableDropColumn(tableName, columnName);
 			try {
 				this.engine.insertData(sqlQuery);
 			} catch (SQLException e) {
@@ -76,6 +80,34 @@ public class RdbmsModifier implements IEngineModifier {
 		}
 	}
 
+	@Override
+	public void addIndex(String tableName, String columnName, String indexName, boolean addIfExists) throws Exception {
+		String indexQuery = null;
+		if(addIfExists) {
+			indexQuery = queryUtil.createIndex(indexName, tableName, columnName);
+		} else {
+			// first need to check if there is an existing index on the column
+			String existingIndicesQuery = queryUtil.allIndexForTableQuery(tableName, this.schmea);
+			IRawSelectWrapper wrapper = WrapperManager.getInstance().getRawWrapper(engine, existingIndicesQuery);
+			while(wrapper.hasNext()) {
+				Object[] values = wrapper.next().getValues();
+				if(values[0].toString().equalsIgnoreCase(indexName)) {
+					throw new IllegalArgumentException("The index name defined already exists");
+				}
+				if(values[1].toString().equalsIgnoreCase(tableName)) {
+					throw new IllegalArgumentException("Index already exists on the column with the name '" + values[1] + "'");
+				}
+			}
+			// we need to see if the index already exists or not
+			indexQuery = queryUtil.createIndex(indexName, tableName, columnName);
+		}
+		
+		try {
+			this.engine.insertData(indexQuery);
+		} catch (SQLException e) {
+			throw new IllegalArgumentException(e.getMessage(), e);
+		}
+	}
 	
 	
 }
