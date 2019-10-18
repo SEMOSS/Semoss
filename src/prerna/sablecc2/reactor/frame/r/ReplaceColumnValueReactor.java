c47c948a9c964afdd0674b6a4db6b0febf0e4125
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-10-18 15:42:34 +0000

    UPDATE ::: R Replace routines can be applied to multiple columns
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@35488 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/r/ReplaceColumnValueReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/ReplaceColumnValueReactor.java
@@ -1,6 +1,10 @@
 package prerna.sablecc2.reactor.frame.r;
 
+import java.util.List;
+import java.util.Vector;
+
 import prerna.ds.r.RDataTable;
+import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
@@ -34,34 +38,35 @@ public class ReplaceColumnValueReactor extends AbstractRFrameReactor{
 
 		// get inputs
 		// first input is the column that we are updating
-		String column = this.keyValue.get(this.keysToGet[0]);
-		if (column.contains("__")) {
-			column = column.split("__")[1];
-		}
+		List<String> columnNames = getColumns();
 
 		// second input is the old value
 		String oldValue = this.keyValue.get(this.keysToGet[1]);
 
 		// third input is the new value
 		String newValue = this.keyValue.get(this.keysToGet[2]);
-
-		// use method to retrieve a single column type
-		String colDataType = getColumnType(table, column);
 		
-		// account for quotes that will be needed in the query with string values
-		String neededQuote = "";
-		if (colDataType.equalsIgnoreCase("string") || colDataType.equalsIgnoreCase("character")) {
-			neededQuote = "\"";
-		} else if (colDataType.equalsIgnoreCase("factor")) {
-			changeColumnType(frame, table, column, "string", "%Y/%m/%d");
-			neededQuote = "\"";
-		}
+		// iterate through all passed columns
+		StringBuilder script = new StringBuilder();
+		for (String column : columnNames) {
+		
+			// use method to retrieve a single column type
+			String colDataType = getColumnType(table, column);
 			
-		String script = "";
-		if (oldValue.equalsIgnoreCase("null") || oldValue.equalsIgnoreCase("NA")) {
-			script = table + "$" + column + "[is.na(" + table + "$" + column + ")] <- " + neededQuote + newValue + neededQuote;
-		} else {
-			script = table + "$" + column + "[" + table + "$" + column + " == " + neededQuote + oldValue + neededQuote + "] <- " + neededQuote + newValue + neededQuote;
+			// account for quotes that will be needed in the query with string values
+			String neededQuote = "";
+			if (colDataType.equalsIgnoreCase("string") || colDataType.equalsIgnoreCase("character")) {
+				neededQuote = "\"";
+			} else if (colDataType.equalsIgnoreCase("factor")) {
+				changeColumnType(frame, table, column, "string", "%Y/%m/%d");
+				neededQuote = "\"";
+			}
+				
+			if (oldValue.equalsIgnoreCase("null") || oldValue.equalsIgnoreCase("NA")) {
+				script.append(table + "$" + column + "[is.na(" + table + "$" + column + ")] <- " + neededQuote + newValue + neededQuote + ";");
+			} else {
+				script.append(table + "$" + column + "[" + table + "$" + column + " == " + neededQuote + oldValue + neededQuote + "] <- " + neededQuote + newValue + neededQuote + ";");
+			}
 		}
 		
 		// NEW TRACKING
@@ -73,7 +78,25 @@ public class ReplaceColumnValueReactor extends AbstractRFrameReactor{
 			
 		// execute the r script
 		// script is of the form FRAME$Director[FRAME$Director == "oldVal"] <- "newVal"
-		frame.executeRScript(script);
+		frame.executeRScript(script.toString());
 		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE);
+	}
+	
+	private List<String> getColumns() {
+		List<String> cols = new Vector<String>();
+
+		GenRowStruct grs = this.store.getNoun(this.keysToGet[0]);
+		if(grs != null && !grs.isEmpty()) {
+			for(int i = 0; i < grs.size(); i++) {
+				String column =grs.get(i).toString();
+				if (column.contains("__")) {
+					column = column.split("__")[1];
+				}
+				cols.add(column);
+			}
+			return cols;
+		}
+		
+		return cols;
 	}
 }
