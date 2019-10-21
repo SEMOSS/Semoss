e62399967b5c905e6f31473b22b5abce83ff86c6
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2019-10-21 15:16:12 +0000

    PyTranslator - Now is the equivalent RJavaTranslator and would be the main class to run all of the python commands. PyExecutorThread - Updated to import smssutil Insight - Way to create the pyTranslator
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@35505 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d60c28e1ad2bbea67b198e54bccb95f7f6a0a365

--- src/prerna/sablecc2/reactor/frame/py/PyReactor.java
+++ src/prerna/sablecc2/reactor/frame/py/PyReactor.java
@@ -25,93 +25,14 @@ public class PyReactor extends AbstractReactor {
 		if(!PyUtils.pyEnabled()) {
 			throw new IllegalArgumentException("Python is not enabled to use the following command");
 		}
-		ReactorSecurityManager tempManager = new ReactorSecurityManager();
-		tempManager.addClass(CLASS_NAME);
-		System.setSecurityManager(tempManager);
 		
-		Logger logger = getLogger(CLASS_NAME);
-		PyExecutorThread pyThread = this.insight.getPy();
-		Object lock = pyThread.getMonitor();
-
-		int size = 3;
-		// will always have an insight path
-		String removePathVariables = "del ROOT";
-		
-		String insightRootAssignment = "";
-		String appRootAssignment = "";
-		String userRootAssignment = "";
-
-		String insightRootPath = null;
-		String appRootPath = null;
-		String userRootPath = null;
-		
-		insightRootPath = this.insight.getInsightFolder().replace('\\', '/');
-		insightRootAssignment = "ROOT = \"" + insightRootPath + "\"";
-		
-		if(this.insight.isSavedInsight()) {
-			appRootPath = this.insight.getAppFolder();
-			appRootPath = appRootPath.replace('\\', '/');
-			appRootAssignment = "APP_ROOT = \"" + appRootPath + "\"";
-			removePathVariables += ", APP_ROOT";
-			size++;
-		}
-		try {
-			userRootPath = AssetUtility.getAssetBasePath(this.insight, AssetUtility.USER_SPACE_KEY, false);
-			userRootPath = userRootPath.replace('\\', '/');
-			userRootAssignment = "USER_ROOT = \"" + userRootPath + "\"";
-			removePathVariables += ", USER_ROOT";
-			size++;
-		} catch(Exception ignore) {
-			// ignore
-		}
-		
-		String[] commands = new String[size];
-		int counter = 0;
-		commands[counter++] = insightRootAssignment;
-		if(appRootAssignment != null && !appRootAssignment.isEmpty()) {
-			commands[counter++] = appRootAssignment;
-		}
-		if(userRootAssignment != null && !userRootAssignment.isEmpty()) {
-			commands[counter++] = userRootAssignment;
-		}
 		String code = Utility.decodeURIComponent(this.curRow.get(0).toString());
-		commands[counter++] = code;
-		commands[counter++] = removePathVariables;
+
+		String output = this.insight.getPyTranslator().runPyAndReturnOutput(code);
 		
-		logger.info("Execution python script: " + code);
-		pyThread.command = commands;
-		
-		Object output = "";
-		synchronized(lock) {
-			try {
-				lock.notify();
-				lock.wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			// waking up now
-			output = pyThread.response.get(code);
-		}
-		
-		// clean up the output
-		// clean up the output
-		if(userRootPath != null && output.toString().contains(userRootPath)) {
-			output = output.toString().replace(userRootPath, "$USER_IF");
-		}
-		if(appRootPath != null && output.toString().contains(appRootPath)) {
-			output = output.toString().replace(appRootPath, "$APP_IF");
-		}
-		if(insightRootPath != null && output.toString().contains(insightRootPath)) {
-			output = output.toString().replace(insightRootPath, "$IF");
-		}
 		
 		List<NounMetadata> outputs = new Vector<NounMetadata>(1);
 		outputs.add(new NounMetadata(output, PixelDataType.CONST_STRING));
-		
-		// set back the original security manager
-		tempManager.removeClass(CLASS_NAME);
-		System.setSecurityManager(defaultManager);	
-		
 		return new NounMetadata(outputs, PixelDataType.CODE, PixelOperationType.CODE_EXECUTION);
 	}
 
