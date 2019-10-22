c446c47e163538a2e4d6c42024a43aacad6338f9
mahkhalil <mahkhalil@deloitte.com>
2019-10-22 19:11:29 +0000

    UPDATE ::: Adding python "Date Adder" clean widget UPDATE ::: Lowercasing input for R "Date Adder" clean widget to make more fault tolerant
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@35528 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/py/DateAddValueReactor.java
+++ src/prerna/sablecc2/reactor/frame/py/DateAddValueReactor.java
@@ -1,4 +1,4 @@
-package prerna.sablecc2.reactor.frame.r;
+package prerna.sablecc2.reactor.frame.py;
 
 import java.util.Arrays;
 import java.util.List;
@@ -6,14 +6,14 @@ import java.util.Vector;
 
 import prerna.algorithm.api.SemossDataType;
 import prerna.ds.OwlTemporalEngineMeta;
-import prerna.ds.r.RDataTable;
+import prerna.ds.py.PandasFrame;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 
-public class DateAddValueReactor extends AbstractRFrameReactor{
+public class DateAddValueReactor extends AbstractPyFrameReactor {
 
 	/*
 	 * Here are the keys that can be passed into the reactor options
@@ -23,12 +23,14 @@ public class DateAddValueReactor extends AbstractRFrameReactor{
 	private static final String NEW_COL = "new_col";
 	
 	/*
-	 * Here are the units that can be used 
+	 * Here are the units that can be used
+	 * This is matched with values that are passed into the Python function
 	 */
-	private static final String DAY = "day";
-	private static final String WEEK = "week";
-	private static final String MONTH = "month";
-	private static final String YEAR = "year";
+
+//	private static final String DAY = "day";
+//	private static final String WEEK = "week";
+//	private static final String MONTH = "month";
+//	private static final String YEAR = "year";
 	
 	public DateAddValueReactor(){
 		this.keysToGet = new String[]{ReactorKeysEnum.COLUMN.getKey(), NEW_COL, UNIT, VAL_TO_ADD};
@@ -36,45 +38,42 @@ public class DateAddValueReactor extends AbstractRFrameReactor{
 	
 	@Override
 	public NounMetadata execute() {
-		// Initiate R
-		init();
 		organizeKeys();
 
-		RDataTable frame = (RDataTable) getFrame();
+		PandasFrame frame = (PandasFrame) getFrame();
 		String table = frame.getName();
+		String wrapperName = frame.getWrapperName();
 
 		String srcCol = this.keyValue.get(this.keysToGet[0]);
 		String newCol = this.keyValue.get(this.keysToGet[1]);
+		if(newCol == null || newCol.isEmpty()) {
+			newCol = srcCol;
+		}
 		String unit = this.keyValue.get(this.keysToGet[2]).toLowerCase();
 		int value = getValue();
 		
 		// make sure source column exists
-		String[] startingColumns = getColumns(table);
+		String[] startingColumns = getColumns(frame);
 		List<String> startingColumnsList = new Vector<String>(startingColumns.length);
 		startingColumnsList.addAll(Arrays.asList(startingColumns));
 		if (srcCol == null || !startingColumnsList.contains(srcCol)) {
 			throw new IllegalArgumentException("Need to define an existing date column.");
 		}
 		
+		// python method
+		// def add_to_date(this, date_column, output_column, unit_of_measure, value):
+		
 		StringBuilder script = new StringBuilder();
-		if(newCol != null && !newCol.isEmpty()){
-			script.append(table).append("$").append(newCol).append(" <- ").append(table).append("$").append(srcCol);
-		} else{
-			script.append(table).append("$").append(srcCol).append(" <- ").append(table).append("$").append(srcCol);
-		}
-		if(unit.equals(DAY)){
-			script.append(" + ").append(value).append(";");
-		} else if(unit.equals(WEEK)){
-			script.append(" + ").append(value * 7).append(";");
-		} else if(unit.equals(MONTH)){
-			script.append(" %m+%").append(" months(").append(value).append(")").append(";");
-		} else if(unit.equals(YEAR)){
-			script.append("%m+%").append(" years(").append(value).append(")").append(";");
-		}
-		this.rJavaTranslator.runR(script.toString());
+		script.append(wrapperName).append(".add_to_date(")
+			.append(srcCol).append(",")
+			.append(newCol).append(",")
+			.append(unit).append(",")
+			.append(value)
+			;
+		frame.runScript(script.toString());
 		
 		NounMetadata retNoun;
-		if(newCol == null || newCol.equals("")){
+		if(newCol.equals(srcCol)){
 			retNoun = new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE);
 		} else{
 			retNoun = new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_HEADERS_CHANGE, PixelOperationType.FRAME_DATA_CHANGE);
