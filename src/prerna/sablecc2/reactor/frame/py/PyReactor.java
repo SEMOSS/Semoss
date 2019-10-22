72efa6b403a0052bedaf286a6e5c3d4a88b8fc47
mahkhalil <mahkhalil@deloitte.com>
2019-10-22 11:51:04 +0000

    BUGFIX ::: Sending the original table name for the pandas interpreter so that retrieval of information from the metadata occurs properly (every query was returning datatypes as string causing issues when adding aggregates) BUGFIX ::: Setting the logger in the PyTranslator that was causing a null pointer error (and adding printstackstrace to speed up debugging in the future)
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@35521 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 9b3e98d77016cfe08a860179664fe044a5f44045

--- src/prerna/sablecc2/reactor/frame/py/PyReactor.java
+++ src/prerna/sablecc2/reactor/frame/py/PyReactor.java
@@ -5,14 +5,12 @@ import java.util.Vector;
 
 import org.apache.log4j.Logger;
 
-import prerna.ds.py.PyExecutorThread;
+import prerna.ds.py.PyTranslator;
 import prerna.ds.py.PyUtils;
-import prerna.sablecc.ReactorSecurityManager;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
-import prerna.util.AssetUtility;
 import prerna.util.Utility;
 
 public class PyReactor extends AbstractReactor {
@@ -25,11 +23,13 @@ public class PyReactor extends AbstractReactor {
 		if(!PyUtils.pyEnabled()) {
 			throw new IllegalArgumentException("Python is not enabled to use the following command");
 		}
-		
+		Logger logger = getLogger(CLASS_NAME);
+
 		String code = Utility.decodeURIComponent(this.curRow.get(0).toString());
 
-		String output = this.insight.getPyTranslator().runPyAndReturnOutput(code);
-		
+		PyTranslator pyTranslator = this.insight.getPyTranslator();
+		pyTranslator.setLogger(logger);
+		String output = pyTranslator.runPyAndReturnOutput(code);
 		
 		List<NounMetadata> outputs = new Vector<NounMetadata>(1);
 		outputs.add(new NounMetadata(output, PixelDataType.CONST_STRING));
