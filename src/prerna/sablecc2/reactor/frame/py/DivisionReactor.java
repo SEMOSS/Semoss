7ddd3cd1cc65d227a51ed5866de3dc879644b066
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-10-28 20:31:50 +0000

    UPDATE ::: Adding col division and RegexReplaceColumnValueReactor for py
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@35679 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/sablecc2/reactor/frame/py/DivisionReactor.java
@@ -0,0 +1,97 @@
+package prerna.sablecc2.reactor.frame.py;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Vector;
+
+
+import prerna.algorithm.api.SemossDataType;
+import prerna.ds.OwlTemporalEngineMeta;
+import prerna.ds.py.PandasFrame;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.execptions.SemossPixelException;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+
+
+public class DivisionReactor extends AbstractPyFrameReactor {
+	private static final String NUMERATOR = "numerator";
+	private static final String DENOMINATOR = "denominator";
+
+	public DivisionReactor() {
+		this.keysToGet = new String[] { NUMERATOR, DENOMINATOR, ReactorKeysEnum.NEW_COLUMN.getKey() };
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		
+		// get frame
+		PandasFrame frame = (PandasFrame) getFrame();
+		String frameName = frame.getName();
+		
+		// get the wrapper name
+		// which is the framename with w in the end
+		String wrapperFrameName = frame.getWrapperName();
+		
+		// get inputs
+		String numerator = this.keyValue.get(NUMERATOR);
+		String denominator = this.keyValue.get(DENOMINATOR);
+		String newColName = this.keyValue.get(ReactorKeysEnum.NEW_COLUMN.getKey());
+		newColName = getCleanNewColName(newColName);
+		
+		// make sure column exists
+		String[] startingColumns = getColumns(frame);
+		List<String> startingColumnsList = new Vector<String>(startingColumns.length);
+		startingColumnsList.addAll(Arrays.asList(startingColumns));
+		if (numerator == null || !startingColumnsList.contains(numerator) || denominator == null
+				|| !startingColumnsList.contains(denominator)) {
+			throw new IllegalArgumentException("Need to define existing numerator and denominator columns.");
+		}
+
+		// create script
+		String script = wrapperFrameName + ".col_division('" + numerator + "', '" + denominator + "', '" + newColName + "')";
+		
+		// run script
+		frame.runScript(script);
+		
+		// check if new column exists
+		String[] endingColumns = getColumns(frame);
+		List<String> endingColumnsList = new Vector<String>(endingColumns.length);
+		endingColumnsList.addAll(Arrays.asList(endingColumns));
+		if (!endingColumnsList.contains(newColName)) {
+			NounMetadata error = NounMetadata.getErrorNounMessage("Unable to perform column division");
+			SemossPixelException exception = new SemossPixelException(error);
+			exception.setContinueThreadOfExecution(false);
+			throw exception;
+		}
+
+		// update meta data to add new column
+		OwlTemporalEngineMeta metaData = frame.getMetaData();
+		metaData.addProperty(frameName, frameName + "__" + newColName);
+		metaData.setAliasToProperty(frameName + "__" + newColName, newColName);
+		metaData.setDataTypeToProperty(frameName + "__" + newColName, SemossDataType.DOUBLE.toString());
+		metaData.setDerivedToProperty(frameName + "__" + newColName, true);
+		frame.syncHeaders();
+
+		NounMetadata retNoun = new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_HEADERS_CHANGE,
+				PixelOperationType.FRAME_DATA_CHANGE);
+		retNoun.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Successfully performed column division"));
+		return retNoun;
+	}
+
+	///////////////////////// KEYS /////////////////////////////////////
+
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if (key.equals(NUMERATOR)) {
+			return "The column to use as the numerator";
+		} else if (key.equals(DENOMINATOR)) {
+			return "The column to use as the denominator";
+		} else {
+			return super.getDescriptionForKey(key);
+		}
+	}
+}
+
