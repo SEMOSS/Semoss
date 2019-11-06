0da2e4865e2c6b8f279153c70f82572067cf897a
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-11-06 15:55:37 +0000

    UPDATE ::: Py null rule adding empty string check
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@35846 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- py/DQ/Rules/isNull.py
+++ py/DQ/Rules/isNull.py
@@ -1,13 +1,14 @@
 import pandas as pd
+
 def isNull(frameWrapper, rule):
 	df = frameWrapper.cache['data']
 	currCol = rule['col']
 	currRule = rule['rule']
 	temp = df[currCol]
 	totLength = len(temp)
-	totErrors = totLength - sum(pd.notnull(temp) + 0)
+	totErrors = totLength - sum(df[currCol].notnull() & (df[currCol]!=u''))
 	totCorrect = totLength - totErrors
-	toPaint = "'NaN'"
+	toPaint = "'NaN', ''"
 	# Create DataFrame 
 	data = {'Columns': [currCol], 'Errors':[totErrors], 'Valid': [totCorrect], 'Total': [totLength], 'Rules': [currRule], 'Description': [''], 'toColor': [toPaint]} 
 	ruleDf = pd.DataFrame(data) 
