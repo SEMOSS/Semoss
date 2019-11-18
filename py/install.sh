4762ebce04f1e597bd64002a4e88e25c7afb9ba0
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-11-18 20:46:00 +0000

    UPDATE ::: Adding pyton direct load from excel. REQUIREMENT ::: pip install xlrd
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@36039 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 368fb329f2166d10b1db05ea5b753feb6d2ed8ab

--- py/install.sh
+++ py/install.sh
@@ -2,10 +2,11 @@ echo 'Uninstalling and reinstalling jep version for SEMOSS'
 pip uninstall Jep
 pip install jep==3.9.0
 echo 'Installing python libraries'
+pip install pandas==0.25.3
 pip install annoy
-pip install pandas
 pip install numpy
 pip install fuzzywuzzy
 pip install random
 pip install datetime
 pip install pyjarowinkler
+pip install xlrd
