a752993ba0814074cb0102d350589584ae24f33a
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2019-12-03 21:18:28 +0000

    PyTranslator, PyExecutorThread.java - Better interstitial for long running transactions ThreadState - New state for threads so the threads are not waiting and is able to show the interstitial GitRepoUtils - Added a binary pull of an asset GetImageAsset - Gets the image asset as a DATA URI <image src='data:image/jpeg;base64:binary></image>
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@36186 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6e9959d3bd9b19235704b7b6ac99b3304b464553

---
+++ src/prerna/util/git/reactors/GetImageAssetReactor.java
@@ -0,0 +1,80 @@
+package prerna.util.git.reactors;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.file.Files;
+import java.util.Base64;
+
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.AssetUtility;
+import prerna.util.git.GitRepoUtils;
+
+public class GetImageAssetReactor extends AbstractReactor {
+
+	// gets a particular asset in a particular version
+	// if the version is not provided - this gets the head
+
+	public GetImageAssetReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.VERSION.getKey(),
+				ReactorKeysEnum.SPACE.getKey() };
+		this.keyRequired = new int[] { 1, 0, 0 };
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+
+		// check if user is logged in
+		String space = this.keyValue.get(this.keysToGet[2]);
+		String assetFolder = AssetUtility.getAssetVersionBasePath(this.insight, space);
+
+		// relative path is used for git if the insight is saved
+		String assetDir = "";
+		if(space == null || AssetUtility.INSIGHT_SPACE_KEY.equalsIgnoreCase(space)) {
+			if (this.insight.isSavedInsight()) {
+				assetDir = AssetUtility.getAssetRelativePath(this.insight, space);
+			} else {
+				// get temp insight folder
+				assetFolder = this.insight.getInsightFolder();
+			}
+		} else {
+			// user space + app holds assets in assets folder
+			assetDir = "assets";
+		}
+
+		// specify a file
+		String asset = keyValue.get(keysToGet[0]);
+		// grab the version
+		String version = null;
+		if (keyValue.containsKey(keysToGet[1])) {
+			version = keyValue.get(keysToGet[1]);
+		}
+
+		// I need a better way than output
+		// probably write the file and volley the file ?
+		byte [] output = GitRepoUtils.getBinary(version, assetDir + "/" + asset, assetFolder);
+		// try to see if I can convert to a string data URI and give it
+		StringWriter sw = new StringWriter();
+		String encodedString = Base64.getEncoder().encodeToString(output);
+		String mimeType = "image/png";
+		try {
+			mimeType = Files.probeContentType(new File(assetFolder + "/" + assetDir + "/" + asset).toPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//sw.write("<html><body>");
+		sw.write("<img src='data:" + mimeType + ";base64," + encodedString + "'>");
+		//pw.write("<img src='data:image/svg+xml;base64," + encodedString + "'>");
+		//sw.write("</body></html>");
+
+		return new NounMetadata(sw.toString(), PixelDataType.CONST_STRING, PixelOperationType.OPERATION);
+	}
+
+}
