2eca4100aecb4ab31f6c2e73af41902d9966bbac
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-12-06 16:46:06 +0000

    UPDATE ::: Getting metamodel for embedded neo4j db
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@36270 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6c7620204978402e89edddc052172ea04ae71e1a

--- src/prerna/sablecc2/reactor/app/upload/neo4j/GetNeo4jMetamodelReactor.java
+++ src/prerna/sablecc2/reactor/app/upload/neo4j/GetNeo4jMetamodelReactor.java
@@ -3,6 +3,10 @@ package prerna.sablecc2.reactor.app.upload.neo4j;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+
+import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,6 +17,7 @@ import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.execptions.SemossPixelException;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.sablecc2.reactor.app.upload.UploadInputUtility;
 import prerna.util.ConnectionUtils;
 import prerna.util.GraphUtility;
 
@@ -20,58 +25,74 @@ public class GetNeo4jMetamodelReactor extends AbstractReactor {
 
 	public GetNeo4jMetamodelReactor() {
 		this.keysToGet = new String[] { ReactorKeysEnum.CONNECTION_STRING_KEY.getKey(),
-				ReactorKeysEnum.USERNAME.getKey(), ReactorKeysEnum.PASSWORD.getKey() };
+				ReactorKeysEnum.USERNAME.getKey(), ReactorKeysEnum.PASSWORD.getKey(),
+				ReactorKeysEnum.FILE_PATH.getKey() };
 	}
 
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
-		String connectionStringKey = this.keyValue.get(this.keysToGet[0]);
-		if (connectionStringKey == null) {
-			String msg = "Requires a Connection URL (e.g. bolt://localhost:9999) to get graph metamodel";
-			SemossPixelException exception = new SemossPixelException(
-					new NounMetadata(msg, PixelDataType.CONST_STRING, PixelOperationType.ERROR));
-			exception.setContinueThreadOfExecution(false);
-			throw exception;
-		}
-		// Prepend jdbc keyword for neo4j
-		connectionStringKey = "jdbc:neo4j:" + connectionStringKey;
-		String username = this.keyValue.get(this.keysToGet[1]);
-		if (username == null) {
-			SemossPixelException exception = new SemossPixelException(new NounMetadata(
-					"Requires username to get graph metamodel.", PixelDataType.CONST_STRING, PixelOperationType.ERROR));
-			exception.setContinueThreadOfExecution(false);
-			throw exception;
-		}
-
-		String password = this.keyValue.get(this.keysToGet[2]);
-		if (password == null) {
-			SemossPixelException exception = new SemossPixelException(new NounMetadata(
-					"Requires password to get graph metamodel.", PixelDataType.CONST_STRING, PixelOperationType.ERROR));
-			exception.setContinueThreadOfExecution(false);
-			throw exception;
-		}
-
 		Map<String, Object> retMap = new HashMap<String, Object>();
-		Connection conn = null;
-		try {
-			// Must specify name of Neo4j Bolt Class for JDBC to find the right
-			// driver to connect to DB
-			Class.forName("org.neo4j.jdbc.bolt.BoltDriver");
-			// Create Connection
-			conn = DriverManager.getConnection(connectionStringKey, username, password);
-			// Get Metamodel
-			retMap = GraphUtility.getMetamodel(conn);
+		String filePath = UploadInputUtility.getFilePath(this.store, this.insight);
+		if (filePath != null) {
+			try {
+				GraphDatabaseService dbService = new GraphDatabaseFactory().newEmbeddedDatabase(new File(filePath));
+				retMap = GraphUtility.getMetamodel(dbService);
+				dbService.shutdown();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			// this is if we want to get the metamodel for a remote graph
+			String connectionStringKey = this.keyValue.get(this.keysToGet[0]);
+			if (connectionStringKey == null) {
+				String msg = "Requires a Connection URL (e.g. bolt://localhost:9999) to get graph metamodel";
+				SemossPixelException exception = new SemossPixelException(
+						new NounMetadata(msg, PixelDataType.CONST_STRING, PixelOperationType.ERROR));
+				exception.setContinueThreadOfExecution(false);
+				throw exception;
+			}
+			// Prepend jdbc keyword for neo4j
+			connectionStringKey = "jdbc:neo4j:" + connectionStringKey;
+			String username = this.keyValue.get(this.keysToGet[1]);
+			if (username == null) {
+				SemossPixelException exception = new SemossPixelException(
+						new NounMetadata("Requires username to get graph metamodel.", PixelDataType.CONST_STRING,
+								PixelOperationType.ERROR));
+				exception.setContinueThreadOfExecution(false);
+				throw exception;
+			}
 
-		} catch (ClassNotFoundException e) {
-			// If org.neo4j.jdbc.bolt.BoltDriver not found
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// From a database access error or if we called on a closed
-			// connection
-			e.printStackTrace();
-		} finally {
-			ConnectionUtils.closeConnection(conn);
+			String password = this.keyValue.get(this.keysToGet[2]);
+			if (password == null) {
+				SemossPixelException exception = new SemossPixelException(
+						new NounMetadata("Requires password to get graph metamodel.", PixelDataType.CONST_STRING,
+								PixelOperationType.ERROR));
+				exception.setContinueThreadOfExecution(false);
+				throw exception;
+			}
+
+			Connection conn = null;
+			try {
+				// Must specify name of Neo4j Bolt Class for JDBC to find the
+				// right
+				// driver to connect to DB
+				Class.forName("org.neo4j.jdbc.bolt.BoltDriver");
+				// Create Connection
+				conn = DriverManager.getConnection(connectionStringKey, username, password);
+				// Get Metamodel
+				retMap = GraphUtility.getMetamodel(conn);
+
+			} catch (ClassNotFoundException e) {
+				// If org.neo4j.jdbc.bolt.BoltDriver not found
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// From a database access error or if we called on a closed
+				// connection
+				e.printStackTrace();
+			} finally {
+				ConnectionUtils.closeConnection(conn);
+			}
 		}
 
 		return new NounMetadata(retMap, PixelDataType.MAP);
