92debb43b2d10a03730f17b31e4400650a8aa252
mahkhalil <mahkhalil@deloitte.com>
2019-12-11 01:06:27 +0000

    UPDATE ::: Adding tinker reactor to replace the data
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@36323 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: cdee248f6b073ddd93a847160aa24cd33f67927e

---
+++ src/prerna/engine/impl/tinker/TinkerUtilities.java
@@ -0,0 +1,23 @@
+package prerna.engine.impl.tinker;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+
+public class TinkerUtilities {
+
+	private static final Logger LOGGER = LogManager.getLogger(TinkerUtilities.class);
+	
+	private TinkerUtilities() {
+		
+	}
+	
+	public static void removeAllVertices(TinkerEngine engine) {
+		GraphTraversal<Vertex, Long> iterate = engine.g.traversal().V().drop().iterate().count();
+		while(iterate.hasNext()) {
+			Long count = iterate.next();
+			LOGGER.info("Dropping " + count + " verticies from engine " + engine.getEngineName() + "__" + engine.getEngineId());
+		}
+	}
+}
