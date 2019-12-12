2d70a5fbf3e5ed26aa9f1d7979e84a7d292247bf
kunalppatel9 <kunalppatel9@deloitte.com>
2019-12-12 16:31:08 +0000

    ADDED ::: Updating all S3 calls and adding a browser
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@36358 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c7695359955405c99c5f6dbd1c5f49dededf30b9

---
+++ src/prerna/sablecc2/reactor/qs/source/S3Utils.java
@@ -0,0 +1,86 @@
+package prerna.sablecc2.reactor.qs.source;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.services.s3.model.S3Object;
+
+import prerna.util.DIHelper;
+
+
+public class S3Utils {
+	
+	private static final Logger LOGGER = LogManager.getLogger(S3Utils.class.getName());
+	
+	private static Properties socialData = null;
+	private static S3Utils instance;
+	private static final String AWS_ACCOUUNT = "aws_account";
+	private static final String AWS_KEY = "aws_key";
+	
+	private static AWSCredentials awsCreds = null;
+	
+	private S3Utils() {
+		loadProps();
+	}
+	
+	public static S3Utils getInstance() throws IllegalArgumentException {
+		if(instance == null) {
+				instance = new S3Utils();
+			}
+		
+		return instance;
+	}
+
+	
+	private void loadProps() {
+		File f = new File(DIHelper.getInstance().getProperty("SOCIAL"));
+		FileInputStream fis = null;
+
+		try {
+			if(f.exists()) {
+				socialData = new Properties();
+				fis = new FileInputStream(f);
+				socialData.load(fis);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(fis != null) {
+				try {
+					fis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}	
+		
+		S3Object fullObject = null;
+
+		String account = socialData.getProperty(AWS_ACCOUUNT);
+		String key = socialData.getProperty(AWS_KEY);
+		if (account != null && account.length() > 0 && key != null && key.length() > 0) {
+		 awsCreds = new BasicAWSCredentials(account, key);
+		} else{ 
+			 awsCreds = new EnvironmentVariableCredentialsProvider().getCredentials();
+		}
+	}
+
+	public AWSCredentials getS3Creds(){
+		return awsCreds;
+	}
+
+}
