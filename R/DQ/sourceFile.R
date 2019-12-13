75f6b7028861a01420b27fd7e4839512194e104c
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-12-13 16:25:20 +0000

    UPDATE ::: Adding character length rule to data quality
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@36378 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d13b5b69b4c69c499985db17e163efb1b56a1a06

--- R/DQ/sourceFile.R
+++ R/DQ/sourceFile.R
@@ -6,7 +6,7 @@ sourceFiles <- function(base){
   source(paste(base, "Rules/isUnique.r", sep = ""))
   source(paste(base, "Rules/findValue.r", sep = ""))
   source(paste(base, "Rules/validator.r", sep = ""))
-  
+  source(paste(base, "Rules/charLengthRule.r", sep = ""))
   source(paste(base, "Rules/regexComparison.R", sep = ""))
   source(paste(base, "Rules/whatRegexValue.R", sep = ""))
 }
