ef90e94091dd20ac5f436cf4d2b70385aff8a9e8
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-12-13 16:25:20 +0000

    UPDATE ::: Adding character length rule to data quality
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@36378 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ R/DQ/Rules/charLengthRule.R
@@ -0,0 +1,22 @@
+charLengthRule <- function(dt, rule) {
+
+  ruleName <- c("Character Length")
+  currCol <- rule$col
+  numOptions = rule$options
+  tempArray <- dt[, get(rule$col)]
+  tempArray <- nchar(tempArray)
+  ####### this can be altered to do <=, >=, !=, >, <
+  tempArray = tempArray != numOptions
+  totLength <- length(tempArray)
+  totCorrect <- totLength - sum(tempArray)
+  totErrors <- totLength - totCorrect
+  ##### grab unique values that are wrong
+  toPaint =  dt[, get(rule$col)][tempArray==TRUE]
+  toPaint <- paste(unique(toPaint), collapse = "\", \"" )
+  toPaint <- paste0('\"', toPaint, '\"')
+  
+  returnTable <- data.table(currCol, totErrors, totCorrect, totLength, ruleName, "", toPaint)
+  names(returnTable) <- c('Columns', 'Errors', 'Valid', 'Total', 'Rules', 'Description', 'toColor')
+  
+  return (returnTable)
+}
