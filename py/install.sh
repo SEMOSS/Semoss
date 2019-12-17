7b0e6dc86552afe40c83b25eee557f22f77b67ff
rramirezjimenez <rramirezjimenez@deloitte.com>
2019-12-17 18:40:50 +0000

    UPDATE ::: Removing annoy from python install file
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@36440 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 5cff318016e36079ca60aae1caf5acba93226d67

--- py/install.sh
+++ py/install.sh
@@ -3,7 +3,6 @@ pip uninstall Jep
 pip install jep==3.9.0
 echo 'Installing python libraries'
 pip install pandas==0.25.3
-pip install annoy
 pip install numpy
 pip install fuzzywuzzy
 pip install random
