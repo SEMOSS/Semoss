fea1695553e8891d0defcf7cf6575ac2781a0825
mahkhalil <mahkhalil@deloitte.com>
2019-12-18 12:40:04 +0000

    UPDATE ::: Adding improvements to python replace/regex replace clean routines
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@36487 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/py/RegexReplaceColumnValueReactor.java
+++ src/prerna/sablecc2/reactor/frame/py/RegexReplaceColumnValueReactor.java
@@ -2,18 +2,19 @@ package prerna.sablecc2.reactor.frame.py;
 
 import java.util.List;
 import java.util.Vector;
+import java.util.regex.Pattern;
 
+import prerna.algorithm.api.SemossDataType;
 import prerna.ds.py.PandasFrame;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
-import prerna.sablecc2.reactor.frame.AbstractFrameReactor;
 import prerna.util.usertracking.AnalyticsTrackerHelper;
 import prerna.util.usertracking.UserTrackerFactory;
 
-public class RegexReplaceColumnValueReactor extends AbstractFrameReactor {
+public class RegexReplaceColumnValueReactor extends AbstractPyFrameReactor {
 
 	/**
 	 * This reactor updates row values based on a regex It replaces all portions
@@ -22,9 +23,10 @@ public class RegexReplaceColumnValueReactor extends AbstractFrameReactor {
 	 * for 3) value to replace the regex with
 	 */
 
+	private static final Pattern NUMERIC_PATTERN = Pattern.compile("-?\\d+(\\.\\d+)?");
+
 	public RegexReplaceColumnValueReactor() {
-		this.keysToGet = new String[] { ReactorKeysEnum.COLUMN.getKey(), ReactorKeysEnum.VALUE.getKey(),
-				ReactorKeysEnum.NEW_VALUE.getKey() };
+		this.keysToGet = new String[] { ReactorKeysEnum.COLUMN.getKey(), ReactorKeysEnum.VALUE.getKey(), ReactorKeysEnum.NEW_VALUE.getKey() };
 	}
 
 	@Override
@@ -52,12 +54,52 @@ public class RegexReplaceColumnValueReactor extends AbstractFrameReactor {
 			throw new IllegalArgumentException("Need to define " + this.keysToGet[2]);
 		}
 
+		int numColumns = columnNames.size();
+		String[] scripts = new String[columnNames.size()];
+		
 		// iterate through all passed columns
-		for (String column : columnNames) {
-			String neededQuote = (boolean) frame.runScript(wrapperFrameName + ".is_numeric('" + column + "')") ? "" : "'";
-			frame.runScript(wrapperFrameName + ".regex_replace_val('" + column + "', '" + regex + "' , " + neededQuote + newValue + neededQuote + ")");
+		for(int i = 0; i < numColumns; i++) {
+			String column = columnNames.get(i);
+		
+			SemossDataType columnDataType = SemossDataType.convertStringToDataType(getColumnType(frame, column));
+
+			if (columnDataType == SemossDataType.INT || columnDataType == SemossDataType.DOUBLE) {
+				if(newValue.isEmpty() || newValue.equalsIgnoreCase("null") || newValue.equalsIgnoreCase("na") || newValue.equalsIgnoreCase("nan")) {
+					newValue = "NaN";
+				} else if(!NUMERIC_PATTERN.matcher(newValue).matches()) {
+					throw new IllegalArgumentException("Cannot update a numeric field with string value = " + newValue);
+				}
+				
+				// TODO: See why this is not executing properly in python!
+				scripts[i] = wrapperFrameName + ".regex_replace_val('" + column + "', " + regex + ", " + newValue + ")";
+			} else if (columnDataType == SemossDataType.DATE) {
+				// what is this doing?
+				
+//				// TODO: should grab value of whole column with matched regex, not values past in
+//				SemossDate oldD = SemossDate.genDateObj(regex);
+//				SemossDate newD = SemossDate.genDateObj(newValue);
+//				String error = "";
+//				if(oldD == null) {
+//					error = "Unable to parse old date value = " + regex;
+//				}
+//				if(newD == null) {
+//					error += "Unable to parse new date value = " + newValue;
+//				}
+//				if(!error.isEmpty()) {
+//					throw new IllegalArgumentException(error);
+//				}
+//				script.append(wrapperFrameName).append(".regex_replace_val('").append(column).append("',")
+//				.append("pd.to_datetime('").append(oldD).append("')").append(" , ").append("pd.to_datetime('")
+//				.append(newD).append("')").append(")");
+//				frame.runScript(script.toString());
+			} else if (columnDataType == SemossDataType.STRING) {
+				scripts[i] = wrapperFrameName + ".regex_replace_val('" + column +"', '" + regex + "' , '" + newValue + "')";
+			}
 		}
 
+		// execute all of the routines after we have done our validation
+		frame.runScript(scripts);
+		
 		// NEW TRACKING
 		UserTrackerFactory.getInstance().trackAnalyticsWidget(this.insight, frame, "RegexReplaceColumnValue",
 				AnalyticsTrackerHelper.getHashInputs(this.store, this.keysToGet));
