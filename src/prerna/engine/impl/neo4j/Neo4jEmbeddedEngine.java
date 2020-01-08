0b14a9db35287884ea4d4f8cd4fc51ee82f0d404
rramirezjimenez <rramirezjimenez@deloitte.com>
2020-01-08 19:38:33 +0000

    UPDATE ::: Pushing CypherInterpreter need to implement the new query node method UPDATE ::: Added typeMap and nameMap to neo4j engines need to implement it in CypherInterpreter
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@36639 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/engine/impl/neo4j/Neo4jEmbeddedEngine.java
+++ src/prerna/engine/impl/neo4j/Neo4jEmbeddedEngine.java
@@ -1,10 +1,13 @@
 package prerna.engine.impl.neo4j;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.Transaction;
@@ -17,14 +20,41 @@ import prerna.engine.impl.AbstractEngine;
 import prerna.engine.impl.SmssUtilities;
 import prerna.query.interpreters.CypherInterpreter;
 import prerna.query.interpreters.IQueryInterpreter;
+import prerna.util.Constants;
 
 public class Neo4jEmbeddedEngine extends AbstractEngine {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Neo4jEmbeddedEngine.class);
 	private GraphDatabaseService db;
+	protected Map<String, String> typeMap = new HashMap<String, String>();
+	protected Map<String, String> nameMap = new HashMap<String, String>();
+	protected boolean useLabel = false;
 
 	@Override
 	public void openDB(String propFile) {
 		super.openDB(propFile);
+		// get type map
+		String typeMapStr = this.prop.getProperty("TYPE_MAP");
+		if (typeMapStr != null && !typeMapStr.trim().isEmpty()) {
+			try {
+				this.typeMap = new ObjectMapper().readValue(typeMapStr, Map.class);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		// get the name map
+		String nameMapStr = this.prop.getProperty("NAME_MAP");
+		if (nameMapStr != null && !nameMapStr.trim().isEmpty()) {
+			try {
+				this.nameMap = new ObjectMapper().readValue(nameMapStr, Map.class);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		if (prop.containsKey(Constants.TINKER_USE_LABEL)) {
+			String booleanStr = prop.get(Constants.TINKER_USE_LABEL).toString();
+			useLabel = Boolean.parseBoolean(booleanStr);
+		}
+
 		String neo4jFile = SmssUtilities.getNeo4jFile(prop).getAbsolutePath();
 		try {
 			LOGGER.info("Opening neo4j graph: " + neo4jFile);
@@ -72,7 +102,9 @@ public class Neo4jEmbeddedEngine extends AbstractEngine {
 
 	@Override
 	public IQueryInterpreter getQueryInterpreter() {
-		return new CypherInterpreter();
+		CypherInterpreter interp = new CypherInterpreter(this.typeMap, this.nameMap);
+		interp.setUseLabel(this.useLabel);
+		return interp;
 	}
 
 	public GraphDatabaseService getGraphDatabaseService() {
