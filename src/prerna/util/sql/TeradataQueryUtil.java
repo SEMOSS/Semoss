5d1021702e5412ab739c1860de590329728b16fe
kunalppatel9 <kunalppatel9@deloitte.com>
2020-01-24 20:58:46 +0000

    ADDED ::: Specific SQL Interpreter for Teradata BUGFIX ::: Modified jdbc string template for Teradata
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@36920 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/util/sql/TeradataQueryUtil.java
@@ -0,0 +1,55 @@
+package prerna.util.sql;
+
+import java.sql.Connection;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import prerna.algorithm.api.ITableDataFrame;
+import prerna.algorithm.api.SemossDataType;
+import prerna.engine.api.IEngine;
+import prerna.query.interpreters.IQueryInterpreter;
+import prerna.query.interpreters.sql.TeradataSqlInterpreter;
+import prerna.sablecc2.om.Join;
+import prerna.util.Utility;
+
+public class TeradataQueryUtil extends AnsiSqlQueryUtil {
+
+	TeradataQueryUtil() {
+		super();
+	}
+	
+	TeradataQueryUtil(String connectionUrl, String username, String password) {
+		super(connectionUrl, username, password);
+	}
+	
+	TeradataQueryUtil(RdbmsTypeEnum dbType, String hostname, String port, String schema, String username, String password) {
+		super(dbType, hostname, port, schema, username, password);
+	}
+	
+	@Override
+	public IQueryInterpreter getInterpreter(IEngine engine) {
+		return new TeradataSqlInterpreter(engine);
+	}
+
+	@Override
+	public IQueryInterpreter getInterpreter(ITableDataFrame frame) {
+		return new TeradataSqlInterpreter(frame);
+	}
+	
+	
+	public StringBuilder addLimitOffsetToQuery(StringBuilder query, long limit, long offset, String tempTable) {
+
+		if(limit > 0) {
+			query=query.insert(0, "SELECT TOP " + limit + " * from (");
+			query=query.append(") as "+ tempTable);
+		}
+		
+		//TODO there is no offset for now
+//		if(offset > 0) {
+//			query = query.append(" OFFSET "+offset);
+//		}
+		return query;
+	}
+
+}
