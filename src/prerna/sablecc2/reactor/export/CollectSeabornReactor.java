836596a51a14201102f5ace6749cb7d0bf16c845
davyzhang <davyzhang@deloitte.com>
2020-01-28 15:44:24 +0000

    UPDATE ::: returning the required task data output; some minor cleanup
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@36957 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/export/CollectSeabornReactor.java
+++ src/prerna/sablecc2/reactor/export/CollectSeabornReactor.java
@@ -1,7 +1,6 @@
 package prerna.sablecc2.reactor.export;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringWriter;
 import java.nio.file.Files;
@@ -24,7 +23,6 @@ import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.om.task.BasicIteratorTask;
 import prerna.sablecc2.om.task.ConstantDataTask;
 import prerna.sablecc2.om.task.options.TaskOptions;
-import prerna.sablecc2.reactor.frame.r.util.AbstractRJavaTranslator;
 import prerna.sablecc2.reactor.task.TaskBuilderReactor;
 import prerna.util.Utility;
 
@@ -41,7 +39,6 @@ public class CollectSeabornReactor extends TaskBuilderReactor {
 	}
 	
 	public NounMetadata execute() {
-		
 		organizeKeys();
 
 		PyTranslator pyt = this.insight.getPyTranslator();
@@ -54,7 +51,6 @@ public class CollectSeabornReactor extends TaskBuilderReactor {
 		
 		if(keyValue.containsKey(keysToGet[1]))
 			format = keyValue.get(keysToGet[1]);
-
 		
 		// I neeed to get the basic iterator and then get types from there
 		// this is typically what we do on seaborn
@@ -75,7 +71,6 @@ public class CollectSeabornReactor extends TaskBuilderReactor {
 			typeMap.put(headers[i],sTypes[i]);
 		}
 		
-		
 		// I need to see how to get this to temp
 		String fileName = Utility.getRandomString(6);
 		String dir = insight.getUserFolder() + "/Temp";
@@ -85,7 +80,6 @@ public class CollectSeabornReactor extends TaskBuilderReactor {
 			tempDir.mkdir();
 		String outputFile = dir + "/" + fileName + ".csv";
 		Utility.writeResultToFile(outputFile, this.task, ",");
-		
 
 		// need something here to adjust the types
 		// need to move this to utilities 
@@ -96,8 +90,6 @@ public class CollectSeabornReactor extends TaskBuilderReactor {
 		String adjustTypes = Utility.adjustTypePy(fileName, headers, typeMap);
 		// run the job
 		//pyt.runEmptyPy(loadDT, adjustTypes);
-		
-		
 		// now comes the building part
 		// I need to ask kunal if he mauls the path so I cannot load seaborn anymore
 		
@@ -130,51 +122,39 @@ public class CollectSeabornReactor extends TaskBuilderReactor {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();		
-		//sw.write("<html><body>");
 		}
 
-
-		
-		
 		// remove the csv and the generated jpeg
 		new File(outputFile).delete();
 		new File(seabornFile).delete();
 		
 		// Need to figure out if I am trying to delete the image and URI encode it at some point.. 
-		
 		ConstantDataTask cdt = new ConstantDataTask();
-		// need to do all the sets
-		cdt.setFormat("TABLE");
 		
 		// I need to create the options here
-		Map outputMap = new HashMap<String, Object>();
-		
-		TaskOptions options = new TaskOptions(outputMap);
-		cdt.setTaskOptions(options);
+		Map<String, Object> outputMap = new HashMap<String, Object>();
+
+		// need to do all the sets
+		cdt.setFormat("TABLE");
+		cdt.setTaskOptions(task.getTaskOptions());
 		cdt.setHeaderInfo(task.getHeaderInfo());
 		cdt.setSortInfo(task.getSortInfo());
+		cdt.setId(task.getId());
 		
 		outputMap.put("headers", new String[] {});
 		outputMap.put("rawHeaders", new String[] {});
 		outputMap.put("values", new String[]{sw.toString()});
-		//outputMap.put("values", new String[]{retFile});
-		//outputMap.put("ggplot", "ggplot=[\"" + ggplotCommand + "\"]");	
-		outputMap.put(keysToGet[0], splot);
-
+		outputMap.put("splot", splot);	
+		outputMap.put("format", format);
 		
 		// set the output so it can give it
-		//cdt.setOutputData(seabornFile);
 		cdt.setOutputData(outputMap);
 
-		// I dont think the filter information is required
-		//cdt.setFilterInfo(task.getFilterInfo());
 		// delete the pivot later
-		return new NounMetadata(cdt, PixelDataType.FORMATTED_DATA_SET, PixelOperationType.FILE);
+		return new NounMetadata(cdt, PixelDataType.FORMATTED_DATA_SET, PixelOperationType.TASK_DATA, PixelOperationType.FILE);
 	}
 	
-
 	// keeping these methods for now.. I am not sure I require them
-	
 	@Override
 	protected void buildTask() {
 		// if the task was already passed in
@@ -243,6 +223,4 @@ public class CollectSeabornReactor extends TaskBuilderReactor {
 	// txt <- base64Encode(readBin(tf1, "raw", file.info(tf1)[1, "size"]), "txt")
 	// tf1 - is just the file location
 	// https://stackoverflow.com/questions/33409363/convert-r-image-to-base-64/36707831?noredirect=1#comment61003382_36707831
-	
-	
 }
