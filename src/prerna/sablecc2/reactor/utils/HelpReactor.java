042646f570e5600c493f4954881bd249ca40c749
mahkhalil <mahkhalil@deloitte.com>
2020-02-06 21:09:36 +0000

    BUGFIX ::: Fixing error with creating the asset app multiple times due to security manager preventing exec to make it gitable
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@37076 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/utils/HelpReactor.java
+++ src/prerna/sablecc2/reactor/utils/HelpReactor.java
@@ -15,54 +15,63 @@ public class HelpReactor extends AbstractReactor {
 	 * There are no inputs to the reactor
 	 */
 	
+	private static final String RESET_KEY = "reset";
+	private static String helpString = null;
+	
+	public HelpReactor() {
+		this.keysToGet = new String[] {RESET_KEY};
+	}
 	
 	@Override
 	public NounMetadata execute() {
-		
-		// create a string builder to keep track of all categories of reactors
-		StringBuilder allReactors = new StringBuilder();
-		
-		// general reactors
-		TreeSet general = new TreeSet(ReactorFactory.reactorHash.keySet());
-		if (general.size() > 0) {
-			allReactors.append("General Reactors: \n").append(formatOutput(general));
+		organizeKeys();
+		boolean reset = Boolean.parseBoolean(this.keyValue.get(this.keysToGet[0]));
+		if(reset || helpString == null) {
+			// create a string builder to keep track of all categories of reactors
+			StringBuilder allReactors = new StringBuilder();
+			
+			// general reactors
+			TreeSet general = new TreeSet(ReactorFactory.reactorHash.keySet());
+			if (general.size() > 0) {
+				allReactors.append("General Reactors: \n").append(formatOutput(general));
+			}
+			
+			// the frame specific reactors
+			//rframe
+			TreeSet rFrame = new TreeSet(ReactorFactory.rFrameHash.keySet());
+			if (rFrame.size() > 0) {
+				allReactors.append("R Frame Reactors: \n").append(formatOutput(rFrame));
+			}
+			
+			//h2
+			TreeSet h2Frame = new TreeSet(ReactorFactory.h2FrameHash.keySet());
+			if (h2Frame.size() > 0) {
+				allReactors.append("H2 Frame Reactors: \n").append(formatOutput(h2Frame));
+			}
+			
+			//native
+			TreeSet nativeFrame = new TreeSet(ReactorFactory.nativeFrameHash.keySet());
+			if (nativeFrame.size() > 0) {
+				allReactors.append("Native Frame Reactors: \n").append(formatOutput(nativeFrame));
+			}
+			
+			//tinker
+			TreeSet tinkerFrame = new TreeSet(ReactorFactory.tinkerFrameHash.keySet());
+			if (tinkerFrame.size() > 0) {
+				allReactors.append("Tinker Frame Reactors: \n").append(formatOutput(tinkerFrame));
+			}
+			
+			// the expression set
+			TreeSet expressionSet = new TreeSet(ReactorFactory.expressionHash.keySet());
+			if (expressionSet.size() > 0) {
+				allReactors.append("Expression Set Reactors: \n").append(formatOutput(expressionSet));
+			}
+			
+			//return a string with all of the reactors
+			HelpReactor.helpString = allReactors.toString();
 		}
 		
-		// the frame specific reactors
-		//rframe
-		TreeSet rFrame = new TreeSet(ReactorFactory.rFrameHash.keySet());
-		if (rFrame.size() > 0) {
-			allReactors.append("R Frame Reactors: \n").append(formatOutput(rFrame));
-		}
-		
-		//h2
-		TreeSet h2Frame = new TreeSet(ReactorFactory.h2FrameHash.keySet());
-		if (h2Frame.size() > 0) {
-			allReactors.append("H2 Frame Reactors: \n").append(formatOutput(h2Frame));
-		}
-		
-		//native
-		TreeSet nativeFrame = new TreeSet(ReactorFactory.nativeFrameHash.keySet());
-		if (nativeFrame.size() > 0) {
-			allReactors.append("Native Frame Reactors: \n").append(formatOutput(nativeFrame));
-		}
-		
-		//tinker
-		TreeSet tinkerFrame = new TreeSet(ReactorFactory.tinkerFrameHash.keySet());
-		if (tinkerFrame.size() > 0) {
-			allReactors.append("Tinker Frame Reactors: \n").append(formatOutput(tinkerFrame));
-		}
-		
-		// the expression set
-		TreeSet expressionSet = new TreeSet(ReactorFactory.expressionHash.keySet());
-		if (expressionSet.size() > 0) {
-			allReactors.append("Expression Set Reactors: \n").append(formatOutput(expressionSet));
-		}
-		
-		//return a string with all of the reactors
-		String reactors = allReactors.toString();
-		return new NounMetadata(reactors, PixelDataType.CONST_STRING, PixelOperationType.HELP);
-		
+		return new NounMetadata(helpString, PixelDataType.CONST_STRING, PixelOperationType.HELP);
 	}
 	
 	private String formatOutput(TreeSet t) {
