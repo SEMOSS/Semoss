4056cbfbef842b144b96e0803acb41daccf9d92b
Maher Khalil <themaherkhalil@gmail.com>
2020-02-23 23:39:07 +0000

    UPDATE ::: Making the same modification in the teradata sql interpreter
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@37326 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 0305c3a1171c5e147d5c877d638db366eb84018b

--- src/prerna/query/interpreters/sql/TeradataSqlInterpreter.java
+++ src/prerna/query/interpreters/sql/TeradataSqlInterpreter.java
@@ -1,16 +1,10 @@
 package prerna.query.interpreters.sql;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Properties;
 import java.util.Vector;
-
-import org.apache.log4j.Logger;
 
 import prerna.algorithm.api.ITableDataFrame;
 import prerna.engine.api.IEngine;
-import prerna.query.interpreters.IQueryInterpreter;
-import prerna.query.querystruct.AbstractQueryStruct;
 import prerna.query.querystruct.HardSelectQueryStruct;
 import prerna.query.querystruct.SelectQueryStruct;
 import prerna.query.querystruct.selectors.IQuerySelector;
@@ -19,25 +13,23 @@ import prerna.query.querystruct.selectors.QueryColumnOrderBySelector.ORDER_BY_DI
 import prerna.query.querystruct.selectors.QueryColumnSelector;
 import prerna.util.Constants;
 import prerna.util.Utility;
-import prerna.util.sql.RdbmsTypeEnum;
-import prerna.util.sql.SqlQueryUtilFactor;
 import prerna.util.sql.TeradataQueryUtil;
 
 public class TeradataSqlInterpreter  extends SqlInterpreter {
 
-	
+
 	public TeradataSqlInterpreter() {
-		
+
 	}
 
 	public TeradataSqlInterpreter(IEngine engine) {
 		super(engine);
-		}
-	
+	}
+
 	public TeradataSqlInterpreter(ITableDataFrame frame) {
 		super(frame);
 	}
-	
+
 	//overriden since i need limit before the order by
 	@Override
 	public String composeQuery()
@@ -45,16 +37,15 @@ public class TeradataSqlInterpreter  extends SqlInterpreter {
 		if(this.qs instanceof HardSelectQueryStruct) {
 			return ((HardSelectQueryStruct)this.qs).getQuery();
 		}
-		
-		
+
 		//If the smss says this app is part of a big data engine, then some changes happen.
 		// 1) grid does not display distinct values
 		// 2) anything with no task options 
-//		Boolean bigDataEngine = false;
-//		Object bigDataProp = this.engine.getProp().get(Constants.BIG_DATA_ENGINE);
-//		if(bigDataProp!= null){
-//			bigDataEngine = Boolean.parseBoolean(this.engine.getProp().get(Constants.BIG_DATA_ENGINE).toString());
-//		}
+		//		Boolean bigDataEngine = false;
+		//		Object bigDataProp = this.engine.getProp().get(Constants.BIG_DATA_ENGINE);
+		//		if(bigDataProp!= null){
+		//			bigDataEngine = Boolean.parseBoolean(this.engine.getProp().get(Constants.BIG_DATA_ENGINE).toString());
+		//		}
 
 		/*
 		 * Need to create the query... 
@@ -68,21 +59,18 @@ public class TeradataSqlInterpreter  extends SqlInterpreter {
 		 * 		the join 
 		 */
 
+		String customFrom = this.qs.getCustomFrom();
+		this.customFromAliasName = this.qs.getCustomFromAliasName();
 		// we do the joins since when we get to adding the from portion of the query
 		// we want to make sure that table is not used within the joins
 		addJoins();
 		addSelectors();
 		addFilters();
 		addHavingFilters();
-		
 
-		 
-		StringBuilder query = new StringBuilder("SELECT ");
-		
 		//if there it is a big data engine, dont put distinct onto qs's with task options
 		//other wise us the standard logic
 		if(this.qs.getBigDataEngine()){
-			
 			//big data engine is never distinct
 			((SelectQueryStruct) this.qs).setDistinct(false);
 
@@ -91,68 +79,72 @@ public class TeradataSqlInterpreter  extends SqlInterpreter {
 			if(isTaskProp != null){
 				taskOptionsExist= Boolean.parseBoolean(isTaskProp.toString());
 			}
-			
-			//if task options exists, it likely a vis
+
+			// if task options exists, it likely a vis
 			if(!taskOptionsExist){
-			// if there is 1 selector, group by it instead
-			 if (((SelectQueryStruct) this.qs).getSelectors().size()==1){
-				  IQuerySelector selector = ((SelectQueryStruct) this.qs).getSelectors().get(0);
-				  if(selector.getSelectorType()==IQuerySelector.SELECTOR_TYPE.COLUMN){
-				  ((SelectQueryStruct) this.qs).addGroupBy((QueryColumnSelector)selector);
-				  }
-				// this.qs.group
-			//	((SelectQueryStruct) this.qs).addGroupBy(selectorList.);
-			}
+				// if there is 1 selector, group by it instead
+				if (((SelectQueryStruct) this.qs).getSelectors().size() == 1){
+					IQuerySelector selector = ((SelectQueryStruct) this.qs).getSelectors().get(0);
+					if(selector.getSelectorType()==IQuerySelector.SELECTOR_TYPE.COLUMN){
+						((SelectQueryStruct) this.qs).addGroupBy((QueryColumnSelector)selector);
+					}
+				}
 			}
 		}
-		
-			
-		
 
+		StringBuilder query = new StringBuilder("SELECT ");
 		String distinct = "";
-	
 		if(((SelectQueryStruct) this.qs).isDistinct()) {
 			distinct = "DISTINCT ";
 		}
-		if(this.engine != null && !engine.isBasic() && joinStructList.isEmpty()) {
-			// if there are no joins, we know we are querying from a single table
-			// the vast majority of the time, there shouldn't be any duplicates if
-			// we are selecting all the columns
-			String table = froms.get(0)[0];
-			if(engine != null && !engine.isBasic()) {
-				String physicalUri = engine.getPhysicalUriFromPixelSelector(table);
-				if( (engine.getPhysicalConcepts().size() == 1) && (engine.getPropertyUris4PhysicalUri(physicalUri).size() + 1) == selectorList.size()) {
-					// plus one is for the concept itself
-					// no distinct needed
-					query.append(selectors);
+
+		// do we have a custom from?
+		if(customFrom != null && !customFrom.isEmpty()) {
+			// at the moment
+			// no join logic with custom from
+			query.append(distinct).append(selectors).append(" FROM (").append(customFrom).append(" ) AS " + this.customFromAliasName);
+		} else {
+			// logic for adding the selectors + the from statement + the joins
+
+			if(this.engine != null && !engine.isBasic() && joinStructList.isEmpty()) {
+				// if there are no joins, we know we are querying from a single table
+				// the vast majority of the time, there shouldn't be any duplicates if
+				// we are selecting all the columns
+				String table = froms.get(0)[0];
+				if(engine != null && !engine.isBasic()) {
+					String physicalUri = engine.getPhysicalUriFromPixelSelector(table);
+					if( (engine.getPhysicalConcepts().size() == 1) && (engine.getPropertyUris4PhysicalUri(physicalUri).size() + 1) == selectorList.size()) {
+						// plus one is for the concept itself
+						// no distinct needed
+						query.append(selectors);
+					} else {
+						query.append(distinct).append(selectors);
+					}
 				} else {
-					query.append(distinct).append(selectors);
+					// need a distinct
+					query.append(distinct).append(selectors).append(" FROM ");
 				}
 			} else {
-				// need a distinct
-				query.append(distinct).append(selectors).append(" FROM ");
+				// default is to use a distinct
+				query.append(distinct).append(selectors);
 			}
-		} else {
-			// default is to use a distinct
-			query.append(distinct).append(selectors);
+
+			// if there is a join
+			// can only have one table in from in general sql case 
+			// thus, the order matters 
+			// so get a good starting from table
+			// we can use any of the froms that is not part of the join
+			List<String> startPoints = new Vector<String>();
+			if(joinStructList.isEmpty()) {
+				query.append(" FROM ");
+				String[] startPoint = froms.get(0);
+				query.append(startPoint[0]).append(" ").append(startPoint[1]).append(" ");
+				startPoints.add(startPoint[1]);
+			} else {
+				query.append(" ").append(joinStructList.getJoinSyntax());
+			}
 		}
-		
-		
-		// if there is a join
-		// can only have one table in from in general sql case 
-		// thus, the order matters 
-		// so get a good starting from table
-		// we can use any of the froms that is not part of the join
-		List<String> startPoints = new Vector<String>();
-		if(joinStructList.isEmpty()) {
-			query.append(" FROM ");
-			String[] startPoint = froms.get(0);
-			query.append(startPoint[0]).append(" ").append(startPoint[1]).append(" ");
-			startPoints.add(startPoint[1]);
-		} else {
-			query.append(" ").append(joinStructList.getJoinSyntax());
-		}
-		
+
 		// add where clause filters
 		int numFilters = this.filterStatements.size();
 		for(int i = 0; i < numFilters; i++) {
@@ -163,7 +155,7 @@ public class TeradataSqlInterpreter  extends SqlInterpreter {
 			}
 			query.append(this.filterStatements.get(i).toString());
 		}
-		
+
 		//grab the order by and get the corresponding display name for that order by column
 		query = appendGroupBy(query);
 		// add having filters
@@ -177,10 +169,10 @@ public class TeradataSqlInterpreter  extends SqlInterpreter {
 			query.append(this.havingFilterStatements.get(i).toString());
 		}
 
-		
+
 		long limit = ((SelectQueryStruct) this.qs).getLimit();
 		long offset = ((SelectQueryStruct) this.qs).getOffset();
-		
+
 		String tempTable = Utility.getRandomString(6);
 		if(((SelectQueryStruct) this.qs).isDistinct()) {
 			query = ((TeradataQueryUtil) this.queryUtil).addLimitOffsetToQuery(query, limit, offset, tempTable);
@@ -190,7 +182,6 @@ public class TeradataSqlInterpreter  extends SqlInterpreter {
 			query = ((TeradataQueryUtil) this.queryUtil).addLimitOffsetToQuery(query, limit, offset);
 			query = appendOrderBy(query);
 		}
-		
 
 		if(query.length() > 500) {	
 			logger.debug("SQL QUERY....  " + query.substring(0,  500) + "...");
@@ -203,70 +194,69 @@ public class TeradataSqlInterpreter  extends SqlInterpreter {
 
 	private StringBuilder appendOrderBy(StringBuilder query, String tempTable) {
 		//grab the order by and get the corresponding display name for that order by column
+		List<QueryColumnOrderBySelector> orderBy = ((SelectQueryStruct) this.qs).getOrderBy();
+		List<StringBuilder> validOrderBys = new Vector<StringBuilder>();
+		for(QueryColumnOrderBySelector orderBySelector : orderBy) {
+			String tableConceptualName = orderBySelector.getTable();
+			String columnConceptualName = orderBySelector.getColumn();
+			ORDER_BY_DIRECTION orderByDir = orderBySelector.getSortDir();
 
-			List<QueryColumnOrderBySelector> orderBy = ((SelectQueryStruct) this.qs).getOrderBy();
-				List<StringBuilder> validOrderBys = new Vector<StringBuilder>();
-				for(QueryColumnOrderBySelector orderBySelector : orderBy) {
-					String tableConceptualName = orderBySelector.getTable();
-					String columnConceptualName = orderBySelector.getColumn();
-					ORDER_BY_DIRECTION orderByDir = orderBySelector.getSortDir();
-					
-					boolean origPrim = false;
-					if(columnConceptualName.equals(SelectQueryStruct.PRIM_KEY_PLACEHOLDER)){
-						origPrim = true;
-						columnConceptualName = getPrimKey4Table(tableConceptualName);
-					} else {
-						columnConceptualName = getPhysicalPropertyNameFromConceptualName(tableConceptualName, columnConceptualName);
-					}
-					
-					StringBuilder thisOrderBy = new StringBuilder();
-					
-					// might want to order by a derived column being returned
-					if(origPrim && this.selectorAliases.contains(tableConceptualName)) {
-						// either instantiate the string builder or add a comma for multi sort
-						if(queryUtil.isSelectorKeyword(tableConceptualName)) {
-							thisOrderBy.append(queryUtil.getEscapeKeyword(tempTable));
-						} else {
-							thisOrderBy.append(tempTable);
-						}
-					}
-					// we need to make sure the sort is a valid one!
-					// if it is not already processed, there is no way to sort it...
-					else if(this.retTableToCols.containsKey(tableConceptualName)){
-						if(this.retTableToCols.get(tableConceptualName).contains(columnConceptualName)) {
-							String orderByColumn = columnConceptualName;
-							if(queryUtil.isSelectorKeyword(orderByColumn)) {
-								orderByColumn = queryUtil.getEscapeKeyword(orderByColumn);
-							}
-							thisOrderBy.append(tempTable).append(".").append(orderByColumn);
-						} else {
-							continue;
-						}
-					} 
-					
-					// well, this is not a valid order by to add
-					else {
-						continue;
-					}
-					
-					if(orderByDir == ORDER_BY_DIRECTION.ASC) {
-						thisOrderBy.append(" ASC ");
-					} else {
-						thisOrderBy.append(" DESC ");
-					}
-					validOrderBys.add(thisOrderBy);
-				}
-				
-				int size = validOrderBys.size();
-				for(int i = 0; i < size; i++) {
-					if(i == 0) {
-						query.append(" ORDER BY ");
-					} else {
-						query.append(", ");
-					}
-					query.append(validOrderBys.get(i).toString());
-				}
-				return query;
+			boolean origPrim = false;
+			if(columnConceptualName.equals(SelectQueryStruct.PRIM_KEY_PLACEHOLDER)){
+				origPrim = true;
+				columnConceptualName = getPrimKey4Table(tableConceptualName);
+			} else {
+				columnConceptualName = getPhysicalPropertyNameFromConceptualName(tableConceptualName, columnConceptualName);
 			}
-	
+
+			StringBuilder thisOrderBy = new StringBuilder();
+
+			// might want to order by a derived column being returned
+			if(origPrim && this.selectorAliases.contains(tableConceptualName)) {
+				// either instantiate the string builder or add a comma for multi sort
+				if(queryUtil.isSelectorKeyword(tableConceptualName)) {
+					thisOrderBy.append(queryUtil.getEscapeKeyword(tempTable));
+				} else {
+					thisOrderBy.append(tempTable);
+				}
+			}
+			// we need to make sure the sort is a valid one!
+			// if it is not already processed, there is no way to sort it...
+			else if(this.retTableToCols.containsKey(tableConceptualName)){
+				if(this.retTableToCols.get(tableConceptualName).contains(columnConceptualName)) {
+					String orderByColumn = columnConceptualName;
+					if(queryUtil.isSelectorKeyword(orderByColumn)) {
+						orderByColumn = queryUtil.getEscapeKeyword(orderByColumn);
+					}
+					thisOrderBy.append(tempTable).append(".").append(orderByColumn);
+				} else {
+					continue;
+				}
+			} 
+
+			// well, this is not a valid order by to add
+			else {
+				continue;
+			}
+
+			if(orderByDir == ORDER_BY_DIRECTION.ASC) {
+				thisOrderBy.append(" ASC ");
+			} else {
+				thisOrderBy.append(" DESC ");
+			}
+			validOrderBys.add(thisOrderBy);
+		}
+
+		int size = validOrderBys.size();
+		for(int i = 0; i < size; i++) {
+			if(i == 0) {
+				query.append(" ORDER BY ");
+			} else {
+				query.append(", ");
+			}
+			query.append(validOrderBys.get(i).toString());
+		}
+		return query;
+	}
+
 }
