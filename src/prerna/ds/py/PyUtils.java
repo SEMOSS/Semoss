9a19028f955e5bf5f764e9791afb34c04d48ee2d
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2020-02-27 14:26:18 +0000

    PyServe - File based Python Processes instead of in-memory java. Things to add to environment / to RDF_MAP - PYTHON	TRUE USE_PYTHON	TRUE PY_WORKER	prerna.pyserve.FileBasedPyWorker PY_WORKER_CP	fst-2.56.jar;jep-3.9.0.jar;log4j-1.2.17.jar;commons-io-2.4.jar;objenesis-2.5.1.jar;jackson-core-2.9.5.jar;javassist-3.20.0-GA.jar;classes // this can even be in the environment LD_LIBRARY_PATH	C:/Python/Python36/Lib/site-packages/jep
    FileBasedPyWorker - the main py worker
    IWorker - interface for py worker
    FileThread - main thread that manages information
    Other updates - incorporate changes for Py
    
    
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@37381 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 1f8b5449d08ec66ff9c5d4e60184e4f76d0dd8bc

--- src/prerna/ds/py/PyUtils.java
+++ src/prerna/ds/py/PyUtils.java
@@ -1,10 +1,20 @@
 package prerna.ds.py;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import prerna.util.Constants;
 import prerna.util.DIHelper;
+import prerna.util.Utility;
 
 public class PyUtils {
 	
@@ -12,6 +22,8 @@ public class PyUtils {
 	
 	private static Boolean pyEnabled = null;
 	private static PyUtils instance;
+	Map userTupleMap = new Hashtable();
+	Map userProcessMap = new Hashtable();
 	
 	private PyUtils() {
 		
@@ -82,4 +94,91 @@ public class PyUtils {
 			LOGGER.info(">>>>>> COMPLETE <<<<<");
 		}
 	}
+	
+	// just gets the directory of where the main user has started
+	public String getTempTupleSpace(Object user, String dir)
+	{
+		if(user != null && !userTupleMap.containsKey(user))
+		{
+			try {
+				LOGGER.info(">>>STARTING PYTHON TUPESPACE FOR USER<<<");
+				// going to create this in insight cache dir
+				//String mainCache = DIHelper.getInstance().getProperty(Constants.INSIGHT_CACHE_DIR);
+				Path mainCachePath = Paths.get(dir);
+				Path tempDirForUser = Files.createTempDirectory(mainCachePath, "a");
+				writeLogConfigurationFile(tempDirForUser.toString());
+				userTupleMap.put(user, tempDirForUser.toString());
+				// this should possibly also launch the thread
+				String cp = DIHelper.getInstance().getProperty("PY_WORKER_CP");
+				Process p = Utility.startPyProcess(cp, tempDirForUser.toString());
+				userProcessMap.put(user,  p);
+				LOGGER.info(">>>TUPLS SPACE SET TO  " + tempDirForUser + " <<<");
+				return tempDirForUser.toString();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else
+			LOGGER.info("=== TUPLE SPACE NOT CREATED ====");
+		return null;
+	}
+	
+	private void writeLogConfigurationFile(String dir)
+	{
+		try {
+			// read the file first
+			dir = dir.replace("\\", "/");
+			String baseFolder = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
+			File logFile = new File(baseFolder + "/Py/log-config/log4j.properties");
+			String logConfig = FileUtils.readFileToString(logFile);
+			logConfig = logConfig.replace("FILE_LOCATION", dir + "/output.log");
+			File newLogFile = new File(dir + "/log4j.properties");
+			FileUtils.writeStringToFile(newLogFile, logConfig);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+		
+	public void killTempTupleSpace(Object user)
+	{
+		// kill the process
+		// take out the dir
+		LOGGER.info(">>>KILLING PYTHON TUPESPACE FOR USER<<<");
+		
+		if(userTupleMap.containsKey(user))
+		{
+			String dir = (String)userTupleMap.get(user);
+		// change this to just creating a file so it is simpler
+			File closer = new File(dir + "/alldone.closeall");
+			try
+			{
+				Process p = (Process)userProcessMap.get(user);
+				closer.createNewFile();
+				p.destroyForcibly();
+				// delete the directory fully
+				FileUtils.deleteDirectory(new File(dir));
+			}catch(Exception ex)
+			{
+				
+			}
+			userTupleMap.remove(user);
+		}
+/*		if(dirProcessMap.containsKey(dir))
+		{
+			Process p = (Process)dirProcessMap.get(dir);
+			p.destroyForcibly();
+			try {
+				FileUtils.deleteDirectory(new File(dir));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+*/		LOGGER.info(">>>KILLING PYTHON TUPESPACE FOR USER - COMPLETE<<<");
+		
+	}
+	
 }
