23cf6ec9c63b60f15494458ab4ed65b5a98b2951
mahkhalil <mahkhalil@deloitte.com>
2020-03-02 15:57:01 +0000

    UPDATE ::: Adding auto-layout generation for the metamodel and saving to disk for existing apps
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@37422 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e0c981b08dbdde1b3d6c4f8b834bc6dfe69271c5

---
+++ src/prerna/sablecc2/reactor/masterdatabase/util/GenerateMetamodelLayout.java
@@ -0,0 +1,192 @@
+package prerna.sablecc2.reactor.masterdatabase.util;
+
+import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+
+import org.graphstream.algorithm.Toolkit;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.MultiGraph;
+import org.graphstream.ui.layout.Layout;
+import org.graphstream.ui.layout.springbox.implementations.SpringBox;
+
+import com.google.gson.GsonBuilder;
+
+import prerna.engine.impl.MetaHelper;
+import prerna.engine.impl.SmssUtilities;
+import prerna.engine.impl.rdf.BigDataEngine;
+import prerna.engine.impl.rdf.RDFFileSesameEngine;
+import prerna.sablecc2.reactor.app.metaeditor.SaveOwlPositions;
+import prerna.test.TestUtilityMethods;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.Utility;
+
+public class GenerateMetamodelLayout {
+
+	public static void generateLayout(String appId) {
+		String smssFile = DIHelper.getInstance().getCoreProp().getProperty(appId + "_" + Constants.STORE);
+		Properties prop = Utility.loadProperties(smssFile);
+		String owlFileLocation = SmssUtilities.getOwlFile(prop).getAbsolutePath();
+		File owlF = new File(owlFileLocation);
+
+		// owl is stored as RDF/XML file
+		RDFFileSesameEngine rfse = new RDFFileSesameEngine();
+		rfse.openFile(owlFileLocation, null, null);
+		// we create the meta helper to facilitate querying the engine OWL
+		MetaHelper helper = new MetaHelper(rfse, null, null);
+		
+		// timer
+		long tStart = System.currentTimeMillis();
+
+		Graph graph = new MultiGraph("embedded");
+		Layout layout = new SpringBox(false);
+		graph.addSink(layout);
+		layout.addAttributeSink(graph);
+
+		List<String> nodes = helper.getPhysicalConcepts();
+		Map<String, Integer> nodeSizes = new HashMap<String, Integer>();
+
+		for (String node : nodes) {
+			String nodeName = Utility.getInstanceName(node);
+			List<String> properties = helper.getPropertyUris4PhysicalUri(node);
+			nodeSizes.put(nodeName, properties.size());
+			graph.addNode(Utility.getInstanceName(node));
+		}
+
+		List<String[]> relationships = helper.getPhysicalRelationships();
+		for (String[] relation : relationships) {
+			String start = Utility.getInstanceName(relation[0]);
+			String end = Utility.getInstanceName(relation[1]);
+			String edge = Utility.getInstanceName(relation[2]) + "-" + start + "-" + end;
+			graph.addEdge(edge, start, end);
+		}
+
+		while (layout.getStabilization() < 0.9) {
+			layout.compute();
+		}
+
+		Iterator<Node> nodeIt = graph.getNodeSet().iterator();
+		Map<String, Map<String, Double>> positionMap = new HashMap<>();
+		Map<String, Rectangle2D> rectangles = new HashMap<>();
+		nodeIt = graph.getNodeSet().iterator();
+
+		while (nodeIt.hasNext()) {
+			Node node = nodeIt.next();
+			String nodeName = node.getId();
+			float nodeSize = 0;
+			if (nodeSizes.containsKey(nodeName)) {
+				nodeSize = nodeSizes.get(nodeName);
+			}
+
+			double pos[] = Toolkit.nodePosition(node);
+
+			float heightMultiplier = 10;
+			if (nodeSize > 1) {
+				heightMultiplier = 5*nodeSize;
+			}
+
+			// (x, y, w, h)
+			rectangles.put(nodeName, new Rectangle2D.Float((float) pos[0], (float) pos[1], 160, 2*heightMultiplier));
+		}
+
+		Rectangles fixRectangles = new Rectangles();
+		// get map of correctly spaced nodes/rectangles for metamodel
+		Map<String, Rectangle2D> fixedRectangles = fixRectangles.fix(rectangles);
+
+		double minx = 0;
+		double miny = 0;
+
+		for (Entry<String, Rectangle2D> fixedRectEntrySet: fixedRectangles.entrySet()) {
+			Rectangle2D rectangle = fixedRectEntrySet.getValue();
+			double x = rectangle.getX();
+			double y = rectangle.getY();
+
+			if (x < minx) {
+				minx = x;
+			}
+			if (x < miny) {
+				miny = x;
+			}
+		}
+
+		nodeIt = graph.getNodeSet().iterator();
+		int numberOfNodes = graph.getNodeCount();
+		double leftM;
+		double topM;
+		// TODO: adjust these numbers after thorough testing to find sweet spot
+		if (numberOfNodes < 10) {
+			leftM = 6;
+			topM = 6;
+		} else if (numberOfNodes > 10 && numberOfNodes < 25) {
+			leftM = 4;
+			topM = 4;
+		} else {
+			leftM = 3;
+			topM = 3;
+		}
+
+		while (nodeIt.hasNext()) {
+			Node node = nodeIt.next();
+			String nodeName = node.getId();
+			Rectangle2D nodePosition = fixedRectangles.get(nodeName);
+			double upperLeftX = nodePosition.getX();
+			double upperLeftY = nodePosition.getY();
+			Map<String, Double> posMap = new HashMap<>();
+
+			if (minx < 0) {
+				posMap.put("left", leftM * (upperLeftX + (-1 * minx)));
+			} else {
+				posMap.put("left", leftM * upperLeftX);
+			}
+
+			if (miny < 0) {
+				posMap.put("top", topM * (upperLeftY + (-1 * miny)));
+			} else {
+				posMap.put("top", topM * upperLeftY);
+			}
+
+			positionMap.put(nodeName, posMap);
+		}
+		
+		long tEnd = System.currentTimeMillis();
+		System.out.println("Compute time = " + (tEnd - tStart) + " ms");
+
+		// now write the file
+		String baseFolder = owlF.getParent();
+		String positionJson = baseFolder + "/" + SaveOwlPositions.FILE_NAME;
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter(positionJson);
+			writer.write(new GsonBuilder().setPrettyPrinting().create().toJson(positionMap));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		TestUtilityMethods.loadAll("C:\\workspace\\Semoss_Dev\\RDF_Map.prop");
+
+		String tapCoreSmss = "C:\\workspace\\Semoss_Dev\\db\\TAP_Core_Data__133db94b-4371-4763-bff9-edf7e5ed021b.smss";
+		BigDataEngine engine = new BigDataEngine();
+		engine.openDB(tapCoreSmss);
+
+		generateLayout("133db94b-4371-4763-bff9-edf7e5ed021b");
+	}
+}
