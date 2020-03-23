7578056b74c29c68cd7aa81554099189a0f286e3
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2020-03-23 00:15:24 +0000

    Updates for the TCPPyTranslator PYTHON	TRUE USE_PYTHON	TRUE #PY_WORKER	prerna.pyserve.FileBasedPyWorker PY_WORKER	prerna.pyserve.TCPPyWorker PY_WORKER_CP	fst-2.56.jar;jep-3.9.0.jar;log4j-1.2.17.jar;commons-io-2.4.jar;objenesis-2.5.1.jar;jackson-core-2.9.5.jar;javassist-3.20.0-GA.jar;netty-all-4.1.47.Final.jar;classes; USE_TCP_PY	TRUE USE_PY_FILE	FALSE // if you want to debug you can use this #FORCE_PORT	8007 LD_LIBRARY_PATH	C:/Python/Python36/Lib/site-packages/jep JEP_LIB	C:/Python/Python36/Lib/site-packages/jep/jep.dll
    TCPPyWorker is the main class to perform the TCP Operation. 
    
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@37805 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 131ca27927b6be45b2c8d9567c064a74ccef86e1

---
+++ src/prerna/pyserve/TCPServerHandler.java
@@ -0,0 +1,156 @@
+package prerna.pyserve;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import prerna.ds.py.PyExecutorThread;
+
+public class TCPServerHandler extends ChannelInboundHandlerAdapter {
+	
+	String inputSoFar = new String();
+	
+	String endChar = "<o>";
+	
+	PyExecutorThread pt = null;
+	String mainFolder = null;
+	
+	public static Logger LOGGER = null;
+		
+	public void setPyExecutorThread(PyExecutorThread pyt)
+	{
+		this.pt = pyt;
+	}
+	
+	public void setMainFolder(String mainFolder)
+	{
+		this.mainFolder = mainFolder;
+	}
+	
+	public void setLogger(Logger LOGGER)
+	{
+		this.LOGGER = LOGGER;
+	}
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+    	
+    	try {
+			ByteBuf in = (ByteBuf) msg;
+			// read it
+			byte[] bytes = new byte[in.readableBytes()];
+			
+			int readerIndex = in.readerIndex();
+			in.getBytes(readerIndex, bytes);
+			
+			String command = new String(bytes);
+			
+			inputSoFar = inputSoFar + command;
+			if(inputSoFar.endsWith(endChar))
+			{
+				inputSoFar = inputSoFar.replace(endChar, "");
+				if(inputSoFar.contains("CLOSE_ALL_LOGOUT"))
+				{
+					LOGGER.info("Closing all comms.. processing logout");
+		
+					pt.keepAlive = false;
+					processCommand("'logout now'"); // this should trigger it and kill it
+					
+					ctx.channel().close();
+					ctx.channel().parent().close();
+					
+					// stop the logger
+					//LogManager.resetConfiguration();
+					
+					FileUtils.deleteDirectory(new File(mainFolder));
+				}
+				else
+				{
+					// execute the command
+					Object retObject = processCommand(inputSoFar);
+					
+					if(retObject == null)
+					{
+						LOGGER.debug("output.. [" + retObject + "]" + "for command" + command);
+						retObject = "";
+					}
+					Object [] outputObj = new Object[2];
+					outputObj[0] = inputSoFar;
+					outputObj[1] = retObject;
+					
+					// write it back
+					ByteArrayOutputStream baos = new ByteArrayOutputStream();
+					
+					// FST
+					/*
+					FSTObjectOutput fo = new FSTObjectOutput(baos);
+					fo.writeObject(outputObj);
+					fo.close();
+					*/
+					
+					ObjectOutputStream oos = new ObjectOutputStream(baos);
+					oos.writeObject(outputObj);
+					//oos.writeChars("<o>");
+					oos.flush();
+					oos.close();
+					
+					byte [] output = baos.toByteArray();
+					
+					ByteBuf buff = Unpooled.buffer(output.length);
+					buff.writeBytes(output);
+		
+					ctx.writeAndFlush(buff);
+					ctx.flush();
+					inputSoFar = "";
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    }
+    
+    
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+    
+    // process the command
+    public Object processCommand(String command)
+    {
+    	// this should be basically be just straight up runscript
+    	LOGGER.info("Running the new version");
+		this.pt.command = new String[] {command};
+		Object monitor = this.pt.getMonitor();
+		Object response = null;
+		synchronized(monitor) {
+			try {
+				monitor.notify();
+				monitor.wait(4000);
+				response = this.pt.response.get(command);
+			} catch (Exception ex) {
+				LOGGER.debug(ex);
+				response = ex.getMessage();
+			}
+		}
+		return response;
+    }
+}
