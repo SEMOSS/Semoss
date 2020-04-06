3f0702d4e62fd11670112aeff54af3958580c0c6
mahkhalil <mahkhalil@deloitte.com>
2020-04-06 18:51:59 +0000

    BUGFIX ::: Accounting for true/false boolean needing to be in quotes for ms sql server
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@38145 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e0a0c592fe3fe96d8e3116c1d3d93fe5940fde61

--- src/prerna/query/interpreters/sql/MicrosoftSqlServerInterpreter.java
+++ src/prerna/query/interpreters/sql/MicrosoftSqlServerInterpreter.java
@@ -1,11 +1,16 @@
 package prerna.query.interpreters.sql;
 
+import java.util.List;
+
 import prerna.algorithm.api.ITableDataFrame;
+import prerna.algorithm.api.SemossDataType;
+import prerna.ds.util.RdbmsQueryBuilder;
 import prerna.engine.api.IEngine;
 import prerna.query.querystruct.HardSelectQueryStruct;
 import prerna.query.querystruct.SelectQueryStruct;
 import prerna.query.querystruct.selectors.IQuerySelector;
 import prerna.query.querystruct.selectors.IQuerySelector.SELECTOR_TYPE;
+import prerna.util.Utility;
 
 public class MicrosoftSqlServerInterpreter extends SqlInterpreter {
 
@@ -38,6 +43,167 @@ public class MicrosoftSqlServerInterpreter extends SqlInterpreter {
 		}
 		// now just feed into the above
 		return super.composeQuery();
+	}
+	
+	/**
+	 * Microsoft SQL Server copies the other BUT boolean fields must be in quotes
+	 * @param dataType
+	 * @param objects
+	 * @param comparator
+	 * @return
+	 */
+	@Override
+	protected String getFormatedObject(String dataType, List<Object> objects, String comparator) {
+		// this will hold the sql acceptable format of the object
+		StringBuilder myObj = new StringBuilder();
+		
+		// defining variables for looping
+		int i = 0;
+		int size = objects.size();
+		if(size == 0) {
+			return "";
+		}
+		// if we can get the data type from the OWL, lets just use that
+		// if we dont have it, we will do type casting...
+		if(dataType != null) {
+			dataType = dataType.toUpperCase();
+			SemossDataType type = SemossDataType.convertStringToDataType(dataType);
+			if(SemossDataType.INT == type || SemossDataType.DOUBLE == type) {
+				// get the first value
+				myObj.append(objects.get(0));
+				i++;
+				// loop through all the other values
+				for(; i < size; i++) {
+					myObj.append(" , ").append(objects.get(i));
+				}
+			} else if(SemossDataType.DATE == type || SemossDataType.TIMESTAMP == type) {
+				String leftWrapper = null;
+				String rightWrapper = null;
+				boolean isSearch = comparator.equalsIgnoreCase(SEARCH_COMPARATOR) || comparator.equals(NOT_SEARCH_COMPARATOR);
+				if(isSearch) {
+					leftWrapper = "'%";
+					rightWrapper = "%'";
+				} else {
+					leftWrapper = "\'";
+					rightWrapper = "\'";
+				}
+				
+				// get the first value
+				Object val = objects.get(0);
+				String d = formatDate(val, type);
+				// get the first value
+				myObj.append(leftWrapper).append(d).append(rightWrapper);
+				i++;
+				for(; i < size; i++) {
+					val = objects.get(i).toString();
+					d = formatDate(val, type);
+					// get the first value
+					myObj.append(" , ").append(leftWrapper).append(d).append(rightWrapper);
+				}
+			} else {
+				String leftWrapper = null;
+				String rightWrapper = null;
+				boolean isSearch = comparator.equalsIgnoreCase(SEARCH_COMPARATOR) || comparator.equals(NOT_SEARCH_COMPARATOR);
+				if(isSearch) {
+					leftWrapper = "'%";
+					rightWrapper = "%'";
+				} else {
+					leftWrapper = "\'";
+					rightWrapper = "\'";
+				}
+				
+				// get the first value
+				String val = RdbmsQueryBuilder.escapeForSQLStatement(objects.get(i).toString());
+				// get the first value
+				if(isSearch && val.endsWith("\\")) {
+					myObj.append(leftWrapper).append(val).append("\\").append(rightWrapper);
+				} else {
+					myObj.append(leftWrapper).append(val).append(rightWrapper);
+				}
+				i++;
+				for(; i < size; i++) {
+					val = RdbmsQueryBuilder.escapeForSQLStatement(objects.get(i).toString());
+					// get the other values
+					if(isSearch && val.endsWith("\\")) {
+						myObj.append(" , ").append(leftWrapper).append(val).append("\\").append(rightWrapper);
+					} else {
+						myObj.append(" , ").append(leftWrapper).append(val).append(rightWrapper);
+					}
+				}
+			}
+		} 
+		else {
+			// do it based on type casting
+			// can't have mixed types
+			// so only using first value
+			Object object = objects.get(0);
+			if(object instanceof Number) {
+				// get the first value
+				myObj.append(objects.get(0));
+				i++;
+				// loop through all the other values
+				for(; i < size; i++) {
+					myObj.append(" , ").append(objects.get(i));
+				}
+			} else if(object instanceof java.util.Date || object instanceof java.sql.Date) {
+				String leftWrapper = null;
+				String rightWrapper = null;
+				boolean isSearch = comparator.equalsIgnoreCase(SEARCH_COMPARATOR) || comparator.equals(NOT_SEARCH_COMPARATOR);
+				if(isSearch) {
+					leftWrapper = "'%";
+					rightWrapper = "%'";
+				} else {
+					leftWrapper = "\'";
+					rightWrapper = "\'";
+				}
+				
+				// get the first value
+				String val = objects.get(0).toString();
+				String d = Utility.getDate(val);
+				// get the first value
+				myObj.append(leftWrapper).append(d).append(rightWrapper);
+				i++;
+				for(; i < size; i++) {
+					val = objects.get(i).toString();
+					d = Utility.getDate(val);
+					// get the first value
+					myObj.append(" , ").append(leftWrapper).append(d).append(rightWrapper);
+				}
+			} else {
+				String leftWrapper = null;
+				String rightWrapper = null;
+				boolean isSearch = comparator.equalsIgnoreCase(SEARCH_COMPARATOR) || comparator.equals(NOT_SEARCH_COMPARATOR);
+				if(isSearch) {
+					leftWrapper = "'%";
+					rightWrapper = "%'";
+				} else {
+					leftWrapper = "\'";
+					rightWrapper = "\'";
+				}
+				
+				// get the first value
+				String val = RdbmsQueryBuilder.escapeForSQLStatement(objects.get(i).toString());
+				// get the first value
+				if(isSearch && val.endsWith("\\")) {
+					myObj.append(leftWrapper).append(val).append("\\").append(rightWrapper);
+				} else {
+					myObj.append(leftWrapper).append(val).append(rightWrapper);
+				}
+				i++;
+				for(; i < size; i++) {
+					val = RdbmsQueryBuilder.escapeForSQLStatement(objects.get(i).toString());
+					// get the first value
+					// get the other values
+					if(isSearch && val.endsWith("\\")) {
+						myObj.append(" , ").append(leftWrapper).append(val).append("\\").append(rightWrapper);
+					} else {
+						myObj.append(" , ").append(leftWrapper).append(val).append(rightWrapper);
+					}
+				}
+			}
+		}
+		
+		return myObj.toString();
 	}
 	
 }
