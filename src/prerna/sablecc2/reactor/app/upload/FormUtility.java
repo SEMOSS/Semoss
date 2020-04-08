bd247318d547c1f46e82dc5df95a3da1ae53e627
rramirezjimenez <rramirezjimenez@deloitte.com>
2020-04-08 19:37:34 +0000

    UPDATE ::: Excel Form validation updating fe components
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@38170 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2ccc269f33da49298d26fddfc533981e584dd538

--- src/prerna/sablecc2/reactor/app/upload/FormUtility.java
+++ src/prerna/sablecc2/reactor/app/upload/FormUtility.java
@@ -3,44 +3,48 @@ package prerna.sablecc2.reactor.app.upload;
 public class FormUtility {
 
 	public static String getTextComponent(String label) {
-		return new StringBuilder("<div smss-form-model=\"" + label + "\">" + "{{item.model}}</div>\\n").toString();
+		return new StringBuilder("<div smss-form-model=\"" + label + "\" class=\"smss-text\">{{item.model}}</div>\\n").toString();
 	}
 
-	public static String getInputComponent(String dataModelComp) {
-		return new StringBuilder(" <smss-input ng-model=\"form.dataModel." + dataModelComp + ".selected\">" + "</smss-input>\\n").toString();
+	public static String getInputComponent(String variableToBind) {
+		return new StringBuilder("<smss-input ng-model=\"form.dataModel." + variableToBind + 
+				".selected\"></smss-input>\\n").toString();
 	}
 
-	public static String getDropdownComponent(String dataModelComp) {
-		return new StringBuilder(
-				"<smss-dropdown model=\"form.dataModel." + dataModelComp + ".selected\" options=\"form.dataModel."
-						+ dataModelComp + ".options\"" + " placeholder=\"Select One\">\\n</smss-dropdown>\\n")
-								.toString();
+	public static String getDropdownComponent(String variableToBind) {
+		return new StringBuilder("<smss-dropdown model=\"form.dataModel." + variableToBind + ".selected\" options=\"form.dataModel." + 
+				variableToBind + ".options\" placeholder=\"Select One\">\\n</smss-dropdown>\\n").toString();
 	}
 
-	public static String getNumberPickerComponent(String dataModelComp) {
-		return new StringBuilder("<smss-input type=\"number\" ng-model=\"form.dataModel." + dataModelComp + ".selected\">" + "</smss-input>\\n")
-				.toString();
+	public static String getNumberPickerComponent(String variableToBind) {
+		return new StringBuilder("<smss-input type=\"number\" ng-model=\"form.dataModel." + variableToBind + 
+				".selected\"></smss-input>\\n").toString();
 	}
 
-	public static String getSliderComponent(String min, String max) {
+	public static String getSliderComponent(String min, String max, String sensitivity) {
 		return new StringBuilder("<smss-slider min=\"" + min + "\" max=\"" + max
-				+ "\" model=\"item.selected\" numerical=\"\"></smss-slider>\\n").toString();
+				+ "\" sensitivity=\"" + sensitivity + "\" model=\"item.selected\" numerical=\"\"></smss-slider>\\n").toString();
 	}
 
-	public static String getTextAreaComponent() {
-		return new StringBuilder("<smss-textarea ng-model=\"item.selected\"></smss-textarea>\\n").toString();
+	public static String getSliderComponent(String min, String max, String sensitivity, String variableToBind) {
+		return new StringBuilder("<smss-slider min=\"" + min + "\" max=\"" + max
+				+ "\" sensitivity=\"" + sensitivity + "\" model=\"form.dataModel." + variableToBind + 
+				".selected\"> numerical=\"\"></smss-slider>\\n").toString();
 	}
 
-	public static String getDatePickerComponent() {
-		return new StringBuilder(
-				"<smss-date-picker model=\"item.selected\" format=\"YYYY-MM-DD\"></smss-date-picker>\\n").toString();
+	public static String getTextAreaComponent(String variableToBind) {
+		return new StringBuilder("<smss-textarea ng-model=\"form.dataModel." + variableToBind + 
+				".selected\"></smss-textarea>\\n").toString();
 	}
 
-	public static String getSubmitComponent() {
-		return new StringBuilder(
-				"<div smss-form-model=\"Submit\" class=\"form-builder__submit\"><smss-btn class=\"smss-btn--primary\""
-						+ " ng-click=\"form.runPixel(form.pixelModel.Insert.pixel)\">{{item.model}}</smss-btn></div>\\n")
-								.toString();
+	public static String getDatePickerComponent(String variableToBind) {
+		return new StringBuilder("<smss-date-picker model=\"form.dataModel." + variableToBind + 
+				".selected\"> format=\"YYYY-MM-DD\"></smss-date-picker>\\n").toString();
+	}
+
+	public static String getSubmitComponent(String crudOperation) {
+		return new StringBuilder("<div smss-form-model=\"Submit\" class=\"form-builder__submit\"><smss-btn class=\"smss-btn--primary\"" + 
+				" ng-click=\"form.runPixel(form.pixelModel." + crudOperation + ".pixel)\">{{item.model}}</smss-btn></div>\\n").toString();
 	}
 
 	public static String getDescriptionComponent(String description) {
