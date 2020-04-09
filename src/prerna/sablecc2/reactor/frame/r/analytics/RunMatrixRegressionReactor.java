83b5212a15341477772f011019b8ee68a18b1eba
mahkhalil <mahkhalil@deloitte.com>
2020-04-09 22:18:23 +0000

    UPDATE ::: Adding improvements to the matrix regression algorithm - need to do a quick debug on the correlation barchart UPDATE ::: Adding shorthand Timestamp reactor which works like Date(format=["yyyy-MM-dd HH:mm:ss"])
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@38202 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3b23509cfcc54e59c47b07a9c9125b13f17099ab

--- src/prerna/sablecc2/reactor/frame/r/analytics/RunMatrixRegressionReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/analytics/RunMatrixRegressionReactor.java
@@ -1,11 +1,13 @@
 package prerna.sablecc2.reactor.frame.r.analytics;
 
 import java.util.List;
+import java.util.Random;
 import java.util.Vector;
 
 import org.apache.log4j.Logger;
 
 import prerna.algorithm.api.ITableDataFrame;
+import prerna.ds.r.RSyntaxHelper;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
@@ -20,15 +22,17 @@ import prerna.util.usertracking.UserTrackerFactory;
 
 public class RunMatrixRegressionReactor extends AbstractRFrameReactor {
 
+	private static Random rand = new Random();
+
 	private static final String CLASS_NAME = RunMatrixRegressionReactor.class.getName();
 
 	private static final String Y_COLUMN = "yColumn";
 	private static final String X_COLUMNS = "xColumns";
-	
+
 	public RunMatrixRegressionReactor() {
 		this.keysToGet = new String[]{Y_COLUMN, X_COLUMNS, ReactorKeysEnum.PANEL.getKey()};
 	}
-	
+
 	@Override
 	public NounMetadata execute() {
 		init();
@@ -36,9 +40,19 @@ public class RunMatrixRegressionReactor extends AbstractRFrameReactor {
 		ITableDataFrame dataFrame = getFrame();
 		String frameName = dataFrame.getName();
 		dataFrame.setLogger(logger);
-		
+
 		// figure out inputs
-		String panelId = getPanelId();
+		List<String> panelIds = getPanelId();
+		if(panelIds == null || panelIds.isEmpty()) {
+			panelIds = new Vector<String>();
+			String panelId = rand.nextInt(5000) + "";
+			panelIds.add(panelId);
+			panelId = rand.nextInt(5000) + "";
+			panelIds.add(panelId);
+		} else if(panelIds.size() < 2) {
+			String panelId = rand.nextInt(5000) + "";
+			panelIds.add(panelId);
+		}
 		String predictionCol = getPrediction(logger);
 		List<String> numericalCols = getColumns();
 		if (numericalCols.contains(predictionCol)) {
@@ -62,70 +76,89 @@ public class RunMatrixRegressionReactor extends AbstractRFrameReactor {
 				retHeaders[i] = header;
 			}
 		}
-		
+
 		// get the correlation data from the run r regression algorithm
 		logger.info("Start iterating through data to determine regression");
-		String resultsList = runRMatrixRegression(frameName, predictionCol, retHeaders);
+		String resultsList = runRLinearRegression(frameName, predictionCol, retHeaders, logger);
 		logger.info("Done iterating through data to determine regression");
-		
+
 		// track GA data
-//		UserTrackerFactory.getInstance().trackAnalyticsPixel(this.insight, "MatrixRegression");
-		
+		//		UserTrackerFactory.getInstance().trackAnalyticsPixel(this.insight, "MatrixRegression");
+
 		// NEW TRACKING
 		UserTrackerFactory.getInstance().trackAnalyticsWidget(
 				this.insight, 
 				dataFrame, 
 				"MatrixRegression", 
 				AnalyticsTrackerHelper.getHashInputs(this.store, this.keysToGet));
-		
+
 		/////////////////////////////////////////////////////////////////////////////
 		/////////////////////////////////////////////////////////////////////////////
 		////////////////////////Coefficient Table Object/////////////////////////////
 		////////////////////////////////////////////////////////////////////////////
 
+		// THIS DOES NOT GET USED:
+
 		// the length of the object will be numCols + 1 (because of the intercept)
 		// there will always be 2 rows (column header and coefficient)
-		int length = numCols + 1;
-		Object[][] retCoefficientOutput = new Object[length][2];
-		
+		Object[][] retCoefficientOutput = new Object[numCols][2];
+
 		// need to fill in the object with the data values
 		// retrieve data using getBulkDataRow
-		String[] coefficientTableHeaders = new String[]{"Column Header", "Coefficient"};
-		
+		String[] coefficientTableHeaders = new String[]{"ColumnName", "Coefficient"};
+
 		// query for retrieving the first item of the list - the coefficient table
-		String queryCoefficients = resultsList + "[[1]]" + "[" + 1 + ":" + length + "]";
+		String queryCoefficients = resultsList + "[[1]]";
 		List<Object[]> bulkRow = this.rJavaTranslator.getBulkDataRow(queryCoefficients, coefficientTableHeaders);
 		// each entry into the list is a row - we need to put this in the form of Object[][]
 		for (int i = 0; i < bulkRow.size(); i++) {
 			retCoefficientOutput[i] = bulkRow.get(i);
 		}
-		
+
+		ITask barTaskData = ConstantTaskCreationHelper.getBarChartInfo(panelIds.get(1), "ColumnName", "Coefficient" , retCoefficientOutput);
+		NounMetadata noun1 = new NounMetadata(barTaskData, PixelDataType.FORMATTED_DATA_SET, PixelOperationType.TASK_DATA);
+
 		/////////////////////////////////////////////////////////////////////////////
 		/////////////////////////////////////////////////////////////////////////////
 		////////////////////////Actuals vs Fitted Object/////////////////////////////
 		////////////////////////////////////////////////////////////////////////////
-		
+
 		// we need to add a unique row id
-		String[] dataTableHeaders = new String[]{"ROW_ID", "Actual", "Fitted"};
-		
+		String[] dataTableHeaders = new String[]{"ROW_ID", "Actual", "Predicted"};
+
 		// query for retrieving the second item of the list - the Actuals vs Fitted
 		String queryDataPoints = resultsList + "[[2]]";
 		this.rJavaTranslator.executeEmptyR(queryDataPoints + "$ROW_ID <- seq.int(nrow(" + queryDataPoints + "))");
+
+		// if it has over 10k rows, then sample it
+		int rows = this.rJavaTranslator.getInt("nrow(" + queryDataPoints + ")");
+		int sampleAmount = 10000;
+		if(rows>sampleAmount) {
+			String sampleScript = queryDataPoints + " <- as.data.frame(" + queryDataPoints + "[sample(nrow("
+					+ queryDataPoints + ")," + sampleAmount + "),])";
+			this.rJavaTranslator.executeEmptyR(sampleScript);
+		}
+
+		// move to java var
 		List<Object[]> bulkRowDataPoints = this.rJavaTranslator.getBulkDataRow(queryDataPoints, dataTableHeaders);
-		
+
 		// create and return a task for the Actuals vs Fitted scatterplot
-		ITask taskData = ConstantTaskCreationHelper.getScatterPlotData(panelId, "ROW_ID", "Actual", "Fitted", bulkRowDataPoints);
-		this.insight.getTaskStore().addTask(taskData);
+		ITask scatterTaskData = ConstantTaskCreationHelper.getScatterPlotData(panelIds.get(0), "ROW_ID", "Actual", "Fitted", bulkRowDataPoints);
+		this.insight.getTaskStore().addTask(scatterTaskData);
 
 		// variable cleanup
 		this.rJavaTranslator.executeEmptyR("rm(" + resultsList + "); gc();");
 
 		// now return this object - for the Scatterplot of Actuals vs Fitted
-		NounMetadata noun = new NounMetadata(taskData, PixelDataType.FORMATTED_DATA_SET, PixelOperationType.TASK_DATA);
-		noun.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Matrix regression ran successfully!"));
-		return noun;
+		NounMetadata noun2 = new NounMetadata(scatterTaskData, PixelDataType.FORMATTED_DATA_SET, PixelOperationType.TASK_DATA);
+		noun2.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Matrix regression ran successfully!"));
+
+		List<NounMetadata> tasks = new Vector<NounMetadata>();
+		tasks.add(noun1);
+		tasks.add(noun2);
+		return new NounMetadata(tasks, PixelDataType.VECTOR, PixelOperationType.VECTOR);
 	}
-	
+
 	////////////////////////////////////////////////////////////
 	////////////////////////////////////////////////////////////
 	////////////////////////////////////////////////////////////
@@ -133,43 +166,53 @@ public class RunMatrixRegressionReactor extends AbstractRFrameReactor {
 	/*
 	 * Running regression R script
 	 */
-	
-	private String runRMatrixRegression(String frameName, String predictionCol, String[] retHeaders) {
-		
-		// stringbuilder for the lm function string
-		// we will pass this into the r method
-		StringBuilder functionInput = new StringBuilder();
-		// the format of the lm function is: lm(y ~ x1 + x2 + x3, data = frameName)
-		functionInput.append("lm(").append(frameName).append("$").append(predictionCol).append(" ~ ");
-		
-		// iteratively add each column name based on the retHeaders
-		for (int i = 0 ; i < retHeaders.length; i++) {
-			functionInput.append(frameName).append("$").append(retHeaders[i]);
-			if (i < retHeaders.length - 1) {
-				functionInput.append(" + ");
-			}
-		}
-		functionInput.append(", data = ").append(frameName).append(")");
+
+	private String runRLinearRegression(String frameName, String predictionCol, String[] retHeaders, Logger logger) {
+		StringBuilder rsb = new StringBuilder();
+
+		// Organize explanatory col headers
+		String indColsVector = RSyntaxHelper.createStringRColVec(retHeaders);
 
 		// create a name for the results list; this list will contain two tables: 
 		// 1) the table of coefficients
 		// 2) the table of actuals vs fitted
 		String resultsListName = "ResultsList" + Utility.getRandomString(10);
 
-		// stringbuilder for the code we will pass to r 
-		StringBuilder rsb = new StringBuilder();
 		// source the r script that will run the numerical correlation routine
 		String regressionScriptFilePath = getBaseFolder() + "\\R\\AnalyticsRoutineScripts\\MatrixRegression.R";
 		regressionScriptFilePath = regressionScriptFilePath.replace("\\", "/");
+		String dataFrameTable = frameName + Utility.getRandomString(6);
 		rsb.append("source(\"" + regressionScriptFilePath + "\");");
+		rsb.append(RSyntaxHelper.asDataFrame(dataFrameTable + "", frameName));
 		// R syntax for the routine: getRegressionCoefficientsFromScript("lm(y~x, data = frameName)", frameName$PredictionCol)
-		rsb.append(resultsListName + "<- getRegressionCoefficientsFromScript(" + "\"" + functionInput.toString() + "\", " + frameName + "$" +  predictionCol +  ");");
+		rsb.append(resultsListName + "<- fit_lm(" + dataFrameTable + ",\"" + predictionCol + "\", " + indColsVector +  ");");
 
 		// run the script
 		this.rJavaTranslator.runR(rsb.toString());
+
+		// see how many rows were dropped
+		int origRows = this.rJavaTranslator.getInt("nrow(" + dataFrameTable + ")");
+		int newRows = this.rJavaTranslator.getInt("nrow(" + resultsListName + "[[2]])");
+		int rowsDropped = origRows - newRows;
+
+		// if all rows were dropped, throw error
+		if(newRows==0) {
+			String errorString = "Cannot run Matrix Regression on data with 0 non-null rows";
+			logger.info(errorString);
+			throw new IllegalArgumentException(errorString);
+		}
+
+		// throw warning to user otherwise
+		if(rowsDropped > 0) {
+			String errorString = "Dropping " + rowsDropped + " rows due to null values";
+			logger.info(errorString);
+		}
+		
+		// cleanup
+		this.rJavaTranslator.runR("gc(" + dataFrameTable + ")");
 		return resultsListName;
 	}
-	
+
 	////////////////////////////////////////////////////////////
 	////////////////////////////////////////////////////////////
 	////////////////////////////////////////////////////////////
@@ -177,18 +220,16 @@ public class RunMatrixRegressionReactor extends AbstractRFrameReactor {
 	/*
 	 * Retrieving inputs
 	 */
-	
-	private String getPanelId() {
+
+	private List<String> getPanelId() {
 		// see if defined as individual key
 		GenRowStruct columnGrs = this.store.getNoun(keysToGet[2]);
-		if(columnGrs != null) {
-			if(columnGrs.size() > 0) {
-				return columnGrs.get(0).toString();
-			}
+		if(columnGrs != null && !columnGrs.isEmpty()) {
+			return columnGrs.getAllStrValues();
 		}
 		return null;
 	}
-	
+
 	private String getPrediction(Logger logger) {
 		// see if defined as individual key
 		GenRowStruct columnGrs = this.store.getNoun(Y_COLUMN);
@@ -197,14 +238,14 @@ public class RunMatrixRegressionReactor extends AbstractRFrameReactor {
 				return columnGrs.get(0).toString();
 			}
 		}
-		
-		// else, we assume it is the first column
+
+		// else, throw error
 		if(this.curRow == null || this.curRow.size() == 0) {
 			String errorString = "Could not find input for variable y";
 			logger.info(errorString);
 			throw new IllegalArgumentException(errorString);
 		}
-		return this.curRow.get(0).toString();
+		return null;
 	}
 
 	private List<String> getColumns() {
@@ -220,6 +261,6 @@ public class RunMatrixRegressionReactor extends AbstractRFrameReactor {
 				return strValues;
 			}
 		}
-	   return null;
+		return null;
 	}
 }
