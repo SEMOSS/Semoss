08fab3094b66035b2859a8d6c3b0353ce1f55b43
mahkhalil <mahkhalil@deloitte.com>
2020-04-09 22:18:23 +0000

    UPDATE ::: Adding improvements to the matrix regression algorithm - need to do a quick debug on the correlation barchart UPDATE ::: Adding shorthand Timestamp reactor which works like Date(format=["yyyy-MM-dd HH:mm:ss"])
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@38202 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/date/reactor/TimestampReactor.java
@@ -0,0 +1,49 @@
+package prerna.date.reactor;
+
+import com.ibm.icu.util.Calendar;
+
+import prerna.date.SemossDate;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class TimestampReactor extends AbstractReactor {
+
+	private static final String DEFAULT_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	
+	public TimestampReactor() {
+		this.keysToGet = new String[]{"date", "format"};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		SemossDate date = null;
+		String pattern = DEFAULT_FORMAT;
+		
+		/*
+		 * If there is no date input, then we will grab todays date
+		 * If there is a date input, we assume it is yyyy-MM-dd format
+		 * If there is a date input and a format, we will use that format
+		 */
+		
+		// determine if we should use the default format
+		// or the user defined format
+		if(this.keyValue.containsKey(this.keysToGet[1])) {
+			pattern = this.keyValue.get(this.keysToGet[1]);
+		}
+					
+		if(this.keyValue.containsKey(this.keysToGet[0])) {
+			String strDate = this.keyValue.get(this.keysToGet[0]);
+			
+			date = new SemossDate(strDate, pattern);
+			date.getDate();
+		} else {
+			// the user hasn't specified a date
+			date = new SemossDate(Calendar.getInstance().getTime(), pattern);
+		}
+		
+		return new NounMetadata(date, PixelDataType.CONST_DATE);
+	}
+
+}
