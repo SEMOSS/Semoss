dd3fd3e19defd580aee9a69885e1f4ecebc91ece
mahkhalil <mahkhalil@deloitte.com>
2020-04-14 20:55:48 +0000

    UPDATE ::: Adding reactor to send back the queried apps
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@38244 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 318b44bf1e2e398948c56b150ebaf1ecaab72a8d

---
+++ src/prerna/sablecc2/reactor/insights/GetQueriedAppsReactor.java
@@ -0,0 +1,23 @@
+package prerna.sablecc2.reactor.insights;
+
+import java.util.List;
+
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class GetQueriedAppsReactor extends AbstractReactor {
+
+	@Override
+	public NounMetadata execute() {
+		List<String> apps = this.insight.getQueriedEngines();
+		if(this.insight.isSavedInsight()) {
+			if(apps.contains(this.insight.getEngineId())) {
+				apps.remove(this.insight.getEngineId());
+			}
+			apps.add(0, this.insight.getInsightId());
+		}
+		return new NounMetadata(apps, PixelDataType.CONST_STRING);
+	}
+
+}
