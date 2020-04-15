ef6612f4df34c77e7e7ed22978b219fbe870d417
mahkhalil <mahkhalil@deloitte.com>
2020-04-15 16:34:46 +0000

    UPDATE ::: Adding another logger for data timing
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@38260 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 06655f2a8dd1165fb7566874688caf7254f3e14f

--- src/prerna/sablecc2/reactor/AssignmentReactor.java
+++ src/prerna/sablecc2/reactor/AssignmentReactor.java
@@ -50,7 +50,11 @@ public class AssignmentReactor extends AbstractReactor implements JavaExecutable
 		if(result.getNounType() == PixelDataType.FORMATTED_DATA_SET && result.getValue() instanceof ITask) {
 			Map<String, Object> flushedOutValue = null;
 			try {
+				LOGGER.info("Start time to iterate through the result set");
+				long start = System.currentTimeMillis();
 				flushedOutValue = ((ITask) result.getValue()).collect(true);
+				long end = System.currentTimeMillis();
+				LOGGER.info("Total time to iterate through the result set = " + (end-start) + "ms");
 			} catch (Exception e) {
 				e.printStackTrace();
 				throw new SemossPixelException(e.getMessage());
