803254aa815085c93f8481eaf3be760e7d03c265
mahkhalil <mahkhalil@deloitte.com>
2020-04-23 22:32:14 +0000

    UPDATE ::: Adding a drop now logic
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@38385 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/task/RemoveTaskReactor.java
+++ src/prerna/sablecc2/reactor/task/RemoveTaskReactor.java
@@ -1,16 +1,22 @@
 package prerna.sablecc2.reactor.task;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.om.task.ITask;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.insight.InsightUtility;
 
 public class RemoveTaskReactor extends AbstractReactor {
 	
+	private static final String DROP_NOW_KEY = "dropNow";
+
 	public RemoveTaskReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.TASK_ID.getKey()};
+		this.keysToGet = new String[]{ReactorKeysEnum.TASK_ID.getKey(), DROP_NOW_KEY};
 	}
 	
 	@Override
@@ -23,9 +29,30 @@ public class RemoveTaskReactor extends AbstractReactor {
 		} else {
 			taskId = inputNoun.getValue().toString();
 		}
-		if(insight.getTaskStore().getTask(taskId) == null) {
+		ITask task = insight.getTaskStore().getTask(taskId);
+		if(task == null) {
 			throw new IllegalArgumentException("Could not find task id = " + taskId);
 		}
+		
+		// drop now
+		if(dropNow()) {
+			InsightUtility.removeTask(this.insight, taskId);
+			Map<String, String> taskMap = new HashMap<>();
+			taskMap.put("taskId", task.getId());
+			return new NounMetadata(taskMap, PixelDataType.MAP, PixelOperationType.REMOVE_TASK);
+		}
+		
 		return new NounMetadata(taskId, PixelDataType.REMOVE_TASK, PixelOperationType.REMOVE_TASK);
+	}
+	
+	/**
+	 * Determine if we should remove right away or during the stream
+	 * @return
+	 */
+	protected boolean dropNow() {
+		if(this.curRow.size() > 1) {
+			return Boolean.parseBoolean(this.curRow.get(1).toString());
+		}
+		return false;
 	}
 }
