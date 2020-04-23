dcc2c46f99bff5ed73f35b05918f5c2c3dd347a3
kunalppatel9 <kunalppatel9@deloitte.com>
2020-04-23 14:23:37 +0000

    BUGFIX ::: Changing cluster pull app call to Utility.getEngine so it only pulls app once
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@38376 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b9a28c44ef96a5bcd2dd47d5f5910a5598c3ab34

--- src/prerna/sablecc2/reactor/masterdatabase/GetDatabaseMetamodelReactor.java
+++ src/prerna/sablecc2/reactor/masterdatabase/GetDatabaseMetamodelReactor.java
@@ -19,6 +19,7 @@ import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.SecurityAppUtils;
 import prerna.cluster.util.CloudClient;
 import prerna.cluster.util.ClusterUtil;
+import prerna.engine.api.IEngine;
 import prerna.engine.impl.SmssUtilities;
 import prerna.nameserver.utility.MasterDatabaseUtility;
 import prerna.sablecc2.om.GenRowStruct;
@@ -73,17 +74,8 @@ public class GetDatabaseMetamodelReactor extends AbstractReactor {
 
 		// this is for the OWL positions for the new layout
 		if(options.contains("positions")) {
-			if(ClusterUtil.IS_CLUSTER){
-				try {
-					CloudClient.getClient().pullApp(engineId);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+			IEngine engine = Utility.getEngine(engineId);
+
 			// if the file is present, pull it and load
 			String smssFile = DIHelper.getInstance().getCoreProp().getProperty(engineId + "_" + Constants.STORE);
 			Properties prop = Utility.loadProperties(smssFile);
