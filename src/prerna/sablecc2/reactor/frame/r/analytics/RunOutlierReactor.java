675041f13003db3c549e417cc55444e11d30ab2d
mahkhalil <mahkhalil@deloitte.com>
2020-04-23 11:57:13 +0000

    UPDATE ::: Updating the impute null values and outliers routine
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@38373 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/r/analytics/RunOutlierReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/analytics/RunOutlierReactor.java
@@ -1,12 +1,12 @@
 package prerna.sablecc2.reactor.frame.r.analytics;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 import org.apache.log4j.Logger;
 
 import prerna.algorithm.api.ITableDataFrame;
-import prerna.ds.OwlTemporalEngineMeta;
+import prerna.ds.r.RDataTable;
 import prerna.ds.r.RSyntaxHelper;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
@@ -20,171 +20,163 @@ import prerna.util.usertracking.AnalyticsTrackerHelper;
 import prerna.util.usertracking.UserTrackerFactory;
 
 /**
- * This reactor updates determines the outliers for a set of values in an instance column based on the selected attribute columns
- * The result is a new column added to the data frame with values of TRUE (is an outlier) or FALSE (not an outlier)
- * The inputs to the reactor are: 
- * 1) the instance column
- * 2) the attribute columns
- * 3) whether each row should be treated as a unique instance (Yes or No)
+ * This reactor updates determines the outlier rows based on the selected
+ * attribute columns The result is a new column added to the data frame with
+ * values of TRUE (is an outlier) or FALSE (not an outlier) The inputs to the
+ * reactor are: 1) the attribute columns 2) alpha
+ * 
+ * RunOutlier(attributes=["Revenue_International","Revenue_Domestic","MovieBudget"],alpha=[0.05],nullHandleType=["As_Is"])
+ * RunOutlier(attributes=["bp_1d","bp_1s","bp_2d","bp_2s"],alpha=[0.05],nullHandleType=["Impute"])
  * 
  */
 
 public class RunOutlierReactor extends AbstractRFrameReactor {
-	
+
 	private static final String CLASS_NAME = RunOutlierReactor.class.getName();
-	
+
 	private static final String ALPHA = "alpha";
-	private static final String UNIQUE_INSTANCE_PER_ROW= "uniqInstPerRow";
-	
+	private static final String NULL_HANDLING = "nullHandleType";
+
 	public RunOutlierReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.INSTANCE_KEY.getKey(), ReactorKeysEnum.ATTRIBUTES.getKey(), UNIQUE_INSTANCE_PER_ROW};
+		this.keysToGet = new String[] { ReactorKeysEnum.ATTRIBUTES.getKey(), ALPHA };
 	}
 
 	@Override
 	public NounMetadata execute() {
 		init();
-		String[] packages = new String[] {"data.table", "HDoutliers", "FactoMineR"};
+		String[] packages = new String[] { "data.table", "mclust", "flashClust", "leaps", "FNN", "FactoMineR",
+				"HDoutliers", "missRanger" };
 		this.rJavaTranslator.checkPackages(packages);
 		Logger logger = this.getLogger(CLASS_NAME);
 		ITableDataFrame dataFrame = getFrame();
 		String frameName = dataFrame.getName();
-		dataFrame.setLogger(logger);
-		OwlTemporalEngineMeta meta = this.getFrame().getMetaData();
-		
+
 		// figure out inputs
-		String instanceColumn = getInstanceCol();
-		List<String> attributeNamesList = getAttributeNames(instanceColumn);
-		
-		// check the unique count of instances for each attribute
-		for (String columnName : attributeNamesList) {
-			int uniqueInstanceCount = dataFrame.getUniqueInstanceCount(columnName);
-			// # of unique instance must be greater than 1 for R routine!!!!
-			if (uniqueInstanceCount < 10) {
-				String msg = "Number of unqiue instances in " + columnName + " is less than 10. Outlier routine is not ideal for small variance.";
-				throw new IllegalArgumentException(msg);
-			}
-		}
-		
-		String[] attributeNames = attributeNamesList.toArray(new String[]{});
+		List<String> attributeNamesList = getAttributeNames();
 		String alpha = getAlpha();
-		String uniqInstPerRowStr = getUniqInstPerRow();
-		
-		// determine the name for the new similarity column to avoid adding columns with same name
+		String nullHandlingType = getNullHandleType();
+
+		// determine the name for the new outlier column to avoid adding columns with
+		// same name
 		String[] allColNames = dataFrame.getColumnHeaders();
 		int counter = 0;
-		String newColName = instanceColumn + "_Outlier";
+		String newColName = "Outlier";
 		while (ArrayUtilityMethods.arrayContainsValue(allColNames, newColName)) {
 			counter++;
-			newColName = instanceColumn + "_Outlier_" + counter;
+			newColName = "Outlier_" + counter;
 		}
-		
+		String[] attributeNames = attributeNamesList.toArray(new String[] {});
+
+		// dont need to check the unique count of instances for each attribute
+		// because of the new null handling options
+		if (nullHandlingType.equals("as_is")) {
+			for (String columnName : attributeNamesList) {
+				// make sure there are no nulls if we are doing as_is
+				int countNulls = this.rJavaTranslator.getInt("sum(is.na(" + frameName + "$" + columnName + "))");
+				if (countNulls > 0) {
+					String msg = columnName + " has " + countNulls
+							+ " null values. Please select Impute or Drop to handle the null values.";
+					throw new IllegalArgumentException(msg);
+				}
+			}
+		}
+
 		// get the data from the outlier algorithm
 		logger.info("Start iterating through data to determine outliers");
-		runROutlierAlgorithm(frameName, instanceColumn, attributeNames, newColName, alpha, uniqInstPerRowStr);
+		String newFrameName = runROutlierAlgorithm(frameName, attributeNames, alpha, nullHandlingType, newColName);
 		logger.info("Done iterating through data to determine outliers");
 
-		// track GA data
-//		UserTrackerFactory.getInstance().trackAnalyticsPixel(this.insight, "OutliersAlgorithm");
+		// check for error message
+		String errorMessage = this.rJavaTranslator.getString(newFrameName);
+		if (errorMessage != null) {
+			String msg = errorMessage;
+			throw new IllegalArgumentException(msg);
+		}
 		
 		// NEW TRACKING
-		UserTrackerFactory.getInstance().trackAnalyticsWidget(
-				this.insight, 
-				dataFrame, 
-				"OutliersAlgorithm", 
+		UserTrackerFactory.getInstance().trackAnalyticsWidget(this.insight, dataFrame, "OutliersAlgorithm",
 				AnalyticsTrackerHelper.getHashInputs(this.store, this.keysToGet));
-		
-		// create the new frame meta
-		meta.addProperty(frameName, frameName + "__" + newColName);
-		meta.setAliasToProperty(frameName + "__" + newColName, newColName);
-		meta.setDataTypeToProperty(frameName + "__" + newColName, "STRING");
-		
-		// now return this object
-		NounMetadata noun = new NounMetadata(dataFrame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE, PixelOperationType.FRAME_HEADERS_CHANGE);
-		noun.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Outlier ran succesfully! See new \"" + newColName + "\" column in the grid."));
+
+		// now create the frame and return the noun
+		this.rJavaTranslator.executeEmptyR(RSyntaxHelper.asDataTable(frameName, newFrameName));
+		RDataTable newTable = createNewFrameFromVariable(frameName);
+		this.insight.setDataMaker(newTable);
+		NounMetadata noun = new NounMetadata(newTable, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE,
+				PixelOperationType.FRAME_HEADERS_CHANGE);
+		this.insight.getVarStore().put(frameName, noun);
+
 		return noun;
 	}
-	
+
 	/**
-	 * R the similarity algorithm using an r script
+	 * Run the outlier algorithm using an r script
+	 * 
 	 * @param frameName
-	 * @param instanceColumn
 	 * @param attributeNames
+	 * @param alpha
 	 * @param newColName
 	 */
-	private void runROutlierAlgorithm(String frameName, String instanceColumn, String[] attributeNames, String newColName, String alpha, String uniqInstPerRowStr) {
-		
+	private String runROutlierAlgorithm(String frameName, String[] attributeNames, String alpha,
+			String nullHandlingType, String newColName) {
+
 		// create a column vector to pass as an input into our R script
 		String colVector = RSyntaxHelper.createStringRColVec(attributeNames);
-		
+
 		// the name of the results table is what we will be passing to the FE
 		String resultsFrameName = "ResultsTable" + Utility.getRandomString(10);
-		
+
 		// create a stringbuilder for our r syntax
 		StringBuilder rsb = new StringBuilder();
-		// source the r script that will run the numerical correlation routine
-		String correlationScriptFilePath = getBaseFolder() + "\\R\\AnalyticsRoutineScripts\\HDoutliers.R";
-		correlationScriptFilePath = correlationScriptFilePath.replace("\\", "/");
-		rsb.append("source(\"" + correlationScriptFilePath + "\");");
-        // R syntax for the routine: ResultsTableName <- DetermineHDOutliers(frameName, "Title", c("col1", "col2"), alpha, "outlierColumnName", TRUE)
-		rsb.append(resultsFrameName + "<- DetermineHDoutliers(" + frameName + ", " + "\"" + instanceColumn + "\"" + ", " + colVector + ", " + alpha + ", " +  "\"" + newColName + "\"" + ", " + uniqInstPerRowStr + ");");
-		rsb.append(frameName +  " <- " + resultsFrameName);
+
+		// source the r script that will run the outlier and impute routine
+		String outlierScriptFilePath = getBaseFolder() + "\\R\\AnalyticsRoutineScripts\\HDoutliers.R";
+		String imputeScriptFilePath = getBaseFolder() + "\\R\\AnalyticsRoutineScripts\\ImputeData.R";
+		outlierScriptFilePath = outlierScriptFilePath.replace("\\", "/");
+		imputeScriptFilePath = imputeScriptFilePath.replace("\\", "/");
+		rsb.append("source(\"" + outlierScriptFilePath + "\");");
+		rsb.append("source(\"" + imputeScriptFilePath + "\");");
+
+		// add function
+		String frameAsDf = frameName + Utility.getRandomString(6);
+		rsb.append(RSyntaxHelper.asDataFrame(frameAsDf, frameName));
+		rsb.append(resultsFrameName + "<- find_outliers(" + frameAsDf + ", " + colVector + ", " + alpha + ", \""
+				+ nullHandlingType + "\"" + ", \"" + newColName + "\")[[2]];");
 
 		// run the script
 		this.rJavaTranslator.runR(rsb.toString());
+
 		// garbage collection
-		this.rJavaTranslator.executeEmptyR("rm(" + resultsFrameName + ", DetermineHDoutliers); gc();");
+		this.rJavaTranslator.executeEmptyR("rm(" + frameAsDf + "); gc();");
+
+		// return new frame
+		return resultsFrameName;
+
 	}
 
 	////////////////////////////////////////////////////////////
 	////////////////////////////////////////////////////////////
 	////////////////////////////////////////////////////////////
-	
+
 	/*
 	 * Retrieving inputs
 	 */
-	private List<String> getAttributeNames(String instanceColumn) {
+
+	private List<String> getAttributeNames() {
 		// see if defined as individual key
-		List<String> retList = new ArrayList<String>();
-		GenRowStruct columnGrs = this.store.getNoun(keysToGet[1]);
+		GenRowStruct columnGrs = this.store.getNoun(this.keysToGet[0]);
 		if (columnGrs != null) {
-			for (NounMetadata noun : columnGrs.vector) {
-				String attribute = noun.getValue().toString();
-				if (!(attribute.equals(instanceColumn))) {
-					retList.add(attribute);
+			if (columnGrs.size() > 0) {
+				List<Object> values = columnGrs.getAllValues();
+				List<String> strValues = new Vector<String>();
+				for (Object obj : values) {
+					strValues.add(obj.toString());
 				}
-			}
-		} else {
-			int rowLength = this.curRow.size();
-			for (int i = 1; i < rowLength; i++) {
-				NounMetadata colNoun = this.curRow.getNoun(i);
-				String attribute = colNoun.getValue().toString();
-				if (!(attribute.equals(instanceColumn))) {
-					retList.add(attribute);
-				}
+				return strValues;
 			}
 		}
-		return retList;
+		return null;
 	}
 
-	private String getInstanceCol() {
-		// check if the instance column is entered with a key
-		GenRowStruct instanceIndexGrs = this.store.getNoun(keysToGet[0]);
-		String instanceCol = "";
-		NounMetadata instanceIndexNoun;
-		if (instanceIndexGrs != null) {
-			instanceIndexNoun = instanceIndexGrs.getNoun(0);
-			instanceCol = (String) instanceIndexNoun.getValue();
-		} else {
-			// else, we assume it is the zero index in the current row -->
-			// runOutlier(FRAME_COL, numSubsetSize, numRuns, [FRAME_COL1, ... ,
-			// FRAME_COLN]);
-			instanceIndexNoun = this.curRow.getNoun(0);
-			instanceCol = (String) instanceIndexNoun.getValue();
-		}
-
-		return instanceCol;
-	}
-	
 	private String getAlpha() {
 		GenRowStruct alphaGrs = this.store.getNoun(ALPHA);
 		if (alphaGrs != null) {
@@ -192,37 +184,33 @@ public class RunOutlierReactor extends AbstractRFrameReactor {
 				return alphaGrs.get(0).toString();
 			}
 		}
-		// default to .10
-		return ".10";
-	} 
-	
-	private String getUniqInstPerRow() {
-		// see if defined as individual key
-		GenRowStruct columnGrs = this.store.getNoun(UNIQUE_INSTANCE_PER_ROW);
-		if (columnGrs != null) {
-			if (columnGrs.size() > 0) {
-				String value = columnGrs.get(0).toString().toUpperCase();
-				if (value.equals("YES")) {
-					return "TRUE";
-				} else if (value.equals("NO")) {
-					return "FALSE";
-				}
-			}
-		} else {
-			return "FALSE";
-		}
-		return null;
+		// default to .05
+		return "0.05";
 	}
-	
-	
+
+	private String getNullHandleType() {
+		// an action specifying how to handle missing data (options:
+		// "impute","drop","as_is")
+		GenRowStruct alphaGrs = this.store.getNoun(NULL_HANDLING);
+		if (alphaGrs != null) {
+			if (alphaGrs.size() > 0) {
+				return alphaGrs.get(0).toString().toLowerCase();
+			}
+		}
+		// default to .05
+		return "as_is";
+	}
+
 	///////////////////////// KEYS /////////////////////////////////////
 
 	@Override
 	protected String getDescriptionForKey(String key) {
-		if (key.equals(UNIQUE_INSTANCE_PER_ROW)) {
-			return "YES or NO indicator of whether each instance should be treated as unique or if non-unique instances should be grouped";
+		if (key.equals(ALPHA)) {
+			return "The statistical threshold to determine the cutoff point for outliers (Defaut = 0.05)";
+		} else if (key.equals(NULL_HANDLING)) {
+			return "An action specifying how to handle missing data (options: \"impute\",\"drop\",\"as_is\")";
 		} else {
 			return super.getDescriptionForKey(key);
 		}
-}
+	}
 }
