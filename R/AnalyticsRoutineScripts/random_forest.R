a44521a3b6de4266e1469352f64ae0f063112933
nesanghvi <nesanghvi@deloitte.com>
2020-05-06 21:22:17 +0000

    UPDATE ::: adding sid's RF changes. Still a WIP
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@38585 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- R/AnalyticsRoutineScripts/random_forest.R
+++ R/AnalyticsRoutineScripts/random_forest.R
@@ -16,14 +16,13 @@ build_rfmodel<-function(ind_vars,dep_var,trainFrame,depth){
 
 	# assemble output
 	out<-list()
-	
-	if(class(trainFrame[, get(dep_var)]) %in% c("factor","character")){
+	if(class(trainFrame[[dep_var]]) %in% c("factor","character")){
 		out[[1]]<-model$prediction.error
-		out[[2]]<-as.factor(trainFrame[,get(dep_var)])
+		out[[2]]<-as.factor(trainFrame[[dep_var]])
 		out[[3]]<-model$predictions
 		out[[4]]<-data.frame(Variables=names(model$variable.importance),Importance=round(unname(model$variable.importance),4))
 	}else{
-		err<-abs(trainFrame[, get(dep_var)]-model$predictions)
+		err<-abs(trainFrame[[dep_var]]-model$predictions)
 		values<-vector()
 		values[1]<-round(min(err),4)
 		values[2]<-round(max(err),4)
@@ -49,7 +48,7 @@ drop_missing<-function(ind_vars,dep_var,tbl){
 	return(out)
 }
 
-rfmodel_mgr<-function(ind_vars,dep_var,trainFrame,model_fn="rf",sample_size=1000,sample_blocks=100,threshold=10000,depth=2){
+rfmodel_mgr<-function(ind_vars,dep_var,trainFrame,model_fn="rf",mis_data="as_is",sample_size=1000,sample_blocks=100,threshold=10000,depth=3){
 # Perform classification and regression for large datasets using random forest
 # Args
 # ind_vars - independent variables (columns in the training dataset)
@@ -62,142 +61,234 @@ rfmodel_mgr<-function(ind_vars,dep_var,trainFrame,model_fn="rf",sample_size=1000
 # Output
 # a list with attributes of the random forest model
 # random forest model and its parameters saved as rds files
-	# remove missing values
-	MODEL_NUMBER<-1
-	TREE_NUMBER<-1
-	trainFrame<-drop_missing(ind_vars,dep_var,trainFrame)
-	n<-nrow(trainFrame)
-	if(n>0){
-		models<-list()
-		if(n > threshold){
-			out<-list()
-			cnt<-vector()
-			for(i in 1:sample_blocks){
-				trainBlock<-trainFrame[unique(sample(n,sample_size,T)),]
-				out[[i]]<-build_rfmodel(ind_vars,dep_var,trainBlock,depth)
-				if(i==MODEL_NUMBER){
-					tree<-build_tree(dep_var,out[[i]][[5]],TREE_NUMBER)
-				}
-				cnt[i]<-nrow(trainBlock)
-				models[[i]]<-out[[i]][[5]]
-			}
-			if(class(trainFrame[, get(dep_var)]) %in% c("factor","character")){
-				type<-1   # classification
+	library(plyr)
+	ACTIONS<-c("impute","drop","as_is")
+	if(mis_data %in% ACTIONS){
+		# remove missing values
+		MODEL_NUMBER<-1
+		TREE_NUMBER<-1
+		
+		# fill in missing data per user action
+		trainFrame<-mis_data_action(trainFrame,c(ind_vars,dep_var),mis_data)
+		n<-nrow(trainFrame)
+		if(n>0){
+			models<-list()
+			if(n > threshold){
+				out<-list()
+				cnt<-vector()
 				for(i in 1:sample_blocks){
-					cur<-out[[i]]
-					if(i==1){
-						err<-cur[[1]]
-						tbl_act<-cur[[2]]
-						tbl_pred<-cur[[3]]
-						imp<-cur[[4]]
-					}else{
-						err<-err+cur[[1]]
-						tbl_act<-append(tbl_act,cur[[2]])
-						tbl_pred<-append(tbl_pred,cur[[3]])
-						imp<-cbind(imp,cur[[4]][,2])
+					trainBlock<-trainFrame[unique(sample(n,sample_size,T)),]
+					out[[i]]<-build_rfmodel(ind_vars,dep_var,trainBlock,depth)
+					if(i==MODEL_NUMBER){
+						tree<-build_tree(dep_var,out[[i]][[5]],TREE_NUMBER)
 					}
+					cnt[i]<-nrow(trainBlock)
+					models[[i]]<-out[[i]][[5]]
 				}
-				z<-list()
-				z[[1]]<-round(err/sample_blocks,4)
-				z[[2]]<-table(true=tbl_act,predicted=tbl_pred)
-				rownames(z[[2]])<-colnames(z[[2]])
-				z[[3]]<-data.frame(Variable=imp[,1],Importance=apply(imp[,-1],1,sum))
+				if(class(trainFrame[[dep_var]]) %in% c("factor","character")){
+					type<-1   # classification
+					opts<-sort(unique(trainFrame[[dep_var]]))
+					confusion_matrix<-matrix(0,nrow=length(opts),ncol=length(opts))
+					colnames(confusion_matrix)<-opts
+					rownames(confusion_matrix)<-opts
+					confusion_matrix<-as.table(confusion_matrix)
+					
+					for(i in 1:sample_blocks){
+						cur<-out[[i]]
+						if(i==1){
+							err<-cur[[1]]
+							
+							cur_matrix<-models[[i]]$confusion.matrix
+							cur_cols<-colnames(cur_matrix)
+							ind<-which(opts %in% cur_cols)
+							for(j in 1:length(ind)){
+								for(k in 1:length(ind)){
+									confusion_matrix[ind[j],ind[k]]<-cur_matrix[j,k]
+								}
+							}
+							imp<-cur[[4]]
+						}else{
+							err<-err+cur[[1]]
+							
+							cur_matrix<-models[[i]]$confusion.matrix
+							cur_cols<-colnames(cur_matrix)
+							ind<-which(opts %in% cur_cols)
+							for(j in 1:length(ind)){
+								for(k in 1:length(ind)){
+									confusion_matrix[ind[j],ind[k]]<-confusion_matrix[ind[j],ind[k]]+cur_matrix[j,k]
+								}
+							}
+							imp<-cbind(imp,cur[[4]][,2])
+						}
+					}
+					z<-list()
+					z[[1]]<-round(err/sample_blocks,4)
+					z[[2]]<-confusion_matrix
+					z[[3]]<-data.frame(Variable=imp[,1],Importance=apply(imp[,-1],1,sum))
+				}else{
+					type<-2   # regression
+					for(i in 1:sample_blocks){
+						cur<-out[[i]]
+						if(i==1){
+							err<-cur[[1]]
+							tbl<-cur[[2]]
+							imp<-cur[[4]]
+						}else{
+							err<-err+cur[[1]]
+							tbl<-cbind(tbl,cur[[2]][,2])
+							imp<-cbind(imp,cur[[4]][,2])
+						}
+					}
+					z<-list()
+					z[[1]]<-round(err/sample_blocks,4)
+					z[[2]]<-data.frame(Attributes=c("Min","Max","Mean","Stdev"),Prediction_Error=c(min(tbl[1,-1]),max(tbl[2,-1]),sum(tbl[3,-1]*cnt)/sum(cnt),sqrt(sum(tbl[4,-1]^2*cnt)/sum(cnt))))
+					z[[3]]<-data.frame(Variable=imp[,1],Importance=apply(imp[,-1],1,sum))
+				}
+				z[[4]]<-sample_blocks
+				z[[5]]<-500
 			}else{
-				type<-2   # regression
-				for(i in 1:sample_blocks){
-					cur<-out[[i]]
-					if(i==1){
-						err<-cur[[1]]
-						tbl<-cur[[2]]
-						imp<-cur[[4]]
-					}else{
-						err<-err+cur[[1]]
-						tbl<-cbind(tbl,cur[[2]][,2])
-						imp<-cbind(imp,cur[[4]][,2])
-					}
-				}
 				z<-list()
-				z[[1]]<-round(err/sample_blocks,4)
-				z[[2]]<-data.frame(Attributes=c("Min","Max","Mean","Stdev"),Prediction_Error=c(min(tbl[1,-1]),max(tbl[2,-1]),sum(tbl[3,-1]*cnt)/sum(cnt),sqrt(sum(tbl[4,-1]^2*cnt)/sum(cnt))))
-				z[[3]]<-data.frame(Variable=imp[,1],Importance=apply(imp[,-1],1,sum))
+				sample_blocks<-1
+				cnt<-vector()
+				type<-0
+				x<-build_rfmodel(ind_vars,dep_var,trainFrame,depth)
+				tree<-build_tree(dep_var,x[[5]],TREE_NUMBER)
+				z[[1]]<-x[[1]]
+				if(class(trainFrame[[dep_var]]) %in% c("factor","character")){
+					z[[2]]<-table(true=x[[2]],predicted=x[[3]])
+					rownames(z[[2]])<-colnames(z[[2]])
+				}else{
+					z[[2]]<-x[[2]]
+				}
+				z[[3]]<-x[[4]]
+				z[[4]]<-1
+				z[[5]]<-500
+				models<-x[[5]]
 			}
-			z[[4]]<-tree
+			params<-list()
+			params[[1]]<-ind_vars
+			params[[2]]<-dep_var
+			params[[3]]<-sample_blocks
+			params[[4]]<-cnt
+			params[[5]]<-type
+			saveRDS(params,paste0(model_fn,"_rfparams.rds"))
+			saveRDS(models,paste0(model_fn,"_rfmodels.rds"))
 		}else{
-			z<-list()
-			sample_blocks<-1
-			cnt<-vector()
-			type<-0
-			x<-build_rfmodel(ind_vars,dep_var,trainFrame,depth)
-			tree<-build_tree(dep_var,x[[5]],TREE_NUMBER)
-			z[[1]]<-x[[1]]
-			if(class(trainFrame[, get(dep_var)]) %in% c("factor","character")){
-				z[[2]]<-table(true=x[[2]],predicted=x[[3]])
-				rownames(z[[2]])<-colnames(z[[2]])
-			}else{
-				z[[2]]<-x[[2]]
-			}
-			z[[3]]<-x[[4]]
-			z[[4]]<-tree
-			models<-x[[5]]
+			z<-"Training dataset without missing values is empty"
 		}
-		params<-list()
-		params[[1]]<-ind_vars
-		params[[2]]<-dep_var
-		params[[3]]<-sample_blocks
-		params[[4]]<-cnt
-		params[[5]]<-type
-		saveRDS(params,paste0(model_fn,"_rfparams.rds"))
-		saveRDS(models,paste0(model_fn,"_rfmodels.rds"))
 	}else{
-		z<-"Training dataset without missing values is empty"
+		z<-"Possible missing data actions are: impute, drop and as is"
 	}
 	gc()
 	return(z)
 }
 
-predict_rfmodel<-function(newFrame,model_fn="rf"){
-# Retrieve the previously saved model and perform the predictions
-# Args
-# newFrame - a dataframe of new independent variables used in the model
-# model_fn - a file name of the model
-	library(ranger)
-	library(data.table)
+get_tree<-function(model_fn,model_nbr=1,tree_nbr=1){
 	params<-readRDS(paste0(model_fn,"_rfparams.rds"))
 	models<-readRDS(paste0(model_fn,"_rfmodels.rds"))
 	cnts<-params[[4]]
 	n<-length(cnts)
 	if(n==0){
-		newFrame[,params[[2]]]<-predict(models,newFrame)$prediction
-		colnames(newFrame)[ncol(newFrame)]<-paste0("Predicted_",params[[2]])
+		model<-models
 	}else{
-		for(i in 1:n){
-			model<-models[[i]]
-			if(i==1){
-				pred<-data.frame(m1=predict(model,newFrame)$predictions)
+		model<-models[[min(n,model_nbr)]]
+	}
+	tree<-build_tree(params[[2]],model,min(500,tree_nbr))
+	gc()
+	return(tree)
+	
+}
+
+predict_rfmodel<-function(newFrame,model_fn="rf",mis_data="as_is"){
+# Retrieve the previously saved model and perform the predictions
+# Args
+# newFrame - a dataframe of new independent variables used in the model
+# model_fn - a file name of the model
+	ACTIONS<-c("impute","drop","as_is")
+	
+	library(ranger)
+	library(data.table)
+	
+	# store the initial frame
+	in_frame<-newFrame
+	if(nrow(in_frame)>0){
+		if(mis_data %in% ACTIONS){
+			# fill in missing data per user action
+			r<-mis_data_action(newFrame,colnames(newFrame),mis_data)
+			newFrame<-r[[1]]
+			valid_rows<-r[[2]]
+			
+			params<-readRDS(paste0(model_fn,"_rfparams.rds"))
+			models<-readRDS(paste0(model_fn,"_rfmodels.rds"))
+			out<-list()
+			if(all(params[[1]] %in% names(newFrame))){
+				cnts<-params[[4]]
+				n<-length(cnts)
+				if(n==0){
+					prediction<-predict(models,newFrame)$prediction
+					if(length(valid_rows)==0){
+						cur_frame<-newFrame
+						cur_frame[,params[[2]]]<-prediction
+					}else{
+						cur_frame<-in_frame
+						dropped_rows<-which(!(seq(nrow(in_frame)) %in% valid_rows))
+						cur_frame[,params[[2]]]<-prediction[1]
+						cur_frame[valid_rows,params[[2]]]<-prediction
+						cur_frame[dropped_rows,params[[2]]]<-NA
+					}
+					colnames(cur_frame)[ncol(cur_frame)]<-paste0("Predicted_",params[[2]])
+				}else{
+					for(i in 1:n){
+						model<-models[[i]]
+						if(i==1){
+							pred<-data.frame(m1=predict(model,newFrame)$predictions)
+						}else{
+							pred[,paste0("m",i)]<-predict(model,newFrame)$predictions
+						}
+					}
+					m<-nrow(pred)
+					if(params[[5]]==1){
+						for(i in 1:n){
+							pred[,paste0("m",i)]<-as.character(pred[,paste0("m",i)])
+						}
+						p<-vector()
+						for(i in 1:m){
+							cur<-data.frame(Prediction=as.character(pred[i,]),Freq=cnts)
+							grp<-aggregate(cur[,2],by=list(cur$Prediction),FUN=sum)
+							grp<-grp[order(-grp$x),]
+							p[i]<-as.character(grp[1,1])
+						}
+					}else{
+						p<-apply(pred,1,mean)
+					}
+					if(length(valid_rows)==0){
+						cur_frame<-newFrame
+						cur_frame[,params[[2]]]<-p
+					}else{
+						cur_frame<-in_frame
+						dropped_rows<-which(!(seq(nrow(cur_frame)) %in% valid_rows))
+						cur_frame[,params[[2]]]<-p[1]
+						cur_frame[valid_rows,params[[2]]]<-p
+						cur_frame[dropped_rows,params[[2]]]<-NA
+					}
+					colnames(cur_frame)[ncol(cur_frame)]<-paste0("Predicted_",params[[2]])
+				}
+				out[[1]]<-0
+				out[[2]]<-params[[1]]
+				out[[3]]<-cur_frame
 			}else{
-				pred[,paste0("m",i)]<-predict(model,newFrame)$predictions
-			}
-		}
-		m<-nrow(pred)
-		if(params[[5]]==1){
-			for(i in 1:n){
-				pred[,paste0("m",i)]<-as.character(pred[,paste0("m",i)])
-			}
-			p<-vector()
-			for(i in 1:m){
-				cur<-data.frame(Prediction=as.character(pred[i,]),Freq=cnts)
-				grp<-aggregate(cur[,2],by=list(cur$Prediction),FUN=sum)
-				grp<-grp[order(-grp$x),]
-				p[i]<-as.character(grp[1,1])
+				out[[1]]<--1
+				out[[2]]<-params[[1]]
+				out[[3]]<-newFrame
 			}
 		}else{
-			p<-apply(pred,1,mean)
+			out<-"Possible missing data actions are: impute, drop and as is"
 		}
-		newFrame[,params[[2]]]<-p
-		colnames(newFrame)[ncol(newFrame)]<-paste0("Predicted_",params[[2]])
+	}else{
+		out<-"Input dataset is empty"
 	}
 	gc()
-	return(newFrame)
+	return(out)
 }
 
 
@@ -251,6 +342,51 @@ build_tree<-function(dep_var,model,nbr){
 }
 
 
+mis_data_action<-function(in_df,attrs,mis_data){
+# Performs missing data action
+# Args
+# in_df - a dataframe to perform missing data action on
+# attrs - a list of column names of df to perform missing data action
+# mis_data - missing data action to perform (possible options are "impute","drop","as_is")
+	ACTIONS<-c("impute","drop","as_is")
+	row_id<-paste(sample(c(1:9,letters,LETTERS),15),collapse="")
+	
+	# add row index
+	in_df[[row_id]]<-seq(nrow(in_df))
+	attrs<-append(attrs,row_id)
+	# subset of required columns
+	cols<-which(colnames(in_df) %in% attrs)
+	cmd<-paste0('df<-in_df[,',paste0('c(',paste(cols,collapse=','),')'),']')
+	eval(parse(text=cmd))
+	
+	valid_rows<-vector()
+	# handle missing data per user request
+	if(tolower(mis_data)==ACTIONS[1]){
+		df<-impute_data(df,attrs)
+		# replace part of the dataframe with imputed data
+		df<-replace_columns(in_df,df,row_id)
+	}else if(tolower(mis_data)==ACTIONS[2]){
+		df[df=="null" | df=="NULL"]<-NA
+		df<-df[complete.cases(df),]
+		if(nrow(in_df) > nrow(df)){
+			valid_rows<-which(in_df[[row_id]] %in% df[[row_id]])
+		}
+	}else{
+		# replace part of the dataframe with imputed data
+		df<-in_df
+	}
+	# drop temporary column row_id
+	ind<-which(colnames(df)==row_id)
+	cmd<-paste0("df<-df[,-",ind,"]")
+	eval(parse(text=cmd))
+	# assemble output
+	out<-list()
+	out[[1]]<-df
+	out[[2]]<-valid_rows
+	
+	gc()
+	return(out)
+}
 
 
 
