65a8c950f1dcdf863756e71d4592b49b6b9498ce
mahkhalil <mahkhalil@deloitte.com>
2020-05-07 15:07:35 +0000

    UPDATE ::: Pushing double lock check fortify fixes UPDATE ::: Pushing change on python to accommodate for jep bug preventing seaborn from working properly
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@38597 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 7f579895c1822ae70366cf488976c79d9ccd5340

--- src/prerna/auth/utils/SecurityUpdateUtils.java
+++ src/prerna/auth/utils/SecurityUpdateUtils.java
@@ -552,31 +552,35 @@ public class SecurityUpdateUtils extends AbstractSecurityUtils {
 			} else {
 				// not added by admin
 				// lets see if he exists or not
-				boolean isNewUser = SecurityQueryUtils.checkUserExist(newUser.getId());
-				if(!isNewUser) {
-					// need to synchronize the adding of new users
-					// so that we do not enter here from different threads 
-					// and add the same user twice
-					synchronized(SecurityUpdateUtils.class) {
-						// need to prevent 2 threads attempting to add the same user
-						isNewUser = SecurityQueryUtils.checkUserExist(newUser.getId());
-						if(!isNewUser) {
-							query = "INSERT INTO USER (ID, NAME, USERNAME, EMAIL, TYPE, ADMIN, PUBLISHER) VALUES ('" + 
-									RdbmsQueryBuilder.escapeForSQLStatement(newUser.getId()) + "', '" + 
-									RdbmsQueryBuilder.escapeForSQLStatement(newUser.getName()) + "', '" + 
-									RdbmsQueryBuilder.escapeForSQLStatement(newUser.getUsername()) + "', '" + 
-									RdbmsQueryBuilder.escapeForSQLStatement(newUser.getEmail()) + "', '" + 
-									newUser.getProvider() + "', " + 
-									"FALSE, " + 
-									!adminSetPublisher() + ");";
-							try {
-								securityDb.insertData(query);
-								securityDb.commit();
-							} catch (SQLException e) {
-								logger.error(STACKTRACE, e);
-							}
-							return true;
+				boolean userExists = SecurityQueryUtils.checkUserExist(newUser.getId());
+
+				if (userExists) {
+					logger.info("oauth user already exists");
+					return false;
+				}
+
+				// need to synchronize the adding of new users
+				// so that we do not enter here from different threads 
+				// and add the same user twice
+				synchronized(SecurityUpdateUtils.class) {
+					// need to prevent 2 threads attempting to add the same user
+					userExists = SecurityQueryUtils.checkUserExist(newUser.getId());
+					if(!userExists) {
+						query = "INSERT INTO USER (ID, NAME, USERNAME, EMAIL, TYPE, ADMIN, PUBLISHER) VALUES ('" + 
+								RdbmsQueryBuilder.escapeForSQLStatement(newUser.getId()) + "', '" + 
+								RdbmsQueryBuilder.escapeForSQLStatement(newUser.getName()) + "', '" + 
+								RdbmsQueryBuilder.escapeForSQLStatement(newUser.getUsername()) + "', '" + 
+								RdbmsQueryBuilder.escapeForSQLStatement(newUser.getEmail()) + "', '" + 
+								newUser.getProvider() + "', " + 
+								"FALSE, " + 
+								!adminSetPublisher() + ");";
+						try {
+							securityDb.insertData(query);
+							securityDb.commit();
+						} catch (SQLException e) {
+							logger.error(STACKTRACE, e);
 						}
+						return true;
 					}
 				}
 			}
--- src/prerna/ds/py/PyExecutorThread.java
+++ src/prerna/ds/py/PyExecutorThread.java
@@ -16,17 +16,17 @@ import prerna.util.DIHelper;
 public class PyExecutorThread extends Thread {
 
 	private static final String CLASS_NAME = PyExecutorThread.class.getName();
-	private static final Logger LOGGER = LogManager.getLogger(CLASS_NAME);
+	private static final Logger logger = LogManager.getLogger(CLASS_NAME);
 	private static transient SecurityManager defaultManager = System.getSecurityManager();
 
 	private static boolean first = true;
 	private Jep jep = null;
 	private Object daLock = new Object();
 	ThreadState curState = ThreadState.init;
-	
-	public String [] command = null;
-	public Hashtable <String, Object> response = new Hashtable<String, Object>();
-	
+
+	public String[] command = null;
+	public Hashtable<String, Object> response = new Hashtable<>();
+
 	public volatile boolean keepAlive = true;
 	private volatile boolean ready = false;
 	private Object driverMonitor = null;
@@ -35,190 +35,186 @@ public class PyExecutorThread extends Thread {
 	public void run() {
 		// wait to see if process is true
 		// if process is true - process, put the result and go back to sleep
-		LOGGER.debug("Running Python thread");
+		logger.debug("Running Python thread");
 		getJep();
 
-		while(this.keepAlive) {
+		while (this.keepAlive) {
 			try {
-				synchronized(daLock) {
-					LOGGER.debug("Waiting for next command");
+				synchronized (daLock) {
+					logger.debug("Waiting for next command");
 					ready = true;
 					curState = ThreadState.wait;
 					daLock.wait();
 					response.clear();
-					
+
 					curState = ThreadState.run;
 					// if someone wakes up
 					// process the command
 					// set the response go back to sleep
-					if(this.keepAlive) {
-						
+					if (this.keepAlive) {
 						ReactorSecurityManager tempManager = new ReactorSecurityManager();
 						tempManager.addClass(CLASS_NAME);
 						System.setSecurityManager(tempManager);
-						
-						for(int cmdLength = 0;command != null && cmdLength < command.length;cmdLength++) {
+
+						for (int cmdLength = 0; command != null && cmdLength < command.length; cmdLength++) {
 							String thisCommand = command[cmdLength];
-							while(thisCommand.endsWith(";"))
-								thisCommand = thisCommand.substring(0, thisCommand.length()-1);
+							while (thisCommand.endsWith(";"))
+								thisCommand = thisCommand.substring(0, thisCommand.length() - 1);
 
 							Object thisResponse = null;
-						    try {
-						    	LOGGER.debug(">>>>>>>>>>>");
-						    	LOGGER.info("Executing Command .. " + thisCommand);
-						    	LOGGER.debug("<<<<<<<<<<<");
-						    	try {
-						    		thisResponse = jep.getValue(thisCommand);
-						    		response.put(thisCommand, thisResponse);
-						    	}catch (Exception ex) {
-						    		jep.eval(thisCommand);
-						    		response.put(thisCommand, "");
-						    	}
-						    	
-						    	//jep.eval("from java.lang import System");
-						    	//jep.eval("System.out.println('.')");
+							try {
+								logger.debug(">>>>>>>>>>>");
+								logger.info("Executing Command .. " + thisCommand);
+								logger.debug("<<<<<<<<<<<");
+								try {
+									thisResponse = jep.getValue(thisCommand);
+									response.put(thisCommand, thisResponse);
+								} catch (Exception ex) {
+									jep.eval(thisCommand);
+									response.put(thisCommand, "");
+								}
 
-						    	daLock.notify();
-						    	if(driverMonitor != null)
-						    	{
-						    		synchronized(driverMonitor)
-						    		{
-						    			driverMonitor.notify();
-						    			driverMonitor = null;
-						    		}
-						    	}
+								daLock.notify();
+								if (driverMonitor != null) {
+									synchronized (driverMonitor) {
+										driverMonitor.notify();
+										driverMonitor = null;
+									}
+								}
 								// seems like when there is an exception..I need to restart the thread
 							} catch (Exception e) {
 								try {
 									daLock.notify();
-							    	if(driverMonitor != null)
-							    	{
-							    		synchronized(driverMonitor)
-							    		{
-							    			driverMonitor.notify();
-							    			driverMonitor = null;
-							    		}
-							    	}
+									if (driverMonitor != null) {
+										synchronized (driverMonitor) {
+											driverMonitor.notify();
+											driverMonitor = null;
+										}
+									}
 								} catch (Exception e1) {
-									e1.printStackTrace();
+									logger.error(e1.getStackTrace());
 								}
-								e.printStackTrace();
+								logger.error(e.getStackTrace());
 							}
 						}
 						command = null;
-						
+
 						// set back the original security manager
 						tempManager.removeClass(CLASS_NAME);
-						System.setSecurityManager(defaultManager);	
+						System.setSecurityManager(defaultManager);
 					}
 				}
 			} catch (InterruptedException e) {
-				e.printStackTrace();
+				Thread.currentThread().interrupt();
+				logger.error(e.getStackTrace());
 			}
 		}
-		LOGGER.debug("Thread ENDED");
+		logger.debug("Thread ENDED");
 	}
-	
-	public void setDriverMonitor(Object driverMonitor)
-	{
+
+	private static void restartThread() {
+		
+	}
+
+	public void setDriverMonitor(Object driverMonitor) {
 		this.driverMonitor = driverMonitor;
 	}
-	
+
 	public boolean isReady() {
 		return ready;
 	}
-	
+
 	public Object getMonitor() {
 		return daLock;
 	}
-	
+
 	public Jep getJep() {
 		try {
-			if(this.jep == null) {
+			if (this.jep == null) {
 				JepConfig aJepConfig = new JepConfig();
 				// this is not longer needed in 3.9.0
-				//https://github.com/ninia/jep/issues/140
-//				aJepConfig.addSharedModules("pandas", 
-//						"numpy",
-//						"sys", 
-//						"fuzzywuzzy", 
-//						"string", 
-//						"random", 
-//						"datetime", 
-//						"annoy",
-//						"sklearn",
-//						"pulp");
-				
+				// https://github.com/ninia/jep/issues/140
+				/*aJepConfig.addSharedModules("pandas", 
+						"numpy",
+						"sys", 
+						"fuzzywuzzy", 
+						"string", 
+						"random", 
+						"datetime", 
+						"annoy",
+						"sklearn",
+						"pulp");*/
+
 				// add the sys.path to python libraries for semoss
 				String pyBase = null;
 				pyBase = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + "/" + Constants.PY_BASE_FOLDER;
-				//pyBase = "c:/users/pkapaleeswaran/workspacej3/semossweb/py";
 				pyBase = pyBase.replace('\\', '/');
 				aJepConfig.addIncludePaths(pyBase);
 				aJepConfig.setRedirectOutputStreams(true);
-				
+
 				// add the libraries
 				String sitepackages = DIHelper.getInstance().getProperty("PYTHON_PACKAGES");
-				if(sitepackages != null && !sitepackages.isEmpty()) {
+				if (sitepackages != null && !sitepackages.isEmpty()) {
 					aJepConfig.addIncludePaths(sitepackages);
 				}
-				
+
 				initSharedInterpreter(aJepConfig);
 				jep = new SharedInterpreter();
-				
+
 				jep.eval("from jep import redirect_streams");
 				jep.eval("redirect_streams.setup()");
-
+				
 				jep.eval("import numpy as np");
 				jep.eval("import pandas as pd");
 				jep.eval("import gc as gc");
 				jep.eval("import sys");
+				// workaround for issue with matplotlib.pyplot.plot() not working with python
+				// 3.7.3; sys.argv is assumed to have length > 0
+				// see https://github.com/ninia/jep/issues/187 for details
+				jep.eval("sys.argv.append('')"); 
 				jep.eval("from fuzzywuzzy import fuzz");
 				jep.eval("import pandas as pd");
 				jep.eval("import string");
 				jep.eval("import random");
 				jep.eval("import datetime");
-				//jep.eval("from annoy import AnnoyIndex");
+				// jep.eval("from annoy import AnnoyIndex");
 				jep.eval("import numpy");
 				jep.eval("import sys");
-				// workaround for issue with matplotlib.pyplot.plot() not working with python 3.7.3; sys.argv is assumed to have length > 0
-				// see https://github.com/ninia/jep/issues/187 for details
-				//jep.eval("sys.argv.append('')");
-				// this is so we do not get a GIL
-				//jep.eval("from java.lang import System");
-				
-				LOGGER.debug("Adding Syspath " + pyBase);				
-				jep.eval("sys.path.append('" + pyBase + "')" );
-				LOGGER.debug(jep.getValue("sys.path"));
-				
-				// these needs to be a better way to do this where we can add other things 
+
+				logger.debug("Adding Syspath " + pyBase);
+				jep.eval("sys.path.append('" + pyBase + "')");
+				logger.debug(jep.getValue("sys.path"));
+
+				// these needs to be a better way to do this where we can add other things
 				jep.eval("from clean import PyFrame");
 				jep.eval("import smssutil");
 			}
 		} catch (JepException e) {
-			e.printStackTrace();
+			logger.error(e.getStackTrace());
 		}
 		return jep;
 	}
-	
+
 	public void killThread() {
 		this.keepAlive = false;
 	}
 
 	/**
 	 * Making init thread safe
+	 * 
 	 * @param aJepConfig
 	 * @throws JepException
 	 */
 	private void initSharedInterpreter(JepConfig aJepConfig) throws JepException {
-		if(first) {
-			synchronized (this) {
-				if(first) {
-					SharedInterpreter.setConfig(aJepConfig);
-					first = false;
-				}
+		if (!first) {
+			return;
+		}
+
+		synchronized (this) {
+			if (first) {
+				SharedInterpreter.setConfig(aJepConfig);
+				first = false;
 			}
 		}
 	}
-	
 }
