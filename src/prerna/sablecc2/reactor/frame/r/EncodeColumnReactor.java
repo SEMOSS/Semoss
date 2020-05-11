699fd57830d63162081c9115dcd3717f615a5f82
mahkhalil <mahkhalil@deloitte.com>
2020-05-11 15:02:15 +0000

    UPDATE ::: Adding encode column logic for grid, r, py
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@38635 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ea855b15c7fed4545801cbd8f0cac1156adfc3f2

---
+++ src/prerna/sablecc2/reactor/frame/r/EncodeColumnReactor.java
@@ -0,0 +1,68 @@
+package prerna.sablecc2.reactor.frame.r;
+
+import java.util.List;
+
+import prerna.ds.r.RDataTable;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.util.usertracking.AnalyticsTrackerHelper;
+import prerna.util.usertracking.UserTrackerFactory;
+
+public class EncodeColumnReactor extends AbstractRFrameReactor {
+
+	public EncodeColumnReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.COLUMNS.getKey() };
+	}
+
+	@Override
+	public NounMetadata execute() {
+		// init R
+		init();
+
+		String[] packages = { "digest" };
+		this.rJavaTranslator.checkPackages(packages);
+
+		RDataTable frame = (RDataTable) getFrame();
+		String frameName = frame.getName();
+		List<String> columns = getColumns();
+		if(columns == null || columns.isEmpty()) {
+			throw new IllegalArgumentException("Need to pass in the columns to encode");
+		}
+
+		StringBuilder script = new StringBuilder();
+		script.append("library(digest);")
+		.append("encode <- function(value) digest(value, algo=\"sha256\");");
+
+		for(String col : columns) {
+			String select = frameName + "$" + col;
+			script.append(select).append(" <- lapply(").append(select).append(", encode);");
+		}
+
+		this.rJavaTranslator.executeEmptyR(script.toString());
+		
+		// NEW TRACKING
+		UserTrackerFactory.getInstance().trackAnalyticsWidget(
+				this.insight, 
+				frame, 
+				"EncodeColumn", 
+				AnalyticsTrackerHelper.getHashInputs(this.store, this.keysToGet));
+
+		NounMetadata noun = new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE);
+		return noun;
+	}
+
+	private List<String> getColumns() {
+		// EncodeColumn(columns=["a","b","c"]);
+		GenRowStruct grs = this.store.getNoun(this.keysToGet[0]);
+		if(grs != null && !grs.isEmpty()) {
+			return grs.getAllStrValues();
+		}
+
+		// this is if passed in directly EncodeColumn("a","b","c");
+		return this.curRow.getAllStrValues();
+	}
+
+}
