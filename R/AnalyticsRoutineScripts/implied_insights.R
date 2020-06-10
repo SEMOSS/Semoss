17b411e232efede1e9dd0f0ffcd56d9376ab439c
mahkhalil <mahkhalil@deloitte.com>
2020-06-10 17:48:49 +0000

    UPDATE ::: Pushing implied insights reactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@39075 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 47d0d3859051fca7848c0e49376ddf6276689cde

---
+++ R/AnalyticsRoutineScripts/implied_insights.R
@@ -0,0 +1,138 @@
+get_dataset_outliers<-function(df,nbr_int=10,method="mean"){
+# Identifies column insights
+# Args
+# df - dataframe/table to search
+# nbr_int - number of interval to discretize data (default 10)
+# ext - extention coefficient (default 1.5)
+# Output
+# list with each element is a list itself 
+# with first element discretized data, second anomalies of the data 
+	library(data.table)
+	library(arules)
+	n<-ncol(df)
+	cols<-colnames(df)
+	out<-data.frame(Column=character(),Outlier=character(),Frequency=integer(),stringsAsFactors=FALSE)
+	if(n>0){
+		for(i in 1:n){
+			item<-list()
+			if(class(df[[i]]) %in% c("integer","numeric")){
+				item[[1]]<-table(discretize(df[[i]],method="interval",breaks=nbr_int))
+				item[[2]]<-get_univarible_outliers(item[[1]],method)
+				m<-length(item[[2]])
+				if(m>0){
+					out<-rbind(out,data.frame(Column=rep(names(df)[i],m),Outlier=names(item[[2]]),Frequency=unname(as.integer(item[[2]]))))
+				}
+			}else if(class(df[[i]]) %in% c("character","factor")){
+				item[[1]]<-table(as.numeric(as.factor(df[[i]])))
+				names(item[[1]])<-levels(as.factor(df[[i]]))
+				item[[2]]<-get_univarible_outliers(item[[1]],method)
+				m<-length(item[[2]])
+				if(m>0){
+					out<-rbind(out,data.frame(Column=rep(names(df)[i],m),Outlier=names(item[[2]]),Frequency=unname(as.integer(item[[2]]))))
+				}
+			}else if(class(df[[i]])=="logical"){
+				item[[1]]<-table(as.numeric(df[[i]]))
+				names(item[[1]])<-unname(sapply(names(out[[i]]),function(x) if(x=="0") "FALSE" else "TRUE"))
+				item[[2]]<-get_univarible_outliers(item[[1]],method)
+				m<-length(item[[2]])
+				if(m>0){
+					out<-rbind(out,data.frame(Column=rep(names(df)[i],m),Outlier=names(item[[2]]),Frequency=unname(as.integer(item[[2]]))))
+				}
+			}
+		}
+	}
+	gc()
+	return(out)
+}
+
+
+get_univarible_outliers<-function (x,method="mean"){
+	if (method=="mean") {
+		avrg <- mean(x)
+		stdev <-sd(x)
+		dtf <- data.frame(ID=seq.int(length(x)), obs=x, outlier=abs(x-avrg)>2*stdev)
+		midp <- avrg
+		lower <- avrg-2*stdev
+		upper <- avrg+2*stdev
+		ind <- which(dtf$outlier)
+	} else {}
+	if (method=="median") {
+		med <- median(x)
+		MAD <-median(abs(med-x))
+		dtf <- data.frame(ID=seq.int(length(x)), obs=x, outlier=abs(x-med)>2*(MAD/0.6745))
+		midp <- med
+		lower <- med-2*(MAD/0.6745)
+		upper <- med+2*(MAD/0.6745)
+		ind <- which(dtf$outlier)
+	} else {}
+	if (method=="boxplot") {
+		Q1 <- quantile(x, 0.25)
+		Q3 <- quantile(x, 0.75)
+		IntQ <-Q3-Q1
+		dtf <- data.frame(ID=seq.int(length(x)), obs=x, outlier=x<Q1-1.5*IntQ | x>Q3+1.5*IntQ)
+		midp <- median(x)
+		lower <- Q1-1.5*IntQ
+		upper <- Q3+1.5*IntQ
+		ind <- which(dtf$outlier)
+	} 
+	if(length(ind)>0){
+		outliers<-x[ind]
+	}else{
+		outliers<-vector()
+	}
+	return(outliers)
+}
+
+get_subset<-function(df,z,col_name){
+	b<-paste0('c("',paste(as.character(z[z$Column==col_name,]$Outlier),collapse='","'),'")')
+	cmd<-paste0("df1<-df[df$",col_name," %in% ",b,",]")
+	eval(parse(text=cmd))
+	return(df1)
+}
+
+
+get_dep_ranking<-function(dtf,dep_vars,mthd = "estevez",desc_mthd="sturges",schm="mid"){
+	library(varrank)
+	out <- varrank(data.df = dtf,method = mthd,variable.important = dep_vars,discretization.method = desc_mthd,algorithm = "forward", scheme = schm,verbose=FALSE)
+	plot(out)
+	r<-diag(out$distance.m)
+	p<-data.frame(Column=names(r),RankScore=unname(r))
+	gc()
+	return(p)
+}
+
+get_frequent_itemsets<-function(df,max_cnt=ncol(df),min_cnt=2,sprt=0.1){
+	library(arules)
+
+	trans<-as(movies,"transactions")
+	itemsets <- apriori(trans, parameter = list(target = "frequent",  supp=sprt, minlen = min_cnt, maxlen=max_cnt))
+	out<-interestMeasure(itemsets, transactions = trans)
+	out$items<-labels(itemsets)
+	out<-out[out$lift>1,]
+	out<-out[order(-out$support,-out$lift),]
+	out<-out[,c(6,1,2,3,4,5)]
+	gc()
+	return(out)	
+}
+
+select_features<-function(df,dep_var){
+	library(Boruta)
+	if(class(df[[dep_var]])=="character"){
+		df[[dep_var]]<-as.factor(df[[dep_var]])
+	}
+	cmd<-paste0("x<-Boruta(",dep_var,"~.,data=df,doTrace=0)")
+	eval(parse(text=cmd))
+	y<-attStats(x)
+	out<-data.frame(Feature=rownames(y),Importance=y$meanImp,Selection=as.character(y$decision))
+	plot(x)
+	gc()
+	return(out)
+}
+
+get_df_scan<-function(df){
+	library(skimr)
+	mynames<-c("type","variable","miss","min_chars","max_chars","empty","unique","whitespace","mean","sd","p0","p25","p75","p100")
+	out<-as.data.frame(skim(df))[,c(1,2,3,5,6,7,8,9,10,11,12,13,14,15)]
+	colnames(out)<-mynames
+	return(out)
+}
---
+++ src/prerna/sablecc2/reactor/algorithms/RunImpliedInsightsReactor.java
@@ -0,0 +1,176 @@
+package prerna.sablecc2.reactor.algorithms;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import prerna.ds.OwlTemporalEngineMeta;
+import prerna.ds.r.RDataTable;
+import prerna.om.InsightPanel;
+import prerna.query.interpreters.RInterpreter;
+import prerna.query.querystruct.SelectQueryStruct;
+import prerna.query.querystruct.filters.GenRowFilters;
+import prerna.query.querystruct.selectors.QueryColumnSelector;
+import prerna.query.querystruct.transform.QSAliasToPhysicalConverter;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.frame.r.AbstractRFrameReactor;
+import prerna.util.Utility;
+
+public class RunImpliedInsightsReactor extends AbstractRFrameReactor {
+
+	public RunImpliedInsightsReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.PANEL.getKey() };
+	}
+
+	@Override
+	public NounMetadata execute() {
+		// set up the class
+		init();
+		organizeKeys();
+		RDataTable frame = (RDataTable) getFrame();
+		OwlTemporalEngineMeta meta = this.getFrame().getMetaData();
+		String dtName = frame.getName();
+		boolean implicitFilter = false;
+		String dtNameIF = "dtFiltered" + Utility.getRandomString(6);
+		InsightPanel panel = getInsightPanel();
+		
+		// check if the required packages are installed
+		String[] packages = new String[] { "data.table" , "arules" , "Boruta" , "rlang" , "tidyselect" ,  "skimr" };
+		this.rJavaTranslator.checkPackages(packages);
+
+		// check if there are filters on the frame. if so then need to run algorithm on subsetted data
+		GenRowFilters filters = panel.getPanelFilters();
+		if(!filters.isEmpty()) {
+			// create a new qs to retrieve filtered frame
+			SelectQueryStruct qs = new SelectQueryStruct();
+			List<String> selectedCols = Arrays.asList(frame.getColumnHeaders());
+			for(String s : selectedCols) {
+				qs.addSelector(new QueryColumnSelector(s));
+			}
+			qs.setImplicitFilters(filters);
+			qs = QSAliasToPhysicalConverter.getPhysicalQs(qs, meta);
+			RInterpreter interp = new RInterpreter();
+			interp.setQueryStruct(qs);
+			interp.setDataTableName(dtName);
+			interp.setColDataTypes(meta.getHeaderToTypeMap());
+			String query = interp.composeQuery();
+			this.rJavaTranslator.runR(dtNameIF + "<- {" + query + "}");
+			implicitFilter = true;
+			
+			//cleanup the temp r variable in the query var
+			this.rJavaTranslator.runR("rm(" + query.split(" <-")[0] + ");gc();");
+		}
+		
+		String targetDt = implicitFilter ? dtNameIF : dtName;
+
+		// run the summary frame functions
+		String summaryFrame = getSummaryFrame(targetDt);
+		String[] summaryColNames = this.rJavaTranslator.getColumns(summaryFrame);
+		List<Object[]> summaryData = this.rJavaTranslator.getBulkDataRow(summaryFrame, summaryColNames);
+		Map<String, Object> summaryRetMap = new HashMap<>();
+		summaryRetMap.put("headers", summaryColNames);
+		summaryRetMap.put("values", summaryData);
+		
+		// run the outlier frame functions
+		String outlierFrame = getOutlierFrame(targetDt);
+		String[] outlierColNames = this.rJavaTranslator.getColumns(outlierFrame);
+		List<Object[]> outlierData = this.rJavaTranslator.getBulkDataRow(outlierFrame, outlierColNames);
+		Map<String, Object> outlierRetMap = new HashMap<>();
+		outlierRetMap.put("headers", outlierColNames);
+		outlierRetMap.put("values", outlierData);
+		
+		// garbage cleanup
+		this.rJavaTranslator.executeEmptyR("rm(" + outlierFrame + " , " + summaryFrame + "); gc();");
+		
+		// format and return the noun
+		List<NounMetadata> tasks = new Vector<>();
+		NounMetadata noun1 = new NounMetadata(summaryRetMap, PixelDataType.CUSTOM_DATA_STRUCTURE);
+		NounMetadata noun2 = new NounMetadata(outlierRetMap, PixelDataType.CUSTOM_DATA_STRUCTURE);
+		
+		tasks.add(noun1);
+		tasks.add(noun2);
+		return new NounMetadata(tasks, PixelDataType.VECTOR, PixelOperationType.VECTOR);
+	}
+
+	private String getSummaryFrame(String targetDt) {
+		// the name of the results table is what we will be passing to the FE
+		String results = "sumFrame" + Utility.getRandomString(10);
+
+		// create a stringbuilder for our r syntax
+		StringBuilder rsb = new StringBuilder();
+
+		// source the r script that will run the outlier and impute routine
+		String scriptFilePath = getBaseFolder() + "\\R\\AnalyticsRoutineScripts\\implied_insights.R";
+		scriptFilePath = scriptFilePath.replace("\\", "/");
+		rsb.append("source(\"" + scriptFilePath + "\");");
+
+		// run function
+		rsb.append(results + " <- get_df_scan(" + targetDt + ");");
+
+		// run the script
+		this.rJavaTranslator.runR(rsb.toString());
+
+		// return new frame
+		return results;
+	}
+	
+	private String getOutlierFrame(String targetDt) {
+		// the name of the results table is what we will be passing to the FE
+		String results = "outlierFrame" + Utility.getRandomString(10);
+
+		// create a stringbuilder for our r syntax
+		StringBuilder rsb = new StringBuilder();
+
+		// source the r script that will run the outlier and impute routine
+		String scriptFilePath = getBaseFolder() + "\\R\\AnalyticsRoutineScripts\\implied_insights.R";
+		scriptFilePath = scriptFilePath.replace("\\", "/");
+		rsb.append("source(\"" + scriptFilePath + "\");");
+
+		// run function
+		rsb.append(results + " <- get_dataset_outliers(" + targetDt + ");");
+
+		// run the script
+		this.rJavaTranslator.runR(rsb.toString());
+
+		// return new frame
+		return results;
+	}
+	
+	protected InsightPanel getInsightPanel() {
+		InsightPanel panel = null;
+
+		// see if panel was passed via generic reactor
+		GenRowStruct genericGrs = this.store.getNoun(ReactorKeysEnum.PANEL.getKey());
+		if (genericGrs != null && !genericGrs.isEmpty()) {
+			String panelId = genericGrs.get(0).toString();
+			panel = this.insight.getInsightPanel(panelId);
+		}
+
+		// or was passed in via a "|"
+		GenRowStruct pipedGrs = this.store.getNoun(PixelDataType.PANEL.toString());
+		if (pipedGrs != null && !pipedGrs.isEmpty()) {
+			panel = (InsightPanel) pipedGrs.get(0);
+		}
+
+		if (panel == null) {
+			// if not, see if it was passed in the grs
+			List<Object> panelNouns = this.curRow.getValuesOfType(PixelDataType.PANEL);
+			if (panelNouns != null && !panelNouns.isEmpty()) {
+				panel = (InsightPanel) panelNouns.get(0);
+			}
+		}
+
+		if (panel == null) {
+			throw new IllegalArgumentException("Invalid panel id passed into With reactor");
+		}
+
+		return panel;
+	}
+	
+}
