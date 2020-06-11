56a003409693d017d863b160b28a9c093523ee4d
mahkhalil <mahkhalil@deloitte.com>
2020-06-11 20:28:56 +0000

    UPDATE ::: Allow the change column type to also change the additional data type
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@39102 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 426d43e460f95cef809a21e3ba9f20c40869637b

--- src/prerna/sablecc2/reactor/frame/rdbms/ChangeColumnTypeReactor.java
+++ src/prerna/sablecc2/reactor/frame/rdbms/ChangeColumnTypeReactor.java
@@ -2,11 +2,10 @@ package prerna.sablecc2.reactor.frame.rdbms;
 
 import java.util.Arrays;
 
-import prerna.algorithm.api.SemossDataType;
 import prerna.ds.rdbms.AbstractRdbmsFrame;
-import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.frame.AbstractFrameReactor;
 import prerna.util.usertracking.AnalyticsTrackerHelper;
@@ -25,21 +24,34 @@ public class ChangeColumnTypeReactor extends AbstractFrameReactor {
 	// this constant used when we specify the format for the dates
 	private static final String FORMAT = "format";
 
+	public ChangeColumnTypeReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.COLUMN.getKey(), ReactorKeysEnum.DATA_TYPE.getKey(), ReactorKeysEnum.ADDITIONAL_DATA_TYPE.getKey(), "format"};
+	}
+	
 	@Override
 	public NounMetadata execute() {
 		// get frame
 		AbstractRdbmsFrame frame = (AbstractRdbmsFrame) getFrame();
-		String column = getColumn();
-		String columnType = getColumnType();
-		columnType = frame.getQueryUtil().cleanType(columnType);
 		
+		// get inputs
 		String table = frame.getName();
+		String column = this.keyValue.get(this.keysToGet[0]);
+		if (column == null) {
+			throw new IllegalArgumentException("Need to define " + ReactorKeysEnum.COLUMN.getKey());
+		}
 		if (column.contains("__")) {
 			String[] split = column.split("__");
 			table = split[0];
 			column = split[1];
 		}
-
+		String newType = this.keyValue.get(this.keysToGet[1]);
+		if (newType == null) {
+			throw new IllegalArgumentException("Need to define " + ReactorKeysEnum.DATA_TYPE.getKey());
+		}
+		newType = frame.getQueryUtil().cleanType(newType);
+		
+		String additionalDataType = this.keyValue.get(this.keysToGet[2]);
+		
 		// check the column exists, if not then throw warning
 		String[] allCol = getColNames(frame);
 		if (!Arrays.asList(allCol).contains(column)) {
@@ -47,26 +59,33 @@ public class ChangeColumnTypeReactor extends AbstractFrameReactor {
 		}
 
 		// if we are changing from a string to a date, we need to parse using the format entered by the user
-		if (columnType.equalsIgnoreCase("date")) {
-			String dateFormat = getDateFormat();
+		if (newType.equalsIgnoreCase("date")) {
+			String dateFormat = this.keyValue.get(this.keysToGet[3]);
+			if(dateFormat == null || dateFormat.isEmpty()) {
+				dateFormat = "yyyy-MM-dd";
+			}
 			// query of form: UPDATE FRAME SET birthday = PARSEDATETIME(column, 'format');
 			String convertString = "UPDATE " + table + " SET " + column + 
 					" = PARSEDATETIME (" + column + ", " + "'" + dateFormat + "');";
 			try {
 				frame.getBuilder().runQuery(convertString);
-				frame.getMetaData().modifyDataTypeToProperty(table + "__" + column, table, columnType);
+				frame.getMetaData().modifyDataTypeToProperty(table + "__" + column, table, newType);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		} else {
 			// if we are not working with a date
-			String update = "ALTER TABLE " + table + " ALTER COLUMN " + column + " " + columnType + " ; ";
+			String update = "ALTER TABLE " + table + " ALTER COLUMN " + column + " " + newType + " ; ";
 			try {
 				frame.getBuilder().runQuery(update);
-				frame.getMetaData().modifyDataTypeToProperty(table + "__" + column, table, columnType);
+				frame.getMetaData().modifyDataTypeToProperty(table + "__" + column, table, newType);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+		}
+		
+		if(additionalDataType != null && !additionalDataType.isEmpty()) {
+			frame.getMetaData().modifyAdditionalDataTypeToProperty(table + "__" + column, table, newType);
 		}
 		
 		// NEW TRACKING
@@ -77,60 +96,5 @@ public class ChangeColumnTypeReactor extends AbstractFrameReactor {
 				AnalyticsTrackerHelper.getHashInputs(this.store, this.keysToGet));
 		
 		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE, PixelOperationType.FRAME_HEADERS_CHANGE);
-	}
-
-	
-	//////////////////////////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////////
-	///////////////////////// GET PIXEL INPUT ////////////////////////////
-	//////////////////////////////////////////////////////////////////////
-	//////////////////////////////////////////////////////////////////////
-	
-	// this method gets the name of the column that we are modifying
-	private String getColumn() {
-		GenRowStruct inputsGRS = this.getCurRow();
-		if (inputsGRS != null && !inputsGRS.isEmpty()) {
-			String colName = inputsGRS.getNoun(0).getValue() + "";
-			if (colName.length() == 0) {
-				throw new IllegalArgumentException("Need to define the new column name");
-			}
-			return colName;
-		}
-		throw new IllegalArgumentException("Need to define the new column name");
-	}
-
-	// this method gets the desired column type
-	private String getColumnType() {
-		GenRowStruct inputsGRS = this.getCurRow();
-		NounMetadata input2 = inputsGRS.getNoun(1);
-		String columnType = input2.getValue() + "";
-		// default to string
-		if (columnType.length() == 0) {
-			columnType = "STRING";
-		}
-		// check if data type is supported
-		SemossDataType dt = SemossDataType.convertStringToDataType(columnType);
-		if (dt == null) {
-			dt = SemossDataType.STRING;
-		}
-		return columnType;
-	}
-
-	// we will get the format entered by the user so we can use PARSEDATETIME
-	private String getDateFormat() {
-		// grab the format that was inputted
-		GenRowStruct formatGRS = this.store.getNoun(FORMAT);
-		if (formatGRS != null) {
-			if (formatGRS.size() > 0) {
-				// get the user input which can either be from the drop down or free text
-				String dateFormat = formatGRS.get(0).toString();
-				// semoss uses underscores where the user might enter spaces
-				// for example, MMM yyyy will be updated to MMM_yyyy
-				dateFormat = dateFormat.replace(" ", "_");
-				return dateFormat;
-			}
-		}
-		// default to date format yyyy-mm-dd
-		return "yyyy-MM-dd";
 	}
 }
