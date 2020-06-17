540f3022afc28cfcd9786ce442273379e8831075
mahkhalil <mahkhalil@deloitte.com>
2020-06-17 21:40:07 +0000

    UPDATE ::: Deprecating and removing logic from legacy file watcher to move to rdbms
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@39171 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/quartz/QuartzWebWatcher.java
+++ src/prerna/quartz/QuartzWebWatcher.java
@@ -29,6 +29,7 @@ import org.quartz.Trigger;
 
 import prerna.util.AbstractFileWatcher;
 
+@Deprecated
 public class QuartzWebWatcher extends AbstractFileWatcher {
 
 	private static final String ACTION_TYPE_EMAIL = "email";
@@ -48,124 +49,127 @@ public class QuartzWebWatcher extends AbstractFileWatcher {
 
 	@Override
 	public void process(String fileName) {
-		// Get the json string from a file
-		String jsonString;
-		try {
-			jsonString = new String(Files.readAllBytes(Paths.get(folderToWatch + "/" + fileName)), "UTF-8");
-		} catch (IOException e) {
-			e.printStackTrace();
-			LOGGER.error("Unable to read the file " + fileName);
-			return;
-		}
-		
-		// Parse the json string into an object
-		JSONObject json;
-		try {
-			json = new JSONObject(jsonString);
-		} catch (JSONException e) {
-			e.printStackTrace();
-			LOGGER.error("Unable to parse json object from " + fileName);
-			return;
-		}
-		
-		// Add data from the json object into the job data map
-		try {
-			JSONObject job = json.getJSONObject("schedule").getJSONObject("job");
-			JSONObject values = job.getJSONObject("values");
-			JSONObject action = job.getJSONObject("action");
-			JSONObject trigger = json.getJSONObject("schedule").getJSONObject("trigger");
-
-			String rdbmsId = values.getString("rdbms-id");
-			String engine = values.getString("engine");
-			String returnColumn = values.getString("return-column");
-			String compareColumn = values.getString("compare-column");
-			String comparator = values.getString("comparator");
-			String valueType = values.getString("value-type");
-			String value = values.getString("value");
-//			String valuePkql = values.getString("value-pkql");
-			
-			// Setup the job chain
-			JobDataMap jobChainMap = new JobDataMap();
-			List<Class<? extends Job>> jobSequence = new ArrayList<Class<? extends Job>>();
-			
-			// WaitForEngineToLoadJob
-			jobSequence.add(WaitForEngineToLoadJob.class);
-			jobChainMap.put(WaitForEngineToLoadJob.IN_ENGINE_NAME_KEY, engine);
-			
-			// GetInsightJob
-			jobSequence.add(GetInsightJob.class);
-			jobChainMap.put(LinkedDataKeys.RDBMS_ID, rdbmsId);
-			jobChainMap.put(GetInsightJob.IN_ENGINE_NAME_KEY, engine);
-			
-			// GetDataFromInsightJob
-			jobSequence.add(GetDataFromInsightJob.class);
-		
-			// PrintDataToConsoleJob
-//			jobSequence.add(PrintDataToConsoleJob.class);
-			
-			//CheckCriteriaJob
-			jobSequence.add(CheckCriteriaJob.class);
-			jobChainMap.put(CheckCriteriaJob.IN_RETURN_COLUMN, returnColumn);
-			jobChainMap.put(CheckCriteriaJob.IN_COMPARE_COLUMN, compareColumn);
-			jobChainMap.put(CheckCriteriaJob.IN_COMPARATOR, comparator);
-			jobChainMap.put(CheckCriteriaJob.IN_VALUE_TYPE, valueType);
-			jobChainMap.put(CheckCriteriaJob.IN_VALUE, value);
-			
-			// TODO make the comparison
-			
-			// TODO IfJob <- action job needs to be passed in as well
-					
-			// Add the sequence
-			jobChainMap.put(JobChain.IN_SEQUENCE, jobSequence);
-			
-			// The job chain itself
-			JobDetail jobChain = newJob(JobChain.class).withIdentity("jobChain", SEMOSS_JOB_GROUP).usingJobData(jobChainMap).build();
-	
-			// Trigger the job chain
-			try {
-				String dateValue = trigger.getString("start-date");
-				Date date = new Date();
-				SimpleDateFormat format = new SimpleDateFormat(
-			            "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
-				format.setTimeZone(TimeZone.getTimeZone("UTC"));
-				if (dateValue != null) {
-					date = format.parse(dateValue);
-				}
-				Integer frequency = Integer.parseInt(trigger.getString("frequency"));
-				
-				Trigger jobTrigger = newTrigger()
-						.withIdentity("defaultTrigger")
-						.startAt(date)
-						.withSchedule(simpleSchedule()
-								.withIntervalInHours(frequency)
-								.repeatForever())
-						.build();
-				
-				Scheduler scheduler = getDefaultScheduler();
-				scheduler.start();
-				scheduler.scheduleJob(jobChain, jobTrigger);
-//				scheduler.triggerJob(jobChain.getKey());
-			} catch (SchedulerException e) {
-				e.printStackTrace();
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			String actionType = action.getString("actiontype");
-			System.out.println(actionType);
-									
-			if (actionType.equals(ACTION_TYPE_EMAIL)) {
-				JSONObject email = action.getJSONObject(ACTION_TYPE_EMAIL);
-				String emailAddress = email.getString("email-address");
-				System.out.println(emailAddress);
-			}
-			
-		} catch (JSONException e) {
-			e.printStackTrace();
-			LOGGER.error("Unable to find necessary key in " + fileName);
-			return;
-		}
+		/*
+		 * COMMENTING OUT THE LOGIC SINCE WE ARE SHIFTING TO RDBMS
+		 */
+//		// Get the json string from a file
+//		String jsonString;
+//		try {
+//			jsonString = new String(Files.readAllBytes(Paths.get(folderToWatch + "/" + fileName)), "UTF-8");
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//			LOGGER.error("Unable to read the file " + fileName);
+//			return;
+//		}
+//		
+//		// Parse the json string into an object
+//		JSONObject json;
+//		try {
+//			json = new JSONObject(jsonString);
+//		} catch (JSONException e) {
+//			e.printStackTrace();
+//			LOGGER.error("Unable to parse json object from " + fileName);
+//			return;
+//		}
+//		
+//		// Add data from the json object into the job data map
+//		try {
+//			JSONObject job = json.getJSONObject("schedule").getJSONObject("job");
+//			JSONObject values = job.getJSONObject("values");
+//			JSONObject action = job.getJSONObject("action");
+//			JSONObject trigger = json.getJSONObject("schedule").getJSONObject("trigger");
+//
+//			String rdbmsId = values.getString("rdbms-id");
+//			String engine = values.getString("engine");
+//			String returnColumn = values.getString("return-column");
+//			String compareColumn = values.getString("compare-column");
+//			String comparator = values.getString("comparator");
+//			String valueType = values.getString("value-type");
+//			String value = values.getString("value");
+////			String valuePkql = values.getString("value-pkql");
+//			
+//			// Setup the job chain
+//			JobDataMap jobChainMap = new JobDataMap();
+//			List<Class<? extends Job>> jobSequence = new ArrayList<Class<? extends Job>>();
+//			
+//			// WaitForEngineToLoadJob
+//			jobSequence.add(WaitForEngineToLoadJob.class);
+//			jobChainMap.put(WaitForEngineToLoadJob.IN_ENGINE_NAME_KEY, engine);
+//			
+//			// GetInsightJob
+//			jobSequence.add(GetInsightJob.class);
+//			jobChainMap.put(LinkedDataKeys.RDBMS_ID, rdbmsId);
+//			jobChainMap.put(GetInsightJob.IN_ENGINE_NAME_KEY, engine);
+//			
+//			// GetDataFromInsightJob
+//			jobSequence.add(GetDataFromInsightJob.class);
+//		
+//			// PrintDataToConsoleJob
+////			jobSequence.add(PrintDataToConsoleJob.class);
+//			
+//			//CheckCriteriaJob
+//			jobSequence.add(CheckCriteriaJob.class);
+//			jobChainMap.put(CheckCriteriaJob.IN_RETURN_COLUMN, returnColumn);
+//			jobChainMap.put(CheckCriteriaJob.IN_COMPARE_COLUMN, compareColumn);
+//			jobChainMap.put(CheckCriteriaJob.IN_COMPARATOR, comparator);
+//			jobChainMap.put(CheckCriteriaJob.IN_VALUE_TYPE, valueType);
+//			jobChainMap.put(CheckCriteriaJob.IN_VALUE, value);
+//			
+//			// TODO make the comparison
+//			
+//			// TODO IfJob <- action job needs to be passed in as well
+//					
+//			// Add the sequence
+//			jobChainMap.put(JobChain.IN_SEQUENCE, jobSequence);
+//			
+//			// The job chain itself
+//			JobDetail jobChain = newJob(JobChain.class).withIdentity("jobChain", SEMOSS_JOB_GROUP).usingJobData(jobChainMap).build();
+//	
+//			// Trigger the job chain
+//			try {
+//				String dateValue = trigger.getString("start-date");
+//				Date date = new Date();
+//				SimpleDateFormat format = new SimpleDateFormat(
+//			            "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+//				format.setTimeZone(TimeZone.getTimeZone("UTC"));
+//				if (dateValue != null) {
+//					date = format.parse(dateValue);
+//				}
+//				Integer frequency = Integer.parseInt(trigger.getString("frequency"));
+//				
+//				Trigger jobTrigger = newTrigger()
+//						.withIdentity("defaultTrigger")
+//						.startAt(date)
+//						.withSchedule(simpleSchedule()
+//								.withIntervalInHours(frequency)
+//								.repeatForever())
+//						.build();
+//				
+//				Scheduler scheduler = getDefaultScheduler();
+//				scheduler.start();
+//				scheduler.scheduleJob(jobChain, jobTrigger);
+////				scheduler.triggerJob(jobChain.getKey());
+//			} catch (SchedulerException e) {
+//				e.printStackTrace();
+//			} catch (ParseException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			
+//			String actionType = action.getString("actiontype");
+//			System.out.println(actionType);
+//									
+//			if (actionType.equals(ACTION_TYPE_EMAIL)) {
+//				JSONObject email = action.getJSONObject(ACTION_TYPE_EMAIL);
+//				String emailAddress = email.getString("email-address");
+//				System.out.println(emailAddress);
+//			}
+//			
+//		} catch (JSONException e) {
+//			e.printStackTrace();
+//			LOGGER.error("Unable to find necessary key in " + fileName);
+//			return;
+//		}
 				
 
 		// Construct the job chain
