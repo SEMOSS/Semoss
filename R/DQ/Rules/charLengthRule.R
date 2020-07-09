35b886801768c03f440520538bf08666e306ef9c
mahkhalil <mahkhalil@deloitte.com>
2020-07-09 17:39:27 +0000

    BUGFIX ::: Fix for character length not working
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@39385 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- R/DQ/Rules/charLengthRule.R
+++ R/DQ/Rules/charLengthRule.R
@@ -3,11 +3,12 @@ charLengthRule <- function(dt, rule) {
   ruleName <- c("Character Length")
   currCol <- rule$col
   numOptions <- rule$options
-  ruleName <- paste0(ruleName, " ", numOptions)
+  ruleName <- paste0(ruleName, " ", paste0(numOptions, collapse=", "))
   tempArray <- dt[, get(rule$col)]
   tempArray <- nchar(tempArray)
   ####### this can be altered to do <=, >=, !=, >, <
-  tempArray <- tempArray != numOptions
+  #tempArray <- tempArray != numOptions
+  tempArray <- unlist(lapply(tempArray, function(e){return (!(e %in% numOptions)) }))
   totLength <- length(tempArray)
   totCorrect <- totLength - sum(tempArray)
   totErrors <- totLength - totCorrect
