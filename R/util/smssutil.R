8543d0e1777c2585f326928f70b58f1d7e35a035
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2020-08-02 15:53:35 +0000

    Has Frame Changed Method for Smart Sync - R
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@39608 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- R/util/smssutil.R
+++ R/util/smssutil.R
@@ -31,3 +31,87 @@ canLoad2 <- function(file)
 	
 	return (unvlist)
 }	
+
+# get the latest 
+# if(exists variable then ask for the latest
+# do a compare 
+# if the compare returns true assign then 
+
+getCurMeta <- function(frame, allframe)
+{
+	key = deparse(substitute(frame))
+	cur_meta <- list()
+	
+	cur_cols <- colnames(frame)
+	for (col in cur_cols)
+	{
+		type <- class(frame[[col]])
+		cur_meta[[col]] <- type		
+	}	
+	#if (!exists('allframe')) 
+	if(!exists(paste0(key, "_meta_old"), allframe))
+	{
+		print("setting all frame")
+		allframe <- list()
+		allframe[[paste0(key, "_meta_new")]] <- cur_meta
+		allframe[[paste0(key, "_meta_old")]] <- cur_meta
+	}
+	else
+	{
+		print("adding new meta")
+		allframe[[paste0(key, "_meta_new")]] <- cur_meta
+	}
+	return (allframe)
+}
+
+hasFrameChanged <- function(key, allframe)
+{	
+	
+	cur_meta <- allframe[[paste0(key, "_meta_new")]]
+	prev_meta <- allframe[[paste0(key, "_meta_old")]]
+	cur_cols <- names(cur_meta)
+	# now we start comparing
+	changed <- FALSE
+	for (col in cur_cols)
+	{
+		type <- cur_meta[[col]]
+		col_exists <- exists(col, prev_meta)		
+		if(!col_exists)
+		{
+			print("mismatch : added column -> true")
+			allframe[[paste0(key, "_meta_old")]] <- cur_meta
+			changed <- TRUE
+			#allframes[[key]] <<- cur_meta
+			#print(col_compared)
+			break
+		}
+		newtype <- prev_meta[[col]]
+		if(is.null(newtype) || type != newtype)
+		{
+			print("mismatch : changed type -> true")
+			allframe[[paste0(key, "_meta_old")]] <- cur_meta
+			#allframes[[key]] <<- cur_meta
+			#return ("true")
+			break
+		}
+		
+	}	
+	# need to compare for removed columns as well 
+	prev_cols <- names(prev_meta)
+	if(!changed)
+	{
+		for (col in prev_cols)
+		{
+			#print("checking removed cols")		
+			if(!exists(col, cur_meta))
+			{
+				print("mismatch : removed column -> true")
+				allframe[[paste0(key, "_meta_old")]] <- cur_meta
+				#return ("true")
+				break
+			}
+		}
+	}
+	
+	return (allframe);
+}
