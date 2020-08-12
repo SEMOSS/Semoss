05821b5de1d44602f74a8f34b2641a4444346a72
Maher Khalil <themaherkhalil@gmail.com>
2020-08-12 18:08:58 +0000

    BUGFIX ::: Fixing the merge map to work properly
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@39711 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 5990cb19b9372631b9a35581036e859f8b1a662a

--- src/prerna/om/InsightPanel.java
+++ src/prerna/om/InsightPanel.java
@@ -300,12 +300,12 @@ public class InsightPanel {
 					} else {
 						// both are not maps
 						// just override
-						mainMap.putAll(newMap);
+						mainMap.put(key, newMap.get(key));
 					}
 				} else {
 					// brand new key
 					// put all into the main map
-					mainMap.putAll(newMap);
+					mainMap.put(key, newMap.get(key));
 				}
 			}
 		}
