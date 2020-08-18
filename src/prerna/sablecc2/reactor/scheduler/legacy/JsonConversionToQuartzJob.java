fd9d84d502a4c66ec7a581eb263c7e4b3933f6e6
mahkhalil <mahkhalil@deloitte.com>
2020-08-18 15:22:48 +0000

    UPDATE ::: Updating scheduler key to run via the end point instead of within the same thread
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@39753 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/scheduler/legacy/JsonConversionToQuartzJob.java
+++ src/prerna/sablecc2/reactor/scheduler/legacy/JsonConversionToQuartzJob.java
@@ -40,7 +40,6 @@ import prerna.sablecc2.reactor.scheduler.SchedulerH2DatabaseUtility;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
 import prerna.util.Utility;
-import prerna.util.sql.AbstractSqlQueryUtil;
 
 @Deprecated
 public class JsonConversionToQuartzJob {
@@ -207,16 +206,7 @@ public class JsonConversionToQuartzJob {
 					.withSchedule(CronScheduleBuilder.cronSchedule(cronExpression)).build();
 
 			// insert into SMSS_JOB_RECIPES table
-			Connection connection = null;
-			AbstractSqlQueryUtil queryUtil = null;
-			try {
-				connection = SchedulerH2DatabaseUtility.connectToSchedulerH2();
-				queryUtil = SchedulerH2DatabaseUtility.getQueryUtil();
-			} catch(Exception e) {
-				connection = connectToSchedulerH2(serverUrl);
-				queryUtil = SchedulerH2DatabaseUtility.getQueryUtil();
-			}
-			SchedulerH2DatabaseUtility.insertIntoJobRecipesTable(connection, queryUtil, userAccess, jobName, jobGroup, cronExpression, pixel, "Default", triggerOnLoad, parameters);
+			SchedulerH2DatabaseUtility.insertIntoJobRecipesTable(userAccess, jobName, jobGroup, cronExpression, pixel, "Default", triggerOnLoad, parameters);
 
 			if (active) {
 				scheduler.scheduleJob(job, trigger);
