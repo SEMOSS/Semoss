6eebdd33e399a14d7561ba424ee4e0ce6d8098ab
Maher Khalil <themaherkhalil@gmail.com>
2020-08-29 21:26:30 +0000

    UPDATE ::: Adding reactors for panel sorts to take both normal column sorting and custom orderings
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@39919 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3f9ac57c5f187ce669a513e9380c1a4f24e959dd

--- src/prerna/sablecc2/reactor/panel/sort/UnsortPanelReactor.java
+++ src/prerna/sablecc2/reactor/panel/sort/UnsortPanelReactor.java
@@ -5,6 +5,7 @@ import java.util.List;
 import prerna.om.InsightPanel;
 import prerna.query.querystruct.selectors.IQuerySort;
 import prerna.query.querystruct.selectors.QueryColumnOrderBySelector;
+import prerna.query.querystruct.selectors.QueryCustomOrderBy;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
@@ -13,15 +14,15 @@ import prerna.sablecc2.om.nounmeta.NounMetadata;
 public class UnsortPanelReactor extends AbstractPanelSortReactor {
 
 	public UnsortPanelReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.PANEL.getKey(), ReactorKeysEnum.COLUMNS.getKey(), ReactorKeysEnum.SORT.getKey()};
+		this.keysToGet = new String[]{ReactorKeysEnum.PANEL.getKey(), ReactorKeysEnum.COLUMNS.getKey()};
 	}
 
 	@Override
 	public NounMetadata execute() {
 		InsightPanel panel = getInsightPanel();
-		
+
 		// get the sort information
-		List<IQuerySort> sorts = getColumnSortBys();
+		List<String> sorts = getColumnsForSort();
 		if(sorts.isEmpty()) {
 			// we want to remove everything
 			panel.getPanelOrderBys().clear();
@@ -30,28 +31,35 @@ public class UnsortPanelReactor extends AbstractPanelSortReactor {
 			List<IQuerySort> curPanelSorts = panel.getPanelOrderBys();
 			int numExistingSorts = curPanelSorts.size();
 			if(numExistingSorts != 0) {
-				REMOVE_SORT_LOOP : for(IQuerySort removeThisSort : sorts) {
-					if(removeThisSort.getQuerySortType() == IQuerySort.QUERY_SORT_TYPE.COLUMN) {
-						String removeThisQsName = ((QueryColumnOrderBySelector) removeThisSort).getQueryStructName();
-						for(int i = 0; i < numExistingSorts; i++) {
-							IQuerySort curPanelSort = curPanelSorts.get(i);
-							if(curPanelSort.getQuerySortType() == IQuerySort.QUERY_SORT_TYPE.COLUMN) {
-								String existingQsName = ((QueryColumnOrderBySelector) curPanelSort).getQueryStructName();
-								if(existingQsName.equals(removeThisQsName)) {
-									// we found the one to remove
-									curPanelSorts.remove(i);
-									// update the number of loops for the next loop
-									numExistingSorts--;
-									// continue
-									continue REMOVE_SORT_LOOP;
-								}
+				REMOVE_SORT_LOOP : for(String removeThisQsName : sorts) {
+					for(int i = 0; i < numExistingSorts; i++) {
+						IQuerySort curPanelSort = curPanelSorts.get(i);
+						if(curPanelSort.getQuerySortType() == IQuerySort.QUERY_SORT_TYPE.COLUMN) {
+							String existingQsName = ((QueryColumnOrderBySelector) curPanelSort).getQueryStructName();
+							if(existingQsName.equals(removeThisQsName)) {
+								// we found the one to remove
+								curPanelSorts.remove(i);
+								// update the number of loops for the next loop
+								numExistingSorts--;
+								// continue
+								continue REMOVE_SORT_LOOP;
+							}
+						} else if(curPanelSort.getQuerySortType() == IQuerySort.QUERY_SORT_TYPE.CUSTOM) {
+							String existingQsName = ((QueryCustomOrderBy) curPanelSort).getColumnToSort().getQueryStructName();
+							if(existingQsName.equals(removeThisQsName)) {
+								// we found the one to remove
+								curPanelSorts.remove(i);
+								// update the number of loops for the next loop
+								numExistingSorts--;
+								// continue
+								continue REMOVE_SORT_LOOP;
 							}
 						}
 					}
 				}
 			}
 		}
-		
+
 		return new NounMetadata(true, PixelDataType.BOOLEAN, PixelOperationType.PANEL_SORT);
 	}
 }
