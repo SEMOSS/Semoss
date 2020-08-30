f26d017f120d7232306e5e8d8dc0fee7edeadec6
mahkhalil <mahkhalil@deloitte.com>
2020-08-30 14:47:44 +0000

    UPDATE ::: Adding security on the job reactors where user must be an admin or editor and above of the app they are in
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@39922 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/scheduler/ResumeJobTriggerReactor.java
+++ src/prerna/sablecc2/reactor/scheduler/ResumeJobTriggerReactor.java
@@ -10,6 +10,9 @@ import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.TriggerKey;
 
+import prerna.auth.User;
+import prerna.auth.utils.SecurityAdminUtils;
+import prerna.auth.utils.SecurityAppUtils;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
@@ -38,6 +41,14 @@ public class ResumeJobTriggerReactor extends AbstractReactor {
 		String jobName = this.keyValue.get(this.keysToGet[0]);
 		String jobGroup = this.keyValue.get(this.keysToGet[1]);
 
+		// the job group is the app the user is in
+		// user must be an admin or editor of the app
+		// to add a scheduled job
+		User user = this.insight.getUser();
+		if(!SecurityAdminUtils.userIsAdmin(user) && !SecurityAppUtils.userCanEditEngine(user, jobGroup)) {
+			throw new IllegalArgumentException("User does not have proper permissions to schedule jobs");
+		}
+		
 		// resume the job in quartz
 		// later grab cron expression and add functionality to resume specific trigger under job
 		try {
