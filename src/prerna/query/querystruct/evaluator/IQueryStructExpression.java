5b857c8e8cc230bdea2c6e2ef6fe83ff614a24cd
mahkhalil <mahkhalil@deloitte.com>
2020-09-03 02:53:28 +0000

    BUGFIX ::: Fixing errors with executing queries on graph frame BUGFIX ::: Deleting the owl position map after performing add to existing db methods
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@39965 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/query/querystruct/evaluator/IQueryStructExpression.java
+++ src/prerna/query/querystruct/evaluator/IQueryStructExpression.java
@@ -10,25 +10,27 @@ public interface IQueryStructExpression {
 
 	static IQueryStructExpression getExpression(String functionName) {
 		functionName = functionName.toLowerCase();
-		if(functionName.equals(QueryFunctionHelper.COUNT)) {
+		if(functionName.equalsIgnoreCase(QueryFunctionHelper.COUNT)) {
 			return new QueryCountExpression();
-		} else if(functionName.equals(QueryFunctionHelper.GROUP_CONCAT)) {
+		} else if(functionName.equalsIgnoreCase(QueryFunctionHelper.GROUP_CONCAT)) {
 			return new QueryGroupConcatExpression();
-		} else if(functionName.equals(QueryFunctionHelper.MAX)) {
+		} else if(functionName.equalsIgnoreCase(QueryFunctionHelper.MAX)) {
 			return new QueryMaxExpression();
-		} else if(functionName.equals(QueryFunctionHelper.MEAN)) {
+		} else if(functionName.equalsIgnoreCase(QueryFunctionHelper.MEAN) 
+				|| functionName.equalsIgnoreCase(QueryFunctionHelper.AVERAGE_1) 
+				|| functionName.equalsIgnoreCase(QueryFunctionHelper.AVERAGE_2)) {
 			return new QueryAverageExpression();
-		} else if(functionName.equals(QueryFunctionHelper.MEDIAN)) {
+		} else if(functionName.equalsIgnoreCase(QueryFunctionHelper.MEDIAN)) {
 			return new QueryMedianExpression();
-		} else if(functionName.equals(QueryFunctionHelper.MIN)) {
+		} else if(functionName.equalsIgnoreCase(QueryFunctionHelper.MIN)) {
 			return new QueryMinExpression();
-		} else if(functionName.equals(QueryFunctionHelper.STDEV_1)) {
+		} else if(functionName.equalsIgnoreCase(QueryFunctionHelper.STDEV_1)) {
 			return new QueryStandardDeviationExpression();
-		} else if(functionName.equals(QueryFunctionHelper.SUM)) {
+		} else if(functionName.equalsIgnoreCase(QueryFunctionHelper.SUM)) {
 			return new QuerySumExpression();
-		} else if(functionName.equals(QueryFunctionHelper.UNIQUE_COUNT)) {
+		} else if(functionName.equalsIgnoreCase(QueryFunctionHelper.UNIQUE_COUNT)) {
 			return new QueryUniqueCountExpression();
-		} else if(functionName.equals(QueryFunctionHelper.UNIQUE_GROUP_CONCAT)) {
+		} else if(functionName.equalsIgnoreCase(QueryFunctionHelper.UNIQUE_GROUP_CONCAT)) {
 			return new QueryUniqueGroupConcatExpression();
 		}
 		
