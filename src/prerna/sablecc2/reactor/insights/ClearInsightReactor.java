02952bbfcfca88df63a0809fdc7ecceba3e2ba30
mahkhalil <mahkhalil@deloitte.com>
2020-09-24 23:52:07 +0000

    UPDATE ::: Shifting methods into utility and also sending back the steps for the FE state during the ClearInsight reactor call
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40335 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/insights/ClearInsightReactor.java
+++ src/prerna/sablecc2/reactor/insights/ClearInsightReactor.java
@@ -1,5 +1,8 @@
 package prerna.sablecc2.reactor.insights;
 
+import java.util.List;
+
+import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.util.insight.InsightUtility;
@@ -7,14 +10,21 @@ import prerna.util.insight.InsightUtility;
 public class ClearInsightReactor extends AbstractReactor {
 
 	public ClearInsightReactor() {
-		this.keysToGet = new String[] {"suppress"};
+		this.keysToGet = new String[] {"suppress", InsightUtility.OUTPUT_TYPE};
 	}
 	
 	@Override
 	public NounMetadata execute() {
+		// first grab the FE state
 		organizeKeys();
 		boolean suppress = Boolean.parseBoolean(this.keyValue.get(this.keysToGet[0]));
+		String outputType = this.keyValue.get(this.keysToGet[1]);
+		if(outputType == null) {
+			outputType = InsightUtility.MAP_OUTPUT;
+		}
+		List<String> uiSteps = InsightUtility.getInsightUIStateSteps(this.insight, outputType);
 		NounMetadata newNoun = InsightUtility.clearInsight(this.insight, suppress);
+		newNoun.addAdditionalReturn(new NounMetadata(uiSteps, PixelDataType.CONST_STRING));
 		return newNoun;
 	}
 	
@@ -22,6 +32,8 @@ public class ClearInsightReactor extends AbstractReactor {
 	protected String getDescriptionForKey(String key) {
 		if(key.equals(this.keysToGet[0])) {
 			return "Suppress the opType to be the default OPERATION";
+		} else if(key.equals(InsightUtility.OUTPUT_TYPE)) {
+			return "The value to return - as a 'string' or 'map'";
 		}
 		return super.getDescriptionForKey(key);
 	}
