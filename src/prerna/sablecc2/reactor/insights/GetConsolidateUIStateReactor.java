69c9a0545025400baeaa75650a5c6f94b384d300
mahkhalil <mahkhalil@deloitte.com>
2020-09-24 23:52:07 +0000

    UPDATE ::: Shifting methods into utility and also sending back the steps for the FE state during the ClearInsight reactor call
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40335 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: bd85a9fa0a540c1deb87032dd0fc13dc09c7684a

--- src/prerna/sablecc2/reactor/insights/GetConsolidateUIStateReactor.java
+++ src/prerna/sablecc2/reactor/insights/GetConsolidateUIStateReactor.java
@@ -1,114 +1,54 @@
 package prerna.sablecc2.reactor.insights;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Vector;
 
-import com.google.gson.Gson;
-
-import prerna.om.InsightPanel;
-import prerna.om.InsightSheet;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
-import prerna.util.gson.InsightPanelAdapter;
-import prerna.util.gson.InsightSheetAdapter;
+import prerna.util.insight.InsightUtility;
 
 public class GetConsolidateUIStateReactor extends AbstractReactor {
 
-	public static final String OUTPUT_TYPE = "output";
-	public static final String MAP = "map";
-	public static final String STRING = "stirng";
+	public GetConsolidateUIStateReactor() {
+		this.keysToGet = new String[] {InsightUtility.OUTPUT_TYPE};
+	}
 	
 	@Override
 	public NounMetadata execute() {
-		Gson gson = new Gson();
-
-		List<String> pixelSteps = new Vector<String>();
 		String outputType = getOutput();
-
-		// we will be doing this for every sheet and every panel
-		for(String sheetId : this.insight.getInsightSheets().keySet()) {
-			pixelSteps.add("AddSheet(" + sheetId + ");");
-		}
-		for(String sheetId : this.insight.getInsightSheets().keySet()) {
-			// we will just serialize the insight sheet
-			InsightSheet sheet = this.insight.getInsightSheet(sheetId);
-			InsightSheetAdapter adapter = new InsightSheetAdapter();
-			String serialization = null;
-			try {
-				serialization = adapter.toJson(sheet);
-			} catch (IOException e) {
-				e.printStackTrace();
-				throw new IllegalArgumentException("Exeption occured generate the sheet state with error: " + e.getMessage());
-			}
-			
-			// turn the serialization into a Map object
-			if(MAP.equals(outputType)) {
-				HashMap<String, Object> json = gson.fromJson(serialization, HashMap.class);
-				pixelSteps.add("SetSheetState(" + gson.toJson(json) + ");");
-			} else {
-				pixelSteps.add("SetSheetState(\"" + serialization + "\");");
-			}
-		}
-		
-		// repeat for each panel
-		for(String panelId : this.insight.getInsightPanels().keySet()) {
-			pixelSteps.add("AddPanel(" + panelId + ");");
-		}
-		for(String panelId : this.insight.getInsightPanels().keySet()) {
-			InsightPanel panel = this.insight.getInsightPanel(panelId);
-			// we will just serialize the insight panel
-			InsightPanelAdapter adapter = new InsightPanelAdapter();
-			String serialization = null;
-			try {
-				serialization = adapter.toJson(panel);
-			} catch (IOException e) {
-				e.printStackTrace();
-				throw new IllegalArgumentException("Exeption occured generate the panel state with error: " + e.getMessage());
-			}
-			// turn the serialization into a Map object
-			if(MAP.equals(outputType)) {
-				HashMap<String, Object> json = gson.fromJson(serialization, HashMap.class);
-				pixelSteps.add("SetPanelState(" + gson.toJson(json) + ");");
-			} else {
-				pixelSteps.add("SetPanelState(\"" + serialization + "\");");
-			}
-		}
-		
+		List<String> pixelSteps = InsightUtility.getInsightUIStateSteps(this.insight, outputType);
 		return new NounMetadata(pixelSteps, PixelDataType.VECTOR);
 	}
 
 	@Override
 	protected String getDescriptionForKey(String key) {
-		if(key.equals(OUTPUT_TYPE)) {
+		if(key.equals(InsightUtility.OUTPUT_TYPE)) {
 			return "The value to return - as a 'string' or 'map'";
 		}
 		return super.getDescriptionForKey(key);
 	}
 	
 	private String getOutput() {
-		GenRowStruct grs = this.store.getNoun(OUTPUT_TYPE);
+		GenRowStruct grs = this.store.getNoun(InsightUtility.OUTPUT_TYPE);
 		if(grs != null && !grs.isEmpty()) {
 			String input = grs.get(0).toString();
-			if(input.equalsIgnoreCase(STRING)) {
-				return STRING;
+			if(input.equalsIgnoreCase(InsightUtility.STRING_OUTPUT)) {
+				return InsightUtility.STRING_OUTPUT;
 			} else {
-				return MAP;
+				return InsightUtility.MAP_OUTPUT;
 			}
 		}
 		
 		if(!this.curRow.isEmpty()) {
-			if(this.curRow.toString().equalsIgnoreCase(STRING)) {
-				return STRING;
+			if(this.curRow.toString().equalsIgnoreCase(InsightUtility.STRING_OUTPUT)) {
+				return InsightUtility.STRING_OUTPUT;
 			} else {
-				return MAP;
+				return InsightUtility.MAP_OUTPUT;
 			}
 		}
 		
-		return MAP;
+		return InsightUtility.MAP_OUTPUT;
 	}
 	
 }
