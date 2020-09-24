11c45f85d39c7f5b1875db0e97c8f755adb1cf06
mahkhalil <mahkhalil@deloitte.com>
2020-09-24 19:20:50 +0000

    UPDATE ::: Adding the ability to get and set the panel state in a single routine instead of splitting into individual pixel commands
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40326 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 43d293eae7af1844c6fc2a218255681fd0763956

---
+++ src/prerna/sablecc2/reactor/panel/SetPanelStateReactor.java
@@ -0,0 +1,85 @@
+package prerna.sablecc2.reactor.panel;
+
+import java.io.IOException;
+import java.util.List;
+
+import net.snowflake.client.jdbc.internal.google.gson.Gson;
+import prerna.om.InsightPanel;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.util.gson.InsightPanelAdapter;
+
+public class SetPanelStateReactor extends AbstractInsightPanelReactor {
+
+	/*
+	 * This class is complimentary to GetPanelStateReactor
+	 */
+	
+	private static final String STATE = "state";
+	
+	public SetPanelStateReactor() {
+		this.keysToGet = new String[] {STATE};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String serialized = getSerialization();
+		if(serialized == null) {
+			throw new NullPointerException("Serialization of the panel state is null");
+		}
+		// we will just read the serialization of the insight panel
+		InsightPanelAdapter adapter = new InsightPanelAdapter();
+		InsightPanel insightPanel = null;
+		try {
+			insightPanel = adapter.fromJson(serialized);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Exeption occured reading the panel state with error: " + e.getMessage());
+		}
+		
+		this.insight.getInsightPanels().put(insightPanel.getPanelId(), insightPanel);
+		NounMetadata noun = new NounMetadata(insightPanel, PixelDataType.PANEL, PixelOperationType.CACHED_PANEL);
+		return noun;
+	}
+
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if(key.equals(STATE)) {
+			return "The serialization for the insight panel";
+		}
+		return super.getDescriptionForKey(key);
+	}
+	
+	private String getSerialization() {
+		GenRowStruct grs = this.store.getNoun(STATE);
+		if(grs != null && !grs.isEmpty()) {
+			List<String> strInput = grs.getAllStrValues();
+			if(strInput != null && !strInput.isEmpty()) {
+				return strInput.get(0);
+			}
+			List<Object> mapInput = grs.getValuesOfType(PixelDataType.MAP);
+			if(mapInput != null && !mapInput.isEmpty()) {
+				Gson gson = new Gson();
+				return gson.toJson(mapInput.get(0));
+			}
+		}
+		
+		if(!this.curRow.isEmpty()) {
+			List<String> strInput = this.curRow.getAllStrValues();
+			if(strInput != null && !strInput.isEmpty()) {
+				return strInput.get(0);
+			}
+			List<Object> mapInput = this.curRow.getValuesOfType(PixelDataType.MAP);
+			if(mapInput != null && !mapInput.isEmpty()) {
+				Gson gson = new Gson();
+				return gson.toJson(mapInput.get(0));
+			}
+		}
+		
+		return null;
+	}
+	
+}
