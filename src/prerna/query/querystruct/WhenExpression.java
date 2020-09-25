3efd6f3a0bfd1fc47ce97ec687214ea5041167f2
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2020-09-25 16:08:29 +0000

    SQL Parser 2 first cut in terms of adding parameters.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40340 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 1a6a1b9c3b1efdd52e48bb49179df9553d526dec

--- src/prerna/query/querystruct/WhenExpression.java
+++ src/prerna/query/querystruct/WhenExpression.java
@@ -11,6 +11,10 @@ public class WhenExpression extends GenExpression {
 	List <String> whens = new Vector<String>();
 	List <String> thens = new Vector<String>();
 	
+	List <GenExpression> whenE = new Vector <GenExpression>();
+	List <GenExpression> thenE = new Vector <GenExpression>();
+	GenExpression elseE = null;
+	
 	public void addWhenThen(String when, String then)
 	{
 		whens.add(when);
@@ -22,7 +26,19 @@ public class WhenExpression extends GenExpression {
 		this.elseClause = elseClause;
 	}
 	
-	public StringBuffer printOutput()
+	public void addWhenThenE(GenExpression when, GenExpression then)
+	{
+		whenE.add(when);
+		thenE.add(then);
+
+	}
+	
+	public void setElseE(GenExpression elseE)
+	{
+		this.elseE = elseE;
+	}
+	
+	public StringBuffer printOutput2()
 	{
 		StringBuffer output = new StringBuffer();
 		
@@ -47,6 +63,45 @@ public class WhenExpression extends GenExpression {
 		
 		return output;
 	}
-	
+
+	public StringBuffer printOutput()
+	{
+		StringBuffer output = new StringBuffer();
+		
+		if(whens.size() > 0)
+		{
+			output.append("case ");
+			
+			for(int whenIndex = 0;whenIndex < whenE.size();whenIndex++)
+			{
+				GenExpression thisWhen = whenE.get(whenIndex);
+				StringBuffer whenBuf = new StringBuffer();
+				whenBuf = thisWhen.printQS(thisWhen, whenBuf);
+
+				GenExpression thisThen = thenE.get(whenIndex);
+				StringBuffer thenBuf = new StringBuffer();
+				thenBuf = thisThen.printQS(thisThen, thenBuf);
+
+				System.err.println("When ...  " + whenBuf);
+				System.err.println("Then ...  " + thenBuf);
+
+				output.append("when " ).append(whenBuf).append(" then ").append(thenBuf).append(" ");
+			}
+			
+			if(elseE != null)
+			{
+				StringBuffer elseBuf = new StringBuffer();
+				elseBuf = elseE.printQS(elseE, elseBuf);
+				output.append(" else ").append(elseBuf);
+			}
+			output.append(" end");
+			
+			if(this.leftAlias != null)
+				output.append(" AS " + this.leftAlias);
+		}
+		
+		return output;
+	}
+
 
 }
