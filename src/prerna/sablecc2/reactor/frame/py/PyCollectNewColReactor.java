c7207922c8c47c73c44bbad657531d38d5fbf9c0
mahkhalil <mahkhalil@deloitte.com>
2020-09-26 19:39:40 +0000

    BUGFIX ::: Fixing errors w/ formula builder not working when multiple frames present + using frame methods to rebuild metadata instead of copying existing code
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40365 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e1adb0b5ef0440980956b53167092bbcfe8f1c6e

--- src/prerna/sablecc2/reactor/frame/py/PyCollectNewColReactor.java
+++ src/prerna/sablecc2/reactor/frame/py/PyCollectNewColReactor.java
@@ -1,100 +1,88 @@
 package prerna.sablecc2.reactor.frame.py;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Vector;
 
-import prerna.algorithm.api.ITableDataFrame;
 import prerna.ds.OwlTemporalEngineMeta;
 import prerna.ds.py.PandasFrame;
-import prerna.ds.py.PandasSyntaxHelper;
 import prerna.ds.py.PyTranslator;
-import prerna.ds.r.RDataTable;
-import prerna.poi.main.HeadersException;
-import prerna.query.interpreters.RInterpreter;
 import prerna.query.querystruct.SelectQueryStruct;
 import prerna.query.querystruct.selectors.IQuerySelector;
+import prerna.query.querystruct.selectors.IQuerySelector.SELECTOR_TYPE;
 import prerna.query.querystruct.selectors.QueryArithmeticSelector;
 import prerna.query.querystruct.selectors.QueryColumnSelector;
 import prerna.query.querystruct.selectors.QueryConstantSelector;
-import prerna.query.querystruct.selectors.QueryFunctionSelector;
-import prerna.query.querystruct.selectors.IQuerySelector.SELECTOR_TYPE;
 import prerna.query.querystruct.transform.QSAliasToPhysicalConverter;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.om.task.BasicIteratorTask;
-import prerna.sablecc2.reactor.imports.ImportUtility;
 import prerna.sablecc2.reactor.task.TaskBuilderReactor;
 
-public class CollectNewColReactor extends TaskBuilderReactor {
+public class PyCollectNewColReactor extends TaskBuilderReactor {
 
 	/**
 	 * This class is responsible for collecting data from a task and returning it
 	 */
 
 	PyTranslator pyt = null;
-	
 
-	public CollectNewColReactor() {
-
+	public PyCollectNewColReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.QUERY_STRUCT.getKey() };
 	}
 
 	public NounMetadata execute() {
+		String warning = null;
 		
-		this.task = getTask();
-
-		PandasFrame frame = (PandasFrame)this.insight.getCurFrame();
-		pyt = insight.getPyTranslator();
+		if(! ((this.task=getTask()) instanceof BasicIteratorTask)) {
+			throw new IllegalArgumentException("Can only add a new column using a basic query on a frame");
+		}
 		
 		// get the query struct
-		SelectQueryStruct sqs = ((BasicIteratorTask)task).getQueryStruct();
-		String warning = null;
-
-		// need to get the query struct
-		// there should be only one selector
-		List <IQuerySelector> allSelectors = sqs.getSelectors();
-
+		SelectQueryStruct sqs = ((BasicIteratorTask) this.task).getQueryStruct();
+		PandasFrame frame = (PandasFrame) sqs.getFrame();
+		pyt = insight.getPyTranslator();
+		
 		OwlTemporalEngineMeta metadata = frame.getMetaData();
-		SelectQueryStruct pqs = QSAliasToPhysicalConverter.getPhysicalQs(sqs, metadata);
-		if(pqs.getCombinedFilters().getFilters() != null && pqs.getCombinedFilters().getFilters().size() > 0 )
-		{
+		SelectQueryStruct pqs = null;
+
+		try {
+			// if the columns are not there. 
+			pqs = QSAliasToPhysicalConverter.getPhysicalQs(sqs, metadata);
+		} catch(Exception ex) {
+			return getWarning("Frame is out of sync / No Such Column. Cannot perform this operation");
+		}
+
+		if(pqs.getCombinedFilters().getFilters() != null && pqs.getCombinedFilters().getFilters().size() > 0 ) {
 			warning = "You are applying a calculation while there are filters, this is not recommended and can lead to unpredictable results";
 			pqs.ignoreFilters = true;
-		}		
+		}
 
 		List<NounMetadata> outputs = new Vector<NounMetadata>();
-		
-		if(allSelectors.size() > 0)
-		{
+		// there should be only one selector
+		List <IQuerySelector> allSelectors = sqs.getSelectors();
+		if(allSelectors.size() > 0) {
 			IQuerySelector onlySelector = allSelectors.get(0);
 			String mainQuery = processSelector(onlySelector, frame.getName()).toString();
 			
 			String alias = onlySelector.getAlias();
 			mainQuery = frame.getName() + "['" + alias + "'] = " + mainQuery;
 			pyt.runEmptyPy(mainQuery);
-			
-			recreateMetadata(frame, true);
+
+			// recreate the metadata
+			frame.recreateMeta();
 			
 			outputs.add(new NounMetadata("Added Col " + alias, PixelDataType.CONST_STRING));
-			outputs.add(new NounMetadata(this.insight.getCurFrame(), PixelDataType.FRAME, PixelOperationType.FRAME_HEADERS_CHANGE));
-			if(warning != null)
+			outputs.add(new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_HEADERS_CHANGE));
+			if(warning != null) {
 				outputs.add(getWarning(warning));
-		}	
-		else
+			}
+		} else {
 			outputs.add(new NounMetadata("No New Columns to add", PixelDataType.CONST_STRING));
-
+		}
 
 		return new NounMetadata(outputs, PixelDataType.CODE, PixelOperationType.CODE_EXECUTION);
-	}
-	
-	public String convertQSToFormula(IQuerySelector selector)
-	{
-		String retString = null;
-		
-		
-		
-		return retString;
 	}
 
 	@Override
@@ -108,68 +96,6 @@ public class CollectNewColReactor extends TaskBuilderReactor {
 		return outputs;
 	}
 
-	@Override
-	protected void buildTask() {
-		// do nothing
-		
-	}
-	
-	/// recreating the metadata
-	protected ITableDataFrame recreateMetadata(PandasFrame frame, boolean override) {
-		// grab the existing metadata from the frame
-		Map<String, String> additionalDataTypes = frame.getMetaData().getHeaderToAdtlTypeMap();
-		Map<String, List<String>> sources = frame.getMetaData().getHeaderToSources();
-		
-		String frameName = frame.getName();
-		PandasFrame newFrame = frame; 
-		// I am just going to try to recreate the frame here
-		if(override) {
-			newFrame = new PandasFrame(frameName);
-			newFrame.setJep(frame.getJep());
-			newFrame.setTranslator(insight.getPyTranslator());
-			String makeWrapper = PandasSyntaxHelper.makeWrapper(newFrame.getWrapperName(), frameName);
-			//newFrame.runScript(makeWrapper);
-			insight.getPyTranslator().runEmptyPy(makeWrapper);
-		}
-		String[] colNames = getColumns(newFrame);
-		// I bet this is being done for pixel.. I will keep the same
-		//newFrame.runScript(PandasSyntaxHelper.cleanFrameHeaders(frameName, colNames));
-		insight.getPyTranslator().runEmptyPy(PandasSyntaxHelper.cleanFrameHeaders(frameName, colNames));
-		colNames = getColumns(newFrame);
-		
-		String[] colTypes = getColumnTypes(newFrame);
-		if(colNames == null || colTypes == null) {
-			throw new IllegalArgumentException("Please make sure the variable " + frameName + " exists and can be a valid data.table object");
-		}
-		
-		// create the pandas frame
-		// and set up everything else
-		ImportUtility.parseTableColumnsAndTypesToFlatTable(newFrame.getMetaData(), colNames, colTypes, frameName, additionalDataTypes, sources);
-		if (override) {
-			this.insight.setDataMaker(newFrame);
-		}
-		// update varStore
-		NounMetadata noun = new NounMetadata(newFrame, PixelDataType.FRAME);
-		this.insight.getVarStore().put(frame.getName(), noun);
-		
-		return newFrame;
-	}
-
-	public String[] getColumns(PandasFrame frame) {		
-		String wrapperName = frame.getWrapperName();
-		// get jep thread and get the names
-		List<String> val = (List<String>) insight.getPyTranslator().runScript("list(" + wrapperName + ".cache['data'])");
-		return val.toArray(new String[val.size()]);
-	}
-	
-	public String[] getColumnTypes(PandasFrame frame) {
-		String wrapperName = frame.getWrapperName();
-		// get jep thread and get the names
-		List<String> val = (List<String>) insight.getPyTranslator().runScript(PandasSyntaxHelper.getTypes(wrapperName + ".cache['data']"));
-		return val.toArray(new String[val.size()]);
-	}
-
-	
 	// compose the expression
 	// keep the expression here and build it
 	
@@ -221,6 +147,9 @@ public class CollectNewColReactor extends TaskBuilderReactor {
 		return new StringBuffer("(" + processSelector(leftSelector, tableName) + " " + mathExpr + " " + processSelector(rightSelector, tableName) + ")");
 	}
 
-
-
+	@Override
+	protected void buildTask() {
+		// do nothing
+		
+	}
 }
--- src/prerna/sablecc2/reactor/frame/r/RCollectNewColReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/RCollectNewColReactor.java
@@ -1,17 +1,10 @@
 package prerna.sablecc2.reactor.frame.r;
 
-import java.io.File;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Vector;
 
 import prerna.ds.OwlTemporalEngineMeta;
 import prerna.ds.r.RDataTable;
-import prerna.ds.r.RSyntaxHelper;
-import prerna.poi.main.HeadersException;
 import prerna.query.interpreters.RInterpreter;
 import prerna.query.querystruct.SelectQueryStruct;
 import prerna.query.querystruct.selectors.IQuerySelector;
@@ -21,13 +14,10 @@ import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.om.task.BasicIteratorTask;
-import prerna.sablecc2.om.task.ConstantDataTask;
 import prerna.sablecc2.reactor.frame.r.util.AbstractRJavaTranslator;
-import prerna.sablecc2.reactor.imports.ImportUtility;
 import prerna.sablecc2.reactor.task.TaskBuilderReactor;
-import prerna.util.Utility;
 
-public class CollectNewColReactor extends TaskBuilderReactor {
+public class RCollectNewColReactor extends TaskBuilderReactor {
 
 	/**
 	 * This class is responsible for collecting data from a task and returning it
@@ -35,31 +25,30 @@ public class CollectNewColReactor extends TaskBuilderReactor {
 
 	AbstractRJavaTranslator rJavaTranslator = null;
 	
-	public CollectNewColReactor() {
+	public RCollectNewColReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.QUERY_STRUCT.getKey() };
 	}
 
 	public NounMetadata execute() {
-		
-		this.task = getTask();
+		String warning = null;
 
-		RDataTable frame = (RDataTable)this.insight.getCurFrame();
+		if(! ((this.task=getTask()) instanceof BasicIteratorTask)) {
+			throw new IllegalArgumentException("Can only add a new column using a basic query on a frame");
+		}
+		
+		// get the query struct
+		SelectQueryStruct sqs = ((BasicIteratorTask) this.task).getQueryStruct();
+		RDataTable frame = (RDataTable) sqs.getFrame();
 		rJavaTranslator = this.insight.getRJavaTranslator(this.getLogger(this.getClass().getName()));
 		rJavaTranslator.startR(); 
 		
-		// get the query struct
-		SelectQueryStruct sqs = ((BasicIteratorTask)task).getQueryStruct();
-		String warning = null;
-		
-
 		OwlTemporalEngineMeta metadata = frame.getMetaData();
 		SelectQueryStruct pqs = null;
 		
-		try
-		{
+		try {
 			// if the columns are not there. 
 			pqs = QSAliasToPhysicalConverter.getPhysicalQs(sqs, metadata);
-		}catch(Exception ex)
-		{
+		} catch(Exception ex) {
 			return getWarning("Frame is out of sync / No Such Column. Cannot perform this operation");
 		}
 		
@@ -68,11 +57,11 @@ public class CollectNewColReactor extends TaskBuilderReactor {
 			warning = "You are applying a calculation while there are filters, this is not recommended and can lead to unpredictable results";
 			pqs.ignoreFilters = true;
 		}
+		
 		RInterpreter interp = new RInterpreter();
 		interp.setQueryStruct(pqs);
 		interp.setDataTableName(frame.getName());
 		interp.setColDataTypes(metadata.getHeaderToTypeMap());
-//		interp.setAdditionalTypes(this.metaData.getHeaderToAdtlTypeMap());
 		interp.setLogger(this.getLogger(this.getClass().getName()));
 		String query = interp.composeQuery();
 		
@@ -92,26 +81,25 @@ public class CollectNewColReactor extends TaskBuilderReactor {
 
 		List<NounMetadata> outputs = new Vector<NounMetadata>();
 		
-		if(allSelectors.size() > 0)
-		{
+		if(allSelectors.size() > 0) {
 			IQuerySelector onlySelector = allSelectors.get(0);
 			
 			String alias = onlySelector.getAlias();
 			mainQuery = frame.getName() + "$" + alias + "  <- " + mainQuery;
 			rJavaTranslator.executeEmptyR(mainQuery);
 			
-			OwlTemporalEngineMeta meta =  genNewMetaFromVariable(frame.getName());
-			
-			// replace the meta in the R Data table
-			((RDataTable)this.insight.getCurFrame()).setMetaData(meta);
-			outputs.add(new NounMetadata("Added Col " + alias, PixelDataType.CONST_STRING));
-			outputs.add(new NounMetadata(this.insight.getCurFrame(), PixelDataType.FRAME, PixelOperationType.FRAME_HEADERS_CHANGE));
-			if(warning != null)
-				outputs.add(getWarning(warning));
-		}	
-		else
-			outputs.add(new NounMetadata("No New Columns to add", PixelDataType.CONST_STRING));
+			// recreate the frame metadata
+			frame.recreateMeta();
 
+			// return the outputs
+			outputs.add(new NounMetadata("Added Col " + alias, PixelDataType.CONST_STRING));
+			outputs.add(new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_HEADERS_CHANGE));
+			if(warning != null) {
+				outputs.add(getWarning(warning));
+			}
+		} else {
+			outputs.add(new NounMetadata("No New Columns to add", PixelDataType.CONST_STRING));
+		}
 
 		return new NounMetadata(outputs, PixelDataType.CODE, PixelOperationType.CODE_EXECUTION);
 	}
@@ -132,46 +120,4 @@ public class CollectNewColReactor extends TaskBuilderReactor {
 		// do nothing
 		
 	}
-	
-	protected OwlTemporalEngineMeta genNewMetaFromVariable(String frameName) {
-		// recreate a new frame and set the frame name
-		String[] colNames = getColumns(frameName);
-		String[] colTypes = getColumnTypes(frameName);
-		//clean headers
-		HeadersException headerChecker = HeadersException.getInstance();
-		colNames = headerChecker.getCleanHeaders(colNames);
-		// update frame header names in R
-		String rColNames = "";
-		for (int i = 0; i < colNames.length; i++) {
-			rColNames += "\"" + colNames[i] + "\"";
-			if (i < colNames.length - 1) {
-				rColNames += ", ";
-			}
-		}
-		String script = "colnames(" + frameName + ") <- c(" + rColNames + ")";
-		this.rJavaTranslator.executeEmptyR(script);
-		
-		OwlTemporalEngineMeta meta = new OwlTemporalEngineMeta();
-		ImportUtility.parseTableColumnsAndTypesToFlatTable(meta, colNames, colTypes, frameName);
-		return meta;
-	}
-	
-	/**
-	 * This method is used to get the column names of a frame
-	 * @param frameName
-	 */
-	public String[] getColumns(String frameName) {
-		return this.rJavaTranslator.getColumns(frameName);
-	}
-
-	/**
-	 * This method is used to get the column types of a frame
-	 * @param frameName
-	 */
-	public String[] getColumnTypes(String frameName) {
-		return this.rJavaTranslator.getColumnTypes(frameName);
-	}
-
-
-
 }
