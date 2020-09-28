2709c75bc315ea28aa54e3d4594621031cce8a1b
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2020-09-28 18:17:11 +0000

    Updates so if the python is enabled. CollectPivot will always pivot to Python if python is enabled.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40375 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/r/CollectPivotReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/CollectPivotReactor.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
+import prerna.ds.py.PyUtils;
 import prerna.ds.r.RSyntaxHelper;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
@@ -41,142 +42,157 @@ public class CollectPivotReactor extends TaskBuilderReactor {
 	}
 
 	public NounMetadata execute() {
-		this.task = getTask();
-
-		AbstractRJavaTranslator rJavaTranslator = this.insight.getRJavaTranslator(this.getLogger(this.getClass().getName()));
-		rJavaTranslator.startR(); 
-
-		// going to do this with r datatable directly
-		//  cubed <- data.table::cube(mv, .(budget=sum(MovieBudget), revenue=mean(RevenueDomestic)), by=c('Genre', 'Studio'))
 		
-
-		String fileName = Utility.getRandomString(6);
-		String dir = (insight.getUserFolder() + "/Temp").replace('\\', '/');
-		File tempDir = new File(dir);
-		if(!tempDir.exists()) {
-			tempDir.mkdir();
-		}
-		String outputFile = dir + "/" + fileName + ".csv";
-		Utility.writeResultToFile(outputFile, this.task, ",");
-
-		// so this is going to come in as vectors
-		List<String> rowGroups = this.store.getNoun(keysToGet[0]).getAllStrValues();
-		List<String> colGroups = this.store.getNoun(keysToGet[1]).getAllStrValues();
-		List<String> values = this.store.getNoun(keysToGet[2]).getAllStrValues();
-
-		// convert the inputs into a cgroup
-		String rows = "by = " + RSyntaxHelper.createStringRColVec(rowGroups);
-		
-		rows = rows.substring(0, rows.length()-1);
-		
-		// we need to add this to the rows
-		// that is how r data table works
-		for(int colIndex = 0;colIndex < colGroups.size();colIndex++)
-			rows = rows + ", \"" + colGroups.get(colIndex) + "\"";
-		
-		rows = rows + ")";
-		
-
-		// last piece is the calculations
-		// not putting headers right now
-		List<Map<String, String>> valuesList = new Vector<Map<String, String>>();
-
-		StringBuilder calcs = new StringBuilder(".(");
-		for(int calcIndex = 0; calcIndex < values.size(); calcIndex++) {
-			Map<String, String> valueMap = new HashMap<String, String>();
-			String curValue = values.get(calcIndex);
-			
-			// get the operator and selector
-			//String [] composite = curValue.split("(");
-			String operator = curValue.substring(0, curValue.indexOf("(")).trim();
-			String operand = curValue.substring(curValue.indexOf("(") + 1, curValue.length()-1).trim();
-			
-			
-			if(calcIndex != 0)
-				calcs.append(",");
-			
-			for (Map.Entry<String, String> mathElement : mathMap.entrySet()) {
-				String key = (String) mathElement.getKey();
-				String value = (String) mathElement.getValue();
-
-				operator = operator.replace(key, value);
-			}
-
-			//budget=sum(MovieBudget)
-			calcs.append(operand).append("_").append(operator).append("=");
-			calcs.append(operator).append("(as.double(").append(operand).append("))");
-						
-			valueMap.put("alias", operand);
-			valueMap.put("math", operator);
-			valuesList.add(valueMap);
-		}
-		calcs.append(")");
-
-		String pivotName = "pivot" + Utility.getRandomString(5);
-		String htmlName = pivotName + ".html";
-
-		// load html
-		StringBuilder pivoter = new StringBuilder("library(xtable);");
-		pivoter.append(RSyntaxHelper.getFReadSyntax(fileName, outputFile, ","));
-		pivoter.append(pivotName + " <- data.table::cube(" + fileName + "," + calcs + "," + rows + ");");
-
-		// make the pivot
-		rJavaTranslator.runR(pivoter.toString());
-		// get the output
-		String htmler = "print(xtable(" + pivotName + "), type=\"html\");";
-		String htmlOutput = rJavaTranslator.runRAndReturnOutput(htmler);
-
-		// delete the variable and pivot
-		rJavaTranslator.runR("rm(" + pivotName + "," + fileName + ");");
-		File outputF = new File(outputFile);
-		outputF.delete();
-
-		// need to create a pivot map for the FE
-		Map<String, Object> pivotMap = new HashMap<String, Object>();
-		pivotMap.put(keysToGet[0], rowGroups);
-		pivotMap.put(keysToGet[1], colGroups);
-		pivotMap.put(keysToGet[2], valuesList);
-		
-		ConstantDataTask cdt = new ConstantDataTask();
-		// need to do all the sets
-		cdt.setFormat("TABLE");
-		cdt.setTaskOptions(task.getTaskOptions());
-		cdt.setHeaderInfo(task.getHeaderInfo());
-		cdt.setSortInfo(task.getSortInfo());
-		cdt.setId(task.getId());
-		Map<String, Object> formatMap = new Hashtable<String, Object>();
-		formatMap.put("type", "TABLE");
-		cdt.setFormatMap(formatMap);
-		
-		// set the output so it can give it
-		Map<String, Object> outputMap = new HashMap<String, Object>();
-		outputMap.put("headers", new String[] {});
-		outputMap.put("rawHeaders", new String[] {});
-		outputMap.put("values", new String[] {htmlOutput});
-		outputMap.put("pivotData", pivotMap);
-		cdt.setOutputData(outputMap);
-				
-		// need to set the task options
-		// hopefully this is the current one I am working with
-		if(this.task.getTaskOptions() != null)
+		if(PyUtils.pyEnabled()) 
 		{
-			prerna.query.querystruct.SelectQueryStruct sqs = (prerna.query.querystruct.SelectQueryStruct)((BasicIteratorTask)task).getQueryStruct();
-			// I really hope this is only one
-			Iterator <String> panelIds = task.getTaskOptions().getPanelIds().iterator();
-			while(panelIds.hasNext()) {
-				String panelId = panelIds.next();
-				// this is a bit silly
-				// but will set the formatter into the task options
-				// so if we pull the task options we have that information
-				// this is for {{@link RefreshPanelTaskReactor}}
-				task.getTaskOptions().setFormatter(task.getFormatter());
-				task.getTaskOptions().getOptions().put("values", values);
-				this.insight.setFinalViewOptions(panelId, sqs, task.getTaskOptions());
-			}
+	
+			// move this guy to py
+			prerna.sablecc2.reactor.frame.py.CollectPivotReactor pyCollect = new prerna.sablecc2.reactor.frame.py.CollectPivotReactor();
+			pyCollect.In();
+			pyCollect.setInsight(insight);
+			pyCollect.setNounStore(this.getNounStore());
+			
+			return pyCollect.execute();
 		}
-
-		
-		return new NounMetadata(cdt, PixelDataType.FORMATTED_DATA_SET, PixelOperationType.TASK_DATA);
+		else
+		{
+			this.task = getTask();
+	
+			AbstractRJavaTranslator rJavaTranslator = this.insight.getRJavaTranslator(this.getLogger(this.getClass().getName()));
+			rJavaTranslator.startR(); 
+	
+			// going to do this with r datatable directly
+			//  cubed <- data.table::cube(mv, .(budget=sum(MovieBudget), revenue=mean(RevenueDomestic)), by=c('Genre', 'Studio'))
+			
+	
+			String fileName = Utility.getRandomString(6);
+			String dir = (insight.getUserFolder() + "/Temp").replace('\\', '/');
+			File tempDir = new File(dir);
+			if(!tempDir.exists()) {
+				tempDir.mkdir();
+			}
+			String outputFile = dir + "/" + fileName + ".csv";
+			Utility.writeResultToFile(outputFile, this.task, ",");
+	
+			// so this is going to come in as vectors
+			List<String> rowGroups = this.store.getNoun(keysToGet[0]).getAllStrValues();
+			List<String> colGroups = this.store.getNoun(keysToGet[1]).getAllStrValues();
+			List<String> values = this.store.getNoun(keysToGet[2]).getAllStrValues();
+	
+			// convert the inputs into a cgroup
+			String rows = "by = " + RSyntaxHelper.createStringRColVec(rowGroups);
+			
+			rows = rows.substring(0, rows.length()-1);
+			
+			// we need to add this to the rows
+			// that is how r data table works
+			for(int colIndex = 0;colIndex < colGroups.size();colIndex++)
+				rows = rows + ", \"" + colGroups.get(colIndex) + "\"";
+			
+			rows = rows + ")";
+			
+	
+			// last piece is the calculations
+			// not putting headers right now
+			List<Map<String, String>> valuesList = new Vector<Map<String, String>>();
+	
+			StringBuilder calcs = new StringBuilder(".(");
+			for(int calcIndex = 0; calcIndex < values.size(); calcIndex++) {
+				Map<String, String> valueMap = new HashMap<String, String>();
+				String curValue = values.get(calcIndex);
+				
+				// get the operator and selector
+				//String [] composite = curValue.split("(");
+				String operator = curValue.substring(0, curValue.indexOf("(")).trim();
+				String operand = curValue.substring(curValue.indexOf("(") + 1, curValue.length()-1).trim();
+				
+				
+				if(calcIndex != 0)
+					calcs.append(",");
+				
+				for (Map.Entry<String, String> mathElement : mathMap.entrySet()) {
+					String key = (String) mathElement.getKey();
+					String value = (String) mathElement.getValue();
+	
+					operator = operator.replace(key, value);
+				}
+	
+				//budget=sum(MovieBudget)
+				calcs.append(operand).append("_").append(operator).append("=");
+				calcs.append(operator).append("(as.double(").append(operand).append("))");
+							
+				valueMap.put("alias", operand);
+				valueMap.put("math", operator);
+				valuesList.add(valueMap);
+			}
+			calcs.append(")");
+	
+			String pivotName = "pivot" + Utility.getRandomString(5);
+			String htmlName = pivotName + ".html";
+	
+			// load html
+			StringBuilder pivoter = new StringBuilder("library(xtable);");
+			pivoter.append(RSyntaxHelper.getFReadSyntax(fileName, outputFile, ","));
+			pivoter.append(pivotName + " <- data.table::cube(" + fileName + "," + calcs + "," + rows + ");");
+	
+			// make the pivot
+			rJavaTranslator.runR(pivoter.toString());
+			// get the output
+			String htmler = "print(xtable(" + pivotName + "), type=\"html\");";
+			String htmlOutput = rJavaTranslator.runRAndReturnOutput(htmler);
+	
+			// delete the variable and pivot
+			rJavaTranslator.runR("rm(" + pivotName + "," + fileName + ");");
+			File outputF = new File(outputFile);
+			outputF.delete();
+	
+			// need to create a pivot map for the FE
+			Map<String, Object> pivotMap = new HashMap<String, Object>();
+			pivotMap.put(keysToGet[0], rowGroups);
+			pivotMap.put(keysToGet[1], colGroups);
+			pivotMap.put(keysToGet[2], valuesList);
+			
+			ConstantDataTask cdt = new ConstantDataTask();
+			// need to do all the sets
+			cdt.setFormat("TABLE");
+			cdt.setTaskOptions(task.getTaskOptions());
+			cdt.setHeaderInfo(task.getHeaderInfo());
+			cdt.setSortInfo(task.getSortInfo());
+			cdt.setId(task.getId());
+			Map<String, Object> formatMap = new Hashtable<String, Object>();
+			formatMap.put("type", "TABLE");
+			cdt.setFormatMap(formatMap);
+			
+			// set the output so it can give it
+			Map<String, Object> outputMap = new HashMap<String, Object>();
+			outputMap.put("headers", new String[] {});
+			outputMap.put("rawHeaders", new String[] {});
+			outputMap.put("values", new String[] {htmlOutput});
+			outputMap.put("pivotData", pivotMap);
+			cdt.setOutputData(outputMap);
+					
+			// need to set the task options
+			// hopefully this is the current one I am working with
+			if(this.task.getTaskOptions() != null)
+			{
+				prerna.query.querystruct.SelectQueryStruct sqs = (prerna.query.querystruct.SelectQueryStruct)((BasicIteratorTask)task).getQueryStruct();
+				// I really hope this is only one
+				Iterator <String> panelIds = task.getTaskOptions().getPanelIds().iterator();
+				while(panelIds.hasNext()) {
+					String panelId = panelIds.next();
+					// this is a bit silly
+					// but will set the formatter into the task options
+					// so if we pull the task options we have that information
+					// this is for {{@link RefreshPanelTaskReactor}}
+					task.getTaskOptions().setFormatter(task.getFormatter());
+					task.getTaskOptions().getOptions().put("values", values);
+					this.insight.setFinalViewOptions(panelId, sqs, task.getTaskOptions());
+				}
+			}
+	
+			
+			return new NounMetadata(cdt, PixelDataType.FORMATTED_DATA_SET, PixelOperationType.TASK_DATA);
+		}
 	}
 
 	@Override
