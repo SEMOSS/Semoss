3f6853d99a57bb9663be6bdc5252c1e641e6d689
kunalppatel9 <kunalppatel9@deloitte.com>
2020-09-30 18:54:27 +0000

    UPDATE ::: Adding in a cassandra query interpreter
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40419 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 53b67c354c5727abc2480f93a617f7b3b7e6dd8a

--- src/prerna/util/sql/CassandraQueryUtil.java
+++ src/prerna/util/sql/CassandraQueryUtil.java
@@ -3,6 +3,12 @@ package prerna.util.sql;
 import java.util.Map;
 import java.util.Properties;
 
+import prerna.algorithm.api.ITableDataFrame;
+import prerna.engine.api.IEngine;
+import prerna.query.interpreters.IQueryInterpreter;
+import prerna.query.interpreters.sql.CassandraSqlInterpreter;
+import prerna.query.interpreters.sql.ImpalaSqlInterpreter;
+
 public class CassandraQueryUtil extends AnsiSqlQueryUtil {
 
 	CassandraQueryUtil() {
@@ -13,6 +19,16 @@ public class CassandraQueryUtil extends AnsiSqlQueryUtil {
 	CassandraQueryUtil(String connectionUrl, String username, String password) {
 		super(connectionUrl, username, password);
 		setDbType(RdbmsTypeEnum.CASSANDRA);
+	}
+	
+	@Override
+	public CassandraSqlInterpreter getInterpreter(IEngine engine) {
+		return new CassandraSqlInterpreter(engine);
+	}
+
+	@Override
+	public CassandraSqlInterpreter getInterpreter(ITableDataFrame frame) {
+		return new CassandraSqlInterpreter(frame);
 	}
 	
 	@Override
