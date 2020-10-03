bb02bdcad39621c25b26d5c620034983d4e0f56c
mahkhalil <mahkhalil@deloitte.com>
2020-10-03 22:20:15 +0000

    UPDATE ::: Sending order by in object format instead of jsonified string within the json
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40463 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: bf86710978a0cc63d94a439cbdde532dac6034d9

--- src/prerna/query/querystruct/selectors/IQuerySort.java
+++ src/prerna/query/querystruct/selectors/IQuerySort.java
@@ -1,11 +1,43 @@
 package prerna.query.querystruct.selectors;
 
+import com.google.gson.TypeAdapter;
+
+import prerna.util.gson.QueryColumnOrderBySelectorAdapter;
+import prerna.util.gson.QueryCustomOrderByAdapter;
+
 public interface IQuerySort {
 
-	// this is a dummy interface
-	
 	public enum QUERY_SORT_TYPE {COLUMN, CUSTOM};
 
 	QUERY_SORT_TYPE getQuerySortType();
+
+	/**
+	 * Get the adapter for the sort type
+	 * @param type
+	 * @return
+	 */
+	static TypeAdapter getAdapterForSort(QUERY_SORT_TYPE type) {
+		if(type == QUERY_SORT_TYPE.COLUMN) {
+			return new QueryColumnOrderBySelectorAdapter();
+		} else if(type == QUERY_SORT_TYPE.CUSTOM) {
+			return new QueryCustomOrderByAdapter();
+		}
+		
+		return null;
+	}
 	
+	/**
+	 * Convert string to SELECTOR_TYPE
+	 * @param s
+	 * @return
+	 */
+	static QUERY_SORT_TYPE convertStringToSortType(String s) {
+		if(s.equals(QUERY_SORT_TYPE.COLUMN.toString())) {
+			return QUERY_SORT_TYPE.COLUMN;
+		} else if(s.equals(QUERY_SORT_TYPE.CUSTOM.toString())) {
+			return QUERY_SORT_TYPE.CUSTOM;
+		}
+		
+		return null;
+	}
 }
