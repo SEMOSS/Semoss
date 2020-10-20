f1949b716b19af6456abe89baa8897ebcd160b95
mahkhalil <mahkhalil@deloitte.com>
2020-10-20 23:32:26 +0000

    UPDATE ::: Adding ability to edit the pixel recipe UPDATE ::: Modifying the GetCurrentRecipeReactor to return both the pixel string and the pixel id
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40705 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/insights/GetCurrentRecipeReactor.java
+++ src/prerna/sablecc2/reactor/insights/GetCurrentRecipeReactor.java
@@ -1,7 +1,12 @@
 package prerna.sablecc2.reactor.insights;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import prerna.om.Pixel;
+import prerna.om.PixelList;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
@@ -11,14 +16,18 @@ public class GetCurrentRecipeReactor extends AbstractReactor {
 
 	@Override
 	public NounMetadata execute() {
-		List<String> pixelRecipe = this.insight.getPixelList().getPixelRecipe();
-		StringBuilder recipe = new StringBuilder();
+		PixelList pixelList = this.insight.getPixelList();
 		
+		List<Map<String, String>> retList = new ArrayList<>();
 		// Don't add GetCurrentRecipe to the end
-		for (int i = 0; i < pixelRecipe.size() - 1; i++) {
-			recipe.append(pixelRecipe.get(i));
+		for (int i = 0; i < pixelList.size() - 1; i++) {
+			Pixel p = pixelList.get(i);
+			Map<String, String> innerMap = new HashMap<>();
+			innerMap.put("id", p.getUid());
+			innerMap.put("expression", p.getPixelString());
+			retList.add(innerMap);
 		}
-		return new NounMetadata(recipe.toString(), PixelDataType.CONST_STRING, PixelOperationType.CURRENT_INSIGHT_RECIPE);
+		return new NounMetadata(retList, PixelDataType.MAP, PixelOperationType.CURRENT_INSIGHT_RECIPE);
 	}
 
 }
