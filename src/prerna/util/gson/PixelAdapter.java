65c3d667c95a4c3c6b5178342d99f9a620470c73
mahkhalil <mahkhalil@deloitte.com>
2020-10-21 19:29:35 +0000

    UPDATE ::: Adding additional metadata to store the starting and ending headers for all the frames at each pixel recipe step
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40728 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/util/gson/PixelAdapter.java
@@ -0,0 +1,88 @@
+package prerna.util.gson;
+
+import java.io.IOException;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+import prerna.om.Pixel;
+
+public class PixelAdapter  extends TypeAdapter<Pixel> {
+	
+	private static final Gson GSON = GsonUtility.getDefaultGson();
+
+	@Override
+	public Pixel read(JsonReader in) throws IOException {
+		if (in.peek() == JsonToken.NULL) {
+			in.nextNull();
+			return null;
+		}
+
+		String id = null;
+		String pixelString = null;
+		Map<String, Map<String, Object>> startingFrameHeaders = null;
+		Map<String, Map<String, Object>> endingFrameHeaders = null;
+		
+		in.beginObject();
+		while(in.hasNext()) {
+			String key = in.nextName();
+			if(in.peek() == JsonToken.NULL) {
+				in.nextNull();
+				continue;
+			}
+			
+			if(key.equals("id")) {
+				id = in.nextString();
+			} else if(key.equals("pixelString")) {
+				pixelString = in.nextString();
+			} else if(key.equals("startingFrameHeaders")) {
+				TypeAdapter adapter = GSON.getAdapter(Map.class);
+				startingFrameHeaders = (Map<String, Map<String, Object>>) adapter.read(in);
+			} else if(key.equals("endingFrameHeaders")) {
+				TypeAdapter adapter = GSON.getAdapter(Map.class);
+				endingFrameHeaders = (Map<String, Map<String, Object>>) adapter.read(in);
+			}
+		}
+		in.endObject();
+
+		Pixel pixel = new Pixel(id, pixelString);
+		pixel.setStartingFrameHeaders(startingFrameHeaders);
+		pixel.setEndingFrameHeaders(endingFrameHeaders);
+		return pixel;
+	}
+	
+	@Override
+	public void write(JsonWriter out, Pixel value) throws IOException {
+		if (value == null) {
+			out.nullValue();
+			return;
+		}
+
+		out.beginObject();
+		out.name("id");
+		out.value(value.getId());
+		out.name("pixelString");
+		out.value(value.getPixelString());
+		out.name("startingFrameHeaders");
+		if(value.getStartingFrameHeaders() != null) {
+			TypeAdapter adapter = GSON.getAdapter(value.getStartingFrameHeaders().getClass());
+			adapter.write(out, value.getStartingFrameHeaders());
+		} else {
+			out.nullValue();
+		}
+		out.name("endingFrameHeaders");
+		if(value.getEndingFrameHeaders() != null) {
+			TypeAdapter adapter = GSON.getAdapter(value.getEndingFrameHeaders().getClass());
+			adapter.write(out, value.getEndingFrameHeaders());
+		} else {
+			out.nullValue();
+		}
+		
+		out.endObject();
+	}
+	
+}
