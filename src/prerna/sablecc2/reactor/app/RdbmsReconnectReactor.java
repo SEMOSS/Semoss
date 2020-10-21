85ab8944e987af6adc25cb99dc6eb9611455255d
mahkhalil <mahkhalil@deloitte.com>
2020-10-21 21:52:23 +0000

    UPDATE ::: Adding reactor to close and restart the connection to a RDBMS engine
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40739 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f6f88600cc2bab018296c24c10b6dbe9454654a2

---
+++ src/prerna/sablecc2/reactor/app/RdbmsReconnectReactor.java
@@ -0,0 +1,49 @@
+package prerna.sablecc2.reactor.app;
+
+import java.sql.SQLException;
+
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.SecurityAppUtils;
+import prerna.engine.api.IEngine;
+import prerna.engine.impl.rdbms.RDBMSNativeEngine;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Utility;
+
+public class RdbmsReconnectReactor extends AbstractReactor {
+
+	public RdbmsReconnectReactor() {
+		this.keysToGet = new String[] {ReactorKeysEnum.APP.getKey()};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String appId = this.keyValue.get(this.keysToGet[0]);
+		
+		// make sure user has at least edit access
+		if (AbstractSecurityUtils.securityEnabled()) {
+			if (!SecurityAppUtils.userCanEditEngine(this.insight.getUser(), appId)) {
+				throw new IllegalArgumentException("User does not have permission to exec query for this app");
+			}
+		}
+		
+		IEngine engine = Utility.getEngine(appId);
+		if(!(engine instanceof RDBMSNativeEngine)) {
+			throw new IllegalArgumentException("App must be an RDBMS native engine");
+		}
+		
+		RDBMSNativeEngine rdbms = (RDBMSNativeEngine) engine;
+		try {
+			rdbms.makeConnection().close();
+			rdbms.makeConnection();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return new NounMetadata(true, PixelDataType.BOOLEAN);
+	}
+
+}
