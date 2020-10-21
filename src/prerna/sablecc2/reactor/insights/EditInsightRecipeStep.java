41c1581808d6fd347ef0c53bf3e823bb2fce3653
mahkhalil <mahkhalil@deloitte.com>
2020-10-21 16:42:11 +0000

    UPDATE ::: Adding log statements
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40720 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3c9ddeb10a4ac6564e4f1236e6ae5e3e231ab8aa

--- src/prerna/sablecc2/reactor/insights/EditInsightRecipeStep.java
+++ src/prerna/sablecc2/reactor/insights/EditInsightRecipeStep.java
@@ -3,6 +3,8 @@ package prerna.sablecc2.reactor.insights;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.logging.log4j.Logger;
+
 import prerna.om.Pixel;
 import prerna.sablecc2.PixelRunner;
 import prerna.sablecc2.om.PixelDataType;
@@ -14,6 +16,8 @@ import prerna.util.Utility;
 
 public class EditInsightRecipeStep extends AbstractReactor {
 
+	private static final String CLASS_NAME = EditInsightRecipeStep.class.getName();
+	
 	public EditInsightRecipeStep() {
 		this.keysToGet = new String[] {ReactorKeysEnum.PIXEL_ID.getKey(), ReactorKeysEnum.PIXEL.getKey()};
 	}
@@ -30,6 +34,7 @@ public class EditInsightRecipeStep extends AbstractReactor {
 			throw new NullPointerException("Pixel string for the substitution cannot be null");
 		}
 		
+		Logger logger = getLogger(CLASS_NAME);
 		String recipe = Utility.decodeURIComponent(encodedRecipe);
 		Pixel p = this.insight.getPixelList().getPixel(pixelId);
 		p.modifyPixelString(recipe);
@@ -37,6 +42,7 @@ public class EditInsightRecipeStep extends AbstractReactor {
 		// now i need to rerun the insight recipe
 		// clear the insight
 		// and re-run it
+		logger.info("Re-executing the insight recipe... please wait as this operation may take some time");
 		PixelRunner runner = this.insight.reRunPixelInsight();
 		// return the recipe steps
 		Map<String, Object> runnerWraper = new HashMap<String, Object>();
