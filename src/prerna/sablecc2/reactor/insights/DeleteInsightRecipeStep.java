b0925667fa27f0f0094e9371bec4c188327b0aad
mahkhalil <mahkhalil@deloitte.com>
2020-10-21 14:18:42 +0000

    UPDATE ::: Adding delete insight recipe step
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40713 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 9a2fe5e88e12bbb0c84d7c37606719e5e5d4204d

---
+++ src/prerna/sablecc2/reactor/insights/DeleteInsightRecipeStep.java
@@ -0,0 +1,71 @@
+package prerna.sablecc2.reactor.insights;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import prerna.om.PixelList;
+import prerna.sablecc2.PixelRunner;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class DeleteInsightRecipeStep extends AbstractReactor {
+
+	public DeleteInsightRecipeStep() {
+		this.keysToGet = new String[] {ReactorKeysEnum.PIXEL_ID.getKey()};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		List<String> pixelIds = getPixelIds();
+		if(pixelIds == null || pixelIds.isEmpty()) {
+			throw new NullPointerException("Pixel ids to remove cannot be null or empty");
+		}
+
+		// TODO: TAKE INTO CONSIDERATION DOWNSTREAM EFFECTS ON THE RECIPE!!!
+		
+		// grab the pixel list
+		// and remove the ids
+		PixelList pixelList = this.insight.getPixelList();
+		pixelList.removeIds(pixelIds);
+		
+		// now i need to rerun the insight recipe
+		// clear the insight
+		// and re-run it
+		PixelRunner runner = this.insight.reRunPixelInsight();
+		// return the recipe steps
+		Map<String, Object> runnerWraper = new HashMap<String, Object>();
+		runnerWraper.put("runner", runner);
+		NounMetadata noun = new NounMetadata(runnerWraper, PixelDataType.PIXEL_RUNNER, PixelOperationType.DELETE_INSIGHT_RECIPE);
+		return noun;
+	}
+
+	/**
+	 * Get the list of ids
+	 * @return
+	 */
+	private List<String> getPixelIds() {
+		List<String> pixelIds = new Vector<>();
+		GenRowStruct grs = this.store.getNoun(this.keysToGet[0]);
+		if(grs != null && !grs.isEmpty()) {
+			for(int i = 0; i < grs.size(); i++) {
+				pixelIds.add(grs.get(i) + "");
+			}
+			return pixelIds;
+		}
+		
+		if(!this.curRow.isEmpty()) {
+			for(int i = 0; i < this.curRow.size(); i++) {
+				pixelIds.add(this.curRow.get(i) + "");
+			}
+		}
+		
+		return pixelIds;
+	}
+	
+}
