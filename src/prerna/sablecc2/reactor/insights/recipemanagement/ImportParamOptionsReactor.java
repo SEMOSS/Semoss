0651960b1bfabb470a3055e747cf1e97d51210ff
mahkhalil <mahkhalil@deloitte.com>
2020-10-24 21:22:32 +0000

    UPDATE ::: Moving packages and adding a way to pull the import QS statements for each recipe
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40825 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 62ce3b09d0f9ad27a0c39d63c59567218814c4a8

---
+++ src/prerna/sablecc2/reactor/insights/recipemanagement/ImportParamOptionsReactor.java
@@ -0,0 +1,52 @@
+package prerna.sablecc2.reactor.insights.recipemanagement;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PushbackReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import prerna.om.Insight;
+import prerna.om.Pixel;
+import prerna.om.PixelList;
+import prerna.sablecc2.PixelPreProcessor;
+import prerna.sablecc2.lexer.Lexer;
+import prerna.sablecc2.lexer.LexerException;
+import prerna.sablecc2.node.Start;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.parser.Parser;
+import prerna.sablecc2.parser.ParserException;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.sablecc2.translations.ImportQueryTranslation;
+
+public class ImportParamOptionsReactor extends AbstractReactor {
+
+	@Override
+	public NounMetadata execute() {
+		PixelList pixelList = this.insight.getPixelList();
+		
+		Insight tempInsight = new Insight();
+		ImportQueryTranslation translation = new ImportQueryTranslation(tempInsight);
+		// loop through recipe
+		for(Pixel pixel : pixelList) {
+			try {
+				String pixelId = pixel.getId();
+				String expression = pixel.getPixelString();
+				translation.setPixelId(pixelId);
+				expression = PixelPreProcessor.preProcessPixel(expression.trim(), new ArrayList<String>(), new HashMap<String, String>());
+				Parser p = new Parser(new Lexer(new PushbackReader(new InputStreamReader(new ByteArrayInputStream(expression.getBytes("UTF-8"))), expression.length())));
+				// parsing the pixel - this process also determines if expression is syntactically correct
+				Start tree = p.parse();
+				// apply the translation.
+				tree.apply(translation);
+			} catch (ParserException | LexerException | IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return new NounMetadata(translation.getImportQsMap(), PixelDataType.MAP);
+	}
+
+}
