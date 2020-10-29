3833e20558e4099ce8de5b4e23165fc8aebd0294
mahkhalil <mahkhalil@deloitte.com>
2020-10-29 00:36:13 +0000

    UPDATE ::: Adding adapters for if else reactors
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@40953 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 33ee42d95f03817eea869311e146a92a6358fdf8

--- src/prerna/query/querystruct/selectors/QueryIfSelector.java
+++ src/prerna/query/querystruct/selectors/QueryIfSelector.java
@@ -13,71 +13,58 @@ public class QueryIfSelector extends AbstractQuerySelector {
 
 	@Override
 	public SELECTOR_TYPE getSelectorType() {
-		// TODO Auto-generated method stub
 		return SELECTOR_TYPE.IF_ELSE;
 	}
 
 	@Override
 	public String getAlias() {
-		// TODO Auto-generated method stub
 		return alias;
 	}
 
 	@Override
 	public boolean isDerived() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public String getDataType() {
-		// TODO Auto-generated method stub
 		// need to figure out if this is a string or a number
 		return null;
 	}
 
 	@Override
 	public String getQueryStructName() {
-		// TODO Auto-generated method stub
 		return pixelString;
 	}
 
 	// will come back to this
 	@Override
 	public List<QueryColumnSelector> getAllQueryColumns() {
-		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	public void setCondition(IQueryFilter condition)
-	{
+
+	public void setCondition(IQueryFilter condition) {
 		this.condition = condition;
 	}
 	
-	public void setPrecedent(IQuerySelector precedent)
-	{
+	public void setPrecedent(IQuerySelector precedent) {
 		this.precedent = precedent;
 	}
 	
-	public void setAntecedent(IQuerySelector antecedent)
-	{
+	public void setAntecedent(IQuerySelector antecedent) {
 		this.antecedent = antecedent;
 	}
 	
-	public IQueryFilter getCondition()
-	{
+	public IQueryFilter getCondition() {
 		return this.condition;
 	}
 	
-	public IQuerySelector getPrecedent()
-	{
+	public IQuerySelector getPrecedent() {
 		return this.precedent;
 	}
 	
-	public IQuerySelector getAntecedent()
-	{
+	public IQuerySelector getAntecedent() {
 		return this.antecedent;
 	}
 	
-
 }
