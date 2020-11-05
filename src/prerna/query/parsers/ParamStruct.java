71b94c0eaa8e0c1cf8607a235d250ada64bde116
mahkhalil <mahkhalil@deloitte.com>
2020-11-05 18:15:25 +0000

    UPDATE ::: Adding initial logic for getting parameter options from the imports that have executed
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@41084 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 38e5cd46fd5866979c944a57be502c93f7f8c064

--- src/prerna/query/parsers/ParamStruct.java
+++ src/prerna/query/parsers/ParamStruct.java
@@ -12,7 +12,7 @@ public class ParamStruct {
 	Object currentValue = null;
 	Object defaultValue = null;
 	String operator = null;
-	
+
 	boolean searchable = false;
 	boolean multiple = false;
 	String paramName = null;
@@ -21,69 +21,127 @@ public class ParamStruct {
 	String model_display = null; // need to turn this into an enum
 	String model_label = null; // how do you want to ask your user what to do ?
 	boolean required = false;
-	
+
 	public String getTableName() {
 		return tableName;
 	}
+
 	public void setTableName(String tableName) {
 		this.tableName = tableName;
 	}
+
+	public String getTableAlias() {
+		return tableAlias;
+	}
+
+	public void setTableAlias(String tableAlias) {
+		this.tableAlias = tableAlias;
+	}
+
 	public String getColumnName() {
 		return columnName;
 	}
+
 	public void setColumnName(String columnName) {
 		this.columnName = columnName;
 	}
+
 	public PixelDataType getType() {
 		return type;
 	}
+
 	public void setType(PixelDataType type) {
 		this.type = type;
 	}
+
 	public Object getCurrentValue() {
-		if(currentValue != null)
-			return currentValue;
-		return defaultValue;
+		return currentValue;
 	}
-	
+
 	public void setCurrentValue(Object currentValue) {
 		this.currentValue = currentValue;
 	}
+
 	public Object getDefaultValue() {
 		return defaultValue;
 	}
+
 	public void setDefaultValue(Object defaultValue) {
 		this.defaultValue = defaultValue;
 	}
+
+	public String getOperator() {
+		return operator;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+
+	public boolean isSearchable() {
+		return searchable;
+	}
+
+	public void setSearchable(boolean searchable) {
+		this.searchable = searchable;
+	}
+
 	public boolean isMultiple() {
 		return multiple;
 	}
-	public void setMultiple(boolean multi) {
-		this.multiple = multi;
+
+	public void setMultiple(boolean multiple) {
+		this.multiple = multiple;
 	}
-	public String getPixel() {
+
+	public String getParamName() {
+		return paramName;
+	}
+
+	public void setParamName(String paramName) {
+		this.paramName = paramName;
+	}
+
+	public String getModel_query() {
 		return model_query;
 	}
-	public void setPixel(String pixel) {
-		this.model_query = pixel;
+
+	public void setModel_query(String model_query) {
+		this.model_query = model_query;
 	}
+
 	public String getManualChoices() {
 		return manualChoices;
 	}
+
 	public void setManualChoices(String manualChoices) {
 		this.manualChoices = manualChoices;
-	};
-	
-	public void setParamName(String paramName)
-	{
-		this.paramName = paramName;
 	}
-	
-	public String getParamName()
-	{
-		return this.paramName;
+
+	public String getModel_display() {
+		return model_display;
 	}
-	
+
+	public void setModel_display(String model_display) {
+		this.model_display = model_display;
+	}
+
+	public String getModel_label() {
+		return model_label;
+	}
+
+	public void setModel_label(String model_label) {
+		this.model_label = model_label;
+	}
+
+	public boolean isRequired() {
+		return required;
+	}
+
+	public void setRequired(boolean required) {
+		this.required = required;
+	}
+
 	public String toString()
 	{
 		StringBuilder builder = new StringBuilder();
@@ -96,11 +154,8 @@ public class ParamStruct {
 		builder.append(currentValue);
 		builder.append("  ");
 		builder.append("from ").append(tableName);
-		
+
 		return builder.toString();
 	}
-	
-	
-	
-	
+
 }
---
+++ src/prerna/query/querystruct/transform/QSParseParamStruct.java
@@ -0,0 +1,118 @@
+package prerna.query.querystruct.transform;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import prerna.query.parsers.ParamStruct;
+import prerna.query.querystruct.filters.AndQueryFilter;
+import prerna.query.querystruct.filters.IQueryFilter;
+import prerna.query.querystruct.filters.OrQueryFilter;
+import prerna.query.querystruct.filters.SimpleQueryFilter;
+import prerna.query.querystruct.selectors.IQuerySelector;
+import prerna.query.querystruct.selectors.QueryColumnSelector;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+
+public class QSParseParamStruct {
+	
+	private static final Logger logger = LogManager.getLogger(QSParseParamStruct.class);
+	
+	private QSParseParamStruct() {
+
+	}
+	
+	/**
+	 * This is the main method for this class.  Parses a generic filter
+	 * @param filter
+	 * @param paramList
+	 */
+	public static void parseFilter(IQueryFilter filter, List<ParamStruct> paramList) {
+		if(filter.getQueryFilterType() == IQueryFilter.QUERY_FILTER_TYPE.SIMPLE) {
+			convertSimpleQueryFilter((SimpleQueryFilter) filter, paramList);
+		} else if(filter.getQueryFilterType() == IQueryFilter.QUERY_FILTER_TYPE.AND) {
+			parseAndQueryFilter((AndQueryFilter) filter, paramList);
+		} else if(filter.getQueryFilterType() == IQueryFilter.QUERY_FILTER_TYPE.OR) {
+			parseOrQueryFilter((OrQueryFilter) filter, paramList);
+		}
+		
+	}
+
+	public static void parseOrQueryFilter(OrQueryFilter filter, List<ParamStruct> paramList) {
+		for(IQueryFilter f : filter.getFilterList()) {
+			parseFilter(f, paramList);
+		}
+	}
+
+	public static void parseAndQueryFilter(AndQueryFilter filter, List<ParamStruct> paramList) {
+		for(IQueryFilter f : filter.getFilterList()) {
+			parseFilter(f, paramList);
+		}
+	}
+
+	public static void convertSimpleQueryFilter(SimpleQueryFilter filter, List<ParamStruct> paramList) {
+		boolean parameterizeLeft = false;
+		boolean parameterizeRight = false;
+		
+		IQuerySelector selector = null;
+		NounMetadata origL = filter.getLComparison();
+		if(origL.getNounType() == PixelDataType.COLUMN) {
+			selector = (IQuerySelector) origL.getValue();
+			parameterizeRight = true;
+		}
+		
+		NounMetadata origR = filter.getRComparison();
+		if(origR.getNounType() == PixelDataType.COLUMN) {
+			selector = (IQuerySelector) origL.getValue();
+			parameterizeLeft = true;
+		}
+		
+		if((parameterizeLeft && parameterizeRight)
+				|| (!parameterizeLeft && !parameterizeRight)) {
+			// cannot have both be columns
+			// and need at least one to be a column
+			// that is already in a sense a dynamically defined filter
+			logger.debug("Must have at one part of the filter be a column");
+			return;
+		}
+		
+		if(!(selector instanceof QueryColumnSelector)) {
+			logger.debug("Cannot parameterize on derived columns");
+			return;
+		}
+		String comparator = filter.getComparator();
+		
+		ParamStruct param = new ParamStruct();
+		param.setOperator(comparator);
+		
+		QueryColumnSelector columnSelector = (QueryColumnSelector) selector;
+		param.setTableName(columnSelector.getTable());
+		param.setColumnName(columnSelector.getColumn());
+		if(parameterizeLeft) {
+			// rhs is the column, lhs is the value
+			Object paramValue = origL.getValue();
+			param.setCurrentValue(paramValue);
+			param.setType(origL.getNounType());
+		} else if(parameterizeRight) {
+			// lhs is the column, rhs is the value
+			Object paramValue = origR.getValue();
+			param.setCurrentValue(paramValue);
+			param.setType(origL.getNounType());
+		}
+		// additional options based on the comparator
+		if(IQueryFilter.comparatorIsNumeric(comparator)
+				|| IQueryFilter.isRegexComparator(comparator)) {
+			// single valued
+			param.setMultiple(false);
+			param.setSearchable(false);
+		} else {
+			// multiple values allowed
+			param.setMultiple(true);
+			param.setSearchable(true);
+		}
+		
+		// add to the list of params
+		paramList.add(param);
+	}
+}
