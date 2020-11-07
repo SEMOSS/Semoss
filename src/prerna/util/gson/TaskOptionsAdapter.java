3640a38127f15bd1a1433efebb8099ec9e5012d5
mahkhalil <mahkhalil@deloitte.com>
2020-11-07 01:39:15 +0000

    BUGFIX ::: Adding forgotten class for compilation
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@41147 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/util/gson/TaskOptionsAdapter.java
@@ -0,0 +1,72 @@
+package prerna.util.gson;
+
+import java.io.IOException;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+import prerna.sablecc2.om.task.options.TaskOptions;
+
+public class TaskOptionsAdapter extends TypeAdapter<TaskOptions> {
+
+	private static Gson GSON = GsonUtility.getDefaultGson();
+
+	@Override
+	public TaskOptions read(JsonReader in) throws IOException {
+		if(in.peek() == JsonToken.NULL) {
+			return null;
+		}
+		
+		Map<String, Object> options = null;
+		boolean isOrnament = false;
+		
+		in.beginObject();
+		while(in.hasNext()) {
+			String name = in.nextName();
+			if(in.peek() == JsonToken.NULL) {
+				in.nextNull();
+			}
+			
+			if(name.equals("options")) {
+				TypeAdapter adapter = GSON.getAdapter(Map.class);
+				options = (Map<String, Object>) adapter.read(in);
+				
+			} else if(name.equals("ornament")){
+				isOrnament = in.nextBoolean();
+			}
+			
+		}
+		in.endObject();
+		
+		TaskOptions taskOptions = new TaskOptions(options);
+		taskOptions.setOrnament(isOrnament);
+		return taskOptions;
+	}
+	
+	@Override
+	public void write(JsonWriter out, TaskOptions value) throws IOException {
+		if(value == null) {
+			out.nullValue();
+			return;
+		}
+		
+		out.beginObject();
+		out.name("options");
+		Map<String, Object> options = value.getOptions();
+		if(options == null) {
+			out.nullValue();
+		} else {
+			TypeAdapter adapter = GSON.getAdapter(options.getClass());
+			adapter.write(out, options);
+		}
+		out.name("ornament");
+		out.value(value.isOrnament());
+		
+		out.endObject();
+	}
+	
+}
