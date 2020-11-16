0f0c01cad8975041c4a43808c55a83053d103f0f
Maher Khalil <themaherkhalil@gmail.com>
2020-11-16 16:18:05 +0000

    UPDATE ::: Adding new BE method for decrypting. To be used with post message semoss-execute-sql
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@41331 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8ccc9b59180ccb3a2b49cb8a009e252b56c3bed1

---
+++ src/prerna/sablecc2/reactor/security/PBEDecryptReactor.java
@@ -0,0 +1,70 @@
+package prerna.sablecc2.reactor.security;
+
+import org.jasypt.encryption.pbe.StandardPBEByteEncryptor;
+
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.Utility;
+
+public class PBEDecryptReactor extends AbstractReactor {
+
+	private static String password = null;
+	
+	public PBEDecryptReactor() {
+		this.keysToGet = new String[] {ReactorKeysEnum.QUERY_KEY.getKey()};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		//grab the query
+		String encryptedQuery = Utility.decodeURIComponent(this.curRow.get(0).toString());
+		
+		StandardPBEByteEncryptor encryptor = new StandardPBEByteEncryptor();
+		encryptor.setPassword(getPassword());
+		byte[] queryBytes = encryptor.decrypt(encryptedQuery.getBytes());
+		String query = new String(queryBytes);
+		
+		return new NounMetadata(query, PixelDataType.CONST_STRING);
+	}
+
+	/**
+	 * Get the password from the SMSS file
+	 * @return
+	 */
+	private static String getPassword() {
+		if(PBEDecryptReactor.password != null) {
+			return PBEDecryptReactor.password;
+		}
+		
+		synchronized (PBEDecryptReactor.class) {
+			if(PBEDecryptReactor.password != null) {
+				return PBEDecryptReactor.password;
+			}
+			
+			PBEDecryptReactor.password = DIHelper.getInstance().getProperty(Constants.PM_SEMOSS_EXECUTE_SQL_ENCRYPTION_PASSWORD);
+		}
+		
+		return PBEDecryptReactor.password;
+	}
+	
+	
+	public static void main(String[] args) {
+		String value = "This is my example sql query";
+		
+		StandardPBEByteEncryptor encryptor = new StandardPBEByteEncryptor();
+		encryptor.setPassword("password123");
+		byte[] queryBytes = encryptor.encrypt(value.getBytes());
+		String encoded = new String(queryBytes);
+		System.out.println("Encoded >> " + encoded);
+		
+//		byte[] decodedBytes = encryptor.decrypt(queryBytes);
+//		String decoded = new String(decodedBytes);
+//		System.out.println("Decoded >> " + decoded);
+	}
+	
+}
