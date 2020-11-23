41c73f10695d0baa8326f0508d7400907115bdf2
Maher Khalil <themaherkhalil@gmail.com>
2020-11-23 18:51:57 +0000

    UPDATE ::: Adding metamodel and database structure caching due to how often these calls are made on the local master database
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@41475 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 610e8f9a5d2c61233b49bdd3b2ad0aa4e4d51a70

--- src/prerna/sablecc2/reactor/app/metaeditor/concepts/EditOwlConceptConceptualNameReactor.java
+++ src/prerna/sablecc2/reactor/app/metaeditor/concepts/EditOwlConceptConceptualNameReactor.java
@@ -11,6 +11,7 @@ import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.app.metaeditor.AbstractMetaEditorReactor;
+import prerna.util.EngineSyncUtility;
 import prerna.util.Utility;
 
 public class EditOwlConceptConceptualNameReactor extends AbstractMetaEditorReactor {
@@ -89,6 +90,7 @@ public class EditOwlConceptConceptualNameReactor extends AbstractMetaEditorReact
 			noun.addAdditionalReturn(new NounMetadata("An error occured attempting to commit modifications", PixelDataType.CONST_STRING, PixelOperationType.ERROR));
 			return noun;
 		}
+		EngineSyncUtility.clearEngineCache(appId);
 		ClusterUtil.reactorPushOwl(appId);
 
 		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
--- src/prerna/sablecc2/reactor/app/metaeditor/properties/RemoveOwlPropertyReactor.java
+++ src/prerna/sablecc2/reactor/app/metaeditor/properties/RemoveOwlPropertyReactor.java
@@ -11,6 +11,7 @@ import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.app.metaeditor.AbstractMetaEditorReactor;
+import prerna.util.EngineSyncUtility;
 import prerna.util.Utility;
 
 public class RemoveOwlPropertyReactor extends AbstractMetaEditorReactor {
@@ -103,6 +104,7 @@ public class RemoveOwlPropertyReactor extends AbstractMetaEditorReactor {
 			noun.addAdditionalReturn(new NounMetadata("An error occured attempting to remove the desired property", PixelDataType.CONST_STRING, PixelOperationType.ERROR));
 			return noun;
 		}
+		EngineSyncUtility.clearEngineCache(appId);
 		ClusterUtil.reactorPushOwl(appId);
 
 		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
