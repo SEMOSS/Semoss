2d4e922f11c006ad25f06ee1fdcd2aa96d89d5ff
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2020-12-06 23:51:05 +0000

    Committing neutralizeFunction
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@41627 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8e06c6fe5ada949b901daeeac715843a82480470

--- src/prerna/query/querystruct/FunctionExpression.java
+++ src/prerna/query/querystruct/FunctionExpression.java
@@ -5,6 +5,8 @@ import java.util.Vector;
 
 public class FunctionExpression extends GenExpression {
 	
+	public boolean neutralizeFunction = false; // removes the function from the given column. This only works if your function has a single column
+	
 	// Function expressions
 	public List <GenExpression> expressions = new Vector <GenExpression>();
 	
