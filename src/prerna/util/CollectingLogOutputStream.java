0840446b87d165df71714e57f6bf94078f3abc31
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2021-01-04 14:34:42 +0000

    PandasFrame - Changes to single quote for some of the commands. AdjustDataTypes is grouped as a single string instead of multiple commands Insight, Command, SetContext - Changing to accomodate no user. So it can work with it. PyUtils, Utility, CmdExecUtil, CollectingLogOutputStream - Updates to accomodate for log file. In the new log format, the old one doesnt work. Updates to make it work on docker. TCPPyWorker, TCPServerHandler.java, NettyClient, TCPClientHandler - Minor refactoring and auto shutdown. When tomcat is killed, all of the existing python processes will automatically get killed CommandReactor - Uncomment the line to see it in action.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@41930 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 4574cb2e38ff6b593d02c9486418e43ea1983723

---
+++ src/prerna/util/CollectingLogOutputStream.java
@@ -0,0 +1,15 @@
+package prerna.util;
+
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.commons.exec.LogOutputStream;
+
+public class CollectingLogOutputStream extends LogOutputStream {
+    private final List<String> lines = new LinkedList<String>();
+    @Override protected void processLine(String line, int level) {
+        lines.add(line);
+    }   
+    public List<String> getLines() {
+        return lines;
+    }
+}
