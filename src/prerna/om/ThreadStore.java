f5e371e268bc03eb75d7a5af4b9355df51669d34
kunalppatel9 <kunalppatel9@deloitte.com>
2021-01-11 19:48:46 +0000

    UPDATE ::: chrome driver adding in route cookie additions and setting in thread store
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@42017 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/om/ThreadStore.java
+++ src/prerna/om/ThreadStore.java
@@ -7,64 +7,72 @@ import prerna.auth.User;
 
 public class ThreadStore {
 
-    private static final ThreadLocal<Map<String, Object>> CURRENT = new ThreadLocal<Map<String, Object>>();
+	private static final ThreadLocal<Map<String, Object>> CURRENT = new ThreadLocal<Map<String, Object>>();
 
-    private static Map<String, Object> getThreadMap() {
+	private static Map<String, Object> getThreadMap() {
 		Map<String, Object> map = CURRENT.get();
-		if(map == null) {
+		if (map == null) {
 			map = new HashMap<String, Object>();
 			CURRENT.set(map);
 		}
 		return map;
 	}
-    
-    public static Map<String, Object> getTheadMapObject() {
-    	return CURRENT.get();
-    }
-    
-    public static void setThreadMapObject(Map<String, Object> mapValues) {
+
+	public static Map<String, Object> getTheadMapObject() {
+		return CURRENT.get();
+	}
+
+	public static void setThreadMapObject(Map<String, Object> mapValues) {
 		Map<String, Object> map = CURRENT.get();
 		map.putAll(mapValues);
-    }
-    
+	}
+
 	public static void setInsightId(String insightId) {
 		Map<String, Object> map = getThreadMap();
 		map.put("insightId", insightId);
 	}
-	
+
 	public static String getInsightId() {
 		return (String) getThreadMap().get("insightId");
 	}
-	
+
 	public static void setSessionId(String sessionId) {
 		Map<String, Object> map = getThreadMap();
 		map.put("sessionId", sessionId);
 	}
-	
+
 	public static String getSessionId() {
 		return (String) getThreadMap().get("sessionId");
 	}
-	
+
+	public static void setRouteId(String routeId) {
+		Map<String, Object> map = getThreadMap();
+		map.put("routeId", routeId);
+	}
+
+	public static String getRouteId() {
+		return (String) getThreadMap().get("routeId");
+	}
+
 	public static void setJobId(String jobId) {
 		Map<String, Object> map = getThreadMap();
 		map.put("jobId", jobId);
 	}
-	
+
 	public static String getJobId() {
 		return (String) getThreadMap().get("jobId");
 	}
-	
+
 	public static void setUser(User user) {
 		Map<String, Object> map = getThreadMap();
 		map.put("user", user);
 	}
-	
+
 	public static User getUser() {
 		return (User) getThreadMap().get("user");
 	}
-	
+
 	public static void remove() {
 		CURRENT.remove();
 	}
 }
-
