4669a525a502348a0700bdf316f900e2da749966
mahkhalil <mahkhalil@deloitte.com>
2021-01-14 04:35:19 +0000

    UPDATE ::: Pushing correct syntax to perform transpose
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@42107 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/py/TransposeReactor.java
+++ src/prerna/sablecc2/reactor/frame/py/TransposeReactor.java
@@ -13,19 +13,43 @@ public class TransposeReactor extends AbstractPyFrameReactor {
 	public NounMetadata execute() {
 		PandasFrame frame = (PandasFrame) getFrame();
 		// get table name
-		String table = frame.getName();
-		String transposeScript = table + " = " + table + ".transpose()";
+		String table = frame.getWrapperName();
+
+		String transposeScript = frame.getName() + "=" + table + ".cache['data'].transpose().rename_axis('rn', axis=0).add_prefix('V').reset_index()";
 		// python transpose creates numeric columns
 		// need to make str type
-		String stringHeaderType = table + ".columns = " + table + ".columns.astype(str)";
 		// change all datatypes to string
-		String stringType = table + " = " + table + ".astype(str)";
-		insight.getPyTranslator().runPyAndReturnOutput(transposeScript, stringHeaderType, stringType);
+		String objectType = frame.getName() + "=" + frame.getName() + ".astype(str)";
+		String stringType = frame.getName() + "=" + frame.getName() + ".astype('string')";
+		insight.getPyTranslator().runEmptyPy(transposeScript, objectType, stringType);
+
+		//String[] colTypes = getColumnTypes(frame);
+		//insight.getPyTranslator().runPyAndReturnOutput(transposeScript, stringHeaderType, stringType);
+
 		// TRACKING
 		UserTrackerFactory.getInstance().trackAnalyticsWidget(this.insight, frame, "Transpose", AnalyticsTrackerHelper.getHashInputs(this.store, this.keysToGet));
 		// the column data has changed
 		frame = (PandasFrame) recreateMetadata(frame);
 		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE, PixelOperationType.FRAME_HEADERS_CHANGE);
 	}
+	
+//	@Override
+//	public NounMetadata execute() {
+//		PandasFrame frame = (PandasFrame) getFrame();
+//		// get table name
+//		String table = frame.getName();
+//		String transposeScript = table + " = " + table + ".transpose()";
+//		// python transpose creates numeric columns
+//		// need to make str type
+//		String stringHeaderType = table + ".columns = " + table + ".columns.astype(str)";
+//		// change all datatypes to string
+//		String stringType = table + " = " + table + ".astype(str)";
+//		insight.getPyTranslator().runPyAndReturnOutput(transposeScript, stringHeaderType, stringType);
+//		// TRACKING
+//		UserTrackerFactory.getInstance().trackAnalyticsWidget(this.insight, frame, "Transpose", AnalyticsTrackerHelper.getHashInputs(this.store, this.keysToGet));
+//		// the column data has changed
+//		frame = (PandasFrame) recreateMetadata(frame);
+//		return new NounMetadata(frame, PixelDataType.FRAME, PixelOperationType.FRAME_DATA_CHANGE, PixelOperationType.FRAME_HEADERS_CHANGE);
+//	}
 
 }
