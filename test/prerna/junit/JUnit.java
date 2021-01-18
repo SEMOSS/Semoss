19ab53eecc685eb0c15970dec974090abd2f3528
mahkhalil <mahkhalil@deloitte.com>
2021-01-18 19:47:48 +0000

    UPDATE ::: Adding logic to fix startup
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@42179 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- test/prerna/junit/JUnit.java
+++ test/prerna/junit/JUnit.java
@@ -3,11 +3,11 @@ package prerna.junit;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
 import org.junit.BeforeClass;
 
 public class JUnit {
@@ -21,13 +21,23 @@ public class JUnit {
 	@BeforeClass
 	public static void configureLog4j() {
 		String log4JPropertyFile = TEST_RESOURCES_DIRECTORY + FILE_SEPARATOR + "log4j.properties";
-		Properties prop = new Properties();
+		FileInputStream fis = null;
+		ConfigurationSource source = null;
 		try {
-			prop.load(new FileInputStream(log4JPropertyFile));
-			PropertyConfigurator.configure(prop);
+			fis = new FileInputStream(log4JPropertyFile);
+			source = new ConfigurationSource(fis);
+			Configurator.initialize(null, source);
 			LOGGER.info("Successfully configured Log4j for JUnit suite.");
 		} catch (IOException e) {
 			LOGGER.warn("Unable to initialize log4j for testing.", e);
+		} finally {
+			if(fis != null) {
+				try {
+					fis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 	
--- test/prerna/junit/PixelUnitTests.java
+++ test/prerna/junit/PixelUnitTests.java
@@ -12,8 +12,8 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.AssumptionViolatedException;
 import org.junit.Before;
