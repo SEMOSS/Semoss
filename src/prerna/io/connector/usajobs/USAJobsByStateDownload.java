cddc056ede04f791e3a77b7c49632ff8f5b7897b
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2021-01-24 03:36:06 +0000

    USAJobsAPI download Reactor - USAJobsByStateDownload.java, UsaJobsUtil.java
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@42311 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/io/connector/usajobs/USAJobsByStateDownload.java
@@ -0,0 +1,45 @@
+package prerna.io.connector.usajobs;
+
+import java.util.List;
+
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.AssetUtility;
+import prerna.util.Utility;
+
+public class USAJobsByStateDownload extends AbstractReactor {
+
+	public USAJobsByStateDownload() {
+		this.keysToGet = new String[] { "cred", "state", ReactorKeysEnum.FILE_NAME.getKey()};
+	}
+
+	public NounMetadata execute() {
+		organizeKeys();
+		
+		// this would be a vector
+		List <String> cred = (List)this.store.getNoun("cred").getAllValues();
+		List <String> state = (List)this.store.getNoun("state").getAllValues();
+		
+		String fileName = null;
+		if(keyValue.containsKey(keysToGet[2]))
+			fileName = keyValue.get(keysToGet[2]);
+		else
+			fileName = Utility.getRandomString(5) + ".csv";
+		
+		
+		// should we put thi in temp
+		fileName  = this.insight.getInsightFolder() + java.nio.file.FileSystems.getDefault().getSeparator() + fileName;
+
+		UsaJobsUtil u = new UsaJobsUtil(fileName);
+		u.runStateSearch(cred, state);
+		
+		// set the file for download
+		String exportName = "USA_JOBS" + UsaJobsUtil.getToday();
+		this.insight.addExportFile(exportName, fileName);
+		NounMetadata retNoun = new NounMetadata(exportName, PixelDataType.CONST_STRING, PixelOperationType.FILE_DOWNLOAD); 
+		return retNoun;
+	}
+}
