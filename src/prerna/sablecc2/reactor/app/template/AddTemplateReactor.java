90092e937b7d7768a26d0ec507ebb1202fe2fcae
mahkhalil <mahkhalil@deloitte.com>
2021-01-29 00:02:50 +0000

    UPDATE ::: Pushing changes on add and update for template to work on cloud UPDATE ::: Adding some code styling to TableToXLSXReactor for my readability
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@42450 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 4156be2e923307f92aefb5a2e2ed98184c205fcb

--- src/prerna/sablecc2/reactor/app/template/AddTemplateReactor.java
+++ src/prerna/sablecc2/reactor/app/template/AddTemplateReactor.java
@@ -1,6 +1,14 @@
 package prerna.sablecc2.reactor.app.template;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import prerna.cluster.util.ClusterUtil;
 import prerna.engine.api.IEngine;
@@ -9,13 +17,13 @@ import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.util.AssetUtility;
+import prerna.util.Constants;
 import prerna.util.Utility;
 
-/** This reactor will add the new template information in the app corresponding template property file. 
- * @author kprasannakumar
- *
- */
 public class AddTemplateReactor extends AbstractReactor {
+
+	private static final Logger classLogger = LogManager.getLogger(AddTemplateReactor.class);
+	private static final String CLASS_NAME = AddTemplateReactor.class.getName();
 
 	public AddTemplateReactor() {
 		this.keysToGet = new String[] { ReactorKeysEnum.APP.getKey(), ReactorKeysEnum.TEMPLATE_NAME.getKey(),
@@ -24,14 +32,51 @@ public class AddTemplateReactor extends AbstractReactor {
 
 	public NounMetadata execute() {
 		organizeKeys();
+		Logger logger = getLogger(CLASS_NAME);
+		
 		String appId = this.keyValue.get(ReactorKeysEnum.APP.getKey());
 		String templateFile = this.keyValue.get(ReactorKeysEnum.TEMPLATE_FILE.getKey());
 		String templateName = this.keyValue.get(ReactorKeysEnum.TEMPLATE_NAME.getKey());
 		
 		IEngine engine = Utility.getEngine(appId);
+		String versionFolder = AssetUtility.getAppAssetFolder(engine.getEngineName(), appId);
+		String fileToMove = versionFolder;
+		if(templateFile.startsWith("/") || templateFile.startsWith("\\")) {
+			fileToMove += templateFile;
+		} else {
+			fileToMove += "/" + templateFile;
+		}
+		fileToMove = fileToMove.replace('\\', '/');
+		File f = new File(fileToMove);
+		// we will move this file over
+		String baseF = this.insight.getInsightFolder();
+		String tempMove = baseF + "/" + UUID.randomUUID().toString() + "." + FilenameUtils.getExtension(fileToMove);
+		File newF = new File(tempMove);
+		if(newF.getParentFile().exists()) {
+			newF.getParentFile().mkdirs();
+		}
+		try {
+			FileUtils.moveFile(f, newF);
+		} catch (IOException e) {
+			classLogger.error(Constants.STACKTRACE, e);
+			throw new IllegalArgumentException("An error occurred moving the template into the template folder", e);
+		}
+		
+		logger.info("Starting to synchronize templates with template directory");
+		// pull from cloud
 		ClusterUtil.reactorPullFolder(engine, AssetUtility.getAppAssetVersionFolder(engine.getEngineName(), appId));
+		// move back the file
+		try {
+			FileUtils.moveFile(newF, f);
+		} catch (IOException e) {
+			classLogger.error(Constants.STACKTRACE, e);
+			throw new IllegalArgumentException("An error occurred moving the template into the template folder", e);
+		}
+		// write/update to properties file
 		Map<String, String> templateDataMap = TemplateUtility.addTemplate(appId, templateFile, templateName);
+		// push to cloud
 		ClusterUtil.reactorPushFolder(engine, AssetUtility.getAppAssetVersionFolder(engine.getEngineName(), appId));
+		logger.info("Finished synchronizing templates with template directory");
 
 		// returning back the updated template information which will contain all the 
 		// template information with template name as key and file name as the value
