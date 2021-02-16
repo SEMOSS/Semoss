8218ec2bb8dc99b080d04b2745b7c8d0f02d3b05
Maher Khalil <themaherkhalil@gmail.com>
2021-02-16 22:23:33 +0000

    BUGFIX ::: Properly resetting the panel specific objects on the refresh tasks reactors
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@42686 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a0f4a025600bce26859a4d277b6627eeaeafd780

--- src/prerna/sablecc2/reactor/task/RefreshAllPanelTasksReactor.java
+++ src/prerna/sablecc2/reactor/task/RefreshAllPanelTasksReactor.java
@@ -12,6 +12,7 @@ import prerna.algorithm.api.ITableDataFrame;
 import prerna.om.InsightPanel;
 import prerna.query.querystruct.AbstractQueryStruct.QUERY_STRUCT_TYPE;
 import prerna.query.querystruct.SelectQueryStruct;
+import prerna.query.querystruct.selectors.IQuerySort;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
@@ -60,6 +61,12 @@ public class RefreshAllPanelTasksReactor extends AbstractReactor {
 				Set<String> layers = lQs.keySet();
 				LAYER_LOOP : for(String layerId : layers) {
 					SelectQueryStruct qs = lQs.get(layerId);
+					// reset the panel specific objects so we can pick up the latest state
+					qs.setPanelList(new Vector<InsightPanel>());
+					qs.setPanelIdList(new Vector<String>());
+					qs.setPanelOrderBy(new Vector<IQuerySort>());
+					// add the panel
+					qs.addPanel(panel);
 					qs.resetPanelState();
 					TaskOptions taskOptions = lTaskOption.get(layerId);
 					
