4193716c717360a2f8bf0fdeb19e891b600ef956
mahkhalil <mahkhalil@deloitte.com>
2021-02-18 15:11:15 +0000

    UPDATE ::: Sending back the panelViewOptions for insights with a parameter input
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@42716 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2beec09dfa4a6fbe63d1e242605f761ea6bd80a1

--- src/prerna/sablecc2/InsightParamTranslation.java
+++ src/prerna/sablecc2/InsightParamTranslation.java
@@ -1,26 +1,45 @@
 package prerna.sablecc2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
 import prerna.sablecc2.analysis.DepthFirstAdapter;
 import prerna.sablecc2.node.AOperation;
+import prerna.sablecc2.node.AOtherOpInput;
 import prerna.sablecc2.node.ARoutineConfiguration;
 import prerna.sablecc2.node.POpInput;
+import prerna.sablecc2.node.POtherOpInput;
 import prerna.sablecc2.node.PRoutine;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.execptions.SemossPixelException;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.util.Utility;
+import prerna.util.gson.GsonUtility;
 
 public class InsightParamTranslation extends DepthFirstAdapter {
 
 	private static final Logger LOGGER = LogManager.getLogger(InsightParamTranslation.class.getName());
-	private boolean hasParam = false;
-	
+	private static Gson GSON = GsonUtility.getDefaultGson();
+
+	private boolean isParam = false;
+	private Map<String, Object> viewOptionsMap = new HashMap<String, Object>();
+
+	private boolean notCacheable = false;
+
+	// require a single panel
 	private Set<String> panelsCreated = new HashSet<String>();
-	
 	
 	@Override
 	public void caseARoutineConfiguration(ARoutineConfiguration node) {
@@ -51,20 +70,54 @@ public class InsightParamTranslation extends DepthFirstAdapter {
 		} else if(reactorId.equals("SetPanelView")) {
         	String view = node.getOpInput().toString().trim();
         	if(view.equals("\"param\"")) {
-        		hasParam = true;
+        		notCacheable = true;
+
+        		isParam = true;
+        		// need to parse to get the json view
+        		// which is sent as a string
+        		String viewOptions = null;
+    			String encodedViewOptions = null;
+        		LinkedList<POtherOpInput> otherInputs = node.getOtherOpInput();
+        		if(!otherInputs.isEmpty()) {
+        			POtherOpInput otherIn = otherInputs.get(0);
+        			if(otherIn instanceof AOtherOpInput) {
+        				AOtherOpInput otherOpInput = (AOtherOpInput) otherIn;
+        				encodedViewOptions = otherOpInput.getOpInput().toString();
+        			}
+        			if(encodedViewOptions != null) {
+        				encodedViewOptions = PixelUtility.removeSurroundingQuotes(encodedViewOptions);
+        				viewOptions = Utility.decodeURIComponent(encodedViewOptions);
+        				if(viewOptions != null && !viewOptions.isEmpty()) {
+        					try {
+        						this.viewOptionsMap = GSON.fromJson(viewOptions, Map.class);
+        					} catch(JsonSyntaxException e) {
+        						throw new SemossPixelException(new NounMetadata("Panel view is not in a valid JSON format after decoding", 
+        								PixelDataType.CONST_STRING, PixelOperationType.ERROR));
+        					}
+        				}
+        			}
+        		}
         	} else if(view.equals("\"default-handle\"")) {
-        		hasParam = true;
+        		notCacheable = true;
         	} else if(view.equals("\"grid-delta\"")) {
-        		hasParam = true;
+        		notCacheable = true;
         	}
         }
 	}
 
-	public boolean hasParam() {
-		if(panelsCreated.size() == 1 && hasParam) {
+	public boolean notCacheable() {
+		if(panelsCreated.size() == 1 && notCacheable) {
 			return true;
 		}
 		return false;
+	}
+	
+	public Map<String, Object> getPanelViewJson() {
+		if(panelsCreated.size() == 1 && isParam) {
+			return this.viewOptionsMap;
+		}
+		
+		return null;
 	}
 
 }
