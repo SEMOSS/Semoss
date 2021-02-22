3244003415c7b7d1bacbb3ec896d0669c9816555
Maher Khalil <themaherkhalil@gmail.com>
2021-02-22 20:42:13 +0000

    UPDATE ::: Forgotten places for naming changes for 'parameter' to 'uiState' on scheduler
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@42756 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d98c1edeeb0d697db71bb33cd607c1bdccf9f6c2

--- src/prerna/rpa/config/JobConfigKeys.java
+++ src/prerna/rpa/config/JobConfigKeys.java
@@ -19,7 +19,7 @@ public class JobConfigKeys {
 	public static final String USER_ACCESS = "-userAccess";
 	public static final String PIXEL = "pixel"; 
 	public static final String PIXEL_PARAMETERS = "pixelParameters"; 
-	public static final String PARAMETERS = "parameters"; // Cannot have the dash due to FE
+	public static final String UI_STATE = "uiState"; // Cannot have the dash due to FE
 
 	// execution side
 	public static final String EXEC_ID = "-execId"; // Cannot have the dash due to FE
