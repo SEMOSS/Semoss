4fe5ac2b8bec7d5b3123804875677befaf905bd6
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2021-03-10 23:28:59 +0000

    Update for creating Netty RServe / PyServe To get Netty_R going - set NETTY_R	true on RDF Map To get NETTY_PYTHON going set NETTY_PYTHON	true
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@42985 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ds/py/TCPPyTranslator.java
+++ src/prerna/ds/py/TCPPyTranslator.java
@@ -3,21 +3,43 @@ package prerna.ds.py;
 import java.util.Hashtable;
 
 import prerna.pyserve.NettyClient;
+import prerna.sablecc2.reactor.frame.r.PayloadStruct;
 
 public class TCPPyTranslator extends PyTranslator {
 
+	public static final String METHOD_DELIMITER = "$$##";
 	public NettyClient nc = null;
+	String method = null;
 	
 	@Override
 	public synchronized Object runScript(String script) 
 	{
+		if(method != null)
+		{
+			script = method +  METHOD_DELIMITER + script;
+			method = null;
+		}
+		
 		//System.out.println(".");
-		Object response = nc.executeCommand(script);
+//		Object response = nc.executeCommand(script);
 		//Object [] outputObj = (Object [])response;
 		
 		//System.out.println("Command was " + outputObj[0] + "<>" + script + "<>" + outputObj[1]);
 		//System.err.println("Got the response back !!!!! WOO HOO " + response);
-		return response;
+		String methodName = new Object(){}.getClass().getEnclosingMethod().getName();
+
+		PayloadStruct ps = constructPayload(methodName, script);
+		ps.engine = PayloadStruct.ENGINE.PYTHON;
+		ps.payloadClasses = new Class[] {String.class};
+		ps = (PayloadStruct)nc.executeCommand(ps);
+		if(ps.ex != null)
+		{
+			logger.info("Exception " + ps.ex);
+		}
+		else
+			return ps.payload[0];
+		
+		return null;
 	}
 
 	@Override
@@ -31,9 +53,21 @@ public class TCPPyTranslator extends PyTranslator {
 	@Override
 	protected void executeEmptyPyDirect(String script)
 	{
+		
 		runScript(script);
 	}
 
+	private PayloadStruct constructPayload(String methodName, Object...objects )
+	{
+		// go through the objects and if they are set to null then make them as string null
+		PayloadStruct ps = new PayloadStruct();
+		ps.engine = PayloadStruct.ENGINE.R;
+		ps.methodName = methodName;
+		ps.payload = objects;
+		
+		return ps;
+	}
 
+	
 	
 }
