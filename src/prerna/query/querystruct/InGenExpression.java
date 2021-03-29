8c1afaf915981739a3d19ee25414c85b608836a5
mahkhalil <mahkhalil@deloitte.com>
2021-03-29 14:27:15 +0000

    BUGFIX ::: Fixing error with potentially sending the same app with favorite as true and false due to the favoriting apps that are global BUGFIX ::: Taking into consideration "NOT IN" syntax for sql parsing
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@43212 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ca29bf8246d67056e0ed1371989d8de72282acc0

--- src/prerna/query/querystruct/InGenExpression.java
+++ src/prerna/query/querystruct/InGenExpression.java
@@ -2,10 +2,21 @@ package prerna.query.querystruct;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 
 public class InGenExpression extends GenExpression {
 	
+	// false = in
+	// true = not in
+	private boolean isNot = false;
+	
 	// sets the condition
 	public List <GenExpression> inList = new ArrayList<GenExpression>();
+	
+	public void setIsNot(boolean negate) {
+		this.isNot = negate;
+	}
+	
+	public boolean isNot() {
+		return this.isNot;
+	}
 }
