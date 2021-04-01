995b58e5db5e03e2012c2e1be5ef5297e98db4d9
mahkhalil <mahkhalil@deloitte.com>
2021-04-01 14:24:08 +0000

    UPDATE ::: Running SetFrameFilter(col==[]) or SetPanelFilter(col==[]) will end up invoking an unfilter instead of throwing an error
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@43292 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e8adec01fc7968621c0b4deb95ca70951bf8d771

--- src/prerna/sablecc2/reactor/frame/filter/SetFrameFilterReactor.java
+++ src/prerna/sablecc2/reactor/frame/filter/SetFrameFilterReactor.java
@@ -3,6 +3,9 @@ package prerna.sablecc2.reactor.frame.filter;
 import prerna.algorithm.api.ITableDataFrame;
 import prerna.query.querystruct.filters.BooleanValMetadata;
 import prerna.query.querystruct.filters.GenRowFilters;
+import prerna.query.querystruct.filters.IQueryFilter;
+import prerna.query.querystruct.filters.SimpleQueryFilter;
+import prerna.query.querystruct.selectors.QueryColumnSelector;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
@@ -23,8 +26,27 @@ public class SetFrameFilterReactor extends AbstractFilterReactor {
 		if (grf.isEmpty()) {
 			throw new IllegalArgumentException("No filter found to set to frame");
 		}
-		frame.setFilter(grf);
+		// check if we are filtering or actually removing a filter
+		if(grf.size() == 1) {
+			IQueryFilter singleFilter = grf.getFilters().get(0);
+			if(singleFilter instanceof SimpleQueryFilter) {
+				boolean unfilter = ((SimpleQueryFilter) singleFilter).isEmptyFilterValues();
+				if(unfilter) {
+					QueryColumnSelector cSelector = singleFilter.getAllQueryColumns().get(0);
+					boolean isValidFilter = frame.unfilter(cSelector.getAlias());
 
+					BooleanValMetadata fFilterVal = BooleanValMetadata.getFrameVal();
+					fFilterVal.setName(frame.getName());
+					fFilterVal.setFilterVal(isValidFilter);
+					NounMetadata noun = new NounMetadata(fFilterVal, PixelDataType.BOOLEAN_METADATA, PixelOperationType.FRAME_FILTER_CHANGE);
+					return noun;
+				}
+			}
+		}
+		
+		// we got to this point, apply the filter
+		frame.setFilter(grf);
+		
 		BooleanValMetadata fFilterVal = BooleanValMetadata.getFrameVal();
 		fFilterVal.setName(frame.getName());
 		fFilterVal.setFilterVal(true);
