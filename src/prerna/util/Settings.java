c29481e7262fe66853aeaa24bd3e02411ca48839
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2021-05-09 14:49:21 +0000

    Settings - Class for keeping all the constants in RDF_MAP - We can slowly start migrating all the settings to this constants class.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@43661 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/util/Settings.java
@@ -0,0 +1,13 @@
+package prerna.util;
+
+public class Settings {
+	
+	public static final String CHECK_MEM = "CHECK_MEM";
+	public static final String MEM_PROFILE_SETTINGS = "MEM_PROFILE_SETTINGS";
+	public static final String CONSTANT_MEM = "CONSTANT";
+	public static final String USER_MEM_LIMIT = "USER_MEM_LIMIT";
+	
+	
+	
+}
+
