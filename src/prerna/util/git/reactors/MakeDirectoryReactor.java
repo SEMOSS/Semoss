6bece136b27f35462c93a56f9b9bb3aaf1428341
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2021-06-04 15:03:41 +0000

    Updates to clone a. Do not allow new files / folders in app_root b. Add repos to a repo list in version folder so we can get to it c. Overload cd so that if the user cds we automatically clone repo for the User.java d. Do no allow clones within other gits.. no sub modules
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@43827 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8c3a4e3510c02636fd00bc57073d0da20d7b1091

--- src/prerna/util/git/reactors/MakeDirectoryReactor.java
+++ src/prerna/util/git/reactors/MakeDirectoryReactor.java
@@ -31,6 +31,10 @@ public class MakeDirectoryReactor extends AbstractReactor {
 		// specify the folder from the base
 		String folderName = keyValue.get(keysToGet[0]);
 		String space = this.keyValue.get(this.keysToGet[1]);
+		
+		if(!folderName.contains("/"))
+			return NounMetadata.getErrorNounMessage("You cannot create directory / files at this level");
+
 		// if security enables, you need proper permissions
 		// this takes in the insight and does a user check that the user has access to perform the operations
 		String baseFolder = AssetUtility.getAssetBasePath(this.insight, space, AbstractSecurityUtils.securityEnabled());
