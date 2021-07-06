10dcdd1afe8fb1479015a55f879e86299a9a488a
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2021-07-06 03:32:39 +0000

    Maven Updates - By default the maven project should be in assets/java - If there is no pom.xml. It will revert back to previous version The output is generated in app_root/target - pom should be configured for that 		<directory>../../../target</directory> should be placed inside build If the classes folder is newer, engine will automatically reload the classes In order for maven to work need the MVN_HOME, JAVA_HOME, REPO_HOME in the RDF_Map or environment.
    AbstractEngine.java - getReactorMvn, other methods 
    MvnReactor - Running Mvn by default it will run clean followed by compile. If other goals are needed try with Mvn(<goals>)
    ReactorKeysEnum - Updates to new enum for maven - Goals
    Settings - MVN_HOME, REPO_HOME, JAVA_HOME
    ReloadInsightClassesReactor - Fix to enable recompile specific to an engine ReloadInsightClasses(<engineid>)
    Utility - Loading maven dependencies before loading the reactor through custom classloader 
    CmdExecUtil - Updates to check cd before adding to the working dir 
    CommandReactor - Check for repolist 
    GlobalFileWatcher - Bugfix to check for null on lastModified 
    
    
    
    
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@44110 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/util/MvnReactor.java
@@ -0,0 +1,149 @@
+package prerna.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.maven.shared.invoker.DefaultInvocationRequest;
+import org.apache.maven.shared.invoker.DefaultInvoker;
+import org.apache.maven.shared.invoker.InvocationOutputHandler;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.InvocationResult;
+import org.apache.maven.shared.invoker.Invoker;
+import org.apache.maven.shared.invoker.MavenInvocationException;
+
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.sablecc2.reactor.ReactorFactory;
+
+
+public class MvnReactor extends AbstractReactor
+{
+	public MvnReactor() {
+		// need repository
+		// Oauth
+		// File name
+		// Content
+		this.keysToGet = new String[]{ReactorKeysEnum.MVN_GOALS.getKey()};
+		this.keyRequired = new int[] {0};
+	}
+
+	
+	
+	public NounMetadata execute()
+	{
+		organizeKeys();
+		// try to find if the system property is set or RDF_MAP has it
+		// JAVA_HOME
+		// MVN_HOME
+		// if not throw error
+		String javaHome = System.getProperty(Settings.JAVA_HOME);
+		if(javaHome == null)
+			javaHome = DIHelper.getInstance().getProperty(Settings.JAVA_HOME);
+		if(javaHome == null)
+			return NounMetadata.getErrorNounMessage("JAVA_HOME is not set, set as environment / RDF_MAP", null);		
+		System.setProperty("JAVA_HOME",  javaHome);
+
+		
+		String mvnHome = System.getProperty(Settings.MVN_HOME);
+		if(mvnHome == null)
+			mvnHome = DIHelper.getInstance().getProperty(Settings.MVN_HOME);
+		if(mvnHome == null)
+			return NounMetadata.getErrorNounMessage("MVN_HOME is not set, set as environment / RDF_MAP", null);
+
+		String workingDir = this.insight.getCmdUtil().getWorkingDir();
+		
+		InvocationRequest request = new DefaultInvocationRequest();
+		InvocationOutputHandler outputHandler = new InvocationOutputHandler(){
+			
+		File file = new File(workingDir + File.separator + "temp.mvn.output");
+		FileWriter fw = null;
+		
+        @Override
+        public void consumeLine(String line) throws IOException {
+
+        	if(fw == null)
+        	{
+        		System.err.println("File writer is null ");
+        		fw = new FileWriter(file);
+        	}
+        	fw.write(line + "\n");
+        	fw.flush();
+        	
+            if (line.contains("<libertySettingsFolder> must be a directory")) {
+                throw new IOException("Caught expected MojoExecutionException - " + line);
+            }
+        }
+	    };
+		Invoker invoker = new DefaultInvoker();		
+		invoker.setOutputHandler(outputHandler);
+		invoker.setMavenHome(new File(mvnHome));
+		
+		// path to maven executable		
+		String pomFileName = workingDir + File.separator + "pom.xml";
+		File pomFile = new File(pomFileName);
+		if(!pomFile.exists())
+			return NounMetadata.getErrorNounMessage("Not a maven project, please navigate to the dir where you have pom.xml", null);
+		request.setPomFile(pomFile);
+		List <String> goals = new ArrayList();
+		goals.add("clean");
+		goals.add("compile");
+		
+		if(keyValue.containsKey(keysToGet[0]))
+		{
+			String inputGoals = (String)keyValue.get(keysToGet[0]);
+			String [] goalTokens = inputGoals.split(" ");
+			
+			goals = Arrays.asList(goalTokens);
+		}
+		request.setGoals( goals);
+				 
+		try {
+			InvocationResult result;
+			result = invoker.execute( request );
+			if ( result.getExitCode() != 0 )
+			{
+				String errorMessage = composeErrorMessage(new File(workingDir + File.separator + "temp.mvn.output"));
+			    return new NounMetadata(errorMessage,PixelDataType.CONST_STRING);
+			}		
+		} catch (MavenInvocationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return new NounMetadata("Compile successful", PixelDataType.CONST_STRING);
+		 
+	}	
+	
+	
+	public String composeErrorMessage(File file)
+	{
+		StringBuffer output = new StringBuffer();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String data = null;
+			
+			while((data = br.readLine() ) != null)
+				output.append(data).append("\n");
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return output.toString();
+	}
+	
+}
