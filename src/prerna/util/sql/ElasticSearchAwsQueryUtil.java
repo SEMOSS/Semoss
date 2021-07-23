9606087aa32f0d0d0b0d2b5a72677c52d1e08694
mahkhalil <mahkhalil@deloitte.com>
2021-07-23 16:15:15 +0000

    UPDATE ::: Adding elastic search distinction between Native and AWS versions
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@44226 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3c3143ffbb8eb4843fab0e2aca5963c675ee1332

---
+++ src/prerna/util/sql/ElasticSearchAwsQueryUtil.java
@@ -0,0 +1,106 @@
+package prerna.util.sql;
+
+import java.util.Map;
+import java.util.Properties;
+
+public class ElasticSearchAwsQueryUtil extends AnsiSqlQueryUtil {
+	
+	ElasticSearchAwsQueryUtil() {
+		super();
+		setDbType(RdbmsTypeEnum.ELASTIC_SEARCH_AWS);
+	}
+	
+	ElasticSearchAwsQueryUtil(String connectionUrl, String username, String password) {
+		super(connectionUrl, username, password);
+		setDbType(RdbmsTypeEnum.ELASTIC_SEARCH_AWS);
+	}
+	
+	@Override
+	public String buildConnectionString(Map<String, Object> configMap) throws RuntimeException {
+		if(configMap.isEmpty()){
+			throw new RuntimeException("Configuration map is empty");
+		}
+		
+		String connectionString = (String) configMap.get(AbstractSqlQueryUtil.CONNECTION_STRING);
+		if(connectionString != null && !connectionString.isEmpty()) {
+			return connectionString;
+		}
+		
+		String urlPrefix = this.dbType.getUrlPrefix();
+		String httpType = (String) configMap.get(AbstractSqlQueryUtil.HTTP_TYPE);
+		if(httpType == null || httpType.isEmpty()) {
+			httpType = "https";
+		}
+		String hostname = (String) configMap.get(AbstractSqlQueryUtil.HOSTNAME);
+		if(hostname == null || hostname.isEmpty()) {
+			throw new RuntimeException("Must pass in a hostname");
+		}
+		
+		String port = (String) configMap.get(AbstractSqlQueryUtil.PORT);
+		if (port != null && !port.isEmpty()) {
+			port = ":" + port;
+		} else {
+			port = "";
+		}
+		
+		// example:
+		// jdbc:elasticsearch://https://remote-host-name?auth=aws_sigv4&region=us-west-1
+		connectionString = urlPrefix+"://"+httpType+"://"+hostname+port;
+		
+		String additonalProperties = (String) configMap.get(AbstractSqlQueryUtil.ADDITIONAL);
+		if(additonalProperties != null && !additonalProperties.isEmpty()) {
+			if(!additonalProperties.startsWith("?")) {
+				connectionString += "?" + additonalProperties;
+			} else {
+				connectionString += additonalProperties;
+			}
+		}
+		
+		return connectionString;
+	}
+
+	@Override
+	public String buildConnectionString(Properties prop) throws RuntimeException {
+		if(prop == null){
+			throw new RuntimeException("Properties ojbect is null");
+		}
+		
+		String connectionString = (String) prop.get(AbstractSqlQueryUtil.CONNECTION_STRING);
+		if(connectionString != null && !connectionString.isEmpty()) {
+			return connectionString;
+		}
+		
+		String urlPrefix = this.dbType.getUrlPrefix();
+		String hostname = (String) prop.get(AbstractSqlQueryUtil.HOSTNAME);
+		if(hostname == null || hostname.isEmpty()) {
+			throw new RuntimeException("Must pass in a hostname");
+		}
+		String httpType = (String) prop.get(AbstractSqlQueryUtil.HTTP_TYPE);
+		if(httpType == null || httpType.isEmpty()) {
+			httpType = "https";
+		}
+		
+		String port = (String) prop.get(AbstractSqlQueryUtil.PORT);
+		if (port != null && !port.isEmpty()) {
+			port = ":" + port;
+		} else {
+			port = "";
+		}
+		
+		// example:
+		// jdbc:es://http://server:3456/?timezone=UTC&page.size=250
+		connectionString = urlPrefix+"://"+httpType+"://"+hostname+port;
+		
+		String additonalProperties = (String) prop.get(AbstractSqlQueryUtil.ADDITIONAL);
+		if(additonalProperties != null && !additonalProperties.isEmpty()) {
+			if(!additonalProperties.startsWith("?")) {
+				connectionString += "?" + additonalProperties;
+			} else {
+				connectionString += additonalProperties;
+			}
+		}
+		
+		return connectionString;
+	}
+	
+}
