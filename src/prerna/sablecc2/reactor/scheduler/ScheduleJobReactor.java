e309c52ae5c9e0983931493183f61404060fdbc5
mahkhalil <mahkhalil@deloitte.com>
2021-08-25 17:11:55 +0000

    UPDATE ::: Changing from db to project for scheduler
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@44715 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/scheduler/ScheduleJobReactor.java
+++ src/prerna/sablecc2/reactor/scheduler/ScheduleJobReactor.java
@@ -28,7 +28,7 @@ import prerna.auth.AccessToken;
 import prerna.auth.AuthProvider;
 import prerna.auth.User;
 import prerna.auth.utils.SecurityAdminUtils;
-import prerna.auth.utils.SecurityDatabaseUtils;
+import prerna.auth.utils.SecurityProjectUtils;
 import prerna.rpa.config.ConfigurableJob;
 import prerna.rpa.config.IllegalConfigException;
 import prerna.rpa.config.JobConfig;
@@ -85,7 +85,7 @@ public class ScheduleJobReactor extends AbstractReactor {
 		// user must be an admin or editor of the app
 		// to add a scheduled job
 		User user = this.insight.getUser();
-		if(!SecurityAdminUtils.userIsAdmin(user) && !SecurityDatabaseUtils.userCanEditDatabase(user, jobGroup)) {
+		if(!SecurityAdminUtils.userIsAdmin(user) && !SecurityProjectUtils.userCanEditProject(user, jobGroup)) {
 			throw new IllegalArgumentException("User does not have proper permissions to schedule jobs");
 		}
 		
