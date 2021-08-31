201bb4f553baa399396df56e8fb3a244bf8d853d
mahkhalil <mahkhalil@deloitte.com>
2021-08-31 17:14:09 +0000

    UPDATE ::: Pushing additional reactors around pulling/utilizing assets from S3
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@44761 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 892b54d8e9b89daa1f92c071103a6f3d2535a6fc

--- src/prerna/sablecc2/reactor/qs/source/S3ListBucketsReactor.java
+++ src/prerna/sablecc2/reactor/qs/source/S3ListBucketsReactor.java
@@ -4,62 +4,47 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.SdkClientException;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
 
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
-import prerna.sablecc2.reactor.AbstractReactor;
 
-public class S3ListBucketsReactor  extends AbstractReactor{
+public class S3ListBucketsReactor extends AbstractS3Reactor {
 
 	public S3ListBucketsReactor() {
-		this.keysToGet = new String[] { "region" };
+		this.keysToGet = getS3KeysToGet(null);
 	}
+	
+	@Override
+	public String getReactorDescription() {
+		return "List the bucket names accessible in S3. Credentials can be optionally set via a profile path/name, or with an explicit access key and secret. Otherwise, credentials from environment variables or social properties are used.";
+	}
+	
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
-		String clientRegion = this.keyValue.get(this.keysToGet[0]);
+				
 		List<HashMap<String, Object>> bucketList = new ArrayList<HashMap<String, Object>>();
-
-
-		AWSCredentials creds = S3Utils.getInstance().getS3Creds();
-		if (creds != null );
 		try{
-			AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-					.withRegion(clientRegion)
-					.withCredentials(new AWSStaticCredentialsProvider(creds))
-					.build();
+			AmazonS3 s3Client = getS3Client();
+			
 			List<Bucket> buckets = s3Client.listBuckets();
 			for (Bucket b : buckets) {
 				HashMap<String, Object> tempMap = new HashMap<String, Object>();
 				tempMap.put("name", b.getName());
 				bucketList.add(tempMap);
-			    System.out.println("* " + b.getName());
+				System.out.println("* " + b.getName());
 			}
-		} catch (AmazonServiceException e) {
-			// The call was transmitted successfully, but Amazon S3 couldn't process 
-			// it, so it returned an error response.
-			e.printStackTrace();
 		} catch (SdkClientException e) {
-			// Amazon S3 couldn't be contacted for a response, or the client
-			// couldn't parse the response from Amazon S3.
 			e.printStackTrace();
-		}catch (Exception e) {
-			e.printStackTrace();
+			return getError("Error occurred listing buckets: " + e.getMessage());
 		}
 		
-	
-		// TODO Auto-generated method stub
 		NounMetadata noun = new NounMetadata(bucketList, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.S3);
 		return noun;
 	}
-
 
 }
