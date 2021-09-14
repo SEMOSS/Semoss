12de270fc389c38cbaf67ad7932edf7e44df818c
mahkhalil <mahkhalil@deloitte.com>
2021-09-14 21:11:57 +0000

    BUGFIX ::: Fixing typo in error message BUGFIX ::: Deleting unused/broken reactor that is replaced by Py,R,Java reactors
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@44896 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/r/RReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/RReactor.java
@@ -43,7 +43,7 @@ public final class RReactor extends AbstractRFrameReactor {
 		String disable_terminal =  DIHelper.getInstance().getProperty(Constants.DISABLE_TERMINAL);
 		if(disable_terminal != null && !disable_terminal.isEmpty() ) {
 			 if(Boolean.parseBoolean(disable_terminal)) {
-					throw new IllegalArgumentException("Terminal and user code execution has been disbled.");
+					throw new IllegalArgumentException("Terminal and user code execution has been disabled.");
 			 };
 		}
 		
