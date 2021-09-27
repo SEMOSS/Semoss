01891496ea5faf71345f6ce8eed19b92063df8ae
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2021-09-27 02:25:30 +0000

    Trying new things with Netty to try to make it fault tolerant.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@44990 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a5cbd9b3bce4884b8a3638ceb8d35b2b4fb41759

---
+++ src/prerna/tcp/SocketServer.java
@@ -0,0 +1,236 @@
+package prerna.tcp;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+
+import jep.Jep;
+import prerna.ds.py.PyExecutorThread;
+import prerna.sablecc2.reactor.frame.r.util.RJavaJriTranslator;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.Utility;
+
+public class SocketServer implements Runnable
+{
+	
+	// basically a process which works by looking for commands in TCP space
+	private static final String CLASS_NAME = SocketServer.class.getName();
+
+	List <String> commandList = new ArrayList<String>(); // this is giving the file name and that too relative
+	public  static Logger LOGGER = null;
+
+	String internalLock = "Internal Lock";
+	private static boolean first = true;
+	public Jep jep = null;
+	boolean SSL = false;
+	
+	public String [] command = null;
+	public Hashtable <String, Object> response = new Hashtable<String, Object>();
+	
+	public volatile boolean keepAlive = true;
+	public volatile boolean ready = false;
+	public Object driverMonitor = null;
+	
+	Properties prop = null; // this is basically reference to the RDF Map
+	List foldersBeingWatched = new ArrayList();
+	public String mainFolder = null;
+	PyExecutorThread pt = null;
+	RJavaJriTranslator rt = null;
+
+	static boolean test = false;
+	boolean done = false;
+	
+	Socket clientSocket = null;
+	ServerSocket serverSocket = null;
+	
+	InputStream is = null;
+	Object crash = new Object();
+	
+	SocketServerHandler ssh = new SocketServerHandler();
+	
+	public static void main(String [] args)
+	{
+		
+		
+		// arg1 - the directory where commands would be thrown
+		// arg2 - access to the rdf map to load
+		// arg3 - port to start
+		
+		// create the watch service
+		// start this thread
+		
+		// when event comes write it to the command
+		// comment this for main execution
+		//-Dlog4j.defaultInitOverride=TRUE
+		
+		if(args == null || args.length == 0)
+		{
+			args = new String[4];
+			args[0] = "C:\\users\\pkapaleeswaran\\workspacej3\\SemossDev\\config";
+			args[1] = "C:\\users\\pkapaleeswaran\\workspacej3\\SemossDev\\RDF_Map.prop";;
+			args[2] = "9999";
+			//args[3] = "py";
+			args[3] = "r";
+			test = true;
+				
+		}
+		
+		String log4JPropFile = Paths.get(Utility.normalizePath(args[0]), "log4j2.properties").toAbsolutePath().toString();
+		
+		FileInputStream fis = null;
+		ConfigurationSource source = null;
+		try {
+			fis = new FileInputStream(Utility.normalizePath(log4JPropFile));
+			source = new ConfigurationSource(fis);
+			//Configuration con = PropertiesConfigurationFactory.getInstance().getConfiguration(new LoggerContext(CLASS_NAME), source);
+			//LOGGER = con.getLoggerContext().getLogger(CLASS_NAME);
+			//LOGGER = Configurator.initialize(null, source).getLogger(CLASS_NAME);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if(fis != null) {
+				try {
+					fis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+		SocketServer worker = new SocketServer();
+		DIHelper.getInstance().loadCoreProp(args[1]);
+		DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
+		
+		worker.prop = new Properties();
+		
+		try(FileInputStream fileInput = new FileInputStream(Utility.normalizePath(args[1]))) {
+			worker.prop.load(fileInput);
+			System.out.println("Loaded the rdf map");
+			
+			// get the library for jep
+			//String jepLib = worker.prop.getProperty("JEP_LIB");
+			
+			//System.loadLibrary(jepLib);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		worker.mainFolder = args[0];
+		String engine = "r"; // setting it up for r
+		if(args.length == 4)
+			engine = args[3];
+		worker.bootServer(Integer.parseInt(args[2]), engine);
+	}
+	
+	public void startPyExecutor()
+	{
+		if(this.pt== null)
+		{
+			pt = new PyExecutorThread();
+			//pt.getJep();
+			pt.start();
+			
+			
+			while(!pt.isReady())
+			{
+				try {
+					// sleep until we get the py
+					Thread.sleep(200);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			LOGGER.info("PyThread Started");
+		}
+	}
+	
+	
+	public void bootServer(int PORT, String engine)
+	{
+		LOGGER = LogManager.getLogger(CLASS_NAME);
+
+        startPyExecutor();
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4444.");
+            System.exit(1);
+        }
+
+        Thread listenerThread = new Thread(this);
+        listenerThread.start();
+        
+	}
+	
+	
+	// start listening for connections
+	public void run()
+	{
+		// do the listening here and then spawn the thread
+		while(!done)
+		{
+			if(this.clientSocket == null)
+			{
+		        try {
+		            clientSocket = serverSocket.accept();
+		        } catch (IOException e) {
+		            System.err.println("Accept failed.");
+		            System.exit(1);
+		        }	
+		        try {
+			        ssh = new SocketServerHandler();
+		        	ssh.setLogger(LOGGER);
+					ssh.setOutputStream(clientSocket.getOutputStream());
+					is = clientSocket.getInputStream();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}   
+		        
+		        // start processing
+		        ssh.setPyExecutorThread(pt);
+		        ssh.is = is;
+		        ssh.socket = serverSocket;
+		        ssh.server = this;
+		        ssh.mainFolder = mainFolder;
+		        Thread readerThread = new Thread(ssh);
+		        readerThread.start();
+			}
+			else
+			{
+				// just sleep
+				// see if something crashed
+				synchronized(crash)
+				{
+					try {
+						crash.wait();
+						clientSocket = null;
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
+}
---
+++ src/prerna/tcp/client/SocketClientHandler.java
@@ -0,0 +1,160 @@
+package prerna.tcp.client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Vector;
+
+import org.apache.logging.log4j.Logger;
+
+import prerna.tcp.PayloadStruct;
+import prerna.util.FstUtil;
+
+public class SocketClientHandler implements Runnable
+{
+
+    String inputSoFar = "";
+    String endChar = "<o>";
+    
+	int offset = 4;
+	int totalBytes = 0;
+	List<ByteBuffer> inputs = new Vector<ByteBuffer>();
+	
+	boolean done = false;    
+	InputStream in = null;
+
+	byte[] lenBytes = null;
+	int lenBytesReadSoFar = 0;
+	byte[] curBytes = null;
+	int bytesReadSoFar = 0;
+	
+    
+    Client nc = null;
+	public  Logger LOGGER = null;
+
+    // I think we should move this also into stream reader or move stream reader here
+
+    public void setClient(Client nc)
+    {
+    	this.nc = nc;
+    }
+    
+
+	
+	public void printObject(Object obj)
+	{
+		// we know this is a payload struct
+		// just print it
+		PayloadStruct ps = (PayloadStruct)obj;
+		//System.err.println("<< Payload " + ps.epoc + " bytes left " + totalBytes);
+		
+		// this is where we inform the nc that this is done
+		// will come to it 
+		try
+		{
+			if(ps != null)
+			{
+				if(ps.ex != null)
+				{
+					System.out.println("Payload came with an exception " + ps.ex);
+					//throw ps.ex;
+				}
+				
+				if(ps.payload != null)
+				{
+					//System.out.println("Got the response for  " + ps.methodName + "  " + ps.epoc);
+				}
+				String id = ps.epoc;
+				
+				PayloadStruct lock = (PayloadStruct)nc.requestMap.remove(id);
+				
+				// put it in response
+				nc.responseMap.put(id, ps);
+				
+				
+				if(lock != null)
+				{
+					synchronized(lock)
+					{
+						lock.notifyAll();
+					}
+				}
+			}		
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}		
+	}
+	
+	public void setInputStream(InputStream in)
+	{
+		this.in = in;
+	}
+	
+	public void setLogger(Logger LOGGER)
+	{
+		this.LOGGER = LOGGER;
+	}
+	
+	@Override
+	public void run()
+	{
+		while(!done)
+		{
+			try
+			{
+				int bytesToRead = offset;
+				int readBytes = 0;
+				if(lenBytes != null)
+				{
+					bytesToRead  = ByteBuffer.wrap(lenBytes).getInt();
+					if(curBytes == null)
+						curBytes = new byte[bytesToRead]; // initialize only if it is not null
+
+					readBytes = in.read(curBytes, bytesReadSoFar, (curBytes.length - bytesReadSoFar)); // block
+					LOGGER.info("  Need bytes " + curBytes.length  + " <> Got bytes " + readBytes);
+					bytesReadSoFar = bytesReadSoFar + readBytes;
+					
+					if(bytesReadSoFar == curBytes.length && readBytes != -1)
+					{
+						try
+						{
+							Object retObject = FstUtil.deserialize(curBytes);
+							if(retObject != null)
+							{
+								printObject(retObject);
+								lenBytes = null;
+								bytesReadSoFar = 0;
+								lenBytesReadSoFar = 0;
+								curBytes = null;
+							}
+							else
+							{
+								LOGGER.info("Failed to deserialize " + curBytes.length + " <> bytes read " + readBytes);								
+							}
+						}catch(Exception ex)
+						{
+							LOGGER.info("Failed to deserialize " + curBytes.length + " <> bytes read " + readBytes);
+						}
+					}
+				}
+				else
+				{
+					if(lenBytes == null)
+						lenBytes = new byte[bytesToRead]; // block it
+					int bytesRead = in.read(lenBytes, lenBytesReadSoFar, (lenBytes.length - lenBytesReadSoFar)); // block
+					lenBytesReadSoFar = lenBytesReadSoFar + bytesRead;
+				}	
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				System.err.println("Time to die !!");
+				done = true; // ba bye
+				// at some point we can relisten if we want.. 
+			}
+		}
+	}
+
+
+
+}
