2dad267cb0a86765443737bf14958fb494366239
mahkhalil <mahkhalil@deloitte.com>
2021-09-28 23:05:35 +0000

    UPDATE ::: Adding additional sql query check if constraints exist before executing
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@45004 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f64ab137bc41631cc1a1c287c827b3e3eaf93eab

--- src/prerna/util/sql/MySQLQueryUtil.java
+++ src/prerna/util/sql/MySQLQueryUtil.java
@@ -153,6 +153,16 @@ public class MySQLQueryUtil extends AnsiSqlQueryUtil {
 	}
 	
 	@Override
+	public String tableConstraintExistsQuery(String constraintName, String tableName, String schema) {
+		return "SELECT CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE CONSTRAINT_NAME = '" + constraintName + "' AND TABLE_NAME = '" + tableName + "' AND CONSTRAINT_SCHEMA='" + schema + "'";
+	}
+
+	@Override
+	public String referentialConstraintExistsQuery(String constraintName, String schema) {
+		return "SELECT CONSTRAINT_NAME FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE CONSTRAINT_NAME = '" + constraintName + "' AND CONSTRAINT_SCHEMA='" + schema + "'";
+	}
+	
+	@Override
 	public String getAllColumnDetails(String tableName, String schema) {
 		return "SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA='" + schema + "' AND TABLE_NAME = '" + tableName + "';";
 	}
