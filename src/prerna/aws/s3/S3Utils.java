23da060c510b8440dc50ce60362aad24a923e9a7
mahkhalil <mahkhalil@deloitte.com>
2022-01-03 19:14:08 +0000

    UPDATE ::: Pushing updates around pushing/pulling from s3 in secured fashion and allowing creation of reactors that extend with custom security scheme
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@45432 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a505a286ef0748daceffde2d47bae4bb0bcfbe8e

--- src/prerna/aws/s3/S3Utils.java
+++ src/prerna/aws/s3/S3Utils.java
@@ -1,13 +1,9 @@
 package prerna.aws.s3;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSCredentialsProviderChain;
@@ -20,93 +16,88 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
-import prerna.aws.s3.S3Utils;
 import prerna.util.DIHelper;
 
 
 public class S3Utils {
-	
+
+	// shared reactor keys
+	public static final String BUCKET = "bucket";
 	public static final String ENDPOINT = "endpoint";
 	public static final String REGION = "region";
 	public static final String CONFIG_PATH = "configPath";
 	public static final String PROFILE = "profile";
 	public static final String ACCESS_KEY = "accessKey";
 	public static final String SECRET = "secret";
-	
-	private static final String AWS_ACCOUUNT = "aws_account";
-	private static final String AWS_KEY = "aws_key";
-	private static final String AWS_ENDPOINT = "aws_endpoint";
-	
-	private static Properties socialData = null;
-	private static S3Utils instance;
-	private static AWSCredentialsProviderChain awsCredsChain = null;
-	private static String awsEndpoint = null;
-	
+	public static final String NO_DEFAULT_CREDS = "noDefaultCreds";
+
+	// property keys
+	private static final String AWS_SHARED_ACCOUNT_KEY = "aws_shared_account";
+	private static final String AWS_SHARED_ACCESS_KEY = "aws_shared_key";
+	private static final String AWS_SHARED_ENDPOINT_KEY = "aws_shared_endpoint";
+
+	// loaded properties
+	private static S3Utils INSTANCE;
+	private static AWSCredentialsProviderChain AWS_CREDS_CHAIN = null;
+	private static String AWS_ENDPOINT = null;
+
 	private S3Utils() {
 		loadProps();
 	}
-	
-	public static S3Utils getInstance() throws IllegalArgumentException {
-		if(instance == null) {
-				instance = new S3Utils();
-			}
-		
-		return instance;
-	}
-	
-	private void loadProps() {
-		File f = new File(DIHelper.getInstance().getProperty("SOCIAL"));
-		FileInputStream fis = null;
 
-		try {
-			if(f.exists()) {
-				socialData = new Properties();
-				fis = new FileInputStream(f);
-				socialData.load(fis);
+	public static S3Utils getInstance() throws IllegalArgumentException {
+		if(INSTANCE == null) {
+				INSTANCE = new S3Utils();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if(fis != null) {
-				try {
-					fis.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}	
-		
-		// creds
+		return INSTANCE;
+	}
+
+	private void loadProps() {
+		Map<String, String> env = System.getenv();
+
+		// credentials
 		List<AWSCredentialsProvider> credProviders = new ArrayList<>();
-		String account = socialData.getProperty(AWS_ACCOUUNT);
-		String key = socialData.getProperty(AWS_KEY);
+		String account = null;
+		if (env.containsKey(AWS_SHARED_ACCOUNT_KEY)) {
+			account = env.get(AWS_SHARED_ACCOUNT_KEY);
+		} else if (DIHelper.getInstance().getProperty(AWS_SHARED_ACCOUNT_KEY) != null
+				&& !(DIHelper.getInstance().getProperty(AWS_SHARED_ACCOUNT_KEY).isEmpty())) {
+			account = DIHelper.getInstance().getProperty(AWS_SHARED_ACCOUNT_KEY);
+		}
+		String key = null;
+		if (env.containsKey(AWS_SHARED_ACCESS_KEY)) {
+			key = env.get(AWS_SHARED_ACCESS_KEY);
+		} else if (DIHelper.getInstance().getProperty(AWS_SHARED_ACCESS_KEY) != null
+				&& !(DIHelper.getInstance().getProperty(AWS_SHARED_ACCESS_KEY).isEmpty())) {
+			key = DIHelper.getInstance().getProperty(AWS_SHARED_ACCESS_KEY);
+		}
 		if (account != null && account.length() > 0 && key != null && key.length() > 0) {
 			credProviders.add(new AWSStaticCredentialsProvider(new BasicAWSCredentials(account, key)));
 		}
 		credProviders.add(new EnvironmentVariableCredentialsProvider());
-		awsCredsChain = new AWSCredentialsProviderChain(credProviders);
-		
+		AWS_CREDS_CHAIN = new AWSCredentialsProviderChain(credProviders);
+
 		// endpoint
-		String socialEndpoint = socialData.getProperty(AWS_ENDPOINT);
-		if (socialEndpoint != null && socialEndpoint.length() > 0) {
-			awsEndpoint = socialEndpoint;
-		} else if (System.getenv().containsKey(AWS_ENDPOINT)) {
-			awsEndpoint = System.getenv().get(AWS_ENDPOINT);
+		if (env.containsKey(AWS_SHARED_ENDPOINT_KEY)) {
+			AWS_ENDPOINT = env.get(AWS_SHARED_ENDPOINT_KEY);
+		} else if (DIHelper.getInstance().getProperty(AWS_SHARED_ENDPOINT_KEY) != null
+				&& !(DIHelper.getInstance().getProperty(AWS_SHARED_ENDPOINT_KEY).isEmpty())) {
+			AWS_ENDPOINT = DIHelper.getInstance().getProperty(AWS_SHARED_ENDPOINT_KEY);
 		}
 	}
-	
+
 	public AmazonS3 getS3Client(Map<String, String> keyValue) {
 		AmazonS3ClientBuilder clientBuilder = AmazonS3ClientBuilder.standard();
-		
+
 		// endpoint & region
 		String region = keyValue.get(REGION);
 		if(region == null || region.isEmpty()) {
 			region = Regions.DEFAULT_REGION.getName();
 		}
-		
+
 		String endpoint = keyValue.get(ENDPOINT);
 		if(endpoint == null || endpoint.isEmpty()) {
-			endpoint = awsEndpoint;
+			endpoint = AWS_ENDPOINT;
 		}
 		if(endpoint != null && !endpoint.isEmpty()) {
 			EndpointConfiguration endpointConfig = new EndpointConfiguration(endpoint, region);
@@ -114,7 +105,7 @@ public class S3Utils {
 		} else {
 			clientBuilder.setRegion(region);
 		}
-		
+
 		// credentials (first provided key & secret, then by profile)
 		List<AWSCredentialsProvider> credProviders = new ArrayList<>();
 		String accessKey = keyValue.get(ACCESS_KEY);
@@ -122,47 +113,47 @@ public class S3Utils {
 		if(accessKey != null && !accessKey.isEmpty() && secret != null && !secret.isEmpty()) {
 			credProviders.add(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secret)));
 		}
+
 		String configPath = keyValue.get(CONFIG_PATH);
 		String profileName = keyValue.get(PROFILE);
 		if(configPath != null && !configPath.isEmpty()) {
 			credProviders.add(new ProfileCredentialsProvider(configPath, profileName));
-		} else {
+		} else if (profileName != null && !profileName.isEmpty()) {
 			credProviders.add(new ProfileCredentialsProvider(profileName));
 		}
-		
-		// fall back on social.properties and/or env vars
-		AWSCredentialsProviderChain socialCredsChain = awsCredsChain;
-		if(socialCredsChain != null) {
-			credProviders.add(socialCredsChain);
-		} else {
-			credProviders.add(new EnvironmentVariableCredentialsProvider());
+
+		// fall back on core props and/or env vars unless not desired
+		Object noDefaultCreds = keyValue.get(NO_DEFAULT_CREDS);
+		if(noDefaultCreds == null || !Boolean.parseBoolean(noDefaultCreds.toString())) {
+			AWSCredentialsProviderChain sharedCredsChain = AWS_CREDS_CHAIN;
+			if(sharedCredsChain != null) {
+				credProviders.add(sharedCredsChain);
+			} else {
+				credProviders.add(new EnvironmentVariableCredentialsProvider());
+			}
 		}
-		
+
+		if(credProviders.isEmpty()) {
+			throw new IllegalArgumentException("No AWS credentials configured");
+		}
+
 		clientBuilder.setCredentials(new AWSCredentialsProviderChain(credProviders));
-		
+
 		return clientBuilder.build();
 	}
-	
-	public AWSCredentialsProviderChain getAwsCredsChain() {
-		return awsCredsChain;
-	}
-	
-	public String getAwsEndpoint() {
-		return awsEndpoint;
-	}
-	
+
 	public static String[] addCommonS3Keys(String[] additionalKeys) {
-		String[] base = new String[] { ENDPOINT, REGION, CONFIG_PATH, PROFILE, ACCESS_KEY, SECRET };
-		
+		String[] base = new String[] { ENDPOINT, REGION, CONFIG_PATH, PROFILE, ACCESS_KEY, SECRET, NO_DEFAULT_CREDS };
+
 		if(additionalKeys == null || additionalKeys.length == 0) {
 			return base;
 		}
-		
+
 		String[] both = Arrays.copyOf(additionalKeys, additionalKeys.length + base.length);
 		System.arraycopy(base, 0, both, additionalKeys.length, base.length);
 		return both;
 	}
-	
+
 	public static String getDescriptionForCommonS3Key(String key) {
 		if(key.equals(ENDPOINT)) {
 			return "Base service endpoint";
@@ -176,6 +167,8 @@ public class S3Utils {
 			return "Account access key";
 		} else if(key.equals(SECRET)) {
 			return "Account secret key";
+		} else if(key.equals(NO_DEFAULT_CREDS)) {
+			return "Flag to disallow usage of the global default AWS credentials. Defaults to false";
 		}
 		return null;
 	}
