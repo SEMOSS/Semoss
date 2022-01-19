4906ae21dceff7f352efef14c40bd011b8341eda
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2022-01-19 03:46:08 +0000

    List all the vars that have been set.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@45505 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 37cb5048af155f5de979087791d246687eb535fa

---
+++ src/prerna/sablecc2/reactor/ListVarReactor.java
@@ -0,0 +1,17 @@
+package prerna.sablecc2.reactor;
+
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+
+public class ListVarReactor extends AbstractReactor {
+
+	@Override
+	public NounMetadata execute() {
+		// TODO Auto-generated method stub
+		// real simple
+		// get all the vars as a vector
+		// pump it out
+		return new NounMetadata(insight.getAllVars(), PixelDataType.VECTOR);
+	}
+
+}
