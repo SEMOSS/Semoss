59c4259bf52bd7036e89d4ed7b2f8a5b7866f289
mahkhalil <mahkhalil@deloitte.com>
2022-02-10 22:38:16 +0000

    UPDATE ::: Changing from time unit to chrono unit for more time options
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@45637 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/date/reactor/DateManipulationReactor.java
+++ src/prerna/date/reactor/DateManipulationReactor.java
@@ -1,5 +1,9 @@
 package prerna.date.reactor;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -14,14 +18,12 @@ public class DateManipulationReactor extends AbstractReactor {
 
 	private static final String DEFAULT_FORMAT = "yyyy-MM-dd";
 
+	// how to use
 	// Frame() | Select( ...... ) | Filter (DATE_COLUMN_NAME >= DateManipulation("subtract", Date(), 7, "days") ) |
-
-	// different flows
+	// different flows 
 	// DateManipulation("add", Date(), 7, "days")
 	// DateManipulation("subtract", Date(), 7, "days")
 	// DateManipulation("diff", Date("2020-11-01"), Date("2020-11-12"), "days")
-
-	// "type" "date" "recurrence" "timeunit"
 
 	public DateManipulationReactor() {
 		this.keysToGet = new String[] { "type", "date", "recurrence", "timeunit" };
@@ -31,7 +33,7 @@ public class DateManipulationReactor extends AbstractReactor {
 	public NounMetadata execute() {
 		organizeKeys();
 		NounMetadata retNoun = null;
-		// get the type of date manipulation
+		// get manipulation type
 		if (this.keyValue.containsKey(this.keysToGet[0])) {
 			String type = this.keyValue.get(this.keysToGet[0]);
 			retNoun = performManip(type);
@@ -40,7 +42,6 @@ public class DateManipulationReactor extends AbstractReactor {
 	}
 
 	private NounMetadata performManip(String type) {
-
 		SemossDate startingDate = null;
 		SemossDate compareDate = null;
 		SemossDate dateToSendBack = null;
@@ -58,47 +59,45 @@ public class DateManipulationReactor extends AbstractReactor {
 			recurrence = this.keyValue.get(this.keysToGet[2]);
 		} 
 
-		// get the type of date manipulation
 		if (this.keyValue.containsKey(this.keysToGet[3])) {
 			timeunit = this.keyValue.get(this.keysToGet[3]);
 		}
 
 		// based on the type of operation we will do the specific date manipulations here
 		if (type.equalsIgnoreCase("add")) {
-			// check integer
+			// integer check
 			if (isInteger(recurrence)) {
 				recurranceInt = Integer.parseInt(recurrence);
 			}
 			// get calendar object
 			// perform manipulation
-			// return date
+			// return a date
 			Calendar c = Calendar.getInstance();
 			c.setTime(startingDate.getDate());
-			Date d = doDateAddition(c.getTime(), recurranceInt, TimeUnit.valueOf(timeunit.toUpperCase()),timeunit.toUpperCase());
+			Date d = dateTimeAddition(c.getTime(), recurranceInt, timeunit);
 			c.setTime(d);
 			dateToSendBack = new SemossDate(c.getTime(), DEFAULT_FORMAT);
 			return new NounMetadata(dateToSendBack, PixelDataType.CONST_DATE);
 
-		} else if (type.equalsIgnoreCase("subtract")) {
-			// check integer
+		} else if (type.equalsIgnoreCase("subtract") || type.equalsIgnoreCase("sub")) {
+			// integer check
 			if (isInteger(recurrence)) {
 				recurranceInt = Integer.parseInt(recurrence);
 			}
 			Calendar c = Calendar.getInstance();
 			c.setTime(startingDate.getDate());
-			Date d = doDateAddition(c.getTime(), -recurranceInt, TimeUnit.valueOf(timeunit.toUpperCase()),timeunit.toUpperCase());
+			Date d = dateTimeAddition(c.getTime(), -recurranceInt, timeunit);
 			c.setTime(d);
 			dateToSendBack = new SemossDate(c.getTime(), DEFAULT_FORMAT);
 			return new NounMetadata(dateToSendBack, PixelDataType.CONST_DATE);
 
-		} else if (type.equalsIgnoreCase("diff")) {
-			//get second date to do diff
+		} else if (type.equalsIgnoreCase("diff") || type.equalsIgnoreCase("difference")) {
 			compareDate = new SemossDate(recurrence, DEFAULT_FORMAT);
 			Calendar c = Calendar.getInstance();
 			c.setTime(startingDate.getDate());
 			Calendar cLater = Calendar.getInstance();
 			cLater.setTime(compareDate.getDate());
-			long l = getDateDiff(c.getTime(),cLater.getTime(),TimeUnit.valueOf(timeunit.toUpperCase()),timeunit.toUpperCase());
+			long l = totalTimeUnitsBetween(c.getTime(),cLater.getTime(),timeunit);
 			return new NounMetadata((int)l, PixelDataType.CONST_INT);
 		}
 		return null;
@@ -113,13 +112,20 @@ public class DateManipulationReactor extends AbstractReactor {
 		return false;
 	}
 	
-	private Date doDateAddition(Date date1, int valToAdd, TimeUnit timeUnit, String timeunit) {
-	    long additionInMillies = date1.getTime() + timeUnit.toMillis(timeUnit.convert((long) valToAdd,TimeUnit.valueOf(timeunit.toUpperCase())));
-	    return new Date(additionInMillies);
+	private Date dateTimeAddition(Date date1, int valToAdd, String timeUnit) {
+		LocalDateTime localdatetime1 = date1.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+		localdatetime1 = localdatetime1.plus(valToAdd, ChronoUnit.valueOf(timeUnit.toUpperCase()));
+	    return Date.from(localdatetime1.atZone(ZoneId.systemDefault()).toInstant());
 	}
 	
-	private long getDateDiff(Date date1, Date date2, TimeUnit timeUnit, String timeunit) {
-	    long diffInMillies = date2.getTime() - date1.getTime();
-	    return timeUnit.convert(diffInMillies,TimeUnit.MILLISECONDS);
+	private long totalTimeUnitsBetween(Date dateBefore, Date dateAfter, String timeUnit) {
+		try {
+			LocalDate localdate1 = dateBefore.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+			LocalDate localdate2 = dateAfter.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+			return ChronoUnit.valueOf(timeUnit.toUpperCase()).between(localdate1, localdate2);
+		} catch (Exception ex) { // for time units smaller than days time-units
+			long diffInMillies = dateAfter.getTime() - dateBefore.getTime();
+			return TimeUnit.valueOf(timeUnit.toUpperCase()).convert(diffInMillies, TimeUnit.MILLISECONDS);
+		}
 	}
 }
