33e8ef1871c9fea7edc0215e4f5a8f68449e509f
kunalppatel9 <kunalppatel9@deloitte.com>
2022-02-14 14:12:31 +0000

    UPDATE ::: adding in pyutils calls for tcp start server and removing import for securepy
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@45665 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/ds/py/PyUtils.java
+++ src/prerna/ds/py/PyUtils.java
@@ -123,6 +123,52 @@ public class PyUtils {
 		}
 		return null;
 	}
+	
+	
+	//chroot
+	public String startTCPServe(Object user, String chrootDir, String paramDir, String port)
+	{
+		if(user != null && !userTupleMap.containsKey(user)) // || (user != null && user instanceof User && !((User)user).getTCPServer(false).isConnected()))
+		{
+			try {
+				LOGGER.info(">>>STARTING PyServe USER<<<");
+				// going to create this in insight cache dir
+				//String mainCache = DIHelper.getInstance().getProperty(Constants.INSIGHT_CACHE_DIR);
+				Path chrootPath = Paths.get(chrootDir);
+				Path mainCachePath = Paths.get(chrootDir+paramDir);
+				Path tempDirForUser = Files.createTempDirectory(mainCachePath, "a");
+				String relative = chrootPath.relativize(tempDirForUser).toString();
+				if(!relative.startsWith("/")) {
+					relative ="/"+relative;
+				}
+				LOGGER.info(">>>Creating Temp Dir at " + tempDirForUser.toString() + " <<<");
+				writeLogConfigurationFile(tempDirForUser.toString(), relative);
+				userTupleMap.put(user, tempDirForUser.toString());
+				// this should possibly also launch the thread
+				String cp = DIHelper.getInstance().getProperty("TCP_WORKER_CP");
+				if(cp == null)
+					LOGGER.info("Unable to see class path ");
+				Process  p = Utility.startTCPServer(cp, chrootDir, relative.toString(), port);
+				
+				
+				if(p != null) {
+					userProcessMap.put(user,  p);
+					
+					// set the py process into the user
+					if(user instanceof User)
+						((User)user).setPyProcess(p);
+				}
+				LOGGER.info(">>>Pyserve Open on " + port + " <<<");
+				return tempDirForUser.toString();
+			} catch (Exception e) {
+				LOGGER.error(Constants.STACKTRACE, e);
+			}
+		}
+		else
+			LOGGER.info("=== TUPLE SPACE NOT CREATED ====");
+		return null;
+	}
+	
 
 	public String startTCPServe(Object user, String dir, String port)
 	{
@@ -172,6 +218,25 @@ public class PyUtils {
 			String logConfig = FileUtils.readFileToString(logFile);
 			//property.filename = target/rolling/rollingtest.log
 			logConfig = logConfig.replace("FILE_LOCATION", dir + "/output.log");
+			File newLogFile = new File(dir + "/log4j2.properties");
+			FileUtils.writeStringToFile(newLogFile, logConfig);
+		} catch (IOException e) {
+			LOGGER.error(Constants.STACKTRACE, e);
+		}
+		
+	}
+	
+	//chroot
+	private void writeLogConfigurationFile(String dir, String paramDir)
+	{
+		try {
+			// read the file first
+			dir = dir.replace("\\", "/");
+			String baseFolder = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
+			File logFile = new File(baseFolder + "/py/log-config/log4j.properties");
+			String logConfig = FileUtils.readFileToString(logFile);
+			//property.filename = target/rolling/rollingtest.log
+			logConfig = logConfig.replace("FILE_LOCATION", paramDir + "/output.log");
 			File newLogFile = new File(dir + "/log4j2.properties");
 			FileUtils.writeStringToFile(newLogFile, logConfig);
 		} catch (IOException e) {
