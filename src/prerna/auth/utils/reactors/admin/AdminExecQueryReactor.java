5f2e0da6a65394255124173e318ac8b2578f6bea
mahkhalil <mahkhalil@deloitte.com>
2022-02-18 13:37:46 +0000

    UPDATE ::: Adding user lock method
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@45687 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 31a68f413360bd8fa25f036eb6017f29bc7fd417

--- src/prerna/auth/utils/reactors/admin/AdminExecQueryReactor.java
+++ src/prerna/auth/utils/reactors/admin/AdminExecQueryReactor.java
@@ -1,4 +1,4 @@
-package prerna.auth.utils.admin.reactors;
+package prerna.auth.utils.reactors.admin;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
---
+++ src/prerna/auth/utils/reactors/admin/AdminLockAccountsReactor.java
@@ -0,0 +1,36 @@
+package prerna.auth.utils.reactors.admin;
+
+import prerna.auth.User;
+import prerna.auth.utils.SecurityAdminUtils;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class AdminLockAccountsReactor extends AbstractReactor {
+
+	public AdminLockAccountsReactor() {
+		this.keysToGet = new String[] {"days"};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		User user = this.insight.getUser();
+		SecurityAdminUtils adminUtils = SecurityAdminUtils.getInstance(user);
+		if(adminUtils == null) {
+			throw new IllegalArgumentException("User must be an admin to perform this function");
+		}
+		
+		organizeKeys();
+		int numDays = 90;
+		String input = this.keyValue.get(this.keysToGet[0]);
+		if(input != null && !(input = input.trim()).isEmpty()) {
+			numDays = (int) Double.parseDouble(input);
+		}
+		
+		int numLocked = adminUtils.lockAccounts(numDays);
+		NounMetadata noun = new NounMetadata(numLocked, PixelDataType.CONST_INT);
+		noun.addAdditionalReturn(getSuccess("Number of accounts locked = " + numLocked));
+		return noun;
+	}
+	
+}
