0b482871c017bca97b9f63c047ebe295729090fc
kunalppatel9 <kunalppatel9@deloitte.com>
2022-02-24 15:04:27 +0000

    UPDATE ::: adding options for individually turning off terminals UPDATE ::: checks for cfg on pushing to gitlab and sourcing scripts
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@45728 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3c51d19fff25e80cb591230a121b02224996dcbc

--- src/prerna/util/git/reactors/GitBaseReactor.java
+++ src/prerna/util/git/reactors/GitBaseReactor.java
@@ -7,12 +7,21 @@ import prerna.auth.AccessToken;
 import prerna.auth.AuthProvider;
 import prerna.auth.User;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
 
 public abstract class GitBaseReactor extends AbstractReactor {
 
 	public String getToken() {
 		User user = insight.getUser();
-		AccessToken gitAccess = user.getAccessToken(AuthProvider.GITHUB);
+		String gitProvider = DIHelper.getInstance().getProperty(Constants.GIT_PROVIDER);
+		AccessToken gitAccess = null;
+		if(gitProvider != null && !(gitProvider.isEmpty()) && gitProvider.toLowerCase().equals(AuthProvider.GITLAB.toString().toLowerCase())) {
+			 gitAccess = user.getAccessToken(AuthProvider.GITLAB);
+		} else {
+			 gitAccess = user.getAccessToken(AuthProvider.GITHUB);
+		}
+
 		if(gitAccess == null) {
 			Map<String, Object> retMap = new HashMap<String, Object>();
 			retMap.put("type", "git");
