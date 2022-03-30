44a13db04405ce34cb24afa51eb76f55edc1d28c
mahkhalil <mahkhalil@deloitte.com>
2022-03-30 14:59:11 +0000

    UPDATE ::: Adding frame in keysToGet for a few reactors that are missing
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@45912 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2286f1c112c88d8d1c18bcc803f76a99c5f118d3

--- src/prerna/sablecc2/reactor/frame/r/TrimColumnsReactor.java
+++ src/prerna/sablecc2/reactor/frame/r/TrimColumnsReactor.java
@@ -22,7 +22,7 @@ public class TrimColumnsReactor extends AbstractRFrameReactor {
 	 */
 	
 	public TrimColumnsReactor() {
-		this.keysToGet = new String[] { ReactorKeysEnum.COLUMNS.getKey() };
+		this.keysToGet = new String[] { ReactorKeysEnum.FRAME.getKey(), ReactorKeysEnum.COLUMNS.getKey() };
 	}
 
 	@Override
@@ -79,7 +79,7 @@ public class TrimColumnsReactor extends AbstractRFrameReactor {
 	private List<String> getColumns() {
 		List<String> columns = new Vector<String>();
 
-		GenRowStruct colGrs = this.store.getNoun(this.keysToGet[0]);
+		GenRowStruct colGrs = this.store.getNoun(this.keysToGet[1]);
 		if (colGrs != null && !colGrs.isEmpty()) {
 			for (int selectIndex = 0; selectIndex < colGrs.size(); selectIndex++) {
 				String column = colGrs.get(selectIndex) + "";
