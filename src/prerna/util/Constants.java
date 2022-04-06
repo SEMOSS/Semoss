00d463f37010f6489f5851d8a3dd48c5f59e4c9a
mahkhalil <mahkhalil@deloitte.com>
2022-04-06 02:10:07 +0000

    UPDATE ::: Adding logic to store the user id (first login method) in the session to be pulled into the access log file
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@45947 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a00afd2be4cc4f7bf4be82f9048ee742f6387ec6

--- src/prerna/util/Constants.java
+++ src/prerna/util/Constants.java
@@ -483,6 +483,7 @@ public class Constants {
 	public static final String USE_LOGOUT_PAGE = "use-logout-page";
 	public static final String CUSTOM_LOGOUT_URL = "custom-logout-url";
 	public static final String SESSION_USER = "semoss_user";
+	// this is so in the server.xml you can update the access-log to contain the userid by adding: %{log_semoss_user_id}s
 	public static final String SESSION_USER_ID_LOG = "log_semoss_user_id";
 	public static final String USER_WORKSPACE_IDS = "USER_WORKSPACE_IDS";
 	public static final String IS_ASSET_APP = "IS_ASSET_APP";
