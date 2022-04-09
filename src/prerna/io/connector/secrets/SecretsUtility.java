da4abb9d461c074ae21613b47d61beba97d35bee
mahkhalil <mahkhalil@deloitte.com>
2022-04-09 02:43:22 +0000

    UPDATE ::: Adding logic to encrypt non-frame files in insight caches
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@45979 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e15dc664fbefcc45daeb793f10a9be8fd4794077

---
+++ src/prerna/io/connector/secrets/SecretsUtility.java
@@ -0,0 +1,107 @@
+package prerna.io.connector.secrets;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import prerna.om.Insight;
+import prerna.util.Constants;
+
+public class SecretsUtility {
+
+	private static final Logger logger = LogManager.getLogger(SecretsUtility.class);
+
+	private SecretsUtility() {
+		
+	}
+	
+	public static Cipher generateCipherForInsight(String insightId, String projectName, String projectId) {
+		ISecrets secretsEngine = SecretsFactory.getSecretConnector();
+		if(secretsEngine == null) {
+			throw new IllegalArgumentException("Encryption services have not been enabled on this instance");
+		}
+		
+		String secret = UUID.randomUUID().toString();
+		String salt = UUID.randomUUID().toString();
+		byte[] iv = new byte[16];
+		Cipher cipher = null;
+		try {
+			SecureRandom randomSecureRandom = new SecureRandom();
+			randomSecureRandom.nextBytes(iv);
+			IvParameterSpec ivspec = new IvParameterSpec(iv);
+			
+			SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+			KeySpec spec = new PBEKeySpec(secret.toCharArray(), salt.getBytes(), 65536, 256);
+			SecretKey tmp = factory.generateSecret(spec);
+			SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+			cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+			cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+		} catch (NoSuchAlgorithmException | InvalidKeyException | InvalidAlgorithmParameterException | NoSuchPaddingException | InvalidKeySpecException e1) {
+			logger.error(Constants.STACKTRACE, e1);
+		}
+		if(cipher == null) {
+			throw new IllegalArgumentException("Unable to generate encryption details for the cache");
+		}
+		
+		Map<String, Object> cacheData = new HashMap<>();
+		cacheData.put(ISecrets.SECRET, secret);
+		cacheData.put(ISecrets.SALT, salt);
+		cacheData.put(ISecrets.IV, iv);
+		secretsEngine.writeInsightEncryptionSecrets(insightId, projectName, projectId, cacheData);
+		return cipher;
+	}
+	
+	public static Cipher retrieveCipherForInsight(Insight in) {
+		return retrieveCipherForInsight(in.getRdbmsId(), in.getProjectName(), in.getProjectId());
+	}
+	
+	public static Cipher retrieveCipherForInsight(String insightId, String projectName, String projectId) {
+		ISecrets secretsEngine = SecretsFactory.getSecretConnector();
+		if(secretsEngine == null) {
+			throw new IllegalArgumentException("Encryption services have not been enabled on this instance");
+		}
+		
+		Map<String, Object> cacheData = secretsEngine.getInsightEncryptionSecrets(insightId, projectName, projectId);
+		String secret = (String) cacheData.get(ISecrets.SECRET);
+		String salt = (String) cacheData.get(ISecrets.SALT);
+		byte[] iv = (byte[]) cacheData.get(ISecrets.IV);
+		Cipher cipher = null;
+		try {
+			IvParameterSpec ivspec = new IvParameterSpec(iv);
+			
+			SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+			KeySpec spec = new PBEKeySpec(secret.toCharArray(), salt.getBytes(), 65536, 256);
+			SecretKey tmp = factory.generateSecret(spec);
+			SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+			cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+			cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
+		} catch (NoSuchAlgorithmException | InvalidKeyException | InvalidAlgorithmParameterException | NoSuchPaddingException | InvalidKeySpecException e1) {
+			logger.error(Constants.STACKTRACE, e1);
+		}
+		if(cipher == null) {
+			throw new IllegalArgumentException("Unable to generate encryption details for the cache");
+		}
+		
+		return cipher;
+	}
+	
+}
