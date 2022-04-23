43cee5ac2cd4dd5bca8c88e2fb98fdef500bde37
mahkhalil <mahkhalil@deloitte.com>
2022-04-23 15:41:06 +0000

    UPDATE ::: Adding in mustache to compile for ToPdf
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46071 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 1171d263f0962b2990bfb877a233667a339a2f4d

--- src/prerna/sablecc2/reactor/export/ToPdfReactor.java
+++ src/prerna/sablecc2/reactor/export/ToPdfReactor.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
-import java.util.Vector;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -22,21 +25,28 @@ import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.SecurityQueryUtils;
 import prerna.om.InsightFile;
 import prerna.om.ThreadStore;
+import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.sablecc2.reactor.export.mustache.MustacheUtility;
 import prerna.util.Constants;
 import prerna.util.Utility;
 
 public class ToPdfReactor extends AbstractReactor {
 
+	private static final Logger classLogger = LogManager.getLogger(ToPdfReactor.class);
 	private static final String CLASS_NAME = ToPdfReactor.class.getName();
 
+	private static final String MUSTACHE = "mustache";
+	private static final String VARMAP = "varMap";
+	
 	public ToPdfReactor() {
 		this.keysToGet = new String[] { ReactorKeysEnum.HTML.getKey(), ReactorKeysEnum.FILE_NAME.getKey(),
-				ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.URL.getKey() };
+				ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.URL.getKey(),
+				MUSTACHE, VARMAP};
 	}
 
 	@Override
@@ -51,39 +61,55 @@ public class ToPdfReactor extends AbstractReactor {
 		// location for pdf resources
 		String insightFolder = this.insight.getInsightFolder();
 		String htmlToParse = this.keyValue.get(ReactorKeysEnum.HTML.getKey());
-		String feUrl = this.keyValue.get(ReactorKeysEnum.URL.getKey());
 		htmlToParse = Utility.decodeURIComponent(htmlToParse);
-		String sessionId = ThreadStore.getSessionId();
+		// see if using mustache template format that needs modifications
+		if(Boolean.parseBoolean(this.keyValue.get(MUSTACHE) + "")) {
+			Map<String, Object> variables = mustacheVariables();
+			try {
+				htmlToParse = MustacheUtility.compile(htmlToParse, variables);
+			} catch (Exception e) {
+				throw new IllegalArgumentException("Invalid mustache template or variables. Detailed error message = " + e.getMessage(), e);
+			}
+			classLogger.info("Exporting final html as: " + htmlToParse);
+		}
+		// keep track for deleting at the end
+		List<String> tempPaths = new ArrayList<>();
 
 		// Find semoss tags
 		Document doc = Jsoup.parse(htmlToParse);
 		Elements semossElements = doc.select("semoss");
-		// keep list of paths to clean up and delete once the pdf is created
-		Vector<String> tempPaths = new Vector<>();
-		// Process all semoss tags
-		int imageNum = 1;
-		for (Element element : semossElements) {
-			String url = element.attr("url");
-
-			// Run headless chrome with semossTagUrl
-			String imagePath = insightFolder + DIR_SEPARATOR + "image" + imageNum + ".png";
-			while(new File(imagePath).exists()) {
-				imageNum++;
-				imagePath = insightFolder + DIR_SEPARATOR + "image" + imageNum + ".png";
+		if(!semossElements.isEmpty()) {
+			String feUrl = this.keyValue.get(ReactorKeysEnum.URL.getKey());
+			if(feUrl == null || feUrl.isEmpty()) {
+				throw new IllegalArgumentException("Must pass in the URL for the UI");
 			}
-			logger.info("Generating image for PDF...");
-			this.insight.getChromeDriver().captureImage(feUrl, url, imagePath, sessionId);
-			tempPaths.add(imagePath);
-			logger.info("Done generating image for PDF...");
+			String sessionId = ThreadStore.getSessionId();
 
-			// Replace semoss tag with img tag
-			element.tagName("img");
-			// Replace url attribute with src attribute
-			element.removeAttr("url");
-			element.attr("src", "image" + imageNum + ".png");
-			imageNum++;
+			// keep list of paths to clean up and delete once the pdf is created
+			// Process all semoss tags
+			int imageNum = 1;
+			for (Element element : semossElements) {
+				String url = element.attr("url");
+	
+				// Run headless chrome with semossTagUrl
+				String imagePath = insightFolder + DIR_SEPARATOR + "image" + imageNum + ".png";
+				while(new File(imagePath).exists()) {
+					imageNum++;
+					imagePath = insightFolder + DIR_SEPARATOR + "image" + imageNum + ".png";
+				}
+				logger.info("Generating image for PDF...");
+				this.insight.getChromeDriver().captureImage(feUrl, url, imagePath, sessionId);
+				tempPaths.add(imagePath);
+				logger.info("Done generating image for PDF...");
+	
+				// Replace semoss tag with img tag
+				element.tagName("img");
+				// Replace url attribute with src attribute
+				element.removeAttr("url");
+				element.attr("src", "image" + imageNum + ".png");
+				imageNum++;
+			}
 		}
-
 		// Convert from html to xhtml
 		doc.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
 
@@ -169,5 +195,34 @@ public class ToPdfReactor extends AbstractReactor {
 		NounMetadata retNoun = new NounMetadata(downloadKey, PixelDataType.CONST_STRING, PixelOperationType.FILE_DOWNLOAD);
 		retNoun.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Successfully generated the tsv file"));
 		return retNoun;
+	}
+	
+	private Map<String, Object> mustacheVariables() {
+		GenRowStruct grs = this.store.getNoun(VARMAP);
+		if(grs != null && !grs.isEmpty()) {
+			Object obj = grs.get(0);
+			if(!(obj instanceof Map)) {
+				throw new IllegalArgumentException(VARMAP + " must be a map object");
+			}
+			return (Map<String, Object>) obj;
+		}
+		
+		List<Object> mapInput = this.curRow.getValuesOfType(PixelDataType.MAP);
+		if(mapInput != null && !mapInput.isEmpty()) {
+			return (Map<String, Object>) mapInput.get(0);
+		}
+		
+		return null;
+	}
+	
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if(key.equals(MUSTACHE)) {
+			return "Boolean if the html passed in is a mustache template";
+		} else if(key.equals(VARMAP)) {
+			return "Map containing the replacement values for a mustache tempalte. Must pass in mustache=true for this to be utilized";
+		}
+		
+		return super.getDescriptionForKey(key);
 	}
 }
