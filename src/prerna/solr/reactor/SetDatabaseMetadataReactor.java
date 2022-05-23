f265fa1d615a1b0750b9a9eb6b69a8916782b9a5
mahkhalil <mahkhalil@deloitte.com>
2022-05-23 14:49:07 +0000

    UPDATE ::: Adding logic such that the database metadata is more generic and the keys can be customized/set based on the FEs purview
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46217 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: bfaf5887889f5de84f7e63f30552869269d157b2

---
+++ src/prerna/solr/reactor/SetDatabaseMetadataReactor.java
@@ -0,0 +1,80 @@
+package prerna.solr.reactor;
+
+import java.util.List;
+import java.util.Map;
+
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.SecurityDatabaseUtils;
+import prerna.auth.utils.SecurityQueryUtils;
+import prerna.nameserver.utility.MasterDatabaseUtility;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.sablecc2.reactor.app.upload.UploadInputUtility;
+
+public class SetDatabaseMetadataReactor extends AbstractReactor {
+	
+	private static final String META = "meta";
+	
+	public SetDatabaseMetadataReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.DATABASE.getKey(), META};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String databaseId = UploadInputUtility.getDatabaseNameOrId(this.store);
+		
+		if(AbstractSecurityUtils.securityEnabled()) {
+			databaseId = SecurityQueryUtils.testUserDatabaseIdForAlias(this.insight.getUser(), databaseId);
+			if(!SecurityDatabaseUtils.userCanEditDatabase(this.insight.getUser(), databaseId)) {
+				throw new IllegalArgumentException("App does not exist or user does not have access to edit database");
+			}
+		} else {
+			databaseId = MasterDatabaseUtility.testDatabaseIdIfAlias(databaseId);
+		}
+		
+		Map<String, Object> metadata = getMetaMap();
+		SecurityDatabaseUtils.updateDatabaseMetadata(databaseId, metadata);
+		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
+		noun.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Successfully set the new metadata values for the database"));
+		return noun;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private Map<String, Object> getMetaMap() {
+		GenRowStruct mapGrs = this.store.getNoun(META);
+		if(mapGrs != null && !mapGrs.isEmpty()) {
+			List<NounMetadata> mapInputs = mapGrs.getNounsOfType(PixelDataType.MAP);
+			if(mapInputs != null && !mapInputs.isEmpty()) {
+				return (Map<String, Object>) mapInputs.get(0).getValue();
+			}
+		}
+
+		List<NounMetadata> mapInputs = this.curRow.getNounsOfType(PixelDataType.MAP);
+		if(mapInputs != null && !mapInputs.isEmpty()) {
+			return (Map<String, Object>) mapInputs.get(0).getValue();
+		}
+
+		throw new IllegalArgumentException("Must define a metadata map");
+	}
+	
+	@Override
+	public String getReactorDescription() {
+		return "Define metadata on an input datasource";
+	}
+	
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if(key.equals(META)) {
+			return "Map containing {'metaKey':['value1','value2', etc.]} containing the list of metadata values to define on the database. The list of values will determine the order that is defined for field";
+		}
+		return super.getDescriptionForKey(key);
+	}
+
+}
