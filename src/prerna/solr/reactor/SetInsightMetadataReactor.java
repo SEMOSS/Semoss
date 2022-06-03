340d1dadf519972cdb609f59a21b3f8be58e5833
mahkhalil <mahkhalil@deloitte.com>
2022-06-03 12:52:06 +0000

    UPDATE ::: Adding set insight metadata UPDATE ::: Editing some error message verbiage
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46255 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 0c74382b70d3865d6610cf22a2a0cfa991fa6bed

---
+++ src/prerna/solr/reactor/SetInsightMetadataReactor.java
@@ -0,0 +1,82 @@
+package prerna.solr.reactor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.SecurityInsightUtils;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.insights.AbstractInsightReactor;
+
+public class SetInsightMetadataReactor extends AbstractInsightReactor {
+	
+	private static final String META = "meta";
+	
+	public SetInsightMetadataReactor() {
+		this.keysToGet = new String[]{
+				ReactorKeysEnum.PROJECT.getKey(), ReactorKeysEnum.ID.getKey(), META
+			};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String projectId = this.getProject(); 
+		String insightId = this.getRdbmsId();
+		
+		if(AbstractSecurityUtils.securityEnabled()) {
+			if(!SecurityInsightUtils.userCanEditInsight(this.insight.getUser(), projectId, insightId)) {
+				throw new IllegalArgumentException("Insight does not exist or user does not have access to edit");
+			}
+		}
+		
+		Map<String, Object> metadata = getMetaMap();
+		
+		SecurityInsightUtils.updateInsightMetadata(projectId, insightId, metadata);
+		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
+		noun.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Successfully set the new metadata values for the insight"));
+		return noun;
+	}
+		
+	private Map<String, Object> getMetaMap() {
+		GenRowStruct mapGrs = this.store.getNoun(META);
+		if(mapGrs != null && !mapGrs.isEmpty()) {
+			List<NounMetadata> mapInputs = mapGrs.getNounsOfType(PixelDataType.MAP);
+			if (mapInputs != null && !mapInputs.isEmpty()) {
+				Map<String, Object> map = new HashMap<>();
+				for (NounMetadata nm : mapInputs) {
+					map.putAll((Map<String, Object>) nm.getValue());
+				}
+				return map;
+			}
+		}
+
+		List<NounMetadata> mapInputs = this.curRow.getNounsOfType(PixelDataType.MAP);
+		if(mapInputs != null && !mapInputs.isEmpty()) {
+			Map<String, Object> map = new HashMap<>();
+			for (NounMetadata nm : mapInputs) {
+				map.putAll((Map<String, Object>) nm.getValue());
+			}
+			return map;
+		}
+
+		throw new IllegalArgumentException("Must define a metadata map");
+	}
+	
+	@Override
+	public String getReactorDescription() {
+		return "Define metadata for an insight";
+	}
+	
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if(key.equals(META)) {
+			return "Map containing {'metaKey':['value1','value2', etc.]} containing the list of metadata values to define on the insight. The list of values will determine the order that is defined for field";
+		}
+		return super.getDescriptionForKey(key);
+	}
+}
