7aab38ae7ea780a60f7c302497af4fb51781ff99
mahkhalil <mahkhalil@deloitte.com>
2022-06-13 16:58:11 +0000

    UPDATE ::: Making reactors return lazy evaluators for methods being executed on project props to hide sensitive values from FE
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46307 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: eacbdc948e8464261c351545580a1e26bf8ef2d2

--- src/prerna/sablecc2/reactor/project/ReloadProjectPropertiesReactor.java
+++ src/prerna/sablecc2/reactor/project/ReloadProjectPropertiesReactor.java
@@ -1,23 +1,20 @@
 package prerna.sablecc2.reactor.project;
 
-import javax.mail.Session;
-
 import org.apache.commons.lang3.StringUtils;
 
 import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.SecurityProjectUtils;
 import prerna.project.api.IProject;
 import prerna.project.impl.ProjectProperties;
-import prerna.project.impl.ProjectPropertyEvaluator;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.util.Utility;
 
-public class GetEmailSessionReactor extends AbstractReactor {
+public class ReloadProjectPropertiesReactor extends AbstractReactor {
 		
-	public GetEmailSessionReactor() {
+	public ReloadProjectPropertiesReactor() {
 		this.keysToGet = new String[]{ReactorKeysEnum.PROJECT.getKey()};
 	}
 
@@ -36,21 +33,11 @@ public class GetEmailSessionReactor extends AbstractReactor {
 			}
 		}
 		
-		// make sure we have the value or throw a null pointer
 		IProject project = Utility.getProject(projectId);
 		ProjectProperties props = project.getProjectProperties();
-		
-		Session emailSession = props.getEmailSession();
-		if(emailSession == null) {
-			throw new IllegalArgumentException("Email Session is not defined for this project");
-		}
-
-		ProjectPropertyEvaluator eval = new ProjectPropertyEvaluator();
-		eval.setProjectId(projectId);
-		eval.setMethodName("getEmailSession");
-		NounMetadata noun = new NounMetadata(eval, PixelDataType.EMAIL_SESSION);
+		props.reloadProps();
+		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
+		noun.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Successfully set new properties for project"));
 		return noun;
 	}
-	
-
 }
