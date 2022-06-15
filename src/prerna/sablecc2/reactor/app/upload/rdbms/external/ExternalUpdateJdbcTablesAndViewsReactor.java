b51569c3e45ad3a11334002f8ef7a7aa878bb261
mahkhalil <mahkhalil@deloitte.com>
2022-06-15 19:36:32 +0000

    BUGFIX ::: Adding security checks for the refresh metadata
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46314 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 08a1b4bb544b6f4b6a853b5a3856910f4057e28c

--- src/prerna/sablecc2/reactor/app/upload/rdbms/external/ExternalUpdateJdbcTablesAndViewsReactor.java
+++ src/prerna/sablecc2/reactor/app/upload/rdbms/external/ExternalUpdateJdbcTablesAndViewsReactor.java
@@ -12,6 +12,10 @@ import java.util.Map;
 
 import org.apache.logging.log4j.Logger;
 
+import prerna.auth.User;
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.SecurityDatabaseUtils;
+import prerna.auth.utils.SecurityQueryUtils;
 import prerna.engine.api.IEngine;
 import prerna.engine.api.IRDBMSEngine;
 import prerna.engine.impl.rdbms.RdbmsConnectionHelper;
@@ -39,6 +43,13 @@ public class ExternalUpdateJdbcTablesAndViewsReactor extends AbstractReactor {
 
 		organizeKeys();
 		String databaseId = this.keyValue.get(this.keysToGet[0]);
+		if(AbstractSecurityUtils.securityEnabled()) {
+			User user = this.insight.getUser();
+			databaseId = SecurityQueryUtils.testUserDatabaseIdForAlias(user, databaseId);
+			if(!SecurityDatabaseUtils.userCanEditDatabase(user, databaseId)) {
+				throw new IllegalArgumentException("User does not have permission to edit this database schema");
+			}
+		}
 		IEngine database = Utility.getEngine(databaseId);
 		IRDBMSEngine nativeDatabase = null;
 		if(database instanceof IRDBMSEngine) {
