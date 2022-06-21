646e888fc7aa54653c07552a1b24873172db59a7
Maher Khalil <themaherkhalil@gmail.com>
2022-06-21 17:24:19 +0000

    UPDATE ::: Adding in logic for ClamAV virus scanning
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46359 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 51f43d52de6c34bb7d70e59f27feb4cf3b0c362f

---
+++ src/prerna/io/connector/antivirus/IVirusScanner.java
@@ -0,0 +1,13 @@
+package prerna.io.connector.antivirus;
+
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Map;
+
+public interface IVirusScanner {
+	
+	String CLAM_AV = "ClamAV";
+	
+	Map<String, Collection<String>> getViruses(InputStream is);
+
+}
---
+++ src/prerna/io/connector/antivirus/clamav/ClamAVScannerUtils.java
@@ -0,0 +1,121 @@
+package prerna.io.connector.antivirus.clamav;
+
+import java.io.InputStream;
+import java.nio.file.FileSystems;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import prerna.io.connector.antivirus.IVirusScanner;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import xyz.capybara.clamav.ClamavClient;
+import xyz.capybara.clamav.Platform;
+import xyz.capybara.clamav.commands.scan.result.ScanResult;
+
+public class ClamAVScannerUtils implements IVirusScanner {
+	
+	private static final Logger logger = LogManager.getLogger(ClamAVScannerUtils.class);
+	
+	public static final String CLAMAV_SCANNING_PORT = "CLAMAV_SCANNING_PORT";
+	public static final String CLAMAV_SCANNING_ADDRESS = "CLAMAV_SCANNING_ADDRESS";
+	public static final String CLAMAV_SCANNING_FS = "CLAMAV_SCANNING_FS";
+	
+	private static ClamAVScannerUtils instance;
+	private ClamavClient client;
+	
+	private ClamAVScannerUtils() throws Exception {
+		int port = Optional.ofNullable(getVirusScanningPort())
+				.orElseThrow(() -> new Exception("Port cannot be null"));
+		
+		String address = Optional.ofNullable(getVirusScanningAddress())
+				.orElseThrow(() -> new Exception("Address cannot be null"));
+
+		Platform platform = Optional.ofNullable(getVirusScanningFileSystem())
+				.orElseThrow(() -> new Exception("Platform cannot be null"));
+		
+		logger.info("address: " + port + " port: " + port + " platform: " + platform.toString());
+		
+		this.client = new ClamavClient(address, port, platform);
+	}
+
+	public static IVirusScanner getInstance() {
+		if(instance != null) {
+			return instance;
+		}
+
+		if(instance == null) {
+			synchronized(ClamAVScannerUtils.class) {
+				if(instance == null) {
+					try {
+						instance = new ClamAVScannerUtils();
+					} catch (Exception e) {
+						logger.error(Constants.STACKTRACE, e);
+					}
+				}
+			}
+		}
+
+		return instance;
+	}
+	
+	@Override
+	public Map<String, Collection<String>> getViruses(InputStream is) {
+		ScanResult sr = this.client.scan(is);
+		
+		if (sr instanceof ScanResult.OK) {
+			return new HashMap<String, Collection<String>>();
+		} else {
+			return ((ScanResult.VirusFound) sr).getFoundViruses();
+		}
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static String getVirusScanningAddress() {
+		return DIHelper.getInstance().getProperty(CLAMAV_SCANNING_ADDRESS);
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static Integer getVirusScanningPort() {
+		String virusScanning = DIHelper.getInstance().getProperty(CLAMAV_SCANNING_PORT);
+		if(virusScanning == null) {
+			// default configuration is false
+			return null;
+		}
+		
+		return Integer.valueOf(virusScanning);
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public static Platform getVirusScanningFileSystem() {
+		String platform = DIHelper.getInstance().getProperty(CLAMAV_SCANNING_FS);
+
+		if ("WINDOWS".equalsIgnoreCase(platform)) {
+			return Platform.WINDOWS;	
+		} else if ("UNIX".equalsIgnoreCase(platform)) {
+			return Platform.UNIX;
+		} else {
+			String sep = FileSystems.getDefault().getSeparator();
+			if (sep.equals("/")) {	
+				return Platform.UNIX;
+			} else if (sep.equals("\\")) {
+				return Platform.WINDOWS;
+			} else {
+				return null;
+			}
+		}
+	}
+}
