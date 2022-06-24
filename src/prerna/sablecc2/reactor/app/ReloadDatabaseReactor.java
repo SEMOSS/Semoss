1dbe90148450585a96e579515c4092835c0adc6d
mahkhalil <mahkhalil@deloitte.com>
2022-06-24 12:11:15 +0000

    UPDATE ::: Adding reload database reactor and changing file package location
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46383 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 924b24aa08ce35723e4a10b752eb6e0750e59c67

---
+++ src/prerna/sablecc2/reactor/app/ReloadDatabaseReactor.java
@@ -0,0 +1,46 @@
+package prerna.sablecc2.reactor.app;
+
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.SecurityAdminUtils;
+import prerna.auth.utils.SecurityDatabaseUtils;
+import prerna.engine.api.IEngine;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Utility;
+
+public class ReloadDatabaseReactor extends AbstractReactor {
+
+	public ReloadDatabaseReactor() {
+		this.keysToGet = new String[] {ReactorKeysEnum.DATABASE.getKey()};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String databaseId = this.keyValue.get(this.keysToGet[0]);
+		
+		// make sure user has at least edit access
+		if (AbstractSecurityUtils.securityEnabled()) {
+			if(!SecurityAdminUtils.userIsAdmin(this.insight.getUser())) {
+				if (!SecurityDatabaseUtils.userCanEditDatabase(this.insight.getUser(), databaseId)) {
+					throw new IllegalArgumentException("User does not have permission to reload the database");
+				}
+			}
+		}
+		
+		IEngine database = Utility.getEngine(databaseId);
+		String propFile = database.getPropFile();
+		database.closeDB();
+		database.setProp(null);
+		try {
+			database.openDB(propFile);
+		} catch (Exception e1) {
+			throw new IllegalArgumentException("An error occurred reloading the database. Please reach out to an administrator for assistance");
+		}
+		
+		return new NounMetadata(true, PixelDataType.BOOLEAN);
+	}
+
+}
