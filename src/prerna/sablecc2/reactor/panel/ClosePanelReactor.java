2c42618ff5b251bfd993844889aa3ec08a54f0c4
mahkhalil <mahkhalil@deloitte.com>
2022-08-10 15:16:23 +0000

    UPDATE ::: Adding initial logic to allow caching of the filter options into a single column frame UPDATE ::: Not adding the select all filter to the filter list since that is the default state
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46669 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 63bfbcb7b0b6fb0c8e60e1f2d5a74b3b43a9a0bd

--- src/prerna/sablecc2/reactor/panel/ClosePanelReactor.java
+++ src/prerna/sablecc2/reactor/panel/ClosePanelReactor.java
@@ -1,13 +1,22 @@
 package prerna.sablecc2.reactor.panel;
 
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import prerna.algorithm.api.ITableDataFrame;
 import prerna.om.InsightPanel;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Constants;
 
 public class ClosePanelReactor extends AbstractReactor {
+	
+	private static final Logger logger = LogManager.getLogger(ClosePanelReactor.class);
 	
 	public ClosePanelReactor() {
 		this.keysToGet = new String[]{ReactorKeysEnum.PANEL.getKey()};
@@ -20,6 +29,14 @@ public class ClosePanelReactor extends AbstractReactor {
 		InsightPanel panelToDelete = this.insight.getInsightPanels().remove(panelId);
 		if(panelToDelete == null) {
 			throw new IllegalArgumentException("Could not find panelId = " + panelId + " to close.");
+		}
+		Map<String, ITableDataFrame> filterCaches = panelToDelete.getCachedFilterModelFrame();
+		for(String key : filterCaches.keySet()) {
+			try {
+				filterCaches.get(key).close();
+			} catch(Exception e) {
+				logger.error(Constants.STACKTRACE, e);
+			}
 		}
 		return new NounMetadata(panelId, PixelDataType.CONST_STRING, PixelOperationType.PANEL_CLOSE);
 	}
