e26e368be6982b8a9ed68f0cc3859f2d6f6051c4
mahkhalil <mahkhalil@deloitte.com>
2022-08-19 20:19:29 +0000

    UPDATE ::: Removing itextpdf from pom and unused code referencing it
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46723 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 97d91a3db3689aa4280547abd3204057de2e9f4e

--- src/prerna/ui/main/listener/impl/GraphImageExportListener.java
+++ src/prerna/ui/main/listener/impl/GraphImageExportListener.java
@@ -44,11 +44,11 @@ import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTabbedPane;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Image;
-import com.itextpdf.text.Rectangle;
-import com.itextpdf.text.pdf.PdfWriter;
+//import com.itextpdf.text.Document;
+//import com.itextpdf.text.DocumentException;
+//import com.itextpdf.text.Image;
+//import com.itextpdf.text.Rectangle;
+//import com.itextpdf.text.pdf.PdfWriter;
 
 import prerna.ui.components.LegendPanel2;
 import prerna.ui.components.api.IChakraListener;
@@ -73,142 +73,142 @@ public class GraphImageExportListener extends AbstractAction implements IChakraL
 	 */
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		Component source = (Component) arg0.getSource(); //Export (>>) button
-		JPanel cont = (JPanel) source.getParent().getParent().getParent().getParent(); //Overall container JPanel
-		
-		JPanel jp = (JPanel) cont.getComponent(0);
-		JTabbedPane tabbedPane = (JTabbedPane) cont.getComponent(1);
-		
-		LegendPanel2 legend = (LegendPanel2) jp.getComponent(0);
-		JProgressBar progressBar = (JProgressBar) jp.getComponent(1);
-		
-		Object[] options = {"High Quality (*.eps)", "Low Quality (*.png)", "PDF"};
-		//0 = High Quality, 1 = Low Quality, 2 = PDF
-		int n = JOptionPane.showOptionDialog(cont,"Please choose the type of export: ",
-				"Export", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
-		
-		String exportType = "";
-		switch(n) {
-			case -1: return;
-			case 0: exportType = this.HIGH_QUALITY_IMAGE_TYPE;
-								 break;
-			case 1: exportType = this.LOW_QUALITY_IMAGE_TYPE;
-								 break;
-			case 2: exportType = this.PDF_TYPE;
-								 break;
-		}
-		
-		FileOutputStream graphicsFileOut = null;
-		try {
-			String workingDir = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
-			String folder = FILE_SEPARATOR+"export"+FILE_SEPARATOR+"Images"+FILE_SEPARATOR;
-			String writeFileName = "Graph_Export_" + DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT).format(new Date()).replace(":", "");
-			if(exportType.equals(this.HIGH_QUALITY_IMAGE_TYPE)) {
-				writeFileName += ".eps";
-			} else {
-				writeFileName += ".png";
-			}
-			
-			//Create folders if necessary to export to correct path
-			String fileLoc = workingDir + folder;
-			File exportFile = new File(fileLoc);
-			if(!exportFile.getCanonicalFile().isDirectory()) {
-				if(!exportFile.mkdirs())
-				Utility.showError("Please create the following directory structure in the working folder where SEMOSS currently resides:\n\n~"+FILE_SEPARATOR+"export"+FILE_SEPARATOR+"Images"+FILE_SEPARATOR);
-				return;
-			}
-			fileLoc += writeFileName;
-			
-			int boundingBoxMinWidth = 0;
-			int boundingBoxMinHeight = 0; //Crop out search panel
-			int boundingBoxMaxWidth = cont.getWidth();
-			int boundingBoxMaxHeight = cont.getHeight() - progressBar.getHeight(); //Crop out progress bar
-			
-			if(exportType.equals(this.HIGH_QUALITY_IMAGE_TYPE)) {
-				graphicsFileOut = new FileOutputStream(fileLoc);
-//				Graphics2D g = new EpsGraphics2D("Graph Export",graphicsFileOut, boundingBoxMinWidth, boundingBoxMinHeight, boundingBoxMaxWidth, boundingBoxMaxHeight);
-//				cont.paint(g);
-			} else {
-				BufferedImage im = new BufferedImage(boundingBoxMaxWidth, boundingBoxMaxHeight, BufferedImage.TYPE_INT_ARGB);
-				cont.paint(im.getGraphics());
-				ImageIO.write(im, "PNG", new File(fileLoc));
-
-				if(exportType.equals(this.PDF_TYPE)) {
-					FileOutputStream fileOut = null;
-					try {
-						Image image1 = Image.getInstance(fileLoc);
-						Rectangle r;
-						if(image1.getHeight() > this.MAX_DIM) {
-							r = new Rectangle((int)image1.getWidth(), (int) MAX_DIM);
-						} else if(image1.getWidth() > this.MAX_DIM) {
-							r = new Rectangle((int) MAX_DIM, (int)image1.getHeight());
-						} else {
-							r = new Rectangle((int)image1.getWidth()+20, (int)image1.getHeight()+20);
-						}
-
-						Document document = new Document(r, 15, 25, 15, 25);
-						fileOut = new FileOutputStream(fileLoc.replace(this.LOW_QUALITY_IMAGE_TYPE.toLowerCase(), this.PDF_TYPE.toLowerCase()));
-						PdfWriter.getInstance(document, fileOut);
-						document.open();
-
-						int pages = (int) Math.ceil((double)im.getHeight() / this.MAX_DIM);
-						System.out.println(im.getHeight());
-						System.out.println(im.getHeight()/this.MAX_DIM);
-						System.out.println(Math.ceil(im.getHeight() / this.MAX_DIM));
-						if(pages == 0)
-							pages = 1;
-						for(int i = 0; i < pages; i++) {
-							BufferedImage temp;
-							if(i < pages-1) {
-								temp = im.getSubimage(0, i*(int)this.MAX_DIM, im.getWidth(), (int)this.MAX_DIM);
-							} else {
-								temp = im.getSubimage(0, i*(int)this.MAX_DIM, im.getWidth(), im.getHeight() % (int)this.MAX_DIM);
-							}
-							File tempFile = new File(i+this.LOW_QUALITY_IMAGE_TYPE);
-							ImageIO.write(temp, this.LOW_QUALITY_IMAGE_TYPE, tempFile);
-							Image croppedImage = Image.getInstance(i+this.LOW_QUALITY_IMAGE_TYPE);
-							document.add(croppedImage);
-							tempFile.delete();
-							
-							if(i < pages-1) {
-								document.newPage();
-							}
-						}
-						document.close();
-
-						File f = new File(fileLoc);
-						f.delete();
-					}catch(RuntimeException e) {
-						e.printStackTrace();
-					} catch (DocumentException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}finally{
-						try{
-							if(fileOut!=null)
-								fileOut.close();
-						}catch(IOException e) {
-							e.printStackTrace();
-						}
-					}
-				} // end if PDF
-			} // end else
-			
-			if(exportType.equals(this.PDF_TYPE)) {
-				Utility.showMessage("Export successful: " + fileLoc.replace(this.LOW_QUALITY_IMAGE_TYPE.toLowerCase(), this.PDF_TYPE.toLowerCase()));
-			} else {
-				Utility.showMessage("Export successful: " + fileLoc);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally{
-			try{
-				if(graphicsFileOut!=null)
-					graphicsFileOut.close();
-			}catch(IOException e) {
-				e.printStackTrace();
-			}
-		}
+//		Component source = (Component) arg0.getSource(); //Export (>>) button
+//		JPanel cont = (JPanel) source.getParent().getParent().getParent().getParent(); //Overall container JPanel
+//		
+//		JPanel jp = (JPanel) cont.getComponent(0);
+//		JTabbedPane tabbedPane = (JTabbedPane) cont.getComponent(1);
+//		
+//		LegendPanel2 legend = (LegendPanel2) jp.getComponent(0);
+//		JProgressBar progressBar = (JProgressBar) jp.getComponent(1);
+//		
+//		Object[] options = {"High Quality (*.eps)", "Low Quality (*.png)", "PDF"};
+//		//0 = High Quality, 1 = Low Quality, 2 = PDF
+//		int n = JOptionPane.showOptionDialog(cont,"Please choose the type of export: ",
+//				"Export", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[1]);
+//		
+//		String exportType = "";
+//		switch(n) {
+//			case -1: return;
+//			case 0: exportType = this.HIGH_QUALITY_IMAGE_TYPE;
+//								 break;
+//			case 1: exportType = this.LOW_QUALITY_IMAGE_TYPE;
+//								 break;
+//			case 2: exportType = this.PDF_TYPE;
+//								 break;
+//		}
+//		
+//		FileOutputStream graphicsFileOut = null;
+//		try {
+//			String workingDir = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
+//			String folder = FILE_SEPARATOR+"export"+FILE_SEPARATOR+"Images"+FILE_SEPARATOR;
+//			String writeFileName = "Graph_Export_" + DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT).format(new Date()).replace(":", "");
+//			if(exportType.equals(this.HIGH_QUALITY_IMAGE_TYPE)) {
+//				writeFileName += ".eps";
+//			} else {
+//				writeFileName += ".png";
+//			}
+//			
+//			//Create folders if necessary to export to correct path
+//			String fileLoc = workingDir + folder;
+//			File exportFile = new File(fileLoc);
+//			if(!exportFile.getCanonicalFile().isDirectory()) {
+//				if(!exportFile.mkdirs())
+//				Utility.showError("Please create the following directory structure in the working folder where SEMOSS currently resides:\n\n~"+FILE_SEPARATOR+"export"+FILE_SEPARATOR+"Images"+FILE_SEPARATOR);
+//				return;
+//			}
+//			fileLoc += writeFileName;
+//			
+//			int boundingBoxMinWidth = 0;
+//			int boundingBoxMinHeight = 0; //Crop out search panel
+//			int boundingBoxMaxWidth = cont.getWidth();
+//			int boundingBoxMaxHeight = cont.getHeight() - progressBar.getHeight(); //Crop out progress bar
+//			
+//			if(exportType.equals(this.HIGH_QUALITY_IMAGE_TYPE)) {
+//				graphicsFileOut = new FileOutputStream(fileLoc);
+////				Graphics2D g = new EpsGraphics2D("Graph Export",graphicsFileOut, boundingBoxMinWidth, boundingBoxMinHeight, boundingBoxMaxWidth, boundingBoxMaxHeight);
+////				cont.paint(g);
+//			} else {
+//				BufferedImage im = new BufferedImage(boundingBoxMaxWidth, boundingBoxMaxHeight, BufferedImage.TYPE_INT_ARGB);
+//				cont.paint(im.getGraphics());
+//				ImageIO.write(im, "PNG", new File(fileLoc));
+//
+//				if(exportType.equals(this.PDF_TYPE)) {
+//					FileOutputStream fileOut = null;
+//					try {
+//						Image image1 = Image.getInstance(fileLoc);
+//						Rectangle r;
+//						if(image1.getHeight() > this.MAX_DIM) {
+//							r = new Rectangle((int)image1.getWidth(), (int) MAX_DIM);
+//						} else if(image1.getWidth() > this.MAX_DIM) {
+//							r = new Rectangle((int) MAX_DIM, (int)image1.getHeight());
+//						} else {
+//							r = new Rectangle((int)image1.getWidth()+20, (int)image1.getHeight()+20);
+//						}
+//
+//						Document document = new Document(r, 15, 25, 15, 25);
+//						fileOut = new FileOutputStream(fileLoc.replace(this.LOW_QUALITY_IMAGE_TYPE.toLowerCase(), this.PDF_TYPE.toLowerCase()));
+//						PdfWriter.getInstance(document, fileOut);
+//						document.open();
+//
+//						int pages = (int) Math.ceil((double)im.getHeight() / this.MAX_DIM);
+//						System.out.println(im.getHeight());
+//						System.out.println(im.getHeight()/this.MAX_DIM);
+//						System.out.println(Math.ceil(im.getHeight() / this.MAX_DIM));
+//						if(pages == 0)
+//							pages = 1;
+//						for(int i = 0; i < pages; i++) {
+//							BufferedImage temp;
+//							if(i < pages-1) {
+//								temp = im.getSubimage(0, i*(int)this.MAX_DIM, im.getWidth(), (int)this.MAX_DIM);
+//							} else {
+//								temp = im.getSubimage(0, i*(int)this.MAX_DIM, im.getWidth(), im.getHeight() % (int)this.MAX_DIM);
+//							}
+//							File tempFile = new File(i+this.LOW_QUALITY_IMAGE_TYPE);
+//							ImageIO.write(temp, this.LOW_QUALITY_IMAGE_TYPE, tempFile);
+//							Image croppedImage = Image.getInstance(i+this.LOW_QUALITY_IMAGE_TYPE);
+//							document.add(croppedImage);
+//							tempFile.delete();
+//							
+//							if(i < pages-1) {
+//								document.newPage();
+//							}
+//						}
+//						document.close();
+//
+//						File f = new File(fileLoc);
+//						f.delete();
+//					}catch(RuntimeException e) {
+//						e.printStackTrace();
+//					} catch (DocumentException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}finally{
+//						try{
+//							if(fileOut!=null)
+//								fileOut.close();
+//						}catch(IOException e) {
+//							e.printStackTrace();
+//						}
+//					}
+//				} // end if PDF
+//			} // end else
+//			
+//			if(exportType.equals(this.PDF_TYPE)) {
+//				Utility.showMessage("Export successful: " + fileLoc.replace(this.LOW_QUALITY_IMAGE_TYPE.toLowerCase(), this.PDF_TYPE.toLowerCase()));
+//			} else {
+//				Utility.showMessage("Export successful: " + fileLoc);
+//			}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}finally{
+//			try{
+//				if(graphicsFileOut!=null)
+//					graphicsFileOut.close();
+//			}catch(IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
 	}
 
 	/**
