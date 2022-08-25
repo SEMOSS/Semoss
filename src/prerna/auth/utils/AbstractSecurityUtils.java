fb0f1d314fb1b8821d283f47a9caf756b70f0c29
mahkhalil <mahkhalil@deloitte.com>
2022-08-25 16:25:54 +0000

    BUGFIX ::: Changing from tags to tag to default the options
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46799 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 1f1b6b16bb8b355f941854bb12ef3ff8e6c65852

--- src/prerna/auth/utils/AbstractSecurityUtils.java
+++ src/prerna/auth/utils/AbstractSecurityUtils.java
@@ -1185,7 +1185,7 @@ public abstract class AbstractSecurityUtils {
 					if(wrapper.hasNext()) {
 						int numrows = ((Number) wrapper.next().getValues()[0]).intValue();
 						if(numrows == 0) {
-							securityDb.insertData(queryUtil.insertIntoTable(tableName, colNames, types, new Object[]{"tags", "multi", 0, "checklist"}));
+							securityDb.insertData(queryUtil.insertIntoTable(tableName, colNames, types, new Object[]{"tag", "multi", 0, "checklist"}));
 							securityDb.insertData(queryUtil.insertIntoTable(tableName, colNames, types, new Object[]{"description", "single", 1, "checklist"}));
 							securityDb.insertData(queryUtil.insertIntoTable(tableName, colNames, types, new Object[]{"domain", "multi", 2, "checklist"}));
 						}
