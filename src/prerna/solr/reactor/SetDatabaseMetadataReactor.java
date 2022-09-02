3975a16f24eb76632c28ea857b28054b41fa3ed6
mahkhalil <mahkhalil@deloitte.com>
2022-09-02 14:43:40 +0000

    UPDATE ::: Adding logic to handle encoded map input
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46864 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/solr/reactor/SetDatabaseMetadataReactor.java
+++ src/prerna/solr/reactor/SetDatabaseMetadataReactor.java
@@ -7,19 +7,15 @@ import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.SecurityDatabaseUtils;
 import prerna.auth.utils.SecurityQueryUtils;
 import prerna.nameserver.utility.MasterDatabaseUtility;
-import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
-import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.sablecc2.reactor.app.upload.UploadInputUtility;
 
-public class SetDatabaseMetadataReactor extends AbstractReactor {
-	
-	private static final String META = "meta";
+public class SetDatabaseMetadataReactor extends AbstractSetMetadataReactor {
 	
 	public SetDatabaseMetadataReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.DATABASE.getKey(), META};
+		this.keysToGet = new String[]{ReactorKeysEnum.DATABASE.getKey(), META, ReactorKeysEnum.ENCODED.getKey()};
 	}
 
 	@Override
@@ -47,27 +43,6 @@ public class SetDatabaseMetadataReactor extends AbstractReactor {
 		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
 		noun.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Successfully set the new metadata values for the database"));
 		return noun;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	private Map<String, Object> getMetaMap() {
-		GenRowStruct mapGrs = this.store.getNoun(META);
-		if(mapGrs != null && !mapGrs.isEmpty()) {
-			List<NounMetadata> mapInputs = mapGrs.getNounsOfType(PixelDataType.MAP);
-			if(mapInputs != null && !mapInputs.isEmpty()) {
-				return (Map<String, Object>) mapInputs.get(0).getValue();
-			}
-		}
-
-		List<NounMetadata> mapInputs = this.curRow.getNounsOfType(PixelDataType.MAP);
-		if(mapInputs != null && !mapInputs.isEmpty()) {
-			return (Map<String, Object>) mapInputs.get(0).getValue();
-		}
-
-		throw new IllegalArgumentException("Must define a metadata map");
 	}
 	
 	@Override
