37de7fce3fe49ee3e19b28a97c5db286388cb841
mahkhalil <mahkhalil@deloitte.com>
2022-09-02 14:43:40 +0000

    UPDATE ::: Adding logic to handle encoded map input
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46864 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a496e2b66b957f5011a01b066ba4a4eb37c4fc52

--- src/prerna/solr/reactor/SetProjectMetadataReactor.java
+++ src/prerna/solr/reactor/SetProjectMetadataReactor.java
@@ -5,19 +5,15 @@ import java.util.Map;
 
 import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.SecurityProjectUtils;
-import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
-import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.sablecc2.reactor.app.upload.UploadInputUtility;
 
-public class SetProjectMetadataReactor extends AbstractReactor {
-	
-	private static final String META = "meta";
+public class SetProjectMetadataReactor extends AbstractSetMetadataReactor {
 	
 	public SetProjectMetadataReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.PROJECT.getKey(), META};
+		this.keysToGet = new String[]{ReactorKeysEnum.PROJECT.getKey(), META, ReactorKeysEnum.ENCODED.getKey()};
 	}
 
 	@Override
@@ -43,27 +39,6 @@ public class SetProjectMetadataReactor extends AbstractReactor {
 		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
 		noun.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Successfully set the new metadata values for the project"));
 		return noun;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	private Map<String, Object> getMetaMap() {
-		GenRowStruct mapGrs = this.store.getNoun(META);
-		if(mapGrs != null && !mapGrs.isEmpty()) {
-			List<NounMetadata> mapInputs = mapGrs.getNounsOfType(PixelDataType.MAP);
-			if(mapInputs != null && !mapInputs.isEmpty()) {
-				return (Map<String, Object>) mapInputs.get(0).getValue();
-			}
-		}
-
-		List<NounMetadata> mapInputs = this.curRow.getNounsOfType(PixelDataType.MAP);
-		if(mapInputs != null && !mapInputs.isEmpty()) {
-			return (Map<String, Object>) mapInputs.get(0).getValue();
-		}
-
-		throw new IllegalArgumentException("Must define a metadata map");
 	}
 	
 	@Override
