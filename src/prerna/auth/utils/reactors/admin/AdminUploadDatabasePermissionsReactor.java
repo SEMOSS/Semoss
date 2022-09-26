842e506631a931996e1e376595bf82f134e5c235
mahkhalil <mahkhalil@deloitte.com>
2022-09-26 19:46:59 +0000

    UPDATE ::: Pushing package restructure for new methods to modify the physical database directly
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46987 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 0669829e9c9c2682f239ed9c121e45a455463c1c

--- src/prerna/auth/utils/reactors/admin/AdminUploadDatabasePermissionsReactor.java
+++ src/prerna/auth/utils/reactors/admin/AdminUploadDatabasePermissionsReactor.java
@@ -27,7 +27,7 @@ import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
-import prerna.sablecc2.reactor.app.upload.UploadInputUtility;
+import prerna.sablecc2.reactor.database.upload.UploadInputUtility;
 import prerna.util.Constants;
 import prerna.util.Utility;
 import prerna.util.sql.AbstractSqlQueryUtil;
--- src/prerna/sablecc2/reactor/algorithms/NLPInstanceCacheReactor.java
+++ src/prerna/sablecc2/reactor/algorithms/NLPInstanceCacheReactor.java
@@ -21,7 +21,7 @@ import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
-import prerna.sablecc2.reactor.app.upload.UploadInputUtility;
+import prerna.sablecc2.reactor.database.upload.UploadInputUtility;
 import prerna.sablecc2.reactor.frame.r.AbstractRFrameReactor;
 import prerna.util.DIHelper;
 import prerna.util.Utility;
