e77b9c96eac5080de8617b09b382878c97004313
Maher Khalil <themaherkhalil@gmail.com>
2022-09-27 19:23:56 +0000

    UPDATE ::: Adding new reactor to generate external database tables and columns
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@46996 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a5865f40ca8e0d404aebd9867d7a37f8dedcd0de

--- src/prerna/sablecc2/reactor/database/physicaleditor/ModifyDatabaseStructureReactor.java
+++ src/prerna/sablecc2/reactor/database/physicaleditor/ModifyDatabaseStructureReactor.java
@@ -1,14 +1,8 @@
 package prerna.sablecc2.reactor.database.physicaleditor;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -31,21 +25,22 @@ import prerna.util.Constants;
 import prerna.util.EngineSyncUtility;
 import prerna.util.Utility;
 import prerna.util.sql.AbstractSqlQueryUtil;
+import prerna.util.sql.DatabaseUpdateMetadata;
 
 public class ModifyDatabaseStructureReactor extends AbstractReactor {
 
-	private static final Logger logger = LogManager.getLogger(ModifyDatabaseStructureReactor.class);
-	
-	public static final String ADDITIONS = "add";
-	
+	private static final Logger classLogger = LogManager.getLogger(ModifyDatabaseStructureReactor.class);
+	private static final String CLASS_NAME = ModifyDatabaseStructureReactor.class.getName();
+
 	public ModifyDatabaseStructureReactor() {
-		this.keysToGet = new String[] {ReactorKeysEnum.DATABASE.getKey(), ADDITIONS};
+		this.keysToGet = new String[] {ReactorKeysEnum.DATABASE.getKey(), ReactorKeysEnum.METAMODEL_ADDITIONS.getKey()};
 	}
 	
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
-		
+		Logger logger = getLogger(CLASS_NAME);
+
 		String databaseId = this.keyValue.get(this.keysToGet[0]);
 		if(AbstractSecurityUtils.securityEnabled()) {
 			databaseId = SecurityQueryUtils.testUserDatabaseIdForAlias(this.insight.getUser(), databaseId);
@@ -59,7 +54,6 @@ public class ModifyDatabaseStructureReactor extends AbstractReactor {
 			}
 		}
 		
-		
 		// table > column > type
 		Map<String, Map<String, String>> updates = getAdditions();
 		
@@ -69,82 +63,9 @@ public class ModifyDatabaseStructureReactor extends AbstractReactor {
 		}
 		ClusterUtil.reactorPullOwl(databaseId);
 
-		Set<String> tableExists = new HashSet<>();
-
-		IRDBMSEngine rdbmsDb = (IRDBMSEngine) engine;
-		AbstractSqlQueryUtil queryUtil = rdbmsDb.getQueryUtil();
-		Map<String, String> typeConversionMap = queryUtil.getTypeConversionMap();
-		
-		try {
-			Connection conn = rdbmsDb.getConnection();
-			String database = rdbmsDb.getDatabase();
-			String schema = rdbmsDb.getSchema();
-			
-			// first run a validation on the input
-			for(String tableName : updates.keySet()) {
-				if(queryUtil.tableExists(engine, tableName, database, schema)) {
-					// we are altering - make sure everything is valid
-					
-					List<String> currentColumns = queryUtil.getTableColumns(conn, tableName, database, schema);
-					Set<String> currentColumnsLower = currentColumns.stream().map(s -> s.toLowerCase()).collect(Collectors.toSet());
-					
-					Set<String> newColumns = updates.get(tableName).keySet();
-					Set<String> newColumnsLower = newColumns.stream().map(s -> s.toLowerCase()).collect(Collectors.toSet());
-					
-					Set<String> overlap = newColumnsLower.stream().filter(s -> currentColumnsLower.contains(s)).collect(Collectors.toSet());
-					if(!overlap.isEmpty()) {
-						throw new IllegalArgumentException("The following column names already exist: " + overlap);
-					}
-
-					tableExists.add(tableName);
-				}
-			}
-		} catch(SQLException e) {
-			logger.error(Constants.STACKTRACE, e);
-			throw new IllegalArgumentException("Error validating the input. Detailed message = " + e.getMessage());
-		}
-		
-		// create an owler to track the meta modifications
-		Owler owler = new Owler(rdbmsDb);
-		
-		StringBuilder errorMessages = new StringBuilder();
-		// now do the operations
-		for(String tableName : updates.keySet()) {
-
-			Map<String, String> finalColumnUpdates = new HashMap<>(updates.size());
-
-			Map<String, String> columnUpdates = updates.get(tableName);
-			for(String column : columnUpdates.keySet()) {
-
-				String columnType = columnUpdates.get(column).toUpperCase();
-				if(typeConversionMap.containsKey(columnType)) {
-					columnType = typeConversionMap.get(columnType);
-				}
-
-				finalColumnUpdates.put(column, columnType);
-			}
-
-			String query = null;
-			if(tableExists.contains(tableName)) {
-				query = queryUtil.alterTableAddColumns(tableName, finalColumnUpdates);
-			} else {
-				// make new table
-				query = queryUtil.createTable(tableName, finalColumnUpdates);
-			}
-			try {
-				rdbmsDb.insertData(query);
-				
-				// add to the owl
-				owler.addConcept(tableName, null, null);
-				for(String column : finalColumnUpdates.keySet()) {
-					String columnType = finalColumnUpdates.get(column);
-					owler.addProp(tableName, column, columnType);
-				}
-			} catch(Exception e) {
-				logger.error(Constants.STACKTRACE, e);
-				errorMessages.append("Error executing query = '" + query +"' with detailed error = " + e.getMessage() + ". ");
-			}
-		}
+		DatabaseUpdateMetadata dbUpdateMeta = AbstractSqlQueryUtil.performDatabaseAdditions((IRDBMSEngine) engine, updates, logger);
+		Owler owler = dbUpdateMeta.getOwler();
+		String errorMessages = dbUpdateMeta.getCombinedErrors();
 		
 		// now push the OWL and sync
 		try {
@@ -155,18 +76,18 @@ public class ModifyDatabaseStructureReactor extends AbstractReactor {
 			syncWithLocal.In();
 			syncWithLocal.execute();
 		} catch (IOException e) {
-			logger.error(Constants.STACKTRACE, e);
-			NounMetadata noun = new NounMetadata(false, PixelDataType.BOOLEAN);
+			classLogger.error(Constants.STACKTRACE, e);
+			NounMetadata noun = new NounMetadata(dbUpdateMeta, PixelDataType.BOOLEAN);
 			noun.addAdditionalReturn(getError("Error occurred savig the metadata file with the executed changes"));
-			if(errorMessages.length() > 0) {
-				noun.addAdditionalReturn(getError(errorMessages.toString()));
+			if(!errorMessages.isEmpty()) {
+				noun.addAdditionalReturn(getError(errorMessages));
 			}
 			return noun;
 		}
 		EngineSyncUtility.clearEngineCache(databaseId);
 		ClusterUtil.reactorPushOwl(databaseId);
 		
-		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
+		NounMetadata noun = new NounMetadata(dbUpdateMeta, PixelDataType.CUSTOM_DATA_STRUCTURE);
 		if(errorMessages.length() > 0) {
 			noun.addAdditionalReturn(getError(errorMessages.toString()));
 		}
@@ -175,7 +96,7 @@ public class ModifyDatabaseStructureReactor extends AbstractReactor {
 	}
 
 	private Map<String, Map<String, String>> getAdditions() {
-		GenRowStruct mapGrs = this.store.getNoun(ADDITIONS);
+		GenRowStruct mapGrs = this.store.getNoun(ReactorKeysEnum.METAMODEL_ADDITIONS.getKey());
 		if(mapGrs != null && !mapGrs.isEmpty()) {
 			List<NounMetadata> mapInputs = mapGrs.getNounsOfType(PixelDataType.MAP);
 			if(mapInputs != null && !mapInputs.isEmpty()) {
