7154b863178229c1ce1efeeced20865960876963
mahkhalil <mahkhalil@deloitte.com>
2022-09-29 13:48:12 +0000

    UPDATE ::: Performing sort in query for MyProjectsReactor - will be required once FE starts doing infinite scroll on project lists BUGFIX ::: Adding empty checks for MyDatabasesReactor, MyDiscoverableDatabasesReactor, and MyProjectsReactor to avoid null pointers
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47028 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/project/MyProjectsReactor.java
+++ src/prerna/sablecc2/reactor/project/MyProjectsReactor.java
@@ -1,8 +1,6 @@
 package prerna.sablecc2.reactor.project;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -51,68 +49,56 @@ public class MyProjectsReactor extends AbstractReactor {
 			projectInfo = SecurityProjectUtils.getAllProjectList(null, limit, offset);
 		}
 
-		int size = projectInfo.size();
-		Map<String, Integer> index = new HashMap<>(projectInfo.size());
-		// now we want to add most executed insights
-		for(int i = 0; i < size; i++) {
-			Map<String, Object> project = projectInfo.get(i);
-			String projectId = project.get("project_id").toString();
-			// keep list of project ids to get the index
-			index.put(projectId, Integer.valueOf(i));
-		}
-
-		IRawSelectWrapper wrapper = null;
-		try {
-			wrapper = SecurityProjectUtils.getProjectMetadataWrapper(index.keySet(), getMetaKeys());
-			while(wrapper.hasNext()) {
-				Object[] data = wrapper.next().getValues();
-				String projectId = (String) data[0];
-
-				String metaKey = (String) data[1];
-				String metaValue = (String) data[2];
-				if(metaValue == null) {
-					continue;
-				}
-
-				int indexToFind = index.get(projectId);
-				Map<String, Object> res = projectInfo.get(indexToFind);
-				// whatever it is, if it is single send a single value, if it is multi send as array
-				if(res.containsKey(metaKey)) {
-					Object obj = res.get(metaKey);
-					if(obj instanceof List) {
-						((List) obj).add(metaValue);
-					} else {
-						List<Object> newList = new ArrayList<>();
-						newList.add(obj);
-						newList.add(metaValue);
-						res.put(metaKey, newList);
-					}
-				} else {
-					res.put(metaKey, metaValue);
-				}
+		if(!projectInfo.isEmpty()) {
+			Map<String, Integer> index = new HashMap<>(projectInfo.size());
+			int size = projectInfo.size();
+			// now we want to add most executed insights
+			for(int i = 0; i < size; i++) {
+				Map<String, Object> project = projectInfo.get(i);
+				String projectId = project.get("project_id").toString();
+				// keep list of project ids to get the index
+				index.put(projectId, Integer.valueOf(i));
 			}
-		} catch (Exception e) {
-			logger.error(Constants.STACKTRACE, e);
-		} finally {
-			if(wrapper != null) {
-				wrapper.cleanUp();
+	
+			IRawSelectWrapper wrapper = null;
+			try {
+				wrapper = SecurityProjectUtils.getProjectMetadataWrapper(index.keySet(), getMetaKeys());
+				while(wrapper.hasNext()) {
+					Object[] data = wrapper.next().getValues();
+					String projectId = (String) data[0];
+	
+					String metaKey = (String) data[1];
+					String metaValue = (String) data[2];
+					if(metaValue == null) {
+						continue;
+					}
+	
+					int indexToFind = index.get(projectId);
+					Map<String, Object> res = projectInfo.get(indexToFind);
+					// whatever it is, if it is single send a single value, if it is multi send as array
+					if(res.containsKey(metaKey)) {
+						Object obj = res.get(metaKey);
+						if(obj instanceof List) {
+							((List) obj).add(metaValue);
+						} else {
+							List<Object> newList = new ArrayList<>();
+							newList.add(obj);
+							newList.add(metaValue);
+							res.put(metaKey, newList);
+						}
+					} else {
+						res.put(metaKey, metaValue);
+					}
+				}
+			} catch (Exception e) {
+				logger.error(Constants.STACKTRACE, e);
+			} finally {
+				if(wrapper != null) {
+					wrapper.cleanUp();
+				}
 			}
 		}
 		
-		// need to go through and sort the values
-		Collections.sort(projectInfo, new Comparator<Map<String, Object>>() {
-
-			// we want descending - not ascending
-			// so values are swapped
-			@Override
-			public int compare(Map<String, Object> o1, Map<String, Object> o2) {
-				String name1 = (String) o1.get("low_project_name");
-				String name2 = (String) o2.get("low_project_name");
-				
-				return name1.compareTo(name2);
-			}
-		});
-
 		return new NounMetadata(projectInfo, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.DATABASE_INFO);
 	}
 	
