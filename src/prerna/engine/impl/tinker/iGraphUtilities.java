010622c832a4a2bf73a5c8ca921057f791c30c6e
mahkhalil <mahkhalil@deloitte.com>
2022-10-11 20:51:27 +0000

    UPDATE ::: Moving more functions out of the java reactor that deal with graphs
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47102 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 8086f306e8f0a844c381f80922f9593f0d746a48

---
+++ src/prerna/engine/impl/tinker/iGraphUtilities.java
@@ -0,0 +1,59 @@
+package prerna.engine.impl.tinker;
+
+import java.util.List;
+
+import org.apache.logging.log4j.Logger;
+
+import prerna.ds.TinkerFrame;
+import prerna.sablecc2.reactor.frame.r.util.AbstractRJavaTranslator;
+import prerna.util.Constants;
+
+public class iGraphUtilities {
+
+	/**
+	 * 
+	 * @param graph
+	 * @param rJavaTranslator
+	 * @param graphName
+	 * @param wd
+	 * @param logger
+	 */
+	public static void synchronizeGraphToR(TinkerFrame graph, AbstractRJavaTranslator rJavaTranslator, String graphName, String wd, Logger logger) {
+		try {
+			String fileName = TinkerUtilities.serializeGraph(graph, wd);
+			wd = wd.replace("\\", "/");
+
+			StringBuilder builder = new StringBuilder("library(\"igraph\");");
+			builder.append(graphName + "<- read_graph(\"" + fileName + "\", \"graphml\");");
+			// load the graph
+			rJavaTranslator.executeEmptyR(builder.toString());
+			graph.setIGraphSynched(true);
+		} catch (Exception ex) {
+			logger.error(Constants.STACKTRACE, ex);
+			throw new IllegalArgumentException("Could not convert TinkerFrame into iGraph. Please make sure iGraph package is installed.");
+		}
+	}
+	
+	/**
+	 * Delete nodes from R iGraph
+	 * 
+	 * @param type
+	 * @param nodeList
+	 */
+	public static void removeNodeFromR(String graphName, AbstractRJavaTranslator rJavaTranslator, String type, List<Object> nodeList, Logger logger) {
+		for (int nodeIndex = 0; nodeIndex < nodeList.size(); nodeIndex++) {
+			String name = type + ":" + nodeList.get(nodeIndex);
+			try {
+				logger.info("Deleting node = " + name);
+				// eval is abstract and is determined by the specific R
+				// implementation
+				rJavaTranslator.executeEmptyR(graphName + " <- delete_vertices(" + graphName + ", V(" + graphName + ")[vertex_attr(" + graphName
+						+ ", \"" + TinkerFrame.TINKER_ID + "\") == \"" + name + "\"])");
+			} catch (Exception ex) {
+				logger.error(Constants.STACKTRACE, ex);
+				throw new IllegalArgumentException("Could not delete node = " + name);
+			}
+		}
+	}
+	
+}
