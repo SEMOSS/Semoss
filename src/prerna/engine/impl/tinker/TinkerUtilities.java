d7a94abc2044e9ddaa795c59a309a500647efb11
mahkhalil <mahkhalil@deloitte.com>
2022-10-11 15:34:20 +0000

    UPDATE ::: Adding in new reactor to move graph frame to R igraph
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47101 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a2ce33a10b6bde556e5232a8d4a05bcfecf48a6d

--- src/prerna/engine/impl/tinker/TinkerUtilities.java
+++ src/prerna/engine/impl/tinker/TinkerUtilities.java
@@ -1,23 +1,65 @@
 package prerna.engine.impl.tinker;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.apache.tinkerpop.gremlin.structure.io.IoCore;
+
+import prerna.ds.TinkerFrame;
+import prerna.util.Constants;
 
 public class TinkerUtilities {
 
-	private static final Logger LOGGER = LogManager.getLogger(TinkerUtilities.class);
+	private static final Logger classLogger = LogManager.getLogger(TinkerUtilities.class);
 	
 	private TinkerUtilities() {
 		
 	}
 	
+	/**
+	 * 
+	 * @param engine
+	 */
 	public static void removeAllVertices(TinkerEngine engine) {
 		GraphTraversal<Vertex, Long> iterate = engine.g.traversal().V().drop().iterate().count();
 		while(iterate.hasNext()) {
 			Long count = iterate.next();
-			LOGGER.info("Dropping " + count + " verticies from engine " + engine.getEngineName() + "__" + engine.getEngineId());
+			classLogger.info("Dropping " + count + " verticies from engine " + engine.getEngineName() + "__" + engine.getEngineId());
 		}
+	}
+	
+	/**
+	 * Serialize the TinkerGraph in GraphML format
+	 * @param dataframe
+	 * @param directory
+	 * @return
+	 */
+	public static String serializeGraph(TinkerFrame dataframe, String directory) {
+		String absoluteFileName = null;
+		final Graph graph = ((TinkerFrame) dataframe).g;
+		absoluteFileName = "output" + java.lang.System.currentTimeMillis() + ".xml";
+		String fileName = directory + "/" + absoluteFileName;
+		OutputStream os = null;
+		try {
+			os = new FileOutputStream(fileName);
+			graph.io(IoCore.graphml()).writer().normalize(true).create().writeGraph(os, graph);
+		} catch (Exception ex) {
+			classLogger.error(Constants.STACKTRACE, ex);
+		} finally {
+			try {
+				if (os != null) {
+					os.close();
+				}
+			} catch (IOException e) {
+				classLogger.error(Constants.STACKTRACE, e);
+			}
+		}
+		return absoluteFileName;
 	}
 }
