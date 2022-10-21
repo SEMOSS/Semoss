5add9e9cf65a98fdcfc7eeaac472197e80af9b41
mahkhalil <mahkhalil@deloitte.com>
2022-10-21 19:07:11 +0000

    UPDATE ::: Adding reactors to pull data from User Activity database
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47187 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/usertracking/reactors/UserActivityReactor.java
@@ -0,0 +1,49 @@
+package prerna.usertracking.reactors;
+
+import prerna.query.querystruct.AbstractQueryStruct;
+import prerna.query.querystruct.SelectQueryStruct;
+import prerna.query.querystruct.selectors.QueryColumnSelector;
+import prerna.query.querystruct.selectors.QueryConstantSelector;
+import prerna.query.querystruct.selectors.QueryFunctionHelper;
+import prerna.query.querystruct.selectors.QueryFunctionSelector;
+import prerna.sablecc2.reactor.qs.AbstractQueryStructReactor;
+
+public class UserActivityReactor extends AbstractQueryStructReactor {
+
+	// UserActivity example
+	// UserActivityH2 ( ) | Import ( frame = [ CreateFrame ( frameType = [ GRID ] , override = [ true ] ) .as ( [ "FRAME961184" ] ) ] ) ;
+	// Frame ( frame = [ FRAME961184 ] ) | UserActivityH2 ( ) | AutoTaskOptions ( panel = [ "0" ] , layout = [ "Grid" ] ) | Collect ( 2000 ) ;
+	
+	// date format function example
+	// Frame ( frame = [ FRAME961184 ] ) | Select ( DateFormat ( DATE_CREATED, "YYYY-MM-dd" ) ) | CollectAll ( ) ;
+	
+	@Override
+	protected AbstractQueryStruct createQueryStruct() {
+		this.qs.setEngineId("UserTrackingDatabase");
+		this.qs.setQsType(SelectQueryStruct.QUERY_STRUCT_TYPE.ENGINE);
+
+		SelectQueryStruct sQs = new SelectQueryStruct();
+		// selectors
+		QueryFunctionSelector fSelector = new QueryFunctionSelector();
+		fSelector.setAlias("COUNT");
+		fSelector.setFunction(QueryFunctionHelper.COUNT);
+		fSelector.addInnerSelector(new QueryColumnSelector("USER_TRACKING" + "__" + "USERID"));
+		sQs.addSelector(fSelector);
+
+		fSelector = new QueryFunctionSelector();
+		fSelector.setAlias("CREATED_ON");
+		fSelector.setFunction(QueryFunctionHelper.DATE_FORMAT);
+		fSelector.addInnerSelector(new QueryColumnSelector("USER_TRACKING" + "__" + "CREATED_ON"));
+		fSelector.addInnerSelector(new QueryConstantSelector("YYYY-MM-dd"));
+		sQs.addSelector(fSelector);
+		// group by
+		fSelector = new QueryFunctionSelector();
+		fSelector.setAlias("CREATED_ON");
+		fSelector.setFunction(QueryFunctionHelper.DATE_FORMAT);
+		fSelector.addInnerSelector(new QueryColumnSelector("USER_TRACKING" + "__" + "CREATED_ON"));
+		fSelector.addInnerSelector(new QueryConstantSelector("YYYY-MM-dd"));
+		sQs.addGroupBy(fSelector);
+		this.qs.merge(sQs);
+		return this.qs;
+	}
+}
---
+++ src/prerna/usertracking/reactors/UserActivitySQLiteReactor.java
@@ -0,0 +1,40 @@
+package prerna.usertracking.reactors;
+
+import prerna.query.querystruct.AbstractQueryStruct;
+import prerna.query.querystruct.SelectQueryStruct;
+import prerna.query.querystruct.selectors.QueryColumnSelector;
+import prerna.query.querystruct.selectors.QueryFunctionHelper;
+import prerna.query.querystruct.selectors.QueryFunctionSelector;
+import prerna.sablecc2.reactor.qs.AbstractQueryStructReactor;
+
+public class UserActivitySQLiteReactor extends AbstractQueryStructReactor {
+
+	// UserActivity example
+	// UserActivity ( ) | Import ( frame = [ CreateFrame ( frameType = [ GRID ] , override = [ true ] ) .as ( [ "FRAME961184" ] ) ] ) ;
+	// Frame ( frame = [ FRAME961184 ] ) | UserActivity ( ) | AutoTaskOptions ( panel = [ "0" ] , layout = [ "Grid" ] ) | Collect ( 2000 ) ;
+	
+	// date format function example
+	// Frame ( frame = [ FRAME961184 ] ) | Select ( DateFormat ( "%Y-%m-%d" , DATE_CREATED ) ) | CollectAll ( ) ;
+	
+	@Override
+	protected AbstractQueryStruct createQueryStruct() {
+		this.qs.setEngineId("UserTrackingDatabase");
+		this.qs.setQsType(SelectQueryStruct.QUERY_STRUCT_TYPE.ENGINE);
+
+		SelectQueryStruct sQs = new SelectQueryStruct();
+		// selectors
+		QueryFunctionSelector fSelector = new QueryFunctionSelector();
+		fSelector.setAlias("COUNT");
+		fSelector.setFunction(QueryFunctionHelper.COUNT);
+		fSelector.addInnerSelector(new QueryColumnSelector("USER_TRACKING" + "__" + "USERID"));
+		sQs.addSelector(fSelector);
+
+		sQs.addSelector(new QueryColumnSelector("USER_TRACKING" + "__" + "CREATED_ON"));
+//		// group by
+		sQs.addGroupBy(new QueryColumnSelector("USER_TRACKING" + "__" + "CREATED_ON"));
+		// order by
+		sQs.addOrderBy("USER_TRACKING" + "__" + "CREATED_ON", "DESC");;
+		this.qs.merge(sQs);
+		return this.qs;
+	}
+}
