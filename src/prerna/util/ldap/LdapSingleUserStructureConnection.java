34b1de8189923e70f4757f35aa52f4beac7de1c4
mahkhalil <mahkhalil@deloitte.com>
2022-10-24 00:48:05 +0000

    UPDATE ::: Continuing to buildout LDAP authentication
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47193 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 96fa837dc624520f544b2f960381c5d92da674b7

--- src/prerna/util/ldap/LdapSingleUserStructureConnection.java
+++ src/prerna/util/ldap/LdapSingleUserStructureConnection.java
@@ -4,13 +4,18 @@ import java.io.IOException;
 import java.util.Properties;
 
 import javax.naming.Context;
+import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
 import javax.naming.directory.DirContext;
 import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import prerna.auth.AccessToken;
 import prerna.util.Constants;
 
 /**
@@ -23,25 +28,51 @@ public class LdapSingleUserStructureConnection extends AbstractLdapAuthenticator
 	
 	@Override
 	public void validate() throws IOException {
-		// TODO Auto-generated method stub
-		
+		super.validate();
+		if(this.securityPrincipalTemplate == null || (this.securityPrincipalTemplate=this.securityPrincipalTemplate.trim()).isEmpty()) {
+			throw new IllegalArgumentException("Must provide the DN template");
+		}
+		if(!this.securityPrincipalTemplate.contains(SECURITY_PRINCIPAL_TEMPLATE_USERNAME)) {
+			throw new IllegalArgumentException("Must provide the a location to fill the username passed from the user using " + SECURITY_PRINCIPAL_TEMPLATE_USERNAME);
+		}
 	}
 	
 	@Override
-	public boolean authenticate(String username, String password) throws Exception {
-		String principalTemplate = this.securityPrincipalTemplate;
-		
+	public AccessToken authenticate(String username, String password) throws Exception {
 		DirContext con = null;
+		String principalTemplate = this.securityPrincipalTemplate;
+		String principalDN = principalTemplate.replace(SECURITY_PRINCIPAL_TEMPLATE_USERNAME, username);
 		try {
 			Properties env = new Properties();
 			env.put(Context.INITIAL_CONTEXT_FACTORY, LDAP_FACTORY);
 			env.put(Context.PROVIDER_URL, this.providerUrl); // "ldap://localhost:10389";
-			env.put(Context.SECURITY_PRINCIPAL, principalTemplate.replace("<CN>", username)); // cn=<CN>,ou=users,ou=system
+			env.put(Context.SECURITY_PRINCIPAL, principalDN); // cn=<username>,ou=users,ou=system
 			env.put(Context.SECURITY_CREDENTIALS, password); // password
 			
 			con = new InitialDirContext(env);
 			
-			return true;
+			// need to search for the user who just logged in
+			// so that i can grab the attributes
+			String searchFilter = "(objectClass=inetOrgPerson)";
+			
+			SearchControls controls = new SearchControls();
+			controls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+			controls.setReturningAttributes(this.requestAttributes);
+			
+			NamingEnumeration<SearchResult> users = con.search("ou=users,ou=system", searchFilter, controls);
+			
+			SearchResult result = null;
+			while(users.hasMoreElements()) {
+				result = users.next();
+				// confirm the DN for this user matches the one used to logged in in case the
+				// search returns too many people
+				if(result.getNameInNamespace().equals(principalDN)) {
+					Attributes attr = result.getAttributes();
+					return generateAccessToken(attr);
+				}
+			}
+			
+			return null;
 		} catch(Exception e) {
 			classLogger.error(Constants.STACKTRACE, e);
 			throw e;
