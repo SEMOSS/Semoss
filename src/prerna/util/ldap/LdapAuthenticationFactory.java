081706a0bfbf7ce521428323f45bc2f16f962081
Maher Khalil <themaherkhalil@gmail.com>
2022-10-25 09:32:36 +0000

    UPDATE ::: Exposing additional parameteres for ldap auth
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47204 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 18fdc75c7a285274ce2369a26b9a705c8e9b5cb1

--- src/prerna/util/ldap/LdapAuthenticationFactory.java
+++ src/prerna/util/ldap/LdapAuthenticationFactory.java
@@ -6,16 +6,15 @@ public class LdapAuthenticationFactory {
 	 * Get the LDAP authentication process
 	 * @return
 	 */
-	public ILdapAuthenticator getAuthenticator() {
-		
-		
+	public static ILdapAuthenticator getAuthenticator(String ldapType) {
+		if(ILdapAuthenticator.LDAP_TYPE_MULTI.equals(ldapType)) {
+			
+		}
+		// default to simple case
 		return new LdapSingleUserStructureConnection();
 	}
 	
 	private LdapAuthenticationFactory() {
 		
 	}
-	
-	
-	
 }
