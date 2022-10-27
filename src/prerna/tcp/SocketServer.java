385fb581685057b781770319870d0196ce441b9b
Maher Khalil <themaherkhalil@gmail.com>
2022-10-27 14:13:11 +0000

    UPDATE ::: Minor styling and logger change UPDATE ::: Closing streams and not re-loading property files twice
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47233 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 9d4ea09d31314208977410ab79238b37bfc27c64

--- src/prerna/tcp/SocketServer.java
+++ src/prerna/tcp/SocketServer.java
@@ -1,8 +1,6 @@
 package prerna.tcp;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
@@ -22,14 +20,13 @@ import prerna.util.Constants;
 import prerna.util.DIHelper;
 import prerna.util.Utility;
 
-public class SocketServer implements Runnable
-{
+public class SocketServer implements Runnable {
 	
 	// basically a process which works by looking for commands in TCP space
 	private static final String CLASS_NAME = SocketServer.class.getName();
 
 	List <String> commandList = new ArrayList<String>(); // this is giving the file name and that too relative
-	public  static Logger LOGGER = null;
+	public static Logger classLogger = null;
 
 	public Jep jep = null;
 	boolean SSL = false;
@@ -58,10 +55,7 @@ public class SocketServer implements Runnable
 		
 	static boolean multi = false; // allow multiple threads at the same time
 	
-	public static void main(String [] args)
-	{
-		
-		
+	public static void main(String [] args) {
 		// arg1 - the directory where commands would be thrown
 		// arg2 - access to the rdf map to load
 		// arg3 - port to start
@@ -73,8 +67,7 @@ public class SocketServer implements Runnable
 		// comment this for main execution
 		//-Dlog4j.defaultInitOverride=TRUE
 		
-		if(args == null || args.length == 0)
-		{
+		if(args == null || args.length == 0) {
 			args = new String[5];
 			args[0] = "C:\\users\\pkapaleeswaran\\workspacej3\\SemossDev\\config";
 			args[1] = "C:\\users\\pkapaleeswaran\\workspacej3\\SemossDev\\RDF_Map.prop";;
@@ -85,112 +78,80 @@ public class SocketServer implements Runnable
 			args[4] = "c:/users/pkapaleeswaran/workspacej3/semossdev/py";
 			test = true;
 			multi = true;
-				
 		}
 		
 		String log4JPropFile = Paths.get(Utility.normalizePath(args[0]), "log4j2.properties").toAbsolutePath().toString();
-		
+
 		// set to say this is not core
 		DIHelper.getInstance().setLocalProperty("core", "false");
 		
 		FileInputStream fis = null;
-		ConfigurationSource source = null;
 		try {
 			fis = new FileInputStream(Utility.normalizePath(log4JPropFile));
-			source = new ConfigurationSource(fis);
-			//Configuration con = PropertiesConfigurationFactory.getInstance().getConfiguration(new LoggerContext(CLASS_NAME), source);
-			//LOGGER = con.getLoggerContext().getLogger(CLASS_NAME);
-			//LOGGER = Configurator.initialize(null, source).getLogger(CLASS_NAME);
+			new ConfigurationSource(fis);
 		} catch (IOException e) {
-			e.printStackTrace();
+			classLogger.error(Constants.STACKTRACE, e);
 		} finally {
 			if(fis != null) {
 				try {
 					fis.close();
 				} catch (IOException e) {
-					e.printStackTrace();
+					classLogger.error(Constants.STACKTRACE, e);
 				}
 			}
 		}
-		
-		
+		classLogger = LogManager.getLogger(CLASS_NAME);
+
+		String rdfMapLocation = Utility.normalizePath(args[1]);
+		Properties rdfMap = Utility.loadProperties(rdfMapLocation);
+        System.out.println("loaded rdf map");
+        classLogger.info("loaded rdf map");
+
 		SocketServer worker = new SocketServer();
+		worker.baseFolder = rdfMap.getProperty(Constants.BASE_FOLDER).replace('\\', '/');
 		
-		try {
-			Properties prop2 = new Properties();
-			prop2.load(new FileInputStream(new File(args[1])));
-			worker.baseFolder = "" + prop2.get(Constants.BASE_FOLDER);
-			worker.baseFolder = worker.baseFolder.replace('\\', '/');
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		DIHelper.getInstance().loadCoreProp(args[1]);
+		DIHelper.getInstance().loadCoreProp(rdfMapLocation);
 		DIHelper.getInstance().getProperty(Constants.BASE_FOLDER);
 		
-		worker.prop = new Properties();
-		
-		try(FileInputStream fileInput = new FileInputStream(Utility.normalizePath(args[1]))) {
-			worker.prop.load(fileInput);
-			System.out.println("Loaded the rdf map");
-			
-			// get the library for jep
-			//String jepLib = worker.prop.getProperty("JEP_LIB");
-			
-			//System.loadLibrary(jepLib);
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		worker.prop = rdfMap;
+
 		worker.mainFolder = args[0];
-		String engine = "r"; // setting it up for r
-		if(args.length >= 4)
+		String engine = "r";
+		if(args.length >= 4) {
 			engine = args[3];
+		}
 		
-		if(args.length >= 5)
+		if(args.length >= 5) {
 			worker.multi = args[4].equalsIgnoreCase("multi");
+		}
 		
 		worker.bootServer(Integer.parseInt(args[2]), engine);
-	}	
+	}
 	
-	public void bootServer(int PORT, String engine)
-	{
-		LOGGER = LogManager.getLogger(CLASS_NAME);
-
-
+	public void bootServer(int PORT, String engine) {
         try {
             serverSocket = new ServerSocket(PORT);
         } catch (IOException e) {
+			classLogger.error(Constants.STACKTRACE, e);
             System.err.println("Could not listen on port: " + PORT);
             System.exit(1);
         }
+        System.out.println("server started");
+        classLogger.info("server started");
 
         Thread listenerThread = new Thread(this);
         listenerThread.start();
-        
 	}
 	
-	
 	// start listening for connections
-	public void run()
-	{
+	public void run() {
 		// do the listening here and then spawn the thread
-		
-		while(!done)
-		{
-			if(this.clientSocket == null || multi)
-			{
+		while(!done) {
+			if(this.clientSocket == null || multi) {
 		        try {
 		            clientSocket = serverSocket.accept();
 		        } catch (IOException e) {
+					classLogger.error(Constants.STACKTRACE, e);
 		            System.err.println("Accept failed.");
 		            System.exit(1);
 		        }	
@@ -198,12 +159,11 @@ public class SocketServer implements Runnable
 			        ssh = new SocketServerHandler();
 			        DIHelper.getInstance().setLocalProperty("SSH", ssh);
 			        ssh.setPyBase(baseFolder + "/" + Constants.PY_BASE_FOLDER); 
-		        	ssh.setLogger(LOGGER);
+		        	ssh.setLogger(classLogger);
 					ssh.setOutputStream(clientSocket.getOutputStream());
 					is = clientSocket.getInputStream();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace(); 
+					classLogger.error(Constants.STACKTRACE, e);
 				}   
 		        
 		        // start processing
@@ -217,19 +177,15 @@ public class SocketServer implements Runnable
 		        ssh.mainFolder = mainFolder;
 		        Thread readerThread = new Thread(ssh);
 		        readerThread.start();
-			}
-			else
-			{
+			} else {
 				// just sleep
 				// see if something crashed
-				synchronized(crash)
-				{
+				synchronized(crash) {
 					try {
 						crash.wait();
 						clientSocket = null;
 					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
+						classLogger.error(Constants.STACKTRACE, e);
 					}
 				}
 			}
