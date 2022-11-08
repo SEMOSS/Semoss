aa7c02f5a970e7d7a918e63d103a0a457d13ee18
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2022-11-08 04:19:44 +0000

    Updating defaults.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47309 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/frame/NLPQueryReactor.java
+++ src/prerna/sablecc2/reactor/frame/NLPQueryReactor.java
@@ -3,7 +3,9 @@ package prerna.sablecc2.reactor.frame;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+import java.util.Vector;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -12,6 +14,7 @@ import org.json.JSONObject;
 import prerna.algorithm.api.ITableDataFrame;
 import prerna.ds.py.PandasFrame;
 import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
@@ -43,11 +46,14 @@ public class NLPQueryReactor extends AbstractReactor {
 		organizeKeys();
 		String query = keyValue.get(keysToGet[0]);
 		
-		String model = "tscholak/3vnuv1vf";
+		String model = "tscholak/cxmefzzi";
 		// will handle huggingface model
 		if(keyValue.containsKey(keysToGet[1]))
 			model = keyValue.get(keysToGet[1]);
 
+		if(DIHelper.getInstance().getProperty("HF_SEQ_2_SQL") != null) // force the model
+			model = DIHelper.getInstance().getProperty("HF_SEQ_2_SQL");
+		
 		String modelVarName = Utility.cleanString(model, true);
 		modelVarName = modelVarName.replaceAll("-", "_");
 
@@ -141,15 +147,20 @@ public class NLPQueryReactor extends AbstractReactor {
 		// check to see if the variable was created
 		// if not this is a bad query
 		boolean frameCreated = (Boolean)insight.getPyTranslator().runScript("'" + frameName + "' in globals()");
+		
+		List<NounMetadata> outputs = new Vector<NounMetadata>(4);
+
 				
 		if(frameCreated)
 		{
 			// now we just need to tell the user here is the frame
 			String frameType = "Py";
 			
-			String finalOutput = "Query Generated : " + sqlDFQuery + " Data : " + frameName;
-			finalOutput = finalOutput + "\r\n To start working with this frame  GenerateFrameFrom" + frameType + "Variable('" + frameName + "')";
-			return new NounMetadata(finalOutput + "https://semoss.org", PixelDataType.CONST_STRING);
+			outputs.add(new NounMetadata("Query Generated : " + sqlDFQuery + " Data : " + frameName, PixelDataType.CONST_STRING));
+			outputs.add(new NounMetadata(this.insight.getPyTranslator().runSingle(insight.getUser().getVarMap(), frameName + ".head(20)"), PixelDataType.CONST_STRING));
+			outputs.add(new NounMetadata("To start working with this frame  GenerateFrameFrom" + frameType + "Variable('" + frameName + "')", PixelDataType.CONST_STRING));
+			
+			return new NounMetadata(outputs, PixelDataType.CODE, PixelOperationType.CODE_EXECUTION);
 		}
 		else
 		{
