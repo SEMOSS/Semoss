1450ed864a0673360ac9ffa22a91eafa43df5271
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2022-11-21 13:55:06 +0000

    Serializing Insight.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47387 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 707eeb9903a9c489cc0b04dd5ff334e3f8ec103e

---
+++ src/prerna/tcp/client/InsightSerializer.java
@@ -0,0 +1,42 @@
+package prerna.tcp.client;
+
+import prerna.om.Insight;
+import prerna.tcp.PayloadStruct;
+
+public class InsightSerializer 
+{
+
+	Insight insight = null;
+	
+	public InsightSerializer(Insight insight)
+	{
+		this.insight = insight;
+	}
+	
+	public void serializeInsight(boolean force)
+	{
+		// see if the insight has been serialized
+		// meh may be even synchronize on insight
+		//synchronized(insight) - this will un-necessarily block. will deal when we get to it
+		{
+			SocketClient sc = (SocketClient)this.insight.getUser().getTCPServer(true);
+			if(!this.insight.serialized || force)
+			{
+				PayloadStruct ps = new PayloadStruct();
+				ps.operation = ps.operation.INSIGHT;
+				
+				// set everything from the noun store
+				// hopefully this serializes well
+				ps.payload = new Object [] {this.insight};
+				ps.payloadClasses = new Class[] {this.insight.getClass()};
+				ps.hasReturn = false;
+				
+				PayloadStruct retStruct = (PayloadStruct)sc.executeCommand(ps);
+				
+				this.insight.serialized = true;
+			}
+		}
+	}
+
+
+}
