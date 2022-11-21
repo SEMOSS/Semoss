2c183474692834f2a1fde765238c71493a5e6046
mahkhalil <mahkhalil@deloitte.com>
2022-11-21 20:38:15 +0000

    UPDATE ::: Removing bad import UPDATE ::: Switching logger version
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47394 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b98ec6ad8ea9f4f2d90795ba98d5cbf53f31446d

--- src/prerna/sablecc2/reactor/frame/FrameProfileReactor.java
+++ src/prerna/sablecc2/reactor/frame/FrameProfileReactor.java
@@ -2,8 +2,8 @@ package prerna.sablecc2.reactor.frame;
 
 import java.util.Map;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import prerna.algorithm.api.DataFrameTypeEnum;
 import prerna.algorithm.api.ITableDataFrame;
