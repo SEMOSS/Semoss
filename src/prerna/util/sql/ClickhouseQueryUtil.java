7b456752a565ae47882d25643e23d57576d4f74a
mahkhalil <mahkhalil@deloitte.com>
2022-11-22 19:57:05 +0000

    adding clickhouse loading logic
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47399 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 342320f2946b9ce8662c7978523570bf72b49210

--- src/prerna/util/sql/ClickhouseQueryUtil.java
+++ src/prerna/util/sql/ClickhouseQueryUtil.java
@@ -1,5 +1,9 @@
 package prerna.util.sql;
 
+import java.util.Map;
+
+import prerna.engine.impl.CaseInsensitiveProperties;
+
 public class ClickhouseQueryUtil extends AnsiSqlQueryUtil {
 
 	ClickhouseQueryUtil() {
@@ -10,6 +14,154 @@ public class ClickhouseQueryUtil extends AnsiSqlQueryUtil {
 	ClickhouseQueryUtil(String connectionUrl, String username, String password) {
 		super(connectionUrl, username, password);
 		setDbType(RdbmsTypeEnum.CLICKHOUSE);
+	}
+	
+	@Override
+	public String setConnectionDetailsfromMap(Map<String, Object> configMap) throws RuntimeException {
+		if(configMap == null || configMap.isEmpty()){
+			throw new RuntimeException("Configuration map is null or empty");
+		}
+		
+		this.connectionUrl = (String) configMap.get(AbstractSqlQueryUtil.CONNECTION_URL);
+		
+		this.hostname = (String) configMap.get(AbstractSqlQueryUtil.HOSTNAME);
+		if((this.connectionUrl == null || this.connectionUrl.isEmpty()) && 
+				(hostname == null || hostname.isEmpty())
+			) {
+			throw new RuntimeException("Must pass in a hostname");
+		}
+		
+		this.port = (String) configMap.get(AbstractSqlQueryUtil.PORT);
+		String port = this.port;
+		if (port != null && !port.isEmpty()) {
+			port = ":" + port;
+		} else {
+			port = "";
+		}
+		
+		this.database = (String) configMap.get(AbstractSqlQueryUtil.DATABASE);
+		if((this.connectionUrl == null || this.connectionUrl.isEmpty()) && 
+				(this.database == null || this.database.isEmpty())
+				){
+			throw new RuntimeException("Must pass in database name");
+		}
+		
+		this.schema = (String) configMap.get(AbstractSqlQueryUtil.SCHEMA);
+		if((this.connectionUrl == null || this.connectionUrl.isEmpty()) && 
+				(this.schema == null || this.schema.isEmpty())
+				){
+			throw new RuntimeException("Must pass in schema name");
+		}
+		
+		this.additionalProps = (String) configMap.get(AbstractSqlQueryUtil.ADDITIONAL);
+
+		// do we need to make the connection url?
+		if(this.connectionUrl == null || this.connectionUrl.isEmpty()) {
+			this.connectionUrl = this.dbType.getUrlPrefix()+"://"+this.hostname+port+"/"+this.database+"?currentSchema="+this.schema;
+			
+			if(this.additionalProps != null && !this.additionalProps.isEmpty()) {
+				if(!this.additionalProps.startsWith(";") && !this.additionalProps.startsWith("&")) {
+					this.connectionUrl += ";" + this.additionalProps;
+				} else {
+					this.connectionUrl += this.additionalProps;
+				}
+			}
+		}
+		
+		return this.connectionUrl;
+	}
+
+	@Override
+	public String setConnectionDetailsFromSMSS(CaseInsensitiveProperties prop) throws RuntimeException {
+		if(prop == null || prop.isEmpty()){
+			throw new RuntimeException("Properties object is null or empty");
+		}
+		
+		this.connectionUrl = (String) prop.get(AbstractSqlQueryUtil.CONNECTION_URL);
+		
+		this.hostname = (String) prop.get(AbstractSqlQueryUtil.HOSTNAME);
+		if((this.connectionUrl == null || this.connectionUrl.isEmpty()) && 
+				(hostname == null || hostname.isEmpty())
+			) {
+			throw new RuntimeException("Must pass in a hostname");
+		}
+		
+		this.port = (String) prop.get(AbstractSqlQueryUtil.PORT);
+		String port = this.port;
+		if (port != null && !port.isEmpty()) {
+			port = ":" + port;
+		} else {
+			port = "";
+		}
+		
+		this.database = (String) prop.get(AbstractSqlQueryUtil.DATABASE);
+		if((this.connectionUrl == null || this.connectionUrl.isEmpty()) && 
+				(this.database == null || this.database.isEmpty())
+				){
+			throw new RuntimeException("Must pass in database name");
+		}
+		
+		this.schema = (String) prop.get(AbstractSqlQueryUtil.SCHEMA);
+		if((this.connectionUrl == null || this.connectionUrl.isEmpty()) && 
+				(this.schema == null || this.schema.isEmpty())
+				){
+			throw new RuntimeException("Must pass in schema name");
+		}
+		
+		this.additionalProps = (String) prop.get(AbstractSqlQueryUtil.ADDITIONAL);
+
+		// do we need to make the connection url?
+		if(this.connectionUrl == null || this.connectionUrl.isEmpty()) {
+			this.connectionUrl = this.dbType.getUrlPrefix()+"://"+this.hostname+port+"/"+this.database+"?currentSchema="+this.schema;
+			
+			if(this.additionalProps != null && !this.additionalProps.isEmpty()) {
+				if(!this.additionalProps.startsWith(";") && !this.additionalProps.startsWith("&")) {
+					this.connectionUrl += ";" + this.additionalProps;
+				} else {
+					this.connectionUrl += this.additionalProps;
+				}
+			}
+		}
+		
+		return this.connectionUrl;
+	}
+
+	@Override
+	public String buildConnectionString() {
+		if(this.connectionUrl != null && !this.connectionUrl.isEmpty()) {
+			return this.connectionUrl;
+		}
+		
+		if(this.hostname == null || this.hostname.isEmpty()) {
+			throw new RuntimeException("Must pass in a hostname");
+		}
+		
+		String port = this.port;
+		if (port != null && !port.isEmpty()) {
+			port = ":" + port;
+		} else {
+			port = "";
+		}
+		
+		if(this.database == null || this.database.isEmpty()) {
+			throw new RuntimeException("Must pass in database name");
+		}
+		
+		if(this.schema == null || this.schema.isEmpty()) {
+			throw new RuntimeException("Must pass in schema name");
+		}
+		
+		this.connectionUrl = this.dbType.getUrlPrefix()+"://"+this.hostname+port+"/"+this.database+"?currentSchema="+this.schema;
+		
+		if(this.additionalProps != null && !this.additionalProps.isEmpty()) {
+			if(!this.additionalProps.startsWith(";") && !this.additionalProps.startsWith("&")) {
+				this.connectionUrl += ";" + this.additionalProps;
+			} else {
+				this.connectionUrl += this.additionalProps;
+			}
+		}
+		
+		return this.connectionUrl;
 	}
 	
 }
