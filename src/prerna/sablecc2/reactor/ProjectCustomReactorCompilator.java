94b41f5e2136ea7e85f0645452656269ed732c66
mahkhalil <mahkhalil@deloitte.com>
2022-11-23 18:31:42 +0000

    UPDATE ::: Now storing if the project compilation failed to not re-attempt compilation and adding a reactor to get the status of the compilation
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47406 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2e6f6b5818076cf01fa5e55ae5bdf98ae80119bb

--- src/prerna/sablecc2/reactor/ProjectCustomReactorCompilator.java
+++ src/prerna/sablecc2/reactor/ProjectCustomReactorCompilator.java
@@ -6,6 +6,7 @@ import java.util.Set;
 public class ProjectCustomReactorCompilator {
 
 	private static Set<String> compiled = new HashSet<>();
+	private static Set<String> failed = new HashSet<>();
 
 	private ProjectCustomReactorCompilator() {
 		
@@ -13,14 +14,29 @@ public class ProjectCustomReactorCompilator {
 	
 	public static void setCompiled(String projectId) {
 		compiled.add(projectId);
+		failed.remove(projectId);
+	}
+	
+	public static void setFailed(String projectId) {
+		compiled.remove(projectId);
+		failed.add(projectId);
+	}
+	
+	public static boolean needsCompilation(String projectId) {
+		return !compiled.contains(projectId) && !failed.contains(projectId);
 	}
 	
 	public static boolean isCompiled(String projectId) {
 		return compiled.contains(projectId);
 	}
 	
+	public static boolean isFailed(String projectId) {
+		return failed.contains(projectId);
+	}
+	
 	public static void reset(String projectId) {
 		compiled.remove(projectId);
+		failed.remove(projectId);
 	}
 	
 }
