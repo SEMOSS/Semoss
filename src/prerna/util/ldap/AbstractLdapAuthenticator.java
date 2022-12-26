272032f761e9ee1b230b35bbb798d11473b0ae18
mahkhalil <mahkhalil@deloitte.com>
2022-12-26 12:27:25 +0000

    UPDATE ::: Improvements to ldap login and initial logic for password change
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47615 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/ldap/AbstractLdapAuthenticator.java
+++ src/prerna/util/ldap/AbstractLdapAuthenticator.java
@@ -1,8 +1,13 @@
 package prerna.util.ldap;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.directory.Attributes;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import prerna.auth.AccessToken;
 import prerna.auth.AuthProvider;
@@ -10,56 +15,61 @@ import prerna.util.SocialPropertiesUtil;
 
 public abstract class AbstractLdapAuthenticator implements ILdapAuthenticator  {
 
+	private static final Logger classLogger = LogManager.getLogger(AbstractLdapAuthenticator.class);
+
 	String providerUrl = null;
-	
+
 	// if multi structure searching
 	// need to have an application login to search for the user CN
 	// and then try to login as the user
 	String applicationSecurityPrincipal = null;
 	String applicationSecurityCredentials = null;
-	
+
 	// if single structure searching
 	// user is providing the CN to the DN structure
 	// and then providing their password as is
 	String securityPrincipalTemplate = null;
-	
+
 	// attribute mapping
 	String attributeIdKey = null;
 	String attributeNameKey = null;
 	String attributeEmailKey = null;
 	String attributeUserNameKey = null;
+
+	// if we require password changes
+	String attributeLastPwdChangeKey = null;
 	
 	String[] requestAttributes = null;
-	
+
 	// searching for user
 	String searchContextName = null;
 	String searchContextScopeString = null;
 	int searchContextScope = 2;
 	String searchMatchingAttributes = null;
-	
+
 	public void load() throws IOException {
 		// try to close anything if valid
 		close();
-		
+
 		// now load the values in social props
 		SocialPropertiesUtil socialData = SocialPropertiesUtil.getInstance();
-		
+
 		this.providerUrl = socialData.getProperty(LDAP_PROVIDER_URL);
 		this.applicationSecurityPrincipal = socialData.getProperty(LDAP_APPLICATION_SECURITY_PRINCIPAL);
 		this.applicationSecurityCredentials = socialData.getProperty(LDAP_APPLICATION_SECURITY_CREDENTIALS);
 		this.securityPrincipalTemplate = socialData.getProperty(LDAP_SECURITY_PRINCIPAL_TEMPLATE);
-		
+
 		this.attributeIdKey = socialData.getProperty(LDAP_ID_KEY);
 		this.attributeNameKey = socialData.getProperty(LDAP_NAME_KEY);
 		this.attributeEmailKey = socialData.getProperty(LDAP_EMAIL_KEY);
 		this.attributeUserNameKey = socialData.getProperty(LDAP_USERNAME_KEY);
-		
-		this.requestAttributes = new String[] {this.attributeIdKey, this.attributeNameKey, this.attributeEmailKey, this.attributeUserNameKey};
+
+		this.attributeLastPwdChangeKey = socialData.getProperty(LDAP_LAST_PWD_CHANGE_KEY);
 		
 		String LDAP_SEARCH_CONTEXT_NAME = LDAP_PREFIX + "search_context_name";
 		String LDAP_SEARCH_CONTEXT_SCOPE = LDAP_PREFIX + "search_context_scope";
 		String LDAP_SEARCH_MATCHING_ATTRIBUTES = LDAP_PREFIX + "search_matching_attributes";
-		
+
 		this.searchContextName = socialData.getProperty(LDAP_SEARCH_CONTEXT_NAME);
 		if(this.searchContextName == null || (this.searchContextName=this.searchContextName.trim()).isEmpty()) {
 			this.searchContextName = "ou=users,ou=system";
@@ -75,8 +85,25 @@ public abstract class AbstractLdapAuthenticator implements ILdapAuthenticator  {
 			this.searchMatchingAttributes = "(objectClass=inetOrgPerson)";
 		}
 		validate();
+		
+		List<String> requestAttributesList = new ArrayList<>();
+		requestAttributesList.add(this.attributeIdKey);
+		if(this.attributeNameKey != null && !this.attributeNameKey.isEmpty()) {
+			requestAttributesList.add(this.attributeNameKey);
+		}
+		if(this.attributeEmailKey != null && !this.attributeEmailKey.isEmpty()) {
+			requestAttributesList.add(this.attributeEmailKey);
+		}
+		if(this.attributeUserNameKey != null && !this.attributeUserNameKey.isEmpty()) {
+			requestAttributesList.add(this.attributeUserNameKey);
+		}
+		if(this.attributeLastPwdChangeKey != null && !this.attributeLastPwdChangeKey.isEmpty()) {
+			requestAttributesList.add(this.attributeLastPwdChangeKey);
+		}
+		
+		this.requestAttributes = requestAttributesList.toArray(new String[]{});
 	}
-	
+
 	@Override
 	public void validate() throws IOException {
 		// always need the provider url
@@ -86,31 +113,26 @@ public abstract class AbstractLdapAuthenticator implements ILdapAuthenticator  {
 		if(!this.providerUrl.startsWith("ldap://")) {
 			this.providerUrl = "ldap://" + this.providerUrl;
 		}
-		
+
 		// need to at least have the ID
 		if(this.attributeIdKey == null || (this.attributeIdKey=this.attributeIdKey.trim()).isEmpty()) {
 			throw new IllegalArgumentException("Must provide the attribute for the user id");
 		}
-		
+
 		try {
 			this.searchContextScope = Integer.parseInt(this.searchContextScopeString);
 		} catch(NumberFormatException e) {
 			throw new IllegalArgumentException("Search Context scope must be of value 0, 1, or 2");
 		}
 	}
-	
-	/**
-	 * Produce the access token from the user attributes
-	 * @param attributes
-	 * @return
-	 * @throws Exception
-	 */
-	protected AccessToken generateAccessToken(Attributes attributes) throws Exception {
+
+	@Override
+	public AccessToken generateAccessToken(Attributes attributes) throws Exception {
 		Object userId = null;
 		Object name = null;
 		Object email = null;
 		Object username = null;
-		
+
 		userId = attributes.get(this.attributeIdKey).get();
 		if(userId == null) {
 			throw new IllegalArgumentException("Cannot login user due to not having a proper attribute for the user id");
@@ -123,6 +145,10 @@ public abstract class AbstractLdapAuthenticator implements ILdapAuthenticator  {
 		}
 		if(this.attributeUserNameKey != null) {
 			username = attributes.get(this.attributeUserNameKey).get();
+		}
+
+		if(requirePasswordChange(attributes)) {
+			throw new LDAPPasswordChangeRequiredException("User must change their password before login");
 		}
 		
 		AccessToken token = new AccessToken();
@@ -137,8 +163,19 @@ public abstract class AbstractLdapAuthenticator implements ILdapAuthenticator  {
 		if(username != null) {
 			token.setUsername(username + "");
 		}
-		
+
 		return token;
 	}
 	
+	@Override
+	public boolean requirePasswordChange(Attributes attributes) {
+		Object lastPwdChange = null;
+		if(this.attributeLastPwdChangeKey != null) {
+			lastPwdChange = attributes.get(this.attributeLastPwdChangeKey);
+			System.out.println(lastPwdChange + " ::: " + lastPwdChange.getClass().getName());
+		}
+		
+		return false;
+	}
+
 }
