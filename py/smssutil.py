eb397dcb7852c84b9550205d135a9db3cf6c7067
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-01-10 02:41:34 +0000

    Update for GPT 3
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47689 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- py/smssutil.py
+++ py/smssutil.py
@@ -1,5 +1,4 @@
 # all of the util functions go here
-
 def getfunctions(file):
 	import inspect
 	print ("Loading file", file)
@@ -177,3 +176,180 @@ def load_hugging_face_model(modelName, typeOfModel, cacheFolder):
 		from transformers import pipeline
 		pipe = pipeline(typeOfModel, model=model, tokenizer=tokenizer)
 	return pipe
+
+def get_function_signature(func_name):
+	from inspect import signature
+	from enum import Enum
+	import types
+	dict = signature(func_name).parameters.copy()
+	keys = list(dict.keys())
+	finalList = {}
+	
+	# add the current name as function name
+	finalList.update({"function_name":func_name.__name__})
+	# first elementis the type
+	# second element is the default value
+	# third item is if this is optional
+	# if there is a default value it is optional
+	value = []
+	for item in keys:
+		key = dict[item].name
+		param_type = dict[item].annotation
+		#print(item)
+		thisValue = dict[item].default
+		
+		# first figure out the types
+		# handle the enumeration
+		processed = False
+		
+		if(param_type.__class__ == Enum.__class__):
+			dropdown = {}
+			inner_dict_list = list(dict[item].annotation.__members__.copy().values())
+			for i in inner_dict_list:
+				dropdown.update({i.name:i.value})
+			value.append('PixelDataType.Multi')
+			value.append(dropdown)
+			value.append('False')
+			finalList.update({key:value})
+			processed = True
+			#print(f"handled as enum {key}: {value} {~processed}")
+			value = []
+			
+		# handle function
+		if(param_type.__class__ == types.FunctionType):
+			#print("handled as function")
+			value.append('PixelDataType.Function')
+			value.append(get_function_signature(param_type))
+			value.append('False')
+			finalList.update({key:value})
+			processed = True
+			value = []
+			
+		if(param_type == dict[item].empty and not processed):	
+			#print("param type is empty")
+			# impute from the default value if you can
+			# if it is empty - that is a string value no default
+			## if it is none it is a int value no default
+			# else this is optional
+			# also check to see if the type is already there
+			if(thisValue == dict[item].empty):
+				# need to check the pixel data type
+				value.append('PixelDataType.Str')
+				value.append('')
+				value.append('False')
+				processed = True
+			else:
+				if(thisValue == None): 
+					value.append('PixelDataType.Int')
+					value.append('')
+					value.append('False')
+				else:
+					# check to see if this starts with quotes
+					if(type(thisValue) == (bool)):
+						value.append('PixelDataType.Boolean')
+					else:
+						if(isinstance(thisValue, (int,float))):
+							value.append('PixelDataType.bool')
+						else:
+							if(~processed):
+							# turn everything else into string
+							#if(isinstance(thisValue, (str))):
+								value.append('PixelDataType.Str')
+					value.append(str(thisValue))
+					value.append('True')
+			finalList.update({key:value})
+			value = []
+		else:
+			if(not processed):
+				# use the param type to fill the data
+				if(param_type == int):
+					value.append('PixelDataType.Int')
+					# check again to see if it is empty
+					if(thisValue == dict[item].empty):
+						value.append('')
+						value.append('False')
+					else:
+						value.append(thisValue)
+						value.append('True')
+				else:
+					if(param_type == bool):
+						value.append('PixelDataType.Boolean')
+						if(thisValue == dict[item].empty):
+							value.append('')
+							value.append('False')
+						else:
+							value.append(thisValue)
+							value.append('True')
+					else:
+						#Everything else is stringif(param_type == bool):
+						value.append('PixelDataType.Str')
+						if(thisValue == dict[item].empty):
+							value.append('')
+							value.append('False')
+						else:
+							value.append(thisValue)
+							value.append('True')
+				finalList.update({key:value})
+				value = []
+
+	# last item is return
+	key = 'return_value'
+	returns = func_name.__annotations__
+	ret_type = 'unknown'
+	if('return' in returns):
+		ret_type = returns['return'].__name__
+	value.append(f'PixelDataType.{ret_type}')
+	value.append(ret_type)
+	value.append('NA')
+	finalList.update({key:value})
+	return finalList
+
+def run_gpt_3(nl_query, max_tokens_value):
+	import os
+	import openai
+	try:
+		openai.api_key
+	except AttributeError:
+		return "Open AI Key Not Available"
+	else:
+		response = openai.Completion.create(model="code-davinci-002", prompt=nl_query, temperature=0, max_tokens=max_tokens_value, top_p=1, frequency_penalty=0, presence_penalty=0,stop=["#", ";"])
+		query = " SELECT " + response.choices[0].text
+		print (query)
+		return query
+
+def convert_pdf_to_text(document_location):
+	import PyPDF2
+	pdfFileObj = open("C:/Users/pkapaleeswaran/Desktop/From C Drive Root/RFP/AF TO 49/fin_management_framework.pdf", 'rb')
+	pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+	outputFile = open("C:/Users/pkapaleeswaran/Desktop/From C Drive Root/RFP/AF TO 49/fin_management_framework.txt", 'w')
+	for i in range(pdfReader.numPages):  
+		page_text = pdfReader.getPage(i).extractText() 
+		outputFile.write(page_text)
+		outputFile.write("\r\n\r\n")
+
+
+def parse_sentence(text):
+	from openie import StanfordOpenIE
+	# https://stanfordnlp.github.io/CoreNLP/openie.html#api
+	# Default value of openie.affinity_probability_cap was 1/3.
+	properties = {
+		'openie.affinity_probability_cap': 2 / 3,
+	}
+	client = StanfordOpenIE(properties=properties)
+	return client.annotate(text)
+
+def parse_paragraph(para):
+	from nltk.tokenize import sent_tokenize
+	sentences = sent_tokenize(para)
+	all_triples = []
+	for s in sentences:
+		all_triples.append(parse_sentence(s))
+	return all_triples
+	
+def run_gptj_causallm(prompt):
+	from transformers import AutoModelForCausalLM, AutoTokenizer
+	model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")
+	tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+	input_ids = tokenizer(prompt, return_tensors="pt").input_ids
+	gen_tokens = model.generate(input_ids, do_sample=True, temperature=0.1,max_length=200,)
+	gen_text = tokenizer.batch_decode(gen_tokens)[0]
