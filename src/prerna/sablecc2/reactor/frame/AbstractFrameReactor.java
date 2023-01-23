6ee6c9c3ba13d9dc2060ba094c840fdf9ff741c8
mahkhalil <mahkhalil@deloitte.com>
2023-01-23 16:14:07 +0000

    UPDATE ::: Moving logic to be generic to pull the latest touched frame vs the default frame UPDATE ::: FrameFilterWithSQL now uses the latest touched frame by default
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47770 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3d4a905c8563a2a5e1ea6cc88e58e9270319a9c5

--- src/prerna/sablecc2/reactor/frame/AbstractFrameReactor.java
+++ src/prerna/sablecc2/reactor/frame/AbstractFrameReactor.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import prerna.algorithm.api.ITableDataFrame;
 import prerna.poi.main.HeadersException;
+import prerna.query.querystruct.SelectQueryStruct;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
@@ -12,6 +13,10 @@ import prerna.sablecc2.reactor.AbstractReactor;
 
 public abstract class AbstractFrameReactor extends AbstractReactor {
 
+	/**
+	 * 
+	 * @return
+	 */
 	protected ITableDataFrame getFrame() {
 		GenRowStruct grs = this.store.getNoun(PixelDataType.FRAME.getKey());
 		// see if a frame is passed in
@@ -29,6 +34,45 @@ public abstract class AbstractFrameReactor extends AbstractReactor {
 		// put this into the noun store
 		// so that we can pull it for other pipeline
 		ITableDataFrame defaultFrame = (ITableDataFrame) this.insight.getDataMaker();
+		if (defaultFrame != null) {
+			this.store.makeNoun(ReactorKeysEnum.FRAME.getKey()).add(new NounMetadata(defaultFrame, PixelDataType.FRAME));
+			return defaultFrame;
+		}
+
+		throw new NullPointerException("No frame found");
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	protected ITableDataFrame getFrameDefaultLast() {
+		GenRowStruct grs = this.store.getNoun(PixelDataType.FRAME.getKey());
+		// see if a frame is passed in
+		if (grs != null && !grs.isEmpty()) {
+			List<Object> frameInputs = grs.getValuesOfType(PixelDataType.FRAME);
+			return (ITableDataFrame) frameInputs.get(0);
+		}
+		
+		List<NounMetadata> curNouns = this.curRow.getNounsOfType(PixelDataType.FRAME);
+		if(curNouns != null && !curNouns.isEmpty()) {
+			return (ITableDataFrame) curNouns.get(0).getValue();
+		}
+		
+		ITableDataFrame defaultFrame = null;
+		// get the last frame
+		SelectQueryStruct qs = this.insight.getLastQS(this.insight.getLastPanelId());
+		if(qs != null) {
+			defaultFrame = qs.getFrame();
+		}
+		if(defaultFrame != null) {
+			return defaultFrame;
+		}
+		
+		// else, grab the default frame from the insight
+		// put this into the noun store
+		// so that we can pull it for other pipeline
+		defaultFrame = (ITableDataFrame) this.insight.getDataMaker();
 		if (defaultFrame != null) {
 			this.store.makeNoun(ReactorKeysEnum.FRAME.getKey()).add(new NounMetadata(defaultFrame, PixelDataType.FRAME));
 			return defaultFrame;
