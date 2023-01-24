f63b369bf72376b52b18b4ce171d832c6b2a0b75
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-01-24 22:59:16 +0000

    Updates to move everything to sqlite. Resolves the issue with doing multiple filters one on top of the other
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47790 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: dd0569915094a0b8c1520b2f33df910bef104160

--- src/prerna/sablecc2/reactor/frame/FrameFilterWithSQLReactor.java
+++ src/prerna/sablecc2/reactor/frame/FrameFilterWithSQLReactor.java
@@ -6,6 +6,7 @@ import prerna.algorithm.api.ITableDataFrame;
 import prerna.ds.py.PandasFrame;
 import prerna.ds.py.PandasSyntaxHelper;
 import prerna.ds.r.RDataTable;
+import prerna.query.querystruct.SelectQueryStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
@@ -33,21 +34,51 @@ public class FrameFilterWithSQLReactor extends AbstractFrameReactor {
 		
 		String query = Utility.decodeURIComponent(this.keyValue.get(this.keysToGet[1]));
 		String newFrameName = Utility.getRandomString(6);
+		String oldFrameName = frame.getName();
 		
 		if(frame instanceof PandasFrame) {
-			String frameMaker = newFrameName + "= pd.DataFrame(sqldf(\"" + query.replace("\"", "\\\"") + "\"))";
+			
+			// drop the old frame and old table
+			// check to see if this is a new frame
+			// if so construct a DataFrame and see
+			PandasFrame pFrame = (PandasFrame)frame;
+			String sqlite = pFrame.getSQLite();
+
+			//pd.read_sql("select * from diab1 where age > 60", conn)
+			
+			query = query.replace("\"", "\\\"");
+			// drop into sqlite the new name
+			String frameMaker = newFrameName + " = pd.read_sql('" + query + "', " + sqlite + ")";
 			logger.info("Creating frame with query..  " + query + " <<>> " + frameMaker);
-			insight.getPyTranslator().runEmptyPy("from pandasql import sqldf");
-			insight.getPyTranslator().runScript(frameMaker); 
+			insight.getPyTranslator().runSingle(insight.getUser().getVarMap(), frameMaker); 
 			// need to make the wrapper in this instance
 			insight.getPyTranslator().runScript(PandasSyntaxHelper.makeWrapper(
 					PandasSyntaxHelper.createFrameWrapperName(newFrameName), newFrameName));
+			
+			// out1.to_sql("diab1", conn, if_exists="replace", index=False)
+			String addSqlTable = newFrameName + ".to_sql('" + newFrameName + "', " + sqlite + ", if_exists='replace', index=False)";
+			insight.getPyTranslator().runScript(addSqlTable);
+
+			// remove frames
+			if(!oldFrameName.equalsIgnoreCase(frame.getOriginalName()))
+			{
+				//"SELECT name FROM sqlite_master where type='table'"
+				String dropTable = sqlite + ".cursor().execute('DROP TABLE " + oldFrameName + "').fetchall()";
+				String delete = "del " + oldFrameName + " ," + oldFrameName + "w";
+				insight.getPyTranslator().runScript(dropTable);
+				insight.getPyTranslator().runScript(delete);				
+			}			
 		} else if(frame instanceof RDataTable){
 			AbstractRJavaTranslator rt = insight.getRJavaTranslator(this.getClass().getName());
 			String frameMaker = newFrameName + " <- as.data.table(sqldf(\"" + query.replace("\"", "\\\"") + "\"))";
 			logger.info("Creating frame with query..  " + query + " <<>> " + frameMaker);
 			rt.runRAndReturnOutput("library(sqldf)");
 			rt.runR(frameMaker); // load the sql df			
+			if(!oldFrameName.equalsIgnoreCase(frame.getOriginalName()))
+			{
+				String delete = "rm(" + oldFrameName+ ")";
+				rt.runR(delete);				
+			}
 		}
 		
 		// reset the name back to the original name
