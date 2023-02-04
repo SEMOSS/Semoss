90cc6cd3722cd0c73487c4cf5b50a3e4c23ed1d8
mahkhalil <mahkhalil@deloitte.com>
2023-02-04 14:22:38 +0000

    UPDATE ::: Adding utility to push smss file to the cloud UPDATE ::: Adding utility to fully push/pull the database to the cloud
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47895 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d0161f5966676259f9184f6ecaccc799a22dadbd

---
+++ src/prerna/sablecc2/reactor/cluster/PushDatabaseToCloudReactor.java
@@ -0,0 +1,40 @@
+package prerna.sablecc2.reactor.cluster;
+
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.SecurityDatabaseUtils;
+import prerna.cluster.util.ClusterUtil;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Utility;
+
+public class PushDatabaseToCloudReactor extends AbstractReactor {
+	
+	public PushDatabaseToCloudReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.DATABASE.getKey()};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String databaseId = this.keyValue.get(this.keysToGet[0]);
+		
+		if(databaseId == null || databaseId.isEmpty()) {
+			throw new IllegalArgumentException("Must input an database id");
+		}
+		
+		if(AbstractSecurityUtils.securityEnabled()) {
+			// make sure valid id for user
+			if(!SecurityDatabaseUtils.userIsOwner(this.insight.getUser(), databaseId)) {
+				// you dont have access
+				throw new IllegalArgumentException("Database does not exist or user is not an owner to force pulling from cloud storage");
+			}
+		}
+		
+		ClusterUtil.reactorPullApp(databaseId, Utility.isEngineLoaded(databaseId));
+		return new NounMetadata(true, PixelDataType.BOOLEAN);
+	}
+
+}
+
