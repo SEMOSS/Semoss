3552acf1eea1340bac53c1f5402bd62a27927c6b
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-02-10 01:16:07 +0000

    Initial Commit for GaaS (Gen AI As a Service - QA Module with Siamese Search )
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47925 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ py/gaas_qa_siamese.py
@@ -0,0 +1,133 @@
+def create_model(folder_name="random", sent_ckpt='msmarco-distilbert-base-v4', encoding='windows-1252', separator="=x=x=x="):
+	import os
+	folder_name = folder_name.replace(os.sep, '/')
+	model_file_name = f"{folder_name}/model/siamese.pt"
+	import pathlib
+	import torch
+	from sentence_transformers import SentenceTransformer, util
+	from transformers import pipeline	
+	model_file = pathlib.Path(model_file_name)
+	
+	if model_file.exists():
+		document_embeddings = torch.load(model_file_name)
+		return document_embeddings
+	else:
+		model_dir = pathlib.Path(model_file.parent)
+		print(model_dir)
+		if not model_dir.exists():
+			os.mkdir(f"{folder_name}/model")
+		
+	bi_encoder = SentenceTransformer(sent_ckpt)
+	bi_encoder.max_seq_length=256
+	# picks all of the text files here
+	# converts it to model
+	import glob
+	file_list = glob.glob(f"{folder_name}/processed/*.txt")
+	import numpy as np
+	master_document = []
+	all_files = ""
+	for i, file in enumerate(file_list):
+		# index this file
+		# encoding windows encoding windows-1252
+		#print("processing file ", file)
+		input_file = pathlib.Path(file)
+		if i==0:
+			all_files = f"{input_file.stem}"
+		else:
+			all_files = f"{all_files}, {input_file.stem}"
+		text = open(file, "r", encoding=encoding).read()
+		#print(text)
+		# separate it by some separator, the default is 
+		documents = np.array(list(filter(lambda x : len(x) > 10, text.split(separator))))
+		master_document.extend(documents)
+		
+	# index all of them	
+	document_embeddings = bi_encoder.encode(master_document, convert_to_tensor=True, show_progress_bar=True)
+	# write it to the folder
+	import torch
+	torch.save(document_embeddings, model_file_name)
+	return document_embeddings
+
+def hydrate_model(folder_name = None):
+	import os
+	folder_name = folder_name.replace(os.sep, '/')
+	from sentence_transformers import SentenceTransformer, util
+	from transformers import pipeline
+	import torch
+	model_file_name = f"{folder_name}/model/siamese.pt"
+	import pathlib
+	model_file = pathlib.Path(model_file_name)		
+	if model_file.exists():
+		model = torch.load(model_file_name)
+		return model
+	else:
+		return None
+
+def delete_model(folder_name=None):
+	import os
+	folder_name = folder_name.replace(os.sep, '/')
+	os.rmdir(f"{folder_name}/model")
+
+def delete_processed(folder_name=None):
+	import os
+	folder_name = folder_name.replace(os.sep, '/')
+	os.rmdir(f"{folder_name}/processed")
+
+
+def search(folder_name=None, sent_ckpt='msmarco-distilbert-base-v4', qa_ckpt="deepset/roberta-base-squad2", encoding='windows-1252', separator="=x=x=x=", model=None, query=None, threshold=0.2):
+	import os
+	folder_name = folder_name.replace(os.sep, '/')
+	from sentence_transformers import SentenceTransformer, util
+	from transformers import pipeline
+	import torch
+	if model is None:
+		return []
+	bi_encoder = SentenceTransformer(sent_ckpt)
+	bi_encoder.max_seq_length=256
+	# picks all of the text files here
+	# converts it to model
+	import glob
+	file_list = glob.glob(f"{folder_name}/processed/*.txt")
+	import numpy as np
+	master_document = []
+	all_files = ""
+	import pathlib
+	for i, file in enumerate(file_list):
+		# index this file
+		# encoding windows encoding windows-1252
+		input_file = pathlib.Path(file)
+		if i==0:
+			all_files = f"{input_file.stem}"
+		else:
+			all_files = f"{all_files}, {input_file.stem}"
+		text = open(file, "r", encoding=encoding).read()
+		# separate it by some separator, the default is 
+		documents = np.array(list(filter(lambda x : len(x) > 10, text.split(separator))))
+		master_document.extend(documents)
+		
+	# index all of them	
+	# write it to the folder
+	nlp = pipeline("question-answering", model=qa_ckpt , tokenizer=qa_ckpt, max_length=10)
+	question_embedding = bi_encoder.encode(query, convert_to_tensor=True)
+	hits = util.semantic_search(question_embedding, model, top_k=3)[0]	
+
+	return_data = []
+	
+	print(hits)
+	print(len(master_document))
+
+	for i,hit in enumerate(hits):
+	  #print(f'Document {i+1} Cos Sim {hit["score"]:.3f}:\n\r {documents[hit["corpus_id"]]}')
+		item = {}
+		
+		if(hit["score"] > threshold):
+			nlp_output = nlp(query, str(master_document[hit['corpus_id']]))
+			print(nlp_output)
+			#item.update({"full_document" : master_document[hit['corpus_id']]})
+			item.update({"start" : nlp_output["start"]})
+			item.update({"end" : nlp_output["end"]})
+			item.update({"answer" : nlp_output["answer"]})
+			item.update({"score" : nlp_output["score"]})
+			return_data.append(item);
+	
+	return return_data
