0b0e10526befc1488aa137bc4e91cf60d2d94447
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-02-10 01:30:31 +0000

    GaaS.qa - Almost all of these types of modules will have 3 or may be 4 reactors - Preprocess, FineTune, CreateModel, Search / Execute model.  which calls the appropriate backend
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@47926 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c567cddc5014c51598c0da6e1fc1104c274e0a35

---
+++ src/prerna/sablecc2/reactor/frame/gaas/qa/QueryQAModelReactor.java
@@ -0,0 +1,94 @@
+package prerna.sablecc2.reactor.frame.gaas.qa;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import prerna.ds.py.PyTranslator;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.AssetUtility;
+import prerna.util.Utility;
+
+public class QueryQAModelReactor extends AbstractReactor {
+
+	// creates a qa model
+	final String modelType = "gaas";
+	final String modelSubType = "qa";
+		
+	public QueryQAModelReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.MODEL.getKey(), ReactorKeysEnum.THRESHOLD.getKey(), ReactorKeysEnum.SEARCH.getKey()};
+		this.keyRequired = new int[] {1,1, 0};
+	}
+
+	
+	
+	@Override
+	public NounMetadata execute() {
+		
+		organizeKeys();
+		
+		String query = null;
+		if(!keyValue.containsKey(keysToGet[3]))
+			return NounMetadata.getErrorNounMessage("Missing query");
+		else
+			query = keyValue.get(keysToGet[3]);
+		
+		double threshold = 0.2;
+		if(keyValue.containsKey(keysToGet[2]))
+			threshold = Double.parseDouble(keyValue.get(keysToGet[2]))/100;
+		// get the folder name
+		// see if the processed folder is already there
+		// if so pass the processed folder with the model to invoke
+		
+		String folderName = keyValue.get(keysToGet[0]);
+		String modelName = keyValue.get(keysToGet[1]);
+		
+		
+		
+		String basePath = AssetUtility.getProjectAssetFolder(this.insight.getProjectId());
+		folderName = basePath + "/" + folderName;
+		folderName = folderName.replace("\\", "/");
+		
+		System.err.println("Folder.. " + folderName);
+
+
+		String semossModelName = modelType + "_" + modelSubType + "_" + modelName;
+
+		PyTranslator pt = this.insight.getPyTranslator();
+		pt.runScript("import " + semossModelName);
+		//  gaas.search_siamese(folder_name="c:/users/pkapaleeswaran/workspacej3/datasets/text/far"
+		//, model=model, query="what is subcontract", 
+		// separator="FAR::::")
+		
+		String modelVariable = insight.getProjectId();
+		modelVariable = Utility.cleanString(modelVariable, true);
+		modelVariable = modelVariable.replace("-", "_");
+		modelVariable = semossModelName + "_" + modelVariable;
+
+		boolean hasModel = (Boolean)pt.runScript("'" + modelVariable + "' in locals()");
+		if(!hasModel)
+		{
+			pt.runScript(modelVariable + " = " + semossModelName + ".hydrate_model(folder_name='" + folderName + "')");
+		}
+		
+			// the model to call it
+		List results	= (List)pt.runScript(semossModelName + ".search("
+												+ "folder_name='" + folderName + "', "
+												+ "model=" + modelVariable +", "
+												+ "threshold=" + threshold + ", "
+												+ "query='" + query + "')"
+												);
+		
+		//pt.runScript(modelVariable + " = " +semossModelName + ".create_model(folder_name='" + folderName + "')");
+		
+		Map outputMap = new HashMap();
+		outputMap.put("query", query);
+		outputMap.put("data", results);
+		
+		return new NounMetadata(outputMap, PixelDataType.MAP);
+	}
+
+}
