b691b718e86a5c06eddbf3dd40a90ea86cc389de
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-03-19 13:26:38 +0000

    SocketServer, Handler - Simple updates for test mode PyExecutor - UPdates for Redirect Stream PyTranslator, TCPPyTranslator - There are 2 ways of running python now, runScript(script) - doesnt provide you output, runScript(script, your_insight) - provides you output. Updates for that ErrorSenderThread.java - Main thread that sends errors back to Front End
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48108 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: caefc8dc464377fb6b7b07445fd27dbdb6d3f915

--- src/prerna/tcp/PayloadStruct.java
+++ src/prerna/tcp/PayloadStruct.java
@@ -10,7 +10,7 @@ public class PayloadStruct implements Serializable{
 	// SET
 	
 	
-	public enum OPERATION {R, PYTHON, CHROME, ECHO, ENGINE, REACTOR, INSIGHT, PROJECT, CMD}; 
+	public enum OPERATION {R, PYTHON, CHROME, ECHO, ENGINE, REACTOR, INSIGHT, PROJECT, CMD, STDOUT, STDERR}; 
 	public OPERATION operation = OPERATION.R; // setting default to R
 	public String methodName = "method";
 	public Object [] payload = null;
