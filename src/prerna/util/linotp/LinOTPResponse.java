bdb37f5eaba7171a7071a1cf68bcc5080b6daf0d
mahkhalil <mahkhalil@deloitte.com>
2023-03-22 20:21:20 +0000

    UPDATE ::: Pulling linotp code out into package
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48126 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d495e84fee97187f2cd52d4fbc77fc311b1d1c6e

---
+++ src/prerna/util/linotp/LinOTPResponse.java
@@ -0,0 +1,37 @@
+package prerna.util.linotp;
+
+import java.util.Map;
+
+import prerna.auth.AccessToken;
+
+public class LinOTPResponse {
+
+	private Map<String, Object> returnMap = null;
+	private AccessToken token = null;
+	private int responseCode = -1;
+	
+	public Map<String, Object> getReturnMap() {
+		return returnMap;
+	}
+	
+	public void setReturnMap(Map<String, Object> returnMap) {
+		this.returnMap = returnMap;
+	}
+	
+	public AccessToken getToken() {
+		return token;
+	}
+	
+	public void setToken(AccessToken token) {
+		this.token = token;
+	}
+	
+	public int getResponseCode() {
+		return responseCode;
+	}
+	
+	public void setResponseCode(int responseCode) {
+		this.responseCode = responseCode;
+	}
+	
+}
