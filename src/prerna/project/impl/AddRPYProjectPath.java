274834e7326a47e387810e629bc86f49e82b8845
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-03-30 14:08:39 +0000

    Ability to add project specific path to the py runner and r runner
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48183 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 38c8cf8d965cc2206fec25ecf0deff959eb21c49

---
+++ src/prerna/project/impl/AddRPYProjectPath.java
@@ -0,0 +1,49 @@
+package prerna.project.impl;
+
+import prerna.ds.py.PyTranslator;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.sablecc2.reactor.frame.r.util.AbstractRJavaTranslator;
+import prerna.util.AssetUtility;
+
+public class AddRPYProjectPath extends AbstractReactor {
+	
+	public AddRPYProjectPath()
+	{
+		this.keysToGet = new String [] {ReactorKeysEnum.PROJECT.getKey()};
+		this.keyRequired = new int[] {1};
+	}
+
+	@Override
+	public NounMetadata execute() 
+	{
+		// TODO Auto-generated method stub
+		// get the project id
+		// set it as part of path
+		organizeKeys();
+		
+		PyTranslator pyt = this.insight.getPyTranslator();
+		AbstractRJavaTranslator rt = this.insight.getRJavaTranslator(this.getClass().getName());
+
+		String projectId = keyValue.get(keysToGet[0]);
+		String basePath = AssetUtility.getProjectAssetFolder(projectId);
+		String folderName = basePath + "/py";
+		folderName = folderName.replace("\\", "/");
+
+
+		if(pyt != null)
+		{	
+			String path = "import sys";
+			pyt.runScript(path);
+			path = "sys.path.append('" + folderName +"')";
+			pyt.runScript(path);
+		}
+		if(rt != null)
+		{
+			rt.runR("setwd('" + folderName + "')");
+		}
+		
+		return NounMetadata.getSuccessNounMessage("Added " + projectId + " to path");
+	}
+}
