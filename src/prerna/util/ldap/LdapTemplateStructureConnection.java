b51b0950904815d80b9a033650594874d03fc533
mahkhalil <mahkhalil@deloitte.com>
2023-04-08 02:20:59 +0000

    UPDATE ::: Adding logic to force password change on first login after password reset by admin
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48251 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 02271f9345687be213986f19aa53d418c31231cc

--- src/prerna/util/ldap/LdapTemplateStructureConnection.java
+++ src/prerna/util/ldap/LdapTemplateStructureConnection.java
@@ -45,11 +45,11 @@ public class LdapTemplateStructureConnection extends AbstractLdapAuthenticator {
 	// if we require password changes
 	String attributeLastPwdChangeKey = null;
 	int requirePwdChangeAfterDays = 90;
-	
+
 	// do we use a different user/pass for changing pwds
 	boolean useCustomContextForPwdChange = false;
 	transient DirContext customPwdChangeLdapContext = null;	
-	
+
 	String[] requestAttributes = null;
 
 	// searching for user
@@ -72,7 +72,7 @@ public class LdapTemplateStructureConnection extends AbstractLdapAuthenticator {
 			securityPrincipalTemplate = new ArrayList<>(1);
 			securityPrincipalTemplate.add(securityPrincipalTemplateStr);
 		}
-		
+
 		this.attributeIdKey = socialData.getProperty(LDAP_ID_KEY);
 		this.attributeNameKey = socialData.getProperty(LDAP_NAME_KEY);
 		this.attributeEmailKey = socialData.getProperty(LDAP_EMAIL_KEY);
@@ -101,7 +101,7 @@ public class LdapTemplateStructureConnection extends AbstractLdapAuthenticator {
 		if(this.searchMatchingAttributes == null || (this.searchMatchingAttributes=this.searchMatchingAttributes.trim()).isEmpty()) {
 			this.searchMatchingAttributes = "(objectClass=inetOrgPerson)";
 		}
-		
+
 		List<String> requestAttributesList = new ArrayList<>();
 		requestAttributesList.add(this.attributeIdKey);
 		if(this.attributeNameKey != null && !this.attributeNameKey.isEmpty()) {
@@ -117,7 +117,7 @@ public class LdapTemplateStructureConnection extends AbstractLdapAuthenticator {
 			requestAttributesList.add(this.attributeLastPwdChangeKey);
 		}
 		this.requestAttributes = requestAttributesList.toArray(new String[]{});
-		
+
 
 		// validate any inputs and throw errors if invalid
 		validate();
@@ -143,7 +143,7 @@ public class LdapTemplateStructureConnection extends AbstractLdapAuthenticator {
 		} catch(NumberFormatException e) {
 			throw new IllegalArgumentException("Search Context scope must be of value 0, 1, or 2");
 		}
-		
+
 		if(this.securityPrincipalTemplate == null || this.securityPrincipalTemplate.isEmpty()) {
 			throw new IllegalArgumentException("Must provide the DN template");
 		}
@@ -152,7 +152,7 @@ public class LdapTemplateStructureConnection extends AbstractLdapAuthenticator {
 				throw new IllegalArgumentException("Must provide the a location to fill the username passed from the user using " + SECURITY_PRINCIPAL_TEMPLATE_USERNAME);
 			}
 		}
-		
+
 		// do we have anything for custom context to change pwds if users are unable to change
 		SocialPropertiesUtil socialData = SocialPropertiesUtil.getInstance();
 		this.useCustomContextForPwdChange = Boolean.parseBoolean(socialData.getProperty(LDAP_USE_CUSTOM_CONTEXT_FOR_PWD_CHANGE_KEY)+"");
@@ -173,7 +173,7 @@ public class LdapTemplateStructureConnection extends AbstractLdapAuthenticator {
 	public AccessToken authenticate(String username, String password) throws Exception {
 		DirContext ldapContext = null;
 		try {
-			LDAPConnectionHelper loginObj = LDAPConnectionHelper.tryLogins(this.providerUrl, this.securityPrincipalTemplate, username, password);
+			LDAPConnectionHelper loginObj = tryLogins(this.providerUrl, this.securityPrincipalTemplate, username, password);
 			String principalTemplate = loginObj.getPrincipalTemplate();
 			String principalDN = loginObj.getPrincipalDN();
 			ldapContext = loginObj.getLdapContext();
@@ -200,65 +200,175 @@ public class LdapTemplateStructureConnection extends AbstractLdapAuthenticator {
 							this.attributeUserNameKey, this.attributeLastPwdChangeKey, this.requirePwdChangeAfterDays);
 				}
 			}
-
-			return null;
 		} catch(Exception e) {
 			classLogger.error(Constants.STACKTRACE, e);
-			throw e;
+
+			// we couldn't authenticate you
+			// but maybe you just need a new password on initial login
+			if(ldapContext != null && this.useCustomContextForPwdChange) {
+				if(this.customPwdChangeLdapContext == null) {
+					classLogger.warn("Invalid configuration - using custom context for pwd change set but context wasn't created");
+				} else {
+					// we need to search for the user to grab his attributes
+					// and see if we must change his password
+
+					// first find the user DN
+					String searchContextName = this.searchContextName.replace(SECURITY_PRINCIPAL_TEMPLATE_USERNAME, username);
+					String searchFilter = this.searchMatchingAttributes.replace(SECURITY_PRINCIPAL_TEMPLATE_USERNAME, username);
+
+					SearchControls controls = new SearchControls();
+					controls.setSearchScope(this.searchContextScope);
+					controls.setReturningAttributes(this.requestAttributes);
+
+					NamingEnumeration<SearchResult> findUser = this.customPwdChangeLdapContext.search(searchContextName, searchFilter, controls);
+
+					String principalDN = null;
+					SearchResult result = null;
+					while(findUser.hasMoreElements()) {
+						result = findUser.next();
+						// we got the user DN
+						principalDN = result.getNameInNamespace();
+						classLogger.info("Found user DN = " + principalDN);
+						try {
+							// now grab the last pwd change key and throw any LDAPPasswordChangeRequiredException
+							this.getLastPwdChange(result.getAttributes(), this.attributeLastPwdChangeKey, this.requirePwdChangeAfterDays);
+						} catch(LDAPPasswordChangeRequiredException e2) {
+							throw e2;
+						} catch(Exception e2) {
+							classLogger.error("An error occurred tryign to search for user last pwd change attribute");
+							classLogger.error(Constants.STACKTRACE, e2);
+						}
+					}
+				}
+			} else {
+				throw e;
+			}
 		} finally {
 			if(ldapContext != null) {
 				ldapContext.close();
 			}
 		}
+		
+		return null;
+	}
+
+	private LDAPConnectionHelper tryLogins(String providerUrl, List<String> securityPrincipalTemplate, String username, String password) throws Exception {
+		int i = 0;
+		Exception lastException = null;
+		do {
+			try {
+				String principalTemplate = securityPrincipalTemplate.get(i);
+				String principalDN = principalTemplate.replace(ILdapAuthenticator.SECURITY_PRINCIPAL_TEMPLATE_USERNAME, username);
+				DirContext ldapContext = createLdapContext(providerUrl, principalDN, password);
+
+				LDAPConnectionHelper retObj = new LDAPConnectionHelper();
+				retObj.setLdapContext(ldapContext);
+				retObj.setPrincipalDN(principalDN);
+				retObj.setPrincipalTemplate(principalTemplate);
+				return retObj;
+			} catch(Exception e) {
+				classLogger.error("Failed connection with template: " + securityPrincipalTemplate.get(i));
+				lastException = e;
+			}
+			i++;
+		} while(i < securityPrincipalTemplate.size());
+
+		// if we dont have any successful login, just throw the last error and also print it to log
+		classLogger.error(Constants.STACKTRACE, lastException);
+		throw lastException;
 	}
 
 	@Override
 	public void updateUserPassword(String username, String curPassword, String newPassword) throws NamingException {
-		DirContext ldapContext = null;
-		String principalDN = null;
-		try {
-			classLogger.info("Attempting login for user " + username + " to confirm has proper current password");
-			LDAPConnectionHelper loginObj = LDAPConnectionHelper.tryLogins(this.providerUrl, this.securityPrincipalTemplate, username, curPassword);
-			String principalTemplate = loginObj.getPrincipalTemplate();
-			principalDN = loginObj.getPrincipalDN();
-			ldapContext = loginObj.getLdapContext();
-			classLogger.info("Successful confirmation of current password for user " + principalDN);
-
-			String quotedPassword = "\"" + newPassword + "\"";
-			char unicodePwd[] = quotedPassword.toCharArray();
-			byte pwdArray[] = new byte[unicodePwd.length * 2];
-			for (int i = 0; i < unicodePwd.length; i++)
-			{
-				pwdArray[i * 2 + 1] = (byte) (unicodePwd[i] >>> 8);
-				pwdArray[i * 2 + 0] = (byte) (unicodePwd[i] & 0xff);
+		// if we have a dedicated context for changing passwords
+		// we dont need to authenticate but will do
+		// a remove and then add attribute context
+		// this will allow us to also handle when we require the user
+		// to reset their password on initial login since a context cannot be created
+		if(this.useCustomContextForPwdChange) {
+			if(this.customPwdChangeLdapContext == null) {
+				throw new IllegalArgumentException("Invalid configuration for changing user passwords - please contact your administrator");
 			}
 
-			ModificationItem[] mods = new ModificationItem[1];
-			mods[0] = new ModificationItem(DirContext.REPLACE_ATTRIBUTE, new BasicAttribute("UnicodePwd", pwdArray));
+			// first find the user DN
+			String searchContextName = this.searchContextName.replace(SECURITY_PRINCIPAL_TEMPLATE_USERNAME, username);
+			String searchFilter = this.searchMatchingAttributes.replace(SECURITY_PRINCIPAL_TEMPLATE_USERNAME, username);
 
-			classLogger.info(principalDN + " is attemping to change password");
-			if(this.useCustomContextForPwdChange) {
-				if(this.customPwdChangeLdapContext == null) {
-					throw new IllegalArgumentException("Invalid configuration for changing user passwords - please contact your administrator");
-				}
+			SearchControls controls = new SearchControls();
+			controls.setSearchScope(this.searchContextScope);
+			controls.setReturningAttributes(this.requestAttributes);
+
+			NamingEnumeration<SearchResult> findUser = this.customPwdChangeLdapContext.search(searchContextName, searchFilter, controls);
+
+			String principalDN = null;
+			boolean foundUser = false;
+			SearchResult result = null;
+			while(findUser.hasMoreElements()) {
+				foundUser = true;
+				result = findUser.next();
+				// we got the user DN
+				principalDN = result.getNameInNamespace();
+				classLogger.info("Found user DN = " + principalDN + " > attemping to login");
+			}
+
+			if(!foundUser) {
+				throw new IllegalArgumentException("Unable to find username = " + username);
+			}
+
+			try {
+				classLogger.info("Using ldap context for pwd change");
+				byte[] oldPwdArray = LDAPConnectionHelper.toUnicodeBytes(curPassword);
+				byte[] newPwdArray = LDAPConnectionHelper.toUnicodeBytes(newPassword);
+				ModificationItem[] mods = new ModificationItem[2];
+				mods[0] = new ModificationItem(DirContext.REMOVE_ATTRIBUTE, new BasicAttribute("UnicodePwd", oldPwdArray));
+				mods[1] = new ModificationItem(DirContext.ADD_ATTRIBUTE, new BasicAttribute("UnicodePwd", newPwdArray));
 				this.customPwdChangeLdapContext.modifyAttributes(principalDN, mods);
-			} else {
+			} catch(Exception e) {
+				String message = null;
+				if(principalDN == null) {
+					message = "Unable to find principal DN for username : " + username;
+				} else {
+					message = "Failed to change password. Error message = " + e.getMessage();
+				}
+				classLogger.error(message);
+				classLogger.error(Constants.STACKTRACE, e);
+				throw new IllegalArgumentException(message);
+			}
+		} else {
+			// we have to try to login with the current users details
+			// and user their context to replace their current password
+			// since we already know they must have the correct password to create the context
+			DirContext ldapContext = null;
+			String principalDN = null;
+			try {
+				classLogger.info("Attempting login for user " + username + " to confirm has proper current password");
+				LDAPConnectionHelper loginObj = tryLogins(this.providerUrl, this.securityPrincipalTemplate, username, curPassword);
+				String principalTemplate = loginObj.getPrincipalTemplate();
+				principalDN = loginObj.getPrincipalDN();
+				ldapContext = loginObj.getLdapContext();
+				classLogger.info("Successful confirmation of current password for user " + principalDN);
+
+				byte pwdArray[] = LDAPConnectionHelper.toUnicodeBytes(newPassword);
+				ModificationItem[] mods = new ModificationItem[1];
+				mods[0] = new ModificationItem(DirContext.REPLACE_ATTRIBUTE, new BasicAttribute("UnicodePwd", pwdArray));
+
+				classLogger.info(principalDN + " is attemping to change password");
 				ldapContext.modifyAttributes(principalDN, mods);
-			}
-			classLogger.info(principalDN + " successfully changed password");
-		} catch (Exception e) {
-			String message = null;
-			if(principalDN == null) {
-				message = "User was unable to authenticate with current password, username entered: " + username;
-			} else {
-				message = "Failed to change password. Error message = " + e.getMessage();
-			}
-			classLogger.error(message);
-			classLogger.error(Constants.STACKTRACE, e);
-			throw new IllegalArgumentException(message);
-		} finally {
-			if(ldapContext != null) {
-				ldapContext.close();
+				classLogger.info(principalDN + " successfully changed password");
+			} catch (Exception e) {
+				String message = null;
+				if(principalDN == null) {
+					message = "User was unable to authenticate with current password, username entered: " + username;
+				} else {
+					message = "Failed to change password. Error message = " + e.getMessage();
+				}
+				classLogger.error(message);
+				classLogger.error(Constants.STACKTRACE, e);
+				throw new IllegalArgumentException(message);
+			} finally {
+				if(ldapContext != null) {
+					ldapContext.close();
+				}
 			}
 		}
 	}
@@ -268,7 +378,7 @@ public class LdapTemplateStructureConnection extends AbstractLdapAuthenticator {
 		if(this.customPwdChangeLdapContext == null) {
 			throw new IllegalArgumentException("Invalid configuration for changing user passwords - please contact your administrator");
 		}
-		
+
 		// first find the user DN
 		String searchContextName = this.searchContextName.replace(SECURITY_PRINCIPAL_TEMPLATE_USERNAME, username);
 		String searchFilter = this.searchMatchingAttributes.replace(SECURITY_PRINCIPAL_TEMPLATE_USERNAME, username);
@@ -289,21 +399,13 @@ public class LdapTemplateStructureConnection extends AbstractLdapAuthenticator {
 			principalDN = result.getNameInNamespace();
 			classLogger.info("Found user DN = " + principalDN + " > attemping to login");
 		}
-		
+
 		if(!foundUser) {
 			throw new IllegalArgumentException("Unable to find username = " + username);
 		}
-		
-		try {
-			String quotedPassword = "\"" + newPassword + "\"";
-			char unicodePwd[] = quotedPassword.toCharArray();
-			byte pwdArray[] = new byte[unicodePwd.length * 2];
-			for (int i = 0; i < unicodePwd.length; i++)
-			{
-				pwdArray[i * 2 + 1] = (byte) (unicodePwd[i] >>> 8);
-				pwdArray[i * 2 + 0] = (byte) (unicodePwd[i] & 0xff);
-			}
 
+		try {
+			byte[] pwdArray = LDAPConnectionHelper.toUnicodeBytes(newPassword);
 			ModificationItem[] mods = new ModificationItem[1];
 			mods[0] = new ModificationItem(DirContext.REPLACE_ATTRIBUTE, new BasicAttribute("UnicodePwd", pwdArray));
 
@@ -315,7 +417,7 @@ public class LdapTemplateStructureConnection extends AbstractLdapAuthenticator {
 			throw new IllegalArgumentException("Failed to change password. Error message: " + e.getMessage());
 		}
 	}
-	
+
 	@Override
 	public void close() {
 		// do nothing
