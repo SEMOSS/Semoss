b809ab52472fa127fcbf367f471b5dde07ac20cd
Maher Khalil <themaherkhalil@gmail.com>
2023-04-13 19:33:37 +0000

    UPDATE ::: Switching singleton design pattern for cloud storage clients to be builders
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48284 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 526db061d95e92799256940e604f5afc544049a3

--- src/prerna/cluster/util/PullAppRunner.java
+++ src/prerna/cluster/util/PullAppRunner.java
@@ -2,6 +2,8 @@ package prerna.cluster.util;
 
 import java.io.IOException;
 
+import prerna.cluster.util.clients.CloudClient;
+
 public class PullAppRunner implements Runnable {
 
 	private final String appId;
---
+++ src/prerna/cluster/util/clients/AbstractClientBuilder.java
@@ -0,0 +1,66 @@
+package prerna.cluster.util.clients;
+
+public abstract class AbstractClientBuilder implements ICloudClientBuilder {
+
+	public static String RCLONE_PATH = "RCLONE_PATH";
+
+	// s3 prefix is used for minio as well
+	public static final String S3_REGION_KEY = "S3_REGION";
+	public static final String S3_BUCKET_KEY = "S3_BUCKET";
+	public static final String S3_ACCESS_KEY = "S3_ACCESS_KEY";
+	public static final String S3_SECRET_KEY = "S3_SECRET_KEY";
+	public static final String S3_ENDPOINT_KEY = "S3_ENDPOINT";
+	
+	// gcp keys
+	public static final String GCP_SERVICE_ACCOUNT_FILE_KEY = "GCP_SERVICE_ACCOUNT_FILE";
+	public static final String GCP_REGION_KEY = "GCP_REGION";
+	public static final String GCP_BUCKET_KEY = "GCP_BUCKET";
+	
+	// az keys
+	public static final String AZ_CONN_STRING = "AZ_CONN_STRING";
+	public static final String AZ_NAME = "AZ_NAME";
+	public static final String AZ_KEY = "AZ_KEY";
+	public static final String SAS_URL = "SAS_URL";
+	public static final String AZ_URI = "AZ_URI";
+	public static final String STORAGE = "STORAGE"; // says if this is local / cluster
+	public static final String KEY_HOME = "KEY_HOME"; // this is where the various keys are cycled
+	
+	protected String rclonePath = "rclone";
+	protected String rcloneConfigF = null;
+	
+	@Override
+	public ICloudClientBuilder setRClonePath(String rclonePath) {
+		if(rclonePath != null && !rclonePath.isEmpty()) {
+			if(rclonePath.contains(" ") && !rclonePath.startsWith("\"")) {
+				// need start quote
+				rclonePath = "\""+rclonePath;
+			}
+			if(rclonePath.contains(" ") && !rclonePath.endsWith("\"")) {
+				// need start quote
+				rclonePath = rclonePath+"\"";
+			}
+			this.rclonePath = rclonePath;
+		}
+		return this;
+	}
+	
+	@Override
+	public ICloudClientBuilder setRCloneConfigFolder(String rcloneConfigF) {
+		this.rcloneConfigF = rcloneConfigF;
+		return this;
+	}
+	
+	@Override
+	public String getRClonePath() {
+		return rclonePath;
+	}
+	
+	@Override
+	public String getRCloneConfigFolder() {
+		return rcloneConfigF;
+	}
+	
+	
+
+}
+ 
