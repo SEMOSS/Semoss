902ead464ffe77c9af3d7ac9af42894ef3e00421
mahkhalil <mahkhalil@deloitte.com>
2023-04-18 17:40:20 +0000

    BUGFIX ::: Using the version path to append the insight id to instead of base project path
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48299 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/cluster/PullInsightFolderFromCloudReactor.java
+++ src/prerna/sablecc2/reactor/cluster/PullInsightFolderFromCloudReactor.java
@@ -38,7 +38,7 @@ public class PullInsightFolderFromCloudReactor extends AbstractReactor {
 		}
 		
 		IProject project = Utility.getProject(projectId);
-		String projectFolderPath = AssetUtility.getProjectBaseFolder(project.getProjectName(), projectId).replace("\\", "/");
+		String projectFolderPath = AssetUtility.getProjectVersionFolder(project.getProjectName(), projectId).replace("\\", "/");
 		ClusterUtil.reactorPullProjectFolder(project, projectFolderPath, rdbmsId);
 
 		return new NounMetadata(true, PixelDataType.BOOLEAN);
