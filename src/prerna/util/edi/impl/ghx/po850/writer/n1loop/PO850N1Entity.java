0ea58ff0cdfbf704eb8e3786ee612d51249c897f
mahkhalil <mahkhalil@deloitte.com>
2023-04-30 23:05:13 +0000

    UPDATE ::: Some restructuring and adding in interface for building x12 format components
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48398 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: d72568723f1fbe24c89eb49255322308bd53f7a0

---
+++ src/prerna/util/edi/impl/ghx/po850/writer/n1loop/PO850N1Entity.java
@@ -0,0 +1,14 @@
+package prerna.util.edi.impl.ghx.po850.writer.n1loop;
+
+import prerna.util.edi.impl.ghx.po850.writer.n1loop.PO850N1;
+import prerna.util.edi.impl.ghx.po850.writer.n1loop.PO850N2;
+import prerna.util.edi.impl.ghx.po850.writer.n1loop.PO850N3;
+
+public class PO850N1Entity {
+
+	private PO850N1 n1;
+	private PO850N2 n2;
+	private PO850N3 n3;
+	private PO850N3 n4;
+
+}
