1aef81ea2c4cc082c6bef1868fb44551b60df998
mahkhalil <mahkhalil@deloitte.com>
2023-05-01 17:04:32 +0000

    UPDATE ::: Adding additional implementation fields for per, n1loop, po1loop
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48407 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/edi/impl/ghx/po850/writer/n1loop/PO850N1Loop.java
+++ src/prerna/util/edi/impl/ghx/po850/writer/n1loop/PO850N1Loop.java
@@ -18,13 +18,42 @@ public class PO850N1Loop implements IX12Format {
 		return builder;
 	}
 	
-	public PO850N1Loop addPO1(PO850N1Entity n1) {
+	public PO850N1Loop addN1Group(PO850N1Entity n1) {
 		n1list.add(n1);
 		return this;
 	}
 	
-	public List<PO850N1Entity> getPO1List() {
+	public List<PO850N1Entity> getN1List() {
 		return this.n1list;
+	}
+	
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PO850N1Loop n1loop = new PO850N1Loop()
+			.addN1Group(new PO850N1Entity()
+				.setN1( new PO850N1()
+					.setEntityCode("ST") // 1 - ship to
+					.setName("Anchorage VA Medical Center") // 2 - name
+					.setIdentificationCode("91") // 3 - 91=assigned by seller
+					.setIdentificationCode("DEMO-ID")
+				)
+				.setN3(new PO850N3()
+					.setAddressInfo1("1201 N Muldoon Rd")
+				)
+				.setN4(new PO850N4()
+					.setCity("Anchorage")
+					.setState("AK")
+					.setZip("99504")
+					.setCountryCode("US")
+				)
+			)
+			;
+	
+		System.out.println(n1loop.generateX12("^", "~\n"));
 	}
 	
 }
--- src/prerna/util/edi/impl/ghx/po850/writer/n1loop/PO850N2.java
+++ src/prerna/util/edi/impl/ghx/po850/writer/n1loop/PO850N2.java
@@ -4,10 +4,39 @@ import prerna.util.edi.IX12Format;
 
 public class PO850N2 implements IX12Format {
 
+	private String n201 = ""; // address information name
+	private String n202 = ""; // address information name
+	
 	@Override
 	public String generateX12(String elementDelimiter, String segmentDelimiter) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		String builder = "N2" 
+				+ elementDelimiter + n201
+				+ elementDelimiter + n202
+				+ segmentDelimiter;
+		
+		return builder;
 	}
-	
+
+	// setters/getter
+
+	public String getN201() {
+		return n201;
+	}
+
+	public PO850N2 setN201(String n201) {
+		this.n201 = n201;
+		return this;
+	}
+
+	public String getN202() {
+		return n202;
+	}
+
+	public PO850N2 setN202(String n202) {
+		this.n202 = n202;
+		return this;
+	}
+
+
 }
