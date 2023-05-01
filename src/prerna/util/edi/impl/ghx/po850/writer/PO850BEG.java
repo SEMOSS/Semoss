821089dd91fb20e996171649b827d37dead67c7c
mahkhalil <mahkhalil@deloitte.com>
2023-05-01 01:05:50 +0000

    UPDATE ::: Adding additional implementation fields
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48400 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/edi/impl/ghx/po850/writer/PO850BEG.java
+++ src/prerna/util/edi/impl/ghx/po850/writer/PO850BEG.java
@@ -1,5 +1,74 @@
 package prerna.util.edi.impl.ghx.po850.writer;
 
-public class PO850BEG {
+import prerna.util.edi.IX12Format;
+
+public class PO850BEG implements IX12Format {
+
+	private String beg01 = "PO";
+	private String beg02 = "";
+	private String beg03 = "";
+	private String beg04 = "";
+	private String beg05 = "";
+	
+	@Override
+	public String generateX12(String elementDelimiter, String segmentDelimiter) {
+		
+		String builder = "BEG" 
+				+ elementDelimiter + beg01
+				+ elementDelimiter + beg02
+				+ elementDelimiter + beg03
+				+ elementDelimiter + beg04
+				+ elementDelimiter + beg05
+				+ segmentDelimiter;
+		
+		return builder;
+	}
+	
+	// setters/getter
+
+	public String getBeg01() {
+		return beg01;
+	}
+
+	public PO850BEG setBeg01(String beg01) {
+		this.beg01 = beg01;
+		return this;
+	}
+
+	public String getBeg02() {
+		return beg02;
+	}
+
+	public PO850BEG setBeg02(String beg02) {
+		this.beg02 = beg02;
+		return this;
+	}
+
+	public String getBeg03() {
+		return beg03;
+	}
+
+	public PO850BEG setBeg03(String beg03) {
+		this.beg03 = beg03;
+		return this;
+	}
+
+	public String getBeg04() {
+		return beg04;
+	}
+
+	public PO850BEG setBeg04(String beg04) {
+		this.beg04 = beg04;
+		return this;
+	}
+
+	public String getBeg05() {
+		return beg05;
+	}
+
+	public PO850BEG setBeg05(String beg05) {
+		this.beg05 = beg05;
+		return this;
+	}
 
 }
--- src/prerna/util/edi/impl/ghx/po850/writer/PO850ST.java
+++ src/prerna/util/edi/impl/ghx/po850/writer/PO850ST.java
@@ -1,5 +1,32 @@
 package prerna.util.edi.impl.ghx.po850.writer;
 
-public class PO850ST {
+import prerna.util.edi.IX12Format;
+
+public class PO850ST implements IX12Format {
+
+	private String st01 = "850";
+	private String st02 = "";
+	
+	@Override
+	public String generateX12(String elementDelimiter, String segmentDelimiter) {
+		
+		String builder = "ST" 
+				+ elementDelimiter + st01
+				+ elementDelimiter + st02
+				+ segmentDelimiter;
+		
+		return builder;
+	}
+
+	// setters/getter
+
+	public String getSt02() {
+		return st02;
+	}
+
+	public PO850ST setSt02(String st02) {
+		this.st02 = st02;
+		return this;
+	}
 
 }
