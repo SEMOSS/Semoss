a56b9fe94f88f3ea1ed16eed4eb19b4c8634972f
mahkhalil <mahkhalil@deloitte.com>
2023-05-01 10:16:07 +0000

    UPDATE ::: Adding additional implementation fields
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48403 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/edi/impl/ghx/po850/writer/n1loop/PO850N1.java
+++ src/prerna/util/edi/impl/ghx/po850/writer/n1loop/PO850N1.java
@@ -1,5 +1,15 @@
 package prerna.util.edi.impl.ghx.po850.writer.n1loop;
 
-public class PO850N1 {
+import prerna.util.edi.IX12Format;
+
+public class PO850N1 implements IX12Format {
+
+	
+	
+	@Override
+	public String generateX12(String elementDelimiter, String segmentDelimiter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
