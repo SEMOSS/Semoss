84d0f509261d277095c69eec9b54e6f4a1a1442c
mahkhalil <mahkhalil@deloitte.com>
2023-05-01 15:38:00 +0000

    UPDATE ::: Adding additional implementation fields for gs, st, beg, ref
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48406 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3693322335d10e984717ae28c8968558c8231a1c

--- src/prerna/util/edi/impl/ghx/po850/writer/PO850BEG.java
+++ src/prerna/util/edi/impl/ghx/po850/writer/PO850BEG.java
@@ -1,14 +1,17 @@
 package prerna.util.edi.impl.ghx.po850.writer;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import prerna.util.edi.IX12Format;
 
 public class PO850BEG implements IX12Format {
 
-	private String beg01 = "PO";
-	private String beg02 = "";
-	private String beg03 = "";
-	private String beg04 = "";
-	private String beg05 = "";
+	private String beg01 = ""; // transaction set purpose code
+	private String beg02 = "ST"; // purchase order type code
+	private String beg03 = ""; // purchase order number
+	private String beg04 = ""; // not used...
+	private String beg05 = ""; // date CCYYMMDD
 	
 	@Override
 	public String generateX12(String elementDelimiter, String segmentDelimiter) {
@@ -35,6 +38,10 @@ public class PO850BEG implements IX12Format {
 		return this;
 	}
 
+	public PO850BEG setTransactionPurposeCode(String beg01) {
+		return setBeg01(beg01);
+	} 
+	
 	public String getBeg02() {
 		return beg02;
 	}
@@ -42,6 +49,10 @@ public class PO850BEG implements IX12Format {
 	public PO850BEG setBeg02(String beg02) {
 		this.beg02 = beg02;
 		return this;
+	}
+	
+	public PO850BEG setPurchaseOrderTypeCode(String beg02) {
+		return setBeg02(beg02);
 	}
 
 	public String getBeg03() {
@@ -52,23 +63,59 @@ public class PO850BEG implements IX12Format {
 		this.beg03 = beg03;
 		return this;
 	}
-
-	public String getBeg04() {
-		return beg04;
+	
+	public PO850BEG setPurchaseOrderNumber(String beg03) {
+		return setBeg03(beg03);
 	}
 
-	public PO850BEG setBeg04(String beg04) {
-		this.beg04 = beg04;
+//	public String getBeg04() {
+//		return beg04;
+//	}
+//
+//	public PO850BEG setBeg04(String beg04) {
+//		this.beg04 = beg04;
+//		return this;
+//	}
+
+	public PO850BEG setDateAndTime() {
+		LocalDateTime now = LocalDateTime.now();
+		return setDateAndTime(now);
+	}
+	
+	public PO850BEG setDateAndTime(LocalDateTime now) {
+		String beg05 = now.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+		setBeg05(beg05);
 		return this;
 	}
-
+	
 	public String getBeg05() {
 		return beg05;
 	}
 
 	public PO850BEG setBeg05(String beg05) {
+		if(beg05 == null || beg05.length() != 8) {
+			throw new IllegalArgumentException("BEG05 Date must be 8 digit date CCYYMMDD");
+		}
 		this.beg05 = beg05;
 		return this;
 	}
 
+	public PO850BEG setDate(String beg05) {
+		return setBeg05(beg05);
+	}
+	
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PO850BEG beg = new PO850BEG()
+			.setTransactionPurposeCode("00") // 1
+			.setPurchaseOrderNumber("RequestID") // 3
+			.setDateAndTime()
+			;
+		
+		System.out.println(beg.generateX12("^", "~\n"));
+	}
 }
