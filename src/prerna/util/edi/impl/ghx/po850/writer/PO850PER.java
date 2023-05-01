5903120623a472cb7e5b6a18ce8c528c0dca3eaf
mahkhalil <mahkhalil@deloitte.com>
2023-05-01 17:04:32 +0000

    UPDATE ::: Adding additional implementation fields for per, n1loop, po1loop
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48407 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 548488c2edd50bb57a7ca46a6ae3a1913134c2c8

--- src/prerna/util/edi/impl/ghx/po850/writer/PO850PER.java
+++ src/prerna/util/edi/impl/ghx/po850/writer/PO850PER.java
@@ -4,12 +4,12 @@ import prerna.util.edi.IX12Format;
 
 public class PO850PER implements IX12Format {
 
-	private String per01 = "";
-	private String per02 = "";
-	private String per03 = "TE";
-	private String per04 = "";
-	private String per05 = "EM";
-	private String per06 = "";
+	private String per01 = ""; // contact function code - BD=Buyer Name
+	private String per02 = ""; // name for per01
+	private String per03 = "TE"; // telephone key
+	private String per04 = ""; // telephone value
+	private String per05 = "EM"; // email key
+	private String per06 = ""; // email value
 	
 	@Override
 	public String generateX12(String elementDelimiter, String segmentDelimiter) {
@@ -36,6 +36,10 @@ public class PO850PER implements IX12Format {
 		this.per01 = per01;
 		return this;
 	}
+	
+	public PO850PER setContactFunctionCode(String per01) {
+		return setPer01(per01);
+	}
 
 	public String getPer02() {
 		return per02;
@@ -44,6 +48,10 @@ public class PO850PER implements IX12Format {
 	public PO850PER setPer02(String per02) {
 		this.per02 = per02;
 		return this;
+	}
+	
+	public PO850PER setContactName(String per02) {
+		return setPer02(per02);
 	}
 
 //	public String getPer03() {
@@ -92,4 +100,19 @@ public class PO850PER implements IX12Format {
 		return this;
 	}
 
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PO850PER per = new PO850PER()
+			.setContactFunctionCode("BD") // 1 - BD=Buyer Name
+			.setContactName("Maher Khalil") // 2
+			.setTelephone("(202)222-2222") // 4
+			.setEmail("mahkhalil@deloitte.com") // 6
+			;
+		
+		System.out.println(per.generateX12("^", "~\n"));
+	}
 }
