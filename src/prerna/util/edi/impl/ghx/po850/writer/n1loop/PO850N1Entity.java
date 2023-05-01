6dfdf912101c550f329db8d58a086e2a5f18a0d5
mahkhalil <mahkhalil@deloitte.com>
2023-05-01 10:16:07 +0000

    UPDATE ::: Adding additional implementation fields
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48403 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/edi/impl/ghx/po850/writer/n1loop/PO850N1Entity.java
+++ src/prerna/util/edi/impl/ghx/po850/writer/n1loop/PO850N1Entity.java
@@ -1,14 +1,21 @@
 package prerna.util.edi.impl.ghx.po850.writer.n1loop;
 
+import prerna.util.edi.IX12Format;
 import prerna.util.edi.impl.ghx.po850.writer.n1loop.PO850N1;
 import prerna.util.edi.impl.ghx.po850.writer.n1loop.PO850N2;
 import prerna.util.edi.impl.ghx.po850.writer.n1loop.PO850N3;
 
-public class PO850N1Entity {
+public class PO850N1Entity implements IX12Format {
 
 	private PO850N1 n1;
 	private PO850N2 n2;
 	private PO850N3 n3;
 	private PO850N3 n4;
+	
+	@Override
+	public String generateX12(String elementDelimiter, String segmentDelimiter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/prerna/util/edi/impl/ghx/po850/writer/po1loop/PO850PO1Entity.java
+++ src/prerna/util/edi/impl/ghx/po850/writer/po1loop/PO850PO1Entity.java
@@ -1,11 +1,18 @@
 package prerna.util.edi.impl.ghx.po850.writer.po1loop;
 
+import prerna.util.edi.IX12Format;
 import prerna.util.edi.impl.ghx.po850.writer.po1loop.PO850PID;
 import prerna.util.edi.impl.ghx.po850.writer.po1loop.PO850PO1;
 
-public class PO850PO1Entity {
+public class PO850PO1Entity implements IX12Format {
 
 	private PO850PO1 po1;
 	private PO850PID pid;
+	
+	@Override
+	public String generateX12(String elementDelimiter, String segmentDelimiter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
