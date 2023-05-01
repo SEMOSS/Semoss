64ad5ed4aa75243276375581587d3b1a90deac33
mahkhalil <mahkhalil@deloitte.com>
2023-05-01 12:55:30 +0000

    UPDATE ::: Adding additional implementation fields
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48404 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 9a9e15fb381849e236ccca45dca924700a6d5928

--- src/prerna/util/edi/impl/ghx/po850/writer/po1loop/PO850PO1Loop.java
+++ src/prerna/util/edi/impl/ghx/po850/writer/po1loop/PO850PO1Loop.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import prerna.util.edi.IX12Format;
-import prerna.util.edi.impl.ghx.po850.writer.po1loop.PO850PO1Entity;
 
 public class PO850PO1Loop implements IX12Format {
 
@@ -12,8 +11,20 @@ public class PO850PO1Loop implements IX12Format {
 
 	@Override
 	public String generateX12(String elementDelimiter, String segmentDelimiter) {
-		// TODO Auto-generated method stub
-		return null;
+		String builder = "";
+		for(PO850PO1Entity loop : po1list) {
+			builder += loop.generateX12(elementDelimiter, segmentDelimiter);
+		}
+		return builder;
+	}
+	
+	public PO850PO1Loop addPO1(PO850PO1Entity po1) {
+		po1list.add(po1);
+		return this;
+	}
+	
+	public List<PO850PO1Entity> getPO1List() {
+		return this.po1list;
 	}
 	
 }
