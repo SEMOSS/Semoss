14a7935f7798dd97b789372324f4307af7fbe025
mahkhalil <mahkhalil@deloitte.com>
2023-05-01 23:20:27 +0000

    UPDATE ::: Adding additional implementation fields and restructuring code for segment counting
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48413 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/util/edi/impl/ghx/po850/writer/heading/PO850PER.java
@@ -0,0 +1,118 @@
+package prerna.util.edi.impl.ghx.po850.writer.heading;
+
+import prerna.util.edi.IX12Format;
+
+public class PO850PER implements IX12Format {
+
+	private String per01 = ""; // contact function code - BD=Buyer Name
+	private String per02 = ""; // name for per01
+	private String per03 = "TE"; // telephone key
+	private String per04 = ""; // telephone value
+	private String per05 = "EM"; // email key
+	private String per06 = ""; // email value
+	
+	@Override
+	public String generateX12(String elementDelimiter, String segmentDelimiter) {
+		
+		String builder = "PER" 
+				+ elementDelimiter + per01
+				+ elementDelimiter + per02
+				+ elementDelimiter + per03
+				+ elementDelimiter + per04
+				+ elementDelimiter + per05
+				+ elementDelimiter + per06
+				+ segmentDelimiter;
+		
+		return builder;
+	}
+
+	// setters/getter
+	
+	public String getPer01() {
+		return per01;
+	}
+
+	public PO850PER setPer01(String per01) {
+		this.per01 = per01;
+		return this;
+	}
+	
+	public PO850PER setContactFunctionCode(String per01) {
+		return setPer01(per01);
+	}
+
+	public String getPer02() {
+		return per02;
+	}
+
+	public PO850PER setPer02(String per02) {
+		this.per02 = per02;
+		return this;
+	}
+	
+	public PO850PER setContactName(String per02) {
+		return setPer02(per02);
+	}
+
+//	public String getPer03() {
+//		return per03;
+//	}
+//
+//	public PO850PER setPer03(String per03) {
+//		this.per03 = per03;
+//		return this;
+//	}
+
+	public String getPer04() {
+		return per04;
+	}
+
+	public PO850PER setPer04(String per04) {
+		this.per04 = per04;
+		return this;
+	}
+	
+	public PO850PER setTelephone(String per04) {
+		this.per04 = per04;
+		return this;
+	}
+
+//	public String getPer05() {
+//		return per05;
+//	}
+//
+//	public PO850PER setPer05(String per05) {
+//		this.per05 = per05;
+//		return this;
+//	}
+
+	public String getPer06() {
+		return per06;
+	}
+
+	public PO850PER setPer06(String per06) {
+		this.per06 = per06;
+		return this;
+	}
+	
+	public PO850PER setEmail(String per06) {
+		this.per06 = per06;
+		return this;
+	}
+
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PO850PER per = new PO850PER()
+			.setContactFunctionCode("BD") // 1 - BD=Buyer Name
+			.setContactName("Maher Khalil") // 2
+			.setTelephone("(202)222-2222") // 4
+			.setEmail("mahkhalil@deloitte.com") // 6
+			;
+		
+		System.out.println(per.generateX12("^", "~\n"));
+	}
+}
---
+++ src/prerna/util/edi/impl/ghx/po850/writer/heading/PO850ST.java
@@ -0,0 +1,53 @@
+package prerna.util.edi.impl.ghx.po850.writer.heading;
+
+import prerna.util.edi.IX12Format;
+
+public class PO850ST implements IX12Format {
+
+	private String st01 = "850";
+	private String st02 = ""; // transaction control number - must match se02
+	
+	@Override
+	public String generateX12(String elementDelimiter, String segmentDelimiter) {
+		
+		String builder = "ST" 
+				+ elementDelimiter + st01
+				+ elementDelimiter + st02
+				+ segmentDelimiter;
+		
+		return builder;
+	}
+
+	// setters/getter
+
+	public String getSt02() {
+		return st02;
+	}
+
+	public PO850ST setSt02(String st02) {
+		if(st02 == null || st02.length() < 4 || st02.length() > 9) {
+			throw new IllegalArgumentException("ST02 transaction control number must be between 4 and 9 digits long");
+		}
+		this.st02 = st02;
+		return this;
+	}
+
+	public PO850ST setTransactionSetControlNumber(String st02) {
+		return setSt02(st02);
+	}
+	
+	
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PO850ST st = new PO850ST()
+			.setTransactionSetControlNumber("0001") // 2
+			;
+		
+		System.out.println(st.generateX12("^", "~\n"));
+	}
+	
+}
