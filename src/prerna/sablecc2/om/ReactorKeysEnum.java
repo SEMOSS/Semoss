033cde9c19095601541c69b2bf4f8327027f6dd9
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-06-08 23:37:50 +0000

    Variable length encoding in FAISS
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48691 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/om/ReactorKeysEnum.java
+++ src/prerna/sablecc2/om/ReactorKeysEnum.java
@@ -37,6 +37,7 @@ public enum ReactorKeysEnum {
 	CONNECTION_STRING_KEY("connectionString", 					"JDBC connection string to connect to an external database"),
 	CONSUMER_ID("consumer", 									"Email / ID of the consumer to be added for API"),
 	CONTENT("content", 										    "The actual content to be persisted on the file. Tab and Newline Separated"),
+	CONTENT_LENGTH("contentLength", 							"Length of the content to chunk information into. Assumes a default of 512 characters"),
 	CONTEXT("context", 									    	"THe workspace to be set command line. This is the base directory."),
 	CUSTOM_SUCCESS_MESSAGE("customSuccessMessage",				"Defines for certain reactors a custom success message after running"),
 	ADDITIONAL_CONNECTION_PARAMS_KEY("connParams",	 			"Additional JDBC connection params to perform connection"),
