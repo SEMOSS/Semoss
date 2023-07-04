b24afeb311b7846f69f46831767d301591e68c33
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-07-04 16:48:43 +0000

    Initial Import for TCP Server - Experimental. Works for most cases but struggles on timestamp fields.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48794 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ py/semoss_console.py
@@ -0,0 +1,16 @@
+
+class SemossConsole(object):
+    def __init__(self,socket_handler=None, payload=None):
+      self.socket_handler = socket_handler
+      self.payload = payload
+      self.output = []
+
+    def write(self,console_line):
+      if self.socket_handler is not None:
+        self.socket_handler.send_output(console_line, self.payload, response=False)
+      else:
+        #self.txtctrl.write(string)
+        self.output.append(console_line)
+
+    def flush(self):
+      pass
