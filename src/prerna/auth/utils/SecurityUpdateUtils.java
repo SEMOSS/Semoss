d59199f3f9164351d54ef8c2055967197cebfddb
mahkhalil <mahkhalil@deloitte.com>
2023-07-07 17:31:18 +0000

    UPDATE ::: Additional restructuring around favorites for database/projects
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48811 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 95f590d13408489228a5f75aacddb5711ff72909

--- src/prerna/auth/utils/SecurityUpdateUtils.java
+++ src/prerna/auth/utils/SecurityUpdateUtils.java
@@ -1485,7 +1485,7 @@ public class SecurityUpdateUtils extends AbstractSecurityUtils {
 			ps.setBoolean(parameterIndex++, exporter);
 			ps.setTimestamp(parameterIndex++, timestamp, cal);
 			ps.execute();
-			securityDb.commit();
+			ps.getConnection().commit();
 		} catch (SQLException e) {
 			logger.error(Constants.STACKTRACE, e);
 		} finally {
@@ -1493,11 +1493,7 @@ public class SecurityUpdateUtils extends AbstractSecurityUtils {
 				try {
 					ps.close();
 					if(securityDb.isConnectionPooling()) {
-						try {
-							ps.getConnection().close();
-						} catch (SQLException e) {
-							logger.error(Constants.STACKTRACE, e);
-						}
+						ps.getConnection().close();
 					}
 				} catch (SQLException e) {
 					logger.error(Constants.STACKTRACE, e);
@@ -1510,167 +1506,6 @@ public class SecurityUpdateUtils extends AbstractSecurityUtils {
 	/*
 	 * Engines
 	 */
-	
-	/**
-	 * Change the user visibility (show/hide) for a database. Without removing its permissions.
-	 * @param user
-	 * @param databaseId
-	 * @param visibility
-	 * @throws SQLException 
-	 * @throws IllegalAccessException 
-	 */
-	public static void setDbVisibility(User user, String databaseId, boolean visibility) throws SQLException, IllegalAccessException {
-		if(!SecurityUserDatabaseUtils.userCanViewDatabase(user, databaseId)) {
-			throw new IllegalAccessException("The user doesn't have the permission to modify his visibility of this app.");
-		}
-		Collection<String> userIdFilters = getUserFiltersQs(user);
-		SelectQueryStruct qs = new SelectQueryStruct();
-		qs.addSelector(new QueryColumnSelector("ENGINEPERMISSION__ENGINEID"));
-		qs.addExplicitFilter(SimpleQueryFilter.makeColToValFilter("ENGINEPERMISSION__ENGINEID", "==", databaseId));
-		qs.addExplicitFilter(SimpleQueryFilter.makeColToValFilter("ENGINEPERMISSION__USERID", "==", userIdFilters));
-
-		IRawSelectWrapper wrapper = null;
-		try {
-			wrapper = WrapperManager.getInstance().getRawWrapper(securityDb, qs);
-			if(wrapper.hasNext()){
-				UpdateQueryStruct uqs = new UpdateQueryStruct();
-				uqs.setEngine(securityDb);
-				uqs.addExplicitFilter(SimpleQueryFilter.makeColToValFilter("ENGINEPERMISSION__ENGINEID", "==", databaseId));
-				uqs.addExplicitFilter(SimpleQueryFilter.makeColToValFilter("ENGINEPERMISSION__USERID", "==", userIdFilters));
-
-				List<IQuerySelector> selectors = new Vector<>();
-				selectors.add(new QueryColumnSelector("ENGINEPERMISSION__VISIBILITY"));
-				List<Object> values = new Vector<>();
-				values.add(visibility);
-				uqs.setSelectors(selectors);
-				uqs.setValues(values);
-				
-				UpdateSqlInterpreter updateInterp = new UpdateSqlInterpreter(uqs);
-				String updateQuery = updateInterp.composeQuery();
-				securityDb.insertData(updateQuery);
-				
-			} else {
-				// need to insert
-				PreparedStatement ps = securityDb.getPreparedStatement("INSERT INTO ENGINEPERMISSION "
-						+ "(USERID, ENGINEID, VISIBILITY, FAVORITE, PERMISSION) VALUES (?,?,?,?,?)");
-				if(ps == null) {
-					throw new IllegalArgumentException("Error generating prepared statement to set app visibility");
-				}
-				try {
-					// we will set the permission to read only
-					for(AuthProvider loginType : user.getLogins()) {
-						String userId = user.getAccessToken(loginType).getId();
-						int parameterIndex = 1;
-						ps.setString(parameterIndex++, userId);
-						ps.setString(parameterIndex++, databaseId);
-						ps.setBoolean(parameterIndex++, visibility);
-						// default favorite as false
-						ps.setBoolean(parameterIndex++, false);
-						ps.setInt(parameterIndex++, 3);
-	
-						ps.addBatch();
-					}
-					ps.executeBatch();
-				} catch(Exception e) {
-					logger.error(Constants.STACKTRACE, e);
-					throw e;
-				} finally {
-					if(ps != null) {
-						ps.close();
-					}
-				}
-			}
-			
-			securityDb.commit();
-		} catch (Exception e) {
-			logger.error(Constants.STACKTRACE, e);
-		} finally {
-			if(wrapper != null) {
-				wrapper.cleanUp();
-			}
-		}
-	}
-	
-	/**
-	 * Change the user favorite (is favorite / not favorite) for a database. Without removing its permissions.
-	 * @param user
-	 * @param databaseId
-	 * @param visibility
-	 * @throws SQLException 
-	 * @throws IllegalAccessException 
-	 */
-	public static void setDbFavorite(User user, String databaseId, boolean isFavorite) throws SQLException, IllegalAccessException {
-		if (!SecurityUserDatabaseUtils.userCanViewDatabase(user, databaseId)) {
-			throw new IllegalAccessException("The user doesn't have the permission to modify his visibility of this database.");
-		}
-		Collection<String> userIdFilters = getUserFiltersQs(user);
-		SelectQueryStruct qs = new SelectQueryStruct();
-		qs.addSelector(new QueryColumnSelector("ENGINEPERMISSION__ENGINEID"));
-		qs.addExplicitFilter(SimpleQueryFilter.makeColToValFilter("ENGINEPERMISSION__ENGINEID", "==", databaseId));
-		qs.addExplicitFilter(SimpleQueryFilter.makeColToValFilter("ENGINEPERMISSION__USERID", "==", userIdFilters));
-
-		IRawSelectWrapper wrapper = null;
-		try {
-			wrapper = WrapperManager.getInstance().getRawWrapper(securityDb, qs);
-			if(wrapper.hasNext()){
-				UpdateQueryStruct uqs = new UpdateQueryStruct();
-				uqs.setEngine(securityDb);
-				uqs.addExplicitFilter(SimpleQueryFilter.makeColToValFilter("ENGINEPERMISSION__ENGINEID", "==", databaseId));
-				uqs.addExplicitFilter(SimpleQueryFilter.makeColToValFilter("ENGINEPERMISSION__USERID", "==", userIdFilters));
-
-				List<IQuerySelector> selectors = new Vector<>();
-				selectors.add(new QueryColumnSelector("ENGINEPERMISSION__FAVORITE"));
-				List<Object> values = new Vector<>();
-				values.add(isFavorite);
-				uqs.setSelectors(selectors);
-				uqs.setValues(values);
-				
-				UpdateSqlInterpreter updateInterp = new UpdateSqlInterpreter(uqs);
-				String updateQuery = updateInterp.composeQuery();
-				securityDb.insertData(updateQuery);
-				
-			} else {
-				// need to insert
-				PreparedStatement ps = securityDb.getPreparedStatement("INSERT INTO ENGINEPERMISSION "
-						+ "(USERID, ENGINEID, VISIBILITY, FAVORITE, PERMISSION) VALUES (?,?,?,?,?)");
-				if(ps == null) {
-					throw new IllegalArgumentException("Error generating prepared statement to set app visibility");
-				}
-				try {
-					// we will set the permission to read only
-					for(AuthProvider loginType : user.getLogins()) {
-						String userId = user.getAccessToken(loginType).getId();
-						int parameterIndex = 1;
-						ps.setString(parameterIndex++, userId);
-						ps.setString(parameterIndex++, databaseId);
-						// default visibility as true
-						ps.setBoolean(parameterIndex++, true);
-						ps.setBoolean(parameterIndex++, isFavorite);
-						ps.setInt(parameterIndex++, 3);
-	
-						ps.addBatch();
-					}
-					ps.executeBatch();
-				} catch(Exception e) {
-					logger.error(Constants.STACKTRACE, e);
-					throw e;
-				} finally {
-					if(ps != null) {
-						ps.close();
-					}
-				}
-			}
-			
-			// commit regardless of insert or update
-			securityDb.commit();
-		} catch (Exception e) {
-			logger.error(Constants.STACKTRACE, e);
-		} finally {
-			if(wrapper != null) {
-				wrapper.cleanUp();
-			}
-		}
-	}
 	
 	/**
 	 * Set if the database is public to all users on this instance
