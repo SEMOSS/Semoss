b3eeb9bedd46bd3764e1778b8cf029047fcdf404
mahkhalil <mahkhalil@deloitte.com>
2023-07-11 16:43:41 +0000

    UPDATE ::: Adding ability to return votes on database
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48824 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 48345273c1dc9ed957f86397e2c5f8d8e532918c

--- src/prerna/solr/reactor/MyDatabasesReactor.java
+++ src/prerna/solr/reactor/MyDatabasesReactor.java
@@ -17,6 +17,7 @@ import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.usertracking.UserCatalogVoteUtils;
 import prerna.util.Constants;
 
 public class MyDatabasesReactor extends AbstractReactor {
@@ -28,7 +29,7 @@ public class MyDatabasesReactor extends AbstractReactor {
 				ReactorKeysEnum.LIMIT.getKey(), ReactorKeysEnum.OFFSET.getKey(),
 				ReactorKeysEnum.ONLY_FAVORITES.getKey(), ReactorKeysEnum.DATABASE.getKey(),
 				ReactorKeysEnum.META_KEYS.getKey(), ReactorKeysEnum.META_FILTERS.getKey(),
-				ReactorKeysEnum.NO_META.getKey()
+				ReactorKeysEnum.NO_META.getKey(), ReactorKeysEnum.INCLUDE_USERTRACKING_KEY.getKey()
 			};
 	}
 
@@ -44,7 +45,8 @@ public class MyDatabasesReactor extends AbstractReactor {
 		Boolean favoritesOnly = Boolean.parseBoolean(this.keyValue.get(this.keysToGet[3]));
 		List<String> databaseFilter = getDatabaseFilters();
 		Boolean noMeta = Boolean.parseBoolean(this.keyValue.get(ReactorKeysEnum.NO_META.getKey()));
-
+		Boolean includeUserT = Boolean.parseBoolean(this.keyValue.get(ReactorKeysEnum.INCLUDE_USERTRACKING_KEY.getKey()));
+		
 		List<Map<String, Object>> dbInfo = new ArrayList<>();
 		Map<String, Object> engineMetadataFilter = getMetaMap();
 		if(AbstractSecurityUtils.securityEnabled()) {
@@ -56,10 +58,9 @@ public class MyDatabasesReactor extends AbstractReactor {
 			dbInfo = SecurityDatabaseUtils.getAllDatabaseList(databaseFilter, engineMetadataFilter, searchTerm, limit, offset);
 		}
 
-		if(!dbInfo.isEmpty() && !noMeta) {
+		if(!dbInfo.isEmpty() && (!noMeta || includeUserT)) {
 			Map<String, Integer> index = new HashMap<>(dbInfo.size());
 			int size = dbInfo.size();
-			// now we want to add most executed insights
 			for(int i = 0; i < size; i++) {
 				Map<String, Object> database = dbInfo.get(i);
 				String databaseId = database.get("database_id").toString();
@@ -67,41 +68,64 @@ public class MyDatabasesReactor extends AbstractReactor {
 				index.put(databaseId, Integer.valueOf(i));
 			}
 			
-			IRawSelectWrapper wrapper = null;
-			try {
-				wrapper = SecurityDatabaseUtils.getDatabaseMetadataWrapper(index.keySet(), getMetaKeys(), true);
-				while(wrapper.hasNext()) {
-					Object[] data = wrapper.next().getValues();
-					String databaseId = (String) data[0];
-	
-					String metaKey = (String) data[1];
-					String metaValue = (String) data[2];
-					if(metaValue == null) {
-						continue;
-					}
-	
-					int indexToFind = index.get(databaseId);
-					Map<String, Object> res = dbInfo.get(indexToFind);
-					// whatever it is, if it is single send a single value, if it is multi send as array
-					if(res.containsKey(metaKey)) {
-						Object obj = res.get(metaKey);
-						if(obj instanceof List) {
-							((List) obj).add(metaValue);
-						} else {
-							List<Object> newList = new ArrayList<>();
-							newList.add(obj);
-							newList.add(metaValue);
-							res.put(metaKey, newList);
+			if(!noMeta) {
+				IRawSelectWrapper wrapper = null;
+				try {
+					wrapper = SecurityDatabaseUtils.getDatabaseMetadataWrapper(index.keySet(), getMetaKeys(), true);
+					while(wrapper.hasNext()) {
+						Object[] data = wrapper.next().getValues();
+						String databaseId = (String) data[0];
+		
+						String metaKey = (String) data[1];
+						String metaValue = (String) data[2];
+						if(metaValue == null) {
+							continue;
 						}
-					} else {
-						res.put(metaKey, metaValue);
+		
+						int indexToFind = index.get(databaseId);
+						Map<String, Object> res = dbInfo.get(indexToFind);
+						// whatever it is, if it is single send a single value, if it is multi send as array
+						if(res.containsKey(metaKey)) {
+							Object obj = res.get(metaKey);
+							if(obj instanceof List) {
+								((List) obj).add(metaValue);
+							} else {
+								List<Object> newList = new ArrayList<>();
+								newList.add(obj);
+								newList.add(metaValue);
+								res.put(metaKey, newList);
+							}
+						} else {
+							res.put(metaKey, metaValue);
+						}
+					}
+				} catch (Exception e) {
+					logger.error(Constants.STACKTRACE, e);
+				} finally {
+					if(wrapper != null) {
+						wrapper.cleanUp();
 					}
 				}
-			} catch (Exception e) {
-				logger.error(Constants.STACKTRACE, e);
-			} finally {
-				if(wrapper != null) {
-					wrapper.cleanUp();
+			} 
+			if(includeUserT) {
+				IRawSelectWrapper wrapper = null;
+				try {
+					wrapper = UserCatalogVoteUtils.getAllVotesWrapper(index.keySet());
+					while(wrapper.hasNext()) {
+						Object[] data = wrapper.next().getValues();
+						String databaseId = (String) data[0];
+						int upvotes = ((Number) data[1]).intValue();
+		
+						int indexToFind = index.get(databaseId);
+						Map<String, Object> res = dbInfo.get(indexToFind);
+						res.put("upvotes", upvotes);
+					}
+				} catch (Exception e) {
+					logger.error(Constants.STACKTRACE, e);
+				} finally {
+					if(wrapper != null) {
+						wrapper.cleanUp();
+					}
 				}
 			}
 		}
