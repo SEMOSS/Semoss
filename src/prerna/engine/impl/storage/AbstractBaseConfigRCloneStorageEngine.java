330e63ee9591096c3d378cb60fa538f0f639e00e
mahkhalil <mahkhalil@deloitte.com>
2023-07-15 17:15:07 +0000

    UPDATE ::: Further buildout of storage engine methods
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48847 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/engine/impl/storage/AbstractBaseConfigRCloneStorageEngine.java
+++ src/prerna/engine/impl/storage/AbstractBaseConfigRCloneStorageEngine.java
@@ -3,7 +3,7 @@ package prerna.engine.impl.storage;
 import java.io.IOException;
 import java.util.List;
 
-public abstract class AbstractBaseConfigRCloneStorageEngine  extends AbstractRCloneStorageEngine {
+public abstract class AbstractBaseConfigRCloneStorageEngine extends AbstractRCloneStorageEngine {
 
 	{
 		this.PROVIDER = "s3";
@@ -19,6 +19,9 @@ public abstract class AbstractBaseConfigRCloneStorageEngine  extends AbstractRCl
 	protected String ACCESS_KEY = null;
 	protected String SECRET_KEY = null;
 
+	/**
+	 * List the folders/files in the path
+	 */
 	public List<String> list(String path) throws IOException, InterruptedException {
 		String rCloneConfig = createRcloneConfig();
 		String rClonePath = rCloneConfig+":";
@@ -34,6 +37,26 @@ public abstract class AbstractBaseConfigRCloneStorageEngine  extends AbstractRCl
 		}
 		List<String> results = runRcloneProcess(rCloneConfig, "rclone", "lsf", rClonePath);
 		return results;
+	}
+	
+	public void moveToStorage(String localFilePath, String storagePath) throws IOException, InterruptedException {
+		String rCloneConfig = createRcloneConfig();
+		String rClonePath = rCloneConfig+":";
+		if(BUCKET != null) {
+			rClonePath += BUCKET;
+		}
+		if(storagePath != null) {
+			if(!storagePath.startsWith("/")) {
+				rClonePath += "/"+storagePath;
+			} else {
+				rClonePath += storagePath;
+			}
+		}
+		// wrap in quotes just in case of spaces, etc.
+		if(!localFilePath.startsWith("\"")) {
+			localFilePath = "\""+localFilePath+"\"";
+		}
+		runRcloneProcess(rCloneConfig, "rclone", "copy", localFilePath, rClonePath);
 	}
 	
 }
