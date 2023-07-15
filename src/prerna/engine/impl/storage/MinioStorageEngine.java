f1cb32d1d3ca6c6ad7e1580e7f058eb56adf1e41
Maher Khalil <themaherkhalil@gmail.com>
2023-07-15 16:18:20 +0000

    UPDATE ::: Further build out for storage engine - setting up main method for testing minio
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48846 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 1a296ee835f0890f3b96cd72bfca4a8d60a61145

--- src/prerna/engine/impl/storage/MinioStorageEngine.java
+++ src/prerna/engine/impl/storage/MinioStorageEngine.java
@@ -1,6 +1,8 @@
 package prerna.engine.impl.storage;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
 
 import prerna.util.Utility;
 
@@ -11,12 +13,27 @@ public class MinioStorageEngine extends AbstractBaseConfigRCloneStorageEngine {
 	 * But the keys are the same as S3 with exception of requiring endpoint
 	 */
 	private static final String S3_REGION_KEY = "S3_REGION";
-	private static final String S3_BUCKET_KEY = "S3_BUCKET";
 	private static final String S3_ACCESS_KEY = "S3_ACCESS_KEY";
 	private static final String S3_SECRET_KEY = "S3_SECRET_KEY";
 	private static final String S3_ENDPOINT_KEY = "S3_ENDPOINT";
 	
+	private static final String S3_BUCKET_KEY = "S3_BUCKET";
+
 	private String ENDPOINT = null;
+	
+	public void connect(Properties smssProp) {
+		super.connect(smssProp);
+		
+		// set this to be the db engine folder
+		this.rcloneConfigFolder = "";
+		this.REGION = smssProp.getProperty(S3_REGION_KEY);
+		this.ACCESS_KEY = smssProp.getProperty(S3_ACCESS_KEY);
+		this.SECRET_KEY = smssProp.getProperty(S3_SECRET_KEY);
+		this.ENDPOINT = smssProp.getProperty(S3_ENDPOINT_KEY);
+		
+		// this is technically not required for minio
+		this.BUCKET = smssProp.getProperty(S3_BUCKET_KEY);
+	}
 	
 	@Override
 	public String createRcloneConfig() throws IOException, InterruptedException {
@@ -28,4 +45,28 @@ public class MinioStorageEngine extends AbstractBaseConfigRCloneStorageEngine {
 		return rcloneConfig;
 	}
 	
+	
+	
+	
+	
+	
+	///////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////
+	
+//	public static void main(String[] args) throws IOException, InterruptedException {
+//		// docker run -p 9000:9000 -p 9001:9001 quay.io/minio/minio server /data --console-address ":9001"
+//
+//		Properties mockSmss = new Properties();
+//		mockSmss.put(S3_REGION_KEY, "us-east-1");
+//		mockSmss.put(S3_ACCESS_KEY, "***REMOVED***");
+//		mockSmss.put(S3_SECRET_KEY, "***REMOVED***");
+//		mockSmss.put(S3_ENDPOINT_KEY, "http://localhost:9000");
+//		
+//		MinioStorageEngine engine = new MinioStorageEngine();
+//		engine.connect(mockSmss);
+//		
+//		List<String> list = engine.list("test1");
+//		System.out.println(list);
+//	}
 }
