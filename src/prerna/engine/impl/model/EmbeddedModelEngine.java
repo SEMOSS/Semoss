9223e879261820495a9271a4726529738e9ae5c4
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-07-25 15:49:15 +0000

    Model Engine Checkin
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48932 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/engine/impl/model/EmbeddedModelEngine.java
@@ -0,0 +1,216 @@
+package prerna.engine.impl.model;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import prerna.ds.py.PyTranslator;
+import prerna.ds.py.TCPPyTranslator;
+import prerna.engine.api.IModelEngine;
+import prerna.om.Insight;
+import prerna.tcp.client.NativePySocketClient;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.Utility;
+
+public class EmbeddedModelEngine implements IModelEngine {
+
+	// starts a embedded model in the same environment
+	// 
+	private static Logger logger = LogManager.getLogger(EmbeddedModelEngine.class);
+	Properties prop = null;
+	File cacheFolder = null;
+	String prefix = null;
+	Process p = null;
+	String port = null;
+	NativePySocketClient socketClient = null;
+	TCPPyTranslator pyt = null;
+	
+	
+	@Override
+	public void loadModel(String modelSmss) {
+		// TODO Auto-generated method stub
+		// starts the model
+		try {
+			prop = new Properties();
+			File file = new File(modelSmss);
+			
+			prop.load(new FileInputStream(file));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void startServer() {
+		// TODO Auto-generated method stub
+		// spin the server
+		// start the client
+		// get the startup command and parameters - at some point we need a better way than the command
+		
+		port = Utility.findOpenPort();
+		// create a generic folder
+		String folderName = "EM_MODEL_" + Utility.getRandomString(6);
+		String fullFolder = DIHelper.getInstance().getProperty(Constants.INSIGHT_CACHE_DIR) + "/" + folderName;
+		cacheFolder = new File(fullFolder);
+		
+		// make the folder if one does not exist
+		if(!cacheFolder.exists())
+			cacheFolder.mkdir();
+			
+		Object [] outputs = Utility.startTCPServerNativePy(fullFolder, port);
+		this.p = (Process)outputs[0];
+		this.prefix = (String)outputs[1];
+		
+		socketClient = new NativePySocketClient();
+		socketClient.connect("127.0.0.1", Integer.parseInt(port), false);
+		
+		// connect the client
+		connectClient();
+		
+		// create the py translator
+		pyt = new TCPPyTranslator();
+		pyt.setClient(socketClient);
+		
+		// execute all the basic commands
+		String initCommands = (String)prop.get(Constants.INIT_MODEL_ENGINE);
+		
+		// break the commands seperated by ;
+		String [] commands = initCommands.split(";");
+		
+		pyt.runEmptyPy(commands);
+		
+	}
+
+	//@Override
+	public PyTranslator getClient() {
+		// TODO Auto-generated method stub
+		return this.pyt;
+	}
+
+	@Override
+	public void stopModel() {
+		// TODO Auto-generated method stub
+		try {
+			socketClient.crash();
+			FileUtils.deleteDirectory(cacheFolder);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public boolean connectClient()
+	{
+		Thread t = new Thread(socketClient);
+		t.start();
+		while(!socketClient.isReady())
+		{
+			synchronized(socketClient)
+			{
+				try 
+				{
+					socketClient.wait();
+					logger.info("Setting the socket client ");
+				} catch (InterruptedException e) {
+					logger.error(Constants.STACKTRACE, e);
+				}								
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	public String ask(String question, String context, Insight insight, Map <String, Object> parameters) 
+	{
+		// TODO Auto-generated method stub
+		//if(this.pyt == null)
+		//	return null;
+		if(!this.socketClient.isConnected())
+			this.startServer();
+		
+		String varName = (String)prop.get("VAR_NAME");
+		
+		StringBuilder callMaker = new StringBuilder().append(varName).append(".ask(");
+		callMaker.append("question='").append(question).append("'");
+		if(context != null)
+		callMaker.append(",").append("context='").append(context).append("'");
+		
+		Iterator <String> paramKeys = parameters.keySet().iterator();
+		while(paramKeys.hasNext())
+		{
+			String key = paramKeys.next();
+			callMaker.append(",").append(key).append("=");
+			Object value = parameters.get(key);
+			if(value instanceof String)
+			{
+				callMaker.append("'").append(value+"").append("'");
+			}
+			else
+			{
+				callMaker.append(value+"");
+			}
+		}
+		callMaker.append(")");
+		System.err.println("call maker.. " + callMaker);
+		
+		Object output = pyt.runScript(callMaker.toString(), insight);
+		
+		return output+"";
+	}
+	
+	private void cleanup()
+	{
+		// remove the directory
+		// force drop it
+		
+	}
+	
+	
+	public static void main(String [] args)
+	{
+		String propFile = "c:/users/pkapaleeswaran/workspacej3/SemossDev/model/Orca_Embedded.smss";
+		
+		DIHelper.getInstance().loadCoreProp("c:/users/pkapaleeswaran/workspacej3/SemossDev/RDF_MAP.prop");
+		
+		IModelEngine eng = new EmbeddedModelEngine();
+		eng.loadModel(propFile);
+		eng.startServer();
+		
+		Map <String, Object> params = new HashMap<String, Object>();
+		params.put("max_new_tokens", 200);
+		params.put("temperature", 0.01);
+		
+		String output = eng.ask("What is the capital of India ?", null, null, params);
+		
+		//PyTranslator pyt = eng.getClient();
+		
+		System.err.println(output);
+		
+		//Object output = pyt.runScript("i.ask(question='What is the capital of India ?')");
+		
+		//System.err.println(output);
+	}
+
+	@Override
+	public String getCatalogType() {
+		// TODO Auto-generated method stub
+		return "Embedded";
+	}
+
+}
---
+++ src/prerna/engine/impl/model/LLMReactor.java
@@ -0,0 +1,59 @@
+package prerna.engine.impl.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import prerna.engine.api.IModelEngine;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Utility;
+
+
+public class LLMReactor extends AbstractReactor
+{
+	public LLMReactor()
+	{
+		this.keysToGet = new String[] {ReactorKeysEnum.ENGINE.getKey(), 
+				ReactorKeysEnum.COMMAND.getKey(), 
+				ReactorKeysEnum.PARAM_VALUES_MAP.getKey()};
+		this.keyRequired = new int[] {1, 1};
+	}
+	
+	// execute method - GREEDY translation
+	public NounMetadata execute()
+	{
+		organizeKeys();
+		
+		String modelId = this.getNounStore().getNoun(keysToGet[0]).get(0) + "";
+		String question = this.getNounStore().getNoun(keysToGet[1]).get(0) + "";
+		Map paramMap = getMap();
+		IModelEngine eng = Utility.getModel(modelId);
+		//Map <String, Object> params = new HashMap<String, Object>();
+		if(paramMap == null)
+			paramMap = new HashMap<String, Object>();
+		String output = eng.ask(question, null, this.insight, paramMap);
+		
+		return new NounMetadata(output, PixelDataType.CONST_STRING, PixelOperationType.OPERATION);
+	}	
+	
+	private Map<String, Object> getMap() 
+	{
+        GenRowStruct mapGrs = this.store.getNoun(keysToGet[2]);
+        if(mapGrs != null && !mapGrs.isEmpty()) {
+            List<NounMetadata> mapInputs = mapGrs.getNounsOfType(PixelDataType.MAP);
+            if(mapInputs != null && !mapInputs.isEmpty()) {
+                return (Map<String, Object>) mapInputs.get(0).getValue();
+            }
+        }
+        List<NounMetadata> mapInputs = this.curRow.getNounsOfType(PixelDataType.MAP);
+        if(mapInputs != null && !mapInputs.isEmpty()) {
+            return (Map<String, Object>) mapInputs.get(0).getValue();
+        }
+        return null;
+    }
+}
