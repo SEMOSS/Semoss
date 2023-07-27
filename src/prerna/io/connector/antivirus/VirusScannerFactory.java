100c0274744443cb112fd7541bd50787972ca1d4
Maher Khalil <themaherkhalil@gmail.com>
2023-07-27 12:13:33 +0000

    UPDATE ::: Adding virus scanning total to factory
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48945 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 7940fbfb4a938a85b95cdcf88f31304ee304cbf9

--- src/prerna/io/connector/antivirus/VirusScannerFactory.java
+++ src/prerna/io/connector/antivirus/VirusScannerFactory.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import prerna.io.connector.antivirus.clamav.ClamAVScannerUtils;
+import prerna.io.connector.antivirus.virustotal.VirusTotalScannerUtils;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
 import prerna.util.Utility;
@@ -20,9 +21,18 @@ public class VirusScannerFactory {
 		if (Utility.isVirusScanningDisabled()) {
 			return null;
 		}
-
-		String scanType = DIHelper.getInstance().getProperty(Constants.VIRUS_SCANNING_METHOD);
-		if (scanType.equalsIgnoreCase(IVirusScanner.CLAM_AV)) {
+		String scanType = DIHelper.getInstance().getProperty(Constants.VIRUS_SCANNING_METHOD).toUpperCase();
+		
+		if(scanType.equals(IVirusScanner.VIRUS_SCANNER_TYPE.CLAM_AV.toString())) {
+			return ClamAVScannerUtils.getInstance();
+		} else if(scanType.equals(IVirusScanner.VIRUS_SCANNER_TYPE.VIRUS_TOTAL.toString())) {
+			return VirusTotalScannerUtils.getInstance();
+		} else if (scanType.equalsIgnoreCase(IVirusScanner.CLAM_AV)) {
+			logger.warn("Using deprecated value - please update parameter value for " + Constants.VIRUS_SCANNING_METHOD + "to CLAM_AV");
+			logger.warn("Using deprecated value - please update parameter value for " + Constants.VIRUS_SCANNING_METHOD + "to CLAM_AV");
+			logger.warn("Using deprecated value - please update parameter value for " + Constants.VIRUS_SCANNING_METHOD + "to CLAM_AV");
+			logger.warn("Using deprecated value - please update parameter value for " + Constants.VIRUS_SCANNING_METHOD + "to CLAM_AV");
+			logger.warn("Using deprecated value - please update parameter value for " + Constants.VIRUS_SCANNING_METHOD + "to CLAM_AV");
 			return ClamAVScannerUtils.getInstance();
 		} else {
 			logger.warn("Virus Scanning is enabled but could not find type for input = '" + scanType + "'");
