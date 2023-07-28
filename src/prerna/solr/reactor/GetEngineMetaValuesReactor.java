f69dacd8abdfbc48d373081e9e175fc6b41e551d
mahkhalil <mahkhalil@deloitte.com>
2023-07-28 13:58:23 +0000

    UPDATE ::: Moving to general engine naming
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48959 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 34e15351cf86b21b4ced1d77aad62aadc2c95a28

--- src/prerna/solr/reactor/GetEngineMetaValuesReactor.java
+++ src/prerna/solr/reactor/GetEngineMetaValuesReactor.java
@@ -6,39 +6,36 @@ import java.util.Map;
 
 import prerna.auth.utils.AbstractSecurityUtils;
 import prerna.auth.utils.SecurityEngineUtils;
-import prerna.engine.api.IDatabase;
 import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 
-public class GetDatabaseMetaValuesReactor extends AbstractReactor {
+public class GetEngineMetaValuesReactor extends AbstractReactor {
     
-    public GetDatabaseMetaValuesReactor() {
-        this.keysToGet = new String[] {ReactorKeysEnum.META_KEYS.getKey()};
+    public GetEngineMetaValuesReactor() {
+        this.keysToGet = new String[] {ReactorKeysEnum.ENGINE_TYPE.getKey(), ReactorKeysEnum.META_KEYS.getKey()};
     }
     
     @Override
     public NounMetadata execute() {
-    	List<String> eTypes = new ArrayList<>();
-		eTypes.add(IDatabase.CATALOG_TYPE);
-		
+    	List<String> engineTypes = getListValues(ReactorKeysEnum.ENGINE_TYPE.getKey());
         List<String> dbList = null;
         if(AbstractSecurityUtils.securityEnabled()) {
-            dbList = SecurityEngineUtils.getUserEngineIdList(this.insight.getUser(), eTypes, true, false, true);
+            dbList = SecurityEngineUtils.getUserEngineIdList(this.insight.getUser(), engineTypes, true, false, true);
         } else {
         	dbList = SecurityEngineUtils.getAllDatabaseIds();
         }
         if(dbList != null && dbList.isEmpty()) {
         	return new NounMetadata(new ArrayList<>(), PixelDataType.CUSTOM_DATA_STRUCTURE);
         }
-        List<Map<String, Object>> ret = SecurityEngineUtils.getAvailableMetaValues(dbList, getMetaKeys());
+        List<Map<String, Object>> ret = SecurityEngineUtils.getAvailableMetaValues(dbList, getListValues(ReactorKeysEnum.META_KEYS.getKey()) );
         return new NounMetadata(ret, PixelDataType.CUSTOM_DATA_STRUCTURE);
     }
     
-    private List<String> getMetaKeys() {
-        GenRowStruct grs = this.store.getNoun(this.keysToGet[0]);
+    private List<String> getListValues(String key) {
+        GenRowStruct grs = this.store.getNoun(key);
         if(grs != null && !grs.isEmpty()) {
             return grs.getAllStrValues();
         }
