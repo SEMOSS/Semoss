d39f800d6ce4707393fbfddd7439c3aa86c1eb3f
Maher Khalil <themaherkhalil@gmail.com>
2023-08-01 13:16:17 +0000

    UPDATE ::: Adding in logic for utility method to start the tcp client UPDATE ::: Changing static variables to public on storage engine
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48982 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: f1b9c41455e0ad21a6d10ad86d6d0ce9b8ada27c

--- src/prerna/engine/impl/storage/GoogleCloudStorageEngine.java
+++ src/prerna/engine/impl/storage/GoogleCloudStorageEngine.java
@@ -11,9 +11,10 @@ public class GoogleCloudStorageEngine extends AbstractRCloneStorageEngine {
 		this.PROVIDER = "google cloud storage";
 	}
 
-	private static final String GCS_REGION = "GCS_REGION";
-	private static final String GCS_SERVICE_ACCOUNT_FILE_KEY = "GCS_SERVICE_ACCOUNT_FILE";
-	
+	public static final String GCS_REGION = "GCS_REGION";
+	public static final String GCS_SERVICE_ACCOUNT_FILE_KEY = "GCS_SERVICE_ACCOUNT_FILE";
+	public static final String GCS_BUCKET_KEY = "GCP_BUCKET";
+
 	// specific values - while not final they shouldn't be modified
 	private String REGION = null;
 	private String GCP_SERVICE_ACCOUNT_FILE = null;
@@ -24,6 +25,7 @@ public class GoogleCloudStorageEngine extends AbstractRCloneStorageEngine {
 		// set this to be the db engine folder
 		this.REGION = smssProp.getProperty(GCS_REGION);
 		this.GCP_SERVICE_ACCOUNT_FILE = smssProp.getProperty(GCS_SERVICE_ACCOUNT_FILE_KEY);
+		this.BUCKET = smssProp.getProperty(GCS_BUCKET_KEY);
 	}
 	
 	@Override
