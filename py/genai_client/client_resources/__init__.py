e4880526fdc6a09b7144e4b5a0b825b8a3bc019a
ttrankle <ttrankle@deloitte.com>
2023-08-02 13:31:45 +0000

    NEW ::: package library for Gen AI clients
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48991 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ce3b583553e2f61183e8b362802a4298e7c626e4

---
+++ py/genai_client/client_resources/__init__.py
@@ -0,0 +1,3 @@
+from genai_client.client_resources.gaas_client_openai import OpenAiClient
+from genai_client.client_resources.gaas_client_fastchat import FastChatClient
+from genai_client.client_resources.gaas_client_textgen import TextGenClient
