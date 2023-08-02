049d1ef0707fb8b89327f8e2346a8bf8ec1f0339
ttrankle <ttrankle@deloitte.com>
2023-08-02 13:31:45 +0000

    NEW ::: package library for Gen AI clients
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48991 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ py/genai_client/model_keys/model_keys_enum.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+class ModelKeysEnum(Enum):
+    api_key = "api_key", "API key being used for the specific model inference"
+    api_base = "api_base", "The base URL for the model inference"
+    model = "model", "The name of the model"
+
+    def __new__(cls, *args, **kwds):
+        obj = object.__new__(cls)
+        obj._value_ = args[0]
+        return obj
+
+    # ignore the first param since it's already set by __new__
+    def __init__(self, _: str, description: str = None):
+        self._description_ = description
+
+    def __str__(self):
+        return self.value
+
+    # this makes sure that the description is read-only
+    @property
+    def description(self):
+        return self._description_
