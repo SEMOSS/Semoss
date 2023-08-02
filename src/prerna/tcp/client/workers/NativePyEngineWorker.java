a1c9f652de3fdc9eb92983578a82c9f0e7e76d6d
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-08-02 16:52:13 +0000

    Checking in Engine Worker
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@48997 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6187db08e7beb9e9c7c4d5f554cb46179e6fb4c0

---
+++ src/prerna/tcp/client/workers/NativePyEngineWorker.java
@@ -0,0 +1,171 @@
+package prerna.tcp.client.workers;
+
+import java.io.Serializable;
+import java.lang.reflect.Method;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.sun.rowset.CachedRowSetImpl;
+
+import prerna.auth.User;
+import prerna.auth.utils.SecurityEngineUtils;
+import prerna.engine.api.IDatabase;
+import prerna.engine.impl.CaseInsensitiveProperties;
+import prerna.tcp.PayloadStruct;
+import prerna.util.Utility;
+
+public class NativePyEngineWorker implements Runnable {
+	
+	// responsible for doing all of the work from an engine's perspective
+	// the server sends information to semoss core to execute something
+	// this thread will work through in terms of executing it
+	// and then send the response back
+	PayloadStruct ps = null;
+	public static final int MAX_ROWS = 50;
+	PayloadStruct output = null;
+	User user = null;
+	
+	public NativePyEngineWorker(User user, PayloadStruct ps)
+	{
+		this.ps = ps;
+		this.user = user;
+	}
+
+	@Override
+	public void run() 
+	{
+		
+		try
+		{
+			// TODO Auto-generated method stub
+			String engineId = ps.objId;
+			
+			// TODO: *****************need to do a security check *************
+			boolean canAccess = SecurityEngineUtils.userIsOwner(user, engineId) 
+								|| SecurityEngineUtils.userCanEditDatabase(user, engineId)
+								|| SecurityEngineUtils.userCanViewDatabase(user, engineId); 
+			
+			if(canAccess)
+			{
+				IDatabase engine = Utility.getDatabase(engineId);
+				Method method = findEngineMethod(engine, ps.methodName, ps.payloadClasses);
+				Object retObject = method.invoke(engine, ps.payload);
+	
+				// the map that comes may not be fully serializable
+				if(retObject instanceof Map && !(retObject instanceof CaseInsensitiveProperties))
+				{
+					Map <String, Object> outputMap = normalizeMap((Map <String, Object>)retObject);
+					ps.payload = new Object[] {outputMap};
+				}
+				else
+				{
+					// need to check for serialization
+					ps.payload = new Object[] {retObject};
+				}
+			}
+			else
+			{
+				ps.payload = new Object[] {"User does not have permission"};
+				ps.payloadClasses = new Class[] {java.lang.String.class};
+			}
+			// got the response
+			ps.response = true;
+			
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+			ps.ex = ex.getLocalizedMessage();
+			ps.response = true;
+		}
+		output = ps;
+	}
+
+    public Method findEngineMethod(IDatabase engine, String methodName, Class [] arguments)
+    {
+    	Method retMethod = null;
+    	
+    	// look for it in the child class if not parent class
+    	// we can even cache this later
+    	try {
+			if(arguments != null)
+			{
+				try
+				{
+					retMethod = engine.getClass().getDeclaredMethod(methodName, arguments);
+				}catch(Exception ex)
+				{
+					
+				}
+				if(retMethod == null)
+					retMethod = engine.getClass().getSuperclass().getDeclaredMethod(methodName, arguments);
+				
+			}
+			else
+			{
+				try
+				{
+					retMethod = engine.getClass().getDeclaredMethod(methodName);				
+				}catch(Exception ex)
+				{
+					
+				}
+				if(retMethod == null)
+					retMethod = engine.getClass().getSuperclass().getDeclaredMethod(methodName, arguments);
+			}
+			//LOGGER.info("Found the method " + retMethod);
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return retMethod;
+    }
+    
+    private Map <String, Object> normalizeMap(Map <String, Object> input)
+    {
+    	// parse through the objects
+    	// if the object is not serializable no go
+    	// if the object is a result set
+    	// turn it into CachedRowsetImpl
+    	Map <String, Object> output = new HashMap<String, Object>();
+    	
+    	Iterator <String> keys = input.keySet().iterator();
+    	
+    	while(keys.hasNext())
+    	{
+    		String key = keys.next();
+    		Object obj = input.get(key);
+    		
+    		if(obj instanceof ResultSet)
+    		{
+    			try {
+					// move this CacheRowSetImpl
+					CachedRowSetImpl impl = new CachedRowSetImpl();
+					impl.setMaxRows(MAX_ROWS);
+					impl.populate((ResultSet)obj);
+					output.put(key, impl);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    		}
+    		else if(obj instanceof Serializable)
+    		{
+    			output.put(key, obj);
+    		}
+    	}
+    	return output;
+    }
+    
+    public PayloadStruct getOutput()
+    {
+    	return this.output;
+    }
+
+	
+}
