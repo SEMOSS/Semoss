9d96653d3143299ece53be47973acf0c9db442ea
mahkhalil <mahkhalil@deloitte.com>
2023-08-03 12:17:28 +0000

    UPDATE ::: Setting so that context project and saved project will be defaulted for running project py + project r UPDATE ::: Adding reconnect server at project lvl
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49004 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/project/ProjectPyReactor.java
+++ src/prerna/sablecc2/reactor/project/ProjectPyReactor.java
@@ -24,11 +24,17 @@ public class ProjectPyReactor extends AbstractReactor {
 	public NounMetadata execute() {
 		organizeKeys();
 		String projectId = this.keyValue.get(this.keysToGet[0]);
-		String code = Utility.decodeURIComponent(this.keyValue.get(this.keysToGet[1]));
-
-		if(projectId == null || projectId.isEmpty()) {
+		if(projectId == null || (projectId=projectId.trim()).isEmpty()) {
+			projectId = this.insight.getContextProjectId();
+			if(projectId == null || (projectId=projectId.trim()).isEmpty()) {
+				projectId = this.insight.getProjectId();
+			}
+		}
+		if(projectId == null || (projectId=projectId.trim()).isEmpty()) {
 			throw new IllegalArgumentException("Must input an project id");
 		}
+		
+		String code = Utility.decodeURIComponent(this.keyValue.get(this.keysToGet[1]));
 		
 		if(AbstractSecurityUtils.securityEnabled()) {
 			// make sure valid id for user
