6966c5d9ab5d45654637261a3bb83a047dbdc389
mahkhalil <mahkhalil@deloitte.com>
2023-08-07 22:58:22 +0000

    UPDATE ::: Adding ExportEngine reactor
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49044 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b9babf09b6af4dae2ab8d5304ffa7e7fed1dfa4a

--- src/prerna/sablecc2/reactor/utils/ExportEngineReactor.java
+++ src/prerna/sablecc2/reactor/utils/ExportEngineReactor.java
@@ -1,12 +1,12 @@
 package prerna.sablecc2.reactor.utils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.zip.ZipOutputStream;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import prerna.auth.User;
@@ -15,8 +15,8 @@ import prerna.auth.utils.SecurityAdminUtils;
 import prerna.auth.utils.SecurityEngineUtils;
 import prerna.auth.utils.SecurityQueryUtils;
 import prerna.engine.api.IDatabase;
+import prerna.engine.api.IEngine;
 import prerna.engine.impl.SmssUtilities;
-import prerna.nameserver.utility.MasterDatabaseUtility;
 import prerna.om.InsightFile;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
@@ -29,71 +29,74 @@ import prerna.util.EngineSyncUtility;
 import prerna.util.Utility;
 import prerna.util.ZipUtils;
 
-@Deprecated
-public class ExportDatabaseReactor extends AbstractReactor {
+public class ExportEngineReactor extends AbstractReactor {
 
-	private static final String CLASS_NAME = ExportDatabaseReactor.class.getName();
+	private static final Logger classLogger = LogManager.getLogger(ExportEngineReactor.class);
+	private static final String CLASS_NAME = ExportEngineReactor.class.getName();
 
-	public ExportDatabaseReactor() {
-		this.keysToGet = new String[] { ReactorKeysEnum.DATABASE.getKey() };
+	public ExportEngineReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.ENGINE.getKey() };
 	}
 
 	@Override
 	public NounMetadata execute() {
 		Logger logger = getLogger(CLASS_NAME);
 		organizeKeys();
-		String databaseId = this.keyValue.get(this.keysToGet[0]);
+		String engineId = this.keyValue.get(this.keysToGet[0]);
 		
 		// security
 		User user = this.insight.getUser();
 		if (AbstractSecurityUtils.securityEnabled()) {
-			databaseId = SecurityQueryUtils.testUserEngineIdForAlias(this.insight.getUser(), databaseId);
+			engineId = SecurityQueryUtils.testUserEngineIdForAlias(this.insight.getUser(), engineId);
 			boolean isAdmin = SecurityAdminUtils.userIsAdmin(user);
 			if (!isAdmin) {
-				boolean isOwner = SecurityEngineUtils.userIsOwner(user, databaseId);
+				boolean isOwner = SecurityEngineUtils.userIsOwner(user, engineId);
 				if (!isOwner) {
-					throw new IllegalArgumentException("Database " + databaseId + " does not exist or user does not have permissions to database. User must be the owner to perform this function.");
+					throw new IllegalArgumentException("Engine " + engineId + " does not exist or user does not have permissions to engine. User must be the owner to perform this function.");
 				}
-			}
-		} else {
-			databaseId = MasterDatabaseUtility.testDatabaseIdIfAlias(databaseId);
-			if (!MasterDatabaseUtility.getAllDatabaseIds().contains(databaseId)) {
-				throw new IllegalArgumentException("Database " + databaseId + " does not exist");
 			}
 		}
 
-		logger.info("Exporting Database Now... ");
-		logger.info("Stopping the engine ... ");
+		logger.info("Exporting engine... ");
 		// remove the database
-		
 		String zipFilePath = null;
-		ReentrantLock lock = EngineSyncUtility.getEngineLock(databaseId);
+		ReentrantLock lock = EngineSyncUtility.getEngineLock(engineId);
 		lock.lock();
+		boolean closed = false;
 		try {
-			IDatabase database = Utility.getDatabase(databaseId);
-			DIHelper.getInstance().removeEngineProperty(databaseId);
-			database.close();
+			IEngine engine = Utility.getEngine(engineId);
+			if(engine instanceof IDatabase) {
+				logger.info("Stopping the database engine... ");
+				DIHelper.getInstance().removeEngineProperty(engineId);
+				try {
+					engine.close();
+					closed = true;
+				} catch (IOException e) {
+					classLogger.error(Constants.STACKTRACE, e);
+				}
+			} else {
+				logger.info("Can export this engine w/o closing... ");
+			}
 			
-			String databaseName = database.getEngineName();
+			String engineName = engine.getEngineName();
 			String OUTPUT_PATH = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + "/export/ZIPs";
-			String engineDir = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + "/db/" + SmssUtilities.getUniqueName(databaseName, databaseId);
-			zipFilePath = OUTPUT_PATH + "/" + databaseName + "_database.zip";
+			String engineDir = DIHelper.getInstance().getProperty(Constants.BASE_FOLDER) + "/db/" + SmssUtilities.getUniqueName(engineName, engineId);
+			zipFilePath = OUTPUT_PATH + "/" + engineName + "_database.zip";
 			
 			// zip database
 			ZipOutputStream zos = null;
 			try {
 				// zip db folder
-				logger.info("Zipping database files...");
+				logger.info("Zipping engine files...");
 				zos = ZipUtils.zipFolder(engineDir, zipFilePath);
-				logger.info("Done zipping database files...");
+				logger.info("Done zipping engine folder");
 				// add smss file
-				File smss = new File(engineDir + "/../" + SmssUtilities.getUniqueName(databaseName, databaseId) + ".smss");
-				logger.info("Saving file " + smss.getName());
+				File smss = new File(engineDir + "/../" + SmssUtilities.getUniqueName(engineName, engineId) + ".smss");
+				logger.info("Adding smss file...");
 				ZipUtils.addToZipFile(smss, zos);
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
+				logger.info("Done adding smss file");
 			} catch (IOException e) {
-				e.printStackTrace();
+				classLogger.error(Constants.STACKTRACE, e);
 			} finally {
 				try {
 					if (zos != null) {
@@ -101,16 +104,23 @@ public class ExportDatabaseReactor extends AbstractReactor {
 						zos.close();
 					}
 				} catch (IOException e) {
-					e.printStackTrace();
+					classLogger.error(Constants.STACKTRACE, e);
 				}
 			}
 
-			logger.info("Synchronize database Complete");
+			logger.info("Finished creating zip");
 		} finally {
 			// open it back up
-			logger.info("Opening the database again ... ");
-			Utility.getDatabase(databaseId);
-			lock.unlock();
+			try {
+				if(closed) {
+					logger.info("Reconnecting to the database...");
+					Utility.getDatabase(engineId);
+					logger.info("Reconnected to the database");
+				}
+			} finally {
+				// in case opening up causing an issue - we always want to unlock
+				lock.unlock();
+			}
 		}
 
 		// store it in the insight so the FE can download it
