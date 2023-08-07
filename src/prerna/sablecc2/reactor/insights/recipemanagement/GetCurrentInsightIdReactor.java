59bddae99c6338fe7c2b0ceab9e0acccc507e3df
mahkhalil <mahkhalil@deloitte.com>
2023-08-07 23:31:03 +0000

    UPDATE ::: Adding reactor to quickly print the current insight id
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49045 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b8c0dd99a99c730c569b19f5c132665b8eea8ce5

---
+++ src/prerna/sablecc2/reactor/insights/recipemanagement/GetCurrentInsightIdReactor.java
@@ -0,0 +1,14 @@
+package prerna.sablecc2.reactor.insights.recipemanagement;
+
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class GetCurrentInsightIdReactor extends AbstractReactor {
+
+	@Override
+	public NounMetadata execute() {
+		return new NounMetadata(this.insight.getInsightId(), PixelDataType.CONST_STRING);
+	}
+
+}
