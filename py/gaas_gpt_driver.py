fb8589aa8ffdaa24515b133b19fbcd13793fa5f2
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-08-08 02:04:42 +0000

    Making the engine to be parameterized
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49051 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e2a4861bf86a117f997c3d77047282eea52e81f3

--- py/gaas_gpt_driver.py
+++ py/gaas_gpt_driver.py
@@ -3,9 +3,9 @@ class Driver():
     self.insight_id = insight_id
   
 
-  def run_questions(self, question=None, num_iterations=1):
+  def run_questions(self, question=None, engine_id="EMB_30991037-1e73-49f5-99d3-f28210e6b95c11", num_iterations=1):
     import gaas_gpt_model as ggm
-    me = ggm.ModelEngine(engine_id="EMB_30991037-1e73-49f5-99d3-f28210e6b95c11")
+    me = ggm.ModelEngine(engine_id=engine_id)
     response = []
     for ask_iter in range(0, num_iterations):
       answer = me.ask(question=question, insight_id=self.insight_id)
