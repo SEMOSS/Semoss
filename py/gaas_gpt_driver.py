89f82a56f392260a480fa8aa43786d877faaad84
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-08-08 02:03:40 +0000

    TCP Server - Updates to enable better logging as well as capturing insight Id. Server Proxy - Insight ID Gaas_gpt_driver - Simple tester class for trying a question as well as to stress test for specific number of times
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49050 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ py/gaas_gpt_driver.py
@@ -0,0 +1,13 @@
+class Driver():
+  def __init__(self, insight_id=None):
+    self.insight_id = insight_id
+  
+
+  def run_questions(self, question=None, num_iterations=1):
+    import gaas_gpt_model as ggm
+    me = ggm.ModelEngine(engine_id="EMB_30991037-1e73-49f5-99d3-f28210e6b95c11")
+    response = []
+    for ask_iter in range(0, num_iterations):
+      answer = me.ask(question=question, insight_id=self.insight_id)
+      response.append(answer)
+    return response
