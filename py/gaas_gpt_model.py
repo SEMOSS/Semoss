075a9f7a7d1267b7461fe3bfe43f2b2ae450ffd3
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-08-10 15:50:54 +0000

    Update to the insight id
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49080 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- py/gaas_gpt_model.py
+++ py/gaas_gpt_model.py
@@ -2,25 +2,45 @@ from gaas_server_proxy import ServerProxy
 
 class ModelEngine(ServerProxy):
   
-  def __init__(self, engine_id=None):
+  def __init__(self, engine_id=None, insight_id=None):
     assert engine_id is not None 
     super().__init__()
     self.engine_id = engine_id
+    self.insight_id = insight_id
     print("initialized")
    
-  def embeddings(self, question=None):
+  def embeddings(self, question=None, insight_id=None):
     assert question is not None
-    return super().call(engine_type='Model', engine_id=self.engine_id, insight_id=None, method_name='embeddings', method_args=[question])
+    if insight_id is None:
+      insight_id = self.insight_id
+    assert insight_id is not None
+    epoc = super().get_next_epoc()
+    return super().call(
+                      epoc = epoc, 
+                      engine_type='Model', 
+                      engine_id=self.engine_id, 
+                      insight_id=insight_id, 
+                      method_name='embeddings', 
+                      method_args=[question],
+                      method_arg_types=['java.lang.String']
+                      )
     
     
   def ask(self, question=None, context=None, insight_id=None, param_dict=None):
     assert question is not None
+    if insight_id is None:
+      insight_id = self.insight_id
+    # should I assert for insight_id as well I think I should
+    assert insight_id is not None    
     epoc = super().get_next_epoc()
-    return super().call(epoc=epoc, 
+    return super().call(
+                        epoc=epoc, 
                         engine_type='Model', 
-                        engine_id=self.engine_id, method_name='ask', 
+                        engine_id=self.engine_id, 
+                        method_name='ask', 
                         method_args=[question,context,insight_id, param_dict],
-                        method_arg_types=['java.lang.String', 'java.lang.String', 'prerna.om.Insight', 'java.util.Map']  
+                        method_arg_types=['java.lang.String', 'java.lang.String', 'prerna.om.Insight', 'java.util.Map'],
+                        insight_id = insight_id
                         )
     
     
