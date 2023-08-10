98c5d409929400447c3d208713793edb042e59f0
mahkhalil <mahkhalil@deloitte.com>
2023-08-10 17:30:11 +0000

    UPDATE ::: Adding in refactoring around creating engines / databases
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49082 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: ab348c2fce15969c8df7bdb2c66200f333fb23ec

--- src/prerna/sablecc2/reactor/database/upload/rdbms/excel/RdbmsReplaceDatabaseExcelUploadReactor.java
+++ src/prerna/sablecc2/reactor/database/upload/rdbms/excel/RdbmsReplaceDatabaseExcelUploadReactor.java
@@ -133,7 +133,7 @@ public class RdbmsReplaceDatabaseExcelUploadReactor extends RdbmsUploadExcelData
 		// TODO:
 		ClusterUtil.reactorPushDatabase(this.databaseId);
 
-		Map<String, Object> retMap = UploadUtilities.getDatabaseReturnData(this.insight.getUser(), this.databaseId);
+		Map<String, Object> retMap = UploadUtilities.getEngineReturnData(this.insight.getUser(), this.databaseId);
 		return new NounMetadata(retMap, PixelDataType.UPLOAD_RETURN_MAP, PixelOperationType.MARKET_PLACE_ADDITION);
 	}
 
