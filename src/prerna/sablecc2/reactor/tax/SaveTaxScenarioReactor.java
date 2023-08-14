78105122e938e1256f08028854ef212c262cb311
mahkhalil <mahkhalil@deloitte.com>
2023-08-14 18:29:22 +0000

    UPDATE ::: Removing deprecated rdbms builder where not needed in methods that dont require refactoring
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49123 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/tax/SaveTaxScenarioReactor.java
+++ src/prerna/sablecc2/reactor/tax/SaveTaxScenarioReactor.java
@@ -2,11 +2,11 @@ package prerna.sablecc2.reactor.tax;
 
 import java.util.Map;
 
-import prerna.ds.util.RdbmsQueryBuilder;
 import prerna.engine.api.IDatabase;
 import prerna.engine.impl.rdbms.RDBMSNativeEngine;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.sql.AbstractSqlQueryUtil;
 
 public class SaveTaxScenarioReactor extends AbstractReactor {
 
@@ -18,7 +18,7 @@ public class SaveTaxScenarioReactor extends AbstractReactor {
 	}
 	
 	public static boolean addNewVersion(IDatabase engine, String clientID, double scenarioID, double latestVersion, double newVersion, Map<String, String> newValues) {
-		clientID = RdbmsQueryBuilder.escapeForSQLStatement(clientID);
+		clientID = AbstractSqlQueryUtil.escapeForSQLStatement(clientID);
 		String sql = "Insert Into INPUTCSV (CLIENT_ID, SCENARIO, VERSION, FORMNAME, FIELDNAME, ALIAS_1, VALUE_1, TYPE_1, HASHCODE, WEIGHT, RETURNTYPE, COLUMN_1) "
 				+ "Select CLIENT_ID, SCENARIO, " + newVersion + ", FORMNAME, FIELDNAME, ALIAS_1, VALUE_1, TYPE_1, HASHCODE, WEIGHT, RETURNTYPE, COLUMN_1 From INPUTCSV "
 				+ "WHERE CLIENT_ID='" + clientID + "' AND SCENARIO=" + scenarioID + " AND VERSION=" + latestVersion;
@@ -27,8 +27,8 @@ public class SaveTaxScenarioReactor extends AbstractReactor {
 		((RDBMSNativeEngine) engine).execUpdateAndRetrieveStatement(sql, true);
 		
 		for(String alias : newValues.keySet()) {
-			alias = RdbmsQueryBuilder.escapeForSQLStatement(alias);
-			String aliasValue = RdbmsQueryBuilder.escapeForSQLStatement(newValues.get(alias));
+			String aliasValue = AbstractSqlQueryUtil.escapeForSQLStatement(newValues.get(alias));
+			alias = AbstractSqlQueryUtil.escapeForSQLStatement(alias);
 			sql = "UPDATE INPUTCSV SET VALUE_1='" + aliasValue + "' "
 					+ "WHERE CLIENT_ID='" + clientID + "' AND SCENARIO=" + scenarioID + " AND VERSION=" + newVersion + " AND ALIAS_1='" + alias + "'";
 			
