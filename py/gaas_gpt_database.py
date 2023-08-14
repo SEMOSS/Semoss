ab7f3452a49986d8cc0d0802c8b325d9280532a4
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-08-14 13:40:22 +0000

    Checking in Database Proxy updates
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49114 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 557d1753193706cc83ebca6431eded6aa1f77fb1

---
+++ py/gaas_gpt_database.py
@@ -0,0 +1,71 @@
+from gaas_server_proxy import ServerProxy
+
+class DatabaseEngine(ServerProxy):
+  
+  def __init__(self, engine_id=None, insight_id=None):
+    assert engine_id is not None 
+    super().__init__()
+    self.engine_id = engine_id
+    self.insight_id = insight_id
+    print("initialized")
+   
+  def execQuery(self, query=None, insight_id=None, return_pandas=True):
+    assert query is not None
+    if insight_id is None:
+      insight_id = self.insight_id
+    assert insight_id is not None
+    epoc = super().get_next_epoc()
+    fileLoc = super().call(
+                      epoc = epoc, 
+                      engine_type='database', 
+                      engine_id=self.engine_id, 
+                      insight_id=insight_id, 
+                      method_name='execQuery', 
+                      method_args=[query],
+                      method_arg_types=['java.lang.String']
+                      )
+    if isinstance(fileLoc, list) and len(fileLoc) > 0:
+      fileLoc = fileLoc[0]
+    if return_pandas:
+      print(f"file Location {fileLoc}")
+      import pandas as pd
+      return pd.read_json(fileLoc)
+    else:
+      return open(fileLoc, "r").read()
+
+
+  def insertData(self, query=None, insight_id=None):
+    assert query is not None
+    if insight_id is None:
+      insight_id = self.insight_id
+    assert insight_id is not None
+    epoc = super().get_next_epoc()
+    return super().call(
+                      epoc = epoc, 
+                      engine_type='database', 
+                      engine_id=self.engine_id, 
+                      insight_id=insight_id, 
+                      method_name='insertData', 
+                      method_args=[query],
+                      method_arg_types=['java.lang.String']
+                      )
+
+
+  def removeData(self, query=None, insight_id=None):
+    assert query is not None
+    if insight_id is None:
+      insight_id = self.insight_id
+    assert insight_id is not None
+    epoc = super().get_next_epoc()
+    return super().call(
+                      epoc = epoc, 
+                      engine_type='database', 
+                      engine_id=self.engine_id, 
+                      insight_id=insight_id, 
+                      method_name='removeData', 
+                      method_args=[query],
+                      method_arg_types=['java.lang.String']
+                      )
+
+  
+    
