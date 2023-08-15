3e32d5c495d1b3b039c2c280621243ffbd392e54
Maher Khalil <themaherkhalil@gmail.com>
2023-08-15 16:29:43 +0000

    BUGFIX ::: Fixing error with DeleteAsset not working properly and accounting for when the file is not a git version location UPDATE ::: Adding new reactor to quickly get the context project id
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49148 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a5ba1592bcce49b3844eac90aeae8e869950b418

--- src/prerna/util/git/reactors/DeleteAssetReactor.java
+++ src/prerna/util/git/reactors/DeleteAssetReactor.java
@@ -1,9 +1,13 @@
 package prerna.util.git.reactors;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 
-import org.h2.store.fs.FileUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import prerna.auth.AccessToken;
 import prerna.auth.AuthProvider;
@@ -15,12 +19,15 @@ import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.util.AssetUtility;
+import prerna.util.Constants;
 import prerna.util.Utility;
 import prerna.util.git.GitDestroyer;
 import prerna.util.git.GitRepoUtils;
 
 public class DeleteAssetReactor extends AbstractReactor {
 
+	private static final Logger classLogger = LogManager.getLogger(DeleteAssetReactor.class);
+	
 	public DeleteAssetReactor() {
 		this.keysToGet = new String[] { ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.COMMENT_KEY.getKey(),
 				ReactorKeysEnum.SPACE.getKey() };
@@ -46,29 +53,79 @@ public class DeleteAssetReactor extends AbstractReactor {
 
 		// get asset base folder
 		String space = this.keyValue.get(this.keysToGet[2]);
-		String assetFolder = AssetUtility.getAssetVersionBasePath(this.insight, space, true);
+		String baseFolderPath = AssetUtility.getAssetVersionBasePath(this.insight, space, AbstractSecurityUtils.securityEnabled());
 		// relative path is used for git if insight is saved
+		// or if we are dealing with project space
 		String relativePath = "";
-		if (this.insight.isSavedInsight()) {
+		if (space != null || insight.isSavedInsight()) {
 			relativePath = AssetUtility.getAssetRelativePath(this.insight, space);
 		}
 
 		// get the file path to delete
-		String fileName = Utility.normalizePath(keyValue.get(keysToGet[0]));
+		String inputFilePath = Utility.normalizePath(keyValue.get(keysToGet[0]));
+		if(inputFilePath == null || (inputFilePath.trim().isEmpty())) {
+			throw new IllegalArgumentException("Must pass in a file name to delete");
+		}
 		String comment = this.keyValue.get(this.keysToGet[1]);
 
-		List<String> files = new Vector<>();
-		files.add(relativePath + "/" + fileName);
-		// if security enables, you need proper permissions
-		// this takes in the insight and does a user check that the user has access to perform the operations
-		String baseFolder = AssetUtility.getAssetBasePath(this.insight, space, AbstractSecurityUtils.securityEnabled());
-		FileUtils.delete(baseFolder + "/" + fileName);
+		String realFilePath = baseFolderPath+"/"+inputFilePath;
+		realFilePath=realFilePath.replace("\\", "/");
+		File realFile = new File(realFilePath);
+		if(!realFile.exists()) {
+			throw new IllegalArgumentException("Cannot find the folder/file at path " + inputFilePath);
+		}
+		
+		List<String> gitRelativeFilePaths = new ArrayList<>();
+		gitRelativeFilePaths.add(relativePath + "/" + inputFilePath);
+
+		if(realFile.isDirectory()) {
+			try {
+				FileUtils.deleteDirectory(realFile);
+			} catch (IOException e) {
+				classLogger.error(Constants.STACKTRACE, e);
+				throw new IllegalArgumentException("Error occurred trying to delete folder at path " + inputFilePath);
+			}
+		} else {
+			try {
+				FileUtils.forceDelete(realFile);
+			} catch (IOException e) {
+				classLogger.error(Constants.STACKTRACE, e);
+				throw new IllegalArgumentException("Error occurred trying to delete file at path " + inputFilePath);
+			}
+		}
 
 		// commit it
-		if (this.insight.isSavedInsight()) {
-			GitDestroyer.removeSpecificFiles(assetFolder, true, files);
-			GitRepoUtils.commitAddedFiles(assetFolder, comment, author, email);
+		// but need to make sure we are in a git...
+		// which depends on the space and if the insight is saved...
+		if(insight.isSavedInsight() && (space == null || AssetUtility.INSIGHT_SPACE_KEY.equalsIgnoreCase(space))) {
+			IProject project = Utility.getProject(this.insight.getProjectId());
+			String gitVersionFolder = AssetUtility.getProjectVersionFolder(project.getProjectName(), insight.getProjectId()).replace("\\", "/");
+			if(realFilePath.startsWith(gitVersionFolder)) {
+				GitDestroyer.removeSpecificFiles(gitVersionFolder, true, gitRelativeFilePaths);
+				GitRepoUtils.commitAddedFiles(gitVersionFolder, comment, author, email);
+			}
+		} else if (AssetUtility.USER_SPACE_KEY.equalsIgnoreCase(space)) {
+			AuthProvider provider = user.getPrimaryLogin();
+			String userProjectId = user.getAssetProjectId(provider);
+			if(userProjectId!=null && !(userProjectId.isEmpty())) {
+				IProject userProject = Utility.getUserAssetWorkspaceProject(userProjectId, true);
+				String gitVersionFolder = AssetUtility.getUserAssetAndWorkspaceAssetFolder(userProject.getProjectName(), userProject.getProjectId()).replace("\\", "/");
+				if(realFilePath.startsWith(gitVersionFolder)) {
+					GitDestroyer.removeSpecificFiles(gitVersionFolder, true, gitRelativeFilePaths);
+					GitRepoUtils.commitAddedFiles(gitVersionFolder, comment, author, email);
+				}
+			}
+		} else {
+			IProject project = Utility.getProject(space);
+			String gitVersionFolder = AssetUtility.getProjectVersionFolder(project.getProjectName(), space).replace("\\", "/");
+			if(realFilePath.startsWith(gitVersionFolder)) {
+				GitDestroyer.removeSpecificFiles(gitVersionFolder, true, gitRelativeFilePaths);
+				GitRepoUtils.commitAddedFiles(gitVersionFolder, comment, author, email);
+			}
 		}
+		
+		//TODO: consolidate below with above
+		//TODO: create new methods to directly deleteFromStorage instead of an entire sync
 		
 		// push to the cloud
 		if(ClusterUtil.IS_CLUSTER) {
@@ -83,11 +140,11 @@ public class DeleteAssetReactor extends AbstractReactor {
 			// is it an insight asset change
 			} else if(space == null || space.trim().isEmpty() || space.equals(AssetUtility.INSIGHT_SPACE_KEY)) {
 				IProject project = Utility.getProject(this.insight.getProjectId());
-				ClusterUtil.reactorPushProjectFolder(project, assetFolder);
+				ClusterUtil.reactorPushProjectFolder(project, baseFolderPath);
 			// this is a project asset. space is the projectId
 			} else {
 				IProject project = Utility.getProject(space);
-				ClusterUtil.reactorPushProjectFolder(project, assetFolder);
+				ClusterUtil.reactorPushProjectFolder(project, baseFolderPath);
 			}
 		}
 
