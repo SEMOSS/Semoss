b443bc8118c2fd7ee53531a1d5244b07d5fc2298
kunalppatel9 <kunalppatel9@deloitte.com>
2023-08-15 17:57:10 +0000

    UPDATE ::: splitting text gen to be able to connect remote and the have a process builder for local model
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49151 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 172fc36a8affd74c3abe4ab386c7abc54a1f2abf

--- src/prerna/engine/impl/model/TextGenerationProcessInference.java
+++ src/prerna/engine/impl/model/TextGenerationProcessInference.java
@@ -26,7 +26,7 @@ import prerna.util.Constants;
 import prerna.util.DIHelper;
 import prerna.util.Utility;
 
-public class TextGenerationProcessInference extends AbstractModelEngine {
+public class TextGenerationProcessInference extends TextGenerationEngine {
 	
 	private static Logger logger = LogManager.getLogger(TextGenerationProcessInference.class);
 	private HashMap<String,String> launchArguments = new HashMap<String,String>();
@@ -179,49 +179,7 @@ public class TextGenerationProcessInference extends AbstractModelEngine {
 	}
 	
 
-	@Override
-	public String askQuestion(String question, String context, Insight insight, Map<String, Object> parameters) {
-		
-		String varName = (String) generalEngineProp.get("VAR_NAME");
-		
-		StringBuilder callMaker = new StringBuilder().append(varName).append(".ask(");
-		callMaker.append("question=\"").append(question).append("\"");
-		if(context != null)
-			callMaker.append(",").append("context=").append(context);
-		
-		
-		if(parameters != null) {
-			if (parameters.containsKey("ROOM_ID")) { //always have to remove roomId so we dont pass it to py client
-				String roomId = (String) parameters.get("ROOM_ID");
-				parameters.remove("ROOM_ID");
-				if (Utility.isModelInferenceLogsEnabled()) { // have to check that inference logs are enabled so that query works
-					String history = getConversationHistory(roomId);
-					if(history != null) //could still be null if its the first question in the convo
-						callMaker.append(",").append("history=").append(history);
-				}
-			}
-						
-			Iterator <String> paramKeys = parameters.keySet().iterator();
-			while(paramKeys.hasNext()) {
-				String key = paramKeys.next();
-				callMaker.append(",").append(key).append("=");
-				Object value = parameters.get(key);
-				if(value instanceof String)
-				{
-					callMaker.append("'").append(value+"").append("'");
-				}
-				else
-				{
-					callMaker.append(value+"");
-				}
-			}
-		}
-		callMaker.append(")");
-		System.out.println(callMaker.toString());
-		Object output = pyt.runScript(callMaker.toString());
-		return output+"";
-	}
-	
+
 
     private static ImmutableMap<String, String> getLauncherArgs() {
     	return new ImmutableMap.Builder<String, String>()
