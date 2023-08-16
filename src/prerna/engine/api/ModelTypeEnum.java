afbd781889db06b60b101870fe9f39a11d53aa4e
mahkhalil <mahkhalil@deloitte.com>
2023-08-16 15:30:33 +0000

    BUGFIX ::: Forgot to commit model type enum class
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49172 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 17ed78e0e00b849f2e9f6cf1b7ace1fefda9f34e

---
+++ src/prerna/engine/api/ModelTypeEnum.java
@@ -0,0 +1,48 @@
+package prerna.engine.api;
+
+public enum ModelTypeEnum {
+
+	EMBEDDED("EMBEDDED","prerna.engine.impl.model.EmbeddedModelEngine"),
+	FAST_CHAT("FAST_CHAT","prerna.engine.impl.model.FastChatProcessModel"),
+	OPEN_AI("OPEN_AI","prerna.engine.impl.model.OpenAiEngine"),
+	TEXT_GENERATION("TEXT_GENERATION","prerna.engine.impl.model.TextGenerationEngine");
+	
+	private String modelName;
+	private String modelClass;
+	
+	ModelTypeEnum(String storageName, String storageClass) {
+		this.modelName = storageName;
+		this.modelClass = storageClass;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getModelClass() {
+		return this.modelClass;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getModelName() {
+		return this.modelName;
+	}
+	
+	/**
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public static ModelTypeEnum getEnumFromName(String name) {
+		ModelTypeEnum[] allValues = values();
+		for(ModelTypeEnum v : allValues) {
+			if(v.getModelName().equalsIgnoreCase(name)) {
+				return v;
+			}
+		}
+		throw new IllegalArgumentException("Invalid input for name " + name);
+	}
+}
