c6c3a97a2d8f435cdfd4470ce2438bde4225ce2a
ttrankle <ttrankle@deloitte.com>
2023-08-17 13:26:32 +0000

    UPDATE ::: allow context to be passed in the ask method
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49186 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/engine/impl/model/LLMReactor.java
+++ src/prerna/engine/impl/model/LLMReactor.java
@@ -20,8 +20,9 @@ public class LLMReactor extends AbstractReactor
 	{
 		this.keysToGet = new String[] {ReactorKeysEnum.ENGINE.getKey(), 
 				ReactorKeysEnum.COMMAND.getKey(), 
+				ReactorKeysEnum.CONTEXT.getKey(), 
 				ReactorKeysEnum.PARAM_VALUES_MAP.getKey()};
-		this.keyRequired = new int[] {1, 1};
+		this.keyRequired = new int[] {1, 1, 0, 0};
 	}
 	
 	// execute method - GREEDY translation
@@ -31,20 +32,21 @@ public class LLMReactor extends AbstractReactor
 		
 		String modelId = this.getNounStore().getNoun(keysToGet[0]).get(0) + "";
 		String question = this.getNounStore().getNoun(keysToGet[1]).get(0) + "";
+		String context = getContext();
 		Map paramMap = getMap();
 		IModelEngine eng = Utility.getModel(modelId);
 		//Map <String, Object> params = new HashMap<String, Object>();
 		if(paramMap == null)
 			paramMap = new HashMap<String, Object>();
 		
-		String output = eng.ask(question, null, this.insight, paramMap);
+		String output = eng.ask(question, context, this.insight, paramMap);
 		
 		return new NounMetadata(output, PixelDataType.CONST_STRING, PixelOperationType.OPERATION);
 	}	
 	
 	private Map<String, Object> getMap() 
 	{
-        GenRowStruct mapGrs = this.store.getNoun(keysToGet[2]);
+        GenRowStruct mapGrs = this.store.getNoun(keysToGet[3]);
         if(mapGrs != null && !mapGrs.isEmpty()) {
             List<NounMetadata> mapInputs = mapGrs.getNounsOfType(PixelDataType.MAP);
             if(mapInputs != null && !mapInputs.isEmpty()) {
@@ -57,4 +59,15 @@ public class LLMReactor extends AbstractReactor
         }
         return null;
     }
+	
+	private String getContext() {
+		GenRowStruct contextGrs = this.store.getNoun(keysToGet[2]);
+		if(contextGrs != null && !contextGrs.isEmpty()) {
+            List<NounMetadata> contextInput = contextGrs.getNounsOfType(PixelDataType.CONST_STRING);
+            if(contextInput != null && !contextInput.isEmpty()) {
+                return (String) contextInput.get(0).getValue();
+            }
+        }
+		return null;
+	}
 }
