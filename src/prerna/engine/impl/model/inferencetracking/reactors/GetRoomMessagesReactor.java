d7b498d08a170882e0ae5ff09593ed810b47ffdd
ttrankle <ttrankle@deloitte.com>
2023-08-17 22:32:16 +0000

    NEW ::: baseline reactors for inference logs
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49202 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/engine/impl/model/inferencetracking/reactors/GetRoomMessagesReactor.java
@@ -0,0 +1,43 @@
+package prerna.engine.impl.model.inferencetracking.reactors;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import prerna.auth.User;
+import prerna.engine.impl.model.inferencetracking.ModelInferenceLogsUtils;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class GetRoomMessagesReactor extends AbstractReactor {
+	@SuppressWarnings("unused")
+	private static final Logger logger = LogManager.getLogger(GetRoomMessagesReactor.class);
+
+    public GetRoomMessagesReactor() {
+        this.keysToGet = new String[] {"roomId"};
+        this.keyRequired = new int[] {1};
+    }
+    
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		User user = this.insight.getUser();
+        if (user == null) {
+            throw new IllegalArgumentException("You are not properly logged in");
+        }
+        String roomId = this.keyValue.get(this.keysToGet[0]);
+        List<Map<String, Object>> output = ModelInferenceLogsUtils.doRetrieveConversation(user.getPrimaryLoginToken().getId(), roomId);
+		return new NounMetadata(output, PixelDataType.VECTOR);
+	}
+	
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if (key.equals("roomId")) {
+			return "The room or conversation ID for a given chat app";
+		} 
+		return super.getDescriptionForKey(key);
+	}
+}
---
+++ src/prerna/engine/impl/model/inferencetracking/reactors/RemoveUserRoomReactor.java
@@ -0,0 +1,40 @@
+package prerna.engine.impl.model.inferencetracking.reactors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import prerna.auth.User;
+import prerna.engine.impl.model.inferencetracking.ModelInferenceLogsUtils;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class RemoveUserRoomReactor extends AbstractReactor {
+	@SuppressWarnings("unused")
+	private static final Logger logger = LogManager.getLogger(RemoveUserRoomReactor.class);
+
+    public RemoveUserRoomReactor() {
+        this.keysToGet = new String[] {"roomId"};
+        this.keyRequired = new int[] {1};
+    }
+    
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		User user = this.insight.getUser();
+        if (user == null) {
+            throw new IllegalArgumentException("You are not properly logged in");
+        }
+        String roomId = this.keyValue.get(this.keysToGet[0]);
+        boolean result = ModelInferenceLogsUtils.doSetRoomToInactive(user.getPrimaryLoginToken().getId(), roomId);
+		return new NounMetadata(result, PixelDataType.BOOLEAN);
+	}
+	
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if (key.equals("roomId")) {
+			return "The room or conversation ID for a given chat app";
+		} 
+		return super.getDescriptionForKey(key);
+	}
+}
