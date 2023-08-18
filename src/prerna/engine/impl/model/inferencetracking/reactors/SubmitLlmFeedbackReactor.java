53337562b8e1276dce64eabc2fe8a2df3a4088a4
ttrankle <ttrankle@deloitte.com>
2023-08-18 11:58:19 +0000

    NEW ::: Ability to submit or update feedback for an LLM response
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49215 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 9450baa07eed748d61ff04a47640669af60d6c92

---
+++ src/prerna/engine/impl/model/inferencetracking/reactors/SubmitLlmFeedbackReactor.java
@@ -0,0 +1,45 @@
+package prerna.engine.impl.model.inferencetracking.reactors;
+
+import java.util.List;
+import java.util.Map;
+
+import prerna.auth.User;
+import prerna.engine.impl.model.inferencetracking.ModelInferenceLogsUtils;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.execptions.SemossPixelException;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class SubmitLlmFeedbackReactor extends AbstractReactor {
+	
+    public SubmitLlmFeedbackReactor() {
+        this.keysToGet = new String[] {"messageId", "feedbackText", "rating"};
+        this.keyRequired = new int[] {1, 0, 1};
+    }
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		User user = this.insight.getUser();
+        if (user == null) {
+            throw new IllegalArgumentException("You are not properly logged in");
+        }
+        String messageId = this.keyValue.get(this.keysToGet[0]);
+        boolean output = ModelInferenceLogsUtils.userIsMessageAuthor(user.getPrimaryLoginToken().getId(), messageId);
+        if (!output) {
+        	throw new SemossPixelException("User is not the author of this message and cannot provide feedback");
+        }
+        String feedbackText = this.keyValue.get(this.keysToGet[1]);
+        boolean rating = Boolean.parseBoolean(this.keyValue.get(this.keysToGet[2]) + "");
+        ModelInferenceLogsUtils.recordFeedback(messageId, feedbackText, rating);
+		return new NounMetadata(true, PixelDataType.BOOLEAN);
+	}
+	
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if (key.equals("messageId")) {
+			return "The unique identififer for the I/O betweeen a user and the LLM response";
+		} 
+		return super.getDescriptionForKey(key);
+	}
+}
