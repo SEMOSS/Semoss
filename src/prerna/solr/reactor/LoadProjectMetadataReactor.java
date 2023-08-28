a4165b6c9972f3c41aaa9f667aa1c3eb7c6b8bb9
mahkhalil <mahkhalil@deloitte.com>
2023-08-28 19:31:26 +0000

    UPDATE ::: Adding reactor to read the project metadata from a file
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49344 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 097e411984ef5df1ce931ee6e550146fd6b8480b

---
+++ src/prerna/solr/reactor/LoadProjectMetadataReactor.java
@@ -0,0 +1,99 @@
+package prerna.solr.reactor;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+import prerna.auth.utils.SecurityProjectUtils;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.util.Constants;
+import prerna.util.Utility;
+import prerna.util.upload.UploadInputUtility;
+
+public class LoadProjectMetadataReactor extends AbstractSetMetadataReactor {
+	
+	private static final Logger classLogger = LogManager.getLogger(LoadProjectMetadataReactor.class);
+
+	public LoadProjectMetadataReactor() {
+		this.keysToGet = new String[]{ ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.SPACE.getKey() };
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String fileLocation = Utility.normalizePath(UploadInputUtility.getFilePath(this.store, this.insight));
+		if(!new File(fileLocation).exists()) {
+			throw new IllegalArgumentException("Unable to locate file");
+		}
+		
+		Map<String, Object> metadata = null;
+		JsonReader jReader = null;
+		FileReader fReader = null;
+		try {
+			Gson gson = new Gson();
+			fReader = new FileReader(fileLocation);
+			jReader = new JsonReader(fReader);
+			metadata = gson.fromJson(jReader, Map.class);
+	    } catch(IOException e) {
+	    	classLogger.error(Constants.STACKTRACE, e);
+	    } finally {
+	    	if(fReader != null) {
+	    		try {
+					fReader.close();
+				} catch (IOException e) {
+			    	classLogger.error(Constants.STACKTRACE, e);
+				}
+	    	}
+	    	if(jReader != null) {
+	    		try {
+					jReader.close();
+				} catch (IOException e) {
+			    	classLogger.error(Constants.STACKTRACE, e);
+				}
+	    	}
+	    }
+		
+		String projectId = (String) metadata.remove("projectId");
+		if(projectId == null) {
+			// assume its in the filename
+			String projectAliasAndId = FilenameUtils.getBaseName(fileLocation);
+			if(projectAliasAndId.contains("__")) {
+				projectId = projectAliasAndId.split("__")[1];
+			} else {
+				projectId = projectAliasAndId;
+			}
+		}
+		
+		if(!SecurityProjectUtils.userCanEditProject(this.insight.getUser(), projectId)) {
+			throw new IllegalArgumentException("Project does not exist or user does not have access to edit");
+		}
+		
+		// check for invalid metakeys
+		List<String> validMetakeys = SecurityProjectUtils.getAllMetakeys();
+		if(!validMetakeys.containsAll(metadata.keySet())) {
+	    	throw new IllegalArgumentException("Unallowed metakeys. Can only use: "+String.join(", ", validMetakeys));
+		}
+		
+		SecurityProjectUtils.updateProjectMetadata(projectId, metadata);
+		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
+		noun.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Successfully set the new metadata values for the engine"));
+		return noun;
+	}
+	
+	@Override
+	public String getReactorDescription() {
+		return "Define metadata on a project through a JSON file";
+	}
+	
+}
