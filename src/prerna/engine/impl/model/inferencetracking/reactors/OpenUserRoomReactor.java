c7c9357d375b245b08ac59c6815621e106f5a65e
ttrankle <ttrankle@deloitte.com>
2023-08-29 12:31:13 +0000

    NEW ::: reactor to allow an insightId to be set
    TODO - need ModelInferenceLogs check. Need to update schema before doing this
    
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49352 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: eeb182dc9fd0c12b353b9154b843e75452bcaa49

---
+++ src/prerna/engine/impl/model/inferencetracking/reactors/OpenUserRoomReactor.java
@@ -0,0 +1,62 @@
+package prerna.engine.impl.model.inferencetracking.reactors;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import prerna.om.Insight;
+import prerna.om.InsightStore;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.insights.AbstractInsightReactor;
+import prerna.util.Utility;
+import prerna.util.insight.InsightUtility;
+
+public class OpenUserRoomReactor extends AbstractInsightReactor {
+	
+	public OpenUserRoomReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.RECIPE.getKey(), ReactorKeysEnum.PARAM_KEY.getKey(), ReactorKeysEnum.ID.getKey()};
+		this.keyRequired = new int [] {0,0,0};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		// create a new empty insight
+		this.organizeKeys();
+		Insight newInsight = new Insight();
+		
+		String insightId = this.keyValue.get(this.keysToGet[2]);
+		if (insightId != null && !insightId.isEmpty() && !InsightStore.getInstance().containsKey(insightId)) {
+			newInsight.setInsightId(insightId);
+		}
+		
+		newInsight.setCacheInWorkspace(true);
+		InsightUtility.transferDefaultVars(this.insight, newInsight);
+		InsightStore.getInstance().put(newInsight);
+		InsightStore.getInstance().addToSessionHash(getSessionId(), newInsight.getInsightId());
+		// set the user in the insight
+		newInsight.setUser(this.insight.getUser());
+
+		List<String> newRecipe = new Vector<String>();
+		try {
+			List<String> recipe = getRecipe();
+			if(recipe != null) {
+				for(String r : recipe) {
+					newRecipe.add(Utility.decodeURIComponent(r));
+				}
+			}
+		} catch(IllegalArgumentException e) {
+			// ignore
+			// by default we throw error when recipe is not passed in
+		}
+		
+		// return the recipe steps
+		Map<String, Object> runnerWraper = new HashMap<String, Object>();
+		runnerWraper.put("runner", newInsight.runPixel(newRecipe));
+		runnerWraper.put("params", getExecutionParams());
+		return new NounMetadata(runnerWraper, PixelDataType.PIXEL_RUNNER, PixelOperationType.NEW_EMPTY_INSIGHT);
+	}
+}
