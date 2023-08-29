a500da9aa16b49edd0333c8ad9c8457d352d3d69
mahkhalil <mahkhalil@deloitte.com>
2023-08-29 15:43:51 +0000

    UPDATE ::: Not returning admin reactors if user is not admin in help
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49359 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 9c4e607a28c2fa2a1272f71b5bbb3c1c6dc7371b

--- src/prerna/sablecc2/reactor/utils/HelpReactor.java
+++ src/prerna/sablecc2/reactor/utils/HelpReactor.java
@@ -2,6 +2,8 @@ package prerna.sablecc2.reactor.utils;
 
 import java.util.TreeSet;
 
+import prerna.auth.User;
+import prerna.auth.utils.SecurityAdminUtils;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
@@ -17,70 +19,94 @@ public class HelpReactor extends AbstractReactor {
 	
 	private static final String RESET_KEY = "reset";
 	private static String helpString = null;
-	
+	private static String adminHelpString = null;
+
 	public HelpReactor() {
 		this.keysToGet = new String[] {RESET_KEY};
 	}
 	
 	@Override
 	public NounMetadata execute() {
+		User user = this.insight.getUser();
+		boolean isAdmin = (SecurityAdminUtils.getInstance(user)) != null;
 		organizeKeys();
 		boolean reset = Boolean.parseBoolean(this.keyValue.get(this.keysToGet[0]));
-		if(reset || helpString == null) {
-			// create a string builder to keep track of all categories of reactors
-			StringBuilder allReactors = new StringBuilder();
-			
-			// general reactors
-			TreeSet general = new TreeSet(ReactorFactory.reactorHash.keySet());
-			if (general.size() > 0) {
-				allReactors.append("General Reactors: \n").append(formatOutput(general));
+		if(isAdmin) {
+			if(reset || adminHelpString == null) {
+				HelpReactor.adminHelpString = createHelpString(true);
 			}
-			
-			// the frame specific reactors
-			//rframe
-			TreeSet rFrame = new TreeSet(ReactorFactory.rFrameHash.keySet());
-			if (rFrame.size() > 0) {
-				allReactors.append("R Frame Reactors: \n").append(formatOutput(rFrame));
+			return new NounMetadata(HelpReactor.adminHelpString, PixelDataType.CONST_STRING, PixelOperationType.HELP);
+		} else {
+			if(reset || helpString == null) {
+				HelpReactor.helpString = createHelpString(false);
 			}
-			
-			//h2
-			TreeSet h2Frame = new TreeSet(ReactorFactory.h2FrameHash.keySet());
-			if (h2Frame.size() > 0) {
-				allReactors.append("H2 Frame Reactors: \n").append(formatOutput(h2Frame));
-			}
-			
-			//native
-			TreeSet nativeFrame = new TreeSet(ReactorFactory.nativeFrameHash.keySet());
-			if (nativeFrame.size() > 0) {
-				allReactors.append("Native Frame Reactors: \n").append(formatOutput(nativeFrame));
-			}
-			
-			//tinker
-			TreeSet tinkerFrame = new TreeSet(ReactorFactory.tinkerFrameHash.keySet());
-			if (tinkerFrame.size() > 0) {
-				allReactors.append("Tinker Frame Reactors: \n").append(formatOutput(tinkerFrame));
-			}
-			
-			// the expression set
-			TreeSet expressionSet = new TreeSet(ReactorFactory.expressionHash.keySet());
-			if (expressionSet.size() > 0) {
-				allReactors.append("Expression Set Reactors: \n").append(formatOutput(expressionSet));
-			}
-			
-			//return a string with all of the reactors
-			HelpReactor.helpString = allReactors.toString();
+			return new NounMetadata(HelpReactor.helpString, PixelDataType.CONST_STRING, PixelOperationType.HELP);
 		}
-		
-		return new NounMetadata(helpString, PixelDataType.CONST_STRING, PixelOperationType.HELP);
 	}
 	
-	private String formatOutput(TreeSet t) {
+	/**
+	 * 
+	 * @param isAdmin
+	 * @return
+	 */
+	private String createHelpString(boolean isAdmin) {
+		// create a string builder to keep track of all categories of reactors
+		StringBuilder allReactors = new StringBuilder();
+		
+		// general reactors
+		TreeSet general = new TreeSet(ReactorFactory.reactorHash.keySet());
+		if (general.size() > 0) {
+			allReactors.append("General Reactors: \n").append(formatOutput(general, isAdmin));
+		}
+		
+		// the frame specific reactors
+		//rframe
+		TreeSet rFrame = new TreeSet(ReactorFactory.rFrameHash.keySet());
+		if (rFrame.size() > 0) {
+			allReactors.append("R Frame Reactors: \n").append(formatOutput(rFrame, isAdmin));
+		}
+		
+		//h2
+		TreeSet h2Frame = new TreeSet(ReactorFactory.h2FrameHash.keySet());
+		if (h2Frame.size() > 0) {
+			allReactors.append("H2 Frame Reactors: \n").append(formatOutput(h2Frame, isAdmin));
+		}
+		
+		//native
+		TreeSet nativeFrame = new TreeSet(ReactorFactory.nativeFrameHash.keySet());
+		if (nativeFrame.size() > 0) {
+			allReactors.append("Native Frame Reactors: \n").append(formatOutput(nativeFrame, isAdmin));
+		}
+		
+		//tinker
+		TreeSet tinkerFrame = new TreeSet(ReactorFactory.tinkerFrameHash.keySet());
+		if (tinkerFrame.size() > 0) {
+			allReactors.append("Tinker Frame Reactors: \n").append(formatOutput(tinkerFrame, isAdmin));
+		}
+		
+		// the expression set
+		TreeSet expressionSet = new TreeSet(ReactorFactory.expressionHash.keySet());
+		if (expressionSet.size() > 0) {
+			allReactors.append("Expression Set Reactors: \n").append(formatOutput(expressionSet, isAdmin));
+		}
+		
+		return allReactors.toString();
+	}
+	
+	private String formatOutput(TreeSet t, boolean isAdmin) {
 		//keep track of the output using a string builder
 		StringBuilder formatString = new StringBuilder();
 		//use a count so that we can go to a new line every three entries (create three columns)
 		int count = 0;
 		//iterate through the values of the tree map and include spaces between them
 		for (Object value : t) {
+			// if not an admin
+			// dont show Admin reactors as options
+			if(!isAdmin) {
+				if(value.toString().toLowerCase().startsWith("admin")) {
+					continue;
+				}
+			}
 			formatString.append(value);
 			//keep track of the amount of spaces to add
 			//add enough to line up the columns
