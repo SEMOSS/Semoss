8adb9e547e036def5bfa7782acb5af8742972c0c
mahkhalil <mahkhalil@deloitte.com>
2023-08-29 16:21:39 +0000

    UPDATE ::: Returning additional metadata on the AdminGetEngines call
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49361 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/solr/reactor/AdminMyEnginesReactor.java
@@ -0,0 +1,222 @@
+package prerna.solr.reactor;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import prerna.auth.User;
+import prerna.auth.utils.SecurityAdminUtils;
+import prerna.auth.utils.SecurityEngineUtils;
+import prerna.engine.api.IRawSelectWrapper;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.usertracking.UserCatalogVoteUtils;
+import prerna.util.Constants;
+
+public class AdminMyEnginesReactor extends AbstractReactor {
+	
+	private static final Logger classLogger = LogManager.getLogger(AdminMyEnginesReactor.class);
+
+	public AdminMyEnginesReactor() {
+		this.keysToGet = new String[] {ReactorKeysEnum.FILTER_WORD.getKey(), 
+				ReactorKeysEnum.LIMIT.getKey(), ReactorKeysEnum.OFFSET.getKey(),
+				ReactorKeysEnum.ENGINE_TYPE.getKey(), ReactorKeysEnum.ENGINE.getKey(),
+				ReactorKeysEnum.META_KEYS.getKey(), ReactorKeysEnum.META_FILTERS.getKey(),
+				ReactorKeysEnum.NO_META.getKey(), ReactorKeysEnum.INCLUDE_USERTRACKING_KEY.getKey()
+			};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		// add creator, upvotes, total views
+		// sort by name, date created, views, upvotes, trending
+		User user = this.insight.getUser();
+		SecurityAdminUtils adminUtils = SecurityAdminUtils.getInstance(user);
+		if(adminUtils == null) {
+			throw new IllegalArgumentException("User must be an admin to perform this function");
+		}
+		
+		organizeKeys();
+		
+		String searchTerm = this.keyValue.get(this.keysToGet[0]);
+		String limit = this.keyValue.get(this.keysToGet[1]);
+		String offset = this.keyValue.get(this.keysToGet[2]);
+		List<String> engineTypes = getEngineTypeFilters();
+		List<String> engineIdFilters = getEngineIdFilters();
+		Boolean noMeta = Boolean.parseBoolean(this.keyValue.get(ReactorKeysEnum.NO_META.getKey()));
+		Boolean includeUserT = Boolean.parseBoolean(this.keyValue.get(ReactorKeysEnum.INCLUDE_USERTRACKING_KEY.getKey()));
+		
+		Map<String, Object> engineMetadataFilter = getMetaMap();
+		List<Map<String, Object>> engineInfo = adminUtils.getAllEngineSettings(engineIdFilters, engineTypes, engineMetadataFilter, searchTerm, limit, offset);
+
+		if(!engineInfo.isEmpty() && (!noMeta || includeUserT)) {
+			Map<String, Integer> index = new HashMap<>(engineInfo.size());
+			int size = engineInfo.size();
+			for(int i = 0; i < size; i++) {
+				Map<String, Object> engine = engineInfo.get(i);
+				String engineId = engine.get("database_id").toString();
+				// keep list of database ids to get the index
+				index.put(engineId, Integer.valueOf(i));
+			}
+			
+			if(!noMeta) {
+				IRawSelectWrapper wrapper = null;
+				try {
+					wrapper = SecurityEngineUtils.getEngineMetadataWrapper(index.keySet(), getMetaKeys(), true);
+					while(wrapper.hasNext()) {
+						Object[] data = wrapper.next().getValues();
+						String databaseId = (String) data[0];
+		
+						String metaKey = (String) data[1];
+						String metaValue = (String) data[2];
+						if(metaValue == null) {
+							continue;
+						}
+		
+						int indexToFind = index.get(databaseId);
+						Map<String, Object> res = engineInfo.get(indexToFind);
+						// whatever it is, if it is single send a single value, if it is multi send as array
+						if(res.containsKey(metaKey)) {
+							Object obj = res.get(metaKey);
+							if(obj instanceof List) {
+								((List) obj).add(metaValue);
+							} else {
+								List<Object> newList = new ArrayList<>();
+								newList.add(obj);
+								newList.add(metaValue);
+								res.put(metaKey, newList);
+							}
+						} else {
+							res.put(metaKey, metaValue);
+						}
+					}
+				} catch (Exception e) {
+					classLogger.error(Constants.STACKTRACE, e);
+				} finally {
+					if(wrapper!=null) {
+						try {
+							wrapper.close();
+						} catch (IOException e) {
+							classLogger.error(Constants.STACKTRACE, e);
+						}
+					}
+				}
+			} 
+			if(includeUserT) {
+				IRawSelectWrapper wrapper = null;
+				try {
+					wrapper = UserCatalogVoteUtils.getAllVotesWrapper(index.keySet());
+					while(wrapper.hasNext()) {
+						Object[] data = wrapper.next().getValues();
+						String databaseId = (String) data[0];
+						int upvotes = ((Number) data[1]).intValue();
+		
+						int indexToFind = index.get(databaseId);
+						Map<String, Object> res = engineInfo.get(indexToFind);
+						res.put("upvotes", upvotes);
+					}
+				} catch (Exception e) {
+					classLogger.error(Constants.STACKTRACE, e);
+				} finally {
+					if(wrapper!=null) {
+						try {
+							wrapper.close();
+						} catch (IOException e) {
+							classLogger.error(Constants.STACKTRACE, e);
+						}
+					}
+				}
+				
+				Map<String, Boolean> voted = UserCatalogVoteUtils.userEngineVotes(User.getUserIdAndType(this.insight.getUser()), index.keySet());
+				for (String ks : index.keySet()) {
+					int indexToFind = index.get(ks);
+					Boolean hasUpvoted = voted.get(ks);
+					if (hasUpvoted == null) {
+						hasUpvoted = false;
+					}
+					engineInfo.get(indexToFind).put("hasUpvoted", hasUpvoted);
+				}
+			}
+		}
+		
+		return new NounMetadata(engineInfo, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.DATABASE_INFO);
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private List<String> getEngineIdFilters() {
+		GenRowStruct grs = this.store.getNoun(ReactorKeysEnum.ENGINE.getKey());
+		if(grs != null && !grs.isEmpty()) {
+			return grs.getAllStrValues();
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private List<String> getEngineTypeFilters() {
+		GenRowStruct grs = this.store.getNoun(ReactorKeysEnum.ENGINE_TYPE.getKey());
+		if(grs != null && !grs.isEmpty()) {
+			return grs.getAllStrValues();
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private List<String> getMetaKeys() {
+		GenRowStruct grs = this.store.getNoun(ReactorKeysEnum.META_KEYS.getKey());
+		if(grs != null && !grs.isEmpty()) {
+			return grs.getAllStrValues();
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private Map<String, Object> getMetaMap() {
+		GenRowStruct mapGrs = this.store.getNoun(ReactorKeysEnum.META_FILTERS.getKey());
+		if(mapGrs != null && !mapGrs.isEmpty()) {
+			List<NounMetadata> mapInputs = mapGrs.getNounsOfType(PixelDataType.MAP);
+			if(mapInputs != null && !mapInputs.isEmpty()) {
+				return (Map<String, Object>) mapInputs.get(0).getValue();
+			}
+		}
+		List<NounMetadata> mapInputs = this.curRow.getNounsOfType(PixelDataType.MAP);
+		if(mapInputs != null && !mapInputs.isEmpty()) {
+			return (Map<String, Object>) mapInputs.get(0).getValue();
+		}
+		return null;
+	}
+
+	
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if(key.equals(ReactorKeysEnum.SORT.getKey())) {
+			return "The sort is a string value containing either 'name' or 'date' for how to sort";
+		} else if(key.equals(ReactorKeysEnum.DATABASE.getKey())) {
+			return "This is an optional database filter";
+		}
+		return super.getDescriptionForKey(key);
+	}
+}
