1346c2c51dfd6efcd2609ee1e9473db99dc4b825
ttrankle <ttrankle@deloitte.com>
2023-08-30 20:23:41 +0000

    UPDATE ::: create the insight when calling the rooms
    TODO - clean it up code after demo
    
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49397 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 76deebccf0d2e1b7599366e343533534aea033b2

--- src/prerna/engine/impl/model/inferencetracking/reactors/GetUserConversationRoomsReactor.java
+++ src/prerna/engine/impl/model/inferencetracking/reactors/GetUserConversationRoomsReactor.java
@@ -1,16 +1,25 @@
 package prerna.engine.impl.model.inferencetracking.reactors;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import prerna.engine.impl.model.inferencetracking.ModelInferenceLogsUtils;
+import prerna.om.Insight;
+import prerna.om.InsightStore;
+import prerna.sablecc2.om.NounStore;
 import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.execptions.SemossPixelException;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Utility;
+import prerna.util.insight.InsightUtility;
 import prerna.auth.User;
 
 public class GetUserConversationRoomsReactor extends AbstractReactor {
@@ -34,6 +43,60 @@ public class GetUserConversationRoomsReactor extends AbstractReactor {
         	projectId = this.insight.getContextProjectId();
         } 
         List<Map<String, Object>> output = ModelInferenceLogsUtils.getUserConversations(user.getPrimaryLoginToken().getId(), projectId);
-		return new NounMetadata(output, PixelDataType.VECTOR);
+		if (output.size() > 0) {
+			for (Map<String, Object> convo : output) {
+				createInsights((String) convo.get("ROOM_ID"));
+				//NounStore ns = new NounStore(ReactorKeysEnum.ALL.getKey());
+				//ns.makeNoun(ReactorKeysEnum.ID.getKey()).addLiteral();
+//				OpenUserRoomReactor newInsight = new OpenUserRoomReactor();
+//				
+//				newInsight.setInsight(this.insight);
+//				newInsight
+//				newInsight.setNounStore(ns);
+//				NounMetadata newInsightCreated = newInsight.execute();
+//				if (newInsightCreated.getNounType() == PixelDataType.ERROR) {
+//					throw new SemossPixelException((String) newInsightCreated.getValue());
+//				}				
+			}
+		}
+        return new NounMetadata(output, PixelDataType.VECTOR);
+	}
+	
+	
+	public NounMetadata createInsights(String insightId) {
+		Insight newInsight = new Insight();
+		if (insightId != null && !insightId.isEmpty() && !InsightStore.getInstance().containsKey(insightId)) {
+			List<Map<String, Object>> output = ModelInferenceLogsUtils.doVerifyConversation(this.insight.getUserId(), insightId);
+			if (output.size() > 0) {
+				newInsight.setInsightId(insightId);
+				String projectId = (String) output.get(0).get("PROJECT_ID");
+				if (projectId != null && !projectId.isEmpty()) {
+					newInsight.setProjectId(projectId);
+				}
+			} else {
+				if (this.insight.getProjectId() != null && !this.insight.getProjectId().isEmpty()) {
+					newInsight.setProjectId(this.insight.getProjectId());
+				} 
+			}
+		}
+		
+		if (this.insight.getContextProjectId() != null && !this.insight.getContextProjectId().isEmpty()) {
+			newInsight.setContextProjectId(this.insight.getContextProjectId());
+		} 
+		
+		newInsight.setCacheInWorkspace(true);
+		InsightUtility.transferDefaultVars(this.insight, newInsight);
+		InsightStore.getInstance().put(newInsight);
+		InsightStore.getInstance().addToSessionHash(getSessionId(), newInsight.getInsightId());
+		// set the user in the insight
+		newInsight.setUser(this.insight.getUser());
+
+		List<String> newRecipe = new Vector<String>();
+
+		
+		// return the recipe steps
+		Map<String, Object> runnerWraper = new HashMap<String, Object>();
+		runnerWraper.put("runner", newInsight.runPixel(newRecipe));
+		return new NounMetadata(runnerWraper, PixelDataType.PIXEL_RUNNER, PixelOperationType.NEW_EMPTY_INSIGHT);
 	}
 }
