9b43031b705ceb3dc97682609dea18702620a69b
ttrankle <ttrankle@deloitte.com>
2023-09-01 14:51:31 +0000

    NEW ::: create constants class for model engine
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49409 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 445dd0228bb9378e6d02a281e5f9819bc279b749

---
+++ src/prerna/engine/impl/model/ModelEngineConstants.java
@@ -0,0 +1,20 @@
+package prerna.engine.impl.model;
+
+public class ModelEngineConstants {
+	
+	public static final String MESSAGE_CONTENT = "content";
+	public static final String ROLE = "role";
+	public static final String GENERATE_ROOM_NAME = "GENERATE_ROOM_NAME";
+	public static final String PY_COMMAND_SEPARATOR = ";";
+	public static final String VAR_NAME = "VAR_NAME";
+	public static final String CUR_DIR = "CUR_DIR";
+	public static final String ENGINE_DIR = "ENGINE_DIR";
+	public static final String KEEP_CONVERSATION_HISTORY = "KEEP_CONVERSATION_HISTORY";
+	public static final String KEEP_INPUT_OUTPUT = "KEEP_INPUT_OUTPUT";
+	@Deprecated
+	public static final String KEEP_CONTEXT = "KEEP_CONTEXT";
+	public static final String RESPONSE = "RESPONSE";
+	public static final String INPUT = "INPUT";
+	public static final String MESSAGE_ID = "messageId";
+	public static final String ROOM_ID = "roomId";
+}
