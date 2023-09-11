d2f65bb67e157bcd74006bbf11d6b8140412cd03
mahkhalil <mahkhalil@deloitte.com>
2023-09-11 22:30:05 +0000

    UPDATE ::: Add Get[Engine][Project]Markdown reactors UPDATE ::: Add AdminGet[Engine][Project]Markdown reactors
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49476 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/auth/utils/reactors/admin/AdminGetEngineMarkdownReactor.java
@@ -0,0 +1,35 @@
+package prerna.auth.utils.reactors.admin;
+
+import prerna.auth.User;
+import prerna.auth.utils.SecurityAdminUtils;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+
+public class AdminGetEngineMarkdownReactor extends AbstractReactor {
+	
+	public AdminGetEngineMarkdownReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.ENGINE.getKey()};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		User user = this.insight.getUser();
+		SecurityAdminUtils adminUtils = SecurityAdminUtils.getInstance(user);
+		if(adminUtils == null) {
+			throw new IllegalArgumentException("User must be an admin to perform this function");
+		}
+		
+		organizeKeys();
+		String engineId = this.keyValue.get(this.keysToGet[0]);
+		if(engineId == null) {
+			throw new IllegalArgumentException("Need to define the engine to get the markdown from");
+		}
+		
+		String databaseMarkdown = adminUtils.getEngineMarkdown(engineId);
+		return new NounMetadata(databaseMarkdown, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.ENGINE_INFO);
+	}
+
+}
