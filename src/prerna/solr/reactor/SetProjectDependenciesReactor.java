b7cb8093f99ffca5bafbbb9a61841fe98e7cecfe
mahkhalil <mahkhalil@deloitte.com>
2023-09-11 22:49:28 +0000

    UPDATE ::: Adding reactor to set project dependencies
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49477 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

---
+++ src/prerna/solr/reactor/SetProjectDependenciesReactor.java
@@ -0,0 +1,61 @@
+package prerna.solr.reactor;
+
+import java.util.List;
+
+import prerna.auth.User;
+import prerna.auth.utils.SecurityProjectUtils;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.util.upload.UploadInputUtility;
+
+public class SetProjectDependenciesReactor extends AbstractSetMetadataReactor {
+	
+	public SetProjectDependenciesReactor() {
+		this.keysToGet = new String[]{
+				ReactorKeysEnum.PROJECT.getKey(), "dependencies", 
+			};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		User user = this.insight.getUser();
+		String projectId = UploadInputUtility.getProjectNameOrId(this.store);
+		projectId = SecurityProjectUtils.testUserProjectIdForAlias(this.insight.getUser(), projectId);
+		if(!SecurityProjectUtils.userCanEditProject(user, projectId)) {
+			throw new IllegalArgumentException("Project does not exist or user does not have access to edit");
+		}
+		
+		List<String> dependentEngineIds = getDependentEngineIds();
+		SecurityProjectUtils.updateProjectDependencies(user, projectId, dependentEngineIds);
+		NounMetadata noun = new NounMetadata(true, PixelDataType.BOOLEAN);
+		noun.addAdditionalReturn(NounMetadata.getSuccessNounMessage("Successfully set the new dependencies"));
+		return noun;
+	}
+	
+	@Override
+	public String getReactorDescription() {
+		return "Set the engine dependencies for a project";
+	}
+	
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if(key.equals("dependencies")) {
+			return "The list of engineid's that this project depends on for full functionality";
+		}
+		return super.getDescriptionForKey(key);
+	}
+	
+	
+	private List<String> getDependentEngineIds() {
+		GenRowStruct colGrs = this.store.getNoun("dependencies");
+		if (colGrs != null && !colGrs.isEmpty()) {
+			return colGrs.getAllStrValues();
+		}
+
+		return null;
+	}
+
+}
