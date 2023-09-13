4bd627164536df4140455d46a9553642d26e25c4
Maher Khalil <themaherkhalil@gmail.com>
2023-09-13 14:09:27 +0000

    UPDATE ::: Add additional engine (database) metadata on the SimilarCatalog
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49489 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: e424d189dc5d1ea0ed51f2f8a10533ddfe61db7c

--- src/prerna/sablecc2/reactor/database/SimilarCatalogReactor.java
+++ src/prerna/sablecc2/reactor/database/SimilarCatalogReactor.java
@@ -7,19 +7,31 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import prerna.auth.utils.SecurityEngineUtils;
 import prerna.ds.py.PyTranslator;
+import prerna.engine.api.IRawSelectWrapper;
 import prerna.nameserver.utility.MasterDatabaseUtility;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.usertracking.UserCatalogVoteUtils;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
+import prerna.util.Utility;
 
 public class SimilarCatalogReactor extends AbstractReactor {
+	
+	private static final Logger classLogger = LogManager.getLogger(SimilarCatalogReactor.class);
 	
 	public SimilarCatalogReactor() {
 		this.keysToGet = new String[] {ReactorKeysEnum.DATABASE.getKey()};
@@ -29,9 +41,16 @@ public class SimilarCatalogReactor extends AbstractReactor {
 	public NounMetadata execute() {
 		organizeKeys();
 		String databaseId = this.keyValue.get(this.keysToGet[0]);
-		String dbAlias = MasterDatabaseUtility.getDatabaseAliasForId(databaseId);
 		
-		List<String> dbIds = MasterDatabaseUtility.getAllDatabaseIds();
+		if (!SecurityEngineUtils.userCanViewEngine(this.insight.getUser(), databaseId)) {
+			throw new IllegalArgumentException("Database cannot be viewed by user.");
+		}
+		
+		String dbAlias = MasterDatabaseUtility.getDatabaseAliasForId(databaseId);
+
+		List<String> engineTypes = new ArrayList<>();
+		List<Map<String, Object>> dbInfo = SecurityEngineUtils.getUserEngineList(this.insight.getUser(), engineTypes, null, false, null, null, null, null, "0");
+		List<String> dbIds = dbInfo.stream().map(s-> s.get("database_id").toString()).collect(Collectors.toList());
 		String metadatas = "metadatas = {";
 		for (String s : dbIds) {
 			String alias = MasterDatabaseUtility.getDatabaseAliasForId(s);
@@ -69,27 +88,134 @@ public class SimilarCatalogReactor extends AbstractReactor {
 		pt.runEmptyPy(dbAlias);
 		pt.runEmptyPy(script);
 		
-		Boolean hasResults = pt.getBoolean("has_results");
-		if (hasResults == null || !hasResults) {
+		List<Object> result = pt.getList("result");
+		if (result == null || result.size() == 0) {
 			return new NounMetadata("No Catalogs found for search term", PixelDataType.CONST_STRING);
 		}
-
-		List<Map<String, String>> dbs = new ArrayList<>();
-
-		List<Object> result = pt.getList("result");
+		
+		Set<String> ids = new HashSet<>();
 		for (Object res : result) {
 			String alias = res.toString();
-			List<String> ids = MasterDatabaseUtility.getDatabaseIdsForAlias(alias);
-			if (ids == null || ids.size() == 0) {
+			List<String> aliasedIds = MasterDatabaseUtility.getDatabaseIdsForAlias(alias);
+			if (aliasedIds == null || aliasedIds.size() == 0) {
 				continue;
 			}
-			Map<String, String> dbMap = new HashMap<>();
-			dbMap.put("database_id", ids.get(0));
-			dbMap.put("database_name", alias);
-			dbs.add(dbMap);
+			ids.addAll(aliasedIds);
 		}
 		
-		return new NounMetadata(dbs, PixelDataType.CUSTOM_DATA_STRUCTURE);
+		List<Map<String, Object>> toReturn = new ArrayList<>();
+		for (Map<String, Object> x : dbInfo) {
+			if (ids.contains(x.get("database_id").toString())) {
+				toReturn.add(x);
+			}
+		}
+		
+		Map<String, Integer> index = new HashMap<>();
+		for (int i = 0; i < toReturn.size(); i++) {
+			index.put(toReturn.get(i).get("database_id").toString(), i);
+		}
+		
+		List<String> toReturnDBIds = toReturn.stream()
+				.map(s -> s.get("database_id").toString())
+				.collect(Collectors.toList());
+		
+		addEngineMetadata(toReturn, toReturnDBIds, index);
+
+		if (Utility.isUserTrackingEnabled()) {
+			addUserTrackingData(toReturn, toReturnDBIds, index);
+		}
+		
+		return new NounMetadata(toReturn, PixelDataType.CUSTOM_DATA_STRUCTURE);
+	}
+
+	/**
+	 * 	
+	 * @param toReturn
+	 * @param toReturnDBIds
+	 * @param index
+	 */
+	private void addUserTrackingData(List<Map<String, Object>> toReturn, List<String> toReturnDBIds, Map<String, Integer> index) {
+		IRawSelectWrapper wrapper = null;
+		try {
+			wrapper = UserCatalogVoteUtils.getAllVotesWrapper(toReturnDBIds);
+			while(wrapper.hasNext()) {
+				Object[] data = wrapper.next().getValues();
+				String databaseId = (String) data[0];
+				int upvotes = ((Number) data[1]).intValue();
+
+				int indexToFind = index.get(databaseId);
+				Map<String, Object> res = toReturn.get(indexToFind);
+				res.put("upvotes", upvotes);
+			}
+		} catch (Exception e) {
+			classLogger.error(Constants.STACKTRACE, e);
+		} finally {
+			if(wrapper!=null) {
+				try {
+					wrapper.close();
+				} catch (IOException e) {
+					classLogger.error(Constants.STACKTRACE, e);
+				}
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @param dbInfo
+	 * @param toReturnDBIds
+	 * @param index
+	 * @return
+	 */
+	private IRawSelectWrapper addEngineMetadata(List<Map<String, Object>> dbInfo, List<String> toReturnDBIds, Map<String, Integer> index) {
+		IRawSelectWrapper wrapper = null;
+		try {
+			List<String> keys = new ArrayList<>();
+			keys.add("markdown");
+			keys.add("description");
+			keys.add("tag");
+			keys.add("domain");
+			
+			wrapper = SecurityEngineUtils.getEngineMetadataWrapper(toReturnDBIds, keys, true);
+			while(wrapper.hasNext()) {
+				Object[] data = wrapper.next().getValues();
+				String databaseId = (String) data[0];
+
+				String metaKey = (String) data[1];
+				String metaValue = (String) data[2];
+				if(metaValue == null) {
+					continue;
+				}
+
+				int indexToFind = index.get(databaseId);
+				Map<String, Object> res = dbInfo.get(indexToFind);
+				// whatever it is, if it is single send a single value, if it is multi send as array
+				if(res.containsKey(metaKey)) {
+					Object obj = res.get(metaKey);
+					if(obj instanceof List) {
+						((List) obj).add(metaValue);
+					} else {
+						List<Object> newList = new ArrayList<>();
+						newList.add(obj);
+						newList.add(metaValue);
+						res.put(metaKey, newList);
+					}
+				} else {
+					res.put(metaKey, metaValue);
+				}
+			}
+		} catch (Exception e) {
+			classLogger.error(Constants.STACKTRACE, e);
+		} finally {
+			if(wrapper!=null) {
+				try {
+					wrapper.close();
+				} catch (IOException e) {
+					classLogger.error(Constants.STACKTRACE, e);
+				}
+			}
+		}
+		return wrapper;
 	}
 
 }
