142ac247f64f87adb9cbdbe7d0d0d77e30a2651e
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-09-14 01:24:38 +0000

    Taking out max_new_tokens
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49492 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c32bb76e66347dae296af96592b17a1c8f50b2dc

--- py/orca_interrogator.py
+++ py/orca_interrogator.py
@@ -18,7 +18,7 @@ class Interrogator(gi.Interrogator):
         prompt = f"### System:\n{system}\n\n### User:\n{question}\n\n### Response:\n"
     console_streamer = SemossStreamer(tokenizer=self.tokenizer, skip_prompt=True)
     console_streamer.set_output_prefix(prefix)
-    super().ask(prompt, streamer=console_streamer, max_new_tokens=1000, **kwargs)
+    super().ask(prompt, streamer=console_streamer, **kwargs)
     output = console_streamer.complete_output
     return output
   
