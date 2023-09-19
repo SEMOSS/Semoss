558f8ffbe610a7a5800e9e5456f2f742e7bfb2d6
mahkhalil <mahkhalil@deloitte.com>
2023-09-19 17:56:33 +0000

    UPDATE ::: Allow optional encode parameter
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49537 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/export/WriteObjectToFileReactor.java
+++ src/prerna/sablecc2/reactor/export/WriteObjectToFileReactor.java
@@ -14,6 +14,7 @@ import prerna.sablecc2.om.execptions.SemossPixelException;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
 import prerna.util.Constants;
+import prerna.util.Utility;
 import prerna.util.upload.UploadInputUtility;
 
 public class WriteObjectToFileReactor extends AbstractReactor {
@@ -21,14 +22,16 @@ public class WriteObjectToFileReactor extends AbstractReactor {
 	private static final Logger classLogger = LogManager.getLogger(WriteObjectToFileReactor.class);
 	
 	public WriteObjectToFileReactor() {
-		this.keysToGet = new String[] {ReactorKeysEnum.VALUE.getKey(), ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.SPACE.getKey()};
+		this.keysToGet = new String[] {ReactorKeysEnum.VALUE.getKey(), ReactorKeysEnum.ENCODED.getKey(), ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.SPACE.getKey()};
 	}
 	
 	@Override
 	public NounMetadata execute() {
 		String filePath = UploadInputUtility.getFilePath(this.store, this.insight);
 		Object value = getObject();
-		
+		if(isEncoded()) {
+			value = Utility.decodeURIComponent(value+"");
+		}
 		File f = new File(filePath);
 		try {
 			FileUtils.writeStringToFile(f, value.toString(), "UTF-8");
@@ -56,4 +59,17 @@ public class WriteObjectToFileReactor extends AbstractReactor {
 		throw new NullPointerException("Must define the object to write to file");
 	}
 
+	/**
+	 * 
+	 * @return
+	 */
+	private boolean isEncoded() {
+		GenRowStruct grs = this.store.getNoun(ReactorKeysEnum.ENCODED.getKey());
+		if(grs != null && !grs.isEmpty()) {
+			return (Boolean) grs.get(0);
+		}
+		
+		return false;
+	}
+	
 }
