6a591dd0e8540e46bbd3b7d31d2db008e532c940
mahkhalil <mahkhalil@deloitte.com>
2023-09-26 15:36:26 +0000

    UPDATE ::: Missing places where we dont set the engineId in a temp OWL database being created UPDATE ::: Adding new reactor to push a local semoss instance to a cloud - primarily used for testing UPDATE ::: Pushing correct OWL for ModelInferenceLogsDatabase
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49597 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: abe1f97ff66bb07bc945ac19884877d44a145a4b

---
+++ src/prerna/cluster/util/AdminPushLocalToCloudReactor.java
@@ -0,0 +1,113 @@
+package prerna.cluster.util;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import prerna.auth.utils.SecurityAdminUtils;
+import prerna.auth.utils.SecurityEngineUtils;
+import prerna.auth.utils.SecurityProjectUtils;
+import prerna.cluster.util.clients.CentralCloudStorage;
+import prerna.engine.api.IEngine;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Constants;
+
+public class AdminPushLocalToCloudReactor extends AbstractReactor{
+
+	private static final Logger classLogger = LogManager.getLogger(AdminPushLocalToCloudReactor.class);
+
+	
+	public AdminPushLocalToCloudReactor() {
+		this.keysToGet = new String[]{ReactorKeysEnum.DRY_RUN.getKey()};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		boolean isAdmin = SecurityAdminUtils.userIsAdmin(this.insight.getUser());
+		if(!isAdmin) {
+			throw new IllegalArgumentException("User must be an admin for this operation!");
+		}
+		
+		organizeKeys();
+		boolean dryRun = true;
+		String dryRunString = this.keyValue.get(ReactorKeysEnum.DRY_RUN.getKey());
+		if (dryRunString != null && !dryRunString.isEmpty() && dryRunString.equalsIgnoreCase("false")) {
+			dryRun = false;
+		}
+		
+		Map<String, Object> pushedChanges = new HashMap<String, Object>();
+		pushedChanges.put("dryRun", dryRun);
+		
+		// get all engines
+		List<String> dbIds = SecurityEngineUtils.getAllEngineIds(Arrays.asList(IEngine.CATALOG_TYPE.DATABASE.name()));
+		List<String> storageIds = SecurityEngineUtils.getAllEngineIds(Arrays.asList(IEngine.CATALOG_TYPE.STORAGE.name()));
+		List<String> modelIds = SecurityEngineUtils.getAllEngineIds(Arrays.asList(IEngine.CATALOG_TYPE.MODEL.name()));
+		List<String> projectIds = SecurityProjectUtils.getAllProjectIds();
+
+		try {
+			CentralCloudStorage cc = CentralCloudStorage.getInstance();
+			Map<String, List<String>> currentContainers = cc.listAllContainersByBucket();
+			// push the current files
+			for(String key : currentContainers.keySet()) {
+				pushedChanges.put(key, currentContainers.get(key));
+			}
+			
+			removeExisitngIds(CentralCloudStorage.DB_BLOB, currentContainers.get(CentralCloudStorage.DB_BLOB), dbIds);
+			removeExisitngIds(CentralCloudStorage.STORAGE_BLOB, currentContainers.get(CentralCloudStorage.STORAGE_BLOB), storageIds);
+			removeExisitngIds(CentralCloudStorage.MODEL_BLOB, currentContainers.get(CentralCloudStorage.MODEL_BLOB), modelIds);
+			removeExisitngIds(CentralCloudStorage.PROJECT_BLOB, currentContainers.get(CentralCloudStorage.PROJECT_BLOB), projectIds);
+			
+			pushedChanges.put("added_dbIds", dbIds);
+			pushedChanges.put("added_storageIds", storageIds);
+			pushedChanges.put("added_modelIds", modelIds);
+			pushedChanges.put("added_projectIds", projectIds);
+			if(!dryRun) {
+				for(String db : dbIds) {
+					cc.pushEngine(db);
+				}
+				for(String storage : storageIds) {
+					cc.pushEngine(storage);
+				}
+				for(String model : modelIds) {
+					cc.pushEngine(model);
+				}
+				for(String project : projectIds) {
+					cc.pushProject(project);
+				}
+			}
+		} catch (Exception e) {
+			classLogger.error(Constants.STACKTRACE, e);
+		}
+		return new NounMetadata(pushedChanges, PixelDataType.MAP);
+	}
+	
+	/**
+	 * 
+	 * @param cloudFiles
+	 * @param startingList
+	 */
+	private void removeExisitngIds(String bucket, List<String> cloudFiles, List<String> startingList) {
+		for(String cloudF : cloudFiles) {
+			if(!cloudF.endsWith("/")) {
+				classLogger.warn("For " + bucket + " there is a file that is not folder = " + cloudF);
+				continue;
+			}
+			// smss folders end with / from list
+			if(cloudF.endsWith(CentralCloudStorage.SMSS_POSTFIX+"/")) {
+				continue;
+			}
+			
+			cloudF = cloudF.substring(0, cloudF.length()-1);
+			// remove it from the list of we have that do not sit in cloud
+			startingList.remove(cloudF);
+		}
+	}
+
+}
