0c41edb89efedbec9b047b75901e35cb78255418
ttrankle <ttrankle@deloitte.com>
2023-09-27 12:39:37 +0000

    NEW ::: enum for IVectorDatabaseEngine
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49604 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: b788e446c367eab66297966aa9b8af988feb630c

---
+++ src/prerna/engine/api/VectorDatabaseTypeEnum.java
@@ -0,0 +1,45 @@
+package prerna.engine.api;
+
+public enum VectorDatabaseTypeEnum {
+
+	FAISS("FAISS","prerna.engine.impl.model.EmbeddedModelEngine");
+	
+	private String vectorDbName;
+	private String vectorDbClass;
+	
+	VectorDatabaseTypeEnum(String storageName, String storageClass) {
+		this.vectorDbName = storageName;
+		this.vectorDbClass = storageClass;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getVectorDatabaseClass() {
+		return this.vectorDbClass;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getVectorDatabaseName() {
+		return this.vectorDbName;
+	}
+	
+	/**
+	 * 
+	 * @param name
+	 * @return
+	 */
+	public static VectorDatabaseTypeEnum getEnumFromName(String name) {
+		VectorDatabaseTypeEnum[] allValues = values();
+		for(VectorDatabaseTypeEnum v : allValues) {
+			if(v.getVectorDatabaseName().equalsIgnoreCase(name)) {
+				return v;
+			}
+		}
+		throw new IllegalArgumentException("Invalid input for name " + name);
+	}
+}
