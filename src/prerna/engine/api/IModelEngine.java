3c606aa975305b23a87b37a63b4646c1598a745c
ttrankle <ttrankle@deloitte.com>
2023-09-27 15:22:21 +0000

    UPDATE ::: Adding descriptions for the interface methods
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49609 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/engine/api/IModelEngine.java
+++ src/prerna/engine/api/IModelEngine.java
@@ -22,37 +22,40 @@ public interface IModelEngine extends IEngine {
 	// StopModel
 	
 	/**
-	 * 
-	 * @return
-	 */
+	 * Gets the type of the model inference engine.  The model engine type is often used to determine what client to use while running questions
+	 * @return the type of the database 
+	*/
 	ModelTypeEnum getModelType();
 	
-	//TODO change this to startModel() 
 	/**
-	 * 
-	 */
+	 * This initializes the model engine client based on the defined INIT SCRIPT. The process will remain alive for 15 minutes before
+	 * automatically shutting down. The client itself will be used as the gateway for all users inferencing with the model engine
+	 * @return
+	*/
 	void startServer();
 	
-	// need to change this to model client
-	// I dont know if I should give this or just have a ask
-	//public PyTranslator getClient();
-	
 	/**
+	 * Passes the string question along with other parameters such as context and temperature to the python client and 
 	 * 
-	 * @param question
-	 * @param context
-	 * @param insight
-	 * @param parameters
-	 * @return
+	 * @param question 		The question being asked to the LLM
+	 * @param context		(Optional) The context passed in by the user 
+	 * @param insight		The insight from where the call is being made. The insight holds user credentials, project information and conversation history tied to the insightId
+	 * @param parameters	Additional parameters such as temperature, top_k, max_new_tokens etc
+	 * @return 	creates a map response with the following keys
+	 * 				- response : The actual string response from the LLM/model
+	 *  			- messageId : The unique identifier of a message (the user's input and the model response)
+	 *  			- roomId: The insightId that the runPixel endpoint is being called from
 	 */
 	Map<String, String> ask(String question, String context, Insight insight, Map <String, Object> parameters);
 	
+	// TODO update embeddings to handle a List of strings and remove unnecessary params
 	/**
+	 * Passes a string question to the model client to be encoded as a vector
 	 * 
-	 * @param question
-	 * @param insight
-	 * @param parameters
-	 * @return
+	 * @param question		The string that needs to be encoded
+	 * @param insight		The insight from where the call is being made. The insight holds user credentials, project information and conversation history tied to the insightId
+	 * @param parameters	Additional parameters such as temperature, top_k, max_new_tokens etc
+	 * @return	an encoded object based on how the LLM encodes strings
 	 */
 	Object embeddings(String question, Insight insight, Map <String, Object> parameters);
 	
