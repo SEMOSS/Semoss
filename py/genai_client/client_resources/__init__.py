deb09a7d4ae772a3f0065e43e8a2620a777f9af0
ttrankle <ttrankle@deloitte.com>
2023-09-28 12:46:51 +0000

    BUGFIX ::: remove unnecessary classes
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49623 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- py/genai_client/client_resources/__init__.py
+++ py/genai_client/client_resources/__init__.py
@@ -1,3 +1,2 @@
 from genai_client.client_resources.gaas_client_openai import OpenAiClient
-from genai_client.client_resources.gaas_client_fastchat import FastChatClient
 from genai_client.client_resources.gaas_client_textgen import TextGenClient
