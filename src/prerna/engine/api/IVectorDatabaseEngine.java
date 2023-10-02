6fd1da76e8e631194b92b1d1be57bfe19f174a9f
ttrankle <ttrankle@deloitte.com>
2023-10-02 12:01:59 +0000

    UPDATE ::: enable FAISS database to add and remove documents
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49647 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 51cf8a9c4e5767f5e469f7e48e023c7bfe71090b

--- src/prerna/engine/api/IVectorDatabaseEngine.java
+++ src/prerna/engine/api/IVectorDatabaseEngine.java
@@ -1,9 +1,16 @@
 package prerna.engine.api;
 
+import java.io.File;
 import java.util.List;
+import java.util.Map;
 
 public interface IVectorDatabaseEngine extends IEngine {
 
+	
+	// this is what the FE sends for the type of storage we are creating
+	// as a result, cannot be a key in the smss file
+	String VECTOR_TYPE = "VECTOR_TYPE";
+		
 	/**
 	 * Gets the type of the model inference engine.  The model engine type is often used to determine what client to use while running questions
 	 * @return the type of the database 
@@ -13,9 +20,9 @@ public interface IVectorDatabaseEngine extends IEngine {
 	// i vector db
 	// open close nearest neighbor, add document - remove document. question
 	
-	void addDocumet(List<String> fileNames) throws Exception;
+	void addDocumet(List<String> filePaths, Map <String, Object> parameters);
 	
-	void removeDocument(List<String> fileNames) throws Exception;
+	void removeDocument(List<String> filePaths, Map <String, Object> parameters);
 	
-	Object nearestNeighbor(String question, String limit);
+	Object nearestNeighbor(String question, int limit, Map <String, Object> parameters);
 }
