54cd5a66025fca97bebef703924753d529402436
ttrankle <ttrankle@deloitte.com>
2023-10-02 12:01:59 +0000

    UPDATE ::: enable FAISS database to add and remove documents
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49647 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/engine/api/VectorDatabaseTypeEnum.java
+++ src/prerna/engine/api/VectorDatabaseTypeEnum.java
@@ -1,15 +1,18 @@
 package prerna.engine.api;
 
+import prerna.util.sql.RdbmsTypeEnum;
+
 public enum VectorDatabaseTypeEnum {
 
 	FAISS("FAISS","prerna.engine.impl.vector.FaissDatabaseEngine");
 	
 	private String vectorDbName;
 	private String vectorDbClass;
+	private String vectorDbInitScript;
 	
-	VectorDatabaseTypeEnum(String storageName, String storageClass) {
-		this.vectorDbName = storageName;
-		this.vectorDbClass = storageClass;
+	VectorDatabaseTypeEnum(String vectorDbName, String vectorDbClass) {
+		this.vectorDbName = vectorDbName;
+		this.vectorDbClass = vectorDbClass;
 	}
 	
 	/**
@@ -41,5 +44,19 @@ public enum VectorDatabaseTypeEnum {
 			}
 		}
 		throw new IllegalArgumentException("Invalid input for name " + name);
+	}
+	
+	/**
+	 * Get the enum from the driver
+	 * @param driver
+	 * @return
+	 */
+	public static VectorDatabaseTypeEnum getEnumFromClass(String vectorDbClass) {
+		for(VectorDatabaseTypeEnum v : VectorDatabaseTypeEnum.values()) {
+			if(vectorDbClass.equalsIgnoreCase(v.vectorDbClass)) {
+				return v;
+			}
+		}
+		return null;
 	}
 }
--- src/prerna/engine/impl/vector/FaissDatabaseUtils.java
+++ src/prerna/engine/impl/vector/FaissDatabaseUtils.java
@@ -1,13 +1,16 @@
 package prerna.engine.impl.vector;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.lang.SystemUtils;
 import org.apache.logging.log4j.LogManager;
@@ -21,7 +24,7 @@ import prerna.sablecc2.reactor.frame.gaas.processors.PPTProcessor;
 public class FaissDatabaseUtils {
 	private static final Logger classLogger = LogManager.getLogger(FaissDatabaseUtils.class);
 	
-	public static void convertFilesToCSV(String csvFileName, int contentLength, int contentOverlap, List <String> fileNames) throws IOException {
+	public static void convertFilesToCSV(String csvFileName, int contentLength, int contentOverlap, File file) throws IOException {
 		CSVWriter writer = new CSVWriter(csvFileName);
 		writer.setTokenLength(contentLength);
 		writer.overlapLength(contentOverlap);
@@ -30,50 +33,111 @@ public class FaissDatabaseUtils {
 		List <String> processedList = new ArrayList<String>();
 
 		// pick up the files and convert them to CSV
-		for(int fileIndex = 0; fileIndex < fileNames.size(); fileIndex++) {
-			String thisFile = fileNames.get(fileIndex);
-			classLogger.info("Processing file : " + thisFile);
+		
 
-			String fileLocation = thisFile;		
-			Path filePath = Paths.get(fileLocation);
-			// process this file
-			String mimeType = null;
-			if (SystemUtils.IS_OS_MAC) {
-			     mimeType = URLConnection.guessContentTypeFromName(filePath.toFile().getName());
+		classLogger.info("Processing file : " + file.getName());
+		
+		Path filePath = Paths.get(file.getAbsolutePath());
+		// process this file
+		String mimeType = null;
+		if (SystemUtils.IS_OS_MAC) {
+		     mimeType = URLConnection.guessContentTypeFromName(filePath.toFile().getName());
 
-			} else {
-				 mimeType = Files.probeContentType(filePath);
-			}
-			if(mimeType != null) {
-				if(mimeType.equalsIgnoreCase("application/vnd.openxmlformats-officedocument.wordprocessingml.document"))
-				{
-					// document
-					DocProcessor dp = new DocProcessor(fileLocation, writer);
-					dp.process();
-					processedList.add(thisFile);
-				}
-				else if(mimeType.equalsIgnoreCase("application/vnd.openxmlformats-officedocument.presentationml.presentation"))
-				{
-					// powerpoint
-					PPTProcessor pp = new PPTProcessor(fileLocation, writer);
-					pp.process();
-					processedList.add(thisFile);
-				}
-				else if(mimeType.equalsIgnoreCase("application/pdf"))
-				{
-					PDFProcessor pdf = new PDFProcessor(fileLocation, writer);
-					pdf.process();
-					processedList.add(thisFile);
-				}
-				else
-				{
-					classLogger.warn("We Currently do not support mime-type " + thisFile);
-				}
-				classLogger.info("Completed Processing file : " + thisFile);
-			}
+		} else {
+			 mimeType = Files.probeContentType(filePath);
 		}
+		if(mimeType != null) {
+			if(mimeType.equalsIgnoreCase("application/vnd.openxmlformats-officedocument.wordprocessingml.document"))
+			{
+				// document
+				DocProcessor dp = new DocProcessor(file.getAbsolutePath(), writer);
+				dp.process();
+				processedList.add(file.getAbsolutePath());
+			}
+			else if(mimeType.equalsIgnoreCase("application/vnd.openxmlformats-officedocument.presentationml.presentation"))
+			{
+				// powerpoint
+				PPTProcessor pp = new PPTProcessor(file.getAbsolutePath(), writer);
+				pp.process();
+				processedList.add(file.getAbsolutePath());
+			}
+			else if(mimeType.equalsIgnoreCase("application/pdf"))
+			{
+				PDFProcessor pdf = new PDFProcessor(file.getAbsolutePath(), writer);
+				pdf.process();
+				processedList.add(file.getAbsolutePath());
+			}
+			else
+			{
+				classLogger.warn("We Currently do not support mime-type " + file.getAbsolutePath());
+			}
+			classLogger.info("Completed Processing file : " + file.getAbsolutePath());
+		
+		}
+		writer.close();
 	}
 	
+	public static boolean verifyFileTypes(List<String> newFilesPaths, List<String> filesInDocumentsFolder) {
+		
+		/*
+		 * First Check
+		 * Make sure the csv files aren't send with non csv files
+		 * TODO refine checks here
+		*/
+		Set<String> newFileTypes = extractFileTypesFromPaths(newFilesPaths);
+        boolean newFilesAreCsv;
+        if (newFileTypes.contains("csv") && newFileTypes.size() == 1) {
+        	newFilesAreCsv = true;
+        } else {
+        	newFilesAreCsv = false;
+        }
+		
+        // TODO update this to do a headers check. Maybe the can do the pre-processing beforehand
+        // cant process csvs unless the have the same headers
+        if (newFileTypes.contains("csv") && !newFilesAreCsv) {
+        	return false;
+        }
+        
+		/*
+		 * Second Check
+		 * Make sure we arent trying to add csv to a mixed file type class 
+		 * TODO refine checks here once they above is addressed
+		*/
+        Set<String> currentFileTypes = extractFileTypesFromPaths(filesInDocumentsFolder);
+        
+        if (currentFileTypes.size() == 0) {
+        	return true;
+        }
+        
+        
+        if (newFilesAreCsv && !currentFileTypes.contains("csv")) {
+        	return false;
+        }
+        
+		/*
+		 * Third Check
+		 * Make sure we are not adding mixed files to just csv class
+		 * TODO refine checks here once they above is addressed
+		*/
+        if (!newFilesAreCsv && currentFileTypes.contains("csv") && currentFileTypes.size() == 1) {
+        	return false;
+        }
+		
+		return true;
+	}
 	
-	
+	public static Set<String> extractFileTypesFromPaths(List<String> filePaths) {
+        Set<String> fileTypes = new HashSet<>();
+        for (String filePath : filePaths) {
+            // Find the last dot (.) in the file path
+            int lastDotIndex = filePath.lastIndexOf(".");
+            
+            if (lastDotIndex >= 0) {
+                // Extract the file extension
+                String fileType = filePath.substring(lastDotIndex + 1).toLowerCase();
+                fileTypes.add(fileType);
+            }
+        }
+        return fileTypes;
+    }
 }
