7cddbdfa428a8447bec84b03b8a984785d500c70
ttrankle <ttrankle@deloitte.com>
2023-10-02 12:21:28 +0000

    UPDATE ::: clean up imports
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49650 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/sablecc2/reactor/vector/CreateEmbeddingsFromDocumentsReactor.java
+++ src/prerna/sablecc2/reactor/vector/CreateEmbeddingsFromDocumentsReactor.java
@@ -23,7 +23,6 @@ import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.execptions.SemossPixelException;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
-import prerna.util.AssetUtility;
 import prerna.util.Constants;
 import prerna.util.Utility;
 import prerna.util.ZipUtils;
