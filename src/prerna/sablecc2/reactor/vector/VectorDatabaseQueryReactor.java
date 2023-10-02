690e1614810ff518fca51ebbacbf77e67aae91cd
ttrankle <ttrankle@deloitte.com>
2023-10-02 12:15:35 +0000

    NEW ::: initial reactors to create, add, remove and search a vector database
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49649 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 028ce874cf5cef810fc55c811ccef36ec772f694

---
+++ src/prerna/sablecc2/reactor/vector/VectorDatabaseQueryReactor.java
@@ -0,0 +1,89 @@
+package prerna.sablecc2.reactor.vector;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import prerna.auth.utils.SecurityEngineUtils;
+import prerna.engine.api.IVectorDatabaseEngine;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.execptions.SemossPixelException;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.sablecc2.reactor.AbstractReactor;
+import prerna.util.Utility;
+
+public class VectorDatabaseQueryReactor extends AbstractReactor {
+
+	public VectorDatabaseQueryReactor() {
+		this.keysToGet = new String[] {
+				ReactorKeysEnum.ENGINE.getKey(),
+				ReactorKeysEnum.COMMAND.getKey(), 
+				ReactorKeysEnum.LIMIT.getKey(),
+				ReactorKeysEnum.PARAM_VALUES_MAP.getKey()
+		};
+		this.keyRequired = new int[] {1, 1, 0, 0};
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String engineId = this.keyValue.get(this.keysToGet[0]);
+		if(!SecurityEngineUtils.userCanViewEngine(this.insight.getUser(), engineId)) {
+			throw new IllegalArgumentException("Vector db " + engineId + " does not exist or user does not have access to this model");
+		}
+		
+		
+		String question = Utility.decodeURIComponent(this.keyValue.get(this.keysToGet[1]));
+		int limit = getLimit();
+
+		
+		Map<String, Object> paramMap = getMap();
+		if(paramMap == null) {
+			paramMap = new HashMap<String, Object>();
+		}
+		
+		IVectorDatabaseEngine eng = Utility.getVectorDatabase(engineId);
+
+		if (eng == null) {
+			throw new SemossPixelException("Unable to find engine");
+		}
+		Object output = eng.nearestNeighbor(question, limit, paramMap);
+		return new NounMetadata(output, PixelDataType.CUSTOM_DATA_STRUCTURE, PixelOperationType.OPERATION);	
+	}
+	
+	private Map<String, Object> getMap() {
+        GenRowStruct mapGrs = this.store.getNoun(keysToGet[3]);
+        if(mapGrs != null && !mapGrs.isEmpty()) {
+            List<NounMetadata> mapInputs = mapGrs.getNounsOfType(PixelDataType.MAP);
+            if(mapInputs != null && !mapInputs.isEmpty()) {
+                return (Map<String, Object>) mapInputs.get(0).getValue();
+            }
+        }
+        List<NounMetadata> mapInputs = this.curRow.getNounsOfType(PixelDataType.MAP);
+        if(mapInputs != null && !mapInputs.isEmpty()) {
+            return (Map<String, Object>) mapInputs.get(0).getValue();
+        }
+        return null;
+    }
+	
+	//returns how much do we need to collect
+	private int getLimit() {
+		// try the key
+		GenRowStruct numGrs = store.getNoun(keysToGet[2]);
+		if(numGrs != null && !numGrs.isEmpty()) {
+			return ((Number) numGrs.get(0)).intValue();
+		}
+		
+		// try the cur row
+		List<Object> allNumericInputs = this.curRow.getAllNumericColumns();
+		if(allNumericInputs != null && !allNumericInputs.isEmpty()) {
+			return ((Number) allNumericInputs.get(0)).intValue();
+		}
+		
+		// default to 500
+		return 5;
+	}
+}
