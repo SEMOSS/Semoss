30bded4a259f5952305e374db28388a320713aec
mahkhalil <mahkhalil@deloitte.com>
2023-10-04 12:42:19 +0000

    UPDATE ::: Adding metadata definitions for RESTFunctionEngine
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49671 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: cc0a35c9bdefb5afd8a076fcfec595da52b07127

--- src/prerna/engine/api/IFunctionEngine.java
+++ src/prerna/engine/api/IFunctionEngine.java
@@ -1,12 +1,69 @@
 package prerna.engine.api;
 
+import java.util.List;
+import java.util.Map;
+
+import prerna.engine.impl.function.FunctionParameter;
+
 public interface IFunctionEngine extends IEngine {
+
+	String NAME_KEY = "FUNCTION_NAME";
+	String DESCRIPTION_KEY = "FUNCTION_DESCRIPTION";
+	String PARAMETER_KEY = "FUNCTION_PARAMETERS";
+	String REQUIRED_PARAMETER_KEY = "FUNCTION_REQUIRED_PARAMETERS";
 
 	/**
 	 * 
 	 * @param args
 	 * @return
 	 */
-	Object execute(Object[] args);
+	Object execute(Map<String, Object> parameterValues);
+	
+	/**
+	 * Unique name of the function
+	 * @return
+	 */
+	String getFunctionName();
+	
+	/**
+	 * 
+	 */
+	void setFunctionName(String functionName);
+	
+	/**
+	 * Description of what this function does
+	 * @return
+	 */
+	String getFunctionDescription();
+	
+	/**
+	 * 
+	 * @param description
+	 */
+	void setFunctionDescription(String description);
+	
+	/**
+	 * 
+	 * @return
+	 */
+	List<FunctionParameter> getParameters();
+	
+	/**
+	 * 
+	 * @param parameters
+	 */
+	void setParameters(List<FunctionParameter> parameters);
+	
+	/**
+	 * 
+	 * @return
+	 */
+	List<String> getRequiredParameters();
+	
+	/**
+	 * 
+	 * @param requiredParameters
+	 */
+	void setRequiredParameters(List<String> requiredParameters);
 	
 }
