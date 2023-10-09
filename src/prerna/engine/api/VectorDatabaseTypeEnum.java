22842c621e1a13fdc4444ac4bf090c760091afc1
ttrankle <ttrankle@deloitte.com>
2023-10-09 23:38:16 +0000

    UPDATE ::: define param options for FAISS Vector Database
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49702 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/engine/api/VectorDatabaseTypeEnum.java
+++ src/prerna/engine/api/VectorDatabaseTypeEnum.java
@@ -3,9 +3,10 @@ package prerna.engine.api;
 public enum VectorDatabaseTypeEnum {
 
 	FAISS("FAISS","prerna.engine.impl.vector.FaissDatabaseEngine");
-	
+		
 	private String vectorDbName;
 	private String vectorDbClass;
+
 	
 	VectorDatabaseTypeEnum(String vectorDbName, String vectorDbClass) {
 		this.vectorDbName = vectorDbName;
@@ -56,4 +57,36 @@ public enum VectorDatabaseTypeEnum {
 		}
 		return null;
 	}
+	
+	public enum ParamValueOptions {
+		COLUMNS_TO_INDEX("columnsToIndex",		"A list of column names in the data you want to create the embeddings from"),
+		COLUMNS_TO_REMOVE("columnsToRemove",	"A list of column names in the data that you dont want to store in the database"),
+		COLUMNS_TO_RETURN("columnsToReturn",	"A list of column names in the data you want returned in the response"),
+		RETURN_THRESHOLD("returnThreshold", 	"The minimun threshold every resoinse should be under"),
+		ASCENDING("ascending", 					"Boolean flag to sort the responses by in ascending order");
+
+        private final String key;
+        private final String description;
+        
+        
+        ParamValueOptions(String key, String description) {
+        	this.key = key;
+            this.description = description;
+        }
+
+        public String getKey() {
+    		return this.key;
+    	}
+        public static String getDescriptionFromKey(String key) {
+    		for(ParamValueOptions e : ParamValueOptions.values()) {
+    			if(e.key.equals(key)) {
+    				return e.description;
+    			}
+    		}
+    		// if we cannot find the description above
+    		// it is not a standardized key
+    		// so just return null
+    		return null;
+    	}
+    }
 }
