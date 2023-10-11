d42b006506491d7eba47339babef4ce860bfa26b
mahkhalil <mahkhalil@deloitte.com>
2023-10-11 23:11:52 +0000

    UPDATE ::: Instead of throwing error - returning map with text saying documentation pending
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49716 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: c37c30dbaff8738506c319e1d11c91a21c17b41c

--- src/prerna/sablecc2/reactor/utils/GetEngineUsageReactor.java
+++ src/prerna/sablecc2/reactor/utils/GetEngineUsageReactor.java
@@ -5,22 +5,16 @@ import java.util.Map;
 
 import prerna.auth.utils.SecurityEngineUtils;
 import prerna.engine.api.IEngine;
-import prerna.engine.impl.model.LLMReactor;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
-import prerna.sablecc2.om.execptions.SemossPixelException;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.reactor.AbstractReactor;
-import prerna.sablecc2.reactor.qs.source.DatabaseReactor;
-import prerna.sablecc2.reactor.storage.StorageReactor;
 
 public class GetEngineUsageReactor extends AbstractReactor {
 
 	private static final String PYTHON = "python";
 	private static final String JAVA = "java";
 	private static final String PIXEL = "pixel";
-	
-	AbstractReactor engineAbstractReactor;
 	
 	public GetEngineUsageReactor() {
 		this.keysToGet = new String[]{ReactorKeysEnum.ENGINE.getKey()};
@@ -46,15 +40,17 @@ public class GetEngineUsageReactor extends AbstractReactor {
 				outputMap = getModelUsage(engineId);
 				break;
 			default:
-				throw new SemossPixelException("Engine Type is undefined");
+				outputMap = new HashMap<>();
+				outputMap.put(PYTHON, "Documentation pending");
+				outputMap.put(JAVA, "Documentation pending");
+				outputMap.put(PIXEL, "Documentation pending");
+				break;
 		}
 		return new NounMetadata(outputMap, PixelDataType.MAP);
 	}
 	
 	private Map<String, String> getModelUsage(String engineId) {
-		this.engineAbstractReactor = new LLMReactor();
-		
-		Map<String, String> usageMap = new HashMap<String, String>();
+		Map<String, String> usageMap = new HashMap<>();
 		usageMap.put(PYTHON,"from gaas_gpt_model import ModelEngine\r\n" + 
 				"question = 'Sample Question'\r\n" +
 				"model = ModelEngine(engine_id = \""+engineId+"\", insight_id = '${i}')\r\n" +
@@ -62,15 +58,13 @@ public class GetEngineUsageReactor extends AbstractReactor {
 		usageMap.put(JAVA,"import prerna.util.Utility;\r\n" + 
 				"import prerna.engine.api.IModelEngine;\r\n" + 
 				"IModelEngine eng = Utility.getModel(\""+engineId+"\");");
-		
 		usageMap.put(PIXEL,"LLM(engine = \""+engineId+"\", command = \"Sample Question\", paramValues = [ {} ] );");
 		
 		return usageMap;
 	}
 	
 	private Map<String, String> getStorageUsage(String engineId) {
-		this.engineAbstractReactor = new StorageReactor();
-		Map<String, String> usageMap = new HashMap<String, String>();
+		Map<String, String> usageMap = new HashMap<>();
 		usageMap.put(PYTHON,"from gaas_gpt_storage import StorageEngine\r\n" + 
 				"storageEngine = StorageEngine(engine_id = \""+engineId+"\", insight_id = '${i}')\r\n" +
 				"storageEngine.list(path = '/your/path/')\r\n" + 
@@ -87,9 +81,7 @@ public class GetEngineUsageReactor extends AbstractReactor {
 	}
 	
 	private Map<String, String> getDatabaseUsage(String engineId) {
-		this.engineAbstractReactor = new DatabaseReactor();
-		
-		Map<String, String> usageMap = new HashMap<String, String>();
+		Map<String, String> usageMap = new HashMap<>();
 		usageMap.put(PYTHON,"from gaas_gpt_database import DatabaseEngine\r\n" + 
 				"databaseEngine = DatabaseEngine(engine_id = \""+engineId+"\", insight_id = '${i}')\r\n" +
 				"databaseEngine.execQuery(query = 'SELECT * FROM table_name')\r\n" + 
