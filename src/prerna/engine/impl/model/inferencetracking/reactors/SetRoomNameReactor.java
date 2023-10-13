380c1771bcbbc0c1dbcabdd98335052279b7fc38
mahkhalil <mahkhalil@deloitte.com>
2023-10-13 11:54:54 +0000

    UPDATE ::: Moving reactors out of sablecc package so lexar/parser logic is separated out
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49733 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 00aad195e1ec53b0d066326368c68e1323eec55b

--- src/prerna/engine/impl/model/inferencetracking/reactors/SetRoomNameReactor.java
+++ src/prerna/engine/impl/model/inferencetracking/reactors/SetRoomNameReactor.java
@@ -2,9 +2,9 @@ package prerna.engine.impl.model.inferencetracking.reactors;
 
 import prerna.auth.User;
 import prerna.engine.impl.model.inferencetracking.ModelInferenceLogsUtils;
+import prerna.reactor.AbstractReactor;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
-import prerna.sablecc2.reactor.AbstractReactor;
 
 public class SetRoomNameReactor extends AbstractReactor {
 	
