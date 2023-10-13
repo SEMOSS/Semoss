ef933fcd2487fb7d4d0324d51f112f4a9660d1de
mahkhalil <mahkhalil@deloitte.com>
2023-10-13 15:45:13 +0000

    UPDATE ::: Adding deprecated abstract reactor so external reactors still work (from extends AbstractReactor)
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49739 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 689b86350569cfa7f01d26fada8bc6305dfa58c5

---
+++ src/prerna/sablecc2/reactor/AbstractReactor.java
@@ -0,0 +1,9 @@
+package prerna.sablecc2.reactor;
+
+/**
+ * SWITCH TO USING {@link prerna.reactor.AbstractReactor}
+ */
+@Deprecated
+public abstract class AbstractReactor extends prerna.reactor.AbstractReactor {
+
+}
