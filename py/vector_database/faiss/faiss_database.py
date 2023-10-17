de4776c91041c5e3868600f093fcdc1874eb0df1
ttrankle <ttrankle@deloitte.com>
2023-10-17 22:22:29 +0000

    UPDATE ::: add documentation and type hinting to faiss_database.py
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49771 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: cf0cde758a546e78c72e36462630897e2ce2191a

--- py/vector_database/faiss/faiss_database.py
+++ py/vector_database/faiss/faiss_database.py
@@ -1,19 +1,57 @@
+from typing import List, Dict, Union, Optional, Any
 from .faiss_client import FAISSSearcher
-from ..encoders.huggingface_encoder import HuggingFaceEncoder
 
 class FAISSDatabase():
-    '''this the primary class for a faiss database'''
+    '''
+    This the primary class to store all the FAISSSearcher for a given faiss database
+    '''
 
-    def __init__(self, searchers:list = [], encoder_class = None):
+    def __init__(
+        self, 
+        searchers: list = [], 
+        encoder_class = None
+    ) -> None:
+        '''
+        Create an instance of FAISSDatabase
+        '''
+        # set the encoder class so it can be used when new searchers/indexClasses are added
         self.encoder_class = encoder_class
+
+        # register all the searchers passed in
         self.searchers = {searcher:FAISSSearcher(encoder_class = self.encoder_class) for searcher in searchers}
 
-    # I think thats all I need?
-    def create_searcher(self, searcher_name, **kwargs):
+    def create_searcher(
+        self, 
+        searcher_name: str, 
+        **kwargs: Any
+    ) -> None:
+        '''
+        Create a new searchers/indexClasses to which a set of documents will be added.
+
+        Args:
+            searcher_name(`str`):
+                The name of the searcher to be added.
+        
+        Returns:
+            `None`
+        '''
         if (searcher_name in self.searchers.keys()):
             raise ValueError("The searcher/table/class already exists")
         
         self.searchers[searcher_name] = FAISSSearcher(encoder_class = self.encoder_class, **kwargs)
 
-    def delete_searcher(self, searcher_name):
+    def delete_searcher(
+        self, 
+        searcher_name: str
+    ) -> None:
+        '''
+        Remove a searcher/indexClass from the the main database object
+
+        Args:
+            searcher_name(`str`):
+                The name of the searcher to be removed from memory after the files have been deleted
+        
+        Returns:
+            `None`
+        '''
         del self.searchers[searcher_name]
