5c75224b4d28baedf13097effa625b9c12f68492
kunalppatel9 <kunalppatel9@deloitte.com>
2023-10-18 16:00:53 +0000

    UPDATE ::: Adding bedrock engine
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@49775 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/engine/api/ModelTypeEnum.java
+++ src/prerna/engine/api/ModelTypeEnum.java
@@ -5,6 +5,7 @@ public enum ModelTypeEnum {
 	EMBEDDED("EMBEDDED","prerna.engine.impl.model.EmbeddedModelEngine"),
 	FAST_CHAT("FAST_CHAT","prerna.engine.impl.model.FastChatProcessModel"),
 	OPEN_AI("OPEN_AI","prerna.engine.impl.model.OpenAiEngine"),
+	BEDROCK("BEDROCK","prerna.engine.impl.model.BedrockEngine"),
 	TEXT_GENERATION("TEXT_GENERATION","prerna.engine.impl.model.TextGenerationEngine"),
 	REMOTE("REMOTE","prerna.engine.impl.remotesemoss.RemoteModelEngine");
 	
