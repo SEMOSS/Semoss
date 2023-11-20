ee0c7779b955c7e7003c14338a7148716061c824
ttrankle <ttrankle@deloitte.com>
2023-11-20 20:49:03 +0000

    UPDATE ::: switch faiss database content length/overlap to tokens instead of characters
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50001 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- py/genai_client/embedders/local_embedder.py
+++ py/genai_client/embedders/local_embedder.py
@@ -1,22 +1,38 @@
 from typing import Optional, Union, List, Dict, Any
 from sentence_transformers import SentenceTransformer, util
 from huggingface_hub import try_to_load_from_cache, _CACHED_NO_EXIST, snapshot_download, hf_hub_download
-from transformers import AutoTokenizer
+from transformers import AutoModel
 from pathlib import Path
+from ..tokenizers import HuggingfaceTokenizer
 
 class LocalEmbedder():
 
     def __init__(
         self,
-        model_path:str = None
+        model_name:str = None,
+        **kwargs
     ) -> None:
-        self.model_name = model_path
-        self.model_folder = self.get_physical_folder(repo_id = model_path)
-        self.encoder = SentenceTransformer(
+        # TODO - remove or options once existing local embedders have been changed
+        self.model_name = model_name or kwargs.get('model_path')
+        
+        assert self.model_name != None
+        
+        self.model_folder = self.get_physical_folder(repo_id = self.model_name)
+        self.embedder = self.get_embedder(
             self.model_folder
         )
-        self.max_tokens = self.encoder.max_seq_length
-        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+        self.tokenizer = HuggingfaceTokenizer(
+            encoder_name = model_name, 
+            max_tokens = kwargs.pop(
+                'max_tokens', 
+                None
+            ),
+            max_input_tokens = kwargs.pop(
+                'max_input_tokens', 
+                None
+            )
+        )
     
     def get_physical_folder(
         self, 
@@ -43,12 +59,29 @@ class LocalEmbedder():
                 return snapshot_download(repo_id)
             except:
                 # really dont want to have to do this
-                from transformers import AutoModel
                 AutoModel.from_pretrained(repo_id)
                 return try_to_load_from_cache(
                     repo_id=repo_id, 
                     filename='config.json'
                 )
+                
+    def get_embedder(
+        self,
+        model_folder:str
+    ):
+        embedder = None
+        try:
+            embedder = SentenceTransformer(
+                self.model_folder
+            )
+        except:
+            # trust_remote_code is needed to use the encode method
+            embedder = AutoModel.from_pretrained(
+                self.model_folder, 
+                trust_remote_code=True
+            )
+
+        return embedder
 
     def embeddings(
         self, 
@@ -59,12 +92,11 @@ class LocalEmbedder():
         assert isinstance(list_to_embed, list) or isinstance(object_to_encode, str)
         
 
-        embedded_tensor = self.encoder.encode(
+        embedded_tensor = self.embedder.encode(
             sentences = list_to_embed, 
-            show_progress_bar=True
         )
         
-        total_tokens = sum([len(self.tokenizer.encode(chunk)) for chunk in list_to_embed])
+        total_tokens = sum([self.tokenizer.count_tokens(chunk) for chunk in list_to_embed])
 
         # TODO find a way to push back batches like OpenAI
         # THIS IS SLOW AS HECK
@@ -72,7 +104,7 @@ class LocalEmbedder():
         # number_of_items_to_encode = len(list_to_embed)
         # for i in range(number_of_items_to_encode):
         #   embedded_list.append(
-        #     self.encoder.encode(list_to_embed[i])
+        #     self.embedder.encode(list_to_embed[i])
         #   )
         #   print(prefix + "Completed Embedding " + str(i) + "/" + str(number_of_items_to_encode) + " Chunks")
         
