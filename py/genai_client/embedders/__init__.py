d6c05ab8033c0ad592e4a3fd0230e77f168d847d
ttrankle <ttrankle@deloitte.com>
2023-11-29 17:14:26 +0000

    UPDATE ::: Adding python client for Text Embeddings Inference
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50026 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 62305daad30540d12a5488346f7e33ffc2835d87

--- py/genai_client/embedders/__init__.py
+++ py/genai_client/embedders/__init__.py
@@ -1,3 +1,4 @@
 from .local_embedder import LocalEmbedder
 from .openai_embedder import OpenAiEmbedder
 from .azure_openai_embedder import AzureOpenAiEmbedder
+from .textgen_embedder import TextEmbeddingsInference
