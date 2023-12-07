bb7b029eec8a2f07a525854ef84f71c6ecb23a57
mahkhalil <mahkhalil@deloitte.com>
2023-12-07 21:46:50 +0000

    UPDATE ::: Wrapping crash() in executor logic to ensure we dont wait / get stuck on synchronizing the payload struct
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50103 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 896b6d17ff2334ad86036f5e798c2feb632fe84c

--- src/prerna/tcp/client/SocketClient.java
+++ src/prerna/tcp/client/SocketClient.java
@@ -7,6 +7,13 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.logging.log4j.LogManager;
@@ -266,18 +273,41 @@ public class SocketClient implements Runnable, Closeable {
     	// make the connected to be false
     	// take everything that is waiting on it
     	// go through request map and start pushing
-    	try {
-	    	for(Object k : this.requestMap.keySet()) {
-	    		PayloadStruct ps = (PayloadStruct) this.requestMap.get(k);
-	    		classLogger.debug("Releasing <" + k + "> <" + ps.methodName + ">");
-	    		ps.ex = "Server has crashed. This happened because you exceeded the memory limits provided or performed an illegal operation. Please relook at your recipe";
-	    		synchronized(ps) {
-	    			ps.notifyAll();
-	    		}
-	    	}
-    	} catch(Exception e) {
-    		classLogger.error(Constants.STACKTRACE, e);
-    	}
+    	
+    	// run as executor since it is synchronized
+    	// and dont want to get stuck if an issue occurs and the notify never happens
+    	// we will close and kill process anyway
+    	ExecutorService executor = Executors.newSingleThreadExecutor();
+    	
+        Callable<String> callableTask = () -> {
+        	try {
+    	    	for(Object k : this.requestMap.keySet()) {
+    	    		PayloadStruct ps = (PayloadStruct) this.requestMap.get(k);
+    	    		classLogger.debug("Releasing <" + k + "> <" + ps.methodName + ">");
+    	    		ps.ex = "Server has crashed. This happened because you exceeded the memory limits provided or performed an illegal operation. Please relook at your recipe";
+    	    		synchronized(ps) {
+    	    			ps.notifyAll();
+    	    		}
+    	    	}
+        	} catch(Exception e) {
+        		classLogger.error(Constants.STACKTRACE, e);
+        	}
+            return "Successfully released the payload structs";
+        };
+
+        Future<String> future = executor.submit(callableTask);
+        try {
+        	// wait 1 minute at most
+            String result = future.get(60, TimeUnit.SECONDS);
+            classLogger.info(result);
+        } catch (TimeoutException e) {
+        	classLogger.warn("Not able to release the payload structs within a timely fashion");
+            future.cancel(true); 
+        } catch (InterruptedException | ExecutionException e) {
+        	classLogger.error(Constants.STACKTRACE, e);
+        } finally {
+            executor.shutdown();
+        }
     	
     	this.close();
     	throw new SemossPixelException("Analytic engine is no longer available. This happened because you exceeded the memory limits provided or performed an illegal operation. Please relook at your recipe");
