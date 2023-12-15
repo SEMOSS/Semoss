ea2bf1429a177b96ac1c658c32279d32e4ed5df3
ttrankle <ttrankle@deloitte.com>
2023-12-15 18:37:11 +0000

    UPDATE ::: update textgen embeddings client to handle batches
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50140 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- py/genai_client/embedders/textgen_embedder.py
+++ py/genai_client/embedders/textgen_embedder.py
@@ -1,3 +1,4 @@
+from typing import List
 import requests
 
 from .local_embedder import LocalEmbedder
@@ -9,6 +10,8 @@ from ..constants import (
 
 class TextEmbeddingsInference(LocalEmbedder):
     
+    BATCH_SIZE = 'batch_size'
+    
     def __init__(
         self,
         endpoint:str,
@@ -18,6 +21,11 @@ class TextEmbeddingsInference(LocalEmbedder):
         self.model_name = model_name
         assert self.model_name != None
 
+        self.batch_size = kwargs.pop(
+            TextEmbeddingsInference.BATCH_SIZE, 
+            32
+        )
+        
         self.embedder = self.get_embedder(
             endpoint = endpoint
         )
@@ -42,17 +50,33 @@ class TextEmbeddingsInference(LocalEmbedder):
             '''
             This is a wrapper class to make api calls using SentenceTransformers method
             '''
-            def __init__(self, endpoint):
+            def __init__(self, endpoint, batch_size):
                 self.endpoint = endpoint
+                self.batch_size = batch_size
             
-            def encode(self, sentences, **kwargs):
-                response = requests.post(self.endpoint, json={"inputs": sentences})
-                result = response.json()
-                if isinstance(result, list):
-                    return result
-                elif list(result.keys())[0] == "error":
-                    raise RuntimeError(
-                        "The model is currently loading, please re-run the query."
-                    )
+            def encode(self, sentences: List[str], **kwargs):                
+                
+                # Split a list into sublists of a specified size.
+                sentence_sublists = [sentences[i:i + self.batch_size] for i in range(0, len(sentences), self.batch_size)]
+                
+                # hold the cumulative embeddings from batches
+                embeddings_list = []
+                for sublist in sentence_sublists:
+                    
+                    response = requests.post(self.endpoint, json={"inputs": sublist, "truncate":True})
+                    if response.status_code != 200:
+                        raise RuntimeError(
+                            f"Error {response.status_code}: {response.text}"
+                        )
+                        
+                    result = response.json()
+                    if isinstance(result, list):
+                        embeddings_list.extend(result)
+                    elif list(result.keys())[0] == "error":
+                        raise RuntimeError(
+                            "Unable to get vector embeddings."
+                        )
+                        
+                return embeddings_list
 
-        return TextGenEmbedder(endpoint = endpoint)
+        return TextGenEmbedder(endpoint = endpoint, batch_size = self.batch_size)
