bcecdcd03065e561915d04a1ca3d2859e66acc0b
pkapaleeswaran <pkapaleeswaran@deloitte.com>
2023-12-22 14:22:39 +0000

    Moving models into model engine so the transition is painless when needed pdf_util - You can pass a pdf and ask it to parse output into a target folder.  pu = pdf_util.PDFUtil(source_file="c:/temp/try1/cbmm.pdf", target_folder="c:/temp/try1/target") table_util - Uses paddleocr and paddlepaddle - Make sure you instantiate en (code defaults) for downloading the specific models. The outputs are generated in target - It creates 1 folder for every table it is processing. This is because metadata is also saved.
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50162 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 0be50702a1f83ef3b22e6fff52502a51ef2e0d7e

--- py/pdf_util.py
+++ py/pdf_util.py
@@ -5,6 +5,9 @@ import os
 import torch
 import csv
 import pandas as pd
+import table_util
+
+
 
 # just give the source file and all set
 
@@ -21,14 +24,9 @@ class PDFUtil:
     else:
       self.target_folder = target_folder
     # load the microsoft object detection to check if it is a table
-    self.detector = None
+    self.detector = table_util.TableUtil()
     # we do need a way to load gaas_gpt_model locally so that way we dont need to switch to remote later
-    if detect_tables:
-      device = "cpu"
-      if torch.cuda.is_available():
-        device="cuda"
-      self.detector = pipeline("object-detection", model="microsoft/table-transformer-detection", device=device)
-      
+    
     self.extract_images()
     self.extract_tables()
       
@@ -37,6 +35,7 @@ class PDFUtil:
     for page_index in range(len(self.doc)):
       page = self.doc[page_index]
       image_list = page.get_images()
+      print(f"Processing Page for Image {page_index}")
       
       # process each image
       for image_index,img in enumerate(image_list, start=1):
@@ -45,37 +44,40 @@ class PDFUtil:
         
         if pix.n - pix.alpha > 3:
           pix = fitz.Pixmap(fitz.csRGB, pix)
-        
-        file_name = f'{self.target_folder}/{self.doc_name}__p_{page_index}__i_{image_index}'
-        pix.save(f"{file_name}.png")
-        
-        if self.is_table(f"{file_name}.png"):
-          #os.rename(f'{file_name}.png',f'{file_name}.table') 
-          self.convert_image_to_pdf(f"{file_name}.png")
-          os.remove(f"{file_name}.png")
           
+        # we can directly move this to PIL
+        # data = pix.getImageData("format")
+        # todo - https://github.com/pymupdf/PyMuPDF/issues/322
+        
+        file_name = f'{self.target_folder}/{self.doc_name}__p_{page_index}__i_{image_index}.png'
+        pix.save(file_name)
+
+        # only process if > 5kb
+        size = (os.stat(file_name).st_size)/1024
+
+        if size > 5 and self.is_table(file_name):
+          #os.rename(f'{file_name}.png',f'{file_name}.table') 
+          # self.convert_image_to_pdf(file_name)
+          #os.remove(f"{file_name}.png")
+          #process this table 
+          #print("parsing image to table")
+          self.detector.parse_table_to_csv(pil_image=file_name)
         pix = None  
   
   def is_table(self, png_file):
     if self.detector is None:
-      return True
+      return True # forcing it to be 1
     else:
-      table_def = self.detector(png_file)
-      if table_def is not None and isinstance(table_def, list):
-        #print(f"{png_file} is a table")
-        return len(table_def) > 0
-      else:
-        return false
-      
+      return self.detector.is_table(png_file)
 
   def extract_tables(self):
     for page_index in range(len(self.doc)):
       page = self.doc[page_index]
       tabs = page.find_tables()
+      print(f"Processing Page for Table {page_index}")
       
       for tab_index, tab in enumerate(tabs, start=1):
 
-        print(f"Processing page {page_index}")
         #print(f"Page {page_index} >> Table {tab_index}")
         file_name = f'{self.target_folder}/{self.doc_name}__p_{page_index + 1}__t_{tab_index}.png'
         
@@ -84,6 +86,8 @@ class PDFUtil:
         if not self.is_list_empty(tab.header.names):
           try:
             bbox = tab.bbox
+            # need to pad the bbox
+            new_bbox = (bbox[0] - self.detector.left, bbox[1] - self.detector.bottom, bbox[2] + self.detector.right, bbox[3]+self.detector.top)
             table_rect = fitz.Rect(bbox)
             pix = page.get_pixmap(matrix=fitz.Matrix(3,3), clip=table_rect)
             
@@ -91,8 +95,9 @@ class PDFUtil:
               pix = fitz.Pixmap(fitz.csRGB, pix)
             
             pix.save(f"{file_name}")
-            self.convert_image_to_pdf(file_name)
-            os.remove(file_name)
+            self.detector.parse_table_to_csv(pil_image=file_name)
+            #self.convert_image_to_pdf(file_name)
+            #os.remove(file_name)
           except:
             print(f"Failed .. {file_name}")
             pass        
