d4d05162afac210494dcb782652945f2841eda75
ttrankle <ttrankle@deloitte.com>
2024-01-23 12:34:53 +0000

    UPDATE ::: send make empty string for Native Py Server if there is no response object
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50266 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- py/semoss_console.py
+++ py/semoss_console.py
@@ -1,20 +1,23 @@
-
 class SemossConsole(object):
-    
-    def __init__(self,socket_handler=None, payload=None):
-      self.socket_handler = socket_handler
-      self.payload = payload
-      self.output = []
+    def __init__(self, socket_handler=None, payload=None):
+        self.socket_handler = socket_handler
+        self.payload = payload
+        self.output = []
 
     def write(self, console_line):
-      if self.socket_handler is not None:
-        self.socket_handler.send_output(console_line, self.payload, response=False)
-      else:
-        #self.txtctrl.write(string)
-        self.output.append(console_line)        
+        if (self.socket_handler is not None):
+            self.socket_handler.send_output(console_line, self.payload, response=False)
+        else:
+            self.output.append(console_line)
+            
+    def set_payload(self, payload):
+        self.payload = payload
+
+    def reset_outputs(self):
+        self.output = []
 
     def flush(self):
-      pass
+        pass
 
     def close(self):
-      pass
+        pass
