e853ece17c405909877885dd7a65383d97900de5
ttrankle <ttrankle@deloitte.com>
2024-02-06 18:38:06 +0000

    BUGFIX ::: remove logger test code from previous commit
    git-svn-id: svn://107.170.10.188/Semoss/dev@50324 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6e303e66776efb10d71337a658d4a9aaa8b3665d

--- py/vector_database/faiss/faiss_client.py
+++ py/vector_database/faiss/faiss_client.py
@@ -10,9 +10,6 @@ import glob
 from genai_client.tokenizers.huggingface_tokenizer import HuggingfaceTokenizer
 from ..constants import ENCODING_OPTIONS
 
-from logging_config import get_logger
-class_logger = get_logger(__name__)
-
 class FAISSSearcher():
     '''
     The primary class for a faiss database classes and searching document embeddings
@@ -159,11 +156,6 @@ class FAISSSearcher():
             [{Score=0.9867115616798401, doc_index=1420-deloitte-independence_11_text}, 
             {Score=0.9855965375900269, doc_index=1420-deloitte-independence_10_text}]
         '''
-        
-        print('nearestNeighbor is being called')
-        print('nearestNeighbor is being called')
-        print('nearestNeighbor is being called')
-        
         # if columns_to_return is None, then by default we return all columns
         if(columns_to_return is None):
             columns_to_return = list(self.ds.features)
@@ -239,11 +231,6 @@ class FAISSSearcher():
             output = {}
             output.update({'Score' : row['distances']})
             data_row = self.ds[int(row['ann'])]
-            x = type(data_row)
-            class_logger.info(
-                data_row,
-                extra={"stack": "BACKEND"},
-            )
             for col in columns_to_return:
                 output.update({col:data_row[col]})
             final_output.append(output)
@@ -499,10 +486,7 @@ class FAISSSearcher():
 
         Returns:
         `None`
-        '''
-        
-        print('THis is a sample doc')
-        
+        '''        
         # make sure they are all in indexed_files dir
         assert {os.path.basename(os.path.dirname(path)) for path in documentFileLocation} == {'indexed_files'}
 
