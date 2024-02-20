8a32a1e4c1f4c56a34ad3937569ee7ddd16b277f
ttrankle <ttrankle@deloitte.com>
2024-02-20 16:03:31 +0000

    UPDATE ::: adding additional methods to the ZK utility class
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50386 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/zookeeper/ZKCuratorUtility.java
+++ src/prerna/zookeeper/ZKCuratorUtility.java
@@ -1,6 +1,7 @@
 package prerna.zookeeper;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
@@ -54,8 +55,34 @@ public final class ZKCuratorUtility {
 	public void createEphemerialNode(String path, byte[] data) throws Exception {
 		this.curator.create()
 			.creatingParentsIfNeeded()
-			.withMode(CreateMode.EPHEMERAL)
+			.withMode(CreateMode.EPHEMERAL_SEQUENTIAL)
 			.forPath(path, data);
+	}
+	
+	/**
+	 * Create a znode with a specified path, mode and data
+	 * 
+	 * @param path
+	 * @param mode
+	 * @throws Exception
+	 */
+	public void createNode(String path, CreateMode mode, byte[] data) throws Exception {
+		this.curator.create()
+			.creatingParentsIfNeeded()
+			.withMode(mode)
+			.forPath(path, data);
+	}
+	
+	
+	/**
+	 * This method returns of list a list of children nodes based on the 
+	 * path passed in. 
+	 * 
+	 * @param path
+	 * @throws Exception
+	 */
+	public List<String> listNodeChildren(String path) throws Exception {
+		return this.curator.getChildren().forPath(path);
 	}
 	
 	/**
@@ -125,6 +152,18 @@ public final class ZKCuratorUtility {
 	}
 	
 	/**
+	 * This method removes a node and all of it's children from the Zookeeper server
+	 * 
+	 * @param path - The path of the node to be deleted
+	 * @throws Exception
+	 */
+	public void deleteRecursively(String path) throws Exception {
+		if (this.curator.checkExists().forPath(path) != null) {
+	        this.curator.delete().deletingChildrenIfNeeded().forPath(path);
+		}
+    }
+	
+	/**
 	 * 
 	 * @param path
 	 * @param watcher
@@ -164,5 +203,26 @@ public final class ZKCuratorUtility {
 	 */
 	public boolean lockHeld(String lockName) throws Exception {
 		return curator.checkExists().forPath(lockName) != null;
+	}
+	
+	
+	/**
+	 * This method attemps to acquire a lock on znode. If successful it will return
+	 * a {@code InterProcessMutex} object, otherwise it will return null.
+	 * 
+	 * @param path - The path of the node to create the lock for
+	 * @throws Exception
+	 */
+	public InterProcessMutex attemptLock(String lockPath) throws Exception {
+	    InterProcessMutex lock = new InterProcessMutex(curator, lockPath);
+	    
+	    // Try to acquire the lock immediately, without waiting
+	    if (lock.acquire(0, TimeUnit.MILLISECONDS)) {
+	        // Successfully acquired the lock
+	        return lock;
+	    }
+	    
+	    // Lock is already held by another process
+	    return null;
 	}
 }
