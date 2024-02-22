6ab697727ddab3714d8b843e90387ea4f10b8e8f
ttrankle <ttrankle@deloitte.com>
2024-02-22 00:58:24 +0000

    NEW ::: create model engine response classes
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50398 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 3a42192d1a447a4921e7b24d6d9475e943f0f552

--- src/prerna/engine/impl/model/AbstractModelEngine.java
+++ src/prerna/engine/impl/model/AbstractModelEngine.java
@@ -1,7 +1,9 @@
 package prerna.engine.impl.model;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,8 +15,17 @@ import java.util.UUID;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.text.StringSubstitutor;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.google.gson.Gson;
 
 import prerna.ds.py.PyUtils;
 import prerna.ds.py.TCPPyTranslator;
@@ -22,8 +33,14 @@ import prerna.engine.api.IEngine;
 import prerna.engine.api.IModelEngine;
 import prerna.engine.impl.SmssUtilities;
 import prerna.engine.impl.model.inferencetracking.ModelInferenceLogsUtils;
+import prerna.engine.impl.model.responses.AskModelEngineResponse;
+import prerna.engine.impl.model.responses.EmbeddingsModelEngineResponse;
+import prerna.engine.impl.model.responses.IModelEngineResponseHandler;
+import prerna.engine.impl.model.responses.IModelEngineResponseStreamHandler;
 import prerna.engine.impl.model.workers.ModelEngineInferenceLogsWorker;
 import prerna.om.Insight;
+import prerna.sablecc2.comm.JobManager;
+import prerna.security.AbstractHttpHelper;
 import prerna.tcp.PayloadStruct;
 import prerna.tcp.client.NativePySocketClient;
 import prerna.util.Constants;
@@ -39,10 +56,6 @@ public abstract class AbstractModelEngine implements IModelEngine {
 
 	private static final String DIR_SEPERATOR = "/";
 	private static final String FILE_SEPARATOR = java.nio.file.FileSystems.getDefault().getSeparator();
-	
-	protected static final String RESPONSE = "response";
-	protected static final String NUMBER_OF_TOKENS_IN_PROMPT = "numberOfTokensInPrompt";
-	protected static final String NUMBER_OF_TOKENS_IN_RESPONSE = "numberOfTokensInResponse";
 	
 	protected String engineId = null;
 	protected String engineName = null;
@@ -179,34 +192,35 @@ public abstract class AbstractModelEngine implements IModelEngine {
 	public abstract Map<String, Object> askQuestion(String question, String context, Insight insight, Map<String, Object> parameters);
 
 	@Override
-	public Map<String, Object> ask(String question, String context, Insight insight, Map<String, Object> parameters) {
+	public AskModelEngineResponse ask(String question, String context, Insight insight, Map<String, Object> parameters) {
 		//Map<String, String> output = new HashMap<String, String>();
 		//TODO turn into threads
 		if(this.socketClient == null || !this.socketClient.isConnected()) {
 			this.startServer();
 		}
 		
-		Map<String, Object> retMap = null;
-		String messageId = UUID.randomUUID().toString();
 		if(parameters == null) {
 			parameters = new HashMap<String, Object>();
 		}
 		
+		LocalDateTime inputTime = LocalDateTime.now();
+		Map<String, Object> modelResponse = askQuestion(question, context, insight, parameters);
+		LocalDateTime outputTime = LocalDateTime.now();
+				
+		AskModelEngineResponse askModelResponse = AskModelEngineResponse.fromMap(modelResponse);
+		askModelResponse.setMessageId(UUID.randomUUID().toString());
+		askModelResponse.setRoomId(insight.getInsightId());
 		
 		if (Utility.isModelInferenceLogsEnabled()) {
-			LocalDateTime inputTime = LocalDateTime.now();
-			retMap = askQuestion(question, context, insight, parameters);
-			LocalDateTime outputTime = LocalDateTime.now();
 			
-			String response = retMap.get(RESPONSE) + "";
-			
+						
 			if (keepConversationHistory) {
 				Map<String, Object> inputMap = new HashMap<String, Object>();
 				Map<String, Object> outputMap = new HashMap<String, Object>();
 				inputMap.put(ModelEngineConstants.ROLE, "user");
 				inputMap.put(ModelEngineConstants.MESSAGE_CONTENT, question);
 				outputMap.put(ModelEngineConstants.ROLE, "assistant");
-				outputMap.put(ModelEngineConstants.MESSAGE_CONTENT, response);
+				outputMap.put(ModelEngineConstants.MESSAGE_CONTENT, askModelResponse.getResponse());
 		        
 				if (chatHistory.containsKey(insight.getInsightId())) {
 			        chatHistory.get(insight.getInsightId()).add(inputMap);
@@ -214,37 +228,32 @@ public abstract class AbstractModelEngine implements IModelEngine {
 				}
 			}
 			Thread inferenceRecorder = new Thread(new ModelEngineInferenceLogsWorker (
-					messageId, 
+					askModelResponse.getMessageId(), 
 					"ask", 
 					this, 
 					insight,
 					context, 
 					question,
-					getTokens(retMap.get(NUMBER_OF_TOKENS_IN_PROMPT)),
+					askModelResponse.getNumberOfTokensInPrompt(),
 					inputTime, 
-					response,
-					getTokens(retMap.get(NUMBER_OF_TOKENS_IN_RESPONSE)),
+					askModelResponse.getResponse(),
+					askModelResponse.getNumberOfTokensInResponse(),
 					outputTime
 			));
 			inferenceRecorder.start();
-		} else {
-			retMap = askQuestion(question, context, insight, parameters);
 		}
-		
-		retMap.put("messageId", messageId);
-		retMap.put("roomId", insight.getInsightId());
 
-		return retMap;
+		return askModelResponse;
 	}
 
 	@Override
-	public Object embeddings(List<String> stringsToEncode, Insight insight, Map <String, Object> parameters) {
+	public EmbeddingsModelEngineResponse embeddings(List<String> stringsToEmbed, Insight insight, Map <String, Object> parameters) {
 		if(this.socketClient == null || !this.socketClient.isConnected())
 			this.startServer();
 		
 		String varName = smssProp.getProperty(ModelEngineConstants.VAR_NAME);
 	 	
-		String pythonListAsString = PyUtils.determineStringType(stringsToEncode);
+		String pythonListAsString = PyUtils.determineStringType(stringsToEmbed);
 		
 		StringBuilder callMaker = new StringBuilder();
 
@@ -258,14 +267,18 @@ public abstract class AbstractModelEngine implements IModelEngine {
 		
 		callMaker.append(")");
 		
-		Map<String, Object> output;
+		
 		classLogger.info("Making embeddings call on engine " + this.engineId);
+		LocalDateTime inputTime = LocalDateTime.now();
+		Object responseObject = pyt.runScript(callMaker.toString(), insight);
+		LocalDateTime outputTime = LocalDateTime.now();
+		classLogger.info("Embeddings Received from engine " + this.engineId);
+		
+		EmbeddingsModelEngineResponse embeddingsResponse = EmbeddingsModelEngineResponse.fromObject(responseObject);
+		
 		
 		if (Utility.isModelInferenceLogsEnabled()) {
 			String messageId = UUID.randomUUID().toString();
-			LocalDateTime inputTime = LocalDateTime.now();
-			output = (Map<String, Object>) pyt.runScript(callMaker.toString(), insight);
-			LocalDateTime outputTime = LocalDateTime.now();
 			Thread inferenceRecorder = new Thread(new ModelEngineInferenceLogsWorker (
 					messageId, 
 					"embeddings", 
@@ -273,19 +286,16 @@ public abstract class AbstractModelEngine implements IModelEngine {
 					insight, 
 					null,
 					"",
-					getTokens(output.get(NUMBER_OF_TOKENS_IN_PROMPT)),
+					embeddingsResponse.getNumberOfTokensInPrompt(),
 					inputTime, 
 					"",
-					getTokens(output.get(NUMBER_OF_TOKENS_IN_RESPONSE)),
+					embeddingsResponse.getNumberOfTokensInResponse(),
 					outputTime
 			));
 			inferenceRecorder.start();
-		} else {
-			output = (Map<String, Object>) pyt.runScript(callMaker.toString(), insight);
 		}
-		classLogger.info("Embeddings Received from engine " + this.engineId);
- 			
-		return output;
+ 		
+		return embeddingsResponse;
 	}
 	
 	@Override
@@ -452,7 +462,9 @@ public abstract class AbstractModelEngine implements IModelEngine {
 	}
 	
 	public Integer getTokens(Object numTokens) {
-		if (numTokens instanceof Long) {
+		if (numTokens instanceof Integer) {
+			return (Integer) numTokens;
+		} else if (numTokens instanceof Long) {
 			return ((Long) numTokens).intValue();
 		} else if (numTokens instanceof Double) {
 			return ((Double) numTokens).intValue();
@@ -461,6 +473,87 @@ public abstract class AbstractModelEngine implements IModelEngine {
 		} else {
 			return null;
 		}
+	}
+	
+	public static IModelEngineResponseHandler postRequestStringBody(String url, Map<String, String> headersMap, String body, ContentType contentType, String keyStore, String keyStorePass, String keyPass, boolean isStream, Class<? extends IModelEngineResponseHandler> responseType, String insightId) {
+		CloseableHttpClient httpClient = null;
+	    CloseableHttpResponse response = null;
+	    try {
+	        httpClient = AbstractHttpHelper.getCustomClient(null, keyStore, keyStorePass, keyPass);
+	        HttpPost httpPost = new HttpPost(url);
+	        if (headersMap != null && !headersMap.isEmpty()) {
+	            for (String key : headersMap.keySet()) {
+	                httpPost.addHeader(key, headersMap.get(key));
+	            }
+	        }
+	        if (body != null && !body.isEmpty()) {
+	            httpPost.setEntity(new StringEntity(body, contentType));
+	        }
+	        response = httpClient.execute(httpPost);
+
+	        int statusCode = response.getStatusLine().getStatusCode();
+	        if (statusCode >= 200 && statusCode < 300) {
+	            HttpEntity entity = response.getEntity();
+	            if (!isStream) {
+	                // Handle regular response
+	                String responseData = entity != null ? EntityUtils.toString(entity) : null;
+	                IModelEngineResponseHandler responseObject = new Gson().fromJson(responseData, responseType);
+	                return responseObject;
+	            } else {
+	                // Handle streaming response
+	                if (entity != null) {
+	                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()))) {
+	                        String line;
+	                        StringBuilder responseAssimilator = new StringBuilder();
+	                        IModelEngineResponseHandler responseObject = responseType.newInstance();
+	                        
+	                        while ((line = reader.readLine()) != null) {
+	                            if (line.contains("data: [DONE]")) {
+	                                break;
+	                            }
+	                            
+	                            if (line.startsWith("data: ")) {
+	                                // Extract JSON part
+	                                String jsonPart = line.substring("data: ".length());
+	                                IModelEngineResponseStreamHandler partialObject = new Gson().fromJson(jsonPart, responseObject.getStreamHandlerClass());
+	                                Object partial = partialObject.getPartialResponse();
+	                                
+	                                if (partial != null) {
+	                                	responseObject.appendStream(partialObject);
+		                                JobManager.getManager().addPartialOut(insightId, partial+"");
+		                                responseAssimilator.append(partial);
+	                                }
+	                            }
+	                        }
+	                        responseObject.setResponse(responseAssimilator.toString());
+	                        return responseObject;
+	                    } catch (Exception e) {
+	            	        classLogger.error(Constants.STACKTRACE, e);
+	            	        throw new IllegalArgumentException("There was an error processing the response from " + url);
+	            	    }
+	                }
+	            }
+	        } else {
+	        	// try to send back the error from the server
+	            String errorResponse = EntityUtils.toString(response.getEntity());
+	            throw new IllegalArgumentException("Connected to " + url + " but received error = " + errorResponse);
+	        }
+	    } catch (IOException e) {
+	        classLogger.error(Constants.STACKTRACE, e);
+	        throw new IllegalArgumentException("Could not connect to URL at " + url);
+	    } finally {
+	        try {
+	            if (response != null) {
+	                response.close();
+	            }
+	            if (httpClient != null) {
+	                httpClient.close();
+	            }
+	        } catch (IOException e) {
+	            classLogger.error("Error while closing resources", e);
+	        }
+	    }
+	    return null; // In case of unexpected flow
 	}
 	
 	public boolean keepsConversationHistory() {
