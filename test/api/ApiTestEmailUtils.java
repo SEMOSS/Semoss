e621973cf97db13decd2d786bf4c2042ce606fc7
mahkhalil <mahkhalil@deloitte.com>
2024-02-24 18:55:03 +0000

    UPDATE ::: Adding new api tests
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50425 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: de10d53d71a4b5f5f43eedd39ad04d889ad5a976

---
+++ test/api/ApiTestEmailUtils.java
@@ -0,0 +1,134 @@
+package api;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.ProcessBuilder.Redirect;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.testcontainers.containers.GenericContainer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ApiTestEmailUtils {
+	
+	private static String MAILPIT_FOLDER = Paths.get(ApiTests.TEST_BASE_DIRECTORY, "mailpit").toString();
+	private static String MAILPIT_EXE = Paths.get(MAILPIT_FOLDER, "mailpit.exe").toString();
+	private static String MAILPIT_LOG = Paths.get(MAILPIT_FOLDER, "mailpit.log").toString();
+	
+	public static boolean serverRunning() {
+		try {
+			URL url = new URL("http://localhost:8025/api/v1/info");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			int status = conn.getResponseCode();
+			return status == 200;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	public static void deleteAllEmails() {
+		try {
+			URL url = new URL("http://localhost:8025/api/v1/messages");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("DELETE");
+			int status = conn.getResponseCode();
+			assertEquals(status, 200);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.toString());
+		}
+	}
+	
+
+	public static List<Map<String, Object>> getAllEmails() {
+		List<Map<String, Object>> m = new ArrayList<>();
+		try {
+			URL url = new URL("http://localhost:8025/api/v1/messages");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			int status = conn.getResponseCode();
+			assertEquals(status, 200);
+			InputStream is = conn.getInputStream();
+			ObjectMapper om = new ObjectMapper();
+			Map<String, Object> map = om.readValue(is, Map.class);
+			m = (List<Map<String, Object>>) map.get("messages");
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.toString());
+		}
+		
+		return m;
+	}
+	
+	public static Map<String, Object> getEmail(String id) {
+		Map<String, Object> m = new HashMap<>();
+		try {
+			URL url = new URL("http://localhost:8025/api/v1/message/" + id);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			int status = conn.getResponseCode();
+			assertEquals(status, 200);
+			InputStream is = conn.getInputStream();
+			ObjectMapper om = new ObjectMapper();
+			m = om.readValue(is, Map.class);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.toString());
+		}
+		
+		return m;
+	}
+
+
+	public static void startEmailDockerContainer() {
+		GenericContainer<?> mailpit = new GenericContainer<>("axllent/mailpit")
+				.withExposedPorts(1025, 8025);
+		List<String> portBindings = new ArrayList<>();
+		portBindings.add("1025:1025");
+		portBindings.add("8025:8025");
+		mailpit.setPortBindings(portBindings);
+		mailpit.start();
+	}
+
+
+	public static void startEmailLocalServer() throws IOException, InterruptedException {
+		if (!serverRunning()) {
+			ProcessBuilder pb = new ProcessBuilder(MAILPIT_EXE);
+			pb.directory(new File(MAILPIT_FOLDER));
+			
+			if (Files.exists(Paths.get(MAILPIT_LOG))) {
+				Files.delete(Paths.get(MAILPIT_LOG));
+			}
+			
+			File log = new File(MAILPIT_LOG);
+			pb.redirectErrorStream(true);
+			pb.redirectOutput(Redirect.appendTo(log));
+			Process p = pb.start();
+			
+			int i = 0;
+			boolean found = false;
+			while (i < 10 && !found) {
+				List<String> lines = Files.readAllLines(Paths.get(MAILPIT_LOG));
+				if (lines.contains("starting server on")) {
+					found = true;
+				}
+				i++;
+				Thread.sleep(1000L);
+			}
+			
+			Runtime.getRuntime().addShutdownHook(new Thread(() -> p.destroy()));
+		}
+	}
+}
---
+++ test/api/ApiTests.java
@@ -0,0 +1,143 @@
+package api;
+
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import com.google.gson.Gson;
+
+import prerna.auth.User;
+import prerna.ds.py.PyExecutorThread;
+import prerna.om.Insight;
+import prerna.util.Constants;
+import prerna.util.DIHelper;
+import prerna.util.gson.GsonUtility;
+
+public class ApiTests {
+	
+	protected static final Logger LOGGER = LogManager.getLogger(ApiTests.class.getName());
+
+	protected static final String BASE_DIRECTORY = new File("").getAbsolutePath();
+	protected static final String TEST_BASE_DIRECTORY = new File("testfolder").getAbsolutePath();
+	
+	protected static final String TEST_DB_DIRECTORY = Paths.get(TEST_BASE_DIRECTORY, "db").toAbsolutePath().toString();
+	protected static final String TEST_PROJECT_DIRECTORY = Paths.get(TEST_BASE_DIRECTORY, "project").toAbsolutePath().toString();
+	protected static final String TEST_RESOURCES_DIRECTORY = Paths.get(BASE_DIRECTORY, "test", "resources", "api")
+			.toAbsolutePath().toString();
+	protected static final Path TEST_CONFIG_DIRECTORY = Paths.get(TEST_BASE_DIRECTORY, "testconfig");
+
+	protected static final Gson GSON = GsonUtility.getDefaultGson();
+	protected static final Gson GSON_PRETTY = GsonUtility.getDefaultGson(true);
+
+
+	static final Path BASE_RDF_MAP = Paths.get(BASE_DIRECTORY, "RDF_Map.prop");
+	static final Path TEST_RDF_MAP = Paths.get(TEST_RESOURCES_DIRECTORY, "RDF_Map.prop");
+	
+	public static Path TEST_INSIGHT_CACHE;
+	
+	// paths to smss files for testing
+	static final String LMD_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.LOCAL_MASTER_DB + ".smss").toAbsolutePath().toString();
+	static final String SECURITY_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.SECURITY_DB + ".smss").toAbsolutePath().toString();
+	static final String SCHEDULER_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.SCHEDULER_DB + ".smss").toAbsolutePath().toString();
+	static final String THEMES_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.THEMING_DB + ".smss").toAbsolutePath().toString();
+	static final String UTDB_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.USER_TRACKING_DB + ".smss").toAbsolutePath().toString();
+	
+	protected static Map<String, String> aliasToAppId = new HashMap<>();
+
+	public static Insight insight = null;
+	protected static User user = null;
+
+	protected PyExecutorThread jep = null;
+	
+	private static Boolean USING_DOCKER = null;
+	
+	private static boolean FIRST_CLASS_RUN = true;
+	
+    @BeforeClass
+    public static void apiTestsSetup() throws Exception {
+    	if (FIRST_CLASS_RUN) {
+    		FIRST_CLASS_RUN = false;
+
+			ApiTestPropsUtils.loadDIHelper();
+			if (runningOnServer()) {
+				ApiTestEmailUtils.startEmailDockerContainer();
+			} else {
+				ApiTestEmailUtils.startEmailLocalServer();
+			}
+			
+			// moved this to the before because its hard to delete databases before each test due to database being in use
+			try {
+				ApiTestEngineUtils.clearNonCoreDBs();
+			} catch (Exception e) {
+				System.out.println("Could not clear DBS that are not semoss/project specific. ");
+				fail(e.toString());
+			}
+			
+			try {
+				ApiTestProjectUtils.clearNonCoreProjects();
+			} catch (Exception e) {
+				System.out.println("Could not clear DBS that are not semoss/project specific. ");
+				fail(e.toString());
+			}
+
+			ApiTestInsightUtils.initializeInsight();
+			ApiTestEngineUtils.initalizeDatabases();
+    	}
+    }
+    
+    @AfterClass
+	public static void destroyContext() {
+//		ApiInsightAndPropsInitUtils.unloadDIHelper();
+//		ApiDatabaseInitUtils.unloadDatabases();
+//		ApiInsightAndPropsInitUtils.unloadSocialProps();
+	}
+    
+
+	// Ensure that everything is pointing in the correct direction before each test to limit damage
+    // in case the DIHelper decides to reload with a different rdf map properties. 
+    @Before
+    public void apiTestsBefore() {
+    	ApiTestEngineUtils.checkDatabasePropMapping();
+    	
+    	try {
+    		ApiTestEngineUtils.deleteAllDataAndAddUser();
+    	} catch (Exception e) {
+    		System.out.println("Could not rebuild core semoss dbs");
+    		fail(e.toString());
+    	}
+    	
+    	ApiTestUserUtils.setDefaultTestUser();
+    	
+    	ApiTestEmailUtils.deleteAllEmails();
+    	ApiTestInsightUtils.clearInsightCacheDifferently();
+    }
+    
+	
+	public static boolean runningOnServer() {
+		if (USING_DOCKER != null) {
+			return USING_DOCKER;
+		}
+		
+		Boolean docker = Boolean.valueOf(DIHelper.getInstance().getProperty("USE_DOCKER"));
+		
+		if (docker == null) {
+			docker = false;
+		}
+		
+		USING_DOCKER = docker;
+		
+		return docker;
+	}
+	
+}
