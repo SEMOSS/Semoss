087fbbfec289553a5ec8b2c6851be02a639a67ac
mahkhalil <mahkhalil@deloitte.com>
2024-03-13 15:09:55 +0000

    UPDATE ::: moving legacy test code into test_legacy folder
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50555 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 2233bf66a95dd9408c979b9a3c3ab45842259e8d

---
+++ test_legacy/prerna/junit/reactors/search/SearchPackageTests.java
@@ -0,0 +1,13 @@
+package prerna.junit.reactors.search;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	
+	})
+public class SearchPackageTests {
+
+}
