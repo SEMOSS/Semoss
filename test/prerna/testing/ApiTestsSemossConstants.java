f6cdb30e9d1bafd38f83e93fb18da53a387b5e55
mahkhalil <mahkhalil@deloitte.com>
2024-03-13 16:26:34 +0000

    UPDATE ::: Adding goal to create test jar UPDATE ::: Shifting test/resources/api to just test/resources
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50557 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 6a3dab1216a640e7c5b0eb19256596b363813683

--- test/prerna/testing/ApiTestsSemossConstants.java
+++ test/prerna/testing/ApiTestsSemossConstants.java
@@ -9,29 +9,22 @@ import prerna.util.Constants;
 public class ApiTestsSemossConstants {
 
 	public static final String BASE_DIRECTORY = new File("").getAbsolutePath();
-	public static final String TEST_RESOURCES_DIRECTORY = Paths.get(BASE_DIRECTORY, "test", "resources", "api")
-			.toAbsolutePath().toString();
+	public static final String TEST_RESOURCES_DIRECTORY = Paths.get(BASE_DIRECTORY, "test", "resources").toAbsolutePath().toString();
 
 	public static final String TEST_BASE_DIRECTORY = new File("testfolder").getAbsolutePath();
 
 	public static final String TEST_DB_DIRECTORY = Paths.get(TEST_BASE_DIRECTORY, "db").toAbsolutePath().toString();
-	public static final String TEST_PROJECT_DIRECTORY = Paths.get(TEST_BASE_DIRECTORY, "project").toAbsolutePath()
-			.toString();;
+	public static final String TEST_PROJECT_DIRECTORY = Paths.get(TEST_BASE_DIRECTORY, "project").toAbsolutePath().toString();
 	public static final Path TEST_CONFIG_DIRECTORY = Paths.get(TEST_BASE_DIRECTORY, "testconfig");
 
 	public static final Path BASE_RDF_MAP = Paths.get(BASE_DIRECTORY, "RDF_Map.prop");
 	public static final Path TEST_RDF_MAP = Paths.get(TEST_RESOURCES_DIRECTORY, "RDF_Map.prop");
 
-	public static final String LMD_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.LOCAL_MASTER_DB + ".smss")
-			.toAbsolutePath().toString();
-	public static final String SECURITY_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.SECURITY_DB + ".smss")
-			.toAbsolutePath().toString();
-	public static final String SCHEDULER_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.SCHEDULER_DB + ".smss")
-			.toAbsolutePath().toString();
-	public static final String THEMES_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.THEMING_DB + ".smss")
-			.toAbsolutePath().toString();
-	public static final String UTDB_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.USER_TRACKING_DB + ".smss")
-			.toAbsolutePath().toString();
+	public static final String LMD_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.LOCAL_MASTER_DB + ".smss").toAbsolutePath().toString();
+	public static final String SECURITY_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.SECURITY_DB + ".smss").toAbsolutePath().toString();
+	public static final String SCHEDULER_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.SCHEDULER_DB + ".smss").toAbsolutePath().toString();
+	public static final String THEMES_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.THEMING_DB + ".smss").toAbsolutePath().toString();
+	public static final String UTDB_SMSS = Paths.get(TEST_DB_DIRECTORY, Constants.USER_TRACKING_DB + ".smss").toAbsolutePath().toString();
 	
 	// default user info
 	public static final String USER_NAME = "user1";	
