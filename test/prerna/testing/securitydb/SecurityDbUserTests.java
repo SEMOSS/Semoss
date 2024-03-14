4cdc13fb58a00ef804961cff945ae1448be36ba2
mahkhalil <mahkhalil@deloitte.com>
2024-03-14 19:52:47 +0000

    UPDATE ::: Building out more tests
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50579 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 0adac46bed85335e3e9946d9a97dc017ef6b3ebb

--- test/prerna/testing/securitydb/SecurityDbUserTests.java
+++ test/prerna/testing/securitydb/SecurityDbUserTests.java
@@ -1,7 +1,49 @@
 package prerna.testing.securitydb;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import prerna.auth.AccessToken;
+import prerna.auth.AuthProvider;
+import prerna.auth.utils.SecurityNativeUserUtils;
 import prerna.testing.AbstractBaseSemossApiTests;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class SecurityDbUserTests extends AbstractBaseSemossApiTests {
 
+	private static final String NATIVE_DUMMY_USER_ID = "DUMMY_USER_123";
+	private static final AuthProvider NATIVE_DUMMY_USER_PROVIDER = AuthProvider.NATIVE;
+	private static final String NATIVE_DUMMY_USERNAME = "DUMMYUSERNAME";
+	private static final String NATIVE_DUMMY_EMAIL = "example@mail.com";
+	private static final String NATIVE_DUMMY_PASSWORD = "SEMoss@123123!@#";
+	
+	@Override
+	@BeforeEach
+	public void beforeEachTest() {
+		this.clearAllDatabasesBetweenTests = false;
+		super.beforeEachTest();
+	}
+
+	@Test
+	@Order(1)
+	public void createNativeUser() {
+		AccessToken newUser = new AccessToken();
+		newUser.setId(NATIVE_DUMMY_USER_ID);
+		newUser.setProvider(NATIVE_DUMMY_USER_PROVIDER);
+		newUser.setUsername(NATIVE_DUMMY_USERNAME);
+		newUser.setEmail(NATIVE_DUMMY_EMAIL);
+		try {
+			assertTrue(SecurityNativeUserUtils.addNativeUser(newUser, NATIVE_DUMMY_PASSWORD));
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
 }
