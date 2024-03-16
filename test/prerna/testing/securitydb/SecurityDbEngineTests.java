9fd4d78dcfc9a2af51bb01b696f4e82576eac474
mahkhalil <mahkhalil@deloitte.com>
2024-03-16 17:47:24 +0000

    UPDATE ::: Adding ability for admin to assign project to group
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50589 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- test/prerna/testing/securitydb/SecurityDbEngineTests.java
+++ test/prerna/testing/securitydb/SecurityDbEngineTests.java
@@ -1,7 +1,28 @@
 package prerna.testing.securitydb;
 
-import prerna.testing.AbstractBaseSemossApiTests;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+import prerna.testing.AbstractBaseSemossApiTests;
+import prerna.testing.ApiSemossTestEngineUtils;
+
+@TestMethodOrder(OrderAnnotation.class)
 public class SecurityDbEngineTests extends AbstractBaseSemossApiTests {
 
+	@BeforeAll
+    public static void initialSetup() throws Exception {
+		AbstractBaseSemossApiTests.initialSetup();
+		// unnecessary if running by itself, but necessary if running {@link prerna.testing.AllTests}
+		ApiSemossTestEngineUtils.deleteAllDataAndAddUser();
+	}
+	
+	@Test
+	@Order(1)
+	public void addEngine() {
+		
+	}
+	
 }
