a076ed0864f5dc9b060012083e441f93bc454c8f
mahkhalil <mahkhalil@deloitte.com>
2024-03-17 00:45:04 +0000

    UPDATE ::: Adding more boilerplate code
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50602 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- test/prerna/testing/cloudstorage/CentralCloudStorageTests.java
+++ test/prerna/testing/cloudstorage/CentralCloudStorageTests.java
@@ -3,18 +3,16 @@ package prerna.testing.cloudstorage;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 
 import prerna.auth.User;
-import prerna.auth.utils.SecurityEngineUtils;
-import prerna.engine.api.IEngine;
 import prerna.testing.AbstractBaseSemossApiTests;
 import prerna.testing.ApiSemossTestEngineUtils;
 import prerna.testing.ApiSemossTestUserUtils;
-import prerna.util.sql.RdbmsTypeEnum;
 
 @TestMethodOrder(OrderAnnotation.class)
 public class CentralCloudStorageTests extends AbstractBaseSemossApiTests {
@@ -24,6 +22,26 @@ public class CentralCloudStorageTests extends AbstractBaseSemossApiTests {
 		AbstractBaseSemossApiTests.initialSetup();
 		// unnecessary if running by itself, but necessary if running {@link prerna.testing.AllTests}
 		ApiSemossTestEngineUtils.deleteAllDataAndAddUser();
+	}
+	
+	@Override
+	@BeforeEach
+	public void beforeEachTest() {
+		this.clearAllDatabasesBetweenTests = false;
+		super.beforeEachTest();
+	}
+
+	@Test
+	@Order(1)
+	public void storeEngine() {
+		User defaultTestAdminUser = ApiSemossTestUserUtils.getUser();
+
+		try {
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
 	
 }
