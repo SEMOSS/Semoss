7c18b705ee57076c8d0a13df9c91e34fc4870f28
ttrankle <ttrankle@deloitte.com>
2024-03-21 18:38:40 +0000

    UPDATE ::: adding tests for python genai clients
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50630 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 5478428d34512518a354a12c649a8e20aa06ae05

---
+++ py/genai_client/tests/all_genai_client_tests.py
@@ -0,0 +1,24 @@
+### ATTENTION 
+### To run this you need to be in the Semoss_Dev/py directory
+### Then run the following command
+### py -3.10 -m unittest genai_client.tests.all_genai_client_tests
+
+import unittest
+
+from genai_client.tests.test_genai_text_generation import AskModelTests
+from genai_client.tests.test_genai_embedders import EmbeddingsModelTests
+
+if __name__ == '__main__':
+    test_loader = unittest.TestLoader()
+    test_suite = unittest.TestSuite()
+
+    # tests for model asks
+    test_text_generation = test_loader.loadTestsFromTestCase(AskModelTests)
+    test_suite.addTest(test_text_generation)
+
+    # tests for embeddings
+    test_embeddings = test_loader.loadTestsFromTestCase(EmbeddingsModelTests)
+    test_suite.addTest(test_embeddings)
+
+    # run tests
+    unittest.TextTestRunner(verbosity=2).run(test_suite)
---
+++ py/genai_client/tests/test_genai_embedders.py
@@ -0,0 +1,115 @@
+### ATTENTION 
+### To run this you need to be in the Semoss_Dev/py directory
+### Then run the following command
+### py -3.10 -m unittest genai_client.tests.test_genai_embedders
+
+import unittest
+
+from typing import Dict
+
+from genai_client import (
+    LocalEmbedder,
+    OpenAiEmbedder,
+    AzureOpenAiEmbedder,
+    TextEmbeddingsInference,
+    VertexAiEmbedder,
+)
+
+from genai_client.constants import (
+    EmbeddingsModelEngineResponse
+)
+
+class EmbeddingsModelTests(unittest.TestCase):
+        
+    def test_local_embedder(self):
+        # declare the model
+        embedder = LocalEmbedder(model_name='BAAI/bge-large-en-v1.5')
+
+        # make sure the ask expected
+        ask_response = embedder.ask(question = 'what is the capital of france ey?')
+        self.assertIsInstance(ask_response, Dict)
+        self.assertEqual(ask_response['response'], 'This model does not support text generation.')
+
+        embeddings_response = embedder.embeddings_call(strings_to_embed = ['hello'])
+        self.assertIsInstance(embeddings_response, EmbeddingsModelEngineResponse)
+        self.assertEqual(len(embeddings_response.response), 1)
+        self.assertEqual(len(embeddings_response.response[0]), 1024)
+        
+    def test_text_generation_embeddings(self):
+        # declare the model
+        embedder = TextEmbeddingsInference(
+            endpoint= "***REMOVED***", 
+            model_name="BAAI/bge-large-en-v1.5"
+        )
+
+        # make sure the ask expected
+        ask_response = embedder.ask(question = 'what is the capital of france ey?')
+        self.assertIsInstance(ask_response, Dict)
+        self.assertEqual(ask_response['response'], 'This model does not support text generation.')
+
+        embeddings_response = embedder.embeddings_call(strings_to_embed = ['hello'])
+        self.assertIsInstance(embeddings_response, EmbeddingsModelEngineResponse)
+        self.assertEqual(len(embeddings_response.response), 1)
+        self.assertEqual(len(embeddings_response.response[0]), 1024)
+        
+    def test_openai_text_davinci(self):
+        import os
+        openai_key = os.environ.get("OPENAI_API_KEY")
+        
+        # declare the model
+        embedder = OpenAiEmbedder(
+            model_name='text-embedding-ada-002',
+            #api_key = openai_key
+            api_key = '***REMOVED***'
+        )
+
+        # make sure the ask expected
+        ask_response = embedder.ask(question = 'what is the capital of france ey?')
+        self.assertIsInstance(ask_response, Dict)
+        self.assertEqual(ask_response['response'], 'This model does not support text generation.')
+
+        embeddings_response = embedder.embeddings_call(strings_to_embed = ['hello'])
+        self.assertIsInstance(embeddings_response, EmbeddingsModelEngineResponse)
+        self.assertEqual(len(embeddings_response.response), 1)
+        self.assertEqual(len(embeddings_response.response[0]), 1536)
+        
+    def test_azure_embedder(self):
+        import os
+        api_key = os.environ.get("AZURE_OPENAI_API_KEY")
+        
+        # declare the model
+        embedder =AzureOpenAiEmbedder(
+            model_name='embedding-model',
+            api_key = "***REMOVED***",
+            endpoint = "***REMOVED***",
+            api_version = "2023-05-15"
+        )
+
+        # make sure the ask expected
+        ask_response = embedder.ask(question = 'what is the capital of france ey?')
+        self.assertIsInstance(ask_response, Dict)
+        self.assertEqual(ask_response['response'], 'This model does not support text generation.')
+
+        embeddings_response = embedder.embeddings_call(strings_to_embed = ['hello'])
+        self.assertIsInstance(embeddings_response, EmbeddingsModelEngineResponse)
+        #self.assertEqual(len(embeddings_response.response), 8192)
+
+    def test_vertex_embedder(self):
+        import os
+        vertex_sercive_key_path = os.environ.get("GOOGLE_SERVIVE_ACCOUNT_FILE_PATH")
+        
+        # declare the model
+        embedder = VertexAiEmbedder(
+            model_name = 'textembedding-gecko@001',
+            service_account_key_file = 'C:\\Users\\ttrankle\\Documents\\Semoss\\CFG.AI\\us-gcp-ame-adv-a66-npd-1-sa 2.json',
+            region='us-central1'
+        ) 
+
+        # make sure the ask expected
+        ask_response = embedder.ask(question = 'what is the capital of france ey?')
+        self.assertIsInstance(ask_response, Dict)
+        self.assertEqual(ask_response['response'], 'This model does not support text generation.')
+
+        embeddings_response = embedder.embeddings_call(strings_to_embed = ['hello'])
+        self.assertIsInstance(embeddings_response, EmbeddingsModelEngineResponse)
+        #self.assertEqual(len(embeddings_response.response), 8192)
