5f95bd8f1c920d625c9803c61aaa33d32acedc5f
mahkhalil <mahkhalil@deloitte.com>
2024-05-15 11:52:59 +0000

    UPDATE ::: Adding additional check on client process wrapper to ensure the port is actually released by the OS
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@50859 ef391c1c-ee15-45f7-b1d6-dde3d27a2169

--- src/prerna/util/PortAllocator.java
+++ src/prerna/util/PortAllocator.java
@@ -91,7 +91,7 @@ public class PortAllocator {
 	 * @param port
 	 * @return
 	 */
-	public boolean isPortAvailable(int port) {
+	public static boolean isPortAvailable(int port) {
 		try (ServerSocket ignored = new ServerSocket(port)) {
 			classLogger.info("Port " + port + " is available");
 			return true;
