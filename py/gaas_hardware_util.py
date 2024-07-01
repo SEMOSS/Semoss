c4338ffd6d0f67e78c91c66b0d913795a2c23bf4
kunalppatel9 <kunalppatel9@deloitte.com>
2024-07-01 14:18:22 +0000

    BUGFIX ::: Contrast - py patch
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@51032 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a16562211493859609d6a323ff0b149cfd3bcf25

--- py/gaas_hardware_util.py
+++ py/gaas_hardware_util.py
@@ -8,160 +8,168 @@ import re
 # sourced from - https://www.thepythoncode.com/article/get-hardware-system-information-python
 class HardwareUtil():
 
-  def __init__(self):
-    self.stats = {}
-  
-  def get_all(self):
-    self.stats = {}
-    self.get_storage()
-    self.get_gpu()
-    self.get_cpu()
-    return self.stats
-    
-  # need to do for specific partition
-  def get_storage(self):
-    storage = {}
-    partitions = psutil.disk_partitions()
-    all_total = 0
-    all_available = 0
-    all_used = 0
-    for partition in partitions:
-      #print(f"=== Device: {partition.device} ===")
-      #print(f"  Mountpoint: {partition.mountpoint}")
-      #print(f"  File system type: {partition.fstype}")
-      try:
-        partition_usage = psutil.disk_usage(partition.mountpoint)
-      except PermissionError:
-        # this can be catched due to the disk that
-        # isn't ready
-        continue
-      #print(f"  Total Size: {self.get_size(partition_usage.total)}")
-      #print(f"  Used: {self.get_size(partition_usage.used)}")
-      #print(f"  Free: {self.get_size(partition_usage.free)}")
-      #print(f"  Percentage: {partition_usage.percent}%")
-      all_total = all_total + partition_usage.total
-      all_used = all_used + partition_usage.used
-      all_available = all_available + partition_usage.free
-      # get IO statistics since boot
-    #disk_io = psutil.disk_io_counters()
-    #print(f"Total read: {self.get_size(disk_io.read_bytes)}")
-    #print(f"Total write: {self.get_size(disk_io.write_bytes)}")    
-    storage = {"total":all_total, "used": all_used, "available": all_available}
-    self.stats.update({"storage": storage})
-    return storage
-    
-  def get_gpu(self): 
-    gpus = GPUtil.getGPUs()
-    list_gpus = []
-    all_total = 0
-    all_used = 0
-    all_available = 0
-    gpu_data = {}
-    for gpu in gpus:
-      # get the GPU id
-      gpu_id = gpu.id
-      # name of GPU
-      gpu_name = gpu.name
-      # get % percentage of GPU usage of that GPU
-      #gpu_load = f"{gpu.load*100}%"
-      # get free memory in MB format
-      gpu_free_memory = gpu.memoryFree
-      # get used memory
-      gpu_used_memory = gpu.memoryUsed
-      # get total memory
-      gpu_total_memory = gpu.memoryTotal
-      # get GPU temperature in Celsius
-      gpu_temperature = f"{gpu.temperature} °C"
-      gpu_uuid = gpu.uuid
-      
-      # add all details
-      all_total = all_total + gpu_total_memory
-      all_used = all_used + gpu_used_memory
-      all_available = all_available + gpu_free_memory
-      
-      #list_gpus.append((
-      #    gpu_id, gpu_name, gpu_load, gpu_free_memory, gpu_used_memory,
-      #    gpu_total_memory, gpu_temperature, gpu_uuid
-      #))
-      gpu_details = {"name":gpu_name,  "total": gpu_total_memory, "available":gpu_free_memory, "used":gpu_used_memory, "temperature":gpu_temperature}
-      gpu_data.update({gpu_id:gpu_details})
-    
-    # sort the info (highest to lowest)
-    gpu_data = {'gpus': {k: v for k, v in sorted(gpu_data.items(), key = lambda item: item[1]['available'], reverse= True)}}
+    def __init__(self):
+        self.stats = {}
 
-    gpu_data.update({"total":all_total})
-    gpu_data.update({"available":all_available})
-    gpu_data.update({"used":all_used})
-    
-    self.stats.update({"gpu": gpu_data})
-    
-    return gpu
-  
-  def get_cpu(self):
-    # let's print CPU information
-    cpu = {}
-    
-    #print("="*40, "CPU Info", "="*40)
-    # number of cores
-    cpu.update({"physical_cores":psutil.cpu_count(logical=False)})
-    cpu.update({"total_cores":psutil.cpu_count(logical=True)})
-    #print("Physical cores:", psutil.cpu_count(logical=False))
-    #print("Total cores:", psutil.cpu_count(logical=True))
-    # CPU frequencies
-    #cpufreq = psutil.cpu_freq()
-    #print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
-    #print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
-    #print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
-    # CPU usage
-    #print("CPU Usage Per Core:")
-    for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
-      core = f"core_{i}"
-      cpu.update({core:percentage})
-      print(f"Core {i}: {percentage}%")
-    print(f"Total CPU Usage: {psutil.cpu_percent()}%")
-    self.stats.update({"cpu": cpu})
-    
- 
-  
-  def convert_to_bytes(self, bytes, suffix="B"):
-    factor = 1024
-    units = ["B", "K", "M", "G", "T", "P"]
-    if suffix in units:
-      multiplier = units.index(suffix)
-      value = bytes * (1024**multiplier)
-      return value
-    else:
-      return bytes
-      
-  def parse_to_bytes(self, readable_value):
-    numeric_value = float(re.findall(r"[-+]?(?:\d*\.*\d+)", readable_value)[0])
-    alpha_value = ''.join(x for x in readable_value if x.isalpha())[0]
-    return self.convert_to_bytes(numeric_value, alpha_value)
-  
-  def get_size(self, val, suffix="B"):
-    """
-    Scale bytes to its proper format
-    e.g:
-        1253656 => '1.20MB'
-        1253656678 => '1.17GB'
-    """
-    factor = 1024
-    for unit in ["", "K", "M", "G", "T", "P"]:
-        if val < factor:
-            return f"{val:.2f}{unit}{suffix}"
-        val /= factor
+    def get_all(self):
+        self.stats = {}
+        self.get_storage()
+        self.get_gpu()
+        self.get_cpu()
+        return self.stats
 
-  def find_available_gpus(self, memory_needed):
-    self.get_gpu()
-    
-    gpu_to_allocate = []
-    if (memory_needed >= self.stats['gpu']['available']):
+    # need to do for specific partition
+    def get_storage(self):
+        storage = {}
+        partitions = psutil.disk_partitions()
+        all_total = 0
+        all_available = 0
+        all_used = 0
+        for partition in partitions:
+            # print(f"=== Device: {partition.device} ===")
+            # print(f"  Mountpoint: {partition.mountpoint}")
+            # print(f"  File system type: {partition.fstype}")
+            try:
+                partition_usage = psutil.disk_usage(partition.mountpoint)
+            except PermissionError:
+                # this can be catched due to the disk that
+                # isn't ready
+                continue
+            # print(f"  Total Size: {self.get_size(partition_usage.total)}")
+            # print(f"  Used: {self.get_size(partition_usage.used)}")
+            # print(f"  Free: {self.get_size(partition_usage.free)}")
+            # print(f"  Percentage: {partition_usage.percent}%")
+            all_total = all_total + partition_usage.total
+            all_used = all_used + partition_usage.used
+            all_available = all_available + partition_usage.free
+            # get IO statistics since boot
+        # disk_io = psutil.disk_io_counters()
+        # print(f"Total read: {self.get_size(disk_io.read_bytes)}")
+        # print(f"Total write: {self.get_size(disk_io.write_bytes)}")
+        storage = {"total": all_total, "used": all_used,
+                   "available": all_available}
+        self.stats.update({"storage": storage})
+        return storage
+
+    def get_gpu(self):
+        gpus = GPUtil.getGPUs()
+        list_gpus = []
+        all_total = 0
+        all_used = 0
+        all_available = 0
+        gpu_data = {}
+        for gpu in gpus:
+            # get the GPU id
+            gpu_id = gpu.id
+            # name of GPU
+            gpu_name = gpu.name
+            # get % percentage of GPU usage of that GPU
+            # gpu_load = f"{gpu.load*100}%"
+            # get free memory in MB format
+            gpu_free_memory = gpu.memoryFree
+            # get used memory
+            gpu_used_memory = gpu.memoryUsed
+            # get total memory
+            gpu_total_memory = gpu.memoryTotal
+            # get GPU temperature in Celsius
+            gpu_temperature = f"{gpu.temperature} °C"
+            gpu_uuid = gpu.uuid
+
+            # add all details
+            all_total = all_total + gpu_total_memory
+            all_used = all_used + gpu_used_memory
+            all_available = all_available + gpu_free_memory
+
+            # list_gpus.append((
+            #    gpu_id, gpu_name, gpu_load, gpu_free_memory, gpu_used_memory,
+            #    gpu_total_memory, gpu_temperature, gpu_uuid
+            # ))
+            gpu_details = {"name": gpu_name,  "total": gpu_total_memory,
+                           "available": gpu_free_memory, "used": gpu_used_memory, "temperature": gpu_temperature}
+            gpu_data.update({gpu_id: gpu_details})
+
+        # sort the info (highest to lowest)
+        gpu_data = {'gpus': {k: v for k, v in sorted(
+            gpu_data.items(), key=lambda item: item[1]['available'], reverse=True)}}
+
+        gpu_data.update({"total": all_total})
+        gpu_data.update({"available": all_available})
+        gpu_data.update({"used": all_used})
+
+        self.stats.update({"gpu": gpu_data})
+
+        return gpu
+
+    def get_cpu(self):
+        # let's print CPU information
+        cpu = {}
+
+        # print("="*40, "CPU Info", "="*40)
+        # number of cores
+        cpu.update({"physical_cores": psutil.cpu_count(logical=False)})
+        cpu.update({"total_cores": psutil.cpu_count(logical=True)})
+        # print("Physical cores:", psutil.cpu_count(logical=False))
+        # print("Total cores:", psutil.cpu_count(logical=True))
+        # CPU frequencies
+        # cpufreq = psutil.cpu_freq()
+        # print(f"Max Frequency: {cpufreq.max:.2f}Mhz")
+        # print(f"Min Frequency: {cpufreq.min:.2f}Mhz")
+        # print(f"Current Frequency: {cpufreq.current:.2f}Mhz")
+        # CPU usage
+        # print("CPU Usage Per Core:")
+        for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
+            core = f"core_{i}"
+            cpu.update({core: percentage})
+            print(f"Core {i}: {percentage}%")
+        print(f"Total CPU Usage: {psutil.cpu_percent()}%")
+        self.stats.update({"cpu": cpu})
+
+    def convert_to_bytes(self, bytes, suffix="B"):
+        factor = 1024
+        units = ["B", "K", "M", "G", "T", "P"]
+        if suffix in units:
+            multiplier = units.index(suffix)
+            value = bytes * (1024**multiplier)
+            return value
+        else:
+            return bytes
+
+    def parse_to_bytes(self, readable_value):
+        try:
+            numeric_value = float(re.findall(
+                r"[-+]?\d*\.?\d+", readable_value)[0])
+            alpha_value = ''.join(x for x in readable_value if x.isalpha())[0]
+            return self.convert_to_bytes(numeric_value, alpha_value)
+        except IndexError:
+            raise ValueError(
+                "Invalid input format: unable to parse numeric value")
+        except Exception as e:
+            raise ValueError(f"An error occurred: {e}")
+
+    def get_size(self, val, suffix="B"):
+        """
+        Scale bytes to its proper format
+        e.g:
+            1253656 => '1.20MB'
+            1253656678 => '1.17GB'
+        """
+        factor = 1024
+        for unit in ["", "K", "M", "G", "T", "P"]:
+            if val < factor:
+                return f"{val:.2f}{unit}{suffix}"
+            val /= factor
+
+    def find_available_gpus(self, memory_needed):
+        self.get_gpu()
+
+        gpu_to_allocate = []
+        if (memory_needed >= self.stats['gpu']['available']):
+            return gpu_to_allocate
+
+        for k, v in self.stats['gpu']['gpus'].items():
+            memory_needed -= v['available']
+            gpu_to_allocate.append(k)
+            if (memory_needed < 0):
+                break
+
         return gpu_to_allocate
-    
-    for k,v in self.stats['gpu']['gpus'].items():
-        memory_needed -= v['available']
-        gpu_to_allocate.append(k)
-        if (memory_needed < 0):
-            break
-
-    return gpu_to_allocate
--- py/genai_client/text_generation/google_clients/vertex_text_client.py
+++ py/genai_client/text_generation/google_clients/vertex_text_client.py
@@ -9,13 +9,14 @@ from ...constants import (
     FULL_PROMPT
 )
 
+
 class VertexTextClient(AbstractVertextAiTextGeneration):
-    
+
     def _get_client(
         self
     ):
         return TextGenerationModel.from_pretrained(self.model_name)
-    
+
     def ask_call(
         self,
         question: str = None,
@@ -30,17 +31,17 @@ class VertexTextClient(AbstractVertextAiTextGeneration):
         **kwargs
     ) -> AskModelEngineResponse:
         assert self.client != None
-        
+
         chat = None
         if FULL_PROMPT in kwargs.keys():
             full_prompt = kwargs.pop(FULL_PROMPT)
 
             # make sure the full prompt param is an even list
             assert len(full_prompt) % 2 != 0
-            
+
             # pull out the last message
             last_msg = full_prompt[-1]
-            
+
             if isinstance(last_msg, dict):
                 question = last_msg.get('content')
                 history = full_prompt[:-1]
@@ -48,46 +49,51 @@ class VertexTextClient(AbstractVertextAiTextGeneration):
                 question = last_msg
                 history = []
             else:
-                raise TypeError("Unable to extract the question from full prompt list")
-            
+                raise TypeError(
+                    "Unable to extract the question from full prompt list")
+
         # build the message chain
         prompt = ''
         author = ''
         try:
-            prompt +=  context or ''
+            prompt += context or ''
             for msg in history:
                 author = msg.get('author', msg['role']) + ':\n'
-                content= msg['content']
-                
+                content = msg['content']
+
                 prompt += author
                 prompt += content
-                
+
                 prompt += '\n\n'
-                
+
             prompt += author
             prompt += question
         except KeyError:
-            raise KeyError("Unable to determine author of the message. No 'author' or 'role' provided.")
-              
+            raise KeyError(
+                "Unable to determine author of the message. No 'author' or 'role' provided.")
+
         # convert ask inputs to vertex ai params
         parameters = {
             "prompt": prompt,
-            "temperature": temperature,  # Temperature controls the degree of randomness in token selection.
-            "max_output_tokens": max_new_tokens,  # Token limit determines the maximum amount of text output.
-            "top_p": top_p,  # Tokens are selected from most probable to least until the sum of their probabilities equals the top_p value.
-            "top_k": top_k,  # A top_k of 1 means the selected token is the most probable among all tokens.
+            # Temperature controls the degree of randomness in token selection.
+            "temperature": temperature,
+            # Token limit determines the maximum amount of text output.
+            "max_output_tokens": max_new_tokens,
+            # Tokens are selected from most probable to least until the sum of their probabilities equals the top_p value.
+            "top_p": top_p,
+            # A top_k of 1 means the selected token is the most probable among all tokens.
+            "top_k": top_k,
             "stop_sequences": stop_sequences,
         }
-        
+
         responses = self.client.predict_streaming(
             **parameters
         )
-        
+
         final_response = ''
         for response in responses:
             final_response += response.text
-            print(prefix + response.text, end ='')
-            
+
         model_engine_response = AskModelEngineResponse(response=final_response)
-        
+
         return model_engine_response
