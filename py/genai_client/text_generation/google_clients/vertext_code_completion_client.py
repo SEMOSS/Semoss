995acb91b07afb582319f2a62db6602b67905064
kunalppatel9 <kunalppatel9@deloitte.com>
2024-07-01 14:18:22 +0000

    BUGFIX ::: Contrast - py patch
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@51032 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: a16562211493859609d6a323ff0b149cfd3bcf25

--- py/genai_client/text_generation/google_clients/vertext_code_completion_client.py
+++ py/genai_client/text_generation/google_clients/vertext_code_completion_client.py
@@ -9,13 +9,14 @@ from ...constants import (
     FULL_PROMPT
 )
 
+
 class VertexCodeCompletionClient(AbstractVertextAiTextGeneration):
-    
+
     def _get_client(
         self
     ):
         return CodeGenerationModel.from_pretrained(self.model_name)
-    
+
     def ask_call(
         self,
         question: str = None,
@@ -27,17 +28,17 @@ class VertexCodeCompletionClient(AbstractVertextAiTextGeneration):
         **kwargs
     ) -> AskModelEngineResponse:
         assert self.client != None
-        
+
         chat = None
         if FULL_PROMPT in kwargs.keys():
             full_prompt = kwargs.pop(FULL_PROMPT)
 
             # make sure the full prompt param is an even list
             assert len(full_prompt) % 2 != 0
-            
+
             # pull out the last message
             last_msg = full_prompt[-1]
-            
+
             if isinstance(last_msg, dict):
                 question = last_msg.get('content')
                 history = full_prompt[:-1]
@@ -45,45 +46,48 @@ class VertexCodeCompletionClient(AbstractVertextAiTextGeneration):
                 question = last_msg
                 history = []
             else:
-                raise TypeError("Unable to extract the question from full prompt list")
-            
+                raise TypeError(
+                    "Unable to extract the question from full prompt list")
+
         # build the message chain
         prompt = ''
         author = ''
         try:
-            prompt +=  context or ''
+            prompt += context or ''
             for msg in history:
                 author = msg.get('author', msg['role']) + ':\n'
-                content= msg['content']
-                
+                content = msg['content']
+
                 prompt += author
                 prompt += content
-                
+
                 prompt += '\n\n'
-                
+
             prompt += author
             prompt += question
         except KeyError:
-            raise KeyError("Unable to determine author of the message. No 'author' or 'role' provided.")
-              
+            raise KeyError(
+                "Unable to determine author of the message. No 'author' or 'role' provided.")
+
         # convert ask inputs to vertex ai params
         parameters = {
             "prefix": prompt,
-            "temperature": temperature,  # Temperature controls the degree of randomness in token selection.
-            "max_output_tokens": max_new_tokens,  # Token limit determines the maximum amount of text output.
+            # Temperature controls the degree of randomness in token selection.
+            "temperature": temperature,
+            # Token limit determines the maximum amount of text output.
+            "max_output_tokens": max_new_tokens,
         }
-                
+
         responses = self.client.predict_streaming(
             **parameters
         )
-        
+
         final_response = ''
         for response in responses:
             final_response += response.text
-            print(prefix + response.text, end ='')
-            
+
         model_engine_response = AskModelEngineResponse(
             response=final_response
         )
-        
+
         return model_engine_response
