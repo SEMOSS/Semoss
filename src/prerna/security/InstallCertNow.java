390aeeb508ee3585d0dc0af231e9ef59a9eaffb9
kunalppatel9 <kunalppatel9@deloitte.com>
2024-07-02 13:00:10 +0000

    BUGFIX ::: Contrast - cyrpto hash change
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@51050 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: 47d09f83f30b6753641690750b4d8a2e06451da7

--- src/prerna/security/InstallCertNow.java
+++ src/prerna/security/InstallCertNow.java
@@ -65,132 +65,128 @@ public class InstallCertNow {
 			logger.error(STACKTRACE, e);
 		}
 	}
-
 	public static void please(String site, String javaLoc, String pass) throws Exception {
-		// need a couple of different parameters
-		// the URL for the site to get the certificate from
-		// the location of java
-		// and possibly the passphrase
-		// java home looks like this - C:\Java\jdk1.8.0_161
+		  // need a couple of different parameters
+		  // the URL for the site to get the certificate from
+		  // the location of java
+		  // and possibly the passphrase
+		  // java home looks like this - C:\Java\jdk1.8.0_161
 
-		if(javaLoc == null)
-			// see if you can get it from environment variable
-			javaLoc = System.getenv("JAVA_HOME");
+		  if(javaLoc == null)
+		    // see if you can get it from environment variable
+		    javaLoc = System.getenv("JAVA_HOME");
 
-		if(site == null || javaLoc == null)
-			return;
+		  if(site == null || javaLoc == null)
+		    return;
 
-		// mod the java locl
-		javaLoc = Utility.normalizePath(javaLoc) + "/jre/lib/security/cacerts";
+		  // mod the java locl
+		  javaLoc = Utility.normalizePath(javaLoc) + "/jre/lib/security/cacerts";
 
-		if(pass == null)
-			pass = "changeit";
+		  if(pass == null)
+		    pass = "changeit";
 
-		String host;
-		int port;
-		char[] passphrase;
-		String[] c = site.split(":");
-		host = c[0];
-		port = (c.length == 1) ? 443 : Integer.parseInt(c[1]);
-		passphrase = pass.toCharArray();
+		  String host;
+		  int port;
+		  char[] passphrase;
+		  String[] c = site.split(":");
+		  host = c[0];
+		  port = (c.length == 1) ? 443 : Integer.parseInt(c[1]);
+		  passphrase = pass.toCharArray();
 
-		File file = new File(javaLoc);
+		  File file = new File(javaLoc);
 
-		logger.debug("Loading KeyStore " + file + "...");
-		InputStream in = null;
-		SSLSocket socket = null;
-		try {
-			in = new FileInputStream(file);
-			KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
-			ks.load(in, passphrase);
-			in.close();
+		  logger.debug("Loading KeyStore " + file + "...");
+		  InputStream in = null;
+		  SSLSocket socket = null;
+		  try {
+		    in = new FileInputStream(file);
+		    KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+		    ks.load(in, passphrase);
+		    in.close();
 
-			SSLContext context = SSLContext.getInstance("TLSv1.2");
-			TrustManagerFactory tmf =
-					TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+		    SSLContext context = SSLContext.getInstance("TLSv1.2");
+		    TrustManagerFactory tmf =
+		        TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
 
-			tmf.init(ks);
-			X509TrustManager defaultTrustManager = (X509TrustManager)tmf.getTrustManagers()[0];
-			SavingTrustManager tm = new SavingTrustManager(defaultTrustManager);
-			context.init(null, new TrustManager[] {tm}, null);
-			SSLSocketFactory factory = context.getSocketFactory();
+		    tmf.init(ks);
+		    X509TrustManager defaultTrustManager = (X509TrustManager)tmf.getTrustManagers()[0];
+		    SavingTrustManager tm = new SavingTrustManager(defaultTrustManager);
+		    context.init(null, new TrustManager[] {tm}, null);
+		    SSLSocketFactory factory = context.getSocketFactory();
 
-			logger.debug("Opening connection to " + host + ":" + port + "...");
-			socket = (SSLSocket)factory.createSocket(host, port);
-			socket.setSoTimeout(10000);
+		    logger.debug("Opening connection to " + host + ":" + port + "...");
+		    socket = (SSLSocket)factory.createSocket(host, port);
+		    socket.setSoTimeout(10000);
 
-			try {
-				logger.debug("Starting SSL handshake...");
-				socket.startHandshake();
-				socket.close();
-				logger.debug("No errors, certificate is already trusted");
-			} catch (SSLException e) {
-				logger.error(STACKTRACE, e);
-			}
+		    try {
+		      logger.debug("Starting SSL handshake...");
+		      socket.startHandshake();
+		      socket.close();
+		      logger.debug("No errors, certificate is already trusted");
+		    } catch (SSLException e) {
+		      logger.error(STACKTRACE, e);
+		    }
 
-			X509Certificate[] chain = tm.chain;
-			if (chain == null) {
-				logger.warn("Could not obtain server certificate chain");
-				return;
-			}
+		    X509Certificate[] chain = tm.chain;
+		    if (chain == null) {
+		      logger.warn("Could not obtain server certificate chain");
+		      return;
+		    }
 
-			logger.debug("Server sent " + chain.length + " certificate(s):");
-			MessageDigest sha1 = MessageDigest.getInstance("SHA1");
-			MessageDigest md5 = MessageDigest.getInstance("MD5");
-			for (int i = 0; i < chain.length; i++) {
-				X509Certificate cert = chain[i];
-				logger.debug(" " + (i + 1) + " Subject " + cert.getSubjectDN());
-				logger.debug("   Issuer  " + cert.getIssuerDN());
-				sha1.update(cert.getEncoded());
-				logger.debug("   sha1    " + toHexString(sha1.digest()));
-				md5.update(cert.getEncoded());
-				logger.debug("   md5     " + toHexString(md5.digest()));
-			}
+		    logger.debug("Server sent " + chain.length + " certificate(s):");
+		    MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+		    for (int i = 0; i < chain.length; i++) {
+		      X509Certificate cert = chain[i];
+		      logger.debug(" " + (i + 1) + " Subject " + cert.getSubjectDN());
+		      logger.debug("   Issuer  " + cert.getIssuerDN());
+		      sha256.update(cert.getEncoded());
+		      logger.debug("   sha256  " + toHexString(sha256.digest()));
+		    }
 
-			String line = "1";
-			int k;
-			try {
-				k = (line.length() == 0) ? 0 : Integer.parseInt(line) - 1;
-			} catch (NumberFormatException e) {
-				logger.error("KeyStore not changed - StackTrace: ", e);
-				return;
-			}
+		    String line = "1";
+		    int k;
+		    try {
+		      k = (line.length() == 0) ? 0 : Integer.parseInt(line) - 1;
+		    } catch (NumberFormatException e) {
+		      logger.error("KeyStore not changed - StackTrace: ", e);
+		      return;
+		    }
 
-			X509Certificate cert = chain[k];
-			String alias = host + "-" + (k + 1);
+		    X509Certificate cert = chain[k];
+		    String alias = host + "-" + (k + 1);
 
-			// there can't be more than 20 netskope hosts for the cert
-			for(int i = 0;i < 20 && ks.containsAlias(alias);i++)
-			{
-				alias = host + "-" + i;
-			}
-			ks.setCertificateEntry(alias, cert);
+		    // there can't be more than 20 netskope hosts for the cert
+		    for(int i = 0;i < 20 && ks.containsAlias(alias);i++)
+		    {
+		      alias = host + "-" + i;
+		    }
+		    ks.setCertificateEntry(alias, cert);
 
+		    OutputStream out = new FileOutputStream(javaLoc);
+		    ks.store(out, passphrase);
+		    out.close();
 
-			OutputStream out = new FileOutputStream(javaLoc);
-			ks.store(out, passphrase);
-			out.close();
+		    logger.debug(cert);
+		    logger.debug("Added certificate to keystore 'jssecacerts' using alias '" + alias + "'");
+		  } finally {
+		    try {
+		      if(in != null) {
+		        in.close();
+		      }
+		    } catch (IOException e) {
+		      e.printStackTrace();
+		    }
 
-			logger.debug(cert);
-			logger.debug("Added certificate to keystore 'jssecacerts' using alias '" + alias + "'");
-		} finally {
-			try {
-				if(in != null) {
-					in.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			try {
-				if(socket != null) {
-					socket.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		    try {
+		      if(socket != null) {
+		        socket.close();
+		      }
+		    } catch (IOException e) {
+		      e.printStackTrace();
+		    }
+		  }
 		}
-	}
+
 
 	private static final char[] HEXDIGITS = "0123456789abcdef".toCharArray();
 
