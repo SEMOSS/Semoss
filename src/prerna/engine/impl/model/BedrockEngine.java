d2b9004ad73d3bba365bb3b743426ec7c0f5ed03
mahkhalil <mahkhalil@deloitte.com>
2024-07-12 20:33:38 +0000

    UPDATE ::: Adding in update to bedrock client to use converse library UPDATE ::: Adding in summarize method for bedrock
    git-svn-id: svn://107.170.10.188:8080/Semoss/dev@51108 ef391c1c-ee15-45f7-b1d6-dde3d27a2169
    
    Former-commit-id: fa1f01615c5fcd65185282014acb6aa2e4371b68

--- src/prerna/engine/impl/model/BedrockEngine.java
+++ src/prerna/engine/impl/model/BedrockEngine.java
@@ -1,12 +1,50 @@
 package prerna.engine.impl.model;
 
 
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import prerna.ds.py.PyUtils;
 import prerna.engine.api.ModelTypeEnum;
+import prerna.engine.impl.model.responses.AskModelEngineResponse;
+import prerna.om.Insight;
 
 public class BedrockEngine extends AbstractPythonModelEngine {
+	
+	private static final Logger classLogger = LogManager.getLogger(BedrockEngine.class);
 
 	@Override
 	public ModelTypeEnum getModelType() {
 		return ModelTypeEnum.BEDROCK;
 	}
+	
+	/**
+	 * 
+	 * @param filePath
+	 * @param insight
+	 * @param parameters
+	 * @return
+	 */
+	public AskModelEngineResponse summarize(String filePath, Insight insight, Map<String, Object> parameters) {
+		checkSocketStatus();
+		
+		StringBuilder callMaker = new StringBuilder(this.varName + ".summarize(");	
+		if (filePath != null) {
+			callMaker.append("file_path")
+					 .append("=")
+					 .append(PyUtils.determineStringType(filePath));
+		} else {
+			throw new IllegalArgumentException("No file path given to summarize");
+		}
+		
+		callMaker.append(")");
+		classLogger.debug("Running >>>" + callMaker.toString());
+		
+		Object output = pyt.runScript(callMaker.toString(), insight);
+		AskModelEngineResponse response = AskModelEngineResponse.fromObject(output);
+		return response;
+	}
+	
 }
