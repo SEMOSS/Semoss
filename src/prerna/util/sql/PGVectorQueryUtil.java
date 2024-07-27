de7843472afe05819f4d6247d4fbc23c4b0b6e8d
Maher Khalil <themaherkhalil@gmail.com>
2024-07-27 13:47:22 -0400

    UPDATE ::: removing unused methods

--- src/prerna/util/sql/PGVectorQueryUtil.java
+++ src/prerna/util/sql/PGVectorQueryUtil.java
@@ -1,16 +1,7 @@
 package prerna.util.sql;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import prerna.util.Constants;
 
 public class PGVectorQueryUtil extends PostgresQueryUtil {
 	
@@ -41,56 +32,4 @@ public class PGVectorQueryUtil extends PostgresQueryUtil {
 		return "CREATE EXTENSION IF NOT EXISTS vector;";
 	}
 
-	public String insertEmbeddings(Connection con, String tableName, Map<Integer, Map<String, Object>> data) {
-		Map<String, Object> rowData = data.get(0);
-		Map<String, String> row = (Map<String, String>) rowData.get("data");
-
-		String columnString = "embedding";
-		for (String column : row.keySet()) {
-			columnString += ", " + column + "";
-		}
-		
-		String insertString = "";
-		int colNum = row.keySet().size();
-		for (int i = 0; i<colNum; i++) {
-			insertString+="?";
-		}
-        String sql = "INSERT INTO "+tableName+" ("+columnString+") VALUES ("+insertString+")";
-        try {
-			PreparedStatement statement = con.prepareStatement(sql);
-
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			classLogger.error(Constants.STACKTRACE, e1);
-		}
-        
-		String valueString = "";
-		for (int i = 0; i < data.size(); i++) {
-			Map<String, Object> r = data.get(i);
-			Map<String, String> e = (Map<String, String>) r.get("data");
-			Object embedding = r.get("embedding");
-			if (e == null || e.size() == 0 || embedding == null) //[1,2,3]
-				continue;
-			if (i != 0)
-				valueString += ",";
-			String tempValueHolder = "('" + embedding+"'";
-			for (Entry<String, String> entry : e.entrySet()) {
-				String val = entry.getValue();
-				String newVal = StringEscapeUtils.escapeJava(val);
-				tempValueHolder += ", '" + newVal +"'";
-			}
-			valueString += tempValueHolder + ")";
-			if(i==data.size()-1) {
-				valueString += ";";
-			}
-		}
-
-		valueString += ";";
-
-		return "INSERT INTO "+ tableName+ " (" + columnString + ") VALUES" + valueString;
-	}
-	
-	public String removeEmbeddings(String tableName, String documentName) {
-		return "DELETE from \""+tableName+"\" where \"source\"='"+documentName+"'";
-	}
 }
