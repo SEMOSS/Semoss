082602c6f1b9e4b1bc730c53364d48c33345d76f
Maher Khalil <themaherkhalil@gmail.com>
2024-07-27 21:42:01 -0400

    UPDATE ::: Adding in metadata table creation

--- src/prerna/util/sql/PGVectorQueryUtil.java
+++ src/prerna/util/sql/PGVectorQueryUtil.java
@@ -17,14 +17,28 @@ public class PGVectorQueryUtil extends PostgresQueryUtil {
 
 	public String createEmbeddingsTable(String table) {
 		return "CREATE TABLE IF NOT EXISTS "+table+"("
-				+ "ID SERIAL PRIMARY KEY, "
-				+ "EMBEDDING vector, "
-				+ "SOURCE text, "
-				+ "MODALITY text, "
-				+ "DIVIDER text, "
-				+ "PART text, "
-				+ "TOKENS text, "
-				+ "CONTENT text "
+				+ "ID INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, "
+				+ "EMBEDDING VECTOR, "
+				+ "SOURCE TEXT, "
+				+ "MODALITY TEXT, "
+				+ "DIVIDER TEXT, "
+				+ "PART TEXT, "
+				+ "TOKENS TEXT, "
+				+ "CONTENT TEXT "
+				+ ");";
+	}
+	
+	public String createEmbeddingsMetadataTable(String table) {
+		return "CREATE TABLE IF NOT EXISTS "+table+"("
+				+ "ID INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, "
+				+ "SOURCE TEXT, "
+				+ "ATTRIBUTE TEXT, "
+				+ "STR_VALUE TEXT, "
+				+ "INT_VALUE INTEGER, "
+				+ "NUM_VALUE NUMERIC(18,4), "
+				+ "BOOL_VALUE BOOLEAN, "
+				+ "DATE_VAL DATE, "
+				+ "TIMESTAMP_VAL TIMESTAMP "
 				+ ");";
 	}
 
