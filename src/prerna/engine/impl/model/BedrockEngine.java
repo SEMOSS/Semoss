bcdd9e59350f95806ce979645cc20ea8949e2539
Michael Moore <michaemoore@deloitte.com>
2024-09-12 16:40:02 -0400

    Added embeddings_call to bedrock client

--- src/prerna/engine/impl/model/BedrockEngine.java
+++ src/prerna/engine/impl/model/BedrockEngine.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.Logger;
 import prerna.ds.py.PyUtils;
 import prerna.engine.api.ModelTypeEnum;
 import prerna.engine.impl.model.responses.AskModelEngineResponse;
+import prerna.engine.impl.model.responses.EmbeddingsModelEngineResponse;
 import prerna.om.Insight;
 
 public class BedrockEngine extends AbstractPythonModelEngine {
@@ -37,6 +38,32 @@ public class BedrockEngine extends AbstractPythonModelEngine {
 					 .append(PyUtils.determineStringType(filePath));
 		} else {
 			throw new IllegalArgumentException("No file path given to summarize");
+		}
+		
+		callMaker.append(")");
+		classLogger.debug("Running >>>" + callMaker.toString());
+		
+		Object output = pyt.runScript(callMaker.toString(), insight);
+		AskModelEngineResponse response = AskModelEngineResponse.fromObject(output);
+		return response;
+	}
+
+	/**
+	 * 
+	 * @param filePath
+	 * @param insight
+	 * @return
+	 */
+	public EmbeddingsModelEngineResponse embeddingsCall(List<String> stringsToEmbed, Insight insight) {
+		checkSocketStatus();
+		
+		StringBuilder callMaker = new StringBuilder(this.varName + ".embeddings_call(");	
+		if (stringsToEmbed != null && !stringsToEmbed.isEmpty()) {
+			callMaker.append("strings_to_embed")
+					 .append("=")
+					 .append(PyUtils.determineStringType(stringsToEmbed));
+		} else {
+			throw new IllegalArgumentException("Nothing given to embed");
 		}
 		
 		callMaker.append(")");
