baf8c34f760df3c247147a0b954cca112e3584c5
Maher Khalil <themaherkhalil@gmail.com>
2024-10-02 14:23:49 -0400

    BUGFIX ::: Adding order by so limit/offset works as expected

--- src/prerna/auth/utils/SecurityEngineUtils.java
+++ src/prerna/auth/utils/SecurityEngineUtils.java
@@ -1727,6 +1727,9 @@ public class SecurityEngineUtils extends AbstractSecurityUtils {
 			or.addFilter(SimpleQueryFilter.makeColToValFilter("SMSS_USER__EMAIL", "?like", searchTerm));
 			qs.addExplicitFilter(or);
 		}
+		qs.addOrderBy(new QueryColumnOrderBySelector("SMSS_USER__NAME"));
+		qs.addOrderBy(new QueryColumnOrderBySelector("SMSS_USER__EMAIL"));
+		qs.addOrderBy(new QueryColumnOrderBySelector("SMSS_USER__ID"));
 		if(limit > 0) {
 			qs.setLimit(limit);
 		}
