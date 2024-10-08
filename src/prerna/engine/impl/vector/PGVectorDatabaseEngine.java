34c71904ee1eea736c759ce7b50889a02584fef5
Khumar23 <manojkannaya4@gmail.com>
2024-10-08 19:19:20 +0530

    Refactor code formatting for distance method selection logic

--- src/prerna/engine/impl/vector/PGVectorDatabaseEngine.java
+++ src/prerna/engine/impl/vector/PGVectorDatabaseEngine.java
@@ -610,22 +610,16 @@ public class PGVectorDatabaseEngine extends RDBMSNativeEngine implements IVector
 //		final String metaTablePrefix = this.vectorTableMetadataName+"__";
 		
 		SelectQueryStruct qs = new SelectQueryStruct();
-		qs.addSelector(
-				new QueryColumnSelector(tablePrefix + VectorDatabaseCSVTable.SOURCE, VectorDatabaseCSVTable.SOURCE));
-		qs.addSelector(new QueryColumnSelector(tablePrefix + VectorDatabaseCSVTable.MODALITY,
-				VectorDatabaseCSVTable.MODALITY));
-		qs.addSelector(
-				new QueryColumnSelector(tablePrefix + VectorDatabaseCSVTable.DIVIDER, VectorDatabaseCSVTable.DIVIDER));
-		qs.addSelector(new QueryColumnSelector(tablePrefix + VectorDatabaseCSVTable.PART, VectorDatabaseCSVTable.PART));
-		qs.addSelector(
-				new QueryColumnSelector(tablePrefix + VectorDatabaseCSVTable.TOKENS, VectorDatabaseCSVTable.TOKENS));
-		qs.addSelector(
-				new QueryColumnSelector(tablePrefix + VectorDatabaseCSVTable.CONTENT, VectorDatabaseCSVTable.CONTENT));
+		qs.addSelector(new QueryColumnSelector(tablePrefix+VectorDatabaseCSVTable.SOURCE, VectorDatabaseCSVTable.SOURCE));
+		qs.addSelector(new QueryColumnSelector(tablePrefix+VectorDatabaseCSVTable.MODALITY, VectorDatabaseCSVTable.MODALITY));
+		qs.addSelector(new QueryColumnSelector(tablePrefix+VectorDatabaseCSVTable.DIVIDER, VectorDatabaseCSVTable.DIVIDER));
+		qs.addSelector(new QueryColumnSelector(tablePrefix+VectorDatabaseCSVTable.PART, VectorDatabaseCSVTable.PART));
+		qs.addSelector(new QueryColumnSelector(tablePrefix+VectorDatabaseCSVTable.TOKENS, VectorDatabaseCSVTable.TOKENS));
+		qs.addSelector(new QueryColumnSelector(tablePrefix+VectorDatabaseCSVTable.CONTENT, VectorDatabaseCSVTable.CONTENT));
 		// Determine the distanceMethod to use for the query
 		if ("Cosine Similarity".equalsIgnoreCase(distanceMethod)) {
 			qs.addSelector(new QueryOpaqueSelector("(EMBEDDING <=> '" + embeddingsResponse.getResponse().get(0) + "')",
 					"Score"));// Store the result in the "Score" field, '<=>' cosine similarity operator.
-
 		} else {
 			qs.addSelector(new QueryOpaqueSelector(
 					"POWER((EMBEDDING <-> '" + embeddingsResponse.getResponse().get(0) + "'),2)", "Score"));
