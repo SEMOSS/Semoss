c5c13c89d54261cd9467c310989931a6c6761704
Kunal Patel <kunal0137@gmail.com>
2024-12-04 13:14:54 -0500

    run tcp native as a non root user

--- src/prerna/util/Utility.java
+++ src/prerna/util/Utility.java
@@ -5136,8 +5136,16 @@ public final class Utility {
 			
 			String outputFile = finalDir + "/console.txt";
 			
-			String[] commands = new String[] {py, gaasServer, "--port", port, "--max_count", "1", "--py_folder", pyBase, "--insight_folder", finalDir, "--prefix", prefix, "--timeout", timeout, "--logger_level" , loggerLevel};
-				
+			String pythonUser = Utility.getDIHelperProperty(Settings.NATIVE_PY_SERVER_USER);
+
+			String[] commands;
+						
+			if (pythonUser != null && !(pythonUser.trim().isEmpty()) ) {
+			    commands = new String[] { "sudo", "-u", pythonUser, py, gaasServer, "--port", port, "--max_count", "1", "--py_folder", pyBase, "--insight_folder", finalDir, "--prefix", prefix, "--timeout", timeout, "--logger_level", loggerLevel};
+			} else {
+			    commands = new String[] {py, gaasServer, "--port", port, "--max_count", "1", "--py_folder", pyBase, "--insight_folder", finalDir, "--prefix", prefix, "--timeout", timeout, "--logger_level", loggerLevel};
+			}
+			
 			// need to make sure we are not windows cause ulimit will not work
 			if (!SystemUtils.IS_OS_WINDOWS && !(Strings.isNullOrEmpty(Utility.getDIHelperProperty(Constants.ULIMIT_R_MEM_LIMIT)))){
 				String ulimit = Utility.getDIHelperProperty(Constants.ULIMIT_R_MEM_LIMIT);
