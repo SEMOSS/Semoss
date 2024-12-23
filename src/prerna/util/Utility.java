e3e69b3820e2648067f9c390a6c31ac62b9646c9
Ramesh Paulraj <Ramesh.Paulraj@kanini.com>
2024-12-23 12:30:57 +0530

    Merge of dev to throttle

--- src/prerna/util/Utility.java
+++ src/prerna/util/Utility.java
@@ -16,7 +16,7 @@
  * ----------------------------------------------------------------------------
  * If your use of this software includes any GPLv2 components:
  * 	This program is free software; you can redistribute it and/or
- * 	modify it under the terms of the GNU General Public License
+ * 	modify it under the terms of the GNU General Public License 	
  * 	as published by the Free Software Foundation; either version 2
  * 	of the License, or (at your option) any later version.
  *
