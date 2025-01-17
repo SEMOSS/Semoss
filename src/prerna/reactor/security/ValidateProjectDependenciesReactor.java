f1c63e2ca90894769155d2e1f8347c430eec8016
rithvik-doshi <doshirithvik@gmail.com>
2025-01-17 16:48:40 -0500

    getting started with the reactor

---
+++ src/prerna/reactor/security/ValidateProjectDependenciesReactor.java
@@ -0,0 +1,68 @@
+package prerna.reactor.security;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import prerna.auth.User;
+import prerna.auth.utils.SecurityEngineUtils;
+import prerna.auth.utils.SecurityProjectUtils;
+import prerna.project.api.IProject;
+import prerna.reactor.AbstractReactor;
+import prerna.reactor.project.ExecuteAppNotebookReactor;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.execptions.SemossPixelException;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.util.UploadInputUtility;
+import prerna.util.Utility;
+
+public class ValidateProjectDependenciesReactor extends AbstractReactor {
+	
+	private static final Logger classLogger = LogManager.getLogger(ValidateProjectDependenciesReactor.class);
+	
+	public ValidateProjectDependenciesReactor() {
+		this.keysToGet = new String[]{ ReactorKeysEnum.PROJECT.getKey() };
+		this.keyRequired = new int[] {1};
+	}
+
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		User user = this.insight.getUser();
+		String projectId = this.keyValue.get(this.keysToGet[0]);
+		if(!SecurityProjectUtils.userCanViewProject(this.insight.getUser(), projectId)) {
+			// you don't have access
+			throw new IllegalArgumentException("Project/App does not exist or user does not have access to the project");
+		}
+		
+		Map<String, Boolean> hasAccess = new HashMap<>();
+		IProject project = Utility.getProject(projectId);
+		IProject.PROJECT_TYPE ptype = project.getProjectType();
+		if (!ptype.equals(IProject.PROJECT_TYPE.BLOCKS)) {
+			throw new SemossPixelException("App " + projectId + " is not a blocks project.");
+		}
+		
+		classLogger.info("Project Dependencies: ", project.getProjectProperties());
+		
+//		List<String> dependentEngineIds = SecurityProjectUtils.getProjectDependencies(projectId);
+//		for(String depEngineId : dependentEngineIds) {
+//			boolean canView = SecurityEngineUtils.userCanViewEngine(user, depEngineId);
+//			hasAccess.put(depEngineId, canView);
+//		}
+//		
+//		NounMetadata noun = new NounMetadata(hasAccess, PixelDataType.MAP);
+//		return noun;
+		
+		return new NounMetadata(project.getProjectProperties(), PixelDataType.MAP);
+	}
+	
+	@Override
+	public String getReactorDescription() {
+		return "Return true if the user has access to each engine dependency listed in this project";
+	}
+	
+}
