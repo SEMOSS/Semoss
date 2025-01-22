8e8de7b8a0bc148bd98eea16a113b703a29ee2ae
Maher Khalil <themaherkhalil@gmail.com>
2025-01-21 19:02:54 -0500

    UPDATE ::: Adding in sorting by displayname to msgraph query

--- src/prerna/graph/MSGraphAPICall.java
+++ src/prerna/graph/MSGraphAPICall.java
@@ -21,11 +21,11 @@ import prerna.util.Constants;
 import prerna.util.SocialPropertiesUtil;
 
 public class MSGraphAPICall {
-	 
+
 	private static final Logger classLogger = LogManager.getLogger(MSGraphAPICall.class);
-    private static AccessToken systemAccessToken = null;
-    private static long tokenExpirationTime = 0;
-    
+	private static AccessToken systemAccessToken = null;
+	private static long tokenExpirationTime = 0;
+
 	/**
 	 *This method use to get users data from graph api with next link having subsequent userdata
 	 *@param accessToken, searchTerm, nextLink
@@ -33,15 +33,15 @@ public class MSGraphAPICall {
 	 */
 	public String getUserDetails(AccessToken accessToken, String groupId, String searchTerm, String nextLink) throws Exception {
 		classLogger.info("getUserDetails based on Graph Api");
-		
-        if (accessToken == null) {
-            classLogger.info("Graph call for Access Token is null, will attempt to use system credentials");
-            if (systemAccessToken == null || isTokenExpired()) {
-                systemAccessToken = refreshSystemAccessToken();
-            }
-            accessToken = systemAccessToken;
-        }
-        
+
+		if (accessToken == null) {
+			classLogger.info("Graph call for Access Token is null, will attempt to use system credentials");
+			if (systemAccessToken == null || isTokenExpired()) {
+				systemAccessToken = refreshSystemAccessToken();
+			}
+			accessToken = systemAccessToken;
+		}
+
 		CloseableHttpClient httpClient = null;
 		HttpResponse response = null;
 		HttpEntity entity = null;
@@ -53,28 +53,28 @@ public class MSGraphAPICall {
 				String searchParam = "";
 				if(searchTerm != null && !(searchTerm=searchTerm.trim()).isEmpty()) {
 					searchParam = "$search=" + 
-										URLEncoder.encode("\"displayName:" + searchTerm 
-										+ "\" OR \"mail:" + searchTerm
-										+ "\" OR \"userPrincipalName:" + searchTerm + "\"", 
-										java.nio.charset.StandardCharsets.UTF_8.toString())
-									+ "&";
+							URLEncoder.encode("\"displayName:" + searchTerm 
+									+ "\" OR \"mail:" + searchTerm
+									+ "\" OR \"userPrincipalName:" + searchTerm + "\"", 
+									java.nio.charset.StandardCharsets.UTF_8.toString())
+					+ "&";
 				}
-                if (groupId == null || groupId.isEmpty()) {
-                    uri = MicrosoftTokenFiller.MS_GRAPH_BASE_API  + "/v1.0/users?" + searchParam + "$top=999&$count=true";
-                } else {
-                    uri = MicrosoftTokenFiller.MS_GRAPH_BASE_API  + "/v1.0/groups/" + groupId + "/members/microsoft.graph.user?" + searchParam + "$top=999&$count=true";
-                }
+				if (groupId == null || groupId.isEmpty()) {
+					uri = MicrosoftTokenFiller.MS_GRAPH_BASE_API  + "/v1.0/users?" + searchParam + "$orderby=displayName&$top=999&$count=true";
+				} else {
+					uri = MicrosoftTokenFiller.MS_GRAPH_BASE_API  + "/v1.0/groups/" + groupId + "/members/microsoft.graph.user?" + searchParam + "$orderby=displayName&$top=999&$count=true";
+				}
 			} else {
 				uri = nextLink;
 			}
-			
+
 			HttpGet httpGet = new HttpGet(uri);
-	        // Set headers
+			// Set headers
 			httpGet.setHeader("Authorization", "Bearer " + accessToken.getAccess_token());
 			httpGet.setHeader("Accept", "application/json");
 			httpGet.setHeader("ConsistencyLevel", "eventual");
-	 
-	        // Execute the request
+
+			// Execute the request
 			response = httpClient.execute(httpGet);
 			entity = response.getEntity();
 			String jsonResponse = EntityUtils.toString(entity);
@@ -96,43 +96,43 @@ public class MSGraphAPICall {
 			}
 		}
 	}
-	
-	   
-    /**
-     * This method exchanges application-specific access keys and secret keys for an access token.
-     * 
-     * @return The access token.
-     * @throws IOException If an error occurs during the token exchange.
-     */
-    private synchronized AccessToken refreshSystemAccessToken() throws IOException {
-        String clientId = SocialPropertiesUtil.getInstance().getProperty("ms_graphapi_client_id");
-        String clientSecret = SocialPropertiesUtil.getInstance().getProperty("ms_graphapi_secret_key");
-        String tenantId = SocialPropertiesUtil.getInstance().getProperty("ms_tenant");
-        
-        String tokenEndpoint = "https://login.microsoftonline.com/" + tenantId + "/oauth2/v2.0/token";
-        String scope = "https://graph.microsoft.com/.default";
 
-        Map<String, String> params = new HashMap<>();
-        params.put("client_id", clientId);
-        params.put("scope", scope);
-        params.put("grant_type", "client_credentials");
-        params.put("client_secret", clientSecret);
 
-        AccessToken newToken = HttpHelperUtility.getAccessToken(tokenEndpoint, params, true, true);
-        if (newToken != null) {
-            // Set the token expiration time based on the current time and the expires_in value, subtracting 5 minutes (300 seconds)
-            tokenExpirationTime = System.currentTimeMillis() + ((newToken.getExpires_in() - 300) * 1000);
-        }
-        return newToken;
-    }
+	/**
+	 * This method exchanges application-specific access keys and secret keys for an access token.
+	 * 
+	 * @return The access token.
+	 * @throws IOException If an error occurs during the token exchange.
+	 */
+	private synchronized AccessToken refreshSystemAccessToken() throws IOException {
+		String clientId = SocialPropertiesUtil.getInstance().getProperty("ms_graphapi_client_id");
+		String clientSecret = SocialPropertiesUtil.getInstance().getProperty("ms_graphapi_secret_key");
+		String tenantId = SocialPropertiesUtil.getInstance().getProperty("ms_tenant");
 
-    /**
-     * Checks if the current system access token is expired.
-     * 
-     * @return True if the token is expired, false otherwise.
-     */
-    private boolean isTokenExpired() {
-        return System.currentTimeMillis() >= tokenExpirationTime;
-    }
-	
+		String tokenEndpoint = "https://login.microsoftonline.com/" + tenantId + "/oauth2/v2.0/token";
+		String scope = "https://graph.microsoft.com/.default";
+
+		Map<String, String> params = new HashMap<>();
+		params.put("client_id", clientId);
+		params.put("scope", scope);
+		params.put("grant_type", "client_credentials");
+		params.put("client_secret", clientSecret);
+
+		AccessToken newToken = HttpHelperUtility.getAccessToken(tokenEndpoint, params, true, true);
+		if (newToken != null) {
+			// Set the token expiration time based on the current time and the expires_in value, subtracting 5 minutes (300 seconds)
+			tokenExpirationTime = System.currentTimeMillis() + ((newToken.getExpires_in() - 300) * 1000);
+		}
+		return newToken;
+	}
+
+	/**
+	 * Checks if the current system access token is expired.
+	 * 
+	 * @return True if the token is expired, false otherwise.
+	 */
+	private boolean isTokenExpired() {
+		return System.currentTimeMillis() >= tokenExpirationTime;
+	}
+
 }
