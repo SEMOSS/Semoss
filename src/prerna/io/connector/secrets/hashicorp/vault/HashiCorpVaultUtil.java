58d1c5e378c2c039bc40dc684ff24faff0458110
Maher Khalil <themaherkhalil@gmail.com>
2025-02-19 15:35:40 -0500

    UPDATE ::: not requiring a nice prefix for engine secret paths

--- src/prerna/io/connector/secrets/hashicorp/vault/HashiCorpVaultUtil.java
+++ src/prerna/io/connector/secrets/hashicorp/vault/HashiCorpVaultUtil.java
@@ -92,7 +92,11 @@ public class HashiCorpVaultUtil extends AbstractSecrets {
 	 * @return
 	 */
 	private String getPathForEngine(IEngine.CATALOG_TYPE eType, String enginePath) {
-		return getBaseForEngine(eType) + "/" + enginePath;
+		String base = getBaseForEngine(eType);
+		if(base != null) {
+			return base + "/" + enginePath;
+		}
+		return enginePath;
 	}
 	
 	/**
