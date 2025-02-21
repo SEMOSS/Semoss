63bac06f9bb35177c7ad86c40c4bc757039b7cf5
Marielle Prihoda <90920625+marprihoda23@users.noreply.github.com>
2025-02-21 11:32:03 -0500

    Update: DateReactorApiTests was fixed. Date and Day unit tests created, 100% CC

--- test/prerna/testing/date/reactor/DateReactorApiTests.java
+++ test/prerna/testing/date/reactor/DateReactorApiTests.java
@@ -9,23 +9,43 @@ import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.testing.ApiSemossTestUtils;
 import prerna.testing.AbstractBaseSemossApiTests;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class DateReactorApiTests extends AbstractBaseSemossApiTests {
 	
 	@Test
 	public void getDate() {
-		String pixel = ApiSemossTestUtils.buildPixelCall(DateReactor.class, "date", "3/19/2022");
+		String pixel = ApiSemossTestUtils.buildPixelCall(DateReactor.class, "date", "2022-03-19");
 		NounMetadata nm = ApiSemossTestUtils.processPixel(pixel);
 		String date = nm.getValue().toString();
-		assertEquals("3/19/2022", date);
+		assertEquals("2022-03-19", date);
 	}
 	
 	@Test
 	public void getDate2() {
 		// sanity check to make sure multiple tests run fine :)
-		String pixel = ApiSemossTestUtils.buildPixelCall(DateReactor.class, "date", "3/19/2022");
+		String pixel = ApiSemossTestUtils.buildPixelCall(DateReactor.class, "date", "2022-03-19");
 		NounMetadata nm = ApiSemossTestUtils.processPixel(pixel);
 		String date = nm.getValue().toString();
-		assertEquals("3/19/2022", date);
+		assertEquals("2022-03-19", date);
 	}
+	
+	 @Test 
+	 public void getDateWithCustomFormat() {
+	     String pixel = ApiSemossTestUtils.buildPixelCall(DateReactor.class, "date", "2022-03-19", "format", "dd/MM/yyyy");
+	     NounMetadata nm = ApiSemossTestUtils.processPixel(pixel);
+	     String date = nm.getValue().toString();
+	     assertEquals("19/03/2022", date);
+	  }
+	 
+	  @Test
+	  public void getDateWithNoInput() {
+	      String pixel = ApiSemossTestUtils.buildPixelCall(DateReactor.class);
+	      NounMetadata nm = ApiSemossTestUtils.processPixel(pixel);
+	      String date = nm.getValue().toString();
+	      String today = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
+	      assertEquals(today, date);
+	   }
 
 }
