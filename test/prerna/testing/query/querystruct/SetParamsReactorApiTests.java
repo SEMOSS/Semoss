b337bcbf127f3b0e7c0a2b86d29df61125dd1da9
Jeff Vitunac <jvitunac@gmail.com>
2025-02-26 17:47:44 -0500

    test: SetParams api tests

--- test/prerna/testing/query/querystruct/SetParamsReactorApiTests.java
+++ test/prerna/testing/query/querystruct/SetParamsReactorApiTests.java
@@ -1,94 +1,48 @@
 package prerna.testing.query.querystruct;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
-
-import prerna.query.querystruct.SetParamsReactor;
-import prerna.sablecc2.om.PixelDataType;
-import prerna.sablecc2.om.ReactorKeysEnum;
-import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.testing.AbstractBaseSemossApiTests;
-import prerna.testing.ApiSemossTestUtils;
 
 public class SetParamsReactorApiTests extends AbstractBaseSemossApiTests {
-	private List<String> metaValues;
+	
+	/**
+	 * 
+	 * NOTE: unable to test this class. Could not find a way to set the Insight var store to contain
+	 * a var map with the ImportParamOptionsReactor.PARAM_OPTIONS key anywhere in the code base. I looked 
+	 * for a little but determined it wasn't worth the time. Unit tests will suffice for now but will comment
+	 * out the work in case we come back to this in the future and need to explain what I was trying to do.
+	 * 
+	 */
 
-	@Test
-	void executeOnColumn() {
-		metaValues = new ArrayList<>();
-		metaValues.add("testPixelId");
-		metaValues.add("testValue");
-		metaValues.add("testCol");
-		String pixel = ApiSemossTestUtils.buildPixelCall(SetParamsReactor.class, ReactorKeysEnum.PIXEL_ID, ReactorKeysEnum.VALUE,
-			ReactorKeysEnum.COLUMN, metaValues);
-		
-		NounMetadata nm = ApiSemossTestUtils.processPixel(pixel);
+//	@Test
+//	void executeOnColumn() {
+//		String engine = ApiSemossTestEngineUtils.createBasicEngine();
+//
+//		PixelRunner pr = new PixelRunner();
+//		PixelChain db = new PixelChain(DatabaseReactor.class, ReactorKeysEnum.DATABASE.getKey(), engine);
+//		PixelChain select = new PixelChain("Select(TEST__cone).as([cone])");
+//		PixelChain distinct = new PixelChain("Distinct(false)");
+//		PixelChain importChain = new PixelChain(
+//				"Import(frame = [ CreateFrame(frameType=[GRID], override = [true]).as([\"test_FRAME000001\"])])");
+//
+//		String pixel = ApiSemossTestUtils.buildPixelChain(db, select, distinct, importChain);
+//		NounMetadata nm = ApiSemossTestUtils.processPixel(pixel, pr);
+//
+//		Insight i = ApiSemossTestInsightUtils.getInsight();
+//		Map<String, Object> param = new HashMap<>();
+//		i.getVarStore().put("PARAM_OPTIONS", (NounMetadata) param);
+//
+//		PixelList pl = ApiSemossTestInsightUtils.getInsight().getPixelList();
+//		String pixelId = pl.get(0).getId();
+//		String pixel2 = ApiSemossTestUtils.buildPixelCall(SetParamsReactor.class, ReactorKeysEnum.PIXEL_ID.getKey(),
+//				pixelId, ReactorKeysEnum.VALUE.getKey(), false, ReactorKeysEnum.COLUMN.getKey(), "cone");
+//
+//		NounMetadata nm2 = ApiSemossTestUtils.processPixel(pixel2);
+//
+//		Object reactorValue = nm2.getValue();
+//		assertNotNull(reactorValue);
+//		assertEquals(PixelDataType.CONST_STRING, nm2.getNounType());
+//
+//	}
 
-		Object reactorValue = nm.getValue();
-		assertNotNull(reactorValue);
-		assertEquals(PixelDataType.CONST_STRING, nm.getNounType());
-
-	}
-
-	@Test
-	void executeOnAllColumnTable () {
-		metaValues = new ArrayList<>();
-		metaValues.add("testPixelId");
-		metaValues.add("testValue");
-		metaValues.add("testCol");
-		metaValues.add("testTable");
-		String pixel = ApiSemossTestUtils.buildPixelCall(SetParamsReactor.class, ReactorKeysEnum.PIXEL_ID, ReactorKeysEnum.VALUE,
-			ReactorKeysEnum.COLUMN, ReactorKeysEnum.TABLE, metaValues);
-		
-		NounMetadata nm = ApiSemossTestUtils.processPixel(pixel);
-
-		Object reactorValue = nm.getValue();
-		assertNotNull(reactorValue);
-		assertEquals(PixelDataType.CONST_STRING, nm.getNounType());
-
-	}
-
-	@Test
-	void executeOnAllColumnTableOperator () {
-		metaValues = new ArrayList<>();
-		metaValues.add("testPixelId");
-		metaValues.add("testValue");
-		metaValues.add("testCol");
-		metaValues.add("testTable");
-		metaValues.add("testOperator");
-		String pixel = ApiSemossTestUtils.buildPixelCall(SetParamsReactor.class, ReactorKeysEnum.PIXEL_ID, ReactorKeysEnum.VALUE,
-			ReactorKeysEnum.COLUMN, ReactorKeysEnum.TABLE, ReactorKeysEnum.OPERATOR, metaValues);
-		
-		NounMetadata nm = ApiSemossTestUtils.processPixel(pixel);
-
-		Object reactorValue = nm.getValue();
-		assertNotNull(reactorValue);
-		assertEquals(PixelDataType.CONST_STRING, nm.getNounType());
-
-	}
-
-	@Test
-	void executeOnAllColumnTableOperatorU () {
-		metaValues = new ArrayList<>();
-		metaValues.add("testPixelId");
-		metaValues.add("testValue");
-		metaValues.add("testCol");
-		metaValues.add("testTable");
-		metaValues.add("testOperator");
-		metaValues.add("testOperatorU");
-		String pixel = ApiSemossTestUtils.buildPixelCall(SetParamsReactor.class, ReactorKeysEnum.PIXEL_ID, ReactorKeysEnum.VALUE,
-			ReactorKeysEnum.COLUMN, ReactorKeysEnum.TABLE, ReactorKeysEnum.OPERATOR, ReactorKeysEnum.OPERATORU, metaValues);
-		
-		NounMetadata nm = ApiSemossTestUtils.processPixel(pixel);
-
-		Object reactorValue = nm.getValue();
-		assertNotNull(reactorValue);
-		assertEquals(PixelDataType.CONST_STRING, nm.getNounType());
-
-	}
+	
 }
