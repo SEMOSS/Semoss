d2ec8aef094d9a41a8f3596192a08316dea19994
Jeff Vitunac <jvitunac@gmail.com>
2025-03-03 08:22:03 -0500

    test: removed unneeded comment

--- test/prerna/unit/auth/utils/reactors/admin/AdminEngineInfoReactorUnitTests.java
+++ test/prerna/unit/auth/utils/reactors/admin/AdminEngineInfoReactorUnitTests.java
@@ -43,7 +43,6 @@ public class AdminEngineInfoReactorUnitTests {
 	private NounStore ns;
 	private GenRowStruct grs;
 
-	// test
 	@BeforeEach
 	void setup() {
 		reactor = new AdminEngineInfoReactor();
