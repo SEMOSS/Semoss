bab28b58f0822ba94b724ea6fc034e289ce9a620
tlin2_deloitte <tlin2@deloitte.com>
2025-03-04 10:01:09 -0500

    test: pixel chained AdminExecQuery

--- test/prerna/testing/auth/utils/reactors/admin/AdminExecQueryReactorApiTests.java
+++ test/prerna/testing/auth/utils/reactors/admin/AdminExecQueryReactorApiTests.java
@@ -4,25 +4,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
+import prerna.algorithm.api.SemossDataType;
+import prerna.auth.utils.reactors.admin.AdminDatabaseReactor;
 import prerna.auth.utils.reactors.admin.AdminExecQueryReactor;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.testing.AbstractBaseSemossApiTests;
 import prerna.testing.ApiSemossTestEngineUtils;
 import prerna.testing.ApiSemossTestUtils;
-
+import prerna.testing.PixelChain;
 import prerna.query.querystruct.AbstractQueryStruct.QUERY_STRUCT_TYPE;
 import prerna.query.querystruct.HardSelectQueryStruct;
 import prerna.query.querystruct.SelectQueryStruct;
 import prerna.query.querystruct.selectors.QueryColumnSelector;
-
-import prerna.util.Constants;
-import prerna.util.Utility;
+import prerna.reactor.export.CollectReactor;
 
 public class AdminExecQueryReactorApiTests extends AbstractBaseSemossApiTests {
 	@Test
@@ -31,10 +34,28 @@ public class AdminExecQueryReactorApiTests extends AbstractBaseSemossApiTests {
 		SelectQueryStruct qs = new SelectQueryStruct();
 		qs.addSelector(new QueryColumnSelector("INSIGHT__INSIGHTID"));
         
-		String pixel = ApiSemossTestUtils.buildPixelCall(AdminExecQueryReactor.class, qs, engine);
-		NounMetadata nm = ApiSemossTestUtils.processPixel(pixel);
         List<PixelOperationType> opTypes = new ArrayList<>();
         opTypes.add(PixelOperationType.ALTER_DATABASE);
+		
+		PixelChain db = new PixelChain(AdminDatabaseReactor.class, "jeff");
+		PixelChain query = new PixelChain("INSERT INTO HOME (Baths, Beds) VALUES (10, 20)");
+//		PixelChain query = new PixelChain(QueryReactor.class, "SELECT * FROM SMSS_USER");
+		PixelChain collect = new PixelChain(CollectReactor.class, 0);
+		PixelChain adminExecQuery = new PixelChain(AdminExecQueryReactor.class, engine);
+		
+		String pixel = ApiSemossTestUtils.buildPixelChain(db, query, collect, adminExecQuery);
+		
+		NounMetadata nm = ApiSemossTestUtils.processPixel(pixel);
+
+//        String query = "AdminDatabase(\"" + database + "\")";
+//        query += "| Query(\"<encode>" + encode + "</encode>\")";
+//        if (collect > 0) {
+//            query += "| Collect(" + collect + ");";
+//        } else {
+//            query += "| AdminExecQuery();";
+//        }
+
+		assertNotNull(nm.getValue());
 		
 		assertNotNull(nm);
         assertEquals(PixelDataType.BOOLEAN, nm.getNounType());
