321d2922224d62fa67c2d6999c47d86617692ee8
Ryan Weiler <ryanweiler92@gmail.com>
2025-03-21 08:40:26 -0400

    feat: handle KServe vision models (#565)
    * feat: kserve vision model integration with vision reactor
    
    * feat: updating token param names in kserve response
    
    * feat: updating kserve vision engine logger class
    
    * feat: image embedding

---
+++ src/prerna/reactor/model/VisionReactor.java
@@ -0,0 +1,85 @@
+package prerna.reactor.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import prerna.auth.User;
+import prerna.auth.utils.SecurityEngineUtils;
+import prerna.engine.api.IModelEngine;
+import prerna.reactor.AbstractReactor;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.util.Utility;
+
+public class VisionReactor extends AbstractReactor {
+	
+	public VisionReactor() {
+		this.keysToGet = new String[] { ReactorKeysEnum.ENGINE.getKey(), ReactorKeysEnum.COMMAND.getKey(),
+				ReactorKeysEnum.IMAGE.getKey(), ReactorKeysEnum.PARAM_VALUES_MAP.getKey() };
+		this.keyRequired = new int[] { 1, 1, 1, 0 };
+	}
+	
+	@Override
+	public NounMetadata execute() {
+		organizeKeys();
+		String engineId = this.keyValue.get(this.keysToGet[0]);
+		User user = this.insight.getUser();
+		if (!SecurityEngineUtils.userCanViewEngine(user, engineId)) {
+			throw new IllegalArgumentException(
+					"Model " + engineId + " does not exist or user does not have access to this model");
+		}
+
+		String prompt = Utility.decodeURIComponent(this.keyValue.get(this.keysToGet[1]));
+		String image = Utility.decodeURIComponent(this.keyValue.get(this.keysToGet[2]));
+
+		Map<String, Object> paramMap = getMap();
+		IModelEngine modelEngine = Utility.getModel(engineId);
+		if (paramMap == null) {
+			paramMap = new HashMap<String, Object>();
+		}
+		
+		paramMap.put("image_url", image);
+
+		Map<String, Object> output = modelEngine.ask(prompt, null, this.insight, paramMap).toMap();
+		return new NounMetadata(output, PixelDataType.MAP, PixelOperationType.OPERATION);
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private Map<String, Object> getMap() {
+        GenRowStruct mapGrs = this.store.getNoun(keysToGet[3]);
+        if(mapGrs != null && !mapGrs.isEmpty()) {
+            List<NounMetadata> mapInputs = mapGrs.getNounsOfType(PixelDataType.MAP);
+            if(mapInputs != null && !mapInputs.isEmpty()) {
+                return (Map<String, Object>) mapInputs.get(0).getValue();
+            }
+        }
+        List<NounMetadata> mapInputs = this.curRow.getNounsOfType(PixelDataType.MAP);
+        if(mapInputs != null && !mapInputs.isEmpty()) {
+            return (Map<String, Object>) mapInputs.get(0).getValue();
+        }
+        return null;
+    }
+	
+	@Override
+	public String getReactorDescription() {
+		return "This method is used to run a vision task with an Image-Text-to-Text model";
+	}
+	
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if(key.equals(ReactorKeysEnum.COMMAND.getKey())) {
+			return "This is the vision prompt to execute against the model";
+		} else if(key.equals(ReactorKeysEnum.IMAGE.getKey())) {
+			return "The image URL or base64 string for the vision task.";
+		}
+		return super.getDescriptionForKey(key);
+	}
+
+}
