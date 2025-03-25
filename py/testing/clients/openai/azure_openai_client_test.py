b10045948d3039d69b7a2c1c344f370b380e23cf
pasupathimuniyappan <pasupathi.muniyappan@kanini.com>
2025-03-25 19:57:24 +0530

    [493] - Test - Create new tests using Pytest - Added test cases for openai client.

---
+++ py/testing/clients/openai/azure_openai_client_test.py
@@ -0,0 +1,50 @@
+"""
+To run the tests, navigate to the Semoss_Dev/py directory and then run the following command:
+
+    pytest -s testing/clients/openai/azure_openai_client_test.py
+    -s => For enabling the print statements
+
+Ensure you have a valid `.env` file with the below required keys:
+    - AZURE_OPENAI_API_KEY
+    - AZURE_OPENAI_ENDPOINT
+
+Install pytest if not already installed:
+    pip install pytest
+"""
+
+import os
+import pytest
+from dotenv import load_dotenv
+from typing import Dict
+from genai_client import AzureOpenAiClient
+
+# Load environment variables from .env file
+load_dotenv()
+
+AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
+AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
+SAMPLE_QUESTION = "What is the capital of Germany?"
+
+
+@pytest.fixture
+def azure_openai_client():
+    """Fixture to create an Azure OpenAI client instance."""
+    return AzureOpenAiClient(
+        model_name="gpt-4o",
+        api_key=AZURE_OPENAI_API_KEY,
+        endpoint=AZURE_OPENAI_ENDPOINT,
+        max_tokens=4097,
+    )
+
+
+def test_azure_chat_completions(azure_openai_client):
+    """Test the Azure OpenAI chat completion."""
+    ask_response = azure_openai_client.ask(question=SAMPLE_QUESTION)
+    print("azure chat completion ask_response - ", ask_response)
+
+    assert isinstance(ask_response, Dict), "Response Should be a Dictionary"
+    assert set(ask_response.keys()) == {
+        "response",
+        "numberOfTokensInPrompt",
+        "numberOfTokensInResponse",
+    }, "Response Keys Mismatch"
---
+++ py/testing/clients/openai/openai_client_test.py
@@ -0,0 +1,74 @@
+"""
+To run the tests, navigate to the Semoss_Dev/py directory and then run the following command:
+
+    pytest -s testing/clients/openai/openai_client_test.py
+    -s => For enabling the print statements
+
+Ensure you have a valid `.env` file with the below required keys:
+    - OPENAI_API_KEY
+
+Install pytest if not already installed:
+    pip install pytest
+"""
+
+import os
+import pytest
+from dotenv import load_dotenv
+from typing import Dict
+from genai_client import OpenAiClient
+
+# Load environment variables from .env file
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+SAMPLE_QUESTION = "What is the capital of India?"
+
+
+@pytest.fixture
+def openai_chat_completion_client():
+    """Fixture to create an OpenAI chat completion client instance."""
+    return OpenAiClient(
+        model_name="gpt-3.5-turbo",
+        api_key=OPENAI_API_KEY,
+        max_tokens=4097,
+        chat_type="chat-completion",
+    )
+
+
+@pytest.fixture
+def openai_completion_client():
+    """Fixture to create an OpenAI completion client instance."""
+    return OpenAiClient(
+        model_name="babbage-002",
+        api_key=OPENAI_API_KEY,
+        max_tokens=4097,
+        chat_type="completions",
+    )
+
+
+def test_openai_chat_completions(openai_chat_completion_client):
+    """Test the OpenAI chat completion client."""
+    ask_response = openai_chat_completion_client.ask(question=SAMPLE_QUESTION)
+    print("openai chat completion ask_response - ", ask_response)
+
+    assert isinstance(
+        ask_response, Dict
+    ), "Response Should be a Dictionary"  # Structured JSON response
+    assert set(ask_response.keys()) == {
+        "response",
+        "numberOfTokensInPrompt",
+        "numberOfTokensInResponse",
+    }, "Response Keys Mismatch"  # Correct response parameters
+
+
+def test_openai_completions(openai_completion_client):
+    """Test the OpenAI completion client."""
+    ask_response = openai_completion_client.ask(question=SAMPLE_QUESTION)
+    print("openai completion ask_response - ", ask_response)
+
+    assert isinstance(ask_response, Dict), "Response Should be a Dictionary"
+    assert set(ask_response.keys()) == {
+        "response",
+        "numberOfTokensInPrompt",
+        "numberOfTokensInResponse",
+    }, "Response Keys Mismatch"
