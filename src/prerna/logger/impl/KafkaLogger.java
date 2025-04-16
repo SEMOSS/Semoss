70b2d66fe2b1d7cdbdedb6c62fb27cef3c0f7293
srikanth bathi <sbathi@deloitte.com>
2025-04-16 15:38:15 +0530

    feat(KafkaLogger): Handle failure case for kafka cluster

--- src/prerna/logger/impl/KafkaLogger.java
+++ src/prerna/logger/impl/KafkaLogger.java
@@ -11,21 +11,47 @@ import java.util.Properties;
 
 public class KafkaLogger implements IQueueLogger {
 
-    private final Producer<String, String> producer;
-    private final String topic;
+    private volatile Producer<String, String> producer;
+    private final Properties config;
+    private final Object lock = new Object();
 
     public KafkaLogger(String config) {
-        Properties props = defaultProps(config);
-        topic = props.getProperty("topic");
-        producer = new KafkaProducer<>(props);
+        this.config = defaultProps(config);
     }
+
+    private Producer<String, String> getOrCreateProducer() {
+        if(producer == null){
+            synchronized (lock) {
+                if(producer == null) {
+                    try{
+                        producer = new KafkaProducer<>(config);
+                    }catch (Exception ex){
+                        System.err.println("Failed to initialize KafkaProducer: "+ ex.getMessage());
+                    }
+                }
+            }
+        }
+        return producer;
+    }
+
     @Override
     public void send(String message) {
-        producer.send(new ProducerRecord<>(topic, message), (metadata, exception) -> {
-            if (exception != null) {
-                System.err.println(exception.getMessage());
-            }
-        });
+        Producer<String, String> kafkaProducer = getOrCreateProducer();
+        if(kafkaProducer == null){
+            System.err.println("Failed to initialize KafkaProducer: null");
+            return;
+        }
+        try{
+            String topic = config.getProperty("topic");
+            producer.send(new ProducerRecord<>(topic, message), (metadata, exception) -> {
+                if (exception != null) {
+                    System.err.println(exception.getMessage());
+                }
+            });
+        }catch (Exception ex){
+            System.err.println("Exception while sending message: "+ ex.getMessage());
+        }
+
     }
 
     @Override
@@ -38,6 +64,9 @@ public class KafkaLogger implements IQueueLogger {
         props.put("acks", "all");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // Tuning
+        props.put("retries", 5);
+
         return props;
     }
 }
