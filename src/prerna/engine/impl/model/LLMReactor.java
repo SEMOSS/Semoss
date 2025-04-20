d6ec900e0b9fe297e218352ee02cfe0cf7be400a
Maher Khalil <themaherkhalil@gmail.com>
2025-04-20 16:11:28 -0400

    fix: fixing key being used for param values, adding more in help function (#713)

--- src/prerna/engine/impl/model/LLMReactor.java
+++ src/prerna/engine/impl/model/LLMReactor.java
@@ -1,5 +1,6 @@
 package prerna.engine.impl.model;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,31 +19,29 @@ import prerna.util.Utility;
 public class LLMReactor extends AbstractReactor {
 	
 	public LLMReactor() {
-		this.keysToGet = new String[] { ReactorKeysEnum.ENGINE.getKey(), ReactorKeysEnum.COMMAND.getKey(), ReactorKeysEnum.USE_HISTORY.getKey(),
-				ReactorKeysEnum.CONTEXT.getKey(), ReactorKeysEnum.PARAM_VALUES_MAP.getKey() };
+		this.keysToGet = new String[] { ReactorKeysEnum.ENGINE.getKey(), ReactorKeysEnum.COMMAND.getKey(), ReactorKeysEnum.CONTEXT.getKey(),
+				 ReactorKeysEnum.USE_HISTORY.getKey(), ReactorKeysEnum.PARAM_VALUES_MAP.getKey() };
 		this.keyRequired = new int[] { 1, 1, 0, 0, 0 };
 	}
 
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
-		String engineId = this.keyValue.get(this.keysToGet[0]);
+		String engineId = this.keyValue.get(ReactorKeysEnum.ENGINE.getKey());
 		User user = this.insight.getUser();
 		if (!SecurityEngineUtils.userCanViewEngine(user, engineId)) {
-			throw new IllegalArgumentException(
-					"Model " + engineId + " does not exist or user does not have access to this model");
+			throw new IllegalArgumentException("Model " + engineId + " does not exist or user does not have access to this model");
 		}
+		
+		// default is true
+		Boolean useHistoryParam = Boolean.parseBoolean(this.keyValue.getOrDefault(ReactorKeysEnum.USE_HISTORY.getKey(), "true")+"");
 
-		String question = Utility.decodeURIComponent(this.keyValue.get(this.keysToGet[1]));
-		String context = this.keyValue.get(this.keysToGet[3]);
+		String question = Utility.decodeURIComponent(ReactorKeysEnum.COMMAND.getKey());
+		String context = this.keyValue.get(ReactorKeysEnum.CONTEXT.getKey());
 		if (context != null) {
 			context = Utility.decodeURIComponent(context);
 		}
 		
-		String useHistory = this.keyValue.get(this.keysToGet[2]);
-		Boolean useHistoryParam = useHistory == null || "true".equalsIgnoreCase(useHistory);
-
-
 		Map<String, Object> paramMap = getMap();
 		IModelEngine modelEngine = Utility.getModel(engineId);
 		if (paramMap == null) {
@@ -61,7 +60,7 @@ public class LLMReactor extends AbstractReactor {
 	 * @return
 	 */
 	private Map<String, Object> getMap() {
-        GenRowStruct mapGrs = this.store.getNoun(keysToGet[3]);
+        GenRowStruct mapGrs = this.store.getNoun(ReactorKeysEnum.PARAM_VALUES_MAP.getKey());
         if(mapGrs != null && !mapGrs.isEmpty()) {
             List<NounMetadata> mapInputs = mapGrs.getNounsOfType(PixelDataType.MAP);
             if(mapInputs != null && !mapInputs.isEmpty()) {
@@ -86,7 +85,14 @@ public class LLMReactor extends AbstractReactor {
 			return "This is the prompt to execute against the LLM";
 		} else if(key.equals(ReactorKeysEnum.CONTEXT.getKey())) {
 			return "The system prompt to use for the LLM call";
+		} else if(key.equals(ReactorKeysEnum.USE_HISTORY.getKey())) {
+			return "Boolean true/false to determine if we should incorporate the user's chat history based on the previous chats in this insight id";
+		} else if(key.equals(ReactorKeysEnum.PARAM_VALUES_MAP.getKey())) {
+			return "Map containing the key-value pairs for model parameters like 'temperature', 'top_p', etc. "
+					+ "In addition, you can pass in 'full_prompt' to represent a full prompt and history via ChatML format which will ignore inputs for " + 
+					Arrays.asList(ReactorKeysEnum.COMMAND.getKey(), ReactorKeysEnum.CONTEXT.getKey(), ReactorKeysEnum.USE_HISTORY.getKey());
 		}
+		
 		return super.getDescriptionForKey(key);
 	}
 	
