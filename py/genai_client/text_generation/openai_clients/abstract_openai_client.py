fe156357d3bdd6c19290277894b2f5a20623d2c0
Weiler, Ryan <ryanweiler92@gmail.com>
2025-05-22 11:31:47 -0400

    feat: handle use_max_tokens smss setting

--- py/genai_client/text_generation/openai_clients/abstract_openai_client.py
+++ py/genai_client/text_generation/openai_clients/abstract_openai_client.py
@@ -23,6 +23,8 @@ class AbstractOpenAiClient(AbstractTextGenerationClient, ABC):
 
         self.model_name = model_name
         self.model_type = model_type
+        self.use_max_tokens_param = kwargs.pop("use_max_tokens", False)
+
         self.tokenizer = self._get_tokenizer(kwargs)
         self.client = self._get_client(api_key=api_key, **kwargs)
         if self.model_type == None:
