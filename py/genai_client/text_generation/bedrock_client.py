7614d50f7f5e91ef7c39599a926262549fb3895c
Kunal Patel <kunal0137@gmail.com>
2025-05-27 08:37:37 -0400

    fix: removing print

--- py/genai_client/text_generation/bedrock_client.py
+++ py/genai_client/text_generation/bedrock_client.py
@@ -169,7 +169,6 @@ class BedrockClient(AbstractTextGenerationClient):
             if 'metadata' in event:
                 metadata = event['metadata']
                 if 'usage' in metadata:
-                    print("\nToken usage")
                     prompt_tokens = metadata['usage']['inputTokens']
                     output_tokens = metadata['usage']['outputTokens']
         return final_response,prompt_tokens,output_tokens
