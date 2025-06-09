2ae3bf274a61dc8bb255041d16c2bc0dbdb7e0fc
samarthKharote <samarth.kharote@kanini.com>
2025-06-09 17:36:35 +0530

    modify SaveAsset Reactor to accept multiple file paths

--- src/prerna/util/git/reactors/SaveAssetReactor.java
+++ src/prerna/util/git/reactors/SaveAssetReactor.java
@@ -2,6 +2,7 @@ package prerna.util.git.reactors;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
@@ -29,54 +30,72 @@ public class SaveAssetReactor extends AbstractReactor {
 	}
 
 	@Override
-	public NounMetadata execute() {
-		organizeKeys();
-		User user = this.insight.getUser();
-		// check if user is logged in
-		if (AbstractSecurityUtils.anonymousUsersEnabled() && user.isAnonymous()) {
-			throwAnonymousUserError();
-		}
+    public NounMetadata execute() {
+        organizeKeys();
 
-		String space = this.keyValue.get(this.keysToGet[3]);
-		// if security is enabled, you need proper permissions
-		// this takes in the insight and does a user check that the user has access to perform the operations
-		String assetFolder = AssetUtility.getAssetBasePath(this.insight, space, true);
-		String fileName = Utility.normalizePath(keyValue.get(keysToGet[0]));
-		
-		// limit saving R/Py Files in prod - No new files can be created but they can be sourced
-		boolean strict_script_source = Boolean.parseBoolean(Utility.getDIHelperProperty(Constants.STRICT_SCRIPT_SOURCE));
-		if(strict_script_source) {
-			 String extension = FilenameUtils.getExtension(fileName);
-			 if(extension.equalsIgnoreCase("py") || extension.equalsIgnoreCase("R") ) {
-					throw new IllegalArgumentException("User is not allowed to create or save R or Py scripts");
-			 }
-		}
-		
-		String filePath = assetFolder + "/" + fileName;
-		String content = keyValue.get(keysToGet[1]);
+        User user = this.insight.getUser();
+        // check if user is logged in
+        if (AbstractSecurityUtils.anonymousUsersEnabled() && user.isAnonymous()) {
+            throwAnonymousUserError();
+        }
 
-		// you cannot save at root level if you are in user/project space
-		if(space != null 
-				&& !space.isEmpty() 
-				&& !space.equals(AssetUtility.INSIGHT_SPACE_KEY) 
-				&& !fileName.contains("/")) {
-			return NounMetadata.getErrorNounMessage("You cannot create directory / files at this level");
-		}
-		
-		// write content to file
-		content = Utility.decodeURIComponent(content);
-		File file = new File(filePath);
-		try {
-			FileUtils.writeStringToFile(file, content);
-		} catch (IOException e) {
-			classLogger.error(Constants.STACKTRACE, e);
-			NounMetadata error = NounMetadata.getErrorNounMessage("Unable to save file");
-			SemossPixelException exception = new SemossPixelException(error);
-			exception.setContinueThreadOfExecution(false);
-			throw exception;
-		}
+        String space = this.keyValue.get(this.keysToGet[3]);
+        String assetFolder = AssetUtility.getAssetBasePath(this.insight, space, true);
 
-		return NounMetadata.getSuccessNounMessage("Success!");
-	}
+        // Retrieve all file names and contents
+        List<String> fileNames = getNounAsStringList(this.keysToGet[0]);
+        List<String> contents = getNounAsStringList(this.keysToGet[1]);
+
+        if(fileNames == null || fileNames.isEmpty() || contents == null || contents.isEmpty()) {
+            throw new IllegalArgumentException("Must pass in at least one file name and content to save");
+        }
+        if(fileNames.size() != contents.size()) {
+            throw new IllegalArgumentException("Number of file names and contents must match");
+        }
+
+        // Check strict script source settings once
+        boolean strictScriptSource = Boolean.parseBoolean(
+            Utility.getDIHelperProperty(Constants.STRICT_SCRIPT_SOURCE)
+        );
+
+        // iterate each fileName/content pair
+        for (int i = 0; i < fileNames.size(); i++) {
+            String rawFileName = fileNames.get(i).trim();
+            String fileName = Utility.normalizePath(rawFileName);
+            if(fileName == null || fileName.isEmpty()) {
+                continue;
+            }
+
+            // limit saving R/Py Files in prod - No new files can be created but they can be sourced
+            if(strictScriptSource) {
+                String extension = FilenameUtils.getExtension(fileName);
+                if("py".equalsIgnoreCase(extension) || "R".equalsIgnoreCase(extension)) {
+                    throw new IllegalArgumentException("User is not allowed to create or save R or Py scripts");
+                }
+            }
+
+            // you cannot save at root level if you are in user/project space
+            if(space != null && !space.isEmpty() && !space.equals(AssetUtility.INSIGHT_SPACE_KEY) && !fileName.contains("/")) {
+                return NounMetadata.getErrorNounMessage("You cannot create directory / files at this level");
+            }
+
+            String filePath = assetFolder + "/" + fileName;
+            String content = contents.get(i);
+            content = Utility.decodeURIComponent(content);
+
+            File file = new File(filePath);
+            try {
+                FileUtils.writeStringToFile(file, content);
+            } catch (IOException e) {
+                classLogger.error(Constants.STACKTRACE, e);
+                NounMetadata error = NounMetadata.getErrorNounMessage("Unable to save file: " + fileName);
+                SemossPixelException exception = new SemossPixelException(error);
+                exception.setContinueThreadOfExecution(false);
+                throw exception;
+            }
+        }
+
+        return NounMetadata.getSuccessNounMessage("Success!");
+    }
 
 }
