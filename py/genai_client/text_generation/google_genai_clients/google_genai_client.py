e03ea12a3559cbe56119d94ea4fff8c686030b41
Weiler, Ryan <ryanweiler92@gmail.com>
2025-06-09 13:42:17 -0400

    feat: google genai client

--- py/genai_client/text_generation/google_genai_clients/google_genai_client.py
+++ py/genai_client/text_generation/google_genai_clients/google_genai_client.py
@@ -2,9 +2,9 @@ from typing import List, Optional, Dict
 from google import genai
 from pydantic import BaseModel
 from google.genai import types
+from ...utils import StringEnum, classify_url
 from ...constants import AskModelEngineResponse, TEMPLATE, TEMPLATE_NAME, FULL_PROMPT
 from ..abstract_text_generation_client import AbstractTextGenerationClient
-from ...clients.client_initializer import google_initializer
 
 
 class AskSettings(BaseModel):
@@ -17,6 +17,38 @@ class AskSettings(BaseModel):
     structured_response: Dict | None = None
     streaming: bool = False
     use_history: bool = True
+    history: Optional[List[Dict]] = None
+    image_url: Optional[List[str]] = None
+
+
+# Mimicking Google Gen AI's usage metadata structure
+class UsageMetadata(BaseModel):
+    candidates_token_count: int
+    prompt_token_count: int
+
+
+# Using this as a response model for streaming responses since Google Gen AI does not return usage metadata in streaming responses
+class StreamingResponse(BaseModel):
+    text: str
+    usage_metadata: Optional[UsageMetadata] = None
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+class ConvertedHistory(BaseModel):
+    """
+    Convert our history format to Google Gen AI's Content format.
+    If I find system instructions, I will return these as well.
+    """
+
+    contents: List[types.Content]
+    system_instructions: str | None = None
+
+
+class Roles(StringEnum):
+    USER = "user"
+    MODEL = "model"
 
 
 class GoogleGenAiClient(AbstractTextGenerationClient):
@@ -37,13 +69,6 @@ class GoogleGenAiClient(AbstractTextGenerationClient):
             template_name=kwargs.pop(TEMPLATE_NAME, None),
         )
 
-        # google_initializer(
-        #     region=region,
-        #     service_account_credentials=service_account_credentials,
-        #     service_account_key_file=service_account_key_file,
-        #     project=project,
-        # )
-
         self.service_account_credentials = self._load_credentials(
             service_account_credentials, service_account_key_file
         )
@@ -52,6 +77,90 @@ class GoogleGenAiClient(AbstractTextGenerationClient):
         self.client = self._get_client(project, region, api_key=api_key)
         self.max_tokens = max_tokens
         self.safety_settings = safety_settings
+
+    def ask_call(
+        self,
+        question: str = None,
+        context: str = None,
+        use_history: bool = True,
+        history: List[Dict] = None,
+        prefix="",
+        **kwargs,
+    ):
+        if self.client is None:
+            raise ValueError("Google Gen AI client is not initialized.")
+
+        ask_settings = self._get_ask_settings(history, use_history, **kwargs)
+
+        converted_history = self._convert_history(
+            history=ask_settings.history,
+            question=question,
+            image_url=ask_settings.image_url,
+        )
+        contents = converted_history.contents
+        if converted_history.system_instructions is not None and context is not None:
+            print(
+                "There are multiple sets of system instructions.. Using context passed to ask_call()"
+            )
+        elif converted_history.system_instructions is not None:
+            context = converted_history.system_instructions
+
+        config = self._convert_args_to_provider_config(context=context, **kwargs)
+
+        if ask_settings.streaming:
+            # STREAMING
+            response = self._handle_streaming(
+                prefix=prefix,
+                contents=contents,
+                config=config,
+            )
+        else:
+            # NON-STREAMING
+            response = self.client.models.generate_content(
+                model=self.model_name, contents=contents, config=config
+            )
+
+        response_tokens = response.usage_metadata.candidates_token_count
+        prompt_tokens = response.usage_metadata.prompt_token_count
+
+        model_engine_response = AskModelEngineResponse(
+            response=response.text,
+            prompt_tokens=prompt_tokens,
+            response_tokens=response_tokens,
+            messageType="CHAT",
+        )
+
+        return model_engine_response
+
+    def _handle_streaming(
+        self,
+        prefix: str,
+        contents: List[types.Content],
+        config: types.GenerateContentConfig,
+    ) -> StreamingResponse:
+        final_response = ""
+
+        for chunk in self.client.models.generate_content_stream(
+            model=self.model_name, contents=contents, config=config
+        ):
+            final_response += chunk.text
+            print(prefix + chunk.text, end="")
+
+        input_tokens = self._count_tokens(contents)
+
+        response_content = [
+            types.Content(
+                role="model", parts=[types.Part.from_text(text=final_response)]
+            )
+        ]
+        output_tokens = self._count_tokens(response_content)
+
+        usage_metadata = UsageMetadata(
+            candidates_token_count=output_tokens,
+            prompt_token_count=input_tokens,
+        )
+
+        return StreamingResponse(text=final_response, usage_metadata=usage_metadata)
 
     def _load_credentials(self, service_account_credentials, service_account_key_file):
         """Load service account credentials with required scopes"""
@@ -131,6 +240,10 @@ class GoogleGenAiClient(AbstractTextGenerationClient):
         full_prompt = kwargs.pop(FULL_PROMPT, None)
         structured_response = kwargs.pop("schema", None)
         streaming = kwargs.pop("streaming", False)
+        image_url = kwargs.pop("image_url", None)
+        # So we can send multiple images
+        if isinstance(image_url, str):
+            image_url = [image_url]
         if not streaming:
             streaming = kwargs.pop("stream", False)
 
@@ -142,103 +255,68 @@ class GoogleGenAiClient(AbstractTextGenerationClient):
             structured_response=structured_response,
             streaming=streaming,
             history=history,
+            image_url=image_url,
         )
 
-    def _handle_streaming(
-        self,
-        prefix: str,
-        prompt: str | Dict,
-        config: types.GenerateContentConfig,
-        history=None,
-    ) -> str:
-        """
-        Handle the streaming response from the Google Gen AI client.
-        """
-        final_response = ""
-        chat_session = self.client.chats.create(
-            model=self.model_name, config=config, history=history
-        )
-        for chunk in chat_session.send_message_stream(prompt):
-            final_response += chunk.text
-            print(prefix + chunk.text, end="")
-        return final_response
-
-    def ask_call(
-        self,
-        question: str = None,
-        context: str = None,
-        use_history: bool = True,
-        history: List[Dict] = None,
-        prefix="",
-        **kwargs,
-    ):
-        if self.client is None:
-            raise ValueError("Google Gen AI client is not initialized.")
-
-        ask_settings = self._get_ask_settings(history, use_history, **kwargs)
-        config = self._convert_args_to_provider_config(context=context, **kwargs)
-
-        if ask_settings.streaming:
-            streaming_response = self._handle_streaming(
-                prefix=prefix,
-                prompt=question,
-                config=config,
-                history=ask_settings.history,
+    def _count_tokens(self, contents: List[types.Content]) -> int:
+        try:
+            response = self.client.models.count_tokens(
+                model=self.model_name,
+                contents=contents,
             )
+            return response.total_tokens
+        except Exception as e:
+            raise RuntimeError(f"Failed to count tokens: {e}")
 
-        response = self.client.models.generate_content(
-            model=self.model_name, contents=question, config=config
+    def _convert_history(
+        self, history: List[Dict] = None, question: str = None, image_url: str = None
+    ) -> ConvertedHistory:
+        """
+        Convert our history format to Google Gen AI's Content format.
+        """
+        google_history = []
+        system_instructions = None
+
+        if history is not None:
+            for message in history:
+                role = message.get("role", "user")
+                content = message.get("content", "")
+                if role == "system":
+                    system_instructions = content
+                    continue
+                if role != "user":
+                    role = Roles.MODEL
+                message = types.Content(
+                    role=role, parts=[types.Part.from_text(text=content)]
+                )
+                google_history.append(message)
+
+        # If there is a question (not full prompt), add it as the last message
+        if question:
+            final_message_parts = []
+            if image_url:
+                image_parts = self._create_image_part(image_url)
+                final_message_parts.extend(image_parts)
+            final_message_parts.append(types.Part.from_text(text=question))
+            final_message = types.Content(role=Roles.USER, parts=final_message_parts)
+            google_history.append(final_message)
+
+        return ConvertedHistory(
+            contents=google_history, system_instructions=system_instructions
         )
 
-        model_engine_response = AskModelEngineResponse(
-            response=response.text,
-        )
+    def _create_image_part(self, image_url: List[str]) -> List[types.Part]:
+        """
+        Create image parts from a list of image URLs depending on their type.
+        """
+        image_parts = []
+        for image in image_url:
+            url_type = classify_url(image)
+            if url_type == "web_url":
+                image_parts.append(types.Part.from_uri(file_uri=image))
+            elif url_type == "base64_image":
+                image_parts.append(types.Part.from_bytes(data=image))
+            else:
+                raise ValueError("Invalid image URL format.")
 
-        print(response)
-
-        return model_engine_response
-
-
-types.GenerateContentResponse()
-
-# class GenerateContentResponse(
-#     candidates: list[Candidate] | None = None,
-#     create_time: datetime | None = None,
-#     response_id: str | None = None,
-#     model_version: str | None = None,
-#     prompt_feedback: GenerateContentResponsePromptFeedback | None = None,
-#     usage_metadata: GenerateContentResponseUsageMetadata | None = None,
-#     automatic_function_calling_history: list[Content] | None = None,
-#     parsed: BaseModel | dict[Any, Any] | Enum | None = None
-# )
-
-# class GenerateContentConfig(
-#     http_options: HttpOptions | None = None,
-#     system_instruction: ContentUnion | None = None,
-#     temperature: float | None = None,
-#     top_p: float | None = None,
-#     top_k: float | None = None,
-#     candidate_count: int | None = None,
-#     max_output_tokens: int | None = None,
-#     stop_sequences: list[str] | None = None,
-#     response_logprobs: bool | None = None,
-#     logprobs: int | None = None,
-#     presence_penalty: float | None = None,
-#     frequency_penalty: float | None = None,
-#     seed: int | None = None,
-#     response_mime_type: str | None = None,
-#     response_schema: SchemaUnion | None = None,
-#     routing_config: GenerationConfigRoutingConfig | None = None,
-#     model_selection_config: ModelSelectionConfig | None = None,
-#     safety_settings: list[SafetySetting] | None = None,
-#     tools: ToolListUnion | None = None,
-#     tool_config: ToolConfig | None = None,
-#     labels: dict[str, str] | None = None,
-#     cached_content: str | None = None,
-#     response_modalities: list[str] | None = None,
-#     media_resolution: MediaResolution | None = None,
-#     speech_config: SpeechConfigUnion | None = None,
-#     audio_timestamp: bool | None = None,
-#     automatic_function_calling: AutomaticFunctionCallingConfig | None = None,
-#     thinking_config: ThinkingConfig | None = None
-# )
+        return image_parts
