85ee54df91d8c7eafe822b17201897d3b0e2d13f
SubhajitMohanty1 <subhajitmohanty7890@gmail.com>
2025-06-13 13:20:20 +0530

    feat(tokens per request): updated (#344)

--- src/prerna/auth/utils/reactors/admin/AdminGetDatabaseUsageReactor.java
+++ src/prerna/auth/utils/reactors/admin/AdminGetDatabaseUsageReactor.java
@@ -17,37 +17,53 @@ public class AdminGetDatabaseUsageReactor extends AbstractReactor {
 	public AdminGetDatabaseUsageReactor() {
 		this.keysToGet = new String[]{ ReactorKeysEnum.DATABASE.getKey(), ReactorKeysEnum.LIMIT.getKey(), ReactorKeysEnum.OFFSET.getKey(), ReactorKeysEnum.START_DATE.getKey(), ReactorKeysEnum.END_DATE.getKey()};
 	}
-	
+
 	@Override
 	public NounMetadata execute() {
 		User user = this.insight.getUser();
 		SecurityAdminUtils adminUtils = SecurityAdminUtils.getInstance(user);
-		if(adminUtils == null) {
+		if (adminUtils == null) {
 			throw new IllegalArgumentException("User must be an admin to perform this function");
 		}
-		
+
 		if (!Utility.isUserTrackingEnabled()) {
-			throw new IllegalArgumentException("User Tracking Must be Enabled For this Report");
+			throw new IllegalArgumentException("User tracking must be enabled for this report");
 		}
-		
+
 		organizeKeys();
 		String databaseId = this.keyValue.get(this.keysToGet[0]);
-		if(databaseId == null || databaseId.isEmpty()) {
+		if (databaseId == null || databaseId.isEmpty()) {
 			throw new IllegalArgumentException("Must input a database id");
 		}
 		String limit = this.keyValue.get(this.keysToGet[1]);
-		String offset = this.keyValue.get(this.keysToGet[2]);	
+		String offset = this.keyValue.get(this.keysToGet[2]);
 		String startDate = this.keyValue.get(ReactorKeysEnum.START_DATE.getKey());
 		String endDate = this.keyValue.get(ReactorKeysEnum.END_DATE.getKey());
-		
+
 		List<Map<String, Object>> databaseUsage = UserTrackingUtils.getDatabaseUsage(databaseId, limit, offset, startDate, endDate);
-		
-		return new NounMetadata(databaseUsage, PixelDataType.FORMATTED_DATA_SET); 
+
+		return new NounMetadata(databaseUsage, PixelDataType.FORMATTED_DATA_SET);
 	}
-	
+
 	@Override
 	public String getReactorDescription() {
-		return "Get the usage metrics for a database";
+		return "Get the usage metrics for a database for admin users.";
+	}
+
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if (key.equals(ReactorKeysEnum.ENGINE.getKey())) {
+			return "Database id of a database " + ReactorKeysEnum.ENGINE.getKey();
+		} else if (key.equals(ReactorKeysEnum.LIMIT.getKey())) {
+			return "Limit of an engine " + ReactorKeysEnum.LIMIT.getKey();
+		} else if (key.equals(ReactorKeysEnum.OFFSET.getKey())) {
+			return "Offset of an engine " + ReactorKeysEnum.OFFSET.getKey();
+		} else if (key.equals(ReactorKeysEnum.START_DATE.getKey())) {
+			return "Start date of an engine " + ReactorKeysEnum.START_DATE.getKey();
+		} else if (key.equals(ReactorKeysEnum.END_DATE.getKey())) {
+			return "End date of an engine " + ReactorKeysEnum.END_DATE.getKey();
+		}
+		return super.getDescriptionForKey(key);
 	}
 
 }
--- src/prerna/auth/utils/reactors/admin/AdminGetEngineUsagePerProjectReactor.java
+++ src/prerna/auth/utils/reactors/admin/AdminGetEngineUsagePerProjectReactor.java
@@ -18,33 +18,54 @@ public class AdminGetEngineUsagePerProjectReactor extends AbstractReactor {
 	public AdminGetEngineUsagePerProjectReactor() {
 		this.keysToGet = new String[]{ReactorKeysEnum.ENGINE.getKey(), ReactorKeysEnum.LIMIT.getKey(), ReactorKeysEnum.OFFSET.getKey(), ReactorKeysEnum.START_DATE.getKey(), ReactorKeysEnum.END_DATE.getKey()};
 	}
-	
+
 	@Override
 	public NounMetadata execute() {
 		User user = this.insight.getUser();
 		SecurityAdminUtils adminUtils = SecurityAdminUtils.getInstance(user);
-		if(adminUtils == null) {
+		if (adminUtils == null) {
 			throw new IllegalArgumentException("User must be an admin to perform this function");
 		}
-		
+
 		if (!Utility.isModelInferenceLogsEnabled()) {
-			throw new IllegalArgumentException("Model Inference Logs Database Must be functioning for this report to be generated");
+			throw new IllegalArgumentException("Model inference logs database must be functioning for this report to be generated");
 		}
-		
+
 		organizeKeys();
 		String engineId = this.keyValue.get(this.keysToGet[0]);
-		if(engineId == null || engineId.isEmpty()) {
+		if (engineId == null || engineId.isEmpty()) {
 			throw new IllegalArgumentException("Must input an engine id");
 		}
 		engineId = SecurityQueryUtils.testUserEngineIdForAlias(this.insight.getUser(), engineId);
 		String limit = this.keyValue.get(this.keysToGet[1]);
-		String offset = this.keyValue.get(this.keysToGet[2]);	
+		String offset = this.keyValue.get(this.keysToGet[2]);
 		String startDate = this.keyValue.get(ReactorKeysEnum.START_DATE.getKey());
 		String endDate = this.keyValue.get(ReactorKeysEnum.END_DATE.getKey());
-		
+
 		List<Map<String, Object>> tokenUsagePerProjectList = ModelInferenceLogsUtils.getTokenUsagePerProjectForEngine(engineId, limit, offset, startDate, endDate);
 
 		return new NounMetadata(tokenUsagePerProjectList, PixelDataType.FORMATTED_DATA_SET);
+	}
+
+	@Override
+	public String getReactorDescription() {
+		return "This reactor returns the number of tokens usage per project for an engine for admin users.";
+	}
+
+	@Override
+	protected String getDescriptionForKey(String key) {
+		if (key.equals(ReactorKeysEnum.ENGINE.getKey())) {
+			return "Engine id of an engine " + ReactorKeysEnum.ENGINE.getKey();
+		} else if (key.equals(ReactorKeysEnum.LIMIT.getKey())) {
+			return "Limit of an engine " + ReactorKeysEnum.LIMIT.getKey();
+		} else if (key.equals(ReactorKeysEnum.OFFSET.getKey())) {
+			return "Offset of an engine " + ReactorKeysEnum.OFFSET.getKey();
+		} else if (key.equals(ReactorKeysEnum.START_DATE.getKey())) {
+			return "Start date of an engine " + ReactorKeysEnum.START_DATE.getKey();
+		} else if (key.equals(ReactorKeysEnum.END_DATE.getKey())) {
+			return "End date of an engine " + ReactorKeysEnum.END_DATE.getKey();
+		}
+		return super.getDescriptionForKey(key);
 	}
 
 }
