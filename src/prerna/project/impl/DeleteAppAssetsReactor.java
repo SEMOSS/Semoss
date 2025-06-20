fa5b0391c327c3f5e7ca8ca4dead13d3fb04c4eb
Maher Khalil <themaherkhalil@gmail.com>
2025-06-20 14:18:53 -0400

    renaming project to app, adding GetAppAsset reactor

--- src/prerna/project/impl/DeleteAppAssetsReactor.java
+++ src/prerna/project/impl/DeleteAppAssetsReactor.java
@@ -24,11 +24,11 @@ import prerna.util.Utility;
 import prerna.util.git.GitDestroyer;
 import prerna.util.git.GitRepoUtils;
 
-public class DeleteProjectAssetsReactor extends AbstractReactor {
+public class DeleteAppAssetsReactor extends AbstractReactor {
 
-	private static final Logger classLogger = LogManager.getLogger(DeleteProjectAssetsReactor.class);
+	private static final Logger classLogger = LogManager.getLogger(DeleteAppAssetsReactor.class);
 
-	public DeleteProjectAssetsReactor() {
+	public DeleteAppAssetsReactor() {
 		this.keysToGet = new String[] { ReactorKeysEnum.PROJECT.getKey(), 
 				ReactorKeysEnum.FILE_PATH.getKey(),
 				ReactorKeysEnum.COMMENT_KEY.getKey() };
