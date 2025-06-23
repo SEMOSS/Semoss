6d2510e10a27e97db5d0249de429a2c0e0be57bc
AndrewRodddd <62724891+AndrewRodddd@users.noreply.github.com>
2025-06-23 15:49:23 -0400

    fix: uses Files.readAllLines instead of Scanner, removes unnecessary comments from resource files

--- test/prerna/configure/MeUnitTests.java
+++ test/prerna/configure/MeUnitTests.java
@@ -1,8 +1,8 @@
 package prerna.configure;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.verify;
 
+import java.io.File;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,7 +10,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.Scanner;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -42,42 +41,42 @@ public class MeUnitTests {
 
         temp = tempDir.resolve("tomcat");
         Files.createDirectory(temp);
-        confDir = tempDir.resolve(temp + "\\conf");
+        confDir = tempDir.resolve(temp + File.separator + "conf");
         Files.createDirectory(confDir);
-        bin = tempDir.resolve(temp + "\\bin");
+        bin = tempDir.resolve(temp + File.separator + "bin");
         Files.createDirectories(bin);
-        temp = tempDir.resolve(temp + "\\webapps");
+        temp = tempDir.resolve(temp + File.separator + "webapps");
         Files.createDirectory(temp);
-        temp = tempDir.resolve(temp + "\\Monolith");
+        temp = tempDir.resolve(temp + File.separator + "Monolith");
         Files.createDirectory(temp);
-        webINF = tempDir.resolve(temp + "\\WEB-INF");
+        webINF = tempDir.resolve(temp + File.separator + "WEB-INF");
         Files.createDirectory(webINF);
 
-        temp = tempDir.resolve(homePath + "\\config");
+        temp = tempDir.resolve(homePath + File.separator + "config");
         Files.createDirectories(temp);
-        openBrowser = tempDir.resolve(temp + "\\openBrowser.bat");
+        openBrowser = tempDir.resolve(temp + File.separator + "openBrowser.bat");
         openBrowser = Files.createFile(openBrowser);
 
-        configured = tempDir.resolve(homePath + "\\configured.txt");
+        configured = tempDir.resolve(homePath + File.separator + "configured.txt");
         configured = Files.createFile(configured);
 
-        setPath = tempDir.resolve(homePath + "\\setPath.bat");
+        setPath = tempDir.resolve(homePath + File.separator + "setPath.bat");
         setPath = Files.createFile(setPath);
 
-        setenv = tempDir.resolve(bin + "\\setenv.bat");
+        setenv = tempDir.resolve(bin + File.separator + "setenv.bat");
         setenv = Files.createFile(setenv);
 
-        rdfTemp = tempDir.resolve(homePath + "\\RDF_Map.prop");
+        rdfTemp = tempDir.resolve(homePath + File.separator + "RDF_Map.prop");
         URL url = Me.class.getResource("test_RDF_Map.prop");
         Path p = Paths.get(url.toURI());
         rdfTemp = Files.copy(p, rdfTemp);
 
-        webxmlTemp = tempDir.resolve(webINF + "\\web.xml");
+        webxmlTemp = tempDir.resolve(webINF + File.separator + "web.xml");
         url = Me.class.getResource("test_web.xml");
         p = Paths.get(url.toURI());
         webxmlTemp = Files.copy(p, webxmlTemp);
 
-        server = tempDir.resolve(confDir + "\\server.xml");
+        server = tempDir.resolve(confDir + File.separator + "server.xml");
         url = Me.class.getResource("test_server.xml");
         p = Paths.get(url.toURI());
         server = Files.copy(p, server);
@@ -93,7 +92,7 @@ public class MeUnitTests {
     @Test
     void mainTest() throws Exception {
         int idx = 0;
-        String tempDirString = temp.toAbsolutePath().toString().replace('\\', '/');
+        String tempDirString = temp.toAbsolutePath().toString().replace(File.separator, "/");
         String[] args = {
             homePath.toAbsolutePath().toString(), 
             rHome.toAbsolutePath().toString(), 
@@ -127,101 +126,45 @@ public class MeUnitTests {
         }};
 
 		for (String x : rdfKeys) {
-			assertTrue(p.getProperty(x).startsWith(homePath.toAbsolutePath().toString().replace('\\', '/')));
+			assertTrue(p.getProperty(x).startsWith(homePath.toAbsolutePath().toString().replace(File.separator, "/")));
 		}
 
-        Scanner scn = new Scanner(webxmlTemp);
-
         // Checks resulting web.xml
-        while (scn.hasNextLine()) {
-            String str = scn.nextLine();
-            if (str.trim().startsWith("<param-value>")) {
-                assertTrue(str.contains(rdfTemp.toAbsolutePath().toString().replace('\\', '/')));
-            }
-
-            continue;
-        }
-        scn.close();
+        List<String> lines = Files.readAllLines(webxmlTemp);
+        assertTrue(lines.get(3).contains(rdfTemp.toAbsolutePath().toString().replace(File.separator, "/")));
 
         // Checks resulting setPath.bat
-        scn = new Scanner(setPath);
-        while (scn.hasNextLine()) {
-            String str = scn.nextLine();
-
-            if (!str.toLowerCase().startsWith("set"))    continue;
-
-            // shortens string to first index of temp dir until end, splits string by ; to test each indiv path in string
-            idx = str.indexOf(tempDirString);
-            if (idx >= 0)
-                str = str.substring(idx);
-            String[] arr = str.split(";");
-            for (String s : arr) {
-                assertTrue(s.contains(tempDirString));
-            }
-        }
-        scn.close();
+        lines = Files.readAllLines(setPath);
+        assertTrue(lines.get(2).contains(tempDirString));
+        assertTrue(lines.get(3).contains(tempDirString));
+        assertTrue(lines.get(5).contains(tempDirString));
 
         // Checks resulting setenv.bat
-        scn = new Scanner(setenv);
-        while (scn.hasNextLine()) {
-            String str = scn.nextLine();
-            idx = str.indexOf(tempDirString);
-            if (idx >= 0)
-                str = str.substring(idx);
-
-            String[] arr = str.split(";");
-            for (String s: arr)
-                assertTrue(s.contains(tempDirString));
-        }
-        scn.close();
+        lines = Files.readAllLines(setenv);
+        assertTrue(lines.get(0).contains(tempDirString));
 
         // Checks resulting server.xml
-        String portNum = "";
-        scn = new Scanner(server);
-        while (scn.hasNextLine()) {
-            String str = scn.nextLine();
-
-            if (str.trim().startsWith("<?xml")) {
-                assertTrue(str.equalsIgnoreCase("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><!--"));
-            } else if (str.trim().startsWith("<Connector connectionTimeout=\"20000\"")) {
-                idx = str.indexOf("port");
-                int tempIdx = str.indexOf(" protocol");
-                if (idx >= 0 && tempIdx >= 0)
-                    str = str.substring(idx, tempIdx);
-                idx = str.indexOf("\"") + 1;
-                tempIdx = str.lastIndexOf("\"");
-                if (idx >= 0 && tempIdx >= 0)
-                    portNum = str.substring(idx, tempIdx);
-                break;
-            }
-        }
-        scn.close();
+        lines = Files.readAllLines(server);
+        assertTrue(lines.get(0).startsWith("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>"));
+        
+        String tempString = lines.get(2).trim();
+        idx = tempString.indexOf("port");
+        tempString = idx >= 0 ? tempString.substring(idx + 6) : ""; // String looks like <PORT_NUMBER>" protocol="...
+        idx = tempString.indexOf(" protocol");
+        tempString = idx >= 0 ? tempString.substring(0, idx) : "";  // String looks like <PORT_NUMBER>"
+        String portNum = (!tempString.equals("")) ? tempString.substring(0, tempString.indexOf("\"")): "";
 
         if(portNum.isEmpty())    return;
 
-        // Checks resulting openBrowser.bat
-        scn = new Scanner(openBrowser);
-        while (scn.hasNextLine()) {
-            String str = scn.nextLine();
-            if (str.equals("ECHO OFF") || str.isEmpty()) continue;
+        // Checks resulting openBrowser.bat 04
+        lines = Files.readAllLines(openBrowser);
+        assertTrue(lines.get(0).contains("http://localhost:" + portNum + "/SemossWeb/"));
+        assertTrue(lines.get(4).contains("http://localhost:" + portNum + "/SemossWeb/"));
 
-            assertTrue(str.contains("http://localhost:" + portNum + "/SemossWeb/"));
-        }
-        scn.close();
+        // Checks resulting configured.txt 0
+        lines = Files.readAllLines(configured);
+        assertTrue(lines.get(0).contains(portNum));
+        assertTrue(lines.get(0).contains("http://localhost:" + portNum + "/SemossWeb/"));
 
-        // Checks resulting configured.txt
-        scn = new Scanner(configured);
-        if (scn.hasNextLine()) {
-            String str = scn.nextLine();
-            idx = str.indexOf("= ");
-            String s ="";
-            if (idx >= 0)
-                s = str.substring(idx);
-            idx = s.indexOf("= ") + 2;
-            if (idx >= 0)
-                assertTrue(s.substring(idx).startsWith(portNum));
-            assertTrue(str.contains("http://localhost:" + portNum + "/SemossWeb/"));
-        }
-        scn.close();
     }
 }
