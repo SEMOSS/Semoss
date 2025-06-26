e9bde806599aba7c67da9247b324e97e25deb220
Weiler, Ryan <ryanweiler92@gmail.com>
2025-06-26 14:27:48 -0400

    feat: update reactor to accept job id.. kill job id from PixelJobManager

--- src/prerna/reactor/StopPixelExecutionReactor.java
+++ src/prerna/reactor/StopPixelExecutionReactor.java
@@ -1,14 +1,24 @@
 package prerna.reactor;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.tcp.client.SocketClient;
+import prerna.sablecc2.comm.PixelJobManager;
 
 public class StopPixelExecutionReactor extends AbstractReactor {
+	
+	private static final Logger classLogger = LogManager.getLogger(StopPixelExecutionReactor.class);
 		
-	public StopPixelExecutionReactor() {}
+	public StopPixelExecutionReactor() {
+		// THIS IS A JOB ID
+		this.keysToGet = new String[] {ReactorKeysEnum.ID.getKey()};
+		this.keyRequired = new int[] {1};
+	}
 	
 	@Override
 	public NounMetadata execute() {
@@ -17,6 +27,13 @@ public class StopPixelExecutionReactor extends AbstractReactor {
 		SocketClient socketClient = this.insight.getUser().getSocketClient(false);
 		
 		String killActiveThreads = socketClient.killActiveThreadsWithInterrupt();
+		
+		String jobId = this.keyValue.get(ReactorKeysEnum.ID.getKey());
+		
+		PixelJobManager jobManager = PixelJobManager.getManager();
+		
+		jobManager.clearJob(jobId);
+		jobManager.removeJob(jobId);
 		
 		return new NounMetadata(killActiveThreads, PixelDataType.CONST_STRING, PixelOperationType.OPERATION);
 	}
