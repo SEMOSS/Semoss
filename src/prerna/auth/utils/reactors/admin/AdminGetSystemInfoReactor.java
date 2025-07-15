45d7965f395fd46d72541fa6b8809f45eb66f506
maxquinones <mquinones@deloitte.com>
2025-07-15 07:22:24 -0700

    refactor: removing unwanted elements in map

--- src/prerna/auth/utils/reactors/admin/AdminGetSystemInfoReactor.java
+++ src/prerna/auth/utils/reactors/admin/AdminGetSystemInfoReactor.java
@@ -43,22 +43,14 @@ public class AdminGetSystemInfoReactor extends AbstractReactor {
             ipaddress = "unknown-ipaddress";
         }
 
-        Map<String, Object> systemInfoMap = AdminGetSystemInfoReactor.getSystemInfoDetailsMap(hostname, ipaddress);
-        return new NounMetadata(systemInfoMap, PixelDataType.MAP);
-    }
-
-    public static Map<String, Object> getSystemInfoDetailsMap(String hostname, String ipaddress) {
-
         Map<String, Object> systemInfoDetailsmap = new HashMap<>();
         systemInfoDetailsmap.put("isClustered", Boolean.valueOf(ClusterUtil.IS_CLUSTER));
         systemInfoDetailsmap.put("STORAGE_PROVIDER", ClusterUtil.STORAGE_PROVIDER);
-        systemInfoDetailsmap.put("REMOTE_RSERVE", ClusterUtil.REMOTE_RSERVE);
-        systemInfoDetailsmap.put("areLoadEnginesLocally", Boolean.valueOf(ClusterUtil.LOAD_ENGINES_LOCALLY));
         systemInfoDetailsmap.put("hostname", hostname);
         systemInfoDetailsmap.put("IPAddress", ipaddress);
-        systemInfoDetailsmap.put("IMAGES_FOLDER_PATH", ClusterUtil.IMAGES_FOLDER_PATH);
         systemInfoDetailsmap.put("IS_CLUSTERED_SCHEDULER", Boolean.valueOf(ClusterUtil.IS_CLUSTERED_SCHEDULER));
-        return systemInfoDetailsmap;
 
+        return new NounMetadata(systemInfoDetailsmap, PixelDataType.MAP);
     }
+ 
 }
