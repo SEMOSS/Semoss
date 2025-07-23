b160110eb03406713aae4806ed52c56386166f63
Rithvik Doshi <doshirithvik@gmail.com>
2025-07-23 14:23:06 -0400

    making some progress integrating both reactors

--- src/prerna/engine/impl/model/AskPlayground2Reactor.java
+++ src/prerna/engine/impl/model/AskPlayground2Reactor.java
@@ -1,18 +1,261 @@
 package prerna.engine.impl.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import prerna.auth.AccessToken;
+import prerna.auth.User;
+import prerna.auth.utils.SecurityEngineUtils;
+import prerna.engine.api.IFunctionEngine;
+import prerna.engine.api.IModelEngine;
+import prerna.engine.impl.model.message.InputMessage;
+import prerna.engine.impl.model.message.MessageUtils;
+import prerna.engine.impl.model.message.ResponseMessage;
+import prerna.project.api.IProject;
 import prerna.reactor.AbstractReactor;
+import prerna.sablecc2.om.GenRowStruct;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.PixelOperationType;
+import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.util.Utility;
 
 /**
  * This reactor calls the specified model with a command, any optional context and tools to use, and returns the response message
  * This reactor will eventually replace the current contents of AskPlaygroundReactor
  */
 public class AskPlayground2Reactor extends AbstractReactor {
+	
+	public AskPlayground2Reactor() {
+		this.keysToGet = new String[] { 
+				ReactorKeysEnum.ENGINE.getKey(), 
+				ReactorKeysEnum.ROOM_ID.getKey(), 
+				ReactorKeysEnum.COMMAND.getKey(), 
+				ReactorKeysEnum.CONTEXT.getKey(), 
+				ReactorKeysEnum.USE_HISTORY.getKey(),
+				ReactorKeysEnum.PARAM_VALUES_MAP.getKey(),
+				ReactorKeysEnum.IMAGE.getKey(),
+				ReactorKeysEnum.URL.getKey(),
+				ReactorKeysEnum.FILE_PATH.getKey()
+		};
+//		TODO: add this back when needed
+//		this.keyRequired = new int[] { 1, 0, 1, 0, 0, 0, 0, 0 };
+	}
 
 	@Override
 	public NounMetadata execute() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		organizeKeys();
+		String engineId = this.keyValue.get(ReactorKeysEnum.ENGINE.getKey());
+		String roomId = this.keyValue.get(ReactorKeysEnum.ROOM_ID.getKey());
+		User user = this.insight.getUser();
+		AccessToken userToken = user.getPrimaryLoginToken();
+		
+		if (!SecurityEngineUtils.userCanViewEngine(user, engineId)) {
+			throw new IllegalArgumentException("Model " + engineId + " does not exist or user does not have access to this model");
+		}
+		
+		String question = Utility.decodeURIComponent(this.keyValue.get(ReactorKeysEnum.COMMAND.getKey()));
+		String context = this.keyValue.get(ReactorKeysEnum.CONTEXT.getKey());
+		if (context != null) {
+			context = Utility.decodeURIComponent(context);
+		}
+		
+		Map<String, Object> paramMap = getParamMap();
+		IModelEngine modelEngine = Utility.getModel(engineId);
+		if (paramMap == null) {
+			paramMap = new HashMap<String, Object>();
+		}
+		Boolean useHistoryParam = Boolean.parseBoolean(this.keyValue.getOrDefault(ReactorKeysEnum.USE_HISTORY.getKey(), "true")+"");
+		paramMap.put("use_history", useHistoryParam);
+		
+        List<String> inputImages = getImages();
+        List<String> inputImageURLs = getImageURLs();
+        
+//        TODO: Gonna need to move the files to a room if exists
+        List<String> inputFiles = getFiles();
+        
+        boolean executeTool = false;
+        String executeToolStr = this.keyValue.get(ReactorKeysEnum.EXECUTE_TOOLS.getKey());
+        if (executeToolStr != null) {
+          executeTool = Boolean.parseBoolean(executeToolStr);
+        }
+        
+        List<String> engineToolIDs = getEngineToolIDs();
+        List<String> projectToolIDs = getProjectToolIDs();
+        
+        if (!engineToolIDs.isEmpty() || !projectToolIDs.isEmpty()) {
+
+            // Check if the "tools_choice" key exists in the paramMap, else add it
+            if (!paramMap.containsKey("tool_choice")) {
+              paramMap.put("tool_choice", "auto");
+            }
+
+            // Check if the "tools" key exists in the paramMap
+            List<Map<String, Object>> toolsList;
+            if (paramMap.containsKey("tools")) {
+              // Retrieve the existing list of tools
+              toolsList = (List<Map<String, Object>>) paramMap.get("tools");
+            } else {
+              // Create a new list for tools
+              toolsList = new ArrayList<Map<String, Object>>();
+              paramMap.put("tools", toolsList);
+            }
+
+            // Iterate over each engine ID and add the function tool to the tools list
+            for (String engineToolID : engineToolIDs) {
+              IFunctionEngine function = Utility.getFunctionEngine(engineToolID);
+              assert function.getCatalogType() == IFunctionEngine.CATALOG_TYPE.FUNCTION;
+              Map<String, Object> functionToolMap = function.buildFunctionEngineToolMap();
+              toolsList.add(functionToolMap);
+            }
+
+            // Iterate over each project ID and add the tool to the tools list
+            for (String projectToolID : projectToolIDs) {
+              IProject project = Utility.getProject(projectToolID);
+              assert project.getProjectType() == IProject.PROJECT_TYPE.CODE;
+              Map<String, Object> projectToolMap = project.buildOpenAIFunctionEngineToolMap();
+              toolsList.add(projectToolMap);
+            }
+          }
+		
+		Room room = RoomUtils.createRoomIfNotExists(roomId, insight, modelEngine, question);
+		
+	///// MESSAGE CREATION //////////
+
+//		Need to make sure we are adding files to the message (if applicable/necessary) and then need to add tools through the builder
+        MessageUtils.copyFilesToRoomFolder(inputImages, room, insight);
+        InputMessage msg;
+        msg = InputMessage.builder(room)
+        .withInputUIPrompt(question)
+        .withInputPrompt(question)
+        .withModelType(modelEngine.getModelType())
+        .withParamMap(paramMap)
+        .withImages(inputImages, room)
+        .withImageUrls(inputImageURLs)
+        .build();
+        
+        ResponseMessage response = room.ask(msg, context, modelEngine);
+		return new NounMetadata(response.getModelEngineResponse().toMap(), PixelDataType.MAP, PixelOperationType.OPERATION);
 	}
+	
+    public List<String> getImages() {
+        List<String> inputStrings = new ArrayList<>();
+        GenRowStruct grs = this.store.getNoun(ReactorKeysEnum.IMAGE.getKey());
+        if (grs != null && !grs.isEmpty()) {
+            int size = grs.size();
+            for (int i = 0; i < size; i++) inputStrings.add(grs.get(i).toString());
+            return inputStrings;
+        }
+        int size = this.curRow.size();
+        for (int i = 0; i < size; i++) inputStrings.add(this.curRow.get(i).toString());
+        return inputStrings;
+    }
+    
+    public List<String> getFiles() {
+        List<String> inputStrings = new ArrayList<>();
+        GenRowStruct grs = this.store.getNoun(ReactorKeysEnum.FILE_PATH.getKey());
+        if (grs != null && !grs.isEmpty()) {
+            int size = grs.size();
+            for (int i = 0; i < size; i++) inputStrings.add(grs.get(i).toString());
+            return inputStrings;
+        }
+        int size = this.curRow.size();
+        for (int i = 0; i < size; i++) inputStrings.add(this.curRow.get(i).toString());
+        return inputStrings;
+    }
+    
+    public List<String> getImageURLs() {
+        List<String> inputStrings = new ArrayList<>();
+        GenRowStruct grs = this.store.getNoun(ReactorKeysEnum.URL.getKey());
+        if (grs != null && !grs.isEmpty()) {
+            int size = grs.size();
+            for (int i = 0; i < size; i++) inputStrings.add(grs.get(i).toString());
+            return inputStrings;
+        }
+        int size = this.curRow.size();
+        for (int i = 0; i < size; i++) inputStrings.add(this.curRow.get(i).toString());
+        return inputStrings;
+    }
+    
+    @SuppressWarnings("unchecked")
+	private Map<String, Object> getParamMap() {
+        GenRowStruct mapGrs = this.store.getNoun(ReactorKeysEnum.PARAM_VALUES_MAP.getKey());
+        if(mapGrs != null && !mapGrs.isEmpty()) {
+            List<NounMetadata> mapInputs = mapGrs.getNounsOfType(PixelDataType.MAP);
+            if(mapInputs != null && !mapInputs.isEmpty()) {
+                return (Map<String, Object>) mapInputs.get(0).getValue();
+            }
+        }
+        List<NounMetadata> mapInputs = this.curRow.getNounsOfType(PixelDataType.MAP);
+        if(mapInputs != null && !mapInputs.isEmpty()) {
+            return (Map<String, Object>) mapInputs.get(0).getValue();
+        }
+        return null;
+    }
+    
+    @Override
+	public String getReactorDescription() {
+//    	TODO: fill this in
+		return "TODO";
+	}
+    
+    /**
+     * TODO: See @LLM2Reactor for how to do this
+     */
+	@Override
+	protected String getDescriptionForKey(String key) {
+		return "";
+	}
+	
+	 /** @return list of engines */
+	  public List<String> getEngineToolIDs() {
+	    List<String> inputStrings = new ArrayList<>();
+
+	    // see if added as key
+	    GenRowStruct grs = this.store.getNoun(this.keysToGet[5]);
+	    if (grs != null && !grs.isEmpty()) {
+	      int size = grs.size();
+	      for (int i = 0; i < size; i++) {
+	        inputStrings.add(grs.get(i).toString());
+	      }
+	      return inputStrings;
+	    }
+
+	    // no key is added, grab all inputs
+	    int size = this.curRow.size();
+	    for (int i = 0; i < size; i++) {
+	      inputStrings.add(this.curRow.get(i).toString());
+	    }
+
+	    return inputStrings;
+	  }
+
+	  /** @return list of project IDs */
+	  public List<String> getProjectToolIDs() {
+	    List<String> inputStrings = new ArrayList<>();
+
+	    // see if added as key
+	    GenRowStruct grs = this.store.getNoun(this.keysToGet[6]);
+	    if (grs != null && !grs.isEmpty()) {
+	      int size = grs.size();
+	      for (int i = 0; i < size; i++) {
+	        inputStrings.add(grs.get(i).toString());
+	      }
+	      return inputStrings;
+	    }
+
+	    // no key is added, grab all inputs
+	    int size = this.curRow.size();
+	    for (int i = 0; i < size; i++) {
+	      inputStrings.add(this.curRow.get(i).toString());
+	    }
+
+	    return inputStrings;
+	  }
+    
 
 }
