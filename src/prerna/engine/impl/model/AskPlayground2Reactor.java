0b12e39ad291471afe1ed803920278f22f483cac
Rithvik Doshi <doshirithvik@gmail.com>
2025-07-23 12:38:34 -0400

    init reactor

---
+++ src/prerna/engine/impl/model/AskPlayground2Reactor.java
@@ -0,0 +1,18 @@
+package prerna.engine.impl.model;
+
+import prerna.reactor.AbstractReactor;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+
+/**
+ * This reactor calls the specified model with a command, any optional context and tools to use, and returns the response message
+ * This reactor will eventually replace the current contents of AskPlaygroundReactor
+ */
+public class AskPlayground2Reactor extends AbstractReactor {
+
+	@Override
+	public NounMetadata execute() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
