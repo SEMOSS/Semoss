d80ed2ab6c28f22db1a18ffe611c04b2de988e28
dineshS96 <drsharma886@gmail.com>
2025-07-25 15:18:59 +0530

    New pojo for jira details

---
+++ src/prerna/reactor/model/JiraTicketDetails.java
@@ -0,0 +1,39 @@
+package prerna.reactor.model;
+
+//This pojo is to return id and link of Jira issues after creation
+public class JiraTicketDetails {
+
+	private String id;
+	private String link;
+	private String summary;
+	
+    public JiraTicketDetails(String id, String link, String summary) { 
+		this.id = id;
+		this.link = link;
+		this.summary = summary;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+}
