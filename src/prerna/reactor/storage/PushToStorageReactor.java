72800cbd51fd4c3acd92342cfd23bd4cc11378a5
RawanAbdelkhalek <66391994+RawanAbdelkhalek@users.noreply.github.com>
2025-07-31 17:01:23 +0300

    feat: clean PushToStorageReactor (#372)
    Signed-off-by: RawanAbdelkhalek <66391994+RawanAbdelkhalek@users.noreply.github.com>

--- src/prerna/reactor/storage/PushToStorageReactor.java
+++ src/prerna/reactor/storage/PushToStorageReactor.java
@@ -27,31 +27,6 @@ public class PushToStorageReactor extends AbstractReactor {
 				ReactorKeysEnum.SPACE.getKey(), ReactorKeysEnum.FILE_PATH.getKey(), ReactorKeysEnum.METADATA.getKey()};
 	}
 	
-//	@Override
-//	public NounMetadata execute() {
-//		organizeKeys();
-//		IStorageEngine storage = getStorage();
-//		// check that the user can edit the engine
-//		if (!SecurityEngineUtils.userCanEditEngine(this.insight.getUser(), storage.getEngineId())) {
-//			throw new IllegalArgumentException("User does not have permission to push into the remote storage");
-//		}
-//		String storageFolderPath = this.keyValue.get(ReactorKeysEnum.STORAGE_PATH.getKey());
-//		String fileLocation = Utility.normalizePath(UploadInputUtility.getFilePath(this.store, this.insight));
-//		if(!new File(fileLocation).exists()) {
-//			throw new IllegalArgumentException("Unable to locate file");
-//		}
-//		
-//		Map<String, Object> metadata = getMetadata();
-//		try {
-//			storage.copyToStorage(fileLocation, storageFolderPath, metadata);
-//			return new NounMetadata(true, PixelDataType.BOOLEAN);
-//		} catch (Exception e) {
-//			classLogger.error(Constants.STACKTRACE, e);
-//			throw new IllegalArgumentException("Error occurred uploading local file to storage");
-//		}
-//	}
-	
-	
 	@Override
 	public NounMetadata execute() {
 	    organizeKeys();
