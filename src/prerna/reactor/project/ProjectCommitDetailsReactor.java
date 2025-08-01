2c82134238d561788cf179272d3b1f9507047d32
SubhajitMohanty1 <subhajitmohanty7890@gmail.com>
2025-08-01 11:40:52 +0530

    feat(Apps History): added pagination to the output of ProjectCommitDetailsReactor (#68)

--- src/prerna/reactor/project/ProjectCommitDetailsReactor.java
+++ src/prerna/reactor/project/ProjectCommitDetailsReactor.java
@@ -26,8 +26,8 @@ public class ProjectCommitDetailsReactor extends AbstractReactor {
 	private static final Logger classLogger = LogManager.getLogger(ProjectCommitDetailsReactor.class);
 
 	public ProjectCommitDetailsReactor() {
-		this.keysToGet = new String[] { ReactorKeysEnum.PROJECT.getKey() };
-		this.keyRequired = new int[] { 1 };
+		this.keysToGet = new String[] { ReactorKeysEnum.PROJECT.getKey(), ReactorKeysEnum.LIMIT.getKey(), ReactorKeysEnum.OFFSET.getKey()};
+		this.keyRequired = new int[] { 1, 1, 1 };
 	}
 
 	@Override
@@ -35,12 +35,14 @@ public class ProjectCommitDetailsReactor extends AbstractReactor {
 
 		organizeKeys();
 		String projectId = this.keyValue.get(this.keysToGet[0]);
+		int limit = Integer.parseInt(this.keyValue.get(this.keysToGet[1]));
+		int offset = Integer.parseInt(this.keyValue.get(this.keysToGet[2]));
 
 		List<Map<String, Object>> commits = new ArrayList<>();
 		String filePath = null;
 
 		try {
-			ProcessBuilder builder = new ProcessBuilder("git", "log", "--pretty=format:%H-%an-%ae-%ad-%s");
+			ProcessBuilder builder = new ProcessBuilder("git", "log", "--pretty=format:%H%n%an%n%ae%n%ad%n%s");
 			IProject project = Utility.getProject(projectId);
 			filePath = AssetUtility.getProjectVersionFolder(project.getProjectName(), projectId);
 			builder.directory(new File(filePath));
@@ -52,26 +54,32 @@ public class ProjectCommitDetailsReactor extends AbstractReactor {
 
 			String[] lines = output.split("\n");
 
-			for (int i = 0; i < lines.length; i++) {
-				String[] commitDetails = lines[i].split("-");
+			for (int i = 0; i < lines.length-3; i += 5) {
 				Map<String, Object> details=new LinkedHashMap<>();
-				details.put("commitId", commitDetails[0]);
+				details.put("commitId", lines[i]);
 				Map<String, String> authorDetails=new LinkedHashMap<>();
-				authorDetails.put("userId", commitDetails[1]);
-				authorDetails.put("userEmail", commitDetails[2]);
+				authorDetails.put("userId", lines[i+1]);
+				authorDetails.put("userEmail", lines[i+2]);
 				details.put("author", authorDetails);
-				details.put("date", commitDetails[3]);
-				details.put("commitMessage", commitDetails[4]);
+				details.put("date", lines[i+3]);
+				details.put("commitMessage", lines[i+4]);
 				commits.add(details);
-				System.out.println(details);
 			}
 
 		} catch (IOException e) {
 			classLogger.error("Please provide a valid directory name " + filePath, e);
 			throw new IllegalArgumentException("Please provide a valid directory name " + filePath, e);
 		}
+		
+		List<Map<String, Object>> paginatedCommits = new ArrayList<>();
 
-		return new NounMetadata(commits, PixelDataType.CONST_STRING, PixelOperationType.PROJECT_INFO);
+		int totalCommits = commits.size();
+		int fromIndex = Math.min((offset - 1) * limit, totalCommits);
+		int toIndex = Math.min(fromIndex + limit, totalCommits);
+
+		paginatedCommits.addAll(commits.subList(fromIndex, toIndex));
+
+		return new NounMetadata(paginatedCommits, PixelDataType.MAP, PixelOperationType.PROJECT_INFO);
 	}
 
 	@Override
