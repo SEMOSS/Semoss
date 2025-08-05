afaa34d69ad9dc086e842659d127e45b21b98826
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2025-08-05 15:52:37 +0300

    fix(StorageEngine): temporary fix for PullFromStorage in AWSNativeBlobStorageEngine

--- src/prerna/engine/impl/storage/AWSNativeBlobStorageEngine.java
+++ src/prerna/engine/impl/storage/AWSNativeBlobStorageEngine.java
@@ -381,7 +381,7 @@ public class AWSNativeBlobStorageEngine extends AbstractStorageEngine {
 			// Normalize prefix using the utility method
 			String prefix = Utility.normalizePath(s3FolderPath);
 			if (!prefix.endsWith("/") && !prefix.isEmpty()) {
-				prefix += "/";
+				// prefix += "/";
 			}
 
 			ListObjectsV2Request request = ListObjectsV2Request.builder().bucket(this.bucket).prefix(prefix)
