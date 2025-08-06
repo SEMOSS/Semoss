8d9de7cc6ef0c171b96b5abeaf4a7a3a48e16e50
AndrewRodddd <62724891+AndrewRodddd@users.noreply.github.com>
2025-08-06 09:39:12 -0400

    test(unit): adds tests for prerna.engine.impl.model.inferencetracking

---
+++ test/prerna/engine/impl/model/inferencetracking/GetAllEngineUsageReactorUnitTests.java
@@ -0,0 +1,91 @@
+package prerna.engine.impl.model.inferencetracking;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import prerna.auth.User;
+import prerna.auth.utils.SecurityEngineUtils;
+import prerna.auth.utils.SecurityQueryUtils;
+import prerna.om.Insight;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+
+public class GetAllEngineUsageReactorUnitTests {
+    User user;
+    Insight insight;
+    Map<String, String> map;
+    GetAllEngineUsageReactor reactor;
+
+    @BeforeEach
+    void setup() {
+        map = new HashMap<>();
+        map.put(ReactorKeysEnum.ENGINE.getKey(), "engine");
+        map.put(ReactorKeysEnum.LIMIT.getKey(), "limit");
+        map.put(ReactorKeysEnum.OFFSET.getKey(), "offset");
+        map.put(ReactorKeysEnum.START_DATE.getKey(), "start date");
+        map.put(ReactorKeysEnum.END_DATE.getKey(), "end date");
+
+        user = mock(User.class);
+        insight = mock(Insight.class);
+
+        reactor = new GetAllEngineUsageReactor();
+
+        reactor.setInsight(insight);
+        when(insight.getUser()).thenReturn(user);
+    }
+
+    @Test
+    void noEngineId() {
+        map.remove(ReactorKeysEnum.ENGINE.getKey());
+        reactor.keyValue = map;
+
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> reactor.execute());
+        assertEquals("Must input an engine id", e.getMessage());
+    }
+
+    @Test
+    void userNotOwner() {
+        reactor.keyValue = map;
+
+        try(MockedStatic<SecurityQueryUtils> queryUtils = Mockito.mockStatic(SecurityQueryUtils.class);
+            MockedStatic<SecurityEngineUtils> engineUtils = Mockito.mockStatic(SecurityEngineUtils.class)) {
+            queryUtils.when(() -> SecurityQueryUtils.testUserEngineIdForAlias(user, "engine")).thenReturn("engine");
+            engineUtils.when(() -> SecurityEngineUtils.userIsOwner(user, "engine")).thenReturn(false);
+        
+            IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> reactor.execute());
+            assertEquals("Engine does not exist or user is not an owner of Engine", e.getMessage());
+        }
+    }
+
+    @Test
+    void allKeys() {
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        reactor.keyValue = map;
+
+        try (MockedStatic<SecurityQueryUtils> queryUtils = Mockito.mockStatic(SecurityQueryUtils.class);
+            MockedStatic<SecurityEngineUtils> engineUtils = Mockito.mockStatic(SecurityEngineUtils.class);
+            MockedStatic<ModelInferenceLogsUtils> modelUtils = Mockito.mockStatic(ModelInferenceLogsUtils.class)) {
+            queryUtils.when(() -> SecurityQueryUtils.testUserEngineIdForAlias(user, "engine")).thenReturn("engine");
+            engineUtils.when(() -> SecurityEngineUtils.userIsOwner(user, "engine")).thenReturn(true);
+            modelUtils.when(() -> ModelInferenceLogsUtils.getOverAllEngineUsageFromModelInferenceLogs("engine", "limit", "offset", "start date", "end date")).thenReturn(list);
+        
+            NounMetadata n = reactor.execute();
+            assertEquals(list, n.getValue());
+            assertEquals(PixelDataType.FORMATTED_DATA_SET, n.getNounType());
+        }
+    }
+}
