c1b989e02838c3d0b2395ad2f3ab3eeaffcda9d3
Maher Khalil <themaherkhalil@gmail.com>
2025-08-21 17:08:59 -0400

    feat: exposing gliner guardrail on inputs to llm (#1163)
    Example GLINER smss file for guardrail engine
    
    ```
    #Base Properties
    ENGINE	fffd1e30-f92b-4ed7-aa31-006147c28925
    ENGINE_ALIAS	GLINER
    ENGINE_TYPE	prerna.engine.impl.guardrail.GLiNERGuardrailEngine
    
    MODEL_NAME urchade/gliner_multi_pii-v1
    ```
    
    Example Detoxify smss file for guardrail engine
    
    ```
    #Base Properties
    ENGINE	aaad1e30-f92b-4ed7-aa31-006147c28925
    ENGINE_ALIAS	Detoxify
    ENGINE_TYPE	prerna.engine.impl.guardrail.DetoxifyGuardrailEngine
    ```
    
    Example pipeline.json to add in a model engine assets
    
    ```
    {
      "pipelines": {
        "ask": {
          "input": [
            {
              "reactorClass": "prerna.reactor.interceptor.GenericGuardrailInputReactor",
              "params": {
                "blockOnGuardrailFailure": true,
                "guardrailEngineId": "fffd1e30-f92b-4ed7-aa31-006147c28925",
                "directParameters": {
                  "labels": [
                    "work",
                    "booking number",
                    "personally identifiable information",
                    "driver licence",
                    "person",
                    "book",
                    "full address",
                    "company",
                    "actor",
                    "character",
                    "email",
                    "passport number",
                    "Social Security Number",
                    "phone number"
                  ],
                  "threshold": 0.7
                },
                "inputMapping": {
                  "prompt": "arg0"
                }
              }
            },
            {
              "reactorClass": "prerna.reactor.interceptor.GenericGuardrailInputReactor",
              "params": {
                "blockOnGuardrailFailure": true,
                "guardrailEngineId": "aaad1e30-f92b-4ed7-aa31-006147c28925",
                "directParameters": {
                  "threshold": 0.8
                },
                "inputMapping": {
                  "prompt": "arg0"
                }
              }
            }
          ]
        }
      }
    }
    ```
    
    * feat: exposing gliner guardrail on inputs to llm
    
    * fix: enabling reactor functions to be thread safe by parsing from a parameter nounstore/genrowstruct instead of setting within the instance that operates as a singleton
    
    * feat: overloading method instead of requiring future implementations to pass in null
    
    * chore: removing debugging code
    
    * feat: changing json structure to allow more generic input guardrail
    
    * chore: code cleanup

--- src/prerna/util/Utility.java
+++ src/prerna/util/Utility.java
@@ -159,6 +159,7 @@ import prerna.date.SemossDate;
 import prerna.engine.api.IDatabaseEngine;
 import prerna.engine.api.IEngine;
 import prerna.engine.api.IFunctionEngine;
+import prerna.engine.api.IGuardrailReactorFunctionEngine;
 import prerna.engine.api.IHeadersDataRow;
 import prerna.engine.api.IModelEngine;
 import prerna.engine.api.IRawSelectWrapper;
@@ -2630,9 +2631,11 @@ public final class Utility {
 			return getVectorDatabase(engineId, pullIfNeeded);
 		} else if(IEngine.CATALOG_TYPE.FUNCTION == type) {
 			return getFunctionEngine(engineId, pullIfNeeded);
+		} else if(IEngine.CATALOG_TYPE.GUARDRAIL == type) {
+			return getGuardrailEngine(engineId, pullIfNeeded);
 		} else if(IEngine.CATALOG_TYPE.VENV == type) {
 			return getVenvEngine(engineId, pullIfNeeded);
-		}
+		} 
 		
 		throw new IllegalArgumentException("Unknown engine type with value " + type);
 	}
@@ -2906,6 +2909,28 @@ public final class Utility {
 		// get the pipeline
 		engine = EngineProxyFactory.createGuardedFunctionEngine((IFunctionEngine) engine);
 		return (IFunctionEngine) engine;
+	}
+	
+	/**
+	 * 
+	 * @param engineId
+	 * @return
+	 */
+	public static IGuardrailReactorFunctionEngine getGuardrailEngine(String engineId) {
+		return getGuardrailEngine(engineId, true);
+	}
+	
+	/**
+	 * 
+	 * @param engineId
+	 * @param pullIfNeeded
+	 * @return
+	 */
+	public static IGuardrailReactorFunctionEngine getGuardrailEngine(String engineId, boolean pullIfNeeded) {
+		IEngine engine = baseGetEngine(engineId, pullIfNeeded);
+		// get the pipeline
+		engine = EngineProxyFactory.createGuardedFunctionEngine((IFunctionEngine) engine);
+		return (IGuardrailReactorFunctionEngine) engine;
 	}
 	
 	/**
