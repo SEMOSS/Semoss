48c783d3857425a38c7f23f0e56617455719f815
rpaulraj <rpaulraj@deloitte.com>
2025-08-27 13:05:07 +0530

    feat: Added a new optional parameter for projectList as filter

--- src/prerna/reactor/security/GetProjectMetaValuesReactor.java
+++ src/prerna/reactor/security/GetProjectMetaValuesReactor.java
@@ -14,21 +14,33 @@ import prerna.sablecc2.om.nounmeta.NounMetadata;
 public class GetProjectMetaValuesReactor extends AbstractReactor {
     
     public GetProjectMetaValuesReactor() {
-        this.keysToGet = new String[] {ReactorKeysEnum.META_KEYS.getKey()};
+        this.keysToGet = new String[] {ReactorKeysEnum.META_KEYS.getKey(), "projectIdList"};
+        this.keyRequired = new int[] { 1, 0};
     }
     
     @Override
     public NounMetadata execute() {
-        List<String> projectIdList = SecurityProjectUtils.getUserProjectIdList(this.insight.getUser(), true, false, true);
-        if(projectIdList != null && projectIdList.isEmpty()) {
-        	return new NounMetadata(new ArrayList<>(), PixelDataType.CUSTOM_DATA_STRUCTURE);
-        }
+    	List<String> projectIdList = getProjectIdList();
+    	if(getProjectIdList()==null) {
+	        projectIdList = SecurityProjectUtils.getUserProjectIdList(this.insight.getUser(), true, false, true);
+	        if(projectIdList != null && projectIdList.isEmpty()) {
+	        	return new NounMetadata(new ArrayList<>(), PixelDataType.CUSTOM_DATA_STRUCTURE);
+	        }
+    	}
         List<Map<String, Object>> ret = SecurityProjectUtils.getAvailableMetaValues(projectIdList, getMetaKeys());
         return new NounMetadata(ret, PixelDataType.CUSTOM_DATA_STRUCTURE);
     }
     
     private List<String> getMetaKeys() {
         GenRowStruct grs = this.store.getNoun(this.keysToGet[0]);
+        if(grs != null && !grs.isEmpty()) {
+            return grs.getAllStrValues();
+        }
+        return this.curRow.getAllStrValues();
+    }
+    
+    private List<String> getProjectIdList() {
+        GenRowStruct grs = this.store.getNoun(this.keysToGet[1]);
         if(grs != null && !grs.isEmpty()) {
             return grs.getAllStrValues();
         }
