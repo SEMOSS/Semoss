66d71abb85007e5baabc74894795924b21a756f5
Ryan Weiler <ryanweiler92@gmail.com>
2025-08-27 19:29:46 -0400

    feat: properly pipe prefix when streaming (#1252)

--- py/genai_client/message_builders/openai/openai_message_builder.py
+++ py/genai_client/message_builders/openai/openai_message_builder.py
@@ -112,6 +112,7 @@ class OpenAIMessageBuilder:
                         param_map["tools"] = self.convert_mcp_to_openai_responses_tools(
                             param_map["tools"]
                         )
+                        param_map["stream"] = False
 
                     openai_messages, param_map = self._clean_param_map_for_responses(
                         openai_messages, param_map
@@ -129,6 +130,7 @@ class OpenAIMessageBuilder:
                                 param_map["tools"]
                             )
                         )
+                        param_map["stream"] = False
 
                     openai_messages, param_map = (
                         self._clean_param_map_for_chat_completions(
