a3aff5cda12a031bf3553dfb4c34b6c74f08e20d
Maher Khalil <themaherkhalil@gmail.com>
2025-09-07 14:36:12 -0400

    chore: moving methods to DIHelper directly instead of Utility

--- src/prerna/reactor/security/AdminFetchRDFMapPropViewReactor.java
+++ src/prerna/reactor/security/AdminFetchRDFMapPropViewReactor.java
@@ -1,7 +1,9 @@
 package prerna.reactor.security;
 
-import java.io.File;
 import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import prerna.auth.User;
 import prerna.auth.utils.SecurityAdminUtils;
@@ -9,35 +11,34 @@ import prerna.reactor.AbstractReactor;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
+import prerna.util.Constants;
 import prerna.util.DIHelper;
-import prerna.util.Utility;
 
 public class AdminFetchRDFMapPropViewReactor extends AbstractReactor {
-	
+
+	private static final Logger classLogger = LogManager.getLogger();
+
 	public AdminFetchRDFMapPropViewReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.PROJECT.getKey()};
+		this.keysToGet = new String[] { ReactorKeysEnum.PROJECT.getKey() };
 	}
-	
+
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
 		User user = this.insight.getUser();
 		if (!SecurityAdminUtils.userIsAdmin(user)) {
-			throw new IllegalArgumentException("User is not an admin and does not have access. Please login as an admin");
+			throw new IllegalArgumentException(
+					"User is not an admin and does not have access. Please login as an admin");
 		}
-		
-		String currentRDFMapFileLoc = DIHelper.getInstance().getRDFMapFileLocation();
-		File currentRDFPropFile = new File(currentRDFMapFileLoc);
-		
-		if(!currentRDFPropFile.exists() || !currentRDFPropFile.isFile()) {
-			throw new IllegalArgumentException("Could not find RDF Map Prop file. Please reach out to an administrator for assistance");
-		}
-	
+
 		StringBuilder currentRDFMapPropContent = null;
 		try {
-			currentRDFMapPropContent = Utility.concealRDFMapPropSensitiveInfo(currentRDFMapFileLoc);
+			currentRDFMapPropContent = DIHelper.getInstance().getConcealedRDFMapContents();
 		} catch (IOException e) {
-			e.printStackTrace();
+			classLogger.error(Constants.STACKTRACE, e);
+			throw new IllegalArgumentException(
+					String.format("Error occurred reading the RDF_Map.prop file. Detailed error = %s", e.getMessage()),
+					e);
 		}
 		return new NounMetadata(currentRDFMapPropContent.toString(), PixelDataType.CONST_STRING);
 	}
