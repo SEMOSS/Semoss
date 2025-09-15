4feb21473440403d0ceb8394003b7c41a224c31a
Tejas Lokeshrao <tlokeshrao@deloitte.com>
2025-09-15 13:56:49 -0400

    fix: merge

--- src/prerna/engine/impl/model/Room.java
+++ src/prerna/engine/impl/model/Room.java
@@ -9,7 +9,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
