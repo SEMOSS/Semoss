17e91258c0a80bba24a4607247c337c74f9f1d7f
Maher Khalil <themaherkhalil@gmail.com>
2025-09-20 13:23:53 -0400

    feat: if the proxy is ever invoked, we will always log even if no input/output reactors

--- src/prerna/reactor/agent/mcp/GetMCPToolsReactor.java
+++ src/prerna/reactor/agent/mcp/GetMCPToolsReactor.java
@@ -17,16 +17,16 @@ import prerna.util.Utility;
 public class GetMCPToolsReactor extends AbstractReactor {
 
 	private static final Logger classLogger = LogManager.getLogger(GetMCPToolsReactor.class);
-	
+
 	public GetMCPToolsReactor() {
-		this.keysToGet = new String[] {ReactorKeysEnum.PROJECT.getKey()};
-		this.keyRequired = new int[] {1};
+		this.keysToGet = new String[] { ReactorKeysEnum.PROJECT.getKey() };
+		this.keyRequired = new int[] { 1 };
 	}
-	
+
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
-		
+
 		User user = this.insight.getUser();
 		// check if user is logged in
 		if (AbstractSecurityUtils.anonymousUsersEnabled() && user.isAnonymous()) {
@@ -37,11 +37,11 @@ public class GetMCPToolsReactor extends AbstractReactor {
 		if (!SecurityProjectUtils.userCanViewProject(user, projectId)) {
 			throw new IllegalArgumentException("Project " + projectId + " does not exist or user does not have access");
 		}
-		
-		classLogger.info("Getting MCP Tools for project .. " + projectId);
-		
+
 		IProject project = Utility.getProject(projectId);
 		JSONObject toolMap = MCPUtility.getAggregatedTools(project);
+		classLogger.info("Getting MCP Tools for project {} {}", projectId, toolMap);
+
 		return new NounMetadata(toolMap, PixelDataType.JSON_OBJECT);
 	}
 
