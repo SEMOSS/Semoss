52b9d2e22f8fefa7a1f9869e9c2164b586880051
Maher Khalil <themaherkhalil@gmail.com>
2025-09-23 10:59:04 -0400

    feat: force compilation of log4j plugin, exposing additional regex pattern matches, moving values to mask into central location

--- src/prerna/logging/MaskingPatternConverter.java
+++ src/prerna/logging/MaskingPatternConverter.java
@@ -2,7 +2,6 @@ package prerna.logging;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,60 +12,67 @@ import org.apache.logging.log4j.core.pattern.ConverterKeys;
 import org.apache.logging.log4j.core.pattern.LogEventPatternConverter;
 import org.apache.logging.log4j.core.pattern.PatternConverter;
 
+import prerna.engine.impl.SmssUtilities;
 import prerna.util.Constants;
-@Plugin(name = "MaskMsg",category =PatternConverter.CATEGORY)
-@ConverterKeys({"maskMsg"})
-public class MaskingPatternConverter  extends LogEventPatternConverter{
+
+@Plugin(name = "MaskMsg", category = PatternConverter.CATEGORY)
+@ConverterKeys({ "maskMsg" })
+public class MaskingPatternConverter extends LogEventPatternConverter {
 
 	private final List<MaskRule> rules;
-	
-	public final static Map<String,String> maskPattern = Map.of(
-            "(?i)(password\s*=\s*)[^, ]+","$1****",
-            "(?i)(token\s*=\s*)[^, ]+","$1****",
-            "(?i)(Session\s*=\s*)[^, ]+","$1****",
-            "(?i)(ssn\s*=\s*)[0-9-]+","$1****"
-    );
-	
-	private static class MaskRule {
-		final Pattern regex;
-		final String replacement;
-		MaskRule(Pattern regex,String replacement){
-			this.regex =regex;
-			this.replacement =replacement;
-		}
-	}
-	
+
 	protected MaskingPatternConverter(List<MaskRule> rules) {
 		super("maskMsg", "maskMsg");
 		this.rules = rules;
 	}
-	
-	@PluginFactory
-	public static MaskingPatternConverter newInstance() {
-		List<MaskRule> rules = new ArrayList<MaskRule>();
-		
-		Map<String, String> pattern = maskPattern;
-		pattern.forEach((k, v) -> {
-			Pattern regex = Pattern.compile(k);
-			String replacement = v;
-			rules.add(new MaskRule(regex, replacement));
-		});
-		 
-		//String maskingKeys = "password,token,ssn,card,Session";//Utility.getDIHelperProperty(Settings.MASKING_KEYS);
-		//keys = Arrays.stream(maskingKeys.split(",")).map(String::trim).filter(s -> !s.isEmpty()).collect(Collectors.toList());
-		return new MaskingPatternConverter(rules);
-	}
 
 	@Override
 	public void format(LogEvent event, StringBuilder toAppendTo) {
-		
 		String message = event.getMessage().getFormattedMessage();
-		for(MaskRule rule : rules) {
+		for (MaskRule rule : rules) {
 			Matcher m = rule.regex.matcher(message);
 			message = m.replaceAll(rule.replacement);
-			//message = event.getMessage().getFormattedMessage().replaceAll("(?i)("+key+"=)[^, ]+", "$1****");
 		}
 		toAppendTo.append(message);
+	}
+
+	@PluginFactory
+	public static MaskingPatternConverter newInstance() {
+		List<MaskRule> rules = new ArrayList<MaskRule>();
+
+		SmssUtilities.SENSITIVE_KEYWORDS.forEach(key -> {
+			String escapedKey = Pattern.quote(key);
+
+			// Pattern 1: key=value format in pixel
+			String pattern1 = "(?i)(" + escapedKey + "\\s*=\\s*)[^, ]+";
+			Pattern regex1 = Pattern.compile(pattern1);
+			rules.add(new MaskRule(regex1, "$1" + Constants.SENSITIVE_INFO_MASK));
+
+			// Pattern 2: JSON-style "key":"value" format
+			String pattern2 = "(?i)(\"\\s*" + escapedKey + "\\s*\"\\s*:\\s*\")[^\"]*(\")";
+			Pattern regex2 = Pattern.compile(pattern2);
+			rules.add(new MaskRule(regex2, "$1" + Constants.SENSITIVE_INFO_MASK + "$2"));
+
+			// Pattern 3: JDBC URL format - example for password parameter in connection
+			String pattern3 = "(?i)([?&]" + escapedKey + "=)[^&\\s]+";
+			Pattern regex3 = Pattern.compile(pattern3);
+			rules.add(new MaskRule(regex3, "$1" + Constants.SENSITIVE_INFO_MASK));
+		});
+
+		return new MaskingPatternConverter(rules);
+	}
+
+	/*
+	 * Simple utility class for regex replacements
+	 */
+	private static class MaskRule {
+		final Pattern regex;
+		final String replacement;
+
+		MaskRule(Pattern regex, String replacement) {
+			this.regex = regex;
+			this.replacement = replacement;
+		}
 	}
 
 }
