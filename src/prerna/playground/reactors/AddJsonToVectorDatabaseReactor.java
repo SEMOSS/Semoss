4f6647416aa32fb478d5e60d3031e1b57e065cd3
Scott <seascott@deloitte.com>
2025-09-25 10:25:25 -0400

    feat: specification for json upload

--- src/prerna/playground/reactors/AddJsonToVectorDatabaseReactor.java
+++ src/prerna/playground/reactors/AddJsonToVectorDatabaseReactor.java
@@ -1,10 +1,48 @@
 package prerna.playground.reactors;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.ToNumberPolicy;
+
+import prerna.reactor.AbstractReactor;
+import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+
 /**
  * This is a temporary reactor. It's functionality can be
  * integrated elsewhere in a more cohesive manner.
  */
-public class AddJsonToVectorDatabaseReactor {
+public class AddJsonToVectorDatabaseReactor extends AbstractReactor{
+
+    private static final Gson GSON = new GsonBuilder()
+            .setObjectToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE)
+            .disableHtmlEscaping()
+            .create();
+
+    public AddJsonToVectorDatabaseReactor() {
+        this.keysToGet = new String[]{
+            "jsonFile",      // 0, required
+            ReactorKeysEnum.VECTORDB.getKey(),    // 1, optional (can be null)
+            ReactorKeysEnum.ROOM_ID.getKey(),     // 2, optional (not required, will use insight)
+            ReactorKeysEnum.COMMAND.getKey(),     // 3, required (actual user query)
+            ReactorKeysEnum.CONTEXT.getKey(),     // 4, tbd on how it is used
+            ReactorKeysEnum.IMAGE.getKey(),       // 5, optional, TODO: add in support
+            ReactorKeysEnum.URL.getKey(),         // 6, optional, TODO: add in support
+            ReactorKeysEnum.PARAM_VALUES_MAP.getKey() // 7, optional
+        };
+
+        this.keyRequired = new int[]{1, 0, 0, 1, 0, 0, 0, 0};
+    }
+	
+	
+	@Override
+	public NounMetadata execute() {
+		// TODO Auto-generated method stub
+		
+		
+		
+		return null;
+	}
 	/**
 	 * TODO:
 	 * This class will take in a file that is a json object
