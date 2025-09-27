cd3f56f079db9962c4897c46b3db8111194e860b
Maher Khalil <themaherkhalil@gmail.com>
2025-09-27 11:57:09 -0400

    feat: exposing new smss_stream_func in handle python to send back different operation type for streaming chunks from python models to tomcat. consolidating methods into MCPUtility for processing tools

--- src/prerna/sablecc2/comm/PixelJobManager.java
+++ src/prerna/sablecc2/comm/PixelJobManager.java
@@ -1,182 +1,209 @@
 package prerna.sablecc2.comm;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
-import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+
+import com.github.f4b6a3.uuid.alt.GUID;
 
 import prerna.om.Insight;
 import prerna.sablecc2.PixelRunner;
 
 public class PixelJobManager {
-	
-	static PixelJobManager manager = new PixelJobManager();
 
-	// obviously I assume the user wont run that many jobs to start with
-	// I will adjust this to a random number generator later
-	
-	// hashtable of job id to stdOut messages
-	private Map<String, List<String>> jobStdOut = new Hashtable<>();
-	
-	//hashtable of job id to offset
-	private Map<String, Integer> stdOutOffset = new Hashtable<>();
-	
-	// hashtable of job id to error messages
-	private Map<String, List<String>> jobError = new Hashtable<>();
+	private static PixelJobManager manager = new PixelJobManager();
 
-	//hashtable of job id to offset
-	private Map<String, Integer> errorOffset = new Hashtable<>();
-
-	// output offset - this will eventually be needed for distributed processing
-	private Map<String, Integer> outputOffset = new Hashtable<>();
-	
 	// keeps the job to thread
-	private Map<String, PixelJobThread> threadPool = new Hashtable<>();
-	
-	// hashtable of job id to stdOut messages
-	private Map<String, StringBuilder> jobPartialOut = new Hashtable<>();
-	
-	//hashtable of job id to offset
-	private Map<String, Integer> jobPartialOutOffset = new Hashtable<>();
-	
+	private Map<String, PixelJobThread> threadPool = new ConcurrentHashMap<>();
+
+	// Map of job id to stdOut messages
+	private Map<String, List<String>> jobStdOut = new ConcurrentHashMap<>();
+
+	// Map of job id to offset
+	private Map<String, Integer> stdOutOffset = new ConcurrentHashMap<>();
+
+	// Map of job id to error messages
+	private Map<String, List<String>> jobError = new ConcurrentHashMap<>();
+
+	// Map of job id to offset
+	private Map<String, Integer> errorOffset = new ConcurrentHashMap<>();
+
+	// Map of job id to streaming chunk map
+	private Map<String, List<Map<String, Object>>> jobStreamMap = new ConcurrentHashMap<>();
+
+	// Map of job id to streaming chunk map
+	private Map<String, Integer> jobStreamMapOffset = new ConcurrentHashMap<>();
+
+	/**
+	 * Map of job id to stdOut messages
+	 * 
+	 * @deprecated for jobStreamMapOffset for returning streaming data
+	 */
+	@Deprecated
+	private Map<String, StringBuilder> jobPartialOut = new ConcurrentHashMap<>();
+
+	/**
+	 * Map of job id to offset
+	 * 
+	 * @deprecated for jobStreamMapOffset for returning streaming data
+	 */
+	@Deprecated
+	private Map<String, Integer> jobPartialOutOffset = new ConcurrentHashMap<>();
+
 	private PixelJobManager() {
-		
+
 	}
-	
+
 	public static PixelJobManager getManager() {
-		if(manager != null) {
+		if (manager != null) {
 			return manager;
 		}
-		
-		synchronized(PixelJobManager.class) {
-			if(manager == null) {
+
+		synchronized (PixelJobManager.class) {
+			if (manager == null) {
 				manager = new PixelJobManager();
 			}
 		}
 		return manager;
 	}
-	
+
 	public PixelJobThread makeJob(Insight insight, String sessionId, String routeId) {
-		String jobId = UUID.randomUUID().toString();
+		String jobId = GUID.v7().toUUID().toString();
 		PixelJobThread jt = new PixelJobThread(jobId, insight, sessionId, routeId);
 		threadPool.put(jobId, jt);
 		return jt;
 	}
-	
+
 	public PixelJobThread makeJob(String jobId, Insight insight, String sessionId, String routeId) {
 		PixelJobThread jt = new PixelJobThread(jobId, insight, sessionId, routeId);
 		threadPool.put(jobId, jt);
 		return jt;
 	}
-	
+
 	public PixelJobThread removeJob(String jobId) {
 		return threadPool.remove(jobId);
 	}
-	
+
 	public PixelJobThread getJob(String jobId) {
 		return threadPool.get(jobId);
 	}
-	
-	public void addStdOut(String jobId, String stdOut)	{
-		List<String> outputList = new Vector<String>();
-		if(jobStdOut.containsKey(jobId)) {
-			outputList = jobStdOut.get(jobId);
-		} else {
-			jobStdOut.put(jobId, outputList);
+
+	public void addStdOut(String jobId, String stdOut) {
+		List<String> outputList = jobStdOut.get(jobId);
+		if (outputList == null) {
+			synchronized (jobStdOut) {
+				outputList = jobStdOut.get(jobId);
+				if (outputList == null) {
+					outputList = Collections.synchronizedList(new ArrayList<>());
+					jobStdOut.put(jobId, outputList);
+				}
+			}
 		}
-		synchronized(outputList) {
-			outputList.add(stdOut);
-		}
+		outputList.add(stdOut);
 	}
 
-	public void addStdErr(String jobId, String stdErr) {
-		List<String> outputList = new Vector<String>();
-		if(jobError.containsKey(jobId)) {
-			outputList = jobError.get(jobId);
-		} else {
-			jobError.put(jobId, outputList);
-		}
-		synchronized(outputList) {
-			outputList.add(stdErr);
-		}
-	}
-	
 	public List<String> getStdOut(String jobId) {
-		int curOffset = 0;
-		if(stdOutOffset.containsKey(jobId)) {
-			curOffset = stdOutOffset.get(jobId);
-		}
+		int curOffset = stdOutOffset.getOrDefault(jobId, 0);
 		return getStdOut(jobId, curOffset);
-	}
-
-	public List<String> getError(String jobId) {
-		int curOffset = 0;
-		if(errorOffset.containsKey(jobId)) {
-			curOffset = errorOffset.get(jobId);		
-		} 
-		return getError(jobId, curOffset);
 	}
 
 	public List<String> getStdOut(String jobId, int offset) {
 		List<String> outputList = jobStdOut.get(jobId);
-		if(outputList == null || outputList.isEmpty()) {
-			return new ArrayList<String>();
+		if (outputList == null || outputList.isEmpty()) {
+			return new ArrayList<>();
 		}
-		synchronized(outputList) {
+		synchronized (outputList) {
 			int size = outputList.size();
-			List<String> output = new Vector<String>(outputList.subList(offset, size));
-			int newOffset = offset+output.size();
+			List<String> output = new ArrayList<>(outputList.subList(offset, size));
+			int newOffset = offset + output.size();
 			// update the offset
 			stdOutOffset.put(jobId, newOffset);
 			return output;
 		}
 	}
-	
+
+	public void addStdErr(String jobId, String stdErr) {
+		List<String> outputList = jobError.get(jobId);
+		if (outputList == null) {
+			synchronized (jobError) {
+				outputList = jobError.get(jobId);
+				if (outputList == null) {
+					outputList = Collections.synchronizedList(new ArrayList<>());
+					jobError.put(jobId, outputList);
+				}
+			}
+		}
+		outputList.add(stdErr);
+	}
+
+	public List<String> getError(String jobId) {
+		int curOffset = errorOffset.getOrDefault(jobId, 0);
+		return getError(jobId, curOffset);
+	}
+
 	public List<String> getError(String jobId, int offset) {
 		List<String> outputList = jobError.get(jobId);
-		if(outputList == null || outputList.isEmpty()) {
-			return new ArrayList<String>();
+		if (outputList == null || outputList.isEmpty()) {
+			return new ArrayList<>();
 		}
-		synchronized(outputList) {
+		synchronized (outputList) {
 			int size = outputList.size();
-			List<String> output =  new Vector<String>(outputList.subList(offset, size));
-			int newOffset = offset+output.size();
+			List<String> output = new ArrayList<>(outputList.subList(offset, size));
+			int newOffset = offset + output.size();
 			// update the offset
 			errorOffset.put(jobId, newOffset);
 			return output;
 		}
 	}
-	
-	public void addPartialOut(String jobId, String stdOut)	{
-		StringBuilder builder = null;
-		if(jobPartialOut.containsKey(jobId)) {
-			builder = jobPartialOut.get(jobId);
-		} else {
-			builder = new StringBuilder();
-			jobPartialOut.put(jobId, builder);
+
+	/**
+	 * @deprecated switch to {@link #addStreamOut(String, Map)} instead.
+	 * @param jobId
+	 * @param stdOut
+	 */
+	@Deprecated
+	public void addPartialOut(String jobId, String stdOut) {
+		StringBuilder builder = jobPartialOut.get(jobId);
+		if (builder == null) {
+			synchronized (jobPartialOut) {
+				builder = jobPartialOut.get(jobId);
+				if (builder == null) {
+					builder = new StringBuilder();
+					jobPartialOut.put(jobId, builder);
+				}
+			}
 		}
-		synchronized(builder) {
+		synchronized (builder) {
 			builder.append(stdOut);
 		}
 	}
-	
+
+	/**
+	 * @deprecated switch to {@link #getStreamOut(String)} instead.
+	 * @param jobId
+	 * @param stdOut
+	 */
+	@Deprecated
 	public Map<String, String> getPartial(String jobId) {
-		int curOffset = 0;
-		if(jobPartialOutOffset.containsKey(jobId)) {
-			curOffset = jobPartialOutOffset.get(jobId);
-		}
+		int curOffset = jobPartialOutOffset.getOrDefault(jobId, 0);
 		return getPartial(jobId, curOffset);
 	}
-	
+
+	/**
+	 * @deprecated switch to {@link #getStreamOut(String, int)} instead.
+	 * @param jobId
+	 * @param stdOut
+	 */
+	@Deprecated
 	public Map<String, String> getPartial(String jobId, int offset) {
 		StringBuilder builder = jobPartialOut.get(jobId);
-		if(builder == null || builder.length() == 0) {
+		if (builder == null || builder.length() == 0) {
 			return new HashMap<>();
 		}
-		synchronized(builder) {
+		synchronized (builder) {
 			Map<String, String> retMap = new HashMap<>();
 			int size = builder.length();
 			retMap.put("total", builder.toString());
@@ -188,58 +215,87 @@ public class PixelJobManager {
 			return retMap;
 		}
 	}
-	
+
+	/**
+	 * 
+	 * @param jobId
+	 * @param stream
+	 */
+	public void addStreamOut(String jobId, Map<String, Object> stream) {
+		List<Map<String, Object>> outputList = jobStreamMap.get(jobId);
+		if (outputList == null) {
+			synchronized (jobStreamMap) {
+				outputList = jobStreamMap.get(jobId);
+				if (outputList == null) {
+					outputList = Collections.synchronizedList(new ArrayList<>());
+					jobStreamMap.put(jobId, outputList);
+				}
+			}
+		}
+		outputList.add(stream);
+	}
+
+	/**
+	 * 
+	 * @param jobId
+	 * @return
+	 */
+	public List<Map<String, Object>> getStreamOut(String jobId) {
+		int curOffset = jobStreamMapOffset.getOrDefault(jobId, 0);
+		return getStreamOut(jobId, curOffset);
+	}
+
+	/**
+	 * 
+	 * @param jobId
+	 * @param offset
+	 * @return
+	 */
+	public List<Map<String, Object>> getStreamOut(String jobId, int offset) {
+		List<Map<String, Object>> outputList = jobStreamMap.get(jobId);
+		if (outputList == null || outputList.isEmpty()) {
+			return new ArrayList<>();
+		}
+		synchronized (outputList) {
+			int size = outputList.size();
+			List<Map<String, Object>> output = new ArrayList<>(outputList.subList(offset, size));
+			int newOffset = offset + output.size();
+			// update the offset
+			jobStreamMapOffset.put(jobId, newOffset);
+			return output;
+		}
+	}
+
 	public String getStatus(String jobId) {
 		return threadPool.get(jobId).getStatus();
 	}
-	
-	public void resetJob(String jobId) // trimming operation
-	{
-		// this is when I want to remove everything until current offset and set offset to zero
-		int curOut = errorOffset.get(jobId);		
-		int curErr = stdOutOffset.get(jobId);
-		
-		List<String> errorList = jobError.get(jobId);
-		List<String> outputList = jobStdOut.get(jobId);
-		
-		// trim the error list
-		synchronized(errorList)	{
-			errorList =  new Vector<String>(errorList.subList(curErr, errorList.size() - 1));
-			jobError.put(jobId, errorList);
-			errorOffset.put(jobId, 0);
-		}
-		
-		// trim the outputlist
-		synchronized(outputList) {
-			outputList =  new Vector<String>(outputList.subList(curOut, outputList.size() - 1));
-			jobStdOut.put(jobId, outputList);
-			stdOutOffset.put(jobId, 0);
-		}
-	}
-	
+
 	public void clearJob(String jobId) {
 		jobError.remove(jobId);
 		stdOutOffset.remove(jobId);
 		errorOffset.remove(jobId);
 		jobStdOut.remove(jobId);
-		
+
 		// partial as well
 		jobPartialOut.remove(jobId);
 		jobPartialOutOffset.remove(jobId);
+
+		jobStreamMap.remove(jobId);
+		jobStreamMapOffset.remove(jobId);
 	}
-	
+
 	public void flagStatus(String jobId, PixelJobStatus status) {
 		threadPool.get(jobId).setStatus(status);
 	}
-	
+
 	public void interruptThread(String jobId) {
-		if(threadPool.get(jobId) != null) {
+		if (threadPool.get(jobId) != null) {
 			PixelJobThread pixelThread = threadPool.get(jobId);
 			pixelThread.interrupt();
 			pixelThread.setStatus(PixelJobStatus.CANCELED);
 		}
 	}
-	
+
 	public PixelRunner getOutput(String jobId) {
 		PixelJobThread jt = threadPool.get(jobId);
 		return jt.getRunner();
