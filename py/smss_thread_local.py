32b72a22eb68c25e90f1c55688f1bca3b32c01fa
Maher Khalil <themaherkhalil@gmail.com>
2025-09-28 13:07:42 -0400

    chore: adding commented out method to set smss_stream when running code outside of gaas_tcp_server_handler (#1371)

--- py/smss_thread_local.py
+++ py/smss_thread_local.py
@@ -1,18 +1,30 @@
 import threading
-from typing import Optional, Callable
+from typing import Optional, Callable, Any
 
 # This object is globally accessible, but its attributes are unique to each thread
 _storage = threading.local()
+
 
 def set_smss_stream(func: Callable) -> None:
     """Sets the smss_stream function for the current thread."""
     _storage.smss_stream = func
 
+
 def get_smss_stream() -> Optional[Callable]:
     """Gets the smss_stream function for the current thread, if it exists."""
-    return getattr(_storage, 'smss_stream', None)
+    return getattr(_storage, "smss_stream", None)
+
 
 def clear_smss_stream() -> None:
     """Clears the smss_stream function for the current thread."""
-    if hasattr(_storage, 'smss_stream'):
+    if hasattr(_storage, "smss_stream"):
         del _storage.smss_stream
+
+
+# Uncomment the below if debugging and need the smss_stream defined
+# def smss_stream_func(data: Any, stream_type: str = "content", interim: bool = True):
+#     structured_output = {"stream_type": stream_type, "data": data}
+#     print(structured_output)
+
+
+# set_smss_stream(smss_stream_func)
