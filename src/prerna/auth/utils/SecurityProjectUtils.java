5b3c88df41b9135a545718a18fef7f4f9683c6f7
Tejas Lokeshrao <tlokeshrao@deloitte.com>
2025-09-30 13:24:48 -0400

    fix: remove param from javadoc comment

--- src/prerna/auth/utils/SecurityProjectUtils.java
+++ src/prerna/auth/utils/SecurityProjectUtils.java
@@ -1824,7 +1824,6 @@ public class SecurityProjectUtils extends AbstractSecurityUtils {
 	/**
 	 * Remove dependency from project
 	 * 
-	 * @param user
 	 * @param projectId
 	 * @param dependentEngineId
 	 */
