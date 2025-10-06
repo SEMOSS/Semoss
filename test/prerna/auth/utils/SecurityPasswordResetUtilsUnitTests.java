e575a935e715e4e78b3d30cb5f510ef049f2119c
roxy <rocioram94@gmail.com>
2025-10-06 17:00:41 -0400

    test(secPasswordReset): adding coverage tests for error messages

--- test/prerna/auth/utils/SecurityPasswordResetUtilsUnitTests.java
+++ test/prerna/auth/utils/SecurityPasswordResetUtilsUnitTests.java
@@ -1,5 +1,6 @@
 package prerna.auth.utils;
 
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,7 +14,6 @@ import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Properties;
 
-import org.eclipse.egit.github.core.client.GsonUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -94,36 +94,75 @@ public class SecurityPasswordResetUtilsUnitTests {
 				modelMaxResponseTime);
 		
 		assertTrue(success, "Insertion of new user should be successful");
+		
 	}
 
 	@Test
 	void testUserEmailExists() throws Exception {
 		boolean exists = SecurityPasswordResetUtils.userEmailExists(email, type);
 		assertTrue(exists);
+
+		exists = SecurityPasswordResetUtils.userEmailExists("test234", type);
+		assertFalse(exists);
 	}
 
 	@Test
 	void testGetUserIdFromEmail() throws Exception {
 		String userId = SecurityPasswordResetUtils.getUserIdFromEmail(email, type);
 		assertEquals(id, userId);
+		
+		userId = SecurityPasswordResetUtils.getUserIdFromEmail("invalid", type);
+		assertEquals(null, userId);
 	}
 	
 	@Test
 	void testAllowUserResetPassword() throws Exception {
 		String token = SecurityPasswordResetUtils.allowUserResetPassword(email, type);
 		assertTrue(token != null && !token.isEmpty());
+		
+		try {
+			SecurityPasswordResetUtils.allowUserResetPassword("test2343", type);
+			fail("invalid email");
+		} catch (IllegalArgumentException e) {
+			assertEquals("The email 'test2343' does not exist for provider NATIVE", e.getMessage());
+		}
+		
+		try {
+			SecurityPasswordResetUtils.allowUserResetPassword("test2343", "GOOGLE");
+			fail("invalid auth type");
+		} catch (IllegalArgumentException e) {
+			assertEquals("Cannot reset password for type = 'GOOGLE'", e.getMessage());
+		}
 	}
 	
 	@Test
 	void testUserResetPassword() throws Exception {
 		String token = SecurityPasswordResetUtils.allowUserResetPassword(email, type);
-		String newPassword = "newPass123!";
+		String newPassword = "newPass123456!";
 		Map<String, Object> retMap = SecurityPasswordResetUtils.userResetPassword(token, newPassword);
-		System.out.println(GsonUtils.getGson().toJson(retMap));
 		assertFalse(retMap.isEmpty());
 		assertEquals(id, retMap.get("userId"));
 		assertEquals(email, retMap.get("email"));
-
+	}
+	
+	@Test
+	void testDeleteToken() throws Exception {
+		String token = SecurityPasswordResetUtils.allowUserResetPassword(email, type);
+		String newPassword = "newPass123!";
+		Map<String, Object> retMap = SecurityPasswordResetUtils.userResetPassword(token, newPassword);
+		assertFalse(retMap.isEmpty());
+		assertEquals(id, retMap.get("userId"));
+		assertEquals(email, retMap.get("email"));
+		
+		SecurityPasswordResetUtils.deleteToken(token);
+		
+		// make sure this is empty
+		try {
+			retMap = SecurityPasswordResetUtils.userResetPassword(token, newPassword);
+			fail("invalid");
+		} catch (Exception e) {
+			assertEquals("Invalid attempt trying to update password", e.getMessage());
+		}
 	}
 
 	@AfterAll
