fdb5929ca680d5a41935b902868dbe782a9401f3
Scott <seascott@deloitte.com>
2025-10-07 00:24:11 -0400

    feat: implement json to embeddings reactor

--- src/prerna/playground/reactors/AddJsonToVectorDatabaseReactor.java
+++ src/prerna/playground/reactors/AddJsonToVectorDatabaseReactor.java
@@ -1,12 +1,28 @@
 package prerna.playground.reactors;
 
+import java.io.File;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.FileNotFoundException;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.ToNumberPolicy;
+import com.google.gson.reflect.TypeToken;
 
+import prerna.engine.api.IModelEngine;
+import prerna.engine.api.IVectorDatabaseEngine;
+import prerna.engine.impl.model.responses.EmbeddingsModelEngineResponse;
+import prerna.playground.PlaygroundUtils;
 import prerna.reactor.AbstractReactor;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.ReactorKeysEnum;
+import prerna.sablecc2.om.execptions.SemossPixelException;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.util.AssetUtility;
 import prerna.util.Utility;
@@ -25,6 +41,7 @@ public class AddJsonToVectorDatabaseReactor extends AbstractReactor{
     public AddJsonToVectorDatabaseReactor() {
         this.keysToGet = new String[]{
             ReactorKeysEnum.VECTORDB.getKey(),
+            ReactorKeysEnum.MODEL.getKey(),
             ReactorKeysEnum.FILE_PATH.getKey(),
             ReactorKeysEnum.SPACE.getKey(), 
             "jsonFields",
@@ -38,37 +55,78 @@ public class AddJsonToVectorDatabaseReactor extends AbstractReactor{
 	@Override
 	public NounMetadata execute() {
 		
+		organizeKeys();
+		
 		String vectorDatabaseId = this.keyValue.get(ReactorKeysEnum.VECTORDB.getKey());
+		String modelId = this.keyValue.get(ReactorKeysEnum.MODEL.getKey());
 		String space = this.keyValue.get(ReactorKeysEnum.SPACE.getKey());
 		String rootFolder =  AssetUtility.getRootFolderPath(this.insight, space, false);
 		
-		String filePath = rootFolder + this.keyValue.get(ReactorKeysEnum.FILE_PATH.getKey());
+		String filePath = rootFolder + "/" + this.keyValue.get(ReactorKeysEnum.FILE_PATH.getKey());
 		filePath = Utility.normalizePath(filePath);
 
-		//We now have the file path. we need to access these files!
+		File jsonFile = new File(rootFolder, this.keyValue.get(ReactorKeysEnum.FILE_PATH.getKey()));
 		
-		//grab the json fields, need to access nounStore directly (store)
+		Reader reader = null;
+		try {
+		reader = new FileReader(jsonFile);
+		}
+		catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("json file " + ReactorKeysEnum.FILE_PATH.getKey() + " does not exist");
+		}
+		
+		List<Map<String, Object>> tools = GSON.fromJson(reader, new TypeToken<List<Map<String, Object>>>(){}.getType());
+		
+		List<String> strippedTools = new ArrayList<String>();
+		for(Map<String, Object> tool : tools) {
+			
+			StringBuilder sb = new StringBuilder();
+			
+			for(String field : this.store.getNoun("jsonFields").getAllStrValues()) {
+				sb.append(field + ": " + tool.get(field) + "\n");
+			}
+			strippedTools.add(sb.toString());
+		}
+
+		
+		IModelEngine model = Utility.getModel(modelId);
+		EmbeddingsModelEngineResponse response = model.embeddings(strippedTools, insight, null);
+		List<List<Double>> embeddingsList = response.getResponse();
+		
+
+		IVectorDatabaseEngine vectorDatabase = Utility.getVectorDatabase(vectorDatabaseId);
 		
 		
-		
-		//grab it
-		//pull out contents and all that
-		
-		
-		
-		return new NounMetadata(filePath, PixelDataType.CONST_STRING);
+		File parentDir = new File(rootFolder);
+		try {
+			File embeddingFile = File.createTempFile("embeddings" + "ADD_UUID", ".csv", parentDir);
+			
+			FileWriter writer = null;
+			writer = new FileWriter(embeddingFile);
+			
+			for(List<Double> vector : embeddingsList) {
+				StringBuilder sb = new StringBuilder();
+				
+				for(int i = 0; i < vector.size() - 1; i++) {
+					sb.append(vector.get(i));
+					if (i < vector.size()  - 1) sb.append(",");
+				}
+				
+			}
+			
+			try {
+				vectorDatabase.addEmbeddingFile(embeddingFile, insight, null);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new SemossPixelException(e.getMessage());
+			}
+			
+			
+			embeddingFile.delete();			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return new NounMetadata(true, PixelDataType.BOOLEAN);
 	}
-	/**
-	 * TODO:
-	 * This class will take in a file that is a json object
-	 * the fields or field to extract
-	 * and an vector db engine id
-	 * 
-	 * It will take each selected field and combine in some standardized manner
-	 * and add each to a row in a csv file we generate
-	 * 
-	 * this csv file + a couple other things will be used as input to
-	 * the vectorEngine method which will turn these rows into embeddings
-	 * will chunk each one
-	 */
 }
