649bec589a9aaddadd109dadd3487ee51263a5c3
Maher Khalil <themaherkhalil@gmail.com>
2025-10-08 10:21:43 -0400

    chore: adding deprecated method for some custom reactors (#1456)

--- src/prerna/sablecc2/om/NounStore.java
+++ src/prerna/sablecc2/om/NounStore.java
@@ -157,6 +157,18 @@ public class NounStore implements Serializable {
 	}
 
 	/**
+	 * Deprecated method to get the genrowstruct
+	 * 
+	 * @param nounName
+	 * @deprecated switch to {@link #getGenRowStruct(String)}
+	 * @return
+	 */
+	@Deprecated
+	public GenRowStruct getNoun(String nounName) {
+		return getGenRowStruct(nounName);
+	}
+
+	/**
 	 * Retrieves a specific occurrence of a noun's GenRowStruct based on its name
 	 * and number. For example, to get the first occurrence of noun "S", use
 	 * `getGenRowStruct("S", 1)`.
