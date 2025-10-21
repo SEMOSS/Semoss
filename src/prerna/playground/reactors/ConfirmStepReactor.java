734ae9f26ac965d0b6435cd64d4c465230f57334
Scott <seascott@deloitte.com>
2025-10-21 13:22:20 -0400

    feat: correct format of confirm step reactor

--- src/prerna/playground/reactors/ConfirmStepReactor.java
+++ src/prerna/playground/reactors/ConfirmStepReactor.java
@@ -27,7 +27,41 @@ import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.util.Utility;
+//TODO:
+/**
+ * Instead of feeding in tool response, and directly grabiing COT.
+ * Call the getMessageHistoryByMessageId thing. we need a message in the COT history
+ * ... hmm. for now grab the most recent child. This may not work if a user interrupts
+ * with a random question. In which case idk lol. will figure out. just make the assumption
+ * for now.
+ * 
+ * use this instead of current components. done.
+ * 
+ * Question: Should I turn off history? Not sure how history works more generally
+ * 
+ * Currently I grab the most recent message and use that to populate the message history.
 
+Should we pass the messageId directly instead?
+
+A bit confused, if useHistory is on (which it is by default). this behavior will
+already occur. Right?
+
+how do we stuff.
+
+
+
+
+
+
+
+
+How does history work?
+1. How do I pick whether or not this current message is added to the history
+2. How do I pick whether or not previous history is used as context for this message
+3. useHistory param?
+4. How is the parentMessageId determined if not manually set? Most recent message?
+
+ */
 public class ConfirmStepReactor extends AbstractReactor {
 
 	private static final Gson GSON = new GsonBuilder().setObjectToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE)
@@ -38,12 +72,12 @@ public class ConfirmStepReactor extends AbstractReactor {
 				ReactorKeysEnum.ENGINE.getKey(),      // 0, required
 	            ReactorKeysEnum.ROOM_ID.getKey(),     // 1, required (this + stepNumber necessary to grab plan/step)
 	            "stepNumber",						  //2, required
-	            "toolResponse",						  //3, required
+	            //"toolResponse",						  //3, required
 	            ReactorKeysEnum.PARAM_VALUES_MAP.getKey() // 4, optional
 
 		};
 		
-		this.keyRequired = new int[] {1, 0, 1, 1, 0};
+		this.keyRequired = new int[] {1, 0, 1, 0};
 	}
 
 	@Override
@@ -62,16 +96,20 @@ public class ConfirmStepReactor extends AbstractReactor {
         
         String roomId = this.keyValue.get(this.keysToGet[1]);
 		Room room = RoomUtils.getOrLoadRoom(roomId, insight);        
+		String lastMessageId = room.getMessages().getLast().getMessageId();
+		
+		String roomHistory = MessageUtils.getMessageHistoryFromMessageId(room.getMessages(), lastMessageId);
 
-		AbstractMessage abstractMessage = room.getMessages().get(1); //Second Message should be the COT
-		ResponseMessage message = null;
-		if (abstractMessage instanceof ResponseMessage) message = (ResponseMessage) abstractMessage;
 		
-		String plan = message.getContent();
+		//AbstractMessage abstractMessage = room.getMessages().get(1); //Second Message should be the COT
+		//ResponseMessage message = null;
+		//if (abstractMessage instanceof ResponseMessage) message = (ResponseMessage) abstractMessage;
+		
+		//String plan = message.getContent();
 
-		String userPrompt = String.format(PlaygroundUtils.CONFIRM_STEP_PROMPT_TEMPLATE, plan, this.keyValue.get("stepNumber"), this.keyValue.get("toolReseponse"));
+		String userPrompt = String.format(PlaygroundUtils.CONFIRM_STEP_PROMPT_TEMPLATE, this.keyValue.get("stepNumber"), roomHistory);
 		
-		
+		//check if this is sufficient
 		Map<String, Object> paramMap = getParamMap();
         if (paramMap == null) paramMap = new HashMap<>();
         
@@ -86,15 +124,17 @@ public class ConfirmStepReactor extends AbstractReactor {
                 .withModelType(modelEngine.getModelType())
                 .withParamMap(paramMap)
                 .build();
-            
-        ResponseMessage response = room.ask(inputMsg, PlaygroundUtils.CONFIRM_STEP_SYSTEM_PROMPT, modelEngine);
-        response.setParentMessageId(inputMsg.getParentMessageId()); //remove from message history
         
+        Boolean appendToHistory = false;
+        String parentMessageId = null;
+        ResponseMessage response = room.ask(inputMsg, PlaygroundUtils.CONFIRM_STEP_SYSTEM_PROMPT, modelEngine, parentMessageId, appendToHistory);        
         
         String jsonResponse = response.getContent();
         
         Map<String, Object> toolCall = PlaygroundUtils.jsonToMap(jsonResponse);
         return new NounMetadata(toolCall, PixelDataType.MAP);
+        
+        
         //TODO: determine if we return the response and description map, true/false,
         //or full inputMessage and responseMessages.
         
