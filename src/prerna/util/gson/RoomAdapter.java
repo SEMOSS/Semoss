526060217defa14ac53997377e5e524fa14df39a
Magudapathy <pmagudapathy@deloitte.com>
2025-10-28 22:44:00 +0530

    feat: changes made for audit logs.Modified gson serializer for converting the request object array to gson

---
+++ src/prerna/util/gson/RoomAdapter.java
@@ -0,0 +1,51 @@
+package prerna.util.gson;
+
+import java.io.IOException;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+import prerna.date.SemossDate;
+import prerna.engine.impl.model.Room;
+import prerna.logging.SemossLogUtils;
+
+public class RoomAdapter extends TypeAdapter<Room>{
+
+	@Override 
+	public Room read(JsonReader in) throws IOException {
+		if (in.peek() == JsonToken.NULL) {
+			in.nextNull();
+			return null;
+		}
+		Room room = new Room();
+		in.beginObject();
+		in.nextName();
+		// engine id, engine name, rdbms id
+		String roomId = in.nextString();
+		room.setId(roomId);
+		in.nextName();
+		String projectId = in.nextString();
+		room.getInsight().setContextProjectId(projectId);
+		in.nextName();
+		String projectName = in.nextString();
+		room.getInsight().setContextProjectName(projectName);
+		
+		return room;
+	}
+
+	@Override 
+	public void write(JsonWriter out, Room room) throws IOException {
+		if (room == null) {
+			out.nullValue();
+			return;
+		}
+		
+		out.beginObject();
+        out.name(SemossLogUtils.ROOM_ID).value(room.getId());
+        out.name(SemossLogUtils.PROJECT_ID).value(room.getInsight().getContextProjectId());
+        out.name(SemossLogUtils.PROJECT_NAME).value(room.getInsight().getContextProjectName());
+        out.endObject();
+	}
+}
---
+++ src/prerna/util/gson/ZoneOffsetTypeAdapter.java
@@ -0,0 +1,29 @@
+package prerna.util.gson;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.time.ZoneOffset;
+
+public class ZoneOffsetTypeAdapter extends TypeAdapter<ZoneOffset> {
+
+	@Override
+	public void write(JsonWriter out, ZoneOffset value) throws IOException {
+		if (value == null) {
+			out.nullValue();
+			return;
+		}
+		out.value(value.getId());
+	}
+
+	@Override
+	public ZoneOffset read(JsonReader in) throws IOException {
+		if (in.peek() == com.google.gson.stream.JsonToken.NULL) {
+			in.nextNull();
+			return null;
+		}
+		return ZoneOffset.of(in.nextString());
+	}
+
+}
