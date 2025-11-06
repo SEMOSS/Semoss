e618d68f274a78c6f31b1097e050529f86f2172a
Maher Khalil <themaherkhalil@gmail.com>
2025-11-06 11:30:39 -0500

    fix: proper syntax for date diff on sql server

--- src/prerna/util/sql/MicrosoftSqlServerQueryUtil.java
+++ src/prerna/util/sql/MicrosoftSqlServerQueryUtil.java
@@ -243,6 +243,11 @@ public class MicrosoftSqlServerQueryUtil extends AnsiSqlQueryUtil {
 	}
 
 	@Override
+	public String buildDateDiffFunctionSyntax(String timeUnit, String dateTimeField1, String dateTimeField2) {
+		return "DATEDIFF(" + timeUnit + "," + dateTimeField1 + "," + dateTimeField2 + ")";
+	}
+
+	@Override
 	public boolean allowsIfExistsTableSyntax() {
 		return false;
 	}
