9d37905cdf8d8c14746132b70f57b5698177d178
Maher Khalil <themaherkhalil@gmail.com>
2025-11-19 16:31:42 -0500

    fix: insight setcontext is responsible for the symlink instead of the reactor

--- src/prerna/om/Insight.java
+++ src/prerna/om/Insight.java
@@ -1493,6 +1493,11 @@ public class Insight implements Serializable {
 			this.getCmdUtil().setWorkingDir(appRootFolder);
 		}
 
+		// if we have a chroot, mount the project for that user.
+		if (Boolean.parseBoolean(Utility.getDIHelperProperty(Constants.CHROOT_ENABLE))) {
+			this.user.getUserSymlinkHelper().symlinkProject(this.user, projectId);
+		}
+
 		this.contextReinitialized = true;
 		return true;
 	}
