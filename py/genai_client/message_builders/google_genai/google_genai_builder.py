4533788d314d40666091f57943236ed325f7a3ca
Ryan Weiler <ryanweiler92@gmail.com>
2025-12-10 17:18:09 -0500

    feat: remove image modaility check

--- py/genai_client/message_builders/google_genai/google_genai_builder.py
+++ py/genai_client/message_builders/google_genai/google_genai_builder.py
@@ -231,17 +231,17 @@ class GoogleGenAIMessageBuilder:
             else ["TEXT"]
         )
 
-        if "IMAGE" in response_modalities:
-            image_config = types.ImageConfig(
-                aspect_ratio=kwargs.pop("image_aspect_ratio", None),
-                image_size=kwargs.pop("image_size", None),
-                output_mime_type=kwargs.pop("output_mime_type", None),
-                output_compression_quality=kwargs.pop(
-                    "output_compression_quality", None
-                ),
-            )
-        else:
-            image_config = None
+        # if "IMAGE" in response_modalities:
+        #     image_config = types.ImageConfig(
+        #         aspect_ratio=kwargs.pop("image_aspect_ratio", None),
+        #         image_size=kwargs.pop("image_size", None),
+        #         output_mime_type=kwargs.pop("output_mime_type", None),
+        #         output_compression_quality=kwargs.pop(
+        #             "output_compression_quality", None
+        #         ),
+        #     )
+        # else:
+        #     image_config = None
 
         config = types.GenerateContentConfig(
             http_options=kwargs.pop("http_options", None),
@@ -260,7 +260,7 @@ class GoogleGenAIMessageBuilder:
             tool_config=tool_config,
             thinking_config=thinking_config,
             response_modalities=response_modalities,
-            image_config=image_config,
+            # image_config=image_config,
         )
 
         return config, stream
