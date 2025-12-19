3fd39ecd15c155e5b90580b90a145aed2754176e
Kunal Patel <kunal0137@gmail.com>
2025-12-19 14:19:34 -0500

    fix: key requirements for add tool

--- src/prerna/reactor/model/AddToolExecutionReactor.java
+++ src/prerna/reactor/model/AddToolExecutionReactor.java
@@ -40,7 +40,7 @@ public class AddToolExecutionReactor extends AbstractReactor {
 			ReactorKeysEnum.MCP_TOOL_STATUS.getKey() // 7
         };
 		//TODO: once we remove the legacy tool_execution_response, we will make toolExecutionResponse mandatory field
-        this.keyRequired = new int[]{1, 1, 1, 0, 0, 0};
+        this.keyRequired = new int[]{1, 1, 1, 0, 0, 0, 0, 0};
     }
 
     @Override
