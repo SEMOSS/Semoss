c0a15a7ff8cca50a0487f1f99c43df3d096b5958
Lin <tlin2@deloitte.com>
2026-01-14 14:46:11 -0500

    feat: added model security check and update ask to askRoom

--- src/prerna/reactor/frame/ConvertFrameToVegaVizReactor.java
+++ src/prerna/reactor/frame/ConvertFrameToVegaVizReactor.java
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * Copyright 2015 Defense Health Agency (DHA)
+ *
+ * If your use of this software does not include any GPLv2 components:
+ * 	Licensed under the Apache License, Version 2.0 (the "License");
+ * 	you may not use this file except in compliance with the License.
+ * 	You may obtain a copy of the License at
+ *
+ * 	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * 	Unless required by applicable law or agreed to in writing, software
+ * 	distributed under the License is distributed on an "AS IS" BASIS,
+ * 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 	See the License for the specific language governing permissions and
+ * 	limitations under the License.
+ * ----------------------------------------------------------------------------
+ * If your use of this software includes any GPLv2 components:
+ * 	This program is free software; you can redistribute it and/or
+ * 	modify it under the terms of the GNU General Public License
+ * 	as published by the Free Software Foundation; either version 2
+ * 	of the License, or (at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *******************************************************************************/
 package prerna.reactor.frame;
 
 import org.apache.logging.log4j.Logger;
@@ -6,8 +33,8 @@ import prerna.algorithm.api.ITableDataFrame;
 import prerna.date.SemossDate;
 import prerna.engine.api.IModelEngine;
 import prerna.engine.impl.model.responses.AskModelEngineResponse;
+import prerna.engine.impl.model.Room;
 import prerna.reactor.frame.AbstractFrameReactor;
-import prerna.sablecc2.om.GenRowStruct;
 import prerna.sablecc2.om.PixelDataType;
 import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
@@ -15,6 +42,9 @@ import prerna.sablecc2.om.VarStore;
 import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.sablecc2.om.execptions.SemossPixelException;
 import prerna.util.Utility;
+
+import prerna.auth.utils.AbstractSecurityUtils;
+import prerna.auth.utils.SecurityProjectUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -187,13 +217,29 @@ public class ConvertFrameToVegaVizReactor extends AbstractFrameReactor {
 	@SuppressWarnings("unchecked")
 	private String callLLM(String context, String question) {
 		String modelId = (String) this.keyValue.get(ReactorKeysEnum.MODEL.getKey());
+        if (modelId == null || modelId.trim().isEmpty()) {
+            throw new SemossPixelException("Model id is required");
+        }
+
+        if (!AbstractSecurityUtils.ignoreDatabase(modelId) && !AbstractSecurityUtils.containsEngineId(modelId)) {
+            throw new SemossPixelException("Model not registered in security DB: " + modelId);
+        }
+        if (AbstractSecurityUtils.adminOnlyEngineAddAccess(modelId)) {
+            throw new SemossPixelException("Insufficient permissions to use model: " + modelId);
+        }
+
+		//////// TEMPORARY ROOM SETUP ////////
+		Room room = new Room();
+		room.setId(this.keyValue.get(PixelDataType.FRAME.getKey()).toString()); // Use frame id for the room id
+		room.setInsight(this.insight);
+		// room.setModelId((String) this.keyValue.get(ReactorKeysEnum.MODEL.getKey())); // try this?
 
 		HashMap<String, Object> paramMap = new HashMap<String, Object>();
 		paramMap.put("use_history", USE_HISTORY);
 		paramMap.put("temperature", TEMPERATURE);
 
 		IModelEngine modelEngine = Utility.getModel(modelId);
-		AskModelEngineResponse<?> modelResponse = modelEngine.ask(question, context, this.insight, paramMap);
+		AskModelEngineResponse<?> modelResponse = modelEngine.askRoom(question, room, null, paramMap);
 		String response = null;
 
 		if (modelResponse != null) {
