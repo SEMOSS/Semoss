45e725465eec02acf4ed74e9f1775ef48fce9a52
Rithvik Doshi <doshirithvik@gmail.com>
2026-01-15 10:42:11 -0500

    revert: revert since removing

--- src/prerna/engine/impl/model/inferencetracking/ModelInferenceLogsUtils.java
+++ src/prerna/engine/impl/model/inferencetracking/ModelInferenceLogsUtils.java
@@ -1,3 +1,30 @@
+/*******************************************************************************
+ * Copyright 2015 Defense Health Agency (DHA)
+ *
+ * If your use of this software does not include any GPLv2 components:
+ * 	Licensed under the Apache License, Version 2.0 (the "License");
+ * 	you may not use this file except in compliance with the License.
+ * 	You may obtain a copy of the License at
+ *
+ * 	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * 	Unless required by applicable law or agreed to in writing, software
+ * 	distributed under the License is distributed on an "AS IS" BASIS,
+ * 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 	See the License for the specific language governing permissions and
+ * 	limitations under the License.
+ * ----------------------------------------------------------------------------
+ * If your use of this software includes any GPLv2 components:
+ * 	This program is free software; you can redistribute it and/or
+ * 	modify it under the terms of the GNU General Public License
+ * 	as published by the Free Software Foundation; either version 2
+ * 	of the License, or (at your option) any later version.
+ *
+ * 	This program is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ *******************************************************************************/
 package prerna.engine.impl.model.inferencetracking;
 
 import java.io.IOException;
@@ -1963,16 +1990,13 @@ public class ModelInferenceLogsUtils {
 			stmt.setString(2, user_id);
 			resultSet = stmt.executeQuery();
 			if (resultSet.next()) {
-				String agentId = resultSet.getString("AGENT_ID");
-				String modelId = resultSet.getString("MODEL_ID");
-				modelId = modelId != null ? modelId : agentId;
 				return new Room(resultSet.getString("ROOM_ID"), resultSet.getString("USER_ID"),
 						resultSet.getString("ROOM_NAME"), resultSet.getString("ROOM_CONTEXT"),
 						resultSet.getString("PROJECT_ID"), resultSet.getString("SHARE_ID"),
 						resultSet.getBoolean("IS_ACTIVE"), resultSet.getTimestamp("DATE_CREATED"),
 						resultSet.getTimestamp("UPDATED_AT"), resultSet.getString("MESSAGES"),
 						resultSet.getBoolean("PINNED"), resultSet.getString("OPTIONS"),
-						modelId);
+						resultSet.getString("MODEL_ID"));
 			} else {
 				return null;
 			}
