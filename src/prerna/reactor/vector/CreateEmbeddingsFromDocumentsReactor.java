5733d54afe4c1817f604558bd49ee2e550b28765
Rithvik Doshi <doshirithvik@gmail.com>
2026-01-20 16:10:02 -0500

    chore: improve reactor description

--- src/prerna/reactor/vector/CreateEmbeddingsFromDocumentsReactor.java
+++ src/prerna/reactor/vector/CreateEmbeddingsFromDocumentsReactor.java
@@ -336,6 +336,8 @@ public class CreateEmbeddingsFromDocumentsReactor extends AbstractReactor {
 				}
 			}
 			return finalDescription.toString();
+		} else if (key.equals(ReactorKeysEnum.SPACE.getKey())) {
+			return "The id to the project space to get files from. This is only needed when we're not calling from a room/we specify the files to come from some custom project, otherwise should be left blank";
 		}
 
 		return super.getDescriptionForKey(key);
