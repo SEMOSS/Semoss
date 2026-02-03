a113f7eded009c75b44421ec3edbf8c0afbc135e
Maher Khalil <themaherkhalil@gmail.com>
2026-02-02 21:53:06 -0500

    chore: removing duplicate code between py and projectpy reactors

--- src/prerna/reactor/project/ProjectPyReactor.java
+++ src/prerna/reactor/project/ProjectPyReactor.java
@@ -27,60 +27,20 @@
  *******************************************************************************/
 package prerna.reactor.project;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import prerna.auth.utils.SecurityProjectUtils;
-import prerna.ds.py.PyTranslator;
-import prerna.project.api.IProject;
-import prerna.reactor.AbstractReactor;
-import prerna.sablecc2.om.PixelDataType;
-import prerna.sablecc2.om.PixelOperationType;
 import prerna.sablecc2.om.ReactorKeysEnum;
-import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.util.Utility;
 
-public class ProjectPyReactor extends AbstractReactor {
-
-	public ProjectPyReactor() {
-		this.keysToGet = new String[] { ReactorKeysEnum.CODE.getKey(), ReactorKeysEnum.PROJECT.getKey() };
-	}
-
-	@Override
-	public NounMetadata execute() {
-		organizeKeys();
-		String projectId = this.keyValue.get(ReactorKeysEnum.PROJECT.getKey());
-		if (projectId == null || (projectId = projectId.trim()).isEmpty()) {
-			projectId = this.insight.getContextProjectId();
-			if (projectId == null || (projectId = projectId.trim()).isEmpty()) {
-				projectId = this.insight.getProjectId();
-			}
-		}
-		if (projectId == null || (projectId = projectId.trim()).isEmpty()) {
-			throw new IllegalArgumentException("Must input an project id");
-		}
-
-		String code = Utility.decodeURIComponent(this.keyValue.get(ReactorKeysEnum.CODE.getKey()));
-
-		// make sure valid id for user
-		projectId = SecurityProjectUtils.testUserProjectIdForAlias(this.insight.getUser(), projectId);
-		if (!SecurityProjectUtils.userCanViewProject(this.insight.getUser(), projectId)) {
-			// you don't have access
-			throw new IllegalArgumentException("Project does not exist or user does not have access to the project");
-		}
-
-		IProject project = Utility.getProject(projectId);
-		PyTranslator projectPyTranslator = project.getProjectPyTranslator();
-		Object output = projectPyTranslator.runScript(code);
-
-		List<NounMetadata> outputs = new ArrayList<>(1);
-		outputs.add(new NounMetadata(output + "", PixelDataType.CONST_STRING));
-		return new NounMetadata(outputs, PixelDataType.CODE, PixelOperationType.CODE_EXECUTION);
-	}
+/**
+ * Executes Python code within a project's dedicated Python process. This
+ * reactor takes a project ID and a string of Python code. It runs the code
+ * within the context of the specified project. The code to be executed is
+ * expected to be wrapped in <encode> </encode> blocks.
+ */
+public class ProjectPyReactor extends AbstractProjectPyReactor {
 
 	@Override
-	public String getReactorDescription() {
-		return "Run Python code in the project's dedicated python process";
+	protected String getDecodedCode() {
+		return Utility.decodeURIComponent(this.keyValue.get(ReactorKeysEnum.CODE.getKey()));
 	}
 
 	@Override
