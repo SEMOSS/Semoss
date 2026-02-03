2e24bee33e9c2b8f7f796946a0dfd99a9cc6863a
Maher Khalil <themaherkhalil@gmail.com>
2026-02-02 21:35:29 -0500

    feat: exposing new reactors to save assuming content is provided as base64 utf-8 string

--- src/prerna/reactor/project/fs/SaveAppAssetsReactor.java
+++ src/prerna/reactor/project/fs/SaveAppAssetsReactor.java
@@ -27,25 +27,12 @@
  *******************************************************************************/
 package prerna.reactor.project.fs;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import prerna.auth.AccessToken;
-import prerna.auth.User;
-import prerna.auth.utils.AbstractSecurityUtils;
-import prerna.auth.utils.SecurityProjectUtils;
-import prerna.cluster.util.ClusterUtil;
-import prerna.project.api.IProject;
-import prerna.reactor.AbstractReactor;
 import prerna.sablecc2.om.ReactorKeysEnum;
-import prerna.sablecc2.om.nounmeta.NounMetadata;
-import prerna.util.AssetUtility;
-import prerna.util.Constants;
 import prerna.util.FileSystemUtil;
-import prerna.util.Utility;
-import prerna.util.git.GitRepoUtils;
 
-public class SaveAppAssetsReactor extends AbstractReactor {
+public class SaveAppAssetsReactor extends AbstractSaveAppAssetsReactor {
 
 	public SaveAppAssetsReactor() {
 		this.keysToGet = new String[] { ReactorKeysEnum.PROJECT.getKey(), ReactorKeysEnum.FILE_PATH.getKey(),
@@ -54,77 +41,13 @@ public class SaveAppAssetsReactor extends AbstractReactor {
 	}
 
 	@Override
-	public NounMetadata execute() {
-		organizeKeys();
-
-		User user = this.insight.getUser();
-		// check if user is logged in
-		if (AbstractSecurityUtils.anonymousUsersEnabled() && user.isAnonymous()) {
-			throwAnonymousUserError();
-		}
-
-		String projectId = this.keyValue.get(this.keysToGet[0]);
-		if (!SecurityProjectUtils.userCanEditProject(user, projectId)) {
-			throw new IllegalArgumentException(
-					"Project " + projectId + " does not exist or user does not have access to edit assets.");
-		}
-		IProject project = Utility.getProject(projectId);
-
-		// Retrieve all file paths and contents
-		List<String> filePaths = getNounAsStringList(this.keysToGet[1]);
-		List<String> contents = getNounAsStringList(this.keysToGet[2]);
-
-		if (filePaths == null || filePaths.isEmpty() || contents == null || contents.isEmpty()) {
-			throw new IllegalArgumentException("Must pass in at least one file name and content to save");
-		}
-		if (filePaths.size() != contents.size()) {
-			throw new IllegalArgumentException("Number of file names and contents must match");
-		}
-
-		String versionGitFolder = AssetUtility.getProjectVersionFolder(project.getProjectName(),
-				project.getProjectId());
-		String assetFolder = AssetUtility.getProjectAssetsFolder(project.getProjectName(), project.getProjectId());
-		String comment = this.keyValue.get(this.keysToGet[3]);
-		if (comment == null) {
-			comment = "add: SaveAppAssets executed";
-		}
-		// Check strict script source settings once
-		boolean strictScriptSource = Boolean.parseBoolean(Utility.getDIHelperProperty(Constants.STRICT_SCRIPT_SOURCE));
-
-		FileSystemUtil.validateAssetFiles(filePaths, strictScriptSource);
+	protected void saveAssetFiles(String assetFolder, List<String> filePaths, List<String> contents) {
 		FileSystemUtil.saveAssetFiles(assetFolder, filePaths, contents);
-
-		// add file to git
-		List<String> gitRelativeFilePaths = new ArrayList<>();
-		for (int i = 0; i < filePaths.size(); i++) {
-			String rawFileName = filePaths.get(i).trim();
-			String fileName = Utility.normalizePath(rawFileName);
-			if (fileName == null || fileName.isEmpty()) {
-				continue;
-			}
-
-			// for git, we need to add the assets folder which is assumed in the path
-			gitRelativeFilePaths.add(Constants.ASSETS_FOLDER + DIR_SEPARATOR + fileName);
-		}
-
-		// Get the user's email
-		AccessToken accessToken = user.getAccessToken(user.getPrimaryLogin());
-		String email = accessToken.getEmail();
-		String author = accessToken.getUsername();
-
-		GitRepoUtils.addSpecificFiles(versionGitFolder, gitRelativeFilePaths);
-		// commit it
-		GitRepoUtils.commitAddedFiles(versionGitFolder, comment, author, email);
-		// handle synchronization to the cloud
-		ClusterUtil.pushProjectFolder(project, assetFolder);
-
-		NounMetadata retNoun = NounMetadata.getSuccessNounMessage("Success!");
-		return retNoun;
 	}
 
 	@Override
 	public String getReactorDescription() {
-		return "Save a single or multiple files in the projects assets folder";
+		return "Save a single or multiple files in the projects assets folder. Content is provided within <encode></encode> blocks.";
 	}
 
 	@Override
@@ -134,7 +57,7 @@ public class SaveAppAssetsReactor extends AbstractReactor {
 		} else if (key.equals(ReactorKeysEnum.FILE_PATH.getKey())) {
 			return "Names of the file(s) to save. This relative path should assume the prefix of '/version/assets/' and not include the prefix in the string value.";
 		} else if (key.equals(ReactorKeysEnum.CONTENT.getKey())) {
-			return "Contents of the file(s) to save";
+			return "Contents of the file(s) to save. Content is provided within <encode></encode> blocks.";
 		} else if (key.equals(ReactorKeysEnum.COMMENT_KEY.getKey())) {
 			return "Comment to add while saving the files within the git repository for the project";
 		}
