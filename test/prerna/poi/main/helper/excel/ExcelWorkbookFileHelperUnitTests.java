e53e6d749331d28a394c9085c3f8e81593900ae1
jachetheboss <77856102+jachetheboss@users.noreply.github.com>
2026-03-20 15:38:28 -0400

    Increasing test coverage for ExcelWorkbookFileHelperUnitTests.java

--- test/prerna/poi/main/helper/excel/ExcelWorkbookFileHelperUnitTests.java
+++ test/prerna/poi/main/helper/excel/ExcelWorkbookFileHelperUnitTests.java
@@ -32,10 +32,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.mockito.MockedStatic;
 
 import java.io.ByteArrayInputStream;
