8e3756e5ff67963b899850b166b26bcb7d344723
Maher Khalil <themaherkhalil@gmail.com>
2026-03-28 21:02:03 -0400

    feat: adding admin version for project smss, adding in secret store logic

--- src/prerna/reactor/security/GetProjectSMSSReactor.java
+++ src/prerna/reactor/security/GetProjectSMSSReactor.java
@@ -31,11 +31,14 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
 
 import prerna.auth.User;
 import prerna.auth.utils.SecurityAdminUtils;
 import prerna.auth.utils.SecurityProjectUtils;
 import prerna.engine.impl.SmssUtilities;
+import prerna.io.connector.secrets.ISecrets;
+import prerna.io.connector.secrets.SecretsFactory;
 import prerna.project.api.IProject;
 import prerna.reactor.AbstractReactor;
 import prerna.sablecc2.om.PixelDataType;
@@ -44,39 +47,55 @@ import prerna.sablecc2.om.nounmeta.NounMetadata;
 import prerna.util.Utility;
 
 public class GetProjectSMSSReactor extends AbstractReactor {
-	
+
 	public GetProjectSMSSReactor() {
-		this.keysToGet = new String[]{ReactorKeysEnum.PROJECT.getKey()};
+		this.keysToGet = new String[] { ReactorKeysEnum.PROJECT.getKey() };
 	}
-	
+
 	@Override
 	public NounMetadata execute() {
 		organizeKeys();
 		String projectId = this.keyValue.get(this.keysToGet[0]);
 		User user = this.insight.getUser();
 		boolean isAdmin = SecurityAdminUtils.userIsAdmin(user);
-		if(!isAdmin) {
+		if (!isAdmin) {
 			boolean isOwner = SecurityProjectUtils.userIsOwner(user, projectId);
-			if(!isOwner) {
-				throw new IllegalArgumentException("Project " + projectId + " does not exist or user does not have permissions to update the smss of the project. User must be the owner to perform this function.");
+			if (!isOwner) {
+				throw new IllegalArgumentException("Project " + projectId
+						+ " does not exist or user does not have permissions to update the smss of the project. User must be the owner to perform this function.");
 			}
 		}
-				
+
 		IProject project = Utility.getProject(projectId);
 		String currentSmssFileLocation = project.getSmssFilePath();
 		File currentSmssFile = new File(currentSmssFileLocation);
-		
-		if(!currentSmssFile.exists() || !currentSmssFile.isFile()) {
-			throw new IllegalArgumentException("Could not find smss file for project " + projectId + ". Please reach out to an administrator for assistance");
+
+		if (!currentSmssFile.exists() || !currentSmssFile.isFile()) {
+			throw new IllegalArgumentException("Could not find smss file for project " + projectId
+					+ ". Please reach out to an administrator for assistance");
 		}
-		
+
 		String currentSmssContent = null;
 		try {
 			currentSmssContent = new String(Files.readAllBytes(Paths.get(currentSmssFile.toURI())));
 		} catch (IOException e) {
-			throw new IllegalArgumentException("An error occurred reading the current project smss details. Detailed message = " + e.getMessage());
+			throw new IllegalArgumentException(
+					"An error occurred reading the current project smss details. Detailed message = " + e.getMessage());
 		}
-		
+
+		ISecrets secretStore = SecretsFactory.getSecretConnector();
+		if (secretStore != null) {
+			Map<String, Object> engineSecrets = secretStore.getEngineSecrets(project.getCatalogType(),
+					project.getEngineId(), project.getEngineName());
+			if (engineSecrets != null && !engineSecrets.isEmpty()) {
+				currentSmssContent += "#Comments below this section will be lost\n#These values are stored in a secret store\n";
+
+				for (String key : engineSecrets.keySet()) {
+					currentSmssContent += "\n" + key + "\t" + engineSecrets.get(key);
+				}
+			}
+		}
+
 		String concealedSmssContent = SmssUtilities.concealSmssSensitiveInfo(currentSmssContent);
 		return new NounMetadata(concealedSmssContent, PixelDataType.CONST_STRING);
 	}
