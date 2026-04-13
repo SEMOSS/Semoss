ae6bb4b3c404a1dcb86b242fad7f04819d20a392
ericgonzal8 <ericgonzalez8@deloitte.com>
2026-04-13 12:16:48 -0400

    fix: Updated DB type when cleaning up DB after test class completes run.

--- test/prerna/auth/utils/AbstractSecurityUtilsUnitTestsSetup.java
+++ test/prerna/auth/utils/AbstractSecurityUtilsUnitTestsSetup.java
@@ -44,6 +44,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import prerna.SemossUnitTest;
+import prerna.engine.api.IDatabaseEngine;
+import prerna.engine.api.IRDBMSEngine;
 import prerna.engine.impl.rdbms.RDBMSNativeEngine;
 import prerna.util.Constants;
 import prerna.util.DIHelper;
@@ -103,7 +105,7 @@ public class AbstractSecurityUtilsUnitTestsSetup extends SemossUnitTest {
 
 	@AfterAll
 	public static void tearDown() throws IOException, SQLException {
-		RDBMSNativeEngine securityDb = (RDBMSNativeEngine) SystemEngineRegistry.getSecurityDb();
+		IRDBMSEngine securityDb = (IRDBMSEngine) SystemEngineRegistry.getSecurityDb();
 		assertTrue(securityDb.getOwlFilePath().contains("junit"));
 		try (Connection c = securityDb.getConnection(); Statement s = c.createStatement()) {
 			assertTrue(c.getMetaData().getURL().contains("junit"));
