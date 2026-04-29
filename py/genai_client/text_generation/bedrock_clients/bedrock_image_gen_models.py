cdd00c5019060f894a17fa603ace12daeb22db44
Ryan Weiler <ryanweiler92@gmail.com>
2026-04-29 14:54:17 +0000

    Revert "feat: room options and aliases for image models (#2452)" (#2469)
    This reverts commit f7fe75752aebce5af3ce362788f4e25d1c75799e.

--- py/genai_client/text_generation/bedrock_clients/bedrock_image_gen_models.py
+++ py/genai_client/text_generation/bedrock_clients/bedrock_image_gen_models.py
@@ -1,6 +1,6 @@
 from typing import List, Optional, Dict, Any, Tuple
 from enum import Enum
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 
 class BedrockImageGenTaskType(str, Enum):
@@ -22,8 +22,8 @@ class ImageGenerationConfig(BaseModel):
     Common image generation parameters for Bedrock models.
     See https://docs.aws.amazon.com/nova/latest/userguide/image-gen-req-resp-structure.html for full usage
     """
-    width: Optional[int] = Field(default=None, alias="imageWidth")
-    height: Optional[int] = Field(default=None, alias="imageHeight")
+    width: Optional[int] = None
+    height: Optional[int] = None
     quality: Optional[str] = None
     cfgScale: Optional[float] = None
     seed: Optional[int] = None
