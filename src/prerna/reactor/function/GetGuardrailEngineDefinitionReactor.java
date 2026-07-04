20c9bf0d560f81e472baa9b53b3a6492d96ce54d
Maher Khalil <themaherkhalil@gmail.com>
2026-07-03 21:56:11 -0400

    fix: correcting typo in name (#2698)

--- src/prerna/reactor/function/GetGuardrailEngineDefinitionReactor.java
+++ src/prerna/reactor/function/GetGuardrailEngineDefinitionReactor.java
@@ -27,14 +27,13 @@
  *******************************************************************************/
 package prerna.reactor.function;
 
-public class GetGuardrailEngineDefintionReactor extends GetFunctionEngineDefintionReactor {
+public class GetGuardrailEngineDefinitionReactor extends GetFunctionEngineDefinitionReactor {
 
 	/*
-	 * Just a convenience method
-	 * Works the same as the function engine
-	 * Since guardrail engine is a function engine as well
+	 * Just a convenience method Works the same as the function engine Since
+	 * guardrail engine is a function engine as well
 	 */
-	
+
 	@Override
 	String getUnableToAccessError(String engineId) {
 		return "Guardrail Engine " + engineId + " does not exist or user does not have access to this guardrail";
