1ddc1bd2be2fb7dc53292b7f32a474b49b462c34
Ryan Weiler <ryanweiler92@gmail.com>
2026-07-07 17:57:50 -0400

    fixing anthropic bedrock import (#2712)

--- py/genai_client/text_generation/anthropic_client/anthropic_text_client.py
+++ py/genai_client/text_generation/anthropic_client/anthropic_text_client.py
@@ -94,9 +94,9 @@ class AnthropicTextClient(AbstractTextGenerationClient):
             )
             return GoogleClient(config=self.client_config).client
         elif self.provider == "bedrock":
-            from anthropic import AnthropicAWS
+            from anthropic.lib.bedrock import AnthropicBedrock
 
-            return AnthropicAWS(
+            return AnthropicBedrock(
                 aws_region=kwargs.pop("aws_region", None),
                 aws_access_key=kwargs.pop("aws_access_key", None),
                 aws_secret_key=kwargs.pop("aws_secret_key", None),
