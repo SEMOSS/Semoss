f4c2ffa566eee229f621139825fda5edc3b18ae9
Maher Khalil <themaherkhalil@gmail.com>
2026-07-20 13:54:52 -0400

    chore: cleanup wrappers logs, use virtual thread for query tracking, remove unused user asset classes, remove unused utility methods (#2774)

--- src/prerna/rdf/engine/wrappers/RawRDF4JSelectWrapper.java
+++ src/prerna/rdf/engine/wrappers/RawRDF4JSelectWrapper.java
@@ -57,15 +57,14 @@ import prerna.engine.api.IRawSelectWrapper;
 import prerna.om.HeadersDataRow;
 import prerna.om.ThreadStore;
 import prerna.usertracking.UserQueryTrackingThread;
-import prerna.util.Constants;
 import prerna.util.Utility;
 
 public class RawRDF4JSelectWrapper extends AbstractWrapper implements IRawSelectWrapper {
 
 	private static final Logger classLogger = LogManager.getLogger(RawRDF4JSelectWrapper.class.getName());
-	
+
 	private final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'");
-	
+
 	private TupleQueryResult tqr = null;
 
 	@Override
@@ -84,7 +83,7 @@ public class RawRDF4JSelectWrapper extends AbstractWrapper implements IRawSelect
 				tqr.close();
 			}
 		} catch (QueryEvaluationException e) {
-			classLogger.error(Constants.STACKTRACE, e);
+			classLogger.error("Error checking for next result in the query result set", e);
 		}
 		return retBool;
 	}
@@ -97,7 +96,7 @@ public class RawRDF4JSelectWrapper extends AbstractWrapper implements IRawSelect
 			Object[] rawRow = new Object[numColumns];
 
 			BindingSet bindSet = tqr.next();
-			for(int colIndex = 0;colIndex < numColumns; colIndex++) {
+			for (int colIndex = 0; colIndex < numColumns; colIndex++) {
 				Value val = bindSet.getValue(rawHeaders[colIndex]);
 				// raw value is the straight return from the binding set
 				rawRow[colIndex] = val;
@@ -107,19 +106,18 @@ public class RawRDF4JSelectWrapper extends AbstractWrapper implements IRawSelect
 
 			return new HeadersDataRow(headers, cleanRow, rawRow);
 		} catch (QueryEvaluationException e) {
-			classLogger.error(Constants.STACKTRACE, e);
+			classLogger.error("Error retrieving next row from the query result set", e);
 		}
 
 		return null;
 	}
-
 
 	private void setVariables() {
 		try {
 			// this makes the assumption that the query is constructed
 			// using the logic within the SPARQL Query Builder
 
-			// get the vars from the tuple result 
+			// get the vars from the tuple result
 			List<String> names = tqr.getBindingNames();
 			numColumns = names.size();
 
@@ -130,19 +128,19 @@ public class RawRDF4JSelectWrapper extends AbstractWrapper implements IRawSelect
 			rawHeaders = names.toArray(new String[names.size()]);
 
 			headers = new String[numColumns];
-			for(int colIndex = 0; colIndex < numColumns; colIndex++){
-				// for the display, if we encounter a "__", we want to 
+			for (int colIndex = 0; colIndex < numColumns; colIndex++) {
+				// for the display, if we encounter a "__", we want to
 				// split and get the second part of the string
 				// that is the display for the column
 				String columnLabel = names.get(colIndex);
-				if(columnLabel.contains("__")){
+				if (columnLabel.contains("__")) {
 					String[] splitColAndTable = columnLabel.split("__");
 					columnLabel = splitColAndTable[1];
 				}
 				headers[colIndex] = columnLabel;
 			}
 		} catch (QueryEvaluationException e) {
-			classLogger.error(Constants.STACKTRACE, e);
+			classLogger.error("Error reading binding names to build result headers", e);
 		}
 
 	}
@@ -152,82 +150,79 @@ public class RawRDF4JSelectWrapper extends AbstractWrapper implements IRawSelect
 		return headers;
 	}
 
-	private Object getRealValue(Object val){
-		if(val == null) {
+	private Object getRealValue(Object val) {
+		if (val == null) {
 			return null;
 		}
 		try {
-			if(val instanceof Literal) {
+			if (val instanceof Literal) {
 				// use datatype if present to determine the type
 				Literal lVal = (Literal) val;
 				IRI lValDataType = lVal.getDatatype();
-				if(lValDataType == null) {
+				if (lValDataType == null) {
 					return lVal.getLabel();
 				}
-				
+
 				// if string, return string
-				if(QueryEvaluationUtil.isStringLiteral(lVal)){
+				if (QueryEvaluationUtil.isStringLiteral(lVal)) {
 					return lVal.getLabel();
 				}
 				// if int
-				else if(lValDataType.getLocalName().equalsIgnoreCase("integer")) {
-					return new Integer(lVal.intValue());
+				else if (lValDataType.getLocalName().equalsIgnoreCase("integer")) {
+					return Integer.valueOf(lVal.intValue());
 				}
 				// if double
-				else if(lValDataType.getLocalName().equalsIgnoreCase("double")) {
-					return new Double(lVal.doubleValue());
+				else if (lValDataType.getLocalName().equalsIgnoreCase("double")) {
+					return Double.valueOf(lVal.doubleValue());
 				}
 				// if boolean
-				else if(lValDataType.getLocalName().equalsIgnoreCase("boolean")) {
+				else if (lValDataType.getLocalName().equalsIgnoreCase("boolean")) {
 					return lVal.booleanValue();
 				}
 				// if datetime
-				else if(lValDataType.getLocalName().equalsIgnoreCase("dateTime")) {
+				else if (lValDataType.getLocalName().equalsIgnoreCase("dateTime")) {
 					try {
 						LocalDateTime ldt = LocalDateTime.parse(lVal.calendarValue().toString(), DATE_FORMATTER);
 						Date d = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
-						SemossDate date = new SemossDate(d, "yyyy-MM-dd HH:mm:ss", ZoneId.of(Utility.getApplicationZoneId()));
+						SemossDate date = new SemossDate(d, "yyyy-MM-dd HH:mm:ss",
+								ZoneId.of(Utility.getApplicationZoneId()));
 						return date;
 					} catch (Exception e) {
-						classLogger.error(Constants.STACKTRACE, e);
+						classLogger.error("Error parsing dateTime value from query result", e);
 					}
 					return null;
-				} 
+				}
 				// if date
-				else if(lValDataType.getLocalName().equalsIgnoreCase("date")) {
+				else if (lValDataType.getLocalName().equalsIgnoreCase("date")) {
 					XMLGregorianCalendar gCalendar = lVal.calendarValue();
 					SemossDate date = new SemossDate(gCalendar.toGregorianCalendar().getTime(), "yyyy-MM-dd");
 					return date;
-				} 
+				}
 				// if float
-				else if(lValDataType.getLocalName().equalsIgnoreCase("float")) {
-					return new Double(lVal.floatValue());
+				else if (lValDataType.getLocalName().equalsIgnoreCase("float")) {
+					return Double.valueOf(lVal.floatValue());
+				} else {
+					classLogger.warn("Unknown data type returned from query = {}", lValDataType);
 				}
-				else  {
-					classLogger.warn("Unknown data type returned from query = " + lValDataType);
-					classLogger.warn("Unknown data type returned from query = " + lValDataType);
-					classLogger.warn("Unknown data type returned from query = " + lValDataType);
-					classLogger.warn("Unknown data type returned from query = " + lValDataType);
-				}
-			} 
+			}
 			// why would i get a jena here?? we are sesame wrapper..
-			else if( val instanceof org.apache.jena.rdf.model.Literal) {
-				classLogger.debug("Class is " + val.getClass());
-				return new Double( ((org.apache.jena.rdf.model.Literal)val).getDouble() );
+			else if (val instanceof org.apache.jena.rdf.model.Literal) {
+				classLogger.debug("Class is {}", val.getClass());
+				return Double.valueOf(((org.apache.jena.rdf.model.Literal) val).getDouble());
 			}
 
 			String value = val + "";
 			return Utility.getInstanceName(value);
-		} catch(RuntimeException ex) {
+		} catch (RuntimeException ex) {
 			classLogger.debug(ex);
 		}
-		
+
 		return val;
 	}
 
 	@Override
 	public SemossDataType[] getTypes() {
-		if(this.types == null) {
+		if (this.types == null) {
 			try {
 				SPARQLParser parser = new SPARQLParser();
 				ParsedQuery parsedQuery = parser.parseQuery(query, null);
@@ -237,17 +232,18 @@ public class RawRDF4JSelectWrapper extends AbstractWrapper implements IRawSelect
 				Set<String> aggregationValues = aggregationVisitor.getValue();
 
 				this.types = new SemossDataType[this.numColumns];
-				for(int i = 0; i < this.numColumns; i++) {
-					if(aggregationValues.contains(this.rawHeaders[i])) {
+				for (int i = 0; i < this.numColumns; i++) {
+					if (aggregationValues.contains(this.rawHeaders[i])) {
 						this.types[i] = SemossDataType.DOUBLE;
 					} else {
 						this.types[i] = SemossDataType.STRING;
 					}
 				}
 			} catch (Exception e) {
-				classLogger.error(Constants.STACKTRACE, e);
+				classLogger.error(
+						"Error parsing query to determine column data types; defaulting all columns to STRING", e);
 				this.types = new SemossDataType[this.numColumns];
-				for(int i = 0; i < this.numColumns; i++) {
+				for (int i = 0; i < this.numColumns; i++) {
 					this.types[i] = SemossDataType.STRING;
 				}
 			}
@@ -257,11 +253,11 @@ public class RawRDF4JSelectWrapper extends AbstractWrapper implements IRawSelect
 
 	@Override
 	public void close() throws IOException {
-		if(this.tqr != null) {
+		if (this.tqr != null) {
 			try {
 				tqr.close();
 			} catch (QueryEvaluationException e) {
-				classLogger.error(Constants.STACKTRACE, e);
+				classLogger.error("Error closing the query result set", e);
 				throw new IOException(e);
 			}
 		}
@@ -269,43 +265,43 @@ public class RawRDF4JSelectWrapper extends AbstractWrapper implements IRawSelect
 
 	@Override
 	public long getNumRows() throws Exception {
-		if(this.numRows == 0) {
+		if (this.numRows == 0) {
 			User user = ThreadStore.getUser();
 			UserQueryTrackingThread queryT = new UserQueryTrackingThread(user, this.engine.getEngineId());
-			
+
 			String query = "select (count(*) as ?count) where { " + this.query + "}";
 			TupleQueryResult tqr = null;
 			try {
 				queryT.setQuery(query);
 				queryT.setStartTimeNow();
-				
+
 				tqr = (TupleQueryResult) engine.execQuery(query);
 				queryT.setEndTimeNow();
-				if(tqr.hasNext()) {
+				if (tqr.hasNext()) {
 					BindingSet bindSet = tqr.next();
 					Object val = bindSet.getValue("count");
 					Object cleanValue = getRealValue(val);
-					if(cleanValue instanceof Number) {
+					if (cleanValue instanceof Number) {
 						this.numRows = ((Number) cleanValue).longValue();
 					}
 				}
 			} catch (QueryEvaluationException e) {
 				queryT.setFailed();
-				classLogger.error(Constants.STACKTRACE, e);
+				classLogger.error("Error executing count query to determine number of rows", e);
 			} finally {
 				try {
 					tqr.close();
 				} catch (QueryEvaluationException e) {
-					classLogger.error(Constants.STACKTRACE, e);
+					classLogger.error("Error closing the count query result set", e);
 				}
-				
-				new Thread(queryT).start();
+
+				Thread.ofVirtual().start(queryT);
 			}
 		}
 
 		return this.numRows;
 	}
-	
+
 	@Override
 	public long getNumRecords() throws Exception {
 		return getNumRows() * getHeaders().length;
@@ -316,12 +312,12 @@ public class RawRDF4JSelectWrapper extends AbstractWrapper implements IRawSelect
 		close();
 		execute();
 	}
-	
+
 	@Override
 	public boolean flushable() {
 		return false;
 	}
-	
+
 	@Override
 	public String flush() {
 		return null;
