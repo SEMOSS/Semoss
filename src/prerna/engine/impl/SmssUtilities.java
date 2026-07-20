84bb35b3817b726313df39129ef6f5d01cc36b52
Tejas Lokeshrao <tlokeshrao@deloitte.com>
2026-07-20 14:18:14 -0400

    fix(smss): re-escape credential values when saving smss so keys aren't corrupted

--- src/prerna/engine/impl/SmssUtilities.java
+++ src/prerna/engine/impl/SmssUtilities.java
@@ -38,6 +38,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.commons.text.StringEscapeUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -904,7 +905,9 @@ public class SmssUtilities {
 						if (allUpperCurrentSmss.get(key) != null) {
 							value = allUpperCurrentSmss.get(key);
 						}
-						constructedSmssContent.append(key).append("\t").append(value);
+						// re-escape: value was de-escaped by Properties.load, so a raw write corrupts \n in keys
+						constructedSmssContent.append(key).append("\t")
+								.append(StringEscapeUtils.escapeJava(value.toString()));
 					} else {
 						// the value has been changed
 						constructedSmssContent.append(curLine);
